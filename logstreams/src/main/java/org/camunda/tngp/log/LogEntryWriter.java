e004e9d28ddcd46a2f8962b38aa6c3b09dea46b7
Daniel Meyer <daniel.meyer@camunda.com>
2016-06-13 07:05:43 +0200

    LogEntryWriter: utility to write log entries

---
+++ logstreams/src/main/java/org/camunda/tngp/log/LogEntryWriter.java
@@ -0,0 +1,38 @@
+package org.camunda.tngp.log;
+
+import org.camunda.tngp.dispatcher.ClaimedFragment;
+import org.camunda.tngp.dispatcher.Dispatcher;
+import org.camunda.tngp.dispatcher.FragmentWriter;
+import org.camunda.tngp.dispatcher.impl.log.DataFrameDescriptor;
+
+/**
+ * Used to write a log entry. Reusable but not thradsafe.
+ *
+ */
+public class LogEntryWriter
+{
+    protected ClaimedFragment claimedFragment = new ClaimedFragment();
+
+    public long write(final Log log, final FragmentWriter writer)
+    {
+        final Dispatcher writeBuffer = log.getWriteBuffer();
+        final int length = writer.getLength();
+
+        long claimedOffset = -1;
+
+        do
+        {
+            claimedOffset = writeBuffer.claim(claimedFragment, length, log.getId());
+        }
+        while(claimedOffset == -2);
+
+        if(claimedOffset >= 0)
+        {
+            writer.write(claimedFragment.getBuffer(), claimedFragment.getOffset());
+            claimedFragment.commit();
+            claimedOffset -= DataFrameDescriptor.alignedLength(length);
+        }
+
+        return claimedOffset;
+    }
+}
