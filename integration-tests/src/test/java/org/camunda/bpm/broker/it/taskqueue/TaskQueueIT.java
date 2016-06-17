778fe3e92410baafc6d13897eb81530d10bf46fa
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2016-06-17 17:38:37 +0200

    test(integration): task creation, polling, completion
    related to camunda-tngp/client-java#1

---
+++ integration-tests/src/test/java/org/camunda/bpm/broker/it/taskqueue/TaskQueueIT.java
@@ -0,0 +1,61 @@
+package org.camunda.bpm.broker.it.taskqueue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.camunda.bpm.broker.it.ClientRule;
+import org.camunda.bpm.broker.it.EmbeddedBrokerRule;
+import org.camunda.tngp.client.AsyncTaskService;
+import org.camunda.tngp.client.TngpClient;
+import org.camunda.tngp.client.cmd.LockedTask;
+import org.camunda.tngp.client.cmd.LockedTasksBatch;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+
+/**
+ * Tests the entire cycle of task creation, polling and completion as a smoke test for when something gets broken
+ *
+ * @author Lindhauer
+ */
+public class TaskQueueIT
+{
+    public EmbeddedBrokerRule brokerRule = new EmbeddedBrokerRule();
+
+    public ClientRule clientRule = new ClientRule();
+
+    @Rule
+    public RuleChain ruleChain = RuleChain
+        .outerRule(brokerRule)
+        .around(clientRule);
+
+    @Test
+    public void testCycle()
+    {
+        final TngpClient client = clientRule.getClient();
+        final AsyncTaskService taskService = client.getAsyncTaskService();
+
+        Long taskId = taskService.createAsyncTask()
+            .taskQueueId(0)
+            .payload("foo")
+            .taskType("bar")
+            .execute();
+
+        assertThat(taskId).isGreaterThanOrEqualTo(0);
+
+        LockedTasksBatch lockedTasksBatch = taskService.pollAndLockAsyncTasks()
+          .taskQueueId(0)
+          .taskType("bar")
+          .execute();
+
+        assertThat(lockedTasksBatch.getLockedTasks()).hasSize(1);
+
+        LockedTask task = lockedTasksBatch.getLockedTasks().get(0);
+        assertThat(task.getId()).isEqualTo(taskId);
+
+        Long completedTaskId = taskService.completeAsyncTask()
+            .taskId(taskId)
+            .execute();
+
+        assertThat(completedTaskId).isEqualTo(taskId);
+    }
+}
