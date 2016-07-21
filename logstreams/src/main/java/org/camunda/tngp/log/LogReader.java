e7dc78c8295dd2ed634f7723fb589abdb4b716c2
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2016-07-21 16:14:22 +0200

    refactor(log): extract LogReader interface
    related to camunda-tngp/camunda-tngp#29

--- logstreams/src/main/java/org/camunda/tngp/log/LogReader.java
+++ logstreams/src/main/java/org/camunda/tngp/log/LogReader.java
@@ -3,50 +3,18 @@ package org.camunda.tngp.log;
 import org.camunda.tngp.util.buffer.BufferReader;
 
 /**
- * Utitilty class for incrementally reading the log in a sequential fashion.
+ * Utility class for incrementally reading the log in a sequential fashion.
  * Maintains a position which is kept between calls to {@link #read(int)}.
  */
-public class LogReader
+public interface LogReader
 {
-    protected Log log;
 
-    /**
-     * The current position of the reader
-     */
-    protected long position;
+    void setLogAndPosition(Log log, long position);
 
-    protected final LogEntryReader entryReader;
+    void setPosition(long position);
 
-    public LogReader(final Log log, final int readBufferSize)
-    {
-        this.log = log;
-        this.position = log.getInitialPosition();
-        entryReader = new LogEntryReader(readBufferSize);
-    }
+    boolean read(BufferReader reader);
 
-    public void setPosition(long position)
-    {
-        this.position = position;
-    }
-
-    public boolean read(BufferReader reader)
-    {
-        final long nextPosition = entryReader.read(log, position, reader);
-
-        boolean hasNext = false;
-
-        if (nextPosition != -1)
-        {
-            this.position = nextPosition;
-            hasNext = true;
-        }
-
-        return hasNext;
-    }
-
-    public long position()
-    {
-        return position;
-    }
+    long position();
 
 }
---
+++ logstreams/src/main/java/org/camunda/tngp/log/LogReaderImpl.java
@@ -0,0 +1,63 @@
+package org.camunda.tngp.log;
+
+import org.camunda.tngp.util.buffer.BufferReader;
+
+/**
+ * Utility class for incrementally reading the log in a sequential fashion.
+ * Maintains a position which is kept between calls to {@link #read(int)}.
+ */
+public class LogReaderImpl implements LogReader
+{
+    protected Log log;
+
+    /**
+     * The current position of the reader
+     */
+    protected long position;
+
+    protected final LogEntryReader entryReader;
+
+    public LogReaderImpl(final int readBufferSize)
+    {
+        this.entryReader = new LogEntryReader(readBufferSize);
+    }
+
+    public LogReaderImpl(final Log log, final int readBufferSize)
+    {
+        this(readBufferSize);
+        this.log = log;
+        this.position = log.getInitialPosition();
+    }
+
+    public void setLogAndPosition(Log log, long position)
+    {
+        this.log = log;
+        this.position = position;
+    }
+
+    public void setPosition(long position)
+    {
+        this.position = position;
+    }
+
+    public boolean read(BufferReader reader)
+    {
+        final long nextPosition = entryReader.read(log, position, reader);
+
+        boolean hasNext = false;
+
+        if (nextPosition != -1)
+        {
+            this.position = nextPosition;
+            hasNext = true;
+        }
+
+        return hasNext;
+    }
+
+    public long position()
+    {
+        return position;
+    }
+
+}
