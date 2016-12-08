4c1c06e4378c7ca6c57a95ee75dabd5d2c0338dc
Mariusz Sielski <mariusz.sielski@camunda.com>
2016-12-08 09:53:58 +0100

    add karma configuration

---
+++ client/karma.conf.js
@@ -0,0 +1,76 @@
+// Karma configuration
+// Generated on Fri Nov 25 2016 10:59:41 GMT+0100 (CET)
+
+var webpackConf = require('./webpack-test.config');
+
+module.exports = function(config) {
+  var configuration = {
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['mocha'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*.test.js'
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'test/**/*.test.js': ['webpack']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+    webpack: webpackConf,
+    webpackMiddleware: {
+      // webpack-dev-middleware configuration
+      // i. e.
+      stats: 'errors-only'
+    }
+  };
+
+  config.set(configuration);
+};
---
+++ client/webpack-test.config.js
@@ -0,0 +1,45 @@
+var config = require('./webpack.config');
+var path = require('path');
+
+deleteUnneededConfigEntries();
+addTestRoot();
+addBabelRewirePlugin();
+addTestFilesBabelLoader();
+
+module.exports = config;
+
+function deleteUnneededConfigEntries() {
+  delete config.entry;
+  delete config.output;
+  delete config.plugins;
+}
+
+function addTestRoot() {
+  config.resolve.modules.push(
+    path.resolve(__dirname, 'test')
+  );
+}
+
+function addBabelRewirePlugin() {
+  config.module.loaders
+    .filter(function(loaderConf){
+      return loaderConf.loader === 'babel-loader';
+    })
+    .forEach(function(loader) {
+      loader.query.plugins.push('babel-plugin-rewire');
+    });
+}
+
+function addTestFilesBabelLoader() {
+  config.module.loaders.push({
+    test: /\.js$/,
+    include: [
+      path.resolve(__dirname, 'test'),
+    ],
+    loader: 'babel-loader',
+    query: {
+      presets: ['latest'],
+      plugins: ['transform-object-rest-spread']
+    }
+  });
+}
