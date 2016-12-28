2a03fb93dc6bd5b7cf801ecac513e6151c63f386
Mariusz Sielski <sielakos@gmail.com>
2016-12-28 18:25:09 +0100

    improve: better styles loading

--- client/app/app.js
+++ client/app/app.js
@@ -2,10 +2,14 @@ import {addSplash} from './splash';
 
 const removeSplash = addSplash();
 
-require.ensure(['lodash.isequal', 'redux', 'babel-polyfill', './init', './styles.scss'], () => {
+require.ensure(['lodash.isequal', 'redux', 'babel-polyfill', './init'], () => {
   removeSplash();
 
   require('./init');
+  require('babel-polyfill');
+});
+
+require.ensure(['./styles.scss'], () => {
   require('./styles.scss');
 });
 
