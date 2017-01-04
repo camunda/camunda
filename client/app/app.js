48b4915b730f7cebee11b6df76efe2b87ba921b8
Mariusz Sielski <mariusz.sielski@camunda.com>
2017-01-04 08:18:46 +0100

    improve: hide splash only when all resources are loaded

--- client/app/app.js
+++ client/app/app.js
@@ -3,15 +3,17 @@ import {addSplash} from './splash';
 const removeSplash = addSplash();
 
 require.ensure(['lodash.isequal', 'redux', './init'], () => {
-  removeSplash();
-
   if (isPolyfillNeeded()) {
     require.ensure(['babel-polyfill'], () => {
+      removeSplash();
+
       require('babel-polyfill');
 
       require('./init');
     });
   } else {
+    removeSplash();
+
     require('./init');
   }
 });
