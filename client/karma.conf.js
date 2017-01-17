3a358b4d37f94234371148a4dea1f4b8c7e9e866
Mariusz Sielski <mariusz.sielski@camunda.com>
2017-01-17 17:44:52 +0100

    chore: add karma to maven
    related to CAM-7299

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -5,21 +5,17 @@ var webpackConf = require('./webpack-test.config');
 
 module.exports = function(config) {
   var configuration = {
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
-
     // list of files / patterns to load in the browser
     files: [
       'test/**/*.test.js'
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -27,34 +23,27 @@ module.exports = function(config) {
       'test/**/*.test.js': ['webpack']
     },
 
-
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
-
+    reporters: ['dots', 'junit'],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -69,6 +58,11 @@ module.exports = function(config) {
       // webpack-dev-middleware configuration
       // i. e.
       stats: 'errors-only'
+    },
+    junitReporter: {
+      outputDir : 'target/surefire-reports/', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'TESTS-TestSuite.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: 'Frontend Tests', // suite will become the package name attribute in xml testsuite element
     }
   };
 
