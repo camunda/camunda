0904ebbe522d412230ef649dce3a2e928a8c3f39
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-01-18 15:02:13 +0100

    test(heatmap): check if return value has expected properties
    related to CAM-7285

---
+++ client/test/main/processDisplay/diagram/heatmap.test.js
@@ -0,0 +1,64 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import generateHeatmap from 'main/processDisplay/diagram/heatmap';
+
+describe('Heatmap', () => {
+  const BOUNDING_BOX = {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100
+  };
+
+  const SAMPLE_ELEMENT = {
+    x: 20,
+    y: 20,
+    width: 50,
+    height: 30,
+    incoming: []
+  };
+
+  const viewer = {
+    get: sinon.stub().returns({
+      getDefaultLayer: sinon.stub().returns({
+        getBBox: sinon.stub().returns(BOUNDING_BOX)
+      }),
+      get: sinon.stub().returns(SAMPLE_ELEMENT)
+    })
+  };
+
+  const heatmapData = {
+    act1: 1,
+    act2: 3
+  };
+
+  let dimensions,
+      img;
+
+  beforeEach(() => {
+    ({dimensions, img} = generateHeatmap(viewer, heatmapData));
+  });
+
+  it('should return an image and a dimensions object', () => {
+    expect(dimensions).to.exist;
+    expect(img).to.exist;
+  });
+
+  describe('dimensions', () => {
+    it('should have height, width and position', () => {
+      const {x, y, width, height} = dimensions;
+
+      expect(typeof x).to.be.eql('number');
+      expect(typeof y).to.be.eql('number');
+      expect(typeof width).to.be.eql('number');
+      expect(typeof height).to.be.eql('number');
+    });
+  });
+
+  describe('image', () => {
+    it('should be a base64 encoded png string', () => {
+      expect(typeof img).to.be.eql('string');
+      expect(img).to.match(/^data:image\/png;base64,/);
+    });
+  });
+});
