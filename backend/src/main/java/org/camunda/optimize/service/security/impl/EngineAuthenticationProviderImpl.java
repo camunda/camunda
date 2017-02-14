ec9f4d331a3487361adab6acc911dbdc61e3111a
Askar Akhmerov <askar.akhmerov@camunda.com>
2017-02-14 15:46:21 +0100

    chor(optimize): add handling of exceptions to engine connection
    related to #OPT-150

--- backend/src/main/java/org/camunda/optimize/service/security/impl/EngineAuthenticationProviderImpl.java
+++ backend/src/main/java/org/camunda/optimize/service/security/impl/EngineAuthenticationProviderImpl.java
@@ -4,6 +4,8 @@ import org.camunda.optimize.dto.engine.AuthenticationResultDto;
 import org.camunda.optimize.dto.optimize.CredentialsDto;
 import org.camunda.optimize.service.security.AuthenticationProvider;
 import org.camunda.optimize.service.util.ConfigurationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,6 +19,7 @@ import javax.ws.rs.core.Response;
  */
 @Component ("engineAuthenticationProvider")
 public class EngineAuthenticationProviderImpl implements AuthenticationProvider {
+  private static final Logger logger = LoggerFactory.getLogger(EngineAuthenticationProviderImpl.class);
 
   @Autowired
   private Client client;
@@ -25,17 +28,21 @@ public class EngineAuthenticationProviderImpl implements AuthenticationProvider 
   private ConfigurationService configurationService;
 
   public boolean authenticate(CredentialsDto credentialsDto) {
-    boolean authenticated;
-    Response response =client
-        .target(configurationService.getEngineRestApiEndpoint())
-        .path(configurationService.getUserValidationEndpoint())
-        .request(MediaType.APPLICATION_JSON)
-        .post(Entity.json(credentialsDto));
-    if (response.getStatus() != 200) {
-      authenticated = false;
-    } else {
-      AuthenticationResultDto responseEntity = response.readEntity(AuthenticationResultDto.class);
-      authenticated = responseEntity.isAuthenticated();
+    boolean authenticated = false;
+    try {
+      Response response =client
+          .target(configurationService.getEngineRestApiEndpoint())
+          .path(configurationService.getUserValidationEndpoint())
+          .request(MediaType.APPLICATION_JSON)
+          .post(Entity.json(credentialsDto));
+
+      if (response.getStatus() == 200) {
+        AuthenticationResultDto responseEntity = response.readEntity(AuthenticationResultDto.class);
+        authenticated = responseEntity.isAuthenticated();
+      }
+
+    } catch (Exception e) {
+      logger.warn("Connection to engine cannot be established", e);
     }
 
     return authenticated;
