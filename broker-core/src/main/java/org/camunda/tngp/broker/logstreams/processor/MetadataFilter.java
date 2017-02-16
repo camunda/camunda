cd543d98c73a79f21233d090fc4e48c08b9d84f9
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2017-02-16 15:56:58 +0100

    improve(broker): javadoc
    related to camunda-tngp/logstreams#51

--- broker-core/src/main/java/org/camunda/tngp/broker/logstreams/processor/MetadataFilter.java
+++ broker-core/src/main/java/org/camunda/tngp/broker/logstreams/processor/MetadataFilter.java
@@ -3,10 +3,20 @@ package org.camunda.tngp.broker.logstreams.processor;
 import java.util.Objects;
 
 import org.camunda.tngp.broker.logstreams.BrokerEventMetadata;
+import org.camunda.tngp.logstreams.processor.StreamProcessor;
 
+/**
+ * Implement to control which events should be handled by a {@link StreamProcessor} based on
+ * the event's metadata.
+ */
 @FunctionalInterface
 public interface MetadataFilter
 {
+    /**
+     * @param metadata the metadata of the event to be processed next
+     * @return true to mark the event for processing; false to skip it
+     * @throws RuntimeException to signal that processing cannot continue
+     */
     boolean applies(BrokerEventMetadata metadata);
 
     default MetadataFilter and(MetadataFilter other)
