6b52337aa957c9c707f1e3f0f24325e2cb8dae5e
Johannes Heinemann <johannes.heinemann@camunda.com>
2017-02-23 11:09:35 +0100

    chore(import): refactor EngineEntityFetcher
    related to #OPT-137

--- backend/src/main/java/org/camunda/optimize/service/importing/EngineEntityFetcher.java
+++ backend/src/main/java/org/camunda/optimize/service/importing/EngineEntityFetcher.java
@@ -4,6 +4,8 @@ import org.camunda.optimize.dto.engine.HistoricActivityInstanceEngineDto;
 import org.camunda.optimize.dto.engine.ProcessDefinitionEngineDto;
 import org.camunda.optimize.dto.engine.ProcessDefinitionXmlEngineDto;
 import org.camunda.optimize.service.util.ConfigurationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +13,21 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import static org.camunda.optimize.service.util.EngineConstantsUtil.INDEX_OF_FIRST_RESULT;
+import static org.camunda.optimize.service.util.EngineConstantsUtil.MAX_RESULTS_TO_RETURN;
+import static org.camunda.optimize.service.util.EngineConstantsUtil.SORT_BY;
+import static org.camunda.optimize.service.util.EngineConstantsUtil.SORT_ORDER;
+import static org.camunda.optimize.service.util.EngineConstantsUtil.SORT_ORDER_TYPE_ASCENDING;
+import static org.camunda.optimize.service.util.EngineConstantsUtil.SORT_TYPE_ID;
+import static org.camunda.optimize.service.util.EngineConstantsUtil.SORT_TYPE_START_TIME;
 
 @Component
 public class EngineEntityFetcher {
+
+  private Logger logger = LoggerFactory.getLogger(EngineEntityFetcher.class);
 
   @Autowired
   private ConfigurationService configurationService;
@@ -23,16 +36,22 @@ public class EngineEntityFetcher {
   private Client client;
 
   public List<HistoricActivityInstanceEngineDto> fetchHistoricActivityInstances(int indexOfFirstResult, int maxPageSize) {
-    List<HistoricActivityInstanceEngineDto> entries = client
-      .target(configurationService.getEngineRestApiEndpointOfCustomEngine())
-      .path(configurationService.getHistoricActivityInstanceEndpoint())
-      .queryParam("sortBy", "startTime")
-      .queryParam("sortOrder", "asc")
-      .queryParam("firstResult", indexOfFirstResult)
-      .queryParam("maxResults", maxPageSize)
-      .request(MediaType.APPLICATION_JSON)
-      .get(new GenericType<List<HistoricActivityInstanceEngineDto>>() {
-      });
+    List<HistoricActivityInstanceEngineDto> entries;
+    try {
+      entries = client
+        .target(configurationService.getEngineRestApiEndpointOfCustomEngine())
+        .path(configurationService.getHistoricActivityInstanceEndpoint())
+        .queryParam(SORT_BY, SORT_TYPE_START_TIME)
+        .queryParam(SORT_ORDER, SORT_ORDER_TYPE_ASCENDING)
+        .queryParam(INDEX_OF_FIRST_RESULT, indexOfFirstResult)
+        .queryParam(MAX_RESULTS_TO_RETURN, maxPageSize)
+        .request(MediaType.APPLICATION_JSON)
+        .get(new GenericType<List<HistoricActivityInstanceEngineDto>>() {
+        });
+    } catch (RuntimeException e) {
+      logger.error("Could not fetch historic activity instances from engine. Please check the connection!");
+      entries = Collections.emptyList();
+    }
 
     return entries;
   }
@@ -43,32 +62,42 @@ public class EngineEntityFetcher {
   }
 
   private List<ProcessDefinitionXmlEngineDto> fetchAllXmls(List<ProcessDefinitionEngineDto> entries) {
-
-    List<ProcessDefinitionXmlEngineDto> xmls = new ArrayList<>(entries.size());
-    for (ProcessDefinitionEngineDto engineDto : entries) {
-      ProcessDefinitionXmlEngineDto xml = client
-        .target(configurationService.getEngineRestApiEndpointOfCustomEngine())
-        .path(configurationService.getProcessDefinitionXmlEndpoint(engineDto.getId()))
-        .request(MediaType.APPLICATION_JSON)
-        .get(new GenericType<ProcessDefinitionXmlEngineDto>() {
-        });
-      xmls.add(xml);
+    List<ProcessDefinitionXmlEngineDto> xmls;
+    try {
+      xmls = new ArrayList<>(entries.size());
+      for (ProcessDefinitionEngineDto engineDto : entries) {
+        ProcessDefinitionXmlEngineDto xml = client
+          .target(configurationService.getEngineRestApiEndpointOfCustomEngine())
+          .path(configurationService.getProcessDefinitionXmlEndpoint(engineDto.getId()))
+          .request(MediaType.APPLICATION_JSON)
+          .get(ProcessDefinitionXmlEngineDto.class);
+        xmls.add(xml);
+      }
+    } catch (RuntimeException e) {
+      logger.error("Could not fetch process definition xmls from engine. Please check the connection!");
+      xmls = Collections.emptyList();
     }
 
     return xmls;
   }
 
   public List<ProcessDefinitionEngineDto> fetchProcessDefinitions(int indexOfFirstResult, int maxPageSize) {
-    List<ProcessDefinitionEngineDto> entries = client
-      .target(configurationService.getEngineRestApiEndpointOfCustomEngine())
-      .path(configurationService.getProcessDefinitionEndpoint())
-      .queryParam("firstResult", indexOfFirstResult)
-      .queryParam("maxResults", maxPageSize)
-      .queryParam("sortBy", "id")
-      .queryParam("sortOrder", "asc")
-      .request(MediaType.APPLICATION_JSON)
-      .get(new GenericType<List<ProcessDefinitionEngineDto>>() {
-      });
+    List<ProcessDefinitionEngineDto> entries;
+    try {
+      entries = client
+        .target(configurationService.getEngineRestApiEndpointOfCustomEngine())
+        .path(configurationService.getProcessDefinitionEndpoint())
+        .queryParam(INDEX_OF_FIRST_RESULT, indexOfFirstResult)
+        .queryParam(MAX_RESULTS_TO_RETURN, maxPageSize)
+        .queryParam(SORT_BY, SORT_TYPE_ID)
+        .queryParam(SORT_ORDER, SORT_ORDER_TYPE_ASCENDING)
+        .request(MediaType.APPLICATION_JSON)
+        .get(new GenericType<List<ProcessDefinitionEngineDto>>() {
+        });
+    } catch (RuntimeException e) {
+      logger.error("Could not fetch process definitions from engine. Please check the connection!");
+      entries = Collections.emptyList();
+    }
 
     return entries;
   }
---
+++ backend/src/main/java/org/camunda/optimize/service/util/EngineConstantsUtil.java
@@ -0,0 +1,15 @@
+package org.camunda.optimize.service.util;
+
+public class EngineConstantsUtil {
+
+  public static final String MAX_RESULTS_TO_RETURN = "maxResults";
+  public static final String INDEX_OF_FIRST_RESULT = "firstResult";
+
+  public static final String SORT_BY = "sortBy";
+  public static final String SORT_TYPE_START_TIME = "startTime";
+  public static final String SORT_TYPE_ID = "id";
+
+  public static final String SORT_ORDER = "sortOrder";
+  public static final String SORT_ORDER_TYPE_ASCENDING = "asc";
+
+}
