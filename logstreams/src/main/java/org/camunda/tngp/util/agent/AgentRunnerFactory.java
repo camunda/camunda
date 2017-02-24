136882f21a63bc0c79539c326b0617223f97522e
Philipp Ossler <philipp.ossler@camunda.com>
2017-02-24 09:25:41 +0100

    chore(agent): move agent runner service into util module
    related to camunda-tngp/util#5

--- logstreams/src/main/java/org/camunda/tngp/util/agent/AgentRunnerFactory.java
+++
@@ -1,9 +0,0 @@
-package org.camunda.tngp.util.agent;
-
-import org.agrona.concurrent.Agent;
-import org.agrona.concurrent.AgentRunner;
-
-public interface AgentRunnerFactory
-{
-    AgentRunner createAgentRunner(Agent agent);
-}
