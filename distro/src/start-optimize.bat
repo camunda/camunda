7b973ea7cb2a5a51fbf6936f0390223c1d0221e6
Johannes Heinemann <johannes.heinemann@camunda.com>
2017-03-10 09:35:03 +0100

    feat(setup): can start distro on windows using start-optimize.bat
    related to #OPT-98

--- distro/src/start-optimize.bat
+++ distro/src/start-optimize.bat
@@ -1,7 +1,43 @@
 @echo off
 
-echo "starting Camunda Optimize ${project.version} with Elasticsearch ${elasticsearch.version}"
+echo Starting Camunda Optimize ${project.version} with Elasticsearch ${elasticsearch.version}
 
-cd server\elasticsearch-${elasticsearch.version}\bin\
-start elasticsearch.bat
+echo Setting up environment variables...
+
+set PROGRAM=optimize
+set BASEDIR=%~dp0
+cd %BASEDIR%
+
+if not exist ".\log" mkdir log
+if not exist ".\run" mkdir run
+
+set LOG_FILE=%BASEDIR%log\%PROGRAM%.log
+
+set RETRIES=5
+set SLEEP_TIME=10
+
+set COMMAND=curl.exe -XGET http://localhost:9200/_cluster/health?wait_for_status=yellow^^^&timeout=10s
+
+echo Environment is set up.
+
+echo Starting Elasticsearch...
+start %BASEDIR%\server\elasticsearch-${elasticsearch.version}\bin\elasticsearch.bat
+
+:while1
+:: query elasticsearch if it's up
+%COMMAND% >nul 2>&1
+:: if there was an error wait and retry
+if %ERRORLEVEL% neq 0 (
+    echo Polling elasticsearch ... %RETRIES% retries left
+	timeout /t %SLEEP_TIME% /nobreak >nul 
+    set /a RETRIES-=1
+    if %RETRIES% leq 0 (
+        echo Error: Elasticsearch did not start!
+        exit /b
+    )
+    goto :while1
+)
+echo Elasticsearch has successfully been started.
+echo Starting jetty...
+java -jar %BASEDIR%optimize-backend-${project.version}.jar > %LOG_FILE% 2>&1
  
