69d88a308d53acbd43438e0f257cae4af7146682
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2017-03-16 12:01:21 +0100

    feat(test-util): map and map factory builder
    related to camunda-tngp/camunda-tngp#181

---
+++ test-util/src/main/java/org/camunda/tngp/test/util/collection/MapFactoryBuilder.java
@@ -0,0 +1,52 @@
+package org.camunda.tngp.test.util.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class MapFactoryBuilder<A, T>
+{
+    protected T returnValue;
+    protected Consumer<Function<A, Map<String, Object>>> factoryCallback;
+
+    protected BiConsumer<A, Map<String, Object>> manipulationChain = (a, m) ->
+    { };
+
+    public MapFactoryBuilder(T returnValue, Consumer<Function<A, Map<String, Object>>> factoryCallback)
+    {
+        this.returnValue = returnValue;
+        this.factoryCallback = factoryCallback;
+    }
+
+    public MapFactoryBuilder<A, T> allOf(Function<A, Map<String, Object>> otherMap)
+    {
+        manipulationChain = manipulationChain.andThen((a, m) -> m.putAll(otherMap.apply(a)));
+        return this;
+    }
+
+    public MapFactoryBuilder<A, T> put(String key, Object value)
+    {
+        manipulationChain = manipulationChain.andThen((a, m) -> m.put(key, value));
+        return this;
+    }
+
+    public MapFactoryBuilder<A, T> put(String key, Function<A, Object> valueFunction)
+    {
+        manipulationChain = manipulationChain.andThen((a, m) -> m.put(key, valueFunction.apply(a)));
+        return this;
+    }
+
+    public T done()
+    {
+        factoryCallback.accept((a) ->
+        {
+            final Map<String, Object> map = new HashMap<>();
+            manipulationChain.accept(a, map);
+            return map;
+        });
+        return returnValue;
+    }
+
+}
