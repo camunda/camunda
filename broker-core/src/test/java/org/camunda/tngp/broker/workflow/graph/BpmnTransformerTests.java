825c2152fe2f5030cad319e8a7e1eeab43a8c001
Philipp Ossler <philipp.ossler@camunda.com>
2017-04-05 13:46:50 +0200

    fix(WF): fix failing workflow test

--- broker-core/src/test/java/org/camunda/tngp/broker/workflow/graph/BpmnTransformerTests.java
+++ broker-core/src/test/java/org/camunda/tngp/broker/workflow/graph/BpmnTransformerTests.java
@@ -11,10 +11,10 @@ import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.camunda.tngp.broker.workflow.graph.model.ExecutableEndEvent;
 import org.camunda.tngp.broker.workflow.graph.model.ExecutableFlowElement;
-import org.camunda.tngp.broker.workflow.graph.model.ExecutableWorkflow;
 import org.camunda.tngp.broker.workflow.graph.model.ExecutableSequenceFlow;
 import org.camunda.tngp.broker.workflow.graph.model.ExecutableServiceTask;
 import org.camunda.tngp.broker.workflow.graph.model.ExecutableStartEvent;
+import org.camunda.tngp.broker.workflow.graph.model.ExecutableWorkflow;
 import org.camunda.tngp.broker.workflow.graph.transformer.BpmnTransformer;
 import org.junit.Test;
 
@@ -37,7 +37,7 @@ public class BpmnTransformerTests
         // then
         final ExecutableFlowElement element = process.findFlowElementById(wrapString("foo"));
         assertThat(element).isInstanceOf(ExecutableStartEvent.class);
-        assertThat(element.getId()).isEqualTo("foo");
+        assertThat(element.getId()).isEqualTo(wrapString("foo"));
         assertThat(element.getName()).isEqualTo("bar");
 
         assertThat(process.getScopeStartEvent()).isEqualTo(element);
@@ -61,9 +61,9 @@ public class BpmnTransformerTests
         assertThat(element).isInstanceOf(ExecutableSequenceFlow.class);
 
         final ExecutableSequenceFlow sequenceFlow = (ExecutableSequenceFlow) element;
-        assertThat(sequenceFlow.getId()).isEqualTo("to");
-        assertThat(sequenceFlow.getSourceNode().getId()).isEqualTo("a");
-        assertThat(sequenceFlow.getTargetNode().getId()).isEqualTo("b");
+        assertThat(sequenceFlow.getId()).isEqualTo(wrapString("to"));
+        assertThat(sequenceFlow.getSourceNode().getId()).isEqualTo(wrapString("a"));
+        assertThat(sequenceFlow.getTargetNode().getId()).isEqualTo(wrapString("b"));
 
         assertThat(sequenceFlow.getSourceNode().getOutgoingSequenceFlows()).hasSize(1).contains(sequenceFlow);
         assertThat(sequenceFlow.getTargetNode().getIncomingSequenceFlows()).hasSize(1).contains(sequenceFlow);
@@ -85,7 +85,7 @@ public class BpmnTransformerTests
         // then
         final ExecutableFlowElement element = process.findFlowElementById(wrapString("foo"));
         assertThat(element).isInstanceOf(ExecutableEndEvent.class);
-        assertThat(element.getId()).isEqualTo("foo");
+        assertThat(element.getId()).isEqualTo(wrapString("foo"));
         assertThat(element.getName()).isEqualTo("bar");
     }
 
@@ -108,7 +108,7 @@ public class BpmnTransformerTests
         assertThat(element).isInstanceOf(ExecutableServiceTask.class);
 
         final ExecutableServiceTask serviceTask = (ExecutableServiceTask) element;
-        assertThat(serviceTask.getId()).isEqualTo("foo");
+        assertThat(serviceTask.getId()).isEqualTo(wrapString("foo"));
         assertThat(serviceTask.getName()).isEqualTo("bar");
 
         assertThat(serviceTask.getTaskMetadata()).isNotNull();
