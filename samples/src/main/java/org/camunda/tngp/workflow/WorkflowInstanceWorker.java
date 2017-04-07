629a1a244a148d22a1a399a82aca6e799bb1a46d
Philipp Ossler <philipp.ossler@camunda.com>
2017-04-07 14:58:14 +0200

    chore(sample): add example client applications

---
+++ samples/src/main/java/org/camunda/tngp/workflow/WorkflowInstanceWorker.java
@@ -0,0 +1,87 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.tngp.workflow;
+
+import java.time.Duration;
+import java.util.Properties;
+import java.util.Scanner;
+
+import org.camunda.tngp.client.ClientProperties;
+import org.camunda.tngp.client.TngpClient;
+import org.camunda.tngp.client.impl.TngpClientImpl;
+import org.camunda.tngp.client.task.TaskSubscription;
+
+public class WorkflowInstanceWorker
+{
+
+    public static void main(String[] args)
+    {
+        final String brokerContactPoint = "127.0.0.1:51015";
+        final int topicId = 0;
+        final String taskType = "foo";
+        final int lockOwner = 24;
+
+        final Properties clientProperties = new Properties();
+        clientProperties.put(ClientProperties.BROKER_CONTACTPOINT, brokerContactPoint);
+
+        final TngpClient tngpClient = new TngpClientImpl(clientProperties);
+
+        System.out.println(String.format("> Connecting to %s ...", brokerContactPoint));
+        tngpClient.connect();
+
+        System.out.println("> Connected.");
+
+        System.out.println(String.format("> Open task subscription for topic '%d' and type '%s'", topicId, taskType));
+
+        final TaskSubscription subscription = tngpClient.taskTopic(topicId)
+            .newTaskSubscription()
+            .taskType(taskType)
+            .lockOwner(lockOwner)
+            .lockTime(Duration.ofSeconds(10))
+            .handler(task ->
+            {
+                System.out.println(String.format(">>> [type: %s, key: %s, lockExpirationTime: %s]\n[headers: %s]\n[payload: %s]\n===",
+                        task.getType(),
+                        task.getKey(),
+                        task.getLockExpirationTime().toString(),
+                        task.getHeaders(),
+                        task.getPayload()));
+
+                task.complete();
+            })
+            .open();
+
+        System.out.println("> Opened.");
+
+        // wait for tasks
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            while (scanner.hasNextLine())
+            {
+                final String nextLine = scanner.nextLine();
+                if (nextLine.contains("exit"))
+                {
+                    System.out.println("> Closing...");
+
+                    subscription.close();
+                    tngpClient.close();
+
+                    System.out.println("> Closed.");
+
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+}
