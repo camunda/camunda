b4b92d093eabd759c2aef623130ac5e1c24eebeb
Mariusz <sielakos@gmail.com>
2017-04-24 08:16:23 +0200

    increase timeout in e2e for ci

--- client/wdio-ci.conf.js
+++ client/wdio-ci.conf.js
@@ -75,7 +75,7 @@ exports.config = {
     baseUrl: 'http://localhost:8090',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 50000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
