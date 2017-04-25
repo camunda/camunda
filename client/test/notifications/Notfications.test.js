96e810b8a1d3be9d3d3d150cc8b060ff636d69fe
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-04-25 13:59:57 +0200

    test(Notification): add test for omission of ":"
    related to OPT-275

--- client/test/notifications/Notfications.test.js
+++ client/test/notifications/Notfications.test.js
@@ -36,4 +36,33 @@ describe('<Notifications>', () => {
     expect(node).to.contain.text('other status');
     expect(node).to.contain.text('other text');
   });
+
+  it('should display a ":" after the status if there is a description text', () => {
+    const state = {
+      notifications: [
+        {
+          status: 'status',
+          text: 'text'
+        }
+      ]
+    };
+
+    update(state);
+
+    expect(node).to.contain.text(':');
+  });
+
+  it('should not display a ":" if there is no description text', () => {
+    const state = {
+      notifications: [
+        {
+          status: 'status'
+        }
+      ]
+    };
+
+    update(state);
+
+    expect(node).to.not.contain.text(':');
+  });
 });
