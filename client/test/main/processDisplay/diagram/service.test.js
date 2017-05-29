25cbdbff4b74853d3fa82e04a20e6d11ba12baf5
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-05-29 13:59:13 +0200

    test(diagram): ensure correct positioning of tooltip
    related to OPT-254

--- client/test/main/processDisplay/diagram/service.test.js
+++ client/test/main/processDisplay/diagram/service.test.js
@@ -9,26 +9,33 @@ describe('Diagram service', () => {
   let addFunction;
   let diagramGraphics;
 
+  let getFunction;
+  let viewboxFunction;
+
   beforeEach(() => {
     addFunction = sinon.spy();
 
     diagramGraphics = document.createElement('div');
     diagramGraphics.innerHTML = '<div class="djs-hit" width="20"></div>';
 
+    getFunction = sinon.stub().returns({
+      x: 0,
+      y: 0
+    });
+
+    viewboxFunction = sinon.stub().returns({
+      x: 0,
+      y: 0,
+      width: 40,
+      height: 30
+    });
+
     viewer = {
       get: sinon.stub().returns({
         getGraphics: sinon.stub().returns(diagramGraphics),
         add: addFunction,
-        get: sinon.stub().returns({
-          x: 0,
-          y: 0
-        }),
-        viewbox: sinon.stub().returns({
-          x: 0,
-          y: 0,
-          width: 40,
-          height: 30
-        })
+        get: getFunction,
+        viewbox: viewboxFunction
       })
     };
   });
@@ -47,5 +54,33 @@ describe('Diagram service', () => {
     const node = addFunction.getCall(0).args[1].html;
 
     expect(node.textContent).to.contain('asdf');
+  });
+
+  it('should position the tooltip on top if there is space available', () => {
+    getFunction.returns({
+      x: 100,
+      y: 100
+    });
+
+    addDiagramTooltip(viewer, diagramElement, 'asdf');
+
+    const node = addFunction.getCall(0).args[1].html;
+
+    expect(node.classList.contains('top')).to.eql(true);
+    expect(node.classList.contains('bottom')).to.eql(false);
+  });
+
+  it('should position the tooltip on top if there is space available', () => {
+    getFunction.returns({
+      x: 100,
+      y: 0
+    });
+
+    addDiagramTooltip(viewer, diagramElement, 'asdf');
+
+    const node = addFunction.getCall(0).args[1].html;
+
+    expect(node.classList.contains('bottom')).to.eql(true);
+    expect(node.classList.contains('top')).to.eql(false);
   });
 });
