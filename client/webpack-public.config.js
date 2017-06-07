d493565e8589919cb12c1214fd4050c5eefd150e
Sebastian Stamm <sebastian.stamm@camunda.com>
2017-06-07 15:16:25 +0200

    fix(build): do not use hardcoded ip in publicpath
    related to OPT-464

--- client/webpack-public.config.js
+++ client/webpack-public.config.js
@@ -1,6 +1,6 @@
 var config = require('./webpack-development.config');
 
 config.devServer.host = '0.0.0.0';
-config.output.publicPath = 'http://192.168.21.120:9000/';
+config.output.publicPath = '/';
 
 module.exports = config;
