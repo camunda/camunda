5c66eae6f0b36c0137988a2250947f0d7b688ba8
Mariusz Sielski <mariusz.sielski@camunda.com>
2017-06-19 12:58:37 +0200

    fix: move url creation to template in StaticLink
    related to OPT-481

--- client/app/router/StaticLink.js
+++ client/app/router/StaticLink.js
@@ -3,9 +3,9 @@ import {getRouter} from './service';
 const router = getRouter();
 
 export function StaticLink({name, params, replace = false}) {
-  const url = router.getUrl(name, params);
+  return node => {
+    const url = router.getUrl(name, params);
 
-  return (node) => {
     node.addEventListener('click', (event) => {
       event.preventDefault();
       router.goTo(name, params, replace);
