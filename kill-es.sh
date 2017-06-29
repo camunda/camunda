56283cbdc8e4d6141ee9ca1c899a57d089a13b4f
Askar Akhmerov <askar.akhmerov@camunda.com>
2017-06-29 11:48:51 +0200

    chor(optimize): adjust kill command
    related to #OPT-494

--- kill-es.sh
+++ kill-es.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-if [ -z $(ps aux | grep -v grep | grep elasticsearch | awk '{ print $2 }') ]
-then
-    kill -9  $(ps aux | grep -v grep | grep elasticsearch | awk '{ print $2 }')
+PID="$(ps aux | grep -v grep | grep elasticsearch | awk '{ print $2 }')"
+if [ -z "$PID" ]
+    then
+    echo "Nothing to kill"
+    else
+    kill -9 $PID
 fi
