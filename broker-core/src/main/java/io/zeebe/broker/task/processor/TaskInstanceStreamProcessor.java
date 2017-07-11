c2392ee2cf915670b77702c9e132a4817821bd73
Daniel Meyer <daniel.meyer@camunda.com>
2017-07-11 09:57:59 +0200

    feat(stream): add prios to streamProcessors
     related to camunda-tngp/zeebe#338

--- broker-core/src/main/java/io/zeebe/broker/task/processor/TaskInstanceStreamProcessor.java
+++ broker-core/src/main/java/io/zeebe/broker/task/processor/TaskInstanceStreamProcessor.java
@@ -42,6 +42,7 @@ import io.zeebe.logstreams.processor.StreamProcessorContext;
 import io.zeebe.logstreams.spi.SnapshotSupport;
 import io.zeebe.protocol.clientapi.EventType;
 import io.zeebe.protocol.clientapi.SubscriptionType;
+import io.zeebe.util.actor.Actor;
 import io.zeebe.util.buffer.BufferUtil;
 
 public class TaskInstanceStreamProcessor implements StreamProcessor
@@ -87,6 +88,12 @@ public class TaskInstanceStreamProcessor implements StreamProcessor
         this.taskSubscriptionManager = taskSubscriptionManager;
 
         this.taskIndex = new TaskInstanceIndex();
+    }
+
+    @Override
+    public int getPriority(long now)
+    {
+        return Actor.PRIORITY_HIGH;
     }
 
     @Override
