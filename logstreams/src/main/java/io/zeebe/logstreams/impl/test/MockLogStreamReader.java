923993b4997b0e8e3011c65367240b5aa9a438b9
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2017-07-19 10:50:44 +0200

    chore(project): move in-memory log stream reader to main package
    - that way we can use it in broker tests
    
    related to camunda-tngp/zeebe#214

--- logstreams/src/main/java/io/zeebe/logstreams/impl/test/MockLogStreamReader.java
+++ logstreams/src/main/java/io/zeebe/logstreams/impl/test/MockLogStreamReader.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.zeebe.logstreams.processor;
+package io.zeebe.logstreams.impl.test;
 
 import java.util.*;
 
