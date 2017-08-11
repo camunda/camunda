4f6b508f34d58ce14e3f874a2b3d5f21acc5cc5e
Philipp Ossler <philipp.ossler@gmail.com>
2017-08-11 13:13:14 +0200

    chore(style): fix code style

--- bpmn-model/src/main/java/io/zeebe/model/bpmn/impl/metadata/TaskHeaderImpl.java
+++ bpmn-model/src/main/java/io/zeebe/model/bpmn/impl/metadata/TaskHeaderImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.zeebe.model.bpmn.impl.metadata;
 
 import javax.xml.bind.annotation.XmlAttribute;
@@ -8,7 +23,7 @@ public class TaskHeaderImpl
 {
     private String key;
     private String value;
-    
+
     @XmlAttribute(name = BpmnConstants.ZEEBE_ATTRIBUTE_TASK_HEADER_KEY)
     public void setKey(String key)
     {
@@ -19,7 +34,7 @@ public class TaskHeaderImpl
     {
         return key;
     }
-    
+
     @XmlAttribute(name = BpmnConstants.ZEEBE_ATTRIBUTE_TASK_HEADER_VALUE)
     public void setValue(String value)
     {
@@ -34,7 +49,7 @@ public class TaskHeaderImpl
     @Override
     public String toString()
     {
-        StringBuilder builder = new StringBuilder();
+        final StringBuilder builder = new StringBuilder();
         builder.append("TaskHeader [key=");
         builder.append(key);
         builder.append(", value=");
@@ -42,5 +57,5 @@ public class TaskHeaderImpl
         builder.append("]");
         return builder.toString();
     }
-    
+
 }
