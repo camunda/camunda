b07992f3cfc279052a2c77bb8632cfab9b18b625
Christian Lipphardt <christian.lipphardt@googlemail.com>
2017-10-11 16:41:13 +0200

    fix(ci/aws): override deployable version for Optimize
    - in the build by grepping for the actual SNAPSHOT version
    
    closes #OPT-605

--- .aws/scripts/provision-camunda.sh
+++ .aws/scripts/provision-camunda.sh
@@ -9,6 +9,7 @@ DB_INSTANCE_ID="camunda-optimize-db-stage"
 DB_NAME=$(awscli rds describe-db-instances --query "DBInstances[?DBInstanceIdentifier=='${DB_INSTANCE_ID}'].DBName" --output text)
 DB_HOST=$(awscli rds describe-db-instances --query "DBInstances[?DBInstanceIdentifier=='${DB_INSTANCE_ID}'].Endpoint.Address" --output text)
 REMOTE_HOST_IP=$(awscli ec2 describe-instances --filters 'Name=tag:Name,Values=Camunda Optimize*' --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+CAMUNDA_OPTIMIZE_VERSION=$(grep 'SNAPSHOT' pom.xml | sed 's/.*<version>\(.*\)<\/version>/\1/')
 
 cd .aws/ansible || exit 1
 
@@ -22,4 +23,5 @@ ansible-playbook bootstrap-python.yml -i hosts
 ansible-playbook camunda.yml \
   -i hosts \
   -e db_name=${DB_NAME} \
-  -e db_host=${DB_HOST}
+  -e db_host=${DB_HOST} \
+  -e camunda_optimize_version="${CAMUNDA_OPTIMIZE_VERSION}"
