2e65afd6b8ed5c9b3461f1b5b5e50eec7302e4d6
Sebastian Menski <sebastian.menski@camunda.com>
2017-10-26 14:19:04 +0200

    chore(): fix compilation

--- gossip/src/main/java/io/zeebe/gossip/Gossip.java
+++ gossip/src/main/java/io/zeebe/gossip/Gossip.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.zeebe.gossip;
 
 import io.zeebe.gossip.broadcasts.BroadcastQueue;
@@ -15,8 +30,6 @@ public class Gossip implements Actor, ServerMessageHandler, ServerRequestHandler
     private final BufferingServerTransport serverTransport;
     private final ClientTransport clientTransport;
 
-    private final ServerSubscriptionController subscriptionController;
-
     private final NodeManager nodeManager;
     private final BroadcastQueue queue;
     private final EventManager eventManager;
@@ -28,8 +41,6 @@ public class Gossip implements Actor, ServerMessageHandler, ServerRequestHandler
         this.serverTransport = serverTransport;
         this.clientTransport = clientTransport;
 
-        subscriptionController = new ServerSubscriptionController("gossip", serverTransport, this, this);
-
         nodeManager = new NodeManager();
         queue = new BroadcastQueue();
         eventManager = new EventManager();
@@ -39,8 +50,6 @@ public class Gossip implements Actor, ServerMessageHandler, ServerRequestHandler
     public int doWork() throws Exception
     {
         int workCount = 0;
-
-        workCount += subscriptionController.doWork();
 
         workCount += nodeManager.doWork();
         workCount += queue.doWork();
