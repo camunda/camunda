747f628e81ab517ee439b1a27bf10767ecdbdec0
Sebastian Menski <sebastian.menski@camunda.com>
2017-11-01 16:18:40 +0100

    chore(docker): allow to configure INITIAL_CONTACT_POINT with env outside of k8s
    - also accept INITIAL_CONTACT_POINT if not running on kubernetes
    - remove configuration files from docker compose to use env variables only
    - allow to set log level over environment in docker-compose file

--- docker/utils/startup.sh
+++ docker/utils/startup.sh
@@ -3,6 +3,8 @@
 cfgHome=/usr/local/zeebe/conf/zeebe.cfg.toml
 cfgLog=/usr/local/zeebe/conf/log4j2.xml
 
+INITIAL_CONTACT_POINT=${INITIAL_CONTACT_POINT:-}
+
 if [[ "$DEPLOY_ON_KUBERNETES" == "true" ]]; then
 
     DNSNAME="${HOST}.${DNS}"
@@ -18,7 +20,12 @@ if [[ "$DEPLOY_ON_KUBERNETES" == "true" ]]; then
 else
 
     sed -i "s/@DNSNAME@/$(hostname -i)/g" $cfgHome
-    sed -i "s/@INITIAL_CONTACT_POINT@//g" $cfgHome
+
+    if [ -n "${INITIAL_CONTACT_POINT}" ]; then
+        sed -i "s/@INITIAL_CONTACT_POINT@/initialContactPoints = \[\n\t\"${INITIAL_CONTACT_POINT}\"\n\]/g" $cfgHome
+    else
+        sed -i "s/@INITIAL_CONTACT_POINT@//g" $cfgHome
+    fi
 
 fi
 
