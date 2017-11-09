de8d254b7dc096a6c7eea728ede186fa1c70fa01
Franz Heidl <franz@franzheidl.de>
2017-11-09 13:21:55 +0100

    Improve: misc minor UI improvements
    related to #OPT-662
    
    Improve: minor footer fixes
    
    Improve system font stack further
    
    related to #OPT-662

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer({version}) {
   return (
     <footer className='Footer'>
-      © Camunda services GmbH 2017, All Rights Reserved {version && `/ ${version}`}
+      © Camunda Services GmbH 2017, All Rights Reserved. | {version && `${version}`}
     </footer>
   );
 }
