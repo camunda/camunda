9f48cdb8af38dfbf1185d0d94b016b786755e264
Franz Heidl <franz@franzheidl.de>
2017-11-09 13:24:01 +0100

    Improve: add tests for Message component
    reralted to OPT-667

---
+++ client/src/components/Message/Message.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import Message from './Message';
+
+
+it('renders without crashing', () => {
+  shallow(<Message />);
+});
+
+it('renders the message text provided as a property', () => {
+  const text = 'This is a Message!';
+
+  const node = mount(<Message message={text} />);
+  expect(node).toIncludeText(text);
+});
+
+it('renders the class name as provided as a property', () => {
+  const type = 'test';
+
+  const node = mount(<Message type={type} />);
+  expect((node).find('.Message')).toHaveClassName('Message--test');
+});
