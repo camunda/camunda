1bbac5cff6ba3db32dbe95257e5de2433814f65a
Franz Heidl <franz@franzheidl.de>
2017-11-13 17:21:24 +0100

    Improve: Add tests for Dropdown WAI-ARIA attributes
    related to OPT-696

--- client/src/modules/components/Dropdown/Dropdown.test.js
+++ client/src/modules/components/Dropdown/Dropdown.test.js
@@ -52,3 +52,43 @@ it('should close when selecting an option', () => {
   expect(node.state('open')).toBe(false);
   expect(node.find('.Dropdown')).not.toMatchSelector('.is-open');
 });
+
+it('should set aria-haspop to true', () => {
+  const node = mount(<Dropdown label='Click me'>
+    <Dropdown.Option>foo</Dropdown.Option>
+  </Dropdown>);
+
+  expect(node.find('.Dropdown__button')).toMatchSelector('.Dropdown__button[aria-haspopup="true"]');
+})
+
+it('should set aria-expanded to false by default', () => {
+  const node = mount(<Dropdown label='Click me'>
+    <Dropdown.Option>foo</Dropdown.Option>
+  </Dropdown>);
+
+  expect(node.find('.Dropdown__button')).toMatchSelector('.Dropdown__button[aria-expanded="false"]');
+});
+
+it('should set aria-expanded to true when open', () => {
+  const node = mount(<Dropdown label='Click me'>
+    <Dropdown.Option>foo</Dropdown.Option>
+  </Dropdown>);
+
+  node.simulate('click');
+
+  expect(node.state('open')).toBe(true);
+  expect(node.find('.Dropdown__button')).toMatchSelector('.Dropdown__button[aria-expanded="true"]');
+})
+
+it('should set aria-expanded to false when closed', () => {
+  const node = mount(<Dropdown label='Click me'>
+    <Dropdown.Option>foo</Dropdown.Option>
+  </Dropdown>);
+
+  node.setState({open: true});
+
+  node.simulate('click');
+
+  expect(node.state('open')).toBe(false);
+  expect(node.find('.Dropdown__button')).toMatchSelector('.Dropdown__button[aria-expanded="false"]');
+})
