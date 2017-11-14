c655165711ff5ca62a085da280f1cae24797db0b
Franz Heidl <franz@franzheidl.de>
2017-11-14 16:34:44 +0100

    improve (Input) simplify setting type attr
    related to OPT-699

--- client/src/modules/components/Input/Input.js
+++ client/src/modules/components/Input/Input.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Input.css';
 
 export default function Input(props) {
-  return (<input type={props.type || 'text'} {...props} className={'Input ' + props.className} >
+  return (<input type='text' {...props} className={'Input ' + props.className} >
     {props.children}
   </input>);
 }
