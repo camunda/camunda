37a36c27020866e9ae2162fe481a2661c5d52a54
Franz Heidl <franz@franzheidl.de>
2017-12-01 14:11:23 +0100

    fix(Input) prevent class=“Input undefined” output
    related to OPT-802

--- client/src/modules/components/Input/Input.js
+++ client/src/modules/components/Input/Input.js
@@ -4,7 +4,7 @@ import './Input.css';
 
 export default class Input extends React.PureComponent {
   render() {
-    return (<input type='text' {...this.props} className={'Input ' + this.props.className} >
+    return (<input type='text' {...this.props} className={'Input' + (this.props.className ? ' ' + this.props.className : '')} >
       {this.props.children}
     </input>);
   }
