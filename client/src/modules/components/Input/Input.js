091d134a4659fa1e825e87efac8ec6c597951cf5
Kyrylo Zakurdaiev <kirill.zakurdaev@gmail.com>
2017-12-05 16:48:10 +0100

    chore(Input component): implement input component as a function
    related to OPT-769

--- client/src/modules/components/Input/Input.js
+++ client/src/modules/components/Input/Input.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import './Input.css';
 
-export default class Input extends React.PureComponent {
-  render() {
-    let allowedProps = {...this.props};
-    delete allowedProps.reference;
+export default function Input(props) {
+  const allowedProps = {...props};
+  delete allowedProps.reference;
 
-    return (<input type='text' {...allowedProps} className={'Input' + (this.props.className ? ' ' + this.props.className : '')} ref={this.props.reference}>
-      {this.props.children}
-    </input>);
-  }
+  return (<input type='text' {...allowedProps} className={'Input' + (props.className ? ' ' + props.className : '')} ref={props.reference}>
+    {props.children}
+  </input>);
 }
