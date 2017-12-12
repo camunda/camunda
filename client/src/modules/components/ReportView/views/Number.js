6a4244fb1bee7ced4af2d77b137eddd7a96d24b7
Franz Heidl <franz@franzheidl.de>
2017-12-12 14:55:18 +0100

    chore(Report) style number report in the report builder
    this should also set tables to be at least 25% wide and center them, too.
    
    related to OPT-845

--- client/src/modules/components/ReportView/views/Number.js
+++ client/src/modules/components/ReportView/views/Number.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Number.css';
+
 export default function Number({data, errorMessage}) {
   if(typeof data !== 'number') {
     return <p>{errorMessage}</p>;
   }
 
-  return <p>{data}</p>;
+  return <span className='Number'>{data}</span>;
 }
