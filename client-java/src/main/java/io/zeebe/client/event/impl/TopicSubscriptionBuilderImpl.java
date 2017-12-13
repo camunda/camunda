znavy/zeebe

feat(client): event subscriber groups for all partitions of a topic
- orchestrates subscribers to all partitions of a topic
- closes group as soon as one of the subscribers aborts (or fails to open
  at all)
- topic subscription handler invocation is serialized across all
  subscribers of a group (i.e. handlers need not be thread-safe)

related to #566

client-java/src/main/java/io/zeebe/client/event/impl/TopicSubscriptionBuilderImpl.java
@@ -15,8 +15,18 @@
  */
 package io.zeebe.client.event.impl;
 
+import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.clustering.impl.ClientTopologyManager;
-import io.zeebe.client.event.*;
+import io.zeebe.client.cmd.ClientException;
+import io.zeebe.client.event.IncidentEventHandler;
+import io.zeebe.client.event.RaftEventHandler;
+import io.zeebe.client.event.TaskEventHandler;
+import io.zeebe.client.event.TopicEventType;
+import io.zeebe.client.event.TopicSubscription;
+import io.zeebe.client.event.TopicSubscriptionBuilder;
+import io.zeebe.client.event.UniversalEventHandler;
+import io.zeebe.client.event.WorkflowEventHandler;
+import io.zeebe.client.event.WorkflowInstanceEventHandler;
 import io.zeebe.client.impl.data.MsgPackMapper;
 import io.zeebe.client.task.impl.subscription.EventAcquisition;
 import io.zeebe.client.workflow.impl.WorkflowInstanceEventImpl;
@@ -35,22 +45,15 @@ public class TopicSubscriptionBuilderImpl implements TopicSubscriptionBuilder
     protected final MsgPackMapper msgPackMapper;
 
     public TopicSubscriptionBuilderImpl(
-            TopicClientImpl client,
+            ZeebeClient client,
             ClientTopologyManager topologyManager,
             String topic,
-            EventAcquisition<TopicSubscriptionImpl> acquisition,
+            EventAcquisition acquisition,
             MsgPackMapper msgPackMapper,
             int prefetchCapacity)
     {
         builder = new TopicSubscriptionImplBuilder(client, topologyManager, topic, acquisition, prefetchCapacity);
         this.msgPackMapper = msgPackMapper;
-    }
-
-    @Override
-    public TopicSubscriptionBuilder partitionId(int partition)
-    {
-        builder.partitionId(partition);
-        return this;
     }
 
     @Override
@@ -98,18 +101,23 @@ public class TopicSubscriptionBuilderImpl implements TopicSubscriptionBuilder
     @Override
     public TopicSubscription open()
     {
+        final TopicSubscriberGroupImpl subscription = buildSubscriberGroup();
+        subscription.open();
+        return subscription;
+    }
+
+    public TopicSubscriberGroupImpl buildSubscriberGroup()
+    {
         EnsureUtil.ensureNotNull("name", builder.getName());
         if (defaultEventHandler == null && taskEventHandler == null && wfEventHandler == null && wfInstanceEventHandler == null && incidentEventHandler == null
                 && raftEventHandler == null)
         {
-            throw new RuntimeException("at least one handler must be set");
+            throw new ClientException("at least one handler must be set");
         }
 
         builder.handler(this::dispatchEvent);
 
-        final TopicSubscriptionImpl subscription = builder.build();
-        subscription.open();
-        return subscription;
+        return builder.build();
     }
 
     protected void dispatchEvent(GeneralEventImpl event) throws Exception
@@ -153,9 +161,9 @@ public class TopicSubscriptionBuilderImpl implements TopicSubscriptionBuilder
     }
 
     @Override
-    public TopicSubscriptionBuilder startAtPosition(long position)
+    public TopicSubscriptionBuilder startAtPosition(int partitionId, long position)
     {
-        builder.startPosition(position);
+        builder.startPosition(partitionId, position);
         return this;
     }
 

client-java/src/main/java/io/zeebe/client/task/impl/subscription/EventAcquisition.java
@@ -26,18 +26,18 @@ import io.zeebe.transport.RemoteAddress;
 import io.zeebe.util.DeferredCommandContext;
 import io.zeebe.util.actor.Actor;
 
-public class EventAcquisition<T extends EventSubscription<T>> implements SubscribedEventHandler, Actor
+public class EventAcquisition implements SubscribedEventHandler, Actor
 {
     protected static final Logger LOGGER = Loggers.SUBSCRIPTION_LOGGER;
 
     protected final String name;
-    protected final EventSubscriptions<T> subscriptions;
+    protected final EventSubscribers subscribers;
     protected DeferredCommandContext asyncContext = new DeferredCommandContext();
 
-    public EventAcquisition(String name, EventSubscriptions<T> subscriptions)
+    public EventAcquisition(String name, EventSubscribers subscriptions)
     {
         this.name = name;
-        this.subscriptions = subscriptions;
+        this.subscribers = subscriptions;
     }
 
     @Override
@@ -50,7 +50,7 @@ public class EventAcquisition<T extends EventSubscription<T>> implements Subscri
     public int doWork() throws Exception
     {
         int workCount = asyncContext.doWork();
-        workCount += subscriptions.maintainState();
+        workCount += subscribers.maintainState();
         return workCount;
     }
 
@@ -58,12 +58,29 @@ public class EventAcquisition<T extends EventSubscription<T>> implements Subscri
     public boolean onEvent(long subscriberKey, GeneralEventImpl event)
     {
         final EventMetadata eventMetadata = event.getMetadata();
-        final T subscription = subscriptions.getSubscription(eventMetadata.getPartitionId(), subscriberKey);
+        EventSubscriber subscriber = subscribers.getSubscriber(eventMetadata.getPartitionId(), subscriberKey);
 
-        if (subscription != null && subscription.isOpen())
+        if (subscriber == null)
         {
-            event.setTopicName(subscription.getTopicName());
-            return subscription.addEvent(event);
+            if (subscribers.isAnySubscriberOpening())
+            {
+                // avoids a race condition when a subscribe request is in progress and we haven't activated the subscriber
+                // yet, but we already receive an event from the broker
+                // in this case, we postpone the event
+                return false;
+            }
+            else
+            {
+                // fetch a second time as the subscriber may have opened (and registered) between the first #getSubscriptions
+                // invocation and the check for opening subscribers
+                subscriber = subscribers.getSubscriber(eventMetadata.getPartitionId(), subscriberKey);
+            }
+        }
+
+        if (subscriber != null && subscriber.isOpen())
+        {
+            event.setTopicName(subscriber.getTopicName());
+            return subscriber.addEvent(event);
         }
         else
         {
@@ -72,27 +89,44 @@ public class EventAcquisition<T extends EventSubscription<T>> implements Subscri
         }
     }
 
-    public void activateSubscription(T subscription)
+    public void activateSubscriber(EventSubscriber subscriber)
     {
-        this.subscriptions.activate(subscription);
+        this.subscribers.activate(subscriber);
     }
 
-    public void stopManageSubscription(T subscription)
+    public void addSubscriber(EventSubscriber subscriber)
     {
-        this.subscriptions.remove(subscription);
+        this.subscribers.addSubscriber(subscriber);
     }
 
-    public CompletableFuture<T> registerSubscriptionAsync(T subscription)
+    public void deactivateSubscriber(EventSubscriber subscriber)
+    {
+        this.subscribers.deactivate(subscriber);
+    }
+
+    public void removeSubscriber(EventSubscriber subscriber)
+    {
+        this.subscribers.removeSubscriber(subscriber);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void stopManageGroup(EventSubscriberGroup subscription)
+    {
+        this.subscribers.removeGroup(subscription);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public CompletableFuture<Void> registerSubscriptionAsync(EventSubscriberGroup subscription)
     {
         return asyncContext.runAsync((future) ->
         {
-            subscriptions.add(subscription);
+            subscribers.addGroup(subscription);
         });
     }
 
     public void reopenSubscriptionsForRemoteAsync(RemoteAddress remoteAddress)
     {
-        asyncContext.runAsync(() -> subscriptions.reopenSubscriptionsForRemote(remoteAddress));
+        asyncContext.runAsync(() -> subscribers.reopenSubscriptionsForRemote(remoteAddress));
     }
 
 }

camunda/camunda

client-java/src/main/java/io/zeebe/client/event/impl/TopicSubscriptionBuilderImpl.java
@@ -15,8 +15,18 @@
  */
 package io.zeebe.client.event.impl;
 
+import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.clustering.impl.ClientTopologyManager;
-import io.zeebe.client.event.*;
+import io.zeebe.client.cmd.ClientException;
+import io.zeebe.client.event.IncidentEventHandler;
+import io.zeebe.client.event.RaftEventHandler;
+import io.zeebe.client.event.TaskEventHandler;
+import io.zeebe.client.event.TopicEventType;
+import io.zeebe.client.event.TopicSubscription;
+import io.zeebe.client.event.TopicSubscriptionBuilder;
+import io.zeebe.client.event.UniversalEventHandler;
+import io.zeebe.client.event.WorkflowEventHandler;
+import io.zeebe.client.event.WorkflowInstanceEventHandler;
 import io.zeebe.client.impl.data.MsgPackMapper;
 import io.zeebe.client.task.impl.subscription.EventAcquisition;
 import io.zeebe.client.workflow.impl.WorkflowInstanceEventImpl;
@@ -31,26 +41,19 @@ public class TopicSubscriptionBuilderImpl implements TopicSubscriptionBuilder
     protected IncidentEventHandler incidentEventHandler;
     protected RaftEventHandler raftEventHandler;
 
-    protected final TopicSubscriptionImplBuilder builder;
+    protected final TopicSubscriberGroupBuilder builder;
     protected final MsgPackMapper msgPackMapper;
 
     public TopicSubscriptionBuilderImpl(
-            TopicClientImpl client,
+            ZeebeClient client,
             ClientTopologyManager topologyManager,
             String topic,
-            EventAcquisition<TopicSubscriptionImpl> acquisition,
+            EventAcquisition acquisition,
             MsgPackMapper msgPackMapper,
             int prefetchCapacity)
     {
-        builder = new TopicSubscriptionImplBuilder(client, topologyManager, topic, acquisition, prefetchCapacity);
+        builder = new TopicSubscriberGroupBuilder(client, topologyManager, topic, acquisition, prefetchCapacity);
         this.msgPackMapper = msgPackMapper;
-    }
-
-    @Override
-    public TopicSubscriptionBuilder partitionId(int partition)
-    {
-        builder.partitionId(partition);
-        return this;
     }
 
     @Override
@@ -98,18 +101,23 @@ public class TopicSubscriptionBuilderImpl implements TopicSubscriptionBuilder
     @Override
     public TopicSubscription open()
     {
+        final TopicSubscriberGroup subscription = buildSubscriberGroup();
+        subscription.open();
+        return subscription;
+    }
+
+    public TopicSubscriberGroup buildSubscriberGroup()
+    {
         EnsureUtil.ensureNotNull("name", builder.getName());
         if (defaultEventHandler == null && taskEventHandler == null && wfEventHandler == null && wfInstanceEventHandler == null && incidentEventHandler == null
                 && raftEventHandler == null)
         {
-            throw new RuntimeException("at least one handler must be set");
+            throw new ClientException("at least one handler must be set");
         }
 
         builder.handler(this::dispatchEvent);
 
-        final TopicSubscriptionImpl subscription = builder.build();
-        subscription.open();
-        return subscription;
+        return builder.build();
     }
 
     protected void dispatchEvent(GeneralEventImpl event) throws Exception
@@ -153,9 +161,9 @@ public class TopicSubscriptionBuilderImpl implements TopicSubscriptionBuilder
     }
 
     @Override
-    public TopicSubscriptionBuilder startAtPosition(long position)
+    public TopicSubscriptionBuilder startAtPosition(int partitionId, long position)
     {
-        builder.startPosition(position);
+        builder.startPosition(partitionId, position);
         return this;
     }
 

client-java/src/main/java/io/zeebe/client/task/impl/subscription/EventAcquisition.java
@@ -26,18 +26,18 @@ import io.zeebe.transport.RemoteAddress;
 import io.zeebe.util.DeferredCommandContext;
 import io.zeebe.util.actor.Actor;
 
-public class EventAcquisition<T extends EventSubscription<T>> implements SubscribedEventHandler, Actor
+public class EventAcquisition implements SubscribedEventHandler, Actor
 {
     protected static final Logger LOGGER = Loggers.SUBSCRIPTION_LOGGER;
 
     protected final String name;
-    protected final EventSubscriptions<T> subscriptions;
+    protected final EventSubscribers subscribers;
     protected DeferredCommandContext asyncContext = new DeferredCommandContext();
 
-    public EventAcquisition(String name, EventSubscriptions<T> subscriptions)
+    public EventAcquisition(String name, EventSubscribers subscriptions)
     {
         this.name = name;
-        this.subscriptions = subscriptions;
+        this.subscribers = subscriptions;
     }
 
     @Override
@@ -50,7 +50,7 @@ public class EventAcquisition<T extends EventSubscription<T>> implements Subscri
     public int doWork() throws Exception
     {
         int workCount = asyncContext.doWork();
-        workCount += subscriptions.maintainState();
+        workCount += subscribers.maintainState();
         return workCount;
     }
 
@@ -58,12 +58,29 @@ public class EventAcquisition<T extends EventSubscription<T>> implements Subscri
     public boolean onEvent(long subscriberKey, GeneralEventImpl event)
     {
         final EventMetadata eventMetadata = event.getMetadata();
-        final T subscription = subscriptions.getSubscription(eventMetadata.getPartitionId(), subscriberKey);
+        EventSubscriber subscriber = subscribers.getSubscriber(eventMetadata.getPartitionId(), subscriberKey);
 
-        if (subscription != null && subscription.isOpen())
+        if (subscriber == null)
         {
-            event.setTopicName(subscription.getTopicName());
-            return subscription.addEvent(event);
+            if (subscribers.isAnySubscriberOpening())
+            {
+                // avoids a race condition when a subscribe request is in progress and we haven't activated the subscriber
+                // yet, but we already receive an event from the broker
+                // in this case, we postpone the event
+                return false;
+            }
+            else
+            {
+                // fetch a second time as the subscriber may have opened (and registered) between the first #getSubscriptions
+                // invocation and the check for opening subscribers
+                subscriber = subscribers.getSubscriber(eventMetadata.getPartitionId(), subscriberKey);
+            }
+        }
+
+        if (subscriber != null && subscriber.isOpen())
+        {
+            event.setTopicName(subscriber.getTopicName());
+            return subscriber.addEvent(event);
         }
         else
         {
@@ -72,27 +89,44 @@ public class EventAcquisition<T extends EventSubscription<T>> implements Subscri
         }
     }
 
-    public void activateSubscription(T subscription)
+    public void activateSubscriber(EventSubscriber subscriber)
     {
-        this.subscriptions.activate(subscription);
+        this.subscribers.activate(subscriber);
     }
 
-    public void stopManageSubscription(T subscription)
+    public void addSubscriber(EventSubscriber subscriber)
     {
-        this.subscriptions.remove(subscription);
+        this.subscribers.addSubscriber(subscriber);
     }
 
-    public CompletableFuture<T> registerSubscriptionAsync(T subscription)
+    public void deactivateSubscriber(EventSubscriber subscriber)
+    {
+        this.subscribers.deactivate(subscriber);
+    }
+
+    public void removeSubscriber(EventSubscriber subscriber)
+    {
+        this.subscribers.removeSubscriber(subscriber);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public void stopManageGroup(EventSubscriberGroup subscription)
+    {
+        this.subscribers.removeGroup(subscription);
+    }
+
+    @SuppressWarnings("rawtypes")
+    public CompletableFuture<Void> registerSubscriptionAsync(EventSubscriberGroup subscription)
     {
         return asyncContext.runAsync((future) ->
         {
-            subscriptions.add(subscription);
+            subscribers.addGroup(subscription);
         });
     }
 
     public void reopenSubscriptionsForRemoteAsync(RemoteAddress remoteAddress)
     {
-        asyncContext.runAsync(() -> subscriptions.reopenSubscriptionsForRemote(remoteAddress));
+        asyncContext.runAsync(() -> subscribers.reopenSubscribersForRemote(remoteAddress));
     }
 
 }