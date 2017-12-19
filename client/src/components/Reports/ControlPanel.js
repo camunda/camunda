75eecea59d73682b59d04afda2e75314d47e1e55
Franz Heidl <franz@franzheidl.de>
2017-12-19 11:01:26 +0100

    improve(ControlPanel) improve select widths
    minor change, no ticket.

--- client/src/components/Reports/ControlPanel.js
+++ client/src/components/Reports/ControlPanel.js
@@ -45,30 +45,30 @@ export default class ControlPanel extends React.Component {
   render() {
     return <div className='ControlPanel'>
       <ul className='ControlPanel__list'>
-        <li className='ControlPanel__item'>
+        <li className='ControlPanel__item ControlPanel__item--select'>
           <label htmlFor='ControlPanel__process-definition' className='ControlPanel__label'>Process definition</label>
-          <Select name='ControlPanel__process-definition' value={this.props.processDefinitionId} onChange={this.changeDefinition}>
+          <Select className='ControlPanel__select' name='ControlPanel__process-definition' value={this.props.processDefinitionId} onChange={this.changeDefinition}>
             {addSelectionOption()}
             {this.state.availableDefinitions.map(definition => <Select.Option value={definition.id} key={definition.id}>{definition.id}</Select.Option>)}
           </Select>
         </li>
-        <li className='ControlPanel__item'>
+        <li className='ControlPanel__item ControlPanel__item--select'>
           <label htmlFor='ControlPanel__view' className='ControlPanel__label'>View</label>
-          <Select name='ControlPanel__view' value={reportLabelMap.objectToKey(this.props.view, reportLabelMap.view)} onChange={this.changeView}>
+          <Select className='ControlPanel__select' name='ControlPanel__view' value={reportLabelMap.objectToKey(this.props.view, reportLabelMap.view)} onChange={this.changeView}>
             {addSelectionOption()}
             {renderOptions(reportLabelMap.getOptions('view'))}
           </Select>
         </li>
-        <li className='ControlPanel__item'>
+        <li className='ControlPanel__item ControlPanel__item--select'>
           <label htmlFor='ControlPanel__group-by' className='ControlPanel__label'>Group by</label>
-          <Select name='ControlPanel__group-by' value={reportLabelMap.objectToKey(this.props.groupBy, reportLabelMap.groupBy)} onChange={this.changeGroup}>
+          <Select className='ControlPanel__select' name='ControlPanel__group-by' value={reportLabelMap.objectToKey(this.props.groupBy, reportLabelMap.groupBy)} onChange={this.changeGroup}>
             {addSelectionOption()}
             {renderOptions(reportLabelMap.getOptions('groupBy'))}
           </Select>
         </li>
-        <li className='ControlPanel__item'>
+        <li className='ControlPanel__item ControlPanel__item--select'>
           <label htmlFor='ControlPanel__visualize-as' className='ControlPanel__label'>Visualize as</label>
-          <Select name='ControlPanel__visualize-as' value={this.props.visualization} onChange={this.changeVisualization}>
+          <Select className='ControlPanel__select' name='ControlPanel__visualize-as' value={this.props.visualization} onChange={this.changeVisualization}>
             {addSelectionOption()}
             {renderOptions(reportLabelMap.getOptions('visualizeAs'))}
           </Select>
