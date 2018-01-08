f721b874c468e538272e95767f1252b933c2bbb7
Sebastian Menski <sebastian.menski@camunda.com>
2018-01-08 11:08:54 +0100

    chore(UnpackedObject): remove retries and debug output
    - the bug should be fixed by https://github.com/zeebe-io/zb-util/commit/f399bd0a2718bbeb9da6fc86ed44f6bef743a5dd
      so the retries and debug output is not needed anymore
    
    related to zeebe-io/zeebe#596

--- msgpack-value/src/main/java/io/zeebe/msgpack/UnpackedObject.java
+++ msgpack-value/src/main/java/io/zeebe/msgpack/UnpackedObject.java
@@ -19,23 +19,16 @@ import io.zeebe.msgpack.spec.MsgPackReader;
 import io.zeebe.msgpack.spec.MsgPackWriter;
 import io.zeebe.msgpack.value.ObjectValue;
 import io.zeebe.util.buffer.BufferReader;
-import io.zeebe.util.buffer.BufferUtil;
 import io.zeebe.util.buffer.BufferWriter;
 import org.agrona.DirectBuffer;
 import org.agrona.MutableDirectBuffer;
-import org.agrona.concurrent.UnsafeBuffer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UnpackedObject extends ObjectValue implements Recyclable, BufferReader, BufferWriter
 {
 
-    private static final Logger LOG = LoggerFactory.getLogger("io.zeebe.msgpack");
-
     protected final MsgPackReader reader = new MsgPackReader();
     protected final MsgPackWriter writer = new MsgPackWriter();
 
-    protected final UnsafeBuffer view = new UnsafeBuffer(0, 0);
 
     public void wrap(DirectBuffer buff)
     {
@@ -45,45 +38,14 @@ public class UnpackedObject extends ObjectValue implements Recyclable, BufferRea
     @Override
     public void wrap(DirectBuffer buff, int offset, int length)
     {
-        int retries = 0;
-        int readerOffset = 0;
-        boolean successful = false;
-
-        view.wrap(buff, offset, length);
-        final String bufferOnFirstInvocation = BufferUtil.bufferAsHexString(view, 32);
-
-        while (!successful)
+        reader.wrap(buff, offset, length);
+        try
         {
-            reader.wrap(buff, offset, length);
-            try
-            {
-                read(reader);
-                successful = true;
-
-                if (retries > 0)
-                {
-                    final DirectBuffer buffer = reader.buffer;
-                    final String lastReadByteBeforeException = String.format("0x%02x", readerOffset < length ? buffer.getByte(readerOffset) : 0);
-                    LOG.warn("Retry {} was successful. Reader previously stuck at offset {} of length {} with last read byte {}.\nBuffer on first invocation:\n{}\nBuffer after last invocation:\n{}",
-                            retries, readerOffset, length, lastReadByteBeforeException, bufferOnFirstInvocation, BufferUtil.bufferAsHexString(buffer, 32));
-
-                }
-            }
-            catch (final Exception e)
-            {
-                final DirectBuffer buffer = reader.buffer;
-                readerOffset = reader.getOffset() - 1;
-                final String lastReadByteBeforeException = String.format("0x%02x", readerOffset < length ? buffer.getByte(readerOffset) : 0);
-                LOG.error("Retry {} could not deserialize object. Deserialization stuck at offset {} of length {} with last read byte {}.\nBuffer on first invocation:\n{}\nBuffer after last invocation:\n{}",
-                        retries, readerOffset, length, lastReadByteBeforeException, bufferOnFirstInvocation, BufferUtil.bufferAsHexString(buffer, 32), e);
-
-                retries++;
-
-                if (retries > 9)
-                {
-                    throw new RuntimeException("Could not deserialize object. Deserialization stuck at offset " + reader.getOffset() + " of length " + length, e);
-                }
-            }
+            read(reader);
+        }
+        catch (final Exception e)
+        {
+            throw new RuntimeException("Could not deserialize object. Deserialization stuck at offset " + reader.getOffset() + " of length " + length, e);
         }
     }
 
