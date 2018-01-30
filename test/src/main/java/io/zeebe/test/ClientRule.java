6679bbc2255f5528357feb1b549f7717dfd4fd2d
Sebastian Menski <sebastian.menski@camunda.com>
2018-01-30 12:59:45 +0100

    chore(ClientRule): use helper method to determine if partition leader

--- test/src/main/java/io/zeebe/test/ClientRule.java
+++ test/src/main/java/io/zeebe/test/ClientRule.java
@@ -109,7 +109,7 @@ public class ClientRule extends ExternalResource
             for (BrokerPartitionState brokerPartitionState : partitions)
             {
                 if (DEFAULT_TOPIC.equals(brokerPartitionState.getTopicName())
-                    && brokerPartitionState.getState().equals("LEADER"))
+                    && brokerPartitionState.isLeader())
                 {
                     defaultPartition = brokerPartitionState.getPartitionId();
                     break;
