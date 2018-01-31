5c1e5191c31a7f94860611b5886f1039f195886d
Sebastian Menski <sebastian.menski@camunda.com>
2018-01-31 16:03:53 +0100

    chore(samples): add typed event logger

---
+++ samples/src/main/java/io/zeebe/topic/TypedEventLogger.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.zeebe.topic;
+
+import java.util.Properties;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+import io.zeebe.client.ClientProperties;
+import io.zeebe.client.ZeebeClient;
+import io.zeebe.client.event.*;
+import io.zeebe.client.impl.ZeebeClientImpl;
+
+public class TypedEventLogger
+{
+
+    public static void main(String[] args)
+    {
+        final String brokerContactPoint = "127.0.0.1:51015";
+
+        final Properties clientProperties = new Properties();
+        clientProperties.put(ClientProperties.BROKER_CONTACTPOINT, brokerContactPoint);
+
+        final ZeebeClient zeebeClient = new ZeebeClientImpl(clientProperties);
+
+        System.out.println(String.format("> Connecting to %s", brokerContactPoint));
+
+        final String topicName = "default-topic";
+
+        System.out.println(String.format("> Open event subscription from topic '%s'", topicName));
+
+        final Consumer<Event> logger = e -> {
+            System.out.println(e.getMetadata());
+            System.out.println(e);
+            System.out.println();
+        };
+
+        final TopicSubscription subscription =
+            zeebeClient.topics()
+                       .newSubscription(topicName)
+                       .name("logger")
+                       .startAtHeadOfTopic()
+                       .forcedStart()
+                       .workflowEventHandler(logger::accept)
+                       .workflowInstanceEventHandler(logger::accept)
+                       .taskEventHandler(logger::accept)
+                       .incidentEventHandler(logger::accept)
+                       .open();
+
+        // wait for events
+        try (Scanner scanner = new Scanner(System.in))
+        {
+            while (scanner.hasNextLine())
+            {
+                final String nextLine = scanner.nextLine();
+                if (nextLine.contains("exit"))
+                {
+                    System.out.println("> Closing...");
+
+                    subscription.close();
+                    zeebeClient.close();
+
+                    System.out.println("> Closed.");
+
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+}
