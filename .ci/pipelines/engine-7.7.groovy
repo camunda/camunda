f71f75fc13f88cc262f8191be404b9c3aab9d74a
Askar Akhmerov <askar.akhmerov@camunda.com>
2018-03-01 13:50:43 +0100

    feat(optimize): add pipelines to test against other engine versions
    related to #OPT-1067

---
+++ .ci/pipelines/engine-7.7.groovy
@@ -0,0 +1,52 @@
+#!/usr/bin/env groovy
+
+def backendModuleName = "backend"
+
+pipeline {
+  agent { label 'Optimize engine-7.7' }
+
+  // Environment
+  environment {
+    DISPLAY = ":0"
+    NODE_ENV = "ci"
+  }
+
+  options {
+    // General Jenkins job properties
+    buildDiscarder(logRotator(numToKeepStr:'10'))
+    // "wrapper" steps that should wrap the entire build execution
+    timestamps()
+    timeout(time: 10, unit: 'MINUTES')
+  }
+
+  stages {
+    stage('Prepare') {
+      steps {
+        configFileProvider([
+            configFile(fileId: 'camunda-maven-settings', replaceTokens: true, targetLocation: 'settings.xml')
+        ]) {}
+      }
+    }
+    stage('IT') {
+      steps {
+        stopAllOptimizeComponents()
+        sh 'mvn -s settings.xml -Pit,engine-7.7,jenkins clean'
+        startElasticsearch()
+        sh 'mvn -s settings.xml -Pit,engine-7.7,jenkins  -f ' + backendModuleName + '/pom.xml verify'
+      }
+      post {
+        always {
+          stopAllOptimizeComponents()
+          copySnapshots()
+          junit testResults: '**/failsafe-reports/**/*.xml', allowEmptyResults: true, healthScaleFactor: 1.0, keepLongStdio: true
+          archiveArtifacts artifacts:  backendModuleName + '/target/it-elasticsearch/**/logs/*.log', onlyIfSuccessful: false
+          archiveArtifacts artifacts:  backendModuleName + '/target/it-elasticsearch/**/_snapshots/*.gz', onlyIfSuccessful: false
+          archiveArtifacts artifacts:  backendModuleName + '/target/failsafe-reports/*.txt', onlyIfSuccessful: false
+          archiveArtifacts artifacts:  backendModuleName + '/target/camunda-tomcat/server/apache-tomcat-8.0.47/logs/*.out', onlyIfSuccessful: false
+        }
+      }
+
+    }
+  }
+
+}
