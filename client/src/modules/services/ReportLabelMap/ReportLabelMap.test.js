6d4fc9ee9a5fa433589fc4a5ab77121f978084e9
Kyrylo Zakurdaiev <kirill.zakurdaev@gmail.com>
2018-03-08 14:59:09 +0100

    test(ReportLabelMap): add tests for new reportLabelMap methods
    related to OPT-1047

--- client/src/modules/services/ReportLabelMap/ReportLabelMap.test.js
+++ client/src/modules/services/ReportLabelMap/ReportLabelMap.test.js
@@ -48,3 +48,81 @@ it('should compose a key for a given group object', () => {
     reportLabelMap.objectToLabel({type: 'startDate', unit: 'year'}, reportLabelMap.groupBy)
   ).toEqual('Start Date of Process Instance - Year');
 });
+
+it('should return the right option if only one is allowed', () => {
+  expect(reportLabelMap.getTheOnlyOption('groupBy', 'rawData_ignored_ignored', '')).toEqual(
+    'none_null'
+  );
+
+  expect(
+    reportLabelMap.getTheOnlyOption('visualization', 'rawData_ignored_ignored', 'none_null')
+  ).toEqual('table');
+
+  expect(reportLabelMap.getTheOnlyOption('groupBy', 'avg_flowNode_duration', '')).toEqual(
+    'flowNode_null'
+  );
+});
+
+it('should return the right new combination given the old one', () => {
+  expect(reportLabelMap.getTheRightCombination('rawData_ignored_ignored', '', '')).toEqual({
+    groupBy: {type: 'none', unit: null},
+    view: {entity: 'ignored', operation: 'rawData', property: 'ignored'},
+    visualization: 'table'
+  });
+
+  expect(
+    reportLabelMap.getTheRightCombination('avg_flowNode_duration', 'none_null', 'table')
+  ).toEqual({
+    groupBy: {type: 'flowNode', unit: null},
+    view: {entity: 'flowNode', operation: 'avg', property: 'duration'},
+    visualization: ''
+  });
+
+  expect(
+    reportLabelMap.getTheRightCombination('avg_processInstance_duration', 'none_null', 'table')
+  ).toEqual({
+    groupBy: {type: 'none', unit: null},
+    view: {entity: 'processInstance', operation: 'avg', property: 'duration'},
+    visualization: 'number'
+  });
+
+  expect(
+    reportLabelMap.getTheRightCombination(
+      'avg_processInstance_duration',
+      'startDate_year',
+      'number'
+    )
+  ).toEqual({
+    groupBy: {type: 'startDate', unit: 'year'},
+    view: {entity: 'processInstance', operation: 'avg', property: 'duration'},
+    visualization: ''
+  });
+
+  //if valid combination passed, same combination returned
+  expect(
+    reportLabelMap.getTheRightCombination('avg_flowNode_duration', 'flowNode_null', 'table')
+  ).toEqual({
+    groupBy: {type: 'flowNode', unit: null},
+    view: {entity: 'flowNode', operation: 'avg', property: 'duration'},
+    visualization: 'table'
+  });
+});
+
+it('should return all possible options for given view and groupBy options', () => {
+  expect(reportLabelMap.getEnabledOptions('groupBy', 'avg_flowNode_duration', '')).toEqual([
+    'flowNode_null'
+  ]);
+
+  expect(
+    reportLabelMap.getEnabledOptions('visualization', 'avg_flowNode_duration', 'flowNode_null')
+  ).toEqual(['heat', 'pie', 'line', 'bar', 'table']);
+
+  expect(reportLabelMap.getEnabledOptions('groupBy', 'avg_processInstance_duration', '')).toEqual([
+    'none_null',
+    'startDate_year',
+    'startDate_month',
+    'startDate_week',
+    'startDate_day',
+    'startDate_hour'
+  ]);
+});
