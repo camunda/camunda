9e405fd79c24c492e17aba20234641f65d2083a7
Askar Akhmerov <askar.akhmerov@camunda.com>
2018-03-08 12:04:27 +0100

    chor(optimize): increase timeout to 3h, related to #OPT-1101

--- .ci/pipelines/performance_large.groovy
+++ .ci/pipelines/performance_large.groovy
@@ -37,7 +37,7 @@ pipeline {
     buildDiscarder(logRotator(numToKeepStr: '10'))
     // "wrapper" steps that should wrap the entire build execution
     timestamps()
-    timeout(time: 120, unit: 'MINUTES')
+    timeout(time: 180, unit: 'MINUTES')
   }
 
   stages {
