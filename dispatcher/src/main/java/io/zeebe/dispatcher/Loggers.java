6aebd966a466e47550cb19c7a68de1c9a8b9789b
Sebastian Menski <sebastian.menski@camunda.com>
2018-03-19 15:59:06 +0100

    chore(dispatcher): use ZbLogger
    related to zeebe-io/zeebe#760

--- dispatcher/src/main/java/io/zeebe/dispatcher/Loggers.java
+++ dispatcher/src/main/java/io/zeebe/dispatcher/Loggers.java
@@ -15,12 +15,12 @@
  */
 package io.zeebe.dispatcher;
 
+import io.zeebe.util.ZbLogger;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Loggers
 {
 
-    public static final Logger DISPATCHER_LOGGER = LoggerFactory.getLogger("io.zeebe.dispatcher");
+    public static final Logger DISPATCHER_LOGGER = new ZbLogger("io.zeebe.dispatcher");
 
 }
