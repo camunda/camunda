59d6662d2ddb5252ac83b7e26539dbb7e4dbdead
Sebastian Menski <sebastian.menski@camunda.com>
2018-03-19 16:01:18 +0100

    chore(logger): use ZbLogger
    related to zeebe-io/zeebe#760

--- logstreams/src/main/java/io/zeebe/logstreams/impl/Loggers.java
+++ logstreams/src/main/java/io/zeebe/logstreams/impl/Loggers.java
@@ -15,12 +15,12 @@
  */
 package io.zeebe.logstreams.impl;
 
+import io.zeebe.util.ZbLogger;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Loggers
 {
 
-    public static final Logger LOGSTREAMS_LOGGER = LoggerFactory.getLogger("io.zeebe.logstreams");
+    public static final Logger LOGSTREAMS_LOGGER = new ZbLogger("io.zeebe.logstreams");
 
 }
