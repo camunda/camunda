107d1adbe9b45fe9802eb52cc778265d845b296e
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-03-22 14:44:45 +0100

    chore(Dashboard): make corners of Reports round

--- client/src/components/Dashboards/Grid/Grid.js
+++ client/src/components/Dashboards/Grid/Grid.js
@@ -12,8 +12,8 @@ export default class Grid extends React.Component {
       'url("data:image/svg+xml;base64,' +
       btoa(
         `<svg xmlns='http://www.w3.org/2000/svg' width='${outerWidth}' height='${outerHeight}'>` +
-          `<rect stroke='rgba(0, 0, 0, 0.2)' stroke-width='1' fill='none' x='${margin /
-            2}' y='${margin / 2}' width='${innerWidth - 1}' height='${innerHeight - 1}'/>` +
+          `<rect stroke='rgba(0, 0, 0, 0.2)' stroke-width='1' fill='none' x='${margin / 2 +
+            1}' y='${margin / 2 + 1}' width='${innerWidth - 3}' height='${innerHeight - 3}'/>` +
           `</svg>`
       ) +
       '")';
