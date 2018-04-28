1f102480e64011e2f88967a022c4175d8438a8f3
Johannes Heinemann <johannes.heinemann@camunda.com>
2018-04-28 16:10:44 +0200

    Revert "fix(login): use correct field name for user id"
    This reverts commit 366fb63a3dab454075bfc0e75fef752fe4317979.
    
    related to #OPT-1229

--- client/src/components/Login/service.js
+++ client/src/components/Login/service.js
@@ -3,7 +3,7 @@ import {store} from 'credentials';
 
 export async function login(username, password) {
   try {
-    const response = await post('/api/authentication', {id: username, password});
+    const response = await post('/api/authentication', {username, password});
     const token = await response.text();
 
     store({username, token});
