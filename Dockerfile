08015d46ac083b8023eab473faa8982ad25cbf21
Sebastian Menski <sebastian.menski@camunda.com>
2018-05-08 11:25:38 +0200

    chore(docker): adjust docker image to latest config changes
    closes #832

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,18 @@ ARG DISTBALL
 
 ENV ZB_HOME=/usr/local/zeebe/ \
     ZEEBE_LOG_LEVEL=info \
-    DEPLOY_ON_KUBERNETES=false
+    DEPLOY_ON_KUBERNETES=false \
+    BOOTSTRAP=0
 
 COPY ${DISTBALL} ${ZB_HOME}
 
 RUN apk add --no-cache bash && \
     tar xfvz ${ZB_HOME}/*.tar.gz --strip 1 -C ${ZB_HOME} && rm ${ZB_HOME}/*.tar.gz
 
-WORKDIR ${ZB_HOME}/bin
+WORKDIR ${ZB_HOME}
 EXPOSE 51016 51017 51015
-VOLUME ${ZB_HOME}/bin/data
+VOLUME ${ZB_HOME}/data
 
 COPY docker/utils/startup.sh /usr/local/bin
-COPY docker/utils/zeebe.cfg.toml $ZB_HOME/conf/
 
 ENTRYPOINT ["/usr/local/bin/startup.sh"]
--- docker/utils/startup.sh
+++ docker/utils/startup.sh
@@ -1,33 +1,20 @@
 #!/bin/bash -xeu
 
-cfgHome=/usr/local/zeebe/conf/zeebe.cfg.toml
-cfgLog=/usr/local/zeebe/conf/log4j2.xml
+configFile=/usr/local/zeebe/conf/zeebe.cfg.toml
 
 INITIAL_CONTACT_POINT=${INITIAL_CONTACT_POINT:-}
 ZEEBE_HOST=${ZEEBE_HOST:-$(hostname -i)}
+BOOTSTAP=${BOOTSTRAP:-0}
 
 if [[ "$DEPLOY_ON_KUBERNETES" == "true" ]]; then
+    ZEEBE_HOST="${HOST}.${DNS}"
+fi
 
-    DNSNAME="${HOST}.${DNS}"
+sed -i "s/bootstrap =.*/bootstrap = ${BOOTSTRAP}/g" $configFile
+sed -i "s/.*host =.*/host = \"${ZEEBE_HOST}\"/g" $configFile
 
-    sed -i "s/@DNSNAME@/${DNSNAME}/g" $cfgHome
-
-    if [[ -n $INITIAL_CONTACT_POINT && ! $HOST =~ .*-0$ ]]; then
-        sed -i "s/@INITIAL_CONTACT_POINT@/initialContactPoints = \[\n\t\"${INITIAL_CONTACT_POINT}\"\n\]/g" $cfgHome
-    else
-        sed -i "s/@INITIAL_CONTACT_POINT@//g" $cfgHome
-    fi
-
-else
-
-    sed -i "s/@DNSNAME@/${ZEEBE_HOST}/g" $cfgHome
-
-    if [ -n "${INITIAL_CONTACT_POINT}" ]; then
-        sed -i "s/@INITIAL_CONTACT_POINT@/initialContactPoints = \[\n\t\"${INITIAL_CONTACT_POINT}\"\n\]/g" $cfgHome
-    else
-        sed -i "s/@INITIAL_CONTACT_POINT@//g" $cfgHome
-    fi
-
+if [ -n "${INITIAL_CONTACT_POINT}" ]; then
+    sed -i "s/# initialContactPoints = \[\].*/initialContactPoints = \[\n\t\"${INITIAL_CONTACT_POINT}\"\n\]/g" $configFile
 fi
 
 exec /usr/local/zeebe/bin/broker
