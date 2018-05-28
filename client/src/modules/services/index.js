21841487fca3aa45bb0c59433d6d218af3b11fb2
Kyrylo Zakurdaiev <kirill.zakurdaev@gmail.com>
2018-05-28 15:34:49 +0200

    chore(services): remove commented lines
    related to OPT-1221

--- client/src/modules/services/index.js
+++ client/src/modules/services/index.js
@@ -1,5 +1,4 @@
 import * as formattersImport from './formatters';
-// import * as numberParserImport from './NumberParser';
 
 export {reportLabelMap} from './ReportLabelMap';
 export {getFlowNodeNames} from './GetFlowNodeNames';
@@ -9,5 +8,4 @@ export {default as processRawData} from './processRawData';
 export {numberParser} from './NumberParser';
 
 // unfortunately, there is no syntax like "export * as formatters from './formatters'"
-// export const numberParser = numberParserImport;
 export const formatters = formattersImport;
