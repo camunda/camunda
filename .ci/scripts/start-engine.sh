ecea98240d6423e2b6a49982f4add4bc4bc608ea
Johannes Heinemann <johannes.heinemann@camunda.com>
2018-05-30 14:49:48 +0200

    chore(ci): adjust start engine script for performance tests

--- .ci/scripts/start-engine.sh
+++ .ci/scripts/start-engine.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -xe
 
-mkdir -p ./backend/target/performance-engine-7.8
-cd ./backend/target/performance-engine-7.8
-wget https://camunda.org/release/camunda-bpm/tomcat/7.8/camunda-bpm-tomcat-7.8.0.tar.gz
-tar -xzvf camunda-bpm-tomcat-7.8.0.tar.gz
-cp ../../../qa/import-performance-tests/src/test/resources/tomcat/* server/apache-tomcat-8.0.47/conf/
-cd server/apache-tomcat-8.0.47/lib
+mkdir -p ./backend/target/performance-engine-7.9
+cd ./backend/target/performance-engine-7.9
+wget https://camunda.org/release/camunda-bpm/tomcat/7.9/camunda-bpm-tomcat-7.9.0.tar.gz
+tar -xzvf camunda-bpm-tomcat-7.9.0.tar.gz
+cp ../../../qa/import-performance-tests/src/test/resources/tomcat/* server/apache-tomcat-9.0.5/conf/
+cd server/apache-tomcat-9.0.5/lib
 wget http://central.maven.org/maven2/org/postgresql/postgresql/42.2.1/postgresql-42.2.1.jar
 cd ../../..
-./server/apache-tomcat-8.0.47/bin/startup.sh
+./server/apache-tomcat-9.0.5/bin/startup.sh
