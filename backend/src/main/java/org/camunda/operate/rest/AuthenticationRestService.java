acbd9ba5d106d9827e9d8a3def9b872b2aa31b2f
Roman <roman.smirnov@camunda.com>
2018-06-08 15:56:55 +0200

    feat(login/logout): provide infrastructure to login and logout
    * in addition the commit adds support for CORS for development purposes
    
    related to OPE-10

---
+++ backend/src/main/java/org/camunda/operate/rest/AuthenticationRestService.java
@@ -0,0 +1,55 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.camunda.operate.rest;
+
+import static org.camunda.operate.rest.AuthenticationRestService.AUTHENTICATION_URL;
+
+import org.camunda.operate.rest.dto.UserDto;
+import org.camunda.operate.rest.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = AUTHENTICATION_URL)
+public class AuthenticationRestService {
+
+  public static final String AUTHENTICATION_URL = "/authentications";
+
+  @Autowired
+  protected UserDetailsService userDetailsService;
+
+  @GetMapping(path = "/user")
+  public UserDto getCurrentAuthentication() {
+    SecurityContext context = SecurityContextHolder.getContext();
+    Authentication authentication = context.getAuthentication();
+
+    String username = authentication.getName();
+
+    try {
+      UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+      return UserDto.fromUserDetails(userDetails);
+    }
+    catch (UsernameNotFoundException e) {
+      throw new UserNotFoundException(String.format("User '%s' not found.", username));
+    }
+  }
+
+}
--- backend/src/main/java/org/camunda/operate/security/WebSecurityConfig.java
+++ backend/src/main/java/org/camunda/operate/security/WebSecurityConfig.java
@@ -1,42 +1,50 @@
 package org.camunda.operate.security;
 
+import static org.apache.http.entity.ContentType.APPLICATION_JSON;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.json.Json;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.http.entity.ContentType;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Svetlana Dorokhova.
  */
-
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+  protected static final String COOKIE_JSESSIONID = "JSESSIONID";
 
   @Bean
   public UserDetailsService userDetailsService() {
+    String password = passwordEncoder().encode("demo");
+    UserDetails userDetails = User.builder()
+      .username("demo")
+      .password(password)
+      .roles("USER")
+      .build();
+
     InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-    manager.createUser(User.builder().username("demo")
-      .password(passwordEncoder().encode("demo"))
-      .roles("USER").build());
+    manager.createUser(userDetails);
     return manager;
   }
 
@@ -48,24 +56,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   public void configure(HttpSecurity http) throws Exception {
     http
-      .csrf().disable()
-      .cors().and()
+      .csrf()
+        .disable()
+      .cors()
+      .and()
       .authorizeRequests()
-        .antMatchers("/workflow-instance/**").permitAll()
-        .antMatchers("/**").authenticated()
+        .anyRequest().authenticated()
       .and()
         .formLogin()
-          .successHandler(successHandler())
-          .failureHandler(failureHandler())
+          .successHandler(this::successHandler)
+          .failureHandler(this::failureHandler)
         .permitAll()
       .and()
         .logout()
-        .logoutSuccessHandler(logoutSuccessHandler())
+        .logoutSuccessHandler(this::logoutSuccessHandler)
         .permitAll()
         .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID")
+        .deleteCookies(COOKIE_JSESSIONID)
       .and()
-        .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint());
+        .exceptionHandling().authenticationEntryPoint(this::failureHandler);
   }
 
   @Bean
@@ -73,48 +82,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*");
+        registry.addMapping("/**")
+          .allowedOrigins("*")
+          .allowedOrigins("http://localhost:3000")
+          .allowedMethods("GET", "POST", "PUT", "DELETE")
+          .allowedHeaders("content-type")
+          .allowCredentials(true);
       }
     };
   }
 
-  private LogoutSuccessHandler logoutSuccessHandler() {
-    return (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) ->
-      httpServletResponse.setStatus(200);
+  private void logoutSuccessHandler(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    response.setStatus(NO_CONTENT.value());
   }
 
-  private AuthenticationEntryPoint authenticationEntryPoint() {
-    return (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException ex) -> {
-      httpServletResponse.getWriter().append(
-        Json.createObjectBuilder()
-          .add("exceptionMessage", ex.getMessage())
-          .build().toString());
-      httpServletResponse.setStatus(401);
-      httpServletResponse.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-    };
+  private void failureHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException ex) throws IOException {
+    PrintWriter writer = response.getWriter();
+
+    String jsonResponse = Json.createObjectBuilder()
+      .add("message", ex.getMessage())
+      .build()
+      .toString();
+
+    writer.append(jsonResponse);
+    response.setStatus(UNAUTHORIZED.value());
+    response.setContentType(APPLICATION_JSON.getMimeType());
   }
 
-  private AuthenticationSuccessHandler successHandler() {
-    return (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) -> {
-      final String name = SecurityContextHolder.getContext().getAuthentication().getName();
-      httpServletResponse.getWriter().append(
-        Json.createObjectBuilder()
-          .add("id", 134L)
-          .add("name", name)
-          .build().toString());
-
-      httpServletResponse.setStatus(200);
-      httpServletResponse.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-    };
-  }
-
-  private AuthenticationFailureHandler failureHandler() {
-    return (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException ex) -> {
-      httpServletResponse.getWriter().append(
-        Json.createObjectBuilder().add("exceptionMessage", ex.getMessage()).build().toString());
-      httpServletResponse.setStatus(401);
-      httpServletResponse.setContentType(ContentType.APPLICATION_JSON.getMimeType());
-    };
+  private void successHandler(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+    httpServletResponse.setStatus(NO_CONTENT.value());
   }
 
 }
