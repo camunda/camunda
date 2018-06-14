a0d3851d7605e5257e40d3279be770da640cc2da
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-06-14 13:50:58 +0200

    chore(Dashboard): fix style import for tile component

--- client/src/App/Dashboard/MetricTile/MetricTile.js
+++ client/src/App/Dashboard/MetricTile/MetricTile.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import * as Styled from './styled.js';
+import * as Styled from './styled';
 
 export default function MetricTile({name, metric, metricColor}) {
   return (
     <div>
       {metricColor === 'themed' ? (
-        <Styled.themedMetric>{metric}</Styled.themedMetric>
+        <Styled.ThemedMetric>{metric}</Styled.ThemedMetric>
       ) : (
         <Styled.Metric metricColor={metricColor}>{metric}</Styled.Metric>
       )}
