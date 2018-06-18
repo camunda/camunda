228a6f884b7a2d7b196e26549e0318b22b2df748
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-06-18 09:12:21 +0200

    test(Dashboard): add api test
    related to OPE-3

---
+++ client/src/App/Dashboard/api.test.js
@@ -0,0 +1,21 @@
+import {loadRunningInst} from './api';
+
+import {mockResolvedAsyncFn} from 'modules/testUtils';
+
+import * as wrappers from 'modules/request/wrappers';
+
+describe('dashboard api', () => {
+  it('should call post with the right url', async () => {
+    //given
+    const successResponse = {json: mockResolvedAsyncFn({count: '123'})};
+    wrappers.post = mockResolvedAsyncFn(successResponse);
+
+    // when
+    const response = await loadRunningInst();
+
+    // then
+    expect(wrappers.post.mock.calls[0][0]).toBe('api/workflow-instances/count');
+    expect(successResponse.json).toBeCalled();
+    expect(response).toEqual('123');
+  });
+});
