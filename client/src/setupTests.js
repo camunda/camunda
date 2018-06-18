107c92235bbf4f4946d5ba722320948840432531
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-06-18 18:49:24 +0200

    chore(tests): add global mock for Icon

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,4 +2,8 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
+jest.mock('modules/components/Icon', () =>
+  import('modules/components/__mocks__/Icon')
+);
+
 Enzyme.configure({adapter: new Adapter()});
