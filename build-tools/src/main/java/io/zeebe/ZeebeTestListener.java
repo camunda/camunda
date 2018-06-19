37a4be1801bdb6e0b9c13a1ec827e01c767dd6ef
Sebastian Menski <sebastian.menski@camunda.com>
2018-06-19 10:08:59 +0200

    test(project): configure JUnit RunListener to log test names in surefire output

---
+++ build-tools/src/main/java/io/zeebe/ZeebeTestListener.java
@@ -0,0 +1,22 @@
+package io.zeebe;
+
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZeebeTestListener extends RunListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger("io.zeebe.test");
+
+    @Override
+    public void testStarted(Description description) throws Exception {
+        LOG.info("Test started: {}", description.getDisplayName());
+    }
+
+    @Override
+    public void testFinished(Description description) throws Exception {
+        LOG.info("Test finished: {}", description.getDisplayName());
+    }
+
+}
