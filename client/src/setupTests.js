56a9b87ca0004f6a658a10fd7256f63b70090ad5
Raluca Stan <raluca.stan@camunda.com>
2018-06-19 13:39:10 +0200

    test(icons): mock icons for jest tests

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,7 +3,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
 jest.mock('modules/components/Icon', () =>
-  import('modules/components/__mocks__/Icon')
+  require('modules/components/__mocks__/Icon')
 );
 
 Enzyme.configure({adapter: new Adapter()});
