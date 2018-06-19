9ed58db5c307446d21cda013fd0bdf2d0842831b
Sebastian Menski <sebastian@menski.org>
2018-06-19 19:27:54 +0200

    chore(build-tools): format code

--- build-tools/src/main/java/io/zeebe/ZeebeTestListener.java
+++ build-tools/src/main/java/io/zeebe/ZeebeTestListener.java
@@ -7,16 +7,15 @@ import org.slf4j.LoggerFactory;
 
 public class ZeebeTestListener extends RunListener {
 
-    private static final Logger LOG = LoggerFactory.getLogger("io.zeebe.test");
+  private static final Logger LOG = LoggerFactory.getLogger("io.zeebe.test");
 
-    @Override
-    public void testStarted(Description description) throws Exception {
-        LOG.info("Test started: {}", description.getDisplayName());
-    }
+  @Override
+  public void testStarted(Description description) throws Exception {
+    LOG.info("Test started: {}", description.getDisplayName());
+  }
 
-    @Override
-    public void testFinished(Description description) throws Exception {
-        LOG.info("Test finished: {}", description.getDisplayName());
-    }
-
+  @Override
+  public void testFinished(Description description) throws Exception {
+    LOG.info("Test finished: {}", description.getDisplayName());
+  }
 }
