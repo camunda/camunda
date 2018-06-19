0557e46e998bffba85af6a8f53929d35db81fbba
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-06-19 09:52:39 +0200

    fix(Styleguide): do not load mock component implementations

--- client/styleguide.config.js
+++ client/styleguide.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  components: 'src/modules/components/**/!(index|styled|service|api).js',
+  components:
+    'src/modules/components/**/!(__mocks__)/!(index|styled|service|api).js',
   require: [path.join(__dirname, 'src/index.css')],
   styleguideComponents: {
     Wrapper: path.join(__dirname, 'src/styleguide/Wrapper')
