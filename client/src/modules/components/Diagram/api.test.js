9bae30324504f0c8a4f540be5946d9b20c2c7dda
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-06-20 10:41:50 +0200

    feat(diagram): sync diagram with backend
    Related to OPE-7

---
+++ client/src/modules/components/Diagram/api.test.js
@@ -0,0 +1,28 @@
+import * as wrappers from 'modules/request/wrappers';
+
+import {mockResolvedAsyncFn} from 'modules/testUtils';
+
+import {workflowXML} from './api';
+
+describe('diagram api', () => {
+  describe('workflowXML', () => {
+    it('should call get with right url and provided workflow id', async () => {
+      // given
+      const id = 'foo';
+      const expectedURL = `/api/workflows/${id}/xml`;
+      const mockXML = '<foo />';
+      const successResponse = {
+        text: mockResolvedAsyncFn(mockXML)
+      };
+      wrappers.get = mockResolvedAsyncFn(successResponse);
+
+      // when
+      const response = await workflowXML(id);
+
+      // then
+      expect(wrappers.get.mock.calls[0][0]).toBe(expectedURL);
+      expect(successResponse.text).toHaveBeenCalledTimes(1);
+      expect(response).toBe(mockXML);
+    });
+  });
+});
