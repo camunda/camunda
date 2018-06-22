78afa3b3c307f274879c16b7e7b6b4ae048b37ec
Christian Lipphardt <christian.lipphardt@googlemail.com>
2018-06-22 08:59:45 +0200

    chore(ci): remove deprecated / useless scripts and ci jobs
    related to #SRE-156

--- .ci/scripts/copy-snapshots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -xe
-
-DIRECTORY=./backend/target/it-elasticsearch/_snapshots/
-
-if [ -d "$DIRECTORY" ]; then
-    echo "folder found, copying resources"
-    tar -czvf snapshots.tar.gz ./backend/target/it-elasticsearch/_snapshots/*
-    mv ./snapshots.tar.gz ./backend/target/it-elasticsearch/_snapshots/
-fi
