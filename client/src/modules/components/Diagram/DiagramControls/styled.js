9e8eb81bf13ecc1936c33093ed744e37d0a18113
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-06-27 10:47:31 +0200

    style(diagram): adjust diagram controls styles
    Related to OPE-7

--- client/src/modules/components/Diagram/DiagramControls/styled.js
+++ client/src/modules/components/Diagram/DiagramControls/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import {Colors, themed, themeStyle} from 'modules/theme';
+import Button from 'modules/components/Button';
 
 export const DiagramControls = styled.div`
   display: flex;
@@ -9,39 +9,26 @@ export const DiagramControls = styled.div`
   right: 20px;
   bottom: 60px;
   z-index: 2;
+  width: 28px;
 `;
 
-export const Box = themed(styled.button`
-  font-size: 20px;
-  border: solid 1px
-    ${themeStyle({
-      dark: Colors.uiDark06,
-      light: Colors.uiLight03
-    })};
-  background-color: ${themeStyle({
-    dark: Colors.uiDark05,
-    light: Colors.uiLight05
-  })};
-  padding: 6px 12px;
-`);
+export const Box = styled(Button)`
+  width: 100%;
+  padding: 5px;
+  height: 28px;
+`;
 
 export const ZoomReset = styled(Box)`
   border-radius: 3px;
   margin-bottom: 10px;
 `;
 
-export const ZoomIn = themed(styled(Box)`
-  border-top-left-radius: 3px;
-  border-top-right-radius: 3px;
-  color: ${themeStyle({
-    dark: '#ffffff'
-  })};
-`);
+export const ZoomIn = styled(Box)`
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+`;
 
-export const ZoomOut = themed(styled(Box)`
-  border-bottom-left-radius: 3px;
-  border-bottom-right-radius: 3px;
-  color: ${themeStyle({
-    dark: '#ffffff'
-  })};
-`);
+export const ZoomOut = styled(Box)`
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+`;
