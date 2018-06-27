ac7c558d2bc66a44e37520fc9f065c355f9af23b
Raluca Stan <raluca.stan@camunda.com>
2018-06-27 15:51:07 +0200

    test(instances): update parseFilterForRequest for the new api implementation

--- client/src/App/Instances/service.test.js
+++ client/src/App/Instances/service.test.js
@@ -11,29 +11,29 @@ describe('Instances service', () => {
       const filter = {active: true, incidents: true};
 
       expect(parseFilterForRequest(filter).running).toBe(true);
-      expect(parseFilterForRequest(filter).withIncidents).toBe(true);
-      expect(parseFilterForRequest(filter).withoutIncidents).toBe(true);
+      expect(parseFilterForRequest(filter).incidents).toBe(true);
+      expect(parseFilterForRequest(filter).active).toBe(true);
     });
     it('should parse only active filter selection', () => {
       const filter = {active: true, incidents: false};
 
       expect(parseFilterForRequest(filter).running).toBe(true);
-      expect(parseFilterForRequest(filter).withIncidents).toBe(false);
-      expect(parseFilterForRequest(filter).withoutIncidents).toBe(true);
+      expect(parseFilterForRequest(filter).incidents).toBe(false);
+      expect(parseFilterForRequest(filter).active).toBe(true);
     });
     it('should parse only incidents filter selection', () => {
       const filter = {active: false, incidents: true};
 
       expect(parseFilterForRequest(filter).running).toBe(true);
-      expect(parseFilterForRequest(filter).withIncidents).toBe(true);
-      expect(parseFilterForRequest(filter).withoutIncidents).toBe(false);
+      expect(parseFilterForRequest(filter).incidents).toBe(true);
+      expect(parseFilterForRequest(filter).active).toBe(false);
     });
     it('should parse empty filter selection', () => {
       const filter = {active: false, incidents: false};
 
       expect(parseFilterForRequest(filter).running).toBe(false);
-      expect(parseFilterForRequest(filter).withIncidents).toBe(false);
-      expect(parseFilterForRequest(filter).withoutIncidents).toBe(false);
+      expect(parseFilterForRequest(filter).incidents).toBe(false);
+      expect(parseFilterForRequest(filter).active).toBe(false);
     });
   });
 
