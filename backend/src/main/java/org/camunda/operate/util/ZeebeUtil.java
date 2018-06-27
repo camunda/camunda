42588e34e1cd9192ba6707f761599b6d23b13537
Svetlana Dorokhova <svetlana.dorokhova@camunda.com>
2018-06-27 11:58:39 +0200

    fix(backend): compilation fix related with changes in Zeebe Client

--- backend/src/main/java/org/camunda/operate/util/ZeebeUtil.java
+++ backend/src/main/java/org/camunda/operate/util/ZeebeUtil.java
@@ -98,7 +98,7 @@ public class ZeebeUtil {
       .jobType(jobType)
       .handler((jobClient, job) -> {
         //incidents for outputMapping for taskA
-        jobClient.newCompleteCommand(job).withoutPayload().send().join();
+        jobClient.newCompleteCommand(job).send().join();
       })
       .name(workerName)
       .timeout(Duration.ofSeconds(2))
