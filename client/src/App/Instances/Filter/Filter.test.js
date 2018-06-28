2ecbdebb7c41875fa8c7c11a39f59b0f1b00cf5e
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-06-28 12:53:23 +0200

    teat(Filter): add basic tests
    related to OPE-53

--- client/src/App/Instances/Filter/Filter.test.js
+++ client/src/App/Instances/Filter/Filter.test.js
@@ -5,33 +5,76 @@ import Filter from './';
 
 describe('Filter', () => {
   let node;
-
+  const mockOnChange = jest.fn();
+  let active = false;
+  let incidents = false;
   beforeEach(() => {
-    // node = shallow(<CompletedFilter />);
+    node = shallow(
+      <Filter
+        type={'running'}
+        filter={{active, incidents}}
+        onChange={mockOnChange}
+      />
+    );
   });
 
-  it('should render three checkboxes with labels', () => {
-    //given
-    // expect(node.exists()).toBe(true);
-    //when
-    //then
+  it('should render three checkboxes', () => {
+    expect(node.children().find('Checkbox').length).toBe(3);
   });
 
-  it('should render the Checkboxes to be unticked by default', () => {
+  it('should render the Checkboxes "checked" according to the filter props', () => {
     //given
-    //when
+    const activityFilter = false;
+    const incidentsFilter = false;
+    expect(node.instance().props.filter.active).toBe(activityFilter);
+    expect(node.instance().props.filter.incidents).toBe(incidentsFilter);
     //then
+    expect(
+      node
+        .childAt(1)
+        .childAt(1)
+        .find('Checkbox')
+        .props().isChecked
+    ).toBe(incidentsFilter);
+
+    expect(
+      node
+        .childAt(1)
+        .childAt(0)
+        .find('Checkbox')
+        .props().isChecked
+    ).toBe(activityFilter);
   });
 
-  it('should set parent to checked if both children are selected', () => {
-    //given
-    //when
-    //then
+  it('parent checkbox should be indetermniate', () => {
+    let node;
+    const mockOnChange = jest.fn();
+    let active = false;
+    let incidents = true;
+
+    node = shallow(
+      <Filter
+        type={'running'}
+        filter={{active, incidents}}
+        onChange={mockOnChange}
+      />
+    );
+    expect(node.childAt(0).find('Checkbox')).toMatchSnapshot();
   });
 
-  it('should set parent to indeterminate if only one child is selected', () => {
-    //given
-    //when
-    //then
+  it('parent checkbox should be unchecked', () => {
+    let node;
+    const mockOnChange = jest.fn();
+    let active = false;
+    let incidents = false;
+
+    node = shallow(
+      <Filter
+        type={'running'}
+        filter={{active, incidents}}
+        onChange={mockOnChange}
+      />
+    );
+    expect(node.childAt(0).find('Checkbox')).toMatchSnapshot();
   });
 });
