672b4bd3a58e9b5533233d6587fa4cc0c087929f
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-07-11 17:40:59 +0200

    feat(Selections): add demo data
    related to OPE-102

--- client/src/modules/api/instances/instances.js
+++ client/src/modules/api/instances/instances.js
@@ -19,3 +19,45 @@ export async function fetchWorkflowInstancesCount(payload) {
   const resJson = await response.json();
   return resJson.count;
 }
+
+export async function batchRety() {
+  // TODO: TBD
+  // const body = { instances }
+  //  await post('/api/selection/...', body);
+}
+
+export async function fetchWorkflowInstanceBySelection(payload) {
+  //TODO: replace mock data;
+  const demoInstance = {
+    id: '4294984040',
+    workflowId: '1',
+    startDate: '2018-07-10T08:58:58.073+0000',
+    endDate: null,
+    state: 'ACTIVE',
+    businessKey: 'demoProcess',
+    incidents: [
+      {
+        id: '4295665536',
+        errorType: 'IO_MAPPING_ERROR',
+        errorMessage: 'No data found for query $.foo.',
+        state: 'ACTIVE',
+        activityId: 'taskA',
+        activityInstanceId: '4294984912',
+        jobId: null
+      }
+    ],
+    activities: []
+  };
+
+  const times = x => f => {
+    if (x > 0) {
+      f();
+      times(x - 1)(f);
+    }
+  };
+
+  const workfowInstances = [];
+  times(10)(() => workfowInstances.push(demoInstance));
+
+  return {workfowInstances, totalCount: 145};
+}
