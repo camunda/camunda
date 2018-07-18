267c87af67a21416f8c5fab2d9f2cd555e8cbd63
OmranAbazid <omran.1994@gmail.com>
2018-07-18 15:52:22 +0200

    fix(Logout): use withErrorHandling HOC to handle logout error
    related to OPT-21

--- client/src/components/Header/LogoutButton.js
+++ client/src/components/Header/LogoutButton.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
 import {destroy} from 'credentials';
 import {get} from 'request';
+import {withErrorHandling} from 'HOC';
 
 import './LogoutButton.css';
 
-export default function LogoutButton() {
-  return (
-    <div className="LogoutButton">
-      <Link to="/login" onClick={logout} title="Log out">
-        Logout
-      </Link>
-    </div>
-  );
-}
+export default withErrorHandling(
+  class LogoutButton extends React.Component {
+    state = {
+      redirect: false
+    };
 
-async function logout() {
-  try {
-    await get('/api/authentication/logout');
-    destroy();
-  } catch (error) {
-    alert('Failed to logout due to internal server error');
+    logout = () => {
+      this.props.mightFail(
+        get('/api/authentication/logout'),
+        () => {
+          destroy();
+          this.setState({
+            redirect: true
+          });
+        },
+        () => {
+          alert('Could not logout');
+        }
+      );
+    };
+
+    render() {
+      return (
+        <div className="LogoutButton">
+          <a onClick={this.logout} title="Log out">
+            Logout
+          </a>
+          {this.state.redirect && (
+            <Redirect
+              to={{
+                pathname: '/login'
+              }}
+            />
+          )}
+        </div>
+      );
+    }
   }
-}
+);
