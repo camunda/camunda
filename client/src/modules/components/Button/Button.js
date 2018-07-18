cfc2f6d368a804f1caccc3ea3eaa3f7d96be53fa
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-07-18 10:11:56 +0200

    chore(Button): remove unnecessary fragment
    Related to OPE-93

--- client/src/modules/components/Button/Button.js
+++ client/src/modules/components/Button/Button.js
@@ -4,11 +4,7 @@ import PropTypes from 'prop-types';
 import * as Styled from './styled';
 
 export default function Button(props) {
-  return (
-    <Fragment>
-      <Styled.Button {...props} />
-    </Fragment>
-  );
+  return <Styled.Button {...props} />;
 }
 
 Button.propTypes = {
