11f99defbcb256a9bb8fc4e5fc917f4a947f4347
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-07-23 15:48:57 +0200

    chore(Selections): remove redundant SelectionDisplay Component

--- client/src/App/Instances/SelectionDisplay/styled.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import {themed, themeStyle, Colors} from 'modules/theme';
-
-export const DebugView = themed(styled.pre`
-  color: ${themeStyle({dark: '#ffffff', light: Colors.uiLight06})};
-`);
