16a784c002ed5ee84047d73119fd2cb1a0dd5c7a
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-08-09 17:07:47 +0200

    fix(DropdownOption): allow isActive state
    related to OPT-1377

--- client/src/modules/components/Dropdown/DropdownOption.js
+++ client/src/modules/components/Dropdown/DropdownOption.js
@@ -3,12 +3,12 @@ import {Icon} from 'components';
 import './DropdownOption.css';
 import classnames from 'classnames';
 
-export default function DropdownOption(props) {
+export default function DropdownOption({active, ...props}) {
   return (
     <div
       {...props}
       onClick={evt => !props.disabled && props.onClick && props.onClick(evt)}
-      className={classnames('DropdownOption', props.className)}
+      className={classnames('DropdownOption', props.className, {'is-active': active})}
     >
       {props.checked && <Icon className="checkMark" type="check-small" size="10px" />}
       {props.children}
