16a6c60e8d38bffe73c12f8d9cda3133eaacd477
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-08-14 09:03:05 +0200

    test(SelectionList): add tests
    related to OPE-106

--- client/src/App/Instances/Selections/SelectionList/SelectionList.test.js
+++ client/src/App/Instances/Selections/SelectionList/SelectionList.test.js
@@ -1,30 +1,51 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {mockResolvedAsyncFn} from 'modules/testUtils';
 
 import SelectionList from './SelectionList';
-import * as selectionsApi from 'modules/api/selections/selections';
 
-selectionsApi.batchRetry = mockResolvedAsyncFn();
+import * as Styled from './styled';
 
 describe('SelectionList', () => {
   let node;
   let selections;
 
   beforeEach(async () => {
-    selections = [];
+    selections = [
+      {
+        queries: [],
+        selectionId: 1,
+        totalCount: 1,
+        workflowInstances: []
+      }
+    ];
     node = shallow(
       <SelectionList
         selections={selections}
-        onDelete={jest.fn()}
-        onToggle={jest.fn()}
-        onChange={jest.fn()}
+        deleteSelection={jest.fn()}
+        toggleSelection={jest.fn()}
+        retrySelection={jest.fn()}
       />
     );
   });
 
-  it('should call the retrySelection with the selection query', () => {
-    node.instance().retrySelection();
-    expect(selectionsApi.batchRetry).toHaveBeenCalled();
+  it('should render "no selection text" when no selection exists', () => {
+    //when
+    selections = [];
+    node.setProps({selections});
+
+    // then
+    expect(node.find(Styled.NoSelectionWrapper)).toMatchSnapshot();
+  });
+
+  it('should render Selection when existent', () => {
+    expect(node.find(Styled.SelectionWrapper));
+  });
+
+  it('should evaluate which Selection is currently open', () => {
+    // given
+    node.setProps({openSelection: 1});
+
+    //then
+    expect(node.find('Selection').props().isOpen).toBe(true);
   });
 });
