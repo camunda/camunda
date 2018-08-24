daniloferreira/zeebe

feat(exporters): load, configure, and validate exporters
- adds isolated JAR class loader
- adds repository to load and cache class loaders from 3rd party JARs
- adds exporter descriptor which knows how to create a configured
exporter instance
- adds exporter repository which can load and cache exporters from
configuration
- adds exporter config to broker configuration

related to: zeebe-io/zeebe#1170

broker-core/src/test/java/io/zeebe/broker/exporter/repo/ExporterRepositoryTest.java
@@ -1,0 +1,150 @@
+/*
+ * Zeebe Broker Core
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.zeebe.broker.exporter.repo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.zeebe.broker.exporter.jar.ExporterJarLoadException;
+import io.zeebe.broker.exporter.util.ControlledTestExporter;
+import io.zeebe.broker.exporter.util.JarCreatorRule;
+import io.zeebe.broker.exporter.util.TestJarExporter;
+import io.zeebe.broker.system.configuration.ExporterCfg;
+import io.zeebe.exporter.context.Context;
+import io.zeebe.exporter.record.Record;
+import io.zeebe.exporter.spi.Exporter;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
+
+public class ExporterRepositoryTest {
+  private TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private JarCreatorRule jarCreator = new JarCreatorRule(temporaryFolder);
+
+  @Rule public RuleChain chain = RuleChain.outerRule(temporaryFolder).around(jarCreator);
+
+  private ExporterRepository repository = new ExporterRepository();
+
+  @Test
+  public void shouldCacheDescriptorOnceLoaded() throws ExporterLoadException {
+    // given
+    final String id = "myExporter";
+    final Class<? extends Exporter> exporterClass = TestJarExporter.class;
+
+    // when
+    final ExporterDescriptor descriptor = repository.load(id, exporterClass, null);
+
+    // then
+    assertThat(descriptor).isNotNull();
+    assertThat(repository.load(id, exporterClass)).isSameAs(descriptor);
+  }
+
+  @Test
+  public void shouldFailToLoadIfExporterExceptionallyInvalid() {
+    // given
+    final String id = "exporter";
+    final Class<? extends Exporter> exporterClass = ExceptionallyInvalidExporter.class;
+
+    // then
+    assertThatThrownBy(() -> repository.load(id, exporterClass))
+        .isInstanceOf(ExporterLoadException.class)
+        .hasCauseInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  public void shouldFailToLoadIfExporterInvalid() {
+    // given
+    final String id = "exporter";
+    final Class<? extends Exporter> exporterClass = InvalidExporter.class;
+
+    // then
+    assertThatThrownBy(() -> repository.load(id, exporterClass))
+        .isInstanceOf(ExporterLoadException.class)
+        .hasNoCause();
+  }
+
+  @Test
+  public void shouldLoadInternalExporter()
+      throws ExporterLoadException, ExporterJarLoadException, InstantiationException,
+          IllegalAccessException {
+    // given
+    final ExporterCfg config = new ExporterCfg();
+    config.setClassName(ControlledTestExporter.class.getCanonicalName());
+    config.setId("controlled");
+    config.setJarPath(null);
+
+    // when
+    final ExporterDescriptor descriptor = repository.load(config);
+
+    // then
+    assertThat(config.isExternal()).isFalse();
+    assertThat(descriptor.newInstance()).isInstanceOf(ControlledTestExporter.class);
+  }
+
+  @Test
+  public void shouldLoadExternalExporter() throws Exception {
+    // given
+    final Class<? extends Exporter> exportedClass = TestJarExporter.class;
+    final File jarFile = jarCreator.create(exportedClass);
+    final ExporterCfg config = new ExporterCfg();
+    final Map<String, Object> args = new HashMap<>();
+
+    // when
+    config.setClassName(exportedClass.getCanonicalName());
+    config.setId("exported");
+    config.setJarPath(jarFile.getAbsolutePath());
+    config.setArgs(args);
+
+    args.put("foo", 1);
+    args.put("bar", false);
+
+    // when
+    final ExporterDescriptor descriptor = repository.load(config);
+
+    // then
+    assertThat(config.isExternal()).isTrue();
+    assertThat(descriptor.getConfiguration().getArguments()).isEqualTo(config.getArgs());
+    assertThat(descriptor.getConfiguration().getId()).isEqualTo(config.getId());
+    assertThat(descriptor.newInstance().getClass().getCanonicalName())
+        .isEqualTo(exportedClass.getCanonicalName());
+  }
+
+  static class ExceptionallyInvalidExporter implements Exporter {
+    @Override
+    public boolean validate(Context context) throws Exception {
+      throw new IllegalArgumentException("what");
+    }
+
+    @Override
+    public void export(Record record) {}
+  }
+
+  static class InvalidExporter implements Exporter {
+    @Override
+    public boolean validate(Context context) throws Exception {
+      return false;
+    }
+
+    @Override
+    public void export(Record record) {}
+  }
+}

broker-core/src/test/java/io/zeebe/broker/system/ConfigurationTest.java
@@ -22,15 +22,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.zeebe.broker.system.configuration.BrokerCfg;
 import io.zeebe.broker.system.configuration.Environment;
+import io.zeebe.broker.system.configuration.ExporterCfg;
 import io.zeebe.broker.system.configuration.NetworkCfg;
 import io.zeebe.broker.system.configuration.SocketBindingClientApiCfg;
 import io.zeebe.broker.system.configuration.SocketBindingManagementCfg;
 import io.zeebe.broker.system.configuration.SocketBindingReplicationCfg;
 import io.zeebe.broker.system.configuration.SocketBindingSubscriptionCfg;
 import io.zeebe.broker.system.configuration.TomlConfigurationReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import org.assertj.core.api.Condition;
 import org.junit.Test;
 
 public class ConfigurationTest {
@@ -104,6 +107,35 @@ public class ConfigurationTest {
         MANAGEMENT_PORT + offset,
         REPLICATION_PORT + offset,
         SUBSCRIPTION_PORT + offset);
+  }
+
+  @Test
+  public void shouldExpandExporterJarPathRelativeToBrokerBaseIffPresent() {
+    // given
+    final InputStream input =
+        new ByteArrayInputStream(
+            ("[[exporter]]\n"
+                    + "id=\"external\"\n"
+                    + "jarPath=\"exporters/exporter.jar\"\n"
+                    + "[[exporter]]\n"
+                    + "id=\"internal-1\"\n"
+                    + "jarPath=\"\"\n"
+                    + "[[exporter]]\n"
+                    + "id=\"internal-2\"")
+                .getBytes());
+    final BrokerCfg config = TomlConfigurationReader.read(input);
+    final String base = "/opt/zeebe";
+
+    // when
+    config.init(base);
+
+    // then
+    assertThat(config.getExporters()).hasSize(3);
+    assertThat(config.getExporters().get(0))
+        .hasFieldOrPropertyWithValue("jarPath", base + "/exporters/exporter.jar")
+        .is(new Condition<>(ExporterCfg::isExternal, "is external"));
+    assertThat(config.getExporters().get(1).isExternal()).isFalse();
+    assertThat(config.getExporters().get(2).isExternal()).isFalse();
   }
 
   private BrokerCfg readConfig(String name) {

camunda/camunda

broker-core/src/test/java/io/zeebe/broker/exporter/repo/ExporterRepositoryTest.java
@@ -1,0 +1,169 @@
+/*
+ * Zeebe Broker Core
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.zeebe.broker.exporter.repo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.zeebe.broker.exporter.jar.ExporterJarLoadException;
+import io.zeebe.broker.exporter.util.ControlledTestExporter;
+import io.zeebe.broker.exporter.util.JarCreatorRule;
+import io.zeebe.broker.exporter.util.TestJarExporter;
+import io.zeebe.broker.system.configuration.ExporterCfg;
+import io.zeebe.exporter.context.Context;
+import io.zeebe.exporter.record.Record;
+import io.zeebe.exporter.spi.Exporter;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TemporaryFolder;
+
+public class ExporterRepositoryTest {
+  private TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private JarCreatorRule jarCreator = new JarCreatorRule(temporaryFolder);
+
+  @Rule public RuleChain chain = RuleChain.outerRule(temporaryFolder).around(jarCreator);
+
+  private ExporterRepository repository = new ExporterRepository();
+
+  @Test
+  public void shouldCacheDescriptorOnceLoaded() throws ExporterLoadException {
+    // given
+    final String id = "myExporter";
+    final Class<? extends Exporter> exporterClass = TestJarExporter.class;
+
+    // when
+    final ExporterDescriptor descriptor = repository.load(id, exporterClass, null);
+
+    // then
+    assertThat(descriptor).isNotNull();
+    assertThat(repository.load(id, exporterClass)).isSameAs(descriptor);
+  }
+
+  @Test
+  public void shouldFailToLoadIfExporterInvalid() {
+    // given
+    final String id = "exporter";
+    final Class<? extends Exporter> exporterClass = InvalidExporter.class;
+
+    // then
+    assertThatThrownBy(() -> repository.load(id, exporterClass))
+        .isInstanceOf(ExporterLoadException.class)
+        .hasCauseInstanceOf(IllegalStateException.class);
+  }
+
+  @Test
+  public void shouldLoadInternalExporter()
+      throws ExporterLoadException, ExporterJarLoadException, InstantiationException,
+          IllegalAccessException {
+    // given
+    final ExporterCfg config = new ExporterCfg();
+    config.setClassName(ControlledTestExporter.class.getCanonicalName());
+    config.setId("controlled");
+    config.setJarPath(null);
+
+    // when
+    final ExporterDescriptor descriptor = repository.load(config);
+
+    // then
+    assertThat(config.isExternal()).isFalse();
+    assertThat(descriptor.newInstance()).isInstanceOf(ControlledTestExporter.class);
+  }
+
+  @Test
+  public void shouldLoadExternalExporter() throws Exception {
+    // given
+    final Class<? extends Exporter> exportedClass = TestJarExporter.class;
+    final File jarFile = jarCreator.create(exportedClass);
+    final ExporterCfg config = new ExporterCfg();
+    final Map<String, Object> args = new HashMap<>();
+
+    // when
+    config.setClassName(exportedClass.getCanonicalName());
+    config.setId("exported");
+    config.setJarPath(jarFile.getAbsolutePath());
+    config.setArgs(args);
+
+    args.put("foo", 1);
+    args.put("bar", false);
+
+    // when
+    final ExporterDescriptor descriptor = repository.load(config);
+
+    // then
+    assertThat(config.isExternal()).isTrue();
+    assertThat(descriptor.getConfiguration().getArguments()).isEqualTo(config.getArgs());
+    assertThat(descriptor.getConfiguration().getId()).isEqualTo(config.getId());
+    assertThat(descriptor.newInstance().getClass().getCanonicalName())
+        .isEqualTo(exportedClass.getCanonicalName());
+  }
+
+  @Test
+  public void shouldFailToLoadNonExporterClasses() throws IOException {
+    // given
+    final Class exportedClass = ExporterRepository.class;
+    final File jarFile = jarCreator.create(exportedClass);
+    final ExporterCfg config = new ExporterCfg();
+    final Map<String, Object> args = new HashMap<>();
+
+    // when
+    config.setClassName(exportedClass.getCanonicalName());
+    config.setId("exported");
+    config.setJarPath(jarFile.getAbsolutePath());
+    config.setArgs(args);
+
+    // then
+    assertThatThrownBy(() -> repository.load(config))
+        .isInstanceOf(ExporterLoadException.class)
+        .hasCauseInstanceOf(ClassCastException.class);
+  }
+
+  @Test
+  public void shouldFailToLoadNonExistingClass() throws IOException {
+    // given
+    final Class exportedClass = ExporterRepository.class;
+    final File jarFile = jarCreator.create(exportedClass);
+    final ExporterCfg config = new ExporterCfg();
+    final Map<String, Object> args = new HashMap<>();
+
+    // when
+    config.setClassName("xyz.i.dont.Exist");
+    config.setId("exported");
+    config.setJarPath(jarFile.getAbsolutePath());
+    config.setArgs(args);
+
+    // then
+    assertThatThrownBy(() -> repository.load(config))
+        .isInstanceOf(ExporterLoadException.class)
+        .hasCauseInstanceOf(ClassNotFoundException.class);
+  }
+
+  static class InvalidExporter implements Exporter {
+    @Override
+    public void configure(Context context) {
+      throw new IllegalStateException("what");
+    }
+
+    @Override
+    public void export(Record record) {}
+  }
+}

broker-core/src/test/java/io/zeebe/broker/system/ConfigurationTest.java
@@ -22,15 +22,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.zeebe.broker.system.configuration.BrokerCfg;
 import io.zeebe.broker.system.configuration.Environment;
+import io.zeebe.broker.system.configuration.ExporterCfg;
 import io.zeebe.broker.system.configuration.NetworkCfg;
 import io.zeebe.broker.system.configuration.SocketBindingClientApiCfg;
 import io.zeebe.broker.system.configuration.SocketBindingManagementCfg;
 import io.zeebe.broker.system.configuration.SocketBindingReplicationCfg;
 import io.zeebe.broker.system.configuration.SocketBindingSubscriptionCfg;
 import io.zeebe.broker.system.configuration.TomlConfigurationReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import org.assertj.core.api.Condition;
 import org.junit.Test;
 
 public class ConfigurationTest {
@@ -104,6 +107,35 @@ public class ConfigurationTest {
         MANAGEMENT_PORT + offset,
         REPLICATION_PORT + offset,
         SUBSCRIPTION_PORT + offset);
+  }
+
+  @Test
+  public void shouldExpandExporterJarPathRelativeToBrokerBaseIffPresent() {
+    // given
+    final InputStream input =
+        new ByteArrayInputStream(
+            ("[[exporters]]\n"
+                    + "id=\"external\"\n"
+                    + "jarPath=\"exporters/exporter.jar\"\n"
+                    + "[[exporters]]\n"
+                    + "id=\"internal-1\"\n"
+                    + "jarPath=\"\"\n"
+                    + "[[exporters]]\n"
+                    + "id=\"internal-2\"")
+                .getBytes());
+    final BrokerCfg config = TomlConfigurationReader.read(input);
+    final String base = "/opt/zeebe";
+
+    // when
+    config.init(base);
+
+    // then
+    assertThat(config.getExporters()).hasSize(3);
+    assertThat(config.getExporters().get(0))
+        .hasFieldOrPropertyWithValue("jarPath", base + "/exporters/exporter.jar")
+        .is(new Condition<>(ExporterCfg::isExternal, "is external"));
+    assertThat(config.getExporters().get(1).isExternal()).isFalse();
+    assertThat(config.getExporters().get(2).isExternal()).isFalse();
   }
 
   private BrokerCfg readConfig(String name) {