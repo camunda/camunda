47b46bba7eb10fc5d14d6d9568a7581ae0971d4b
OmranAbazid <omran.1994@gmail.com>
2018-08-29 16:59:17 +0200

    chore(services): add loadEntity function to services
    related to OPT-1416

--- client/src/modules/services/index.js
+++ client/src/modules/services/index.js
@@ -7,6 +7,7 @@ export {loadProcessDefinitions} from './LoadProcessDefinitions';
 export {default as processRawData} from './processRawData';
 export {numberParser} from './NumberParser';
 export {isDurationValue} from './isDurationValue';
+export {loadEntity} from './loadEntity';
 
 // unfortunately, there is no syntax like "export * as formatters from './formatters'"
 export const formatters = formattersImport;
---
+++ client/src/modules/services/loadEntity.js
@@ -0,0 +1,17 @@
+import {get} from 'request';
+
+export async function loadEntity(api, numResults, sortBy) {
+  const url = `/api/${api}`;
+
+  const params = {};
+  if (numResults) {
+    params['numResults'] = numResults;
+  }
+
+  if (sortBy) {
+    params['orderBy'] = sortBy;
+  }
+
+  const response = await get(url, params);
+  return await response.json();
+}
