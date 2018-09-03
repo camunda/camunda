b12ef62d2e7f23c1ad05cde34871c3755dd25657
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-09-03 09:49:15 +0200

    feat(SubMenu): Add SubMenu Component
    related to OPE-106

---
+++ client/src/modules/components/Dropdown/SubMenu/SubMenu.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Right} from 'modules/components/Icon';
+
+import * as Styled from './styled';
+
+export default class SubMenu extends React.Component {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ]),
+    label: PropTypes.string,
+    onStateChange: PropTypes.func,
+    isOpen: PropTypes.bool,
+    isFixed: PropTypes.bool
+  };
+
+  state = {submenuActive: false, isFocused: false};
+
+  handleOnClick = evt => {
+    const {isOpen, isFixed} = this.props;
+
+    if (!isOpen) {
+      this.props.onStateChange({
+        isSubMenuOpen: !isOpen,
+        isSubmenuFixed: !isFixed
+      });
+      this.setState({submenuActive: !this.state.submenuActive});
+    }
+
+    if (isOpen) {
+      this.props.onStateChange({
+        isSubmenuFixed: !isFixed
+      });
+      this.setState({submenuActive: !this.state.submenuActive});
+    }
+
+    if (isOpen && isFixed) {
+      this.props.onStateChange({
+        isSubmenuFixed: !isFixed,
+        isSubMenuOpen: !isOpen
+      });
+    }
+  };
+
+  handleMouseLeave = evt => {
+    const {isOpen, isFixed} = this.props;
+
+    if (!isFixed && isOpen) {
+      this.props.onStateChange({
+        isSubMenuOpen: !isOpen
+      });
+      this.setState({submenuActive: false});
+    }
+  };
+
+  handleMenuMouseOver = () => {
+    this.setState({submenuActive: true});
+  };
+
+  handleButtonMouseOver = evt => {
+    const {isOpen} = this.props;
+
+    if (!isOpen) {
+      this.props.onStateChange({
+        isSubMenuOpen: !isOpen
+      });
+    }
+  };
+
+  render() {
+    const {isOpen, children, label} = this.props;
+    return (
+      <Styled.SubMenu onMouseLeave={this.handleMouseLeave}>
+        <Styled.SubMenuButton
+          onClick={() => this.handleOnClick()}
+          onMouseOver={this.handleButtonMouseOver}
+          submenuActive={this.state.submenuActive}
+        >
+          <span>{label}</span>
+          <Right />
+        </Styled.SubMenuButton>
+        {isOpen ? (
+          <Styled.Ul>
+            {React.Children.map(children, (child, index) => (
+              <Styled.Li onMouseOver={this.handleMenuMouseOver} key={index}>
+                {child}
+              </Styled.Li>
+            ))}
+          </Styled.Ul>
+        ) : null}
+      </Styled.SubMenu>
+    );
+  }
+}
---
+++ client/src/modules/components/Dropdown/SubMenu/styled.js
@@ -0,0 +1,123 @@
+import styled from 'styled-components';
+import {Colors, themed, themeStyle} from 'modules/theme';
+
+export const SubMenu = themed(styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`);
+
+export const SubMenuButton = themed(styled.button`
+  /* Display & Box Model */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+  padding: 0 10px;
+
+  outline: none;
+  border: none;
+  border-radius: 0 0 2px 2px;
+
+  /* Color */
+  background: ${({submenuActive}) =>
+    submenuActive
+      ? themeStyle({
+          dark: Colors.darkActive,
+          light: Colors.lightActive
+        })
+      : 'none'};
+  color: ${themeStyle({
+    dark: 'rgba(255, 255, 255, 0.9)',
+    light: 'rgba(98, 98, 110, 0.9)'
+  })};
+
+  /* Text */
+  font-size: 15px;
+  font-weight: 600;
+  text-align: left;
+  line-height: 36px;
+
+  /* Other */
+  cursor: pointer;
+
+  &:hover {
+    background: ${themeStyle({
+      dark: Colors.uiDark06,
+      light: Colors.uiLight05
+    })};
+  }
+
+  &:active {
+    background: ${themeStyle({
+      dark: Colors.darkActive,
+      light: Colors.lightActive
+    })};
+  }
+
+  &:focus {
+    box-shadow: ${themeStyle({
+      dark: `0 0 0 1px ${Colors.focusOuter},0 0 0 4px ${Colors.darkFocusInner}`,
+      light: `0 0 0 1px ${Colors.focusOuter}, 0 0 0 4px ${
+        Colors.lightFocusInner
+      }`
+    })};
+  }
+`);
+
+export const Ul = themed(styled.ul`
+  /* Positioning */
+  position: absolute;
+  right: -44px;
+  bottom: 4px;
+
+  /* Display & Box Model */
+  width: 45px;
+  padding-left: 0px;
+  box-shadow: 0 0 2px 0
+    ${themeStyle({
+      dark: 'rgba(0, 0, 0, 0.6)',
+      light: ' rgba(0, 0, 0, 0.2)'
+    })};
+
+  /* Color */
+  background: ${themeStyle({
+    dark: Colors.uiDark04,
+    light: Colors.uiLight02
+  })};
+  color: ${themeStyle({
+    dark: '#ffffff',
+    light: Colors.uiLight06
+  })};
+
+  /* Other */
+  list-style-type: none;
+  border-radius: 3px;
+  border: 1px solid
+    ${themeStyle({dark: Colors.uiDark06, light: Colors.uiLight05})};
+`);
+
+export const Li = themed(styled.li`
+  /* Add Border between options */
+  &:not(:last-child) {
+    border-bottom: 1px solid
+      ${themeStyle({
+        dark: Colors.uiDark06,
+        light: Colors.uiLight05
+      })};
+  }
+
+  &:first-child button {
+    border-radius: 2px 2px 0 0;
+  }
+  &:last-child button {
+    border-radius: 0 0 2px 2px;
+  }
+
+  /* Border radius if only one child exists */
+  &:first-child:last-child button {
+    border-radius: 2px 2px 2px 2px;
+  }
+`);
