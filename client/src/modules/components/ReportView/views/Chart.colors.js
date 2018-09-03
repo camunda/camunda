124189787482870dc703e2c68c5214339f1eff34
Sebastian Stamm <sebastian.stamm@camunda.com>
2018-09-03 15:20:14 +0200

    fix(Dashboard): make pie chart labels readable in dark mode
    related to OPT-1393

---
+++ client/src/modules/components/ReportView/views/Chart.colors.js
@@ -0,0 +1,17 @@
+export const darkColors = {
+  bar: '#1991c8',
+  targetBar: '#A62A31',
+  grid: 'rgba(255,255,255,0.1)',
+  label: '#ddd',
+  area: '#4d5356',
+  border: '#333'
+};
+
+export const lightColors = {
+  bar: '#1991c8',
+  targetBar: '#A62A31',
+  grid: 'rgba(0,0,0,0.1)',
+  label: '#666',
+  area: '#e5f2f8',
+  border: '#fff'
+};
--- client/src/modules/components/ReportView/views/Chart.js
+++ client/src/modules/components/ReportView/views/Chart.js
@@ -8,24 +8,7 @@ import {formatters} from 'services';
 import {themed} from 'theme';
 
 import './Chart.css';
-
-const darkColors = {
-  bar: '#1991c8',
-  targetBar: '#A62A31',
-  grid: 'rgba(255,255,255,0.1)',
-  label: '#ddd',
-  area: '#4d5356',
-  border: '#333'
-};
-
-const lightColors = {
-  bar: '#1991c8',
-  targetBar: '#A62A31',
-  grid: 'rgba(0,0,0,0.1)',
-  label: '#666',
-  area: '#e5f2f8',
-  border: '#fff'
-};
+import {darkColors, lightColors} from './Chart.colors.js';
 
 const {convertToMilliseconds} = formatters;
 
@@ -172,7 +155,7 @@ export default themed(
 
     createPieOptions = () => {
       return {
-        legend: {display: true}
+        legend: {display: true, labels: {fontColor: this.getColorFor('label')}}
       };
     };
 
