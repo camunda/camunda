41fe3e34644c831bc9f52cdaab0e93cedc58e483
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-09-04 15:42:19 +0200

    test(SelectionList): add props test
    related to OPE-103

--- client/src/App/Instances/Selections/SelectionList/SelectionList.test.js
+++ client/src/App/Instances/Selections/SelectionList/SelectionList.test.js
@@ -2,7 +2,11 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import SelectionList from './SelectionList';
+import ContextualMessage from 'modules/components/ContextualMessage';
+
 import {NO_SELECTIONS_MESSAGE} from './constants';
+
+import {xTimes, createSelection} from 'modules/testUtils';
 
 import * as Styled from './styled';
 
@@ -11,14 +15,10 @@ describe('SelectionList', () => {
   let selections;
 
   beforeEach(async () => {
-    selections = [
-      {
-        queries: [],
-        selectionId: 1,
-        totalCount: 1,
-        workflowInstances: []
-      }
-    ];
+    selections = [];
+
+    xTimes(1)(index => selections.push(createSelection(index)));
+
     node = shallow(
       <SelectionList
         selections={selections}
@@ -29,7 +29,7 @@ describe('SelectionList', () => {
     );
   });
 
-  it('should render "no selection text" when no selection exists', () => {
+  it('should render a message when no selection exists', () => {
     //when
     selections = [];
     node.setProps({selections});
@@ -41,15 +41,32 @@ describe('SelectionList', () => {
     expect(node).toMatchSnapshot();
   });
 
+  it('should render contexual message when max. number of selections is reached', () => {
+    // given
+    selections = [];
+    expect(node.find(ContextualMessage)).not.toExist();
+
+    // when
+    xTimes(10)(index => selections.push(createSelection(index)));
+    node.setProps({selections});
+
+    // then
+    expect(node.find(ContextualMessage)).toExist();
+  });
+
   it('should render Selection when existent', () => {
     expect(node.find(Styled.MessageWrapper));
   });
 
   it('should evaluate which Selection is currently open', () => {
-    // given
+    // when
     node.setProps({openSelection: 1});
 
-    //then
+    // then
     expect(node.find('Selection').props().isOpen).toBe(true);
+  });
+
+  it('should pass props to the Selection component', () => {
+    expect(node.find('Selection').props()).toMatchSnapshot();
   });
 });
--- client/src/modules/testUtils.js
+++ client/src/modules/testUtils.js
@@ -20,3 +20,27 @@ export const mockResolvedAsyncFn = value => {
 export const mockRejectedAsyncFn = value => {
   return jest.fn(() => Promise.reject(value));
 };
+
+/**
+ * @returns a higher order function which executes the wrapped method x times;
+ * @param {*} x number of times the method should be executed
+ */
+export const xTimes = x => method => {
+  if (x > 0) {
+    method(x);
+    xTimes(x - 1)(method);
+  }
+};
+
+/**
+ * @returns a mocked Selection Object with a unique id
+ * @param {*} id num value to create unique selection;
+ */
+export const createSelection = id => {
+  return {
+    queries: [],
+    selectionId: id,
+    totalCount: 1,
+    workflowInstances: []
+  };
+};
