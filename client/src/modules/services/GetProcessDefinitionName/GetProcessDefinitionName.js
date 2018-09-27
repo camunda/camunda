7470ed6e1b9d2bbca012f33b3bf309edad65eef6
OmranAbazid <omran.1994@gmail.com>
2018-09-27 12:21:39 +0200

    fix(Analysis): always return empty string for process defs with no name
    related to OPT-1478

--- client/src/modules/services/GetProcessDefinitionName/GetProcessDefinitionName.js
+++ client/src/modules/services/GetProcessDefinitionName/GetProcessDefinitionName.js
@@ -3,5 +3,5 @@ export default function extractProcessDefinitionName(processDefinitionXml) {
   const xmlDoc = parser.parseFromString(processDefinitionXml, 'text/xml');
 
   const processNode = xmlDoc.getElementsByTagNameNS('*', 'process')[0];
-  return processNode ? processNode.getAttribute('name') : '' || '';
+  return processNode ? processNode.getAttribute('name') || '' : '';
 }
