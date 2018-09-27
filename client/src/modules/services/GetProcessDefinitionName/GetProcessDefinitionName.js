365e2a6332a63555691af5ce46b8730c09d37fed
OmranAbazid <omran.1994@gmail.com>
2018-09-27 10:15:18 +0200

    fix(Analysis): get process definition name for processes with bpmn2 tag
    related to OPT-1478

--- client/src/modules/services/GetProcessDefinitionName/GetProcessDefinitionName.js
+++ client/src/modules/services/GetProcessDefinitionName/GetProcessDefinitionName.js
@@ -3,6 +3,8 @@ export default function extractProcessDefinitionName(processDefinitionXml) {
   const xmlDoc = parser.parseFromString(processDefinitionXml, 'text/xml');
 
   const procesNode =
-    xmlDoc.getElementsByTagName('bpmn:process')[0] || xmlDoc.getElementsByTagName('process')[0];
+    xmlDoc.getElementsByTagName('bpmn:process')[0] ||
+    xmlDoc.getElementsByTagName('bpmn2:process')[0] ||
+    xmlDoc.getElementsByTagName('process')[0];
   return procesNode.getAttribute('name') || '';
 }
