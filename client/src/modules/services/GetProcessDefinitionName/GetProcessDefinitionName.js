21c1b83feae630a8707a220fdeb238d61702d1db
OmranAbazid <omran.1994@gmail.com>
2018-09-27 11:52:31 +0200

    imrpove(Analysis): use getElementsByTagNameNS to get process def name
    related to OPT-1478

--- client/src/modules/services/GetProcessDefinitionName/GetProcessDefinitionName.js
+++ client/src/modules/services/GetProcessDefinitionName/GetProcessDefinitionName.js
@@ -2,9 +2,6 @@ export default function extractProcessDefinitionName(processDefinitionXml) {
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(processDefinitionXml, 'text/xml');
 
-  const procesNode =
-    xmlDoc.getElementsByTagName('bpmn:process')[0] ||
-    xmlDoc.getElementsByTagName('bpmn2:process')[0] ||
-    xmlDoc.getElementsByTagName('process')[0];
-  return procesNode ? procesNode.getAttribute('name') : '' || '';
+  const processNode = xmlDoc.getElementsByTagNameNS('*', 'process')[0];
+  return processNode ? processNode.getAttribute('name') : '' || '';
 }
