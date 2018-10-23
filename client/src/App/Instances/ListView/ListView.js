3545fe03587b005b9c49d4d29156021a60327c83
Raluca Stan <raluca.stan@camunda.com>
2018-10-23 13:17:38 +0200

    fix(instances): fix pagination bug related to OPE-225

--- client/src/App/Instances/ListView/ListView.js
+++ client/src/App/Instances/ListView/ListView.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 
 import SplitPane from 'modules/components/SplitPane';
 import {EXPAND_STATE, SORT_ORDER, DEFAULT_SORTING} from 'modules/constants';
-import {isEmpty} from 'modules/utils';
-import {isEqual} from 'lodash';
+import {isEqual, isEmpty} from 'lodash';
 import {parseFilterForRequest} from 'modules/utils/filter';
 import {fetchWorkflowInstances} from 'modules/api/instances';
 
@@ -40,18 +39,16 @@ export default class ListView extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    // set firstElement to 0 when filter changes
-    if (
-      prevProps.filter !== this.props.filter &&
-      this.state.firstElement !== 0
-    ) {
-      return this.setState({firstElement: 0});
-    }
-
     const hasFilterChanged = !isEqual(prevProps.filter, this.props.filter);
     const hasFirstElementChanged =
       prevState.firstElement !== this.state.firstElement;
     const hasSortingChanged = !isEqual(prevState.sorting, this.state.sorting);
+
+    // set firstElement to 0 when filter changes
+    if (hasFilterChanged && this.state.firstElement !== 0) {
+      console.log('changed filter', this.props.filter);
+      return this.setState({firstElement: 0});
+    }
 
     if (hasFilterChanged || hasFirstElementChanged || hasSortingChanged) {
       this.loadData();
