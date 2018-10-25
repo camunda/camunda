eeda1ae02c81e512f9ab573fcec9742c27d9149e
Sebastian Bathke <sebastian.bathke@camunda.com>
2018-10-25 17:43:37 +0200

    improve(test): removal of SpyTransportClientFactory
    - was not used anymore
    - it leaked memory with each call to es
    
    relates to #OPT-1554

--- backend/src/it/java/org/camunda/optimize/rest/MetadataRestServiceIT.java
+++ backend/src/it/java/org/camunda/optimize/rest/MetadataRestServiceIT.java
@@ -1,30 +1,16 @@
 package org.camunda.optimize.rest;
 
 import org.camunda.optimize.dto.optimize.query.OptimizeVersionDto;
-import org.camunda.optimize.dto.optimize.query.status.ConnectionStatusDto;
-import org.camunda.optimize.dto.optimize.query.status.StatusWithProgressDto;
-import org.camunda.optimize.rest.engine.EngineContext;
-import org.camunda.optimize.rest.engine.EngineContextFactory;
 import org.camunda.optimize.service.metadata.MetadataService;
-import org.camunda.optimize.service.security.SessionService;
-import org.camunda.optimize.service.status.StatusCheckingService;
 import org.camunda.optimize.test.it.rule.ElasticSearchIntegrationTestRule;
 import org.camunda.optimize.test.it.rule.EmbeddedOptimizeRule;
 import org.camunda.optimize.test.it.rule.EngineIntegrationRule;
-import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
-import org.mockito.Mockito;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNull.notNullValue;
 
 
 public class MetadataRestServiceIT {
