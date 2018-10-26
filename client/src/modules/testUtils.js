47de178b8d6c90df01f691532dc5e2e93c295bb7
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-10-26 16:19:25 +0200

    test(testUtils): add default data models

--- client/src/modules/testUtils.js
+++ client/src/modules/testUtils.js
@@ -45,19 +45,101 @@ export const createSelection = id => {
   };
 };
 
+const createRandId = () => {
+  return Math.random()
+    .toString(36)
+    .replace(/[^a-z]+/g, '')
+    .substr(2, 10);
+};
+
+/**
+ * @returns a mocked incident Object
+ * @param {*} customProps Obj with any type of custom property
+ */
+export const createIncident = ({
+  activityId = createRandId(),
+  activityInstanceId = createRandId(),
+  errorMessage = '',
+  errorType = '',
+  id = createRandId(),
+  jobId = createRandId(),
+  state = 'ACTIVE'
+} = {}) => {
+  return {
+    activityId,
+    activityInstanceId,
+    errorMessage,
+    errorType,
+    id,
+    jobId,
+    state
+  };
+};
+
+/**
+ * @returns a mocked incident Object
+ * @param {*} customProps Obj with any type of custom property
+ */
+export const createOperation = ({
+  endDate = '2018-10-10T09:20:38.661Z',
+  errorMessage = 'string',
+  startDate = '2018-10-10T09:20:38.661Z',
+  state = 'SCHEDULED',
+  type = 'UPDATE_RETRIES'
+} = {}) => {
+  return {
+    endDate,
+    errorMessage,
+    startDate,
+    state,
+    type
+  };
+};
+
+/**
+ * @returns a mocked activity Object
+ * @param {*} customProps Obj with any type of custom property
+ */
+export const createActivity = ({
+  activityId = createRandId(),
+  endDate = '2018-10-10T09:20:38.658Z',
+  id = createRandId(),
+  startDate = '2018-10-10T09:20:38.658Z',
+  state = 'ACTIVE'
+} = {}) => {
+  return {
+    activityId,
+    endDate,
+    id,
+    startDate,
+    state
+  };
+};
+
 /**
  * @returns a mocked instance Object with a unique id
  * @param {*} id num value to create unique instance;
  */
-export const createInstance = ({id, state}) => {
+export const createInstance = ({
+  activities = [createActivity()],
+  businessKey = 'someKey',
+  endDate = null,
+  id = createRandId(),
+  incidents = [createIncident()],
+  operations = [createOperation()],
+  startDate = '2018-06-21T11:13:31.094+0000',
+  state = 'ACTIVE',
+  workflowId = '2'
+} = {}) => {
   return {
-    activities: [],
-    businessKey: 'someKey',
-    endDate: null,
-    id: id,
-    incidents: [],
-    startDate: '2018-06-21T11:13:31.094+0000',
-    state: state,
-    workflowId: '2'
+    activities,
+    businessKey,
+    endDate,
+    id,
+    incidents,
+    operations,
+    startDate,
+    state,
+    workflowId
   };
 };
