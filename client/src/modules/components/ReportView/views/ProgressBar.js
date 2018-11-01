bee461ee17e936ae142ea0c6cbf1a9f14750b9d4
OmranAbazid <omran.1994@gmail.com>
2018-11-01 15:31:40 +0100

    feat(Report): add goal exceeded indicator to progressbar
    related to OPT-1276

--- client/src/modules/components/ReportView/views/ProgressBar.js
+++ client/src/modules/components/ReportView/views/ProgressBar.js
@@ -5,19 +5,36 @@ import './ProgressBar.scss';
 
 export default function ProgressBar({min, max, value, formatter}) {
   const relative = Math.min(1, Math.max(0, (value - min) / (max - min)));
+  const goalPercentage = max * 100 / (value - min);
+  const goalExceeded = max < value;
 
   return (
     <div className="ProgressBar">
+      {goalExceeded && (
+        <div
+          className="goalOverlay"
+          style={{
+            width: `${goalPercentage}%`
+          }}
+        >
+          <span className={classnames('goalLabel', {rightSide: goalPercentage > 50})}>
+            Goal: {formatter(max)}
+          </span>
+        </div>
+      )}
       <div className="ProgressBar__label">{formatter(value)}</div>
       <div className="ProgressBar--filled" style={{width: `${relative * 100}%`}} />
       <div
         className={classnames('ProgressBar__range', {
           'ProgressBar__range--belowBase': value <= min,
-          'ProgressBar__range--aboveTarget': value >= max
+          'ProgressBar__range--aboveTarget': value >= max,
+          flippedColors: goalExceeded
         })}
       >
         {formatter(min)}
-        <span className="ProgressBar__range--right">Target: {formatter(max)}</span>
+        <span className="ProgressBar__range--right">
+          {goalExceeded ? formatter(value) : 'Goal:' + max}
+        </span>
       </div>
     </div>
   );
--- client/src/modules/components/ReportView/views/ProgressBar.test.js
+++ client/src/modules/components/ReportView/views/ProgressBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import ProgressBar from './ProgressBar';
 
@@ -11,13 +11,26 @@ const props = {
 };
 
 it('should use the provided formatter', () => {
-  const node = mount(<ProgressBar {...props} />);
+  const node = shallow(<ProgressBar {...props} />);
 
   expect(node).toIncludeText('formatted');
 });
 
 it('should fill according to the provided value, min and max properties', () => {
-  const node = mount(<ProgressBar {...props} />);
+  const node = shallow(<ProgressBar {...props} />);
 
   expect(node.find('.ProgressBar--filled')).toHaveProp('style', {width: '20%'});
+});
+
+it('should show the overlay with the goal value when the goal value is exceeded', () => {
+  const props = {
+    min: 0,
+    max: 100,
+    value: 150,
+    formatter: () => 'formatted'
+  };
+
+  const node = shallow(<ProgressBar {...props} />);
+
+  expect(node.find('.goalOverlay')).toIncludeText('Goal');
 });
