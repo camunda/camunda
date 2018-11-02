50a1c8642af1d301f634f26035a474255ebce801
Sebastian Bathke <sebastian.bathke@camunda.com>
2018-11-02 14:07:13 +0100

    fix(metadata): limit clean version constant to three elements

--- util/optimize-commons/src/main/java-templates/org/camunda/optimize/service/metadata/Version.java
+++ util/optimize-commons/src/main/java-templates/org/camunda/optimize/service/metadata/Version.java
@@ -7,6 +7,7 @@ public final class Version {
   private static final String RAW_VERSION = "${project.version}";
   // cleaned from potential clutter added by maven, e.g. -SNAPSHOT
   public static final String VERSION = Arrays.stream(RAW_VERSION.split("[^0-9]"))
+    .limit(3)
     .filter(part -> part.chars().allMatch(Character::isDigit))
     .collect(Collectors.joining("."));
   public static final String VERSION_MAJOR = VERSION.split("\\.")[0];
