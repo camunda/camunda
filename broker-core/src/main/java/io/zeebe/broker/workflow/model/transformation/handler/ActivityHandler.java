pedramha/zeebe

feat(broker-core): support interrupting timer boundary events
- adds CatchEventOutput class to BpmnStepContext to allow
BpmnStepHandlers to subscribe/unsubscribe to catch events
- refactors Terminate*Handler and Complete*Handler for flow
nodes/activities
- refactors message related handlers to use CatchEventOutput
- refactors timer related handlers to use CatchEventOutput
- adds new WorkflowInstanceIntent.BOUNDARY_EVENT_TRIGGERED as the only
lifecycle state of a boundary event
- refactor applying input/output mappings into a stateless helper class
which is reused by several step handlers
- adds new property handlerNodeId to TimerRecord to determine which
boundary event should be triggered
- adds new BoundaryEventHelper to help determine and/or trigger boundary
events from outside of a step processor, as well as terminate the
associated activity if need be

broker-core/src/main/java/io/zeebe/broker/workflow/model/transformation/handler/ActivityHandler.java
@@ -18,7 +18,7 @@
 package io.zeebe.broker.workflow.model.transformation.handler;
 
 import io.zeebe.broker.workflow.model.BpmnStep;
-import io.zeebe.broker.workflow.model.element.ExecutableFlowNode;
+import io.zeebe.broker.workflow.model.element.ExecutableActivity;
 import io.zeebe.broker.workflow.model.element.ExecutableWorkflow;
 import io.zeebe.broker.workflow.model.transformation.ModelElementTransformer;
 import io.zeebe.broker.workflow.model.transformation.TransformContext;
@@ -26,7 +26,6 @@ import io.zeebe.model.bpmn.instance.Activity;
 import io.zeebe.protocol.intent.WorkflowInstanceIntent;
 
 public class ActivityHandler implements ModelElementTransformer<Activity> {
-
   @Override
   public Class<Activity> getType() {
     return Activity.class;
@@ -35,19 +34,16 @@ public class ActivityHandler implements ModelElementTransformer<Activity> {
   @Override
   public void transform(Activity element, TransformContext context) {
     final ExecutableWorkflow workflow = context.getCurrentWorkflow();
-    final ExecutableFlowNode activity =
-        workflow.getElementById(element.getId(), ExecutableFlowNode.class);
+    final ExecutableActivity activity =
+        workflow.getElementById(element.getId(), ExecutableActivity.class);
 
-    bindLifecycle(context, activity);
-  }
-
-  private void bindLifecycle(TransformContext context, final ExecutableFlowNode activity) {
-    activity.bindLifecycleState(WorkflowInstanceIntent.ELEMENT_READY, BpmnStep.APPLY_INPUT_MAPPING);
-    // ACTIVATED is bound per activity type
+    activity.bindLifecycleState(WorkflowInstanceIntent.ELEMENT_READY, BpmnStep.SETUP_ACTIVITY);
     activity.bindLifecycleState(
-        WorkflowInstanceIntent.ELEMENT_COMPLETING, BpmnStep.APPLY_OUTPUT_MAPPING);
+        WorkflowInstanceIntent.ELEMENT_COMPLETING, BpmnStep.COMPLETE_ACTIVITY);
     activity.bindLifecycleState(
         WorkflowInstanceIntent.ELEMENT_COMPLETED, context.getCurrentFlowNodeOutgoingStep());
+    activity.bindLifecycleState(
+        WorkflowInstanceIntent.ELEMENT_TERMINATING, BpmnStep.TERMINATE_ACTIVITY);
     activity.bindLifecycleState(
         WorkflowInstanceIntent.ELEMENT_TERMINATED, BpmnStep.PROPAGATE_TERMINATION);
   }

broker-core/src/main/java/io/zeebe/broker/workflow/model/transformation/handler/BoundaryEventHandler.java
@@ -1,0 +1,76 @@
+/*
+ * Zeebe Broker Core
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.zeebe.broker.workflow.model.transformation.handler;
+
+import io.zeebe.broker.workflow.model.BpmnStep;
+import io.zeebe.broker.workflow.model.element.ExecutableActivity;
+import io.zeebe.broker.workflow.model.element.ExecutableBoundaryEvent;
+import io.zeebe.broker.workflow.model.element.ExecutableWorkflow;
+import io.zeebe.broker.workflow.model.transformation.ModelElementTransformer;
+import io.zeebe.broker.workflow.model.transformation.TransformContext;
+import io.zeebe.model.bpmn.instance.Activity;
+import io.zeebe.model.bpmn.instance.BoundaryEvent;
+import io.zeebe.model.bpmn.instance.EventDefinition;
+import io.zeebe.model.bpmn.instance.TimerEventDefinition;
+import io.zeebe.protocol.intent.WorkflowInstanceIntent;
+import java.time.Duration;
+
+public class BoundaryEventHandler implements ModelElementTransformer<BoundaryEvent> {
+  @Override
+  public Class<BoundaryEvent> getType() {
+    return BoundaryEvent.class;
+  }
+
+  @Override
+  public void transform(BoundaryEvent event, TransformContext context) {
+    final ExecutableWorkflow workflow = context.getCurrentWorkflow();
+    final ExecutableBoundaryEvent element =
+        workflow.getElementById(event.getId(), ExecutableBoundaryEvent.class);
+
+    element.setCancelActivity(event.cancelActivity());
+    handleEventDefinition(event, element);
+    attachToActivity(event, workflow, element);
+
+    element.bindLifecycleState(
+        WorkflowInstanceIntent.BOUNDARY_EVENT_TRIGGERED, BpmnStep.BOUNDARY_EVENT_TRIGGERED);
+  }
+
+  private void handleEventDefinition(BoundaryEvent event, ExecutableBoundaryEvent element) {
+    final EventDefinition eventDefinition = event.getEventDefinitions().iterator().next();
+    if (eventDefinition instanceof TimerEventDefinition) {
+      handleTimerBoundaryEvent(element, (TimerEventDefinition) eventDefinition);
+    }
+  }
+
+  private void handleTimerBoundaryEvent(
+      ExecutableBoundaryEvent element, TimerEventDefinition eventDefinition) {
+    final String timeDuration = eventDefinition.getTimeDuration().getTextContent();
+    final Duration duration = Duration.parse(timeDuration);
+    element.setDuration(duration);
+  }
+
+  private void attachToActivity(
+      BoundaryEvent event, ExecutableWorkflow workflow, ExecutableBoundaryEvent element) {
+    final Activity attachedToActivity = event.getAttachedTo();
+    final ExecutableActivity attachedToElement =
+        workflow.getElementById(attachedToActivity.getId(), ExecutableActivity.class);
+
+    attachedToElement.attach(element);
+    element.setAttachedTo(attachedToElement);
+  }
+}

broker-core/src/main/java/io/zeebe/broker/workflow/processor/BpmnStepHandlers.java
@@ -20,20 +20,25 @@ package io.zeebe.broker.workflow.processor;
 import io.zeebe.broker.subscription.command.SubscriptionCommandSender;
 import io.zeebe.broker.workflow.model.BpmnStep;
 import io.zeebe.broker.workflow.model.element.ExecutableFlowElement;
-import io.zeebe.broker.workflow.processor.activity.InputMappingHandler;
-import io.zeebe.broker.workflow.processor.activity.OutputMappingHandler;
-import io.zeebe.broker.workflow.processor.activity.PropagateTerminationHandler;
+import io.zeebe.broker.workflow.processor.activity.CompleteActivityHandler;
+import io.zeebe.broker.workflow.processor.activity.SetupActivityHandler;
+import io.zeebe.broker.workflow.processor.activity.TerminateActivityHandler;
+import io.zeebe.broker.workflow.processor.boundary.BoundaryEventTriggeredHandler;
+import io.zeebe.broker.workflow.processor.flownode.ActivateFlowNodeHandler;
+import io.zeebe.broker.workflow.processor.flownode.CompleteFlowNodeHandler;
 import io.zeebe.broker.workflow.processor.flownode.ConsumeTokenHandler;
+import io.zeebe.broker.workflow.processor.flownode.PropagateTerminationHandler;
 import io.zeebe.broker.workflow.processor.flownode.TakeSequenceFlowHandler;
-import io.zeebe.broker.workflow.processor.flownode.TerminateElementHandler;
+import io.zeebe.broker.workflow.processor.flownode.TerminateFlowNodeHandler;
 import io.zeebe.broker.workflow.processor.gateway.ExclusiveSplitHandler;
 import io.zeebe.broker.workflow.processor.gateway.ParallelSplitHandler;
 import io.zeebe.broker.workflow.processor.message.MessageCatchElementHandler;
 import io.zeebe.broker.workflow.processor.message.TerminateIntermediateMessageHandler;
+import io.zeebe.broker.workflow.processor.message.TerminateReceiveTaskHandler;
 import io.zeebe.broker.workflow.processor.process.CompleteProcessHandler;
 import io.zeebe.broker.workflow.processor.sequenceflow.ActivateGatewayHandler;
 import io.zeebe.broker.workflow.processor.sequenceflow.ParallelMergeHandler;
-import io.zeebe.broker.workflow.processor.sequenceflow.StartStatefulElementHandler;
+import io.zeebe.broker.workflow.processor.sequenceflow.StartFlowNodeHandler;
 import io.zeebe.broker.workflow.processor.sequenceflow.TriggerEndEventHandler;
 import io.zeebe.broker.workflow.processor.servicetask.CreateJobHandler;
 import io.zeebe.broker.workflow.processor.servicetask.TerminateServiceTaskHandler;
@@ -52,22 +57,30 @@ public class BpmnStepHandlers {
 
   public BpmnStepHandlers(
       SubscriptionCommandSender subscriptionCommandSender, WorkflowState workflowState) {
-
     // activity
+    stepHandlers.put(BpmnStep.SETUP_ACTIVITY, new SetupActivityHandler());
+    stepHandlers.put(BpmnStep.COMPLETE_ACTIVITY, new CompleteActivityHandler());
+    stepHandlers.put(BpmnStep.TERMINATE_ACTIVITY, new TerminateActivityHandler());
+
+    // boundary events
+    stepHandlers.put(BpmnStep.BOUNDARY_EVENT_TRIGGERED, new BoundaryEventTriggeredHandler());
+
+    // service task
     stepHandlers.put(BpmnStep.CREATE_JOB, new CreateJobHandler());
-    stepHandlers.put(BpmnStep.APPLY_INPUT_MAPPING, new InputMappingHandler());
-    stepHandlers.put(BpmnStep.APPLY_OUTPUT_MAPPING, new OutputMappingHandler());
 
     // exclusive gateway
     stepHandlers.put(BpmnStep.EXCLUSIVE_SPLIT, new ExclusiveSplitHandler());
 
-    // flow node
+    // flow nodes
     stepHandlers.put(BpmnStep.CONSUME_TOKEN, new ConsumeTokenHandler(workflowState));
-    stepHandlers.put(BpmnStep.TAKE_SEQUENCE_FLOW, new TakeSequenceFlowHandler());
+    stepHandlers.put(BpmnStep.START_FLOW_NODE, new StartFlowNodeHandler());
+    stepHandlers.put(BpmnStep.ACTIVATE_FLOW_NODE, new ActivateFlowNodeHandler());
+    stepHandlers.put(BpmnStep.COMPLETE_FLOW_NODE, new CompleteFlowNodeHandler());
+    stepHandlers.put(BpmnStep.TERMINATE_ELEMENT, new TerminateFlowNodeHandler());
 
     // sequence flow
+    stepHandlers.put(BpmnStep.TAKE_SEQUENCE_FLOW, new TakeSequenceFlowHandler());
     stepHandlers.put(BpmnStep.ACTIVATE_GATEWAY, new ActivateGatewayHandler());
-    stepHandlers.put(BpmnStep.START_STATEFUL_ELEMENT, new StartStatefulElementHandler());
     stepHandlers.put(BpmnStep.TRIGGER_END_EVENT, new TriggerEndEventHandler());
     stepHandlers.put(BpmnStep.PARALLEL_MERGE, new ParallelMergeHandler(workflowState));
 
@@ -78,21 +91,18 @@ public class BpmnStepHandlers {
     stepHandlers.put(BpmnStep.PARALLEL_SPLIT, new ParallelSplitHandler());
 
     // termination
-    stepHandlers.put(BpmnStep.TERMINATE_ELEMENT, new TerminateElementHandler());
     stepHandlers.put(BpmnStep.TERMINATE_JOB_TASK, new TerminateServiceTaskHandler());
-    stepHandlers.put(BpmnStep.TERMINATE_TIMER, new TerminateTimerHandler(workflowState));
+    stepHandlers.put(BpmnStep.TERMINATE_TIMER, new TerminateTimerHandler());
+    stepHandlers.put(BpmnStep.TERMINATE_RECEIVE_TASK, new TerminateReceiveTaskHandler());
     stepHandlers.put(
-        BpmnStep.TERMINATE_INTERMEDIATE_MESSAGE,
-        new TerminateIntermediateMessageHandler(workflowState, subscriptionCommandSender));
+        BpmnStep.TERMINATE_INTERMEDIATE_MESSAGE, new TerminateIntermediateMessageHandler());
     stepHandlers.put(
         BpmnStep.TERMINATE_CONTAINED_INSTANCES,
         new TerminateContainedElementsHandler(workflowState));
     stepHandlers.put(BpmnStep.PROPAGATE_TERMINATION, new PropagateTerminationHandler());
 
     // intermediate catch event
-    stepHandlers.put(
-        BpmnStep.SUBSCRIBE_TO_INTERMEDIATE_MESSAGE,
-        new MessageCatchElementHandler(subscriptionCommandSender, workflowState));
+    stepHandlers.put(BpmnStep.SUBSCRIBE_TO_INTERMEDIATE_MESSAGE, new MessageCatchElementHandler());
     stepHandlers.put(BpmnStep.CREATE_TIMER, new CreateTimerHandler());
 
     // process
@@ -101,15 +111,14 @@ public class BpmnStepHandlers {
 
   public void handle(BpmnStepContext context) {
     final ExecutableFlowElement flowElement = context.getElement();
-    final BpmnStep step =
-        flowElement.getStep((WorkflowInstanceIntent) context.getRecord().getMetadata().getIntent());
+    final WorkflowInstanceIntent state =
+        (WorkflowInstanceIntent) context.getRecord().getMetadata().getIntent();
+    final BpmnStep step = flowElement.getStep(state);
 
     if (step != null) {
       final BpmnStepHandler stepHandler = stepHandlers.get(step);
-
-      if (stepHandler != null) {
-        stepHandler.handle(context);
-      }
+      assert stepHandler != null : "no step handler configured for step " + step.toString();
+      stepHandler.handle(context);
     }
   }
 }

camunda/camunda

broker-core/src/main/java/io/zeebe/broker/workflow/model/transformation/handler/ActivityHandler.java
@@ -18,7 +18,7 @@
 package io.zeebe.broker.workflow.model.transformation.handler;
 
 import io.zeebe.broker.workflow.model.BpmnStep;
-import io.zeebe.broker.workflow.model.element.ExecutableFlowNode;
+import io.zeebe.broker.workflow.model.element.ExecutableActivity;
 import io.zeebe.broker.workflow.model.element.ExecutableWorkflow;
 import io.zeebe.broker.workflow.model.transformation.ModelElementTransformer;
 import io.zeebe.broker.workflow.model.transformation.TransformContext;
@@ -26,7 +26,6 @@ import io.zeebe.model.bpmn.instance.Activity;
 import io.zeebe.protocol.intent.WorkflowInstanceIntent;
 
 public class ActivityHandler implements ModelElementTransformer<Activity> {
-
   @Override
   public Class<Activity> getType() {
     return Activity.class;
@@ -35,19 +34,16 @@ public class ActivityHandler implements ModelElementTransformer<Activity> {
   @Override
   public void transform(Activity element, TransformContext context) {
     final ExecutableWorkflow workflow = context.getCurrentWorkflow();
-    final ExecutableFlowNode activity =
-        workflow.getElementById(element.getId(), ExecutableFlowNode.class);
+    final ExecutableActivity activity =
+        workflow.getElementById(element.getId(), ExecutableActivity.class);
 
-    bindLifecycle(context, activity);
-  }
-
-  private void bindLifecycle(TransformContext context, final ExecutableFlowNode activity) {
-    activity.bindLifecycleState(WorkflowInstanceIntent.ELEMENT_READY, BpmnStep.APPLY_INPUT_MAPPING);
-    // ACTIVATED is bound per activity type
+    activity.bindLifecycleState(WorkflowInstanceIntent.ELEMENT_READY, BpmnStep.ACTIVATE_ACTIVITY);
     activity.bindLifecycleState(
-        WorkflowInstanceIntent.ELEMENT_COMPLETING, BpmnStep.APPLY_OUTPUT_MAPPING);
+        WorkflowInstanceIntent.ELEMENT_COMPLETING, BpmnStep.COMPLETE_ACTIVITY);
     activity.bindLifecycleState(
         WorkflowInstanceIntent.ELEMENT_COMPLETED, context.getCurrentFlowNodeOutgoingStep());
+    activity.bindLifecycleState(
+        WorkflowInstanceIntent.ELEMENT_TERMINATING, BpmnStep.TERMINATE_ACTIVITY);
     activity.bindLifecycleState(
         WorkflowInstanceIntent.ELEMENT_TERMINATED, BpmnStep.PROPAGATE_TERMINATION);
   }

broker-core/src/main/java/io/zeebe/broker/workflow/model/transformation/handler/BoundaryEventHandler.java
@@ -1,0 +1,76 @@
+/*
+ * Zeebe Broker Core
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.zeebe.broker.workflow.model.transformation.handler;
+
+import io.zeebe.broker.workflow.model.BpmnStep;
+import io.zeebe.broker.workflow.model.element.ExecutableActivity;
+import io.zeebe.broker.workflow.model.element.ExecutableBoundaryEvent;
+import io.zeebe.broker.workflow.model.element.ExecutableWorkflow;
+import io.zeebe.broker.workflow.model.transformation.ModelElementTransformer;
+import io.zeebe.broker.workflow.model.transformation.TransformContext;
+import io.zeebe.model.bpmn.instance.Activity;
+import io.zeebe.model.bpmn.instance.BoundaryEvent;
+import io.zeebe.model.bpmn.instance.EventDefinition;
+import io.zeebe.model.bpmn.instance.TimerEventDefinition;
+import io.zeebe.protocol.intent.WorkflowInstanceIntent;
+import java.time.Duration;
+
+public class BoundaryEventHandler implements ModelElementTransformer<BoundaryEvent> {
+  @Override
+  public Class<BoundaryEvent> getType() {
+    return BoundaryEvent.class;
+  }
+
+  @Override
+  public void transform(BoundaryEvent event, TransformContext context) {
+    final ExecutableWorkflow workflow = context.getCurrentWorkflow();
+    final ExecutableBoundaryEvent element =
+        workflow.getElementById(event.getId(), ExecutableBoundaryEvent.class);
+
+    element.setCancelActivity(event.cancelActivity());
+    transformEventDefinition(event, element);
+    attachToActivity(event, workflow, element);
+
+    element.bindLifecycleState(
+        WorkflowInstanceIntent.BOUNDARY_EVENT_TRIGGERED, BpmnStep.BOUNDARY_EVENT_TRIGGERED);
+  }
+
+  private void transformEventDefinition(BoundaryEvent event, ExecutableBoundaryEvent element) {
+    final EventDefinition eventDefinition = event.getEventDefinitions().iterator().next();
+    if (eventDefinition instanceof TimerEventDefinition) {
+      transformTimerEventDefinition(element, (TimerEventDefinition) eventDefinition);
+    }
+  }
+
+  private void transformTimerEventDefinition(
+      ExecutableBoundaryEvent element, TimerEventDefinition eventDefinition) {
+    final String timeDuration = eventDefinition.getTimeDuration().getTextContent();
+    final Duration duration = Duration.parse(timeDuration);
+    element.setDuration(duration);
+  }
+
+  private void attachToActivity(
+      BoundaryEvent event, ExecutableWorkflow workflow, ExecutableBoundaryEvent element) {
+    final Activity attachedToActivity = event.getAttachedTo();
+    final ExecutableActivity attachedToElement =
+        workflow.getElementById(attachedToActivity.getId(), ExecutableActivity.class);
+
+    attachedToElement.attach(element);
+    element.setAttachedTo(attachedToElement);
+  }
+}

broker-core/src/main/java/io/zeebe/broker/workflow/processor/BpmnStepHandlers.java
@@ -20,20 +20,25 @@ package io.zeebe.broker.workflow.processor;
 import io.zeebe.broker.subscription.command.SubscriptionCommandSender;
 import io.zeebe.broker.workflow.model.BpmnStep;
 import io.zeebe.broker.workflow.model.element.ExecutableFlowElement;
-import io.zeebe.broker.workflow.processor.activity.InputMappingHandler;
-import io.zeebe.broker.workflow.processor.activity.OutputMappingHandler;
-import io.zeebe.broker.workflow.processor.activity.PropagateTerminationHandler;
+import io.zeebe.broker.workflow.processor.activity.ActivateActivityHandler;
+import io.zeebe.broker.workflow.processor.activity.CompleteActivityHandler;
+import io.zeebe.broker.workflow.processor.activity.TerminateActivityHandler;
+import io.zeebe.broker.workflow.processor.boundary.BoundaryEventTriggeredHandler;
+import io.zeebe.broker.workflow.processor.flownode.ActivateFlowNodeHandler;
+import io.zeebe.broker.workflow.processor.flownode.CompleteFlowNodeHandler;
 import io.zeebe.broker.workflow.processor.flownode.ConsumeTokenHandler;
+import io.zeebe.broker.workflow.processor.flownode.PropagateTerminationHandler;
 import io.zeebe.broker.workflow.processor.flownode.TakeSequenceFlowHandler;
-import io.zeebe.broker.workflow.processor.flownode.TerminateElementHandler;
+import io.zeebe.broker.workflow.processor.flownode.TerminateFlowNodeHandler;
 import io.zeebe.broker.workflow.processor.gateway.ExclusiveSplitHandler;
 import io.zeebe.broker.workflow.processor.gateway.ParallelSplitHandler;
 import io.zeebe.broker.workflow.processor.message.MessageCatchElementHandler;
 import io.zeebe.broker.workflow.processor.message.TerminateIntermediateMessageHandler;
+import io.zeebe.broker.workflow.processor.message.TerminateReceiveTaskHandler;
 import io.zeebe.broker.workflow.processor.process.CompleteProcessHandler;
 import io.zeebe.broker.workflow.processor.sequenceflow.ActivateGatewayHandler;
 import io.zeebe.broker.workflow.processor.sequenceflow.ParallelMergeHandler;
-import io.zeebe.broker.workflow.processor.sequenceflow.StartStatefulElementHandler;
+import io.zeebe.broker.workflow.processor.sequenceflow.StartFlowNodeHandler;
 import io.zeebe.broker.workflow.processor.sequenceflow.TriggerEndEventHandler;
 import io.zeebe.broker.workflow.processor.servicetask.CreateJobHandler;
 import io.zeebe.broker.workflow.processor.servicetask.TerminateServiceTaskHandler;
@@ -52,22 +57,30 @@ public class BpmnStepHandlers {
 
   public BpmnStepHandlers(
       SubscriptionCommandSender subscriptionCommandSender, WorkflowState workflowState) {
-
     // activity
+    stepHandlers.put(BpmnStep.ACTIVATE_ACTIVITY, new ActivateActivityHandler());
+    stepHandlers.put(BpmnStep.COMPLETE_ACTIVITY, new CompleteActivityHandler());
+    stepHandlers.put(BpmnStep.TERMINATE_ACTIVITY, new TerminateActivityHandler());
+
+    // boundary events
+    stepHandlers.put(BpmnStep.BOUNDARY_EVENT_TRIGGERED, new BoundaryEventTriggeredHandler());
+
+    // service task
     stepHandlers.put(BpmnStep.CREATE_JOB, new CreateJobHandler());
-    stepHandlers.put(BpmnStep.APPLY_INPUT_MAPPING, new InputMappingHandler());
-    stepHandlers.put(BpmnStep.APPLY_OUTPUT_MAPPING, new OutputMappingHandler());
 
     // exclusive gateway
     stepHandlers.put(BpmnStep.EXCLUSIVE_SPLIT, new ExclusiveSplitHandler());
 
-    // flow node
+    // flow nodes
     stepHandlers.put(BpmnStep.CONSUME_TOKEN, new ConsumeTokenHandler(workflowState));
-    stepHandlers.put(BpmnStep.TAKE_SEQUENCE_FLOW, new TakeSequenceFlowHandler());
+    stepHandlers.put(BpmnStep.START_FLOW_NODE, new StartFlowNodeHandler());
+    stepHandlers.put(BpmnStep.ACTIVATE_FLOW_NODE, new ActivateFlowNodeHandler());
+    stepHandlers.put(BpmnStep.COMPLETE_FLOW_NODE, new CompleteFlowNodeHandler());
+    stepHandlers.put(BpmnStep.TERMINATE_ELEMENT, new TerminateFlowNodeHandler());
 
     // sequence flow
+    stepHandlers.put(BpmnStep.TAKE_SEQUENCE_FLOW, new TakeSequenceFlowHandler());
     stepHandlers.put(BpmnStep.ACTIVATE_GATEWAY, new ActivateGatewayHandler());
-    stepHandlers.put(BpmnStep.START_STATEFUL_ELEMENT, new StartStatefulElementHandler());
     stepHandlers.put(BpmnStep.TRIGGER_END_EVENT, new TriggerEndEventHandler());
     stepHandlers.put(BpmnStep.PARALLEL_MERGE, new ParallelMergeHandler(workflowState));
 
@@ -78,21 +91,18 @@ public class BpmnStepHandlers {
     stepHandlers.put(BpmnStep.PARALLEL_SPLIT, new ParallelSplitHandler());
 
     // termination
-    stepHandlers.put(BpmnStep.TERMINATE_ELEMENT, new TerminateElementHandler());
     stepHandlers.put(BpmnStep.TERMINATE_JOB_TASK, new TerminateServiceTaskHandler());
-    stepHandlers.put(BpmnStep.TERMINATE_TIMER, new TerminateTimerHandler(workflowState));
+    stepHandlers.put(BpmnStep.TERMINATE_TIMER, new TerminateTimerHandler());
+    stepHandlers.put(BpmnStep.TERMINATE_RECEIVE_TASK, new TerminateReceiveTaskHandler());
     stepHandlers.put(
-        BpmnStep.TERMINATE_INTERMEDIATE_MESSAGE,
-        new TerminateIntermediateMessageHandler(workflowState, subscriptionCommandSender));
+        BpmnStep.TERMINATE_INTERMEDIATE_MESSAGE, new TerminateIntermediateMessageHandler());
     stepHandlers.put(
         BpmnStep.TERMINATE_CONTAINED_INSTANCES,
         new TerminateContainedElementsHandler(workflowState));
     stepHandlers.put(BpmnStep.PROPAGATE_TERMINATION, new PropagateTerminationHandler());
 
     // intermediate catch event
-    stepHandlers.put(
-        BpmnStep.SUBSCRIBE_TO_INTERMEDIATE_MESSAGE,
-        new MessageCatchElementHandler(subscriptionCommandSender, workflowState));
+    stepHandlers.put(BpmnStep.SUBSCRIBE_TO_INTERMEDIATE_MESSAGE, new MessageCatchElementHandler());
     stepHandlers.put(BpmnStep.CREATE_TIMER, new CreateTimerHandler());
 
     // process
@@ -101,15 +111,14 @@ public class BpmnStepHandlers {
 
   public void handle(BpmnStepContext context) {
     final ExecutableFlowElement flowElement = context.getElement();
-    final BpmnStep step =
-        flowElement.getStep((WorkflowInstanceIntent) context.getRecord().getMetadata().getIntent());
+    final WorkflowInstanceIntent state =
+        (WorkflowInstanceIntent) context.getRecord().getMetadata().getIntent();
+    final BpmnStep step = flowElement.getStep(state);
 
     if (step != null) {
       final BpmnStepHandler stepHandler = stepHandlers.get(step);
-
-      if (stepHandler != null) {
-        stepHandler.handle(context);
-      }
+      assert stepHandler != null : "no step handler configured for step " + step.toString();
+      stepHandler.handle(context);
     }
   }
 }