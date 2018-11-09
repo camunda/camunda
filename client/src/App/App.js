5286ea434a158e9546c2b60113335a3a49d3da90
Seif Eddine Ghezala <siffogh3@gmail.com>
2018-11-09 09:37:20 +0100

    fix(router): change to hash router
    Related to OPE-226

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 import {ThemeProvider} from 'modules/contexts/ThemeContext';
 import {CollapsablePanelProvider} from 'modules/contexts/CollapsablePanelContext';
