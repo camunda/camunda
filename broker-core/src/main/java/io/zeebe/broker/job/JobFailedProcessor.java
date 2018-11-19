pedramha/zeebe

feat(broker-core): impl new incident concept

broker-core/src/main/java/io/zeebe/broker/job/JobFailedProcessor.java
@@ -1,0 +1,56 @@
+/*
+ * Zeebe Broker Core
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.zeebe.broker.job;
+
+import io.zeebe.broker.incident.data.ErrorType;
+import io.zeebe.broker.incident.data.IncidentRecord;
+import io.zeebe.broker.logstreams.processor.TypedRecord;
+import io.zeebe.broker.logstreams.processor.TypedRecordProcessor;
+import io.zeebe.broker.logstreams.processor.TypedResponseWriter;
+import io.zeebe.broker.logstreams.processor.TypedStreamWriter;
+import io.zeebe.protocol.impl.record.value.job.JobHeaders;
+import io.zeebe.protocol.impl.record.value.job.JobRecord;
+import io.zeebe.protocol.intent.IncidentIntent;
+
+public final class JobFailedProcessor implements TypedRecordProcessor<JobRecord> {
+  private final IncidentRecord incidentEvent = new IncidentRecord();
+
+  @Override
+  public void processRecord(
+      TypedRecord<JobRecord> event,
+      TypedResponseWriter responseWriter,
+      TypedStreamWriter streamWriter) {
+    final JobRecord value = event.getValue();
+
+    if (value.getRetries() <= 0) {
+      final JobHeaders jobHeaders = value.getHeaders();
+
+      incidentEvent.reset();
+      incidentEvent
+          .setErrorType(ErrorType.JOB_NO_RETRIES)
+          .setErrorMessage("No more retries left.")
+          .setBpmnProcessId(jobHeaders.getBpmnProcessId())
+          .setWorkflowInstanceKey(jobHeaders.getWorkflowInstanceKey())
+          .setElementId(jobHeaders.getElementId())
+          .setElementInstanceKey(jobHeaders.getElementInstanceKey())
+          .setJobKey(event.getKey());
+
+      streamWriter.writeNewCommand(IncidentIntent.CREATE, incidentEvent);
+    }
+  }
+}

camunda/camunda

 * incident event processors are added to the single stream processor
 * incident state is ported to rocks db
 Incident creation:
 * job incidents need an job in failed state otherwise create is
 rejected
 * workflow incidents need an failed token otherwise create is rejected
 Incident resolving:
 * resolve command is currently written on update payload and update
 retries -> with #1651 we will use an external command written by the
 client to explicit resolve the incident
 * on resolving of workflow incidents the BpmnStepProcessors is directly called
 * on resolving of an job incident the state is set to activatable
 again
 * on RESOLVED incident event the incident state is cleaned up
 * this is used  also for the cancel processing - they write an RESOLVED incident event
 * The incident lifecycle is cut down to CREATE->CREATED
 RESOLVE->RESOLVED
 * incidents have no longer an payload

broker-core/src/main/java/io/zeebe/broker/job/JobFailedProcessor.java
@@ -1,0 +1,56 @@
+/*
+ * Zeebe Broker Core
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.zeebe.broker.job;
+
+import io.zeebe.broker.incident.data.ErrorType;
+import io.zeebe.broker.incident.data.IncidentRecord;
+import io.zeebe.broker.logstreams.processor.TypedRecord;
+import io.zeebe.broker.logstreams.processor.TypedRecordProcessor;
+import io.zeebe.broker.logstreams.processor.TypedResponseWriter;
+import io.zeebe.broker.logstreams.processor.TypedStreamWriter;
+import io.zeebe.protocol.impl.record.value.job.JobHeaders;
+import io.zeebe.protocol.impl.record.value.job.JobRecord;
+import io.zeebe.protocol.intent.IncidentIntent;
+
+public final class JobFailedProcessor implements TypedRecordProcessor<JobRecord> {
+  private final IncidentRecord incidentEvent = new IncidentRecord();
+
+  @Override
+  public void processRecord(
+      TypedRecord<JobRecord> event,
+      TypedResponseWriter responseWriter,
+      TypedStreamWriter streamWriter) {
+    final JobRecord value = event.getValue();
+
+    if (value.getRetries() <= 0) {
+      final JobHeaders jobHeaders = value.getHeaders();
+
+      incidentEvent.reset();
+      incidentEvent
+          .setErrorType(ErrorType.JOB_NO_RETRIES)
+          .setErrorMessage("No more retries left.")
+          .setBpmnProcessId(jobHeaders.getBpmnProcessId())
+          .setWorkflowInstanceKey(jobHeaders.getWorkflowInstanceKey())
+          .setElementId(jobHeaders.getElementId())
+          .setElementInstanceKey(jobHeaders.getElementInstanceKey())
+          .setJobKey(event.getKey());
+
+      streamWriter.appendNewCommand(IncidentIntent.CREATE, incidentEvent);
+    }
+  }
+}