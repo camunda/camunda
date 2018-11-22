32bc691db77a884ce5f93138059e84ef434c71bc
Sebastian Menski <sebastian.menski@camunda.com>
2018-11-22 01:08:52 +0100

    feat(clients/go): add error message to fail job command

--- clients/go/commands/failJob_command_test.go
+++ clients/go/commands/failJob_command_test.go
@@ -34,3 +34,33 @@ func TestFailJobCommand(t *testing.T) {
 		t.Errorf("Failed to receive response")
 	}
 }
+
+func TestFailJobCommand_ErrorMessage(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	client := mock_pb.NewMockGatewayClient(ctrl)
+
+	errorMessage := "something went wrong"
+
+	request := &pb.FailJobRequest{
+		JobKey:       123,
+		Retries:      12,
+		ErrorMessage: errorMessage,
+	}
+	stub := &pb.FailJobResponse{}
+
+	client.EXPECT().FailJob(gomock.Any(), &utils.RpcTestMsg{Msg: request}).Return(stub, nil)
+
+	command := NewFailJobCommand(client, utils.DefaultTestTimeout)
+
+	response, err := command.JobKey(123).Retries(12).ErrorMessage(errorMessage).Send()
+
+	if err != nil {
+		t.Errorf("Failed to send request")
+	}
+
+	if response != stub {
+		t.Errorf("Failed to receive response")
+	}
+}
