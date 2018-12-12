899221ead4b03a2c017fcb410ba22bc4367a6985
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2018-12-12 19:46:21 +0100

    feat(test): adds a slew of Exporter test utilities
    - mock implementations of most io.zeebe.exporter.* interfaces
    - provide utility to run all scheduled tasks with optional delay
    - provides ExporterTestHarness to simulate different points of the
    exporter lifecycle
    - provides utility to stream record to an exporter

---
+++ test/src/main/java/io/zeebe/test/exporter/MockContext.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.zeebe.test.exporter;
+
+import io.zeebe.exporter.context.Configuration;
+import io.zeebe.exporter.context.Context;
+import org.slf4j.Logger;
+
+public class MockContext implements Context {
+
+  private Logger logger;
+  private Configuration configuration;
+
+  public MockContext() {}
+
+  public MockContext(Logger logger, Configuration configuration) {
+    this.logger = logger;
+    this.configuration = configuration;
+  }
+
+  @Override
+  public Logger getLogger() {
+    return logger;
+  }
+
+  public void setLogger(Logger logger) {
+    this.logger = logger;
+  }
+
+  @Override
+  public Configuration getConfiguration() {
+    return configuration;
+  }
+
+  public void setConfiguration(Configuration configuration) {
+    this.configuration = configuration;
+  }
+}
---
+++ test/src/main/java/io/zeebe/test/exporter/MockController.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.zeebe.test.exporter;
+
+import io.zeebe.exporter.context.Controller;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class MockController implements Controller {
+
+  public static final long UNKNOWN_POSITION = -1;
+
+  private final List<MockScheduledTask> scheduledTasks = new ArrayList<>();
+  private long lastRanAtMs = 0;
+  private long position = UNKNOWN_POSITION;
+
+  @Override
+  public void updateLastExportedRecordPosition(long position) {
+    this.position = position;
+  }
+
+  @Override
+  public void scheduleTask(Duration delay, Runnable task) {
+    final MockScheduledTask scheduledTask = new MockScheduledTask(delay, task);
+    scheduledTasks.add(scheduledTask);
+  }
+
+  public void resetScheduler() {
+    lastRanAtMs = 0;
+    scheduledTasks.clear();
+  }
+
+  public List<MockScheduledTask> getScheduledTasks() {
+    return scheduledTasks;
+  }
+
+  public long getPosition() {
+    return position;
+  }
+
+  /**
+   * Will run all tasks scheduled since the last time this was executed + the given duration.
+   *
+   * @param elapsed upper bound of tasks delay
+   */
+  public void runScheduledTasks(Duration elapsed) {
+    final Duration upperBound = elapsed.plusMillis(lastRanAtMs);
+
+    scheduledTasks
+        .stream()
+        .filter(t -> t.getDelay().compareTo(upperBound) <= 0)
+        .sorted(Comparator.comparing(MockScheduledTask::getDelay))
+        .forEach(MockScheduledTask::run);
+
+    lastRanAtMs = upperBound.toMillis();
+  }
+}
