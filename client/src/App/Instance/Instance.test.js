caaf0e9ba2351be0b6e9994a8bb18041be20ddcb
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2018-12-14 09:47:22 +0100

    test(Instance): improve tests after refactoring

--- client/src/App/Instance/Instance.test.js
+++ client/src/App/Instance/Instance.test.js
@@ -30,18 +30,27 @@ api.fetchWorkflowXML = mockResolvedAsyncFn(xmlMock);
 const FOO_ACTIVITY = createActivity({
   id: 'foo',
   activityId: 'foo',
-  state: ACTIVITY_STATE.COMPLETED
+  state: ACTIVITY_STATE.COMPLETED,
+  type: FLOW_NODE_TYPE.END_EVENT
 });
 const BAR_ACTIVITY = createActivity({
   id: 'bar',
   activityId: 'bar',
-  state: ACTIVITY_STATE.COMPLETED
+  state: ACTIVITY_STATE.COMPLETED,
+  type: FLOW_NODE_TYPE.TASK
 });
 const TASKA_ACTIVITY = createActivity({
   id: '4294983744',
   activityId: 'taskA',
-  state: ACTIVITY_STATE.INCIDENT
+  state: ACTIVITY_STATE.INCIDENT,
+  type: FLOW_NODE_TYPE.TASK
 });
+
+const ACTIVITIES_DETAILS = {
+  foo: FOO_ACTIVITY,
+  bar: BAR_ACTIVITY,
+  taskA: TASKA_ACTIVITY
+};
 
 const INCIDENT = createIncident({
   id: '4295763008',
@@ -78,14 +87,85 @@ describe('Instance', () => {
     api.fetchWorkflowInstance.mockClear();
   });
 
-  describe('initial state', () => {
+  describe('render', () => {
+    let node;
+    beforeEach(async () => {
+      node = shallow(component);
+      node.setState({
+        instance: INSTANCE,
+        activitiesDetails: ACTIVITIES_DETAILS,
+        selection: {
+          activityInstanceId: '4294983744',
+          flowNodeId: 'foo'
+        }
+      });
+      await flushPromises();
+      node.update();
+    });
+
+    it('should render a header', async () => {
+      // Transparent Heading
+      expect(node.contains(`Camunda Operate Instance ${INSTANCE.id}`)).toBe(
+        true
+      );
+      // HeaderNode
+      const HeaderNode = node.find(Header);
+      expect(HeaderNode).toHaveLength(1);
+      // Detail in Header
+      const DetailNode = HeaderNode.prop('detail');
+      expect(DetailNode.type).toBe(InstanceDetail);
+      expect(DetailNode.props.instance).toEqual(INSTANCE);
+    });
+
+    it('should render a diagram', () => {
+      const DiagramPanelNode = node.find(DiagramPanel);
+      expect(DiagramPanelNode).toHaveLength(1);
+      expect(DiagramPanelNode.prop('instance')).toEqual(INSTANCE);
+      expect(DiagramPanelNode.prop('selectableFlowNodes')).toEqual([
+        'foo',
+        'bar',
+        'taskA'
+      ]);
+      expect(DiagramPanelNode.prop('selectedFlowNode')).toBe('foo');
+      expect(DiagramPanelNode.prop('onFlowNodeSelected')).toBe(
+        node.instance().handleFlowNodeSelection
+      );
+
+      expect(DiagramPanelNode.prop('flowNodeStateOverlays')).toEqual([
+        {
+          id: 'foo',
+          state: ACTIVITY_STATE.COMPLETED
+        },
+        {
+          id: 'taskA',
+          state: ACTIVITY_STATE.INCIDENT
+        }
+      ]);
+    });
+
+    it('should render a the instance history', () => {
+      const InstanceHistoryNode = node.find(InstanceHistory);
+      expect(InstanceHistoryNode).toHaveLength(1);
+      expect(InstanceHistoryNode.prop('instance')).toEqual(INSTANCE);
+      expect(InstanceHistoryNode.prop('activitiesDetails')).toEqual(
+        ACTIVITIES_DETAILS
+      );
+      expect(InstanceHistoryNode.prop('selectedActivityInstanceId')).toBe(
+        '4294983744'
+      );
+      expect(InstanceHistoryNode.prop('onActivityInstanceSelected')).toBe(
+        node.instance().handleActivityInstanceSelection
+      );
+      // snapshot
+      expect(node).toMatchSnapshot();
+    });
+
     it('should render initially with no data', () => {
       const node = shallow(component);
       expect(node.state()).toEqual(initialState);
       expect(node.text()).toContain('Loading');
     });
   });
-
   describe('handleActivityInstanceSelection', () => {
     it('should update the state.selection according to the value', () => {
       // given
@@ -179,92 +259,6 @@ describe('Instance', () => {
       });
       // second activity should be ignored since it doesn't have corresponding flownode details
       expect(activitiesDetails[secondId]).toBeUndefined();
-    });
-  });
-
-  describe('rendering', () => {
-    it('should display a Header, DiagramPanel and Copyright', async () => {
-      // given
-      const node = shallow(component);
-      const ACTIVITIES_DETAILS = {
-        foo: {
-          activityId: 'foo',
-          state: ACTIVITY_STATE.COMPLETED,
-          type: FLOW_NODE_TYPE.END_EVENT
-        },
-        bar: {
-          activityId: 'bar',
-          state: ACTIVITY_STATE.COMPLETED,
-          type: FLOW_NODE_TYPE.TASK
-        },
-        taskA: {
-          activityId: 'taskA',
-          state: ACTIVITY_STATE.INCIDENT,
-          type: FLOW_NODE_TYPE.TASK
-        }
-      };
-      node.setState({
-        instance: INSTANCE,
-        activitiesDetails: ACTIVITIES_DETAILS,
-        selection: {
-          activityInstanceId: '4294983744',
-          flowNodeId: 'foo'
-        }
-      });
-      await flushPromises();
-      node.update();
-
-      // then
-      // Transparent Heading
-      expect(node.contains(`Camunda Operate Instance ${INSTANCE.id}`)).toBe(
-        true
-      );
-      // HeaderNode
-      const HeaderNode = node.find(Header);
-      expect(HeaderNode).toHaveLength(1);
-      // Detail in Header
-      const DetailNode = HeaderNode.prop('detail');
-      expect(DetailNode.type).toBe(InstanceDetail);
-      expect(DetailNode.props.instance).toEqual(INSTANCE);
-      //DiagramPanel
-      const DiagramPanelNode = node.find(DiagramPanel);
-      expect(DiagramPanelNode).toHaveLength(1);
-      expect(DiagramPanelNode.prop('instance')).toEqual(INSTANCE);
-      expect(DiagramPanelNode.prop('selectableFlowNodes')).toEqual([
-        'foo',
-        'bar',
-        'taskA'
-      ]);
-      expect(DiagramPanelNode.prop('selectedFlowNode')).toBe('foo');
-      expect(DiagramPanelNode.prop('onFlowNodeSelected')).toBe(
-        node.instance().handleFlowNodeSelection
-      );
-      expect(DiagramPanelNode.prop('flowNodeStateOverlays')).toEqual([
-        {
-          id: 'foo',
-          state: ACTIVITY_STATE.COMPLETED
-        },
-        {
-          id: 'taskA',
-          state: ACTIVITY_STATE.INCIDENT
-        }
-      ]);
-
-      // InstanceHistory
-      const InstanceHistoryNode = node.find(InstanceHistory);
-      expect(InstanceHistoryNode).toHaveLength(1);
-      expect(InstanceHistoryNode.prop('instance')).toEqual(INSTANCE);
-      expect(InstanceHistoryNode.prop('activitiesDetails')).toEqual(
-        ACTIVITIES_DETAILS
-      );
-      expect(InstanceHistoryNode.prop('selectedActivityInstanceId')).toBe(
-        '4294983744'
-      );
-      expect(InstanceHistoryNode.prop('onActivityInstanceSelected')).toBe(
-        node.instance().handleActivityInstanceSelection
-      );
-      // snapshot
-      expect(node).toMatchSnapshot();
     });
   });
 });
