866f0a1e20ebc21dc7cf6a33b0c352ec72f8df83
Sebastian Bathke <sebastian.bathke@camunda.com>
2019-01-14 16:40:20 +0100

    feat(reimport): reimport as part of the distribution
    relates to #OPT-1790

---
+++ distro/src/reimport/reimport.bat
@@ -0,0 +1,37 @@
+@echo off
+
+:: ####################################################################################################################
+:: #                                                                                                                  #
+:: #                                        Optimize Reimport Script                                                  #
+:: #                                                                                                                  #
+:: #   Purges imported engine data from the Optimize database in order to perform a reimport of engine data           #
+:: #   without losing your Optimize data (e.g. report definitions). See:                                              #
+:: #   https://docs.camunda.org/optimize/latest/technical-guide/update/#force-reimport-of-engine-data-in-optimize     #
+:: #                                                                                                                  #
+:: ####################################################################################################################
+
+set BASEDIR=%~dp0
+cd "%BASEDIR%"
+
+:: now set the path to java
+IF DEFINED JAVA_HOME (
+  set JAVA="%JAVA_HOME%\bin\java.exe"
+) ELSE (
+  set JAVA=java
+)
+
+:: check if there are custom JVM options set.
+IF NOT DEFINED OPTIMIZE_JAVA_OPTS (
+  set OPTIMIZE_JAVA_OPTS=-Xms128m -Xmx512m -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m
+)
+
+:: Set up classpath
+set OPTIMIZE_CLASSPATH="%BASEDIR%\..\environment;%BASEDIR%\*;%BASEDIR%\..\optimize-backend-${project.version}.jar"
+
+echo.
+echo Starting Camunda Optimize Reimport ${project.version}...
+echo.
+
+:: start optimize
+set OPTIMIZE_CMD=%JAVA% %OPTIMIZE_JAVA_OPTS% -cp %OPTIMIZE_CLASSPATH% -Dfile.encoding=UTF-8 org.camunda.optimize.reimport.preparation.ReimportPreparation
+call %OPTIMIZE_CMD%
