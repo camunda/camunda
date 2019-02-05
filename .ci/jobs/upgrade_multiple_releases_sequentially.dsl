7aa000eb42e5e0f823e9c4db0339e0199b3cf2c0
Sebastian Bathke <sebastian.bathke@camunda.com>
2019-02-05 13:02:28 +0100

    chore(migration-test): nightly jenkins sequential upgrade job
    relates to #OPT-1884

---
+++ .ci/jobs/upgrade_multiple_releases_sequentially.dsl
@@ -0,0 +1,24 @@
+pipelineJob('upgrade_multiple_releases_sequentially') {
+
+  displayName 'Sequential Optimize Upgrade'
+  description 'Upgrades sequentially over multiple Optimize releases and ensures reports still work.'
+
+  definition {
+    cps {
+      script(readFileFromWorkspace('.ci/pipelines/upgrade_multiple_releases_sequentially.groovy'))
+      sandbox()
+    }
+  }
+
+  parameters {
+    stringParam('BRANCH', 'master', 'Branch to use for performance tests.')
+    stringParam('CAMBPM_VERSION', '7.10.0', 'Camunda BPM version to use.')
+    stringParam('ES_VERSION', '6.2.0', 'Elasticsearch version to use.')
+    stringParam('START_VERSION', '2.1.0', 'Version to start the sequential upgrade from')
+    stringParam('UPGRADE_TIMEOUT', '1800', 'Timeout for the upgrade to complete')
+  }
+
+  triggers {
+    cron('H 5 * * *')
+  }
+}
---
+++ .ci/pipelines/upgrade_multiple_releases_sequentially.groovy
@@ -0,0 +1,219 @@
+#!/usr/bin/env groovy
+
+// general properties for CI execution
+def static MAVEN_DOCKER_IMAGE() { return "maven:3.5.3-jdk-8-slim" }
+
+def static CAMBPM_DOCKER_IMAGE(String cambpmVersion) {
+  return "registry.camunda.cloud/camunda-bpm-platform-ee:${cambpmVersion}"
+}
+
+def static ELASTICSEARCH_DOCKER_IMAGE(String esVersion) {
+  return "docker.elastic.co/elasticsearch/elasticsearch-oss:${esVersion}"
+}
+
+static String mavenUpgradeTestAgent(esVersion = '6.2.0', cambpmVersion = '7.10.0') {
+  return """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    agent: optimize-ci-build
+spec:
+  nodeSelector:
+    cloud.google.com/gke-nodepool: slaves
+  imagePullSecrets:
+    - name: registry-camunda-cloud-secret
+  volumes:
+  - name: cambpm-config
+    configMap:
+      # Defined in: https://github.com/camunda-ci/k8s-infrastructure/tree/master/infrastructure/ci-30-162810/deployments/optimize
+      name: ci-optimize-cambpm-config
+  initContainers:
+    - name: init-sysctl
+      image: busybox
+      imagePullPolicy: Always
+      command: ["sysctl", "-w", "vm.max_map_count=262144"]
+      securityContext:
+        privileged: true
+  containers:
+  - name: maven
+    image: ${MAVEN_DOCKER_IMAGE()}
+    command: ["cat"]
+    tty: true
+    env:
+      # every JVM process will get a 1/4 of HEAP from total memory
+      - name: JAVA_TOOL_OPTIONS
+        value: |
+          -XX:+UnlockExperimentalVMOptions
+          -XX:+UseCGroupMemoryLimitForHeap
+      - name: LIMITS_CPU
+        valueFrom:
+          resourceFieldRef:
+            resource: limits.cpu
+      - name: TZ
+        value: Europe/Berlin
+      - name: DOCKER_HOST
+        value: tcp://localhost:2375
+    resources:
+      limits:
+        cpu: 3
+        memory: 3Gi
+      requests:
+        cpu: 3
+        memory: 3Gi
+  - name: cambpm
+    image: ${CAMBPM_DOCKER_IMAGE(cambpmVersion)}
+    env:
+      - name: JAVA_TOOL_OPTIONS
+        value: |
+          -XX:+UnlockExperimentalVMOptions
+          -XX:+UseCGroupMemoryLimitForHeap
+          -XX:MaxRAMFraction=1
+      - name: TZ
+        value: Europe/Berlin
+    resources:
+      limits:
+        cpu: 1
+        memory: 1Gi
+      requests:
+        cpu: 1
+        memory: 1Gi
+    volumeMounts:
+    - name: cambpm-config
+      mountPath: /camunda/conf/bpm-platform.xml
+      subPath: bpm-platform.xml
+    - name: cambpm-config
+      mountPath: /camunda/conf/tomcat-users.xml
+      subPath: tomcat-users.xml
+    - name: cambpm-config
+      mountPath: /camunda/webapps/manager/META-INF/context.xml
+      subPath: context.xml
+  - name: elasticsearch
+    image: ${ELASTICSEARCH_DOCKER_IMAGE(esVersion)}
+    env:
+    - name: ES_JAVA_OPTS
+      value: "-Xms1g -Xmx1g"
+    - name: cluster.name
+      value: elasticsearch
+    - name: discovery.type
+      value: single-node
+    - name: bootstrap.memory_lock
+      value: true
+    securityContext:
+      privileged: true
+      capabilities:
+        add: ["IPC_LOCK"]
+    resources:
+      requests:
+        cpu: 1
+        memory: 2Gi
+"""
+}
+
+void buildNotification(String buildStatus) {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
+
+  String buildResultUrl = "${env.BUILD_URL}"
+  if (env.RUN_DISPLAY_URL) {
+    buildResultUrl = "${env.RUN_DISPLAY_URL}"
+  }
+
+  def subject = "[${buildStatus}] - ${env.JOB_NAME} - Build #${env.BUILD_NUMBER}"
+  def body = "See: ${buildResultUrl}"
+  def recipients = [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']]
+
+  emailext subject: subject, body: body, recipientProviders: recipients
+}
+
+void runMaven(String cmd) {
+  sh("mvn ${cmd} -s settings.xml -B --fail-at-end -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn")
+}
+
+void upgradeTestSteps(String startVersion, String targetVersion) {
+  container('maven') {
+    def versionList = generateUpgradeList(startVersion, targetVersion)
+    runMaven("install -Dskip.docker -DskipTests -pl backend -am -Pproduction,engine-latest")
+    runMaven("verify -Dskip.docker -pl qa/upgrade-es-schema-tests -Pupgrade-es-schema-tests -Dupgrade.versions=${versionList}")
+  }
+}
+
+static String generateUpgradeList(String startVersion, String targetVersion) {
+  def tokenizedStartVersion = startVersion.tokenize('.')
+  def startMajorVersion = tokenizedStartVersion[0] as Integer
+  def startMinorVersion = tokenizedStartVersion[1] as Integer
+
+  def tokenizedTargetVersion = targetVersion.tokenize('.')
+  def targetMajorVersion = tokenizedTargetVersion[0] as Integer
+  def targetMinorVersion = tokenizedTargetVersion[1] as Integer
+
+  def versionList = []
+  for (def majorVersion = startMajorVersion; majorVersion <= targetMajorVersion; majorVersion++) {
+    for (def minorVersion = startMinorVersion; minorVersion < targetMinorVersion; minorVersion++) {
+      versionList.add("$majorVersion.$minorVersion.0")
+    }
+  }
+
+  versionList.add(targetVersion)
+
+  return versionList.join(",")
+}
+
+pipeline {
+
+  agent {
+    kubernetes {
+      cloud 'optimize-ci'
+      label "optimize-ci-build-seq-upgrade-${env.BUILD_ID}"
+      defaultContainer 'jnlp'
+      yaml mavenUpgradeTestAgent()
+    }
+  }
+
+  environment {
+    NEXUS = credentials("camunda-nexus")
+  }
+
+  options {
+    buildDiscarder(logRotator(numToKeepStr: '5'))
+    timestamps()
+    timeout(time: 30, unit: 'MINUTES')
+  }
+
+  stages {
+    stage('Prepare') {
+      steps {
+        git url: 'git@github.com:camunda/camunda-optimize',
+                branch: "${params.BRANCH}",
+                credentialsId: 'camunda-jenkins-github-ssh',
+                poll: false
+      }
+    }
+    stage('Install required packages') {
+      steps {
+        container('maven') {
+          sh 'apt-get update && apt-get install jq netcat -y'
+        }
+      }
+    }
+    stage('Sequential Upgrade Test') {
+      environment {
+        VERSION = readMavenPom().getVersion()
+      }
+      steps {
+        upgradeTestSteps("${params.START_VERSION}", "${VERSION}")
+      }
+      post {
+        always {
+          junit testResults: 'backend/target/failsafe-reports/**/*.xml', allowEmptyResults: true, keepLongStdio: true
+        }
+      }
+    }
+  }
+
+  post {
+    changed {
+      buildNotification(currentBuild.result)
+    }
+  }
+}
