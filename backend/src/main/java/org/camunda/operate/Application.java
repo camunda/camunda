88239b134debaf3934bc2c231b1c8f7613914e34
Svetlana Dorokhova <svetlana.dorokhova@camunda.com>
2019-02-07 17:14:32 +0100

    conf(backend): configure log4j instead of logback
    Related with OPE-388

--- backend/src/main/java/org/camunda/operate/Application.java
+++ backend/src/main/java/org/camunda/operate/Application.java
@@ -16,6 +16,10 @@ public class Application {
   private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   public static void main(String[] args) throws Exception {
+
+    //To ensure that debug logging performed using java.util.logging is routed into Log4j 2
+    System.setProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
+
     final SpringApplication springApplication = new SpringApplication(Application.class);
     springApplication.setAdditionalProfiles("auth");
     springApplication.run(args);
