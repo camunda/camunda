1e084753f400bd3482e87ad1849f57a0dbf857af
Sebastian Menski <sebastian.menski@camunda.com>
2019-02-18 09:12:58 +0100

    chore(qa): increase timeout for BrokerReprocessingTest to 2min

--- qa/integration-tests/src/test/java/io/zeebe/broker/it/startup/BrokerReprocessingTest.java
+++ qa/integration-tests/src/test/java/io/zeebe/broker/it/startup/BrokerReprocessingTest.java
@@ -143,7 +143,7 @@ public class BrokerReprocessingTest {
 
   @Rule public ExpectedException exception = ExpectedException.none();
 
-  @Rule public Timeout timeout = new Timeout(30, TimeUnit.SECONDS);
+  @Rule public Timeout timeout = new Timeout(120, TimeUnit.SECONDS);
 
   private Runnable restartAction = () -> {};
 
