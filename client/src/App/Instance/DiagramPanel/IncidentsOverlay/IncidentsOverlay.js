99dc5966754f9c521613ff0c4c7aa8e73d371492
Raluca Stan <raluca.stan@camunda.com>
2019-02-28 14:27:43 +0100

    refactor: replace component with function related to OPE-439

--- client/src/App/Instance/DiagramPanel/IncidentsOverlay/IncidentsOverlay.js
+++ client/src/App/Instance/DiagramPanel/IncidentsOverlay/IncidentsOverlay.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import * as Styled from './styled';
 
-export default class IncidentsOverlay extends React.Component {
-  render() {
-    return <Styled.Overlay>' '</Styled.Overlay>;
-  }
+function IncidentsOverlay() {
+  return <Styled.Overlay>' '</Styled.Overlay>;
 }
+
+export default IncidentsOverlay;
