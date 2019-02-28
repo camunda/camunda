c79451b4e32a3d1bb799b806356a16ab70d05d9b
Raluca Stan <raluca.stan@camunda.com>
2019-02-28 11:52:17 +0100

    fix: fix typo

--- client/src/App/Instance/DiagramPanel/Incidents/Incidents.js
+++ client/src/App/Instance/DiagramPanel/Incidents/Incidents.js
@@ -3,6 +3,6 @@ import * as Styled from './styled';
 
 export default class Incidents extends React.Component {
   render() {
-    return <Styled.Wrapper>incidents baby</Styled.Wrapper>;
+    return <Styled.Wrapper>' '</Styled.Wrapper>;
   }
 }
