01143ea47e6aa8389050b005656fa55c987c4341
siffogh <siffogh3@gmail.com>
2019-03-04 14:14:38 +0100

    chore(lint): fix linting issues

--- client/src/App/Instance/DiagramPanel/IncidentsOverlay/IncidentsOverlay.js
+++ client/src/App/Instance/DiagramPanel/IncidentsOverlay/IncidentsOverlay.js
@@ -1,8 +1,23 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a commercial license.
+ * You may not use this file except in compliance with the commercial license.
+ */
+
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import * as Styled from './styled';
 
 function IncidentsOverlay(props) {
   return <Styled.Overlay>{props.children}</Styled.Overlay>;
 }
+
+IncidentsOverlay.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node)
+  ])
+};
 
 export default IncidentsOverlay;
