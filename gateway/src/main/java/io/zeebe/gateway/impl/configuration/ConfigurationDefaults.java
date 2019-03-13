449ea10e61712d768cccb166ecc8f5ed79e2f71d
Sebastian Menski <sebastian.menski@camunda.com>
2019-03-13 23:24:08 +0100

    fix(broker): increase gateway transport buffer to default 64 megabyte

--- gateway/src/main/java/io/zeebe/gateway/impl/configuration/ConfigurationDefaults.java
+++ gateway/src/main/java/io/zeebe/gateway/impl/configuration/ConfigurationDefaults.java
@@ -21,7 +21,7 @@ public class ConfigurationDefaults {
   public static final int DEFAULT_PORT = 26500;
   public static final String DEFAULT_CONTACT_POINT_HOST = "127.0.0.1";
   public static final int DEFAULT_CONTACT_POINT_PORT = 26501;
-  public static final String DEFAULT_TRANSPORT_BUFFER_SIZE = "2M";
+  public static final String DEFAULT_TRANSPORT_BUFFER_SIZE = "128M";
   public static final int DEFAULT_MANAGEMENT_THREADS = 1;
   public static final String DEFAULT_REQUEST_TIMEOUT = "15s";
 }
