13ea140b3f854e2b3b13a2c6d61130f96fb57b30
pede <patrick.dehn@camunda.com>
2019-05-02 16:18:24 +0200

    style(dashboard): add scrollbars to incident panels
    related to OPE-418

--- client/src/App/Dashboard/styled.js
+++ client/src/App/Dashboard/styled.js
@@ -6,27 +6,24 @@
 
 import styled from 'styled-components';
 import {Colors, themed, themeStyle} from 'modules/theme';
-import {HEADER_HEIGHT} from './../Header/styled';
 
 export const Dashboard = styled.main`
   padding: 0 20px 38px;
   display: flex;
   flex-direction: column;
-  height: calc(100vh - ${HEADER_HEIGHT}px);
 `;
 
 export const TitleWrapper = styled.div`
   display: flex;
-  flex-grow: 1;
 `;
 
 export const Tile = themed(styled.div`
   // flex-parent
   display: flex;
   flex-direction: column;
+  overflow: auto;
 
   // flex-child
-  flex-grow: 1;
   margin: 8px 8px 0 0;
   padding: 24px 12px 24px 32px;
   width: 50%;
@@ -68,6 +65,5 @@ export const TileTitle = themed(styled.h2`
 
 export const TileContent = styled.div`
   overflow: auto;
-  flex-grow: 1;
-  position: relative;
+  height: 644px;
 `;
