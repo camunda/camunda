03d3a087578435ffba1149a68fd3f3ce90047900
Sebastian Menski <sebastian.menski@camunda.com>
2019-05-03 15:41:47 +0200

    chore(ci): test in offline mode to prevent dependency resolving race condition
    - download all dependency in the build phase using dependency:go-offline
    - add profile prepare-offline to add explicit surefire-junit dependency which
      are otherwise missed by dependency:go-offline
    - during unit and it tests run maven in offline mode
    
    closes #2379

--- .ci/scripts/distribution/build-java.sh
+++ .ci/scripts/distribution/build-java.sh
@@ -3,4 +3,4 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-su jenkins -c "mvn -B -T2 -s settings.xml -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check install -Pspotbugs"
+su jenkins -c "mvn -B -T$LIMITS_CPU -s settings.xml -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline install -Pspotbugs,prepare-offline"
--- .ci/scripts/distribution/it-java.sh
+++ .ci/scripts/distribution/it-java.sh
@@ -3,4 +3,4 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-su jenkins -c "mvn -B -T$LIMITS_CPU -s settings.xml verify -P skip-unstable-ci,parallel-tests -pl qa/integration-tests"
+su jenkins -c "mvn -o -B -T$LIMITS_CPU -s settings.xml verify -P skip-unstable-ci,parallel-tests -pl qa/integration-tests"
