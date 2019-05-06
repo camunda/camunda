aa9ea69f3938a21fca3a217cdc1b94fad7ca959e
Fabian Hinsenkamp <Fabian.Hinsenkamp@camunda.com>
2019-05-06 12:59:33 +0200

    style(PopoverOverlay): adjust font size and color
    related to OPE-451

--- client/src/modules/components/Diagram/PopoverOverlay/styled.js
+++ client/src/modules/components/Diagram/PopoverOverlay/styled.js
@@ -265,21 +265,29 @@ export const CodeLine = themed(styled.p`
   margin: 3px;
   margin-left: 0;
   line-height: 14px;
+  color: ${themeStyle({
+    dark: 'rgba(255, 255, 255, 0.9)',
+    light: Colors.uiLight06
+  })};
   font-family: IBMPlexMono;
   font-size: 14px;
 
   &:before {
+    font-size: 12px;
     box-sizing: border-box;
     text-align: right;
     counter-increment: line;
     content: counter(line);
     color: ${themeStyle({
       dark: '#ffffff',
-      light: '#000000'
+      light: Colors.uiLight06
     })};
     display: inline-block;
     width: 35px;
-    opacity: 0.5;
+    opacity: ${themeStyle({
+      dark: 0.5,
+      light: 0.65
+    })};
     padding-right: 11px;
     -webkit-user-select: none;
   }
