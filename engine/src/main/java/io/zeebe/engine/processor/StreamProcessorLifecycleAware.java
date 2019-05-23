naveendevelops/zeebe

chore(engine): refactor stream processing
 * merge TypedStreamProcessing with StreamProcessorController to
 StreamProcessor
 * StreamProcessor also becomes own service, easier to install
 * Builders and Contexts are removed or replaced
 * remove StreamProcessorControl from tests
 * refactor processing tests

engine/src/main/java/io/zeebe/engine/processor/StreamProcessorLifecycleAware.java
@@ -19,10 +19,10 @@ package io.zeebe.engine.processor;
 
 public interface StreamProcessorLifecycleAware {
 
-  default void onOpen(TypedStreamProcessor streamProcessor) {}
+  default void onOpen(ProcessingContext context) {}
 
   /** Callback after reprocessing was successful and before regular processing begins */
-  default void onRecovered(TypedStreamProcessor streamProcessor) {}
+  default void onRecovered(ProcessingContext context) {}
 
   default void onClose() {}
 }

engine/src/main/java/io/zeebe/engine/processor/workflow/timer/DueDateTimerChecker.java
@@ -17,9 +17,8 @@
  */
 package io.zeebe.engine.processor.workflow.timer;
 
+import io.zeebe.engine.processor.ProcessingContext;
 import io.zeebe.engine.processor.StreamProcessorLifecycleAware;
-import io.zeebe.engine.processor.TypedStreamEnvironment;
-import io.zeebe.engine.processor.TypedStreamProcessor;
 import io.zeebe.engine.processor.TypedStreamWriterImpl;
 import io.zeebe.engine.state.deployment.WorkflowState;
 import io.zeebe.engine.state.instance.TimerInstance;
@@ -104,15 +103,13 @@ public class DueDateTimerChecker implements StreamProcessorLifecycleAware {
   }
 
   @Override
-  public void onOpen(final TypedStreamProcessor streamProcessor) {
-    this.actor = streamProcessor.getActor();
-
-    final TypedStreamEnvironment env = streamProcessor.getEnvironment();
-    streamWriter = new TypedStreamWriterImpl(env.getStream(), env.getEventRegistry());
+  public void onOpen(final ProcessingContext processingContext) {
+    this.actor = processingContext.getActor();
+    streamWriter = new TypedStreamWriterImpl(processingContext.getLogStream());
   }
 
   @Override
-  public void onRecovered(final TypedStreamProcessor streamProcessor) {
+  public void onRecovered(final ProcessingContext processingContext) {
     // check if timers are due after restart
     triggerTimers();
   }

engine/src/test/java/io/zeebe/engine/processor/workflow/WorkflowInstanceStreamProcessorRule.java
@@ -26,17 +26,15 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import io.zeebe.engine.processor.ProcessingContext;
 import io.zeebe.engine.processor.StreamProcessorLifecycleAware;
 import io.zeebe.engine.processor.TypedRecord;
-import io.zeebe.engine.processor.TypedStreamProcessor;
 import io.zeebe.engine.processor.workflow.job.JobEventProcessors;
 import io.zeebe.engine.processor.workflow.message.command.SubscriptionCommandSender;
 import io.zeebe.engine.processor.workflow.timer.DueDateTimerChecker;
-import io.zeebe.engine.state.ZeebeState;
 import io.zeebe.engine.state.deployment.WorkflowState;
 import io.zeebe.engine.util.CopiedTypedEvent;
 import io.zeebe.engine.util.Records;
-import io.zeebe.engine.util.StreamProcessorControl;
 import io.zeebe.engine.util.StreamProcessorRule;
 import io.zeebe.engine.util.TypedRecordStream;
 import io.zeebe.model.bpmn.Bpmn;
@@ -84,9 +82,7 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
 
   private SubscriptionCommandSender mockSubscriptionCommandSender;
 
-  private StreamProcessorControl streamProcessor;
   private WorkflowState workflowState;
-  private ZeebeState zeebeState;
   private ActorControl actor;
 
   public WorkflowInstanceStreamProcessorRule(StreamProcessorRule streamProcessorRule) {
@@ -114,26 +110,20 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
             anyLong(), anyLong(), anyLong(), any(), any()))
         .thenReturn(true);
 
-    streamProcessor =
-        environmentRule.runTypedStreamProcessor(
-            (typedEventStreamProcessorBuilder, zeebeDb, dbContext) -> {
-              zeebeState = new ZeebeState(zeebeDb, dbContext);
-              workflowState = zeebeState.getWorkflowState();
-              WorkflowEventProcessors.addWorkflowProcessors(
-                  typedEventStreamProcessorBuilder,
-                  zeebeState,
-                  mockSubscriptionCommandSender,
-                  new CatchEventBehavior(zeebeState, mockSubscriptionCommandSender, 1),
-                  new DueDateTimerChecker(workflowState));
+    environmentRule.startTypedStreamProcessor(
+        (typedRecordProcessors, zeebeState) -> {
+          workflowState = zeebeState.getWorkflowState();
+          WorkflowEventProcessors.addWorkflowProcessors(
+              zeebeState,
+              typedRecordProcessors,
+              mockSubscriptionCommandSender,
+              new CatchEventBehavior(zeebeState, mockSubscriptionCommandSender, 1),
+              new DueDateTimerChecker(workflowState));
 
-              JobEventProcessors.addJobProcessors(typedEventStreamProcessorBuilder, zeebeState);
-              typedEventStreamProcessorBuilder.withListener(this);
-              return typedEventStreamProcessorBuilder.build();
-            });
-  }
-
-  public StreamProcessorControl getStreamProcessor() {
-    return streamProcessor;
+          JobEventProcessors.addJobProcessors(typedRecordProcessors, zeebeState);
+          typedRecordProcessors.withListener(this);
+          return typedRecordProcessors;
+        });
   }
 
   public void deploy(final BpmnModelInstance modelInstance, int deploymentKey, int version) {
@@ -236,17 +226,6 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
             .orElse(null);
   }
 
-  private TypedRecord<WorkflowInstanceRecord> awaitAndGetFirstRecordInState(
-      final WorkflowInstanceIntent state) {
-    awaitFirstRecordInState(state);
-    return environmentRule
-        .events()
-        .onlyWorkflowInstanceRecords()
-        .withIntent(state)
-        .findFirst()
-        .get();
-  }
-
   private TypedRecord<JobRecord> awaitAndGetFirstRecordInState(final JobIntent state) {
     awaitFirstRecordInState(state);
     return environmentRule.events().onlyJobRecords().withIntent(state).findFirst().get();
@@ -319,12 +298,12 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
   }
 
   @Override
-  public void onOpen(TypedStreamProcessor streamProcessor) {
-    actor = streamProcessor.getActor();
+  public void onOpen(ProcessingContext processingContext) {
+    actor = processingContext.getActor();
   }
 
   @Override
-  public void onRecovered(TypedStreamProcessor streamProcessor) {
+  public void onRecovered(ProcessingContext processingContext) {
     // recovered
   }
 

camunda/camunda

 * to avoid shared mutable state introduced read only context
 * refactor processing and reprocessing tests

engine/src/main/java/io/zeebe/engine/processor/StreamProcessorLifecycleAware.java
@@ -19,10 +19,10 @@ package io.zeebe.engine.processor;
 
 public interface StreamProcessorLifecycleAware {
 
-  default void onOpen(TypedStreamProcessor streamProcessor) {}
+  default void onOpen(ReadonlyProcessingContext context) {}
 
   /** Callback after reprocessing was successful and before regular processing begins */
-  default void onRecovered(TypedStreamProcessor streamProcessor) {}
+  default void onRecovered(ReadonlyProcessingContext context) {}
 
   default void onClose() {}
 }

engine/src/main/java/io/zeebe/engine/processor/workflow/timer/DueDateTimerChecker.java
@@ -17,9 +17,8 @@
  */
 package io.zeebe.engine.processor.workflow.timer;
 
+import io.zeebe.engine.processor.ReadonlyProcessingContext;
 import io.zeebe.engine.processor.StreamProcessorLifecycleAware;
-import io.zeebe.engine.processor.TypedStreamEnvironment;
-import io.zeebe.engine.processor.TypedStreamProcessor;
 import io.zeebe.engine.processor.TypedStreamWriterImpl;
 import io.zeebe.engine.state.deployment.WorkflowState;
 import io.zeebe.engine.state.instance.TimerInstance;
@@ -104,15 +103,13 @@ public class DueDateTimerChecker implements StreamProcessorLifecycleAware {
   }
 
   @Override
-  public void onOpen(final TypedStreamProcessor streamProcessor) {
-    this.actor = streamProcessor.getActor();
-
-    final TypedStreamEnvironment env = streamProcessor.getEnvironment();
-    streamWriter = new TypedStreamWriterImpl(env.getStream(), env.getEventRegistry());
+  public void onOpen(final ReadonlyProcessingContext processingContext) {
+    this.actor = processingContext.getActor();
+    streamWriter = new TypedStreamWriterImpl(processingContext.getLogStream());
   }
 
   @Override
-  public void onRecovered(final TypedStreamProcessor streamProcessor) {
+  public void onRecovered(final ReadonlyProcessingContext processingContext) {
     // check if timers are due after restart
     triggerTimers();
   }

engine/src/test/java/io/zeebe/engine/processor/workflow/WorkflowInstanceStreamProcessorRule.java
@@ -26,17 +26,15 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import io.zeebe.engine.processor.ReadonlyProcessingContext;
 import io.zeebe.engine.processor.StreamProcessorLifecycleAware;
 import io.zeebe.engine.processor.TypedRecord;
-import io.zeebe.engine.processor.TypedStreamProcessor;
 import io.zeebe.engine.processor.workflow.job.JobEventProcessors;
 import io.zeebe.engine.processor.workflow.message.command.SubscriptionCommandSender;
 import io.zeebe.engine.processor.workflow.timer.DueDateTimerChecker;
-import io.zeebe.engine.state.ZeebeState;
 import io.zeebe.engine.state.deployment.WorkflowState;
 import io.zeebe.engine.util.CopiedTypedEvent;
 import io.zeebe.engine.util.Records;
-import io.zeebe.engine.util.StreamProcessorControl;
 import io.zeebe.engine.util.StreamProcessorRule;
 import io.zeebe.engine.util.TypedRecordStream;
 import io.zeebe.model.bpmn.Bpmn;
@@ -84,9 +82,7 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
 
   private SubscriptionCommandSender mockSubscriptionCommandSender;
 
-  private StreamProcessorControl streamProcessor;
   private WorkflowState workflowState;
-  private ZeebeState zeebeState;
   private ActorControl actor;
 
   public WorkflowInstanceStreamProcessorRule(StreamProcessorRule streamProcessorRule) {
@@ -114,26 +110,20 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
             anyLong(), anyLong(), anyLong(), any(), any()))
         .thenReturn(true);
 
-    streamProcessor =
-        environmentRule.runTypedStreamProcessor(
-            (typedEventStreamProcessorBuilder, zeebeDb, dbContext) -> {
-              zeebeState = new ZeebeState(zeebeDb, dbContext);
-              workflowState = zeebeState.getWorkflowState();
-              WorkflowEventProcessors.addWorkflowProcessors(
-                  typedEventStreamProcessorBuilder,
-                  zeebeState,
-                  mockSubscriptionCommandSender,
-                  new CatchEventBehavior(zeebeState, mockSubscriptionCommandSender, 1),
-                  new DueDateTimerChecker(workflowState));
+    environmentRule.startTypedStreamProcessor(
+        (typedRecordProcessors, zeebeState) -> {
+          workflowState = zeebeState.getWorkflowState();
+          WorkflowEventProcessors.addWorkflowProcessors(
+              zeebeState,
+              typedRecordProcessors,
+              mockSubscriptionCommandSender,
+              new CatchEventBehavior(zeebeState, mockSubscriptionCommandSender, 1),
+              new DueDateTimerChecker(workflowState));
 
-              JobEventProcessors.addJobProcessors(typedEventStreamProcessorBuilder, zeebeState);
-              typedEventStreamProcessorBuilder.withListener(this);
-              return typedEventStreamProcessorBuilder.build();
-            });
-  }
-
-  public StreamProcessorControl getStreamProcessor() {
-    return streamProcessor;
+          JobEventProcessors.addJobProcessors(typedRecordProcessors, zeebeState);
+          typedRecordProcessors.withListener(this);
+          return typedRecordProcessors;
+        });
   }
 
   public void deploy(final BpmnModelInstance modelInstance, int deploymentKey, int version) {
@@ -236,17 +226,6 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
             .orElse(null);
   }
 
-  private TypedRecord<WorkflowInstanceRecord> awaitAndGetFirstRecordInState(
-      final WorkflowInstanceIntent state) {
-    awaitFirstRecordInState(state);
-    return environmentRule
-        .events()
-        .onlyWorkflowInstanceRecords()
-        .withIntent(state)
-        .findFirst()
-        .get();
-  }
-
   private TypedRecord<JobRecord> awaitAndGetFirstRecordInState(final JobIntent state) {
     awaitFirstRecordInState(state);
     return environmentRule.events().onlyJobRecords().withIntent(state).findFirst().get();
@@ -319,12 +298,12 @@ public class WorkflowInstanceStreamProcessorRule extends ExternalResource
   }
 
   @Override
-  public void onOpen(TypedStreamProcessor streamProcessor) {
-    actor = streamProcessor.getActor();
+  public void onOpen(ReadonlyProcessingContext processingContext) {
+    actor = processingContext.getActor();
   }
 
   @Override
-  public void onRecovered(TypedStreamProcessor streamProcessor) {
+  public void onRecovered(ReadonlyProcessingContext processingContext) {
     // recovered
   }
 