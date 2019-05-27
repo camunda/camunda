ab16b6a1f578bfdd962a9312439126c4f861593f
Sebastian Menski <sebastian.menski@camunda.com>
2019-05-27 15:48:22 +0200

    chore(docker): set ZEEBE_GATEWAY_CLUSTER_HOST in start-up script
    closes #2526

--- docker/utils/startup.sh
+++ docker/utils/startup.sh
@@ -3,6 +3,7 @@
 configFile=/usr/local/zeebe/conf/zeebe.cfg.toml
 
 export ZEEBE_HOST=${ZEEBE_HOST:-$(hostname -i)}
+export ZEEBE_GATEWAY_CLUSTER_HOST=${ZEEBE_GATEWAY_CLUSTER_HOST:-${ZEEBE_HOST}}
 
 if [ "$ZEEBE_STANDALONE_GATEWAY" = "true" ]; then
     exec /usr/local/zeebe/bin/gateway
