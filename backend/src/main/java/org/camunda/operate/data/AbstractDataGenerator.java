49506631e7631a7650ac36d4af7c08d616912278
SvetlanaDorokhova <sveta_d@list.ru>
2019-05-29 09:56:38 +0200

    impl(data): get rid of Zeebe Get workflows request
    Related with OPE-570

--- backend/src/main/java/org/camunda/operate/data/AbstractDataGenerator.java
+++ backend/src/main/java/org/camunda/operate/data/AbstractDataGenerator.java
@@ -6,13 +6,19 @@
 package org.camunda.operate.data;
 
 import javax.annotation.PreDestroy;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+
+import org.camunda.operate.property.OperateProperties;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.zeebe.client.ZeebeClient;
-import io.zeebe.client.api.commands.Workflows;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 
 public abstract class AbstractDataGenerator implements DataGenerator {
@@ -23,6 +29,13 @@ public abstract class AbstractDataGenerator implements DataGenerator {
 
   @Autowired
   protected ZeebeClient client;
+
+  @Autowired
+  @Qualifier("zeebeEsClient")
+  private RestHighLevelClient zeebeEsClient;
+
+  @Autowired
+  private OperateProperties operateProperties;
 
   protected boolean manuallyCalled = false;
 
@@ -64,10 +77,16 @@ public abstract class AbstractDataGenerator implements DataGenerator {
 
   public boolean shouldCreateData(boolean manuallyCalled) {
     if (!manuallyCalled) {    //when called manually, always create the data
-      final Workflows workflows = client.newWorkflowRequest().send().join();
-      if (workflows != null && workflows.getWorkflows().size() > 0) {
-        //data already exists
-        logger.debug("Data already exists in Zeebe.");
+      try {
+        GetIndexRequest request = new GetIndexRequest(operateProperties.getZeebeElasticsearch().getPrefix() + "*");
+        boolean exists = zeebeEsClient.indices().exists(request, RequestOptions.DEFAULT);
+        if (exists) {
+          //data already exists
+          logger.debug("Data already exists in Zeebe.");
+          return false;
+        }
+      } catch (IOException io) {
+        logger.debug("Error occurred while checking existance of data in Zeebe: {}. Demo data won't be created.", io.getMessage());
         return false;
       }
     }
