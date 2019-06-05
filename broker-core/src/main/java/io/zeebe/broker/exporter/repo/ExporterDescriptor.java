faac031f3b84557793778b504dd02699477bdf0a
Sebastian Menski <sebastian.menski@camunda.com>
2019-06-05 23:56:51 +0200

    fix(exporter-api): move Exporter interface to upper api package
    BREAKING CHANGE:
    - move io.zeebe.exporter.api.spi.Exporter interface to io.zeebe.exporter.api.ExpExporter

--- broker-core/src/main/java/io/zeebe/broker/exporter/repo/ExporterDescriptor.java
+++ broker-core/src/main/java/io/zeebe/broker/exporter/repo/ExporterDescriptor.java
@@ -18,7 +18,7 @@
 package io.zeebe.broker.exporter.repo;
 
 import io.zeebe.broker.exporter.context.ExporterConfiguration;
-import io.zeebe.exporter.api.spi.Exporter;
+import io.zeebe.exporter.api.Exporter;
 import java.util.Map;
 
 public class ExporterDescriptor {
--- exporters/elasticsearch-exporter/src/main/java/io/zeebe/exporter/ElasticsearchExporter.java
+++ exporters/elasticsearch-exporter/src/main/java/io/zeebe/exporter/ElasticsearchExporter.java
@@ -16,10 +16,10 @@
 package io.zeebe.exporter;
 
 import io.zeebe.exporter.ElasticsearchExporterConfiguration.IndexConfiguration;
+import io.zeebe.exporter.api.Exporter;
 import io.zeebe.exporter.api.context.Context;
 import io.zeebe.exporter.api.context.Controller;
 import io.zeebe.exporter.api.record.Record;
-import io.zeebe.exporter.api.spi.Exporter;
 import io.zeebe.protocol.clientapi.RecordType;
 import io.zeebe.protocol.clientapi.ValueType;
 import java.time.Duration;
