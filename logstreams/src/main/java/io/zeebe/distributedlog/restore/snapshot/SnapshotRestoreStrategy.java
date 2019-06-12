afirth/zeebe

chore(logstreams): reliable snapshot replication for restore

logstreams/src/main/java/io/zeebe/distributedlog/restore/snapshot/SnapshotRestoreStrategy.java
@@ -13,65 +13,57 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.zeebe.distributedlog.restore.impl;
+package io.zeebe.distributedlog.restore.snapshot;
 
 import io.atomix.cluster.MemberId;
-import io.zeebe.distributedlog.restore.RestoreClient;
 import io.zeebe.distributedlog.restore.RestoreStrategy;
 import io.zeebe.distributedlog.restore.log.LogReplicator;
-import io.zeebe.logstreams.state.SnapshotRequester;
 import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 
 public class SnapshotRestoreStrategy implements RestoreStrategy {
 
-  private final SnapshotRequester snapshotReplicator;
   private final MemberId server;
-  private final RestoreClient client;
   private final LogReplicator logReplicator;
-  private final Logger logger;
+  private final Logger log;
   private final long backupPosition;
+  private final SnapshotRestoreInfo snapshotRestoreInfo;
   private final long latestLocalPosition;
+  private RestoreSnapshotReplicator replicator;
 
   public SnapshotRestoreStrategy(
-      RestoreClient client,
       LogReplicator logReplicator,
-      SnapshotRequester snapshotReplicator,
+      RestoreSnapshotReplicator replicator,
+      SnapshotRestoreInfo snapshotRestoreInfo,
       long latestLocalPosition,
       long backupPosition,
       MemberId server,
-      Logger logger) {
-    this.client = client;
+      Logger log) {
     this.logReplicator = logReplicator;
-    this.snapshotReplicator = snapshotReplicator;
+    this.replicator = replicator;
+    this.snapshotRestoreInfo = snapshotRestoreInfo;
     this.latestLocalPosition = latestLocalPosition;
     this.backupPosition = backupPosition;
     this.server = server;
-    this.logger = logger;
+    this.log = log;
   }
 
   @Override
   public CompletableFuture<Long> executeRestoreStrategy() {
-    logger.debug("Restoring from snapshot");
-    final CompletableFuture<Long> replicated = CompletableFuture.completedFuture(null);
-
-    return replicated
-        .thenCompose(nothing -> client.requestSnapshotInfo(server))
-        .thenCompose(
-            numSnapshots -> snapshotReplicator.getLatestSnapshotsFrom(server, numSnapshots > 1))
-        .thenCompose(nothing -> onSnapshotsReplicated());
+    return replicator
+        .replicate(server, snapshotRestoreInfo.getSnapshotId(), snapshotRestoreInfo.getNumChunks())
+        .thenCompose(tuple -> onSnapshotsReplicated(tuple.getLeft(), tuple.getRight()));
   }
 
-  private CompletableFuture<Long> onSnapshotsReplicated() {
-    final long exporterPosition = snapshotReplicator.getExporterPosition();
-    final long processedPosition = snapshotReplicator.getProcessedPosition();
+  private CompletableFuture<Long> onSnapshotsReplicated(
+      long exporterPosition, long processedPosition) {
     final long fromPosition =
         Math.max(
             latestLocalPosition, // if exporter position is behind latestLocalPosition
             getFirstEventToBeReplicated(exporterPosition, processedPosition));
     final long toPosition = Math.max(processedPosition, backupPosition);
     // TODO: logstream.deleteAll(). https://github.com/zeebe-io/zeebe/issues/2509
-    logger.debug("Restored snapshot. Restoring events from {} to {}", fromPosition, toPosition);
+    log.debug("Restored snapshot. Restoring events from {} to {}", fromPosition, toPosition);
     return logReplicator.replicate(
         server, fromPosition, toPosition, fromPosition > latestLocalPosition);
   }

logstreams/src/test/java/io/zeebe/distributedlog/restore/snapshot/RestoreSnapshotReplicatorTest.java
@@ -1,0 +1,163 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.zeebe.distributedlog.restore.snapshot;
+
+import static io.zeebe.test.util.TestUtil.waitUntil;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.atomix.cluster.MemberId;
+import io.zeebe.distributedlog.restore.impl.ControllableRestoreClient;
+import io.zeebe.distributedlog.restore.impl.ControllableSnapshotRestoreContext;
+import io.zeebe.distributedlog.restore.snapshot.impl.DefaultSnapshotRestoreResponse;
+import io.zeebe.logstreams.state.SnapshotChunk;
+import io.zeebe.util.collection.Tuple;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestoreSnapshotReplicatorTest {
+  private ControllableRestoreClient client = new ControllableRestoreClient();
+  private ControllableSnapshotRestoreContext restoreContext =
+      new ControllableSnapshotRestoreContext();
+  private RecordingSnapshotConsumer snapshotConsumer = new RecordingSnapshotConsumer();
+  private RestoreSnapshotReplicator snapshotReplicator =
+      new RestoreSnapshotReplicator(
+          client, restoreContext, snapshotConsumer, null, 1, Runnable::run);
+  private MemberId server = MemberId.anonymous();
+  private final ControllableSnapshotChunk responseChunk = new ControllableSnapshotChunk();
+
+  @Before
+  public void setup() {
+    client.reset();
+    restoreContext.reset();
+    snapshotConsumer.reset();
+  }
+
+  @Test
+  public void shouldReplicateSnapshot() {
+
+    // given
+    final long snapshotId = 10;
+    final int numChunks = 5;
+
+    restoreContext.setExporterPositionSupplier(() -> 5L);
+    restoreContext.setProcessorPositionSupplier(() -> 10L);
+
+    for (int i = 0; i < numChunks; i++) {
+      client.completeRequestSnapshotChunk(
+          i, new DefaultSnapshotRestoreResponse(true, responseChunk.withChunk(snapshotId, i)));
+    }
+
+    // when
+    final CompletableFuture<Tuple<Long, Long>> replicate =
+        snapshotReplicator.replicate(server, snapshotId, numChunks);
+
+    final Tuple<Long, Long> longLongTuple = replicate.join();
+
+    // then
+    assertThat(snapshotConsumer.getConsumedChunks().size()).isEqualTo(numChunks);
+    assertThat(snapshotConsumer.isSnapshotValid(snapshotId)).isTrue();
+    assertThat(longLongTuple.getLeft()).isEqualTo(5L);
+    assertThat(longLongTuple.getRight()).isEqualTo(10L);
+  }
+
+  @Test
+  public void shouldCompleteExceptionallyWhenInvalidResponse() {
+    // given
+    final long snapshotId = 10;
+    final int numChunks = 5;
+
+    restoreContext.setExporterPositionSupplier(() -> 5L);
+    restoreContext.setProcessorPositionSupplier(() -> 10L);
+
+    client.completeRequestSnapshotChunk(0, new DefaultSnapshotRestoreResponse(false, null));
+
+    // when
+    final CompletableFuture<Tuple<Long, Long>> replicate =
+        snapshotReplicator.replicate(server, snapshotId, numChunks);
+
+    // then
+    assertThat(replicate).isCompletedExceptionally();
+  }
+
+  @Test
+  public void shouldClearTmpSnapshotsIfReplicationFails() {
+    // given
+    final long snapshotId = 10;
+    final int numChunks = 5;
+
+    restoreContext.setExporterPositionSupplier(() -> 5L);
+    restoreContext.setProcessorPositionSupplier(() -> 10L);
+
+    final CompletableFuture<Tuple<Long, Long>> replicate =
+        snapshotReplicator.replicate(server, snapshotId, numChunks);
+
+    client.completeRequestSnapshotChunk(
+        0, new DefaultSnapshotRestoreResponse(true, responseChunk.withChunk(snapshotId, 0)));
+    client.completeRequestSnapshotChunk(
+        1, new DefaultSnapshotRestoreResponse(true, responseChunk.withChunk(snapshotId, 1)));
+
+    assertThat(snapshotConsumer.getConsumedChunks().size()).isEqualTo(2);
+
+    // when
+    client.completeRequestSnapshotChunk(2, new RuntimeException());
+
+    // then
+    waitUntil(() -> replicate.isDone());
+    assertThat(replicate).isCompletedExceptionally();
+    assertThat(snapshotConsumer.getConsumedChunks().size()).isEqualTo(0);
+  }
+
+  private static final class ControllableSnapshotChunk implements SnapshotChunk {
+
+    private long snapshotId;
+    private final int totalCount = 1;
+    private String name;
+    private final long checksum = 1;
+    private final byte[] content = new byte[0];
+
+    public ControllableSnapshotChunk withChunk(long snapshotId, int chunkIdx) {
+      this.snapshotId = snapshotId;
+      this.name = String.valueOf(chunkIdx);
+      return this;
+    }
+
+    @Override
+    public long getSnapshotPosition() {
+      return snapshotId;
+    }
+
+    @Override
+    public int getTotalCount() {
+      return totalCount;
+    }
+
+    @Override
+    public String getChunkName() {
+      return name;
+    }
+
+    @Override
+    public long getChecksum() {
+      return checksum;
+    }
+
+    @Override
+    public byte[] getContent() {
+      return content;
+    }
+  }
+}

camunda/camunda

logstreams/src/main/java/io/zeebe/distributedlog/restore/snapshot/SnapshotRestoreStrategy.java
@@ -13,65 +13,57 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.zeebe.distributedlog.restore.impl;
+package io.zeebe.distributedlog.restore.snapshot;
 
 import io.atomix.cluster.MemberId;
-import io.zeebe.distributedlog.restore.RestoreClient;
 import io.zeebe.distributedlog.restore.RestoreStrategy;
 import io.zeebe.distributedlog.restore.log.LogReplicator;
-import io.zeebe.logstreams.state.SnapshotRequester;
 import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 
 public class SnapshotRestoreStrategy implements RestoreStrategy {
 
-  private final SnapshotRequester snapshotReplicator;
   private final MemberId server;
-  private final RestoreClient client;
   private final LogReplicator logReplicator;
-  private final Logger logger;
+  private final Logger log;
   private final long backupPosition;
+  private final SnapshotRestoreInfo snapshotRestoreInfo;
   private final long latestLocalPosition;
+  private RestoreSnapshotReplicator replicator;
 
   public SnapshotRestoreStrategy(
-      RestoreClient client,
       LogReplicator logReplicator,
-      SnapshotRequester snapshotReplicator,
+      RestoreSnapshotReplicator replicator,
+      SnapshotRestoreInfo snapshotRestoreInfo,
       long latestLocalPosition,
       long backupPosition,
       MemberId server,
-      Logger logger) {
-    this.client = client;
+      Logger log) {
     this.logReplicator = logReplicator;
-    this.snapshotReplicator = snapshotReplicator;
+    this.replicator = replicator;
+    this.snapshotRestoreInfo = snapshotRestoreInfo;
     this.latestLocalPosition = latestLocalPosition;
     this.backupPosition = backupPosition;
     this.server = server;
-    this.logger = logger;
+    this.log = log;
   }
 
   @Override
   public CompletableFuture<Long> executeRestoreStrategy() {
-    logger.debug("Restoring from snapshot");
-    final CompletableFuture<Long> replicated = CompletableFuture.completedFuture(null);
-
-    return replicated
-        .thenCompose(nothing -> client.requestSnapshotInfo(server))
-        .thenCompose(
-            numSnapshots -> snapshotReplicator.getLatestSnapshotsFrom(server, numSnapshots > 1))
-        .thenCompose(nothing -> onSnapshotsReplicated());
+    return replicator
+        .restore(server, snapshotRestoreInfo.getSnapshotId(), snapshotRestoreInfo.getNumChunks())
+        .thenCompose(tuple -> onSnapshotsReplicated(tuple.getLeft(), tuple.getRight()));
   }
 
-  private CompletableFuture<Long> onSnapshotsReplicated() {
-    final long exporterPosition = snapshotReplicator.getExporterPosition();
-    final long processedPosition = snapshotReplicator.getProcessedPosition();
+  private CompletableFuture<Long> onSnapshotsReplicated(
+      long exporterPosition, long processedPosition) {
     final long fromPosition =
         Math.max(
             latestLocalPosition, // if exporter position is behind latestLocalPosition
             getFirstEventToBeReplicated(exporterPosition, processedPosition));
     final long toPosition = Math.max(processedPosition, backupPosition);
     // TODO: logstream.deleteAll(). https://github.com/zeebe-io/zeebe/issues/2509
-    logger.debug("Restored snapshot. Restoring events from {} to {}", fromPosition, toPosition);
+    log.debug("Restored snapshot. Restoring events from {} to {}", fromPosition, toPosition);
     return logReplicator.replicate(
         server, fromPosition, toPosition, fromPosition > latestLocalPosition);
   }

logstreams/src/test/java/io/zeebe/distributedlog/restore/snapshot/RestoreSnapshotReplicatorTest.java
@@ -1,0 +1,163 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.zeebe.distributedlog.restore.snapshot;
+
+import static io.zeebe.test.util.TestUtil.waitUntil;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.atomix.cluster.MemberId;
+import io.zeebe.distributedlog.restore.impl.ControllableRestoreClient;
+import io.zeebe.distributedlog.restore.impl.ControllableSnapshotRestoreContext;
+import io.zeebe.distributedlog.restore.snapshot.impl.InvalidSnapshotRestoreResponse;
+import io.zeebe.distributedlog.restore.snapshot.impl.SuccessSnapshotRestoreResponse;
+import io.zeebe.logstreams.state.SnapshotChunk;
+import io.zeebe.util.collection.Tuple;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RestoreSnapshotReplicatorTest {
+  private ControllableRestoreClient client = new ControllableRestoreClient();
+  private ControllableSnapshotRestoreContext restoreContext =
+      new ControllableSnapshotRestoreContext();
+  private RecordingSnapshotConsumer snapshotConsumer = new RecordingSnapshotConsumer();
+  private RestoreSnapshotReplicator snapshotReplicator =
+      new RestoreSnapshotReplicator(client, restoreContext, snapshotConsumer, 1, Runnable::run);
+  private MemberId server = MemberId.anonymous();
+  private final ControllableSnapshotChunk responseChunk = new ControllableSnapshotChunk();
+
+  @Before
+  public void setup() {
+    client.reset();
+    restoreContext.reset();
+    snapshotConsumer.reset();
+  }
+
+  @Test
+  public void shouldReplicateSnapshot() {
+
+    // given
+    final long snapshotId = 10;
+    final int numChunks = 5;
+
+    restoreContext.setExporterPositionSupplier(() -> 5L);
+    restoreContext.setProcessorPositionSupplier(() -> 10L);
+
+    for (int i = 0; i < numChunks; i++) {
+      client.completeRequestSnapshotChunk(
+          i, new SuccessSnapshotRestoreResponse(responseChunk.withChunk(snapshotId, i)));
+    }
+
+    // when
+    final CompletableFuture<Tuple<Long, Long>> replicate =
+        snapshotReplicator.restore(server, snapshotId, numChunks);
+
+    final Tuple<Long, Long> longLongTuple = replicate.join();
+
+    // then
+    assertThat(snapshotConsumer.getConsumedChunks().size()).isEqualTo(numChunks);
+    assertThat(snapshotConsumer.isSnapshotValid(snapshotId)).isTrue();
+    assertThat(longLongTuple.getLeft()).isEqualTo(5L);
+    assertThat(longLongTuple.getRight()).isEqualTo(10L);
+  }
+
+  @Test
+  public void shouldCompleteExceptionallyWhenInvalidResponse() {
+    // given
+    final long snapshotId = 10;
+    final int numChunks = 5;
+
+    restoreContext.setExporterPositionSupplier(() -> 5L);
+    restoreContext.setProcessorPositionSupplier(() -> 10L);
+
+    client.completeRequestSnapshotChunk(0, new InvalidSnapshotRestoreResponse());
+
+    // when
+    final CompletableFuture<Tuple<Long, Long>> replicate =
+        snapshotReplicator.restore(server, snapshotId, numChunks);
+
+    // then
+    assertThat(replicate).isCompletedExceptionally();
+  }
+
+  @Test
+  public void shouldClearTmpSnapshotsIfReplicationFails() {
+    // given
+    final long snapshotId = 10;
+    final int numChunks = 5;
+
+    restoreContext.setExporterPositionSupplier(() -> 5L);
+    restoreContext.setProcessorPositionSupplier(() -> 10L);
+
+    final CompletableFuture<Tuple<Long, Long>> replicate =
+        snapshotReplicator.restore(server, snapshotId, numChunks);
+
+    client.completeRequestSnapshotChunk(
+        0, new SuccessSnapshotRestoreResponse(responseChunk.withChunk(snapshotId, 0)));
+    client.completeRequestSnapshotChunk(
+        1, new SuccessSnapshotRestoreResponse(responseChunk.withChunk(snapshotId, 1)));
+
+    assertThat(snapshotConsumer.getConsumedChunks().size()).isEqualTo(2);
+
+    // when
+    client.completeRequestSnapshotChunk(2, new RuntimeException());
+
+    // then
+    waitUntil(() -> replicate.isDone());
+    assertThat(replicate).isCompletedExceptionally();
+    assertThat(snapshotConsumer.getConsumedChunks().size()).isEqualTo(0);
+  }
+
+  private static final class ControllableSnapshotChunk implements SnapshotChunk {
+
+    private long snapshotId;
+    private final int totalCount = 1;
+    private String name;
+    private final long checksum = 1;
+    private final byte[] content = new byte[0];
+
+    public ControllableSnapshotChunk withChunk(long snapshotId, int chunkIdx) {
+      this.snapshotId = snapshotId;
+      this.name = String.valueOf(chunkIdx);
+      return this;
+    }
+
+    @Override
+    public long getSnapshotPosition() {
+      return snapshotId;
+    }
+
+    @Override
+    public int getTotalCount() {
+      return totalCount;
+    }
+
+    @Override
+    public String getChunkName() {
+      return name;
+    }
+
+    @Override
+    public long getChecksum() {
+      return checksum;
+    }
+
+    @Override
+    public byte[] getContent() {
+      return content;
+    }
+  }
+}