78fa33f49dab2b7a69d427ab155130745b0ba049
Christopher Zell <zelldon91@googlemail.com>
2019-06-21 09:42:35 +0200

    chore(ci): fix go build output

--- .ci/scripts/distribution/build-go.sh
+++ .ci/scripts/distribution/build-go.sh
@@ -1,4 +1,5 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
+set -o pipefail
 
 export CGO_ENABLED=0
 
@@ -12,9 +13,9 @@ go get -u github.com/jstemmer/go-junit-report
 cd ${ORG_DIR}/zeebe/clients/go
 make install-deps
 
-make test 2>&1 | go-junit-report > TEST-go.xml
+make test | go-junit-report > TEST-go.xml
 
 cd ${ORG_DIR}/zeebe/clients/zbctl
-make test 2>&1 | go-junit-report > TEST-zbctl.xml
+make test | go-junit-report > TEST-zbctl.xml
 
 ./build.sh
