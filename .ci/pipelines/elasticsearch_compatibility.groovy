772c9bda53a3b0e24d2874d688eec3e9db08a631
Sebastian Bathke <sebastian.bathke@camunda.com>
2019-07-02 14:57:30 +0200

    chore(ci): increased elasticsearch pipeline timeout

--- .ci/pipelines/elasticsearch_compatibility.groovy
+++ .ci/pipelines/elasticsearch_compatibility.groovy
@@ -248,7 +248,7 @@ pipeline {
   options {
     buildDiscarder(logRotator(numToKeepStr: '5'))
     timestamps()
-    timeout(time: 30, unit: 'MINUTES')
+    timeout(time: 60, unit: 'MINUTES')
   }
 
   stages {
