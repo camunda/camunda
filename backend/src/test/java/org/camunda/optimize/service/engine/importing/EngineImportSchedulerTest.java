ade38a3fd87bd4a6ea227c2a39c59b1030a94c88
Michael Wagner <michael.wagner@camunda.com>
2019-07-10 13:46:11 +0200

    chore(import-status): adds unit tests to check isImporting logic
    relates to OPT-2369

---
+++ backend/src/test/java/org/camunda/optimize/service/engine/importing/EngineImportSchedulerTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a commercial license.
+ * You may not use this file except in compliance with the commercial license.
+ */
+package org.camunda.optimize.service.engine.importing;
+
+import org.camunda.optimize.service.engine.importing.service.mediator.EngineImportMediator;
+import org.camunda.optimize.service.util.ImportJobExecutor;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EngineImportSchedulerTest {
+
+  @Mock
+  private EngineImportMediator mockedImportMediator;
+
+  @Mock
+  private ImportJobExecutor mockedImportJobExecutor;
+
+
+  private EngineImportScheduler underTest;
+
+  @Before
+  public void before() {
+    underTest = new EngineImportScheduler(
+      Collections.singletonList(mockedImportMediator),
+      "camundabpm"
+    );
+  }
+
+  @Test
+  public void isImportingIsTrueWhenImporting() {
+    // given
+    Mockito.when(mockedImportMediator.canImport()).thenReturn(true);
+
+    // when
+    underTest.scheduleNextRound();
+
+    // then
+    assertThat(underTest.isImporting(), is(true));
+  }
+
+
+  @Test
+  public void isImportingIsTrueWhenActiveImportJob() {
+    // given
+    Mockito.when(mockedImportMediator.canImport()).thenReturn(true);
+    // first round of importing
+    underTest.scheduleNextRound();
+
+    Mockito.when(mockedImportMediator.canImport()).thenReturn(false);
+    Mockito.when(mockedImportMediator.getImportJobExecutor()).thenReturn(mockedImportJobExecutor);
+    Mockito.when(mockedImportJobExecutor.isActive()).thenReturn(true);
+
+    // when
+    underTest.scheduleNextRound();
+
+    // then
+    assertThat(underTest.isImporting(), is(true));
+  }
+
+  @Test
+  public void isImportingIsFalseWhenNoActiveImportJob() {
+    // given
+
+    Mockito.when(mockedImportMediator.canImport()).thenReturn(true);
+    // first round of importing
+    underTest.scheduleNextRound();
+
+    Mockito.when(mockedImportMediator.canImport()).thenReturn(false);
+    Mockito.when(mockedImportMediator.getImportJobExecutor()).thenReturn(mockedImportJobExecutor);
+    Mockito.when(mockedImportJobExecutor.isActive()).thenReturn(false);
+
+    // when
+    underTest.scheduleNextRound();
+
+    // then
+    assertThat(underTest.isImporting(), is(false));
+  }
+
+}
