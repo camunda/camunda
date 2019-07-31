14510a166b618227305caf68c948c85aaf02039a
Sebastian Menski <sebastian.menski@camunda.com>
2019-07-31 15:49:05 +0200

    chore(backend): only enable swagger in dev profile
    related to OPE-600

--- backend/src/main/java/org/camunda/operate/rest/SwaggerConfig.java
+++ backend/src/main/java/org/camunda/operate/rest/SwaggerConfig.java
@@ -8,6 +8,7 @@ package org.camunda.operate.rest;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -18,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**
  * See https://springfox.github.io/springfox/docs/current/ for how to customize swagger output.
  */
+@Profile("dev")
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
