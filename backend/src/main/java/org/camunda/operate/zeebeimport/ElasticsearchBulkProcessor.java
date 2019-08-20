078167595c47254cdef76621709614543ec1e113
Ralf Puchert <ralf.puchert@camunda.com>
2019-08-20 15:02:39 +0200

    fix(backend): remove test_count metric
    Related with OPE-642

--- backend/src/main/java/org/camunda/operate/zeebeimport/ElasticsearchBulkProcessor.java
+++ backend/src/main/java/org/camunda/operate/zeebeimport/ElasticsearchBulkProcessor.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.camunda.operate.Metrics;
 import org.camunda.operate.exceptions.PersistenceException;
 import org.camunda.operate.util.ElasticsearchUtil;
 import org.camunda.operate.zeebeimport.processors.ActivityInstanceZeebeRecordProcessor;
@@ -59,70 +58,59 @@ public class ElasticsearchBulkProcessor {
 
   @Autowired
   private SequenceFlowZeebeRecordProcessor sequenceFlowZeebeRecordProcessor;
-  
-  @Autowired
-  Metrics metrics;
 
   public void persistZeebeRecords(List<Record> zeebeRecords, ImportValueType importValueType) throws PersistenceException {
-    
     logger.debug("Writing [{}] Zeebe records to Elasticsearch", zeebeRecords.size());
 
-      BulkRequest bulkRequest = new BulkRequest();
+    BulkRequest bulkRequest = new BulkRequest();
 
-      switch (importValueType.getValueType()) {
-      case WORKFLOW_INSTANCE:
-        Map<Long, List<RecordImpl<WorkflowInstanceRecordValueImpl>>> groupedWIRecords =
-            zeebeRecords.stream()
-              .map(obj -> (RecordImpl<WorkflowInstanceRecordValueImpl>) obj)
-              .collect(Collectors.groupingBy(obj -> obj.getValue().getWorkflowInstanceKey()));
-        listViewZeebeRecordProcessor.processWorkflowInstanceRecord(groupedWIRecords, bulkRequest);
-        Map<Long, List<RecordImpl<WorkflowInstanceRecordValueImpl>>> groupedWIRecordsPerActivity =
-            zeebeRecords.stream()
-                .map(obj -> (RecordImpl<WorkflowInstanceRecordValueImpl>) obj)
-                .collect(Collectors.groupingBy(obj -> obj.getKey()));
-        activityInstanceZeebeRecordProcessor.processWorkflowInstanceRecord(groupedWIRecordsPerActivity, bulkRequest);
-        eventZeebeRecordProcessor.processWorkflowInstanceRecord(groupedWIRecordsPerActivity, bulkRequest);
-        for (Record record : zeebeRecords) {
-          sequenceFlowZeebeRecordProcessor.processSequenceFlowRecord(record, bulkRequest);
-        }
-        break;
-      case INCIDENT:
-        //old style
-        for (Record record : zeebeRecords) {
-          listViewZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
-          activityInstanceZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
-          incidentZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
-          eventZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
-        }
-        break;
-      case VARIABLE:
-        //old style
-        for (Record record : zeebeRecords) {
-          listViewZeebeRecordProcessor.processVariableRecord(record, bulkRequest);
-          variableZeebeRecordProcessor.processVariableRecord(record, bulkRequest);
-        }
-        break;
-      case DEPLOYMENT:
-        for (Record record : zeebeRecords) {
-          //deployment records can be processed one by one
-          workflowZeebeRecordProcessor.processDeploymentRecord(record, bulkRequest);
-        }
-        break;
-      case JOB:
-        //per activity
-        Map<Long, List<RecordImpl<JobRecordValueImpl>>> groupedJobRecords =
-            zeebeRecords.stream()
-                .map(obj -> (RecordImpl<JobRecordValueImpl>) obj)
-                .collect(Collectors.groupingBy(obj -> obj.getValue().getElementInstanceKey()));
-        eventZeebeRecordProcessor.processJobRecord(groupedJobRecords, bulkRequest);
-        break;
-      default:
-        logger.debug("Default case triggered for type {}",importValueType.getValueType());
-        break;
+    switch (importValueType.getValueType()) {
+    case WORKFLOW_INSTANCE:
+      Map<Long, List<RecordImpl<WorkflowInstanceRecordValueImpl>>> groupedWIRecords = zeebeRecords.stream()
+          .map(obj -> (RecordImpl<WorkflowInstanceRecordValueImpl>) obj).collect(Collectors.groupingBy(obj -> obj.getValue().getWorkflowInstanceKey()));
+      listViewZeebeRecordProcessor.processWorkflowInstanceRecord(groupedWIRecords, bulkRequest);
+      Map<Long, List<RecordImpl<WorkflowInstanceRecordValueImpl>>> groupedWIRecordsPerActivity = zeebeRecords.stream()
+          .map(obj -> (RecordImpl<WorkflowInstanceRecordValueImpl>) obj).collect(Collectors.groupingBy(obj -> obj.getKey()));
+      activityInstanceZeebeRecordProcessor.processWorkflowInstanceRecord(groupedWIRecordsPerActivity, bulkRequest);
+      eventZeebeRecordProcessor.processWorkflowInstanceRecord(groupedWIRecordsPerActivity, bulkRequest);
+      for (Record record : zeebeRecords) {
+        sequenceFlowZeebeRecordProcessor.processSequenceFlowRecord(record, bulkRequest);
       }
+      break;
+    case INCIDENT:
+      // old style
+      for (Record record : zeebeRecords) {
+        listViewZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
+        activityInstanceZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
+        incidentZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
+        eventZeebeRecordProcessor.processIncidentRecord(record, bulkRequest);
+      }
+      break;
+    case VARIABLE:
+      // old style
+      for (Record record : zeebeRecords) {
+        listViewZeebeRecordProcessor.processVariableRecord(record, bulkRequest);
+        variableZeebeRecordProcessor.processVariableRecord(record, bulkRequest);
+      }
+      break;
+    case DEPLOYMENT:
+      for (Record record : zeebeRecords) {
+        // deployment records can be processed one by one
+        workflowZeebeRecordProcessor.processDeploymentRecord(record, bulkRequest);
+      }
+      break;
+    case JOB:
+      // per activity
+      Map<Long, List<RecordImpl<JobRecordValueImpl>>> groupedJobRecords = zeebeRecords.stream().map(obj -> (RecordImpl<JobRecordValueImpl>) obj)
+          .collect(Collectors.groupingBy(obj -> obj.getValue().getElementInstanceKey()));
+      eventZeebeRecordProcessor.processJobRecord(groupedJobRecords, bulkRequest);
+      break;
+    default:
+      logger.debug("Default case triggered for type {}", importValueType.getValueType());
+      break;
+    }
 
     ElasticsearchUtil.processBulkRequest(esClient, bulkRequest, true);
-    metrics.recordCounts(Metrics.OPERATE_NAMESPACE+"test_count", zeebeRecords.size());
   }
 
 }
