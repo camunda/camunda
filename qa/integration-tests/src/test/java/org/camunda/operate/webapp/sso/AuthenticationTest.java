231df8978edbd4c4236a1b7f5fe23627aae546ff
Ralf Puchert <ralf.puchert@camunda.com>
2019-09-25 16:59:59 +0200

    impl(backend): add license header
    Related with OPE-696

--- qa/integration-tests/src/test/java/org/camunda/operate/webapp/sso/AuthenticationTest.java
+++ qa/integration-tests/src/test/java/org/camunda/operate/webapp/sso/AuthenticationTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a commercial license.
+ * You may not use this file except in compliance with the commercial license.
+ */
 package org.camunda.operate.webapp.sso;
 
 import static org.assertj.core.api.Assertions.assertThat;
