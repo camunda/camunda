94f0ee7741c75157bf63ab737be74e5953ca6d92
Sebastian Menski <sebastian.menski@camunda.com>
2019-10-11 13:52:03 +0200

    chore(ci): adjust to new jenkins version
    - remove all the su jenkins calls
    - use batch option to analyse-java.sh script
    - move maven repository into jenkins home folder to share deps

--- .ci/scripts/distribution/build-java.sh
+++ .ci/scripts/distribution/build-java.sh
@@ -3,4 +3,4 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-su jenkins -c "mvn -B -T$LIMITS_CPU -s .ci/settings.xml -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline install -Pspotbugs,prepare-offline"
+mvn -B -T$LIMITS_CPU -s .ci/settings.xml -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check org.apache.maven.plugins:maven-dependency-plugin:3.1.1:go-offline install -Pspotbugs,prepare-offline
