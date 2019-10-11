d393fc669ab76c00c0ab808003f2fb05acf7572d
Sebastian Menski <sebastian.menski@camunda.com>
2019-10-11 13:52:03 +0200

    chore(ci): adjust to new jenkins version
    - remove all the su jenkins calls
    - use batch option to analyse-java.sh script
    - move maven repository into jenkins home folder to share deps

--- .ci/scripts/distribution/test-java8.sh
+++ .ci/scripts/distribution/test-java8.sh
@@ -3,6 +3,6 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-su jenkins -c "mvn -v"
+mvn -v
 
-su jenkins -c "mvn -o -B -T$LIMITS_CPU -s .ci/settings.xml verify -pl clients/java -DtestMavenId=3"
+mvn -o -B -T$LIMITS_CPU -s .ci/settings.xml verify -pl clients/java -DtestMavenId=3
