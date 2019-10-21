6da30236a4c64f06b45104c96b126829bf601ec1
Sebastian Bathke <sebastian.bathke@camunda.com>
2019-10-21 11:24:25 +0200

    fix(dataGeneration): make reading sample users compatible when in jar
    relates to #OPT-2755

--- qa/data-generation/src/main/java/org/camunda/optimize/data/generation/UserGenerator.java
+++ qa/data-generation/src/main/java/org/camunda/optimize/data/generation/UserGenerator.java
@@ -7,13 +7,12 @@ package org.camunda.optimize.data.generation;
 
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.camunda.bpm.engine.rest.dto.identity.UserCredentialsDto;
 import org.camunda.bpm.engine.rest.dto.identity.UserDto;
 import org.camunda.bpm.engine.rest.dto.identity.UserProfileDto;
 import org.camunda.optimize.data.generation.generators.client.SimpleEngineClient;
 
-import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -23,8 +22,8 @@ public class UserGenerator {
 
   @SneakyThrows
   public void generateUsers() {
-    final List<String> lines = FileUtils.readLines(
-      new File(DataGenerationExecutor.class.getResource("/users.csv").toURI()), StandardCharsets.UTF_8
+    final List<String> lines = IOUtils.readLines(
+      getClass().getResourceAsStream("/users.csv"), StandardCharsets.UTF_8
     );
     lines.stream().parallel()
       .forEach(rawUserLine -> {
