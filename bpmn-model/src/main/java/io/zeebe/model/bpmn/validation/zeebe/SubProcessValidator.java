spulci/zeebe

feat(broker): support event subprocess with non-interrupt timer

bpmn-model/src/main/java/io/zeebe/model/bpmn/validation/zeebe/SubProcessValidator.java
@@ -15,13 +15,20 @@
  */
 package io.zeebe.model.bpmn.validation.zeebe;
 
+import io.zeebe.model.bpmn.instance.EventDefinition;
+import io.zeebe.model.bpmn.instance.MessageEventDefinition;
 import io.zeebe.model.bpmn.instance.StartEvent;
 import io.zeebe.model.bpmn.instance.SubProcess;
+import io.zeebe.model.bpmn.instance.TimerEventDefinition;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import org.camunda.bpm.model.xml.validation.ModelElementValidator;
 import org.camunda.bpm.model.xml.validation.ValidationResultCollector;
 
 public class SubProcessValidator implements ModelElementValidator<SubProcess> {
+  private static final List<Class> SUPPORTED_START_TYPES =
+      Arrays.asList(TimerEventDefinition.class, MessageEventDefinition.class);
 
   @Override
   public Class<SubProcess> getElementType() {
@@ -36,6 +43,15 @@ public class SubProcessValidator implements ModelElementValidator<SubProcess> {
       validationResultCollector.addError(0, "Must have exactly one start event");
     }
 
+    if (isEventSubprocess(element)) {
+      validateEventSubprocess(validationResultCollector, startEvents);
+    } else {
+      validateEmbeddedSubprocess(validationResultCollector, startEvents);
+    }
+  }
+
+  private void validateEmbeddedSubprocess(
+      ValidationResultCollector validationResultCollector, Collection<StartEvent> startEvents) {
     startEvents.forEach(
         event -> {
           if (!event.getEventDefinitions().isEmpty()) {
@@ -43,5 +59,30 @@ public class SubProcessValidator implements ModelElementValidator<SubProcess> {
                 0, "Start events in subprocesses must be of type none");
           }
         });
+  }
+
+  private void validateEventSubprocess(
+      ValidationResultCollector validationResultCollector, Collection<StartEvent> startEvents) {
+    startEvents.forEach(
+        e -> {
+          final Collection<EventDefinition> eventDefinitions = e.getEventDefinitions();
+          if (eventDefinitions.isEmpty()) {
+            validationResultCollector.addError(
+                0, "Start events in event subprocesses can't be of type none");
+          }
+
+          eventDefinitions.forEach(
+              def -> {
+                if (SUPPORTED_START_TYPES.stream()
+                    .noneMatch(type -> type.isAssignableFrom(def.getClass()))) {
+                  validationResultCollector.addError(
+                      0, "Start events in event subprocesses must of type message or timer");
+                }
+              });
+        });
+  }
+
+  private static boolean isEventSubprocess(SubProcess subProcess) {
+    return "true".equals(subProcess.getAttributeValue("triggeredByEvent"));
   }
 }

camunda/camunda

bpmn-model/src/main/java/io/zeebe/model/bpmn/validation/zeebe/SubProcessValidator.java
@@ -15,13 +15,20 @@
  */
 package io.zeebe.model.bpmn.validation.zeebe;
 
+import io.zeebe.model.bpmn.instance.EventDefinition;
+import io.zeebe.model.bpmn.instance.MessageEventDefinition;
 import io.zeebe.model.bpmn.instance.StartEvent;
 import io.zeebe.model.bpmn.instance.SubProcess;
+import io.zeebe.model.bpmn.instance.TimerEventDefinition;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import org.camunda.bpm.model.xml.validation.ModelElementValidator;
 import org.camunda.bpm.model.xml.validation.ValidationResultCollector;
 
 public class SubProcessValidator implements ModelElementValidator<SubProcess> {
+  private static final List<Class> SUPPORTED_START_TYPES =
+      Arrays.asList(TimerEventDefinition.class, MessageEventDefinition.class);
 
   @Override
   public Class<SubProcess> getElementType() {
@@ -36,11 +43,38 @@ public class SubProcessValidator implements ModelElementValidator<SubProcess> {
       validationResultCollector.addError(0, "Must have exactly one start event");
     }
 
-    startEvents.forEach(
-        event -> {
-          if (!event.getEventDefinitions().isEmpty()) {
+    if (!startEvents.isEmpty()) {
+      final StartEvent startEvent = startEvents.iterator().next();
+
+      if (element.triggeredByEvent()) {
+        validateEventSubprocess(validationResultCollector, startEvent);
+      } else {
+        validateEmbeddedSubprocess(validationResultCollector, startEvent);
+      }
+    }
+  }
+
+  private void validateEmbeddedSubprocess(
+      ValidationResultCollector validationResultCollector, StartEvent start) {
+    if (!start.getEventDefinitions().isEmpty()) {
+      validationResultCollector.addError(0, "Start events in subprocesses must be of type none");
+    }
+  }
+
+  private void validateEventSubprocess(
+      ValidationResultCollector validationResultCollector, StartEvent start) {
+    final Collection<EventDefinition> eventDefinitions = start.getEventDefinitions();
+    if (eventDefinitions.isEmpty()) {
+      validationResultCollector.addError(
+          0, "Start events in event subprocesses must be of type message or timer");
+    }
+
+    eventDefinitions.forEach(
+        def -> {
+          if (SUPPORTED_START_TYPES.stream()
+              .noneMatch(type -> type.isAssignableFrom(def.getClass()))) {
             validationResultCollector.addError(
-                0, "Start events in subprocesses must be of type none");
+                0, "Start events in event subprocesses must of type message or timer");
           }
         });
   }