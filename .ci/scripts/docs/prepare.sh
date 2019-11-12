3bf47271adeb43e5ec893cf59f70ea7093241486
Sebastian Menski <sebastian.menski@camunda.com>
2019-11-12 12:39:40 +0100

    chore(ci): migrate docs build to debian image
    - binary releases of mdbook and mdbook-linkcheck require glibc and therefore
      are not compatible with alpine out of the box

--- .ci/scripts/docs/prepare.sh
+++ .ci/scripts/docs/prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -eux
 
+apt-get update
+apt-get install -y curl rsync openssh-client
+
 MDBOOK_VERSION="v0.3.4"
 LINKCHECK_VERSION="v0.5.0"
 
-wget -O - https://github.com/rust-lang/mdBook/releases/download/${MDBOOK_VERSION}/mdbook-${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz | tar xzvf - -C /usr/bin mdbook
-wget -O - https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/download/${LINKCHECK_VERSION}/mdbook-linkcheck-${LINKCHECK_VERSION}-x86_64-unknown-linux-gnu.tar.gz | tar xzvf - -C /usr/bin mdbook-linkcheck
-
-apk add --no-cache rsync openssh
+curl -sL https://github.com/rust-lang/mdBook/releases/download/${MDBOOK_VERSION}/mdbook-${MDBOOK_VERSION}-x86_64-unknown-linux-gnu.tar.gz | tar xzvf - -C /usr/bin mdbook
+curl -sL https://github.com/Michael-F-Bryan/mdbook-linkcheck/releases/download/${LINKCHECK_VERSION}/mdbook-linkcheck-${LINKCHECK_VERSION}-x86_64-unknown-linux-gnu.tar.gz | tar xzvf - -C /usr/bin mdbook-linkcheck
