005d30c07d35d6f0d6af6e1f853a90dca6e95b52
Sebastian Bathke <sebastian.bathke@camunda.com>
2019-11-13 13:12:13 +0100

    chore(ci): no modification of post-migration-test during release
    relates to #OPT-2921

--- .ci/pipelines/release_optimize.groovy
+++ .ci/pipelines/release_optimize.groovy
@@ -116,15 +116,6 @@ void runRelease(params) {
       sed -i "s/project.previousVersion>.*</project.previousVersion>${params.RELEASE_VERSION}</g" pom.xml
       git add pom.xml
 
-      cd qa/upgrade-optimize-data/post-migration-test/
-
-      sed -i "s/,\\\${project.version}<\\/upgrade.versions>/<\\/upgrade.versions>/g" pom.xml
-      sed -i "s/<\\/upgrade.versions>/,${params.RELEASE_VERSION}<\\/upgrade.versions>/g" pom.xml
-      sed -i "s/<\\/upgrade.versions>/,\\\${project.version}<\\/upgrade.versions>/g" pom.xml
-      git add pom.xml
-
-      cd ../../../
-
       git diff --staged --quiet
       if [ \$? -ne 0 ]; then
         git commit -m \"chore(release): update previousVersion to new release version ${params.RELEASE_VERSION}\"
