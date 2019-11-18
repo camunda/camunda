f9dd73e08897fd53324fa467ed7818d28aff6ec8
SvetlanaDorokhova <sveta_d@list.ru>
2019-11-18 13:06:20 +0100

    chore(test): ignore index or template does not exist
    Related with OPE-691

--- qa/integration-tests/src/test/java/org/camunda/operate/util/TestUtil.java
+++ qa/integration-tests/src/test/java/org/camunda/operate/util/TestUtil.java
@@ -26,6 +26,7 @@ import org.camunda.operate.entities.listview.WorkflowInstanceForListViewEntity;
 import org.camunda.operate.entities.listview.WorkflowInstanceState;
 import org.camunda.operate.webapp.rest.dto.listview.ListViewQueryDto;
 import org.camunda.operate.webapp.rest.dto.listview.ListViewRequestDto;
+import org.elasticsearch.ElasticsearchStatusException;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.template.delete.DeleteIndexTemplateRequest;
 import org.elasticsearch.client.RequestOptions;
@@ -291,7 +292,7 @@ public abstract class TestUtil {
       logger.info("Removing indices");
       esClient.indices().delete(new DeleteIndexRequest(prefix + "*"), RequestOptions.DEFAULT);
       esClient.indices().deleteTemplate(new DeleteIndexTemplateRequest(prefix + "*"), RequestOptions.DEFAULT);
-    } catch (IOException ex) {
+    } catch (ElasticsearchStatusException | IOException ex) {
       //do nothing
     }
   }
