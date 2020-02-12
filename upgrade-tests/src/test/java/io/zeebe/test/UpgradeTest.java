b48fc7c07a56913ce9dbc3ddf51f765aefd56a63
Miguel Pires <miguel.pires@camunda.com>
2020-02-12 11:02:24 +0100

    chore(upgrade-tests): test job upgradability
    * refactor upgradability test suite
    * introduce ContainerStateRule to manage docker state
    * move upgrade-tests to QA stage
    * add previous version to VersionUtil

--- upgrade-tests/src/test/java/io/zeebe/test/UpgradeTest.java
+++ upgrade-tests/src/test/java/io/zeebe/test/UpgradeTest.java
@@ -7,193 +7,203 @@
  */
 package io.zeebe.test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.response.ActivateJobsResponse;
-import io.zeebe.containers.ZeebeBrokerContainer;
-import io.zeebe.containers.ZeebePort;
-import io.zeebe.containers.ZeebeStandaloneGatewayContainer;
 import io.zeebe.model.bpmn.Bpmn;
 import io.zeebe.model.bpmn.BpmnModelInstance;
 import io.zeebe.test.util.TestUtil;
+import io.zeebe.util.VersionUtil;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import org.junit.BeforeClass;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import org.agrona.IoUtil;
+import org.assertj.core.util.Files;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
 import org.junit.rules.TemporaryFolder;
-import org.junit.rules.TestWatcher;
 import org.junit.rules.Timeout;
-import org.junit.runner.Description;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.event.Level;
-import org.testcontainers.containers.Network;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class UpgradeTest {
 
-  private static final Logger LOG = LoggerFactory.getLogger(UpgradeTest.class);
   private static final String CURRENT_VERSION = "current-test";
   private static final String PROCESS_ID = "process";
   private static final String TASK = "task";
-  private static final BpmnModelInstance WORKFLOW =
-      Bpmn.createExecutableProcess(PROCESS_ID)
-          .startEvent()
-          .serviceTask(TASK, t -> t.zeebeTaskType(TASK))
-          .endEvent()
-          .done();
-  private static String lastVersion = "0.22.1";
+  private static final File SHARED_DATA;
+  private static String lastVersion = VersionUtil.getPreviousVersion();
 
-  @Rule public Timeout timeout = new Timeout(2, TimeUnit.MINUTES);
-  @Rule public TemporaryFolder temp = new TemporaryFolder();
-  @Rule public TestWatcher watchman = new ContainerTestWatcher();
+  static {
+    final var sharedDataPath =
+        Optional.ofNullable(System.getenv("ZEEBE_CI_SHARED_DATA"))
+            .map(Paths::get)
+            .orElse(Paths.get(System.getProperty("tmpdir", "/tmp"), "shared"));
+    SHARED_DATA = sharedDataPath.toAbsolutePath().toFile();
+    IoUtil.ensureDirectoryExists(SHARED_DATA, "temporary folder for Docker");
+  }
 
-  private ZeebeBrokerContainer container;
-  private ZeebeStandaloneGatewayContainer gateway;
-  private ZeebeClient client;
-  private Network network;
+  @Rule public TemporaryFolder tmpFolder = new TemporaryFolder(SHARED_DATA);
+  @Rule public ContainerStateRule state = new ContainerStateRule();
 
-  @BeforeClass
-  public static void beforeClass() {
-    final String version = System.getProperty("lastVersion");
-    if (version != null) {
-      lastVersion = version;
-    } else {
-      LOG.info(
-          "Expected last version property to be set but none was found. Running test with default version {}",
-          lastVersion);
-    }
+  @Rule
+  public RuleChain chain =
+      RuleChain.outerRule(new Timeout(5, TimeUnit.MINUTES)).around(tmpFolder).around(state);
+
+  @Parameter public String name;
+
+  @Parameter(1)
+  public TestCase testCase;
+
+  @Parameters(name = "{0}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][] {
+          {
+            "job",
+            scenario()
+                .createInstance(jobWorkflow())
+                .beforeUpgrade(activateJob())
+                .afterUpgrade(completeJob())
+          },
+        });
   }
 
   @Test
-  public void shouldCompleteJobAfterUpgrade() {
+  public void oldGatewayWithNewBroker() {
     // given
-    startZeebe(lastVersion);
+    state
+        .broker(CURRENT_VERSION, tmpFolder.getRoot().getPath())
+        .withStandaloneGateway(lastVersion)
+        .start();
+    testCase.createInstance().accept(state.client());
 
     // when
-    client.newDeployCommand().addWorkflowModel(WORKFLOW, PROCESS_ID + ".bpmn").send().join();
-    client.newCreateInstanceCommand().bpmnProcessId(PROCESS_ID).latestVersion().send().join();
-
-    final ActivateJobsResponse jobsResponse =
-        client.newActivateJobsCommand().jobType(TASK).maxJobsToActivate(1).send().join();
-
-    TestUtil.waitUntil(() -> findElementInState(TASK, "ACTIVATED"));
-    close();
-
-    startZeebe(CURRENT_VERSION);
-    client.newCompleteCommand(jobsResponse.getJobs().get(0).getKey()).send().join();
+    final long key = testCase.before().apply(state);
 
     // then
-    TestUtil.waitUntil(() -> findElementInState(PROCESS_ID, "ELEMENT_COMPLETED"));
+    testCase.after().accept(state, key);
+    TestUtil.waitUntil(() -> state.hasElementInState(PROCESS_ID, "ELEMENT_COMPLETED"));
   }
 
   @Test
-  public void shouldSupportOlderVersionedGateway() {
+  public void upgradeWithSnapshot() {
+    upgradeZeebe(false);
+  }
+
+  @Test
+  public void upgradeWithoutSnapshot() {
+    upgradeZeebe(true);
+  }
+
+  private void upgradeZeebe(final boolean deleteSnapshot) {
     // given
-    startZeebe(false, CURRENT_VERSION, lastVersion);
+    state.broker(lastVersion, tmpFolder.getRoot().getPath()).start();
+    testCase.createInstance().accept(state.client());
+    final Long key = testCase.before().apply(state);
 
     // when
-    client.newDeployCommand().addWorkflowModel(WORKFLOW, PROCESS_ID + ".bpmn").send().join();
-    client.newCreateInstanceCommand().bpmnProcessId(PROCESS_ID).latestVersion().send().join();
+    state.close();
+    final File snapshot = new File(tmpFolder.getRoot(), "raft-partition/partitions/1/snapshots/");
 
-    final ActivateJobsResponse jobsResponse =
-        client.newActivateJobsCommand().jobType(TASK).maxJobsToActivate(1).send().join();
-
-    client.newCompleteCommand(jobsResponse.getJobs().get(0).getKey()).send().join();
+    assertThat(snapshot).exists();
+    if (deleteSnapshot) {
+      Files.delete(snapshot);
+    }
 
     // then
-    TestUtil.waitUntil(() -> findElementInState(PROCESS_ID, "ELEMENT_COMPLETED"));
+    state.broker(CURRENT_VERSION, tmpFolder.getRoot().getPath()).start();
+    testCase.after().accept(state, key);
+
+    TestUtil.waitUntil(() -> state.hasElementInState(PROCESS_ID, "ELEMENT_COMPLETED"));
   }
 
-  private void startZeebe(final String version) {
-    startZeebe(true, version, null);
+  private static BpmnModelInstance jobWorkflow() {
+    return Bpmn.createExecutableProcess(PROCESS_ID)
+        .startEvent()
+        .serviceTask(TASK, t -> t.zeebeTaskType(TASK))
+        .endEvent()
+        .done();
   }
 
-  private void startZeebe(
-      final boolean embeddedGateway, final String brokerVersion, final String gatewayVersion) {
-    network = Network.newNetwork();
+  private static Function<ContainerStateRule, Long> activateJob() {
+    return (ContainerStateRule state) -> {
+      final ActivateJobsResponse jobsResponse =
+          state.client().newActivateJobsCommand().jobType(TASK).maxJobsToActivate(1).send().join();
 
-    container =
-        new ZeebeBrokerContainer(brokerVersion)
-            .withFileSystemBind(temp.getRoot().getPath(), "/usr/local/zeebe/data")
-            .withNetwork(network)
-            .withEmbeddedGateway(embeddedGateway)
-            .withDebug(true)
-            .withLogLevel(Level.DEBUG);
-    container.start();
-
-    String contactPoint = container.getExternalAddress(ZeebePort.GATEWAY);
-
-    if (!embeddedGateway) {
-      gateway =
-          new ZeebeStandaloneGatewayContainer(gatewayVersion)
-              .withContactPoint(container.getContactPoint())
-              .withNetwork(network)
-              .withLogLevel(Level.DEBUG);
-      gateway.start();
-      contactPoint = gateway.getExternalAddress(ZeebePort.GATEWAY);
-    }
-
-    client = ZeebeClient.newClientBuilder().brokerContactPoint(contactPoint).usePlaintext().build();
+      TestUtil.waitUntil(() -> state.hasElementInState(TASK, "ACTIVATED"));
+      return jobsResponse.getJobs().get(0).getKey();
+    };
   }
 
-  private boolean findElementInState(final String element, final String intent) {
-    final String[] lines = container.getLogs().split("\n");
-
-    for (int i = lines.length - 1; i >= 0; --i) {
-      if (lines[i].contains(String.format("\"elementId\":\"%s\"", element))
-          && lines[i].contains(String.format("\"intent\":\"%s\"", intent))) {
-        return true;
-      }
-    }
-
-    return false;
+  private static BiConsumer<ContainerStateRule, Long> completeJob() {
+    return (ContainerStateRule state, Long key) ->
+        state.client().newCompleteCommand(key).send().join();
   }
 
-  private void close() {
-    if (client != null) {
-      client.close();
-      client = null;
-    }
-
-    if (gateway != null) {
-      gateway.close();
-      gateway = null;
-    }
-
-    if (container != null) {
-      container.close();
-      container = null;
-    }
-
-    if (network != null) {
-      network.close();
-      network = null;
-    }
+  private static TestCase scenario() {
+    return new TestCase();
   }
 
-  private class ContainerTestWatcher extends TestWatcher {
+  private static class TestCase {
+    private Consumer<ZeebeClient> createInstance;
+    private Function<ContainerStateRule, Long> before;
+    private BiConsumer<ContainerStateRule, Long> after;
 
-    @Override
-    protected void succeeded(Description description) {
-      close();
+    TestCase createInstance(final BpmnModelInstance model) {
+      this.createInstance =
+          client -> {
+            client.newDeployCommand().addWorkflowModel(model, PROCESS_ID + ".bpmn").send().join();
+            client
+                .newCreateInstanceCommand()
+                .bpmnProcessId(PROCESS_ID)
+                .latestVersion()
+                .send()
+                .join();
+          };
+      return this;
     }
 
-    @Override
-    protected void failed(Throwable e, Description description) {
-      if (container != null && LOG.isErrorEnabled()) {
-        LOG.error(
-            String.format(
-                "===============================================%nBroker logs%n===============================================%n%s",
-                container.getLogs()));
-      }
+    /**
+     * Should make zeebe write records and write to state of the feature being tested (e.g., jobs,
+     * messages). The workflow should be left in a waiting state so Zeebe can be restarted and
+     * execution can be continued after. Takes the container rule as input and outputs a long which
+     * can be used after the upgrade to continue the execution.
+     */
+    TestCase beforeUpgrade(final Function<ContainerStateRule, Long> func) {
+      this.before = func;
+      return this;
+    }
+    /**
+     * Should continue the instance after the upgrade in a way that will complete the workflow.
+     * Takes the container rule and a long (e.g., a key) as input.
+     */
+    TestCase afterUpgrade(final BiConsumer<ContainerStateRule, Long> func) {
+      this.after = func;
+      return this;
+    }
 
-      if (gateway != null && LOG.isErrorEnabled()) {
-        LOG.error(
-            String.format(
-                "===============================================%nGateway logs%n===============================================%n%s",
-                gateway.getLogs()));
-      }
+    public Consumer<ZeebeClient> createInstance() {
+      return createInstance;
+    }
 
-      close();
+    public Function<ContainerStateRule, Long> before() {
+      return before;
+    }
+
+    public BiConsumer<ContainerStateRule, Long> after() {
+      return after;
     }
   }
 }
