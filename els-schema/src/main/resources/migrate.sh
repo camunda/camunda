9e55be0d399f911f39b6409c5f0f0e1bb5086075
Ralf Puchert <ralf.puchert@camunda.com>
2020-02-24 15:16:45 +0100

    impl(backend): add check for existing schema
    - avoid error message for empty pipeline folder (OPE-913)
    - add migration files to docker image
    - add check for no schema and previous schema
    - delete old indices in one step after succeeded migration
    
    closes OPE-875, OPE-913

--- els-schema/src/main/resources/migrate.sh
+++ els-schema/src/main/resources/migrate.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+# change to folder where migration.sh exists
+cd $(dirname $0)	
 # Define migration functions
 ES=${1:-http://localhost:9200}
+echo "Use elasticsearch server $ES"
 # For testing prefix with 'echo ' 
 RESTCLIENT="curl -s -K curl.config"
 
@@ -10,12 +13,26 @@ error_exit(){
    exit 1
 }
 
+success_exit(){
+	msg=$1 || "Success. Exit."
+	echo $msg
+	exit 0
+}
+
 checkElasticsearchConnection(){
-  $RESTCLIENT --request GET --url $ES/_cluster/health &> /dev/null || error_exit "Connection to $ES failed"
+  $RESTCLIENT --request GET --url $ES/_cluster/health &> /dev/null || error_exit "Connection to $ES failed."
+}
+
+checkNoSchemaExists(){
+  $RESTCLIENT --request GET --url $ES/operate-* | grep "operate" &> /dev/null || success_exit "No operate schema exists."
+}
+
+checkIsPreviousVersionInstalled(){
+  $RESTCLIENT --request GET --url $ES/operate-*-${schema.previous_version}* | grep "${schema.previous_version}" &> /dev/null || error_exit "Can't migrate current schema to '${schema.version}'. Only migration from ${schema.previous_version} to ${schema.version} is supported."
 }
 
 checkIsCurrentVersionAlreadyInstalled(){
-  $RESTCLIENT --request GET --url $ES/operate-*-${schema.version}* | grep "${schema.version}" &> /dev/null && error_exit "Schema version '${schema.version}' exists already."
+  $RESTCLIENT --request GET --url $ES/operate-*-${schema.version}* | grep "${schema.version}" &> /dev/null && success_exit "Schema version '${schema.version}' exists already."
 }
 
 createNewTemplatesAndTheirIndexes(){
@@ -45,6 +62,7 @@ createNewIndexes(){
 }
 
 createPipelines(){
+	shopt -s nullglob
 	for pipeline in migrate/pipeline/*.json; do
     	pipelinename=`basename $pipeline .json`
     	echo "Create pipeline $pipelinename"
@@ -53,6 +71,7 @@ createPipelines(){
     	echo
     	echo "-------------------------------"
 	done
+	shopt -u nullglob
 }
 
 removePipelines(){
@@ -64,6 +83,7 @@ removePipelines(){
 }
 
 removeOldTemplates(){
+	shopt -s nullglob
 	echo "Delete all old templates"
 	for template in create/template/*.json; do
 		templatename=`basename $template .json`
@@ -74,6 +94,7 @@ removeOldTemplates(){
 		echo
     	echo "-------------------------------"
 	done
+	shopt -u nullglob
 }
 
 migrate(){
@@ -84,6 +105,14 @@ migrate(){
 		echo "-------------------------------"
     	$RESTCLIENT --request POST --url $ES/_reindex?wait_for_completion=true --data @$index || error_exit "Failed to reindex $indexname"
     	echo
+    	echo "-------------------------------"
+	done
+}
+
+deleteOldIndices(){
+	echo "Delete old indices"
+	for index in migrate/reindex/*.json; do
+		indexname=`basename $index .json`
     	echo "Delete ${indexname}-${schema.previous_version}* "
 		echo "-------------------------------"
     	$RESTCLIENT --request DELETE --url $ES/${indexname}-${schema.previous_version}* || error_exit "Failed to delete indices that match ${indexname}-${schema.previous_version}*"
@@ -93,11 +122,17 @@ migrate(){
 }
 
 ## main
+
+# checks for schema
 checkElasticsearchConnection
-#checkIsCurrentVersionAlreadyInstalled
+checkNoSchemaExists
+checkIsCurrentVersionAlreadyInstalled
+checkIsPreviousVersionInstalled 
+# start migration
 removeOldTemplates
 createNewIndexes
 createNewTemplatesAndTheirIndexes
 createPipelines
 migrate
+deleteOldIndices
 removePipelines
