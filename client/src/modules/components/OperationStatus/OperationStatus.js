6395471f46a150b91107c5399bc8fcb3b7adf4c0
pedesen <patrick.dehn@camunda.com>
2020-03-10 11:04:46 +0100

    chore(Actions): remove failed operation state

--- client/src/modules/components/OperationStatus/OperationStatus.js
+++ client/src/modules/components/OperationStatus/OperationStatus.js
@@ -7,23 +7,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {ACTIVE_OPERATION_STATES, OPERATION_STATE} from 'modules/constants';
-import {OPERATION_TYPE} from 'modules/constants';
-
-import StatusItems from './StatusItems';
+import {ACTIVE_OPERATION_STATES} from 'modules/constants';
 
 import * as Styled from './styled';
-
-const getTitleByOperationType = (instanceId, type) => {
-  switch (type) {
-    case OPERATION_TYPE.RESOLVE_INCIDENT:
-      return `Retrying Instance ${instanceId} failed`;
-    case OPERATION_TYPE.CANCEL_WORKFLOW_INSTANCE:
-      return `Canceling Instance ${instanceId} failed`;
-    default:
-      return '';
-  }
-};
 
 const OperationStatus = ({
   operationState,
@@ -38,19 +24,9 @@ const OperationStatus = ({
       <Styled.OperationSpinner
         selected={selected}
         title={`Instance ${instance.id} has scheduled Operations`}
+        data-test="action-spinner"
         {...props}
       />
-    );
-  }
-
-  if (operationState === OPERATION_STATE.FAILED) {
-    return (
-      <StatusItems {...props}>
-        <StatusItems.Item
-          type={operationType}
-          title={getTitleByOperationType(instance.id, operationType)}
-        />
-      </StatusItems>
     );
   }
 
