3df97811e3bfc9037168122eda2a2dce92193727
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2020-05-13 10:34:13 +0200

    chore(ci): set correct MAVEN_SETTINGS_XML

--- .ci/pipelines/release_zeebe_java8.groovy
+++ .ci/pipelines/release_zeebe_java8.groovy
@@ -109,9 +109,11 @@ spec:
         stage('Maven Release') {
             steps {
                 container('maven') {
-                    sshagent(['camunda-jenkins-github-ssh']) {
-                        sh '.ci/scripts/release/maven-release.sh'
+                  sshagent(['camunda-jenkins-github-ssh']) {
+                    configFileProvider([configFile(fileId: 'maven-nexus-settings-zeebe', variable: 'MAVEN_SETTINGS_XML')]) {
+                      sh '.ci/scripts/release/maven-release.sh'
                     }
+                  }
                 }
             }
         }
