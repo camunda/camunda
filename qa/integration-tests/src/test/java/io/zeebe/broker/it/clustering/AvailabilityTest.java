1e24ea05aa648f25d1eed09b4727e7017a483058
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2020-05-15 13:51:48 +0200

    chore(qa): add test

--- qa/integration-tests/src/test/java/io/zeebe/broker/it/clustering/AvailabilityTest.java
+++ qa/integration-tests/src/test/java/io/zeebe/broker/it/clustering/AvailabilityTest.java
@@ -7,6 +7,7 @@
  */
 package io.zeebe.broker.it.clustering;
 
+import static io.zeebe.test.util.TestUtil.waitUntil;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.zeebe.broker.it.GrpcClientRule;
@@ -15,9 +16,11 @@ import io.zeebe.client.api.response.BrokerInfo;
 import io.zeebe.model.bpmn.Bpmn;
 import io.zeebe.model.bpmn.BpmnModelInstance;
 import io.zeebe.protocol.record.Record;
+import io.zeebe.protocol.record.intent.JobIntent;
 import io.zeebe.protocol.record.intent.WorkflowInstanceCreationIntent;
 import io.zeebe.test.util.record.RecordingExporter;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -133,6 +136,41 @@ public class AvailabilityTest {
               .getJobs();
       jobs.forEach(job -> activatedJobsKey.add(job.getKey()));
     }
+
+    // then
+    assertThat(activatedJobsKey).hasSize(numInstances);
+  }
+
+  @Test
+  public void shouldActivateJobsRoundRobinWhenOnePartitionDown() {
+    // given
+    final int numInstances = 2 * partitionCount;
+    final BrokerInfo leaderForPartition = clusteringRule.getLeaderForPartition(partitionCount);
+    clusteringRule.stopBroker(leaderForPartition.getNodeId(), false);
+
+    final List<Long> workflows = new ArrayList<>();
+    for (int i = 0; i < numInstances; i++) {
+      workflows.add(clientRule.createWorkflowInstance(workflowKey));
+    }
+
+    // when
+    waitUntil(
+        () ->
+            RecordingExporter.jobRecords(JobIntent.CREATED).limit(numInstances).count()
+                == numInstances);
+
+    final Set<Long> activatedJobsKey = new HashSet<>();
+    final List<ActivatedJob> jobs =
+        clientRule
+            .getClient()
+            .newActivateJobsCommand()
+            .jobType(JOBTYPE)
+            .maxJobsToActivate(2 * numInstances)
+            .timeout(Duration.ofMinutes(5))
+            .send()
+            .join()
+            .getJobs();
+    jobs.forEach(job -> activatedJobsKey.add(job.getKey()));
 
     // then
     assertThat(activatedJobsKey).hasSize(numInstances);
