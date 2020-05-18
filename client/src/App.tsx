a9a42c7db61c651d7125ba8c06052a0f69b852a4
Vinícius Goulart <goulart.vs@gmail.com>
2020-05-18 10:56:51 +0200

    chore:(client): add styled-components (#19)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,26 +8,30 @@
 
 import * as React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
 
 import {PrivateRoute} from './PrivateRoute';
 import {Tasklist} from './Tasklist';
 import {Login} from './Login';
 import {Pages} from './pages';
+import {theme} from './theme';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <PrivateRoute
-          exact
-          path={Pages.Initial}
-          component={Tasklist}
-          redirectPath={Pages.Login}
-        />
-        <Route path={Pages.Login} component={Login} />
-        <Route render={() => <h1>Page not found</h1>} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute
+            exact
+            path={Pages.Initial}
+            component={Tasklist}
+            redirectPath={Pages.Login}
+          />
+          <Route path={Pages.Login} component={Login} />
+          <Route render={() => <h1>Page not found</h1>} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
