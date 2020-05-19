a0df3a54e7f92c90054f330e02b9d35fbac78701
Vinícius Goulart <goulart.vs@gmail.com>
2020-05-19 15:11:03 +0200

    feat(client): add colors to theme (#29)

--- client/src/theme.tsx
+++ client/src/theme.tsx
@@ -4,6 +4,15 @@
  * You may not use this file except in compliance with the commercial license.
  */
 
-const theme = {} as const;
+const theme = {
+  colors: {
+    blue: '#4d90ff',
+    green: '#10d070',
+    red: '#ff3d3d',
+    orange: '#ffa533',
+    ui: ['#f2f3f5', '#f7f8fa', '#b0bac7', '#fdfdfe', '#d8dce3', '#62626e'],
+    item: {odd: '#fdfdfe', even: '#f9fafc'},
+  },
+} as const;
 
 export {theme};
