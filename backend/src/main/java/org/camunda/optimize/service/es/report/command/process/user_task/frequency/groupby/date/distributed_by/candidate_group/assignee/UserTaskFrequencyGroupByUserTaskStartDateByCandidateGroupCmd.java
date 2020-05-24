d500e862e513c4ac777f4cf827566c8d9e840dd6
Johannes Heinemann <johannes.heinemann@camunda.com>
2020-05-24 20:26:42 +0200

    feat(report): add user task frequency by date by candidate group (#2092)
    related to #OPT-3684

---
+++ backend/src/main/java/org/camunda/optimize/service/es/report/command/process/user_task/frequency/groupby/date/distributed_by/candidate_group/assignee/UserTaskFrequencyGroupByUserTaskStartDateByCandidateGroupCmd.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a commercial license.
+ * You may not use this file except in compliance with the commercial license.
+ */
+package org.camunda.optimize.service.es.report.command.process.user_task.frequency.groupby.date.distributed_by.candidate_group.assignee;
+
+import org.camunda.optimize.dto.optimize.query.report.ReportEvaluationResult;
+import org.camunda.optimize.dto.optimize.query.report.single.process.SingleProcessReportDefinitionDto;
+import org.camunda.optimize.dto.optimize.query.report.single.result.hyper.ReportHyperMapResultDto;
+import org.camunda.optimize.service.es.report.command.CommandContext;
+import org.camunda.optimize.service.es.report.command.ProcessCmd;
+import org.camunda.optimize.service.es.report.command.exec.ProcessReportCmdExecutionPlan;
+import org.camunda.optimize.service.es.report.command.exec.builder.ReportCmdExecutionPlanBuilder;
+import org.camunda.optimize.service.es.report.command.modules.distributed_by.process.identity.ProcessDistributedByCandidateGroup;
+import org.camunda.optimize.service.es.report.command.modules.group_by.process.date.ProcessGroupByUserTaskStartDate;
+import org.camunda.optimize.service.es.report.command.modules.view.process.frequency.ProcessViewCountUserTaskFrequency;
+import org.camunda.optimize.service.es.report.result.process.SingleProcessHyperMapReportResult;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserTaskFrequencyGroupByUserTaskStartDateByCandidateGroupCmd extends ProcessCmd<ReportHyperMapResultDto> {
+
+  public UserTaskFrequencyGroupByUserTaskStartDateByCandidateGroupCmd(final ReportCmdExecutionPlanBuilder builder) {
+    super(builder);
+  }
+
+  @Override
+  protected ProcessReportCmdExecutionPlan<ReportHyperMapResultDto> buildExecutionPlan(final ReportCmdExecutionPlanBuilder builder) {
+    return builder.createExecutionPlan()
+      .processCommand()
+      .view(ProcessViewCountUserTaskFrequency.class)
+      .groupBy(ProcessGroupByUserTaskStartDate.class)
+      .distributedBy(ProcessDistributedByCandidateGroup.class)
+      .resultAsHyperMap()
+      .build();
+  }
+
+  @Override
+  public ReportEvaluationResult evaluate(final CommandContext<SingleProcessReportDefinitionDto> commandContext) {
+    final ReportHyperMapResultDto evaluate = executionPlan.evaluate(commandContext);
+    return new SingleProcessHyperMapReportResult(evaluate, commandContext.getReportDefinition());
+  }
+}
--- backend/src/test/java/org/camunda/optimize/test/util/TemplatedProcessReportDataBuilder.java
+++ backend/src/test/java/org/camunda/optimize/test/util/TemplatedProcessReportDataBuilder.java
@@ -266,6 +266,17 @@ public class TemplatedProcessReportDataBuilder {
           .dateInterval(dateInterval)
           .build();
         break;
+        case USER_TASK_FREQUENCY_GROUP_BY_USER_TASK_START_DATE_BY_CANDIDATE_GROUP:
+        reportData = new ProcessReportDataBuilderHelper()
+          .viewEntity(ProcessViewEntity.USER_TASK)
+          .viewProperty(ProcessViewProperty.FREQUENCY)
+          .groupByType(ProcessGroupByType.START_DATE)
+          .distributedBy(DistributedBy.CANDIDATE_GROUP)
+          .processDefinitionKey(processDefinitionKey)
+          .processDefinitionVersions(processDefinitionVersions)
+          .dateInterval(dateInterval)
+          .build();
+        break;
       case USER_TASK_FREQUENCY_GROUP_BY_USER_TASK_END_DATE:
         reportData = new ProcessReportDataBuilderHelper()
           .viewEntity(ProcessViewEntity.USER_TASK)
@@ -293,6 +304,17 @@ public class TemplatedProcessReportDataBuilder {
           .viewProperty(ProcessViewProperty.FREQUENCY)
           .groupByType(ProcessGroupByType.END_DATE)
           .distributedBy(DistributedBy.ASSIGNEE)
+          .processDefinitionKey(processDefinitionKey)
+          .processDefinitionVersions(processDefinitionVersions)
+          .dateInterval(dateInterval)
+          .build();
+        break;
+      case USER_TASK_FREQUENCY_GROUP_BY_USER_TASK_END_DATE_BY_CANDIDATE_GROUP:
+        reportData = new ProcessReportDataBuilderHelper()
+          .viewEntity(ProcessViewEntity.USER_TASK)
+          .viewProperty(ProcessViewProperty.FREQUENCY)
+          .groupByType(ProcessGroupByType.END_DATE)
+          .distributedBy(DistributedBy.CANDIDATE_GROUP)
           .processDefinitionKey(processDefinitionKey)
           .processDefinitionVersions(processDefinitionVersions)
           .dateInterval(dateInterval)
