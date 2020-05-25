c7ad579db2e69b534c548159d11f23666fe96ed1
Sebastian Bathke <sebastian.bathke@camunda.com>
2020-05-25 09:55:24 +0200

    chore(ci): increased elasticsearch compatibility timeout
    - AWS execution exceeded the 60 minutes mark recently

--- .ci/pipelines/elasticsearch_compatibility.groovy
+++ .ci/pipelines/elasticsearch_compatibility.groovy
@@ -220,7 +220,7 @@ pipeline {
   options {
     buildDiscarder(logRotator(numToKeepStr: '10'))
     timestamps()
-    timeout(time: 60, unit: 'MINUTES')
+    timeout(time: 90, unit: 'MINUTES')
   }
 
   stages {
