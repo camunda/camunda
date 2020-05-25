lwille/zeebe

chore(broker): rate limit user requests received by CommandApi handler

broker/src/main/java/io/zeebe/broker/transport/TransportServiceNames.java
@@ -7,16 +7,19 @@
  */
 package io.zeebe.broker.transport;
 
-import io.zeebe.broker.transport.commandapi.CommandApiMessageHandler;
+import io.zeebe.broker.transport.backpressure.RequestLimiter;
+import io.zeebe.broker.transport.commandapi.CommandApiRequestResponseService;
 import io.zeebe.servicecontainer.ServiceName;
 import io.zeebe.transport.ServerTransport;
 
 public class TransportServiceNames {
-  public static final ServiceName<CommandApiMessageHandler> COMMAND_API_MESSAGE_HANDLER =
+  public static final ServiceName<CommandApiRequestResponseService> COMMAND_API_MESSAGE_HANDLER =
       ServiceName.newServiceName(
-          "transport.commandApi.messageHandler", CommandApiMessageHandler.class);
+          "transport.commandApi.messageHandler", CommandApiRequestResponseService.class);
 
   public static final String COMMAND_API_SERVER_NAME = "commandApi.server";
+  public static final ServiceName<RequestLimiter> REQUEST_LIMITER_SERVICE =
+      ServiceName.newServiceName("transport.commandApu.requestLimiter", RequestLimiter.class);
 
   public static ServiceName<ServerTransport> serverTransport(String identifier) {
     return ServiceName.newServiceName(

broker/src/test/java/io/zeebe/broker/transport/commandapi/CommandApiMessageHandlerTest.java
@@ -15,6 +15,10 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 
+import com.netflix.concurrency.limits.limit.SettableLimit;
+import io.zeebe.broker.transport.backpressure.CommandRateLimiter;
+import io.zeebe.broker.transport.backpressure.NoneRequestLimiter;
+import io.zeebe.broker.transport.backpressure.RequestLimiter;
 import io.zeebe.distributedlog.DistributedLogstreamService;
 import io.zeebe.distributedlog.impl.DefaultDistributedLogstreamService;
 import io.zeebe.distributedlog.impl.DistributedLogstreamPartition;
@@ -85,6 +89,7 @@ public class CommandApiMessageHandlerTest {
   private LogStream logStream;
   private CommandApiMessageHandler messageHandler;
   private DistributedLogstreamService distributedLogImpl;
+  private RequestLimiter noneLimiter = new NoneRequestLimiter();
 
   @Before
   public void setup() {
@@ -147,7 +152,7 @@ public class CommandApiMessageHandlerTest {
     logStream.openAppender().join();
 
     messageHandler = new CommandApiMessageHandler();
-    messageHandler.addPartition(logStream);
+    messageHandler.addPartition(logStream, noneLimiter);
   }
 
   @After
@@ -298,6 +303,33 @@ public class CommandApiMessageHandlerTest {
     final ErrorResponseDecoder errorDecoder = serverOutput.getAsErrorResponse(0);
 
     assertThat(errorDecoder.errorCode()).isEqualTo(ErrorCode.UNSUPPORTED_MESSAGE);
+  }
+
+  @Test
+  public void shouldSendErrorMessageOnRequestLimitReached() {
+    // given
+    final CommandRateLimiter settableLimiter =
+        CommandRateLimiter.builder().limit(new SettableLimit(1)).build();
+    messageHandler = new CommandApiMessageHandler();
+    messageHandler.addPartition(logStream, settableLimiter);
+    settableLimiter.tryAcquire(0, 1, null);
+
+    // when
+    final int writtenLength =
+        writeCommandRequestToBuffer(
+            buffer, LOG_STREAM_PARTITION_ID, null, ValueType.JOB, JobIntent.CREATE);
+    final boolean isHandled =
+        messageHandler.onRequest(
+            serverOutput, DEFAULT_ADDRESS, buffer, 0, writtenLength, REQUEST_ID);
+    assertThat(isHandled).isTrue();
+
+    // then
+    final List<DirectBuffer> sentResponses = serverOutput.getSentResponses();
+    assertThat(sentResponses).hasSize(1);
+
+    final ErrorResponseDecoder errorDecoder = serverOutput.getAsErrorResponse(0);
+
+    assertThat(errorDecoder.errorCode()).isEqualTo(ErrorCode.RESOURCE_EXHAUSTED);
   }
 
   protected int writeCommandRequestToBuffer(

broker/src/test/java/io/zeebe/broker/transport/commandapi/CommandResponseWriterImplTest.java
@@ -10,12 +10,18 @@ package io.zeebe.broker.transport.commandapi;
 import static io.zeebe.util.StringUtil.getBytes;
 import static io.zeebe.util.VarDataUtil.readBytes;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import io.zeebe.broker.transport.backpressure.NoneRequestLimiter;
+import io.zeebe.broker.transport.backpressure.RequestLimiter;
 import io.zeebe.protocol.record.ExecuteCommandResponseDecoder;
 import io.zeebe.protocol.record.MessageHeaderDecoder;
 import io.zeebe.protocol.record.RecordType;
 import io.zeebe.protocol.record.ValueType;
 import io.zeebe.protocol.record.intent.JobIntent;
+import io.zeebe.transport.ServerOutput;
 import io.zeebe.util.buffer.DirectBufferWriter;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.Before;
@@ -40,7 +46,7 @@ public class CommandResponseWriterImplTest {
   @Test
   public void shouldWriteResponse() {
     // given
-    responseWriter = new CommandResponseWriterImpl(null);
+    responseWriter = new CommandResponseWriterImpl(null, new NoneRequestLimiter());
 
     eventWriter.wrap(new UnsafeBuffer(EVENT), 0, EVENT.length);
 
@@ -80,5 +86,23 @@ public class CommandResponseWriterImplTest {
 
     final byte[] event = readBytes(responseDecoder::getValue, responseDecoder::valueLength);
     assertThat(event).isEqualTo(EVENT);
+  }
+
+  @Test
+  public void shouldInvokeLimiterOnResponse() {
+    // given
+    final int remoteStreamId = 2;
+    final long requestId = 100;
+    final int partitionId = 5;
+    final RequestLimiter mockLimiter = mock(RequestLimiter.class);
+    responseWriter = new CommandResponseWriterImpl(mock(ServerOutput.class), mockLimiter);
+    eventWriter.wrap(new UnsafeBuffer(EVENT), 0, EVENT.length);
+
+    // when
+    responseWriter.partitionId(partitionId).valueWriter(eventWriter);
+    responseWriter.tryWriteResponse(remoteStreamId, requestId);
+
+    // then
+    verify(mockLimiter, times(1)).onResponse(remoteStreamId, requestId);
   }
 }

camunda/camunda

broker-core/src/main/java/io/zeebe/broker/transport/TransportServiceNames.java
@@ -7,14 +7,13 @@
  */
 package io.zeebe.broker.transport;
 
-import io.zeebe.broker.transport.commandapi.CommandApiMessageHandler;
+import io.zeebe.broker.transport.commandapi.CommandApiService;
 import io.zeebe.servicecontainer.ServiceName;
 import io.zeebe.transport.ServerTransport;
 
 public class TransportServiceNames {
-  public static final ServiceName<CommandApiMessageHandler> COMMAND_API_MESSAGE_HANDLER =
-      ServiceName.newServiceName(
-          "transport.commandApi.messageHandler", CommandApiMessageHandler.class);
+  public static final ServiceName<CommandApiService> COMMAND_API_SERVICE_NAME =
+      ServiceName.newServiceName("transport.commandApi.messageHandler", CommandApiService.class);
 
   public static final String COMMAND_API_SERVER_NAME = "commandApi.server";
 

broker-core/src/test/java/io/zeebe/broker/transport/commandapi/CommandApiMessageHandlerTest.java
@@ -15,6 +15,10 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 
+import com.netflix.concurrency.limits.limit.SettableLimit;
+import io.zeebe.broker.transport.backpressure.CommandRateLimiter;
+import io.zeebe.broker.transport.backpressure.NoopRequestLimiter;
+import io.zeebe.broker.transport.backpressure.RequestLimiter;
 import io.zeebe.distributedlog.DistributedLogstreamService;
 import io.zeebe.distributedlog.impl.DefaultDistributedLogstreamService;
 import io.zeebe.distributedlog.impl.DistributedLogstreamPartition;
@@ -85,6 +89,7 @@ public class CommandApiMessageHandlerTest {
   private LogStream logStream;
   private CommandApiMessageHandler messageHandler;
   private DistributedLogstreamService distributedLogImpl;
+  private RequestLimiter noneLimiter = new NoopRequestLimiter();
 
   @Before
   public void setup() {
@@ -152,7 +157,7 @@ public class CommandApiMessageHandlerTest {
     logStream.openAppender().join();
 
     messageHandler = new CommandApiMessageHandler();
-    messageHandler.addPartition(logStream);
+    messageHandler.addPartition(logStream, noneLimiter);
   }
 
   @After
@@ -303,6 +308,33 @@ public class CommandApiMessageHandlerTest {
     final ErrorResponseDecoder errorDecoder = serverOutput.getAsErrorResponse(0);
 
     assertThat(errorDecoder.errorCode()).isEqualTo(ErrorCode.UNSUPPORTED_MESSAGE);
+  }
+
+  @Test
+  public void shouldSendErrorMessageOnRequestLimitReached() {
+    // given
+    final CommandRateLimiter settableLimiter =
+        CommandRateLimiter.builder().limit(new SettableLimit(1)).build();
+    messageHandler = new CommandApiMessageHandler();
+    messageHandler.addPartition(logStream, settableLimiter);
+    settableLimiter.tryAcquire(0, 1, null);
+
+    // when
+    final int writtenLength =
+        writeCommandRequestToBuffer(
+            buffer, LOG_STREAM_PARTITION_ID, null, ValueType.JOB, JobIntent.CREATE);
+    final boolean isHandled =
+        messageHandler.onRequest(
+            serverOutput, DEFAULT_ADDRESS, buffer, 0, writtenLength, REQUEST_ID);
+    assertThat(isHandled).isTrue();
+
+    // then
+    final List<DirectBuffer> sentResponses = serverOutput.getSentResponses();
+    assertThat(sentResponses).hasSize(1);
+
+    final ErrorResponseDecoder errorDecoder = serverOutput.getAsErrorResponse(0);
+
+    assertThat(errorDecoder.errorCode()).isEqualTo(ErrorCode.RESOURCE_EXHAUSTED);
   }
 
   protected int writeCommandRequestToBuffer(

broker-core/src/test/java/io/zeebe/broker/transport/commandapi/CommandResponseWriterImplTest.java
@@ -10,12 +10,18 @@ package io.zeebe.broker.transport.commandapi;
 import static io.zeebe.util.StringUtil.getBytes;
 import static io.zeebe.util.VarDataUtil.readBytes;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import io.zeebe.broker.transport.backpressure.NoopRequestLimiter;
+import io.zeebe.broker.transport.backpressure.RequestLimiter;
 import io.zeebe.protocol.record.ExecuteCommandResponseDecoder;
 import io.zeebe.protocol.record.MessageHeaderDecoder;
 import io.zeebe.protocol.record.RecordType;
 import io.zeebe.protocol.record.ValueType;
 import io.zeebe.protocol.record.intent.JobIntent;
+import io.zeebe.transport.ServerOutput;
 import io.zeebe.util.buffer.DirectBufferWriter;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.junit.Before;
@@ -40,7 +46,7 @@ public class CommandResponseWriterImplTest {
   @Test
   public void shouldWriteResponse() {
     // given
-    responseWriter = new CommandResponseWriterImpl(null);
+    responseWriter = new CommandResponseWriterImpl(null, new NoopRequestLimiter());
 
     eventWriter.wrap(new UnsafeBuffer(EVENT), 0, EVENT.length);
 
@@ -80,5 +86,23 @@ public class CommandResponseWriterImplTest {
 
     final byte[] event = readBytes(responseDecoder::getValue, responseDecoder::valueLength);
     assertThat(event).isEqualTo(EVENT);
+  }
+
+  @Test
+  public void shouldInvokeLimiterOnResponse() {
+    // given
+    final int remoteStreamId = 2;
+    final long requestId = 100;
+    final int partitionId = 5;
+    final RequestLimiter mockLimiter = mock(RequestLimiter.class);
+    responseWriter = new CommandResponseWriterImpl(mock(ServerOutput.class), mockLimiter);
+    eventWriter.wrap(new UnsafeBuffer(EVENT), 0, EVENT.length);
+
+    // when
+    responseWriter.partitionId(partitionId).valueWriter(eventWriter);
+    responseWriter.tryWriteResponse(remoteStreamId, requestId);
+
+    // then
+    verify(mockLimiter, times(1)).onResponse(remoteStreamId, requestId);
   }
 }