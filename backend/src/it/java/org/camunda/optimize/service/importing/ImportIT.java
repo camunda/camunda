18adf6d92e82237a7617f005498a94bc7408ec3b
Sebastian Bathke <sebastian.bathke@camunda.com>
2020-05-29 12:40:28 +0200

    chore(test): reduce sleeps in import test
    - overall runtime was 1.5minutes

--- backend/src/it/java/org/camunda/optimize/service/importing/ImportIT.java
+++ backend/src/it/java/org/camunda/optimize/service/importing/ImportIT.java
@@ -84,14 +84,14 @@ public class ImportIT extends AbstractImportIT {
 
     // make sure fetching endpoint is called during import
     embeddedOptimizeExtension.startContinuousImportScheduling();
-    Thread.sleep(5000);
+    Thread.sleep(1000);
     esMockServer.verify(importFetcherEndpointMatcher);
 
     // endpoint no longer fails
     esMockServer.reset();
 
     // wait for import to finish
-    Thread.sleep(5000);
+    Thread.sleep(1000);
     elasticSearchIntegrationTestExtension.refreshAllOptimizeIndices();
 
     // then everything has been imported
