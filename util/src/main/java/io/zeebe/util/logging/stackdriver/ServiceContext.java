47165571003bce6e92c3c6422c8e4de8843833a5
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2020-06-07 16:12:36 +0200

    chore(util): introduce StackdriverLayout
    - adds a Stackdriver log4j2 layout, configurable via a layout plugin
      builder
    - supports setting severity, context, source location, and the error
      report type format
    - make ZbLogger location aware; if the delegate logger given to ZbLogger
      is location aware, then ZbLogger will properly pass the correct logger
      FQCN (itself) to ensure that the LogEvent source location is properly
      computed

---
+++ util/src/main/java/io/zeebe/util/logging/stackdriver/ServiceContext.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.0. You may not use this file
+ * except in compliance with the Zeebe Community License 1.0.
+ */
+package io.zeebe.util.logging.stackdriver;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * The service context is used to properly aggregate errors in the Stackdriver Error Reporting tool.
+ *
+ * <p>Errors are grouped by service name and service version, allowing us to track which versions
+ * and clusters are affected by a set of errors.
+ *
+ * <p>https://cloud.google.com/error-reporting/docs/formatting-error-messages
+ */
+@JsonInclude(Include.NON_EMPTY)
+final class ServiceContext {
+  @JsonProperty("service")
+  private String service;
+
+  @JsonProperty("version")
+  private String version;
+
+  public String getService() {
+    return service;
+  }
+
+  public void setService(final String service) {
+    this.service = service;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setVersion(final String version) {
+    this.version = version;
+  }
+}
