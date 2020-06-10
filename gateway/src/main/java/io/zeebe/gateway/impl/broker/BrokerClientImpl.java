uuace/zeebe

chore(gateway): donot retry partition-independent requests when node not available
* Do not retry activate job and create workflow when the leader for partition is not available
* Retry create workflow request with a new partition immediately

gateway/src/main/java/io/zeebe/gateway/impl/broker/BrokerClientImpl.java
@@ -22,7 +22,6 @@ import io.zeebe.transport.impl.AtomixClientTransportAdapter;
 import io.zeebe.util.exception.UncheckedExecutionException;
 import io.zeebe.util.sched.ActorScheduler;
 import io.zeebe.util.sched.clock.ActorClock;
-import io.zeebe.util.sched.future.ActorFuture;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -134,8 +133,20 @@ public final class BrokerClientImpl implements BrokerClient {
    * @see io.zeebe.gateway.impl.broker.BrokerClient#sendRequest(io.zeebe.gateway.impl.broker.request.BrokerRequest)
    */
   @Override
-  public <T> ActorFuture<BrokerResponse<T>> sendRequest(final BrokerRequest<T> request) {
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequest(final BrokerRequest<T> request) {
     return requestManager.sendRequest(request);
+  }
+
+  @Override
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequest(
+      final BrokerRequest<T> request, final boolean shouldRetry) {
+    return requestManager.sendRequest(request, shouldRetry);
+  }
+
+  @Override
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequest(
+      final BrokerRequest<T> request, final boolean shouldRetry, final Duration requestTimeout) {
+    return requestManager.sendRequest(request, shouldRetry, requestTimeout);
   }
 
   @Override
@@ -143,11 +154,20 @@ public final class BrokerClientImpl implements BrokerClient {
       final BrokerRequest<T> request,
       final BrokerResponseConsumer<T> responseConsumer,
       final Consumer<Throwable> throwableConsumer) {
-    requestManager.sendRequest(request, responseConsumer, throwableConsumer);
+    requestManager
+        .sendRequest(request)
+        .whenComplete(
+            (response, error) -> {
+              if (error == null) {
+                responseConsumer.accept(response.getKey(), response.getResponse());
+              } else {
+                throwableConsumer.accept(error);
+              }
+            });
   }
 
   @Override
-  public <T> ActorFuture<BrokerResponse<T>> sendRequest(
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequest(
       final BrokerRequest<T> request, final Duration requestTimeout) {
     return requestManager.sendRequest(request, requestTimeout);
   }
@@ -158,7 +178,17 @@ public final class BrokerClientImpl implements BrokerClient {
       final BrokerResponseConsumer<T> responseConsumer,
       final Consumer<Throwable> throwableConsumer,
       final Duration requestTimeout) {
-    requestManager.sendRequest(request, responseConsumer, throwableConsumer, requestTimeout);
+
+    requestManager
+        .sendRequest(request, requestTimeout)
+        .whenComplete(
+            (response, error) -> {
+              if (error == null) {
+                responseConsumer.accept(response.getKey(), response.getResponse());
+              } else {
+                throwableConsumer.accept(error);
+              }
+            });
   }
 
   @Override

qa/integration-tests/src/test/java/io/zeebe/broker/it/gateway/GatewayIntegrationTest.java
@@ -11,16 +11,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.atomix.cluster.AtomixCluster;
 import io.zeebe.broker.test.EmbeddedBrokerRule;
+import io.zeebe.gateway.cmd.BrokerRejectionException;
 import io.zeebe.gateway.impl.broker.BrokerClientImpl;
 import io.zeebe.gateway.impl.broker.request.BrokerCreateWorkflowInstanceRequest;
 import io.zeebe.gateway.impl.broker.response.BrokerRejection;
-import io.zeebe.gateway.impl.broker.response.BrokerResponse;
 import io.zeebe.gateway.impl.configuration.GatewayCfg;
-import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceCreationRecord;
 import io.zeebe.protocol.record.RejectionType;
 import io.zeebe.test.util.socket.SocketUtil;
 import io.zeebe.util.sched.clock.ControlledActorClock;
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -61,18 +62,27 @@ public final class GatewayIntegrationTest {
   }
 
   @Test
-  public void shouldReturnRejectionWithCorrectTypeAndReason() {
+  public void shouldReturnRejectionWithCorrectTypeAndReason() throws InterruptedException {
     // given
+    final CountDownLatch latch = new CountDownLatch(1);
+    final AtomicReference<Throwable> errorResponse = new AtomicReference<>();
 
     // when
-    final BrokerResponse<WorkflowInstanceCreationRecord> response =
-        client.sendRequest(new BrokerCreateWorkflowInstanceRequest()).join();
+    client.sendRequest(
+        new BrokerCreateWorkflowInstanceRequest(),
+        (k, r) -> {},
+        error -> {
+          errorResponse.set(error);
+          latch.countDown();
+        });
 
     // then
-    assertThat(response.isRejection()).isTrue();
-    final BrokerRejection error = response.getRejection();
-    assertThat(error.getType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
-    assertThat(error.getReason())
+    latch.await();
+    final var error = errorResponse.get();
+    assertThat(error instanceof BrokerRejectionException).isTrue();
+    final BrokerRejection rejection = ((BrokerRejectionException) error).getRejection();
+    assertThat(rejection.getType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
+    assertThat(rejection.getReason())
         .isEqualTo("Expected at least a bpmnProcessId or a key greater than -1, but none given");
   }
 }

camunda/camunda

gateway/src/main/java/io/zeebe/gateway/impl/broker/BrokerClientImpl.java
@@ -22,7 +22,6 @@ import io.zeebe.transport.impl.AtomixClientTransportAdapter;
 import io.zeebe.util.exception.UncheckedExecutionException;
 import io.zeebe.util.sched.ActorScheduler;
 import io.zeebe.util.sched.clock.ActorClock;
-import io.zeebe.util.sched.future.ActorFuture;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -130,35 +129,44 @@ public final class BrokerClientImpl implements BrokerClient {
     LOG.debug("Gateway broker client closed.");
   }
 
-  /* (non-Javadoc)
-   * @see io.zeebe.gateway.impl.broker.BrokerClient#sendRequest(io.zeebe.gateway.impl.broker.request.BrokerRequest)
-   */
   @Override
-  public <T> ActorFuture<BrokerResponse<T>> sendRequest(final BrokerRequest<T> request) {
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequest(final BrokerRequest<T> request) {
     return requestManager.sendRequest(request);
   }
 
   @Override
-  public <T> void sendRequest(
-      final BrokerRequest<T> request,
-      final BrokerResponseConsumer<T> responseConsumer,
-      final Consumer<Throwable> throwableConsumer) {
-    requestManager.sendRequest(request, responseConsumer, throwableConsumer);
-  }
-
-  @Override
-  public <T> ActorFuture<BrokerResponse<T>> sendRequest(
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequest(
       final BrokerRequest<T> request, final Duration requestTimeout) {
     return requestManager.sendRequest(request, requestTimeout);
   }
 
   @Override
-  public <T> void sendRequest(
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequestWithRetry(
+      final BrokerRequest<T> request) {
+    return requestManager.sendRequestWithRetry(request);
+  }
+
+  @Override
+  public <T> CompletableFuture<BrokerResponse<T>> sendRequestWithRetry(
+      final BrokerRequest<T> request, final Duration requestTimeout) {
+    return requestManager.sendRequestWithRetry(request, requestTimeout);
+  }
+
+  @Override
+  public <T> void sendRequestWithRetry(
       final BrokerRequest<T> request,
       final BrokerResponseConsumer<T> responseConsumer,
-      final Consumer<Throwable> throwableConsumer,
-      final Duration requestTimeout) {
-    requestManager.sendRequest(request, responseConsumer, throwableConsumer, requestTimeout);
+      final Consumer<Throwable> throwableConsumer) {
+    requestManager
+        .sendRequestWithRetry(request)
+        .whenComplete(
+            (response, error) -> {
+              if (error == null) {
+                responseConsumer.accept(response.getKey(), response.getResponse());
+              } else {
+                throwableConsumer.accept(error);
+              }
+            });
   }
 
   @Override
@@ -179,6 +187,24 @@ public final class BrokerClientImpl implements BrokerClient {
                   return CompletableFuture.completedFuture(null);
                 })
             .join();
+  }
+
+  public <T> void sendRequestWithRetry(
+      final BrokerRequest<T> request,
+      final BrokerResponseConsumer<T> responseConsumer,
+      final Consumer<Throwable> throwableConsumer,
+      final Duration requestTimeout) {
+
+    requestManager
+        .sendRequestWithRetry(request, requestTimeout)
+        .whenComplete(
+            (response, error) -> {
+              if (error == null) {
+                responseConsumer.accept(response.getKey(), response.getResponse());
+              } else {
+                throwableConsumer.accept(error);
+              }
+            });
   }
 
   private void doAndLogException(final Runnable r) {

qa/integration-tests/src/test/java/io/zeebe/broker/it/gateway/GatewayIntegrationTest.java
@@ -11,16 +11,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.atomix.cluster.AtomixCluster;
 import io.zeebe.broker.test.EmbeddedBrokerRule;
+import io.zeebe.gateway.cmd.BrokerRejectionException;
 import io.zeebe.gateway.impl.broker.BrokerClientImpl;
 import io.zeebe.gateway.impl.broker.request.BrokerCreateWorkflowInstanceRequest;
 import io.zeebe.gateway.impl.broker.response.BrokerRejection;
-import io.zeebe.gateway.impl.broker.response.BrokerResponse;
 import io.zeebe.gateway.impl.configuration.GatewayCfg;
-import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceCreationRecord;
 import io.zeebe.protocol.record.RejectionType;
 import io.zeebe.test.util.socket.SocketUtil;
 import io.zeebe.util.sched.clock.ControlledActorClock;
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -61,18 +62,27 @@ public final class GatewayIntegrationTest {
   }
 
   @Test
-  public void shouldReturnRejectionWithCorrectTypeAndReason() {
+  public void shouldReturnRejectionWithCorrectTypeAndReason() throws InterruptedException {
     // given
+    final CountDownLatch latch = new CountDownLatch(1);
+    final AtomicReference<Throwable> errorResponse = new AtomicReference<>();
 
     // when
-    final BrokerResponse<WorkflowInstanceCreationRecord> response =
-        client.sendRequest(new BrokerCreateWorkflowInstanceRequest()).join();
+    client.sendRequestWithRetry(
+        new BrokerCreateWorkflowInstanceRequest(),
+        (k, r) -> {},
+        error -> {
+          errorResponse.set(error);
+          latch.countDown();
+        });
 
     // then
-    assertThat(response.isRejection()).isTrue();
-    final BrokerRejection error = response.getRejection();
-    assertThat(error.getType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
-    assertThat(error.getReason())
+    latch.await();
+    final var error = errorResponse.get();
+    assertThat(error).isInstanceOf(BrokerRejectionException.class);
+    final BrokerRejection rejection = ((BrokerRejectionException) error).getRejection();
+    assertThat(rejection.getType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
+    assertThat(rejection.getReason())
         .isEqualTo("Expected at least a bpmnProcessId or a key greater than -1, but none given");
   }
 }