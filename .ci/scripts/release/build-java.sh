832aef3bf44877d2f2189610df2849dd645e87ce
Nico korthout <nico.korthout@camunda.com>
2020-07-15 18:26:51 +0200

    chore(build): improve fmt and license checks
    - only activate fmt-plugin on jdk9+
    - use profiles to run checks from ci builds
    
    The latter is necessary to make sure the version from the pom is used,
    instead of the latest
    
    (cherry picked from commit a0b1be2d221c40eabc6d5cfc814d911e9095626c)

--- .ci/scripts/release/build-java.sh
+++ .ci/scripts/release/build-java.sh
@@ -2,4 +2,4 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-mvn -B -s ${MAVEN_SETTINGS_XML} -DskipTests clean com.mycila:license-maven-plugin:check com.coveo:fmt-maven-plugin:check install -Pprepare-offline
+mvn -B -s ${MAVEN_SETTINGS_XML} -DskipTests clean install -Pchecks,prepare-offline
