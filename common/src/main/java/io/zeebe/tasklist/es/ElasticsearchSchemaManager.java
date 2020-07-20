b235c33a0b50cab44a27290396b03ff961b34263
Ralf Puchert <49151958+ralfpuchert@users.noreply.github.com>
2020-07-20 19:53:14 +0200

    #186 check for existing templates and indices (#187)
    Related with #186

--- common/src/main/java/io/zeebe/tasklist/es/ElasticsearchSchemaManager.java
+++ common/src/main/java/io/zeebe/tasklist/es/ElasticsearchSchemaManager.java
@@ -5,8 +5,6 @@
  */
 package io.zeebe.tasklist.es;
 
-import static io.zeebe.tasklist.util.CollectionUtil.map;
-
 import io.zeebe.tasklist.es.schema.indices.IndexDescriptor;
 import io.zeebe.tasklist.es.schema.templates.TemplateDescriptor;
 import io.zeebe.tasklist.exceptions.TasklistRuntimeException;
@@ -21,6 +19,7 @@ import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.IndexTemplatesExistRequest;
 import org.elasticsearch.client.indices.PutIndexTemplateRequest;
 import org.elasticsearch.common.xcontent.XContentHelper;
 import org.elasticsearch.common.xcontent.XContentType;
@@ -41,66 +40,70 @@ public class ElasticsearchSchemaManager {
   @Autowired private List<TemplateDescriptor> templateDescriptors;
 
   @PostConstruct
-  public boolean initializeSchema() {
+  public void initializeSchema() {
     if (tasklistProperties.getElasticsearch().isCreateSchema() && !schemaAlreadyExists()) {
-      LOGGER.info("Elasticsearch schema is empty. Indices will be created.");
-      return createSchema();
+      LOGGER.info(
+          "Elasticsearch schema is empty or not complete. Templates and indices will be created.");
+      createSchema();
     } else {
       LOGGER.info(
           "Elasticsearch schema won't be created, it either already exist, or schema creation is disabled in configuration.");
-      return false;
     }
   }
 
-  public boolean createSchema() {
-    return createTemplates() && createIndices();
-  }
-
-  public boolean createIndices() {
-    return !map(indexDescriptors, this::createIndex).contains(false);
-  }
-
-  public boolean createTemplates() {
-    return !map(templateDescriptors, this::createTemplate).contains(false);
-  }
-
-  protected boolean createIndex(IndexDescriptor indexDescriptor) {
-    final Map<String, Object> indexDescription = readJSONFileToMap(indexDescriptor.getFileName());
-    // Adjust aliases in case of other configured indexNames, e.g. non-default prefix
-    indexDescription.put(
-        "aliases", Collections.singletonMap(indexDescriptor.getAlias(), Collections.EMPTY_MAP));
-
-    return createIndex(
-        new CreateIndexRequest(indexDescriptor.getIndexName()).source(indexDescription),
-        indexDescriptor.getIndexName());
-  }
-
-  protected boolean createTemplate(TemplateDescriptor templateDescriptor) {
-    final Map<String, Object> template = readJSONFileToMap(templateDescriptor.getFileName());
-
-    // Adjust prefixes and aliases in case of other configured indexNames, e.g. non-default prefix
-    template.put("index_patterns", Collections.singletonList(templateDescriptor.getIndexPattern()));
-    template.put(
-        "aliases", Collections.singletonMap(templateDescriptor.getAlias(), Collections.EMPTY_MAP));
-
-    return putIndexTemplate(
-            new PutIndexTemplateRequest(templateDescriptor.getTemplateName()).source(template),
-            templateDescriptor.getTemplateName())
-        // This is necessary, otherwise we won't find indexes at startup
-        && createIndex(
-            new CreateIndexRequest(templateDescriptor.getMainIndexName()),
-            templateDescriptor.getMainIndexName());
-  }
-
   public boolean schemaAlreadyExists() {
-    try {
-      final String indexName = indexDescriptors.get(0).getAlias();
-      return esClient.indices().exists(new GetIndexRequest(indexName), RequestOptions.DEFAULT);
-    } catch (IOException e) {
-      final String message =
-          String.format("Exception occurred, while checking schema existence: %s", e.getMessage());
-      LOGGER.error(message, e);
-      throw new TasklistRuntimeException(message, e);
+    return indexDescriptors.stream().allMatch(i -> indexExists(i.getAlias()))
+        && templateDescriptors.stream()
+            .allMatch(
+                t -> templateExists(t.getTemplateName()) && indexExists(t.getMainIndexName()));
+  }
+
+  public void createSchema() {
+    createTemplates();
+    createIndices();
+  }
+
+  public void createIndices() {
+    indexDescriptors.forEach(this::createIndex);
+  }
+
+  public void createTemplates() {
+    templateDescriptors.forEach(this::createTemplate);
+  }
+
+  protected void createIndex(IndexDescriptor indexDescriptor) {
+    if (!indexExists(indexDescriptor.getIndexName())) {
+      final Map<String, Object> indexDescription = readJSONFileToMap(indexDescriptor.getFileName());
+      // Adjust aliases in case of other configured indexNames, e.g. non-default prefix
+      indexDescription.put(
+          "aliases", Collections.singletonMap(indexDescriptor.getAlias(), Collections.EMPTY_MAP));
+
+      createIndex(
+          new CreateIndexRequest(indexDescriptor.getIndexName()).source(indexDescription),
+          indexDescriptor.getIndexName());
+    }
+  }
+
+  protected void createTemplate(TemplateDescriptor templateDescriptor) {
+    if (!templateExists(templateDescriptor.getTemplateName())) {
+      final Map<String, Object> template = readJSONFileToMap(templateDescriptor.getFileName());
+
+      // Adjust prefixes and aliases in case of other configured indexNames, e.g. non-default prefix
+      template.put(
+          "index_patterns", Collections.singletonList(templateDescriptor.getIndexPattern()));
+      template.put(
+          "aliases",
+          Collections.singletonMap(templateDescriptor.getAlias(), Collections.EMPTY_MAP));
+
+      putIndexTemplate(
+          new PutIndexTemplateRequest(templateDescriptor.getTemplateName()).source(template),
+          templateDescriptor.getTemplateName());
+    }
+    if (!indexExists(templateDescriptor.getMainIndexName())) {
+      // This is necessary, otherwise we won't find indexes at startup
+      createIndex(
+          new CreateIndexRequest(templateDescriptor.getMainIndexName()),
+          templateDescriptor.getMainIndexName());
     }
   }
 
@@ -118,20 +121,21 @@ public class ElasticsearchSchemaManager {
     return result;
   }
 
-  protected boolean createIndex(final CreateIndexRequest createIndexRequest, String indexName) {
+  protected void createIndex(final CreateIndexRequest createIndexRequest, String indexName) {
     try {
       final boolean acknowledged =
           esClient.indices().create(createIndexRequest, RequestOptions.DEFAULT).isAcknowledged();
       if (acknowledged) {
-        LOGGER.debug("Index [{}] was successfully created", indexName);
+        LOGGER.info("Index [{}] was successfully created", indexName);
+      } else {
+        LOGGER.info("Index [{}] wasn't created", indexName);
       }
-      return acknowledged;
     } catch (IOException e) {
-      throw new TasklistRuntimeException("Failed to create index ", e);
+      throw new TasklistRuntimeException("Failed to create index " + indexName, e);
     }
   }
 
-  protected boolean putIndexTemplate(
+  protected void putIndexTemplate(
       final PutIndexTemplateRequest putIndexTemplateRequest, String templateName) {
     try {
       final boolean acknowledged =
@@ -140,11 +144,31 @@ public class ElasticsearchSchemaManager {
               .putTemplate(putIndexTemplateRequest, RequestOptions.DEFAULT)
               .isAcknowledged();
       if (acknowledged) {
-        LOGGER.debug("Template [{}] was successfully created", templateName);
+        LOGGER.info("Template [{}] was successfully created", templateName);
+      } else {
+        LOGGER.info("Template [{}] wasn't created", templateName);
       }
-      return acknowledged;
     } catch (IOException e) {
-      throw new TasklistRuntimeException("Failed to put index template", e);
+      throw new TasklistRuntimeException("Failed to put index template " + templateName, e);
+    }
+  }
+
+  protected boolean templateExists(final String templateName) {
+    try {
+      return esClient
+          .indices()
+          .existsTemplate(new IndexTemplatesExistRequest(templateName), RequestOptions.DEFAULT);
+    } catch (IOException e) {
+      throw new TasklistRuntimeException(
+          "Failed to check existence of template " + templateName, e);
+    }
+  }
+
+  protected boolean indexExists(final String indexName) {
+    try {
+      return esClient.indices().exists(new GetIndexRequest(indexName), RequestOptions.DEFAULT);
+    } catch (IOException e) {
+      throw new TasklistRuntimeException("Failed to check existence of index " + indexName, e);
     }
   }
 }
