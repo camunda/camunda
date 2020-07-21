eff7f33db6c7e40e15184cc2e714a7335bb586d3
pihme <pihme@users.noreply.github.com>
2020-07-21 14:42:22 +0200

    fix(atomix): separate test data folders for parallel tests

---
+++ test-util/src/main/java/io/zeebe/test/util/TestEnvironment.java
@@ -0,0 +1,47 @@
+package io.zeebe.test.util;
+
+import io.zeebe.util.ZbLogger;
+import org.slf4j.Logger;
+
+public class TestEnvironment {
+  private static final Logger LOG = new ZbLogger("io.zeebe.test.util");
+
+  private static final String TEST_FORK_NUMBER_PROPERTY_NAME = "testForkNumber";
+  private static final String TEST_MAVEN_ID_PROPERTY_NAME = "testMavenId";
+
+  public static int getTestForkNumber() {
+    int testForkNumber = 0;
+    try {
+      final String testForkNumberProperty = System.getProperty(TEST_FORK_NUMBER_PROPERTY_NAME);
+      if (testForkNumberProperty != null) {
+        testForkNumber = Integer.valueOf(testForkNumberProperty);
+      } else {
+        LOG.warn(
+            "No system property '{}' set, using default value {}",
+            TEST_FORK_NUMBER_PROPERTY_NAME,
+            testForkNumber);
+      }
+    } catch (final Exception e) {
+      LOG.warn("Failed to read test fork number system property", e);
+    }
+    return testForkNumber;
+  }
+
+  public static int getTestMavenId() {
+    int testMavenId = 0;
+    try {
+      final String testMavenIdProperty = System.getProperty(TEST_MAVEN_ID_PROPERTY_NAME);
+      if (testMavenIdProperty != null) {
+        testMavenId = Integer.valueOf(testMavenIdProperty);
+      } else {
+        LOG.warn(
+            "No system property '{}' set, using default value {}",
+            TEST_MAVEN_ID_PROPERTY_NAME,
+            testMavenId);
+      }
+    } catch (final Exception e) {
+      LOG.warn("Failed to read test maven id system property", e);
+    }
+    return testMavenId;
+  }
+}
