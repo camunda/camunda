5b03c4d257e39a7f8aa3f10d175c5bcad3e622ec
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2020-07-27 11:42:37 +0200

    chore(atomix): add metrics for journal start time and segment count

--- atomix/storage/src/main/java/io/atomix/storage/statistics/JournalMetrics.java
+++ atomix/storage/src/main/java/io/atomix/storage/statistics/JournalMetrics.java
@@ -16,32 +16,50 @@
  */
 package io.atomix.storage.statistics;
 
+import io.prometheus.client.Gauge;
 import io.prometheus.client.Histogram;
 
 public class JournalMetrics {
 
+  private static final String NAMESPACE = "atomix";
+  private static final String PARTITION_LABEL = "partition";
   private static final Histogram SEGMENT_CREATION_TIME =
       Histogram.build()
-          .namespace("atomix")
+          .namespace(NAMESPACE)
           .name("segment_creation_time")
           .help("Time spend to create a new segment")
-          .labelNames("partition")
+          .labelNames(PARTITION_LABEL)
           .register();
 
   private static final Histogram SEGMENT_TRUNCATE_TIME =
       Histogram.build()
-          .namespace("atomix")
+          .namespace(NAMESPACE)
           .name("segment_truncate_time")
           .help("Time spend to truncate a segment")
-          .labelNames("partition")
+          .labelNames(PARTITION_LABEL)
           .register();
 
   private static final Histogram SEGMENT_FLUSH_TIME =
       Histogram.build()
-          .namespace("atomix")
+          .namespace(NAMESPACE)
           .name("segment_flush_time")
           .help("Time spend to flush segment to disk")
-          .labelNames("partition")
+          .labelNames(PARTITION_LABEL)
+          .register();
+
+  private static final Gauge SEGMENT_COUNT =
+      Gauge.build()
+          .namespace(NAMESPACE)
+          .name("segment_count")
+          .help("Number of segments")
+          .labelNames(PARTITION_LABEL)
+          .register();
+  private static final Gauge JOURNAL_OPEN_DURATION =
+      Gauge.build()
+          .namespace(NAMESPACE)
+          .name("journal_open_time")
+          .help("Time taken to open the journal")
+          .labelNames(PARTITION_LABEL)
           .register();
 
   private final String logName;
@@ -60,5 +78,17 @@ public class JournalMetrics {
 
   public void observeSegmentTruncation(final Runnable segmentTruncation) {
     SEGMENT_TRUNCATE_TIME.labels(logName).time(segmentTruncation);
+  }
+
+  public void observeJournalOpenDuration(final long durationMillis) {
+    JOURNAL_OPEN_DURATION.labels(logName).set(durationMillis);
+  }
+
+  public void incSegmentCount() {
+    SEGMENT_COUNT.labels(logName).inc();
+  }
+
+  public void decSegmentCount() {
+    SEGMENT_COUNT.labels(logName).dec();
   }
 }
