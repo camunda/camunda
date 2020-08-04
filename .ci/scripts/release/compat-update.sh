5ab93ae1d3c7639a9c64c0690c7e340b7f695bc1
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2020-08-04 10:14:37 +0200

    chore(ci): commit only if there are changes to commit

--- .ci/scripts/release/compat-update.sh
+++ .ci/scripts/release/compat-update.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -xue
+
 if [[ "$RELEASE_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   mvn versions:set-property -DgenerateBackupPoms=false -Dproperty=backwards.compat.version -DnewVersion=${RELEASE_VERSION}
   gocompat save --path=clients/go/.gocompat.json clients/go/...
@@ -5,8 +7,8 @@ if [[ "$RELEASE_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   FILE=$(mvn help:evaluate -Dexpression=ignored.changes.file -q -DforceStdout)
 
   rm -f clients/java/$FILE test/$FILE exporter-api/$FILE protocol/$FILE bpmn-model/$FILE
+
+  git commit -am "chore(project): update versions"
 else
   echo "Version $RELEASE_VERSION"
 fi
-
-git commit -am "chore(project): update versions"
