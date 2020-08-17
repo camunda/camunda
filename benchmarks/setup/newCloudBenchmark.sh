095f7b22c6fd18ecfbd4f778cd0cd93912fcf67c
Christopher Zell <zelldon91@googlemail.com>
2020-08-17 14:42:42 +0200

    chore(benchmarks): add cloud benchmark setup

---
+++ benchmarks/setup/newCloudBenchmark.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -exo pipefail
+
+if [ -z $1 ]
+then
+  echo "Please provide an namespace name!"
+  exit 1
+fi
+
+### Cloud Benchmark helper script
+### First parameter is used as namespace name
+### For a new namespace a new folder will be created
+
+
+namespace=$1
+
+kubectl create namespace $namespace
+kubens $namespace
+cp -rv cloud-default/ $namespace
+cd $namespace
+
+sed -i "s/default/$namespace/g" Makefile starter.yaml timer.yaml simpleStarter.yaml worker.yaml
