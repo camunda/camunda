029d21fb77e8bb30e1e47f4c975702be477ccbec
Ralf Puchert <49151958+ralfpuchert@users.noreply.github.com>
2020-08-24 15:29:57 +0200

    OPE-1006 Elasticsearch http auth (#678)
    - Add username and password to ElasticsearchProperties
    - Add test for basic auth
    - add possibility to define full URL including HTTP/HTTPS schema
    
    Related with OPE-1006

--- common/src/main/java/org/camunda/operate/es/ElasticsearchConnector.java
+++ common/src/main/java/org/camunda/operate/es/ElasticsearchConnector.java
@@ -6,6 +6,7 @@
 package org.camunda.operate.es;
 
 import java.io.IOException;
+import java.net.URI;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
@@ -13,6 +14,12 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.camunda.operate.property.ElasticsearchProperties;
 import org.camunda.operate.property.OperateProperties;
 
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
@@ -32,8 +39,9 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import static org.camunda.operate.util.ThreadUtil.sleepFor;
+import org.springframework.util.StringUtils;
 
+import static org.camunda.operate.util.ThreadUtil.sleepFor;
 
 @Component
 @Configuration
@@ -48,14 +56,14 @@ public class ElasticsearchConnector {
   public RestHighLevelClient esClient() {
     //some weird error when ELS sets available processors number for Netty - see https://discuss.elastic.co/t/elasticsearch-5-4-1-availableprocessors-is-already-set/88036/3
     System.setProperty("es.set.netty.runtime.available.processors", "false");
-    return createEsClient(operateProperties.getElasticsearch().getHost(), operateProperties.getElasticsearch().getPort());
+    return createEsClient(operateProperties.getElasticsearch());
   }
 
   @Bean("zeebeEsClient")
   public RestHighLevelClient zeebeEsClient() {
     //some weird error when ELS sets available processors number for Netty - see https://discuss.elastic.co/t/elasticsearch-5-4-1-availableprocessors-is-already-set/88036/3
     System.setProperty("es.set.netty.runtime.available.processors", "false");
-    return createEsClient(operateProperties.getZeebeElasticsearch().getHost(), operateProperties.getZeebeElasticsearch().getPort());
+    return createEsClient(operateProperties.getZeebeElasticsearch());
   }
   
   public static void closeEsClient(RestHighLevelClient esClient) {
@@ -68,10 +76,12 @@ public class ElasticsearchConnector {
     }
   }
 
-  public RestHighLevelClient createEsClient(String host, int port) {
+  public RestHighLevelClient createEsClient(ElasticsearchProperties elsConfig) {
     logger.debug("Creating Elasticsearch connection...");
     RestHighLevelClient esClient;
-    esClient = new RestHighLevelClient(RestClient.builder(new HttpHost(host, port, "http")));
+    esClient = new RestHighLevelClient(RestClient.builder(getHttpHost(elsConfig))
+        .setHttpClientConfigCallback(
+            httpClientBuilder -> setupAuthentication(httpClientBuilder, elsConfig)));
     if (!checkHealth(esClient, true)) {
       logger.warn("Elasticsearch cluster is not accessible");
     } else {
@@ -80,23 +90,48 @@ public class ElasticsearchConnector {
     return esClient;
   }
 
+  private HttpHost getHttpHost(ElasticsearchProperties elsConfig) {
+    URI uri = elsConfig.getURI();
+    return new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+  }
+
+  private HttpAsyncClientBuilder setupAuthentication(final HttpAsyncClientBuilder builder,
+      ElasticsearchProperties elsConfig) {
+    if (StringUtils.isEmpty(elsConfig.getUsername()) || StringUtils
+        .isEmpty(elsConfig.getPassword())) {
+      logger.warn(
+          "Username and/or password for are empty. Basic authentication for elasticsearch is not used.");
+      return builder;
+    }
+    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(
+        AuthScope.ANY,
+        new UsernamePasswordCredentials(
+            elsConfig.getUsername(),
+            elsConfig.getPassword()));
+
+    builder.setDefaultCredentialsProvider(credentialsProvider);
+    return builder;
+  }
+
   public boolean checkHealth(RestHighLevelClient esClient, boolean reconnect) {
     //TODO temporary solution
+    ElasticsearchProperties elsConfig = operateProperties.getElasticsearch();
     int attempts = 0, maxAttempts = 50;
     boolean successfullyConnected = false;
     while (attempts == 0 || (reconnect && attempts < maxAttempts && !successfullyConnected)) {
       try {
-        final ClusterHealthResponse clusterHealthResponse = esClient.cluster().health(new ClusterHealthRequest(), RequestOptions.DEFAULT);
+        final ClusterHealthResponse clusterHealthResponse = esClient.cluster()
+            .health(new ClusterHealthRequest(), RequestOptions.DEFAULT);
         //!clusterHealthResponse.getStatus().equals(ClusterHealthStatus.RED)
         //TODO do we need this?
-        successfullyConnected = clusterHealthResponse.getClusterName().equals(operateProperties.getElasticsearch().getClusterName());
+        successfullyConnected = clusterHealthResponse.getClusterName()
+            .equals(elsConfig.getClusterName());
       } catch (IOException ex) {
-        logger.error("Error occurred while connecting to Elasticsearch: clustername [{}], {}:{}. Will be retried ({}/{}) ...",
-            operateProperties.getElasticsearch().getClusterName(),
-            operateProperties.getElasticsearch().getHost(),
-            operateProperties.getElasticsearch().getPort(),
-            attempts, maxAttempts,
-            ex);
+        logger.error(
+            "Error occurred while connecting to Elasticsearch: clustername [{}], {}:{}. Will be retried ({}/{}) ...",
+            elsConfig.getClusterName(),
+            elsConfig.getHost(), elsConfig.getPort(), attempts, maxAttempts, ex);
         sleepFor(3000);
       }
       attempts++;
--- common/src/main/java/org/camunda/operate/property/ElasticsearchProperties.java
+++ common/src/main/java/org/camunda/operate/property/ElasticsearchProperties.java
@@ -5,6 +5,10 @@
  */
 package org.camunda.operate.property;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.camunda.operate.exceptions.OperateRuntimeException;
+
 public class ElasticsearchProperties {
 
   public static final String DATE_FORMAT_DEFAULT = "yyyy-MM-dd'T'HH:mm:ss.SSSZZ";
@@ -24,6 +28,10 @@ public class ElasticsearchProperties {
   private int batchSize = 2000;
 
   private boolean createSchema = true;
+
+  private String url;
+  private String username;
+  private String password;
 
   public String getClusterName() {
     return clusterName;
@@ -79,5 +87,50 @@ public class ElasticsearchProperties {
 
   public void setCreateSchema(boolean createSchema) {
     this.createSchema = createSchema;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+    setHost(getURI().getHost());
+    setPort(getURI().getPort());
+  }
+
+  public URI getURI(){
+    if (url == null) {
+      try {
+        return new URI(String.format("http://%s:%d", getHost(), getPort()));
+      } catch (URISyntaxException e) {
+        throw new OperateRuntimeException(
+            String
+                .format("Failed to build URI from host %s and port %d url ", getHost(), getPort()),
+            e);
+      }
+    } else {
+      try {
+        return new URI(url);
+      } catch (final URISyntaxException e) {
+        throw new OperateRuntimeException("Failed to parse url " + url, e);
+      }
+    }
   }
 }
