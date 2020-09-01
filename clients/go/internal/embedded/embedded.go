fa986d39d0d0c48ae3744a6d37d274b38aa0efa8
camunda-jenkins <ci@camunda.com>
2020-09-01 11:46:55 +0000

    chore(project): update go embedded version data

--- clients/go/internal/embedded/embedded.go
+++ clients/go/internal/embedded/embedded.go
@@ -93,7 +93,7 @@ func version() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "VERSION", size: 14, mode: os.FileMode(420), modTime: time.Unix(1598952037, 0)}
+	info := bindataFileInfo{name: "VERSION", size: 14, mode: os.FileMode(420), modTime: time.Unix(1598960814, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
