69017cc906aa1d05f44b29a1a76193234efffddf
Ralf Puchert <49151958+ralfpuchert@users.noreply.github.com>
2020-09-29 09:24:04 +0200

    #311 add basic http auth for elasticsearch (#371)
    Related with #311

--- common/src/main/java/io/zeebe/tasklist/es/ElasticsearchConnector.java
+++ common/src/main/java/io/zeebe/tasklist/es/ElasticsearchConnector.java
@@ -5,6 +5,8 @@
  */
 package io.zeebe.tasklist.es;
 
+import static io.zeebe.tasklist.util.ThreadUtil.sleepFor;
+
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
@@ -12,14 +14,20 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import io.zeebe.tasklist.exceptions.TasklistRuntimeException;
+import io.zeebe.tasklist.property.ElasticsearchProperties;
 import io.zeebe.tasklist.property.TasklistProperties;
-import io.zeebe.tasklist.util.ThreadUtil;
 import java.io.IOException;
+import java.net.URI;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
 import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -31,6 +39,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 @Configuration
@@ -45,9 +54,7 @@ public class ElasticsearchConnector {
     // some weird error when ELS sets available processors number for Netty - see
     // https://discuss.elastic.co/t/elasticsearch-5-4-1-availableprocessors-is-already-set/88036/3
     System.setProperty("es.set.netty.runtime.available.processors", "false");
-    return createEsClient(
-        tasklistProperties.getElasticsearch().getHost(),
-        tasklistProperties.getElasticsearch().getPort());
+    return createEsClient(tasklistProperties.getElasticsearch());
   }
 
   @Bean("zeebeEsClient")
@@ -55,9 +62,7 @@ public class ElasticsearchConnector {
     // some weird error when ELS sets available processors number for Netty - see
     // https://discuss.elastic.co/t/elasticsearch-5-4-1-availableprocessors-is-already-set/88036/3
     System.setProperty("es.set.netty.runtime.available.processors", "false");
-    return createEsClient(
-        tasklistProperties.getZeebeElasticsearch().getHost(),
-        tasklistProperties.getZeebeElasticsearch().getPort());
+    return createEsClient(tasklistProperties.getZeebeElasticsearch());
   }
 
   public static void closeEsClient(RestHighLevelClient esClient) {
@@ -70,10 +75,13 @@ public class ElasticsearchConnector {
     }
   }
 
-  public RestHighLevelClient createEsClient(String host, int port) {
+  public RestHighLevelClient createEsClient(ElasticsearchProperties elsConfig) {
     LOGGER.debug("Creating Elasticsearch connection...");
     final RestHighLevelClient esClient =
-        new RestHighLevelClient(RestClient.builder(new HttpHost(host, port, "http")));
+        new RestHighLevelClient(
+            RestClient.builder(getHttpHost(elsConfig))
+                .setHttpClientConfigCallback(
+                    httpClientBuilder -> setupAuthentication(httpClientBuilder, elsConfig)));
     if (!checkHealth(esClient, true)) {
       LOGGER.warn("Elasticsearch cluster is not accessible");
     } else {
@@ -84,34 +92,53 @@ public class ElasticsearchConnector {
 
   public boolean checkHealth(RestHighLevelClient esClient, boolean reconnect) {
     // TODO temporary solution
+    final ElasticsearchProperties elsConfig = tasklistProperties.getElasticsearch();
     int attempts = 0;
+    final int maxAttempts = 50;
     boolean successfullyConnected = false;
-    while (attempts == 0 || (reconnect && attempts < 10 && !successfullyConnected)) {
+    while (attempts == 0 || (reconnect && attempts < maxAttempts && !successfullyConnected)) {
       try {
         final ClusterHealthResponse clusterHealthResponse =
             esClient.cluster().health(new ClusterHealthRequest(), RequestOptions.DEFAULT);
         // TODO do we need this?
         successfullyConnected =
-            clusterHealthResponse
-                .getClusterName()
-                .equals(tasklistProperties.getElasticsearch().getClusterName());
+            clusterHealthResponse.getClusterName().equals(elsConfig.getClusterName());
       } catch (IOException ex) {
         LOGGER.error(
-            String.format(
-                "Error occurred while connecting to Elasticsearch: clustername [%s], %s:%s. Will be retried...",
-                tasklistProperties.getElasticsearch().getClusterName(),
-                tasklistProperties.getElasticsearch().getHost(),
-                tasklistProperties.getElasticsearch().getPort()),
+            "Error occurred while connecting to Elasticsearch: clustername [{}], {}:{}. Will be retried ({}/{}) ...",
+            elsConfig.getClusterName(),
+            elsConfig.getHost(),
+            elsConfig.getPort(),
+            attempts,
+            maxAttempts,
             ex);
-        ThreadUtil.sleepFor(3000);
+        sleepFor(3000);
       }
       attempts++;
     }
     return successfullyConnected;
   }
 
-  public boolean checkHealth(boolean reconnect) {
-    return checkHealth(esClient(), reconnect);
+  private HttpHost getHttpHost(ElasticsearchProperties elsConfig) {
+    final URI uri = elsConfig.getURI();
+    return new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+  }
+
+  private HttpAsyncClientBuilder setupAuthentication(
+      final HttpAsyncClientBuilder builder, ElasticsearchProperties elsConfig) {
+    if (StringUtils.isEmpty(elsConfig.getUsername())
+        || StringUtils.isEmpty(elsConfig.getPassword())) {
+      LOGGER.warn(
+          "Username and/or password for are empty. Basic authentication for elasticsearch is not used.");
+      return builder;
+    }
+    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    credentialsProvider.setCredentials(
+        AuthScope.ANY,
+        new UsernamePasswordCredentials(elsConfig.getUsername(), elsConfig.getPassword()));
+
+    builder.setDefaultCredentialsProvider(credentialsProvider);
+    return builder;
   }
 
   public static class CustomOffsetDateTimeSerializer extends JsonSerializer<OffsetDateTime> {
@@ -161,47 +188,7 @@ public class ElasticsearchConnector {
     @Override
     public Instant deserialize(JsonParser parser, DeserializationContext context)
         throws IOException {
-      return Instant.ofEpochMilli(Long.valueOf(parser.getText()));
+      return Instant.ofEpochMilli(Long.parseLong(parser.getText()));
     }
   }
-
-  //  public static class CustomLocalDateSerializer extends JsonSerializer<LocalDate> {
-  //
-  //    private DateTimeFormatter formatter;
-  //
-  //    public CustomLocalDateSerializer(DateTimeFormatter formatter) {
-  //      this.formatter = formatter;
-  //    }
-  //
-  //    @Override
-  //    public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider provider)
-  // throws IOException {
-  //      gen.writeString(value.format(this.formatter));
-  //    }
-  //  }
-  //
-  //  public static class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
-  //
-  //    private DateTimeFormatter formatter;
-  //
-  //    public CustomLocalDateDeserializer(DateTimeFormatter formatter) {
-  //      this.formatter = formatter;
-  //    }
-  //
-  //    @Override
-  //    public LocalDate deserialize(JsonParser parser, DeserializationContext context) throws
-  // IOException {
-  //
-  //      LocalDate parsedDate;
-  //      try {
-  //        parsedDate = LocalDate.parse(parser.getText(), this.formatter);
-  //      } catch(DateTimeParseException exception) {
-  //        //
-  //        parsedDate = LocalDate
-  //          .parse(parser.getText(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-  //      }
-  //      return parsedDate;
-  //    }
-  //  }
-
 }
--- common/src/main/java/io/zeebe/tasklist/property/ElasticsearchProperties.java
+++ common/src/main/java/io/zeebe/tasklist/property/ElasticsearchProperties.java
@@ -5,6 +5,10 @@
  */
 package io.zeebe.tasklist.property;
 
+import io.zeebe.tasklist.exceptions.TasklistRuntimeException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 public class ElasticsearchProperties {
 
   public static final String DATE_FORMAT_DEFAULT = "yyyy-MM-dd'T'HH:mm:ss.SSSXX";
@@ -16,6 +20,10 @@ public class ElasticsearchProperties {
   private String host = "localhost";
 
   private int port = 9200;
+
+  private String url;
+  private String username;
+  private String password;
 
   private String dateFormat = DATE_FORMAT_DEFAULT;
 
@@ -79,5 +87,50 @@ public class ElasticsearchProperties {
 
   public void setCreateSchema(boolean createSchema) {
     this.createSchema = createSchema;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+    setHost(getURI().getHost());
+    setPort(getURI().getPort());
+  }
+
+  public URI getURI() {
+    if (url == null) {
+      try {
+        return new URI(String.format("http://%s:%d", getHost(), getPort()));
+      } catch (URISyntaxException e) {
+        throw new TasklistRuntimeException(
+            String.format(
+                "Failed to build URI from host %s and port %d url ", getHost(), getPort()),
+            e);
+      }
+    } else {
+      try {
+        return new URI(url);
+      } catch (final URISyntaxException e) {
+        throw new TasklistRuntimeException("Failed to parse url " + url, e);
+      }
+    }
   }
 }
