46b772113e32cde9aa24351d22a6a1c82b55f9f0
OmranAbazid <omran.1994@gmail.com>
2020-09-30 13:06:02 +0200

    improve(Report): add formating to legends for distribute by date variable
    related to OPT-4325

--- client/src/modules/components/ReportRenderer/HyperReportRenderer.js
+++ client/src/modules/components/ReportRenderer/HyperReportRenderer.js
@@ -7,8 +7,11 @@
 import React from 'react';
 
 import {ColorPicker} from 'components';
+import {formatters} from 'services';
 
 import CombinedReportRenderer from './CombinedReportRenderer';
+
+const {formatReportResult} = formatters;
 
 export default function HyperReportRenderer({report, ...rest}) {
   const convertedReport = {
@@ -26,7 +29,7 @@ export default function HyperReportRenderer({report, ...rest}) {
 
   const newResultData = {};
 
-  report.result.data[0].value.forEach(({key, label}) => {
+  formatResult(report.data, report.result.data[0].value).forEach(({key, label}) => {
     newResultData[key] = {
       combined: false,
       id: key,
@@ -58,4 +61,26 @@ function getVisualization(visualization) {
     return visualization;
   }
   return 'bar';
+}
+
+function formatResult(data, result) {
+  const {
+    configuration: {distributedBy, distributeByDateVariableUnit},
+  } = data;
+
+  if (distributedBy.type === 'variable' && distributedBy.value.type === 'Date') {
+    return formatReportResult(
+      {
+        ...data,
+        groupBy: distributedBy,
+        configuration: {
+          ...data.configuration,
+          groupByDateVariableUnit: distributeByDateVariableUnit,
+        },
+      },
+      result
+    );
+  }
+
+  return result;
 }
