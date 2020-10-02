feb39cdcabc2bde1dd3decf63138c947a2a42a85
Svetlana Dorokhova <svetlana.dorokhova@camunda.com>
2020-10-02 13:40:59 +0200

    OPE-1016 Use StackdriverLayout from zeebe-util (#772)
    * chore(logging): use StackdriverLayout from zeebe-util
    
    * remove our StackdriverJSONLayout.java
    * use StackdriverJSONLayout from zeebe-util instead
    * adjust log4j2.xml in distro
    
    * chore(pom): cleanup dependency version in poms

--- common/src/test/java/org/camunda/operate/util/StackdriverJSONLayoutTest.java
+++ common/src/test/java/org/camunda/operate/util/StackdriverJSONLayoutTest.java
@@ -7,63 +7,53 @@ package org.camunda.operate.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.StringLayout;
-import org.apache.logging.log4j.core.appender.WriterAppender;
-import org.apache.logging.log4j.core.config.LoggerConfig;
-import org.junit.Before;
-import org.junit.Test;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.junit.LoggerContextRule;
+import org.apache.logging.log4j.test.appender.ListAppender;
+import org.junit.Rule;
+import org.junit.Test;
 
+/**
+ * This test ensures that StackdriverLayout defined in zeebe-util library is working as expected.
+ * If the test is failing, then probably smth was changed on Zeebe side and we need to make adjustmets accordingly,
+ * including /distro/config/log4j2.xml file and docs on logging.
+ */
 public class StackdriverJSONLayoutTest {
 
-  private Logger logger = LogManager.getLogger();
-  private LoggerContext context = (LoggerContext) LogManager.getContext(false);
-  private Writer logOutput;
+  public static final String STACKDRIVER_APPENDER_NAME = "Stackdriver";
+
+  @Rule
+  public LoggerContextRule loggerRule = new LoggerContextRule("log4j2.xml");
+
   private ObjectReader jsonReader = new ObjectMapper().reader();
 
-  @Before
-  public void before() {
-    logOutput = new StringWriter();
-  }
-
   @Test
-  public void testJSONOutput() throws IOException {
-    // Given
-    createTestAppenderWithLayout(new StackdriverJSONLayout());
-    // when
-    logger.error("Should appear as JSON formatted ouput");
-    // then
-    Map<String, String> jsonMap = logOutputToJSONMap();
-    assertThat(jsonMap).containsKeys("logger", "message", "severity", "thread", "timestampNanos", "timestampSeconds");
-    assertThat(jsonMap).containsEntry("message", "Should appear as JSON formatted ouput");
-    assertThat(jsonMap).containsEntry("logger", logger.getName());
-  }
+  public void testLayout() throws Exception {
+    //having Stackdriver appender activated
+    final LoggerContext ctx = loggerRule.getLoggerContext();
+    final ListAppender app = loggerRule.getListAppender(STACKDRIVER_APPENDER_NAME);
+    ctx.getRootLogger().addAppender(app);
+    final Logger logger = loggerRule.getLogger();
 
-  private Map<String, String> logOutputToJSONMap() throws IOException {
-    return jsonReader.withValueToUpdate(new HashMap<String, String>()).readValue(logOutput.toString());
-  }
+    //when
+    logger.warn("Test message");
 
-  private void createTestAppenderWithLayout(StringLayout layout) {
-    Appender appender = WriterAppender.createAppender(layout, null, logOutput, "test", false, false);
-    LoggerConfig loggerConfig = context.getConfiguration().getRootLogger();
-    loggerConfig.addAppender(appender, Level.ALL, null);
-    context.updateLoggers();
-    appender.start();
+    //then
+    final List<String> messages = app.getMessages();
+    assertThat(messages).hasSize(1);
+    final Map<String, Object> logMap = jsonReader.withValueToUpdate(new HashMap<String, String>())
+        .readValue(messages.get(0));
+    assertThat(logMap.get("serviceContext")).isNotNull();
+    assertThat(((Map<String, String>) logMap.get("serviceContext")).get("service"))
+        .isEqualTo("customService");
+    assertThat(((Map<String, String>) logMap.get("serviceContext")).get("version"))
+        .isEqualTo("customVersion");
   }
 
 }
