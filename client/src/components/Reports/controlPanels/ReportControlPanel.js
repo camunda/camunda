4e0c53b610ff50ce17dedbb2210e04634bded581
OmranAbazid <omran.1994@gmail.com>
2020-10-09 08:38:28 +0200

    fix(Report): reset canceled node filter if defintion changes
    related to OPT-4405

--- client/src/components/Reports/controlPanels/ReportControlPanel.js
+++ client/src/components/Reports/controlPanels/ReportControlPanel.js
@@ -127,6 +127,7 @@ export default withErrorHandling(
               ![
                 'executedFlowNodes',
                 'executingFlowNodes',
+                'canceledFlowNodes',
                 'variable',
                 'assignee',
                 'candidateGroup',
