a3e7f97a5d5803bc37ac85841854bf4297746681
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2020-10-14 14:23:23 +0200

    chore(gateway): introduce error details

--- gateway/src/test/java/io/zeebe/gateway/grpc/GrpcErrorMapperTest.java
+++ gateway/src/test/java/io/zeebe/gateway/grpc/GrpcErrorMapperTest.java
@@ -9,9 +9,13 @@ package io.zeebe.gateway.grpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.rpc.Status;
 import io.grpc.Status.Code;
 import io.grpc.StatusRuntimeException;
+import io.grpc.protobuf.StatusProto;
 import io.zeebe.gateway.cmd.BrokerErrorException;
+import io.zeebe.gateway.impl.broker.RequestRetriesExhaustedException;
 import io.zeebe.gateway.impl.broker.response.BrokerError;
 import io.zeebe.protocol.record.ErrorCode;
 import io.zeebe.util.logging.RecordingAppender;
@@ -25,13 +29,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.slf4j.helpers.NOPLogger;
 
 @Execution(ExecutionMode.CONCURRENT)
 final class GrpcErrorMapperTest {
   private final RecordingAppender recorder = new RecordingAppender();
   private final Logger log = (Logger) LogManager.getLogger(GrpcErrorMapperTest.class);
-  private final GrpcErrorMapper errorMapper =
-      new GrpcErrorMapper(new Log4jLogger(log, log.getName()));
+  private final Log4jLogger logger = new Log4jLogger(log, log.getName());
+  private final GrpcErrorMapper errorMapper = new GrpcErrorMapper();
 
   @BeforeEach
   void setUp() {
@@ -54,12 +59,42 @@ final class GrpcErrorMapperTest {
 
     // when
     log.setLevel(Level.TRACE);
-    final StatusRuntimeException statusException = errorMapper.mapError(exception);
+    final StatusRuntimeException statusException = errorMapper.mapError(exception, logger);
 
     // then
     assertThat(statusException.getStatus().getCode()).isEqualTo(Code.RESOURCE_EXHAUSTED);
     assertThat(recorder.getAppendedEvents()).hasSize(1);
     final LogEvent event = recorder.getAppendedEvents().get(0);
     assertThat(event.getLevel()).isEqualTo(Level.TRACE);
+  }
+
+  @Test
+  void shouldAddDetailsForRequestRetriesExhaustedException() throws InvalidProtocolBufferException {
+    // given
+    final RequestRetriesExhaustedException exception = new RequestRetriesExhaustedException();
+    final BrokerError brokerError =
+        new BrokerError(ErrorCode.PARTITION_LEADER_MISMATCH, "Wrong partition");
+    final BrokerErrorException detailException = new BrokerErrorException(brokerError);
+    final Status expectedDetail =
+        StatusProto.fromThrowable(errorMapper.mapError(detailException, NOPLogger.NOP_LOGGER));
+
+    // when
+    exception.addSuppressed(detailException);
+    log.setLevel(Level.TRACE);
+    final StatusRuntimeException statusException = errorMapper.mapError(exception, logger);
+    final Status status = StatusProto.fromThrowable(statusException);
+
+    // then
+    assertThat(statusException.getStatus().getCode()).isEqualTo(Code.RESOURCE_EXHAUSTED);
+    assertThat(recorder.getAppendedEvents()).hasSize(2);
+    assertThat(recorder.getAppendedEvents().get(0).getLevel())
+        .isEqualTo(Level.ERROR); // partition leader mismatch
+    assertThat(recorder.getAppendedEvents().get(1).getLevel())
+        .isEqualTo(Level.TRACE); // resource exhausted
+    assertThat(status.getDetailsCount()).isEqualTo(1);
+
+    final Status statusDetail = status.getDetails(0).unpack(Status.class);
+    assertThat(statusDetail.getCode()).isEqualTo(expectedDetail.getCode());
+    assertThat(statusDetail.getMessage()).isEqualTo(expectedDetail.getMessage());
   }
 }
