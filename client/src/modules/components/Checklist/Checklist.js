3eb6b94d35689ccbda8dded9f923bf0fd421901f
OmranAbazid <omran.1994@gmail.com>
2020-10-15 09:30:04 +0200

    improve(checklist): add icons to search button
    related to OPT-4391

--- client/src/modules/components/Checklist/Checklist.js
+++ client/src/modules/components/Checklist/Checklist.js
@@ -7,7 +7,7 @@
 import React, {useState} from 'react';
 import classnames from 'classnames';
 
-import {Input, LabeledInput, Tag, LoadingIndicator} from 'components';
+import {Input, LabeledInput, Tag, LoadingIndicator, Icon} from 'components';
 import {t} from 'translation';
 
 import './Checklist.scss';
@@ -83,22 +83,28 @@ export default function Checklist({
               onChange={({target: {checked}}) => (checked ? selectAll() : deselectAll())}
             />
           )}
-          <Input
-            value={query}
-            className="searchInput"
-            placeholder={labels.search}
-            type="text"
-            onChange={(evt) => {
-              setQuery(evt.target.value);
-              onSearch(evt.target.value);
-            }}
-          />
+          <div className="searchInputContainer">
+            <Input
+              value={query}
+              className="searchInput"
+              placeholder={labels.search}
+              type="text"
+              onChange={(evt) => {
+                setQuery(evt.target.value);
+                onSearch(evt.target.value);
+              }}
+              onClear={() => {
+                setQuery('');
+                onSearch('');
+              }}
+            />
+            <Icon className="searchIcon" type="search" size="20" />
+          </div>
           {selectedItems.length > 0 && (
             <Tag onRemove={deselectAll}>{selectedItems.length} Selected</Tag>
           )}
         </div>
       )}
-
       <div className="itemsList">
         {loading && <LoadingIndicator />}
         {!loading && (
