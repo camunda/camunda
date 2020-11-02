a73d82c411379dd0c3f131c6a95f156479d13205
Alexey Vinogradov <vinogradov.a.i.93@gmail.com>
2020-11-02 15:29:59 +0700

    chore(broker): start an embedded gateway after a topology manager

--- broker/src/main/java/io/zeebe/broker/Broker.java
+++ broker/src/main/java/io/zeebe/broker/Broker.java
@@ -211,6 +211,7 @@ public final class Broker implements AutoCloseable {
     startContext.addStep("subscription api", () -> subscriptionAPIStep(localBroker));
 
     startContext.addStep("cluster services", () -> atomix.start().join());
+    startContext.addStep("topology manager", () -> topologyManagerStep(clusterCfg, localBroker));
     if (brokerCfg.getGateway().isEnable()) {
       startContext.addStep(
           "embedded gateway",
@@ -219,7 +220,6 @@ public final class Broker implements AutoCloseable {
             return embeddedGatewayService;
           });
     }
-    startContext.addStep("topology manager", () -> topologyManagerStep(clusterCfg, localBroker));
     startContext.addStep("monitoring services", () -> monitoringServerStep(localBroker));
     startContext.addStep("disk space monitor", () -> diskSpaceMonitorStep(brokerCfg.getData()));
     startContext.addStep(
