b9e330c5296d47a3d5653e5b9d0607ab5f94e7d1
Nico korthout <nico.korthout@camunda.com>
2020-11-12 15:36:55 +0100

    chore(ci): split IT prepare-offline from verify
    (cherry picked from commit e499ce74508a392b67abaf9c0f01bea5dc8afebd)

--- .ci/scripts/distribution/it-java.sh
+++ .ci/scripts/distribution/it-java.sh
@@ -5,7 +5,9 @@ export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
 tmpfile=$(mktemp)
 
-mvn -B --fail-never -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -pl qa/integration-tests -pl update-tests -DtestMavenId=2 -Dsurefire.rerunFailingTestsCount=7 | tee ${tmpfile}
+mvn -B -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} initialize -Pprepare-offline -pl qa/integration-tests -pl update-tests
+
+mvn -o -B --fail-never -T$LIMITS_CPU -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests -pl qa/integration-tests -pl update-tests -DtestMavenId=2 -Dsurefire.rerunFailingTestsCount=7 | tee ${tmpfile}
 
 status=${PIPESTATUS[0]}
 
