781f506c255796425c5ad3efc7a55736ec297e31
Christopher Zell <zelldon91@googlemail.com>
2020-11-19 10:16:39 +0100

    chore(engine): do not write job activate record
     * job activated record is no longer written to the log. It is already
       present in JobBatchRecord
     * Job record is also not copied multiple times just wrapped without
       variables for state updates

--- protocol-impl/src/main/java/io/zeebe/protocol/impl/record/value/job/JobRecord.java
+++ protocol-impl/src/main/java/io/zeebe/protocol/impl/record/value/job/JobRecord.java
@@ -73,14 +73,13 @@ public final class JobRecord extends UnifiedRecordValue implements JobRecordValu
         .declareProperty(elementInstanceKeyProp);
   }
 
-  public void wrap(final JobRecord record) {
+  public void wrapWithoutVariables(final JobRecord record) {
     deadlineProp.setValue(record.getDeadline());
     workerProp.setValue(record.getWorkerBuffer());
     retriesProp.setValue(record.getRetries());
     typeProp.setValue(record.getTypeBuffer());
     final DirectBuffer customHeaders = record.getCustomHeadersBuffer();
     customHeadersProp.setValue(customHeaders, 0, customHeaders.capacity());
-    variableProp.setValue(record.getVariablesBuffer());
     errorMessageProp.setValue(record.getErrorMessageBuffer());
     errorCodeProp.setValue(record.getErrorCodeBuffer());
     bpmnProcessIdProp.setValue(record.getBpmnProcessIdBuffer());
