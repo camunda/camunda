8076ea3d1984fcd1b24b0877658c99c8590ee4c8
Josh Windels <joshuawindels@gmail.com>
2020-11-24 11:47:08 +0100

    tests(performance): add query performance tests for event fetching
    relates to OPT-4561

--- qa/query-performance-tests/src/test/java/org/camunda/optimize/test/query/performance/OptimizeEventsQueryPerformanceTest.java
+++ qa/query-performance-tests/src/test/java/org/camunda/optimize/test/query/performance/OptimizeEventsQueryPerformanceTest.java
@@ -7,6 +7,9 @@ package org.camunda.optimize.test.query.performance;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
+import org.camunda.optimize.dto.optimize.query.event.DeletableEventDto;
+import org.camunda.optimize.dto.optimize.query.event.EventSearchRequestDto;
+import org.camunda.optimize.dto.optimize.query.event.process.EventDto;
 import org.camunda.optimize.dto.optimize.query.event.process.EventScopeType;
 import org.camunda.optimize.dto.optimize.query.event.process.EventSourceEntryDto;
 import org.camunda.optimize.dto.optimize.query.event.process.EventSourceType;
@@ -15,7 +18,10 @@ import org.camunda.optimize.dto.optimize.query.event.sequence.EventCountRequestD
 import org.camunda.optimize.dto.optimize.query.event.sequence.EventCountResponseDto;
 import org.camunda.optimize.dto.optimize.query.event.sequence.EventSequenceCountDto;
 import org.camunda.optimize.dto.optimize.query.sorting.SortOrder;
+import org.camunda.optimize.dto.optimize.rest.pagination.PaginationRequestDto;
 import org.camunda.optimize.dto.optimize.rest.sorting.EventCountSorter;
+import org.camunda.optimize.dto.optimize.rest.sorting.SortRequestDto;
+import org.camunda.optimize.service.es.schema.index.events.EventIndex;
 import org.camunda.optimize.service.es.schema.index.events.EventSequenceCountIndex;
 import org.camunda.optimize.service.util.IdGenerator;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,6 +30,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.ws.rs.core.Response;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,6 +40,8 @@ import java.util.stream.Stream;
 import static org.camunda.optimize.dto.optimize.query.event.sequence.EventCountResponseDto.Fields.eventName;
 import static org.camunda.optimize.dto.optimize.query.event.sequence.EventCountResponseDto.Fields.group;
 import static org.camunda.optimize.dto.optimize.query.event.sequence.EventCountResponseDto.Fields.source;
+import static org.camunda.optimize.dto.optimize.query.sorting.SortOrder.ASC;
+import static org.camunda.optimize.dto.optimize.query.sorting.SortOrder.DESC;
 import static org.camunda.optimize.upgrade.es.ElasticsearchConstants.EXTERNAL_EVENTS_INDEX_SUFFIX;
 
 @Slf4j
@@ -45,15 +54,16 @@ public class OptimizeEventsQueryPerformanceTest extends AbstractQueryPerformance
         embeddedOptimizeExtension.getOptimizeElasticClient(),
         new EventSequenceCountIndex(EXTERNAL_EVENTS_INDEX_SUFFIX)
       );
+    embeddedOptimizeExtension.getConfigurationService().getEventBasedProcessAccessUserIds().add(DEFAULT_USER);
   }
 
   @ParameterizedTest
   @MethodSource("eventCountSorterAndSearchTerm")
   public void testQueryPerformance_getEventCounts(EventCountSorter eventCountSorter, String searchTerm) {
     // given
-    final int numberOfDifferentEvents = getNumberOfEvents();
-
+    final int numberOfDifferentEvents = getNumberOfEventsToIngest();
     addEventSequenceCountsToOptimize(numberOfDifferentEvents);
+
     EventCountRequestDto countRequest = EventCountRequestDto.builder()
       .eventSources(Collections.singletonList(
         EventSourceEntryDto.builder()
@@ -68,6 +78,28 @@ public class OptimizeEventsQueryPerformanceTest extends AbstractQueryPerformance
       () -> embeddedOptimizeExtension.getRequestExecutor()
         .buildPostEventCountRequest(eventCountSorter, searchTerm, countRequest)
         .executeAndReturnList(EventCountResponseDto.class, Response.Status.OK.getStatusCode())
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("eventListSearchParameters")
+  public void testQueryPerformance_getEvents(final String searchTerm, final SortRequestDto sortRequestDto) {
+    // given
+    final int numberOfDifferentEvents = getNumberOfEventsToIngest();
+    addEventsToOptimize(numberOfDifferentEvents);
+
+    EventSearchRequestDto searchRequestDto = new EventSearchRequestDto(
+      searchTerm,
+      sortRequestDto,
+      new PaginationRequestDto(getNumberOfEvents(), 0)
+    );
+
+    // when & then
+    assertThatListEndpointMaxAllowedQueryTimeIsMet(
+      getNumberOfEvents(),
+      () -> embeddedOptimizeExtension.getRequestExecutor()
+        .buildGetEventListRequest(searchRequestDto)
+        .executeAndGetPage(DeletableEventDto.class, Response.Status.OK.getStatusCode()).getResults()
     );
   }
 
@@ -90,20 +122,52 @@ public class OptimizeEventsQueryPerformanceTest extends AbstractQueryPerformance
     elasticSearchIntegrationTestExtension.refreshAllOptimizeIndices();
   }
 
+  private void addEventsToOptimize(final int numberOfDifferentEvents) {
+    final Map<String, Object> eventsById = IntStream.range(0, numberOfDifferentEvents)
+      .mapToObj(index -> EventDto.builder()
+        .timestamp(Instant.now().toEpochMilli())
+        .ingestionTimestamp(Instant.now().toEpochMilli())
+        .traceId("traceId_" + index % 4)
+        .eventName("eventName_" + index % 4)
+        .group("group_" + index % 5)
+        .source("source_" + index % 6)
+        .id(IdGenerator.getNextId())
+        .build())
+      .collect(Collectors.toMap(EventDto::getId, event -> event));
+    elasticSearchIntegrationTestExtension.addEntriesToElasticsearch(
+      new EventIndex().getIndexName(), eventsById
+    );
+    elasticSearchIntegrationTestExtension.refreshAllOptimizeIndices();
+  }
+
   private static Stream<Arguments> eventCountSorterAndSearchTerm() {
     return Stream.of(
       Arguments.of(null, null),
       Arguments.of(null, "a"),
-      Arguments.of(new EventCountSorter(eventName, SortOrder.ASC), "a"),
-      Arguments.of(new EventCountSorter(eventName, SortOrder.ASC), null),
+      Arguments.of(new EventCountSorter(eventName, ASC), "a"),
+      Arguments.of(new EventCountSorter(eventName, ASC), null),
       Arguments.of(new EventCountSorter(eventName, SortOrder.DESC), null),
-      Arguments.of(new EventCountSorter(group, SortOrder.ASC), "a"),
-      Arguments.of(new EventCountSorter(group, SortOrder.ASC), null),
+      Arguments.of(new EventCountSorter(group, ASC), "a"),
+      Arguments.of(new EventCountSorter(group, ASC), null),
       Arguments.of(new EventCountSorter(group, SortOrder.DESC), null),
-      Arguments.of(new EventCountSorter(source, SortOrder.ASC), "a"),
-      Arguments.of(new EventCountSorter(source, SortOrder.ASC), null),
+      Arguments.of(new EventCountSorter(source, ASC), "a"),
+      Arguments.of(new EventCountSorter(source, ASC), null),
       Arguments.of(new EventCountSorter(source, SortOrder.DESC), null)
     );
+  }
+
+  private static Stream<Arguments> eventListSearchParameters() {
+    return EventSearchRequestDto.sortableFields.stream()
+      .flatMap(sortableField -> Stream.of(
+        new SortRequestDto(sortableField, ASC),
+        new SortRequestDto(sortableField, DESC)
+      ).flatMap(sort -> Stream.of(
+        Arguments.of(null, sort),
+        Arguments.of("eventName_1", sort),
+        Arguments.of("traceId_1", sort),
+        Arguments.of("group_1", sort),
+        Arguments.of("source_1", sort)
+      )));
   }
 
 }
