cd402d3eb75bf094ad0c464adaddb0790ee34898
Josh Windels <joshuawindels@gmail.com>
2020-11-26 12:39:16 +0100

    chore(ci): use stable nodepool for java compatibility job

--- .ci/pipelines/java_compatibility.groovy
+++ .ci/pipelines/java_compatibility.groovy
@@ -4,7 +4,7 @@
 @Library('camunda-ci') _
 
 // general properties for CI execution
-def static NODE_POOL() { return "agents-n1-standard-32-netssd-preempt" }
+def static NODE_POOL() { return "agents-n1-standard-32-netssd-stable" }
 def static OPENJDK_MAVEN_DOCKER_IMAGE(String javaVersion) { return "maven:3.6.3-jdk-${javaVersion}" }
 def static CAMBPM_DOCKER_IMAGE(String cambpmVersion) { return "registry.camunda.cloud/cambpm-ee/camunda-bpm-platform-ee:${cambpmVersion}" }
 def static ELASTICSEARCH_DOCKER_IMAGE(String esVersion) { return "docker.elastic.co/elasticsearch/elasticsearch-oss:${esVersion}" }
