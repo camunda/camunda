18a69b911608b167348142ce04ffe997b3219065
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2020-11-27 15:10:42 +0100

    chore(parent): enable junit parallel tests
    - junit5 parallel tests are enabled with profile parallel-tests
    - default configuration is based on number of cores divided by 2
    - parallel tests are opt in based on annotations
    - junit thread count can be overriden via the property junitThreadCount

--- .ci/scripts/distribution/it-java.sh
+++ .ci/scripts/distribution/it-java.sh
@@ -4,6 +4,7 @@
 LIMITS_CPU=${LIMITS_CPU:-$(getconf _NPROCESSORS_ONLN)}
 MAVEN_PARALLELISM=${MAVEN_PARALLELISM:-$LIMITS_CPU}
 SUREFIRE_FORK_COUNT=${SUREFIRE_FORK_COUNT:-}
+JUNIT_THREAD_COUNT=${JUNIT_THREAD_COUNT:-}
 MAVEN_PROPERTIES=(
   -DtestMavenId=2
   -Dsurefire.rerunFailingTestsCount=7
@@ -14,6 +15,10 @@ if [ ! -z "$SUREFIRE_FORK_COUNT" ]; then
   MAVEN_PROPERTIES+=("-DforkCount=$SUREFIRE_FORK_COUNT")
   # if we know the fork count, we can limit the max heap for each fork to ensure we're not OOM killed
   JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS} -XX:MaxRAMPercentage=$((100 / (MAVEN_PARALLELISM * $SUREFIRE_FORK_COUNT)))"
+fi
+
+if [ ! -z "$JUNIT_THREAD_COUNT" ]; then
+  MAVEN_PROPERTIES+=("-DjunitThreadCount=$JUNIT_THREAD_COUNT")
 fi
 
 # make sure to specify the profiles used in the verify goal when running preparing to go offline, as
