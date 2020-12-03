59ee1ccbc0770cc5bbbafad5fe3df5b359ab936b
SvetlanaDorokhova <sveta_d@list.ru>
2020-12-03 09:01:07 +0100

    chore(backend): upgrade Zeebe to 0.26.0-alpha2

--- .ci/pipelines/test_e2e.groovy
+++ .ci/pipelines/test_e2e.groovy
@@ -133,7 +133,7 @@ spec:
           cpu: 2
           memory: 4Gi
     - name: zeebe
-      image: camunda/zeebe:0.26.0-alpha1
+      image: camunda/zeebe:0.26.0-alpha2
       #imagePullPolicy: Always   #this must be uncommented when snapshot is used
       env:
       volumeMounts:
