96c4080227b429647b10813dc954bc119267e182
pihme <pihme@users.noreply.github.com>
2020-12-21 11:56:22 +0100

    chore(samples): adjust examples to support connecting to cloud

--- samples/src/main/java/io/zeebe/example/data/HandleVariablesAsPojo.java
+++ samples/src/main/java/io/zeebe/example/data/HandleVariablesAsPojo.java
@@ -14,14 +14,42 @@ import io.zeebe.client.api.worker.JobClient;
 import io.zeebe.client.api.worker.JobHandler;
 import java.util.Scanner;
 
+/**
+ * Sample application that connects to a cluster on Camunda Cloud, or a locally deployed cluster.
+ *
+ * <p>When connecting to a cluster in Camunda Cloud, this application assumes that the following
+ * environment variables are set:
+ *
+ * <ul>
+ *   <li>ZEEBE_ADDRESS
+ *   <li>ZEEBE_CLIENT_ID
+ *   <li>ZEEBE_CLIENT_SECRET
+ *   <li>ZEEBE_AUTHORIZATION_SERVER_URL
+ * </ul>
+ *
+ * <p><strong>Hint:</strong> When you create client credentials in Camunda Cloud you have the option
+ * to download a file with above lines filled out for you.
+ *
+ * <p>When {@code ZEEBE_ADDRESS} is not set, it connects to a broker running on localhost with
+ * default ports
+ */
 public final class HandleVariablesAsPojo {
   public static void main(final String[] args) {
-    final String broker = "127.0.0.1:26500";
+    final String defaultAddress = "localhost:26500";
+    final String envVarAddress = System.getenv("ZEEBE_ADDRESS");
 
-    final ZeebeClientBuilder builder =
-        ZeebeClient.newClientBuilder().gatewayAddress(broker).usePlaintext();
+    final ZeebeClientBuilder clientBuilder;
+    if (envVarAddress != null) {
+      /* Connect to Camunda Cloud Cluster, assumes that credentials are set in environment variables.
+       * See JavaDoc on class level for details
+       */
+      clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(envVarAddress);
+    } else {
+      // connect to local deployment; assumes that authentication is disabled
+      clientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(defaultAddress).usePlaintext();
+    }
 
-    try (final ZeebeClient client = builder.build()) {
+    try (final ZeebeClient client = clientBuilder.build()) {
       final Order order = new Order();
       order.setOrderId(31243);
 
