e9ddc571f534076185f243d972b4441083847ace
Lars Lange <lars.lange@camunda.com>
2021-02-01 14:08:17 +0100

    chore(ci): remove cloud org package token (#715)

--- .ci/pipelines/release.groovy
+++ .ci/pipelines/release.groovy
@@ -153,9 +153,6 @@ pipeline {
       }
     }
     stage('Maven Release') {
-      environment {
-        GITHUB_CAMUNDA_CLOUD_PACKAGES_TOKEN = credentials('github-camunda-cloud-packages-token')
-      }
       steps {
         container('maven') {
           sshagent(['camunda-jenkins-github-ssh']) {
