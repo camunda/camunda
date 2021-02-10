70a78592b4970c7abeae5ac9e4176c0ff5e24603
Christopher Zell <zelldon91@googlemail.com>
2021-02-10 11:53:21 +0100

    chore(zb-db): add DbInt

---
+++ zb-db/src/main/java/io/zeebe/db/impl/DbInt.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.0. You may not use this file
+ * except in compliance with the Zeebe Community License 1.0.
+ */
+package io.zeebe.db.impl;
+
+import static io.zeebe.db.impl.ZeebeDbConstants.ZB_DB_BYTE_ORDER;
+
+import io.zeebe.db.DbKey;
+import io.zeebe.db.DbValue;
+import org.agrona.DirectBuffer;
+import org.agrona.MutableDirectBuffer;
+
+public final class DbInt implements DbKey, DbValue {
+
+  private int intValue;
+
+  public void wrapInt(final int value) {
+    intValue = value;
+  }
+
+  @Override
+  public void wrap(final DirectBuffer buffer, final int offset, final int length) {
+    intValue = buffer.getInt(offset, ZB_DB_BYTE_ORDER);
+  }
+
+  @Override
+  public int getLength() {
+    return Integer.BYTES;
+  }
+
+  @Override
+  public void write(final MutableDirectBuffer buffer, final int offset) {
+    buffer.putInt(offset, intValue, ZB_DB_BYTE_ORDER);
+  }
+
+  public int getValue() {
+    return intValue;
+  }
+}
