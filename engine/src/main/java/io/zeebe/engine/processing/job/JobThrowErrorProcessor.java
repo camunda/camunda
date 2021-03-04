zhudebin/zeebe

chore(engine): refactor logic to trigger an event
This commit achieves an intermediate state in which the JobThrowErrorProcessor does not modify state.
It is an intermediate state, because the JobThrowErrorProcessor still writes the EVENT_OCCURRED record,
whilst the ZEP mandates that instead the element shall be terminated and the termination of the element
shall trigger the event element. A new issue was created to address this follow up work.

This commit also expands the random workflow generation to now also generate
boundary error events for service tasks.

There are two behavior changes that break with the goal of a simple refactoring:
- the trigger event method is now called with the jobKey as second parameter.
This was suggested and approved by Phiipp. According to his info the parameter
is not used later an, and this branch of code might not live very long (see above)
- there is now a magic ID to indicate that no catch event could be found.
This is to avoid looking for the catch event again and again. Currently, the lookup
still has to be repeated between processor and applier. After the follow up work is done,
most likely the applier won't need to do the lookup anymore.

engine/src/main/java/io/zeebe/engine/processing/job/JobThrowErrorProcessor.java
@@ -9,43 +9,61 @@ package io.zeebe.engine.processing.job;
 
 import static io.zeebe.util.buffer.BufferUtil.wrapString;
 
-import io.zeebe.engine.processing.common.ErrorEventHandler;
+import io.zeebe.engine.processing.deployment.model.element.ExecutableFlowElement;
+import io.zeebe.engine.processing.deployment.model.element.ExecutableStartEvent;
 import io.zeebe.engine.processing.streamprocessor.CommandProcessor;
 import io.zeebe.engine.processing.streamprocessor.TypedRecord;
 import io.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
+import io.zeebe.engine.state.KeyGenerator;
 import io.zeebe.engine.state.ZeebeState;
+import io.zeebe.engine.state.analyzers.CatchEventAnalyzer;
 import io.zeebe.engine.state.immutable.ElementInstanceState;
+import io.zeebe.engine.state.immutable.EventScopeInstanceState;
 import io.zeebe.engine.state.immutable.JobState;
+import io.zeebe.engine.state.instance.ElementInstance;
 import io.zeebe.protocol.impl.record.value.incident.IncidentRecord;
 import io.zeebe.protocol.impl.record.value.job.JobRecord;
+import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceRecord;
+import io.zeebe.protocol.record.RejectionType;
 import io.zeebe.protocol.record.intent.IncidentIntent;
 import io.zeebe.protocol.record.intent.Intent;
 import io.zeebe.protocol.record.intent.JobIntent;
+import io.zeebe.protocol.record.intent.WorkflowInstanceIntent;
+import io.zeebe.protocol.record.value.BpmnElementType;
 import io.zeebe.protocol.record.value.ErrorType;
 import org.agrona.DirectBuffer;
 
 public class JobThrowErrorProcessor implements CommandProcessor<JobRecord> {
+
+  /**
+   * Marker element ID. This ID is used to indicate that a given catch event could not be found. The
+   * marker ID is used to prevent repeated catch event lookups, which is an expensive operation
+   * (particularly when no catch event can be found)
+   */
+  public static final String NO_CATCH_EVENT_FOUND = "NO_CATCH_EVENT_FOUND";
+
   private final IncidentRecord incidentEvent = new IncidentRecord();
+  private final WorkflowInstanceRecord eventOccurredRecord = new WorkflowInstanceRecord();
 
   private final JobState jobState;
   private final ElementInstanceState elementInstanceState;
   private final DefaultJobCommandPreconditionGuard<JobRecord> defaultProcessor;
-  private final ErrorEventHandler errorEventHandler;
+  private final CatchEventAnalyzer structureAnalyzer;
+  private final KeyGenerator keyGenerator;
+  private final EventScopeInstanceState eventScopeInstanceState;
 
   public JobThrowErrorProcessor(final ZeebeState state) {
+    keyGenerator = state.getKeyGenerator();
     jobState = state.getJobState();
     elementInstanceState = state.getElementInstanceState();
-    errorEventHandler =
-        new ErrorEventHandler(
-            state.getWorkflowState(),
-            state.getElementInstanceState(),
-            state.getEventScopeInstanceState(),
-            state.getKeyGenerator());
+    eventScopeInstanceState = state.getEventScopeInstanceState();
 
     defaultProcessor =
         new DefaultJobCommandPreconditionGuard<>(
             "throw an error for", jobState, this::acceptCommand);
+
+    structureAnalyzer = new CatchEventAnalyzer(state.getWorkflowState(), elementInstanceState);
   }
 
   @Override
@@ -62,7 +80,31 @@ public class JobThrowErrorProcessor implements CommandProcessor<JobRecord> {
     job.setErrorCode(command.getValue().getErrorCodeBuffer());
     job.setErrorMessage(command.getValue().getErrorMessageBuffer());
 
-    commandControl.accept(JobIntent.ERROR_THROWN, job);
+    final var serviceTaskInstanceKey = job.getElementInstanceKey();
+    final var serviceTaskInstance = elementInstanceState.getInstance(serviceTaskInstanceKey);
+
+    final var errorCode = job.getErrorCodeBuffer();
+    final var foundCatchEvent = structureAnalyzer.findCatchEvent(errorCode, serviceTaskInstance);
+
+    if (foundCatchEvent == null) {
+      job.setElementId(NO_CATCH_EVENT_FOUND);
+
+      commandControl.accept(JobIntent.ERROR_THROWN, job);
+    } else if (!serviceTaskInstanceIsActive(serviceTaskInstance)) {
+      commandControl.reject(
+          RejectionType.INVALID_STATE, "Service task is not active " + serviceTaskInstance);
+    } else if (!eventScopeInstanceState.isAcceptingEvent(
+        foundCatchEvent.getElementInstance().getKey())) {
+      commandControl.reject(
+          RejectionType.INVALID_STATE,
+          "Event scope is not accepting events " + foundCatchEvent.getElementInstance());
+    } else {
+      commandControl.accept(JobIntent.ERROR_THROWN, job);
+    }
+  }
+
+  private boolean serviceTaskInstanceIsActive(final ElementInstance serviceTaskInstance) {
+    return serviceTaskInstance != null && serviceTaskInstance.isActive();
   }
 
   @Override
@@ -73,19 +115,47 @@ public class JobThrowErrorProcessor implements CommandProcessor<JobRecord> {
       final Intent intent,
       final JobRecord job) {
 
-    final var serviceTaskInstanceKey = job.getElementInstanceKey();
-    final var serviceTaskInstance = elementInstanceState.getInstance(serviceTaskInstanceKey);
+    final var serviceTaskInstanceKey = job.getElementId();
+    if (!NO_CATCH_EVENT_FOUND.equals(serviceTaskInstanceKey)) {
 
-    if (serviceTaskInstance != null && serviceTaskInstance.isActive()) {
-
+      final var serviceTaskInstance = elementInstanceState.getInstance(job.getElementInstanceKey());
       final var errorCode = job.getErrorCodeBuffer();
 
-      final boolean errorThrown =
-          errorEventHandler.throwErrorEvent(errorCode, serviceTaskInstance, stateWriter);
-      if (!errorThrown) {
-        raiseIncident(jobKey, job, commandWriter);
-      }
+      final var foundCatchEvent = structureAnalyzer.findCatchEvent(errorCode, serviceTaskInstance);
+      // TODO (#6472) send TERMINATE_ELEMENT command instead
+      writeEventOccurredRecord(stateWriter, foundCatchEvent);
+    } else {
+      raiseIncident(jobKey, job, commandWriter);
     }
+  }
+
+  private void writeEventOccurredRecord(
+      final StateWriter stateWriter, final CatchEventAnalyzer.CatchEventTuple foundCatchEvent) {
+    final var eventScopeInstance = foundCatchEvent.getElementInstance();
+    final var catchEvent = foundCatchEvent.getCatchEvent();
+
+    final long eventOccurredKey;
+
+    if (isEventSubprocess(catchEvent)) {
+
+      eventOccurredKey = keyGenerator.nextKey();
+      eventOccurredRecord.wrap(eventScopeInstance.getValue());
+      eventOccurredRecord
+          .setElementId(catchEvent.getId())
+          .setBpmnElementType(BpmnElementType.START_EVENT)
+          .setFlowScopeKey(eventScopeInstance.getKey());
+    } else {
+      eventOccurredKey = eventScopeInstance.getKey();
+      eventOccurredRecord.wrap(eventScopeInstance.getValue());
+    }
+
+    stateWriter.appendFollowUpEvent(
+        eventOccurredKey, WorkflowInstanceIntent.EVENT_OCCURRED, eventOccurredRecord);
+  }
+
+  private boolean isEventSubprocess(final ExecutableFlowElement catchEvent) {
+    return catchEvent instanceof ExecutableStartEvent
+        && ((ExecutableStartEvent) catchEvent).getEventSubProcess() != null;
   }
 
   private void raiseIncident(

engine/src/main/java/io/zeebe/engine/state/instance/DbEventScopeInstanceState.java
@@ -106,6 +106,14 @@ public final class DbEventScopeInstanceState implements MutableEventScopeInstanc
   }
 
   @Override
+  public boolean isAcceptingEvent(final long eventScopeKey) {
+    this.eventScopeKey.wrapLong(eventScopeKey);
+    final EventScopeInstance instance = eventScopeInstanceColumnFamily.get(this.eventScopeKey);
+
+    return isAcceptingEvents(instance);
+  }
+
+  @Override
   public boolean triggerEvent(
       final long eventScopeKey,
       final long eventKey,
@@ -114,7 +122,7 @@ public final class DbEventScopeInstanceState implements MutableEventScopeInstanc
     this.eventScopeKey.wrapLong(eventScopeKey);
     final EventScopeInstance instance = eventScopeInstanceColumnFamily.get(this.eventScopeKey);
 
-    if (instance != null && instance.isAccepting()) {
+    if (isAcceptingEvents(instance)) {
       if (instance.isInterrupting(elementId)) {
         instance.setAccepting(false);
         eventScopeInstanceColumnFamily.put(this.eventScopeKey, instance);
@@ -126,6 +134,10 @@ public final class DbEventScopeInstanceState implements MutableEventScopeInstanc
     } else {
       return false;
     }
+  }
+
+  private boolean isAcceptingEvents(final EventScopeInstance instance) {
+    return instance != null && instance.isAccepting();
   }
 
   private void createTrigger(

camunda/camunda

engine/src/main/java/io/zeebe/engine/processing/job/JobThrowErrorProcessor.java
@@ -9,43 +9,61 @@ package io.zeebe.engine.processing.job;
 
 import static io.zeebe.util.buffer.BufferUtil.wrapString;
 
-import io.zeebe.engine.processing.common.ErrorEventHandler;
+import io.zeebe.engine.processing.deployment.model.element.ExecutableFlowElement;
+import io.zeebe.engine.processing.deployment.model.element.ExecutableStartEvent;
 import io.zeebe.engine.processing.streamprocessor.CommandProcessor;
 import io.zeebe.engine.processing.streamprocessor.TypedRecord;
 import io.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
+import io.zeebe.engine.state.KeyGenerator;
 import io.zeebe.engine.state.ZeebeState;
+import io.zeebe.engine.state.analyzers.CatchEventAnalyzer;
 import io.zeebe.engine.state.immutable.ElementInstanceState;
+import io.zeebe.engine.state.immutable.EventScopeInstanceState;
 import io.zeebe.engine.state.immutable.JobState;
+import io.zeebe.engine.state.instance.ElementInstance;
 import io.zeebe.protocol.impl.record.value.incident.IncidentRecord;
 import io.zeebe.protocol.impl.record.value.job.JobRecord;
+import io.zeebe.protocol.impl.record.value.workflowinstance.WorkflowInstanceRecord;
+import io.zeebe.protocol.record.RejectionType;
 import io.zeebe.protocol.record.intent.IncidentIntent;
 import io.zeebe.protocol.record.intent.Intent;
 import io.zeebe.protocol.record.intent.JobIntent;
+import io.zeebe.protocol.record.intent.WorkflowInstanceIntent;
+import io.zeebe.protocol.record.value.BpmnElementType;
 import io.zeebe.protocol.record.value.ErrorType;
 import org.agrona.DirectBuffer;
 
 public class JobThrowErrorProcessor implements CommandProcessor<JobRecord> {
+
+  /**
+   * Marker element ID. This ID is used to indicate that a given catch event could not be found. The
+   * marker ID is used to prevent repeated catch event lookups, which is an expensive operation
+   * (particularly when no catch event can be found)
+   */
+  public static final String NO_CATCH_EVENT_FOUND = "NO_CATCH_EVENT_FOUND";
+
   private final IncidentRecord incidentEvent = new IncidentRecord();
+  private final WorkflowInstanceRecord eventOccurredRecord = new WorkflowInstanceRecord();
 
   private final JobState jobState;
   private final ElementInstanceState elementInstanceState;
   private final DefaultJobCommandPreconditionGuard<JobRecord> defaultProcessor;
-  private final ErrorEventHandler errorEventHandler;
+  private final CatchEventAnalyzer stateAnalyzer;
+  private final KeyGenerator keyGenerator;
+  private final EventScopeInstanceState eventScopeInstanceState;
 
   public JobThrowErrorProcessor(final ZeebeState state) {
+    keyGenerator = state.getKeyGenerator();
     jobState = state.getJobState();
     elementInstanceState = state.getElementInstanceState();
-    errorEventHandler =
-        new ErrorEventHandler(
-            state.getWorkflowState(),
-            state.getElementInstanceState(),
-            state.getEventScopeInstanceState(),
-            state.getKeyGenerator());
+    eventScopeInstanceState = state.getEventScopeInstanceState();
 
     defaultProcessor =
         new DefaultJobCommandPreconditionGuard<>(
             "throw an error for", jobState, this::acceptCommand);
+
+    stateAnalyzer = new CatchEventAnalyzer(state.getWorkflowState(), elementInstanceState);
   }
 
   @Override
@@ -62,7 +80,33 @@ public class JobThrowErrorProcessor implements CommandProcessor<JobRecord> {
     job.setErrorCode(command.getValue().getErrorCodeBuffer());
     job.setErrorMessage(command.getValue().getErrorMessageBuffer());
 
-    commandControl.accept(JobIntent.ERROR_THROWN, job);
+    final var serviceTaskInstanceKey = job.getElementInstanceKey();
+    final var serviceTaskInstance = elementInstanceState.getInstance(serviceTaskInstanceKey);
+
+    final var errorCode = job.getErrorCodeBuffer();
+    final var foundCatchEvent = stateAnalyzer.findCatchEvent(errorCode, serviceTaskInstance);
+
+    if (foundCatchEvent == null) {
+      job.setElementId(NO_CATCH_EVENT_FOUND);
+
+      commandControl.accept(JobIntent.ERROR_THROWN, job);
+    } else if (!serviceTaskInstanceIsActive(serviceTaskInstance)) {
+      commandControl.reject(
+          RejectionType.INVALID_STATE,
+          "Expected to find active service task, but was " + serviceTaskInstance);
+    } else if (!eventScopeInstanceState.isAcceptingEvent(
+        foundCatchEvent.getElementInstance().getKey())) {
+      commandControl.reject(
+          RejectionType.INVALID_STATE,
+          "Expected to find event scope that is accepting events, but was "
+              + foundCatchEvent.getElementInstance());
+    } else {
+      commandControl.accept(JobIntent.ERROR_THROWN, job);
+    }
+  }
+
+  private boolean serviceTaskInstanceIsActive(final ElementInstance serviceTaskInstance) {
+    return serviceTaskInstance != null && serviceTaskInstance.isActive();
   }
 
   @Override
@@ -73,19 +117,46 @@ public class JobThrowErrorProcessor implements CommandProcessor<JobRecord> {
       final Intent intent,
       final JobRecord job) {
 
-    final var serviceTaskInstanceKey = job.getElementInstanceKey();
-    final var serviceTaskInstance = elementInstanceState.getInstance(serviceTaskInstanceKey);
+    final var serviceTaskInstanceKey = job.getElementId();
 
-    if (serviceTaskInstance != null && serviceTaskInstance.isActive()) {
-
-      final var errorCode = job.getErrorCodeBuffer();
-
-      final boolean errorThrown =
-          errorEventHandler.throwErrorEvent(errorCode, serviceTaskInstance, stateWriter);
-      if (!errorThrown) {
-        raiseIncident(jobKey, job, commandWriter);
-      }
+    if (NO_CATCH_EVENT_FOUND.equals(serviceTaskInstanceKey)) {
+      raiseIncident(jobKey, job, commandWriter);
+      return;
     }
+
+    final var serviceTaskInstance = elementInstanceState.getInstance(job.getElementInstanceKey());
+    final var errorCode = job.getErrorCodeBuffer();
+
+    final var foundCatchEvent = stateAnalyzer.findCatchEvent(errorCode, serviceTaskInstance);
+    // TODO (#6472) send TERMINATE_ELEMENT command instead
+    writeEventOccurredRecord(stateWriter, foundCatchEvent);
+  }
+
+  private void writeEventOccurredRecord(
+      final StateWriter stateWriter, final CatchEventAnalyzer.CatchEventTuple foundCatchEvent) {
+    final var eventScopeInstance = foundCatchEvent.getElementInstance();
+    final var catchEvent = foundCatchEvent.getCatchEvent();
+
+    eventOccurredRecord.wrap(eventScopeInstance.getValue());
+
+    final long eventOccurredKey;
+    if (isEventSubprocess(catchEvent)) {
+      eventOccurredKey = keyGenerator.nextKey();
+      eventOccurredRecord
+          .setElementId(catchEvent.getId())
+          .setBpmnElementType(BpmnElementType.START_EVENT)
+          .setFlowScopeKey(eventScopeInstance.getKey());
+    } else {
+      eventOccurredKey = eventScopeInstance.getKey();
+    }
+
+    stateWriter.appendFollowUpEvent(
+        eventOccurredKey, WorkflowInstanceIntent.EVENT_OCCURRED, eventOccurredRecord);
+  }
+
+  private boolean isEventSubprocess(final ExecutableFlowElement catchEvent) {
+    return catchEvent instanceof ExecutableStartEvent
+        && ((ExecutableStartEvent) catchEvent).getEventSubProcess() != null;
   }
 
   private void raiseIncident(

engine/src/main/java/io/zeebe/engine/state/instance/DbEventScopeInstanceState.java
@@ -106,6 +106,14 @@ public final class DbEventScopeInstanceState implements MutableEventScopeInstanc
   }
 
   @Override
+  public boolean isAcceptingEvent(final long eventScopeKey) {
+    this.eventScopeKey.wrapLong(eventScopeKey);
+    final EventScopeInstance instance = eventScopeInstanceColumnFamily.get(this.eventScopeKey);
+
+    return isAcceptingEvent(instance);
+  }
+
+  @Override
   public boolean triggerEvent(
       final long eventScopeKey,
       final long eventKey,
@@ -114,7 +122,7 @@ public final class DbEventScopeInstanceState implements MutableEventScopeInstanc
     this.eventScopeKey.wrapLong(eventScopeKey);
     final EventScopeInstance instance = eventScopeInstanceColumnFamily.get(this.eventScopeKey);
 
-    if (instance != null && instance.isAccepting()) {
+    if (isAcceptingEvent(instance)) {
       if (instance.isInterrupting(elementId)) {
         instance.setAccepting(false);
         eventScopeInstanceColumnFamily.put(this.eventScopeKey, instance);
@@ -126,6 +134,10 @@ public final class DbEventScopeInstanceState implements MutableEventScopeInstanc
     } else {
       return false;
     }
+  }
+
+  private boolean isAcceptingEvent(final EventScopeInstance instance) {
+    return instance != null && instance.isAccepting();
   }
 
   private void createTrigger(