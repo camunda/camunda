2d60bd39a8b065a5c1e421a6efdd591fae2671ce
OmranAbazid <omran.1994@gmail.com>
2021-03-05 09:14:14 +0100

    improve(tooltips): reposition tooltips to stay visible
    related to OPT-4577

--- client/src/modules/components/Tooltip/Tooltip.js
+++ client/src/modules/components/Tooltip/Tooltip.js
@@ -4,7 +4,7 @@
  * You may not use this file except in compliance with the commercial license.
  */
 
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import classnames from 'classnames';
 
@@ -22,21 +22,53 @@ export default function Tooltip({
   const [timeout, setTimeout] = useState();
   const [hovering, setHovering] = useState(false);
   const [style, setStyle] = useState();
+  const [tooltipAlign, setTooltipAlign] = useState(align);
+  const [tooltipPosition, setTooltipPosition] = useState(position);
+
+  const hoverElement = useRef();
+  const tooltip = useRef();
+
+  useEffect(() => {
+    if (!tooltip.current || !hoverElement.current) {
+      return;
+    }
+
+    const body = document.fullscreenElement || document.body;
+    const tooltipBox = tooltip.current.getBoundingClientRect();
+    const hoverElementBox = hoverElement.current.getBoundingClientRect();
+
+    const widthToArrow = align === 'center' ? tooltipBox.width / 2 : tooltipBox.width;
+    const left = {
+      center: hoverElementBox.x + hoverElementBox.width / 2,
+      left: hoverElementBox.x,
+      right: hoverElementBox.x + hoverElementBox.width,
+    };
+
+    let tooltipAlign = align;
+    if (widthToArrow - left[align] > 0) {
+      tooltipAlign = 'left';
+    } else if (left[align] + widthToArrow > body.clientWidth) {
+      tooltipAlign = 'right';
+    }
+
+    let tooltipPosition = position;
+    if (position === 'bottom' && hoverElementBox.y + tooltipBox.height > body.clientHeight) {
+      tooltipPosition = 'top';
+    } else if (position === 'top' && hoverElementBox.y - tooltipBox.height < 0) {
+      tooltipPosition = 'bottom';
+    }
+
+    setTooltipPosition(tooltipPosition);
+    setTooltipAlign(tooltipAlign);
+    setStyle({
+      width: tooltipBox.width,
+      left: left[tooltipAlign] + 'px',
+      top: hoverElementBox[tooltipPosition] + 'px',
+    });
+  }, [align, hovering, position]);
 
   if (!content) {
     return children;
-  }
-
-  function calculatePosition(element) {
-    const box = element.getBoundingClientRect();
-
-    let left = box.x + box.width / 2;
-    if (align === 'left') {
-      left = box.x;
-    } else if (align === 'right') {
-      left = box.x + box.width;
-    }
-    setStyle({left: left + 'px', top: box[position] + 'px'});
   }
 
   let useLightTheme = theme === 'light';
@@ -51,13 +83,15 @@ export default function Tooltip({
           onMouseEnter: (evt) => {
             const {currentTarget} = evt;
             if (!overflowOnly || currentTarget.scrollWidth > currentTarget.clientWidth) {
-              calculatePosition(currentTarget);
+              hoverElement.current = currentTarget;
               setTimeout(window.setTimeout(() => setHovering(true), delay));
             }
             child.props.onMouseEnter && child.props.onMouseEnter(evt);
           },
           onMouseLeave: (evt) => {
+            hoverElement.current = null;
             setHovering(false);
+            setStyle();
             window.clearTimeout(timeout);
             child.props.onMouseLeave && child.props.onMouseLeave(evt);
           },
@@ -66,8 +100,14 @@ export default function Tooltip({
       {hovering &&
         ReactDOM.createPortal(
           <div
-            className={classnames('Tooltip', position, align, useLightTheme ? 'light' : 'dark')}
+            className={classnames(
+              'Tooltip',
+              tooltipPosition,
+              tooltipAlign,
+              useLightTheme ? 'light' : 'dark'
+            )}
             style={style}
+            ref={tooltip}
           >
             {content}
           </div>,
