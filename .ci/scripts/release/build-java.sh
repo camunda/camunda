6e57f2ebb4a35366367662257a3e2254b738a93d
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-04-09 09:52:44 +0200

    ci: enable dev profile for release

--- .ci/scripts/release/build-java.sh
+++ .ci/scripts/release/build-java.sh
@@ -2,5 +2,4 @@
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -XX:MaxRAMFraction=$((LIMITS_CPU))"
 
-# single quote the !development profile as this can cause issues with shell substitution
-mvn -B -s ${MAVEN_SETTINGS_XML} -DskipTests clean install -Pchecks,prepare-offline -P '!development'
+mvn -B -s ${MAVEN_SETTINGS_XML} -DskipTests clean install -Pchecks,prepare-offline
