33cd3ee6c5f99a6f7c8c1ff282513d3714b23a76
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-04-12 13:29:44 +0200

    build: remove development only profile
    The development only profile was to separate modules which were not
    necessary for release (such as QA, update tests, and benchmarks) from
    the release process. This causes quite a few issues with our build
    pipeline, and the advantages aren't really worth the trouble.

--- .ci/scripts/release/maven-release.sh
+++ .ci/scripts/release/maven-release.sh
@@ -2,15 +2,13 @@
 
 export ZBCTL_ROOT_DIR=${PWD}
 
-# single quote the !development profile as this can cause issues with shell substitution
 mvn -s ${MAVEN_SETTINGS_XML} release:prepare release:perform -B \
-    -P '!development' \
-    -Dgpg.passphrase="${GPG_PASS}" \
-    -Dresume=false \
-    -Dtag=${RELEASE_VERSION} \
-    -DreleaseVersion=${RELEASE_VERSION} \
-    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-    -DpushChanges=${PUSH_CHANGES} \
-    -DremoteTagging=${PUSH_CHANGES} \
-    -DlocalCheckout=${SKIP_DEPLOY} \
-    -Darguments='--settings=${MAVEN_SETTINGS_XML} -P "!development" -DskipTests=true -Dgpg.passphrase="${GPG_PASS}" -Dskip.central.release=${SKIP_DEPLOY} -Dskip.camunda.release=${SKIP_DEPLOY} -Dzbctl.force -Dzbctl.rootDir=${ZBCTL_ROOT_DIR}'
+  -Dgpg.passphrase="${GPG_PASS}" \
+  -Dresume=false \
+  -Dtag=${RELEASE_VERSION} \
+  -DreleaseVersion=${RELEASE_VERSION} \
+  -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
+  -DpushChanges=${PUSH_CHANGES} \
+  -DremoteTagging=${PUSH_CHANGES} \
+  -DlocalCheckout=${SKIP_DEPLOY} \
+  -Darguments='--settings=${MAVEN_SETTINGS_XML} -DskipTests=true -Dgpg.passphrase="${GPG_PASS}" -Dskip.central.release=${SKIP_DEPLOY} -Dskip.camunda.release=${SKIP_DEPLOY} -Dzbctl.force -Dzbctl.rootDir=${ZBCTL_ROOT_DIR}'
