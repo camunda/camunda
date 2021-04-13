68160e151264a9ac5428733cf0debd97df3e0d90
Ralf Puchert <ralf.puchert@camunda.com>
2021-04-13 08:47:38 +0200

    chore(project): update to zeebe-1.0.0-SNAPSHOT (from 1.0.0-alpha5)

--- .ci/pipelines/test_e2e.groovy
+++ .ci/pipelines/test_e2e.groovy
@@ -133,7 +133,7 @@ spec:
           cpu: 2
           memory: 4Gi
     - name: zeebe
-      image: camunda/zeebe:1.0.0-alpha5
+      image: camunda/zeebe:SNAPSHOT
       imagePullPolicy: Always   #this must be uncommented when snapshot is used
       env:
       volumeMounts:
