aadc129bd9bd8ce7fa310bafc853065134b85774
Vinícius Goulart <vinicius.goulart@camunda.com>
2021-04-15 16:02:05 +0200

    #805 Hide submit button (#841)

--- client/src/Tasklist/Task/FormJS/styled.tsx
+++ client/src/Tasklist/Task/FormJS/styled.tsx
@@ -27,42 +27,42 @@ const FormCustomStyling = createGlobalStyle`
   ${({theme}) => css`
     .fjs-container {
       height: min-content;
-    }
 
-    .fjs-container .fjs-form-field-label {
-      color: ${theme.colors.label01};
-      font-size: 14px;
-      font-weight: 600;
-    }
+      .fjs-form-field-label {
+        color: ${theme.colors.label01};
+        font-size: 14px;
+        font-weight: 600;
+      }
 
-    .fjs-container .fjs-input[type='text'],
-    .fjs-container .fjs-input[type='number'],
-    .fjs-container .fjs-textarea,
-    .fjs-container .fjs-select {
-      border: 1px solid ${theme.colors.ui05};
-
-      :focus {
-        box-shadow: ${theme.shadows.fakeOutline};
+      .fjs-input[type='text'],
+      .fjs-input[type='number'],
+      .fjs-textarea,
+      .fjs-select {
         border: 1px solid ${theme.colors.ui05};
-        padding: 8px;
+
+        :focus {
+          box-shadow: ${theme.shadows.fakeOutline};
+          border: 1px solid ${theme.colors.ui05};
+          padding: 8px;
+        }
       }
-    }
 
-    .fjs-container .fjs-has-errors .fjs-input[type='text'],
-    .fjs-container .fjs-has-errors .fjs-input[type='number'],
-    .fjs-container .fjs-has-errors .fjs-textarea,
-    .fjs-container .fjs-has-errors .fjs-select {
-      border: 1px solid ${({theme}) => theme.colors.red};
+      .fjs-has-errors .fjs-input[type='text'],
+      .fjs-has-errors .fjs-input[type='number'],
+      .fjs-has-errors .fjs-textarea,
+      .fjs-has-errors .fjs-select {
+        border: 1px solid ${({theme}) => theme.colors.red};
 
-      :focus {
-        border: 1px solid ${({theme}) => theme.colors.ui05};
-        box-shadow: ${({theme}) => theme.shadows.invalidInput};
+        :focus {
+          border: 1px solid ${({theme}) => theme.colors.ui05};
+          box-shadow: ${({theme}) => theme.shadows.invalidInput};
+        }
       }
-    }
 
-    .fjs-powered-by,
-    .fjs-form-field button[type='submit'] {
-      display: none;
+      .fjs-powered-by,
+      .fjs-form-field button[type='submit'] {
+        display: none;
+      }
     }
   `}
 `;
