aldler/zeebe

chore(raft): persist last appended index

atomix/cluster/src/main/java/io/atomix/raft/storage/serializer/MetaStoreSerializer.java
@@ -143,4 +143,28 @@ public class MetaStoreSerializer {
 
     return metaDecoder.votedFor();
   }
+
+  public long readLastAppendedIndex(final MutableDirectBuffer buffer, final int offset) {
+    headerDecoder.wrap(buffer, offset);
+    metaDecoder.wrap(
+        buffer,
+        headerDecoder.encodedLength(),
+        headerDecoder.blockLength(),
+        headerDecoder.version());
+
+    return metaDecoder.appendedIndex();
+  }
+
+  public void writeLastAppendedIndex(
+      final long index, final MutableDirectBuffer buffer, final int offset) {
+    headerEncoder
+        .wrap(buffer, offset)
+        .blockLength(metaEncoder.sbeBlockLength())
+        .templateId(metaEncoder.sbeTemplateId())
+        .schemaId(metaEncoder.sbeSchemaId())
+        .version(metaEncoder.sbeSchemaVersion());
+
+    metaEncoder.wrap(buffer, headerEncoder.encodedLength());
+    metaEncoder.appendedIndex(index);
+  }
 }

atomix/cluster/src/main/java/io/atomix/raft/storage/system/MetaStore.java
@@ -45,6 +45,7 @@ import org.slf4j.LoggerFactory;
  * Configuration}, including cluster membership.
  */
 public class MetaStore implements AutoCloseable {
+
   private static final byte VERSION = 1;
   private static final int VERSION_LENGTH = Byte.BYTES;
 
@@ -87,9 +88,11 @@ public class MetaStore implements AutoCloseable {
 
   private void initializeMetaBuffer() {
     final var term = loadTerm();
+    final long index = loadLastAppendedIndex();
     final var voted = loadVote();
     metaBuffer.put(0, VERSION);
     storeTerm(term);
+    storeLastAppendedIndex(index);
     storeVote(voted);
   }
 
@@ -158,6 +161,27 @@ public class MetaStore implements AutoCloseable {
     }
     final String id = serializer.readVotedFor(new UnsafeBuffer(metaBuffer), VERSION_LENGTH);
     return id.isEmpty() ? null : MemberId.from(id);
+  }
+
+  public synchronized long loadLastAppendedIndex() {
+    try {
+      metaFileChannel.read(metaBuffer, 0);
+    } catch (final IOException e) {
+      throw new StorageException(e);
+    }
+    return serializer.readLastAppendedIndex(new UnsafeBuffer(metaBuffer), VERSION_LENGTH);
+  }
+
+  public synchronized void storeLastAppendedIndex(final long index) {
+    log.trace("Store last appended index {}", index);
+
+    try {
+      serializer.writeLastAppendedIndex(index, new UnsafeBuffer(metaBuffer), VERSION_LENGTH);
+      metaFileChannel.write(metaBuffer, 0);
+      metaFileChannel.force(true);
+    } catch (final IOException e) {
+      throw new StorageException(e);
+    }
   }
 
   /**

atomix/cluster/src/test/java/io/atomix/raft/roles/PassiveRoleTest.java
@@ -16,7 +16,10 @@
 package io.atomix.raft.roles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.atomix.raft.impl.RaftContext;
@@ -24,8 +27,10 @@ import io.atomix.raft.metrics.RaftReplicationMetrics;
 import io.atomix.raft.protocol.AppendRequest;
 import io.atomix.raft.protocol.AppendResponse;
 import io.atomix.raft.storage.RaftStorage;
+import io.atomix.raft.storage.log.IndexedRaftLogEntry;
 import io.atomix.raft.storage.log.PersistedRaftRecord;
 import io.atomix.raft.storage.log.RaftLog;
+import io.zeebe.journal.JournalException.InvalidChecksum;
 import io.zeebe.snapshots.raft.PersistedSnapshot;
 import io.zeebe.snapshots.raft.ReceivableSnapshotStore;
 import java.io.IOException;
@@ -45,10 +50,12 @@ public class PassiveRoleTest {
   @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
   private RaftLog log;
   private PassiveRole role;
+  private RaftContext ctx;
+  private RaftStorage storage;
 
   @Before
   public void setup() throws IOException {
-    final RaftStorage storage = mock(RaftStorage.class);
+    storage = mock(RaftStorage.class);
 
     log = RaftLog.builder().withDirectory(temporaryFolder.newFolder("data")).build();
 
@@ -59,7 +66,7 @@ public class PassiveRoleTest {
     final ReceivableSnapshotStore store = mock(ReceivableSnapshotStore.class);
     when(store.getLatestSnapshot()).thenReturn(Optional.of(snapshot));
 
-    final RaftContext ctx = mock(RaftContext.class);
+    ctx = mock(RaftContext.class);
     when(ctx.getStorage()).thenReturn(storage);
     when(ctx.getLog()).thenReturn(log);
     when(ctx.getPersistedSnapshotStore()).thenReturn(store);
@@ -81,5 +88,46 @@ public class PassiveRoleTest {
 
     // then
     assertThat(response.succeeded()).isFalse();
+  }
+
+  @Test
+  public void shouldStoreLastAppendedIndex() {
+    // given
+    final RaftLog log = mock(RaftLog.class);
+    when(log.append(any(PersistedRaftRecord.class))).thenReturn(mock(IndexedRaftLogEntry.class));
+    when(ctx.getLog()).thenReturn(log);
+
+    final List<PersistedRaftRecord> entries = new ArrayList<>();
+    entries.add(new PersistedRaftRecord(1, 1, 1, 1, new byte[1]));
+    final AppendRequest request = new AppendRequest(2, "", 0, 0, entries, 1);
+
+    // when
+    role.handleAppend(request).join();
+
+    // then
+    verify(ctx).setLastAppendedIndex(eq(1L));
+  }
+
+  @Test
+  public void shouldStoreLastAppendedWithSomeFailure() {
+    // given
+    final RaftLog log = mock(RaftLog.class);
+    when(log.append(any(PersistedRaftRecord.class)))
+        .thenReturn(mock(IndexedRaftLogEntry.class))
+        .thenReturn(mock(IndexedRaftLogEntry.class))
+        .thenThrow(new InvalidChecksum("expected"));
+    when(ctx.getLog()).thenReturn(log);
+
+    final List<PersistedRaftRecord> entries = new ArrayList<>();
+    entries.add(new PersistedRaftRecord(1, 1, 1, 1, new byte[1]));
+    entries.add(new PersistedRaftRecord(1, 2, 2, 1, new byte[1]));
+    entries.add(new PersistedRaftRecord(1, 3, 3, 1, new byte[1]));
+    final AppendRequest request = new AppendRequest(1, "", 0, 0, entries, 3);
+
+    // when
+    role.handleAppend(request).join();
+
+    // then
+    verify(ctx).setLastAppendedIndex(eq(2L));
   }
 }

camunda/camunda

atomix/cluster/src/main/java/io/atomix/raft/storage/serializer/MetaStoreSerializer.java
@@ -143,4 +143,28 @@ public class MetaStoreSerializer {
 
     return metaDecoder.votedFor();
   }
+
+  public long readFlushedIndex(final MutableDirectBuffer buffer, final int offset) {
+    headerDecoder.wrap(buffer, offset);
+    metaDecoder.wrap(
+        buffer,
+        headerDecoder.encodedLength(),
+        headerDecoder.blockLength(),
+        headerDecoder.version());
+
+    return metaDecoder.flushedIndex();
+  }
+
+  public void writeFlushedIndex(
+      final long index, final MutableDirectBuffer buffer, final int offset) {
+    headerEncoder
+        .wrap(buffer, offset)
+        .blockLength(metaEncoder.sbeBlockLength())
+        .templateId(metaEncoder.sbeTemplateId())
+        .schemaId(metaEncoder.sbeSchemaId())
+        .version(metaEncoder.sbeSchemaVersion());
+
+    metaEncoder.wrap(buffer, headerEncoder.encodedLength());
+    metaEncoder.flushedIndex(index);
+  }
 }

atomix/cluster/src/main/java/io/atomix/raft/storage/system/MetaStore.java
@@ -45,6 +45,7 @@ import org.slf4j.LoggerFactory;
  * Configuration}, including cluster membership.
  */
 public class MetaStore implements AutoCloseable {
+
   private static final byte VERSION = 1;
   private static final int VERSION_LENGTH = Byte.BYTES;
 
@@ -87,9 +88,11 @@ public class MetaStore implements AutoCloseable {
 
   private void initializeMetaBuffer() {
     final var term = loadTerm();
+    final long index = loadFlushedIndex();
     final var voted = loadVote();
     metaBuffer.put(0, VERSION);
     storeTerm(term);
+    storeFlushedIndex(index);
     storeVote(voted);
   }
 
@@ -158,6 +161,27 @@ public class MetaStore implements AutoCloseable {
     }
     final String id = serializer.readVotedFor(new UnsafeBuffer(metaBuffer), VERSION_LENGTH);
     return id.isEmpty() ? null : MemberId.from(id);
+  }
+
+  public synchronized long loadFlushedIndex() {
+    try {
+      metaFileChannel.read(metaBuffer, 0);
+    } catch (final IOException e) {
+      throw new StorageException(e);
+    }
+    return serializer.readFlushedIndex(new UnsafeBuffer(metaBuffer), VERSION_LENGTH);
+  }
+
+  public synchronized void storeFlushedIndex(final long index) {
+    log.trace("Store last flushed index {}", index);
+
+    try {
+      serializer.writeFlushedIndex(index, new UnsafeBuffer(metaBuffer), VERSION_LENGTH);
+      metaFileChannel.write(metaBuffer, 0);
+      metaFileChannel.force(true);
+    } catch (final IOException e) {
+      throw new StorageException(e);
+    }
   }
 
   /**

atomix/cluster/src/test/java/io/atomix/raft/roles/PassiveRoleTest.java
@@ -17,6 +17,7 @@ package io.atomix.raft.roles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -48,12 +49,11 @@ public class PassiveRoleTest {
   @Rule public Timeout timeout = new Timeout(30, TimeUnit.SECONDS);
   private RaftLog log;
   private PassiveRole role;
+  private RaftContext ctx;
 
   @Before
   public void setup() throws IOException {
-
-    final RaftContext ctx = mock(RaftContext.class);
-    final RaftStorage storage = mock(RaftStorage.class);
+    ctx = mock(RaftContext.class);
 
     log = mock(RaftLog.class);
     when(log.shouldFlushExplicitly()).thenReturn(true);
@@ -66,6 +66,7 @@ public class PassiveRoleTest {
     final ReceivableSnapshotStore store = mock(ReceivableSnapshotStore.class);
     when(store.getLatestSnapshot()).thenReturn(Optional.of(snapshot));
 
+    final RaftStorage storage = mock(RaftStorage.class);
     when(ctx.getStorage()).thenReturn(storage);
     when(ctx.getLog()).thenReturn(log);
     when(ctx.getPersistedSnapshotStore()).thenReturn(store);
@@ -150,5 +151,45 @@ public class PassiveRoleTest {
     // then
     verify(log, never()).flush();
     assertThat(response.lastLogIndex()).isEqualTo(0);
+  }
+
+  @Test
+  public void shouldStoreLastFlushedIndex() {
+    // given
+    final List<PersistedRaftRecord> entries =
+        List.of(new PersistedRaftRecord(1, 1, 1, 1, new byte[1]));
+    final AppendRequest request = new AppendRequest(2, "", 0, 0, entries, 1);
+
+    when(log.append(any(PersistedRaftRecord.class))).thenReturn(mock(IndexedRaftLogEntry.class));
+    when(ctx.getLog()).thenReturn(log);
+
+    // when
+    role.handleAppend(request).join();
+
+    // then
+    verify(ctx).setFlushedIndex(eq(1L));
+  }
+
+  @Test
+  public void shouldStoreLastFlushedWithSomeFailure() {
+    // given
+    final List<PersistedRaftRecord> entries =
+        List.of(
+            new PersistedRaftRecord(1, 1, 1, 1, new byte[1]),
+            new PersistedRaftRecord(1, 2, 2, 1, new byte[1]),
+            new PersistedRaftRecord(1, 3, 3, 1, new byte[1]));
+    final AppendRequest request = new AppendRequest(1, "", 0, 0, entries, 3);
+
+    when(log.append(any(PersistedRaftRecord.class)))
+        .thenReturn(mock(IndexedRaftLogEntry.class))
+        .thenReturn(mock(IndexedRaftLogEntry.class))
+        .thenThrow(new InvalidChecksum("expected"));
+    when(ctx.getLog()).thenReturn(log);
+
+    // when
+    role.handleAppend(request).join();
+
+    // then
+    verify(ctx).setFlushedIndex(eq(2L));
   }
 }