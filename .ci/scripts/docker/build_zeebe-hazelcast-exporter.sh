b4bba97a0e3533dff80b2dc13b803af8cffc996d
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-04-19 10:02:16 +0200

    ci: disable BPMN TCK
    The BPMN TCK relies on an outdated version of Testcontainers which is
    not compatible with Docker 20.10+ - let's disable it for now as it's not
    critical to our infrastructure, and will need to be reworkd for 1.0
    anyway

--- .ci/scripts/docker/build_zeebe-hazelcast-exporter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -eux
-
-echo "Building Zeebe with Hazelast exporter Docker image zeebe-hazelcast-exporter:current-test"
-docker build --no-cache -f .ci/docker/Dockerfile_zeebe-hazelcast-exporter . -t zeebe-hazelcast-exporter:current-test
