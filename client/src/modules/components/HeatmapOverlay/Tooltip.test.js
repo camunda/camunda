92998a887949f199338025c859da65f36938a42a
OmranAbazid <omran.1994@gmail.com>
2021-04-23 14:44:57 +0200

    improve(Tooltips): add delay to tooltip close
    related to OPT-4806

--- client/src/modules/components/HeatmapOverlay/Tooltip.test.js
+++ client/src/modules/components/HeatmapOverlay/Tooltip.test.js
@@ -10,6 +10,8 @@ import {shallow} from 'enzyme';
 import Tooltip from './Tooltip';
 import {addDiagramTooltip} from './service';
 
+jest.useFakeTimers();
+
 jest.mock('./service', () => {
   return {
     addDiagramTooltip: jest.fn(),
@@ -33,7 +35,14 @@ const data = {elementId: 'elementName'};
 it('should create a tooltip', () => {
   shallow(<Tooltip viewer={viewer} data={data} formatter={(v) => v} />);
 
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'elementId', 'elementName', undefined);
+  expect(addDiagramTooltip).toHaveBeenCalledWith({
+    viewer,
+    element: 'elementId',
+    tooltipContent: 'elementName',
+    theme: undefined,
+    onMouseEnter: expect.any(Function),
+    onMouseLeave: expect.any(Function),
+  });
 });
 
 it('should remove old tooltips on rerendering', () => {
@@ -46,7 +55,14 @@ it('should remove old tooltips on rerendering', () => {
 it('should pass the theme to addDiagramTooltip if defined', () => {
   shallow(<Tooltip theme="light" viewer={viewer} data={data} formatter={(v) => v} />);
 
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'elementId', 'elementName', 'light');
+  expect(addDiagramTooltip).toHaveBeenCalledWith({
+    viewer,
+    element: 'elementId',
+    tooltipContent: 'elementName',
+    theme: 'light',
+    onMouseEnter: expect.any(Function),
+    onMouseLeave: expect.any(Function),
+  });
 });
 
 it('should not add tooltip for every element if hideAbsoluteValue and hideReltiveValue are undefined', () => {
@@ -54,17 +70,60 @@ it('should not add tooltip for every element if hideAbsoluteValue and hideReltiv
   const data = {a: '1', b: '2', c: '3', d: '4', e: '5'};
   shallow(<Tooltip viewer={viewer} data={data} formatter={(v) => v} />);
 
-  expect(addDiagramTooltip).not.toHaveBeenCalledWith(viewer, 'a', '1');
+  expect(addDiagramTooltip).not.toHaveBeenCalledWith({viewer, element: 'a', tooltipContent: '1'});
 });
 
 it('should add a tooltip for every element if alwaysShowAbsolute or alwaysShowRelative are true', () => {
   addDiagramTooltip.mockClear();
   const data = {a: '1', b: '2', c: '3', d: '4', e: '5'};
   shallow(<Tooltip theme="light" viewer={viewer} data={data} formatter={(v) => v} alwaysShow />);
+  Object.keys(data).forEach((key) => {
+    expect(addDiagramTooltip).toHaveBeenCalledWith({
+      viewer,
+      element: key,
+      tooltipContent: data[key],
+      theme: 'light',
+    });
+  });
+});
 
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'a', '1', 'light');
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'b', '2', 'light');
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'c', '3', 'light');
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'd', '4', 'light');
-  expect(addDiagramTooltip).toHaveBeenCalledWith(viewer, 'e', '5', 'light');
+it('should clear tooltip on mouse leave', () => {
+  removeSpy.mockClear();
+  addDiagramTooltip.mockImplementationOnce(({onMouseLeave}) => {
+    onMouseLeave();
+  });
+  shallow(<Tooltip viewer={viewer} data={data} formatter={(v) => v} />);
+
+  expect(removeSpy).toHaveBeenCalledTimes(2);
+});
+
+it('should reset scheduled remove on mouse enter', async () => {
+  const viewer = {
+    i: 0,
+    get: () => {
+      return {
+        on: (__, fct) => {
+          viewer.i++;
+
+          return fct({element: {id: viewer.i === 1 ? 'elementId' : undefined}});
+        },
+        remove: removeSpy,
+      };
+    },
+  };
+
+  removeSpy.mockClear();
+  let mouseEnterFunc;
+  addDiagramTooltip.mockImplementationOnce(({onMouseEnter}) => {
+    mouseEnterFunc = onMouseEnter;
+    return 'elementId';
+  });
+
+  const node = shallow(<Tooltip viewer={viewer} data={data} formatter={(v) => v} />);
+  await node.update();
+  node.instance().componentDidMount();
+  mouseEnterFunc();
+  jest.runAllTimers();
+
+  expect(removeSpy).not.toHaveBeenCalledWith('elementId');
 });
