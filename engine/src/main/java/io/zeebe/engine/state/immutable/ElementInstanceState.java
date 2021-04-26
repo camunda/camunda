6aee8aab2ffe62513d8f65a62e728410a0277e07
Philipp Ossler <philipp.ossler@gmail.com>
2021-04-26 13:45:38 +0200

    feat(engine): store the number of taken sequence flows
    * extend the state to store the number of taken sequence flows that are connected to a joining parallel gateway
    * the number helps to determine if a parallel gateway can be activated or not
    * the new column family replaces the usage of deferred records that have a more general intention but are not so efficient for this case

--- engine/src/main/java/io/zeebe/engine/state/immutable/ElementInstanceState.java
+++ engine/src/main/java/io/zeebe/engine/state/immutable/ElementInstanceState.java
@@ -12,6 +12,7 @@ import io.zeebe.engine.state.instance.ElementInstance;
 import io.zeebe.engine.state.instance.IndexedRecord;
 import io.zeebe.engine.state.instance.StoredRecord;
 import java.util.List;
+import org.agrona.DirectBuffer;
 
 public interface ElementInstanceState {
 
@@ -26,4 +27,16 @@ public interface ElementInstanceState {
   IndexedRecord getFailedRecord(long key);
 
   AwaitProcessInstanceResultMetadata getAwaitResultRequestMetadata(long processInstanceKey);
+
+  /**
+   * Returns the number of the taken sequence flows that are connected to the given parallel
+   * (joining) gateway. Each sequence flow counts only as one, even if it is taken multiple times.
+   *
+   * <p>The number helps to determine if a parallel gateway can be activated or not.
+   *
+   * @param flowScopeKey the key of the flow scope that contains the gateway
+   * @param gatewayElementId the element id of the gateway
+   * @return the number of taken sequence flows of the given gateway
+   */
+  int getNumberOfTakenSequenceFlows(final long flowScopeKey, final DirectBuffer gatewayElementId);
 }
--- engine/src/main/java/io/zeebe/engine/state/mutable/MutableElementInstanceState.java
+++ engine/src/main/java/io/zeebe/engine/state/mutable/MutableElementInstanceState.java
@@ -14,6 +14,7 @@ import io.zeebe.engine.state.instance.StoredRecord.Purpose;
 import io.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
 import io.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import java.util.function.Consumer;
+import org.agrona.DirectBuffer;
 
 public interface MutableElementInstanceState extends ElementInstanceState {
 
@@ -39,4 +40,32 @@ public interface MutableElementInstanceState extends ElementInstanceState {
 
   void setAwaitResultRequestMetadata(
       long processInstanceKey, AwaitProcessInstanceResultMetadata metadata);
+
+  /**
+   * Increments the number that counts how often the given sequence flow has been taken.
+   *
+   * <p>The number helps to determine if a parallel gateway can be activated or not. It should be
+   * incremented when one of the incoming sequence flows is taken.
+   *
+   * @param flowScopeKey the key of the flow scope that contains the gateway
+   * @param gatewayElementId the element id of the gateway that is the target of the sequence flow
+   * @param sequenceFlowElementId the element id of the sequence flow that is taken
+   */
+  void incrementNumberOfTakenSequenceFlows(
+      final long flowScopeKey,
+      final DirectBuffer gatewayElementId,
+      final DirectBuffer sequenceFlowElementId);
+
+  /**
+   * Decrements the numbers that counts how often a sequence flow of the given gateway has been
+   * taken.
+   *
+   * <p>The number helps to determine if a parallel gateway can be activated or not. It should be
+   * decremented when the gateway is activated.
+   *
+   * @param flowScopeKey the key of the flow scope that contains the gateway
+   * @param gatewayElementId the element id of the gateway
+   */
+  void decrementNumberOfTakenSequenceFlows(
+      final long flowScopeKey, final DirectBuffer gatewayElementId);
 }
