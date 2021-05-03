04ff883e7a1e276ce6e11cf758e07d0f8f2c5dc9
Sebastian Menski <sebastian.menski@camunda.com>
2021-05-03 14:38:24 +0200

    chore(ci): adjust docker prepare script to use io.camunda.zeebe group id

--- .ci/scripts/docker/prepare.sh
+++ .ci/scripts/docker/prepare.sh
@@ -2,10 +2,10 @@
 
 mvn dependency:get -B \
     -DremoteRepositories="camunda-nexus::::https://app.camunda.com/nexus/content/repositories/public" \
-    -DgroupId="io.zeebe" -DartifactId="camunda-cloud-zeebe" \
+    -DgroupId="io.camunda.zeebe" -DartifactId="camunda-cloud-zeebe" \
     -Dversion="${VERSION}" -Dpackaging="tar.gz" -Dtransitive=false
 
 mvn dependency:copy -B \
-    -Dartifact="io.zeebe:camunda-cloud-zeebe:${VERSION}:tar.gz" \
+    -Dartifact="io.camunda.zeebe:camunda-cloud-zeebe:${VERSION}:tar.gz" \
     -DoutputDirectory=${WORKSPACE} \
     -Dmdep.stripVersion=true
