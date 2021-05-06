608c10a131067be071334bb23d4f89a609a68671
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-05-06 10:47:52 +0200

    build: fix docker pulling from wrong groupId

--- .ci/scripts/docker/prepare.sh
+++ .ci/scripts/docker/prepare.sh
@@ -2,10 +2,10 @@
 
 mvn dependency:get -B \
     -DremoteRepositories="camunda-nexus::::https://app.camunda.com/nexus/content/repositories/public" \
-    -DgroupId="io.camunda.zeebe" -DartifactId="camunda-cloud-zeebe" \
+    -DgroupId="io.camunda" -DartifactId="camunda-cloud-zeebe" \
     -Dversion="${VERSION}" -Dpackaging="tar.gz" -Dtransitive=false
 
 mvn dependency:copy -B \
-    -Dartifact="io.camunda.zeebe:camunda-cloud-zeebe:${VERSION}:tar.gz" \
+    -Dartifact="io.camunda:camunda-cloud-zeebe:${VERSION}:tar.gz" \
     -DoutputDirectory=${WORKSPACE} \
     -Dmdep.stripVersion=true
