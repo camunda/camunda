ce3e64c96bea40da2f6c53087d8484b00755b4ff
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-05-06 15:56:32 +0200

    test(exporters): remove ignoreVariablesAbove tests

--- exporters/elasticsearch-exporter/src/test/java/io/camunda/zeebe/exporter/ElasticsearchClientTest.java
+++ exporters/elasticsearch-exporter/src/test/java/io/camunda/zeebe/exporter/ElasticsearchClientTest.java
@@ -9,9 +9,7 @@ package io.camunda.zeebe.exporter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,13 +18,10 @@ import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.value.VariableRecordValue;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatchers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -46,69 +41,6 @@ public class ElasticsearchClientTest extends AbstractElasticsearchExporterIntegr
     logSpy = spy(LoggerFactory.getLogger(ElasticsearchClientTest.class));
     bulkRequest = new ArrayList<>();
     client = new ElasticsearchClient(configuration, logSpy, bulkRequest);
-  }
-
-  @Test
-  public void shouldNotLogWarningWhenIndexingSmallVariableValue() {
-    // given
-    final String variableValue = "x".repeat(configuration.index.ignoreVariablesAbove);
-
-    final Record<VariableRecordValue> recordMock = mock(Record.class);
-    when(recordMock.getPartitionId()).thenReturn(1);
-    when(recordMock.getKey()).thenReturn(RECORD_KEY);
-    when(recordMock.getValueType()).thenReturn(ValueType.VARIABLE);
-    when(recordMock.toJson()).thenReturn("{}");
-
-    final VariableRecordValue value = mock(VariableRecordValue.class);
-    when(value.getValue()).thenReturn(variableValue);
-    when(recordMock.getValue()).thenReturn(value);
-
-    // when
-    client.index(recordMock);
-
-    // then
-    verify(logSpy, never()).warn(anyString(), ArgumentMatchers.<Object[]>any());
-  }
-
-  @Test
-  public void shouldLogWarnWhenIndexingLargeVariableValue() {
-    // given
-    final String variableName = "varName";
-    final String variableValue = "x".repeat(configuration.index.ignoreVariablesAbove + 1);
-    final long scopeKey = 1234L;
-    final long processInstanceKey = 5678L;
-
-    final Record<VariableRecordValue> recordMock = mock(Record.class);
-    when(recordMock.getPartitionId()).thenReturn(1);
-    when(recordMock.getKey()).thenReturn(RECORD_KEY);
-    when(recordMock.getValueType()).thenReturn(ValueType.VARIABLE);
-    when(recordMock.toJson()).thenReturn("{}");
-
-    final VariableRecordValue value = mock(VariableRecordValue.class);
-    when(value.getName()).thenReturn(variableName);
-    when(value.getValue()).thenReturn(variableValue);
-    when(value.getScopeKey()).thenReturn(scopeKey);
-    when(value.getProcessInstanceKey()).thenReturn(processInstanceKey);
-
-    when(recordMock.getValue()).thenReturn(value);
-
-    // when
-    client.index(recordMock);
-
-    // then
-    final ArgumentCaptor<Object[]> argumentCaptor = ArgumentCaptor.forClass(Object[].class);
-
-    verify(logSpy).warn(anyString(), argumentCaptor.capture());
-
-    // required to explicitly cast List<Objects[]> to List<Object>
-    final List<Object> args = new ArrayList<>(argumentCaptor.getAllValues());
-    assertThat(args)
-        .contains(
-            RECORD_KEY,
-            variableName,
-            variableValue.getBytes().length,
-            scopeKey,
-            processInstanceKey);
   }
 
   @Test
