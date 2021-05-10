55e02edb0b41d8d7a6a41982185b16d4543e2745
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-05-10 17:52:17 +0200

    refactor(clients): wrap ObjectMapper instead of extending it
    I previously accepted a PR where we extend a library defined type,
    ObjectMapper, instead of providing an adapter for our interface.
    Extending a class we have no control over for something like this is a
    bad idea, as you may inadvertently start using inherited features from
    the new class where you shouldn't (since it's not meant to be a drop in
    replacement of ObjectMapper). Instead, we now wrap an existing one and
    use it where we need, and only expose the methods we need to.

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/ZeebeObjectMapper.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/ZeebeObjectMapper.java
@@ -25,7 +25,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-public final class ZeebeObjectMapper extends ObjectMapper implements JsonMapper {
+public final class ZeebeObjectMapper implements JsonMapper {
 
   private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE =
       new TypeReference<Map<String, Object>>() {};
@@ -33,49 +33,57 @@ public final class ZeebeObjectMapper extends ObjectMapper implements JsonMapper 
   private static final TypeReference<Map<String, String>> STRING_MAP_TYPE_REFERENCE =
       new TypeReference<Map<String, String>>() {};
 
+  private final ObjectMapper objectMapper;
+
   public ZeebeObjectMapper() {
-    configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper = new ObjectMapper();
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
   }
 
+  @Override
   public <T> T fromJson(final String json, final Class<T> typeClass) {
     try {
-      return readValue(json, typeClass);
+      return objectMapper.readValue(json, typeClass);
     } catch (final IOException e) {
       throw new InternalClientException(
           String.format("Failed to deserialize json '%s' to class '%s'", json, typeClass), e);
     }
   }
 
+  @Override
   public Map<String, Object> fromJsonAsMap(final String json) {
     try {
-      return readValue(json, MAP_TYPE_REFERENCE);
+      return objectMapper.readValue(json, MAP_TYPE_REFERENCE);
     } catch (final IOException e) {
       throw new InternalClientException(
           String.format("Failed to deserialize json '%s' to 'Map<String, Object>'", json), e);
     }
   }
 
+  @Override
   public Map<String, String> fromJsonAsStringMap(final String json) {
     try {
-      return readValue(json, STRING_MAP_TYPE_REFERENCE);
+      return objectMapper.readValue(json, STRING_MAP_TYPE_REFERENCE);
     } catch (final IOException e) {
       throw new InternalClientException(
           String.format("Failed to deserialize json '%s' to 'Map<String, String>'", json), e);
     }
   }
 
+  @Override
   public String toJson(final Object value) {
     try {
-      return writeValueAsString(value);
+      return objectMapper.writeValueAsString(value);
     } catch (final JsonProcessingException e) {
       throw new InternalClientException(
           String.format("Failed to serialize object '%s' to json", value), e);
     }
   }
 
+  @Override
   public String validateJson(final String propertyName, final String jsonInput) {
     try {
-      return readTree(jsonInput).toString();
+      return objectMapper.readTree(jsonInput).toString();
     } catch (final IOException e) {
       throw new InternalClientException(
           String.format(
@@ -84,9 +92,10 @@ public final class ZeebeObjectMapper extends ObjectMapper implements JsonMapper 
     }
   }
 
+  @Override
   public String validateJson(final String propertyName, final InputStream jsonInput) {
     try {
-      return readTree(jsonInput).toString();
+      return objectMapper.readTree(jsonInput).toString();
     } catch (final IOException e) {
       throw new InternalClientException(
           String.format("Failed to validate json input stream for property '%s'", propertyName), e);
