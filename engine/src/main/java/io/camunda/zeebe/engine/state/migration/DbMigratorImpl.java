32e5486b6ea89dd18ba07f265bb02a45b0469e99
pihme <pihme@users.noreply.github.com>
2021-06-03 20:30:35 +0200

    feature(engine): add mechanism to run migrations on startup

---
+++ engine/src/main/java/io/camunda/zeebe/engine/state/migration/DbMigratorImpl.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.state.migration;
+
+import io.camunda.zeebe.engine.state.mutable.MutableZeebeState;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DbMigratorImpl implements DbMigrator {
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(DbMigratorImpl.class.getPackageName());
+
+  // add new migration tasks here, migrations are executed in the order they appear in the list
+  private static final List<MigrationTask> MIGRATION_TASKS = Collections.emptyList();
+  // Be mindful of https://github.com/camunda-cloud/zeebe/issues/7248. In particular, that issue
+  // should be solved first, before adding any migration that can take a long time
+
+  private final MutableZeebeState zeebeState;
+  private final Supplier<List<MigrationTask>> migrationSupplier;
+  private boolean abortRequested = false;
+
+  private MigrationTask currentMigration;
+
+  public DbMigratorImpl(final MutableZeebeState zeebeState) {
+    this(zeebeState, () -> MIGRATION_TASKS);
+  }
+
+  DbMigratorImpl(
+      final MutableZeebeState zeebeState, final Supplier<List<MigrationTask>> migrationSupplier) {
+
+    this.zeebeState = zeebeState;
+    this.migrationSupplier = migrationSupplier;
+  }
+
+  @Override
+  public void runMigrations() {
+
+    final var migrationTasks = migrationSupplier.get();
+    logPreview(migrationTasks);
+
+    final var executedMigrations = new ArrayList<MigrationTask>();
+    for (int index = 1; index <= migrationTasks.size() && !abortRequested; index++) {
+      // one based index looks nicer in logs
+
+      final var migration = migrationTasks.get(index - 1);
+      final var executed = handleMigrationTask(migration, index, migrationTasks.size());
+      if (executed) {
+        executedMigrations.add(migration);
+      }
+    }
+    if (!abortRequested) {
+      logSummary(executedMigrations);
+    }
+  }
+
+  private void logPreview(final List<MigrationTask> migrationTasks) {
+    LOGGER.info(
+        "Starting processing of migration tasks (use LogLevel.DEBUG for more details) ... ");
+    LOGGER.debug(
+        "Found "
+            + migrationTasks.size()
+            + " migration tasks: "
+            + migrationTasks.stream()
+                .map(MigrationTask::getIdentifier)
+                .collect(Collectors.joining(", ")));
+  }
+
+  private void logSummary(final List<MigrationTask> migrationTasks) {
+    LOGGER.info(
+        "Completed processing of migration tasks (use LogLevel.DEBUG for more details) ... ");
+    LOGGER.debug(
+        "Executed "
+            + migrationTasks.size()
+            + " migration tasks: "
+            + migrationTasks.stream()
+                .map(MigrationTask::getIdentifier)
+                .collect(Collectors.joining(", ")));
+  }
+
+  @Override
+  public void abort() {
+    final var message =
+        currentMigration == null
+            ? "Received abort signal (no migration running)"
+            : "Aborting " + currentMigration.getIdentifier() + " migration as requested";
+    LOGGER.info(message);
+    abortRequested = true;
+  }
+
+  private boolean handleMigrationTask(
+      final MigrationTask migrationTask, final int index, final int total) {
+    if (migrationTask.needsToRun(zeebeState)) {
+      try {
+        currentMigration = migrationTask;
+        runMigration(migrationTask, index, total);
+      } finally {
+        currentMigration = null;
+      }
+      return true;
+    } else {
+      logMigrationSkipped(migrationTask, index, total);
+      return false;
+    }
+  }
+
+  private void logMigrationSkipped(
+      final MigrationTask migrationTask, final int index, final int total) {
+    LOGGER.info(
+        "Skipping "
+            + migrationTask.getIdentifier()
+            + " migration ("
+            + index
+            + "/"
+            + total
+            + ").  It was determined it does not need to run right now.");
+  }
+
+  private void runMigration(final MigrationTask migrationTask, final int index, final int total) {
+    LOGGER.info(
+        "Starting " + migrationTask.getIdentifier() + " migration (" + index + "/" + total + ")");
+    final var startTime = System.currentTimeMillis();
+    migrationTask.runMigration(zeebeState);
+    final var duration = System.currentTimeMillis() - startTime;
+
+    LOGGER.debug(migrationTask.getIdentifier() + " migration completed in " + duration + " ms.");
+    LOGGER.info(
+        "Finished " + migrationTask.getIdentifier() + " migration (" + index + "/" + total + ")");
+  }
+}
---
+++ engine/src/test/java/io/camunda/zeebe/engine/state/migration/DbMigrationControllerTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.state.migration;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import io.camunda.zeebe.engine.processing.streamprocessor.ReadonlyProcessingContext;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DbMigrationControllerTest {
+
+  private ReadonlyProcessingContext mockContext;
+  private DbMigrator mockDbMigrator;
+
+  private DbMigrationController sutMigrationController;
+
+  @BeforeEach
+  public void setUp() {
+    mockContext = mock(ReadonlyProcessingContext.class);
+    mockDbMigrator = mock(DbMigrator.class);
+
+    sutMigrationController = new DbMigrationController(zeebeState -> mockDbMigrator);
+  }
+
+  @Test
+  public void shouldTriggerMigrationsWhenOnRecoveredEventIsReceived() {
+    // when
+    sutMigrationController.onRecovered(mockContext);
+
+    // then
+    verify(mockDbMigrator).runMigrations();
+  }
+
+  @Test
+  public void shouldAbortMigratorWhenOnCloseEventIsReceived() {
+    // given
+    final var countdownLatch = new CountDownLatch(1);
+    makeMigrationsMockWaitForCountDownLatch(countdownLatch);
+
+    // when
+    sendOnRecoveredInNewThreadAndWaitForMigrationToStart();
+    sutMigrationController.onClose();
+    countdownLatch.countDown();
+
+    // then
+    verify(mockDbMigrator).abort();
+  }
+
+  @Test
+  public void shouldAbortMigratorWhenOnFailedEventIsReceived() {
+    // given
+    final var countdownLatch = new CountDownLatch(1);
+    makeMigrationsMockWaitForCountDownLatch(countdownLatch);
+
+    // when
+    sendOnRecoveredInNewThreadAndWaitForMigrationToStart();
+    sutMigrationController.onFailed();
+    countdownLatch.countDown();
+
+    // then
+    verify(mockDbMigrator).abort();
+  }
+
+  private void sendOnRecoveredInNewThreadAndWaitForMigrationToStart() {
+    new Thread(() -> sutMigrationController.onRecovered(mockContext)).start();
+    await()
+        .pollInterval(10, TimeUnit.MILLISECONDS)
+        .untilAsserted(() -> verify(mockDbMigrator).runMigrations());
+  }
+
+  private void makeMigrationsMockWaitForCountDownLatch(final CountDownLatch countDownLatch) {
+    doAnswer(
+            invocationOnMock -> {
+              countDownLatch.await();
+              return null;
+            })
+        .when(mockDbMigrator)
+        .runMigrations();
+  }
+}
