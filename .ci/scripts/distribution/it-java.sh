09455cd296332cb79fca549d66ca32cd61aee8df
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-06-10 16:51:51 +0200

    build: use failsafe for integration tests
    Splits unit and integration tests by which Maven plugin is used to run
    them. Integration tests are handled by Failsafe, and unit tests by
    Surefire.
    
    Introduces two new variables to allow exclusions of each type:
      - skipUTs: skips unit tests by disabling the execution of Surefire
      - skipITs: skips integration tests by disabling the execution of
        Failsafe
      - skipTests: using skipTests will still skip all tests, as before.
    
    Moves the update tests under the qa module to allow them to share the
    same testing configuration as the integration test module, and renames
    them from zeebe-update-tests to zeebe-qa-update-tests to align with the
    other modules.
    
    (cherry picked from commit 1a46f5aa90405a9f6aee7ce18ed5e837953647b8)

--- .ci/scripts/distribution/it-java.sh
+++ .ci/scripts/distribution/it-java.sh
@@ -8,6 +8,7 @@ MAVEN_PARALLELISM=${MAVEN_PARALLELISM:-$LIMITS_CPU}
 SUREFIRE_FORK_COUNT=${SUREFIRE_FORK_COUNT:-}
 JUNIT_THREAD_COUNT=${JUNIT_THREAD_COUNT:-}
 MAVEN_PROPERTIES=(
+  -DskipUTs
   -DtestMavenId=2
   -Dsurefire.rerunFailingTestsCount=7
 )
@@ -23,7 +24,7 @@ if [ ! -z "$JUNIT_THREAD_COUNT" ]; then
   MAVEN_PROPERTIES+=("-DjunitThreadCount=$JUNIT_THREAD_COUNT")
 fi
 
-mvn -o -B --fail-never -T${MAVEN_PARALLELISM} -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,skip-random-tests,parallel-tests -pl qa/integration-tests,update-tests "${MAVEN_PROPERTIES[@]}" | tee ${tmpfile}
+mvn -o -B --fail-never -T${MAVEN_PARALLELISM} -s ${MAVEN_SETTINGS_XML} verify -P skip-unstable-ci,parallel-tests "${MAVEN_PROPERTIES[@]}" | tee ${tmpfile}
 status=${PIPESTATUS[0]}
 
 # delay checking the maven status after we've analysed flaky tests
