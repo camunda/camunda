c4f090876876ff9f77643285b2134406be0a9bc4
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-06-27 18:22:56 +0200

    test: temporarily run only a few tests
    Only run test files ending in SomethingImpossible to simplify/speed up
    testing, since otherwise we need to run the whole pipeline for no
    reason.

---
+++ engine/src/test/java/io/camunda/zeebe/engine/TestSomethingImpossible.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.junit.jupiter.api.Test;
+
+final class TestSomethingImpossible {
+  private static final AtomicBoolean TOGGLE = new AtomicBoolean();
+  private static final AtomicBoolean TOGGLE_PRIME = new AtomicBoolean();
+
+  @Test
+  void aFlakyTest() {
+    assertThat(TOGGLE.getAndSet(true))
+        .as("should fail on the first try but pass on the second")
+        .isTrue();
+  }
+
+  @Test
+  void aSecondFlakyTest() {
+    assertThat(TOGGLE_PRIME.getAndSet(true))
+        .as("should fail on the first try but pass on the second")
+        .isTrue();
+  }
+}
