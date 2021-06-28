d687d11d6bcd5ac267d0240885f9f590dd73712f
Sebastian Bathke <sebastian.bathke@camunda.com>
2021-06-28 15:45:30 +0200

    chore(cloud): disable license filter in cloud
    relates to #OPT-5317

--- backend/src/main/java/org/camunda/optimize/jetty/LicenseFilter.java
+++ backend/src/main/java/org/camunda/optimize/jetty/LicenseFilter.java
@@ -5,11 +5,9 @@
  */
 package org.camunda.optimize.jetty;
 
-import com.google.common.collect.Sets;
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.optimize.service.exceptions.license.OptimizeLicenseException;
 import org.camunda.optimize.service.license.LicenseManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -21,20 +19,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Set;
 
+import static org.camunda.optimize.service.util.configuration.ConfigurationServiceConstants.CLOUD_PROFILE;
+
+@Slf4j
 public class LicenseFilter implements Filter {
-
-  private static final Logger logger = LoggerFactory.getLogger(LicenseFilter.class);
-
-  private LicenseManager licenseManager;
-
-  private SpringAwareServletConfiguration awareDelegate;
-
-  private static final Set<String> EXCLUDED_EXTENSIONS = Sets.newHashSet("css", "html", "js", "ico");
-
-  private static final List<String> EXCLUDED_API_CALLS = List.of(
+  private static final Set<String> EXCLUDED_EXTENSIONS = Set.of("css", "html", "js", "ico");
+  private static final Set<String> EXCLUDED_API_CALLS = Set.of(
     "authentication",
     "localization",
     "ui-configuration",
@@ -44,7 +37,7 @@ public class LicenseFilter implements Filter {
     "readyz"
   );
 
-  private static final List<String> SSO_PATHS = List.of("sso/auth0", "sso-callback");
+  private final SpringAwareServletConfiguration awareDelegate;
 
   public LicenseFilter(SpringAwareServletConfiguration awareDelegate) {
     this.awareDelegate = awareDelegate;
@@ -63,15 +56,14 @@ public class LicenseFilter implements Filter {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
                                                                                             IOException,
                                                                                             ServletException {
-    setLicenseManager();
     HttpServletResponse servletResponse = (HttpServletResponse) response;
     HttpServletRequest servletRequest = (HttpServletRequest) request;
 
     if (isLicenseCheckNeeded(servletRequest)) {
       try {
-        licenseManager.validateLicenseStoredInOptimize();
+        getLicenseManager().validateLicenseStoredInOptimize();
       } catch (OptimizeLicenseException e) {
-        logger.warn("Given License is invalid or not available!");
+        log.warn("Given License is invalid or not available!");
         constructForbiddenResponse(servletResponse, e);
         return;
       }
@@ -87,10 +79,8 @@ public class LicenseFilter implements Filter {
     servletResponse.setStatus(Response.Status.FORBIDDEN.getStatusCode());
   }
 
-  private void setLicenseManager() {
-    if (licenseManager == null) {
-      licenseManager = awareDelegate.getApplicationContext().getBean(LicenseManager.class);
-    }
+  private LicenseManager getLicenseManager() {
+    return awareDelegate.getApplicationContext().getBean(LicenseManager.class);
   }
 
   private boolean isLicenseCheckNeeded(HttpServletRequest servletRequest) {
@@ -99,14 +89,14 @@ public class LicenseFilter implements Filter {
 
     return !isStaticResource(requestPath)
       && !isRootRequest(requestPath)
-      && !isSsoPath(requestPath)
+      && !isCloudEnvironment()
       && !isExcludedApiPath(pathInfo)
       && !isStatusRequest(requestPath);
   }
 
-  // TODO to be removed with disabling this filter in cloud environments, see OPT-5317
-  private boolean isSsoPath(final String requestPath) {
-    return requestPath != null && SSO_PATHS.stream().anyMatch(requestPath::contains);
+  private boolean isCloudEnvironment() {
+    return Arrays.stream(awareDelegate.getApplicationContext().getEnvironment().getActiveProfiles())
+      .anyMatch(CLOUD_PROFILE::equalsIgnoreCase);
   }
 
   private static boolean isStatusRequest(String requestPath) {
