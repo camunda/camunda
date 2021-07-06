7fd048cf6740fefa510d8cc58521c0f123f331e2
Sebastian Stamm <sebastian.stamm@camunda.com>
2021-07-06 16:50:10 +0200

    test(Reports): add e2e tests for multi-definition reports
    related to OPT-5264

--- client/e2e/tests/Filter.elements.js
+++ client/e2e/tests/Filter.elements.js
@@ -37,3 +37,5 @@ export const customValueCheckbox = Selector('.Modal .customValueCheckbox');
 export const addValueButton = Selector('.Modal .customValueButton');
 export const customValueInput = Selector('.Modal .customValueInput input');
 export const addValueToListButton = Selector('.Modal .customValueInput button');
+export const removeButtonFor = (text) =>
+  Selector('.Modal__content .Tag').withText(text).find('.close.Button');
