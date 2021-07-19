93c5fb968b35085c9cc2d893ab335b3ba0565a20
huygur <45518829+huygur@users.noreply.github.com>
2021-07-19 06:25:11 +0200

    chore(storybook): add stories for dashboard and its components (#1435)

---
+++ client/src/App/Dashboard/MetricPanel/stories.tsx
@@ -0,0 +1,80 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a commercial license.
+ * You may not use this file except in compliance with the commercial license.
+ */
+
+/* istanbul ignore file */
+
+import {MemoryRouter} from 'react-router-dom';
+import {Story} from '@storybook/react';
+
+import {MetricPanel} from './index';
+import {statisticsStore} from 'modules/stores/statistics';
+import {rest} from 'msw';
+import {useEffect} from 'react';
+
+export default {
+  title: 'Components/MetricPanel',
+};
+
+const Metrics: Story = () => {
+  useEffect(() => {
+    statisticsStore.init();
+    return () => statisticsStore.reset();
+  }, []);
+
+  return (
+    <MemoryRouter>
+      <MetricPanel />
+    </MemoryRouter>
+  );
+};
+
+Metrics.parameters = {
+  msw: [
+    rest.get('/api/process-instances/core-statistics', (_, res, ctx) => {
+      return res(
+        ctx.json({
+          running: 1087,
+          active: 210,
+          withIncidents: 877,
+        })
+      );
+    }),
+  ],
+};
+
+const MetricsFailed: Story = () => {
+  useEffect(() => {
+    statisticsStore.init();
+    return () => statisticsStore.reset();
+  }, []);
+
+  return (
+    <MemoryRouter>
+      <MetricPanel />
+    </MemoryRouter>
+  );
+};
+
+MetricsFailed.storyName = 'Metrics - Failed';
+MetricsFailed.parameters = {
+  msw: [
+    rest.get('/api/process-instances/core-statistics', (_, res, ctx) => {
+      return res(ctx.status(500), ctx.json({}));
+    }),
+  ],
+};
+
+const MetricsLoading: Story = () => {
+  return (
+    <MemoryRouter>
+      <MetricPanel />
+    </MemoryRouter>
+  );
+};
+
+MetricsLoading.storyName = 'Metrics - Loading';
+
+export {Metrics, MetricsFailed, MetricsLoading};
