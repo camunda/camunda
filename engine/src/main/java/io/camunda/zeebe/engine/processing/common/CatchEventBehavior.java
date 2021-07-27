17c7019c463a0aefcda8f6d6a8f85fd9cf8961dc
Nico Korthout <nico.korthout@camunda.com>
2021-07-27 16:02:56 +0200

    refactor(engine): replace event subscription exceptions with either
    Replaces the usage of exceptions in the catch event and bpmn event
    subscription behavior classes with only eithers.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/common/CatchEventBehavior.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/common/CatchEventBehavior.java
@@ -8,15 +8,12 @@
 package io.camunda.zeebe.engine.processing.common;
 
 import static io.camunda.zeebe.util.buffer.BufferUtil.cloneBuffer;
-import static java.util.stream.Collectors.toMap;
 
 import io.camunda.zeebe.el.Expression;
 import io.camunda.zeebe.engine.processing.bpmn.BpmnElementContext;
-import io.camunda.zeebe.engine.processing.common.ExpressionProcessor.EvaluationException;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableCatchEvent;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableCatchEventSupplier;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableMessage;
-import io.camunda.zeebe.engine.processing.message.MessageCorrelationKeyException;
 import io.camunda.zeebe.engine.processing.message.command.SubscriptionCommandSender;
 import io.camunda.zeebe.engine.processing.streamprocessor.sideeffect.SideEffects;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
@@ -27,7 +24,6 @@ import io.camunda.zeebe.engine.state.immutable.ProcessMessageSubscriptionState;
 import io.camunda.zeebe.engine.state.immutable.TimerInstanceState;
 import io.camunda.zeebe.engine.state.instance.TimerInstance;
 import io.camunda.zeebe.engine.state.message.ProcessMessageSubscription;
-import io.camunda.zeebe.engine.state.mutable.MutableEventScopeInstanceState;
 import io.camunda.zeebe.engine.state.mutable.MutableZeebeState;
 import io.camunda.zeebe.model.bpmn.util.time.Timer;
 import io.camunda.zeebe.protocol.impl.SubscriptionUtil;
@@ -38,10 +34,9 @@ import io.camunda.zeebe.protocol.record.intent.TimerIntent;
 import io.camunda.zeebe.protocol.record.value.BpmnElementType;
 import io.camunda.zeebe.util.Either;
 import io.camunda.zeebe.util.buffer.BufferUtil;
-import io.camunda.zeebe.util.collection.Tuple;
 import io.camunda.zeebe.util.sched.clock.ActorClock;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 import org.agrona.DirectBuffer;
 
 public final class CatchEventBehavior {
@@ -51,7 +46,6 @@ public final class CatchEventBehavior {
   private final int partitionsCount;
   private final StateWriter stateWriter;
 
-  private final MutableEventScopeInstanceState eventScopeInstanceState;
   private final ProcessMessageSubscriptionState processMessageSubscriptionState;
   private final TimerInstanceState timerInstanceState;
 
@@ -73,7 +67,6 @@ public final class CatchEventBehavior {
     this.stateWriter = stateWriter;
     this.partitionsCount = partitionsCount;
 
-    eventScopeInstanceState = zeebeState.getEventScopeInstanceState();
     timerInstanceState = zeebeState.getTimerState();
     processMessageSubscriptionState = zeebeState.getProcessMessageSubscriptionState();
 
@@ -91,53 +84,57 @@ public final class CatchEventBehavior {
     unsubscribeFromMessageEvents(context, sideEffects);
   }
 
-  public void subscribeToEvents(
+  /** @return either a failure or a list of the events it subscribed to */
+  public Either<Failure, List<ExecutableCatchEvent>> subscribeToEvents(
       final BpmnElementContext context,
       final ExecutableCatchEventSupplier supplier,
       final SideEffects sideEffects,
-      final TypedCommandWriter commandWriter)
-      throws MessageCorrelationKeyException {
+      final TypedCommandWriter commandWriter) {
 
     final List<ExecutableCatchEvent> events = supplier.getEvents();
 
-    // collect all message names from their respective variables, as this might fail and
-    // we might need to raise an incident
-    final Either<Failure, Map<DirectBuffer, DirectBuffer>> extractedMessageNames =
-        evaluateMessageNames(events, context);
-    // collect all message correlation keys from their respective variables, as this might fail and
-    // we might need to raise an incident. This works the same for timers
-    final Either<Failure, Map<DirectBuffer, DirectBuffer>> extractedCorrelationKeys =
-        evaluateMessageCorrelationKeys(events, context);
-    final Either<Failure, Map<DirectBuffer, Timer>> evaluatedTimers =
-        evaluateTimers(events, context);
+    final var failureOrEvalResult =
+        events.stream()
+            .filter(event -> event.isTimer() || event.isMessage())
+            .map(event -> evalExpressions(event, context))
+            .collect(Either.collector());
 
-    if (extractedMessageNames.isLeft()
-        || extractedCorrelationKeys.isLeft()
-        || evaluatedTimers.isLeft()) {
-      // todo: don't throw here, but return an either
-      throw new EvaluationException(evaluatedTimers.getLeft().getMessage());
-    }
+    // todo introduce peek
+    failureOrEvalResult.ifRight(
+        results ->
+            results.forEach(
+                result -> {
+                  final var event = result.event;
+                  if (event.isMessage()) {
+                    final var correlationKey = result.correlationKey;
+                    final var messageName = result.messageName;
+                    subscribeToMessageEvent(
+                        context, event, correlationKey, messageName, sideEffects);
+                  } else if (event.isTimer()) {
+                    final var timer = result.timer;
+                    subscribeToTimerEvent(
+                        context.getElementInstanceKey(),
+                        context.getProcessInstanceKey(),
+                        context.getProcessDefinitionKey(),
+                        event.getId(),
+                        timer,
+                        commandWriter,
+                        sideEffects);
+                  }
+                }));
 
-    // if all subscriptions are valid then open the subscriptions
-    for (final ExecutableCatchEvent event : events) {
-      if (event.isTimer()) {
-        subscribeToTimerEvent(
-            context.getElementInstanceKey(),
-            context.getProcessInstanceKey(),
-            context.getProcessDefinitionKey(),
-            event.getId(),
-            evaluatedTimers.get().get(event.getId()),
-            commandWriter,
-            sideEffects);
-      } else if (event.isMessage()) {
-        subscribeToMessageEvent(
-            context,
-            event,
-            extractedCorrelationKeys.get().get(event.getId()),
-            extractedMessageNames.get().get((event.getId())),
-            sideEffects);
-      }
-    }
+    // we can only deal with the first failure, so it's enough to return that
+    return failureOrEvalResult
+        .mapLeft(f -> f.get(0))
+        .map(r -> r.stream().map(EvalResult::event).collect(Collectors.toList()));
+  }
+
+  private Either<Failure, EvalResult> evalExpressions(
+      final ExecutableCatchEvent event, final BpmnElementContext context) {
+    return evaluateMessageName(event, context)
+        .map(name -> new EvalResult(event).messageName(name))
+        .flatMap(result -> evaluateCorrelationKey(event, context).map(result::correlationKey))
+        .flatMap(result -> evaluateTimer(event, context).map(result::timer));
   }
 
   public void subscribeToTimerEvent(
@@ -280,59 +277,11 @@ public final class CatchEventBehavior {
         closeOnCorrelate);
   }
 
-  /**
-   * Evaluates all message names for events
-   *
-   * @param events All catch events to evaluate to message names
-   * @param context element context used to determine the evaluation scope
-   * @return either the first failure it encounters or a mapping of event-ids to message names
-   */
-  private Either<Failure, Map<DirectBuffer, DirectBuffer>> evaluateMessageNames(
-      final List<ExecutableCatchEvent> events, final BpmnElementContext context) {
-    return events.stream()
-        .filter(ExecutableCatchEvent::isMessage)
-        .map(e -> evaluateMessageName(e, context).map(key -> Tuple.of(e.getId(), key)))
-        .collect(Either.collector())
-        .mapLeft(failures -> failures.get(0))
-        .map(t -> t.stream().collect(toMap(Tuple::getLeft, Tuple::getRight)));
-  }
-
-  /**
-   * Evaluates all message correlation keys for events
-   *
-   * @param events All catch events to evaluate to message correlation keys
-   * @param context element context used to determine the evaluation scope
-   * @return either the first failure it encounters or a mapping of event-ids to correlation keys
-   */
-  private Either<Failure, Map<DirectBuffer, DirectBuffer>> evaluateMessageCorrelationKeys(
-      final List<ExecutableCatchEvent> events, final BpmnElementContext context) {
-    return events.stream()
-        .filter(ExecutableCatchEvent::isMessage)
-        .map(e -> evaluateCorrelationKey(e, context).map(key -> Tuple.of(e.getId(), key)))
-        .collect(Either.collector())
-        .mapLeft(failures -> failures.get(0))
-        .map(t -> t.stream().collect(toMap(Tuple::getLeft, Tuple::getRight)));
-  }
-
-  /**
-   * Evaluates all timer events
-   *
-   * @param events All catch events to evaluate to timers, any non-timer catch events are skipped
-   * @param context element context used to determine the evaluation scope
-   * @return either the first failure it encounters or a mapping of event-ids to timers
-   */
-  private Either<Failure, Map<DirectBuffer, Timer>> evaluateTimers(
-      final List<ExecutableCatchEvent> events, final BpmnElementContext context) {
-    return events.stream()
-        .filter(ExecutableCatchEvent::isTimer)
-        .map(e -> evaluateTimer(e, context).map(timer -> Tuple.of(e.getId(), timer)))
-        .collect(Either.collector())
-        .mapLeft(failures -> failures.get(0))
-        .map(t -> t.stream().collect(toMap(Tuple::getLeft, Tuple::getRight)));
-  }
-
   private Either<Failure, DirectBuffer> evaluateCorrelationKey(
       final ExecutableCatchEvent event, final BpmnElementContext context) {
+    if (!event.isMessage()) {
+      return Either.right(null);
+    }
     final var expression = event.getMessage().getCorrelationKeyExpression();
     final long scopeKey =
         event.getElementType() == BpmnElementType.BOUNDARY_EVENT
@@ -340,23 +289,59 @@ public final class CatchEventBehavior {
             : context.getElementInstanceKey();
     return expressionProcessor
         .evaluateMessageCorrelationKeyExpression(expression, scopeKey)
-        .map(BufferUtil::wrapString);
+        .map(BufferUtil::wrapString)
+        .mapLeft(f -> new Failure(f.getMessage(), f.getErrorType(), scopeKey));
   }
 
-  /** @return either a failure or an evaluated timer */
   private Either<Failure, Timer> evaluateTimer(
       final ExecutableCatchEvent event, final BpmnElementContext context) {
+    if (!event.isTimer()) {
+      return Either.right(null);
+    }
     final var scopeKey = context.getElementInstanceKey();
     return event.getTimerFactory().apply(expressionProcessor, scopeKey);
   }
 
   private Either<Failure, DirectBuffer> evaluateMessageName(
       final ExecutableCatchEvent event, final BpmnElementContext context) {
+    if (!event.isMessage()) {
+      return Either.right(null);
+    }
     final var scopeKey = context.getElementInstanceKey();
     final ExecutableMessage message = event.getMessage();
     final Expression messageNameExpression = message.getMessageNameExpression();
     return expressionProcessor
         .evaluateStringExpression(messageNameExpression, scopeKey)
         .map(BufferUtil::wrapString);
+  }
+
+  private static class EvalResult {
+    private final ExecutableCatchEvent event;
+    private DirectBuffer messageName;
+    private DirectBuffer correlationKey;
+    private Timer timer;
+
+    public EvalResult(final ExecutableCatchEvent event) {
+      this.event = event;
+    }
+
+    public EvalResult messageName(final DirectBuffer messageName) {
+      this.messageName = messageName;
+      return this;
+    }
+
+    public EvalResult correlationKey(final DirectBuffer correlationKey) {
+      this.correlationKey = correlationKey;
+      return this;
+    }
+
+    public EvalResult timer(final Timer timer) {
+      this.timer = timer;
+      return this;
+    }
+
+    public ExecutableCatchEvent event() {
+      return event;
+    }
   }
 }
