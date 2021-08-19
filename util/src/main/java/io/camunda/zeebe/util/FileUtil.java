a0bfae3d449073c00a79f4c8a863c31f49141484
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-08-19 17:09:17 +0200

    fix(util): flushDirectory is a no-op on Windows
    Ensure flushign a directory is a no-op on Windows, as Windows will not
    let you do so, and it's also not required there after creating a file or
    renaming it.

--- util/src/main/java/io/camunda/zeebe/util/FileUtil.java
+++ util/src/main/java/io/camunda/zeebe/util/FileUtil.java
@@ -25,6 +25,7 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Objects;
+import org.agrona.SystemUtil;
 import org.slf4j.Logger;
 
 public final class FileUtil {
@@ -34,10 +35,9 @@ public final class FileUtil {
 
   /**
    * Ensures updates to the given path are flushed to disk, with the same guarantees as {@link
-   * FileChannel#force(boolean)}. This can be used for both directory and files. Note that if you
-   * already have a file channel open, then use the {@link FileChannel#force(boolean)} method
-   * directly. This method is mostly useful when flushing directories after file creation and/or
-   * file rename operations.
+   * FileChannel#force(boolean)}. This can be used for files only; for directories, call {@link
+   * #flushDirectory(Path)}. Note that if you already have a file channel open, then use the {@link
+   * FileChannel#force(boolean)} method directly.
    *
    * @param path the path to synchronize
    * @throws IOException can be thrown on opening and on flushing the file
@@ -46,6 +46,27 @@ public final class FileUtil {
     try (final var channel = FileChannel.open(path, StandardOpenOption.READ)) {
       channel.force(true);
     }
+  }
+
+  /**
+   * This method flushes the given directory. Note that on Windows this is a no-op, as Windows does
+   * not allow to flush directories except when opening the sys handle with a specific backup flag,
+   * which is not possible to do without custom JNI code. However, as the main use case here is for
+   * durability to sync the parent directory after creating a new file or renaming it, this is safe
+   * to do as Windows (or rather NTFS) does not need this.
+   *
+   * @param path the path to synchronize
+   * @throws IOException can be thrown on opening and on flushing the file
+   */
+  public static void flushDirectory(final Path path) throws IOException {
+    // Windows does not allow flushing a directory except under very specific conditions which are
+    // not possible to produce with the standard JDK; it's also not necessary to flush a directory
+    // in Windows
+    if (SystemUtil.isWindows()) {
+      return;
+    }
+
+    flush(path);
   }
 
   /**
@@ -61,7 +82,7 @@ public final class FileUtil {
   public static void moveDurably(final Path source, final Path target, final CopyOption... options)
       throws IOException {
     Files.move(source, target, options);
-    flush(target.getParent());
+    flushDirectory(target.getParent());
   }
 
   public static void deleteFolder(final String path) throws IOException {
