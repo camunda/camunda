0b252d7e03f4a21ceddb8c9395db67bd9e1141ce
Christopher Zell <zelldon91@googlemail.com>
2021-08-30 16:48:28 +0200

    test: create install req handling test class
    One test which makes sure that the follower is able to become leader, to make sure that even when log was compacted the new snapshot is readable and he can work with it.
    One test which makes sure that the follower after receiving and snapshot and doing some replay can also become leader and complete an instance, ensures that replay still works.

---
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/clustering/InstallRequestHandlingTest.java
@@ -0,0 +1,97 @@
+package io.camunda.zeebe.it.clustering;
+
+import io.camunda.zeebe.it.util.GrpcClientRule;
+import io.camunda.zeebe.it.util.ZeebeAssertHelper;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.protocol.record.intent.JobIntent;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import java.time.Duration;
+import org.awaitility.Awaitility;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.Timeout;
+
+public class InstallRequestHandlingTest {
+  private static final Duration SNAPSHOT_PERIOD = Duration.ofMinutes(5);
+
+  public final Timeout testTimeout = Timeout.seconds(120);
+  public final ClusteringRule clusteringRule =
+      new ClusteringRule(
+          1,
+          3,
+          3,
+          config -> {
+            config.getData().setSnapshotPeriod(SNAPSHOT_PERIOD);
+          });
+  public final GrpcClientRule clientRule = new GrpcClientRule(clusteringRule);
+
+  @Rule
+  public RuleChain ruleChain =
+      RuleChain.outerRule(testTimeout).around(clusteringRule).around(clientRule);
+
+  @Test
+  public void shouldBeAbleToBecomeLeader() {
+    // given
+    final var followerId = clusteringRule.stopAnyFollower();
+    final var jobKey = clientRule.createSingleJob("type");
+
+    clusteringRule.triggerAndWaitForSnapshots();
+
+    // when
+    clusteringRule.startBroker(followerId);
+
+    // then
+    clusteringRule.waitForSnapshotAtBroker(followerId);
+    stepDownUntilRightLeaderIsChosen(followerId);
+
+    clientRule.getClient().newCompleteCommand(jobKey).send().join();
+    ZeebeAssertHelper.assertProcessInstanceCompleted("process");
+  }
+
+  @Test
+  public void shouldBeAbleToContinueWithReplayState() {
+    // given
+    final var followerId = clusteringRule.stopAnyFollower();
+
+    final var processDefinitionKey =
+        clientRule.deployProcess(
+            Bpmn.createExecutableProcess("process")
+                .startEvent()
+                .serviceTask("task", task -> task.zeebeJobType("type"))
+                .endEvent()
+                .done());
+    clusteringRule.triggerAndWaitForSnapshots();
+    clusteringRule.startBroker(followerId);
+    clusteringRule.waitForSnapshotAtBroker(followerId);
+
+    // when
+    clientRule.createProcessInstance(processDefinitionKey);
+
+    // then
+    stepDownUntilRightLeaderIsChosen(followerId);
+
+    final var jobKey =
+        RecordingExporter.jobRecords(JobIntent.CREATED)
+            .withType("type")
+            .withElementId("task")
+            .getFirst()
+            .getKey();
+    clientRule.getClient().newCompleteCommand(jobKey).send().join();
+    ZeebeAssertHelper.assertProcessInstanceCompleted("process");
+  }
+
+  private void stepDownUntilRightLeaderIsChosen(final int expectedLeader) {
+    do {
+      final var previousLeader = clusteringRule.getCurrentLeaderForPartition(1);
+      clusteringRule.stepDown(previousLeader.getNodeId(), 1);
+      Awaitility.await()
+          .pollInterval(Duration.ofMillis(100))
+          .atMost(Duration.ofSeconds(10))
+          .until(
+              () -> clusteringRule.getCurrentLeaderForPartition(1),
+              newLeader -> !previousLeader.equals(newLeader));
+
+    } while (clusteringRule.getCurrentLeaderForPartition(1).getNodeId() != expectedLeader);
+  }
+}
