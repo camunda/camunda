f8e76572060eccbe2ee5a16c211d2669bcdd55c7
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2021-09-17 09:15:03 +0200

    test(broker): test classes can be private

--- broker/src/test/java/io/camunda/zeebe/broker/system/partitions/impl/steps/SnapshotDirectorPartitionTransitionStepTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/system/partitions/impl/steps/SnapshotDirectorPartitionTransitionStepTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class SnapshotDirectorPartitionTransitionStepTest {
+class SnapshotDirectorPartitionTransitionStepTest {
 
   TestPartitionTransitionContext transitionContext = new TestPartitionTransitionContext();
 
