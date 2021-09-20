7cab31de1c0c14d0897fe4b38e02e4856574d3d7
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-09-20 15:18:16 +0200

    build(ci): skip checks on upload

--- .ci/scripts/distribution/upload.sh
+++ .ci/scripts/distribution/upload.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -eux
 
-mvn -B -T1C -s ${MAVEN_SETTINGS_XML} generate-sources source:jar javadoc:jar deploy -DskipTests
+mvn -B -T1C -s ${MAVEN_SETTINGS_XML} generate-sources source:jar javadoc:jar deploy -DskipTests -DskipChecks
