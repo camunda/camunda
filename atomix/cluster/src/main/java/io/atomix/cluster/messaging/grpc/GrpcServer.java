c8acf0981893eb1e9f92a156cfb9d0ad825fea42
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-09-20 10:15:38 +0200

    style: licensing

--- atomix/cluster/src/main/java/io/atomix/cluster/messaging/grpc/GrpcServer.java
+++ atomix/cluster/src/main/java/io/atomix/cluster/messaging/grpc/GrpcServer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2020 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.atomix.cluster.messaging.grpc;
 
 import io.atomix.cluster.messaging.MessagingConfig;
@@ -54,7 +69,8 @@ final class GrpcServer implements AutoCloseable {
   private Server buildServer() {
     final var firstAddress = bindAddresses.get(0);
     final var builder =
-        transportFactory.createServerBuilder(firstAddress.resolve())
+        transportFactory
+            .createServerBuilder(firstAddress.resolve())
             .compressorRegistry(CompressorRegistry.getDefaultInstance())
             .addService(messagingService);
     return builder.build();
--- atomix/cluster/src/main/java/io/atomix/cluster/messaging/grpc/service/SyncMessagingHandler.java
+++ atomix/cluster/src/main/java/io/atomix/cluster/messaging/grpc/service/SyncMessagingHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2020 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.atomix.cluster.messaging.grpc.service;
 
 import com.google.protobuf.ByteString;
@@ -20,7 +35,9 @@ public class SyncMessagingHandler extends RequestHandler<Response> {
 
   @Override
   protected void handle(
-      final Address replyTo, final byte[] payload, final StreamObserver<Response> responseObserver) {
+      final Address replyTo,
+      final byte[] payload,
+      final StreamObserver<Response> responseObserver) {
     final var responsePayload = handler.apply(replyTo, payload);
     final var response =
         Response.newBuilder().setPayload(ByteString.copyFrom(responsePayload)).build();
