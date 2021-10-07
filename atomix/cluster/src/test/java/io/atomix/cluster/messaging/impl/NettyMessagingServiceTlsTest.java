84709d86e23aeceb494907489fd4d123a2dd27da
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-10-07 18:40:10 +0200

    feat(atomix/cluster): allow secure inter-node communication over TLS
    Introduces security configuration similar to the gateway's for
    inter-node communication. Users can now configure a certificate chain
    and private key which is loaded by the NettyMessagingService in order to
    encrypt communication between all cluster nodes. TLS version is fixed at
    1.3, and uses BoringSSL via the tcnative artifact provided by Netty.
    
    This solution assumes all nodes share the same certificates, as we load
    the certificate chain as part of the trust manager for client
    connections, and there's no real way to specify a different trust
    manager. Assuming they share the same intermediate chain, it could be
    possible to have different final certificates, but this is not tested.

---
+++ atomix/cluster/src/test/java/io/atomix/cluster/messaging/impl/NettyMessagingServiceTlsTest.java
@@ -0,0 +1,106 @@
+package io.atomix.cluster.messaging.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.atomix.cluster.messaging.MessagingConfig;
+import io.atomix.utils.net.Address;
+import io.camunda.zeebe.test.util.socket.SocketUtil;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+import java.net.ConnectException;
+import java.security.cert.CertificateException;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.Test;
+
+final class NettyMessagingServiceTlsTest {
+  @Test
+  void shouldCommunicateOverTls() throws CertificateException {
+    // given
+    final var certificate = new SelfSignedCertificate();
+    final var client = createSecureMessagingService(certificate);
+    final var server = createSecureMessagingService(certificate);
+    final var payload = "foo".getBytes();
+
+    // when
+    client.start().join();
+    server.start().join();
+    server.registerHandler(
+        "topic",
+        (sender, request) ->
+            CompletableFuture.completedFuture((new String(request) + "bar").getBytes()));
+    final var response = client.sendAndReceive(server.address(), "topic", payload).join();
+
+    // then
+    assertThat(response).isEqualTo("foobar".getBytes());
+  }
+
+  @Test
+  void shouldFailWhenClientIsNotUsingTls() throws CertificateException {
+    // given
+    final var certificate = new SelfSignedCertificate();
+    final var client = createInsecureMessagingService();
+    final var server = createSecureMessagingService(certificate);
+    final var payload = "foo".getBytes();
+
+    // when
+    client.start().join();
+    server.start().join();
+    server.registerHandler(
+        "topic",
+        (sender, request) ->
+            CompletableFuture.completedFuture((new String(request) + "bar").getBytes()));
+    final var response =
+        client.sendAndReceive(server.address(), "topic", payload, true, Duration.ofSeconds(10));
+
+    // then
+    assertThat(response)
+        .failsWithin(Duration.ofSeconds(10))
+        .withThrowableOfType(ExecutionException.class)
+        .havingRootCause()
+        .isInstanceOf(ConnectException.class);
+  }
+
+  @Test
+  void shouldFailWhenServerIsNotUsingTls() throws CertificateException {
+    // given
+    final var certificate = new SelfSignedCertificate();
+    final var server = createInsecureMessagingService();
+    final var client = createSecureMessagingService(certificate);
+    final var payload = "foo".getBytes();
+
+    // when
+    client.start().join();
+    server.start().join();
+    server.registerHandler(
+        "topic",
+        (sender, request) ->
+            CompletableFuture.completedFuture((new String(request) + "bar").getBytes()));
+    final var response =
+        client.sendAndReceive(server.address(), "topic", payload, true, Duration.ofSeconds(1));
+
+    // then
+    assertThat(response)
+        .failsWithin(Duration.ofSeconds(2))
+        .withThrowableOfType(ExecutionException.class)
+        .havingRootCause()
+        .isInstanceOf(ConnectException.class);
+  }
+
+  private NettyMessagingService createInsecureMessagingService() {
+    final var config =
+        new MessagingConfig().setPort(SocketUtil.getNextAddress().getPort()).setTlsEnabled(false);
+    return new NettyMessagingService("cluster", Address.from(config.getPort()), config);
+  }
+
+  private NettyMessagingService createSecureMessagingService(
+      final SelfSignedCertificate certificate) {
+    final var config =
+        new MessagingConfig()
+            .setPort(SocketUtil.getNextAddress().getPort())
+            .setTlsEnabled(true)
+            .setCertificateChain(certificate.certificate())
+            .setPrivateKey(certificate.privateKey());
+    return new NettyMessagingService("cluster", Address.from(config.getPort()), config);
+  }
+}
