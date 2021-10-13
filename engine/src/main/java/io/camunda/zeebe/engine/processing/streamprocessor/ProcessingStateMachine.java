e780615d4d10a64291108b32606d0a699ef53797
Christopher Zell <zelldon91@googlemail.com>
2021-10-13 10:16:30 +0200

    refactor: use Timer in ProcessingStateMachine
    We should prefer the Prometheus Timer api, instead of calculating the
    time by our own, since it is error prone and depends on the clock we
    use.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/streamprocessor/ProcessingStateMachine.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/streamprocessor/ProcessingStateMachine.java
@@ -30,7 +30,9 @@ import io.camunda.zeebe.util.retry.AbortableRetryStrategy;
 import io.camunda.zeebe.util.retry.RecoverableRetryStrategy;
 import io.camunda.zeebe.util.retry.RetryStrategy;
 import io.camunda.zeebe.util.sched.ActorControl;
+import io.camunda.zeebe.util.sched.clock.ActorClock;
 import io.camunda.zeebe.util.sched.future.ActorFuture;
+import io.prometheus.client.Histogram;
 import java.time.Duration;
 import java.util.function.BooleanSupplier;
 import org.slf4j.Logger;
@@ -138,7 +140,7 @@ public final class ProcessingStateMachine {
   private volatile boolean onErrorHandlingLoop;
   private int onErrorRetries;
   // Used for processing duration metrics
-  private long processingStartTime;
+  private Histogram.Timer processingTimer;
 
   public ProcessingStateMachine(
       final ProcessingContext context, final BooleanSupplier shouldProcessNext) {
@@ -204,7 +206,11 @@ public final class ProcessingStateMachine {
       return;
     }
 
-    processingStartTime = System.currentTimeMillis();
+    // Here we need to get the current time, since we want to calculate
+    // how long it took between writing to the dispatcher and processing.
+    // In all other cases we should prefer to use the Prometheus Timer API.
+    final var processingStartTime = ActorClock.currentTimeMillis();
+    processingTimer = metrics.startProcessingDurationTimer(metadata.getRecordType());
 
     try {
       final UnifiedRecordValue value = recordValues.readRecordValue(event, metadata.getValueType());
@@ -403,8 +409,9 @@ public final class ProcessingStateMachine {
 
           notifyProcessedListener(typedEvent);
 
-          metrics.processingDuration(
-              metadata.getRecordType(), processingStartTime, System.currentTimeMillis());
+          // observe the processing duration
+          processingTimer.close();
+
           // continue with next event
           currentProcessor = null;
           actor.submit(this::readNextEvent);
