dc8fe2866b1061ad93481ae99582cb52a27af309
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-10-20 11:35:43 +0200

    refactor(dist): rename test class to a more descriptive name

--- dist/src/test/java/io/camunda/zeebe/gateway/StandaloneGatewaySecurityTest.java
+++ dist/src/test/java/io/camunda/zeebe/gateway/StandaloneGatewaySecurityTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-final class StandaloneGatewayTest {
+final class StandaloneGatewaySecurityTest {
   private SelfSignedCertificate certificate;
   private StandaloneGateway gateway;
 
