94fda2b6830bd49fd892acbea052d52e2deab9c5
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-10-20 17:01:03 +0200

    refactor(broker): avoid static initialization issues with new decorating rule

--- broker/src/test/java/io/camunda/zeebe/broker/BrokerNetworkSecurityTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/BrokerNetworkSecurityTest.java
@@ -14,53 +14,62 @@ import io.camunda.zeebe.test.util.asserts.SslAssert;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import java.net.SocketAddress;
 import java.security.cert.CertificateException;
+import org.agrona.LangUtil;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
 public final class BrokerNetworkSecurityTest {
-  // must be public because it must be declared before BROKER, and checkstyle flags it if we set it
-  // anything else as a false positive
-  public static final SelfSignedCertificate CERTIFICATE;
-
   @ClassRule
-  public static final EmbeddedBrokerRule BROKER =
-      new EmbeddedBrokerRule(BrokerNetworkSecurityTest::configureBroker);
-
-  static {
-    try {
-      CERTIFICATE = new SelfSignedCertificate();
-    } catch (final CertificateException e) {
-      throw new RuntimeException(e);
-    }
-  }
+  public static final SecuredEmbeddedBrokerRule BROKER_RULE = new SecuredEmbeddedBrokerRule();
 
   @Test
   public void shouldSecureCommandApi() {
     // given
     final SocketAddress commandApiAddress =
-        BROKER.getBrokerCfg().getNetwork().getCommandApi().getAddress();
+        BROKER_RULE.broker.getBrokerCfg().getNetwork().getCommandApi().getAddress();
 
     // then
-    SslAssert.assertThat(commandApiAddress).isSecuredBy(CERTIFICATE);
+    SslAssert.assertThat(commandApiAddress).isSecuredBy(BROKER_RULE.certificate);
   }
 
   @Test
   public void shouldSecureInternalApi() {
     // given
     final SocketAddress internalApiAddress =
-        BROKER.getBrokerCfg().getNetwork().getInternalApi().getAddress();
+        BROKER_RULE.broker.getBrokerCfg().getNetwork().getInternalApi().getAddress();
 
     // then
-    SslAssert.assertThat(internalApiAddress).isSecuredBy(CERTIFICATE);
+    SslAssert.assertThat(internalApiAddress).isSecuredBy(BROKER_RULE.certificate);
   }
 
-  private static void configureBroker(final BrokerCfg config) {
-    config
-        .getNetwork()
-        .setSecurity(
-            new SecurityCfg()
-                .setEnabled(true)
-                .setCertificateChainPath(CERTIFICATE.certificate())
-                .setPrivateKeyPath(CERTIFICATE.privateKey()));
+  private static final class SecuredEmbeddedBrokerRule extends ExternalResource {
+    private SelfSignedCertificate certificate;
+    private EmbeddedBrokerRule broker;
+
+    @Override
+    public Statement apply(final Statement base, final Description description) {
+      try {
+        certificate = new SelfSignedCertificate();
+      } catch (final CertificateException e) {
+        LangUtil.rethrowUnchecked(e);
+      }
+
+      broker = new EmbeddedBrokerRule(this::configureBroker);
+      final var statement = super.apply(base, description);
+      return broker.apply(statement, description);
+    }
+
+    private void configureBroker(final BrokerCfg config) {
+      config
+          .getNetwork()
+          .setSecurity(
+              new SecurityCfg()
+                  .setEnabled(true)
+                  .setCertificateChainPath(certificate.certificate())
+                  .setPrivateKeyPath(certificate.privateKey()));
+    }
   }
 }
