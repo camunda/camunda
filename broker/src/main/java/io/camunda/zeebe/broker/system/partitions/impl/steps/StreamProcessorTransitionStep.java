83526472c1b877cf611f0da21f16a98d8c9cded0
pihme <pihme@users.noreply.github.com>
2021-10-26 11:21:39 +0200

    refactor(broker): always set stream processor in context
    With this commit the stream processor is always set in the transition context, regardless of whether it could
    be started successfully or not. The main benefit is that the reference to the newly created stream processor
    is always stored in the context, and so it can be found there and closed.
    
    Previously, if the opening of the stream processor failed, it was in an undefined state - potentially running
    and potentially registered in ActorScheduler which holds a permanent reference to it.

--- broker/src/main/java/io/camunda/zeebe/broker/system/partitions/impl/steps/StreamProcessorTransitionStep.java
+++ broker/src/main/java/io/camunda/zeebe/broker/system/partitions/impl/steps/StreamProcessorTransitionStep.java
@@ -78,14 +78,13 @@ public final class StreamProcessorTransitionStep implements PartitionTransitionS
     if (shouldInstallOnTransition(targetRole, currentRole)
         || (context.getStreamProcessor() == null && targetRole != Role.INACTIVE)) {
       final StreamProcessor streamProcessor = streamProcessorCreator.apply(context, targetRole);
+      context.setStreamProcessor(streamProcessor);
       final ActorFuture<Void> openFuture = streamProcessor.openAsync(!context.shouldProcess());
       final ActorFuture<Void> future = concurrencyControl.createFuture();
 
       openFuture.onComplete(
           (nothing, err) -> {
             if (err == null) {
-              context.setStreamProcessor(streamProcessor);
-
               // Have to pause/resume it here in case the state changed after streamProcessor was
               // created
               if (!context.shouldProcess()) {
