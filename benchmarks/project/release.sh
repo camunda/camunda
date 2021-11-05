b47922a72b7d0f314ddabc70edd3eff48949459c
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-11-05 18:38:18 +0100

    build(benchmarks): adds release script for images
    Adds a release script for the benchmark worker/starter images which will
    checkout the version you need in a worktree (so isolated), build the
    images for that version, and optionally push them. To use, you would go
    to the benchmarks/project folder and run `./release.sh -p VERSION`, e.g.
    `./release.sh -p 1.2.4`.

---
+++ benchmarks/project/release.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -eu
+
+WORKTREE="$(pwd)/.release"
+function cleanup() {
+  if [ -d "${WORKTREE}" ]; then
+    popd > /dev/null 2>&1
+	  echo "Cleaning up release worktree at ${WORKTREE}"
+	  git worktree remove -f "${WORKTREE}"
+	  rm -rf "${WORKTREE}"
+  fi
+}
+trap cleanup INT TERM EXIT HUP
+
+function killChildren()  {
+  local children
+  children="$(jobs -p)"
+
+  if [ -n "${children}" ]; then
+    echo "${children}" | xargs kill -TERM
+  fi
+}
+
+# Ensure this runs in a subshell so we have our own trap function
+pushImages() {
+  trap killChildren INT TERM HUP
+  docker push "gcr.io/zeebe-io/starter:$TAG" 1>/dev/null &
+  docker push "gcr.io/zeebe-io/worker:$TAG" 1>/dev/null &
+	wait
+}
+
+if [ "$#" -eq "0" ]; then
+  echo "Usage: ./release.sh [-p] VERSION"
+  echo "For example, to release version 1.2.4:"
+  echo "  $ ./release.sh 1.2.4"
+  echo "This will build and ask the user whether to push or not"
+  echo "If you wish to push without being asked, use the \`-p\` flag. e.g.:"
+  echo "  $ ./release.sh -p 1.2.4"
+  echo "This will build and push automatically without any user interaction"
+  exit 0
+fi
+
+if [ "${1:-}" == "-p" ]; then
+  PUSH=1
+  shift
+fi
+
+VERSION=${1:-}
+if [ -n "$VERSION" ]; then
+	echo "Checking out release worktree under .worktree/release. In case of interruption, make sure to manually clean it up afterwards"
+	git worktree add -d -q "${WORKTREE}" "${VERSION}"
+	pushd "${WORKTREE}/benchmarks/project" > /dev/null 2>&1
+fi
+
+TAG=${VERSION:-SNAPSHOT}
+echo "Building gcr.io/zeebe-io/starter:${TAG}"
+docker build -t "gcr.io/zeebe-io/starter:${TAG}" --target starter .
+
+echo "Building gcr.io/zeebe-io/worker:${TAG}"
+docker build -t "gcr.io/zeebe-io/worker:${TAG}" --target worker .
+
+PUSH=${PUSH:-0}
+if [ "${PUSH}" -ne "1" ]; then
+	read -p "Push images? (y/n) " -n 1 -r
+  echo
+
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		PUSH=1
+	fi
+fi
+
+if [ "$PUSH" -eq "1" ]; then
+	echo "Pushing image gcr.io/zeebe-io/starter:$TAG and gcr.io/zeebe-io/worker:$TAG"
+  (pushImages)
+else
+	echo "Skipping push..."
+fi
