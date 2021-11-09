2d5ed67ca0e0d2ee2233f8ce2df32cb64cf9094d
Ole Schönburg <ole.schoenburg@gmail.com>
2021-11-09 12:11:05 +0100

    test(dist): Rewrite ControlledActorClockEndpointTest as a unit test

--- dist/src/test/java/io/camunda/zeebe/shared/management/ControlledActorClockEndpointTest.java
+++ dist/src/test/java/io/camunda/zeebe/shared/management/ControlledActorClockEndpointTest.java
@@ -10,94 +10,81 @@ package io.camunda.zeebe.shared.management;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.byLessThan;
 
-import io.camunda.zeebe.broker.StandaloneBroker;
+import io.camunda.zeebe.shared.management.ActorClockEndpoint.Response;
+import io.camunda.zeebe.util.sched.clock.ControlledActorClock;
 import java.time.Instant;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactory;
+import org.assertj.core.api.ObjectAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(
-    webEnvironment = WebEnvironment.RANDOM_PORT,
-    classes = StandaloneBroker.class,
-    properties = "zeebe.clock.controlled=true")
-@ActiveProfiles("test")
 final class ControlledActorClockEndpointTest {
 
-  @Autowired private TestRestTemplate restTemplate;
+  private final InstanceOfAssertFactory<Response, ObjectAssert<Response>> instanceOfRecord =
+      new InstanceOfAssertFactory<>(Response.class, Assertions::assertThat);
+
+  private final ActorClockEndpoint endpoint =
+      new ActorClockEndpoint(new ControlledActorClockService(new ControlledActorClock()));
 
   @BeforeEach
   private void resetClock() {
-    restTemplate.delete("/actuator/clock/");
+    endpoint.resetTime();
   }
 
   @Test
   void canRetrieveCurrentClock() {
-    final var response =
-        restTemplate.getForEntity("/actuator/clock/", ActorClockEndpoint.Response.class);
-    assertThat(response.getStatusCode()).matches(HttpStatus::is2xxSuccessful);
+    final var response = endpoint.getCurrentClock();
+    assertThat(response.getStatus()).isEqualTo(200);
   }
 
   @Test
   void canPinMutableClock() {
     // given
     final var millis = 1635672964533L;
-    final var request = jsonRequest(String.format("{\"epochMilli\": %s}", millis));
 
     // when
-    final var response =
-        restTemplate.postForEntity(
-            "/actuator/clock/pin", request, ActorClockEndpoint.Response.class);
+    final var response = endpoint.modify("pin", millis, null);
 
     // then
-    assertThat(response.getStatusCode()).matches(HttpStatus::is2xxSuccessful);
-    assertThat(response.getBody()).isNotNull();
-    assertThat(response.getBody().epochMilli).isEqualTo(millis);
+
+    assertThat(response.getStatus()).isEqualTo(200);
+    assertThat(response.getBody())
+        .asInstanceOf(instanceOfRecord)
+        .satisfies((body) -> assertThat(body.epochMilli).isEqualTo(millis))
+        .isNotNull();
   }
 
   @Test
   void canOffsetMutableClock() {
     // given
     final var offset = 10000L;
-    final var request = jsonRequest(String.format("{\"offsetMilli\": %s}", offset));
 
     // when
-    final var response =
-        restTemplate.postForEntity(
-            "/actuator/clock/add", request, ActorClockEndpoint.Response.class);
+    final var response = endpoint.modify("add", null, offset);
 
     // then
-    assertThat(response.getStatusCode()).matches(HttpStatus::is2xxSuccessful);
-    assertThat(response.getBody()).isNotNull();
-    assertThat(response.getBody().epochMilli)
-        .isCloseTo(Instant.now().toEpochMilli(), byLessThan(offset));
+    assertThat(response.getStatus()).isEqualTo(200);
+    assertThat(response.getBody())
+        .isNotNull()
+        .asInstanceOf(instanceOfRecord)
+        .satisfies(
+            (body) ->
+                assertThat(body.epochMilli)
+                    .isCloseTo(Instant.now().toEpochMilli(), byLessThan(offset)));
   }
 
   @Test
   void offsetClockDoesAdvance() throws InterruptedException {
     // given
     final var offset = 10000L;
-    final var request = jsonRequest(String.format("{\"offsetMilli\": %s}", offset));
-    final var firstResponse =
-        restTemplate.postForEntity(
-            "/actuator/clock/add", request, ActorClockEndpoint.Response.class);
+    final var firstResponse = endpoint.modify("add", null, offset);
     assertThat(firstResponse.getBody()).isNotNull();
-    final var firstMillis = firstResponse.getBody().epochMilli;
+    final var firstMillis = ((Response) firstResponse.getBody()).epochMilli;
 
     // when
     Thread.sleep(100);
-    final var secondResponse =
-        restTemplate.getForEntity("/actuator/clock", ActorClockEndpoint.Response.class);
+    final var secondResponse = endpoint.getCurrentClock();
     assertThat(secondResponse.getBody()).isNotNull();
     final var secondMillis = secondResponse.getBody().epochMilli;
 
@@ -109,18 +96,16 @@ final class ControlledActorClockEndpointTest {
   void pinnedClockDoesNotAdvance() throws InterruptedException {
     // given
     final var millis = 1635672964533L;
-    final var request = jsonRequest(String.format("{\"epochMilli\": %s}", millis));
-    final var firstResponse =
-        restTemplate.postForEntity(
-            "/actuator/clock/pin", request, ActorClockEndpoint.Response.class);
-    assertThat(firstResponse.getStatusCode()).matches(HttpStatus::is2xxSuccessful);
-    assertThat(firstResponse.getBody()).isNotNull();
-    assertThat(firstResponse.getBody().epochMilli).isEqualTo(millis);
+    final var firstResponse = endpoint.modify("pin", millis, null);
+    assertThat(firstResponse.getStatus()).isEqualTo(200);
+    assertThat(firstResponse.getBody())
+        .isNotNull()
+        .asInstanceOf(instanceOfRecord)
+        .satisfies((body) -> assertThat(body.epochMilli).isEqualTo(millis));
 
     // when
     Thread.sleep(100);
-    final var secondResponse =
-        restTemplate.getForEntity("/actuator/clock", ActorClockEndpoint.Response.class);
+    final var secondResponse = endpoint.getCurrentClock();
 
     // then
     assertThat(secondResponse.getBody()).isNotNull();
@@ -130,19 +115,12 @@ final class ControlledActorClockEndpointTest {
   @Test
   void instantMatchesEpochMilli() {
     // when
-    final var response =
-        restTemplate.getForEntity("/actuator/clock", ActorClockEndpoint.Response.class);
+    final var response = endpoint.getCurrentClock();
     assertThat(response.getBody()).isNotNull();
     final var millis = response.getBody().epochMilli;
     final var instant = response.getBody().instant;
 
     // then
     assertThat(Instant.ofEpochMilli(millis)).isEqualTo(instant);
-  }
-
-  private HttpEntity<String> jsonRequest(String body) {
-    final var headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    return new HttpEntity<>(body, headers);
   }
 }
