81501ff13f0bc924af50467430e86d28df79362d
Nico Korthout <nico.korthout@camunda.com>
2021-11-23 10:36:05 +0100

    test(ci): adds half covered code that sonar should detect

---
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/ThisIsMyNewClassThatIsAmazing.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.processing;
+
+public final class ThisIsMyNewClassThatIsAmazing {
+
+  public String callAMethod() {
+    if (1 + 1 == 2) {
+      return "mathematically-correct";
+    } else {
+      return "what is this world we live in?";
+    }
+  }
+}
---
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/ThisIsMyNewClassThatIsAmazingTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.processing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class ThisIsMyNewClassThatIsAmazingTest {
+
+  @Test
+  public void doATest() {
+    assertThat(new ThisIsMyNewClassThatIsAmazing().callAMethod())
+        .isEqualTo("mathematically-correct");
+  }
+}
