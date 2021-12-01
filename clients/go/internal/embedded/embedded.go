21757c68145387929bb559ab687fc0984c65b4cf
103484 <ci@camunda.com>
2021-12-01 16:10:19 +0000

    chore(project): update go embedded version data

--- clients/go/internal/embedded/embedded.go
+++ clients/go/internal/embedded/embedded.go
@@ -78,7 +78,7 @@ func (fi bindataFileInfo) Sys() interface{} {
 	return nil
 }
 
-var _version = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\x32\xd4\x33\xd2\x33\xd5\x0d\xf6\x73\x0c\x08\xf6\xf0\x0f\xe1\x02\x04\x00\x00\xff\xff\x16\xce\x26\x2c\x0f\x00\x00\x00")
+var _version = []byte("\x1f\x8b\x08\x00\x00\x00\x00\x00\x00\xff\x32\xd4\x33\xd2\x33\xe5\x02\x04\x00\x00\xff\xff\xa6\x94\x00\x2a\x06\x00\x00\x00")
 
 func versionBytes() ([]byte, error) {
 	return bindataRead(
@@ -93,7 +93,7 @@ func version() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "VERSION", size: 15, mode: os.FileMode(420), modTime: time.Unix(1636121217, 0)}
+	info := bindataFileInfo{name: "VERSION", size: 6, mode: os.FileMode(420), modTime: time.Unix(1638375019, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
