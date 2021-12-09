3a7cf18494accc2d762d121ccefd260974639591
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2021-12-09 13:53:00 +0100

    fix(broker): ensure ongoing transition are not stuck while closing ZeebePartition
    When `super.closeAsync` is called, it discards jobs from the Actor's queue. This resulted in an ongoing transition to get stuck, because the jobs related to it were discarded. Transition to inactive triggered by the close would then never completes, because it is waiting for the ongoing transition to complete. Thus ZeebePartition cannot be closed.
    In this commit, we fix this behavior by ensuring that transitionToActive is called before actor close is requested. We also ensure that no new transitions can be enqueued, once the closing is started. This is important because we don't want new transition after it has all steps are closed by transitioning to inactive.

--- broker/src/main/java/io/camunda/zeebe/broker/system/partitions/ZeebePartition.java
+++ broker/src/main/java/io/camunda/zeebe/broker/system/partitions/ZeebePartition.java
@@ -57,6 +57,7 @@ public final class ZeebePartition extends Actor
   private final PartitionTransition transition;
   private CompletableActorFuture<Void> closeFuture;
   private ActorFuture<Void> currentTransitionFuture;
+  private boolean closing = false;
 
   public ZeebePartition(
       final PartitionStartupAndTransitionContextImpl transitionContext,
@@ -143,38 +144,22 @@ public final class ZeebePartition extends Actor
 
   @Override
   protected void onActorClosing() {
-    context.getRaftPartition().getServer().removeSnapshotReplicationListener(this);
-    context.getRaftPartition().removeRoleChangeListener(this);
-
-    transitionToInactive()
+    // Already transitioned to inactive
+    startupProcess
+        .shutdown(actor, startupContext)
         .onComplete(
-            (nothing, err) -> {
-              context
-                  .getComponentHealthMonitor()
-                  .removeComponent(context.getRaftPartition().name());
+            (newStartupContext, error) -> {
+              if (error != null) {
+                LOG.error(error.getMessage(), error);
+              }
 
-              startupProcess
-                  .shutdown(actor, startupContext)
-                  .onComplete(
-                      (newStartupContext, error) -> {
-                        if (error != null) {
-                          LOG.error(error.getMessage(), error);
-                        }
+              // reset contexts to null to not have lingering references that could
+              // cause OOM problems in tests which start/stop partitions
+              startupContext = null;
+              context = null;
 
-                        // reset contexts to null to not have lingering references that could
-                        // cause OOM problems in tests which start/stop partitions
-                        startupContext = null;
-                        context = null;
-
-                        closeFuture.complete(null);
-                      });
+              closeFuture.complete(null);
             });
-  }
-
-  @Override
-  protected void onActorCloseRequested() {
-    LOG.debug("Closing ZeebePartition {}", context.getPartitionId());
-    context.getComponentHealthMonitor().removeComponent(zeebePartitionHealth.getName());
   }
 
   @Override
@@ -186,10 +171,19 @@ public final class ZeebePartition extends Actor
     closeFuture = new CompletableActorFuture<>();
 
     actor.run(
-        () ->
-            // allows to await current transition to avoid concurrent modifications and
-            // transitioning
-            currentTransitionFuture.onComplete((nothing, err) -> super.closeAsync()));
+        () -> {
+          LOG.debug("Closing ZeebePartition {}", context.getPartitionId());
+          closing = true;
+
+          removeListeners();
+          context.getComponentHealthMonitor().removeComponent(zeebePartitionHealth.getName());
+
+          final var inactiveTransitionFuture = transitionToInactive();
+
+          // allows to await current transition to avoid concurrent modifications and
+          // transitioning
+          inactiveTransitionFuture.onComplete((nothing, err) -> super.closeAsync());
+        });
 
     return closeFuture;
   }
@@ -210,7 +204,12 @@ public final class ZeebePartition extends Actor
   @Override
   @Deprecated // will be removed from public API of ZeebePartition
   public void onNewRole(final Role newRole, final long newTerm) {
-    actor.run(() -> onRoleChange(newRole, newTerm));
+    actor.run(
+        () -> {
+          if (!closing) {
+            onRoleChange(newRole, newTerm);
+          }
+        });
   }
 
   private void onRoleChange(final Role newRole, final long newTerm) {
@@ -298,6 +297,12 @@ public final class ZeebePartition extends Actor
     context.getRaftPartition().addRoleChangeListener(this);
     context.getComponentHealthMonitor().addFailureListener(this);
     context.getRaftPartition().getServer().addSnapshotReplicationListener(this);
+  }
+
+  private void removeListeners() {
+    context.getRaftPartition().removeRoleChangeListener(this);
+    context.getComponentHealthMonitor().removeFailureListener(this);
+    context.getRaftPartition().getServer().removeSnapshotReplicationListener(this);
   }
 
   @Override
@@ -455,14 +460,24 @@ public final class ZeebePartition extends Actor
     // restart from a new state. So we transition to Inactive to close existing services. The
     // services will be reinstalled when snapshot replication is completed.
     // We do not want to mark it as unhealthy, hence we don't reuse transitionToInactive()
-    actor.run(() -> currentTransitionFuture = transition.toInactive(context.getCurrentTerm()));
+    actor.run(
+        () -> {
+          if (!closing) {
+            currentTransitionFuture = transition.toInactive(context.getCurrentTerm());
+          }
+        });
   }
 
   @Override
   public void onSnapshotReplicationCompleted(final long term) {
     // Snapshot is received only by the followers. Hence we can safely assume that we have to
     // re-install follower services.
-    actor.run(() -> currentTransitionFuture = followerTransition(term));
+    actor.run(
+        () -> {
+          if (!closing) {
+            currentTransitionFuture = followerTransition(term);
+          }
+        });
   }
 
   public ActorFuture<Role> getCurrentRole() {
