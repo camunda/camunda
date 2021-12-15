f68169b214c0e8fdc95616a9c166dd654db7b0c6
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-12-15 18:42:29 +0100

    build(ci): update scripts to use pre-fetched binaries

--- .ci/scripts/release/compat-update-java.sh
+++ .ci/scripts/release/compat-update-java.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -xue
 
-if [[ "$RELEASE_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  mvn -B -s ${MAVEN_SETTINGS_XML} versions:set-property -DgenerateBackupPoms=false -Dproperty=backwards.compat.version -DnewVersion=${RELEASE_VERSION}
-
-  FILE=$(mvn -B -s ${MAVEN_SETTINGS_XML} help:evaluate -Dexpression=ignored.changes.file -q -DforceStdout)
-
-  rm -f clients/java/$FILE test/$FILE exporter-api/$FILE protocol/$FILE bpmn-model/$FILE
-
-  git commit -am "chore(project): update java compat versions"
-  git push origin ${RELEASE_BRANCH}
-else
-  echo "Skipping updating the compat version as $RELEASE_VERSION is not a stable version"
+if [[ ! "$RELEASE_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Skipping updating the compat version as ${RELEASE_VERSION} is not a stable version"
+  exit 0
 fi
+
+mvn -B -s "${MAVEN_SETTINGS_XML}" versions:set-property -DgenerateBackupPoms=false -Dproperty=backwards.compat.version -DnewVersion="${RELEASE_VERSION}"
+FILE=$(mvn -B -s "${MAVEN_SETTINGS_XML}" help:evaluate -Dexpression=ignored.changes.file -q -DforceStdout)
+rm -f "clients/java/${FILE}" "test/${FILE}" "exporter-api/${FILE}" "protocol/${FILE}" "bpmn-model/${FILE}"
+git commit -am "build(project): update java compat versions"
+git push origin "${RELEASE_BRANCH}"
--- .ci/scripts/release/github-release.sh
+++ .ci/scripts/release/github-release.sh
@@ -4,31 +4,29 @@ export GITHUB_TOKEN=${GITHUB_TOKEN_PSW}
 export GITHUB_ORG=camunda-cloud
 export GITHUB_REPO=zeebe
 
-curl -sL  https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2 | bzip2 -fd - > github-release
-chmod +x github-release
-GITHUB_RELEASE=${PWD}/github-release
+BINDIR=${BINDIR:-/tmp}
+GITHUB_RELEASE=${GITHUB_RELEASE:-"${BINDIR}/github-release"}
+"${GITHUB_RELEASE}" release --user "${GITHUB_ORG}" --repo "${GITHUB_REPO}" --tag "${RELEASE_VERSION}" --draft --name "Zeebe ${RELEASE_VERSION}" --description ""
 
-${GITHUB_RELEASE} release --user ${GITHUB_ORG} --repo ${GITHUB_REPO} --tag ${RELEASE_VERSION} --draft --name "Zeebe ${RELEASE_VERSION}" --description ""
-
-git tag clients/go/v${RELEASE_VERSION}
-git push origin clients/go/v${RELEASE_VERSION}
+git tag "clients/go/v${RELEASE_VERSION}"
+git push origin "clients/go/v${RELEASE_VERSION}"
 
 function upload {
-  pushd ${1}
+  local -r buildDir="${1}"
+  local -r artifact="${2}"
+  local -r checksum="${artifact}.sha1sum"
 
-  local artifact=${2}
-  local checksum=${artifact}.sha1sum
+  pushd "${buildDir}"
 
-  sha1sum ${artifact} > ${checksum}
-
-  ${GITHUB_RELEASE} upload --user ${GITHUB_ORG} --repo ${GITHUB_REPO} --tag ${RELEASE_VERSION} --name "${artifact}" --file "${artifact}"
-  ${GITHUB_RELEASE} upload --user ${GITHUB_ORG} --repo ${GITHUB_REPO} --tag ${RELEASE_VERSION} --name "${checksum}" --file "${checksum}"
+  sha1sum "${artifact}" > "${checksum}"
+  "${GITHUB_RELEASE}" upload --user "${GITHUB_ORG}" --repo "${GITHUB_REPO}" --tag "${RELEASE_VERSION}" --name "${artifact}" --file "${artifact}"
+  "${GITHUB_RELEASE}" upload --user "${GITHUB_ORG}" --repo "${GITHUB_REPO}" --tag "${RELEASE_VERSION}" --name "${checksum}" --file "${checksum}"
 
   popd
 }
 
-upload dist/target camunda-cloud-zeebe-${RELEASE_VERSION}.tar.gz
-upload dist/target camunda-cloud-zeebe-${RELEASE_VERSION}.zip
+upload dist/target "camunda-cloud-zeebe-${RELEASE_VERSION}.tar.gz"
+upload dist/target "camunda-cloud-zeebe-${RELEASE_VERSION}.zip"
 upload clients/go/cmd/zbctl/dist zbctl
 upload clients/go/cmd/zbctl/dist zbctl.exe
 upload clients/go/cmd/zbctl/dist zbctl.darwin
