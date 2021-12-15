73b7c1c15728911eeb87708d005b926bd82a5dbc
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-12-15 18:40:01 +0100

    build(ci): download all required binary utilities during preparation

--- .ci/scripts/release/prepare.sh
+++ .ci/scripts/release/prepare.sh
@@ -6,13 +6,19 @@ apt install -y gpg
 
 # remove origin and use GitHub App (reflected on filesystem and globally active)
 git remote remove origin
-git remote add origin https://${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}@github.com/camunda-cloud/zeebe.git
+git remote add origin "https://${GITHUB_TOKEN_USR}:${GITHUB_TOKEN_PSW}@github.com/camunda-cloud/zeebe.git"
 
 # configure Jenkins GitHub user for Maven container
 git config --global user.email "ci@camunda.com"
 git config --global user.name "${GITHUB_TOKEN_USR}"
 
 # setup maven central gpg keys
-gpg -q --allow-secret-key-import --import --no-tty --batch --yes ${GPG_SEC_KEY}
-gpg -q --import --no-tty --batch --yes ${GPG_PUB_KEY}
-rm ${GPG_SEC_KEY} ${GPG_PUB_KEY}
+gpg -q --allow-secret-key-import --import --no-tty --batch --yes "${GPG_SEC_KEY}"
+gpg -q --import --no-tty --batch --yes "${GPG_PUB_KEY}"
+rm "${GPG_SEC_KEY}" "${GPG_PUB_KEY}"
+
+# install binary tools; binary tools are installed outside of the repo to avoid dirtying it
+BINDIR=${BINDIR:-/tmp}
+curl -sL  https://github.com/github-release/github-release/releases/download/v0.10.0/linux-amd64-github-release.bz2 | bzip2 -fd - > github-release
+chmod +x github-release
+mv github-release "${BINDIR}/github-release"
