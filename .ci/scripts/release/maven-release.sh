8e2bbe3ce602b34844f9bf371b361c7dada44a70
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-12-16 11:31:23 +0100

    build(ci): increase timeout to 4 hours

--- .ci/scripts/release/maven-release.sh
+++ .ci/scripts/release/maven-release.sh
@@ -11,5 +11,5 @@ mvn -s ${MAVEN_SETTINGS_XML} release:prepare release:perform -B \
   -DpushChanges=${PUSH_CHANGES} \
   -DremoteTagging=${PUSH_CHANGES} \
   -DlocalCheckout=${SKIP_DEPLOY} \
-  -DstagingProgressTimeoutMinutes=25
-  -Darguments='--settings=${MAVEN_SETTINGS_XML} -DstagingProgressTimeoutMinutes=25 -DskipTests=true -Dgpg.passphrase="${GPG_PASS}" -Dskip.central.release=${SKIP_DEPLOY} -Dskip.camunda.release=${SKIP_DEPLOY} -Dzbctl.force -Dzbctl.rootDir=${ZBCTL_ROOT_DIR}'
+  -DstagingProgressTimeoutMinutes=240
+  -Darguments='--settings=${MAVEN_SETTINGS_XML} -DstagingProgressTimeoutMinutes=240 -DskipTests=true -Dgpg.passphrase="${GPG_PASS}" -Dskip.central.release=${SKIP_DEPLOY} -Dskip.camunda.release=${SKIP_DEPLOY} -Dzbctl.force -Dzbctl.rootDir=${ZBCTL_ROOT_DIR}'
