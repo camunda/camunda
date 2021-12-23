136beda3483142dbc19d99713c80916ffef37cce
Nico Korthout <nico.korthout@camunda.com>
2021-12-23 12:25:08 +0100

    build: skip checks to create benchmark
    No need to spend time on running the checks. We also don't run the
    tests. That's what we have CI for.
    
    (cherry picked from commit f90dac73a78ed9acd1bfd531a1451711e7ff55e6)

--- createBenchmark.sh
+++ createBenchmark.sh
@@ -28,7 +28,7 @@ if ! docker info >/dev/null 2>&1; then
     exit 1
 fi
 
-mvn clean install -DskipTests -T1C
+mvn clean install -DskipTests -DskipChecks -T1C
 
 docker build --build-arg DISTBALL=dist/target/camunda-cloud-zeebe-*.tar.gz --build-arg APP_ENV=dev -t "gcr.io/zeebe-io/zeebe:$benchmark" .
 docker push "gcr.io/zeebe-io/zeebe:$benchmark"
