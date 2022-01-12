98bb2fc159967de09069dc5d3b34e6e429b854e3
Svetlana Dorokhova <sveta_d@list.ru>
2022-01-12 16:57:39 +0100

    #2084 adjust after 1.3.0 (#2086)
    * chore(backend): adjust projects after minor release 1.3.0
    
    closes #2084

---
+++ importer-1_4/src/main/java/io/camunda/operate/zeebeimport/v1_4/record/RecordValueWithPayloadImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a commercial license.
+ * You may not use this file except in compliance with the commercial license.
+ */
+package io.camunda.operate.zeebeimport.v1_4.record;
+
+import io.camunda.zeebe.protocol.record.RecordValue;
+import io.camunda.zeebe.protocol.record.RecordValueWithVariables;
+import java.util.Map;
+import java.util.Objects;
+
+public abstract class RecordValueWithPayloadImpl implements RecordValue, RecordValueWithVariables {
+  private Map<String, Object> variables;
+
+  public RecordValueWithPayloadImpl() {
+  }
+
+  @Override
+  public Map<String, Object> getVariables() {
+    return variables;
+  }
+
+  public void setVariables(Map<String, Object> variables) {
+    this.variables = variables;
+  }
+
+  @Override
+  public String toJson() {
+    throw new UnsupportedOperationException("toJson operation is not supported");
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+
+    RecordValueWithPayloadImpl that = (RecordValueWithPayloadImpl) o;
+
+    return Objects.equals(variables, that.variables);
+  }
+
+  @Override
+  public int hashCode() {
+    return variables != null ? variables.hashCode() : 0;
+  }
+}
