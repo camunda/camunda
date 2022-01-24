87e74b78a6430d8cb4e149b7a6759c2a1eb792b5
Nico Korthout <nico.korthout@camunda.com>
2022-01-24 18:23:16 +0100

    docs(dmn): add comprehensive javadoc
    When writing the javadoc for the evaluateDecisionById method, it became
    clear that some of the other javadoc was not using the same wording. I
    believe it makes sense to link these concepts together and explain how
    they relate to eachother. This should also make navigation through the
    docs easier.

--- dmn/src/main/java/io/camunda/zeebe/dmn/DecisionContext.java
+++ dmn/src/main/java/io/camunda/zeebe/dmn/DecisionContext.java
@@ -1,3 +1,10 @@
 package io.camunda.zeebe.dmn;
 
+/**
+ * {@link ParsedDecision Decisions} can only be made within in a specific context. The context must
+ * contain all input data required by the decision in the {@link ParsedDecisionRequirementsGraph
+ * decision requirements graph} in order to successfully make a decision.
+ *
+ * @see DecisionEngine
+ */
 public interface DecisionContext {}
--- dmn/src/main/java/io/camunda/zeebe/dmn/DecisionResult.java
+++ dmn/src/main/java/io/camunda/zeebe/dmn/DecisionResult.java
@@ -1,3 +1,9 @@
 package io.camunda.zeebe.dmn;
 
+/**
+ * The result of a {@link ParsedDecision}. If successful it contains the output of the decision that
+ * was made, otherwise it contains the evaluation failure.
+ *
+ * @see DecisionEngine
+ */
 public interface DecisionResult {}
