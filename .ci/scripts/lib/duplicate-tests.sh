17425098fc7145f931760b6d81c7c2a79059c3f4
Ole Schönburg <ole.schoenburg@gmail.com>
2022-01-25 15:32:17 +0100

    ci: improve duplicate test run detection script
    * write error messages to stderr
    * better documentation
    
    (cherry picked from commit 990a3eabea8b44f6dd0826203a0c8b861aae26af)

--- .ci/scripts/lib/duplicate-tests.sh
+++ .ci/scripts/lib/duplicate-tests.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
-# Script that parses maven output to find any tests that are run more than once.
-# Returns 1 when no tests are found (likely an error, for example if the maven output changes) or
-# if any test is run more than once. Otherwise return 0.
-
-# We are using files instead of variables so that we don't spam stdout too much when tracing is enabled.
+#######################################
+# Parses Maven output to find if any tests was run more than once.
+# Globals:
+#   None
+# Arguments:
+#   1 - file containing the test outputs
+#   2 - writable file which where the duplicate tests will be written
+# Outputs:
+#   Error message on STDERR if duplicate tests were found.
+#   Error message on STDERR if no tests were found, indicating the function is broken
+# Returns:
+#   0 if no tests were run multiple times
+#   1 if at least one test was run multiple times or of no tests were found
+#######################################
 function findDuplicateTestRuns() {
   local -r testInputFile="$1"
   local -r outputFile="$2"
@@ -13,11 +21,11 @@ function findDuplicateTestRuns() {
   if [ -s "$tmpFile" ]; then  # found tests
       sort "$tmpFile" | uniq -d | tee "$outputFile"
       if [ -s "$outputFile" ]; then
-          echo "Found duplicate test runs!"
+          echo "Found duplicate test runs!" >&2
           return 1
       fi
       return 0
   fi
-  echo "Could not find any tests, duplicate detection is broken."
+  echo "Could not find any tests, duplicate detection is broken." >&2
   return 1
 }
