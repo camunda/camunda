510ab02929c38f696bf57808782cbfb4cad049cc
Remco Westerhoud <remco@westerhoud.nl>
2022-02-01 13:51:48 +0100

    feat(ci): fail early when no go binaries are available

--- .ci/scripts/release/post-release-go.sh
+++ .ci/scripts/release/post-release-go.sh
@@ -8,7 +8,7 @@ git push origin "clients/go/v${RELEASE_VERSION}"
 pushd "clients/go/internal/embedded" || exit $?
 
 echo "${DEVELOPMENT_VERSION}" > data/VERSION
-"${GOPATH}/go-bindata" -pkg embedded -o embedded.go -prefix data/ data/
+go-bindata -pkg embedded -o embedded.go -prefix data/ data/
 
 git commit -am "build(project): prepare next development version (Go client)"
 git push origin "${RELEASE_BRANCH}"
