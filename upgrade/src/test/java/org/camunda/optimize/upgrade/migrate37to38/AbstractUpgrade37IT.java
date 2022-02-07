9de39674efde6f1d527c27bef171ea44f07f2a18
PHWaechtler <21290204+PHWaechtler@users.noreply.github.com>
2022-02-07 09:53:18 +0100

    fix(upgrade): fix filterType and logscale upgrade
    related to OPT-5954

--- upgrade/src/test/java/org/camunda/optimize/upgrade/migrate37to38/AbstractUpgrade37IT.java
+++ upgrade/src/test/java/org/camunda/optimize/upgrade/migrate37to38/AbstractUpgrade37IT.java
@@ -5,6 +5,8 @@
  */
 package org.camunda.optimize.upgrade.migrate37to38;
 
+import org.camunda.optimize.service.es.schema.index.report.SingleDecisionReportIndex;
+import org.camunda.optimize.service.es.schema.index.report.SingleProcessReportIndex;
 import org.camunda.optimize.upgrade.AbstractUpgradeIT;
 import org.camunda.optimize.upgrade.migrate37to38.indices.PositionBasedImportIndexOld;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,14 +16,21 @@ import java.util.List;
 public abstract class AbstractUpgrade37IT extends AbstractUpgradeIT {
 
   protected static final String FROM_VERSION = "3.7.0";
+  protected static final String TO_VERSION = "3.8.0";
 
   protected static final PositionBasedImportIndexOld POSITION_BASED_INDEX = new PositionBasedImportIndexOld();
+  protected static final SingleProcessReportIndex SINGLE_PROCESS_REPORT_INDEX =
+    new SingleProcessReportIndex();
+  protected static final SingleDecisionReportIndex SINGLE_DECISION_REPORT_INDEX =
+    new SingleDecisionReportIndex();
 
   @BeforeEach
   protected void setUp() throws Exception {
     super.setUp();
     initSchema(List.of(
-      POSITION_BASED_INDEX
+      POSITION_BASED_INDEX,
+      SINGLE_PROCESS_REPORT_INDEX,
+      SINGLE_DECISION_REPORT_INDEX
     ));
     setMetadataVersion(FROM_VERSION);
   }
