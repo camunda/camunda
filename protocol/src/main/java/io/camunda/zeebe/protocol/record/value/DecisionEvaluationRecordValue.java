892fa783c29ef0052f661273ff0440d65da1b455
Philipp Ossler <philipp.ossler@gmail.com>
2022-02-07 08:22:53 +0100

    feat(protocol): new record for DMN evaluation

---
+++ protocol/src/main/java/io/camunda/zeebe/protocol/record/value/DecisionEvaluationRecordValue.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.zeebe.protocol.record.value;
+
+import io.camunda.zeebe.protocol.record.RecordValue;
+import java.util.List;
+
+/** Represents the evaluation of a DMN decision. */
+public interface DecisionEvaluationRecordValue extends RecordValue {
+
+  /** @return the key of the evaluated decision */
+  long getDecisionKey();
+
+  /** @return the id of the evaluated decision in the DMN */
+  String getDecisionId();
+
+  /** @return the name of the evaluated decision in the DMN */
+  String getDecisionName();
+
+  /** @return the version of the evaluated decision */
+  int getDecisionVersion();
+
+  /** @return the id of the DRG in the DMN the evaluated decision belongs to */
+  String getDecisionRequirementsId();
+
+  /** @return the key of the deployed DRG the evaluated decision belongs to */
+  long getDecisionRequirementsKey();
+
+  /** @return the output of the evaluated decision as JSON string */
+  String getDecisionOutput();
+
+  /** @return the BPMN process id in which context the decision was evaluated */
+  String getBpmnProcessId();
+
+  /** @return the key of the process in which context the decision was evaluated */
+  long getProcessDefinitionKey();
+
+  /** @return the key of the process instance in which context the decision was evaluated */
+  long getProcessInstanceKey();
+
+  /** @return the id of the element in the BPMN in which context the decision was evaluated */
+  String getElementId();
+
+  /** @return the key of the element instance in which context the decision was evaluated */
+  long getElementInstanceKey();
+
+  /**
+   * Returns the {@link EvaluatedDecisionValue details} of the evaluated decision and its required
+   * decisions. The order depends on the evaluation order, starting from the required decisions.
+   *
+   * @return details of the evaluated decisions
+   */
+  List<EvaluatedDecisionValue> getEvaluatedDecisions();
+}
---
+++ protocol/src/main/java/io/camunda/zeebe/protocol/record/value/EvaluatedDecisionValue.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.zeebe.protocol.record.value;
+
+import io.camunda.zeebe.protocol.record.RecordValue;
+import java.util.List;
+
+/**
+ * An evaluated DMN decision. It contains details of the evaluation depending on the decision type.
+ */
+public interface EvaluatedDecisionValue extends RecordValue {
+
+  /** @return the id of the evaluated decision */
+  String getDecisionId();
+
+  /** @return the name of the evaluated decision */
+  String getDecisionName();
+
+  /** @return the type of the evaluated decision */
+  String getDecisionType();
+
+  /** @return the output of the evaluated decision as JSON string */
+  String getDecisionOutput();
+
+  /**
+   * If the decision is a decision table then it returns the {@link EvaluatedInputValue evaluated
+   * inputs}. The inputs are not available for other types of decision.
+   *
+   * @return the evaluated inputs, or an empty list if the decision is not a decision table
+   */
+  List<EvaluatedInputValue> getEvaluatedInputs();
+
+  /**
+   * If the decision is a decision table then it returns the matched rules. The {@link
+   * MatchedRuleValue matched rules} are not available for other types of decision.
+   *
+   * @return the matched rules, or an empty list if the decision is not a decision table
+   */
+  List<MatchedRuleValue> getMatchedRules();
+}
