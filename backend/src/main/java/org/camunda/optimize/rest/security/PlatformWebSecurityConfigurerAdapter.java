1d678f9429bd102c177506dfcaa43ce99dd53180
Andromachi Rozaki <42938116+andromaqui@users.noreply.github.com>
2022-02-10 10:02:00 +0100

    removed labeling sub path from api resources (#4411)

--- backend/src/main/java/org/camunda/optimize/rest/security/PlatformWebSecurityConfigurerAdapter.java
+++ backend/src/main/java/org/camunda/optimize/rest/security/PlatformWebSecurityConfigurerAdapter.java
@@ -119,8 +119,6 @@ public class PlatformWebSecurityConfigurerAdapter extends WebSecurityConfigurerA
         .antMatchers(createApiPath(INGESTION_PATH, VARIABLE_SUB_PATH)).permitAll()
         // public api
         .antMatchers(createApiPath(PUBLIC_PATH, DEEP_SUB_PATH_ANY)).permitAll()
-      // modify variable label endpoint
-        .antMatchers(createApiPath("PUBLIC_PATH", DEEP_SUB_PATH_ANY)).permitAll()
       // public share related resources
         .antMatchers(EXTERNAL_SUB_PATH).permitAll()
         .antMatchers(
