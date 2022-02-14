259a194cf3ae7b95f77b8411f2080c4cf2aa7f80
Nico Korthout <nico.korthout@camunda.com>
2022-02-14 18:59:08 +0100

    test(qa): verify authority overriding
    Adds a test case to the SecurityTest that verifies that the authority
    (used in the TLS handshake) can be overridden.
    
    A minor refactoring was necessary to make it possible to create the
    client in each test, while also passing the same configuration to the
    cluster rule. The client rule is not needed anymore because of this.

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/SecurityTest.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/SecurityTest.java
@@ -9,14 +9,16 @@ package io.camunda.zeebe.it.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
 import io.camunda.zeebe.client.api.response.Topology;
 import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
 import io.camunda.zeebe.it.clustering.ClusteringRule;
-import io.camunda.zeebe.it.clustering.DeploymentClusteredTest;
-import io.camunda.zeebe.it.util.GrpcClientRule;
 import io.netty.util.NetUtil;
 import java.io.File;
+import java.net.URL;
+import java.security.cert.CertificateException;
+import org.assertj.core.api.Assertions;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
@@ -29,44 +31,62 @@ public final class SecurityTest {
       new ClusteringRule(
           1, 1, 1, brokerCfg -> {}, this::configureGatewayForTls, this::configureClientForTls);
 
-  public final GrpcClientRule clientRule =
-      new GrpcClientRule(
-          cfg ->
-              configureClientForTls(
-                  cfg.gatewayAddress(
-                      NetUtil.toSocketAddressString(clusteringRule.getGatewayAddress()))));
-
-  @Rule
-  public RuleChain ruleChain =
-      RuleChain.outerRule(testTimeout).around(clusteringRule).around(clientRule);
+  @Rule public RuleChain ruleChain = RuleChain.outerRule(testTimeout).around(clusteringRule);
 
   @Test
   public void shouldEstablishSecureConnection() {
+    final var client = newSecureClient().build();
+
     // when
-    final Topology topology = clientRule.getClient().newTopologyRequest().send().join();
+    final Topology topology = client.newTopologyRequest().send().join();
 
     // then
     assertThat(topology.getBrokers().size()).isEqualTo(1);
   }
 
+  @Test
+  public void shouldAllowToOverrideAuthority() {
+    final var client = newSecureClient().overrideAuthority("localhost").build();
+
+    // when
+    final Topology topology = client.newTopologyRequest().send().join();
+
+    // then
+    assertThat(topology.getBrokers().size()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldRejectDifferentAuthority() {
+    // given
+    final var client = newSecureClient().overrideAuthority("virtualhost").build();
+
+    // when, then
+    Assertions.assertThatThrownBy(() -> client.newTopologyRequest().send().join())
+        .hasRootCauseInstanceOf(CertificateException.class)
+        .hasRootCauseMessage("No name matching virtualhost found");
+  }
+
+  private ZeebeClientBuilder newSecureClient() {
+    return configureClientForTls(ZeebeClient.newClientBuilder())
+        .gatewayAddress(NetUtil.toSocketAddressString(clusteringRule.getGatewayAddress()));
+  }
+
   private ZeebeClientBuilder configureClientForTls(final ZeebeClientBuilder clientBuilder) {
-    return clientBuilder.caCertificatePath(
-        DeploymentClusteredTest.class
-            .getClassLoader()
-            .getResource("security/test-chain.cert.pem")
-            .getPath());
+    return clientBuilder.caCertificatePath(getResource("security/test-chain.cert.pem").getPath());
   }
 
   private void configureGatewayForTls(final GatewayCfg gatewayCfg) {
-    final String certificatePath =
-        getClass().getClassLoader().getResource("security/test-chain.cert.pem").getFile();
-    final String privateKeyPath =
-        getClass().getClassLoader().getResource("security/test-server.key.pem").getFile();
+    final String certificatePath = getResource("security/test-chain.cert.pem").getFile();
+    final String privateKeyPath = getResource("security/test-server.key.pem").getFile();
 
     gatewayCfg
         .getSecurity()
         .setEnabled(true)
         .setCertificateChainPath(new File(certificatePath))
         .setPrivateKeyPath(new File(privateKeyPath));
+  }
+
+  private URL getResource(final String name) {
+    return getClass().getClassLoader().getResource(name);
   }
 }
