12099efc9ac098119b4c5a8dc6bddc01ead649ff
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-02-16 15:03:56 +0100

    feat(test-util): extend EitherAssert with extractions
    Extends the existing `EitherAssert` set of assertions with new
    extraction capabilities. This allows one to write something like:
    
    ```java
    EitherAssert.assertThat(either).left().isEqualTo(1);
    EitherAssert.assertThat(instantEither)
    	.right()
    	.asInstanceOf(InstanceOfAssertFactories.INSTANT)
    	.isBetween(today, tomorrow);
    ```
    
    Assertion info is correctly preserved across the whole chain.
    
    (cherry picked from commit 46c88e63907960799b681053f468a86e2d465419)

--- test-util/src/main/java/io/camunda/zeebe/test/util/asserts/EitherAssert.java
+++ test-util/src/main/java/io/camunda/zeebe/test/util/asserts/EitherAssert.java
@@ -9,22 +9,95 @@ package io.camunda.zeebe.test.util.asserts;
 
 import io.camunda.zeebe.util.Either;
 import org.assertj.core.api.AbstractObjectAssert;
+import org.assertj.core.api.AssertFactory;
+import org.assertj.core.api.InstanceOfAssertFactory;
+import org.assertj.core.api.ObjectAssert;
 
+/**
+ * AssertJ assertions to help you test {@link Either} instances. Typically, you will check if it is
+ * of the expected side using {@link #isLeft()} or {@link #isRight()}, and then extract that value
+ * using {@link #left()} and {@link #right()} to assert properties on the actual result. You can
+ * further chain these with a standard {@link #asInstanceOf(InstanceOfAssertFactory)} to get a more
+ * specific type.
+ *
+ * @param <L> the left type
+ * @param <R> the right type
+ */
 public final class EitherAssert<L, R>
     extends AbstractObjectAssert<EitherAssert<L, R>, Either<L, R>> {
   public EitherAssert(final Either<L, R> actual) {
     super(actual, EitherAssert.class);
   }
 
+  /**
+   * Convenience factory method which you can use as an {@link AssertFactory}.
+   *
+   * @param actual the actual object under test
+   * @param <L> the left type
+   * @param <R> the right type
+   * @return an instance of {@link EitherAssert} on {@code actual}
+   */
   public static <L, R> EitherAssert<L, R> assertThat(final Either<L, R> actual) {
     return new EitherAssert<>(actual);
   }
 
+  /**
+   * Fails if {@link #actual} has no right member or is null.
+   *
+   * @return itself for chaining
+   */
   public EitherAssert<L, R> isRight() {
+    isNotNull();
+
     if (actual.isLeft()) {
       failWithMessage("Expected <%s> to be right, but was left <%s>", actual, actual.getLeft());
     }
 
     return myself;
+  }
+
+  /**
+   * Extracts the right member of {@link #actual} and returns an {@link ObjectAssert} wrapping it.
+   * Fails if {@link #actual} has no right member or is null.
+   *
+   * @return a new {@link ObjectAssert} around the right member of {@link #actual}
+   */
+  public ObjectAssert<R> right() {
+    isNotNull();
+
+    // use asInstanceOf to preserve the current assertion info and description
+    return asInstanceOf(
+        new InstanceOfAssertFactory<>(
+            Either.Right.class, object -> new ObjectAssert<>(actual.get())));
+  }
+
+  /**
+   * Fails if {@link #actual} has no left member or is null.
+   *
+   * @return itself for chaining
+   */
+  public EitherAssert<L, R> isLeft() {
+    isNotNull();
+
+    if (actual.isRight()) {
+      failWithMessage("Expected <%s> to be left, but was right <%s>", actual, actual.get());
+    }
+
+    return myself;
+  }
+
+  /**
+   * Extracts the left member of {@link #actual} and returns an {@link ObjectAssert} wrapping it.
+   * Fails if {@link #actual} has no left member or is null.
+   *
+   * @return a new {@link ObjectAssert} around the left member of {@link #actual}
+   */
+  public ObjectAssert<L> left() {
+    isNotNull();
+
+    // use asInstanceOf to preserve the current assertion info and description
+    return asInstanceOf(
+        new InstanceOfAssertFactory<>(
+            Either.Left.class, object -> new ObjectAssert<>(actual.getLeft())));
   }
 }
