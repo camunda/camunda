ced07548ee92acf8f4b806e6945f1429b29b0960
Josh Windels <joshua.windels@camunda.com>
2022-02-23 12:25:51 +0100

    feature(index management): ignore shard configuration for archive indices
    relates to OPT-5932

--- util/optimize-commons/src/main/java/org/camunda/optimize/service/es/schema/index/ProcessInstanceArchiveIndex.java
+++ util/optimize-commons/src/main/java/org/camunda/optimize/service/es/schema/index/ProcessInstanceArchiveIndex.java
@@ -5,7 +5,13 @@
  */
 package org.camunda.optimize.service.es.schema.index;
 
+import org.camunda.optimize.service.util.configuration.ConfigurationService;
 import org.camunda.optimize.upgrade.es.ElasticsearchConstants;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+
+import java.io.IOException;
+
+import static org.camunda.optimize.upgrade.es.ElasticsearchConstants.NUMBER_OF_SHARDS_SETTING;
 
 public class ProcessInstanceArchiveIndex extends ProcessInstanceIndex {
 
@@ -16,6 +22,12 @@ public class ProcessInstanceArchiveIndex extends ProcessInstanceIndex {
   @Override
   protected String getIndexPrefix() {
     return ElasticsearchConstants.PROCESS_INSTANCE_ARCHIVE_INDEX_PREFIX;
+  }
+
+  @Override
+  public XContentBuilder getStaticSettings(XContentBuilder xContentBuilder,
+                                           ConfigurationService configurationService) throws IOException {
+    return xContentBuilder.field(NUMBER_OF_SHARDS_SETTING, 1);
   }
 
 }
