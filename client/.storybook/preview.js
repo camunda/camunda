f756a5a0fa23c83534eef00a6b58a796b4d7e357
Vinícius Goulart <vinicius.goulart@camunda.com>
2022-03-01 11:13:43 +0100

    fix(client): fix storybook again (#2338)

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {addDecorator} from '@storybook/react';
 import {ThemeProvider} from 'modules/theme/ThemeProvider';
-
+import {NotificationProvider} from 'modules/notifications';
 import {theme} from 'modules/theme';
 import GlobalStyles from 'App/GlobalStyles';
 import 'index.css';
 import {initializeWorker, mswDecorator} from 'msw-storybook-addon';
-initializeWorker();
-addDecorator(mswDecorator);
 
-addDecorator((storyFn) => {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      {storyFn()}
-    </ThemeProvider>
-  );
-});
+initializeWorker();
+
+export const decorators = [
+  mswDecorator,
+  (storyFn) => {
+    return (
+      <NotificationProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          {storyFn()}
+        </ThemeProvider>
+      </NotificationProvider>
+    );
+  },
+];
