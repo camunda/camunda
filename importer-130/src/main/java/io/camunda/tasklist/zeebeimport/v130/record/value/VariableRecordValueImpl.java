0dd4739d34a310d9792c6535d350246146103610
huygur <45518829+huygur@users.noreply.github.com>
2022-03-03 07:41:48 +0100

    Update Zeebe to 1.4.0-alpha2 and IAM to 1.3.5 (#1674)
    * Update Zeebe to 1.4.0-alpha2 and IAM to 1.3.5
    
    * chore(backend): fix compilation
    
    Co-authored-by: SvetlanaDorokhova <sveta_d@list.ru>

--- importer-130/src/main/java/io/camunda/tasklist/zeebeimport/v130/record/value/VariableRecordValueImpl.java
+++ importer-130/src/main/java/io/camunda/tasklist/zeebeimport/v130/record/value/VariableRecordValueImpl.java
@@ -15,6 +15,7 @@ public class VariableRecordValueImpl extends RecordValueImpl implements Variable
   private long scopeKey;
   private long processInstanceKey;
   private long processDefinitionKey;
+  private String bpmnProcessId;
 
   @Override
   public String getName() {
@@ -41,6 +42,11 @@ public class VariableRecordValueImpl extends RecordValueImpl implements Variable
     return processDefinitionKey;
   }
 
+  @Override
+  public String getBpmnProcessId() {
+    return bpmnProcessId;
+  }
+
   public void setProcessDefinitionKey(long processDefinitionKey) {
     this.processDefinitionKey = processDefinitionKey;
   }
@@ -59,6 +65,11 @@ public class VariableRecordValueImpl extends RecordValueImpl implements Variable
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public VariableRecordValueImpl setBpmnProcessId(final String bpmnProcessId) {
+    this.bpmnProcessId = bpmnProcessId;
+    return this;
   }
 
   @Override
