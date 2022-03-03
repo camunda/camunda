Gathin23/zeebe

test: add additional tests for `ColumnFamily`

zb-db/src/test/java/io/camunda/zeebe/db/impl/ColumnFamilyTest.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.db.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.camunda.zeebe.db.ColumnFamily;
 import io.camunda.zeebe.db.ZeebeDb;
@@ -231,10 +232,7 @@ public final class ColumnFamilyTest {
     putKeyValuePair(Short.MAX_VALUE, 1);
 
     // when
-    columnFamily.forEach(
-        (key, value) -> {
-          columnFamily.delete(key);
-        });
+    columnFamily.forEach((key, value) -> columnFamily.delete(key));
 
     final List<Long> keys = new ArrayList<>();
     final List<Long> values = new ArrayList<>();
@@ -326,6 +324,36 @@ public final class ColumnFamilyTest {
 
     columnFamily.delete(key);
     assertThat(columnFamily.isEmpty()).isTrue();
+  }
+
+  @Test
+  public void shouldThrowOnInsert() {
+    key.wrapLong(1);
+    value.wrapLong(10);
+    columnFamily.insert(key, value);
+    assertThatThrownBy(() -> columnFamily.insert(key, value))
+        .getRootCause()
+        .hasMessageContaining("already exists")
+        .isInstanceOf(IllegalStateException.class);
+  }
+
+  @Test
+  public void shouldThrowOnUpdate() {
+    key.wrapLong(1);
+    value.wrapLong(10);
+    assertThatThrownBy(() -> columnFamily.update(key, value))
+        .getRootCause()
+        .hasMessageContaining("does not exist")
+        .isInstanceOf(IllegalStateException.class);
+  }
+
+  @Test
+  public void shouldThrowOnDeleteExisting() {
+    key.wrapLong(1);
+    assertThatThrownBy(() -> columnFamily.deleteExisting(key))
+        .getRootCause()
+        .hasMessageContaining("does not exist")
+        .isInstanceOf(IllegalStateException.class);
   }
 
   private void putKeyValuePair(final int key, final int value) {

camunda/camunda

zb-db/src/test/java/io/camunda/zeebe/db/impl/ColumnFamilyTest.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.db.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.camunda.zeebe.db.ColumnFamily;
 import io.camunda.zeebe.db.ZeebeDb;
@@ -327,6 +328,36 @@ public final class ColumnFamilyTest {
 
     columnFamily.delete(key);
     assertThat(columnFamily.isEmpty()).isTrue();
+  }
+
+  @Test
+  public void shouldThrowOnInsert() {
+    key.wrapLong(1);
+    value.wrapLong(10);
+    columnFamily.insert(key, value);
+    assertThatThrownBy(() -> columnFamily.insert(key, value))
+        .getRootCause()
+        .hasMessageContaining("already exists")
+        .isInstanceOf(ZeebeDbInconsistentException.class);
+  }
+
+  @Test
+  public void shouldThrowOnUpdate() {
+    key.wrapLong(1);
+    value.wrapLong(10);
+    assertThatThrownBy(() -> columnFamily.update(key, value))
+        .getRootCause()
+        .hasMessageContaining("does not exist")
+        .isInstanceOf(ZeebeDbInconsistentException.class);
+  }
+
+  @Test
+  public void shouldThrowOnDeleteExisting() {
+    key.wrapLong(1);
+    assertThatThrownBy(() -> columnFamily.deleteExisting(key))
+        .getRootCause()
+        .hasMessageContaining("does not exist")
+        .isInstanceOf(ZeebeDbInconsistentException.class);
   }
 
   private void putKeyValuePair(final int key, final int value) {