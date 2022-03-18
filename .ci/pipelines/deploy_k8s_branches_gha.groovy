678ddc781427ba584c8201331483b8e85e3377e1
Leonhardt Wille <lwille@users.noreply.github.com>
2022-03-18 22:26:13 +0100

    Preview Environment fixes (INFRA-3091) (#2428)
    * fix(preview-env): rewrite deployment name to `stage` when deploying `master` branch
    * feat(preview-env): add workflow to tear down deleted branches
    * fix(preview-env): drop branch-name label because it might contain illegal characters
    * fix(preview-env): fix app_name sanitation
      renovate branch names like renovate/bpmn-js-9.x can't be used as namespaces
    
    related to INFRA-3091

--- .ci/pipelines/deploy_k8s_branches_gha.groovy
+++ .ci/pipelines/deploy_k8s_branches_gha.groovy
@@ -10,7 +10,7 @@ camundaGitHubWorkflowDispatch([
   credentialsId: 'github-cloud-operate-app',
   dryRun: params.DRY_RUN,
   inputs: [
-    app_name:  GitUtilities.getSanitizedBranchName([BRANCH_NAME: params.BRANCH]),
+    app_name:  GitUtilities.getSanitizedBranchName([BRANCH_NAME: params.BRANCH]).replaceAll(/[^a-z0-9]/, '-'),
     chart_ref: params.BRANCH,
     docker_tag: params.DOCKER_TAG,
   ],
