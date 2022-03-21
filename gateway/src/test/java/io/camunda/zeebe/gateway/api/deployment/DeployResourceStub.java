bfa6d8b18cfcd90516156a00e3419d66c2e43776
Nico Korthout <nico.korthout@camunda.com>
2022-03-21 17:23:47 +0100

    test(gateway): verify the deploy resource rpc mapping
    This test we write for each rpc that is supported by the gateway. It
    makes sure that all properties of the response are correctly mapped.

--- gateway/src/test/java/io/camunda/zeebe/gateway/api/deployment/DeployResourceStub.java
+++ gateway/src/test/java/io/camunda/zeebe/gateway/api/deployment/DeployResourceStub.java
@@ -14,14 +14,15 @@ import io.camunda.zeebe.gateway.api.util.StubbedBrokerClient.RequestStub;
 import io.camunda.zeebe.gateway.impl.broker.request.BrokerDeployResourceRequest;
 import io.camunda.zeebe.gateway.impl.broker.response.BrokerResponse;
 import io.camunda.zeebe.protocol.impl.record.value.deployment.DeploymentRecord;
+import io.camunda.zeebe.util.buffer.BufferUtil;
 import org.agrona.DirectBuffer;
 
-public final class DeployProcessStub
+public final class DeployResourceStub
     implements RequestStub<BrokerDeployResourceRequest, BrokerResponse<DeploymentRecord>> {
 
   private static final long KEY = 123;
-  private static final long PROCESS_KEY = 456;
-  private static final int PROCESS_VERSION = 789;
+  private static final long PROCESS_KEY = 234;
+  private static final int PROCESS_VERSION = 345;
   private static final DirectBuffer CHECKSUM = wrapString("checksum");
 
   @Override
@@ -50,14 +51,36 @@ public final class DeployProcessStub
         .iterator()
         .forEachRemaining(
             r -> {
-              deploymentRecord
-                  .processesMetadata()
-                  .add()
-                  .setBpmnProcessId(r.getResourceNameBuffer())
-                  .setResourceName(r.getResourceNameBuffer())
-                  .setVersion(PROCESS_VERSION)
-                  .setKey(PROCESS_KEY)
-                  .setChecksum(CHECKSUM);
+              if (r.getResourceName().endsWith(".bpmn")) {
+                deploymentRecord
+                    .processesMetadata()
+                    .add()
+                    .setBpmnProcessId(r.getResourceNameBuffer())
+                    .setResourceName(r.getResourceNameBuffer())
+                    .setVersion(PROCESS_VERSION)
+                    .setKey(PROCESS_KEY)
+                    .setChecksum(CHECKSUM);
+              } else if (r.getResourceName().endsWith(".dmn")) {
+                deploymentRecord
+                    .decisionsMetadata()
+                    .add()
+                    .setDecisionId(r.getResourceName())
+                    .setDecisionName(r.getResourceName())
+                    .setVersion(456)
+                    .setDecisionKey(567)
+                    .setDecisionRequirementsId(r.getResourceName())
+                    .setDecisionRequirementsKey(678);
+                deploymentRecord
+                    .decisionRequirementsMetadata()
+                    .add()
+                    .setDecisionRequirementsId(r.getResourceName())
+                    .setDecisionRequirementsName(r.getResourceName())
+                    .setDecisionRequirementsVersion(456)
+                    .setDecisionRequirementsKey(678)
+                    .setNamespace(r.getResourceName())
+                    .setResourceName(r.getResourceName())
+                    .setChecksum(BufferUtil.wrapString("checksum"));
+              }
             });
     return new BrokerResponse<>(deploymentRecord, 0, KEY);
   }
