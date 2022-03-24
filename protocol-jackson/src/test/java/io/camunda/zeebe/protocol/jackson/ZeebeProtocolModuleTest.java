d8d14deeec9e0f04bf35f50ecc4ec1083b4fffcf
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-03-24 15:40:15 +0100

    test(protocol-jackson): simplify test using protocol factory

--- protocol-jackson/src/test/java/io/camunda/zeebe/protocol/jackson/ZeebeProtocolModuleTest.java
+++ protocol-jackson/src/test/java/io/camunda/zeebe/protocol/jackson/ZeebeProtocolModuleTest.java
@@ -11,23 +11,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import io.camunda.zeebe.protocol.record.ImmutableRecord;
 import io.camunda.zeebe.protocol.record.Record;
-import io.camunda.zeebe.protocol.record.RecordType;
+import io.camunda.zeebe.protocol.record.RecordAssert;
 import io.camunda.zeebe.protocol.record.RecordValue;
 import io.camunda.zeebe.protocol.record.ValueType;
-import io.camunda.zeebe.protocol.record.intent.VariableIntent;
-import io.camunda.zeebe.protocol.record.value.ImmutableJobBatchRecordValue;
-import io.camunda.zeebe.protocol.record.value.ImmutableJobRecordValue;
-import io.camunda.zeebe.protocol.record.value.ImmutableVariableRecordValue;
-import io.camunda.zeebe.protocol.record.value.JobBatchRecordValue;
-import io.camunda.zeebe.protocol.record.value.JobRecordValue;
-import io.camunda.zeebe.protocol.record.value.VariableRecordValue;
+import io.camunda.zeebe.protocol.util.ValueTypeMapping;
+import io.camunda.zeebe.protocol.util.ValueTypeMapping.Mapping;
+import io.camunda.zeebe.test.broker.protocol.record.ProtocolFactory;
 import java.io.IOException;
-import org.assertj.core.util.Maps;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @SuppressWarnings("unchecked")
 @Execution(ExecutionMode.CONCURRENT)
@@ -35,40 +31,28 @@ final class ZeebeProtocolModuleTest {
   private static final ObjectMapper MAPPER =
       new ObjectMapper().registerModule(new ZeebeProtocolModule());
 
-  @Test
-  void shouldDeserialize() throws IOException {
+  private final ProtocolFactory factory = new ProtocolFactory();
+
+  @ParameterizedTest
+  @MethodSource("provideValueTypes")
+  void shouldDeserializeRecord(final ValueType valueType) throws IOException {
     // given
-    final JobRecordValue value =
-        ImmutableJobRecordValue.builder()
-            .withBpmnProcessId("bpmnProcessId")
-            .withVariables(Maps.newHashMap("foo", "bar"))
-            .build();
-    final JobBatchRecordValue batch =
-        ImmutableJobBatchRecordValue.builder()
-            .addJob(value)
-            .addJobKey(1L)
-            .withWorker("worker")
-            .build();
+    final Record<RecordValue> record = factory.generateRecord(valueType);
+    final byte[] serialized = MAPPER.writeValueAsBytes(record);
 
     // when
-    final byte[] serialized = MAPPER.writeValueAsBytes(batch);
-    final JobBatchRecordValue deserialized =
-        MAPPER.readValue(serialized, JobBatchRecordValue.class);
-    final JobBatchRecordValue other = MAPPER.readValue(serialized, JobBatchRecordValue.class);
+    final Record<RecordValue> deserialized =
+        (Record<RecordValue>) MAPPER.readValue(serialized, Record.class);
 
-    assertThat(deserialized).isEqualTo(other).isEqualTo(batch);
+    // then
+    RecordAssert.assertThat(deserialized).isEqualTo(record);
   }
 
-  @Test
-  void shouldIgnoreUnknownPropertiesOfRecord() throws IOException {
+  @ParameterizedTest
+  @MethodSource("provideValueTypes")
+  void shouldIgnoreUnknownPropertiesOfRecord(final ValueType valueType) throws IOException {
     // given
-    final Record<RecordValue> record =
-        ImmutableRecord.builder()
-            .withValueType(ValueType.VARIABLE)
-            .withValue(ImmutableVariableRecordValue.builder().build())
-            .withIntent(VariableIntent.CREATED)
-            .withRecordType(RecordType.EVENT)
-            .build();
+    final Record<RecordValue> record = factory.generateRecord(valueType);
     final ObjectNode jsonRecord = MAPPER.valueToTree(record);
 
     // when
@@ -78,22 +62,27 @@ final class ZeebeProtocolModuleTest {
         (Record<RecordValue>) MAPPER.readValue(serialized, Record.class);
 
     // then
-    assertThat(deserialized).isEqualTo(record);
+    RecordAssert.assertThat(deserialized).isEqualTo(record);
   }
 
-  @Test
-  void shouldIgnoreUnknownPropertiesOfRecordValue() throws IOException {
+  @ParameterizedTest
+  @MethodSource("provideValueTypes")
+  void shouldIgnorePropertiesOfRecordValue(final ValueType valueType) throws IOException {
     // given
-    final VariableRecordValue value = ImmutableVariableRecordValue.builder().build();
+    final Mapping<?, ?> typeInfo = ValueTypeMapping.get(valueType);
+    final RecordValue value = factory.generateObject(typeInfo.getValueClass());
     final ObjectNode jsonValue = MAPPER.valueToTree(value);
 
     // when
     jsonValue.put("nonExistentProperty", "something");
     final byte[] serialized = MAPPER.writeValueAsBytes(jsonValue);
-    final VariableRecordValue deserialized =
-        MAPPER.readValue(serialized, VariableRecordValue.class);
+    final RecordValue deserialized = MAPPER.readValue(serialized, typeInfo.getValueClass());
 
     // then
     assertThat(deserialized).isEqualTo(value);
+  }
+
+  private static Stream<ValueType> provideValueTypes() {
+    return ValueTypeMapping.getAcceptedValueTypes().stream();
   }
 }
