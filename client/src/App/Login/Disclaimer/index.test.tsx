6f6116473029d3240281d4afb34929465c894764
Sebastian Menski <sebastian.menski@camunda.com>
2022-03-28 13:13:02 +0200

    chore: update terms and conditions link (#2470)

--- client/src/App/Login/Disclaimer/index.test.tsx
+++ client/src/App/Login/Disclaimer/index.test.tsx
@@ -30,7 +30,7 @@ describe('<Disclaimer />', () => {
       screen.getByRole('link', {name: 'terms & conditions page'})
     ).toHaveAttribute(
       'href',
-      'https://camunda.com/legal/terms/cloud-terms-and-conditions/camunda-cloud-self-managed-free-edition-terms/'
+      'https://camunda.com/legal/terms/camunda-platform/camunda-platform-8-self-managed/'
     );
     expect(screen.getByRole('link', {name: 'contact sales'})).toHaveAttribute(
       'href',
@@ -51,7 +51,7 @@ describe('<Disclaimer />', () => {
       screen.getByRole('link', {name: 'terms & conditions page'})
     ).toHaveAttribute(
       'href',
-      'https://camunda.com/legal/terms/cloud-terms-and-conditions/camunda-cloud-self-managed-free-edition-terms/'
+      'https://camunda.com/legal/terms/camunda-platform/camunda-platform-8-self-managed/'
     );
     expect(screen.getByRole('link', {name: 'contact sales'})).toHaveAttribute(
       'href',
