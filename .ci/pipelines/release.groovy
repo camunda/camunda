08158946bf5e3d3bd5b01e70d76215b4841ea7e1
Sebastian Menski <sebastian.menski@camunda.com>
2022-03-31 14:21:36 +0200

    chore(project): rename artifacts (#1739)

--- .ci/pipelines/release.groovy
+++ .ci/pipelines/release.groovy
@@ -38,7 +38,7 @@ void runRelease(params) {
 def githubRelease = '''\
 #!/bin/bash
 
-ARTIFACT="camunda-cloud-tasklist"
+ARTIFACT="camunda-tasklist"
 ZEEBE_VERSION=$(mvn help:evaluate -Dexpression=version.zeebe -q -DforceStdout)
 
 cd target/checkout/distro/target
