bec608bc9b5e1233898390af732dce32a0521ab9
Luiz Santana <3302415+santanaluiz@users.noreply.github.com>
2022-04-05 11:04:38 -0300

    chore: update license (#2564)
    * impl(license): update license.txt
    
    * impl(license): update license header
    
    * impl(license): update license-build.js
    
    * impl(commercial-license): update commercial license text
    
    * impl(license): fix license on client
    
    * fix: remove old license
    
    Co-authored-by: Vinicius Goulart <vinicius.goulart@camunda.com>

--- els-schema/src/main/java/io/camunda/operate/schema/ElasticsearchSchemaManager.java
+++ els-schema/src/main/java/io/camunda/operate/schema/ElasticsearchSchemaManager.java
@@ -1,9 +1,9 @@
 /*
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
- * under one or more contributor license agreements. Licensed under a commercial license.
- * You may not use this file except in compliance with the commercial license.
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
  */
-
 package io.camunda.operate.schema;
 
 import java.io.IOException;
--- webapp/src/main/java/io/camunda/operate/webapp/api/v1/exceptions/ClientException.java
+++ webapp/src/main/java/io/camunda/operate/webapp/api/v1/exceptions/ClientException.java
@@ -1,7 +1,8 @@
 /*
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
- * under one or more contributor license agreements. Licensed under a commercial license.
- * You may not use this file except in compliance with the commercial license.
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
  */
 package io.camunda.operate.webapp.api.v1.exceptions;
 
