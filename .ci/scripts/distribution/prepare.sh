4cb70d4b69faddefa45849d4f77966350d9238b9
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-04-08 17:36:43 +0200

    test: REVERT ME - force push commit to check the whitelisting

--- .ci/scripts/distribution/prepare.sh
+++ .ci/scripts/distribution/prepare.sh
@@ -6,3 +6,17 @@ apt-get install --no-install-recommends -qq -y jq libatomic1
 useradd -u 1000 -m jenkins
 
 chmod -R a+x .
+
+# remove origin and use GitHub App (reflected on filesystem and globally active)
+git remote remove origin
+git remote add origin "https://infra-jenkins-zeebe[bot]:${GITHUB_TOKEN_PSW}@github.com/camunda/zeebe.git"
+
+# configure Jenkins GitHub user for Maven container
+git config --global user.email "ci@camunda.com"
+git config --global user.name "infra-jenkins-zeebe[bot]"
+
+echo "hello" >> some-test-file
+git add some-test-file
+git commit -m"test ignore"
+git push --set-upstream origin 9069-bot-git-name
+
