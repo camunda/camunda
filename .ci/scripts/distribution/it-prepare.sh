9d1b46f9f482312b15ba92ae6354a12612bc65c7
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-04-10 11:54:56 +0200

    build(ci): ensure flaky test plugin is resolved without adding it as a dependency

--- .ci/scripts/distribution/it-prepare.sh
+++ .ci/scripts/distribution/it-prepare.sh
@@ -11,4 +11,4 @@ MAVEN_PROPERTIES=(
 
 # make sure to specify the profiles used in the verify goal when running preparing to go offline, as
 # these may require some additional plugin dependencies
-mvn -B -T "${MAVEN_PARALLELISM}" -s "${MAVEN_SETTINGS_XML}" package -Pprepare-offline,parallel-tests "${MAVEN_PROPERTIES[@]}"
+mvn -B -T "${MAVEN_PARALLELISM}" -s "${MAVEN_SETTINGS_XML}" package -Pprepare-offline,extract-flaky-tests,parallel-tests "${MAVEN_PROPERTIES[@]}"
