c60d1f8b1144d263810f1252c44c2562a7f63ee8
pihme <pihme@users.noreply.github.com>
2022-04-20 11:03:38 +0200

    fix(engine): detect writing to wrong value type
    - also change variables in tests, which had been confused
    
    (cherry picked from commit 6de9f65426db0f0337aa703bf2dda755131512b5)

--- engine/src/test/java/io/camunda/zeebe/engine/processing/bpmn/behavior/OutputCollectionBehaviorTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/bpmn/behavior/OutputCollectionBehaviorTest.java
@@ -25,6 +25,7 @@ import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableMul
 import io.camunda.zeebe.msgpack.spec.MsgPackWriter;
 import io.camunda.zeebe.protocol.record.value.ErrorType;
 import io.camunda.zeebe.util.Either;
+import io.camunda.zeebe.util.buffer.BufferUtil;
 import java.util.Optional;
 import org.agrona.DirectBuffer;
 import org.agrona.ExpandableArrayBuffer;
@@ -40,20 +41,20 @@ public class OutputCollectionBehaviorTest {
   void shouldReturnFailureWhenWritingToOutputCollectionOutOfBounds() {
     // given
     final var collectionWithSize1 = createCollection(1);
-    final var elementToAdd = wrapString("element to add");
+    final var elementToAdd = createMsgPackString("element to add");
     final var indexThatIsOutOfBounds = 2;
-    final var outputElementsExpression = new StaticExpression("OUTPUT_COLLECTION");
-    final var outputElementName = wrapString("OUTPUT_ELEMENT");
+    final var outputElementExpression = new StaticExpression("dummy expression");
+    final var outputCollectionName = wrapString("OUTPUT_COLLECTION");
     final var loopCharacteristics =
-        createLoopCharacteristics(outputElementName, outputElementsExpression);
+        createLoopCharacteristics(outputCollectionName, outputElementExpression);
     final var flowScopeContextKey = 12345L;
 
     final var mockStateBehavior = mock(BpmnStateBehavior.class, Answers.RETURNS_DEEP_STUBS);
-    when(mockStateBehavior.getLocalVariable(any(), eq(outputElementName)))
+    when(mockStateBehavior.getLocalVariable(any(), eq(outputCollectionName)))
         .thenReturn(collectionWithSize1);
 
     final var mockExpressionProcessor = mock(ExpressionProcessor.class);
-    when(mockExpressionProcessor.evaluateAnyExpression(eq(outputElementsExpression), anyLong()))
+    when(mockExpressionProcessor.evaluateAnyExpression(eq(outputElementExpression), anyLong()))
         .thenReturn(Either.right(elementToAdd));
 
     final var mockElement = mock(ExecutableMultiInstanceBody.class);
@@ -79,19 +80,66 @@ public class OutputCollectionBehaviorTest {
     assertThat(failure.getErrorType()).isEqualTo(ErrorType.EXTRACT_VALUE_ERROR);
     assertThat(failure.getMessage())
         .isEqualTo(
-            "Unable to update item in output collection 'OUTPUT_ELEMENT' at position 2 because the size of the collection is: 1. This happens when multiple BPMN elements write to the same variable.");
+            "Unable to update item in output collection 'OUTPUT_COLLECTION' at position 2 because the size of the collection is: 1. This happens when multiple BPMN elements write to the same variable.");
+    assertThat(failure.getVariableScopeKey()).isEqualTo(flowScopeContextKey);
+  }
+
+  @Test
+  void shouldReturnFailureWhenWritingToOutputCollectionWhichIsNotArray() {
+    // given
+    final var unexpectedValueType = createMsgPackString("lorem ipsum");
+    final var elementToAdd = createMsgPackString("element to add");
+    final var index = 1;
+    final var outputElementExpression = new StaticExpression("dummy expression");
+    final var outputCollectionName = wrapString("OUTPUT_COLLECTION");
+    final var loopCharacteristics =
+        createLoopCharacteristics(outputCollectionName, outputElementExpression);
+    final var flowScopeContextKey = 12345L;
+
+    final var mockStateBehavior = mock(BpmnStateBehavior.class, Answers.RETURNS_DEEP_STUBS);
+    when(mockStateBehavior.getLocalVariable(any(), eq(outputCollectionName)))
+        .thenReturn(unexpectedValueType);
+
+    final var mockExpressionProcessor = mock(ExpressionProcessor.class);
+    when(mockExpressionProcessor.evaluateAnyExpression(eq(outputElementExpression), anyLong()))
+        .thenReturn(Either.right(elementToAdd));
+
+    final var mockElement = mock(ExecutableMultiInstanceBody.class);
+    when(mockElement.getLoopCharacteristics()).thenReturn(loopCharacteristics);
+
+    final var mockChildContext = mock(BpmnElementContext.class);
+    when(mockStateBehavior.getElementInstance(mockChildContext).getMultiInstanceLoopCounter())
+        .thenReturn(index);
+
+    final var mockFlowScopeContext = mock(BpmnElementContext.class);
+    when(mockFlowScopeContext.getFlowScopeKey()).thenReturn(flowScopeContextKey);
+
+    final var sut = new OutputCollectionBehavior(mockStateBehavior, mockExpressionProcessor);
+
+    // when
+    final var result =
+        sut.updateOutputCollection(mockElement, mockChildContext, mockFlowScopeContext);
+
+    // then
+    assertThat(result.isLeft()).isTrue();
+
+    final var failure = result.getLeft();
+    assertThat(failure.getErrorType()).isEqualTo(ErrorType.EXTRACT_VALUE_ERROR);
+    assertThat(failure.getMessage())
+        .isEqualTo(
+            "Unable to update item in output collection 'OUTPUT_COLLECTION' because the type of the variable is: STRING. This happens when multiple BPMN elements write to the same variable.");
     assertThat(failure.getVariableScopeKey()).isEqualTo(flowScopeContextKey);
   }
 
   private ExecutableLoopCharacteristics createLoopCharacteristics(
-      final DirectBuffer outputElementName, final Expression outputElementExpression) {
+      final DirectBuffer outputCollection, final Expression outputElement) {
     return new ExecutableLoopCharacteristics(
         false,
         Optional.empty(),
         null,
         Optional.empty(),
-        Optional.of(outputElementName),
-        Optional.of(outputElementExpression));
+        Optional.of(outputCollection),
+        Optional.of(outputElement));
   }
 
   private DirectBuffer createCollection(final int size) {
@@ -105,6 +153,20 @@ public class OutputCollectionBehaviorTest {
     for (var i = 0; i < size; i++) {
       writer.writeNil();
     }
+
+    final var length = writer.getOffset();
+
+    return cloneBuffer(buffer, 0, length);
+  }
+
+  private DirectBuffer createMsgPackString(final String input) {
+    final var writer = new MsgPackWriter();
+    final var buffer = new ExpandableArrayBuffer();
+
+    writer.wrap(buffer, 0);
+
+    // initialize the array with nil
+    writer.writeString(BufferUtil.wrapString(input));
 
     final var length = writer.getOffset();
 
