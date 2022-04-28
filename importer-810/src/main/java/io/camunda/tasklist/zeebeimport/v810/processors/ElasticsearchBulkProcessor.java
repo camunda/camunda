62e2d03f2a6958decaee6cdb7b6606e0cef030fc
Svetlana Dorokhova <sveta_d@list.ru>
2022-04-28 23:34:56 +0200

    chore: adjust backend after minor 8.0.0 (#1771)
    * add importer for 8.1
    * add new test fixture to migration tests
    * small fixes
    
    closes #2595

---
+++ importer-810/src/main/java/io/camunda/tasklist/zeebeimport/v810/processors/ElasticsearchBulkProcessor.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.tasklist.zeebeimport.v810.processors;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.camunda.tasklist.exceptions.PersistenceException;
+import io.camunda.tasklist.exceptions.TasklistRuntimeException;
+import io.camunda.tasklist.util.ElasticsearchUtil;
+import io.camunda.tasklist.zeebe.ImportValueType;
+import io.camunda.tasklist.zeebeimport.AbstractImportBatchProcessor;
+import io.camunda.tasklist.zeebeimport.ImportBatch;
+import io.camunda.tasklist.zeebeimport.v810.record.RecordImpl;
+import io.camunda.tasklist.zeebeimport.v810.record.value.JobRecordValueImpl;
+import io.camunda.tasklist.zeebeimport.v810.record.value.ProcessInstanceRecordValueImpl;
+import io.camunda.tasklist.zeebeimport.v810.record.value.VariableDocumentRecordImpl;
+import io.camunda.tasklist.zeebeimport.v810.record.value.VariableRecordValueImpl;
+import io.camunda.tasklist.zeebeimport.v810.record.value.deployment.DeployedProcessImpl;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RecordValue;
+import java.util.List;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ElasticsearchBulkProcessor extends AbstractImportBatchProcessor {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ElasticsearchBulkProcessor.class);
+
+  @Autowired private ProcessInstanceZeebeRecordProcessor processInstanceZeebeRecordProcessor;
+
+  @Autowired private VariableZeebeRecordProcessor variableZeebeRecordProcessor;
+
+  @Autowired private JobZeebeRecordProcessor jobZeebeRecordProcessor;
+
+  @Autowired private ProcessZeebeRecordProcessor processZeebeRecordProcessor;
+
+  @Autowired private ObjectMapper objectMapper;
+
+  @Override
+  protected void processZeebeRecords(ImportBatch importBatch, BulkRequest bulkRequest)
+      throws PersistenceException {
+
+    final JavaType valueType =
+        objectMapper
+            .getTypeFactory()
+            .constructParametricType(
+                RecordImpl.class, getRecordValueClass(importBatch.getImportValueType()));
+    final List<Record> zeebeRecords =
+        ElasticsearchUtil.mapSearchHits(importBatch.getHits(), objectMapper, valueType);
+
+    final ImportValueType importValueType = importBatch.getImportValueType();
+
+    LOGGER.debug("Writing [{}] Zeebe records to Elasticsearch", zeebeRecords.size());
+    for (Record record : zeebeRecords) {
+      switch (importValueType) {
+        case PROCESS_INSTANCE:
+          processInstanceZeebeRecordProcessor.processProcessInstanceRecord(record, bulkRequest);
+          break;
+        case VARIABLE:
+          variableZeebeRecordProcessor.processVariableRecord(record, bulkRequest);
+          break;
+        case JOB:
+          jobZeebeRecordProcessor.processJobRecord(record, bulkRequest);
+          break;
+        case PROCESS:
+          // deployment records can be processed one by one
+          processZeebeRecordProcessor.processDeploymentRecord(record, bulkRequest);
+          break;
+        default:
+          LOGGER.debug("Default case triggered for type {}", importValueType);
+          break;
+      }
+    }
+  }
+
+  protected Class<? extends RecordValue> getRecordValueClass(ImportValueType importValueType) {
+    switch (importValueType) {
+      case PROCESS_INSTANCE:
+        return ProcessInstanceRecordValueImpl.class;
+      case JOB:
+        return JobRecordValueImpl.class;
+      case VARIABLE:
+        return VariableRecordValueImpl.class;
+      case VARIABLE_DOCUMENT:
+        return VariableDocumentRecordImpl.class;
+      case PROCESS:
+        return DeployedProcessImpl.class;
+      default:
+        throw new TasklistRuntimeException(
+            String.format("No value type class found for: %s", importValueType));
+    }
+  }
+
+  @Override
+  public String getZeebeVersion() {
+    return "8.1";
+  }
+}
---
+++ importer-810/src/main/java/io/camunda/tasklist/zeebeimport/v810/processors/JobZeebeRecordProcessor.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.tasklist.zeebeimport.v810.processors;
+
+import static io.camunda.tasklist.util.ElasticsearchUtil.UPDATE_RETRY_COUNT;
+import static io.camunda.tasklist.zeebeimport.v810.record.Intent.CANCELED;
+import static io.camunda.tasklist.zeebeimport.v810.record.Intent.COMPLETED;
+import static io.camunda.tasklist.zeebeimport.v810.record.Intent.CREATED;
+import static io.camunda.zeebe.protocol.Protocol.USER_TASK_ASSIGNEE_HEADER_NAME;
+import static io.camunda.zeebe.protocol.Protocol.USER_TASK_CANDIDATE_GROUPS_HEADER_NAME;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.camunda.tasklist.entities.TaskEntity;
+import io.camunda.tasklist.entities.TaskState;
+import io.camunda.tasklist.exceptions.PersistenceException;
+import io.camunda.tasklist.property.TasklistProperties;
+import io.camunda.tasklist.schema.templates.TaskTemplate;
+import io.camunda.tasklist.util.DateUtil;
+import io.camunda.tasklist.zeebeimport.v810.record.value.JobRecordValueImpl;
+import io.camunda.zeebe.protocol.Protocol;
+import io.camunda.zeebe.protocol.record.Record;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobZeebeRecordProcessor {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(JobZeebeRecordProcessor.class);
+
+  @Autowired private ObjectMapper objectMapper;
+
+  @Autowired private TaskTemplate taskTemplate;
+
+  @Autowired private TasklistProperties tasklistProperties;
+
+  public void processJobRecord(Record record, BulkRequest bulkRequest) throws PersistenceException {
+    final JobRecordValueImpl recordValue = (JobRecordValueImpl) record.getValue();
+    if (recordValue.getType().equals(Protocol.USER_TASK_JOB_TYPE)) {
+      bulkRequest.add(persistTask(record, recordValue));
+    }
+    // else skip task
+  }
+
+  private UpdateRequest persistTask(Record record, JobRecordValueImpl recordValue)
+      throws PersistenceException {
+    final String processDefinitionId = String.valueOf(recordValue.getProcessDefinitionKey());
+    final TaskEntity entity =
+        new TaskEntity()
+            .setId(String.valueOf(record.getKey()))
+            .setKey(record.getKey())
+            .setPartitionId(record.getPartitionId())
+            .setFlowNodeBpmnId(recordValue.getElementId())
+            .setFlowNodeInstanceId(String.valueOf(recordValue.getElementInstanceKey()))
+            .setProcessInstanceId(String.valueOf(recordValue.getProcessInstanceKey()))
+            .setBpmnProcessId(recordValue.getBpmnProcessId())
+            .setProcessDefinitionId(processDefinitionId);
+    final String formKey =
+        recordValue.getCustomHeaders().get(Protocol.USER_TASK_FORM_KEY_HEADER_NAME);
+    entity.setFormKey(formKey);
+
+    final String assignee = recordValue.getCustomHeaders().get(USER_TASK_ASSIGNEE_HEADER_NAME);
+    if (assignee != null) {
+      entity.setAssignee(assignee);
+    }
+
+    final String candidateGroups =
+        recordValue.getCustomHeaders().get(USER_TASK_CANDIDATE_GROUPS_HEADER_NAME);
+    if (candidateGroups != null) {
+      try {
+        entity.setCandidateGroups(objectMapper.readValue(candidateGroups, String[].class));
+      } catch (JsonProcessingException e) {
+        LOGGER.warn(
+            String.format(
+                "Candidate groups can't be parsed from %s: %s", candidateGroups, e.getMessage()),
+            e);
+      }
+    }
+
+    final String taskState = record.getIntent().name();
+    LOGGER.debug("JobState {}", taskState);
+    if (taskState.equals(CANCELED.name())) {
+      entity
+          .setState(TaskState.CANCELED)
+          .setCompletionTime(
+              DateUtil.toOffsetDateTime(Instant.ofEpochMilli(record.getTimestamp())));
+    } else if (taskState.equals(COMPLETED.name())) {
+      entity
+          .setState(TaskState.COMPLETED)
+          .setCompletionTime(
+              DateUtil.toOffsetDateTime(Instant.ofEpochMilli(record.getTimestamp())));
+    } else if (taskState.equals(CREATED.name())) {
+      entity
+          .setState(TaskState.CREATED)
+          .setCreationTime(DateUtil.toOffsetDateTime(Instant.ofEpochMilli(record.getTimestamp())));
+    } else {
+      LOGGER.warn(String.format("TaskState %s not supported", taskState));
+    }
+    return getTaskQuery(entity);
+  }
+
+  private UpdateRequest getTaskQuery(TaskEntity entity) throws PersistenceException {
+    try {
+      LOGGER.debug("Task instance: id {}", entity.getId());
+      final Map<String, Object> updateFields = new HashMap<>();
+      updateFields.put(TaskTemplate.STATE, entity.getState());
+      updateFields.put(TaskTemplate.COMPLETION_TIME, entity.getCompletionTime());
+
+      // format date fields properly
+      final Map<String, Object> jsonMap =
+          objectMapper.readValue(objectMapper.writeValueAsString(updateFields), HashMap.class);
+
+      return new UpdateRequest()
+          .index(taskTemplate.getFullQualifiedName())
+          .id(entity.getId())
+          .upsert(objectMapper.writeValueAsString(entity), XContentType.JSON)
+          .doc(jsonMap)
+          .retryOnConflict(UPDATE_RETRY_COUNT);
+
+    } catch (IOException e) {
+      throw new PersistenceException(
+          String.format("Error preparing the query to upsert task instance [%s]", entity.getId()),
+          e);
+    }
+  }
+}
