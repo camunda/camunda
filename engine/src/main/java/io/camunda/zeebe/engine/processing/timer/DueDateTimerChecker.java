e2e651d510d72bc1d2b260edbb6bb5fcc78d4483
pihme <pihme@users.noreply.github.com>
2022-04-28 11:50:47 +0200

    refactor(engine): extract lambda into class
    (cherry picked from commit 3a5bd48d68a3f39d930709b590963eb537e555da)

--- engine/src/main/java/io/camunda/zeebe/engine/processing/timer/DueDateTimerChecker.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/timer/DueDateTimerChecker.java
@@ -10,42 +10,22 @@ package io.camunda.zeebe.engine.processing.timer;
 import io.camunda.zeebe.engine.processing.scheduled.DueDateChecker;
 import io.camunda.zeebe.engine.processing.streamprocessor.ReadonlyProcessingContext;
 import io.camunda.zeebe.engine.processing.streamprocessor.StreamProcessorLifecycleAware;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
 import io.camunda.zeebe.engine.state.immutable.TimerInstanceState;
 import io.camunda.zeebe.protocol.impl.record.value.timer.TimerRecord;
 import io.camunda.zeebe.protocol.record.intent.TimerIntent;
 import io.camunda.zeebe.util.sched.clock.ActorClock;
 import java.time.Duration;
+import java.util.function.Function;
 
 public class DueDateTimerChecker implements StreamProcessorLifecycleAware {
 
   private static final long TIMER_RESOLUTION = Duration.ofMillis(100).toMillis();
   private final DueDateChecker dueDateChecker;
 
-  private final TimerRecord timerRecord = new TimerRecord();
-
   public DueDateTimerChecker(final TimerInstanceState timerInstanceState) {
     dueDateChecker =
-        new DueDateChecker(
-            TIMER_RESOLUTION,
-            typedCommandWriter ->
-                timerInstanceState.findTimersWithDueDateBefore(
-                    ActorClock.currentTimeMillis(),
-                    timer -> {
-                      timerRecord.reset();
-                      timerRecord
-                          .setElementInstanceKey(timer.getElementInstanceKey())
-                          .setProcessInstanceKey(timer.getProcessInstanceKey())
-                          .setDueDate(timer.getDueDate())
-                          .setTargetElementId(timer.getHandlerNodeId())
-                          .setRepetitions(timer.getRepetitions())
-                          .setProcessDefinitionKey(timer.getProcessDefinitionKey());
-
-                      typedCommandWriter.reset();
-                      typedCommandWriter.appendFollowUpCommand(
-                          timer.getKey(), TimerIntent.TRIGGER, timerRecord);
-
-                      return typedCommandWriter.flush() > 0;
-                    }));
+        new DueDateChecker(TIMER_RESOLUTION, new TriggerTimersSideEffect(timerInstanceState));
   }
 
   public void scheduleTimer(final long dueDate) {
@@ -75,5 +55,39 @@ public class DueDateTimerChecker implements StreamProcessorLifecycleAware {
   @Override
   public void onResumed() {
     dueDateChecker.onResumed();
+  }
+
+  protected static final class TriggerTimersSideEffect
+      implements Function<TypedCommandWriter, Long> {
+
+    private final TimerRecord timerRecord = new TimerRecord();
+
+    private final TimerInstanceState timerInstanceState;
+
+    public TriggerTimersSideEffect(final TimerInstanceState timerInstanceState) {
+      this.timerInstanceState = timerInstanceState;
+    }
+
+    @Override
+    public Long apply(final TypedCommandWriter typedCommandWriter) {
+      return timerInstanceState.processTimersWithDueDateBefore(
+          ActorClock.currentTimeMillis(),
+          timer -> {
+            timerRecord.reset();
+            timerRecord
+                .setElementInstanceKey(timer.getElementInstanceKey())
+                .setProcessInstanceKey(timer.getProcessInstanceKey())
+                .setDueDate(timer.getDueDate())
+                .setTargetElementId(timer.getHandlerNodeId())
+                .setRepetitions(timer.getRepetitions())
+                .setProcessDefinitionKey(timer.getProcessDefinitionKey());
+
+            typedCommandWriter.reset();
+            typedCommandWriter.appendFollowUpCommand(
+                timer.getKey(), TimerIntent.TRIGGER, timerRecord);
+
+            return typedCommandWriter.flush() > 0; // means the write was successful
+          });
+    }
   }
 }
