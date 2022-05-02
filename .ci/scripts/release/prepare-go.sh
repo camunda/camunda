2ea0d1527f956c688a709ee680b338341b6e88b3
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-05-02 17:20:58 +0200

    build(ci): fix gocompat compilation issue

--- .ci/scripts/release/prepare-go.sh
+++ .ci/scripts/release/prepare-go.sh
@@ -9,7 +9,15 @@ git config --global user.name "${GITHUB_TOKEN_USR}"
 # `go install` (and possibly GO111MODULE=on is not required)
 export CGO_ENABLED=0
 export GO111MODULE=on
-go get -u "github.com/smola/gocompat/...@v0.3.0"
+
+# We cannot build tooling dependencies from scratch, as these might suddenly rely on a dependency
+# does not build on our system anymore. Better to fetch the pre-compiled binary, unfortunately.
+# With Go 1.17, it might be possible that `go install` will solve this issue, so we should try again
+curl -sL https://github.com/smola/gocompat/releases/download/v0.3.0/gocompat_linux_amd64.tar.gz | tar -xz
+mv gocompat_linux_amd64 $GOPATH/bin/gocompat
+
+# go-bindata does not provided pre-compiled binaries, so we have to build it ourselves. That said,
+# we should switch to go:embed once we update the go version we use.
 go get -u "github.com/go-bindata/go-bindata/...@v3"
 
 # Verify binary is available under $GOPATH/bin
