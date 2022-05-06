2ea243b1ac4b130232ad7557a1af62d0f3a64ac2
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-05-06 17:58:52 +0200

    feat(exporter-test): add exporter API test implementations
    Adds test implementation for the exporter API, allowing easier testing
    of exporters.
    
    Configuration/context is kept to a minimum: users can set the
    configuration that will be instantiated beforehand, but no instantiation
    is done for them from the provided args. This is a limitation at the
    moment, to avoid coupling the broker's implementation and this one here.
    It's sufficient for our own use cases for now.
    
    The controller can schedule tasks and process them deterministically.
    This is done manually, by having users "tick" the controller manually -
    it will then run all scheduled tasks which have "expired" in order,
    synchronously. Tasks are kept in memory, even after being executed, so
    we can still assert properties of previous scheduled, canceled, or
    executed tasks.
    
    (cherry picked from commit 041c5226f33b468cdb2d38ac1e983d8f951369d1)

---
+++ exporter-test/src/main/java/io/camunda/zeebe/exporter/test/ExporterTestContext.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.exporter.test;
+
+import io.camunda.zeebe.exporter.api.context.Configuration;
+import io.camunda.zeebe.exporter.api.context.Context;
+import java.util.Objects;
+import net.jcip.annotations.NotThreadSafe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A mutable implementation of {@link Context} for testing. The context is passed only during the
+ * configuration phase, and any modifications afterwards isn't really used, so there is no real need
+ * to make this thread-safe at the moment.
+ */
+@NotThreadSafe
+public final class ExporterTestContext implements Context {
+  private static final Logger DEFAULT_LOGGER = LoggerFactory.getLogger(ExporterTestContext.class);
+
+  private Configuration configuration;
+  private RecordFilter recordFilter;
+
+  @Override
+  public Logger getLogger() {
+    return DEFAULT_LOGGER;
+  }
+
+  @Override
+  public Configuration getConfiguration() {
+    return configuration;
+  }
+
+  public ExporterTestContext setConfiguration(final Configuration configuration) {
+    this.configuration = Objects.requireNonNull(configuration, "must specify a configuration");
+    return this;
+  }
+
+  @Override
+  public void setFilter(final RecordFilter filter) {
+    recordFilter = filter;
+  }
+
+  public RecordFilter getRecordFilter() {
+    return recordFilter;
+  }
+}
