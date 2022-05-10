afd0bafeb9b0d78b9f44627b375ac7c9dd57c546
chaima-mnsr <25367434+chaima-mnsr@users.noreply.github.com>
2022-05-10 21:38:51 +0200

    adjust the helm chart to include the persistent environment
    Related to INFRA-3188

--- .github/actions/deploy-env/deploy_script.sh
+++ .github/actions/deploy-env/deploy_script.sh
@@ -7,10 +7,6 @@
 #
 #!/bin/bash
 
-persistent_deploy_arguments="--helm-set env=persistent \
-    --helm-set optimize.env.elasticsearchUrl="elasticsearch-es-http:9200" \
-    --helm-set cambpm.env.postgresUrl="optimize-persistent-postgres.optimize-persistent:5432""
-
 deploy_arguments="--dest-namespace optimize-${APP_NAME} \
     --file .ci/deployments-resources/argo/application.yml \
     --helm-set optimize.image.tag=${DOCKER_TAG} \
@@ -22,13 +18,11 @@ deploy_arguments="--dest-namespace optimize-${APP_NAME} \
     --project optimize-previews \
     --upsert"
 
-set -x
 
 if [[ "${APP_NAME}" == "persistent" ]];
 then
   argocd app create $deploy_arguments $persistent_deploy_arguments --revision master --helm-set git.branch=master \
-  --helm-set cambpm.env.javaOpts = \"\-Xms1g\ \-Xmx1g\ \-XX:MaxMetaspaceSize=256m\ \-Ddb.username=\$DB_USERNAME\ \-Ddb.password=\$DB_PASSWORD\"
-
+  --helm-set env=persistent
 else
   argocd app create $deploy_arguments  --revision ${REVISION} --helm-set git.branch=${REVISION}
 fi
