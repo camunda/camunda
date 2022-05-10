bdbd2cccc6390645595c1f7206be3b182ecf5feb
chaima-mnsr <25367434+chaima-mnsr@users.noreply.github.com>
2022-05-10 10:31:47 +0200

    Adjust the deploy bash script
    Related to INFRA-3188

--- .github/actions/deploy-env/deploy_script.sh
+++ .github/actions/deploy-env/deploy_script.sh
@@ -7,27 +7,27 @@
 #
 #!/bin/bash
 
-persistent_deploy_arguments='--helm-set env=persistent \
-    --helm-set optimize.env.elasticsearchUrl="elasticsearch-es-http:9200" \
-    --helm-set cambpm.env.javaOpts="-Xms1g \-Xmx1g \-XX:MaxMetaspaceSize=256m \-Ddb.username=$DB_USERNAME \-Ddb.password=$DB_PASSWORD" \
-    --helm-set cambpm.env.postgresUrl="optimize-persistent-postgres.optimize-persistent:5432"'
+persistent_deploy_arguments="--helm-set env=persistent \
+    --helm-set optimize.env.elasticsearchUrl='elasticsearch-es-http:9200' \
+    --helm-set cambpm.env.javaOpts='-Xms1g -Xmx1g -XX:MaxMetaspaceSize=256m -Ddb.username=\$DB_USERNAME -Ddb.password=\$DB_PASSWORD' \
+    --helm-set cambpm.env.postgresUrl='optimize-persistent-postgres.optimize-persistent:5432'"
 
-deploy_arguments='--dest-namespace optimize-${{ env.APP_NAME }} \
+deploy_arguments="--dest-namespace optimize-${APP_NAME} \
     --file .ci/deployments-resources/argo/application.yml \
-    --helm-set optimize.image.tag=${{ inputs.docker_tag }} \
-    --helm-set optimize.image.repository="gcr.io/ci-30-162810/camunda-optimize" \
-    --helm-set cambpm.image.tag=${{ inputs.cambpm_version }} \
-    --helm-set elasticsearch.image.tag=${{ inputs.es_version }} \
-    --helm-set git.branch=${{ inputs.revision }} \
-    --helm-set global.labels.app=${{ env.APP_NAME }} \
-    --helm-set global.labels.commit=${{ github.sha }} \
-    --name optimize-${{ env.APP_NAME }} \
+    --helm-set optimize.image.tag=${DOCKER_TAG} \
+    --helm-set optimize.image.repository=\"gcr.io/ci-30-162810/camunda-optimize\" \
+    --helm-set cambpm.image.tag=${CAMBPM_VERSION} \
+    --helm-set elasticsearch.image.tag=${ES_VERSION} \
+    --helm-set git.branch=${REVISION} \
+    --helm-set global.labels.app=${APP_NAME} \
+    --helm-set global.labels.commit=${SHA} \
+    --name optimize-${APP_NAME} \
     --project optimize-previews \
-    --revision ${{ inputs.revision }} \
-    --upsert'
+    --revision ${REVISION} \
+    --upsert"
 
 
-if [[ "$1" == "persistent" ]];
+if [[ "${APP_NAME}" == "persistent" ]];
 then
   argocd app create $deploy_arguments $persistent_deploy_arguments
 else
