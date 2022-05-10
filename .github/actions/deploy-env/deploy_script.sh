cac3b57fa19633c8907e7ef28c0865fabe6f1ece
chaima-mnsr <25367434+chaima-mnsr@users.noreply.github.com>
2022-05-10 21:07:01 +0200

    adjust the deploy script to use the right argument
    Related to INFRA-3188

--- .github/actions/deploy-env/deploy_script.sh
+++ .github/actions/deploy-env/deploy_script.sh
@@ -7,17 +7,15 @@
 #
 #!/bin/bash
 
-persistent_deploy_arguments='--helm-set env=persistent
-    --helm-set optimize.env.elasticsearchUrl="elasticsearch-es-http:9200"
-    --helm-set cambpm.env.javaOpts="-Xms1g \-Xmx1g \-XX:MaxMetaspaceSize=256m \-Ddb.username=\$DB_USERNAME \-Ddb.password=\$DB_PASSWORD"
-    --helm-set cambpm.env.postgresUrl="optimize-persistent-postgres.optimize-persistent:5432"'
+persistent_deploy_arguments="--helm-set env=persistent \
+    --helm-set optimize.env.elasticsearchUrl="elasticsearch-es-http:9200" \
+    --helm-set cambpm.env.postgresUrl="optimize-persistent-postgres.optimize-persistent:5432""
 
 deploy_arguments="--dest-namespace optimize-${APP_NAME} \
     --file .ci/deployments-resources/argo/application.yml \
     --helm-set optimize.image.tag=${DOCKER_TAG} \
     --helm-set cambpm.image.tag=${CAMBPM_VERSION} \
     --helm-set elasticsearch.image.tag=${ES_VERSION} \
-    --helm-set git.branch=${REVISION} \
     --helm-set global.labels.app=${APP_NAME} \
     --helm-set global.labels.commit=${SHA} \
     --name optimize-${APP_NAME} \
@@ -28,7 +26,9 @@ set -x
 
 if [[ "${APP_NAME}" == "persistent" ]];
 then
-  argocd app create $deploy_arguments $persistent_deploy_arguments --revision master
+  argocd app create $deploy_arguments $persistent_deploy_arguments --revision master --helm-set git.branch=master \
+  --helm-set cambpm.env.javaOpts = \"\-Xms1g\ \-Xmx1g\ \-XX:MaxMetaspaceSize=256m\ \-Ddb.username=\$DB_USERNAME\ \-Ddb.password=\$DB_PASSWORD\"
+
 else
-  argocd app create $deploy_arguments  --revision ${REVISION}
+  argocd app create $deploy_arguments  --revision ${REVISION} --helm-set git.branch=${REVISION}
 fi
