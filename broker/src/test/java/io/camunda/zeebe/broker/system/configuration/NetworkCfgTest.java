fbab77c60793af4104cc7f94dd099eef7139e600
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-05-31 18:21:56 +0200

    refactor(broker): replace internals static long value

--- broker/src/test/java/io/camunda/zeebe/broker/system/configuration/NetworkCfgTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/system/configuration/NetworkCfgTest.java
@@ -11,7 +11,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.zeebe.gateway.impl.configuration.NetworkCfg;
 import java.net.InetSocketAddress;
-import org.assertj.core.internal.bytebuddy.utility.RandomString;
 import org.junit.Test;
 
 public class NetworkCfgTest {
@@ -20,7 +19,7 @@ public class NetworkCfgTest {
   public void shouldNotFailOnLongHostName() {
     // given
     final var port = 1000;
-    final var hostName = RandomString.make(139);
+    final var hostName = "a".repeat(140);
     final var networkCfg = new NetworkCfg();
     networkCfg.setPort(port);
 
