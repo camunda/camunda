85a450ec7259e0bde4472eb19a9a31fb9eb2f9cb
pihme <pihme@users.noreply.github.com>
2022-06-02 17:49:14 +0200

    feat(gateway): map start instructions

--- gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerCreateProcessInstanceRequest.java
+++ gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerCreateProcessInstanceRequest.java
@@ -7,9 +7,11 @@
  */
 package io.camunda.zeebe.gateway.impl.broker.request;
 
+import io.camunda.zeebe.gateway.protocol.GatewayOuterClass.ProcessInstanceCreationStartInstruction;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationRecord;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceCreationIntent;
+import java.util.List;
 import org.agrona.DirectBuffer;
 
 public class BrokerCreateProcessInstanceRequest
@@ -38,6 +40,19 @@ public class BrokerCreateProcessInstanceRequest
 
   public BrokerCreateProcessInstanceRequest setVariables(final DirectBuffer variables) {
     requestDto.setVariables(variables);
+    return this;
+  }
+
+  public BrokerCreateProcessInstanceRequest setStartInstructions(
+      final List<ProcessInstanceCreationStartInstruction> startInstructionsList) {
+    startInstructionsList.stream()
+        .map(
+            startInstructionReq ->
+                new io.camunda.zeebe.protocol.impl.record.value.processinstance
+                        .ProcessInstanceCreationStartInstruction()
+                    .setElementId(startInstructionReq.getElementId()))
+        .forEach(requestDto::addStartInstruction);
+
     return this;
   }
 
