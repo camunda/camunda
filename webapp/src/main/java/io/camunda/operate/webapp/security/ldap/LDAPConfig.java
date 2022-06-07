b4368e2999bf79cdf5b8ddd032d71f0e6f729659
Svetlana Dorokhova <sveta_d@list.ru>
2022-06-07 20:48:48 +0200

    fix(ldap): clean up LdapConfig (#2863)

--- webapp/src/main/java/io/camunda/operate/webapp/security/ldap/LDAPConfig.java
+++ webapp/src/main/java/io/camunda/operate/webapp/security/ldap/LDAPConfig.java
@@ -6,17 +6,20 @@
  */
 package io.camunda.operate.webapp.security.ldap;
 
+import static io.camunda.operate.webapp.security.OperateProfileService.LDAP_AUTH_PROFILE;
+
 import io.camunda.operate.property.OperateProperties;
-import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 
 @Configuration
+@Profile(LDAP_AUTH_PROFILE)
 public class LDAPConfig {
 
   protected final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -24,30 +27,24 @@ public class LDAPConfig {
   @Autowired
   private OperateProperties operateProperties;
 
-  private LdapContextSource contextSource;
-
-  @PostConstruct
-  public void init(){
-    contextSource = new LdapContextSource();
-    contextSource.setUrl(operateProperties.getLdap().getUrl());
-    contextSource.setUserDn(operateProperties.getLdap().getManagerDn());
-    contextSource.setPassword(operateProperties.getLdap().getManagerPassword());
-  }
-
   @Bean
   public LdapTemplate ldapTemplate() {
     try {
-      contextSource
+      getContextSource()
           .getContext(operateProperties.getLdap().getManagerDn(),
               operateProperties.getLdap().getManagerPassword());
-    }catch (Exception e){
+    } catch (Exception e) {
       logger.error("Authentication for lookup failed.", e);
     }
-    return new LdapTemplate(contextSource);
+    return new LdapTemplate(getContextSource());
   }
 
   @Bean
-  public LdapContextSource contextSource() {
+  public LdapContextSource getContextSource() {
+    LdapContextSource contextSource = new LdapContextSource();
+    contextSource.setUrl(operateProperties.getLdap().getUrl());
+    contextSource.setUserDn(operateProperties.getLdap().getManagerDn());
+    contextSource.setPassword(operateProperties.getLdap().getManagerPassword());
     return contextSource;
   }
 }
