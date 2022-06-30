8175f320c9f993a777a5986efed8af98b57d4aea
Christopher Zell <zelldon91@googlemail.com>
2022-06-30 16:21:45 +0200

    migrate PendingProcessMessageSubscriptionChecker.java

--- engine/src/main/java/io/camunda/zeebe/engine/processing/message/PendingProcessMessageSubscriptionChecker.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/message/PendingProcessMessageSubscriptionChecker.java
@@ -8,12 +8,12 @@
 package io.camunda.zeebe.engine.processing.message;
 
 import io.camunda.zeebe.engine.processing.message.command.SubscriptionCommandSender;
+import io.camunda.zeebe.engine.processing.streamprocessor.ProcessingResult;
 import io.camunda.zeebe.engine.processing.streamprocessor.ReadonlyProcessingContext;
+import io.camunda.zeebe.engine.processing.streamprocessor.StreamProcessor.ProcessingSchedulingServiceImpl;
 import io.camunda.zeebe.engine.processing.streamprocessor.StreamProcessorLifecycleAware;
 import io.camunda.zeebe.engine.state.message.ProcessMessageSubscription;
 import io.camunda.zeebe.engine.state.mutable.MutablePendingProcessMessageSubscriptionState;
-import io.camunda.zeebe.util.sched.ActorControl;
-import io.camunda.zeebe.util.sched.ScheduledTimer;
 import io.camunda.zeebe.util.sched.clock.ActorClock;
 import java.time.Duration;
 
@@ -27,8 +27,7 @@ public final class PendingProcessMessageSubscriptionChecker
   private final MutablePendingProcessMessageSubscriptionState pendingState;
   private final long subscriptionTimeoutInMillis;
 
-  private ActorControl actor;
-  private ScheduledTimer timer;
+  private ProcessingSchedulingServiceImpl processingSchedulingService;
 
   public PendingProcessMessageSubscriptionChecker(
       final SubscriptionCommandSender commandSender,
@@ -40,46 +39,16 @@ public final class PendingProcessMessageSubscriptionChecker
 
   @Override
   public void onRecovered(final ReadonlyProcessingContext context) {
-    actor = context.getActor();
-    scheduleTimer();
+    processingSchedulingService = context.getProcessingSchedulingService();
+    processingSchedulingService.runWithDelay(
+        SUBSCRIPTION_CHECK_INTERVAL, this::checkPendingSubscriptions);
   }
 
-  @Override
-  public void onClose() {
-    cancelTimer();
-  }
-
-  @Override
-  public void onFailed() {
-    cancelTimer();
-  }
-
-  @Override
-  public void onPaused() {
-    cancelTimer();
-  }
-
-  @Override
-  public void onResumed() {
-    scheduleTimer();
-  }
-
-  private void scheduleTimer() {
-    if (timer == null) {
-      timer = actor.runAtFixedRate(SUBSCRIPTION_CHECK_INTERVAL, this::checkPendingSubscriptions);
-    }
-  }
-
-  private void cancelTimer() {
-    if (timer != null) {
-      timer.cancel();
-      timer = null;
-    }
-  }
-
-  private void checkPendingSubscriptions() {
+  private ProcessingResult checkPendingSubscriptions() {
     pendingState.visitSubscriptionBefore(
         ActorClock.currentTimeMillis() - subscriptionTimeoutInMillis, this::sendPendingCommand);
+
+    return ProcessingResult.empty();
   }
 
   private boolean sendPendingCommand(final ProcessMessageSubscription subscription) {
