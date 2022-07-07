8ba158243f2b218e344b16079fb1de3ed9617998
pihme <pihme@users.noreply.github.com>
2022-07-07 15:15:44 +0200

    refactor(engine): prepare engine abstraction
    - Add new package for classes that will fall into the scope of the platform
    - Implement arch unit test to check that no unwanted dependencies between packages exist

---
+++ engine/src/test/java/io/camunda/zeebe/engine/ArchitectureTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packages = "io.camunda.zeebe")
+public class ArchitectureTest {
+
+  @ArchTest
+  public static final ArchRule RULE_ENGINE_CLASSES_MUST_NOT_DEPEND_ON_STREAMPROCESSOR_PACKAGE =
+      noClasses()
+          .that()
+          .resideInAPackage("io.camunda.zeebe.engine..")
+          .should()
+          .dependOnClassesThat()
+          .resideInAPackage("io.camunda.zeebe.streamprocessor..");
+}
