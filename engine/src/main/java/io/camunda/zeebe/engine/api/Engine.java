e834d88a19c6bb69d3513a8f7dddd7d350d70c3d
pihme <pihme@users.noreply.github.com>
2022-07-07 15:33:30 +0200

    refactor(engine): add skeleton interface for engine abstraction

---
+++ engine/src/main/java/io/camunda/zeebe/engine/api/Engine.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.api;
+
+import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecord;
+
+public interface Engine {
+
+  void init(EngineContext engineContext);
+
+  void replay(TypedRecord record, ReplayContext replayContext);
+
+  ProcessingResult process(TypedRecord record, ProcessingContext processingContext);
+
+  ProcessingResult onProcessingError(
+      Throwable processingException,
+      TypedRecord record,
+      long position,
+      ErrorHandlingContext errorHandlingContext);
+}
