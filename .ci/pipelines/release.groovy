da09f40f570c9ffdd98bc6e4f6376de346fc3d75
Ralf Puchert <49151958+ralfpuchert@users.noreply.github.com>
2022-07-11 15:45:29 +0200

    chore(project): different artifact for 1.3 releases (#2997)

--- .ci/pipelines/release.groovy
+++ .ci/pipelines/release.groovy
@@ -38,7 +38,15 @@ void runRelease(params) {
 def githubRelease = '''\
 #!/bin/bash
 
-ARTIFACT="camunda-operate"
+CURRENT_BRANCH=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+if [[ $CURRENT_BRANCH =~ ^1.3..*-SNAPSHOT$ ]]
+then
+    ARTIFACT="camunda-cloud-operate"
+else
+    ARTIFACT="camunda-operate"
+fi
+
 ZEEBE_VERSION=$(mvn help:evaluate -Dexpression=version.zeebe -q -DforceStdout)
 
 cd target/checkout/distro/target
