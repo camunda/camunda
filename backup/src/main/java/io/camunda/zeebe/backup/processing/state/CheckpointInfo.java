defc73174ad1f6973aeb20df31ce4bbee20d33f6
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-07-14 15:52:14 +0200

    feat(backup): add checkpoint db state
    Checkpoint id and checkpoint position must be stored in the database.

---
+++ backup/src/main/java/io/camunda/zeebe/backup/processing/state/CheckpointInfo.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.backup.processing.state;
+
+import io.camunda.zeebe.db.DbValue;
+import io.camunda.zeebe.msgpack.UnpackedObject;
+import io.camunda.zeebe.msgpack.property.LongProperty;
+
+/** Checkpoint info stored in db in msgpack format. */
+public class CheckpointInfo extends UnpackedObject implements DbValue {
+  private final LongProperty idProperty = new LongProperty("id");
+  private final LongProperty positionProperty = new LongProperty("position");
+
+  public CheckpointInfo() {
+    declareProperty(idProperty).declareProperty(positionProperty);
+  }
+
+  public long getId() {
+    return idProperty.getValue();
+  }
+
+  public CheckpointInfo setId(final long id) {
+    idProperty.setValue(id);
+    return this;
+  }
+
+  public long getPosition() {
+    return positionProperty.getValue();
+  }
+
+  public CheckpointInfo setPosition(final long position) {
+    positionProperty.setValue(position);
+    return this;
+  }
+}
---
+++ backup/src/main/java/io/camunda/zeebe/backup/processing/state/CheckpointState.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.backup.processing.state;
+
+public interface CheckpointState {
+
+  long NO_CHECKPOINT = 0L;
+
+  /**
+   * Returns the id of the last created checkpoint
+   *
+   * @return checkpointId
+   */
+  long getCheckpointId();
+
+  /**
+   * Returns the position of the last created checkpoint
+   *
+   * @return checkpointPosition
+   */
+  long getCheckpointPosition();
+
+  /**
+   * Set checkpointId and checkpointPosition
+   *
+   * @param checkpointId id of the checkpoint
+   * @param checkpointPosition position of the checkpoint
+   */
+  void setCheckpointInfo(final long checkpointId, final long checkpointPosition);
+}
