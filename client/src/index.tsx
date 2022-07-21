f1d5387b826bf5ad2da8542b82292f0d65241500
huygur <45518829+huygur@users.noreply.github.com>
2022-07-21 07:57:58 +0300

    style: use font family from carbon (#2917)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -8,7 +8,6 @@
 import ReactDOM from 'react-dom';
 import {App} from './App';
 import './index.scss';
-import './index.css';
 import '@camunda-cloud/common-ui/dist/common-ui/common-ui.css';
 import 'dmn-js/dist/assets/dmn-js-decision-table.css';
 import 'dmn-js/dist/assets/dmn-js-shared.css';
