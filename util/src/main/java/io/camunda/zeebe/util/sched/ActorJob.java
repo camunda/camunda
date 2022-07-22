99286ac000b3a0886bc53e937849ab6b9e4f592c
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-07-22 16:19:06 +0200

    refactor(scheduler): remove isAutoCompleting as all tasks but subscriptions are auto completing
    (cherry picked from commit df99b9451738dd9659b55b73b60fcf6ad96ebeaa)

--- util/src/main/java/io/camunda/zeebe/util/sched/ActorJob.java
+++ util/src/main/java/io/camunda/zeebe/util/sched/ActorJob.java
@@ -27,7 +27,6 @@ public final class ActorJob {
   private Callable<?> callable;
   private Runnable runnable;
   private Object invocationResult;
-  private boolean isAutoCompleting;
   private ActorFuture resultFuture;
   private ActorSubscription subscription;
 
@@ -54,7 +53,7 @@ public final class ActorJob {
       actorThread = null;
 
       // in any case, success or exception, decide if the job should be resubmitted
-      if (isTriggeredBySubscription() || (isAutoCompleting && runnable == null)) {
+      if (isTriggeredBySubscription() || runnable == null) {
         schedulingState = TaskSchedulingState.TERMINATED;
       } else {
         schedulingState = TaskSchedulingState.QUEUED;
@@ -99,14 +98,9 @@ public final class ActorJob {
     callable = null;
     runnable = null;
     invocationResult = null;
-    isAutoCompleting = true;
 
     resultFuture = null;
     subscription = null;
-  }
-
-  public void setAutoCompleting(final boolean isAutoCompleting) {
-    this.isAutoCompleting = isAutoCompleting;
   }
 
   @Override
