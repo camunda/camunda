227302c0ac6c62496bc56cc91af6fc0b9da2b4c0
Ole Schönburg <ole.schoenburg@gmail.com>
2022-07-25 16:04:11 +0200

    fix: backoff for retrying deployment distribution
    Since we have changed the behavior slightly and now only acknowledge
    the distribution after processing the command on the receiver partition,
    we need to make sure that we don't overload slow partitions by retrying
    distribution too frequently.
    
    Here we keep track of the retry count to implement a simple exponential
    backoff, statically configured to start of at 10 seconds until it
    reaches a maximum of 5 minutes, doubling every time.
    
    Since we keep the retry counter in-memory, the backoff is reset when
    the deployment partition is recovered. Whether that is good behavior
    depends on the specific reason why a distribution is pending. For
    network issues this might be beneficial as a new leader for the
    deployment partition might not have the same network issues. If the
    receiving partition is unavailable or lagging, restarting with no
    backoff is bad but acceptable.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/distribute/DeploymentRedistributor.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/distribute/DeploymentRedistributor.java
@@ -13,13 +13,26 @@ import io.camunda.zeebe.engine.api.ReadonlyStreamProcessorContext;
 import io.camunda.zeebe.engine.api.StreamProcessorLifecycleAware;
 import io.camunda.zeebe.engine.state.immutable.DeploymentState;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.function.Predicate;
+import org.agrona.DirectBuffer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DeploymentRedistributor implements StreamProcessorLifecycleAware {
 
   public static final Duration DEPLOYMENT_REDISTRIBUTION_INTERVAL = Duration.ofSeconds(10);
+  private static final Duration RETRY_MAX_BACKOFF_DURATION = Duration.ofMinutes(5);
+  private static final long RETRY_MAX_BACKOFF_ATTEMPTS =
+      RETRY_MAX_BACKOFF_DURATION.dividedBy(DEPLOYMENT_REDISTRIBUTION_INTERVAL);
+  private static final Logger LOG = LoggerFactory.getLogger(DeploymentRedistributor.class);
   private final int partitionsCount;
   private final DeploymentDistributionCommandSender deploymentDistributionCommandSender;
   private final DeploymentState deploymentState;
+  private final Map<PendingDistribution, Long> distributionAttempts = new HashMap<>();
+  private DeploymentDistributionBehavior deploymentDistributionBehavior;
 
   public DeploymentRedistributor(
       final int partitionsCount,
@@ -35,15 +48,57 @@ public class DeploymentRedistributor implements StreamProcessorLifecycleAware {
     if (context.getPartitionId() != DEPLOYMENT_PARTITION) {
       return;
     }
-    final var deploymentDistributionBehavior =
+    deploymentDistributionBehavior =
         new DeploymentDistributionBehavior(
             context.getWriters(), partitionsCount, deploymentDistributionCommandSender);
 
-    final var schedulingService = context.getScheduleService();
-    schedulingService.runAtFixedRate(
-        DEPLOYMENT_REDISTRIBUTION_INTERVAL,
-        () ->
-            deploymentState.foreachPendingDeploymentDistribution(
-                deploymentDistributionBehavior::distributeDeploymentToPartition));
+    context
+        .getScheduleService()
+        .runAtFixedRate(DEPLOYMENT_REDISTRIBUTION_INTERVAL, this::runRetryCycle);
   }
+
+  private void runRetryCycle() {
+    final var pendingDistributions = new HashSet<PendingDistribution>();
+    deploymentState.foreachPendingDeploymentDistribution(
+        (deploymentKey, partitionId, directBuffer) -> {
+          final var pending = new PendingDistribution(deploymentKey, partitionId);
+          pendingDistributions.add(pending);
+          retryDistribution(pending, directBuffer);
+        });
+    // Remove attempts for completed distributions
+    distributionAttempts.keySet().removeIf(Predicate.not(pendingDistributions::contains));
+  }
+
+  private void retryDistribution(
+      final PendingDistribution pending, final DirectBuffer copiedDeploymentBuffer) {
+    if (!shouldRetryNow(pending)) {
+      return;
+    }
+
+    LOG.info(
+        "Retrying to distribute deployment {} to partition {}",
+        pending.deploymentKey,
+        pending.partitionId);
+    deploymentDistributionBehavior.distributeDeploymentToPartition(
+        pending.deploymentKey, pending.partitionId, copiedDeploymentBuffer);
+  }
+
+  private boolean shouldRetryNow(final PendingDistribution distributionKey) {
+    // attempt starts off at 0, ensuring that we wait between DEPLOYMENT_REDISTRIBUTION_INTERVAL and
+    // 2 * DEPLOYMENT_REDISTRIBUTION_INTERVAL before retrying distribution.
+    final long attempt =
+        distributionAttempts.compute(
+            distributionKey, (k, attempts) -> attempts != null ? attempts + 1 : 0L);
+
+    if (attempt >= RETRY_MAX_BACKOFF_ATTEMPTS) {
+      // Retry in intervals of RETRY_MAX_BACKOFF_DURATION
+      return attempt % RETRY_MAX_BACKOFF_ATTEMPTS == 0;
+    } else {
+      // Retry in intervals of DEPLOYMENT_REDISTRIBUTION_INTERVAL
+      // The interval is doubling until we reached RETRY_MAX_BACKOFF_DURATION
+      return Long.bitCount(attempt) == 1;
+    }
+  }
+
+  private record PendingDistribution(long deploymentKey, int partitionId) {}
 }
