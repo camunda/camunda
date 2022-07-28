97b989b24859f40d8db9bbb0cf32068da72c6933
Remco Westerhoud <remco@westerhoud.nl>
2022-07-28 15:46:23 +0200

    fix: licensing
    Set license headers to the year 2017 like the maven plugin wants me to.
    When automatically adding a license header also set it to 2017

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/response/ModifyProcessInstanceResponseImpl.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/response/ModifyProcessInstanceResponseImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 camunda services GmbH (info@camunda.com)
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
