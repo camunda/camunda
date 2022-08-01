971837d62d48f99c13dbe08ce592936c9cc4628c
Ole Schönburg <ole.schoenburg@gmail.com>
2022-08-01 19:33:33 +0200

    test: verify handling of pause exporting request

--- broker/src/test/java/io/camunda/zeebe/broker/transport/adminapi/AdminApiRequestHandlerTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/transport/adminapi/AdminApiRequestHandlerTest.java
@@ -8,149 +8,55 @@
 package io.camunda.zeebe.broker.transport.adminapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import io.atomix.primitive.partition.ManagedPartitionGroup;
 import io.atomix.raft.RaftServer.Role;
 import io.atomix.raft.partition.RaftPartition;
 import io.atomix.raft.partition.RaftPartitionGroup;
+import io.camunda.zeebe.broker.partitioning.PartitionAdminAccess;
 import io.camunda.zeebe.broker.partitioning.PartitionManagerImpl;
 import io.camunda.zeebe.protocol.impl.encoding.AdminRequest;
 import io.camunda.zeebe.protocol.impl.encoding.AdminResponse;
 import io.camunda.zeebe.protocol.impl.encoding.ErrorResponse;
 import io.camunda.zeebe.protocol.management.AdminRequestType;
 import io.camunda.zeebe.protocol.record.ErrorCode;
+import io.camunda.zeebe.scheduler.future.CompletableActorFuture;
 import io.camunda.zeebe.scheduler.testing.ControlledActorSchedulerExtension;
 import io.camunda.zeebe.transport.ServerOutput;
 import io.camunda.zeebe.transport.impl.AtomixServerTransport;
 import io.camunda.zeebe.util.Either;
-import io.camunda.zeebe.util.buffer.BufferWriter;
 import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import org.agrona.ExpandableArrayBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
+import org.mockito.Answers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-class AdminApiRequestHandlerTest {
-  @RegisterExtension
-  final ControlledActorSchedulerExtension scheduler = new ControlledActorSchedulerExtension();
-
-  final AtomixServerTransport transport = mock(AtomixServerTransport.class);
-
-  @Test
-  void shouldRejectRequestWithInvalidType() {
-    // given
-    final var handler = setupDefaultHandler();
-    final var request = new AdminRequest();
-    request.setType(AdminRequestType.NULL_VAL);
-
-    // when
-    final var responseFuture = handleRequest(request, handler);
-
-    // then
-    assertThat(responseFuture)
-        .succeedsWithin(Duration.ofMinutes(1))
-        .matches(Either::isLeft)
-        .matches(Either::isLeft)
-        .extracting(Either::getLeft)
-        .extracting(ErrorResponse::getErrorCode)
-        .isEqualTo(ErrorCode.UNSUPPORTED_MESSAGE);
-  }
-
-  @Test
-  void shouldInitiateStepdown() {
-    // given
-    final var handler = setupDefaultHandler();
-    final var request = new AdminRequest();
-    request.setType(AdminRequestType.STEP_DOWN_IF_NOT_PRIMARY);
-
-    // when
-    final var responseFuture = handleRequest(request, handler);
-
-    // then
-    assertThat(responseFuture).succeedsWithin(Duration.ofMinutes(1)).matches(Either::isRight);
-  }
-
-  @Test
-  void shouldRejectRequestWhenNoPartitionsAreKnown() {
-    // given
-    final var partitionManager = mock(PartitionManagerImpl.class);
-    final var partitionGroup = mock(RaftPartitionGroup.class);
-    when(partitionGroup.getPartitionIds()).thenReturn(List.of());
-    when(partitionManager.getPartitionGroup()).thenReturn(partitionGroup);
-    final var handler = new AdminApiRequestHandler(transport, partitionManager);
-    scheduler.submitActor(handler);
-    scheduler.workUntilDone();
-
-    final var request = new AdminRequest();
-    request.setType(AdminRequestType.STEP_DOWN_IF_NOT_PRIMARY);
-
-    // when
-    final var responseFuture = handleRequest(request, handler);
-
-    // then
-    assertThat(responseFuture)
-        .succeedsWithin(Duration.ofMinutes(1))
-        .matches(Either::isLeft)
-        .extracting(Either::getLeft)
-        .extracting(ErrorResponse::getErrorCode)
-        .isEqualTo(ErrorCode.PARTITION_LEADER_MISMATCH); // no partitions -> no handler subscribed
-  }
-
-  @Test
-  void shouldRejectRequestWhenNotLeader() {
-    // given
-    final var partitionManager = mock(PartitionManagerImpl.class);
-    final var partitionGroup = mock(RaftPartitionGroup.class);
-    final var partition = mock(RaftPartition.class);
-    when(partition.getRole()).thenReturn(Role.FOLLOWER);
-    when(partitionGroup.getPartition(anyInt())).thenReturn(partition);
-    when(partitionManager.getPartitionGroup()).thenReturn(partitionGroup);
-    final var handler = new AdminApiRequestHandler(transport, partitionManager);
-    scheduler.submitActor(handler);
-    scheduler.workUntilDone();
-
-    final var request = new AdminRequest();
-    request.setType(AdminRequestType.STEP_DOWN_IF_NOT_PRIMARY);
-
-    // when
-    final var responseFuture = handleRequest(request, handler);
-
-    // then
-    assertThat(responseFuture)
-        .succeedsWithin(Duration.ofMinutes(1))
-        .matches(Either::isLeft)
-        .extracting(Either::getLeft)
-        .extracting(ErrorResponse::getErrorCode)
-        .isEqualTo(ErrorCode.PARTITION_LEADER_MISMATCH); // no partitions -> no handler subscribed
-  }
-
-  private AdminApiRequestHandler setupDefaultHandler() {
-    final var partitionManager = mock(PartitionManagerImpl.class);
-    final var partitionGroup = mock(RaftPartitionGroup.class);
-    final var raftPartition = mock(RaftPartition.class);
-    when(raftPartition.getRole()).thenReturn(Role.LEADER);
-    when(raftPartition.stepDownIfNotPrimary()).thenReturn(CompletableFuture.completedFuture(null));
-    when(partitionGroup.name()).thenReturn("test");
-    when(partitionGroup.getPartition(anyInt())).thenReturn(raftPartition);
-    when(partitionManager.getPartitionGroup()).thenReturn(partitionGroup);
-    final var handler = new AdminApiRequestHandler(transport, partitionManager);
-    scheduler.submitActor(handler);
-    scheduler.workUntilDone();
-    return handler;
-  }
+@Execution(ExecutionMode.CONCURRENT)
+final class AdminApiRequestHandlerTest {
 
   private CompletableFuture<Either<ErrorResponse, AdminResponse>> handleRequest(
-      final BufferWriter request, final AdminApiRequestHandler handler) {
+      final AdminRequest request, final AdminApiRequestHandler handler) {
     final var future = new CompletableFuture<Either<ErrorResponse, AdminResponse>>();
     final ServerOutput serverOutput = createServerOutput(future);
     final var requestBuffer = new UnsafeBuffer(new byte[request.getLength()]);
     request.write(requestBuffer, 0);
-    handler.onRequest(serverOutput, 0, 0, requestBuffer, 0, request.getLength());
-    scheduler.workUntilDone();
+    handler.onRequest(
+        serverOutput, request.getPartitionId(), 0, requestBuffer, 0, request.getLength());
     return future;
   }
 
@@ -175,5 +81,209 @@ class AdminApiRequestHandlerTest {
         future.completeExceptionally(e);
       }
     };
+  }
+
+  @Nested
+  @ExtendWith(MockitoExtension.class)
+  final class OtherRequest {
+    @RegisterExtension
+    final ControlledActorSchedulerExtension scheduler = new ControlledActorSchedulerExtension();
+
+    private final AdminApiRequestHandler handler;
+
+    OtherRequest(
+        @Mock final AtomixServerTransport transport,
+        @Mock final PartitionManagerImpl partitionManager) {
+      when(partitionManager.getPartitionGroup()).thenReturn(mock(ManagedPartitionGroup.class));
+      when(partitionManager.getPartitions()).thenReturn(List.of());
+      handler = new AdminApiRequestHandler(transport, partitionManager);
+    }
+
+    @BeforeEach
+    void installHandler() {
+      scheduler.submitActor(handler);
+      scheduler.workUntilDone();
+    }
+
+    @Test
+    void shouldRejectRequestWithInvalidType() {
+      // given
+      final var request = new AdminRequest();
+      request.setType(AdminRequestType.NULL_VAL);
+
+      // when
+      final var responseFuture = handleRequest(request, handler);
+      scheduler.workUntilDone();
+
+      // then
+      assertThat(responseFuture)
+          .succeedsWithin(Duration.ofMinutes(1))
+          .matches(Either::isLeft)
+          .matches(Either::isLeft)
+          .extracting(Either::getLeft)
+          .extracting(ErrorResponse::getErrorCode)
+          .isEqualTo(ErrorCode.UNSUPPORTED_MESSAGE);
+    }
+  }
+
+  @Nested
+  @ExtendWith(MockitoExtension.class)
+  final class PauseExportingRequest {
+    @RegisterExtension
+    final ControlledActorSchedulerExtension scheduler = new ControlledActorSchedulerExtension();
+
+    private final PartitionAdminAccess adminAccess;
+    private final AdminApiRequestHandler handler;
+
+    public PauseExportingRequest(
+        @Mock(answer = Answers.RETURNS_SELF) final PartitionAdminAccess adminAccess,
+        @Mock final PartitionManagerImpl partitionManager,
+        @Mock final AtomixServerTransport transport) {
+      this.adminAccess = adminAccess;
+      when(partitionManager.getPartitionGroup()).thenReturn(mock(ManagedPartitionGroup.class));
+      when(partitionManager.getPartitions()).thenReturn(List.of());
+      when(partitionManager.createAdminAccess(any())).thenReturn(adminAccess);
+      handler = new AdminApiRequestHandler(transport, partitionManager);
+    }
+
+    @BeforeEach
+    void startHandler() {
+      scheduler.submitActor(handler);
+      scheduler.workUntilDone();
+    }
+
+    @Test
+    void shouldPauseExportingForGivenPartition() {
+      when(adminAccess.pauseExporting()).thenReturn(CompletableActorFuture.completed(null));
+
+      final var request = new AdminRequest();
+      request.setPartitionId(1);
+      request.setType(AdminRequestType.PAUSE_EXPORTING);
+
+      // when
+      final var responseFuture = handleRequest(request, handler);
+      scheduler.workUntilDone();
+
+      // then
+      assertThat(responseFuture).succeedsWithin(Duration.ofMinutes(1)).matches(Either::isRight);
+      verify(adminAccess).forPartition(request.getPartitionId());
+      verify(adminAccess).pauseExporting();
+    }
+
+    @Test
+    void shouldRespondWithFailureIfPausingFails() {
+      // given
+      when(adminAccess.pauseExporting())
+          .thenReturn(
+              CompletableActorFuture.completedExceptionally(
+                  new RuntimeException("Exporting fails")));
+
+      final var request = new AdminRequest();
+      request.setPartitionId(1);
+      request.setType(AdminRequestType.PAUSE_EXPORTING);
+
+      // when
+      final var responseFuture = handleRequest(request, handler);
+      scheduler.workUntilDone();
+
+      // then
+      assertThat(responseFuture)
+          .succeedsWithin(Duration.ofMinutes(1))
+          .matches(Either::isLeft)
+          .extracting(Either::getLeft)
+          .extracting(ErrorResponse::getErrorCode)
+          .isEqualTo(ErrorCode.INTERNAL_ERROR);
+    }
+  }
+
+  @Nested
+  @ExtendWith(MockitoExtension.class)
+  final class StepdownRequest {
+    @RegisterExtension
+    final ControlledActorSchedulerExtension scheduler = new ControlledActorSchedulerExtension();
+
+    private final AdminApiRequestHandler handler;
+    private final PartitionManagerImpl partitionManager;
+
+    StepdownRequest(
+        @Mock final AtomixServerTransport transport,
+        @Mock final PartitionManagerImpl partitionManager) {
+      this.partitionManager = partitionManager;
+      when(partitionManager.getPartitionGroup()).thenReturn(mock(ManagedPartitionGroup.class));
+      when(partitionManager.getPartitions()).thenReturn(List.of());
+      handler = new AdminApiRequestHandler(transport, partitionManager);
+    }
+
+    @BeforeEach
+    void installHandler() {
+      scheduler.submitActor(handler);
+      scheduler.workUntilDone();
+    }
+
+    @Test
+    void shouldInitiateStepdown() {
+      // given
+      final var partition = mock(RaftPartition.class);
+      when(partition.getRole()).thenReturn(Role.LEADER);
+
+      final var partitionGroup = mock(ManagedPartitionGroup.class);
+      when(partitionGroup.getPartition(anyInt())).thenReturn(partition);
+      when(partitionManager.getPartitionGroup()).thenReturn(partitionGroup);
+
+      final var request = new AdminRequest();
+      request.setType(AdminRequestType.STEP_DOWN_IF_NOT_PRIMARY);
+
+      // when
+      final var responseFuture = handleRequest(request, handler);
+      scheduler.workUntilDone();
+
+      // then
+      assertThat(responseFuture).succeedsWithin(Duration.ofMinutes(1)).matches(Either::isRight);
+    }
+
+    @Test
+    void shouldRejectRequestWhenNoPartitionsAreKnown() {
+      // given
+
+      final var request = new AdminRequest();
+      request.setType(AdminRequestType.STEP_DOWN_IF_NOT_PRIMARY);
+
+      // when
+      final var responseFuture = handleRequest(request, handler);
+      scheduler.workUntilDone();
+
+      // then
+      assertThat(responseFuture)
+          .succeedsWithin(Duration.ofMinutes(1))
+          .matches(Either::isLeft)
+          .extracting(Either::getLeft)
+          .extracting(ErrorResponse::getErrorCode)
+          .isEqualTo(ErrorCode.PARTITION_LEADER_MISMATCH); // no partitions -> no handler subscribed
+    }
+
+    @Test
+    void shouldRejectRequestWhenNotLeader() {
+      // given
+      final var partitionGroup = mock(RaftPartitionGroup.class);
+      final var partition = mock(RaftPartition.class);
+      when(partition.getRole()).thenReturn(Role.FOLLOWER);
+      when(partitionGroup.getPartition(anyInt())).thenReturn(partition);
+      when(partitionManager.getPartitionGroup()).thenReturn(partitionGroup);
+
+      final var request = new AdminRequest();
+      request.setType(AdminRequestType.STEP_DOWN_IF_NOT_PRIMARY);
+
+      // when
+      final var responseFuture = handleRequest(request, handler);
+      scheduler.workUntilDone();
+
+      // then
+      assertThat(responseFuture)
+          .succeedsWithin(Duration.ofMinutes(1))
+          .matches(Either::isLeft)
+          .extracting(Either::getLeft)
+          .extracting(ErrorResponse::getErrorCode)
+          .isEqualTo(ErrorCode.PARTITION_LEADER_MISMATCH); // no partitions -> no handler subscribed
+    }
   }
 }
