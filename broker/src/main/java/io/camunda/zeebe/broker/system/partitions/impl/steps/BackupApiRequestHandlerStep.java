4402a685c2afbc8b48a822fa6ff59fab9617645e
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-08-02 10:39:43 +0200

    refactor(broker): fix name of BackupRequestHandler step

--- broker/src/main/java/io/camunda/zeebe/broker/system/partitions/impl/steps/BackupApiRequestHandlerStep.java
+++ broker/src/main/java/io/camunda/zeebe/broker/system/partitions/impl/steps/BackupApiRequestHandlerStep.java
@@ -42,7 +42,7 @@ public final class BackupApiRequestHandlerStep implements PartitionTransitionSte
 
   @Override
   public String getName() {
-    return "BackupApiRequestHandlerStep";
+    return "BackupApiRequestHandler";
   }
 
   private ActorFuture<Void> installRequestHandler(final PartitionTransitionContext context) {
