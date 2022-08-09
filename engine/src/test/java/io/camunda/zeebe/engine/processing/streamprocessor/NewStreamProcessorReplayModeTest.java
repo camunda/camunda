ca16cb2360cbfedff47c1f215dccade9ec31849e
Christopher Zell <zelldon91@googlemail.com>
2022-08-09 12:35:26 +0200

    test: create StreamPlatformExtension

---
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/streamprocessor/NewStreamProcessorReplayModeTest.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.processing.streamprocessor;
+
+import static io.camunda.zeebe.engine.util.RecordToWrite.command;
+import static io.camunda.zeebe.engine.util.RecordToWrite.event;
+import static io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent.ACTIVATE_ELEMENT;
+import static io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent.ELEMENT_ACTIVATING;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.timeout;
+
+import io.camunda.zeebe.engine.util.Records;
+import io.camunda.zeebe.engine.util.StreamPlatform;
+import io.camunda.zeebe.engine.util.StreamPlatformExtension;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
+import io.camunda.zeebe.streamprocessor.StreamProcessor.Phase;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.verification.VerificationWithTimeout;
+
+@ExtendWith(StreamPlatformExtension.class)
+public final class NewStreamProcessorReplayModeTest {
+
+  private static final long TIMEOUT_MILLIS = 2_000L;
+  private static final VerificationWithTimeout TIMEOUT = timeout(TIMEOUT_MILLIS);
+
+  private static final ProcessInstanceRecord RECORD = Records.processInstance(1);
+
+  @SuppressWarnings("unused") // injected by the extension
+  private StreamPlatform streamPlatform;
+
+  @Test
+  public void shouldProcessAfterReplay() {
+    // given
+    streamPlatform.writeBatch(
+        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+
+    // when
+    streamPlatform
+        .startStreamProcessorNotWaitForOpening();
+
+    Awaitility.await()
+        .untilAsserted(
+            () -> assertThat(streamPlatform.getCurrentProcessorPhase()).isEqualTo(Phase.PROCESSING));
+
+    streamPlatform.writeBatch(
+        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+
+    // then
+    final var recordProcessor = streamPlatform.getRecordProcessor();
+    final InOrder inOrder = inOrder(recordProcessor);
+    inOrder.verify(recordProcessor, TIMEOUT).replay(any());
+    inOrder.verify(recordProcessor, TIMEOUT).process(any(), any());
+    inOrder.verifyNoMoreInteractions();
+  }
+
+//
+//  @Test
+//  public void shouldReplayIfNoEventsAfterSnapshot() {
+//    // given
+//    startStreamProcessor(replayContinuously);
+//    final var snapshotPosition = 1L;
+//    replayContinuously.getLastProcessedPositionState().markAsProcessed(snapshotPosition);
+//    replayContinuously.snapshot();
+//    replayContinuously.closeStreamProcessor();
+//
+//    // when - restart with snapshot, but the events in the snapshot are not available at startup
+//    startStreamProcessor(replayContinuously);
+//
+//    // event already applied in snapshot
+//    replayContinuously.writeEvent(
+//        ProcessInstanceIntent.ELEMENT_ACTIVATING,
+//        RECORD,
+//        writer -> writer.key(1L).sourceRecordPosition(snapshotPosition));
+//
+//    // new events to replay
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    // then
+//    final InOrder inOrder = inOrder(typedRecordProcessor, eventApplier);
+//    inOrder
+//        .verify(eventApplier, TIMEOUT.times(1))
+//        .applyState(anyLong(), eq(ELEMENT_ACTIVATING), any());
+//    inOrder.verifyNoMoreInteractions();
+//  }
+//
+//  @Test
+//  public void shouldNotReplayWhenPaused() {
+//    // given
+//    startWithPausedStreamProcessor(replayContinuously);
+//
+//    // when
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    // then
+//    final var inOrder = inOrder(typedRecordProcessor, eventApplier);
+//    inOrder.verifyNoMoreInteractions();
+//    assertThat(getCurrentPhase(replayContinuously)).isEqualTo(Phase.PAUSED);
+//  }
+//
+//  @Test
+//  public void shouldPauseReplay() {
+//    // given
+//    final var streamProcessor = startStreamProcessor(replayContinuously);
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    Awaitility.await("should have replayed first events")
+//        .until(replayContinuously::getLastSuccessfulProcessedRecordPosition, (pos) -> pos > 0);
+//
+//    // when
+//    streamProcessor.pauseProcessing().join();
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    // then
+//    final var inOrder = inOrder(typedRecordProcessor, eventApplier);
+//    inOrder
+//        .verify(eventApplier, TIMEOUT.times(1))
+//        .applyState(anyLong(), eq(ELEMENT_ACTIVATING), any());
+//    inOrder.verifyNoMoreInteractions();
+//
+//    assertThat(getCurrentPhase(replayContinuously)).isEqualTo(Phase.PAUSED);
+//  }
+//
+//  @Test
+//  public void shouldReplayAfterResumed() {
+//    // given
+//    startWithPausedStreamProcessor(replayContinuously);
+//
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    // when
+//    replayContinuously.resumeProcessing(1);
+//
+//    // then
+//    verify(eventApplier, TIMEOUT.times(1)).applyState(anyLong(), eq(ELEMENT_ACTIVATING), any());
+//
+//    verifyNoMoreInteractions(eventApplier);
+//
+//    assertThat(getCurrentPhase(replayContinuously)).isEqualTo(Phase.REPLAY);
+//  }
+//
+//  @Test
+//  public void shouldReplayMoreAfterResumed() {
+//    // given
+//    final var streamProcessor = startStreamProcessor(replayContinuously);
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    Awaitility.await("should have replayed first events")
+//        .until(replayContinuously::getLastSuccessfulProcessedRecordPosition, (pos) -> pos > 0);
+//    streamProcessor.pauseProcessing().join();
+//    replayContinuously.writeBatch(
+//        command().processInstance(ACTIVATE_ELEMENT, RECORD),
+//        event().processInstance(ELEMENT_ACTIVATING, RECORD).causedBy(0));
+//
+//    // when
+//    streamProcessor.resumeProcessing();
+//
+//    // then
+//    final var inOrder = inOrder(typedRecordProcessor, eventApplier);
+//    inOrder
+//        .verify(eventApplier, TIMEOUT.times(2))
+//        .applyState(anyLong(), eq(ELEMENT_ACTIVATING), any());
+//    inOrder.verifyNoMoreInteractions();
+//
+//    assertThat(getCurrentPhase(replayContinuously)).isEqualTo(Phase.REPLAY);
+//  }
+//
+//  @Test
+//  public void shouldUpdateLastProcessedAndWrittenPositionOnReplay() {
+//    // given
+//    startStreamProcessor(replayContinuously);
+//
+//    // when
+//    final var commandPosition = replayContinuously.writeCommand(ACTIVATE_ELEMENT, RECORD);
+//    final var eventPosition =
+//        replayContinuously.writeEvent(
+//            ELEMENT_ACTIVATING, RECORD, event -> event.sourceRecordPosition(commandPosition));
+//
+//    // then
+//    verify(eventApplier, TIMEOUT).applyState(anyLong(), eq(ELEMENT_ACTIVATING), any());
+//
+//    Awaitility.await()
+//        .untilAsserted(
+//            () -> {
+//              final var lastProcessedPosition = getLastProcessedPosition(replayContinuously);
+//              final var lastWrittenPosition = getLastWrittenPosition(replayContinuously);
+//
+//              assertThat(lastProcessedPosition)
+//                  .describedAs(
+//                      "Expected the position of the command to be the last processed position")
+//                  .isEqualTo(commandPosition);
+//
+//              assertThat(lastWrittenPosition)
+//                  .describedAs("Expected the position of the event to be the last written position")
+//                  .isEqualTo(eventPosition);
+//            });
+//  }
+//
+//  @Test
+//  public void shouldSetLastProcessedPositionOnStateToSourcePosition() {
+//    // given
+//    startStreamProcessor(replayContinuously);
+//
+//    // when
+//    final var commandPosition = replayContinuously.writeCommand(ACTIVATE_ELEMENT, RECORD);
+//    replayContinuously.writeEvent(
+//        ELEMENT_ACTIVATING, RECORD, event -> event.sourceRecordPosition(commandPosition));
+//
+//    verify(eventApplier, TIMEOUT).applyState(anyLong(), eq(ELEMENT_ACTIVATING), any());
+//
+//    Awaitility.await()
+//        .until(() -> getLastProcessedPosition(replayContinuously), isEqual(commandPosition));
+//
+//    // then
+//    assertThat(replayContinuously.getLastSuccessfulProcessedRecordPosition())
+//        .describedAs("Last processed position in the state must be the last source position")
+//        .isEqualTo(commandPosition);
+//  }
+//
+//  @Test
+//  public void shouldNotSetLastProcessedPositionIfLessThanSnapshotPosition() {
+//    // given
+//    final var commandPositionBeforeSnapshot = 1L;
+//    final var snapshotPosition = 2L;
+//
+//    startStreamProcessor(replayContinuously);
+//
+//    replayContinuously.getLastProcessedPositionState().markAsProcessed(snapshotPosition);
+//
+//    replayContinuously.snapshot();
+//    replayContinuously.closeStreamProcessor();
+//
+//    // when
+//    startStreamProcessor(replayContinuously);
+//
+//    Awaitility.await()
+//        .untilAsserted(
+//            () -> assertThat(getCurrentPhase(replayContinuously)).isEqualTo(Phase.REPLAY));
+//
+//    final var eventPosition =
+//        replayContinuously.writeEvent(
+//            ELEMENT_ACTIVATING,
+//            RECORD,
+//            writer -> writer.sourceRecordPosition(commandPositionBeforeSnapshot));
+//
+//    verify(replayContinuously.getMockStreamProcessorListener(), TIMEOUT)
+//        .onReplayed(-1L, eventPosition);
+//
+//    // then
+//    final var lastProcessedPositionState = replayContinuously.getLastProcessedPositionState();
+//
+//    assertThat(lastProcessedPositionState.getLastSuccessfulProcessedRecordPosition())
+//        .describedAs(
+//            "Expected that the last processed position is not less than the snapshot position")
+//        .isEqualTo(snapshotPosition);
+//  }
+//
+//  private StreamProcessor startStreamProcessor(final StreamProcessorRule streamProcessorRule) {
+//    return streamProcessorRule
+//        .startTypedStreamProcessorNotAwaitOpening(
+//            (processors, context) ->
+//                processors.onCommand(
+//                    ValueType.PROCESS_INSTANCE, ACTIVATE_ELEMENT, typedRecordProcessor));
+//  }
+//
+//  private void startWithPausedStreamProcessor(final StreamProcessorRule streamProcessorRule) {
+//    startStreamProcessor(streamProcessorRule).pauseProcessing().join();
+//  }
+//
+//  private Phase getCurrentPhase(final StreamProcessorRule streamProcessorRule) {
+//    return getStreamProcessor(streamProcessorRule).getCurrentPhase().join();
+//  }
+//
+//  private Long getLastProcessedPosition(final StreamProcessorRule streamProcessorRule) {
+//    return getStreamProcessor(streamProcessorRule).getLastProcessedPositionAsync().join();
+//  }
+//
+//  private Long getLastWrittenPosition(final StreamProcessorRule streamProcessorRule) {
+//    return getStreamProcessor(streamProcessorRule).getLastWrittenPositionAsync().join();
+//  }
+//
+//  private StreamProcessor getStreamProcessor(final StreamProcessorRule streamProcessorRule) {
+//    return streamProcessorRule.getStreamProcessor(PARTITION_ID);
+//  }
+}
---
+++ engine/src/test/java/io/camunda/zeebe/engine/util/StreamPlatform.java
@@ -0,0 +1,401 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.util;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import io.camunda.zeebe.db.ZeebeDb;
+import io.camunda.zeebe.db.ZeebeDbFactory;
+import io.camunda.zeebe.engine.Loggers;
+import io.camunda.zeebe.engine.api.EmptyProcessingResult;
+import io.camunda.zeebe.engine.api.ReadonlyStreamProcessorContext;
+import io.camunda.zeebe.engine.api.RecordProcessor;
+import io.camunda.zeebe.engine.api.StreamProcessorLifecycleAware;
+import io.camunda.zeebe.engine.processing.streamprocessor.StreamProcessorListener;
+import io.camunda.zeebe.engine.processing.streamprocessor.TypedEventRegistry;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.CommandResponseWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.LegacyTypedStreamWriter;
+import io.camunda.zeebe.logstreams.log.LogStreamBatchWriter;
+import io.camunda.zeebe.logstreams.log.LogStreamReader;
+import io.camunda.zeebe.logstreams.log.LoggedEvent;
+import io.camunda.zeebe.logstreams.storage.LogStorage;
+import io.camunda.zeebe.logstreams.util.ListLogStorage;
+import io.camunda.zeebe.logstreams.util.SyncLogStream;
+import io.camunda.zeebe.logstreams.util.SynchronousLogStream;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.scheduler.Actor;
+import io.camunda.zeebe.scheduler.ActorScheduler;
+import io.camunda.zeebe.scheduler.future.ActorFuture;
+import io.camunda.zeebe.streamprocessor.StreamProcessor;
+import io.camunda.zeebe.streamprocessor.StreamProcessor.Phase;
+import io.camunda.zeebe.streamprocessor.StreamProcessorMode;
+import io.camunda.zeebe.util.FileUtil;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import org.slf4j.Logger;
+
+public final class StreamPlatform {
+
+  private static final String SNAPSHOT_FOLDER = "snapshot";
+  private static final Map<Class<?>, ValueType> VALUE_TYPES = new HashMap<>();
+  private static final Logger LOG = Loggers.STREAM_PROCESSING;
+  private static final int DEFAULT_PARTITION = 1;
+  private static final String STREAM_NAME = "stream-";
+
+  static {
+    TypedEventRegistry.EVENT_REGISTRY.forEach((v, c) -> VALUE_TYPES.put(c, v));
+  }
+
+  private final Path dataDirectory;
+  private final List<AutoCloseable> closeables;
+  private final ActorScheduler actorScheduler;
+  private final CommandResponseWriter mockCommandResponseWriter;
+  private final StreamProcessorListener mockStreamProcessorListener;
+  private final Map<String, LogContext> logContextMap = new HashMap<>();
+  private final Map<String, ProcessorContext> streamContextMap = new HashMap<>();
+  private boolean snapshotWasTaken = false;
+  private final StreamProcessorMode streamProcessorMode = StreamProcessorMode.PROCESSING;
+  private RecordProcessor recordProcessor;
+
+  private final WriteActor writeActor = new WriteActor();
+  private final ZeebeDbFactory zeebeDbFactory;
+
+  public StreamPlatform(
+      final Path dataDirectory,
+      final List<AutoCloseable> closeables,
+      final ActorScheduler actorScheduler,
+      final ZeebeDbFactory zeebeDbFactory) {
+    this.dataDirectory = dataDirectory;
+    this.closeables = closeables;
+    this.actorScheduler = actorScheduler;
+    this.zeebeDbFactory = zeebeDbFactory;
+
+    mockCommandResponseWriter = mock(CommandResponseWriter.class);
+    when(mockCommandResponseWriter.intent(any())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.key(anyLong())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.partitionId(anyInt())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.recordType(any())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.rejectionType(any())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.rejectionReason(any())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.valueType(any())).thenReturn(mockCommandResponseWriter);
+    when(mockCommandResponseWriter.valueWriter(any())).thenReturn(mockCommandResponseWriter);
+
+    when(mockCommandResponseWriter.tryWriteResponse(anyInt(), anyLong())).thenReturn(true);
+    mockStreamProcessorListener = mock(StreamProcessorListener.class);
+    actorScheduler.submitActor(writeActor);
+  }
+
+  public CommandResponseWriter getMockedResponseWriter() {
+    return mockCommandResponseWriter;
+  }
+
+  public StreamProcessorListener getMockStreamProcessorListener() {
+    return mockStreamProcessorListener;
+  }
+
+  public SynchronousLogStream createLogStream(final String name, final int partitionId) {
+    final var listLogStorage = new ListLogStorage();
+    return createLogStream(
+        name,
+        partitionId,
+        listLogStorage,
+        logStream -> listLogStorage.setPositionListener(logStream::setLastWrittenPosition));
+  }
+
+  private SynchronousLogStream createLogStream(
+      final String name,
+      final int partitionId,
+      final LogStorage logStorage,
+      final Consumer<SyncLogStream> logStreamConsumer) {
+    final var logStream =
+        SyncLogStream.builder()
+            .withLogName(name)
+            .withLogStorage(logStorage)
+            .withPartitionId(partitionId)
+            .withActorSchedulingService(actorScheduler)
+            .build();
+
+    logStreamConsumer.accept(logStream);
+
+    final LogContext logContext = LogContext.createLogContext(logStream, logStorage);
+    logContextMap.put(name, logContext);
+    closeables.add(logContext);
+    closeables.add(() -> logContextMap.remove(name));
+    return logStream;
+  }
+
+  public long getLastWrittenPosition(final String name) {
+    return getLogStream(name).getLastWrittenPosition();
+  }
+
+  public SynchronousLogStream getLogStream(final String name) {
+    return logContextMap.get(name).getLogStream();
+  }
+
+  public Stream<LoggedEvent> events(final String logName) {
+    final SynchronousLogStream logStream = getLogStream(logName);
+
+    final LogStreamReader reader = logStream.newLogStreamReader();
+    closeables.add(reader);
+
+    reader.seekToFirstEvent();
+
+    final Iterable<LoggedEvent> iterable = () -> reader;
+
+    return StreamSupport.stream(iterable.spliterator(), false);
+  }
+
+  public Path createRuntimeFolder(final SynchronousLogStream stream) {
+    final Path rootDirectory =
+        dataDirectory.resolve(stream.getLogName()).resolve("state");
+
+    try {
+      Files.createDirectories(rootDirectory);
+    } catch (final FileAlreadyExistsException ignored) {
+      // totally fine if it already exists
+    } catch (final IOException e) {
+      throw new UncheckedIOException(e);
+    }
+
+    return rootDirectory.resolve("runtime");
+  }
+
+  public StreamProcessor startStreamProcessor() {
+    final var logName = getLogName(DEFAULT_PARTITION);
+    final SynchronousLogStream stream = getLogStream(logName);
+    return buildStreamProcessor(stream, null, true);
+  }
+
+  public StreamProcessor startStreamProcessorNotWaitForOpening() {
+    final var logName = getLogName(DEFAULT_PARTITION);
+    final SynchronousLogStream stream = getLogStream(logName);
+    return buildStreamProcessor(stream, null, false);
+  }
+
+  public StreamProcessor buildStreamProcessor(
+      final SynchronousLogStream stream,
+      final Function<LogStreamBatchWriter, LegacyTypedStreamWriter> streamWriterFactory,
+      final boolean awaitOpening) {
+    final var storage = createRuntimeFolder(stream);
+    final var snapshot = storage.getParent().resolve(SNAPSHOT_FOLDER);
+
+    final var recoveredLatch = new CountDownLatch(1);
+    final var recoveredAwaiter =
+        new StreamProcessorLifecycleAware() {
+          @Override
+          public void onRecovered(final ReadonlyStreamProcessorContext context) {
+            recoveredLatch.countDown();
+          }
+        };
+
+    final ZeebeDb<?> zeebeDb;
+    if (snapshotWasTaken) {
+      zeebeDb = zeebeDbFactory.createDb(snapshot.toFile());
+    } else {
+      zeebeDb = zeebeDbFactory.createDb(storage.toFile());
+    }
+    final String logName = stream.getLogName();
+    recordProcessor = mock(RecordProcessor.class);
+    when(recordProcessor.process(any(), any())).thenReturn(EmptyProcessingResult.INSTANCE);
+    when(recordProcessor.onProcessingError(any(), any(), any())).thenReturn(EmptyProcessingResult.INSTANCE);
+
+    final var builder =
+        StreamProcessor.builder()
+            .logStream(stream.getAsyncLogStream())
+            .zeebeDb(zeebeDb)
+            .actorSchedulingService(actorScheduler)
+            .commandResponseWriter(mockCommandResponseWriter)
+            .recordProcessor(recordProcessor)
+            .streamProcessorMode(streamProcessorMode);
+
+    builder.getLifecycleListeners().add(recoveredAwaiter);
+
+    if (streamWriterFactory != null) {
+      builder.typedStreamWriterFactory(streamWriterFactory);
+    }
+    final StreamProcessor streamProcessor = builder.build();
+    final var openFuture = streamProcessor.openAsync(false);
+
+    if (awaitOpening) { // and recovery
+      try {
+        recoveredLatch.await(15, TimeUnit.SECONDS);
+      } catch (final InterruptedException e) {
+        Thread.interrupted();
+      }
+    }
+    openFuture.join(15, TimeUnit.SECONDS);
+
+    final LogContext context = logContextMap.get(logName);
+    final ProcessorContext processorContext =
+        ProcessorContext.createStreamContext(context, streamProcessor, zeebeDb, storage, snapshot);
+    streamContextMap.put(logName, processorContext);
+    closeables.add(processorContext);
+
+    return streamProcessor;
+  }
+
+  public void pauseProcessing(final String streamName) {
+    streamContextMap.get(streamName).streamProcessor.pauseProcessing().join();
+    LOG.info("Paused processing for stream {}", streamName);
+  }
+
+  public void resumeProcessing(final String streamName) {
+    streamContextMap.get(streamName).streamProcessor.resumeProcessing();
+    LOG.info("Resume processing for stream {}", streamName);
+  }
+
+  public void snapshot(final String streamName) {
+    streamContextMap.get(streamName).snapshot();
+    snapshotWasTaken = true;
+    LOG.info("Snapshot database for stream {}", streamName);
+  }
+
+  public void closeProcessor(final String streamName) throws Exception {
+    streamContextMap.remove(streamName).close();
+    LOG.info("Closed stream {}", streamName);
+  }
+
+  public RecordProcessor getRecordProcessor() {
+    return recordProcessor;
+  }
+
+  public Phase getCurrentProcessorPhase() {
+    return getStreamProcessor(getLogName(DEFAULT_PARTITION)).getCurrentPhase().join(10, TimeUnit.SECONDS);
+  }
+
+  public StreamProcessor getStreamProcessor() {
+    return getStreamProcessor(getLogName(DEFAULT_PARTITION));
+  }
+
+  public StreamProcessor getStreamProcessor(final String streamName) {
+    return Optional.ofNullable(streamContextMap.get(streamName))
+        .map(c -> c.streamProcessor)
+        .orElseThrow(
+            () -> new NoSuchElementException("No stream processor found with name: " + streamName));
+  }
+
+  public LogStreamBatchWriter setupBatchWriter(
+      final String logName, final RecordToWrite[] recordToWrites) {
+    final SynchronousLogStream logStream = getLogStream(logName);
+    final LogStreamBatchWriter logStreamBatchWriter = logStream.newLogStreamBatchWriter();
+    for (final RecordToWrite recordToWrite : recordToWrites) {
+      logStreamBatchWriter
+          .event()
+          .key(recordToWrite.getKey())
+          .sourceIndex(recordToWrite.getSourceIndex())
+          .metadataWriter(recordToWrite.getRecordMetadata())
+          .valueWriter(recordToWrite.getUnifiedRecordValue())
+          .done();
+    }
+    return logStreamBatchWriter;
+  }
+
+  public static String getLogName(final int partitionId) {
+    return STREAM_NAME + partitionId;
+  }
+
+  public long writeBatch(final RecordToWrite... recordsToWrite) {
+    final var batchWriter = setupBatchWriter(getLogName(DEFAULT_PARTITION), recordsToWrite);
+    return writeActor.submit(batchWriter::tryWrite).join();
+  }
+
+  /** Used to run writes within an actor thread. */
+  private static final class WriteActor extends Actor {
+    public ActorFuture<Long> submit(final Callable<Long> write) {
+      return actor.call(write);
+    }
+  }
+
+  private static final class LogContext implements AutoCloseable {
+    private final SynchronousLogStream logStream;
+
+    private LogContext(final SynchronousLogStream logStream, final LogStorage logStorage) {
+      this.logStream = logStream;
+    }
+
+    public static LogContext createLogContext(
+        final SyncLogStream logStream, final LogStorage logStorage) {
+      return new LogContext(logStream, logStorage);
+    }
+
+    @Override
+    public void close() {
+      logStream.close();
+    }
+
+    public SynchronousLogStream getLogStream() {
+      return logStream;
+    }
+  }
+
+  private static final class ProcessorContext implements AutoCloseable {
+    private final ZeebeDb zeebeDb;
+    private final StreamProcessor streamProcessor;
+    private final Path runtimePath;
+    private final Path snapshotPath;
+    private boolean closed = false;
+
+    private ProcessorContext(
+        final StreamProcessor streamProcessor,
+        final ZeebeDb zeebeDb,
+        final Path runtimePath,
+        final Path snapshotPath) {
+      this.streamProcessor = streamProcessor;
+      this.zeebeDb = zeebeDb;
+      this.runtimePath = runtimePath;
+      this.snapshotPath = snapshotPath;
+    }
+
+    public static ProcessorContext createStreamContext(
+        final LogContext logContext,
+        final StreamProcessor streamProcessor,
+        final ZeebeDb zeebeDb,
+        final Path runtimePath,
+        final Path snapshotPath) {
+      return new ProcessorContext(streamProcessor, zeebeDb, runtimePath, snapshotPath);
+    }
+
+    public void snapshot() {
+      zeebeDb.createSnapshot(snapshotPath.toFile());
+    }
+
+    @Override
+    public void close() throws Exception {
+      if (closed) {
+        return;
+      }
+
+      LOG.debug("Close stream processor");
+      streamProcessor.closeAsync().join();
+      zeebeDb.close();
+      if (runtimePath.toFile().exists()) {
+        FileUtil.deleteFolder(runtimePath);
+      }
+      closed = true;
+    }
+  }
+}
