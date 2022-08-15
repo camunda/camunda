b9beaadc20a01947c6df754ddd677c984dc657ed
Nico Korthout <nico.korthout@camunda.com>
2022-08-15 19:15:29 +0200

    feat(test-util): summarize deployment distribution records
    (cherry picked from commit 855769c2829a266d320274d07687c6e5bbe22e7d)

--- test-util/src/main/java/io/camunda/zeebe/test/util/record/CompactRecordLogger.java
+++ test-util/src/main/java/io/camunda/zeebe/test/util/record/CompactRecordLogger.java
@@ -19,6 +19,7 @@ import io.camunda.zeebe.protocol.record.RecordType;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.IncidentIntent;
 import io.camunda.zeebe.protocol.record.intent.Intent;
+import io.camunda.zeebe.protocol.record.value.DeploymentDistributionRecordValue;
 import io.camunda.zeebe.protocol.record.value.DeploymentRecordValue;
 import io.camunda.zeebe.protocol.record.value.ErrorRecordValue;
 import io.camunda.zeebe.protocol.record.value.IncidentRecordValue;
@@ -89,6 +90,7 @@ public class CompactRecordLogger {
 
   {
     valueLoggers.put(ValueType.DEPLOYMENT, this::summarizeDeployment);
+    valueLoggers.put(ValueType.DEPLOYMENT_DISTRIBUTION, this::summarizeDeploymentDistribution);
     valueLoggers.put(ValueType.PROCESS, this::summarizeProcess);
     valueLoggers.put(ValueType.INCIDENT, this::summarizeIncident);
     valueLoggers.put(ValueType.JOB, this::summarizeJob);
@@ -266,6 +268,11 @@ public class CompactRecordLogger {
     return value.getProcessesMetadata().stream()
         .map(ProcessMetadataValue::getResourceName)
         .collect(Collectors.joining(", "));
+  }
+
+  private String summarizeDeploymentDistribution(final Record<?> record) {
+    final var value = (DeploymentDistributionRecordValue) record.getValue();
+    return "on partition %d".formatted(value.getPartitionId());
   }
 
   private String summarizeProcess(final Record<?> record) {
