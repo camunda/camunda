775a51c656eebe406c6ce857f5f9ed2299fbc2fc
lzgabel <lz19960321lz@gmail.com>
2022-08-22 19:06:36 +0800

    test: avoid timer cycle with start time triggering multiple times

--- engine/src/test/java/io/camunda/zeebe/engine/processing/timer/TimerStartEventTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/timer/TimerStartEventTest.java
@@ -1242,4 +1242,56 @@ public final class TimerStartEventTest {
                 .exists())
         .isTrue();
   }
+
+  @Test
+  public void shouldAvoidTriggeringMultipleTimes() {
+    // given
+    final ZonedDateTime start =
+        ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault()).plusMinutes(30);
+    final BpmnModelInstance model =
+        Bpmn.createExecutableProcess("process")
+            .startEvent("start")
+            .timerWithCycle(String.format("R3/%s/PT10M", start))
+            .endEvent("end")
+            .done();
+    final var deployedProcess =
+        engine
+            .deployment()
+            .withXmlResource(model)
+            .deploy()
+            .getValue()
+            .getProcessesMetadata()
+            .get(0);
+    final long processDefinitionKey = deployedProcess.getProcessDefinitionKey();
+
+    // when
+    engine.stop();
+    final long engineStoppedTime = engine.getClock().getCurrentTimeInMillis();
+    engine.increaseTime(Duration.ofMinutes(35));
+    RecordingExporter.reset();
+    engine.start();
+
+    // then
+    final Record<TimerRecordValue> firstRecord =
+        RecordingExporter.timerRecords(TimerIntent.TRIGGERED)
+            .withProcessDefinitionKey(processDefinitionKey)
+            .getFirst();
+
+    Assertions.assertThat(firstRecord.getValue())
+        .hasDueDate(start.toInstant().toEpochMilli())
+        .hasTargetElementId("start")
+        .hasElementInstanceKey(TimerInstance.NO_ELEMENT_INSTANCE);
+
+    assertThat(firstRecord.getTimestamp()).isGreaterThan(engineStoppedTime);
+
+    final TimerRecordValue secondTimerRecord =
+        RecordingExporter.timerRecords(TimerIntent.CREATED)
+            .withProcessDefinitionKey(processDefinitionKey)
+            .skip(1)
+            .getFirst()
+            .getValue();
+
+    final long now = engine.getClock().getCurrentTimeInMillis();
+    assertThat(secondTimerRecord.getDueDate()).isGreaterThan(now);
+  }
 }
