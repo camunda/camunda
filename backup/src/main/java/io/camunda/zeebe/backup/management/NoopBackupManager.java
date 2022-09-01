b5448e1970f2d98b58fd27b269c9adb2c2f777f1
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-09-01 10:57:05 +0200

    feat(backup): add a noop backup manager that can be configured for follower roles and when no backup store is configured

---
+++ backup/src/main/java/io/camunda/zeebe/backup/management/NoopBackupManager.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.backup.management;
+
+import io.camunda.zeebe.backup.api.BackupManager;
+import io.camunda.zeebe.backup.api.BackupStatus;
+import io.camunda.zeebe.backup.processing.state.CheckpointState;
+import io.camunda.zeebe.scheduler.future.ActorFuture;
+import io.camunda.zeebe.scheduler.future.CompletableActorFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoopBackupManager implements BackupManager {
+
+  private static final Logger LOG = LoggerFactory.getLogger(NoopBackupManager.class);
+  private final String errorMessage;
+
+  public NoopBackupManager(final String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+  @Override
+  public void takeBackup(final long checkpointId, final long checkpointPosition) {
+    LOG.warn("Attempted to take backup, but cannot take backup. {}", errorMessage);
+  }
+
+  @Override
+  public ActorFuture<BackupStatus> getBackupStatus(final long checkpointId) {
+    return CompletableActorFuture.completedExceptionally(
+        new UnsupportedOperationException(errorMessage));
+  }
+
+  @Override
+  public ActorFuture<Void> deleteBackup(final long checkpointId) {
+    return CompletableActorFuture.completedExceptionally(
+        new UnsupportedOperationException(errorMessage));
+  }
+
+  @Override
+  public ActorFuture<Void> closeAsync() {
+    return CompletableActorFuture.completed(null);
+  }
+
+  @Override
+  public void failInProgressBackup(final long lastCheckpointId) {
+    if (lastCheckpointId == CheckpointState.NO_CHECKPOINT) {
+      return;
+    }
+    LOG.warn("Attempted to update in progress backup, but cannot do it. {}", errorMessage);
+  }
+}
