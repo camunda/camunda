3ffecaec352777a1f83ddf33897f486cc7519ebb
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-09-01 10:54:54 +0200

    build(.ci): use GH CLI to do releases

--- .ci/scripts/release/post-release-go.sh
+++ .ci/scripts/release/post-release-go.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd "clients/go/internal/embedded"
+# Publish Go tag for the release
+git tag "clients/go/v${RELEASE_VERSION}"
+git push origin "clients/go/v${RELEASE_VERSION}"
+
+# Prepare Go version for the next release
+pushd "clients/go/internal/embedded" || exit $?
 
 echo "${DEVELOPMENT_VERSION}" > data/VERSION
 go-bindata -pkg embedded -o embedded.go -prefix data/ data/
