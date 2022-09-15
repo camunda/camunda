52e8bfee0ff71ce54aebeb07830073dd4ea93ae8
Nico Korthout <nico.korthout@camunda.com>
2022-09-15 12:48:54 +0200

    test(it): accept modify command
    This tests that a process instance can be modified from the java client,
    using multiple activation instructions with scoped variables and a
    terminate instruction. This test only needs to test the communication
    between the client and the engine. It does not need to verify that the
    engine actually modified the instance. Checking that it wasn't rejected
    is enough as there are other tests that show case rejections.

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/ModifyProcessInstanceTest.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/ModifyProcessInstanceTest.java
@@ -7,13 +7,17 @@
  */
 package io.camunda.zeebe.it.client.command;
 
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.camunda.zeebe.broker.test.EmbeddedBrokerRule;
 import io.camunda.zeebe.client.api.command.ClientStatusException;
 import io.camunda.zeebe.it.util.GrpcClientRule;
 import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.test.util.BrokerClassRuleHelper;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -30,14 +34,65 @@ public class ModifyProcessInstanceTest {
 
   @Rule public final BrokerClassRuleHelper helper = new BrokerClassRuleHelper();
   private String processId;
+  private String processId2;
   private long processDefinitionKey;
 
   @Before
   public void deploy() {
     processId = helper.getBpmnProcessId();
+    processId2 = helper.getBpmnProcessId();
     processDefinitionKey =
         CLIENT_RULE.deployProcess(
             Bpmn.createExecutableProcess(processId).startEvent().endEvent().done());
+    CLIENT_RULE.deployProcess(
+        Bpmn.createExecutableProcess(processId2)
+            .startEvent()
+            .userTask("A")
+            .parallelGateway()
+            .userTask("B")
+            .moveToLastGateway()
+            .userTask("C")
+            .endEvent()
+            .done());
+  }
+
+  @Test
+  public void shouldModifyExistingProcessInstance() {
+    // given
+    final var processInstance =
+        CLIENT_RULE
+            .getClient()
+            .newCreateInstanceCommand()
+            .bpmnProcessId(processId2)
+            .latestVersion()
+            .send()
+            .join();
+    final var processInstanceKey = processInstance.getProcessInstanceKey();
+
+    final var activatedUserTask =
+        RecordingExporter.processInstanceRecords()
+            .withProcessInstanceKey(processInstanceKey)
+            .limit("A", ProcessInstanceIntent.ELEMENT_ACTIVATED)
+            .getFirst();
+
+    // when
+    final var command =
+        CLIENT_RULE
+            .getClient()
+            .newModifyProcessInstanceCommand(processInstanceKey)
+            .activateElement("B")
+            .withVariables(Map.of("foo", "bar"), "B")
+            .and()
+            .activateElement("C")
+            .withVariables(Map.of("fizz", "buzz"), "C")
+            .and()
+            .terminateElement(activatedUserTask.getKey())
+            .send();
+
+    // then
+    assertThatNoException()
+        .describedAs("Expect that modification command is not rejected")
+        .isThrownBy(command::join);
   }
 
   @Test
