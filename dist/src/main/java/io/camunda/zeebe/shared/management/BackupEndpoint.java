80956a60b23ae6ccb32d3b4ddcf22a6fb81f5620
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-09-19 14:15:10 +0200

    feat(dist): add backup actuator
    Adds a new backup management endpoint that can be enabled as a normal
    actuator. This can be enabled on all nodes, not just gateway nodes, as
    any node can now have a `BrokerClient`.

---
+++ dist/src/main/java/io/camunda/zeebe/shared/management/BackupEndpoint.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.shared.management;
+
+import io.camunda.zeebe.gateway.admin.backup.BackupApi;
+import io.camunda.zeebe.gateway.admin.backup.BackupRequestHandler;
+import io.camunda.zeebe.gateway.impl.broker.BrokerClient;
+import io.camunda.zeebe.util.VisibleForTesting;
+import java.util.concurrent.CompletionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebEndpoint(id = "backups", enableByDefault = false)
+@ConditionalOnProperty(prefix = "zeebe.broker.experimental.features", name = "enableBackup")
+final class BackupEndpoint {
+  private final BackupApi api;
+
+  @SuppressWarnings("unused") // used by Spring
+  @Autowired
+  public BackupEndpoint(final BrokerClient client) {
+    this(new BackupRequestHandler(client));
+  }
+
+  BackupEndpoint(final BackupApi api) {
+    this.api = api;
+  }
+
+  @WriteOperation
+  public WebEndpointResponse<?> take(@Selector @NonNull final long id) {
+    try {
+      final long backupId = api.takeBackup(id).toCompletableFuture().join();
+      return new WebEndpointResponse<>(new TakeBackupResponse(backupId));
+    } catch (final CompletionException e) {
+      return new WebEndpointResponse<>(
+          new TakeBackupError(id, e.getCause().getMessage()),
+          WebEndpointResponse.STATUS_INTERNAL_SERVER_ERROR);
+    } catch (final Exception e) {
+      return new WebEndpointResponse<>(
+          new TakeBackupError(id, e.getMessage()),
+          WebEndpointResponse.STATUS_INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @VisibleForTesting
+  record TakeBackupResponse(long id) {}
+
+  @VisibleForTesting
+  record TakeBackupError(long id, String failure) {}
+}
