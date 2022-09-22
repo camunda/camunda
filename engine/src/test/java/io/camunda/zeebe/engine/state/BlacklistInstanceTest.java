dea0de09a0a5ed2ac815f119616bc9089be7d0e3
Christopher Zell <zelldon91@googlemail.com>
2022-09-22 16:12:33 +0200

    refactor: move BlacklistInstanceTest
    this test is more related to the state

--- engine/src/test/java/io/camunda/zeebe/engine/state/BlacklistInstanceTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/state/BlacklistInstanceTest.java
@@ -5,7 +5,7 @@
  * Licensed under the Zeebe Community License 1.1. You may not use this file
  * except in compliance with the Zeebe Community License 1.1.
  */
-package io.camunda.zeebe.engine.processing.streamprocessor;
+package io.camunda.zeebe.engine.state;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
