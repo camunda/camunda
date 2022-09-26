aad4a69367bd694384d067ff55c2210a1e272f7c
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-09-26 13:27:35 +0200

    test(raft): verify snapshot is replicated after restart
    (cherry picked from commit e310b8b456b5bf2fdfff6dce5fc02ba5a7947b40)

--- atomix/cluster/src/test/java/io/atomix/raft/RaftStartupConsistencyCheckTest.java
+++ atomix/cluster/src/test/java/io/atomix/raft/RaftStartupConsistencyCheckTest.java
@@ -7,10 +7,12 @@
  */
 package io.atomix.raft;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import io.atomix.raft.snapshot.TestSnapshotStore;
 import java.util.concurrent.CompletableFuture;
+import org.awaitility.Awaitility;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -27,7 +29,7 @@ public class RaftStartupConsistencyCheckTest {
     final var commitIndex = raftRule.appendEntries(10);
     final var leader = raftRule.getLeader().orElseThrow();
 
-    // lower the threshold so that the follower recieves snapshot instead of log events
+    // lower the threshold so that the follower receives snapshot instead of log events
     leader.getContext().setPreferSnapshotReplicationThreshold(1);
     final long snapshotIndex = commitIndex - 1;
     raftRule.doSnapshotOnMember(leader, snapshotIndex, 1);
@@ -35,7 +37,7 @@ public class RaftStartupConsistencyCheckTest {
     final TestSnapshotStore testSnapshotStore =
         (TestSnapshotStore) followerToRestart.getContext().getPersistedSnapshotStore();
     final CompletableFuture<Void> followerCrashed = new CompletableFuture<>();
-    // Throw exception before committing the snapshot and trigger shutdown to simulat "crash"
+    // Throw exception before committing the snapshot and trigger shutdown to simulate "crash"
     testSnapshotStore.interceptOnNewSnapshot(
         () -> {
           try {
@@ -57,11 +59,18 @@ public class RaftStartupConsistencyCheckTest {
 
     // then
     // restart the follower
+    final String followerId = followerToRestart.cluster().getLocalMember().memberId().id();
     assertThatNoException()
         .describedAs("Consistency check on startup does not fail.")
         .isThrownBy(
             () ->
                 // recreate the server and rejoin the cluster
-                raftRule.joinCluster(followerToRestart.cluster().getLocalMember().memberId().id()));
+                raftRule.joinCluster(followerId));
+
+    Awaitility.await("Restarted follower has received new snapshot")
+        .untilAsserted(
+            () ->
+                assertThat(raftRule.getPersistedSnapshotStore(followerId).getCurrentSnapshotIndex())
+                    .isEqualTo(snapshotIndex));
   }
 }
