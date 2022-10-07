2371cb5add65fe77b63f06a1ec40b9f9cd1d3c1f
Remco Westerhoud <remco@westerhoud.nl>
2022-10-07 11:52:09 +0200

    feat(engine): create ProcessInstanceModifiedEventApplier
    This event applier will be responsible for identifying if any element instances have been activated withing an interrupted flow scope. If this is the case the interrupted state needs to be cleared from this scope.

---
+++ engine/src/main/java/io/camunda/zeebe/engine/state/appliers/ProcessInstanceModifiedEventApplier.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.state.appliers;
+
+import io.camunda.zeebe.engine.state.TypedEventApplier;
+import io.camunda.zeebe.engine.state.mutable.MutableElementInstanceState;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceModificationRecord;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceModificationIntent;
+
+final class ProcessInstanceModifiedEventApplier
+    implements TypedEventApplier<
+        ProcessInstanceModificationIntent, ProcessInstanceModificationRecord> {
+
+  private final MutableElementInstanceState elementInstanceState;
+
+  public ProcessInstanceModifiedEventApplier(
+      final MutableElementInstanceState elementInstanceState) {
+    this.elementInstanceState = elementInstanceState;
+  }
+
+  @Override
+  public void applyState(final long key, final ProcessInstanceModificationRecord value) {}
+}
