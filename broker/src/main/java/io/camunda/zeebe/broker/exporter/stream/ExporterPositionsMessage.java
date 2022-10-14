ranodeepbanerjee/zeebe

poc: inject sequence value in exported event

broker/src/main/java/io/camunda/zeebe/broker/exporter/stream/ExporterPositionsMessage.java
@@ -9,6 +9,7 @@ package io.camunda.zeebe.broker.exporter.stream;
 
 import io.camunda.zeebe.clustering.management.ExporterPositionsDecoder;
 import io.camunda.zeebe.clustering.management.ExporterPositionsEncoder;
+import io.camunda.zeebe.clustering.management.ExporterPositionsEncoder.BucketsEncoder;
 import io.camunda.zeebe.clustering.management.ExporterPositionsEncoder.PositionsEncoder;
 import io.camunda.zeebe.protocol.impl.encoding.SbeBufferWriterReader;
 import io.camunda.zeebe.util.buffer.BufferUtil;
@@ -22,6 +23,7 @@ public class ExporterPositionsMessage
     extends SbeBufferWriterReader<ExporterPositionsEncoder, ExporterPositionsDecoder> {
 
   private final Map<String, Long> exporterPositions = new HashMap<>();
+  private final Map<String, Map<String, Long>> exporterSequences = new HashMap<>();
   private final ExporterPositionsEncoder encoder = new ExporterPositionsEncoder();
   private final ExporterPositionsDecoder decoder = new ExporterPositionsDecoder();
 
@@ -39,6 +41,7 @@ public class ExporterPositionsMessage
   public void reset() {
     super.reset();
     exporterPositions.clear();
+    exporterSequences.clear();
   }
 
   @Override
@@ -52,7 +55,23 @@ public class ExporterPositionsMessage
                     + PositionsEncoder.exporterIdHeaderLength()
                     + id.length()));
 
-    return super.getLength() + PositionsEncoder.sbeHeaderSize() + length.get();
+    exporterSequences.forEach(
+        (id, seqs) -> {
+          seqs.forEach(
+              (v, s) -> {
+                length.addAndGet(
+                    BucketsEncoder.sequenceEncodingLength()
+                        + BucketsEncoder.valueTypeHeaderLength()
+                        + v.length()
+                        + BucketsEncoder.exporterIdHeaderLength()
+                        + id.length());
+              });
+        });
+
+    return super.getLength()
+        + PositionsEncoder.sbeHeaderSize()
+        + BucketsEncoder.sbeHeaderSize()
+        + length.get();
   }
 
   @Override
@@ -65,6 +84,27 @@ public class ExporterPositionsMessage
         (id, pos) -> {
           final var idBuffer = BufferUtil.wrapString(id);
           positionsEncoder.next().position(pos).putExporterId(idBuffer, 0, idBuffer.capacity());
+        });
+
+    final var size = new MutableInteger();
+    exporterSequences.forEach(
+        (k, seqs) -> {
+          size.addAndGet(seqs.size());
+        });
+
+    final var bucketsEncoder = encoder.bucketsCount(size.get());
+    exporterSequences.forEach(
+        (e, seqs) -> {
+          final var exporterIdBuffer = BufferUtil.wrapString(e);
+          seqs.forEach(
+              (v, s) -> {
+                final var valueTypeBuffer = BufferUtil.wrapString(v);
+                bucketsEncoder
+                    .next()
+                    .sequence(s)
+                    .putValueType(valueTypeBuffer, 0, valueTypeBuffer.capacity())
+                    .putExporterId(exporterIdBuffer, 0, exporterIdBuffer.capacity());
+              });
         });
   }
 
@@ -83,13 +123,44 @@ public class ExporterPositionsMessage
       next.getExporterId(bytes, 0, exporterIdLength);
       exporterPositions.put(new String(bytes), position);
     }
+
+    final var bucketsDecoder = decoder.buckets();
+
+    while (bucketsDecoder.hasNext()) {
+      final var next = bucketsDecoder.next();
+      final var sequence = next.sequence();
+
+      final var valueTypeLength = next.valueTypeLength();
+      final var valueTypeBytes = new byte[valueTypeLength];
+      next.getValueType(valueTypeBytes, 0, valueTypeLength);
+      final var valueType = new String(valueTypeBytes);
+
+      final var exporterIdLength = next.exporterIdLength();
+      final var exporterIdBytes = new byte[exporterIdLength];
+      next.getExporterId(exporterIdBytes, 0, exporterIdLength);
+      final var exporterId = new String(exporterIdBytes);
+
+      final var seqs =
+          exporterSequences.computeIfAbsent(exporterId, (k) -> new HashMap<String, Long>());
+      seqs.put(valueType, sequence);
+    }
   }
 
   public void putExporter(final String exporterId, final long lastExportedPosition) {
     exporterPositions.put(exporterId, lastExportedPosition);
   }
 
+  public void putSequence(final String exporterId, final String valueType, final long sequence) {
+    final var sequenceMap =
+        exporterSequences.computeIfAbsent(exporterId, (k) -> new HashMap<String, Long>());
+    sequenceMap.put(valueType, sequence);
+  }
+
   public Map<String, Long> getExporterPositions() {
     return exporterPositions;
+  }
+
+  public Map<String, Map<String, Long>> getExporterSequences() {
+    return exporterSequences;
   }
 }

exporters/elasticsearch-exporter/src/test/java/io/camunda/zeebe/exporter/ElasticsearchClientIT.java
@@ -52,13 +52,13 @@ public class ElasticsearchClientIT extends AbstractElasticsearchExporterIntegrat
             i -> {
               when(recordMock.getKey()).thenReturn(RECORD_KEY + i);
               when(recordMock.toJson()).thenReturn("invalid-json-" + i);
-              client.index(recordMock);
+              client.index(recordMock, 0);
             });
 
     // and one valid record
     when(recordMock.getKey()).thenReturn(RECORD_KEY + bulkSize);
     when(recordMock.toJson()).thenReturn("{}");
-    client.index(recordMock);
+    client.index(recordMock, 0);
 
     // when/then
     assertThatThrownBy(client::flush)
@@ -76,11 +76,11 @@ public class ElasticsearchClientIT extends AbstractElasticsearchExporterIntegrat
     when(recordMock.getKey()).thenReturn(RECORD_KEY + 1);
     when(recordMock.toJson()).thenReturn("{}");
 
-    client.index(recordMock);
+    client.index(recordMock, 0);
     assertThat(bulkRequest).hasSize(1);
 
     // when
-    client.index(recordMock);
+    client.index(recordMock, 0);
 
     // then
     assertThat(bulkRequest).hasSize(1);
@@ -112,14 +112,14 @@ public class ElasticsearchClientIT extends AbstractElasticsearchExporterIntegrat
     when(recordMock.toJson()).thenReturn(jsonRecord.apply(variableValue1));
 
     // when
-    client.index(recordMock);
+    client.index(recordMock, 0);
 
     assertThat(client.shouldFlush()).isFalse();
 
     when(recordMock.getKey()).thenReturn(2L);
     when(recordMock.toJson()).thenReturn(jsonRecord.apply(variableValue2));
 
-    client.index(recordMock);
+    client.index(recordMock, 0);
 
     // then
     assertThat(client.shouldFlush()).isTrue();

camunda/camunda

broker/src/main/java/io/camunda/zeebe/broker/exporter/stream/ExporterPositionsMessage.java
@@ -9,6 +9,7 @@ package io.camunda.zeebe.broker.exporter.stream;
 
 import io.camunda.zeebe.broker.protocol.ExporterPositionsDecoder;
 import io.camunda.zeebe.broker.protocol.ExporterPositionsEncoder;
+import io.camunda.zeebe.broker.protocol.ExporterPositionsEncoder.BucketsEncoder;
 import io.camunda.zeebe.broker.protocol.ExporterPositionsEncoder.PositionsEncoder;
 import io.camunda.zeebe.protocol.impl.encoding.SbeBufferWriterReader;
 import io.camunda.zeebe.util.buffer.BufferUtil;
@@ -22,6 +23,7 @@ public class ExporterPositionsMessage
     extends SbeBufferWriterReader<ExporterPositionsEncoder, ExporterPositionsDecoder> {
 
   private final Map<String, Long> exporterPositions = new HashMap<>();
+  private final Map<String, Map<String, Long>> exporterSequences = new HashMap<>();
   private final ExporterPositionsEncoder encoder = new ExporterPositionsEncoder();
   private final ExporterPositionsDecoder decoder = new ExporterPositionsDecoder();
 
@@ -39,6 +41,7 @@ public class ExporterPositionsMessage
   public void reset() {
     super.reset();
     exporterPositions.clear();
+    exporterSequences.clear();
   }
 
   @Override
@@ -52,7 +55,23 @@ public class ExporterPositionsMessage
                     + PositionsEncoder.exporterIdHeaderLength()
                     + id.length()));
 
-    return super.getLength() + PositionsEncoder.sbeHeaderSize() + length.get();
+    exporterSequences.forEach(
+        (id, seqs) -> {
+          seqs.forEach(
+              (v, s) -> {
+                length.addAndGet(
+                    BucketsEncoder.sequenceEncodingLength()
+                        + BucketsEncoder.valueTypeHeaderLength()
+                        + v.length()
+                        + BucketsEncoder.exporterIdHeaderLength()
+                        + id.length());
+              });
+        });
+
+    return super.getLength()
+        + PositionsEncoder.sbeHeaderSize()
+        + BucketsEncoder.sbeHeaderSize()
+        + length.get();
   }
 
   @Override
@@ -65,6 +84,27 @@ public class ExporterPositionsMessage
         (id, pos) -> {
           final var idBuffer = BufferUtil.wrapString(id);
           positionsEncoder.next().position(pos).putExporterId(idBuffer, 0, idBuffer.capacity());
+        });
+
+    final var size = new MutableInteger();
+    exporterSequences.forEach(
+        (k, seqs) -> {
+          size.addAndGet(seqs.size());
+        });
+
+    final var bucketsEncoder = encoder.bucketsCount(size.get());
+    exporterSequences.forEach(
+        (e, seqs) -> {
+          final var exporterIdBuffer = BufferUtil.wrapString(e);
+          seqs.forEach(
+              (v, s) -> {
+                final var valueTypeBuffer = BufferUtil.wrapString(v);
+                bucketsEncoder
+                    .next()
+                    .sequence(s)
+                    .putValueType(valueTypeBuffer, 0, valueTypeBuffer.capacity())
+                    .putExporterId(exporterIdBuffer, 0, exporterIdBuffer.capacity());
+              });
         });
   }
 
@@ -83,13 +123,44 @@ public class ExporterPositionsMessage
       next.getExporterId(bytes, 0, exporterIdLength);
       exporterPositions.put(new String(bytes), position);
     }
+
+    final var bucketsDecoder = decoder.buckets();
+
+    while (bucketsDecoder.hasNext()) {
+      final var next = bucketsDecoder.next();
+      final var sequence = next.sequence();
+
+      final var valueTypeLength = next.valueTypeLength();
+      final var valueTypeBytes = new byte[valueTypeLength];
+      next.getValueType(valueTypeBytes, 0, valueTypeLength);
+      final var valueType = new String(valueTypeBytes);
+
+      final var exporterIdLength = next.exporterIdLength();
+      final var exporterIdBytes = new byte[exporterIdLength];
+      next.getExporterId(exporterIdBytes, 0, exporterIdLength);
+      final var exporterId = new String(exporterIdBytes);
+
+      final var seqs =
+          exporterSequences.computeIfAbsent(exporterId, (k) -> new HashMap<String, Long>());
+      seqs.put(valueType, sequence);
+    }
   }
 
   public void putExporter(final String exporterId, final long lastExportedPosition) {
     exporterPositions.put(exporterId, lastExportedPosition);
   }
 
+  public void putSequence(final String exporterId, final String valueType, final long sequence) {
+    final var sequenceMap =
+        exporterSequences.computeIfAbsent(exporterId, (k) -> new HashMap<String, Long>());
+    sequenceMap.put(valueType, sequence);
+  }
+
   public Map<String, Long> getExporterPositions() {
     return exporterPositions;
+  }
+
+  public Map<String, Map<String, Long>> getExporterSequences() {
+    return exporterSequences;
   }
 }

exporters/elasticsearch-exporter/src/test/java/io/camunda/zeebe/exporter/ElasticsearchClientIT.java
@@ -79,7 +79,7 @@ final class ElasticsearchClientIT {
     // date, which must be a positive number of milliseconds since the UNIX epoch
     final var invalidRecord =
         recordFactory.generateRecord(ValueType.VARIABLE, b -> b.withTimestamp(Long.MIN_VALUE));
-    client.index(invalidRecord);
+    client.index(invalidRecord, 0);
     client.putComponentTemplate();
     client.putIndexTemplate(ValueType.VARIABLE);
 