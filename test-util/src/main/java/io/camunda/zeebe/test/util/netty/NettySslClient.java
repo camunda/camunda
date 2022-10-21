e87557cc80db24fba08a130fd89fd2bc455d74b7
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2022-10-21 15:16:44 +0200

    refactor(test-util): retry prematurely closed channel
    In a few instances, the Netty channel may be closed prematurely from the
    client side - for example, if a read ends up reading less data than
    expected, the channel will be silently closed by Netty. In most cases,
    it's perfectly safe to retry, as if the server was unavailable we would
    get a connection refused error instead. Thus we add a basic retry
    mechanism, up to 3 times, in case the channel is closed implicitly
    before the SSL handshake could take place.

--- test-util/src/main/java/io/camunda/zeebe/test/util/netty/NettySslClient.java
+++ test-util/src/main/java/io/camunda/zeebe/test/util/netty/NettySslClient.java
@@ -7,13 +7,18 @@
  */
 package io.camunda.zeebe.test.util.netty;
 
+import io.camunda.zeebe.util.exception.RecoverableException;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslHandler;
@@ -21,11 +26,16 @@ import io.netty.handler.ssl.util.SelfSignedCertificate;
 import io.netty.util.concurrent.Future;
 import java.io.UncheckedIOException;
 import java.net.SocketAddress;
+import java.nio.channels.ClosedChannelException;
 import java.security.cert.Certificate;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLPeerUnverifiedException;
+import org.agrona.LangUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A simple Netty based client which only connects to SSL/TLS secured connections and extracts their
@@ -40,6 +50,9 @@ import javax.net.ssl.SSLPeerUnverifiedException;
  * reuses this class to perform its assertions.
  */
 public final class NettySslClient {
+  private static final int MAX_ATTEMPT_COUNT = 3;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(NettySslClient.class);
   private final SslContext sslContext;
 
   public NettySslClient(final SslContext sslContext) {
@@ -72,30 +85,66 @@ public final class NettySslClient {
    * @return the certificate chain used by the remote server
    */
   public Certificate[] getRemoteCertificateChain(final SocketAddress address) {
+    return getRemoteCertificateChain(address, 1);
+  }
+
+  /**
+   * It can happen that the channel is closed too early due to network hiccups, in which case we
+   * want to retry a few times (assuming the channel was closed implicitly from the client side). If
+   * it fails 3 times, it's very likely to be a real error.
+   */
+  private Certificate[] getRemoteCertificateChain(
+      final SocketAddress address, final int attemptCount) {
+    if (attemptCount > MAX_ATTEMPT_COUNT) {
+      throw new IllegalStateException(
+          "Failed to obtain remote certificate chain after retrying 3 times; see logs for more");
+    }
+
     final var certificatesFuture = new CompletableFuture<Certificate[]>();
     final var executor = new NioEventLoopGroup(1);
 
     try {
-      final var channel =
+      final var connectFuture =
           new Bootstrap()
               .handler(new SslCertificateExtractor(certificatesFuture))
               .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5_000)
+              // prevent implicitly the channel on write errors so we can deal with errors in an
+              // explicit fashion
+              .option(ChannelOption.AUTO_CLOSE, false)
               .group(executor)
               .channel(NioSocketChannel.class)
               .connect(address)
-              .addListener(onConnect -> onChannelConnect(address, certificatesFuture, onConnect))
-              .channel();
+              .addListener(onConnect -> onChannelConnect(address, certificatesFuture, onConnect));
+
+      final var channel = connectFuture.channel();
       channel
           .closeFuture()
           .addListener(onClose -> onChannelClose(address, certificatesFuture, onClose));
 
-      final Certificate[] certificateChain =
-          certificatesFuture.orTimeout(10, TimeUnit.SECONDS).join();
-      channel.close();
+      try {
+        return certificatesFuture.orTimeout(10, TimeUnit.SECONDS).join();
+      } catch (final CompletionException e) {
+        if (e.getCause() instanceof PrematurelyClosedChannelException) {
+          LOGGER.debug(
+              "Retrying prematurely closed connection to {}, attempt #{}", address, attemptCount);
+          return getRemoteCertificateChain(address, attemptCount + 1);
+        }
 
-      return certificateChain;
+        // rethrow otherwise
+        throw e;
+      }
     } finally {
-      executor.shutdownGracefully(10, 100, TimeUnit.MILLISECONDS);
+      closeExecutor(executor);
+    }
+  }
+
+  private static void closeExecutor(final EventLoopGroup executor) {
+    try {
+      // will close any opened channel
+      executor.shutdownGracefully(10, 100, TimeUnit.MILLISECONDS).sync();
+    } catch (final InterruptedException e) {
+      Thread.currentThread().interrupt();
+      LangUtil.rethrowUnchecked(e);
     }
   }
 
@@ -103,34 +152,47 @@ public final class NettySslClient {
       final SocketAddress address,
       final CompletableFuture<Certificate[]> certificates,
       final Future<? super Void> onConnect) {
-    if (!onConnect.isSuccess()) {
-      final var errorMessage =
-          String.format("Failed to establish a secure connection to %s", address);
-      certificates.completeExceptionally(getErrorWithOptionalCause(onConnect, errorMessage));
+    if (onConnect.isSuccess()) {
+      return;
     }
+
+    final var errorMessage =
+        String.format("Failed to establish a secure connection to %s", address);
+    certificates.completeExceptionally(getErrorWithOptionalCause(onConnect, errorMessage));
   }
 
   private void onChannelClose(
       final SocketAddress address,
       final CompletableFuture<Certificate[]> certificates,
       final Future<? super Void> onClose) {
-    final var errorMessage =
-        String.format(
-            "Channel to remote peer %s was unexpectedly closed before any certificates were "
-                + "extracted",
-            address);
-    certificates.completeExceptionally(getErrorWithOptionalCause(onClose, errorMessage));
+    if (onClose.cause() == null || (onClose.cause() instanceof ClosedChannelException)) {
+      certificates.completeExceptionally(new PrematurelyClosedChannelException(onClose.cause()));
+    } else {
+      final var errorMessage =
+          String.format(
+              "Channel to remote peer %s was unexpectedly closed before any certificates were "
+                  + "extracted",
+              address);
+      certificates.completeExceptionally(getErrorWithOptionalCause(onClose, errorMessage));
+    }
   }
 
   private Throwable getErrorWithOptionalCause(
-      final Future<? super Void> onClose, final String errorMessage) {
+      final Future<?> operation, final String errorMessage) {
     final Throwable error;
-    if (onClose.cause() != null) {
-      error = new IllegalStateException(errorMessage, onClose.cause());
+    if (operation.cause() != null) {
+      error = new IllegalStateException(errorMessage, operation.cause());
     } else {
       error = new IllegalStateException(errorMessage);
     }
     return error;
+  }
+
+  private static final class PrematurelyClosedChannelException extends RecoverableException {
+
+    public PrematurelyClosedChannelException(final Throwable cause) {
+      super(cause);
+    }
   }
 
   private final class SslCertificateExtractor extends ChannelInitializer<SocketChannel> {
@@ -143,10 +205,39 @@ public final class NettySslClient {
     @Override
     protected void initChannel(final SocketChannel channel) {
       final var sslHandler = sslContext.newHandler(channel.alloc());
-      channel.pipeline().addLast("tls", sslHandler);
       sslHandler
           .handshakeFuture()
           .addListener(onHandshake -> extractCertificate(sslHandler, onHandshake));
+      sslHandler.sslCloseFuture().addListener(this::onSslClose);
+      sslHandler.setHandshakeTimeoutMillis(5_000);
+
+      channel.pipeline().addLast("tls", sslHandler);
+      channel
+          .pipeline()
+          .addLast(
+              new LoggingHandler(LogLevel.TRACE) {
+                @Override
+                public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause)
+                    throws Exception {
+                  extractedCertificate.completeExceptionally(cause);
+                  super.exceptionCaught(ctx, cause);
+                }
+              });
+    }
+
+    @Override
+    public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause)
+        throws Exception {
+      extractedCertificate.completeExceptionally(cause);
+      super.exceptionCaught(ctx, cause);
+    }
+
+    private void onSslClose(final Future<? super Channel> onSslClose) {
+      if (!onSslClose.isSuccess()) {
+        extractedCertificate.completeExceptionally(
+            getErrorWithOptionalCause(
+                onSslClose, "SSL engine closed before certificates were extracted"));
+      }
     }
 
     private void extractCertificate(
@@ -154,6 +245,9 @@ public final class NettySslClient {
         throws SSLPeerUnverifiedException {
       if (onHandshake.isSuccess()) {
         extractedCertificate.complete(sslHandler.engine().getSession().getPeerCertificates());
+      } else {
+        extractedCertificate.completeExceptionally(
+            getErrorWithOptionalCause(onHandshake, "Failed to perform SSL handshake"));
       }
     }
   }
