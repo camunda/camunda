14f70d3582ca9867dbb48c5a9b806bd504394964
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-11-08 13:33:07 +0100

    refactor(ci): inline e2e test script

--- .ci/scripts/distribution/e2e-testbench.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-chmod +x clients/go/cmd/zbctl/dist/zbctl
-
-zbctl="clients/go/cmd/zbctl/dist/zbctl"
-
-"${zbctl}" create instance e2e_testbench_protocol --variables "{\"zeebeImage\":\"$ZEEBE_IMAGE\", \"generationTemplate\":\"$GENERATION_TEMPLATE\", \"channel\":\"Internal Dev\", \"clusterPlan\":\"Production - M\", \"region\":\"new chaos\", \"properties\":[\"allInstancesAreCompleted\"], \"testProcessId\": \"e2e-test\", \"branch\": \"$BRANCH_NAME\", \"build\": \"$BUILD_URL\", \"testParams\": { \"maxTestDuration\": \"$MAX_TEST_DURATION\", \"starter\": [ {\"rate\": 50, \"processId\": \"one-task-one-timer\" } ], \"verifier\" : { \"maxInstanceDuration\" : \"15m\" } } }"
