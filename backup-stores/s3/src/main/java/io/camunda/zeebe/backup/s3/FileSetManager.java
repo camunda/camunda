9ef87022c47472e6526492e3b51e5bed0f1851c9
Ole Schönburg <ole.schoenburg@gmail.com>
2022-11-16 11:17:18 +0100

    feat(backups/s3): opportunistically compress files before uploading
    If backup compression is enabled and the fileset manager has to upload a
    file that is larger than a constant threshold (currently 8MiB, chosen
    arbitrarily), it first writes zstd compressed content to a temporary
    file and uploads that file instead. The file metadata is set so that
    the fileset manager knows what compression algorithm (if any) to use
    when restoring.

--- backup-stores/s3/src/main/java/io/camunda/zeebe/backup/s3/FileSetManager.java
+++ backup-stores/s3/src/main/java/io/camunda/zeebe/backup/s3/FileSetManager.java
@@ -9,11 +9,19 @@ package io.camunda.zeebe.backup.s3;
 
 import io.camunda.zeebe.backup.api.NamedFileSet;
 import io.camunda.zeebe.backup.common.NamedFileSetImpl;
+import io.camunda.zeebe.backup.s3.S3BackupStoreException.BackupCompressionFailed;
 import io.camunda.zeebe.backup.s3.manifest.FileSet;
 import io.camunda.zeebe.backup.s3.util.CompletableFutureUtils;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map.Entry;
 import java.util.concurrent.CompletableFuture;
+import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
@@ -23,6 +31,10 @@ import software.amazon.awssdk.services.s3.S3AsyncClient;
 final class FileSetManager {
 
   private static final Logger LOG = LoggerFactory.getLogger(FileSetManager.class);
+  private static final String COMPRESSION_ALGORITHM = CompressorStreamFactory.getZstandard();
+  private static final int COMPRESSION_SIZE_THRESHOLD = 8 * 1024 * 1024; // 8 MiB
+  private static final String TMP_COMPRESSION_PREFIX = "zb-backup-compress-";
+  private static final String TMP_DECOMPRESSION_PREFIX = "zb-backup-decompress-";
   private final S3AsyncClient client;
   private final S3BackupConfig config;
 
@@ -42,12 +54,67 @@ final class FileSetManager {
 
   private CompletableFuture<FileSet.FileMetadata> saveFile(
       final String prefix, final String fileName, final Path filePath) {
+    if (shouldCompressFile(filePath)) {
+      final var algorithm = COMPRESSION_ALGORITHM;
+      final var compressed = compressFile(filePath, algorithm);
+      LOG.trace("Saving compressed file {}({}) in prefix {}", fileName, compressed, prefix);
+      return client
+          .putObject(
+              put -> put.bucket(config.bucketName()).key(prefix + fileName),
+              AsyncRequestBody.fromFile(compressed))
+          .thenRunAsync(() -> cleanupCompressedFile(compressed))
+          .thenApply(unused -> FileSet.FileMetadata.withCompression(algorithm));
+    }
+
     LOG.trace("Saving file {}({}) in prefix {}", fileName, filePath, prefix);
     return client
         .putObject(
             put -> put.bucket(config.bucketName()).key(prefix + fileName),
             AsyncRequestBody.fromFile(filePath))
         .thenApply(unused -> FileSet.FileMetadata.none());
+  }
+
+  private void cleanupCompressedFile(final Path compressedFile) {
+    try {
+      Files.delete(compressedFile);
+    } catch (final IOException e) {
+      LOG.warn(
+          "Failed to clean up temporary file used for (de-)compression: {}", compressedFile, e);
+    }
+  }
+
+  private boolean shouldCompressFile(final Path filePath) {
+    try {
+      return config.enableCompression() && Files.size(filePath) > COMPRESSION_SIZE_THRESHOLD;
+    } catch (final IOException e) {
+      LOG.warn("Failed to determine if file should be compressed, assuming no: {}", filePath);
+      return false;
+    }
+  }
+
+  private Path compressFile(final Path file, final String algorithm) {
+    try {
+      final var compressedFile = Files.createTempFile(TMP_COMPRESSION_PREFIX, null);
+      LOG.trace("Compressing file {} to {} using {}", file, compressedFile, algorithm);
+      try (final var input = new BufferedInputStream(Files.newInputStream(file));
+          final var output = new BufferedOutputStream(Files.newOutputStream(compressedFile));
+          final var compressedOutput =
+              new CompressorStreamFactory().createCompressorOutputStream(algorithm, output)) {
+        IOUtils.copy(input, compressedOutput);
+        if (LOG.isTraceEnabled()) {
+          LOG.trace(
+              "Compressed file {} to {}. Uncompressed: {} bytes, compressed: {} bytes",
+              file,
+              compressedFile,
+              Files.size(file),
+              Files.size(compressedFile));
+        }
+        return compressedFile;
+      }
+    } catch (final IOException | CompressorException e) {
+      throw new BackupCompressionFailed(
+          "Failed to compress file %s using %s".formatted(file, algorithm), e);
+    }
   }
 
   CompletableFuture<NamedFileSet> restore(
