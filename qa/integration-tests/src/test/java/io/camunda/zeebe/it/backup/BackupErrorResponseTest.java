a000e1eb9252d284d1a13e2738269db69625fcf8
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-12-06 16:34:21 +0100

    test(qa): nest test cases

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/backup/BackupErrorResponseTest.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/backup/BackupErrorResponseTest.java
@@ -13,6 +13,7 @@ import io.camunda.zeebe.backup.s3.S3BackupConfig.Builder;
 import io.camunda.zeebe.backup.s3.S3BackupStore;
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.broker.system.configuration.backup.BackupStoreCfg.BackupStoreType;
+import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
 import io.camunda.zeebe.it.clustering.ClusteringRuleExtension;
 import io.camunda.zeebe.qa.util.testcontainers.MinioContainer;
 import io.camunda.zeebe.shared.management.BackupEndpoint;
@@ -20,6 +21,8 @@ import java.time.Duration;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -30,77 +33,81 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 class BackupErrorResponseTest {
 
-  @Container private static final MinioContainer S3 = new MinioContainer();
-  private BackupEndpoint backupEndpoint;
+  @Nested
+  final class DuplicateBackupIdTest {
 
-  private String bucketName;
+    @Container private static final MinioContainer S3 = new MinioContainer();
+    private BackupEndpoint backupEndpoint;
 
-  @RegisterExtension
-  private final ClusteringRuleExtension clusteringRule =
-      new ClusteringRuleExtension(1, 1, 1, this::configureBackupStore);
+    private String bucketName;
 
-  private void configureBackupStore(final BrokerCfg config) {
-    config.getExperimental().getFeatures().setEnableBackup(true);
+    @RegisterExtension
+    private final ClusteringRuleExtension clusteringRule =
+        new ClusteringRuleExtension(1, 1, 1, this::configureBackupStore);
 
-    final var backupConfig = config.getData().getBackup();
-    backupConfig.setStore(BackupStoreType.S3);
+    private void configureBackupStore(final BrokerCfg config) {
+      config.getExperimental().getFeatures().setEnableBackup(true);
 
-    final var s3Config = backupConfig.getS3();
+      final var backupConfig = config.getData().getBackup();
+      backupConfig.setStore(BackupStoreType.S3);
 
-    generateBucketName();
+      final var s3Config = backupConfig.getS3();
 
-    s3Config.setBucketName(bucketName);
-    s3Config.setEndpoint(S3.externalEndpoint());
-    s3Config.setRegion(S3.region());
-    s3Config.setAccessKey(S3.accessKey());
-    s3Config.setSecretKey(S3.secretKey());
-    s3Config.setForcePathStyleAccess(true);
-  }
+      generateBucketName();
 
-  private void generateBucketName() {
-    // Generate only once per test
-    if (bucketName == null) {
-      bucketName = RandomStringUtils.randomAlphabetic(10).toLowerCase();
+      s3Config.setBucketName(bucketName);
+      s3Config.setEndpoint(S3.externalEndpoint());
+      s3Config.setRegion(S3.region());
+      s3Config.setAccessKey(S3.accessKey());
+      s3Config.setSecretKey(S3.secretKey());
+      s3Config.setForcePathStyleAccess(true);
     }
-  }
 
-  void createBucket() {
-    // Create bucket before for storing backups
-    final var s3ClientConfig =
-        new Builder()
-            .withBucketName(bucketName)
-            .withEndpoint(S3.externalEndpoint())
-            .withRegion(S3.region())
-            .withCredentials(S3.accessKey(), S3.secretKey())
-            .withApiCallTimeout(Duration.ofSeconds(15))
-            .forcePathStyleAccess(true)
-            .build();
-    try (final var s3Client = S3BackupStore.buildClient(s3ClientConfig)) {
-      s3Client.createBucket(builder -> builder.bucket(bucketName).build()).join();
+    private void generateBucketName() {
+      // Generate only once per test
+      if (bucketName == null) {
+        bucketName = RandomStringUtils.randomAlphabetic(10).toLowerCase();
+      }
     }
-  }
 
-  @BeforeEach
-  void setup() {
-    backupEndpoint = new BackupEndpoint(clusteringRule.getGateway().getBrokerClient());
-    createBucket();
-  }
+    void createBucket() {
+      // Create bucket before for storing backups
+      final var s3ClientConfig =
+          new Builder()
+              .withBucketName(bucketName)
+              .withEndpoint(S3.externalEndpoint())
+              .withRegion(S3.region())
+              .withCredentials(S3.accessKey(), S3.secretKey())
+              .withApiCallTimeout(Duration.ofSeconds(15))
+              .forcePathStyleAccess(true)
+              .build();
+      try (final var s3Client = S3BackupStore.buildClient(s3ClientConfig)) {
+        s3Client.createBucket(builder -> builder.bucket(bucketName).build()).join();
+      }
+    }
 
-  @AfterEach
-  void close() {
-    // reset so that each test can use a different bucket name
-    bucketName = null;
-  }
+    @BeforeEach
+    void setup() {
+      backupEndpoint = new BackupEndpoint(clusteringRule.getGateway().getBrokerClient());
+      createBucket();
+    }
 
-  @ParameterizedTest
-  @CsvSource({"1,1", "2,1"})
-  @Timeout(value = 60)
-  void shouldFailTakeBackupIfCheckpointAlreadyExists(
-      final long existingCheckpoint, final long backupIdToTake) {
-    // given
-    backupEndpoint.take(existingCheckpoint);
+    @AfterEach
+    void close() {
+      // reset so that each test can use a different bucket name
+      bucketName = null;
+    }
 
-    // when - then
-    assertThat(backupEndpoint.take(backupIdToTake).getStatus()).isEqualTo(409);
+    @ParameterizedTest
+    @CsvSource({"1,1", "2,1"})
+    @Timeout(value = 60)
+    void shouldFailTakeBackupIfCheckpointAlreadyExists(
+        final long existingCheckpoint, final long backupIdToTake) {
+      // given
+      backupEndpoint.take(existingCheckpoint);
+
+      // when - then
+      assertThat(backupEndpoint.take(backupIdToTake).getStatus()).isEqualTo(409);
+    }
   }
 }
