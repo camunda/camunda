20d3eeabcb5c5055507e19bf6e9310a8a3288402
Ralf Puchert <49151958+ralfpuchert@users.noreply.github.com>
2022-12-07 14:21:56 +0100

    fix(backend): distinguish JwtToken and UserToken authentication (#3819)
    Related with #3818

--- webapp/src/main/java/io/camunda/operate/webapp/security/sso/SSOUserService.java
+++ webapp/src/main/java/io/camunda/operate/webapp/security/sso/SSOUserService.java
@@ -7,20 +7,25 @@
 package io.camunda.operate.webapp.security.sso;
 
 import io.camunda.operate.webapp.security.OperateProfileService;
+import io.camunda.operate.webapp.security.Permission;
 import io.camunda.operate.webapp.security.UserService;
 
+import java.util.List;
 import java.util.Map;
+
 import io.camunda.operate.property.OperateProperties;
 import io.camunda.operate.webapp.rest.dto.UserDto;
 import io.camunda.operate.webapp.security.sso.model.ClusterMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import com.auth0.jwt.interfaces.Claim;
 
 @Component
 @Profile(OperateProfileService.SSO_AUTH_PROFILE)
-public class SSOUserService implements UserService<TokenAuthentication> {
+public class SSOUserService implements UserService<AbstractAuthenticationToken> {
 
   @Autowired
   private OperateProperties operateProperties;
@@ -30,7 +35,28 @@ public class SSOUserService implements UserService<TokenAuthentication> {
 
   @Override
   public UserDto createUserDtoFrom(
-      final TokenAuthentication authentication) {
+      final AbstractAuthenticationToken abstractAuthentication) {
+    if (abstractAuthentication instanceof TokenAuthentication) {
+      return getUserDtoFor((TokenAuthentication) abstractAuthentication);
+    } else if (abstractAuthentication instanceof JwtAuthenticationToken){
+      return getUserDtoFor((JwtAuthenticationToken) abstractAuthentication);
+    } else {
+      return null;
+    }
+  }
+
+  private UserDto getUserDtoFor(JwtAuthenticationToken authentication) {
+    // Token is already validated in CCSaaSJwtAuthenticationTokenValidator
+    // ,and we don't have permission yet.
+    final List<Permission> permissions = List.of(Permission.READ, Permission.WRITE);
+    return new UserDto()
+        .setUserId(authentication.getName())
+        .setDisplayName(authentication.getName())
+        .setCanLogout(false)
+        .setPermissions(permissions);
+  }
+
+  private UserDto getUserDtoFor(final TokenAuthentication authentication) {
     Map<String, Claim> claims = authentication.getClaims();
     String name = "No name";
     if (claims.containsKey(operateProperties.getAuth0().getNameKey())) {
@@ -41,13 +67,9 @@ public class SSOUserService implements UserService<TokenAuthentication> {
     if ( clusterMetadata != null ) {
       appNames2Urls = clusterMetadata.getUrls();
     }
-    return new UserDto()
-        .setUserId(authentication.getName())
-        .setDisplayName(name)
-        .setCanLogout(false)
+    return new UserDto().setUserId(authentication.getName()).setDisplayName(name).setCanLogout(false)
         .setPermissions(authentication.getPermissions())
         .setRoles(authentication.getRoles(operateProperties.getAuth0().getOrganizationsKey()))
-        .setSalesPlanType(authentication.getSalesPlanType())
-        .setC8Links(appNames2Urls);
+        .setSalesPlanType(authentication.getSalesPlanType()).setC8Links(appNames2Urls);
   }
 }
