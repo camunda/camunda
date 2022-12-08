2cab237b0b79f536bfa19fcea7dd13ef41af2cfd
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2022-12-08 16:36:23 +0100

    test(backup-store/s3): remove unnecessary container in the test

--- backup-stores/s3/src/test/java/io/camunda/zeebe/backup/s3/ConnectionErrorTest.java
+++ backup-stores/s3/src/test/java/io/camunda/zeebe/backup/s3/ConnectionErrorTest.java
@@ -18,56 +18,34 @@ import java.util.concurrent.ExecutionException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 
 @Testcontainers
 final class ConnectionErrorTest {
   private static final String ACCESS_KEY = "letmein";
   private static final String SECRET_KEY = "letmein1234";
   private static final int DEFAULT_PORT = 9000;
-
-  @SuppressWarnings("resource")
-  @Container
-  private static final GenericContainer<?> S3 =
-      new GenericContainer<>(DockerImageName.parse("minio/minio"))
-          .withCommand("server /data")
-          .withExposedPorts(DEFAULT_PORT)
-          .withEnv("MINIO_ACCESS_KEY", ACCESS_KEY)
-          .withEnv("MINIO_SECRET_KEY", SECRET_KEY)
-          .withEnv("MINIO_DOMAIN", "localhost")
-          .waitingFor(
-              new HttpWaitStrategy()
-                  .forPath("/minio/health/ready")
-                  .forPort(DEFAULT_PORT)
-                  .withStartupTimeout(Duration.ofMinutes(1)));
-
   private S3BackupStore store;
 
   @BeforeEach
-  void setupBucket() {
+  void createBackupStore() {
     final var config =
         new Builder()
             .withBucketName(RandomStringUtils.randomAlphabetic(10).toLowerCase())
-            .withEndpoint("http://%s:%d".formatted(S3.getHost(), S3.getMappedPort(DEFAULT_PORT)))
+            .withEndpoint("http://%s:%d".formatted("localhost", DEFAULT_PORT))
             .withRegion(Region.US_EAST_1.id())
             .withCredentials(ACCESS_KEY, SECRET_KEY)
             .forcePathStyleAccess(true)
             .build();
     final var client = S3BackupStore.buildClient(config);
     store = new S3BackupStore(config, client);
-    client.createBucket(CreateBucketRequest.builder().bucket(config.bucketName()).build()).join();
   }
 
   @Test
   void shouldFailListWhenStoreIsNotReachable() {
     // given
-    S3.close();
+    // s3 container is not running
 
     // when
     final var res =
@@ -84,7 +62,7 @@ final class ConnectionErrorTest {
   @Test
   void shouldFailGetWhenStoreIsNotReachable() {
     // given
-    S3.close();
+    // s3 container is not running
 
     // when
     final var res = store.getStatus(new BackupIdentifierImpl(0, 1, 1));
