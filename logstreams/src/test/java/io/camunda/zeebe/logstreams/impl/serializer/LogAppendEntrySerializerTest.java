193e1be5bac7468c8e041550d0b525df957e43f2
Ole Schönburg <ole.schoenburg@gmail.com>
2022-12-09 15:30:51 +0100

    test(logstream): verify that serialized batch is readable

--- logstreams/src/test/java/io/camunda/zeebe/logstreams/impl/serializer/LogAppendEntrySerializerTest.java
+++ logstreams/src/test/java/io/camunda/zeebe/logstreams/impl/serializer/LogAppendEntrySerializerTest.java
@@ -7,16 +7,13 @@
  */
 package io.camunda.zeebe.logstreams.impl.serializer;
 
+import static io.camunda.zeebe.logstreams.impl.serializer.TestUtils.readString;
 import static io.camunda.zeebe.util.buffer.BufferUtil.wrapString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 import io.camunda.zeebe.logstreams.impl.log.LoggedEventImpl;
 import io.camunda.zeebe.logstreams.util.MutableLogAppendEntry;
-import io.camunda.zeebe.util.buffer.BufferReader;
-import io.camunda.zeebe.util.buffer.BufferUtil;
-import java.util.function.Consumer;
-import org.agrona.DirectBuffer;
 import org.agrona.ExpandableArrayBuffer;
 import org.agrona.MutableDirectBuffer;
 import org.junit.jupiter.api.Test;
@@ -95,20 +92,5 @@ final class LogAppendEntrySerializerTest {
     // when - then
     assertThatCode(() -> serializer.serialize(writeBuffer, 0, entry, -1, 3, 4))
         .isInstanceOf(IllegalArgumentException.class);
-  }
-
-  private String readString(final Consumer<BufferReader> reader) {
-    final var value = new StringReader();
-    reader.accept(value);
-    return value.value;
-  }
-
-  private static final class StringReader implements BufferReader {
-    private String value;
-
-    @Override
-    public void wrap(final DirectBuffer buffer, final int offset, final int length) {
-      value = BufferUtil.bufferAsString(buffer, offset, length);
-    }
   }
 }
