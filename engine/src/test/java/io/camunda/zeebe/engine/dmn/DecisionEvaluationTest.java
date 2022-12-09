1bb52a58ced8cbcc48fe75526b531a7241de7635
Nikola Koevski <nikola.koevski@camunda.com>
2022-12-09 16:19:47 +0100

    test(engine): test standalone decision evaluation
    Add test coverage for the EvaluateDecisionProcessor. To enable standalone decision evaluation testing, a new DecisionEvaluationClient is added.

---
+++ engine/src/test/java/io/camunda/zeebe/engine/dmn/DecisionEvaluationTest.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.dmn;
+
+import static io.camunda.zeebe.protocol.record.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.engine.util.EngineRule;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.intent.DecisionEvaluationIntent;
+import io.camunda.zeebe.protocol.record.value.DecisionEvaluationRecordValue;
+import io.camunda.zeebe.protocol.record.value.DeploymentRecordValue;
+import io.camunda.zeebe.protocol.record.value.deployment.DecisionRecordValue;
+import io.camunda.zeebe.test.util.record.RecordingExporterTestWatcher;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class DecisionEvaluationTest {
+
+  @ClassRule public static final EngineRule ENGINE = EngineRule.singlePartition();
+  private static final String DMN_RESOURCE = "/dmn/drg-force-user.dmn";
+  private static final String DECISION_ID = "jedi_or_sith";
+  private static final String EXPECTED_DECISION_OUTPUT = "\"Jedi\"";
+  private static final String EXPECTED_FAILURE_MSG =
+      "Expected to evaluate decision 'jedi_or_sith', but failed to evaluate expression 'lightsaberColor': no variable found for name 'lightsaberColor'";
+
+  @Rule
+  public final RecordingExporterTestWatcher recordingExporterTestWatcher =
+      new RecordingExporterTestWatcher();
+
+  @Test
+  public void shouldEvaluateDecisionById() {
+    // given
+    ENGINE.deployment().withXmlClasspathResource(DMN_RESOURCE).deploy();
+
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE
+            .decision()
+            .ofDecisionId(DECISION_ID)
+            .withVariable("lightsaberColor", "blue")
+            .evaluate();
+
+    // then
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.EVALUATED);
+    assertThat(record.getValue()).hasDecisionOutput(EXPECTED_DECISION_OUTPUT);
+  }
+
+  @Test
+  public void shouldFailEvaluationOfDecisionById() {
+    // given
+    ENGINE.deployment().withXmlClasspathResource(DMN_RESOURCE).deploy();
+
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE.decision().ofDecisionId(DECISION_ID).expectFailure().evaluate();
+
+    // then
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.FAILED);
+    assertThat(record.getValue()).hasFailedDecisionId(DECISION_ID);
+    assertThat(record.getValue()).hasEvaluationFailureMessage(EXPECTED_FAILURE_MSG);
+  }
+
+  @Test
+  public void shouldEvaluateDecisionByKey() {
+    // given
+    final Record<DeploymentRecordValue> deploymentRecord =
+        ENGINE.deployment().withXmlClasspathResource(DMN_RESOURCE).deploy();
+    final DecisionRecordValue decisionRecord =
+        deploymentRecord.getValue().getDecisionsMetadata().stream()
+            .filter(decisionRecordValue -> decisionRecordValue.getDecisionId().equals(DECISION_ID))
+            .findAny()
+            .get();
+    final long decisionKey = decisionRecord.getDecisionKey();
+
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE
+            .decision()
+            .ofDecisionKey(decisionKey)
+            .withVariable("lightsaberColor", "blue")
+            .evaluate();
+
+    // then
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.EVALUATED);
+    assertThat(record.getValue()).hasDecisionOutput(EXPECTED_DECISION_OUTPUT);
+  }
+
+  @Test
+  public void shouldRejectDecisionUsingIdAndKey() {
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE.decision().ofDecisionId("falseId").ofDecisionKey(123L).expectRejection().evaluate();
+
+    // then
+    assertThat(record.getRejectionType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.EVALUATE);
+    assertThat(record.getRejectionReason())
+        .isEqualTo(
+            "Expected either a decisionId or a decisionKey greater than -1, but none or both provided");
+  }
+
+  @Test
+  public void shouldRejectDecisionWithoutIdAndKey() {
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE.decision().ofDecisionId("").ofDecisionKey(-1L).expectRejection().evaluate();
+
+    // then
+    assertThat(record.getRejectionType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.EVALUATE);
+    assertThat(record.getRejectionReason())
+        .isEqualTo(
+            "Expected either a decisionId or a decisionKey greater than -1, but none or both provided");
+  }
+
+  @Test
+  public void shouldRejectDecisionOnFalseId() {
+    // given
+    final var falseDecisionId = "falseId";
+
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE.decision().ofDecisionId(falseDecisionId).expectRejection().evaluate();
+
+    // then
+    assertThat(record.getRejectionType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.EVALUATE);
+    assertThat(record.getRejectionReason())
+        .isEqualTo(
+            "Expected to evaluate decision '"
+                + falseDecisionId
+                + "', but no decision found for id '"
+                + falseDecisionId
+                + "'");
+  }
+
+  @Test
+  public void shouldRejectDecisionOnFalseKey() {
+    // given
+    final var falseDecisionKey = 123L;
+
+    // when
+    final Record<DecisionEvaluationRecordValue> record =
+        ENGINE.decision().ofDecisionKey(falseDecisionKey).expectRejection().evaluate();
+
+    // then
+    assertThat(record.getRejectionType()).isEqualTo(RejectionType.INVALID_ARGUMENT);
+    assertThat(record.getIntent()).isEqualTo(DecisionEvaluationIntent.EVALUATE);
+    assertThat(record.getRejectionReason())
+        .isEqualTo(
+            "Expected to evaluate decision '"
+                + falseDecisionKey
+                + "', but no decision found for key '"
+                + falseDecisionKey
+                + "'");
+  }
+}
