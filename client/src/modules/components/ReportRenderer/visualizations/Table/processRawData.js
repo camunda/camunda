9d020df0ef1fb8b13dcb606999de786862378f54
Michal Konopski <michal.konopski@camunda.com>
2022-12-14 10:48:21 +0100

    feat(raw data table): disable sorting for flow node duration columns
    related to OPT-6657

--- client/src/modules/components/ReportRenderer/visualizations/Table/processRawData.js
+++ client/src/modules/components/ReportRenderer/visualizations/Table/processRawData.js
@@ -172,6 +172,7 @@ export default function processRawData({
           id: 'flowNodeDuration:' + key,
           label: getLabelWithType(label, 'flowNodeDuration'),
           title: label,
+          sortable: false,
         };
       })
     );
