eea7fb7531bf13cff0cedf3ffbb8d4220afb42f1
Michal Konopski <michal.konopski@camunda.com>
2022-12-14 16:24:02 +0100

    test(whats new modal): add unit tests
    related to OPT-6653

---
+++ client/src/components/Header/WhatsNewModal/WhatsNewModal.test.js
@@ -0,0 +1,63 @@
+import {runAllEffects} from 'react';
+import {shallow} from 'enzyme';
+
+import {WhatsNewModal} from './WhatsNewModal';
+import {getMarkdownText, isChangeLogSeen, setChangeLogAsSeen} from './service';
+
+jest.mock('./service', () => ({
+  isChangeLogSeen: jest.fn().mockReturnValue({seen: false}),
+  setChangeLogAsSeen: jest.fn(),
+  getMarkdownText: jest.fn().mockReturnValue('#hello world'),
+}));
+
+const props = {
+  mightFail: jest.fn().mockImplementation((data, cb) => cb(data)),
+};
+
+it('should show whats new text', () => {
+  const node = shallow(<WhatsNewModal {...props} open />);
+
+  runAllEffects();
+
+  expect(getMarkdownText).toHaveBeenCalled()
+  expect(node.find('reactMarkdown').childAt(0)).toHaveText('#hello world');
+});
+
+it('should show optimize version', async () => {
+  const node = shallow(<WhatsNewModal {...props} />);
+
+  runAllEffects();
+  await flushPromises();
+
+  expect(node.childAt(0).html()).toContain('2.7.0');
+});
+
+it('should not show the modal if it is seen before', () => {
+  isChangeLogSeen.mockReturnValueOnce({seen: true});
+  const node = shallow(<WhatsNewModal {...props} />);
+
+  runAllEffects();
+
+  expect(node.find('Modal').props().open).toBe(false);
+});
+
+it('should call onClose when closing modal', () => {
+  const spy = jest.fn();
+  const node = shallow(<WhatsNewModal {...props} onClose={spy} />);
+
+  runAllEffects();
+
+  node.find('Modal').prop('onClose')();
+
+  expect(spy).toHaveBeenCalled();
+});
+
+it('should set status as seen when closing the whats new modal', () => {
+  const node = shallow(<WhatsNewModal {...props} onClose={jest.fn()} />);
+
+  runAllEffects();
+
+  node.find('Modal').prop('onClose')();
+
+  expect(setChangeLogAsSeen).toHaveBeenCalled();
+});
