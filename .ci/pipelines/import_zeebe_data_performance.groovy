8b61a0a7b004df510e7253b34a42d96dc3a2665c
Andromachi Rozaki <42938116+andromaqui@users.noreply.github.com>
2022-12-14 13:45:05 +0100

    chor(jenkins-pipelines): Renamed 8-physsd nodes tp 8-netssd poles (#5647)

--- .ci/pipelines/import_zeebe_data_performance.groovy
+++ .ci/pipelines/import_zeebe_data_performance.groovy
@@ -5,7 +5,7 @@
 
 def static MAVEN_DOCKER_IMAGE() { return "maven:3.8.1-jdk-11-slim" }
 
-def static NODE_POOL() { return "agents-n1-standard-8-physsd-stable" }
+def static NODE_POOL() { return "agents-n1-standard-8-netssd-stable" }
 
 String gCloudAndMavenAgent() {
   """
