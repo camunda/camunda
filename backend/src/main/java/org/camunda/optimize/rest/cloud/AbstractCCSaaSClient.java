16b623ab2d2e36036239c0f50ae048d8f1af65b7
grlimacan <91879848+grlimacan@users.noreply.github.com>
2023-01-05 12:58:14 +0200

    fix(loading):Added timeout to external API requests so that Optimize … (#5726)
    * fix(loading):Added timeout to external API requests so that Optimize is not blocked
    
    * fix(loading):Added comment for operation timeout
    
    * fix(loading): Addressed PR comments

--- backend/src/main/java/org/camunda/optimize/rest/cloud/AbstractCCSaaSClient.java
+++ backend/src/main/java/org/camunda/optimize/rest/cloud/AbstractCCSaaSClient.java
@@ -6,10 +6,11 @@
 package org.camunda.optimize.rest.cloud;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.camunda.optimize.service.util.configuration.ConfigurationService;
 import org.camunda.optimize.service.util.configuration.condition.CCSaaSCondition;
 import org.camunda.optimize.service.util.configuration.security.CloudAuthConfiguration;
@@ -20,6 +21,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PreDestroy;
 import javax.ws.rs.core.HttpHeaders;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import static org.camunda.optimize.dto.optimize.query.ui_configuration.AppName.CONSOLE;
 import static org.camunda.optimize.dto.optimize.query.ui_configuration.AppName.MODELER;
@@ -48,9 +50,24 @@ public abstract class AbstractCCSaaSClient {
   protected AbstractCCSaaSClient(final ObjectMapper objectMapper, final ConfigurationService configurationService) {
     this.objectMapper = objectMapper;
     this.configurationService = configurationService;
-    this.httpClient = HttpClients.createDefault();
+    HttpClientBuilder builder = HttpClientBuilder.create();
+
+    // Setting a general timeout for external requests to 5000 milliseconds, so that outgoing requests don't block the
+    // execution flow from Optimize
+    int timeout = 5000;
+    builder.setConnectionTimeToLive(timeout, TimeUnit.MILLISECONDS);
+    builder.evictIdleConnections(timeout, TimeUnit.MILLISECONDS);
+    RequestConfig rc = RequestConfig.custom()
+      .setConnectionRequestTimeout(timeout)
+      .setConnectTimeout(timeout)
+      .setSocketTimeout(timeout)
+      .build();
+    builder.setDefaultRequestConfig(rc);
+    this.httpClient = builder.build();
   }
 
+  // In case the connection times out, the execution will throw a SocketTimeoutException or a
+  // ConnectionTimeoutException (depending on the reason), which are both also IOExceptions
   protected CloseableHttpResponse performRequest(final HttpRequestBase request, final String accessToken) throws IOException {
     request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
     return httpClient.execute(request);
