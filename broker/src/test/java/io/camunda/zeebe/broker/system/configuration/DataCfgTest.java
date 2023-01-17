308c0ea1ee8bee57e470f190c7194ffde7fb6021
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-01-17 14:07:38 +0100

    test(broker): update datacfg test to verify new freespace config

--- broker/src/test/java/io/camunda/zeebe/broker/system/configuration/DataCfgTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/system/configuration/DataCfgTest.java
@@ -8,25 +8,46 @@
 package io.camunda.zeebe.broker.system.configuration;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import io.camunda.zeebe.broker.system.configuration.DiskCfg.FreeSpaceCfg;
+import java.time.Duration;
 import org.junit.Test;
+import org.springframework.util.unit.DataSize;
 
 public class DataCfgTest {
 
   @Test
-  public void shouldSetWatermarksTo1IfDisabled() {
-    // given
-    final DataCfg dataCfg = new DataCfg();
-    dataCfg.setDiskUsageCommandWatermark(0.1);
-    dataCfg.setDiskUsageReplicationWatermark(0.2);
-    dataCfg.setDiskUsageMonitoringEnabled(false);
-
+  public void shouldUseDefaultFreeSpaceConfig() {
     // when
+    final DataCfg dataCfg = new DataCfg();
     dataCfg.init(new BrokerCfg(), "/base");
 
     // then
-    assertThat(dataCfg.isDiskUsageMonitoringEnabled()).isFalse();
-    assertThat(dataCfg.getDiskUsageCommandWatermark()).isEqualTo(1.0);
-    assertThat(dataCfg.getDiskUsageReplicationWatermark()).isEqualTo(1.0);
+    assertThat(dataCfg.getFreeDiskSpaceReplicationWatermark())
+        .isEqualTo(DataSize.ofGigabytes(1).toBytes());
+    assertThat(dataCfg.getFreeDiskSpaceCommandWatermark())
+        .isEqualTo(DataSize.ofGigabytes(2).toBytes());
+  }
+
+  @Test
+  public void shouldOverrideWhenOldWatermarksConfigProvided() {
+    // when
+    final DataCfg dataCfg = new DataCfg();
+    final FreeSpaceCfg mockFreeSpaceCfg = mock(FreeSpaceCfg.class);
+    dataCfg.getDisk().setFreeSpace(mockFreeSpaceCfg);
+    dataCfg.setDiskUsageMonitoringInterval(Duration.ofMinutes(5));
+    dataCfg.setDiskUsageMonitoringEnabled(false);
+    dataCfg.setDiskUsageCommandWatermark(0.1);
+    dataCfg.setDiskUsageReplicationWatermark(0.2);
+    dataCfg.init(new BrokerCfg(), "/base");
+
+    // then
+    verify(mockFreeSpaceCfg, times(1)).setProcessing("90.0%");
+    verify(mockFreeSpaceCfg, times(1)).setReplication("80.0%");
+    assertThat(dataCfg.getDisk().isEnableMonitoring()).isFalse();
+    assertThat(dataCfg.getDisk().getMonitoringInterval()).isEqualTo(Duration.ofMinutes(5));
   }
 }
