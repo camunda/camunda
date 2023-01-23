d8936237c106f3a9a94f80cf8e5003a5cdf7a8b9
Patrick Dehn <pedesen@users.noreply.github.com>
2023-01-23 13:31:31 +0100

    chore: show linting warnings in pre-commit hook (#3975)
    * chore: show linting warnings in pre-commit hook
    
    * chore: remove unused import

--- client/src/modules/components/DateRangeField/index.test.tsx
+++ client/src/modules/components/DateRangeField/index.test.tsx
@@ -5,7 +5,7 @@
  * except in compliance with the proprietary license.
  */
 
-import {render, screen, waitFor} from 'modules/testing-library';
+import {render, screen} from 'modules/testing-library';
 import {
   applyDateRange,
   pickDateTimeRange,
