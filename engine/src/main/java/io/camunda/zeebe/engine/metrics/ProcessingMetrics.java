314c4fc33e2a01af3d3714902639f16e2e85d915
Christopher Zell <zelldon91@googlemail.com>
2023-02-06 14:25:09 +0100

    feat: observe batch processing command count

--- engine/src/main/java/io/camunda/zeebe/engine/metrics/ProcessingMetrics.java
+++ engine/src/main/java/io/camunda/zeebe/engine/metrics/ProcessingMetrics.java
@@ -20,16 +20,32 @@ public class ProcessingMetrics {
           .namespace(NAMESPACE)
           .name("stream_processor_batch_processing_duration")
           .help("Time spent in batch processing (in seconds)")
+          .buckets(.0001, .001, .01, 0.1, .250, 0.5, 1, 2)
+          .labelNames(LABEL_NAME_PARTITION)
+          .register();
+
+  private static final Histogram BATCH_PROCESSING_COMMANDS =
+      Histogram.build()
+          .namespace(NAMESPACE)
+          .name("stream_processor_batch_processing_commands")
+          .help("Records the distribution of commands in a batch over time")
+          .buckets(1, 2, 4, 8, 16, 32, 64, 128)
           .labelNames(LABEL_NAME_PARTITION)
           .register();
 
   private final Child batchProcessingDuration;
+  private final Child batchProcessingCommands;
 
   public ProcessingMetrics(final String partitionIdLabel) {
     batchProcessingDuration = BATCH_PROCESSING_DURATION.labels(partitionIdLabel);
+    batchProcessingCommands = BATCH_PROCESSING_COMMANDS.labels(partitionIdLabel);
   }
 
   public Histogram.Timer startBatchProcessingDurationTimer() {
     return batchProcessingDuration.startTimer();
+  }
+
+  public void observeCommandCount(final int commandCount) {
+    batchProcessingCommands.observe(commandCount);
   }
 }
