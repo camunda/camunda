fec0bc0e2ac5a98b05de39c35f54b9e5c0182982
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-02-07 15:47:37 +0100

    feat(atomix): add RaftLogFlusher API
    Adds a new interface to implement various flushing strategies for the
    RaftLog. Additionally adds three implementations:
    
    - NoOpFlusher: simply does nothing
    - DirectFlusher: flushes the journal on every call
    - DelayedFlusher: flushes the log after a specific delay has passed.
      Will merge flush requests together.
    
    The DelayedFlusher is currently not thread safe and expects to be
    executed on the Raft thread, sequenced with other Raft operations. This
    will be improved in a subsequent PR.

---
+++ atomix/cluster/src/main/java/io/atomix/raft/storage/log/DelayedFlusher.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.atomix.raft.storage.log;
+
+import io.atomix.utils.concurrent.Scheduled;
+import io.atomix.utils.concurrent.Scheduler;
+import io.camunda.zeebe.journal.Journal;
+import java.time.Duration;
+import java.util.Objects;
+
+/**
+ * An implementation of {@link RaftLogFlusher} which treats calls to {@link #flush(Journal)} as
+ * signals that there is data to be flushed. When that happens, an asynchronous operation is
+ * scheduled with a predefined delay. If a flush was already scheduled, then the signal is ignored.
+ *
+ * <p>In other words, this implementation flushes at least every given period, if there is anything
+ * to flush.
+ *
+ * <p>NOTE: this class is not thread safe, and is expected to run from the same thread as the
+ * journal write path, e.g. the Raft thread.
+ */
+public final class DelayedFlusher implements RaftLogFlusher {
+  private final Scheduler scheduler;
+  private final Duration delayTime;
+
+  private Scheduled scheduledFlush;
+  private boolean closed;
+
+  public DelayedFlusher(final Scheduler scheduler, final Duration delayTime) {
+    this.scheduler = Objects.requireNonNull(scheduler, "must specify a thread context");
+    this.delayTime = Objects.requireNonNull(delayTime, "must specify a valid flush delay");
+  }
+
+  @Override
+  public void flush(final Journal journal) {
+    scheduleFlush(journal);
+  }
+
+  @Override
+  public boolean isDirect() {
+    return false;
+  }
+
+  @Override
+  public void close() {
+    if (closed) {
+      return;
+    }
+
+    closed = true;
+    if (scheduledFlush != null) {
+      scheduledFlush.cancel();
+    }
+  }
+
+  private void asyncFlush(final Journal journal) {
+    scheduledFlush = null;
+
+    if (closed || !journal.isOpen()) {
+      return;
+    }
+
+    journal.flush();
+  }
+
+  private void scheduleFlush(final Journal journal) {
+    if (scheduledFlush == null) {
+      scheduledFlush = scheduler.schedule(delayTime, () -> asyncFlush(journal));
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "DelayedFlusher{"
+        + "scheduler="
+        + scheduler
+        + ", delay="
+        + delayTime
+        + ", scheduledFlush="
+        + scheduledFlush
+        + ", closed="
+        + closed
+        + '}';
+  }
+}
