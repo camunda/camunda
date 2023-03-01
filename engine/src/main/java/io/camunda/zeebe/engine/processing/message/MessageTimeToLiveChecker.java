b4ff81eba6804225de7852fc775ec9b2019d5535
Nico Korthout <nico.korthout@camunda.com>
2023-03-01 18:49:36 +0100

    fix(engine): limit expire msg commands in result
    This adds a virtual limit to the number of EXPIRE Message commands the
    MessageTimeToLiveChecker adds to a result when executed, to prevent that
    it clogs the log stream with too many of these commands.
    
    It achieves this by keeping track of the current visitor index, and
    reusing this index in subsequent executions. The index is used to start
    the visiting at a specific key in the column family. This was added in
    the previous commit.
    
    This index must be reset once the iteration reaches the point that there
    are no messages with an expired deadline in the column family any more.
    The reset is necessary to make sure we don't skip any messages whose
    deadline has expired, but was added during or after the iteration.
    
    The visit method informs the checker (when the visiting stops) whether
    it was stopped by the visitor or not. If it wasn't, then this means the
    end was reached and the index can be reset.
    
    The checker reschedules itself. Either immediately, when it wants to
    continue where it left off in another run, or after the interval if it
    reached the end. Note that we need to continue to schedule the task
    async.
    
    The limit and the interval are not yet configurable, because this
    requires changes to the `broker` module, i.e., exposing the config to
    the engine. Since this module is shared between ZPA and ZDP, I prefer to
    do this in a separate pull request, to make that easier to review.
    
    Note that this commit breaks userspace, because the engine no longer
    favors message expirations over regular processing and instead provides
    a more equal balance between the two. It's unlikely that users depend on
    this behavior.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/message/MessageTimeToLiveChecker.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/message/MessageTimeToLiveChecker.java
@@ -7,33 +7,75 @@
  */
 package io.camunda.zeebe.engine.processing.message;
 
+import io.camunda.zeebe.engine.api.ProcessingScheduleService;
 import io.camunda.zeebe.engine.api.Task;
 import io.camunda.zeebe.engine.api.TaskResult;
 import io.camunda.zeebe.engine.api.TaskResultBuilder;
 import io.camunda.zeebe.engine.state.immutable.MessageState;
+import io.camunda.zeebe.engine.state.immutable.MessageState.Index;
 import io.camunda.zeebe.protocol.impl.record.value.message.MessageRecord;
 import io.camunda.zeebe.protocol.record.intent.MessageIntent;
 import io.camunda.zeebe.scheduler.clock.ActorClock;
+import java.time.Duration;
+import org.agrona.collections.MutableInteger;
 
+/**
+ * The Message TTL Checker looks for expired message deadlines, and for each of those it writes an
+ * EXPIRE Message command.
+ *
+ * <p>To prevent that it clogs the log stream with too many EXPIRE Message commands, it only writes
+ * a limited number of these commands in a single run of {@link #execute(TaskResultBuilder)}.
+ *
+ * <p>It determines whether to reschedule itself immediately, or after the configured {@link
+ * MessageObserver#MESSAGE_TIME_TO_LIVE_CHECK_INTERVAL interval}. If it reschedules itself
+ * immediately, then it will continue where it left off the last time. Otherwise, it starts with the
+ * first expired message deadline it can find.
+ */
 public final class MessageTimeToLiveChecker implements Task {
 
   private static final MessageRecord EMPTY_DELETE_MESSAGE_COMMAND =
       new MessageRecord().setName("").setCorrelationKey("").setTimeToLive(-1L);
 
-  private final MessageState messageState;
+  private static final int EXPIRE_COMMANDS_LIMIT_DEFAULT = 10;
 
-  public MessageTimeToLiveChecker(final MessageState messageState) {
+  /** This determines the maximum number of EXPIRE commands it will attempt to fit in the result. */
+  private static final int LIMIT = EXPIRE_COMMANDS_LIMIT_DEFAULT;
+
+  private final ProcessingScheduleService scheduleService;
+  private final MessageState messageState;
+  /** Keeps track of where to continue between iterations. */
+  private MessageState.Index lastIndex;
+
+  public MessageTimeToLiveChecker(
+      final ProcessingScheduleService scheduleService, final MessageState messageState) {
+    this.scheduleService = scheduleService;
     this.messageState = messageState;
+    lastIndex = null;
   }
 
   @Override
   public TaskResult execute(final TaskResultBuilder taskResultBuilder) {
-    messageState.visitMessagesWithDeadlineBeforeTimestamp(
-        ActorClock.currentTimeMillis(),
-        null,
-        ((deadline, expiredMessageKey) -> taskResultBuilder.appendCommandRecord(
-            expiredMessageKey, MessageIntent.EXPIRE, EMPTY_DELETE_MESSAGE_COMMAND));
+    final var counter = new MutableInteger(0);
+
+    final boolean shouldContinueWhereLeftOff =
+        messageState.visitMessagesWithDeadlineBeforeTimestamp(
+            ActorClock.currentTimeMillis(),
+            lastIndex,
+            (deadline, expiredMessageKey) -> {
+              lastIndex = new Index(expiredMessageKey, deadline);
+              final boolean stillFitsInResult =
+                  taskResultBuilder.appendCommandRecord(
+                      expiredMessageKey, MessageIntent.EXPIRE, EMPTY_DELETE_MESSAGE_COMMAND);
+              return stillFitsInResult && counter.incrementAndGet() <= LIMIT;
+            });
+
+    if (shouldContinueWhereLeftOff) {
+      scheduleService.runDelayedAsync(Duration.ZERO, this);
+    } else {
+      lastIndex = null;
+      scheduleService.runDelayedAsync(MessageObserver.MESSAGE_TIME_TO_LIVE_CHECK_INTERVAL, this);
+    }
+
     return taskResultBuilder.build();
   }
-
 }
