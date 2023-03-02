271c221cc81e688ff4345dc596cc8c1a42421f24
Nico Korthout <nico.korthout@camunda.com>
2023-03-02 17:51:11 +0100

    feat(broker/engine): add msg ttl checker configs
    Adds the the `broker.engine.messages` configuration, allowing to
    configure the:
    - `ttlCheckerBatchLimit`
    - `ttlCheckerInterval`

---
+++ broker/src/test/java/io/camunda/zeebe/broker/system/configuration/EngineCfgTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.broker.system.configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+final class EngineCfgTest {
+
+  public final Map<String, String> environment = new HashMap<>();
+
+  @Test
+  void shouldCreateEngineConfigurationFromDefaults() {
+    // given
+    final BrokerCfg cfg = TestConfigReader.readConfig("empty", environment);
+
+    // when
+    final var configuration = cfg.getEngine().createEngineConfiguration();
+
+    // then
+    assertThat(configuration.getMessagesTtlCheckerBatchLimit()).isEqualTo(10);
+    assertThat(configuration.getMessagesTtlCheckerInterval()).isEqualTo(Duration.ofMinutes(1));
+  }
+
+  @Test
+  void shouldCreateEngineConfigurationFromConfig() {
+    // given
+    final BrokerCfg cfg = TestConfigReader.readConfig("engine", environment);
+
+    // when
+    final var configuration = cfg.getEngine().createEngineConfiguration();
+
+    // then
+    assertThat(configuration.getMessagesTtlCheckerBatchLimit()).isEqualTo(1000);
+    assertThat(configuration.getMessagesTtlCheckerInterval()).isEqualTo(Duration.ofSeconds(15));
+  }
+}
