y-fedorov/zeebe

refactor: make invalid adn valid processes more explicit in atomic deployments test case
The invalid process in test case shouldDoAtomicDeployments was not trivial to understand. Therefore, I updated the invalid process in a way that it doesn't have a start event and refactor naming of process instance variables as invalid and valid.

engine/src/test/java/io/camunda/zeebe/engine/processing/deployment/DeploymentRejectionTest.java
@@ -265,22 +265,17 @@ public class DeploymentRejectionTest {
   @Test
   public void shouldDoAtomicDeployments() {
     // given
-    final String process1Id = "process1";
-    final BpmnModelInstance process1 =
-        Bpmn.createExecutableProcess(process1Id)
-            .startEvent()
-            .businessRuleTask("invalid_business_rule_task_id")
-            .endEvent()
-            .done();
-    final String process2Id = "process2";
-    final BpmnModelInstance process2 =
-        Bpmn.createExecutableProcess(process2Id).startEvent().task().endEvent().done();
+    final String invalidProcessId = "invalid_process_without_start_event";
+    final BpmnModelInstance invalidProcess = Bpmn.createExecutableProcess(invalidProcessId).done();
+    final String validProcessId = "valid_process";
+    final BpmnModelInstance validProcess =
+        Bpmn.createExecutableProcess(validProcessId).startEvent().task().endEvent().done();
 
     // when
     ENGINE
         .deployment()
-        .withXmlResource(process1)
-        .withXmlResource(process2)
+        .withXmlResource(invalidProcess)
+        .withXmlResource(validProcess)
         .expectRejection()
         .deploy();
 

camunda/camunda

engine/src/test/java/io/camunda/zeebe/engine/processing/deployment/DeploymentRejectionTest.java
@@ -229,22 +229,17 @@ public class DeploymentRejectionTest {
   @Test
   public void shouldDoAtomicDeployments() {
     // given
-    final String process1Id = "process1";
-    final BpmnModelInstance process1 =
-        Bpmn.createExecutableProcess(process1Id)
-            .startEvent()
-            .businessRuleTask("invalid_business_rule_task_id")
-            .endEvent()
-            .done();
-    final String process2Id = "process2";
-    final BpmnModelInstance process2 =
-        Bpmn.createExecutableProcess(process2Id).startEvent().manualTask().endEvent().done();
+    final String invalidProcessId = "invalid_process_without_start_event";
+    final BpmnModelInstance invalidProcess = Bpmn.createExecutableProcess(invalidProcessId).done();
+    final String validProcessId = "valid_process";
+    final BpmnModelInstance validProcess =
+        Bpmn.createExecutableProcess(validProcessId).startEvent().manualTask().endEvent().done();
 
     // when
     ENGINE
         .deployment()
-        .withXmlResource(process1)
-        .withXmlResource(process2)
+        .withXmlResource(invalidProcess)
+        .withXmlResource(validProcess)
         .expectRejection()
         .deploy();
 