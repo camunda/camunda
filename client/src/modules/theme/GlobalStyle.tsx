70fd6d644a4c8f4ef532c2963faef452a410f5dd
Vinícius Goulart <goulart.vs@gmail.com>
2023-03-03 12:57:10 +0100

    chore: Preload main fonts (#2579)

--- client/src/modules/theme/GlobalStyle.tsx
+++ client/src/modules/theme/GlobalStyle.tsx
@@ -18,7 +18,6 @@ const GlobalStyle = createGlobalStyle`
       width: 100%;
       height: 100%;
       overflow: hidden;
-      font-family: IBM Plex Sans;
       margin: 0;
       padding: 0;
     }
