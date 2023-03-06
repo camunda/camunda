716d388a01e50903c6eae048cfb78ed6dfd678a8
Ole Schönburg <ole.schoenburg@gmail.com>
2023-03-06 11:57:51 +0100

    feat(backup/gcs): restore app uses store when configured

--- dist/src/main/java/io/camunda/zeebe/restore/BackupStoreComponent.java
+++ dist/src/main/java/io/camunda/zeebe/restore/BackupStoreComponent.java
@@ -8,12 +8,12 @@
 package io.camunda.zeebe.restore;
 
 import io.camunda.zeebe.backup.api.BackupStore;
+import io.camunda.zeebe.backup.gcs.GcsBackupStore;
 import io.camunda.zeebe.backup.s3.S3BackupConfig;
 import io.camunda.zeebe.backup.s3.S3BackupConfig.Builder;
 import io.camunda.zeebe.backup.s3.S3BackupStore;
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.broker.system.configuration.backup.BackupStoreCfg;
-import io.camunda.zeebe.broker.system.configuration.backup.BackupStoreCfg.BackupStoreType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -35,28 +35,31 @@ final class BackupStoreComponent {
 
   private BackupStore buildBackupStore(final BackupStoreCfg backupCfg) {
     final var store = backupCfg.getStore();
-    if (store == BackupStoreType.NONE) {
-      throw new IllegalArgumentException("No backup store configured, cannot restore from backup.");
-    }
+    return switch (store) {
+      case S3 -> buildS3BackupStore(backupCfg);
+      case GCS -> new GcsBackupStore();
+      case NONE -> throw new IllegalArgumentException(
+          "No backup store configured, cannot restore from backup.");
+    };
+  }
 
-    if (store == BackupStoreType.S3) {
-      final var s3Config = backupCfg.getS3();
-      final S3BackupConfig storeConfig =
-          new Builder()
-              .withBucketName(s3Config.getBucketName())
-              .withEndpoint(s3Config.getEndpoint())
-              .withRegion(s3Config.getRegion())
-              .withCredentials(s3Config.getAccessKey(), s3Config.getSecretKey())
-              .withApiCallTimeout(s3Config.getApiCallTimeout())
-              .forcePathStyleAccess(s3Config.isForcePathStyleAccess())
-              .withCompressionAlgorithm(s3Config.getCompression())
-              .withBasePath(s3Config.getBasePath())
-              .build();
-      return new S3BackupStore(storeConfig);
-    } else {
-      throw new IllegalArgumentException(
-          "The configured backup store type (%s) is unsupported. Cannot restore from backup"
-              .formatted(store));
-    }
+  private static S3BackupStore buildS3BackupStore(final BackupStoreCfg backupStoreCfg) {
+    final var s3Config = backupStoreCfg.getS3();
+    final S3BackupConfig storeConfig =
+        new Builder()
+            .withBucketName(s3Config.getBucketName())
+            .withEndpoint(s3Config.getEndpoint())
+            .withRegion(s3Config.getRegion())
+            .withCredentials(s3Config.getAccessKey(), s3Config.getSecretKey())
+            .withApiCallTimeout(s3Config.getApiCallTimeout())
+            .forcePathStyleAccess(s3Config.isForcePathStyleAccess())
+            .withCompressionAlgorithm(s3Config.getCompression())
+            .withBasePath(s3Config.getBasePath())
+            .build();
+    return new S3BackupStore(storeConfig);
+  }
+
+  private static GcsBackupStore buildGcsBackupStore(final BackupStoreCfg backupStoreCfg) {
+    return new GcsBackupStore();
   }
 }
