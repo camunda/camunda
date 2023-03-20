d3f8303090a48ea670aa3bce335e5c74d81dd6dc
Meggle (Sebastian Bathke) <sebastian.bathke@camunda.com>
2023-03-20 17:42:57 +0100

    test(auth): generalised keycloak setup

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/gateway/GatewayAuthenticationIdentityIT.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/gateway/GatewayAuthenticationIdentityIT.java
@@ -10,9 +10,9 @@ package io.camunda.zeebe.it.gateway;
 import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import dasniko.testcontainers.keycloak.KeycloakContainer;
 import io.camunda.zeebe.client.CredentialsProvider;
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.ZeebeClientBuilder;
 import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
@@ -21,7 +21,7 @@ import io.grpc.Metadata;
 import io.grpc.Metadata.Key;
 import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
-import io.zeebe.containers.cluster.ZeebeCluster;
+import io.zeebe.containers.ZeebeContainer;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -46,30 +46,35 @@ public class GatewayAuthenticationIdentityIT {
   public static final String KEYCLOAK_PASSWORD = "admin";
   public static final BpmnModelInstance PROCESS_MODEL =
       Bpmn.createExecutableProcess("model").startEvent().endEvent().done();
-  private static final String KEYCLOAK_CONTEXT_PATH = "/auth";
-  private static final String KEYCLOAK_PATH_CAMUNDA_REALM =
-      KEYCLOAK_CONTEXT_PATH + "/realms/camunda-platform";
+  private static final String KEYCLOAK_PATH_CAMUNDA_REALM = "/realms/camunda-platform";
   private static final String ZEEBE_CLIENT_ID = "zeebe";
   private static final String ZEEBE_CLIENT_AUDIENCE = "zeebe-api";
   private static final String ZEEBE_CLIENT_SECRET = "zecret";
   private static final Network NETWORK = Network.newNetwork();
 
   @Container
-  private static final KeycloakContainer KEYCLOAK =
-      new KeycloakContainer("quay.io/keycloak/keycloak:19.0")
-          .withContextPath(KEYCLOAK_CONTEXT_PATH)
-          .withAdminUsername(KEYCLOAK_USER)
-          .withAdminPassword(KEYCLOAK_PASSWORD)
+  private static final GenericContainer KEYCLOAK =
+      new GenericContainer<>("quay.io/keycloak/keycloak:19.0")
+          .withEnv("KC_HEALTH_ENABLED", "true")
+          .withEnv("KEYCLOAK_ADMIN", KEYCLOAK_USER)
+          .withEnv("KEYCLOAK_ADMIN_PASSWORD", KEYCLOAK_PASSWORD)
           .withNetwork(NETWORK)
           .withNetworkAliases("keycloak")
-          .withExposedPorts(8080);
+          .withExposedPorts(8080)
+          .withCommand("start-dev")
+          .waitingFor(
+              new HttpWaitStrategy()
+                  .forPort(8080)
+                  .forPath("/health/ready")
+                  .allowInsecure()
+                  .forStatusCode(200));
 
   @Container
   private static final GenericContainer<?> IDENTITY =
       new GenericContainer<>(
               DockerImageName.parse("camunda/identity").withTag(getIdentityImageTag()))
           .dependsOn(KEYCLOAK)
-          .withEnv("KEYCLOAK_URL", "http://keycloak:8080" + KEYCLOAK_CONTEXT_PATH)
+          .withEnv("KEYCLOAK_URL", "http://keycloak:8080")
           .withEnv(
               "IDENTITY_AUTH_PROVIDER_BACKEND_URL",
               "http://keycloak:8080" + KEYCLOAK_PATH_CAMUNDA_REALM)
@@ -95,32 +100,22 @@ public class GatewayAuthenticationIdentityIT {
           .withNetworkAliases("identity");
 
   @Container
-  private final ZeebeCluster cluster =
-      ZeebeCluster.builder()
-          .withBrokersCount(1)
-          .withPartitionsCount(1)
-          .withGatewaysCount(0)
-          .withEmbeddedGateway(true)
-          .withImage(ZeebeTestContainerDefaults.defaultTestImage())
+  private static final ZeebeContainer ZEEBE =
+      new ZeebeContainer(ZeebeTestContainerDefaults.defaultTestImage())
           .withNetwork(NETWORK)
-          .withGatewayConfig(
-              zeebeGatewayNode ->
-                  zeebeGatewayNode
-                      // as topology check would require auth, we skip it here
-                      .withoutTopologyCheck()
-                      .withEnv("ZEEBE_BROKER_GATEWAY_SECURITY_AUTHENTICATION_MODE", "identity")
-                      .withEnv(
-                          "ZEEBE_BROKER_GATEWAY_SECURITY_AUTHENTICATION_IDENTITY_ISSUERBACKENDURL",
-                          "http://keycloak:8080" + KEYCLOAK_PATH_CAMUNDA_REALM)
-                      .withEnv(
-                          "ZEEBE_BROKER_GATEWAY_SECURITY_AUTHENTICATION_IDENTITY_AUDIENCE",
-                          ZEEBE_CLIENT_AUDIENCE))
-          .build();
+          .withoutTopologyCheck()
+          .withEnv("ZEEBE_BROKER_GATEWAY_SECURITY_AUTHENTICATION_MODE", "identity")
+          .withEnv(
+              "ZEEBE_BROKER_GATEWAY_SECURITY_AUTHENTICATION_IDENTITY_ISSUERBACKENDURL",
+              "http://keycloak:8080" + KEYCLOAK_PATH_CAMUNDA_REALM)
+          .withEnv(
+              "ZEEBE_BROKER_GATEWAY_SECURITY_AUTHENTICATION_IDENTITY_AUDIENCE",
+              ZEEBE_CLIENT_AUDIENCE);
 
   @Test
   void deployModelFailsWithoutAuthToken() {
     // given
-    try (final var client = createAnonymousZeebeClient()) {
+    try (final var client = createZeebeClientBuilder().build()) {
       // when
       final var deploymentFuture =
           client
@@ -151,11 +146,7 @@ public class GatewayAuthenticationIdentityIT {
   void deployModelFailsWithInvalidAuthToken() {
     // given
     try (final var client =
-        cluster
-            .newClientBuilder()
-            .defaultRequestTimeout(Duration.ofMinutes(1))
-            .credentialsProvider(new InvalidAuthTokenProvider())
-            .build()) {
+        createZeebeClientBuilder().credentialsProvider(new InvalidAuthTokenProvider()).build()) {
       // when
       final var deploymentFuture =
           client
@@ -187,9 +178,7 @@ public class GatewayAuthenticationIdentityIT {
     awaitCamundaRealmAvailabilityOnKeycloak();
 
     try (final var client =
-        cluster
-            .newClientBuilder()
-            .defaultRequestTimeout(Duration.ofMinutes(1))
+        createZeebeClientBuilder()
             .credentialsProvider(
                 new OAuthCredentialsProviderBuilder()
                     .clientId(ZEEBE_CLIENT_ID)
@@ -236,8 +225,11 @@ public class GatewayAuthenticationIdentityIT {
     return "http://localhost:" + KEYCLOAK.getFirstMappedPort() + KEYCLOAK_PATH_CAMUNDA_REALM;
   }
 
-  private ZeebeClient createAnonymousZeebeClient() {
-    return cluster.newClientBuilder().defaultRequestTimeout(Duration.ofMinutes(1)).build();
+  private ZeebeClientBuilder createZeebeClientBuilder() {
+    return ZeebeClient.newClientBuilder()
+        .gatewayAddress(ZEEBE.getExternalGatewayAddress())
+        .defaultRequestTimeout(Duration.ofMinutes(1))
+        .usePlaintext();
   }
 
   private static String getIdentityImageTag() {
