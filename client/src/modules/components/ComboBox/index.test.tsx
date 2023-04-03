cae495908c1ad89d8116e191d48ed8b762d3032e
Patrick Dehn <pedesen@users.noreply.github.com>
2023-04-03 14:02:23 +0200

    chore: add ComboBox component (#4324)

---
+++ client/src/modules/components/ComboBox/index.test.tsx
@@ -0,0 +1,88 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+
+import {render, screen} from 'modules/testing-library';
+import {ComboBox} from '.';
+
+const items = [
+  {
+    id: 'process1',
+    label: 'Process One',
+  },
+  {
+    id: 'process2',
+    label: 'Process Two',
+  },
+  {
+    id: 'process3',
+    label: 'Process Three',
+  },
+];
+
+describe('<ComboBox />', () => {
+  it('should be disabled when there are no items', () => {
+    render(
+      <ComboBox
+        id="process-name"
+        titleText="Process"
+        items={[]}
+        value=""
+        onChange={() => {}}
+      />
+    );
+
+    expect(screen.getByLabelText('Process')).toBeDisabled();
+  });
+
+  it('should initially select item', () => {
+    render(
+      <ComboBox
+        id="process-name"
+        titleText="Process"
+        items={items}
+        value="process2"
+        onChange={() => {}}
+      />
+    );
+
+    expect(screen.getByLabelText('Process')).toHaveValue('Process Two');
+  });
+
+  it('should filter items', async () => {
+    const {user} = render(
+      <ComboBox
+        id="process-name"
+        titleText="Process"
+        items={items}
+        value=""
+        onChange={() => {}}
+      />
+    );
+
+    await user.type(screen.getByLabelText('Process'), 'ONE');
+    expect(screen.getAllByRole('option')).toHaveLength(1);
+    expect(screen.getAllByRole('option')[0]?.textContent).toEqual(
+      'Process One'
+    );
+
+    await user.click(screen.getByRole('button', {name: 'Clear selected item'}));
+
+    await user.type(screen.getByLabelText('Process'), 'process t');
+    expect(screen.getAllByRole('option')).toHaveLength(2);
+    expect(screen.getAllByRole('option')[0]?.textContent).toEqual(
+      'Process Two'
+    );
+    expect(screen.getAllByRole('option')[1]?.textContent).toEqual(
+      'Process Three'
+    );
+
+    await user.click(screen.getByRole('button', {name: 'Clear selected item'}));
+
+    await user.type(screen.getByLabelText('Process'), 'unknown');
+    expect(screen.queryByRole('option')).not.toBeInTheDocument();
+  });
+});
---
+++ client/src/modules/components/ComboBox/index.tsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+
+import React from 'react';
+import {observer} from 'mobx-react';
+import {ComboBox as BaseComboBox} from '@carbon/react';
+
+type Item = {id: string; label: string};
+type Props = {
+  id: string;
+  value: string;
+  titleText: string;
+  onChange: (data: {[selectedItem: string]: Item | null | undefined}) => void;
+  items: Item[];
+};
+
+const ComboBox: React.FC<Props> = observer(
+  ({id, items, onChange, value, ...props}) => {
+    const getItemById = (id: string) => {
+      return items.find((item) => item.id === id);
+    };
+
+    return (
+      <BaseComboBox
+        id={id}
+        items={items}
+        onChange={onChange}
+        selectedItem={getItemById(value) ?? null}
+        disabled={items.length === 0}
+        shouldFilterItem={(data) => {
+          const {inputValue, item} = data;
+          return (
+            inputValue !== undefined &&
+            item.label.toLowerCase().includes(inputValue?.toLowerCase())
+          );
+        }}
+        {...props}
+      />
+    );
+  }
+);
+
+export {ComboBox};
