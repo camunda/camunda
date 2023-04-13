347a9299354a22a37bcc1953d9b0aae2631e9fe6
Ole Schönburg <ole.schoenburg@gmail.com>
2023-04-13 14:24:12 +0200

    feat: scheduled tasks can access `TimerInstanceState`
    This state is used by the timer due date checker scheduled task and
    should not be shared with regular processing.
    
    (cherry picked from commit 79ed91dd9c6916d79e478551fbc53f66acf4e8b3)

--- engine/src/main/java/io/camunda/zeebe/engine/state/ScheduledTaskDbState.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/ScheduledTaskDbState.java
@@ -10,18 +10,26 @@ package io.camunda.zeebe.engine.state;
 import io.camunda.zeebe.db.TransactionContext;
 import io.camunda.zeebe.db.ZeebeDb;
 import io.camunda.zeebe.engine.state.immutable.MessageState;
+import io.camunda.zeebe.engine.state.immutable.TimerInstanceState;
+import io.camunda.zeebe.engine.state.instance.DbTimerInstanceState;
 import io.camunda.zeebe.engine.state.message.DbMessageState;
 
 /** Contains read-only state that can be accessed safely by scheduled tasks. */
 public final class ScheduledTaskDbState {
   private final MessageState messageState;
+  private final TimerInstanceState timerInstanceState;
 
   public ScheduledTaskDbState(
       final ZeebeDb<ZbColumnFamilies> zeebeDb, final TransactionContext transactionContext) {
     messageState = new DbMessageState(zeebeDb, transactionContext);
+    timerInstanceState = new DbTimerInstanceState(zeebeDb, transactionContext);
   }
 
   public MessageState getMessageState() {
     return messageState;
+  }
+
+  public TimerInstanceState getTimerState() {
+    return timerInstanceState;
   }
 }
