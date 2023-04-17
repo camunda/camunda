945d3c6a9947515744cb6fe660e149e1a73abe96
Hüsna Uygur <45518829+huygur@users.noreply.github.com>
2023-04-17 16:06:45 +0200

    style: create general layout for carbon (#4376)
    * style: create general layout for carbon
    
    * refactor: remove unnecessary container

--- client/src/App/Carbon/Layout/index.tsx
+++ client/src/App/Carbon/Layout/index.tsx
@@ -5,40 +5,19 @@
  * except in compliance with the proprietary license.
  */
 
-import {Outlet, useMatch} from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import {AppHeader} from './AppHeader';
-import {Footer, Grid, PageContent} from './styled';
-import {Copyright} from 'modules/components/Copyright';
-import {CarbonPaths} from 'modules/carbonRoutes';
-import {modificationsStore} from 'modules/stores/modifications';
+import {PageContent} from './styled';
 import {observer} from 'mobx-react';
-import {OperationsPanel} from 'modules/components/OperationsPanel';
 
 const Layout: React.FC = observer(() => {
-  const instancesMatch = useMatch(CarbonPaths.processes());
-  const decisionsMatch = useMatch(CarbonPaths.decisions());
-  const dashboardMatch = useMatch(CarbonPaths.dashboard());
-  const showFooter =
-    instancesMatch === null &&
-    decisionsMatch === null &&
-    !modificationsStore.isModificationModeEnabled;
-  const showOperationsPanel =
-    instancesMatch !== null || decisionsMatch !== null;
-
   return (
-    <Grid numberOfRows={showFooter ? 3 : 2}>
+    <>
       <AppHeader />
-      {showOperationsPanel && <OperationsPanel />}
       <PageContent>
-        <div>this is the carbon layout</div>
         <Outlet />
       </PageContent>
-      {showFooter && (
-        <Footer variant={dashboardMatch === null ? 'default' : 'dashboard'}>
-          <Copyright />
-        </Footer>
-      )}
-    </Grid>
+    </>
   );
 });
 
--- client/src/App/Carbon/Layout/styled.tsx
+++ client/src/App/Carbon/Layout/styled.tsx
@@ -5,58 +5,11 @@
  * except in compliance with the proprietary license.
  */
 
-import {FOOTER_HEIGHT, PAGE_TOP_PADDING} from 'modules/constants';
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 
-type GridProps = {
-  numberOfRows?: 2 | 3;
-};
-
-const Grid = styled.div<GridProps>`
-  ${({numberOfRows}) => {
-    return css`
-      width: 100%;
-      height: 100%;
-      display: grid;
-      grid-template-columns: 100%;
-      grid-template-rows: ${numberOfRows === 2
-        ? css`100%`
-        : css`calc(100% - ${FOOTER_HEIGHT}px) ${FOOTER_HEIGHT}px`};
-    `;
-  }}
+const PageContent = styled.main`
+  padding-top: var(--cds-spacing-09);
+  height: 100%;
 `;
 
-const PageContent = styled.div`
-  padding-top: ${PAGE_TOP_PADDING}px;
-`;
-
-type Props = {
-  variant?: 'dashboard' | 'default';
-};
-
-const Footer = styled.div<Props>`
-  ${({theme, variant = 'default'}) => {
-    const colors = theme.colors.layout;
-
-    return css`
-      padding: 12px 20px 11px;
-      text-align: right;
-      display: flex;
-      align-items: flex-end;
-
-      ${variant === 'dashboard'
-        ? css`
-            background-color: ${colors.dashboard.backgroundColor};
-          `
-        : ''}
-      ${variant === 'default'
-        ? css`
-            border-top: solid 1px ${theme.colors.borderColor};
-            background-color: ${colors.default.backgroundColor};
-          `
-        : ''}
-    `;
-  }}
-`;
-
-export {Grid, PageContent, Footer};
+export {PageContent};
