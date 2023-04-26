0e3062b1066bb22591239fb114b70c8932ceb905
Remco Westerhoud <remco@westerhoud.nl>
2023-04-26 11:56:55 +0200

    feat(engine): implement TerminateProcessInstanceBatchProcessor
    Adds the implementation to the processor. The processor will iterate over the children of the parent.
    
    It will check if it is able to write this command and a follow-up batch command.
    If possible it will write the TERMINATE command for the child.
    It returns true to indicate we should continue with the next child.
    
    If it is unable to write both commands, we only write the follow-up batch command.
    We set the index of this command to the element instance key of this child.
    This is to indicate that we should start at this child in the next batch.
    It returns false to indicate we should stop here, and not proceed with the next child.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/TerminateProcessInstanceBatchProcessor.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/TerminateProcessInstanceBatchProcessor.java
@@ -11,7 +11,10 @@ import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessor;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.immutable.ElementInstanceState;
+import io.camunda.zeebe.engine.state.instance.ElementInstance;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceBatchRecord;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceBatchIntent;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
 import io.camunda.zeebe.stream.api.state.KeyGenerator;
 
@@ -32,5 +35,46 @@ public final class TerminateProcessInstanceBatchProcessor
   }
 
   @Override
-  public void processRecord(final TypedRecord<ProcessInstanceBatchRecord> record) {}
+  public void processRecord(final TypedRecord<ProcessInstanceBatchRecord> record) {
+    final var recordValue = record.getValue();
+
+    elementInstanceState.forEachChild(
+        recordValue.getBatchElementInstanceKey(),
+        recordValue.getIndex(),
+        (childKey, childInstance) -> {
+          if (canWriteCommand(record, childInstance)) {
+            terminateChildInstance(childInstance);
+            return true;
+          } else {
+            final var nextBatchRecord =
+                new ProcessInstanceBatchRecord()
+                    .setProcessInstanceKey(recordValue.getProcessInstanceKey())
+                    .setBatchElementInstanceKey(recordValue.getBatchElementInstanceKey())
+                    .setIndex(childKey);
+            final long key = keyGenerator.nextKey();
+            commandWriter.appendFollowUpCommand(
+                key, ProcessInstanceBatchIntent.TERMINATE, nextBatchRecord);
+            return false;
+          }
+        });
+  }
+
+  private boolean canWriteCommand(
+      final TypedRecord<ProcessInstanceBatchRecord> record, final ElementInstance childInstance) {
+    // We must have space in the batch to write both the TERMINATE command as the potential
+    // follow-up batch command. An excessive 8Kb is added to account for metadata. This is way
+    // more than will be necessary.
+    final var expectedCommandLength =
+        childInstance.getValue().getLength() + record.getLength() + (1024 * 8);
+    return commandWriter.canWriteCommandOfLength(expectedCommandLength);
+  }
+
+  private void terminateChildInstance(final ElementInstance childInstance) {
+    if (childInstance.canTerminate()) {
+      commandWriter.appendFollowUpCommand(
+          childInstance.getKey(),
+          ProcessInstanceIntent.TERMINATE_ELEMENT,
+          childInstance.getValue());
+    }
+  }
 }
