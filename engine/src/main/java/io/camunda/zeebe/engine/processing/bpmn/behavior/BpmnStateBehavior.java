1687455b1f95ff5742e25e4e235c97762174cebb
lzgabel <lz19960321lz@gmail.com>
2023-05-03 01:59:38 +0800

    Revert "refactor(engine): delete unused method"
    This reverts commit 0f3f458221a72b2fae27e9a1f5bc7dad890c4395.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnStateBehavior.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnStateBehavior.java
@@ -21,6 +21,7 @@ import io.camunda.zeebe.engine.state.instance.ElementInstance;
 import io.camunda.zeebe.protocol.record.value.BpmnElementType;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import org.agrona.DirectBuffer;
 
 public final class BpmnStateBehavior {
@@ -96,6 +97,15 @@ public final class BpmnStateBehavior {
 
   public ElementInstance getFlowScopeInstance(final BpmnElementContext context) {
     return elementInstanceState.getInstance(context.getFlowScopeKey());
+  }
+
+  public List<BpmnElementContext> getChildInstances(final BpmnElementContext context) {
+    return elementInstanceState.getChildren(context.getElementInstanceKey()).stream()
+        .map(
+            childInstance ->
+                context.copy(
+                    childInstance.getKey(), childInstance.getValue(), childInstance.getState()))
+        .collect(Collectors.toList());
   }
 
   public BpmnElementContext getFlowScopeContext(final BpmnElementContext context) {
