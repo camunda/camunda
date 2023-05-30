1a57d779d243f2132c20c71b36f1b5d379934796
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2023-05-30 09:08:50 +0200

    feat: add configuration timeout for install request
    (cherry picked from commit 7aeb14bf1afd4b9b47f92c221494265b4033a241)

--- broker/src/main/java/io/camunda/zeebe/broker/partitioning/RaftPartitionGroupFactory.java
+++ broker/src/main/java/io/camunda/zeebe/broker/partitioning/RaftPartitionGroupFactory.java
@@ -82,6 +82,7 @@ public final class RaftPartitionGroupFactory {
             .withElectionTimeout(clusterCfg.getElectionTimeout())
             .withHeartbeatInterval(clusterCfg.getHeartbeatInterval())
             .withRequestTimeout(experimentalCfg.getRaft().getRequestTimeout())
+            .withSnapshotRequestTimeout(experimentalCfg.getRaft().getSnapshotRequestTimeout())
             .withMaxQuorumResponseTimeout(experimentalCfg.getRaft().getMaxQuorumResponseTimeout())
             .withMinStepDownFailureCount(experimentalCfg.getRaft().getMinStepDownFailureCount())
             .withPreferSnapshotReplicationThreshold(
