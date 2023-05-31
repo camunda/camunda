7ed16e287a32e89046d28c1a3afea9550ccbed79
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2023-05-31 14:19:09 +0200

    fix: address comments

--- atomix/cluster/src/main/java/io/atomix/raft/partition/RaftPartitionConfig.java
+++ atomix/cluster/src/main/java/io/atomix/raft/partition/RaftPartitionConfig.java
@@ -22,7 +22,6 @@ import java.time.Duration;
 public class RaftPartitionConfig {
 
   private static final Duration DEFAULT_ELECTION_TIMEOUT = Duration.ofMillis(2500);
-  private static final Duration DEFAULT_SNAPSHOT_REQUEST_TIMEOUT = Duration.ofSeconds(5);
   private static final Duration DEFAULT_HEARTBEAT_INTERVAL = Duration.ofMillis(250);
   private static final boolean DEFAULT_PRIORITY_ELECTION = true;
   private static final Duration DEFAULT_REQUEST_TIMEOUT = Duration.ofSeconds(5);
@@ -38,7 +37,7 @@ public class RaftPartitionConfig {
   private int maxAppendBatchSize = 32 * 1024;
   private boolean priorityElectionEnabled = DEFAULT_PRIORITY_ELECTION;
   private Duration requestTimeout = DEFAULT_REQUEST_TIMEOUT;
-  private Duration snapshotRequestTimeout = DEFAULT_SNAPSHOT_REQUEST_TIMEOUT;
+  private Duration snapshotRequestTimeout = DEFAULT_REQUEST_TIMEOUT;
   private int minStepDownFailureCount = DEFAULT_MIN_STEP_DOWN_FAILURE_COUNT;
   private Duration maxQuorumResponseTimeout = DEFAULT_MAX_QUORUM_RESPONSE_TIMEOUT;
   private PartitionDistributor partitionDistributor = DEFAULT_PARTITION_DISTRIBUTOR;
