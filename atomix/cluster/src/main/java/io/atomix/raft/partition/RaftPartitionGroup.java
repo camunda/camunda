mvawork/zeebe

feat: add configuration timeout for install request

atomix/cluster/src/main/java/io/atomix/raft/partition/RaftPartitionGroup.java
@@ -416,6 +416,17 @@ public final class RaftPartitionGroup implements ManagedPartitionGroup {
     }
 
     /**
+     * Sets the install request timeout for all messages sent between raft replicas.
+     *
+     * @param installRequestTimeout the timeout
+     * @return the Raft Partition group builder
+     */
+    public Builder withInstallRequestTimeout(final Duration installRequestTimeout) {
+      config.getPartitionConfig().setInstallRequestTimeout(installRequestTimeout);
+      return this;
+    }
+
+    /**
      * If the leader is not able to reach the quorum, the leader may step down. This is triggered
      * after minStepDownFailureCount number of requests fails to get a response from the quorum of
      * followers as well as if the last response was received before maxQuorumResponseTime.

broker/src/main/java/io/camunda/zeebe/broker/partitioning/RaftPartitionGroupFactory.java
@@ -82,6 +82,7 @@ public final class RaftPartitionGroupFactory {
             .withElectionTimeout(clusterCfg.getElectionTimeout())
             .withHeartbeatInterval(clusterCfg.getHeartbeatInterval())
             .withRequestTimeout(experimentalCfg.getRaft().getRequestTimeout())
+            .withInstallRequestTimeout(experimentalCfg.getRaft().getInstallRequestTimeout())
             .withMaxQuorumResponseTimeout(experimentalCfg.getRaft().getMaxQuorumResponseTimeout())
             .withMinStepDownFailureCount(experimentalCfg.getRaft().getMinStepDownFailureCount())
             .withPreferSnapshotReplicationThreshold(

camunda/camunda

(cherry picked from commit 7aeb14bf1afd4b9b47f92c221494265b4033a241)

atomix/cluster/src/main/java/io/atomix/raft/partition/RaftPartitionGroup.java
@@ -423,6 +423,17 @@ public final class RaftPartitionGroup implements ManagedPartitionGroup {
     }
 
     /**
+     * Sets the snapshot request timeout for all messages sent between raft replicas.
+     *
+     * @param snapshotRequestTimeout the timeout
+     * @return the Raft Partition group builder
+     */
+    public Builder withSnapshotRequestTimeout(final Duration snapshotRequestTimeout) {
+      config.getPartitionConfig().setSnapshotRequestTimeout(snapshotRequestTimeout);
+      return this;
+    }
+
+    /**
      * If the leader is not able to reach the quorum, the leader may step down. This is triggered
      * after minStepDownFailureCount number of requests fails to get a response from the quorum of
      * followers as well as if the last response was received before maxQuorumResponseTime.

broker/src/main/java/io/camunda/zeebe/broker/partitioning/RaftPartitionGroupFactory.java
@@ -74,6 +74,7 @@ public final class RaftPartitionGroupFactory {
             .withElectionTimeout(clusterCfg.getElectionTimeout())
             .withHeartbeatInterval(clusterCfg.getHeartbeatInterval())
             .withRequestTimeout(experimentalCfg.getRaft().getRequestTimeout())
+            .withSnapshotRequestTimeout(experimentalCfg.getRaft().getSnapshotRequestTimeout())
             .withMaxQuorumResponseTimeout(experimentalCfg.getRaft().getMaxQuorumResponseTimeout())
             .withMinStepDownFailureCount(experimentalCfg.getRaft().getMinStepDownFailureCount())
             .withPreferSnapshotReplicationThreshold(