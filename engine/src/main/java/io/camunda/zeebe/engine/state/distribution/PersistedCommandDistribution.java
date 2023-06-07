3948923f7d572c92977e5fd91e88ebb1d9758612
Nico Korthout <nico.korthout@camunda.com>
2023-06-07 19:10:21 +0200

    refactor: add intent to persisted command distribution
    We want to store the intent in the state, so we can read it later in the
    Command Redistributor.
    
    The wrap method is only called from the DbDistributionState when a
    command distribution is added to the state, which is only done as part
    of the STARTED event applier. The STARTED event has the `intent` filled
    in already.

--- engine/src/main/java/io/camunda/zeebe/engine/state/distribution/PersistedCommandDistribution.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/distribution/PersistedCommandDistribution.java
@@ -10,26 +10,32 @@ package io.camunda.zeebe.engine.state.distribution;
 import io.camunda.zeebe.db.DbValue;
 import io.camunda.zeebe.msgpack.UnpackedObject;
 import io.camunda.zeebe.msgpack.property.EnumProperty;
+import io.camunda.zeebe.msgpack.property.IntegerProperty;
 import io.camunda.zeebe.msgpack.property.ObjectProperty;
 import io.camunda.zeebe.protocol.impl.record.UnifiedRecordValue;
 import io.camunda.zeebe.protocol.impl.record.value.distribution.CommandDistributionRecord;
 import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.Intent;
 import org.agrona.concurrent.UnsafeBuffer;
 
 public class PersistedCommandDistribution extends UnpackedObject implements DbValue {
 
   private final EnumProperty<ValueType> valueTypeProperty =
       new EnumProperty<>("valueType", ValueType.class);
+  private final IntegerProperty intentProperty = new IntegerProperty("intent", Intent.NULL_VAL);
   private final ObjectProperty<UnifiedRecordValue> commandValueProperty =
       new ObjectProperty<>("commandValue", new UnifiedRecordValue());
 
   public PersistedCommandDistribution() {
-    declareProperty(valueTypeProperty).declareProperty(commandValueProperty);
+    declareProperty(valueTypeProperty)
+        .declareProperty(intentProperty)
+        .declareProperty(commandValueProperty);
   }
 
   public PersistedCommandDistribution wrap(
       final CommandDistributionRecord commandDistributionRecord) {
     valueTypeProperty.setValue(commandDistributionRecord.getValueType());
+    intentProperty.setValue(commandDistributionRecord.getIntent().value());
 
     final var commandValue = commandDistributionRecord.getCommandValue();
     final var valueBuffer = new UnsafeBuffer(0, 0);
@@ -43,6 +49,17 @@ public class PersistedCommandDistribution extends UnpackedObject implements DbVa
 
   public ValueType getValueType() {
     return valueTypeProperty.getValue();
+  }
+
+  public Intent getIntent() {
+    final int intentValue = intentProperty.getValue();
+    if (intentValue < 0 || intentValue > Short.MAX_VALUE) {
+      throw new IllegalStateException(
+          String.format(
+              "Expected to read the intent, but it's persisted value '%d' is not a short integer",
+              intentValue));
+    }
+    return Intent.fromProtocolValue(getValueType(), (short) intentValue);
   }
 
   public UnifiedRecordValue getCommandValue() {
