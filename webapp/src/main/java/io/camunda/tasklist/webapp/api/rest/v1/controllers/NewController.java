97e001d70bcfba1df03f933b361d23bdb1949ddb
Marcos Vieira <122778055+marcosgvieira@users.noreply.github.com>
2023-06-09 23:56:18 +0100

    fix: using forward (#3084)
    * Revert "fix: SaaS public Forms (#3082)"
    
    This reverts commit 7dcd8a94f6faf9a8247e1605a7ee94ecbe7d1b4c.
    
    * Revert "fix: public forms without authentication (#3079)"
    
    This reverts commit 7f8c2e89fcb9d31629b7be3adb89755161d79aca.
    
    * fix: public form using controller
    
    * fixing docker versions
    
    * fix: using forward

--- webapp/src/main/java/io/camunda/tasklist/webapp/api/rest/v1/controllers/NewController.java
+++ webapp/src/main/java/io/camunda/tasklist/webapp/api/rest/v1/controllers/NewController.java
@@ -7,6 +7,16 @@
 package io.camunda.tasklist.webapp.api.rest.v1.controllers;
 
 import io.camunda.tasklist.webapp.security.TasklistURIs;
+import jakarta.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +26,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = TasklistURIs.START_PUBLIC_PROCESS)
 public class NewController {
 
+  @Autowired private ServletContext context;
+
   @GetMapping("/{bpmnProcessId}")
-  public String index(@PathVariable String bpmnProcessId) {
-    return "index";
+  public ModelAndView index(Model model, @PathVariable String bpmnProcessId) {
+    final ModelAndView modelAndView = new ModelAndView("forward:/index.html");
+    modelAndView.setStatus(HttpStatus.OK);
+
+    return modelAndView;
+
   }
 }
