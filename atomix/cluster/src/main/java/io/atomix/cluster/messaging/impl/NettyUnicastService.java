0c7797df9b70b41710da9ffa20fadebb7817ba66
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-06-12 14:10:00 +0200

    fix(atomix): only bind to wildcard interface
    When using the SO_BROADCAST flag, Without root privilege, it's
    not possible to bind to any address but the wildcard address. It also
    does not make sense to support binding to multiple addresses if we're
    going to bind to 0.0.0.0 anyway.
    
    Code is simplified to support that, with improved logging, and also
    respecting the shutdown time outs configuration.

--- atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyUnicastService.java
+++ atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyUnicastService.java
@@ -18,7 +18,6 @@ package io.atomix.cluster.messaging.impl;
 
 import static io.atomix.utils.concurrent.Threads.namedThreads;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import io.atomix.cluster.impl.AddressSerializer;
 import io.atomix.cluster.messaging.ManagedUnicastService;
@@ -40,12 +39,14 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.DatagramChannel;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.util.concurrent.Future;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 import org.slf4j.Logger;
@@ -65,20 +66,27 @@ public class NettyUnicastService implements ManagedUnicastService {
 
   private final Logger log = LoggerFactory.getLogger(getClass());
 
-  private final Address address;
+  private final Address advertisedAddress;
   private final MessagingConfig config;
+  private final int preamble;
   private final Map<String, Map<BiConsumer<Address, byte[]>, Executor>> listeners =
       Maps.newConcurrentMap();
   private final AtomicBoolean started = new AtomicBoolean();
+  private final Address bindAddress;
+
   private EventLoopGroup group;
   private DatagramChannel channel;
-  private final int preamble;
 
   public NettyUnicastService(
-      final String clusterId, final Address address, final MessagingConfig config) {
-    this.address = address;
+      final String clusterId, final Address advertisedAddress, final MessagingConfig config) {
+    this.advertisedAddress = advertisedAddress;
     this.config = config;
     preamble = clusterId.hashCode();
+
+    // as we use SO_BROADCAST, it's only possible to bind to wildcard without root privilege, so we
+    // don't support binding to multiple interfaces here; wouldn't make sense anyway
+    final var port = config.getPort() != null ? config.getPort() : advertisedAddress.port();
+    bindAddress = Address.from("0.0.0.0", port);
   }
 
   @Override
@@ -95,7 +103,7 @@ public class NettyUnicastService implements ManagedUnicastService {
       return;
     }
 
-    final Message message = new Message(this.address, subject, payload);
+    final Message message = new Message(this.advertisedAddress, subject, payload);
     final byte[] bytes = SERIALIZER.encode(message);
     final ByteBuf buf = channel.alloc().buffer(Integer.BYTES + Integer.BYTES + bytes.length);
     buf.writeInt(preamble);
@@ -107,7 +115,7 @@ public class NettyUnicastService implements ManagedUnicastService {
   @Override
   public synchronized void addListener(
       final String subject, final BiConsumer<Address, byte[]> listener, final Executor executor) {
-    listeners.computeIfAbsent(subject, s -> Maps.newConcurrentMap()).put(listener, executor);
+    listeners.computeIfAbsent(subject, s -> new ConcurrentHashMap<>()).put(listener, executor);
   }
 
   @Override
@@ -131,8 +139,7 @@ public class NettyUnicastService implements ManagedUnicastService {
                 new SimpleChannelInboundHandler<DatagramPacket>() {
                   @Override
                   protected void channelRead0(
-                      final ChannelHandlerContext context, final DatagramPacket packet)
-                      throws Exception {
+                      final ChannelHandlerContext context, final DatagramPacket packet) {
                     handleReceivedPacket(packet);
                   }
                 })
@@ -163,66 +170,38 @@ public class NettyUnicastService implements ManagedUnicastService {
     }
   }
 
-  /**
-   * Binds the given bootstrap to the appropriate interfaces.
-   *
-   * @param bootstrap the bootstrap to bind
-   * @return a future to be completed once the bootstrap has been bound to all interfaces
-   */
   private CompletableFuture<Void> bind(final Bootstrap bootstrap) {
     final CompletableFuture<Void> future = new CompletableFuture<>();
-    final int port = config.getPort() != null ? config.getPort() : address.port();
-    if (config.getInterfaces().isEmpty()) {
-      bind(bootstrap, Lists.newArrayList("0.0.0.0").iterator(), port, future);
-    } else {
-      bind(bootstrap, config.getInterfaces().iterator(), port, future);
-    }
-    return future;
-  }
+    bootstrap
+        .bind(bindAddress.host(), bindAddress.port())
+        .addListener(
+            (ChannelFutureListener)
+                f -> {
+                  if (!f.isSuccess()) {
+                    future.completeExceptionally(f.cause());
+                    return;
+                  }
 
-  /**
-   * Recursively binds the given bootstrap to the given interfaces.
-   *
-   * @param bootstrap the bootstrap to bind
-   * @param ifaces an iterator of interfaces to which to bind
-   * @param port the port to which to bind
-   * @param future the future to completed once the bootstrap has been bound to all provided
-   *     interfaces
-   */
-  private void bind(
-      final Bootstrap bootstrap,
-      final Iterator<String> ifaces,
-      final int port,
-      final CompletableFuture<Void> future) {
-    if (ifaces.hasNext()) {
-      final String iface = ifaces.next();
-      bootstrap
-          .bind(iface, port)
-          .addListener(
-              (ChannelFutureListener)
-                  f -> {
-                    if (f.isSuccess()) {
-                      log.info("UDP server listening for connections on {}:{}", iface, port);
-                      channel = (DatagramChannel) f.channel();
-                      bind(bootstrap, ifaces, port, future);
-                    } else {
-                      log.warn(
-                          "Failed to bind TCP server to port {}:{} due to {}",
-                          iface,
-                          port,
-                          f.cause());
-                      future.completeExceptionally(f.cause());
-                    }
-                  });
-    } else {
-      future.complete(null);
-    }
+                  channel = (DatagramChannel) f.channel();
+                  future.complete(null);
+                });
+
+    return future;
   }
 
   @Override
   public CompletableFuture<UnicastService> start() {
     group = new NioEventLoopGroup(0, namedThreads("netty-unicast-event-nio-client-%d", log));
-    return bootstrap().thenRun(() -> started.set(true)).thenApply(v -> this);
+    return bootstrap()
+        .thenRun(() -> started.set(true))
+        .thenApply(
+            v -> {
+              log.info(
+                  "Started plaintext unicast service bound to {}, advertising {}",
+                  bindAddress,
+                  advertisedAddress);
+              return this;
+            });
   }
 
   @Override
@@ -236,26 +215,48 @@ public class NettyUnicastService implements ManagedUnicastService {
       return CompletableFuture.completedFuture(null);
     }
 
-    if (channel != null) {
-      final CompletableFuture<Void> future = new CompletableFuture<>();
-      channel
-          .close()
-          .addListener(
-              f ->
-                  group
-                      .shutdownGracefully()
-                      .addListener(
-                          f2 -> {
-                            future.complete(null);
-                          }));
-      channel = null;
-      return future;
-    }
-
-    return CompletableFuture.completedFuture(null);
+    return CompletableFuture.runAsync(this::doStop);
   }
 
-  /** Internal unicast service message. */
+  private void doStop() {
+    boolean interrupted = false;
+    if (channel != null) {
+      try {
+        channel.close().sync();
+      } catch (final InterruptedException e) {
+        interrupted = true;
+      }
+
+      channel = null;
+    }
+
+    final Future<?> shutdownFuture =
+        group.shutdownGracefully(
+            config.getShutdownQuietPeriod().toMillis(),
+            config.getShutdownTimeout().toMillis(),
+            TimeUnit.MILLISECONDS);
+    try {
+      shutdownFuture.sync();
+    } catch (final InterruptedException e) {
+      interrupted = true;
+    }
+
+    log.info(
+        "Stopped plaintext unicast service bound to {}, advertising {}",
+        bindAddress,
+        advertisedAddress);
+
+    if (interrupted) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  /**
+   * Internal unicast service message.
+   *
+   * <p>NOTE: Cannot be converted to a record as this would break kryo backwards compatibility
+   */
+  @SuppressWarnings("ClassCanBeRecord")
   static final class Message {
     private final Address source;
     private final String subject;
