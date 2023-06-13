ccc0fda56150ed55edcb44e7212b932c78a47bf1
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-06-13 17:18:13 +0200

    test(journal): copy serializedRecord

--- journal/src/test/java/io/camunda/zeebe/journal/file/SegmentedJournalTest.java
+++ journal/src/test/java/io/camunda/zeebe/journal/file/SegmentedJournalTest.java
@@ -434,7 +434,7 @@ class SegmentedJournalTest {
                 firstRecord.index(),
                 firstRecord.asqn(),
                 BufferUtil.cloneBuffer(firstRecord.data())),
-            null);
+            firstRecord.serializedRecord());
     journal.append(journalFactory.entry());
 
     // close the journal before corrupting the segment; since we "flush" when closing, we need to
