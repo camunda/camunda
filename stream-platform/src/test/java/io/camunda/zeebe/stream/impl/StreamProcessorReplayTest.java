71c8f5b05de6dd9a8f6a4ee6772773d177ade1fd
Christopher Zell <zelldon91@googlemail.com>
2023-06-26 14:25:36 +0200

    test: add regrestion test
    Add regrestion test for https://github.com/camunda/zeebe/issues/13101
    
    Test verifies that event which has been written during onProcessingError and is part of
    snapshot shouldn't replayed after restart.

--- stream-platform/src/test/java/io/camunda/zeebe/stream/impl/StreamProcessorReplayTest.java
+++ stream-platform/src/test/java/io/camunda/zeebe/stream/impl/StreamProcessorReplayTest.java
@@ -10,15 +10,24 @@ package io.camunda.zeebe.stream.impl;
 import static io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent.ACTIVATE_ELEMENT;
 import static io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent.ELEMENT_ACTIVATING;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.verify;
 
 import io.camunda.zeebe.protocol.Protocol;
+import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
+import io.camunda.zeebe.protocol.record.RecordType;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.ErrorIntent;
+import io.camunda.zeebe.stream.api.ProcessingResultBuilder;
 import io.camunda.zeebe.stream.api.RecordProcessor;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
 import io.camunda.zeebe.stream.util.RecordToWrite;
 import io.camunda.zeebe.stream.util.Records;
+import io.camunda.zeebe.test.util.junit.RegressionTest;
 import org.assertj.core.api.Assertions;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
@@ -255,6 +264,72 @@ final class StreamProcessorReplayTest {
                     .isEqualTo(4L));
     Assertions.assertThat(Protocol.decodeKeyInPartition(streamPlatform.getCurrentKey()))
         .isEqualTo(21L);
+  }
+
+  @RegressionTest("https://github.com/camunda/zeebe/issues/13101")
+  public void shouldNotApplyErrorEventOnReplay() throws Exception {
+    // given
+    final var processorWhichFails = setupProcessorWhichFailsOnProcessing();
+    setupOnErrorReactionForProcessor(processorWhichFails);
+
+    streamPlatform.startStreamProcessor();
+    // commands to process -> which should fail
+    streamPlatform.writeBatch(
+        RecordToWrite.command().processInstance(ACTIVATE_ELEMENT, Records.processInstance(1)),
+        RecordToWrite.command().processInstance(ACTIVATE_ELEMENT, Records.processInstance(2)));
+
+    // await that two commands are failed and processed, to make sure error event has been committed
+    verify(processorWhichFails, TIMEOUT.times(2)).onProcessingError(any(), any(), any());
+    // the snapshot should contain last processed position
+    streamPlatform.snapshot();
+    streamPlatform.closeStreamProcessor();
+    streamPlatform.resetMockInvocations();
+
+    // when
+    streamPlatform.startStreamProcessor();
+    streamPlatform.writeBatch(
+        RecordToWrite.command().processInstance(ACTIVATE_ELEMENT, Records.processInstance(1)));
+
+    // then
+    verifyProcessingErrorLifecycle(processorWhichFails);
+    // we shouldn't replay any events - due to snapshot
+    // we shouldn't replay any events - due to snapshot
+    verify(processorWhichFails, never()).replay(any());
+  }
+
+  private static void verifyProcessingErrorLifecycle(final RecordProcessor processorWhichFails) {
+    final var inOrder = inOrder(processorWhichFails);
+    inOrder.verify(processorWhichFails, TIMEOUT).init(any());
+    inOrder.verify(processorWhichFails, TIMEOUT).accepts(ValueType.PROCESS_INSTANCE);
+    inOrder.verify(processorWhichFails, TIMEOUT).process(any(), any());
+    inOrder.verify(processorWhichFails, TIMEOUT).onProcessingError(any(), any(), any());
+    inOrder.verifyNoMoreInteractions();
+  }
+
+  private RecordProcessor setupProcessorWhichFailsOnProcessing() {
+    final var processorWhichFails = streamPlatform.getDefaultMockedRecordProcessor();
+    doThrow(new RuntimeException("processing error"))
+        .when(processorWhichFails)
+        .process(any(), any());
+    return processorWhichFails;
+  }
+
+  private static void setupOnErrorReactionForProcessor(final RecordProcessor processorWhichFails) {
+    doAnswer(
+            invocationOnMock -> {
+              // writing error event on failure
+              final var builder = (ProcessingResultBuilder) invocationOnMock.getArgument(2);
+              final RecordMetadata recordMetadata = new RecordMetadata();
+              recordMetadata
+                  .valueType(ValueType.ERROR)
+                  .intent(ErrorIntent.CREATED)
+                  .recordType(RecordType.EVENT);
+              builder.appendRecord(6, Records.processInstance(6), recordMetadata);
+              return builder.build();
+            })
+        .when(processorWhichFails)
+        .onProcessingError(
+            any(Throwable.class), any(TypedRecord.class), any(ProcessingResultBuilder.class));
   }
 
   @Test
