79c16ded87ec6f13213410963b156e69545beb84
Remco Westerhoud <remco@westerhoud.nl>
2023-06-27 16:05:43 +0200

    test(engine): verify getting latest version of event applier
    These tests verify that we can successfully get the latest version of an
     event applier in different scenarios.

--- engine/src/test/java/io/camunda/zeebe/engine/state/appliers/EventAppliersTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/state/appliers/EventAppliersTest.java
@@ -13,6 +13,8 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import io.camunda.zeebe.engine.state.TypedEventApplier;
 import io.camunda.zeebe.protocol.record.RecordValue;
 import io.camunda.zeebe.protocol.record.intent.Intent;
+import io.camunda.zeebe.protocol.record.intent.ProcessIntent;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -80,5 +82,58 @@ public class EventAppliersTest {
     // then
     Mockito.verify(mockedApplier, Mockito.never()).applyState(anyLong(), any());
     Mockito.verify(anotherMockedApplier).applyState(anyLong(), any());
+  }
+
+  @Test
+  void shouldGetLatestVersionOfOnlyRegisteredVersion() {
+    // given
+    final var expectedVersion = 1;
+    eventAppliers.register(Intent.UNKNOWN, expectedVersion, mockedApplier);
+
+    // when
+    final var actualVersion = eventAppliers.getLatestVersion(Intent.UNKNOWN);
+
+    // then
+    Assertions.assertEquals(expectedVersion, actualVersion);
+  }
+
+  @Test
+  void shouldGetLatestVersionOfTwoRegisteredVersions() {
+    // given
+    final var expectedVersion = 2;
+    eventAppliers.register(Intent.UNKNOWN, 1, mockedApplier);
+    eventAppliers.register(Intent.UNKNOWN, expectedVersion, mockedApplier);
+
+    // when
+    final var actualVersion = eventAppliers.getLatestVersion(Intent.UNKNOWN);
+
+    // then
+    Assertions.assertEquals(expectedVersion, actualVersion);
+  }
+
+  @Test
+  void shouldGetLatestVersionMinusOneWhenNoRegisteredVersion() {
+    // given
+    final var expectedVersion = -1;
+
+    // when
+    final var actualVersion = eventAppliers.getLatestVersion(Intent.UNKNOWN);
+
+    // then
+    Assertions.assertEquals(expectedVersion, actualVersion);
+  }
+
+  @Test
+  void shouldGetLatestVersionWhenMultipleRegisteredEventAppliersWithDifferentIntents() {
+    // given
+    final var expectedVersion = 1;
+    eventAppliers.register(Intent.UNKNOWN, expectedVersion, mockedApplier);
+    eventAppliers.register(ProcessIntent.CREATED, expectedVersion, mockedApplier);
+
+    // when
+    final var actualVersion = eventAppliers.getLatestVersion(Intent.UNKNOWN);
+
+    // then
+    Assertions.assertEquals(expectedVersion, actualVersion);
   }
 }
