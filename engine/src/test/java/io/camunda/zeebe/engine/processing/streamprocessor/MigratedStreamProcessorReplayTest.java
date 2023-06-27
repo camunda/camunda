8720e548bcd1dc15c5ccd75a3367cecedb26d716
Christopher Kujawa (Zell) <zelldon91@googlemail.com>
2023-06-27 16:54:53 +0200

    refactor: adjust test method name
    Co-authored-by: Deepthi Devaki Akkoorath <deepthidevaki@users.noreply.github.com>

--- engine/src/test/java/io/camunda/zeebe/engine/processing/streamprocessor/MigratedStreamProcessorReplayTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/streamprocessor/MigratedStreamProcessorReplayTest.java
@@ -43,7 +43,7 @@ final class MigratedStreamProcessorReplayTest {
   private StreamPlatform streamPlatform;
 
   @RegressionTest("https://github.com/camunda/zeebe/issues/13101")
-  public void shouldNotApplyErrorEventOnReplay() throws Exception {
+  public void shouldNotReplayErrorEventAppliedInSnapshot() throws Exception {
     // given
     final var processorWhichFails = setupProcessorWhichFailsOnProcessing();
     setupOnErrorReactionForProcessor(processorWhichFails);
