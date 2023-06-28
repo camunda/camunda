7d9184ed72311e55b4ef258c3d7926ebe693dfd5
Nico Korthout <nico.korthout@camunda.com>
2023-06-28 15:21:05 +0200

    fix: reorder imports

--- engine/src/main/java/io/camunda/zeebe/engine/processing/message/MessageObserver.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/message/MessageObserver.java
@@ -8,8 +8,8 @@
 package io.camunda.zeebe.engine.processing.message;
 
 import io.camunda.zeebe.engine.processing.message.command.SubscriptionCommandSender;
-import io.camunda.zeebe.engine.state.mutable.MutablePendingMessageSubscriptionState;
 import io.camunda.zeebe.engine.state.immutable.ScheduledTaskState;
+import io.camunda.zeebe.engine.state.mutable.MutablePendingMessageSubscriptionState;
 import io.camunda.zeebe.stream.api.ReadonlyStreamProcessorContext;
 import io.camunda.zeebe.stream.api.StreamProcessorLifecycleAware;
 import java.time.Duration;
