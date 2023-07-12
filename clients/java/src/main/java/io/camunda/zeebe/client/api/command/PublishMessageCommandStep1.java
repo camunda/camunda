7bdca4631c3b0ead8bcd6ce3b3b38215ec3174b5
Nicola Puppa <nicola.puppa@camunda.com>
2023-07-12 11:24:40 +0200

    feat(java-client): add .variable() API in publish message command
    With this new API users can provide a single variable to publish a
    message, reducing also the overhead of wrap the single variable in
    another object

--- clients/java/src/main/java/io/camunda/zeebe/client/api/command/PublishMessageCommandStep1.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/api/command/PublishMessageCommandStep1.java
@@ -104,5 +104,15 @@ public interface PublishMessageCommandStep1 {
      *     it to the broker.
      */
     PublishMessageCommandStep3 variables(Object variables);
+
+    /**
+     * Set a single variable of the message.
+     *
+     * @param key the key of the variable as string
+     * @param value the value of the variable as object
+     * @return the builder for this command. Call {@link #send()} to complete the command and send
+     *     it to the broker.
+     */
+    PublishMessageCommandStep3 variable(String key, Object value);
   }
 }
