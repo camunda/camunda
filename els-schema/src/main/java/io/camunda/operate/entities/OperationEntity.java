e46c5f7c27f12bc8d214aa55e8037dd1aaa08df9
mihail-ca <122792941+mihail-ca@users.noreply.github.com>
2023-08-01 17:02:57 +0200

    feat(backend): Update schema to support DELETE_DECISION_DEFINITION (#5035)
    feat(backend): Update schema to support DELETE_DECISION_DEFINITION
    
    - Added new operation type, DELETE_DECISION_DEFINITION
    - Added field decisionDefinitionKey to operation schema, required by the operation
    
    Closes #5034

--- els-schema/src/main/java/io/camunda/operate/entities/OperationEntity.java
+++ els-schema/src/main/java/io/camunda/operate/entities/OperationEntity.java
@@ -21,6 +21,10 @@ public class OperationEntity extends OperateEntity<OperationEntity> {
    * Attention! This field will be filled in only for data imported after v. 8.2.0.
    */
   private String bpmnProcessId;
+  /**
+   * Attention! This field will be filled in only for data imported after v. 8.3.0.
+   */
+  private Long decisionDefinitionKey;
   private Long incidentKey;
   private Long scopeKey;
   private String variableName;
@@ -57,6 +61,14 @@ public class OperationEntity extends OperateEntity<OperationEntity> {
 
   public void setBpmnProcessId(String bpmnProcessId) {
     this.bpmnProcessId = bpmnProcessId;
+  }
+
+  public Long getDecisionDefinitionKey() {
+    return decisionDefinitionKey;
+  }
+
+  public void setDecisionDefinitionKey(Long decisionDefinitionKey) {
+    this.decisionDefinitionKey = decisionDefinitionKey;
   }
 
   public Long getIncidentKey() {
@@ -178,25 +190,26 @@ public class OperationEntity extends OperateEntity<OperationEntity> {
       return false;
     OperationEntity that = (OperationEntity) o;
     return Objects.equals(processInstanceKey, that.processInstanceKey) && Objects.equals(processDefinitionKey, that.processDefinitionKey) && Objects.equals(
-        bpmnProcessId, that.bpmnProcessId) && Objects.equals(incidentKey, that.incidentKey) && Objects.equals(scopeKey, that.scopeKey) && Objects.equals(
-        variableName, that.variableName) && Objects.equals(variableValue, that.variableValue) && type == that.type && Objects.equals(lockExpirationTime,
-        that.lockExpirationTime) && Objects.equals(lockOwner, that.lockOwner) && state == that.state && Objects.equals(errorMessage, that.errorMessage)
-        && Objects.equals(batchOperationId, that.batchOperationId) && Objects.equals(zeebeCommandKey, that.zeebeCommandKey) && Objects.equals(username,
-        that.username) && Objects.equals(modifyInstructions, that.modifyInstructions);
+        bpmnProcessId, that.bpmnProcessId) && Objects.equals(decisionDefinitionKey, that.decisionDefinitionKey) && Objects.equals(incidentKey, that.incidentKey)
+        && Objects.equals(scopeKey, that.scopeKey) && Objects.equals(variableName, that.variableName) && Objects.equals(variableValue, that.variableValue)
+        && type == that.type && Objects.equals(lockExpirationTime, that.lockExpirationTime) && Objects.equals(lockOwner, that.lockOwner) && state == that.state
+        && Objects.equals(errorMessage, that.errorMessage) && Objects.equals(batchOperationId, that.batchOperationId) && Objects.equals(zeebeCommandKey,
+        that.zeebeCommandKey) && Objects.equals(username, that.username) && Objects.equals(modifyInstructions, that.modifyInstructions);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(super.hashCode(), processInstanceKey, processDefinitionKey, bpmnProcessId, incidentKey, scopeKey, variableName, variableValue, type,
-        lockExpirationTime, lockOwner, state, errorMessage, batchOperationId, zeebeCommandKey, username, modifyInstructions);
+    return Objects.hash(super.hashCode(), processInstanceKey, processDefinitionKey, bpmnProcessId, decisionDefinitionKey, incidentKey, scopeKey, variableName,
+        variableValue, type, lockExpirationTime, lockOwner, state, errorMessage, batchOperationId, zeebeCommandKey, username, modifyInstructions);
   }
 
   @Override
   public String toString() {
     return "OperationEntity{" + "processInstanceKey=" + processInstanceKey + ", processDefinitionKey=" + processDefinitionKey + ", bpmnProcessId='"
-        + bpmnProcessId + '\'' + ", incidentKey=" + incidentKey + ", scopeKey=" + scopeKey + ", variableName='" + variableName + '\'' + ", variableValue='"
-        + variableValue + '\'' + ", type=" + type + ", lockExpirationTime=" + lockExpirationTime + ", lockOwner='" + lockOwner + '\'' + ", state=" + state
-        + ", errorMessage='" + errorMessage + '\'' + ", batchOperationId='" + batchOperationId + '\'' + ", zeebeCommandKey=" + zeebeCommandKey + ", username='"
-        + username + '\'' + ", modifyInstructions='" + modifyInstructions + '\'' + '}';
+        + bpmnProcessId + '\'' + ", decisionDefinitionKey=" + decisionDefinitionKey + ", incidentKey=" + incidentKey + ", scopeKey=" + scopeKey
+        + ", variableName='" + variableName + '\'' + ", variableValue='" + variableValue + '\'' + ", type=" + type + ", lockExpirationTime="
+        + lockExpirationTime + ", lockOwner='" + lockOwner + '\'' + ", state=" + state + ", errorMessage='" + errorMessage + '\'' + ", batchOperationId='"
+        + batchOperationId + '\'' + ", zeebeCommandKey=" + zeebeCommandKey + ", username='" + username + '\'' + ", modifyInstructions='" + modifyInstructions
+        + '\'' + '}';
   }
 }
--- els-schema/src/main/java/io/camunda/operate/schema/templates/OperationTemplate.java
+++ els-schema/src/main/java/io/camunda/operate/schema/templates/OperationTemplate.java
@@ -19,6 +19,7 @@ public class OperationTemplate extends AbstractTemplateDescriptor implements Pro
   public static final String PROCESS_INSTANCE_KEY = "processInstanceKey";
   public static final String PROCESS_DEFINITION_KEY = "processDefinitionKey";
   public static final String BPMN_PROCESS_ID = "bpmnProcessId";
+  public static final String DECISION_DEFINITION_KEY = "decisionDefinitionKey";
   public static final String INCIDENT_KEY = "incidentKey";
   public static final String SCOPE_KEY = "scopeKey";
   public static final String VARIABLE_NAME = "variableName";
@@ -39,7 +40,7 @@ public class OperationTemplate extends AbstractTemplateDescriptor implements Pro
 
   @Override
   public String getVersion() {
-    return "8.2.0";
+    return "8.3.0";
   }
 
 }
