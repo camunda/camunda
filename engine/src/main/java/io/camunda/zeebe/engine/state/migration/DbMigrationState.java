f6d11db37d098ca4dab010e58f8dbdd7d2221e87
Remco Westerhoud <remco@westerhoud.nl>
2023-08-02 17:34:58 +0200

    refactor(engine): rename object
    It was confusing to have two MigrationState objects. Renaming this to
    MigrationTaskState makes it more obvious it's about a specific migration
     task, and not the migration state as a whole.
    
    (cherry picked from commit 892a605c8f30ef98c1d99e3f0a6c05c8f56df942)

--- engine/src/main/java/io/camunda/zeebe/engine/state/migration/DbMigrationState.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/migration/DbMigrationState.java
@@ -15,7 +15,7 @@ import io.camunda.zeebe.db.impl.DbLong;
 import io.camunda.zeebe.db.impl.DbNil;
 import io.camunda.zeebe.db.impl.DbString;
 import io.camunda.zeebe.engine.state.ZbColumnFamilies;
-import io.camunda.zeebe.engine.state.migration.MigrationState.State;
+import io.camunda.zeebe.engine.state.migration.MigrationTaskState.State;
 import io.camunda.zeebe.engine.state.mutable.MutableElementInstanceState;
 import io.camunda.zeebe.engine.state.mutable.MutableEventScopeInstanceState;
 import io.camunda.zeebe.engine.state.mutable.MutableMessageSubscriptionState;
@@ -32,8 +32,8 @@ public class DbMigrationState implements MutableMigrationState {
 
   // Meta ColumnFamily to keep track if migration have run
   private final DbString migrationIdentifier;
-  private final MigrationState migrationState;
-  private final ColumnFamily<DbString, MigrationState> migrationStateColumnFamily;
+  private final MigrationTaskState migrationTaskState;
+  private final ColumnFamily<DbString, MigrationTaskState> migrationStateColumnFamily;
 
   // ZbColumnFamilies.MESSAGE_SUBSCRIPTION_BY_SENT_TIME
   // (sentTime, elementInstanceKey, messageName) => \0
@@ -64,13 +64,13 @@ public class DbMigrationState implements MutableMigrationState {
   public DbMigrationState(
       final ZeebeDb<ZbColumnFamilies> zeebeDb, final TransactionContext transactionContext) {
     migrationIdentifier = new DbString();
-    migrationState = new MigrationState();
+    migrationTaskState = new MigrationTaskState();
     migrationStateColumnFamily =
         zeebeDb.createColumnFamily(
             ZbColumnFamilies.MIGRATIONS_STATE,
             transactionContext,
             migrationIdentifier,
-            migrationState);
+            migrationTaskState);
 
     messageSubscriptionElementInstanceKey = new DbLong();
     messageSubscriptionMessageName = new DbString();
@@ -224,7 +224,7 @@ public class DbMigrationState implements MutableMigrationState {
   @Override
   public void markMigrationFinished(final String identifier) {
     migrationIdentifier.wrapString(identifier);
-    migrationStateColumnFamily.upsert(migrationIdentifier, new MigrationState());
+    migrationStateColumnFamily.upsert(migrationIdentifier, new MigrationTaskState());
   }
 
   @Override
