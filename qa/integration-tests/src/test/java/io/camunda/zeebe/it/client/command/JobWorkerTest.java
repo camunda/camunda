0d84e27a8fbe1b8e6230af4eca0314755b6b83a2
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-08-09 11:15:40 +0200

    test(qa): add streaming job worker QA tests

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/JobWorkerTest.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/JobWorkerTest.java
@@ -10,18 +10,26 @@ package io.camunda.zeebe.it.client.command;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobWorker;
+import io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1.JobWorkerBuilderStep3;
 import io.camunda.zeebe.it.clustering.ClusteringRuleExtension;
 import io.camunda.zeebe.it.util.GrpcClientRule;
 import io.camunda.zeebe.it.util.RecordingJobHandler;
 import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.qa.util.JobStreamServiceAssert;
 import io.camunda.zeebe.test.util.Strings;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
 import org.assertj.core.data.Offset;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 final class JobWorkerTest {
 
@@ -38,8 +46,9 @@ final class JobWorkerTest {
     client = new GrpcClientRule(CLUSTER.getClient());
   }
 
-  @Test
-  void shouldActivateJob() {
+  @ParameterizedTest
+  @MethodSource("provideWorkerConfigurators")
+  void shouldActivateJob(final BiFunction<String, JobWorkerBuilderStep3, JobWorker> configurator) {
     // given
     final var process =
         Bpmn.createExecutableProcess("process")
@@ -67,7 +76,7 @@ final class JobWorkerTest {
             .handler(jobHandler)
             .name("test")
             .timeout(5000);
-    try (final var ignored = builder.open()) {
+    try (final var ignored = configurator.apply(jobType, builder)) {
       Awaitility.await("until all jobs are activated")
           .untilAsserted(() -> assertThat(jobHandler.getHandledJobs()).hasSize(1));
     }
@@ -86,8 +95,10 @@ final class JobWorkerTest {
     assertThat(job.getVariablesAsMap()).isEqualTo(Map.of("a", 1, "b", 2));
   }
 
-  @Test
-  void shouldActivateJobsOfDifferentTypes() {
+  @ParameterizedTest
+  @MethodSource("provideWorkerConfigurators")
+  void shouldActivateJobsOfDifferentTypes(
+      final BiFunction<String, JobWorkerBuilderStep3, JobWorker> configurator) {
     // given
     final var jobX = client.createSingleJob(jobType + "x");
     final var jobY = client.createSingleJob(jobType + "y");
@@ -97,8 +108,8 @@ final class JobWorkerTest {
     final var builderY = client.getClient().newWorker().jobType(jobType + "y").handler(jobHandlerY);
 
     // when
-    try (final var ignoredX = builderX.open();
-        final var ignoredY = builderY.open()) {
+    try (final var ignoredX = configurator.apply(jobType + "x", builderX);
+        final var ignoredY = configurator.apply(jobType + "y", builderY)) {
       Awaitility.await("until all jobs are activated")
           .untilAsserted(() -> assertThat(jobHandlerX.getHandledJobs()).hasSize(1));
       Awaitility.await("until all jobs are activated")
@@ -116,8 +127,10 @@ final class JobWorkerTest {
         .contains(jobY);
   }
 
-  @Test
-  void shouldFetchOnlySpecifiedVariables() {
+  @ParameterizedTest
+  @MethodSource("provideWorkerConfigurators")
+  void shouldFetchOnlySpecifiedVariables(
+      final BiFunction<String, JobWorkerBuilderStep3, JobWorker> configurator) {
     // given
     client.createSingleJob(jobType, b -> {}, "{\"a\":1, \"b\":2, \"c\":3,\"d\":4}");
 
@@ -131,7 +144,7 @@ final class JobWorkerTest {
             .jobType(jobType)
             .handler(jobHandler)
             .fetchVariables(fetchVariables);
-    try (final var ignored = builder.open()) {
+    try (final var ignored = configurator.apply(jobType, builder)) {
       Awaitility.await("until all jobs are activated")
           .untilAsserted(() -> assertThat(jobHandler.getHandledJobs()).hasSize(1));
     }
@@ -139,5 +152,46 @@ final class JobWorkerTest {
     // then
     final ActivatedJob job = jobHandler.getHandledJobs().get(0);
     assertThat(job.getVariablesAsMap()).isEqualTo(Map.of("a", 1, "b", 2));
+  }
+
+  @Test
+  void shouldStreamAndActivateJobs() {
+    // given - a job created before any stream was registered, i.e. can only be polled
+    client.createSingleJob(jobType, b -> {});
+
+    // when - create a worker that streams, and create a new job after the stream is registered
+    final var jobHandler = new RecordingJobHandler();
+    final var builder =
+        client.getClient().newWorker().jobType(jobType).handler(jobHandler).enableStreaming();
+    try (final var ignored = builder.open()) {
+      awaitStreamRegistered(jobType);
+      client.createSingleJob(jobType, b -> {});
+
+      // then - expect both jobs to be activated
+      Awaitility.await("until all jobs are activated")
+          .untilAsserted(() -> assertThat(jobHandler.getHandledJobs()).hasSize(2));
+    }
+  }
+
+  private static Stream<Named<BiFunction<String, JobWorkerBuilderStep3, JobWorker>>>
+      provideWorkerConfigurators() {
+    return Stream.of(
+        Named.named("polling", (ignored, builder) -> builder.open()),
+        Named.named("streaming", JobWorkerTest::prepareStreamingWorker));
+  }
+
+  private static JobWorker prepareStreamingWorker(
+      final String jobType, final JobWorkerBuilderStep3 builder) {
+    final var worker = builder.enableStreaming().open();
+    awaitStreamRegistered(jobType);
+    return worker;
+  }
+
+  private static void awaitStreamRegistered(final String jobType) {
+    final var jobStreamService = CLUSTER.getBrokerBridge(0).getJobStreamService().orElseThrow();
+    Awaitility.await("until stream is registered")
+        .untilAsserted(
+            () ->
+                JobStreamServiceAssert.assertThat(jobStreamService).hasStreamWithType(1, jobType));
   }
 }
