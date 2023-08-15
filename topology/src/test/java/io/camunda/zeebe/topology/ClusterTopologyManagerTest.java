fb8996a71fdf3be19de7a0be72bcc1be625567b0
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-08-15 15:02:01 +0200

    test(topology): move ClusterTopologyTest to topology module

--- topology/src/test/java/io/camunda/zeebe/topology/ClusterTopologyManagerTest.java
+++ topology/src/test/java/io/camunda/zeebe/topology/ClusterTopologyManagerTest.java
@@ -5,13 +5,14 @@
  * Licensed under the Zeebe Community License 1.1. You may not use this file
  * except in compliance with the Zeebe Community License 1.1.
  */
-package io.camunda.zeebe.broker.clustering.topology;
+package io.camunda.zeebe.topology;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import io.atomix.cluster.MemberId;
-import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
+import io.atomix.primitive.partition.PartitionId;
+import io.atomix.primitive.partition.PartitionMetadata;
 import io.camunda.zeebe.scheduler.testing.TestConcurrencyControl;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -19,25 +20,23 @@ import java.nio.file.Path;
 import java.time.Duration;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 final class ClusterTopologyManagerTest {
 
   @Test
-  void shouldInitializeClusterTopologyFromBrokerCfg(@TempDir final Path topologyFile) {
+  void shouldInitializeClusterTopologyFromProvidedConfig(@TempDir final Path topologyFile) {
     // given
     final ClusterTopologyManager clusterTopologyManager =
         new ClusterTopologyManager(
             new TestConcurrencyControl(),
             new PersistedClusterTopology(topologyFile.resolve("topology.temp")));
-    final BrokerCfg brokerCfg = new BrokerCfg();
-    brokerCfg.getCluster().setClusterSize(3);
-    brokerCfg.getCluster().setPartitionsCount(3);
-    brokerCfg.getCluster().setReplicationFactor(1);
 
     // when
-    clusterTopologyManager.start(brokerCfg).join();
+    clusterTopologyManager.start(this::getPartitionDistribution).join();
 
     // then
     final ClusterTopology clusterTopology = clusterTopologyManager.getClusterTopology().join();
@@ -45,6 +44,18 @@ final class ClusterTopologyManagerTest {
         .hasMemberWithPartitions(0, Set.of(1))
         .hasMemberWithPartitions(1, Set.of(2))
         .hasMemberWithPartitions(2, Set.of(3));
+  }
+
+  private Set<PartitionMetadata> getPartitionDistribution() {
+    final var members =
+        IntStream.range(0, 3)
+            .mapToObj(i -> MemberId.from(String.valueOf(i)))
+            .collect(Collectors.toSet());
+    final var priorities =
+        members.stream().collect(Collectors.toMap(m -> m, m -> Integer.valueOf(m.id()) + 1));
+    return Set.of(
+        new PartitionMetadata(
+            PartitionId.from("test", 1), members, priorities, 1, MemberId.from("0")));
   }
 
   @Test
@@ -63,7 +74,7 @@ final class ClusterTopologyManagerTest {
             new TestConcurrencyControl(), new PersistedClusterTopology(existingTopologyFile));
 
     // when
-    clusterTopologyManager.start(new BrokerCfg()).join();
+    clusterTopologyManager.start(this::getPartitionDistribution).join();
 
     // then
     final ClusterTopology clusterTopology = clusterTopologyManager.getClusterTopology().join();
@@ -82,7 +93,7 @@ final class ClusterTopologyManagerTest {
             new TestConcurrencyControl(), new PersistedClusterTopology(existingTopologyFile));
 
     // when - then
-    assertThat(clusterTopologyManager.start(new BrokerCfg()))
+    assertThat(clusterTopologyManager.start(this::getPartitionDistribution))
         .failsWithin(Duration.ofMillis(100))
         .withThrowableThat()
         .withCauseInstanceOf(JsonParseException.class);
