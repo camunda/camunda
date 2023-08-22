d08d58144f7d436d88ae0e029c1f57b68abd0a1b
Nikola Koevski <nikola.koevski@camunda.com>
2023-08-22 11:14:18 +0200

    feat(protocol): add an authorization field to record metadata
    Add an authorization field to the RecordMetadata SBE message of the Zeebe protocol.
    Initially,the authorization field will be populated only for Command records, and
    this data won't be exported. The field will contain information on the authorizations
    of the user that made the client request that created the Command record. The field type
    is of variable length, as authorizations may vary per user.

--- protocol/src/main/java/io/camunda/zeebe/protocol/record/Record.java
+++ protocol/src/main/java/io/camunda/zeebe/protocol/record/Record.java
@@ -16,6 +16,7 @@
 package io.camunda.zeebe.protocol.record;
 
 import io.camunda.zeebe.protocol.record.intent.Intent;
+import java.util.Map;
 import org.immutables.value.Value;
 
 /** Represents a record published to the log stream. */
@@ -86,6 +87,11 @@ public interface Record<T extends RecordValue> extends JsonSerializable {
    * @return the version of the broker that wrote this record
    */
   String getBrokerVersion();
+
+  /**
+   * @return a Map of authorization data for this record or an empty Map if not set.
+   */
+  Map<String, Object> getAuthorizations();
 
   /**
    * A record version is an integer starting from 1. The version of a record is defined when it is
