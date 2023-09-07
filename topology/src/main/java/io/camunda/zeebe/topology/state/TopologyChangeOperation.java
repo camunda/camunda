683c355095708c655e11ce13fe9c8d99029f0d6f
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-09-07 14:25:44 +0200

    feat(topology): add partition specific operation to TopologyChangeOperation

--- topology/src/main/java/io/camunda/zeebe/topology/state/TopologyChangeOperation.java
+++ topology/src/main/java/io/camunda/zeebe/topology/state/TopologyChangeOperation.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.topology.state;
 
 import io.atomix.cluster.MemberId;
+import java.util.Optional;
 
 /**
  * An operation that changes the topology. The operation could be a member join or leave a cluster,
@@ -16,4 +17,32 @@ import io.atomix.cluster.MemberId;
 public interface TopologyChangeOperation {
 
   MemberId memberId();
+
+  default Operation getOperation() {
+    return Operation.NONE;
+  }
+
+  record PartitionOperation(
+      int partitionId, PartitionOperationType operationType, Optional<Integer> priority)
+      implements Operation {
+    @Override
+    public boolean isPartitionOperation() {
+      return true;
+    }
+  }
+
+  interface Operation {
+    Operation NONE = new Operation() {};
+
+    default boolean isPartitionOperation() {
+      return false;
+    }
+  }
+
+  enum PartitionOperationType {
+    UNKNOWN,
+    JOIN,
+    LEAVE
+    // Add PROMOTE and DEMOTE when we want to support them
+  }
 }
