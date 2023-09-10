aba43d7540ad0fd5b27164540c420b018f034127
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-09-10 15:23:24 +0200

    fix(transport): catch potential exception

--- transport/src/main/java/io/camunda/zeebe/transport/stream/impl/ClientStreamImpl.java
+++ transport/src/main/java/io/camunda/zeebe/transport/stream/impl/ClientStreamImpl.java
@@ -9,6 +9,7 @@ package io.camunda.zeebe.transport.stream.impl;
 
 import io.atomix.cluster.MemberId;
 import io.camunda.zeebe.scheduler.future.ActorFuture;
+import io.camunda.zeebe.scheduler.future.CompletableActorFuture;
 import io.camunda.zeebe.transport.stream.api.ClientStream;
 import io.camunda.zeebe.transport.stream.api.ClientStreamConsumer;
 import io.camunda.zeebe.util.buffer.BufferWriter;
@@ -25,7 +26,11 @@ record ClientStreamImpl<M extends BufferWriter>(
     implements ClientStream<M> {
 
   ActorFuture<Void> push(final DirectBuffer payload) {
-    return clientStreamConsumer.push(payload);
+    try {
+      return clientStreamConsumer.push(payload);
+    } catch (final Exception e) {
+      return CompletableActorFuture.completedExceptionally(e);
+    }
   }
 
   @Override
