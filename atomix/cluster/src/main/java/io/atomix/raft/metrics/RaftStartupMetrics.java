7cfaa5d19996323690ba3be065d8760f48f3eb5d
Ole Schönburg <ole.schoenburg@gmail.com>
2023-09-11 12:38:52 +0200

    refactor: only measure bootstrap duration
    Start and bootstrap durations are essentially the same now so there's no
    sense in tracking them separately.
    Our grafana dashboard still contains queries for the start duration, but
    we don't have to remove them for now.

--- atomix/cluster/src/main/java/io/atomix/raft/metrics/RaftStartupMetrics.java
+++ atomix/cluster/src/main/java/io/atomix/raft/metrics/RaftStartupMetrics.java
@@ -18,15 +18,6 @@ package io.atomix.raft.metrics;
 import io.prometheus.client.Gauge;
 
 public class RaftStartupMetrics extends RaftMetrics {
-  private static final Gauge START_DURATION =
-      Gauge.build()
-          .namespace(NAMESPACE)
-          .labelNames(PARTITION_GROUP_NAME_LABEL, PARTITION_LABEL)
-          .help(
-              "Time taken to start the partition server (in ms). This includes the bootstrap time.")
-          .name("partition_server_startup_time")
-          .register();
-
   private static final Gauge BOOTSTRAP_DURATION =
       Gauge.build()
           .namespace(NAMESPACE)
@@ -35,17 +26,11 @@ public class RaftStartupMetrics extends RaftMetrics {
           .name("partition_server_bootstrap_time")
           .register();
 
-  private final Gauge.Child startDuration;
   private final Gauge.Child bootstrapDuration;
 
   public RaftStartupMetrics(final String partitionName) {
     super(partitionName);
-    startDuration = START_DURATION.labels(partitionGroupName, partition);
     bootstrapDuration = BOOTSTRAP_DURATION.labels(partitionGroupName, partition);
-  }
-
-  public void observeStartupDuration(final long durationMillis) {
-    startDuration.set(durationMillis);
   }
 
   public void observeBootstrapDuration(final long durationMillis) {
