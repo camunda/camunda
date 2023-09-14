01bb3e56967e6c9cdc37da7c0485c25b450e48eb
Christopher Zell <zelldon91@googlemail.com>
2023-09-14 15:32:46 +0200

    test: use correct types

--- atomix/cluster/src/test/java/io/atomix/raft/RaftAppendTest.java
+++ atomix/cluster/src/test/java/io/atomix/raft/RaftAppendTest.java
@@ -16,7 +16,7 @@
 package io.atomix.raft;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
@@ -89,7 +89,7 @@ public class RaftAppendTest {
     raftRule.appendEntry(); // awaits commit
 
     // then
-    verify(committedEntryListener, timeout(1000L).times(1)).onCommit(any());
+    verify(committedEntryListener, timeout(1000L).times(1)).onCommit(anyLong());
   }
 
   @Test
