294fd35f62dbc4e2a774c014f7fd3abc5b5b97ab
Nikola Koevski <nikola.koevski@camunda.com>
2023-09-21 13:07:21 +0200

    feat(engine): job records inherit parent pi's tenant id
    Ensure that nn JobRecord creation, the JobRecord inherits the
    tenant id of the parent process instance. This declares that
    the job is owned by the same tenant as the parent process
    instance.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnJobBehavior.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnJobBehavior.java
@@ -208,7 +208,8 @@ public final class BpmnJobBehavior {
         .setProcessDefinitionKey(context.getProcessDefinitionKey())
         .setProcessInstanceKey(context.getProcessInstanceKey())
         .setElementId(jobWorkerElement.getId())
-        .setElementInstanceKey(context.getElementInstanceKey());
+        .setElementInstanceKey(context.getElementInstanceKey())
+        .setTenantId(context.getTenantId());
 
     final var jobKey = keyGenerator.nextKey();
     stateWriter.appendFollowUpEvent(jobKey, JobIntent.CREATED, jobRecord);
