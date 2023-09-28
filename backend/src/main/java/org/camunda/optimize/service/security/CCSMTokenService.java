1b5af8c23b05f4872548680f10e747cc271049b8
Josh Windels <joshua.windels@camunda.com>
2023-09-28 18:10:12 +0200

    chore(logging): add redirect logging for C8SM auth flow (#8805)

--- backend/src/main/java/org/camunda/optimize/service/security/CCSMTokenService.java
+++ backend/src/main/java/org/camunda/optimize/service/security/CCSMTokenService.java
@@ -108,8 +108,9 @@ public class CCSMTokenService {
 
   public URI buildAuthorizeUri(final String redirectUri) {
     // If a redirect root URL is explicitly set, we use that. Otherwise, we use the one provided
-    final String authorizeUri = getConfiguredRedirectUri().orElse(redirectUri);
-    return authentication().authorizeUriBuilder(appendCallbackSubpath(authorizeUri)).build();
+    final String authorizeUri = appendCallbackSubpath(getConfiguredRedirectUri().orElse(redirectUri));
+    log.trace("Authorizing with authorizeUri: {}", authorizeUri);
+    return authentication().authorizeUriBuilder(authorizeUri).build();
   }
 
   public Tokens exchangeAuthCode(final AuthCodeDto authCode, final ContainerRequestContext requestContext) {
@@ -118,6 +119,7 @@ public class CCSMTokenService {
     final String redirectUri = getConfiguredRedirectUri()
       .map(CCSMTokenService::appendCallbackSubpath)
       .orElse(requestContext.getUriInfo().getAbsolutePath().toString());
+    log.trace("Exchanging auth code with redirectUri: {}", redirectUri);
     return authentication().exchangeAuthCode(authCode, redirectUri);
   }
 
