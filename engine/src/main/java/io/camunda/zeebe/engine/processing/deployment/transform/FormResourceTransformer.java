1577e8453fc91cd73250f2110ca756a36e18f0da
Remco Westerhoud <remco@westerhoud.nl>
2023-09-29 15:43:32 +0200

    fix(engine): use correct tenant
    We were trying to find the latest form by id for the tenant in the
    metadata record. This metadata record was only just created. No tenant
    was set on it, so it would always result in the default tenant.
    
    We must take the tenant from the deployment record. This is the tenant
    the user provided when doing the deployment.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/transform/FormResourceTransformer.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/transform/FormResourceTransformer.java
@@ -62,7 +62,8 @@ public final class FormResourceTransformer implements DeploymentResourceTransfor
                     .map(
                         noDuplicates -> {
                           final FormMetadataRecord formRecord = deployment.formMetadata().add();
-                          appendMetadataToFormRecord(formRecord, formId, resource);
+                          appendMetadataToFormRecord(
+                              formRecord, formId, resource, deployment.getTenantId());
                           writeFormRecord(formRecord, resource);
 
                           return null;
@@ -105,7 +106,10 @@ public final class FormResourceTransformer implements DeploymentResourceTransfor
   }
 
   private void appendMetadataToFormRecord(
-      final FormMetadataRecord formRecord, final String formId, final DeploymentResource resource) {
+      final FormMetadataRecord formRecord,
+      final String formId,
+      final DeploymentResource resource,
+      final String tenantId) {
     final LongSupplier newFormKey = keyGenerator::nextKey;
     final DirectBuffer checksum = checksumGenerator.apply(resource);
 
@@ -114,7 +118,7 @@ public final class FormResourceTransformer implements DeploymentResourceTransfor
     formRecord.setResourceName(resource.getResourceName());
 
     formState
-        .findLatestFormById(wrapString(formRecord.getFormId()), formRecord.getTenantId())
+        .findLatestFormById(wrapString(formRecord.getFormId()), tenantId)
         .ifPresentOrElse(
             latestForm -> {
               final int latestVersion = latestForm.getVersion();
