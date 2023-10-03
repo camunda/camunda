475711319ed7c476c8ced3231c457f57e5781cb2
Remco Westerhoud <remco@westerhoud.nl>
2023-10-03 09:38:33 +0200

    Revert "feat(engine): check if migration already ran"
    This reverts commit 8ef05cda8d9d7677c8779dd190c798fb1c6b79bf.

--- engine/src/main/java/io/camunda/zeebe/engine/state/migration/DbMigratorImpl.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/migration/DbMigratorImpl.java
@@ -116,12 +116,6 @@ public class DbMigratorImpl implements DbMigrator {
 
   private boolean handleMigrationTask(
       final MigrationTask migrationTask, final int index, final int total) {
-    // The migration is already finished. We don't need to run it again.
-    if (processingState.getMigrationState().isMigrationFinished(migrationTask.getIdentifier())) {
-      logMigrationTaskAlreadyExecuted(migrationTask, index, total);
-      return false;
-    }
-
     if (migrationTask.needsToRun(processingState)) {
       try {
         currentMigration = migrationTask;
@@ -136,18 +130,6 @@ public class DbMigratorImpl implements DbMigrator {
       processingState.getMigrationState().markMigrationFinished(migrationTask.getIdentifier());
       return false;
     }
-  }
-
-  private void logMigrationTaskAlreadyExecuted(
-      final MigrationTask migrationTask, final int index, final int total) {
-    LOGGER.info(
-        "Migration was executed before "
-            + migrationTask.getIdentifier()
-            + " migration ("
-            + index
-            + "/"
-            + total
-            + ").  It does not need to run again.");
   }
 
   private void logMigrationSkipped(
