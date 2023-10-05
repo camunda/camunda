a315c5b88471a400fc06d8ff13666f633bb380b3
Josh Windels <joshua.windels@camunda.com>
2023-10-05 11:35:17 +0200

    chore(deps): update to alpine 3.18.4 in release job (#8872)

--- .ci/pipelines/release_optimize.groovy
+++ .ci/pipelines/release_optimize.groovy
@@ -442,7 +442,7 @@ pipeline {
           export VERSION=${VERSION}
           export DATE=${DATE}
           export REVISION=${REVISION}
-          export BASE_IMAGE=docker.io/library/alpine:3.18.3
+          export BASE_IMAGE=docker.io/library/alpine:3.18.4
           apk update
           apk add jq
 
