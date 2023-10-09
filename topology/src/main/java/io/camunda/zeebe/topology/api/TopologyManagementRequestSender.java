441bb3098f26be8ecfac441fc4f26b77901136ec
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-10-09 14:15:05 +0200

    feat(topology): add client that can send topology request to coordinator

---
+++ topology/src/main/java/io/camunda/zeebe/topology/api/TopologyManagementRequestSender.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.topology.api;
+
+import io.atomix.cluster.MemberId;
+import io.atomix.cluster.messaging.ClusterCommunicationService;
+import io.camunda.zeebe.scheduler.ConcurrencyControl;
+import io.camunda.zeebe.scheduler.future.ActorFuture;
+import io.camunda.zeebe.topology.api.TopologyManagementRequests.AddMembersRequest;
+import io.camunda.zeebe.topology.api.TopologyManagementResponses.TopologyChangeStatus;
+import io.camunda.zeebe.topology.serializer.TopologyRequestsSerializer;
+import java.time.Duration;
+
+/** Forwards all requests to the coordinator. */
+final class TopologyManagementRequestSender implements TopologyManagementAPI {
+  private static final Duration TIMEOUT = Duration.ofSeconds(10);
+  private final ClusterCommunicationService communicationService;
+  private final MemberId coordinator;
+  private final ConcurrencyControl executor;
+  private final TopologyRequestsSerializer serializer;
+
+  public TopologyManagementRequestSender(
+      final ClusterCommunicationService communicationService,
+      final MemberId coordinator,
+      final TopologyRequestsSerializer serializer,
+      final ConcurrencyControl executor) {
+    this.communicationService = communicationService;
+    this.coordinator = coordinator;
+    this.executor = executor;
+    this.serializer = serializer;
+  }
+
+  @Override
+  public ActorFuture<TopologyChangeStatus> addMembers(final AddMembersRequest addMembersRequest) {
+    final ActorFuture<TopologyChangeStatus> resultFuture = executor.createFuture();
+    final var responseFuture =
+        communicationService.send(
+            TopologyRequestTopics.ADD_MEMBER.topic(),
+            addMembersRequest,
+            serializer::encode,
+            serializer::decodeTopologyChangeStatus,
+            coordinator,
+            TIMEOUT);
+    responseFuture
+        .thenAccept(resultFuture::complete)
+        .exceptionally(
+            error -> {
+              resultFuture.completeExceptionally(error);
+              return null;
+            });
+    return resultFuture;
+  }
+}
