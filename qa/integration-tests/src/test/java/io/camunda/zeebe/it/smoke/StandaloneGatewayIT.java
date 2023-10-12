b9f3de0bab07727cf24b9f1376da2316a2cc6de8
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-10-12 09:30:39 +0200

    refactor(qa): use testing infrastructure

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/smoke/StandaloneGatewayIT.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/smoke/StandaloneGatewayIT.java
@@ -11,77 +11,47 @@ import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.gateway.StandaloneGateway;
-import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
+import io.camunda.zeebe.qa.util.cluster.TestStandaloneGateway;
+import io.camunda.zeebe.qa.util.cluster.TestZeebePort;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
+import io.camunda.zeebe.test.util.junit.AutoCloseResources;
+import io.camunda.zeebe.test.util.junit.AutoCloseResources.AutoCloseResource;
 import io.restassured.http.ContentType;
-import io.restassured.specification.RequestSpecification;
 import java.util.concurrent.TimeUnit;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.BeforeEach;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = StandaloneGateway.class)
-@ContextConfiguration(
-    initializers = {RandomPortInitializer.class, CollectorRegistryInitializer.class})
-@ActiveProfiles("test")
+@AutoCloseResources
+@ZeebeIntegration
 final class StandaloneGatewayIT {
 
-  @SuppressWarnings("unused")
-  @Autowired
-  private GatewayCfg config;
+  @TestZeebe(awaitReady = false, awaitCompleteTopology = false) // no brokers
+  private final TestStandaloneGateway gateway = new TestStandaloneGateway();
 
-  @SuppressWarnings("unused")
-  @LocalServerPort
-  private int managementPort;
+  @AutoCloseResource private ZeebeClient client;
+
+  @BeforeEach
+  void beforeEach() {
+    client = gateway.newClientBuilder().build();
+  }
 
   /** A simple smoke test which checks that the gateway can start and accept requests. */
   @SmokeTest
   void smokeTest() {
     // given
-    try (final var client =
-        ZeebeClient.newClientBuilder()
-            .usePlaintext()
-            .gatewayAddress("localhost:" + config.getNetwork().getPort())
-            .build()) {
-      // when
-      final var topology = client.newTopologyRequest().send();
+    // when
+    final var topology = client.newTopologyRequest().send();
 
-      // then
-      given()
-          .contentType(ContentType.JSON)
-          .port(managementPort)
-          .when()
-          .get("/actuator")
-          .then()
-          .statusCode(200);
+    // then
+    given()
+        .contentType(ContentType.JSON)
+        .port(gateway.mappedPort(TestZeebePort.MONITORING))
+        .when()
+        .get("/actuator")
+        .then()
+        .statusCode(200);
 
-      final var result = topology.join(5L, TimeUnit.SECONDS);
-      assertThat(result.getBrokers()).as("there are no known brokers").isEmpty();
-    }
-  }
-
-  @Test
-  void shouldCustomizeMonitoringPort() {
-    // given
-    final RequestSpecification gatewayServerSpec =
-        new RequestSpecBuilder()
-            .setContentType(ContentType.JSON)
-            .setPort(managementPort)
-            .addFilter(new ResponseLoggingFilter())
-            .addFilter(new RequestLoggingFilter())
-            .build();
-
-    // when - then
-    given().spec(gatewayServerSpec).when().get("/actuator").then().statusCode(200);
+    final var result = topology.join(5L, TimeUnit.SECONDS);
+    assertThat(result.getBrokers()).as("there are no known brokers").isEmpty();
   }
 }
