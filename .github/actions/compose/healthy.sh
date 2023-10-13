f178d5ddfe2199782d78328dcf6b3dec633dc1c1
Max Danilov <maxdanilov@users.noreply.github.com>
2023-10-13 16:54:08 +0200

    chore(ci): Add debugging commands for testing
    Related: https://camunda.slack.com/archives/C5AHF1D8T/p1697189518318749

--- .github/actions/compose/healthy.sh
+++ .github/actions/compose/healthy.sh
@@ -5,13 +5,17 @@
 # Edge case; Self-hosted runners don't support "docker compose" yet even though on v2
 VERSION=$(docker-compose version --short)
 
+echo "Detected docker-compose version: $VERSION"
+
 if [[ "$VERSION" =~ ^1\.[0-9]+\.[0-9]+ ]]; then
     # if docker-compose is v1, we're setting it to docker compose, which should be v2
-    echo "Deteceted v1, setting to v2"
+    echo "Detected v1, setting to v2"
+    echo "Using docker compose (not docker-compose): $(docker compose version)"
     DOCKER_COMMAND="docker compose -f ${FILE} ${COMPOSE_FLAGS}"
 else
     # e.g. locally or on self-hosted runners docker-compose can be v2
     echo "Detected v2"
+    echo "Using docker-compose (not docker compose): $(docker-compose version)"
     DOCKER_COMMAND="docker-compose -f ${FILE} ${COMPOSE_FLAGS}"
 fi
 
@@ -37,6 +41,7 @@ while [ $(date +%s) -lt $endTime ]; do
     echo -en "\rWaiting for services... $cnt/$(eval $DOCKER_COMMAND ps --format json | jq -n '[inputs] | flatten | .[].Status' | wc -l)"
     if [[ $cnt -eq $(eval $DOCKER_COMMAND ps --format json | jq -n '[inputs] | flatten | .[].Status' | wc -l) ]]; then
         echo ""
+        eval $DOCKER_COMMAND ps
         exit 0
     fi
     sleep 1
