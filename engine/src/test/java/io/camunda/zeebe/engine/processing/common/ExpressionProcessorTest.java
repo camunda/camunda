7f1eb36abb73e6b2140f416d6892021323dca550
Philipp Ossler <philipp.ossler@gmail.com>
2023-10-27 06:57:03 +0200

    test: Verify failure message with warnings

--- engine/src/test/java/io/camunda/zeebe/engine/processing/common/ExpressionProcessorTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/common/ExpressionProcessorTest.java
@@ -19,6 +19,7 @@ import io.camunda.zeebe.util.Either;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -87,6 +88,139 @@ class ExpressionProcessorTest {
           Arguments.of(
               "= [null]",
               "Expected result of the expression ' [null]' to be 'ARRAY' containing 'STRING' items, but was 'ARRAY' containing at least one non-'STRING' item."));
+    }
+  }
+
+  @Nested
+  @TestInstance(Lifecycle.PER_CLASS)
+  class EvaluationWarningsTest {
+
+    @Test
+    void testStringExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateStringExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be 'STRING', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testLongExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateLongExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be 'NUMBER', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testBooleanExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateBooleanExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be 'BOOLEAN', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testIntervalExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateIntervalExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be one of '[DURATION, PERIOD, STRING]', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testDateTimeExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateDateTimeExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be one of '[DATE_TIME, STRING]', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testArrayExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateArrayExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be 'ARRAY', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testStringArrayExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=[x]");
+      assertThat(processor.evaluateArrayOfStringsExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression '[x]' to be 'ARRAY' containing 'STRING' items, \
+              but was 'ARRAY' containing at least one non-'STRING' item. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testMessageCorrelationKeyExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateMessageCorrelationKeyExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Failed to extract the correlation key for 'x': \
+              The value must be either a string or a number, but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+
+    @Test
+    void testVariableMappingExpression() {
+      final var processor = new ExpressionProcessor(EXPRESSION_LANGUAGE, DEFAULT_CONTEXT_LOOKUP);
+      final var parsedExpression = EXPRESSION_LANGUAGE.parseExpression("=x");
+      assertThat(processor.evaluateVariableMappingExpression(parsedExpression, -1L))
+          .isLeft()
+          .extracting(r -> r.getLeft().getMessage())
+          .isEqualTo(
+              """
+              Expected result of the expression 'x' to be 'OBJECT', but was 'NULL'. \
+              The evaluation reported the following warnings: \
+              [NO_VARIABLE_FOUND] No variable found with name 'x'""");
     }
   }
 }
