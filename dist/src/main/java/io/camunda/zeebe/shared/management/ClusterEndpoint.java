654311fd4073ee91ae37e32d69f8ea32fd769a7c
Ole Schönburg <ole.schoenburg@gmail.com>
2023-10-30 10:51:29 +0100

    feat(dist): use cluster API response types from OpenAPI spec

--- dist/src/main/java/io/camunda/zeebe/shared/management/ClusterEndpoint.java
+++ dist/src/main/java/io/camunda/zeebe/shared/management/ClusterEndpoint.java
@@ -8,10 +8,28 @@
 package io.camunda.zeebe.shared.management;
 
 import io.atomix.cluster.MemberId;
+import io.camunda.zeebe.management.cluster.BrokerState;
+import io.camunda.zeebe.management.cluster.BrokerStateCode;
+import io.camunda.zeebe.management.cluster.Operation;
+import io.camunda.zeebe.management.cluster.Operation.OperationEnum;
+import io.camunda.zeebe.management.cluster.PartitionState;
+import io.camunda.zeebe.management.cluster.PartitionStateCode;
+import io.camunda.zeebe.management.cluster.PostOperationResponse;
+import io.camunda.zeebe.topology.api.TopologyChangeResponse;
 import io.camunda.zeebe.topology.api.TopologyManagementRequest.JoinPartitionRequest;
 import io.camunda.zeebe.topology.api.TopologyManagementRequest.LeavePartitionRequest;
 import io.camunda.zeebe.topology.api.TopologyManagementRequestSender;
+import io.camunda.zeebe.topology.state.MemberState;
+import io.camunda.zeebe.topology.state.PartitionState.State;
+import io.camunda.zeebe.topology.state.TopologyChangeOperation;
+import io.camunda.zeebe.topology.state.TopologyChangeOperation.MemberJoinOperation;
+import io.camunda.zeebe.topology.state.TopologyChangeOperation.MemberLeaveOperation;
+import io.camunda.zeebe.topology.state.TopologyChangeOperation.PartitionChangeOperation.PartitionJoinOperation;
+import io.camunda.zeebe.topology.state.TopologyChangeOperation.PartitionChangeOperation.PartitionLeaveOperation;
+import io.camunda.zeebe.topology.state.TopologyChangeOperation.PartitionChangeOperation.PartitionReconfigurePriorityOperation;
+import java.time.ZoneOffset;
 import java.util.List;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
 import org.springframework.boot.actuate.endpoint.annotation.Selector;
@@ -56,7 +74,7 @@ public class ClusterEndpoint {
   }
 
   @WriteOperation
-  public WebEndpointResponse<?> addSubResource(
+  public WebEndpointResponse<PostOperationResponse> addSubResource(
       @Selector final Resource resource,
       @Selector final String resourceId,
       @Selector final Resource subResource,
@@ -66,28 +84,30 @@ public class ClusterEndpoint {
       case BROKERS -> switch (subResource) {
         case PARTITIONS -> new WebEndpointResponse<>(
             // POST /cluster/brokers/1/partitions/2
-            requestSender
-                .joinPartition(
-                    new JoinPartitionRequest(
-                        MemberId.from(resourceId), Integer.parseInt(subResourceId), priority))
-                .join());
+            mapResponseType(
+                requestSender
+                    .joinPartition(
+                        new JoinPartitionRequest(
+                            MemberId.from(resourceId), Integer.parseInt(subResourceId), priority))
+                    .join()));
         case BROKERS -> new WebEndpointResponse<>(404);
       };
       case PARTITIONS -> switch (subResource) {
         case BROKERS -> new WebEndpointResponse<>(
             // POST /cluster/partitions/1/brokers/2
-            requestSender
-                .joinPartition(
-                    new JoinPartitionRequest(
-                        MemberId.from(subResourceId), Integer.parseInt(resourceId), priority))
-                .join());
+            mapResponseType(
+                requestSender
+                    .joinPartition(
+                        new JoinPartitionRequest(
+                            MemberId.from(subResourceId), Integer.parseInt(resourceId), priority))
+                    .join()));
         case PARTITIONS -> new WebEndpointResponse<>(404);
       };
     };
   }
 
   @DeleteOperation
-  public WebEndpointResponse<?> removeSubResource(
+  public WebEndpointResponse<PostOperationResponse> removeSubResource(
       @Selector final Resource resource,
       @Selector final String resourceId,
       @Selector final Resource subResource,
@@ -96,23 +116,106 @@ public class ClusterEndpoint {
       case BROKERS -> switch (subResource) {
         case PARTITIONS -> new WebEndpointResponse<>(
             // DELETE /cluster/brokers/1/partitions/2
-            requestSender
-                .leavePartition(
-                    new LeavePartitionRequest(
-                        MemberId.from(resourceId), Integer.parseInt(subResourceId)))
-                .join());
+            mapResponseType(
+                requestSender
+                    .leavePartition(
+                        new LeavePartitionRequest(
+                            MemberId.from(resourceId), Integer.parseInt(subResourceId)))
+                    .join()));
         case BROKERS -> new WebEndpointResponse<>(404);
       };
       case PARTITIONS -> switch (subResource) {
         case BROKERS -> new WebEndpointResponse<>(
             // DELETE /cluster/partitions/1/brokers/2
-            requestSender
-                .leavePartition(
-                    new LeavePartitionRequest(
-                        MemberId.from(subResourceId), Integer.parseInt(resourceId)))
-                .join());
+            mapResponseType(
+                requestSender
+                    .leavePartition(
+                        new LeavePartitionRequest(
+                            MemberId.from(subResourceId), Integer.parseInt(resourceId)))
+                    .join()));
         case PARTITIONS -> new WebEndpointResponse<>(404);
       };
+    };
+  }
+
+  private static PostOperationResponse mapResponseType(final TopologyChangeResponse response) {
+    return new PostOperationResponse()
+        .changeId((int) response.changeId())
+        .currentTopology(mapBrokerStates(response.currentTopology()))
+        .expectedTopology(mapBrokerStates(response.expectedTopology()))
+        .plannedChanges(mapOperations(response.plannedChanges()));
+  }
+
+  private static List<Operation> mapOperations(final List<TopologyChangeOperation> operations) {
+    return operations.stream().map(ClusterEndpoint::mapOperation).toList();
+  }
+
+  private static Operation mapOperation(final TopologyChangeOperation operation) {
+    return switch (operation) {
+      case final MemberJoinOperation join -> new Operation()
+          .operation(OperationEnum.BROKER_ADD)
+          .brokerId(Integer.parseInt(join.memberId().id()));
+      case final MemberLeaveOperation leave -> new Operation()
+          .operation(OperationEnum.BROKER_REMOVE)
+          .brokerId(Integer.parseInt(leave.memberId().id()));
+      case final PartitionJoinOperation join -> new Operation()
+          .operation(OperationEnum.PARTITION_JOIN)
+          .brokerId(Integer.parseInt(join.memberId().id()))
+          .partitionId(join.partitionId())
+          .priority(join.priority());
+      case final PartitionLeaveOperation leave -> new Operation()
+          .operation(OperationEnum.PARTITION_LEAVE)
+          .brokerId(Integer.parseInt(leave.memberId().id()))
+          .partitionId(leave.partitionId());
+      case final PartitionReconfigurePriorityOperation reconfigure -> new Operation()
+          .operation(OperationEnum.PARTITION_RECONFIGURE_PRIORITY)
+          .brokerId(Integer.parseInt(reconfigure.memberId().id()))
+          .partitionId(reconfigure.partitionId())
+          .priority(reconfigure.priority());
+    };
+  }
+
+  private static List<BrokerState> mapBrokerStates(final Map<MemberId, MemberState> topology) {
+    return topology.entrySet().stream()
+        .map(
+            entry ->
+                new BrokerState()
+                    .id(Integer.parseInt(entry.getKey().id()))
+                    .state(mapBrokerState(entry.getValue().state()))
+                    .lastUpdatedAt(entry.getValue().lastUpdated().atOffset(ZoneOffset.UTC))
+                    .version((int) entry.getValue().version())
+                    .partitions(mapPartitionStates(entry.getValue().partitions())))
+        .toList();
+  }
+
+  private static BrokerStateCode mapBrokerState(final MemberState.State state) {
+    return switch (state) {
+      case ACTIVE -> BrokerStateCode.ACTIVE;
+      case JOINING -> BrokerStateCode.JOINING;
+      case LEAVING -> BrokerStateCode.LEAVING;
+      case LEFT -> BrokerStateCode.LEFT;
+      case UNINITIALIZED -> BrokerStateCode.UNKNOWN;
+    };
+  }
+
+  private static List<PartitionState> mapPartitionStates(
+      final Map<Integer, io.camunda.zeebe.topology.state.PartitionState> partitions) {
+    return partitions.entrySet().stream()
+        .map(
+            entry ->
+                new PartitionState()
+                    .id(entry.getKey())
+                    .priority(entry.getValue().priority())
+                    .state(mapPartitionState(entry.getValue().state())))
+        .toList();
+  }
+
+  private static PartitionStateCode mapPartitionState(final State state) {
+    return switch (state) {
+      case JOINING -> PartitionStateCode.JOINING;
+      case ACTIVE -> PartitionStateCode.ACTIVE;
+      case LEAVING -> PartitionStateCode.LEAVING;
+      case UNKNOWN -> PartitionStateCode.UNKNOWN;
     };
   }
 
