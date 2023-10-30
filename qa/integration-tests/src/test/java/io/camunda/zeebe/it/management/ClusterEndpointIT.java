2f1c542d415ed828cd976c9bf61abc890973b9d6
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-10-30 15:49:49 +0100

    test(qa/integration-test): add integration test for query cluster topology

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/management/ClusterEndpointIT.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/management/ClusterEndpointIT.java
@@ -7,12 +7,13 @@
  */
 package io.camunda.zeebe.it.management;
 
+import static org.assertj.core.api.Assertions.*;
+
 import io.camunda.zeebe.management.cluster.Operation;
 import io.camunda.zeebe.management.cluster.Operation.OperationEnum;
 import io.camunda.zeebe.qa.util.actuator.ClusterActuator;
 import io.camunda.zeebe.qa.util.cluster.TestCluster;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
@@ -22,6 +23,32 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 @Execution(ExecutionMode.CONCURRENT)
 final class ClusterEndpointIT {
   private static final int BROKER_COUNT = 2;
+  private static final int PARTITION_COUNT = 2;
+
+  @Test
+  void shouldQueryCurrentClusterTopology() {
+    final ClusterActuator actuator;
+    try (final var cluster = createCluster(2)) {
+      // given
+      cluster.awaitCompleteTopology();
+      actuator = ClusterActuator.of(cluster.availableGateway());
+
+      // when
+      final var response = actuator.getTopology();
+
+      // then - topology is as expected
+      assertThat(response.getBrokers())
+          .hasSize(BROKER_COUNT)
+          .allSatisfy(
+              b ->
+                  assertThat(b.getPartitions())
+                      .describedAs("All brokers have two partitions each")
+                      .hasSize(PARTITION_COUNT));
+      assertThat(response.getChange().getId())
+          .describedAs("Initial topology has no completed or pending changes")
+          .isNull();
+    }
+  }
 
   @Test
   void shouldRequestPartitionLeave() {
@@ -34,7 +61,7 @@ final class ClusterEndpointIT {
       // when -- request a leave
       final var response = actuator.leavePartition(1, 2);
       // then
-      Assertions.assertThat(response.getPlannedChanges())
+      assertThat(response.getPlannedChanges())
           .singleElement()
           .asInstanceOf(InstanceOfAssertFactories.type(Operation.class))
           .returns(OperationEnum.PARTITION_LEAVE, Operation::getOperation)
@@ -52,7 +79,7 @@ final class ClusterEndpointIT {
       // when -- request a join
       final var response = actuator.joinPartition(0, 2, 3);
       // then
-      Assertions.assertThat(response.getPlannedChanges())
+      assertThat(response.getPlannedChanges())
           .singleElement()
           .asInstanceOf(InstanceOfAssertFactories.type(Operation.class))
           .returns(OperationEnum.PARTITION_JOIN, Operation::getOperation)
@@ -66,7 +93,7 @@ final class ClusterEndpointIT {
     return TestCluster.builder()
         .withEmbeddedGateway(true)
         .withBrokersCount(BROKER_COUNT)
-        .withPartitionsCount(2)
+        .withPartitionsCount(PARTITION_COUNT)
         .withReplicationFactor(replicationFactor)
         .withBrokerConfig(
             broker ->
