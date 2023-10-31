7d27ca653d3b4a2c147778c3f7679fc58f216b88
Oleksandr Kriuchenko <105281107+oleksandr-kriuchenko-lohika@users.noreply.github.com>
2023-10-31 09:32:25 +0200

    fix(backend): Fix DecisionWriterIT (#5711)
    Co-authored-by: Oleksandr Kriuchenko <okriuchenko@lohika.com>

--- qa/integration-tests/src/test/java/io/camunda/operate/it/DecisionWriterIT.java
+++ qa/integration-tests/src/test/java/io/camunda/operate/it/DecisionWriterIT.java
@@ -6,29 +6,22 @@
  */
 package io.camunda.operate.it;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
+import io.camunda.operate.elasticsearch.TestSearchRepository;
 import io.camunda.operate.exceptions.OperateRuntimeException;
 import io.camunda.operate.schema.indices.DecisionIndex;
 import io.camunda.operate.schema.indices.DecisionRequirementsIndex;
 import io.camunda.operate.schema.templates.DecisionInstanceTemplate;
 import io.camunda.operate.util.OperateZeebeAbstractIT;
+import io.camunda.operate.webapp.api.v1.entities.DecisionRequirements;
 import io.camunda.operate.webapp.writer.DecisionWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DecisionWriterIT extends OperateZeebeAbstractIT {
 
@@ -45,7 +38,7 @@ public class DecisionWriterIT extends OperateZeebeAbstractIT {
   private DecisionInstanceTemplate decisionInstanceTemplate;
 
   @Autowired
-  private RestHighLevelClient esClient;
+  private TestSearchRepository testSearchRepository;
 
   @Before
   public void before() {
@@ -108,10 +101,10 @@ public class DecisionWriterIT extends OperateZeebeAbstractIT {
 
   protected Long deployDecisionRequirements() {
     tester.deployDecision("invoiceBusinessDecisions_v_1.dmn").waitUntil().decisionsAreDeployed(2);
-    List<SearchHit> hits = searchAllDocuments(decisionRequirementsIndex.getAlias());
-    Map<String, Object> decisionRequirementsDoc = hits.get(0).getSourceAsMap();
-    long decisionRequirementsKey = Long.parseLong(decisionRequirementsDoc.get("key").toString());
-    return decisionRequirementsKey;
+
+    return searchAllDocuments(decisionRequirementsIndex.getAlias(), DecisionRequirements.class)
+      .get(0)
+      .getKey();
   }
 
   protected Long deployProcessWithDecision() {
@@ -131,11 +124,9 @@ public class DecisionWriterIT extends OperateZeebeAbstractIT {
     tester.waitUntil().decisionInstancesAreCreated(count);
   }
 
-  protected List<SearchHit> searchAllDocuments(String index) {
-    SearchRequest searchRequest = new SearchRequest(index).source(new SearchSourceBuilder().size(1000).query(QueryBuilders.matchAllQuery()));
+  protected <R> List<R> searchAllDocuments(String index, Class<R> clazz) {
     try {
-      SearchResponse response = esClient.search(searchRequest, RequestOptions.DEFAULT);
-      return Arrays.asList(response.getHits().getHits());
+      return testSearchRepository.searchAll(decisionRequirementsIndex.getAlias(), clazz);
     } catch (IOException ex) {
       throw new OperateRuntimeException("Search failed for index " + index, ex);
     }
