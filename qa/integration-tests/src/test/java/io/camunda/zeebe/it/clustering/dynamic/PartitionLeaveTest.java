9a14c24cff72f13642c0ac108b77d97bf3c7d215
Ole Schönburg <ole.schoenburg@gmail.com>
2023-11-02 13:41:14 +0100

    test(qa): rely on operation retry instead of waiting for a leader

--- qa/integration-tests/src/test/java/io/camunda/zeebe/it/clustering/dynamic/PartitionLeaveTest.java
+++ qa/integration-tests/src/test/java/io/camunda/zeebe/it/clustering/dynamic/PartitionLeaveTest.java
@@ -15,7 +15,6 @@ import static io.camunda.zeebe.it.clustering.dynamic.Utils.assertChangeIsPlanned
 import io.camunda.zeebe.management.cluster.PostOperationResponse;
 import io.camunda.zeebe.qa.util.actuator.ClusterActuator;
 import io.camunda.zeebe.qa.util.cluster.TestCluster;
-import io.camunda.zeebe.test.util.asserts.TopologyAssert;
 import java.time.Duration;
 import java.util.stream.Stream;
 import org.awaitility.Awaitility;
@@ -50,21 +49,12 @@ final class PartitionLeaveTest {
       Awaitility.await("Leaving is completed in time")
           .untilAsserted(() -> assertChangeIsCompleted(cluster, leave));
       assertChangeIsApplied(cluster, leave);
-      try (final var client = cluster.newClientBuilder().build()) {
-        Awaitility.await("All partition have a leader")
-            .pollDelay(Duration.ofSeconds(10))
-            .timeout(Duration.ofMinutes(1))
-            .untilAsserted(
-                () ->
-                    TopologyAssert.assertThat(client.newTopologyRequest().send().join())
-                        .hasLeaderForEachPartition(
-                            scenario.initialClusterState().partitionCount()));
-      }
       // when
       final var join = revertOperation(cluster, scenario.operation());
       // then
       assertChangeIsPlanned(join);
       Awaitility.await("Rejoining is completed in time")
+          .timeout(Duration.ofMinutes(1))
           .untilAsserted(() -> assertChangeIsCompleted(cluster, join));
       assertChangeIsApplied(cluster, join);
     }
