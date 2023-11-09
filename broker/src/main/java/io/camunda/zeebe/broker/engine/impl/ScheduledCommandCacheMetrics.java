b2bb6fba50584d2891f8bb1d120560dca45aeadd
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-11-09 11:34:37 +0100

    refactor(broker): add cache metrics

---
+++ broker/src/main/java/io/camunda/zeebe/broker/engine/impl/ScheduledCommandCacheMetrics.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.broker.engine.impl;
+
+import io.camunda.zeebe.protocol.record.intent.Intent;
+import io.prometheus.client.Gauge;
+import java.util.function.IntConsumer;
+
+/** Defines metrics for scheduled command cache implementations. */
+public interface ScheduledCommandCacheMetrics {
+
+  /**
+   * Returns a consumer for a given intent, which will be called whenever the underlying cache for
+   * this intent changes size.
+   */
+  IntConsumer forIntent(final Intent intent);
+
+  /**
+   * A metrics implementation specifically for the {@link
+   * io.camunda.zeebe.broker.engine.impl.BoundedScheduledCommandCache}.
+   */
+  class BoundedCommandCacheMetrics implements ScheduledCommandCacheMetrics {
+    private static final Gauge SIZE =
+        Gauge.build()
+            .namespace("zeebe")
+            .subsystem("stream_processor")
+            .name("scheduled_command_cache_size")
+            .labelNames("partitionId", "intent")
+            .help("Reports the size of each bounded cache per partition and intent")
+            .register();
+
+    private final String partitionId;
+
+    public BoundedCommandCacheMetrics(final int partitionId) {
+      this.partitionId = String.valueOf(partitionId);
+    }
+
+    @Override
+    public IntConsumer forIntent(final Intent intent) {
+      return SIZE.labels(partitionId, intent.name())::set;
+    }
+  }
+}
--- broker/src/test/java/io/camunda/zeebe/broker/engine/impl/BoundedCommandCacheTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/engine/impl/BoundedCommandCacheTest.java
@@ -10,6 +10,7 @@ package io.camunda.zeebe.broker.engine.impl;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.agrona.collections.LongHashSet;
 import org.junit.jupiter.api.Test;
 
@@ -17,7 +18,7 @@ final class BoundedCommandCacheTest {
   @Test
   void shouldNotExceedCapacity() {
     // given
-    final var cache = new BoundedCommandCache(4);
+    final var cache = new BoundedCommandCache();
     cache.add(setOf(1, 2, 3, 4));
 
     // when
@@ -27,6 +28,21 @@ final class BoundedCommandCacheTest {
     assertThat(cache.size()).isEqualTo(4);
     assertThat(cache.contains(5)).isTrue();
     assertThat(cache.contains(6)).isTrue();
+  }
+
+  @Test
+  void shouldReportSizeChanges() {
+    // given
+    final var reportedSize = new AtomicInteger();
+    final var cache = new BoundedCommandCache(reportedSize::set);
+
+    // when - then
+    cache.add(setOf(1, 2, 3, 4));
+    assertThat(reportedSize).hasValue(4);
+
+    // when - then
+    cache.remove(1);
+    assertThat(reportedSize).hasValue(3);
   }
 
   private LongHashSet setOf(final long... keys) {
