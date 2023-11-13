7068be51fc7d206be94077b09e8882b744610a9f
ana.vinogradova <ana.vinogradova@camunda.com>
2023-11-13 09:59:01 +0000

    feat(client): Add Job Update Timeout command to the Java Client
    - added 'newUpdateTimeoutCommand' to 'ZeebeClient' and
    implementation to 'ZeebeClientImpl'
    - added 'UpdateTimeoutJobCommandStep1'
    - added 'JobUpdateTimeoutCommandImpl'
    - added 'UpdateTimeoutJobResponse'
    - added 'UpdateTimeoutJobResponseImpl'

--- clients/java/src/main/java/io/camunda/zeebe/client/ZeebeClient.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/ZeebeClient.java
@@ -30,6 +30,7 @@ import io.camunda.zeebe.client.api.command.ResolveIncidentCommandStep1;
 import io.camunda.zeebe.client.api.command.SetVariablesCommandStep1;
 import io.camunda.zeebe.client.api.command.TopologyRequestStep1;
 import io.camunda.zeebe.client.api.command.UpdateRetriesJobCommandStep1;
+import io.camunda.zeebe.client.api.command.UpdateTimeoutJobCommandStep1;
 import io.camunda.zeebe.client.api.response.ActivatedJob;
 import io.camunda.zeebe.client.api.worker.JobClient;
 import io.camunda.zeebe.client.api.worker.JobWorkerBuilderStep1;
@@ -357,6 +358,10 @@ public interface ZeebeClient extends AutoCloseable, JobClient {
    * @return a builder for the command
    */
   UpdateRetriesJobCommandStep1 newUpdateRetriesCommand(ActivatedJob job);
+
+  UpdateTimeoutJobCommandStep1 newUpdateTimeoutCommand(long jobKey);
+
+  UpdateTimeoutJobCommandStep1 newUpdateTimeoutCommand(ActivatedJob job);
 
   /**
    * Registers a new job worker for jobs of a given type.
