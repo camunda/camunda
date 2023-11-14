0986f1272aafb5fdd3bdd89efcd4eec489c6a995
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-11-14 09:05:00 +0100

    test(topology): verify ongoing change can be cancelled

--- topology/src/test/java/io/camunda/zeebe/topology/changes/TopologyChangeCoordinatorImplTest.java
+++ topology/src/test/java/io/camunda/zeebe/topology/changes/TopologyChangeCoordinatorImplTest.java
@@ -16,9 +16,12 @@ import io.camunda.zeebe.scheduler.testing.TestConcurrencyControl;
 import io.camunda.zeebe.topology.ClusterTopologyAssert;
 import io.camunda.zeebe.topology.ClusterTopologyManager;
 import io.camunda.zeebe.topology.api.TopologyRequestFailedException;
+import io.camunda.zeebe.topology.api.TopologyRequestFailedException.InvalidRequest;
 import io.camunda.zeebe.topology.api.TopologyRequestFailedException.OperationNotAllowed;
 import io.camunda.zeebe.topology.changes.TopologyChangeCoordinator.TopologyChangeRequest;
+import io.camunda.zeebe.topology.state.ClusterChangePlan;
 import io.camunda.zeebe.topology.state.ClusterTopology;
+import io.camunda.zeebe.topology.state.CompletedChange;
 import io.camunda.zeebe.topology.state.MemberState;
 import io.camunda.zeebe.topology.state.PartitionState;
 import io.camunda.zeebe.topology.state.TopologyChangeOperation;
@@ -26,6 +29,7 @@ import io.camunda.zeebe.topology.state.TopologyChangeOperation.PartitionChangeOp
 import io.camunda.zeebe.topology.state.TopologyChangeOperation.PartitionChangeOperation.PartitionLeaveOperation;
 import io.camunda.zeebe.util.Either;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -179,7 +183,8 @@ final class TopologyChangeCoordinatorImplTest {
     // given
     clusterTopologyManager.setClusterTopology(ClusterTopology.init());
     final var coordinator =
-        new TopologyChangeCoordinatorImpl(clusterTopologyManager, new TestConcurrencyControl());
+        new TopologyChangeCoordinatorImpl(
+            clusterTopologyManager, syncRequester, new TestConcurrencyControl());
 
     // when
 
@@ -194,6 +199,73 @@ final class TopologyChangeCoordinatorImplTest {
         .withCauseInstanceOf(TopologyRequestFailedException.InvalidRequest.class)
         .withMessageContaining(
             "Expected to leave partition, but the local member does not exist in the topology");
+  }
+
+  @Test
+  void shouldCancelOngoingChange() {
+    // given
+    final ClusterTopology initialTopology =
+        ClusterTopology.init()
+            .addMember(MemberId.from("1"), MemberState.initializeAsActive(Map.of()))
+            .addMember(
+                MemberId.from("2"),
+                MemberState.initializeAsActive(Map.of(1, PartitionState.active(1))));
+    clusterTopologyManager.setClusterTopology(initialTopology);
+
+    final List<TopologyChangeOperation> operations =
+        List.of(new PartitionJoinOperation(MemberId.from("1"), 1, 1));
+
+    final var coordinator =
+        new TopologyChangeCoordinatorImpl(
+            clusterTopologyManager, syncRequester, new TestConcurrencyControl());
+    final var applyResult = coordinator.applyOperations(getTransformer(operations)).join();
+
+    // when
+    final var cancelFuture = coordinator.cancelChange(applyResult.changeId());
+
+    // then
+    assertThat(cancelFuture).succeedsWithin(Duration.ofMillis(100));
+    final ClusterTopology cancelledTopology = clusterTopologyManager.getClusterTopology().join();
+    assertThat(cancelledTopology.lastChange())
+        .isNotEmpty()
+        .get()
+        .extracting(CompletedChange::status)
+        .isEqualTo(ClusterChangePlan.Status.CANCELLED);
+    assertThat(cancelledTopology.pendingChanges()).isEmpty();
+    assertThat(cancelledTopology.members())
+        .usingRecursiveComparison()
+        .ignoringFieldsOfTypes(Instant.class)
+        .isEqualTo(initialTopology.members());
+  }
+
+  @Test
+  void shouldNotCancelOngoingChangeIdIsDifferent() {
+    // given
+    final ClusterTopology initialTopology =
+        ClusterTopology.init()
+            .addMember(MemberId.from("1"), MemberState.initializeAsActive(Map.of()))
+            .addMember(
+                MemberId.from("2"),
+                MemberState.initializeAsActive(Map.of(1, PartitionState.active(1))));
+    clusterTopologyManager.setClusterTopology(initialTopology);
+
+    final List<TopologyChangeOperation> operations =
+        List.of(new PartitionJoinOperation(MemberId.from("1"), 1, 1));
+
+    final var coordinator =
+        new TopologyChangeCoordinatorImpl(
+            clusterTopologyManager, syncRequester, new TestConcurrencyControl());
+    final var applyResult = coordinator.applyOperations(getTransformer(operations)).join();
+
+    // when
+    final var cancelFuture = coordinator.cancelChange(applyResult.changeId() - 1);
+
+    // then
+    assertThat(cancelFuture)
+        .failsWithin(Duration.ofMillis(100))
+        .withThrowableOfType(ExecutionException.class)
+        .withCauseInstanceOf(InvalidRequest.class)
+        .withMessageContaining("it is not the current change");
   }
 
   private static final class TestClusterTopologyManager implements ClusterTopologyManager {
