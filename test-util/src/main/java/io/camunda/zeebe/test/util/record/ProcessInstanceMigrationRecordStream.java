df45bf54266d525e1fb643e39a6e0cd8697251e8
Nico Korthout <nico.korthout@camunda.com>
2023-11-15 10:59:41 +0100

    feat(test-util): support pim record stream
    We need this record stream when we want to add test cases where we want
    to use the recording exporter to filter for records of the
    ProcessInstanceMigration ValueType.

---
+++ test-util/src/main/java/io/camunda/zeebe/test/util/record/ProcessInstanceMigrationRecordStream.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.test.util.record;
+
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.value.ProcessInstanceMigrationRecordValue;
+import java.util.stream.Stream;
+
+public final class ProcessInstanceMigrationRecordStream
+    extends ExporterRecordStream<
+        ProcessInstanceMigrationRecordValue, ProcessInstanceMigrationRecordStream> {
+
+  public ProcessInstanceMigrationRecordStream(
+      final Stream<Record<ProcessInstanceMigrationRecordValue>> records) {
+    super(records);
+  }
+
+  @Override
+  protected ProcessInstanceMigrationRecordStream supply(
+      final Stream<Record<ProcessInstanceMigrationRecordValue>> wrappedStream) {
+    return new ProcessInstanceMigrationRecordStream(wrappedStream);
+  }
+
+  public ProcessInstanceMigrationRecordStream withProcessInstanceKey(
+      final long processInstanceKey) {
+    return valueFilter(v -> v.getProcessInstanceKey() == processInstanceKey);
+  }
+}
