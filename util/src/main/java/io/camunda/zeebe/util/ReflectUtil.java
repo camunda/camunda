bbde6eb6cbfcf04422df3992fe309df2a074b25f
Ole Schönburg <ole.schoenburg@gmail.com>
2023-11-27 14:03:44 +0100

    test(topology): property test for persisting cluster topology

--- util/src/main/java/io/camunda/zeebe/util/ReflectUtil.java
+++ util/src/main/java/io/camunda/zeebe/util/ReflectUtil.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.util;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.stream.Stream;
 
 public final class ReflectUtil {
 
@@ -25,5 +26,23 @@ public final class ReflectUtil {
               "Failed to instantiate class %s with the default constructor", clazz.getName()),
           e);
     }
+  }
+
+  /**
+   * Returns a stream of all non-interface classes that are a subtypes of the given sealed class.
+   */
+  public static <T> Stream<Class<T>> implementationsOfSealedInterface(final Class<T> clazz) {
+    if (!clazz.isSealed()) {
+      throw new IllegalArgumentException(String.format("Class %s is not sealed", clazz.getName()));
+    }
+    return Stream.of(clazz.getPermittedSubclasses())
+        .flatMap(
+            c -> {
+              if (c.isSealed()) {
+                return implementationsOfSealedInterface((Class<T>) c);
+              } else {
+                return Stream.of((Class<T>) c);
+              }
+            });
   }
 }
