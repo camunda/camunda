3d9a0247c284c3b1da049d62eeb27f4b6c48c0f5
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2023-11-28 17:15:36 +0100

    refactor(engine): align processors with job potential
    All BPMN processors that potentially lead to job execution share common routines:
    - Check if the element is executed as job or natively
    - Delegate to job task processor if executed as job
    - Implement specific native logic if not executed as job
    
    The script, business rule, and user task processors share this behavior.
    To align those processors, the common actions mentioned above are rather extracted into a superclass.
    The processors inherit the common routines and focus on their specific behavior.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/task/UserTaskProcessor.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/task/UserTaskProcessor.java
@@ -8,28 +8,17 @@
 package io.camunda.zeebe.engine.processing.bpmn.task;
 
 import io.camunda.zeebe.engine.processing.bpmn.BpmnElementContext;
-import io.camunda.zeebe.engine.processing.bpmn.BpmnElementProcessor;
 import io.camunda.zeebe.engine.processing.bpmn.BpmnProcessingException;
 import io.camunda.zeebe.engine.processing.bpmn.behavior.BpmnBehaviors;
-import io.camunda.zeebe.engine.processing.bpmn.behavior.BpmnEventSubscriptionBehavior;
-import io.camunda.zeebe.engine.processing.bpmn.behavior.BpmnIncidentBehavior;
-import io.camunda.zeebe.engine.processing.bpmn.behavior.BpmnStateBehavior;
 import io.camunda.zeebe.engine.processing.bpmn.behavior.BpmnStateTransitionBehavior;
-import io.camunda.zeebe.engine.processing.bpmn.behavior.BpmnVariableMappingBehavior;
-import io.camunda.zeebe.engine.processing.common.EventTriggerBehavior;
-import io.camunda.zeebe.engine.processing.common.ExpressionProcessor;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableUserTask;
 
-public final class UserTaskProcessor implements BpmnElementProcessor<ExecutableUserTask> {
-
-  private final UserTaskBehavior zeebeUserTaskBehavior;
-  private final UserTaskBehavior jobWorkerTaskBehavior;
+public final class UserTaskProcessor extends PotentialJobTaskProcessor<ExecutableUserTask> {
 
   public UserTaskProcessor(
       final BpmnBehaviors bpmnBehaviors,
       final BpmnStateTransitionBehavior stateTransitionBehavior) {
-    zeebeUserTaskBehavior = new ZeebeUserTaskBehavior(bpmnBehaviors, stateTransitionBehavior);
-    jobWorkerTaskBehavior = new JobWorkerTaskBehavior(bpmnBehaviors, stateTransitionBehavior);
+    super(bpmnBehaviors, stateTransitionBehavior);
   }
 
   @Override
@@ -38,98 +27,33 @@ public final class UserTaskProcessor implements BpmnElementProcessor<ExecutableU
   }
 
   @Override
-  public void onActivate(final ExecutableUserTask element, final BpmnElementContext context) {
-    eventBehaviorOf(element, context).onActivate(element, context);
-  }
-
-  @Override
-  public void onComplete(final ExecutableUserTask element, final BpmnElementContext context) {
-    eventBehaviorOf(element, context).onComplete(element, context);
-  }
-
-  @Override
-  public void onTerminate(final ExecutableUserTask element, final BpmnElementContext context) {
-    eventBehaviorOf(element, context).onTerminate(element, context);
-  }
-
-  private UserTaskBehavior eventBehaviorOf(
+  protected boolean isJobBehavior(
       final ExecutableUserTask element, final BpmnElementContext context) {
-    if (element.getJobWorkerProperties() != null) {
-      return jobWorkerTaskBehavior;
-    } else if (element.getUserTaskProperties() != null) {
-      return zeebeUserTaskBehavior;
-    } else {
+    if (element.getUserTaskProperties() != null) {
+      return false;
+    }
+    if (element.getJobWorkerProperties() == null) {
       throw new BpmnProcessingException(
           context, "Expected to process user task, but could not determine processing behavior");
     }
+    return true;
   }
 
-  private static final class ZeebeUserTaskBehavior implements UserTaskBehavior {
-
-    private final BpmnIncidentBehavior incidentBehavior;
-    private final BpmnStateTransitionBehavior stateTransitionBehavior;
-    private final BpmnVariableMappingBehavior variableMappingBehavior;
-    private final BpmnEventSubscriptionBehavior eventSubscriptionBehavior;
-    private final BpmnStateBehavior stateBehavior;
-    private final ExpressionProcessor expressionProcessor;
-
-    private final EventTriggerBehavior eventTriggerBehavior;
-
-    public ZeebeUserTaskBehavior(
-        final BpmnBehaviors bpmnBehaviors,
-        final BpmnStateTransitionBehavior stateTransitionBehavior) {
-      eventSubscriptionBehavior = bpmnBehaviors.eventSubscriptionBehavior();
-      incidentBehavior = bpmnBehaviors.incidentBehavior();
-      this.stateTransitionBehavior = stateTransitionBehavior;
-      variableMappingBehavior = bpmnBehaviors.variableMappingBehavior();
-      stateBehavior = bpmnBehaviors.stateBehavior();
-      expressionProcessor = bpmnBehaviors.expressionBehavior();
-      eventTriggerBehavior = bpmnBehaviors.eventTriggerBehavior();
-    }
-
-    @Override
-    public void onActivate(final ExecutableUserTask element, final BpmnElementContext context) {}
-
-    @Override
-    public void onComplete(final ExecutableUserTask element, final BpmnElementContext context) {}
-
-    @Override
-    public void onTerminate(final ExecutableUserTask element, final BpmnElementContext context) {}
+  @Override
+  protected void onActivateInternal(
+      final ExecutableUserTask element, final BpmnElementContext context) {
+    // TODO will be added with https://github.com/camunda/zeebe/issues/15275
   }
 
-  private static final class JobWorkerTaskBehavior implements UserTaskBehavior {
-
-    private final JobWorkerTaskProcessor delegate;
-
-    public JobWorkerTaskBehavior(
-        final BpmnBehaviors bpmnBehaviors,
-        final BpmnStateTransitionBehavior stateTransitionBehavior) {
-      delegate = new JobWorkerTaskProcessor(bpmnBehaviors, stateTransitionBehavior);
-    }
-
-    @Override
-    public void onActivate(final ExecutableUserTask element, final BpmnElementContext activating) {
-      delegate.onActivate(element, activating);
-    }
-
-    @Override
-    public void onComplete(final ExecutableUserTask element, final BpmnElementContext completing) {
-      delegate.onComplete(element, completing);
-    }
-
-    @Override
-    public void onTerminate(
-        final ExecutableUserTask element, final BpmnElementContext terminating) {
-      delegate.onTerminate(element, terminating);
-    }
+  @Override
+  protected void onCompleteInternal(
+      final ExecutableUserTask element, final BpmnElementContext context) {
+    // TODO will be added with increment 3 of https://github.com/camunda/zeebe/issues/14938
   }
 
-  /** Extract different behaviors depending on the type of task. */
-  private interface UserTaskBehavior {
-    void onActivate(ExecutableUserTask element, BpmnElementContext activating);
-
-    void onComplete(ExecutableUserTask element, BpmnElementContext completing);
-
-    void onTerminate(ExecutableUserTask element, BpmnElementContext terminating);
+  @Override
+  protected void onTerminateInternal(
+      final ExecutableUserTask element, final BpmnElementContext context) {
+    // TODO will be added with increment 2 of https://github.com/camunda/zeebe/issues/14938
   }
 }
