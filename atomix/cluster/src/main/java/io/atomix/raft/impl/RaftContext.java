0de15459d1f5b1dcde1413e553b60af07e40a27c
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2023-11-29 13:45:49 +0100

    fix(raft): update role metrics when closing or transitioning to inactive
    (cherry picked from commit 35ea7ba3fefc06ddb12bf24b31062eeccd18b0f2)

--- atomix/cluster/src/main/java/io/atomix/raft/impl/RaftContext.java
+++ atomix/cluster/src/main/java/io/atomix/raft/impl/RaftContext.java
@@ -741,6 +741,7 @@ public class RaftContext implements AutoCloseable, HealthMonitorable {
   private RaftRole createRole(final Role role) {
     switch (role) {
       case INACTIVE:
+        raftRoleMetrics.becomingInactive();
         return new InactiveRole(this);
       case PASSIVE:
         return new PassiveRole(this);
@@ -803,6 +804,7 @@ public class RaftContext implements AutoCloseable, HealthMonitorable {
 
   @Override
   public void close() {
+    raftRoleMetrics.becomingInactive();
     started = false;
     // Unregister protocol listeners.
     unregisterHandlers(protocol);
