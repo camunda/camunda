aab5204eb81a59b09dfefc6828e8e852c9e148be
Nicola Puppa <nicola.puppa@camunda.com>
2023-11-30 13:59:48 +0100

    feat(engine): provide methods to restore backoff
    Added two methods to the JobState to retrieve failed jobs and the key
    of
    the jobs present in the backoff column
    Added also a method to modify
    the JobState by inserting missing job backoff record
    
    (cherry picked from commit 5a941aa748f509c151230e1147c34da3178762cc)

--- engine/src/main/java/io/camunda/zeebe/engine/state/instance/DbJobState.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/instance/DbJobState.java
@@ -23,8 +23,10 @@ import io.camunda.zeebe.engine.state.mutable.MutableJobState;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import io.camunda.zeebe.protocol.impl.record.value.job.JobRecord;
 import io.camunda.zeebe.util.EnsureUtil;
+import java.util.HashSet;
 import io.camunda.zeebe.util.buffer.BufferUtil;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 import org.agrona.DirectBuffer;
@@ -247,6 +249,21 @@ public final class DbJobState implements JobState, MutableJobState {
         });
   }
 
+  @Override
+  public void restoreBackoff() {
+    final var failedKeys = getFailedJobKeys();
+
+    failedKeys.removeAll(getBackoffJobKey());
+    failedKeys.forEach(
+        key -> {
+          jobKey.wrapLong(key);
+          final var jobRecord = jobsColumnFamily.get(jobKey);
+          if (jobRecord != null) {
+            addJobBackoff(key, jobRecord.getRecord().getRecurringTime());
+          }
+        });
+  }
+
   private void createJob(final long key, final JobRecord record, final DirectBuffer type) {
     createJobRecord(key, record);
     initializeJobState();
@@ -357,6 +374,11 @@ public final class DbJobState implements JobState, MutableJobState {
     return nextBackOffDueDate;
   }
 
+  @Override
+  public boolean isJobBackoffToRestore() {
+    return getFailedJobKeys().size() > getBackoffJobKey().size();
+  }
+
   boolean visitJob(final long jobKey, final BiPredicate<Long, JobRecord> callback) {
     final JobRecord job = getJob(jobKey);
     if (job == null) {
@@ -446,5 +468,23 @@ public final class DbJobState implements JobState, MutableJobState {
       backoffKey.wrapLong(backoff);
       backoffColumnFamily.deleteIfExists(backoffJobKey);
     }
+  }
+
+  private Set<Long> getFailedJobKeys() {
+    final Set<Long> failedJobKeys = new HashSet<>();
+    statesJobColumnFamily.forEach(
+        (key, value) -> {
+          if ((State.FAILED).equals(value.getState())) {
+            failedJobKeys.add(key.inner().getValue());
+          }
+        });
+    return failedJobKeys;
+  }
+
+  private Set<Long> getBackoffJobKey() {
+    final Set<Long> backoffJobKeys = new HashSet<>();
+    backoffColumnFamily.forEach(
+        (key, value) -> backoffJobKeys.add(key.second().inner().getValue()));
+    return backoffJobKeys;
   }
 }
