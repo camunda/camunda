8918eea1db39f354d3b02e25b9939d944a578ca0
Nicola Puppa <nicola.puppa@camunda.com>
2023-11-30 15:41:42 +0100

    feat(engine): create new backoff restore migration

---
+++ engine/src/main/java/io/camunda/zeebe/engine/state/migration/JobBackoffRestoreMigration.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.state.migration;
+
+import io.camunda.zeebe.engine.state.immutable.ProcessingState;
+import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
+
+public class JobBackoffRestoreMigration implements MigrationTask {
+
+  @Override
+  public String getIdentifier() {
+    return getClass().getSimpleName();
+  }
+
+  @Override
+  public boolean needsToRun(final ProcessingState processingState) {
+    // The migration should run only if there are failed jobs in the state, but the backoff column
+    // family is empty or
+    // the sie of the failed jobs is bigger than the size of the job in the backoff
+    return processingState.getJobState().isJobBackoffToRestore();
+  }
+
+  @Override
+  public void runMigration(final MutableProcessingState processingState) {
+    processingState.getJobState().restoreBackoff();
+  }
+}
