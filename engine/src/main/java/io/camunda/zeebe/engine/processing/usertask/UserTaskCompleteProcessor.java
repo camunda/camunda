e832d44b6aceeda0f3df0223fd17d5e7b6a7460e
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2023-12-07 15:06:39 +0100

    refactor(engine): extract user task command checks
    All user task commands need to fulfill a common set of preconditions.
    Those precondition checks are extracted into a reusable checker class.
    At this point, the checks compose of the following:
    * Ensure the task exists.
    * Ensure the task exists for the provided tenant id.
    * Ensure the task is in an allowed state for the command to be processed.

--- engine/src/main/java/io/camunda/zeebe/engine/processing/usertask/UserTaskCompleteProcessor.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/usertask/UserTaskCompleteProcessor.java
@@ -19,25 +19,20 @@ import io.camunda.zeebe.engine.state.immutable.UserTaskState;
 import io.camunda.zeebe.engine.state.immutable.UserTaskState.LifecycleState;
 import io.camunda.zeebe.engine.state.instance.ElementInstance;
 import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
-import io.camunda.zeebe.protocol.record.RejectionType;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.protocol.record.intent.UserTaskIntent;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
-import io.camunda.zeebe.util.Either;
-import io.camunda.zeebe.util.collection.Tuple;
+import java.util.List;
 
 public final class UserTaskCompleteProcessor implements TypedRecordProcessor<UserTaskRecord> {
 
-  private static final String NO_USER_TASK_FOUND_MESSAGE =
-      "Expected to %s user task with key '%d', but no such user task was found";
-  private static final String INVALID_USER_TASK_STATE_MESSAGE =
-      "Expected to %s user task with key '%d', but it is in state '%s'";
   private final UserTaskState userTaskState;
   private final ElementInstanceState elementInstanceState;
   private final EventHandle eventHandle;
   private final StateWriter stateWriter;
   private final TypedCommandWriter commandWriter;
   private final TypedRejectionWriter rejectionWriter;
+  private final UserTaskCommandPreconditionChecker preconditionChecker;
 
   public UserTaskCompleteProcessor(
       final ProcessingState state, final EventHandle eventHandle, final Writers writers) {
@@ -47,11 +42,15 @@ public final class UserTaskCompleteProcessor implements TypedRecordProcessor<Use
     stateWriter = writers.state();
     commandWriter = writers.command();
     rejectionWriter = writers.rejection();
+    preconditionChecker =
+        new UserTaskCommandPreconditionChecker(
+            List.of(LifecycleState.CREATED), "complete", userTaskState);
   }
 
   @Override
   public void processRecord(final TypedRecord<UserTaskRecord> userTaskRecord) {
-    checkTaskState(userTaskRecord)
+    preconditionChecker
+        .check(userTaskRecord)
         .ifRightOrLeft(
             ok -> {
               final long userTaskKey = userTaskRecord.getKey();
@@ -89,33 +88,5 @@ public final class UserTaskCompleteProcessor implements TypedRecordProcessor<Use
             userTaskElementInstance.getValue());
       }
     }
-  }
-
-  private Either<Tuple<RejectionType, String>, Void> checkTaskState(
-      final TypedRecord<UserTaskRecord> userTaskRecord) {
-    final long userTaskKey = userTaskRecord.getKey();
-    final UserTaskRecord persistedRecord =
-        userTaskState.getUserTask(userTaskKey, userTaskRecord.getAuthorizations());
-    if (persistedRecord == null) {
-      return Either.left(
-          Tuple.of(
-              RejectionType.NOT_FOUND,
-              String.format(NO_USER_TASK_FOUND_MESSAGE, UserTaskIntent.COMPLETE, userTaskKey)));
-    }
-
-    final LifecycleState lifecycleState = userTaskState.getLifecycleState(userTaskKey);
-
-    if (lifecycleState == LifecycleState.CREATED) {
-      return Either.right(null);
-    }
-
-    return Either.left(
-        Tuple.of(
-            RejectionType.INVALID_STATE,
-            String.format(
-                INVALID_USER_TASK_STATE_MESSAGE,
-                UserTaskIntent.COMPLETE,
-                userTaskKey,
-                lifecycleState)));
   }
 }
