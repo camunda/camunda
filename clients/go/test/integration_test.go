cc2f1a9d91a747df513938f3fbb08e9885315462
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-12-08 23:50:04 +0100

    test(clients): add integration test for StreamJobsCommand

--- clients/go/test/integration_test.go
+++ clients/go/test/integration_test.go
@@ -16,11 +16,17 @@ package test
 
 import (
 	"context"
-	"github.com/camunda/zeebe/clients/go/v8/internal/containersuite"
-	"github.com/camunda/zeebe/clients/go/v8/pkg/zbc"
-	"github.com/stretchr/testify/suite"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"testing"
 	"time"
+
+	"github.com/camunda/zeebe/clients/go/v8/internal/containersuite"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/entities"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/zbc"
+	"github.com/stretchr/testify/suite"
 )
 
 type integrationTestSuite struct {
@@ -245,6 +251,85 @@ func (s *integrationTestSuite) TestActivateJobs() {
 	}
 }
 
+func (s *integrationTestSuite) TestStreamJobs() {
+	// given
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	deployment, err := s.client.NewDeployResourceCommand().AddResourceFile("testdata/service_task.bpmn").Send(ctx)
+	if err != nil {
+		s.T().Fatal(err)
+	}
+
+	deployedResource := deployment.GetDeployments()[0]
+	s.NotNil(deployedResource)
+
+	process := deployedResource.GetProcess()
+	s.NotNil(process)
+
+	// when
+	// the stream will be closed by the deferred context cancellation
+	ctx, cancel = context.WithCancel(context.Background())
+	defer cancel()
+	jobsChan := make(chan *entities.Job)
+	go s.client.NewStreamJobsCommand().JobType("task").Consumer(jobsChan).Timeout(time.Minute * 5).
+		WorkerName("worker").RequestTimeout(time.Duration(1) * time.Minute).Send(ctx)
+
+	// Await until the stream is created on the broker
+	// TODO: find a less verbose way of doing this
+	streamExists := false
+	for start := time.Now(); !streamExists && time.Since(start) < 5*time.Second; {
+		response, err := http.Get(fmt.Sprintf("http://%s/actuator/jobstreams/remote", s.MonitoringAddress))
+		if err != nil {
+			time.Sleep(1)
+			continue
+		}
+
+		remoteStreams := make([]remoteJobStream, 1)
+		responseData, err := io.ReadAll(response.Body)
+		if err != nil {
+			time.Sleep(1)
+			continue
+		}
+
+		err = json.Unmarshal(responseData, &remoteStreams)
+		if err != nil {
+			time.Sleep(1)
+			continue
+		}
+
+		streamExists = len(remoteStreams) > 0
+	}
+
+	s.True(streamExists, "Expected remote stream to exist on broker after 5 seconds, but none yet exist")
+
+	// create two PIs and expect two jobs
+	_, err = s.client.NewCreateInstanceCommand().ProcessDefinitionKey(process.GetProcessDefinitionKey()).Send(ctx)
+	s.NoError(err)
+	_, err = s.client.NewCreateInstanceCommand().ProcessDefinitionKey(process.GetProcessDefinitionKey()).Send(ctx)
+	s.NoError(err)
+
+	// then - expect two jobs
+	jobs := make([]*entities.Job, 0)
+	for i := 0; i < 2; i++ {
+		select {
+		case job, ok := <-jobsChan:
+			if ok {
+				jobs = append(jobs, job)
+			}
+			break
+		}
+	}
+	s.Len(jobs, 2, "Expected to receive 2 jobs")
+
+	for _, job := range jobs {
+		s.EqualValues(process.GetProcessDefinitionKey(), job.GetProcessDefinitionKey())
+		s.EqualValues(process.GetBpmnProcessId(), job.GetBpmnProcessId())
+		s.EqualValues("service_task", job.GetElementId())
+		s.Greater(job.GetRetries(), int32(0))
+	}
+}
+
 func (s *integrationTestSuite) TestFailJob() {
 	// given
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -291,4 +376,21 @@ func (s *integrationTestSuite) TestFailJob() {
 			s.T().Fatal("Empty fail job response")
 		}
 	}
+}
+
+type remoteJobStream struct {
+	JobType   string                    `json:"jobType"`
+	Metadata  remoteJobStreamMetadata   `json:"metadata"`
+	Consumers []remoteJobStreamConsumer `json:"consumers"`
+}
+
+type remoteJobStreamMetadata struct {
+	Worker         string   `json:"worker"`
+	Timeout        string   `json:"timeout"`
+	FetchVariables []string `json:"fetchVariables"`
+}
+
+type remoteJobStreamConsumer struct {
+	Id       string `json:"id"`
+	Receiver string `json:"receiver"`
 }
