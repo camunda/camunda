ddbaa5a694c5d382af76a12c970bf621a9407828
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2023-12-19 21:10:06 +0100

    test(clients): add slow worker/backpressure tests for Go

--- clients/go/test/integration_test.go
+++ clients/go/test/integration_test.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -31,6 +32,8 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
+const dockerImageName = "camunda/zeebe:current-test"
+
 type integrationTestSuite struct {
 	*containersuite.ContainerSuite
 	client zbc.Client
@@ -40,7 +43,7 @@ func TestIntegration(t *testing.T) {
 	suite.Run(t, &integrationTestSuite{
 		ContainerSuite: &containersuite.ContainerSuite{
 			WaitTime:       time.Second,
-			ContainerImage: "camunda/zeebe:current-test",
+			ContainerImage: dockerImageName,
 		},
 	})
 }
@@ -66,6 +69,7 @@ func (s *integrationTestSuite) TearDownSuite() {
 
 	s.ContainerSuite.TearDownSuite()
 }
+
 func (s *integrationTestSuite) TestTopology() {
 	// when
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -259,7 +263,7 @@ func (s *integrationTestSuite) TestStreamJobs() {
 	defer cancel()
 
 	taskType := uuid.NewString()
-	bpmn, err := s.readBpmnWithCustomJobType("testdata/service_task.bpmn", taskType)
+	bpmn, err := readBpmnWithCustomJobType("testdata/service_task.bpmn", taskType)
 	s.NoError(err)
 
 	deployment, err := s.client.NewDeployResourceCommand().AddResource(bpmn, "service_task.bpmn").Send(ctx)
@@ -369,7 +373,7 @@ func (s *integrationTestSuite) TestStreamingJobWorker() {
 	defer cancel()
 
 	taskType := uuid.NewString()
-	bpmn, err := s.readBpmnWithCustomJobType("testdata/service_task.bpmn", taskType)
+	bpmn, err := readBpmnWithCustomJobType("testdata/service_task.bpmn", taskType)
 	s.NoError(err)
 
 	deployment, err := s.client.NewDeployResourceCommand().AddResource(bpmn, "service_task.bpmn").Send(ctx)
@@ -439,7 +443,119 @@ func (s *integrationTestSuite) TestStreamingJobWorker() {
 	}
 }
 
-func (s integrationTestSuite) readBpmnWithCustomJobType(path string, jobType string) ([]byte, error) {
+type slowWorkerSuite struct {
+	*integrationTestSuite
+}
+
+func TestSlowWorker(t *testing.T) {
+	suite.Run(t, &slowWorkerSuite{
+		integrationTestSuite: &integrationTestSuite{
+			ContainerSuite: &containersuite.ContainerSuite{
+				WaitTime:       time.Second,
+				ContainerImage: dockerImageName,
+				Env: map[string]string{
+					"ZEEBE_DEBUG":     "true",
+					"ZEEBE_LOG_LEVEL": "debug",
+				},
+			},
+		},
+	})
+}
+
+func (s *slowWorkerSuite) TestSlowStreamingJobWorker() {
+	// given
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	taskType := uuid.NewString()
+	bpmn, err := readBpmnWithCustomJobType("testdata/service_task.bpmn", taskType)
+	s.NoError(err)
+
+	deployment, err := s.client.NewDeployResourceCommand().AddResource(bpmn, "service_task.bpmn").Send(ctx)
+	if err != nil {
+		s.T().Fatal(err)
+	}
+
+	deployedResource := deployment.GetDeployments()[0]
+	s.NotNil(deployedResource)
+
+	process := deployedResource.GetProcess()
+	s.NotNil(process)
+
+	didYield := atomic.Bool{}
+	s.ContainerSuite.ConsumeLogs(func(log string) {
+		if strings.Contains(log, "\"valueType\":\"JOB\"") && strings.Contains(log, "\"intent\":\"YIELDED\"") {
+			didYield.Store(true)
+		}
+	})
+
+	// when
+	// the stream will be closed by the deferred context cancellation
+	ctx, cancel = context.WithCancel(context.Background())
+	defer cancel()
+
+	jobsChan := make(chan entities.Job)
+	defer close(jobsChan)
+
+	workerName := uuid.New().String()
+	jobWorker := s.client.NewJobWorker().
+		JobType(taskType).
+		Handler(func(client worker.JobClient, job entities.Job) {
+			jobsChan <- job
+		}).
+		Timeout(time.Minute * 5).
+		PollInterval(1 * time.Hour). // poll very slowly to make sure we get our jobs from streaming
+		Name(workerName).
+		Concurrency(1).
+		MaxJobsActive(1).
+		StreamEnabled(true).
+		RequestTimeout(time.Duration(1) * time.Second).
+		Open()
+	defer jobWorker.Close()
+
+	// Await until the stream is created on the broker
+	streamExists := utils.AwaitJobStreamExists(workerName, s.MonitoringAddress)
+	s.True(streamExists, "Expected remote stream to exist on broker after 5 seconds, but none yet exist")
+
+	// create PIs until we start yielding; since we only yield once buffers fill up, it's hard to predict how fast
+	// it will take to do so. we can cheat by using 32KB has our payload size, which at the time of writing, is
+	// the hard-coded  threshold for a gRPC stream to become not ready
+	variables := map[string]interface{}{"foo": strings.Repeat("x", 32*1024)}
+	for start := time.Now(); !didYield.Load() && time.Since(start) < 30*time.Second; {
+		cmd, err := s.client.NewCreateInstanceCommand().ProcessDefinitionKey(process.GetProcessDefinitionKey()).VariablesFromMap(variables)
+		s.Require().NoError(err)
+		_, err = cmd.Send(ctx)
+		s.Require().NoError(err)
+		time.Sleep(100 * time.Millisecond)
+	}
+	s.Require().Truef(didYield.Load(), "Expected to have yielded at least one job within 30 seconds, but did not; did back pressure not kick in?")
+
+	// then - consume any remaining jobs
+	jobs := make([]entities.Job, 0)
+jobConsumingLoop:
+	for {
+		select {
+		case job := <-jobsChan:
+			jobs = append(jobs, job)
+		case <-time.After(5 * time.Second):
+			break jobConsumingLoop
+		}
+	}
+
+	s.Require().NotEmptyf(jobs, "Expected to receive some jobs")
+	jobKeys := make([]int64, 0)
+	for _, job := range jobs {
+		s.NotContains(jobKeys, job.Key)
+		s.EqualValues(process.GetProcessDefinitionKey(), job.GetProcessDefinitionKey())
+		s.EqualValues(process.GetBpmnProcessId(), job.GetBpmnProcessId())
+		s.EqualValues("service_task", job.GetElementId())
+		s.Greater(job.GetRetries(), int32(0))
+
+		jobKeys = append(jobKeys, job.Key)
+	}
+}
+
+func readBpmnWithCustomJobType(path string, jobType string) ([]byte, error) {
 	b, err := os.ReadFile(path)
 	if err != nil {
 		return nil, err
