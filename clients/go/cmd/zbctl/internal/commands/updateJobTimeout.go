a778bb362696984a6c1a96e4844c37ae1adbdc7b
ana.vinogradova <ana.vinogradova@camunda.com>
2023-12-20 10:07:12 +0000

    feat(client): disable linting

--- clients/go/cmd/zbctl/internal/commands/updateJobTimeout.go
+++ clients/go/cmd/zbctl/internal/commands/updateJobTimeout.go
@@ -12,9 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package commands
-
 //nolint:dupl
+package commands
 
 import (
 	"context"
