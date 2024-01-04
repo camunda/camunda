25d6c2de3e374c73de22dc920d18ad43df1be93e
Patrick Dehn <pedesen@users.noreply.github.com>
2024-01-04 10:02:48 +0100

    chore: update Zeebe and Identity to 8.4.0 (#6135)

--- .ci/pipelines/create_elasticsearch_snapshot_generated_data.groovy
+++ .ci/pipelines/create_elasticsearch_snapshot_generated_data.groovy
@@ -113,7 +113,7 @@ spec:
           cpu: 2
           memory: 4Gi
     - name: zeebe
-      image: camunda/zeebe:8.4.0-alpha2
+      image: camunda/zeebe:8.4.0
       #imagePullPolicy: Always   #this must be uncommented when snapshot is used
       env:
         - name: ZEEBE_BROKER_EXPORTERS_ELASTICSEARCH_CLASSNAME
