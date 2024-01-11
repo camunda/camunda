3a4e2a2302b771d5810e9b23b88ae796ae93634f
Michał Konopski <michal.konopski@camunda.com>
2024-01-11 18:12:44 +0100

    feat(publish mddal): use carbon components
    related to #11150

--- client/src/components/Events/PublishModal.test.tsx
+++ client/src/components/Events/PublishModal.test.tsx
@@ -9,10 +9,10 @@ import {runLastEffect} from '__mocks__/react';
 import {shallow} from 'enzyme';
 
 import {showError, addNotification} from 'notifications';
-import {User} from 'HOC';
+import {useErrorHandling} from 'hooks';
 
 import {publish, getUsers} from './service';
-import {PublishModal} from './PublishModal';
+import PublishModal from './PublishModal';
 
 jest.mock('./service', () => ({
   publish: jest.fn(),
@@ -26,20 +26,25 @@ jest.mock('./service', () => ({
   ]),
 }));
 
+jest.mock('hooks', () => ({
+  useErrorHandling: jest.fn(() => ({
+    mightFail: jest.fn().mockImplementation((data, cb) => cb(data)),
+  })),
+  useUser: jest.fn().mockImplementation(() => ({
+    user: {id: 'demo'},
+  })),
+}));
+
 jest.mock('notifications', () => ({
   showError: jest.fn(),
   addNotification: jest.fn(),
 }));
 
 const props = {
-  user: {id: 'demo'} as User,
-  getUser: jest.fn(),
-  refreshUser: jest.fn(),
   id: 'processId',
   onPublish: jest.fn(),
   onClose: jest.fn(),
   republish: false,
-  mightFail: jest.fn().mockImplementation((data, cb) => cb(data)),
 };
 
 it('should show different text depending on the republish prop', () => {
@@ -73,13 +78,11 @@ it('should show a success notification', () => {
 });
 
 it('should show an error message', () => {
+  (useErrorHandling as jest.Mock).mockImplementation(() => ({
+    mightFail: (promise: any, cb: any, err: any) => err('errorMessage'),
+  }));
   (showError as jest.Mock).mockClear();
-  const node = shallow(
-    <PublishModal
-      {...props}
-      mightFail={jest.fn().mockImplementation((data, cb, err) => err('errorMessage'))}
-    />
-  );
+  const node = shallow(<PublishModal {...props} />);
   node.find('.confirm').simulate('click');
 
   expect(showError).toHaveBeenCalled();
