e65662a6b4cb96549d2e281e243737fd32f46e40
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-01-15 07:54:02 +0100

    refactor: simplify setting up old health routes

--- dist/src/main/java/io/camunda/zeebe/broker/health/BrokerHealthRoutes.java
+++ dist/src/main/java/io/camunda/zeebe/broker/health/BrokerHealthRoutes.java
@@ -8,66 +8,34 @@
 package io.camunda.zeebe.broker.health;
 
 import io.camunda.zeebe.shared.management.ConditionalOnManagementContext;
-import java.util.Map;
+import java.net.URI;
 import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
 import org.springframework.boot.actuate.autoconfigure.web.ManagementContextType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import reactor.core.publisher.Mono;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Profile("broker")
 @ManagementContextConfiguration(value = ManagementContextType.ANY, proxyBeanMethods = false)
 public class BrokerHealthRoutes {
 
-  private static final Map<String, String> HEALTH_REDIRECTS =
-      Map.of(
-          "/health",
-          "/actuator/health/liveness",
-          "/ready",
-          "/actuator/health/readiness",
-          "/startup",
-          "/actuator/health/startup");
-
-  private final WebFilter redirects;
-
-  public BrokerHealthRoutes() {
-    this(new CustomRedirect(HEALTH_REDIRECTS));
-  }
-
-  public BrokerHealthRoutes(final WebFilter redirects) {
-    this.redirects = redirects;
-  }
-
   @Bean
   @ConditionalOnManagementContext
-  public WebFilter healthRedirects() {
-    return redirects;
-  }
-
-  @SuppressWarnings("ClassCanBeRecord")
-  private static final class CustomRedirect implements WebFilter {
-    private final Map<String, String> redirects;
-
-    private CustomRedirect(final Map<String, String> redirects) {
-      this.redirects = redirects;
-    }
-
-    @SuppressWarnings("NullableProblems")
-    @Override
-    public Mono<Void> filter(
-        final ServerWebExchange exchange, final WebFilterChain webFilterChain) {
-      final var request = exchange.getRequest();
-      final var path = request.getPath().value();
-      final var redirect = redirects.get(path);
-      if (redirect != null) {
-        final var redirected = exchange.mutate().request(builder -> builder.path(redirect)).build();
-        return webFilterChain.filter(redirected);
-      }
-
-      return webFilterChain.filter(exchange);
-    }
+  public RouterFunction<ServerResponse> routes() {
+    return RouterFunctions.route()
+        .GET(
+            "/health",
+            req ->
+                ServerResponse.permanentRedirect(URI.create("/actuator/health/liveness")).build())
+        .GET(
+            "/ready",
+            req ->
+                ServerResponse.permanentRedirect(URI.create("/actuator/health/readiness")).build())
+        .GET(
+            "/startup",
+            req -> ServerResponse.permanentRedirect(URI.create("/actuator/health/startup")).build())
+        .build();
   }
 }
