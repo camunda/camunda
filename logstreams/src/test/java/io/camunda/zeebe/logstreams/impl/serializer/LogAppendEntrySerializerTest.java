0d26a0d7f2672c8e58ece7d8f1f917438b07950e
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-01-18 15:56:17 +0100

    test(logstreams): add regression test for new metadata size
    (cherry picked from commit 8dcee42c164387f40f478f6e31703b43b0db26a4)

--- logstreams/src/test/java/io/camunda/zeebe/logstreams/impl/serializer/LogAppendEntrySerializerTest.java
+++ logstreams/src/test/java/io/camunda/zeebe/logstreams/impl/serializer/LogAppendEntrySerializerTest.java
@@ -14,6 +14,11 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import io.camunda.zeebe.logstreams.impl.log.LoggedEventImpl;
 import io.camunda.zeebe.logstreams.log.LogAppendEntry;
 import io.camunda.zeebe.logstreams.util.TestEntry;
+import io.camunda.zeebe.msgpack.property.StringProperty;
+import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
+import io.camunda.zeebe.protocol.impl.record.UnifiedRecordValue;
+import io.camunda.zeebe.test.util.junit.RegressionTest;
+import io.camunda.zeebe.util.buffer.BufferUtil;
 import org.agrona.ExpandableArrayBuffer;
 import org.agrona.MutableDirectBuffer;
 import org.junit.jupiter.api.Test;
@@ -33,6 +38,7 @@ final class LogAppendEntrySerializerTest {
     // then
     event.wrap(writeBuffer, 0);
     assertThatEntry(entry).matchesLoggedEvent(event);
+    assertThat(event.getVersion()).isEqualTo((short) 1);
     assertThat(event.getKey()).isEqualTo(1);
     assertThat(event.getPosition()).isEqualTo(2);
     assertThat(event.getSourceEventPosition()).isEqualTo(3);
@@ -98,5 +104,48 @@ final class LogAppendEntrySerializerTest {
     // when - then
     assertThatCode(() -> LogAppendEntrySerializer.serialize(writeBuffer, 0, entry, -1, 3, 4))
         .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @RegressionTest("https://github.com/camunda/zeebe/issues/15989")
+  void shouldWriteLargeMetadata() {
+    // given
+    final var rejection = "foo".repeat(Short.MAX_VALUE * 2);
+    final var entry = TestEntry.builder().withRecordValue(new TestValue().setFoo("bar")).build();
+    final var event = new LoggedEventImpl();
+    final var metadata = new RecordMetadata();
+    final var value = new TestValue();
+    entry.recordMetadata().rejectionReason(rejection);
+
+    // when
+    final var serializedLength =
+        LogAppendEntrySerializer.serialize(
+            writeBuffer, 0, entry, 0, -1, System.currentTimeMillis());
+    event.wrap(writeBuffer, 0);
+    event.readMetadata(metadata);
+    event.readValue(value);
+
+    // then
+    assertThat(serializedLength).isGreaterThan(Short.MAX_VALUE);
+    assertThat(metadata.getRejectionReason()).isEqualTo(rejection);
+    assertThat(value.getFoo()).isEqualTo("bar");
+  }
+
+  private static final class TestValue extends UnifiedRecordValue {
+
+    private final StringProperty foo = new StringProperty("foo");
+
+    private TestValue() {
+      super(1);
+      declareProperty(foo);
+    }
+
+    private String getFoo() {
+      return BufferUtil.bufferAsString(foo.getValue());
+    }
+
+    private TestValue setFoo(final String value) {
+      foo.setValue(value);
+      return this;
+    }
   }
 }
