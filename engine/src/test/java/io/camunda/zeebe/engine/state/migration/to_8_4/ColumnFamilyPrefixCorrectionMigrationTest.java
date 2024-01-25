09482c63a2e3d1c4760e55ab3a72e571e09fefd6
Nico Korthout <nico.korthout@camunda.com>
2024-01-25 17:30:39 +0100

    test: verify column family 49 moves
    (cherry picked from commit 26cb24b5a6665e348349dd57386abf876d3dc9ab)

--- engine/src/test/java/io/camunda/zeebe/engine/state/migration/to_8_4/ColumnFamilyPrefixCorrectionMigrationTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/state/migration/to_8_4/ColumnFamilyPrefixCorrectionMigrationTest.java
@@ -13,9 +13,11 @@ import io.camunda.zeebe.db.ZeebeDb;
 import io.camunda.zeebe.db.impl.DbCompositeKey;
 import io.camunda.zeebe.db.impl.DbInt;
 import io.camunda.zeebe.db.impl.DbLong;
+import io.camunda.zeebe.db.impl.DbNil;
 import io.camunda.zeebe.db.impl.DbString;
 import io.camunda.zeebe.engine.state.message.DbMessageState;
 import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.ColumnFamily48Corrector;
+import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.ColumnFamily49Corrector;
 import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
 import io.camunda.zeebe.engine.util.ProcessingStateExtension;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
@@ -162,6 +164,130 @@ public class ColumnFamilyPrefixCorrectionMigrationTest {
           .isNotNull()
           .extracting(DbLong::getValue)
           .isEqualTo(123L);
+    }
+  }
+
+  /**
+   * Test correction from
+   * DEPRECATED_DMN_DECISION_REQUIREMENTS_KEY_BY_DECISION_REQUIREMENT_ID_AND_VERSION ->
+   * PROCESS_INSTANCE_KEY_BY_DEFINITION_KEY
+   */
+  @Nested
+  @ExtendWith(ProcessingStateExtension.class)
+  class ColumnFamily49CorrectorTestTest {
+    private ZeebeDb<ZbColumnFamilies> zeebeDb;
+    private MutableProcessingState processingState;
+    private TransactionContext transactionContext;
+
+    private ColumnFamily49Corrector sut;
+
+    private DbLong elementInstanceKey;
+    private DbLong processDefinitionKey;
+    private DbCompositeKey<DbLong, DbLong> processInstanceKeyByProcessDefinitionKey;
+
+    private ColumnFamily<DbCompositeKey<DbLong, DbLong>, DbNil> wrongPiKeyByProcDefKeyColumnFamily;
+    private ColumnFamily<DbCompositeKey<DbLong, DbLong>, DbNil>
+        correctPiKeyByProcDefKeyColumnFamily;
+
+    private DbString decisionRequirementsId;
+    private DbInt decisionRequirementsVersion;
+    private DbCompositeKey<DbString, DbInt> decisionRequirementsIdAndVersion;
+    private DbLong decisionRequirementsKey;
+
+    private ColumnFamily<DbCompositeKey<DbString, DbInt>, DbLong>
+        correctDecisionRequirementsKeyColumnFamily;
+
+    @BeforeEach
+    void setup() {
+      sut = new ColumnFamily49Corrector(zeebeDb, transactionContext);
+
+      elementInstanceKey = new DbLong();
+      processDefinitionKey = new DbLong();
+      processInstanceKeyByProcessDefinitionKey =
+          new DbCompositeKey<>(processDefinitionKey, elementInstanceKey);
+      wrongPiKeyByProcDefKeyColumnFamily =
+          zeebeDb.createColumnFamily(
+              ZbColumnFamilies
+                  .DEPRECATED_DMN_DECISION_REQUIREMENTS_KEY_BY_DECISION_REQUIREMENT_ID_AND_VERSION,
+              transactionContext,
+              processInstanceKeyByProcessDefinitionKey,
+              DbNil.INSTANCE);
+
+      correctPiKeyByProcDefKeyColumnFamily =
+          zeebeDb.createColumnFamily(
+              ZbColumnFamilies.PROCESS_INSTANCE_KEY_BY_DEFINITION_KEY,
+              transactionContext,
+              processInstanceKeyByProcessDefinitionKey,
+              DbNil.INSTANCE);
+
+      decisionRequirementsId = new DbString();
+      decisionRequirementsVersion = new DbInt();
+      decisionRequirementsIdAndVersion =
+          new DbCompositeKey<>(decisionRequirementsId, decisionRequirementsVersion);
+      decisionRequirementsKey = new DbLong();
+      correctDecisionRequirementsKeyColumnFamily =
+          zeebeDb.createColumnFamily(
+              ZbColumnFamilies
+                  .DEPRECATED_DMN_DECISION_REQUIREMENTS_KEY_BY_DECISION_REQUIREMENT_ID_AND_VERSION,
+              transactionContext,
+              decisionRequirementsIdAndVersion,
+              decisionRequirementsKey);
+    }
+
+    @Test
+    void shouldMovePiKeyByProcDefKeyToCorrectColumnFamily() {
+      // given
+      elementInstanceKey.wrapLong(123);
+      processDefinitionKey.wrapLong(456);
+      wrongPiKeyByProcDefKeyColumnFamily.insert(
+          processInstanceKeyByProcessDefinitionKey, DbNil.INSTANCE);
+
+      // when
+      sut.correctColumnFamilyPrefix();
+
+      // then
+      Assertions.assertThat(wrongPiKeyByProcDefKeyColumnFamily.isEmpty()).isTrue();
+      Assertions.assertThat(
+              correctPiKeyByProcDefKeyColumnFamily.exists(processInstanceKeyByProcessDefinitionKey))
+          .isTrue();
+    }
+
+    @Test
+    void shouldIgnoreProcessInstanceKeyByDefinitionKeyEntries() {
+      // given
+      decisionRequirementsId.wrapString("decisionRequirements");
+      decisionRequirementsVersion.wrapInt(1);
+      decisionRequirementsKey.wrapLong(543);
+      correctDecisionRequirementsKeyColumnFamily.insert(
+          decisionRequirementsIdAndVersion, decisionRequirementsKey);
+
+      elementInstanceKey.wrapLong(123);
+      processDefinitionKey.wrapLong(456);
+      wrongPiKeyByProcDefKeyColumnFamily.insert(
+          processInstanceKeyByProcessDefinitionKey, DbNil.INSTANCE);
+
+      decisionRequirementsId.wrapString("decisionRequirements2");
+      decisionRequirementsVersion.wrapInt(2);
+      decisionRequirementsKey.wrapLong(987);
+      correctDecisionRequirementsKeyColumnFamily.insert(
+          decisionRequirementsIdAndVersion, decisionRequirementsKey);
+
+      Assertions.assertThat(correctDecisionRequirementsKeyColumnFamily.count()).isEqualTo(3);
+
+      // when
+      sut.correctColumnFamilyPrefix();
+
+      // then
+      // we can no longer use wrongPiKeyByProcDefKeyColumnFamily.isEmpty() as there are entries in
+      // there just no longer process instance keys by process definition key entries, but we can
+      // simply count the entries
+      Assertions.assertThat(correctDecisionRequirementsKeyColumnFamily.count()).isEqualTo(2);
+
+      elementInstanceKey.wrapLong(123);
+      processDefinitionKey.wrapLong(456);
+      Assertions.assertThat(
+              correctPiKeyByProcDefKeyColumnFamily.exists(processInstanceKeyByProcessDefinitionKey))
+          .isTrue();
     }
   }
 }
