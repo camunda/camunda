d02c87463a4d617753fd587059c3da7e6dbfade4
Nico Korthout <nico.korthout@camunda.com>
2024-01-25 16:33:52 +0100

    style: simplify names
    a class name of 80 chars is too much for me
    
    (cherry picked from commit 2f462dabd4ac45e9a8a47dd15db470ef49094cf7)

--- engine/src/main/java/io/camunda/zeebe/engine/state/migration/to_8_4/DbColumnFamilyCorrectionMigrationState.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/migration/to_8_4/DbColumnFamilyCorrectionMigrationState.java
@@ -9,35 +9,27 @@ package io.camunda.zeebe.engine.state.migration.to_8_4;
 
 import io.camunda.zeebe.db.TransactionContext;
 import io.camunda.zeebe.db.ZeebeDb;
-import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.DmnDecisionKeyByDecisionIdAndVersionColumnFamilyCorrector;
-import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.DmnDecisionRequirementsKeyByDecisionRequirementIdAndVersionColumnFamilyCorrector;
-import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.SignalNameAndSubscriptionKeyColumnFamilyCorrector;
+import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.ColumnFamily48Corrector;
+import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.ColumnFamily49Corrector;
+import io.camunda.zeebe.engine.state.migration.to_8_4.corrections.ColumnFamily50Corrector;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
 
 public final class DbColumnFamilyCorrectionMigrationState {
 
-  private final SignalNameAndSubscriptionKeyColumnFamilyCorrector
-      signalNameAndSubscriptionKeyColumnFamilyCorrector;
-  private final DmnDecisionKeyByDecisionIdAndVersionColumnFamilyCorrector
-      dmnDecisionKeyByDecisionIdAndVersionColumnFamilyCorrector;
-  private final DmnDecisionRequirementsKeyByDecisionRequirementIdAndVersionColumnFamilyCorrector
-      dmnDecisionRequirementsKeyByDecisionRequirementIdAndVersionColumnFamilyCorrector;
+  private final ColumnFamily48Corrector columnFamily48Corrector;
+  private final ColumnFamily49Corrector columnFamily49Corrector;
+  private final ColumnFamily50Corrector columnFamily50Corrector;
 
   public DbColumnFamilyCorrectionMigrationState(
       final ZeebeDb<ZbColumnFamilies> zeebeDb, final TransactionContext transactionContext) {
-    signalNameAndSubscriptionKeyColumnFamilyCorrector =
-        new SignalNameAndSubscriptionKeyColumnFamilyCorrector(zeebeDb, transactionContext);
-    dmnDecisionKeyByDecisionIdAndVersionColumnFamilyCorrector =
-        new DmnDecisionKeyByDecisionIdAndVersionColumnFamilyCorrector(zeebeDb, transactionContext);
-    dmnDecisionRequirementsKeyByDecisionRequirementIdAndVersionColumnFamilyCorrector =
-        new DmnDecisionRequirementsKeyByDecisionRequirementIdAndVersionColumnFamilyCorrector(
-            zeebeDb, transactionContext);
+    columnFamily48Corrector = new ColumnFamily48Corrector(zeebeDb, transactionContext);
+    columnFamily49Corrector = new ColumnFamily49Corrector(zeebeDb, transactionContext);
+    columnFamily50Corrector = new ColumnFamily50Corrector(zeebeDb, transactionContext);
   }
 
   public void correctColumnFamilyPrefix() {
-    signalNameAndSubscriptionKeyColumnFamilyCorrector.correctColumnFamilyPrefix();
-    dmnDecisionKeyByDecisionIdAndVersionColumnFamilyCorrector.correctColumnFamilyPrefix();
-    dmnDecisionRequirementsKeyByDecisionRequirementIdAndVersionColumnFamilyCorrector
-        .correctColumnFamilyPrefix();
+    columnFamily48Corrector.correctColumnFamilyPrefix();
+    columnFamily49Corrector.correctColumnFamilyPrefix();
+    columnFamily50Corrector.correctColumnFamilyPrefix();
   }
 }
