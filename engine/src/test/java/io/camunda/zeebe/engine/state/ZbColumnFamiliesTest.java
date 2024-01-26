5ce69fa95a83f511f5eb9e8733bb3b6ed7ef196c
Nico Korthout <nico.korthout@camunda.com>
2024-01-26 12:52:03 +0100

    test(protocol): remove redundant test case
    This test case is no longer needed. From now on, the ZbColumnFamilies
    values must remain the same over time (we have hardcoded them) and must
    be unique (there is another test case that checks this). But there is no
    need anymore for the values to match exactly the ordinals.
    
    (cherry picked from commit 759d3b056e974765feb184795c94b7836ddc5a79)

--- engine/src/test/java/io/camunda/zeebe/engine/state/ZbColumnFamiliesTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/state/ZbColumnFamiliesTest.java
@@ -12,21 +12,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 class ZbColumnFamiliesTest {
-
-  /**
-   * This is just a temporary test to ensure that the enum values are not reordered, when we
-   * re-implment the getValue() method.
-   */
-  @ParameterizedTest
-  @MethodSource("values")
-  void shouldReturnOrdinalForEnumValue(final ZbColumnFamilies columnFamily) {
-    assertThat(columnFamily.getValue()).isEqualTo(columnFamily.ordinal());
-  }
 
   @Test
   void shouldNotReuseEnumValues() {
