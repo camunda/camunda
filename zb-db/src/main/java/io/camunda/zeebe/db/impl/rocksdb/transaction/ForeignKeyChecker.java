1ee57fed71a0fc2830b323b4c29e190e1ea309d8
Nico Korthout <nico.korthout@camunda.com>
2024-01-26 13:24:41 +0100

    fix(zb-db): use static value in foreignkey lookup
    As database entries are stored with the column family's value prefixed
    to the key, so too do we look them up with that value when checking
    foreign key constraints.
    
    We assume that the foreignKey.columnFamily() returns an instance of
    EnumValue so we can call the .getValue() on it. This is safe to do
    because the column family must anyways be an instance extending Enum<?
    extends EnumValue>.
    
    Alternatively, we could've avoided this cast if we would make the
    foreign key understand that the column family type also actually
    implements EnumValue, using an intersection type. However, that would've
    required adding this intersection type as an additional generic on the
    DbForeignKey<DbKey, CFType>. As this class is used often in our state
    classes, it would deteriorate the readability of those classes, as we
    currently can refer to it as DbForeignKey<DbKey>. This is why I went for
    casting rather than more generics.
    
    (cherry picked from commit aa61c8b82d9c8e95bcc7886bc33c7e6e1dee0a0b)
    
    There was a conflict in the following file because 8.1 does not yet
    support multi-tenancy, but this test class contained some changes in
    tests related to multi-tenancy.
    
    Conflicts:
      zb-db/src/test/java/io/camunda/zeebe/db/impl/ForeignKeyTest.java

--- zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/transaction/ForeignKeyChecker.java
+++ zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/transaction/ForeignKeyChecker.java
@@ -13,6 +13,7 @@ import io.camunda.zeebe.db.DbKey;
 import io.camunda.zeebe.db.ZeebeDbInconsistentException;
 import io.camunda.zeebe.db.impl.DbForeignKey;
 import io.camunda.zeebe.db.impl.ZeebeDbConstants;
+import io.camunda.zeebe.protocol.EnumValue;
 import io.camunda.zeebe.util.buffer.BufferUtil;
 import org.agrona.ExpandableArrayBuffer;
 
@@ -48,7 +49,8 @@ public final class ForeignKeyChecker {
       return;
     }
 
-    keyBuffer.putLong(0, foreignKey.columnFamily().ordinal(), ZeebeDbConstants.ZB_DB_BYTE_ORDER);
+    final int columnFamilyValue = ((EnumValue) foreignKey.columnFamily()).getValue();
+    keyBuffer.putLong(0, columnFamilyValue, ZeebeDbConstants.ZB_DB_BYTE_ORDER);
     foreignKey.write(keyBuffer, Long.BYTES);
     final var keyBufferLength = Long.BYTES + foreignKey.getLength();
 
