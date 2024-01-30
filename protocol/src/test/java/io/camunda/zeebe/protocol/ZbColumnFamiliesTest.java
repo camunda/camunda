1a36072406986ea74f4557ebb42530d99483c3a0
Nico Korthout <nico.korthout@camunda.com>
2024-01-30 15:09:53 +0100

    test(procotol): check enum values sequence
    This makes it almost impossible to make mistakes, because we can no
    longer accidentally skip an enum value.
    
    (cherry picked from commit fb9bab08a15c23c3e4007107548feb416cf26fdd)

--- protocol/src/test/java/io/camunda/zeebe/protocol/ZbColumnFamiliesTest.java
+++ protocol/src/test/java/io/camunda/zeebe/protocol/ZbColumnFamiliesTest.java
@@ -18,6 +18,7 @@ package io.camunda.zeebe.protocol;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.Arguments;
@@ -28,6 +29,15 @@ class ZbColumnFamiliesTest {
   void shouldNotReuseEnumValues() {
     assertThat(Arrays.stream(ZbColumnFamilies.values()).map(ZbColumnFamilies::getValue))
         .doesNotHaveDuplicates();
+  }
+
+  /** If this test case fails, you can update ZbColumnFamilies to include all known enum values. */
+  @Test
+  void shouldNotSkipEnumValues() {
+    assertThat(
+            Arrays.stream(ZbColumnFamilies.values()).mapToInt(ZbColumnFamilies::getValue).toArray())
+        .describedAs("The enum values must be sequential")
+        .isEqualTo(IntStream.range(0, ZbColumnFamilies.values().length).toArray());
   }
 
   public static Stream<Arguments> values() {
