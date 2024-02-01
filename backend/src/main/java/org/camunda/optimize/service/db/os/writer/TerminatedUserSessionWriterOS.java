9caaa9a5884cfe2bd0e9229148b94c6f99254288
Ruslan Kovalenko <ruslan.kovalenko-ext@camunda.com>
2024-02-01 11:38:09 +0200

    fix-terminated-user-session-removing (#11454)
    Co-authored-by: kovaIenko <“ruslan.kovalenko-ext@camunda.com”>

--- backend/src/main/java/org/camunda/optimize/service/db/os/writer/TerminatedUserSessionWriterOS.java
+++ backend/src/main/java/org/camunda/optimize/service/db/os/writer/TerminatedUserSessionWriterOS.java
@@ -48,7 +48,7 @@ public class TerminatedUserSessionWriterOS extends TerminatedUserSessionWriter {
   @Override
   protected void performDeleteTerminatedUserSessionOlderThan(final OffsetDateTime timestamp) throws IOException {
     final Query filterQuery =
-      QueryDSL.gt(TerminatedUserSessionIndex.TERMINATION_TIMESTAMP, dateTimeFormatter.format(timestamp));
+      QueryDSL.lte(TerminatedUserSessionIndex.TERMINATION_TIMESTAMP, dateTimeFormatter.format(timestamp));
 
     osClient.deleteByQuery(filterQuery, true, TERMINATED_USER_SESSION_INDEX_NAME);
   }
