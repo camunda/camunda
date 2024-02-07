4d91ecd4cbf3b109de3f23d4ea8abd3390296a08
ana.vinogradova <ana.vinogradova@camunda.com>
2024-02-07 15:24:39 +0000

    feat(processing): update BpmnUserTaskBehavior
    - userTaskBehavior.createNewUserTask now returns userTaskRecord
    - creation timestamp is set to the current value of currentTimeMillis()
    - writeUserTaskEvent method is removed as it is not needed anymore
    - userTaskCreated method is removed

--- engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnUserTaskBehavior.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnUserTaskBehavior.java
@@ -26,6 +26,7 @@ import io.camunda.zeebe.msgpack.value.DocumentValue;
 import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
 import io.camunda.zeebe.protocol.record.intent.UserTaskIntent;
 import io.camunda.zeebe.protocol.record.value.ErrorType;
+import io.camunda.zeebe.scheduler.clock.ActorClock;
 import io.camunda.zeebe.stream.api.state.KeyGenerator;
 import io.camunda.zeebe.util.Either;
 import java.time.ZonedDateTime;
@@ -93,21 +94,30 @@ public final class BpmnUserTaskBehavior {
                     .map(p::externalFormReference));
   }
 
-  public long createNewUserTask(
+  public UserTaskRecord createNewUserTask(
       final BpmnElementContext context,
       final ExecutableUserTask element,
       final UserTaskProperties userTaskProperties) {
     final var userTaskKey = keyGenerator.nextKey();
-    writeUserTaskEvent(userTaskKey, context, element, UserTaskIntent.CREATING, userTaskProperties);
-    return userTaskKey;
-  }
 
-  public void userTaskCreated(
-      final long userTaskKey,
-      final BpmnElementContext context,
-      final ExecutableUserTask element,
-      final UserTaskProperties userTaskProperties) {
-    writeUserTaskEvent(userTaskKey, context, element, UserTaskIntent.CREATED, userTaskProperties);
+    userTaskRecord
+        .setUserTaskKey(userTaskKey)
+        .setAssignee(userTaskProperties.getAssignee())
+        .setCandidateGroups(userTaskProperties.getCandidateGroups())
+        .setCandidateUsers(userTaskProperties.getCandidateUsers())
+        .setDueDate(userTaskProperties.getDueDate())
+        .setFollowUpDate(userTaskProperties.getFollowUpDate())
+        .setFormKey(userTaskProperties.getFormKey())
+        .setBpmnProcessId(context.getBpmnProcessId())
+        .setProcessDefinitionVersion(context.getProcessVersion())
+        .setProcessDefinitionKey(context.getProcessDefinitionKey())
+        .setProcessInstanceKey(context.getProcessInstanceKey())
+        .setElementId(element.getId())
+        .setElementInstanceKey(context.getElementInstanceKey())
+        .setTenantId(context.getTenantId())
+        .setCreationTimestamp(ActorClock.currentTimeMillis());
+
+    return userTaskRecord;
   }
 
   public Either<Failure, String> evaluateAssigneeExpression(
@@ -202,30 +212,8 @@ public final class BpmnUserTaskBehavior {
     }
   }
 
-  private void writeUserTaskEvent(
-      final long userTaskKey,
-      final BpmnElementContext context,
-      final ExecutableUserTask userTask,
-      final UserTaskIntent intent,
-      final UserTaskProperties props) {
-
-    userTaskRecord
-        .setUserTaskKey(userTaskKey)
-        .setAssignee(props.getAssignee())
-        .setCandidateGroups(props.getCandidateGroups())
-        .setCandidateUsers(props.getCandidateUsers())
-        .setDueDate(props.getDueDate())
-        .setFollowUpDate(props.getFollowUpDate())
-        .setFormKey(props.getFormKey())
-        .setExternalFormReference(props.getExternalFormReference())
-        .setBpmnProcessId(context.getBpmnProcessId())
-        .setProcessDefinitionVersion(context.getProcessVersion())
-        .setProcessDefinitionKey(context.getProcessDefinitionKey())
-        .setProcessInstanceKey(context.getProcessInstanceKey())
-        .setElementId(userTask.getId())
-        .setElementInstanceKey(context.getElementInstanceKey())
-        .setTenantId(context.getTenantId());
-
+  public void appendFollowUpEvent(
+      final long userTaskKey, final UserTaskIntent intent, final UserTaskRecord userTaskRecord) {
     stateWriter.appendFollowUpEvent(userTaskKey, intent, userTaskRecord);
   }
 
