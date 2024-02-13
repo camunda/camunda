10ec0dfcaba20626bfd98fdd34ff9c367cba3205
Nico Korthout <nico.korthout@camunda.com>
2024-02-13 08:40:58 +0100

    test: only assert events
    Like the test changed in the previous commit, this test case was also
    asserting events and commands in a certain order unnecessarily. This
    test case doesn't have to assert commands, because it simply wants to
    verify that the process instance is created as expected. That we use a
    command in between the activation and completion of the start event is
    irrelevant as its an implementation detail.
    
    (cherry picked from commit 83448f920cb0a94cbf5d58946a9dd6e477a6fccc)

--- engine/src/test/java/io/camunda/zeebe/engine/processing/multitenancy/TenantAwareTimerStartEventTest.java
+++ engine/src/test/java/io/camunda/zeebe/engine/processing/multitenancy/TenantAwareTimerStartEventTest.java
@@ -106,6 +106,7 @@ public class TenantAwareTimerStartEventTest {
     // then
     assertThat(
             RecordingExporter.processInstanceRecords()
+                .onlyEvents()
                 .withProcessDefinitionKey(processDefinitionKey)
                 .withElementType(BpmnElementType.START_EVENT)
                 .withEventType(BpmnEventType.TIMER)
@@ -114,7 +115,6 @@ public class TenantAwareTimerStartEventTest {
         .containsSequence(
             tuple(TENANT, ProcessInstanceIntent.ELEMENT_ACTIVATING),
             tuple(TENANT, ProcessInstanceIntent.ELEMENT_ACTIVATED),
-            tuple(TENANT, ProcessInstanceIntent.COMPLETE_ELEMENT),
             tuple(TENANT, ProcessInstanceIntent.ELEMENT_COMPLETING),
             tuple(TENANT, ProcessInstanceIntent.ELEMENT_COMPLETED));
   }
