0a1fe06ad65445538f247496630d709869d7f20d
Ole Schönburg <ole.schoenburg@gmail.com>
2024-02-15 15:24:44 +0100

    refactor(stream-platform): allow retrieving record metadata
    (cherry picked from commit e93c4598a3e96e40982369403e44b91adc3772db)

--- stream-platform/src/main/java/io/camunda/zeebe/stream/impl/records/TypedRecordImpl.java
+++ stream-platform/src/main/java/io/camunda/zeebe/stream/impl/records/TypedRecordImpl.java
@@ -37,6 +37,10 @@ public final class TypedRecordImpl implements TypedRecord {
     this.value = value;
   }
 
+  public RecordMetadata getMetadata() {
+    return metadata;
+  }
+
   @Override
   public long getPosition() {
     return rawEvent.getPosition();
