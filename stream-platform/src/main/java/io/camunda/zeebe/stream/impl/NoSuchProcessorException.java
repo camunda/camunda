a8669827956acfab98e4275224b0bac438502b61
Ole Schönburg <ole.schoenburg@gmail.com>
2024-02-15 14:56:55 +0100

    refactor(stream-processor): `NoSuchProcessorException` supports arbitrary record types
    (cherry picked from commit 888c83f1ec71e9ddaddba7b4ae33abf0ec87517a)

--- stream-platform/src/main/java/io/camunda/zeebe/stream/impl/NoSuchProcessorException.java
+++ stream-platform/src/main/java/io/camunda/zeebe/stream/impl/NoSuchProcessorException.java
@@ -16,7 +16,20 @@ import io.camunda.zeebe.util.exception.UnrecoverableException;
  */
 final class NoSuchProcessorException extends UnrecoverableException {
 
-  NoSuchProcessorException(final TypedRecord<?> command) {
-    super("No processor registered for command type %s".formatted(command.getValueType()));
+  private NoSuchProcessorException(final String message) {
+    super(message);
+  }
+
+  public static NoSuchProcessorException forRecord(final TypedRecord<?> record) {
+    final var message =
+        switch (record.getRecordType()) {
+          case EVENT ->
+              "No processor registered for event type %s".formatted(record.getValueType());
+          case COMMAND ->
+              "No processor registered for command type %s".formatted(record.getValueType());
+          case COMMAND_REJECTION, SBE_UNKNOWN, NULL_VAL ->
+              ("No processor registered for record type %s").formatted(record.getRecordType());
+        };
+    return new NoSuchProcessorException(message);
   }
 }
