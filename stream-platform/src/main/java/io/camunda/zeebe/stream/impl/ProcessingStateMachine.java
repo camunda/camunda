834a53f38567716050943456b5d56072995d0623
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2024-02-22 13:28:51 +0100

    fix(stream-platform): reset error state only after processing current command is completed
    (cherry picked from commit 352d6ceb2b7d485faf9dca02bd480d70742efde4)

--- stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingStateMachine.java
+++ stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingStateMachine.java
@@ -200,18 +200,21 @@ public final class ProcessingStateMachine {
 
   private void skipRecord() {
     notifySkippedListener(currentRecord);
-    inProcessing = false;
+    markProcessingCompleted();
     actor.submit(this::readNextRecord);
     metrics.eventSkipped();
   }
 
   void readNextRecord() {
+    tryToReadNextRecord();
+  }
+
+  void markProcessingCompleted() {
+    inProcessing = false;
     if (onErrorRetries > 0) {
       onErrorRetries = 0;
       errorHandlingPhase = ErrorHandlingPhase.NO_ERROR;
     }
-
-    tryToReadNextRecord();
   }
 
   private void tryToReadNextRecord() {
@@ -700,7 +703,7 @@ public final class ProcessingStateMachine {
           processingTimer.close();
 
           // continue with next record
-          inProcessing = false;
+          markProcessingCompleted();
           actor.submit(this::readNextRecord);
         });
   }
