82e702e023eabbd1a26a66e47c2687808ec917bd
Nicola Puppa <nicola.puppa@camunda.com>
2024-02-23 16:47:29 +0100

    fix: apply PR suggestions
    Rename variables and methods, add more accurate assertions
    
    (cherry picked from commit 88e030e9d03e28f7916401bac6202d444ece3461)

--- engine/src/main/java/io/camunda/zeebe/engine/state/instance/DbJobState.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/instance/DbJobState.java
@@ -255,7 +255,7 @@ public final class DbJobState implements JobState, MutableJobState {
   public void restoreBackoff() {
     final var failedKeys = getFailedJobKeys();
 
-    removeWrongJobs();
+    removeJobsWithoutRetries();
 
     failedKeys.removeAll(getBackoffJobKeys());
     failedKeys.forEach(
@@ -520,12 +520,10 @@ public final class DbJobState implements JobState, MutableJobState {
    * Removing jobs that are mistakenly inserted into the backoff column family due to <a
    * href="https://github.com/camunda/zeebe/issues/15954">this issue</a>
    */
-  private void removeWrongJobs() {
-    final var failedKeys = getFailedJobKeys();
-    final var backoffJobs = getBackoffJobKey();
+  private void removeJobsWithoutRetries() {
+    final var backoffJobKeys = getBackoffJobKeys();
 
-    backoffJobs.retainAll(failedKeys);
-    backoffJobs.forEach(
+    backoffJobKeys.forEach(
         key -> {
           jobKey.wrapLong(key);
           final var jobRecord = jobsColumnFamily.get(jobKey);
