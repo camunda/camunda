8724d9c296898d70899cd1bf56de615a0a85ac8a
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2024-03-05 13:23:15 +0100

    test: streamline user task QA base test
    Removes superfluous methods and generalization from the user task QA base test class.
    Reduces the test setup to one broker to keep the test cluster to a minimum.
    Makes better use of defaults and provided annotations for test setup and tear down.

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/UserTaskTest.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/UserTaskTest.java
@@ -16,7 +16,6 @@ import io.camunda.zeebe.client.api.response.PartitionInfo;
 import io.camunda.zeebe.client.api.response.Topology;
 import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
-import io.camunda.zeebe.model.bpmn.builder.UserTaskBuilder;
 import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.intent.CommandDistributionIntent;
 import io.camunda.zeebe.protocol.record.intent.DeploymentIntent;
@@ -26,42 +25,28 @@ import io.camunda.zeebe.qa.util.cluster.TestZeebePort;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
 import io.camunda.zeebe.test.util.junit.AutoCloseResources;
+import io.camunda.zeebe.test.util.junit.AutoCloseResources.AutoCloseResource;
 import io.camunda.zeebe.test.util.record.RecordingExporter;
 import java.time.Duration;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @ZeebeIntegration
 @AutoCloseResources
 public abstract class UserTaskTest {
 
-  @TestZeebe(autoStart = false)
+  @TestZeebe(clusterSize = 1, partitionCount = 1, replicationFactor = 1)
   private static final TestCluster CLUSTER =
-      TestCluster.builder()
-          .withEmbeddedGateway(false)
-          .withGatewaysCount(1)
-          .withBrokersCount(3)
-          .withPartitionsCount(3)
-          .withReplicationFactor(3)
-          .build();
+      TestCluster.builder().withEmbeddedGateway(false).withGatewaysCount(1).build();
 
   protected long userTaskKey;
-  protected ZeebeClient client;
 
-  @BeforeAll
-  static void init() {
-    CLUSTER.start().awaitCompleteTopology();
-  }
+  @AutoCloseResource protected ZeebeClient client;
 
   @BeforeEach
   void initClientAndInstances() {
-    final var gateway = CLUSTER.anyGateway();
+    final var gateway = CLUSTER.availableGateway();
     client =
         CLUSTER
             .newClientBuilder()
@@ -73,31 +58,19 @@ public abstract class UserTaskTest {
   }
 
   protected long createSingleUserTask() {
-    return createUserTasks(b -> {}, "{}", 1).getFirst();
-  }
-
-  protected List<Long> createUserTasks(
-      final Consumer<UserTaskBuilder> consumer, final String variables, final int amount) {
-
-    final BpmnModelInstance modelInstance = createSingleUserTaskModelInstance(consumer);
-    final long processDefinitionKey = deployProcess(modelInstance);
-
-    final var processInstanceKeys =
-        IntStream.range(0, amount)
-            .boxed()
-            .map(i -> createProcessInstance(processDefinitionKey, variables))
-            .toList();
-
-    final List<Long> userTaskKeys =
+    final var modelInstance = createSingleUserTaskModelInstance();
+    final var processDefinitionKey = deployProcess(modelInstance);
+    final var processInstanceKey = createProcessInstance(processDefinitionKey);
+    final var userTaskKey =
         RecordingExporter.userTaskRecords(UserTaskIntent.CREATED)
-            .filter(r -> processInstanceKeys.contains(r.getValue().getProcessInstanceKey()))
-            .limit(amount)
+            .filter(r -> processInstanceKey == r.getValue().getProcessInstanceKey())
             .map(Record::getKey)
-            .collect(Collectors.toList());
+            .findFirst()
+            .orElse(-1L);
 
-    assertThat(userTaskKeys).describedAs("Expected %d created user tasks", amount).hasSize(amount);
+    assertThat(userTaskKey).describedAs("Expected a created user task").isGreaterThan(0L);
 
-    return userTaskKeys;
+    return userTaskKey;
   }
 
   protected long deployProcess(final BpmnModelInstance modelInstance) {
@@ -107,28 +80,17 @@ public abstract class UserTaskTest {
             .addProcessModel(modelInstance, "process.bpmn")
             .send()
             .join();
-    //    waitUntilDeploymentIsDone(deploymentEvent.getKey());
-    return deploymentEvent.getProcesses().get(0).getProcessDefinitionKey();
+    waitUntilDeploymentIsDone(deploymentEvent.getKey());
+    return deploymentEvent.getProcesses().getFirst().getProcessDefinitionKey();
   }
 
-  protected BpmnModelInstance createSingleUserTaskModelInstance(
-      final Consumer<UserTaskBuilder> taskBuilderConsumer) {
+  protected BpmnModelInstance createSingleUserTaskModelInstance() {
     return Bpmn.createExecutableProcess("process")
         .startEvent("start")
-        .userTask("task", taskBuilderConsumer)
+        .userTask("task")
         .zeebeUserTask()
         .endEvent("end")
         .done();
-  }
-
-  protected long createProcessInstance(final long processDefinitionKey, final String variables) {
-    return client
-        .newCreateInstanceCommand()
-        .processDefinitionKey(processDefinitionKey)
-        .variables(variables)
-        .send()
-        .join()
-        .getProcessInstanceKey();
   }
 
   protected long createProcessInstance(final long processDefinitionKey) {
