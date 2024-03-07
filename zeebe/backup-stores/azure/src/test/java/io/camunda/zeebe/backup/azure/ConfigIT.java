6721226879af82f214ed1ba0e35b518c4180f270
Martijn Suijlen <141611129+Boboltus@users.noreply.github.com>
2024-03-07 10:23:09 +0100

    modified:   zeebe/backup-stores/azure/src/test/java/io/camunda/zeebe/backup/azure/ConfigIT.java

--- zeebe/backup-stores/azure/src/test/java/io/camunda/zeebe/backup/azure/ConfigIT.java
+++ zeebe/backup-stores/azure/src/test/java/io/camunda/zeebe/backup/azure/ConfigIT.java
@@ -13,104 +13,104 @@ import org.junit.jupiter.api.Test;
 
 public class ConfigIT {
 
-  private static final String VALID_CONNECTION_STRING =
-      "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
-          + "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
-          + "BlobEndpoint=http://127.0.0.1:";
+    private static final String VALID_CONNECTION_STRING =
+            "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;"
+                    + "AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;"
+                    + "BlobEndpoint=http://127.0.0.1:";
 
-  private static final String VALID_ENDPOINT = "http://127.0.0.1";
+    private static final String VALID_ENDPOINT = "https://127.0.0.1";
 
-  @Test
-  void shouldSuccessfullyValidateCredentialsConfig() {
-    final AzureBackupConfig azureBackupConfig =
-        new AzureBackupConfig.Builder()
-            .withEndpoint("http://127.0.0.1:10000/devstoreaccount1")
-            .withAccountName("devstoreaccount1")
-            .withAccountKey(
-                "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==")
-            .withContainerName(UUID.randomUUID().toString())
-            .build();
+    @Test
+    void shouldSuccessfullyValidateCredentialsConfig() {
+        final AzureBackupConfig azureBackupConfig =
+                new AzureBackupConfig.Builder()
+                        .withEndpoint("http://127.0.0.1:10000/devstoreaccount1")
+                        .withAccountName("devstoreaccount1")
+                        .withAccountKey(
+                                "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==")
+                        .withContainerName(UUID.randomUUID().toString())
+                        .build();
 
-    Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
-        .doesNotThrowAnyException();
-    Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
-        .doesNotThrowAnyException();
-  }
+        Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
+                .doesNotThrowAnyException();
+        Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
+                .doesNotThrowAnyException();
+    }
 
-  @Test
-  void shouldValidateConnectionStringConfig() {
+    @Test
+    void shouldValidateConnectionStringConfig() {
 
-    final AzureBackupConfig azureBackupConfig =
-        new AzureBackupConfig.Builder()
-            .withConnectionString(VALID_CONNECTION_STRING)
-            .withContainerName(UUID.randomUUID().toString())
-            .build();
+        final AzureBackupConfig azureBackupConfig =
+                new AzureBackupConfig.Builder()
+                        .withConnectionString(VALID_CONNECTION_STRING)
+                        .withContainerName(UUID.randomUUID().toString())
+                        .build();
 
-    Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
-        .doesNotThrowAnyException();
-    Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
-        .doesNotThrowAnyException();
-  }
+        Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
+                .doesNotThrowAnyException();
+        Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
+                .doesNotThrowAnyException();
+    }
 
-  @Test
-  void shouldValidateEndpointConfig() {
+    @Test
+    void shouldValidateEndpointConfig() {
 
-    final AzureBackupConfig azureBackupConfig =
-        new AzureBackupConfig.Builder()
-            .withEndpoint(VALID_ENDPOINT)
-            .withContainerName(UUID.randomUUID().toString())
-            .build();
+        final AzureBackupConfig azureBackupConfig =
+                new AzureBackupConfig.Builder()
+                        .withEndpoint(VALID_ENDPOINT)
+                        .withContainerName(UUID.randomUUID().toString())
+                        .build();
 
-    Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
-        .doesNotThrowAnyException();
-    Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
-        .doesNotThrowAnyException();
-  }
+        Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
+                .doesNotThrowAnyException();
+        Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
+                .doesNotThrowAnyException();
+    }
 
-  @Test
-  void shouldFailValidationBecauseOfMissingAccountName() {
-    final AzureBackupConfig azureBackupConfig =
-        new AzureBackupConfig.Builder()
-            .withEndpoint("http://127.0.0.1:10000/devstoreaccount1")
-            .withAccountName(null)
-            .withAccountKey(
-                "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==")
-            .withContainerName(UUID.randomUUID().toString())
-            .build();
+    @Test
+    void shouldFailValidationBecauseOfMissingAccountName() {
+        final AzureBackupConfig azureBackupConfig =
+                new AzureBackupConfig.Builder()
+                        .withEndpoint("http://127.0.0.1:10000/devstoreaccount1")
+                        .withAccountName(null)
+                        .withAccountKey(
+                                "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==")
+                        .withContainerName(UUID.randomUUID().toString())
+                        .build();
 
-    Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
-        .hasMessage("'accountName' cannot be null.");
-    Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
-        .hasMessage(
-            "Connection string, endpoint, or all of connection information (account name, account key, and endpoint) must be provided.");
-  }
+        Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
+                .hasMessage("'accountName' cannot be null.");
+        Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
+                .hasMessage(
+                        "Connection string, endpoint, or all of connection information (account name, account key, and endpoint) must be provided.");
+    }
 
-  @Test
-  void shouldFailValidationBecauseOfMissingConnectionString() {
+    @Test
+    void shouldFailValidationBecauseOfMissingConnectionString() {
 
-    final AzureBackupConfig azureBackupConfig =
-        new AzureBackupConfig.Builder()
-            .withConnectionString(null)
-            .withContainerName(UUID.randomUUID().toString())
-            .build();
+        final AzureBackupConfig azureBackupConfig =
+                new AzureBackupConfig.Builder()
+                        .withConnectionString(null)
+                        .withContainerName(UUID.randomUUID().toString())
+                        .build();
 
-    Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
-        .hasMessage("The Azure Storage endpoint url is malformed.");
-    Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
-        .hasMessage(
-            "Connection string, endpoint, or all of connection information (account name, account key, and endpoint) must be provided.");
-  }
+        Assertions.assertThatCode(() -> new AzureBackupStore(azureBackupConfig))
+                .hasMessage("The Azure Storage endpoint url is malformed.");
+        Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
+                .hasMessage(
+                        "Connection string, endpoint, or all of connection information (account name, account key, and endpoint) must be provided.");
+    }
 
-  @Test
-  void containerNameCannotBeMissing() {
-    // given
-    final AzureBackupConfig azureBackupConfig =
-        new AzureBackupConfig.Builder()
-            .withConnectionString(VALID_CONNECTION_STRING)
-            .withContainerName(null)
-            .build();
+    @Test
+    void containerNameCannotBeMissing() {
+        // given
+        final AzureBackupConfig azureBackupConfig =
+                new AzureBackupConfig.Builder()
+                        .withConnectionString(VALID_CONNECTION_STRING)
+                        .withContainerName(null)
+                        .build();
 
-    Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
-        .hasMessage("Container name cannot be null.");
-  }
+        Assertions.assertThatCode(() -> AzureBackupStore.validateConfig(azureBackupConfig))
+                .hasMessage("Container name cannot be null.");
+    }
 }
