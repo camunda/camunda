f4aad7b0c8e5621247063fbde19fda720cc62f4d
Nico Korthout <nico.korthout@camunda.com>
2024-03-15 12:47:21 +0100

    style: apply spotless

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceMigrationMigrateProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceMigrationMigrateProcessor.java
@@ -292,42 +292,45 @@ public class ProcessInstanceMigrationMigrateProcessor
             .getProcess()
             .getElementById(targetElementId, ExecutableActivity.class);
     final List<DirectBuffer> subscribedMessageNames = new ArrayList<>();
-    catchEventBehavior.subscribeToEvents(
-        context,
-        targetElement,
-        catchEvent -> {
-          final var element = catchEvent.element();
-          final String targetCatchEventId = BufferUtil.bufferAsString(element.getId());
-          if (sourceElementIdToTargetElementId.containsValue(targetCatchEventId)) {
-            throw new ProcessInstanceMigrationPreconditionFailedException(
-                """
-                Expected to migrate process instance '%s' \
-                but active element with id '%s' is mapped to element with id '%s' \
-                that must be subscribed to mapped catch event with id '%s'. \
-                Migrating active elements with mapped catch events is not possible yet."""
-                    .formatted(processInstanceKey, elementId, targetElementId, targetCatchEventId),
-                RejectionType.INVALID_STATE);
-          }
-          if (element.isMessage()) {
-            requireNoSubscriptionForMessage(
-                elementInstance,
-                catchEvent.messageName(),
-                elementInstanceRecord.getTenantId(),
-                targetCatchEventId);
-            subscribedMessageNames.add(catchEvent.messageName());
-          }
-          return true;
-        }).ifLeft(
-        failure -> {
-          throw new ProcessInstanceMigrationPreconditionFailedException(
-              """
-              Expected to migrate process instance '%s' \
-              but active element with id '%s' is mapped to element with id '%s' \
-              that must be subscribed to a message catch event. %s"""
-                  .formatted(
-                      processInstanceKey, elementId, targetElementId, failure.getMessage()),
-              RejectionType.INVALID_STATE);
-        });
+    catchEventBehavior
+        .subscribeToEvents(
+            context,
+            targetElement,
+            catchEvent -> {
+              final var element = catchEvent.element();
+              final String targetCatchEventId = BufferUtil.bufferAsString(element.getId());
+              if (sourceElementIdToTargetElementId.containsValue(targetCatchEventId)) {
+                throw new ProcessInstanceMigrationPreconditionFailedException(
+                    """
+                    Expected to migrate process instance '%s' \
+                    but active element with id '%s' is mapped to element with id '%s' \
+                    that must be subscribed to mapped catch event with id '%s'. \
+                    Migrating active elements with mapped catch events is not possible yet."""
+                        .formatted(
+                            processInstanceKey, elementId, targetElementId, targetCatchEventId),
+                    RejectionType.INVALID_STATE);
+              }
+              if (element.isMessage()) {
+                requireNoSubscriptionForMessage(
+                    elementInstance,
+                    catchEvent.messageName(),
+                    elementInstanceRecord.getTenantId(),
+                    targetCatchEventId);
+                subscribedMessageNames.add(catchEvent.messageName());
+              }
+              return true;
+            })
+        .ifLeft(
+            failure -> {
+              throw new ProcessInstanceMigrationPreconditionFailedException(
+                  """
+                  Expected to migrate process instance '%s' \
+                  but active element with id '%s' is mapped to element with id '%s' \
+                  that must be subscribed to a message catch event. %s"""
+                      .formatted(
+                          processInstanceKey, elementId, targetElementId, failure.getMessage()),
+                  RejectionType.INVALID_STATE);
+            });
 
     catchEventBehavior.unsubscribeFromMessageEvents(
         elementInstance.getKey(),
