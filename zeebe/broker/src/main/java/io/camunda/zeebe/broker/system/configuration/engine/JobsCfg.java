ed283f7bb9e93b9ff422451d45c73c6e4986a5dc
Mustafa Dagher <mustafa.dagher@camunda.com>
2024-03-16 01:09:27 +0100

    feat: make JobTimeoutChecker batchLimit configurable

--- zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/JobsCfg.java
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/JobsCfg.java
@@ -14,6 +14,8 @@ import java.time.Duration;
 public class JobsCfg implements ConfigurationEntry {
   private Duration timeoutCheckerPollingInterval =
       EngineConfiguration.DEFAULT_JOBS_TIMEOUT_POLLING_INTERVAL;
+  private int timeoutCheckerBatchLimit =
+      EngineConfiguration.DEFAULT_JOBS_TIMEOUT_CHECKER_BATCH_LIMIT;
 
   public Duration getTimeoutCheckerPollingInterval() {
     return timeoutCheckerPollingInterval;
@@ -23,8 +25,21 @@ public class JobsCfg implements ConfigurationEntry {
     this.timeoutCheckerPollingInterval = timeoutCheckerPollingInterval;
   }
 
+  public int getTimeoutCheckerBatchLimit() {
+    return timeoutCheckerBatchLimit;
+  }
+
+  public void setTimeoutCheckerBatchLimit(final int timeoutCheckerBatchLimit) {
+    this.timeoutCheckerBatchLimit = timeoutCheckerBatchLimit;
+  }
+
   @Override
   public String toString() {
-    return "JobsCfg{" + "timeoutCheckerPollingInterval=" + timeoutCheckerPollingInterval + '}';
+    return "JobsCfg{"
+        + "timeoutCheckerPollingInterval="
+        + timeoutCheckerPollingInterval
+        + ", timeoutCheckerBatchLimit="
+        + timeoutCheckerBatchLimit
+        + '}';
   }
 }
