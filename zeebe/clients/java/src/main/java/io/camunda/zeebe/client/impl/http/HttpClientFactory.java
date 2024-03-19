bdb87d87cb32047118ee9b7b8dc7b9ed026509ca
Nikola Koevski <nikola.koevski@camunda.com>
2024-03-19 13:30:08 +0100

    feat: enable tls for rest api requests in the java client
    Enable secure communication between the Zeebe Java client
    and the Zeebe Gateway over REST by supporting TLS/SSL
    configuration of the embedded HTTP client in the Zeebe
    Java client.

--- zeebe/clients/java/src/main/java/io/camunda/zeebe/client/impl/http/HttpClientFactory.java
+++ zeebe/clients/java/src/main/java/io/camunda/zeebe/client/impl/http/HttpClientFactory.java
@@ -20,10 +20,25 @@ import io.camunda.zeebe.client.CredentialsProvider;
 import io.camunda.zeebe.client.ZeebeClientConfiguration;
 import io.camunda.zeebe.client.impl.NoopCredentialsProvider;
 import io.camunda.zeebe.client.impl.util.VersionUtil;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Collections;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManagerFactory;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.config.RequestConfig.Builder;
 import org.apache.hc.client5.http.impl.async.CloseableHttpAsyncClient;
@@ -31,12 +46,17 @@ import org.apache.hc.client5.http.impl.async.HttpAsyncClientBuilder;
 import org.apache.hc.client5.http.impl.async.HttpAsyncClients;
 import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManager;
 import org.apache.hc.client5.http.impl.nio.PoolingAsyncClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
+import org.apache.hc.client5.http.ssl.DefaultHostnameVerifier;
+import org.apache.hc.client5.http.ssl.HttpClientHostnameVerifier;
 import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpHeaders;
 import org.apache.hc.core5.http.config.CharCodingConfig;
 import org.apache.hc.core5.http.message.BasicHeader;
+import org.apache.hc.core5.http.nio.ssl.TlsStrategy;
 import org.apache.hc.core5.net.URIBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.TimeValue;
 import org.apache.hc.core5.util.Timeout;
 
@@ -90,8 +110,16 @@ public class HttpClientFactory {
                 ", ",
                 ContentType.APPLICATION_JSON.getMimeType(),
                 ContentType.APPLICATION_PROBLEM_JSON.getMimeType()));
+
+    final HttpClientHostnameVerifier hostnameVerifier =
+        new HostnameVerifier(config.getOverrideAuthority());
+    final TlsStrategy tlsStrategy =
+        ClientTlsStrategyBuilder.create()
+            .setSslContext(createSslContext())
+            .setHostnameVerifier(hostnameVerifier)
+            .build();
     final PoolingAsyncClientConnectionManager connectionManager =
-        PoolingAsyncClientConnectionManagerBuilder.create().build();
+        PoolingAsyncClientConnectionManagerBuilder.create().setTlsStrategy(tlsStrategy).build();
 
     return HttpAsyncClients.custom()
         .setConnectionManager(connectionManager)
@@ -111,5 +139,75 @@ public class HttpClientFactory {
         // hard cancellation may cause other requests to fail as it will kill the connection; can be
         // enabled when using HTTP/2
         .setHardCancellationEnabled(false);
+  }
+
+  private SSLContext createSslContext() {
+    if (config.isPlaintextConnectionEnabled() || config.getCaCertificatePath() == null) {
+      return SSLContexts.createDefault();
+    }
+
+    final KeyStore keyStore = createKeyStore();
+    try {
+      final TrustManagerFactory factory =
+          TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      factory.init(keyStore);
+
+      final SSLContext context = SSLContexts.custom().build();
+      context.init(null, factory.getTrustManagers(), null);
+      return context;
+    } catch (final NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private KeyStore createKeyStore() {
+    final File pemChain = new File(config.getCaCertificatePath());
+
+    try (final FileInputStream certificateStream = new FileInputStream(pemChain)) {
+      final KeyStore store = KeyStore.getInstance(KeyStore.getDefaultType());
+      store.load(null);
+
+      final CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509");
+      int i = 1;
+      while (certificateStream.available() > 0) {
+        final Certificate certificate = certificateFactory.generateCertificate(certificateStream);
+        final String alias = Integer.toString(i);
+        store.setCertificateEntry(alias, certificate);
+        i++;
+      }
+
+      return store;
+    } catch (final CertificateException
+        | IOException
+        | KeyStoreException
+        | NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static final class HostnameVerifier implements HttpClientHostnameVerifier {
+    private final HttpClientHostnameVerifier delegate = new DefaultHostnameVerifier();
+    private final String overriddenAuthority;
+
+    private HostnameVerifier(final String overriddenAuthority) {
+      this.overriddenAuthority = overriddenAuthority;
+    }
+
+    @Override
+    public void verify(final String host, final X509Certificate cert) throws SSLException {
+      final String hostname = host.equals("0.0.0.0") ? "localhost" : host;
+      if (overriddenAuthority != null) {
+        delegate.verify(overriddenAuthority, cert);
+      } else {
+
+        delegate.verify(hostname, cert);
+      }
+    }
+
+    @Override
+    public boolean verify(final String hostname, final SSLSession session) {
+      final String host = hostname.equals("0.0.0.0") ? "localhost" : hostname;
+      return delegate.verify(overriddenAuthority == null ? host : overriddenAuthority, session);
+    }
   }
 }
