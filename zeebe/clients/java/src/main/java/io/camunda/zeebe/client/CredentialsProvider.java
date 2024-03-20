5cb15142b239a5ae456f4708f454b94024445c1f
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-03-20 16:23:45 +0100

    refactor: update CredentialsProvider to be protocol agnostic - BREAKING CHANGE

--- zeebe/clients/java/src/main/java/io/camunda/zeebe/client/CredentialsProvider.java
+++ zeebe/clients/java/src/main/java/io/camunda/zeebe/client/CredentialsProvider.java
@@ -25,24 +25,70 @@ public interface CredentialsProvider {
 
   /**
    * Adds credentials to the headers. For an example of this, see {@link
-   * OAuthCredentialsProvider#applyCredentials(Metadata)}
+   * OAuthCredentialsProvider#applyCredentials(CredentialsApplier)}
    *
-   * @param headers gRPC headers to be modified
+   * @param applier where to headers
    */
-  void applyCredentials(Metadata headers) throws IOException;
+  void applyCredentials(final CredentialsApplier applier) throws IOException;
 
   /**
    * Returns true if the request should be retried; otherwise returns false. For an example of this,
-   * see {@link OAuthCredentialsProvider#shouldRetryRequest(Throwable)}
+   * see {@link OAuthCredentialsProvider#shouldRetryRequest(StatusCode)}.
    *
-   * @param throwable error that caused the request to fail
+   * <p><strong>Only called for REST calls</strong>
+   *
+   * @param statusCode the response code for the failure
    */
-  boolean shouldRetryRequest(Throwable throwable);
+  boolean shouldRetryRequest(final StatusCode statusCode);
 
   /**
    * @return a builder to configure and create a new {@link OAuthCredentialsProvider}.
    */
   static OAuthCredentialsProviderBuilder newCredentialsProviderBuilder() {
     return new OAuthCredentialsProviderBuilder();
+  }
+
+  /**
+   * Used to apply call credentials on a per-request basis, abstracting over gRPC and REST. This
+   * interface is only meant to be consumed, not implemented externally.
+   */
+  interface CredentialsApplier {
+
+    /**
+     * Puts the given header key and value into the request headers (e.g. HTTP headers or gRPC
+     * metadata).
+     *
+     * @param key the header key
+     * @param value the header value
+     */
+    void put(final String key, final String value);
+
+    /**
+     * Helper method to build a credentials applier out of gRPC metadata.
+     *
+     * @param metadata the gRPC metadata on which to apply
+     */
+    static CredentialsApplier ofMetadata(final Metadata metadata) {
+      return (key, value) ->
+          metadata.put(Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER), value);
+    }
+  }
+
+  /**
+   * Represents the result of a failed call, abstracting over gRPC and standard HTTP status codes.
+   */
+  interface StatusCode {
+
+    /** Returns the raw status code, e.g. HTTP 401 Unauthorized, or gRPC 16 Unauthenticated. */
+    int code();
+
+    /** Returns true if the request was failed because the user cannot be authenticated. */
+    boolean isUnauthorized();
+
+    /**
+     * Returns true if the request was failed because the authenticated user does not have
+     * permissions.
+     */
+    boolean isForbidden();
   }
 }
