92e3f71c0a75f3dcc5a8f3b651bdf261b36df319
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-03-25 12:41:32 +0100

    refactor: support management endpoints base path as well

--- dist/src/main/java/io/camunda/zeebe/broker/health/BrokerHealthRoutes.java
+++ dist/src/main/java/io/camunda/zeebe/broker/health/BrokerHealthRoutes.java
@@ -8,8 +8,8 @@
 package io.camunda.zeebe.broker.health;
 
 import io.camunda.zeebe.shared.management.ConditionalOnManagementContext;
-import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
 import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
 import org.springframework.boot.actuate.autoconfigure.web.ManagementContextType;
 import org.springframework.boot.actuate.autoconfigure.web.server.ManagementServerProperties;
@@ -19,6 +19,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilderFactory;
 import reactor.core.publisher.Mono;
 
 @Profile("broker")
@@ -26,24 +28,41 @@ import reactor.core.publisher.Mono;
 @ManagementContextConfiguration(value = ManagementContextType.ANY, proxyBeanMethods = false)
 public class BrokerHealthRoutes {
 
-  private final ManagementServerProperties properties;
+  private final UriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory();
+
+  private final ManagementServerProperties serverProperties;
+  private final WebEndpointProperties actuatorProperties;
 
   @Autowired
-  public BrokerHealthRoutes(final ManagementServerProperties properties) {
-    this.properties = properties;
+  public BrokerHealthRoutes(
+      final ManagementServerProperties serverProperties,
+      final WebEndpointProperties actuatorProperties) {
+    this.serverProperties = serverProperties;
+    this.actuatorProperties = actuatorProperties;
   }
 
   @Bean
   public RouterFunction<ServerResponse> routes() {
-    final String basePath = properties.getBasePath();
+    final var serverBasePath = serverProperties.getBasePath();
+    final var actuatorBasePath = actuatorProperties.getBasePath();
+
     return RouterFunctions.route()
-        .GET("/health", req -> movedPermanently(basePath + "/actuator/health/status"))
-        .GET("/ready", req -> movedPermanently(basePath + "/actuator/health/readiness"))
-        .GET("/startup", req -> movedPermanently(basePath + "/actuator/health/startup"))
+        .GET("/health", req -> movedPermanently(serverBasePath, actuatorBasePath, "/health/status"))
+        .GET(
+            "/ready",
+            req -> movedPermanently(serverBasePath, actuatorBasePath, "/health/readiness"))
+        .GET(
+            "/startup",
+            req -> movedPermanently(serverBasePath, actuatorBasePath, "/health/startup"))
         .build();
   }
 
-  private Mono<ServerResponse> movedPermanently(final String path) {
-    return ServerResponse.status(HttpStatus.MOVED_PERMANENTLY).location(URI.create(path)).build();
+  private Mono<ServerResponse> movedPermanently(final String... paths) {
+    final var builder = uriBuilderFactory.builder();
+    for (final var path : paths) {
+      builder.path(path);
+    }
+
+    return ServerResponse.status(HttpStatus.MOVED_PERMANENTLY).location(builder.build()).build();
   }
 }
