33174380799a5867507e66bf7f0e44b5d386bd15
Nico Korthout <nico.korthout@camunda.com>
2024-03-26 18:13:23 +0100

    fix: make due date checker threadsafe
    Initially we looked at making the schedule method synchronized. However,
    lockless threadsafe code typically runs faster.
    
    To make the method threadsafe, we used an atomic reference. This can be
    updated in a threadsafe lockless manner. Any side effect produced by the
    update method (like scheduling the next execution) can be rolled back.
    The rollback is simply a cancellation of the newly scheduled task.
    
    We've extracted this threadsafe lockless update logic into a new
    AtomicUtil class and generalized it for potential reuse.
    
    Co-authored-by: Ole Schönburg <ole.schoenburg@gmail.com>
    (cherry picked from commit 7fd78b9d7a076c0b4e8f5cc0956d374cbbe14377)
    (cherry picked from commit 3dc3ea3ed7918a1d5b77f54af9733891c217c814)

---
+++ util/src/main/java/io/camunda/zeebe/util/AtomicUtil.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.util;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/** Utility class for common tasks with {@link AtomicReference} instances. */
+public final class AtomicUtil {
+
+  private AtomicUtil() {}
+
+  /**
+   * Locklessly updates an atomic reference using the provided update function and rollbacks if the
+   * reference changed during the update, e.g. by another thread.
+   *
+   * <p>If the update function returns an empty optional, the atomic reference is not updated.
+   *
+   * <p>If the atomic reference has been updated by another thread in the meantime, the rollback
+   * function is called with the value provided by the update function, and the update function is
+   * called again.
+   *
+   * @param ref The atomic reference to update
+   * @param update The update function used to provide a new value to the atomic reference; if
+   *     {@code empty} the atomic reference is not updated
+   * @param rollback The rollback function used to revert any side effect produced by the update
+   *     function
+   * @return The previous value of the atomic reference
+   * @param <T> The type of the atomic reference
+   */
+  public static <T> T update(
+      final AtomicReference<T> ref,
+      final Function<T, Optional<T>> update,
+      final Consumer<T> rollback) {
+    T currentVal;
+    T newVal = null;
+    do {
+      if (newVal != null) {
+        // another thread appears to have replaced the value in the meantime
+        rollback.accept(newVal);
+      }
+      currentVal = ref.get();
+      final var result = update.apply(currentVal);
+      if (result.isEmpty()) {
+        return currentVal;
+      }
+      newVal = result.get();
+    } while (!ref.compareAndSet(currentVal, newVal));
+    return currentVal;
+  }
+}
