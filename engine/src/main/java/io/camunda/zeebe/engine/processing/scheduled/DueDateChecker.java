6ae459139a203057bc39168f9608ffb87f9e8cba
Nico Korthout <nico.korthout@camunda.com>
2024-03-26 16:09:53 +0100

    refactor: remove unnecessary execute class
    Execution of the due date checker is pretty simple (and has nothing to
    do with TriggerEntities anymore). So, we replaced the
    TriggerEntitiesTask class with a single private method execute close to
    the top of the DueDateChecker class. It's close to the top, because it's
    the main function of the checker.
    
    This removed a bunch of indirection that made the class confusing to
    read.
    
    Co-authored-by: Ole Schönburg <ole.schoenburg@gmail.com>
    (cherry picked from commit cc65836d9190e08ac313fb059f0b39c7e4ef1aa8)

--- engine/src/main/java/io/camunda/zeebe/engine/processing/scheduled/DueDateChecker.java
+++ engine/src/main/java/io/camunda/zeebe/engine/processing/scheduled/DueDateChecker.java
@@ -22,7 +22,6 @@ public final class DueDateChecker implements StreamProcessorLifecycleAware {
   private final boolean scheduleAsync;
   private final long timerResolution;
   private final Function<TaskResultBuilder, Long> visitor;
-  private final TriggerEntitiesTask triggerEntitiesTask = new TriggerEntitiesTask();
 
   private ScheduleDelayed scheduleService;
 
@@ -47,6 +46,22 @@ public final class DueDateChecker implements StreamProcessorLifecycleAware {
     this.visitor = visitor;
   }
 
+  private TaskResult execute(final TaskResultBuilder taskResultBuilder) {
+    nextExecution = null;
+
+    if (shouldRescheduleChecker) {
+      final long nextDueDate = visitor.apply(taskResultBuilder);
+
+      // reschedule the runnable if there are timers left
+
+      if (nextDueDate > 0) {
+        schedule(nextDueDate);
+      }
+    }
+
+    return taskResultBuilder.build();
+  }
+
   public void schedule(final long dueDate) {
 
     // We schedule only one runnable for all timers.
@@ -63,7 +78,7 @@ public final class DueDateChecker implements StreamProcessorLifecycleAware {
     if (nextExecution == null || nextExecution.nextDueDate() - dueDate > timerResolution) {
       cancelNextExecution();
       final var delay = calculateDelayForNextRun(dueDate);
-      final var task = scheduleService.runDelayed(delay, triggerEntitiesTask);
+      final var task = scheduleService.runDelayed(delay, this::execute);
       nextExecution = new NextExecution(dueDate, task);
     }
   }
@@ -84,7 +99,7 @@ public final class DueDateChecker implements StreamProcessorLifecycleAware {
     // cover all edge cases.
     cancelNextExecution();
 
-    final var task = scheduleService.runDelayed(Duration.ZERO, triggerEntitiesTask);
+    final var task = scheduleService.runDelayed(Duration.ZERO, this::execute);
     nextExecution = new NextExecution(-1, task);
   }
 
@@ -156,25 +171,5 @@ public final class DueDateChecker implements StreamProcessorLifecycleAware {
      * ProcessingScheduleService#runDelayedAsync(Duration, Task)}
      */
     ScheduledTask runDelayed(final Duration delay, final Task task);
-  }
-
-  private final class TriggerEntitiesTask implements Task {
-
-    @Override
-    public TaskResult execute(final TaskResultBuilder taskResultBuilder) {
-      nextExecution = null;
-
-      if (shouldRescheduleChecker) {
-        final long nextDueDate = visitor.apply(taskResultBuilder);
-
-        // reschedule the runnable if there are timers left
-
-        if (nextDueDate > 0) {
-          schedule(nextDueDate);
-        }
-      }
-
-      return taskResultBuilder.build();
-    }
   }
 }
