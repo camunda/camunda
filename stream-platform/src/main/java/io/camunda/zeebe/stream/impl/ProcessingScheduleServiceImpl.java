5d0dfddc993e6724874dd87b7b419d67e47f1b93
Ole Schönburg <ole.schoenburg@gmail.com>
2024-03-26 09:59:04 +0100

    refactor: inline helper method
    The method is only used once and only adds indirection that makes it difficult to return something from `runDelayed`.
    
    Co-authored-by: Nico Korthout <nico.korthout@camunda.com>

--- stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingScheduleServiceImpl.java
+++ stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingScheduleServiceImpl.java
@@ -51,7 +51,11 @@ public class ProcessingScheduleServiceImpl
 
   @Override
   public void runDelayed(final Duration delay, final Runnable followUpTask) {
-    useActorControl(() -> actorControl.runDelayed(delay, followUpTask));
+    if (actorControl == null) {
+      LOG.debug("ProcessingScheduleService hasn't been opened yet, ignore scheduled task.");
+      return;
+    }
+    actorControl.runDelayed(delay, followUpTask);
   }
 
   @Override
@@ -71,14 +75,6 @@ public class ProcessingScheduleServiceImpl
                 runAtFixedRate(delay, task);
               }
             }));
-  }
-
-  private void useActorControl(final Runnable task) {
-    if (actorControl == null) {
-      LOG.debug("ProcessingScheduleService hasn't been opened yet, ignore scheduled task.");
-      return;
-    }
-    task.run();
   }
 
   public ActorFuture<Void> open(final ActorControl control) {
