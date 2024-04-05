adccba3e8f81d85c6bf4c33b9620a5e29905b917
Houssain Barouni <146710021+houssain-barouni@users.noreply.github.com>
2024-04-05 21:53:40 +0200

    chore: remove secrets (#4819)

--- tasklist/qa/integration-tests/src/test/java/io/camunda/tasklist/it/ZeebeConnectorSecureIT.java
+++ tasklist/qa/integration-tests/src/test/java/io/camunda/tasklist/it/ZeebeConnectorSecureIT.java
@@ -10,22 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.tasklist.property.TasklistProperties;
 import io.camunda.tasklist.property.ZeebeProperties;
+import io.camunda.tasklist.util.CertificateUtil;
 import io.camunda.tasklist.util.ContainerVersionsUtil;
 import io.camunda.tasklist.zeebe.ZeebeConnector;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.BrokerInfo;
 import io.zeebe.containers.ZeebeContainer;
+import java.io.File;
 import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
@@ -36,50 +38,63 @@ import org.testcontainers.utility.MountableFile;
       TasklistProperties.PREFIX + ".archiver.rolloverEnabled = false",
     })
 @ExtendWith(SpringExtension.class)
-@Testcontainers
 public class ZeebeConnectorSecureIT {
 
   private static final String CERTIFICATE_FILE = "zeebe-test-chain.cert.pem";
   private static final String PRIVATE_KEY_FILE = "zeebe-test-server.key.pem";
-
   private static final DockerImageName ZEEBE_DOCKER_IMAGE =
       DockerImageName.parse("camunda/zeebe")
           .withTag(
               ContainerVersionsUtil.readProperty(
                   ContainerVersionsUtil.ZEEBE_CURRENTVERSION_DOCKER_PROPERTY_NAME));
   @Autowired ZeebeConnector zeebeConnector;
-  private final MountableFile certsDir = MountableFile.forClasspathResource("certs");
-
-  @Container
-  public ZeebeContainer zeebeContainer =
-      new ZeebeContainer(ZEEBE_DOCKER_IMAGE)
-          .withFileSystemBind(certsDir.getFilesystemPath(), "/usr/local/zeebe/certs")
-          .withEnv(
-              Map.of(
-                  "ZEEBE_BROKER_GATEWAY_SECURITY_CERTIFICATECHAINPATH",
-                      "/usr/local/zeebe/certs/" + CERTIFICATE_FILE,
-                  "ZEEBE_BROKER_GATEWAY_SECURITY_PRIVATEKEYPATH",
-                      "/usr/local/zeebe/certs/" + PRIVATE_KEY_FILE,
-                  "ZEEBE_BROKER_GATEWAY_SECURITY_ENABLED", "true"))
-          // Can't use connection wait strategy because of TLS
-          .waitingFor(
-              new LogMessageWaitStrategy()
-                  .withRegEx(".*Broker is ready!.*")
-                  .withStartupTimeout(Duration.ofSeconds(101)));
+  private ZeebeContainer zeebeContainer;
+  private ZeebeClient zeebeClient;
 
   @Test
-  public void shouldConnectWithTLS() {
+  public void shouldConnectWithTLS(@TempDir final File tempDir) throws Exception {
     // given
-    final ZeebeClient zeebeClient =
+    final File certFile = new File(tempDir, CERTIFICATE_FILE);
+    final File privateKeyFile = new File(tempDir, PRIVATE_KEY_FILE);
+    CertificateUtil.generateRSACertificate(certFile, privateKeyFile);
+    zeebeContainer =
+        new ZeebeContainer(ZEEBE_DOCKER_IMAGE)
+            .withCopyFileToContainer(
+                MountableFile.forHostPath(tempDir.toPath(), 0755), "/usr/local/zeebe/certs")
+            .withEnv(
+                Map.of(
+                    "ZEEBE_BROKER_GATEWAY_SECURITY_CERTIFICATECHAINPATH",
+                    "/usr/local/zeebe/certs/" + CERTIFICATE_FILE,
+                    "ZEEBE_BROKER_GATEWAY_SECURITY_PRIVATEKEYPATH",
+                    "/usr/local/zeebe/certs/" + PRIVATE_KEY_FILE,
+                    "ZEEBE_BROKER_GATEWAY_SECURITY_ENABLED",
+                    "true"))
+            // Can't use connection wait strategy because of TLS
+            .waitingFor(
+                new LogMessageWaitStrategy()
+                    .withRegEx(".*Broker is ready!.*")
+                    .withStartupTimeout(Duration.ofSeconds(101)));
+    zeebeContainer.start();
+    zeebeClient =
         zeebeConnector.newZeebeClient(
             new ZeebeProperties()
                 .setGatewayAddress(zeebeContainer.getExternalGatewayAddress())
                 .setSecure(true)
-                .setCertificatePath(certsDir.getFilesystemPath() + "/" + CERTIFICATE_FILE));
+                .setCertificatePath(tempDir.getCanonicalPath() + "/" + CERTIFICATE_FILE));
     // when
     final List<BrokerInfo> brokerInfos =
         zeebeClient.newTopologyRequest().send().join().getBrokers();
     // then
     assertThat(brokerInfos).isNotEmpty();
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    if (zeebeClient != null) {
+      zeebeClient.close();
+    }
+    if (zeebeContainer != null) {
+      zeebeContainer.stop();
+    }
   }
 }
---
+++ tasklist/qa/integration-tests/src/test/java/io/camunda/tasklist/util/CertificateUtil.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH
+ * under one or more contributor license agreements. Licensed under a proprietary license.
+ * See the License.txt file for more information. You may not use this file
+ * except in compliance with the proprietary license.
+ */
+package io.camunda.tasklist.util;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.math.BigInteger;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+
+public final class CertificateUtil {
+
+  private static final String ALGORITHM = "RSA";
+  private static final int CERTIFICATE_KEY_SIZE = 2048;
+
+  private CertificateUtil() {}
+
+  public static void generateRSACertificate(final File certFile, final File privateKeyFile)
+      throws Exception {
+    final KeyPair keyPair = generateKeyPair();
+    final X509Certificate cert =
+        generateSelfSignedCertificate(keyPair, "CN=localhost", "SHA256withRSA");
+    saveCertificateToFile(cert, certFile);
+    savePrivateKeyToFile(keyPair.getPrivate(), privateKeyFile);
+  }
+
+  private static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+    final KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(ALGORITHM);
+    keyPairGenerator.initialize(CERTIFICATE_KEY_SIZE);
+    return keyPairGenerator.generateKeyPair();
+  }
+
+  private static void saveCertificateToFile(final X509Certificate cert, final File certFile)
+      throws CertificateEncodingException, IOException {
+    try (final Writer writer = new FileWriter(certFile);
+        final JcaPEMWriter pemWriter = new JcaPEMWriter(writer)) {
+      pemWriter.writeObject(cert);
+    }
+  }
+
+  private static void savePrivateKeyToFile(final PrivateKey privateKey, final File privateKeyFile)
+      throws IOException {
+    try (final Writer writer = new FileWriter(privateKeyFile);
+        final JcaPEMWriter pemWriter = new JcaPEMWriter(writer)) {
+      pemWriter.writeObject(privateKey);
+    }
+  }
+
+  private static X509Certificate generateSelfSignedCertificate(
+      final KeyPair keyPair, final String dn, final String signatureAlgorithm) throws Exception {
+    final X500Name subjectName = new X500Name(dn);
+    final X500Name issuerName = subjectName; // Self-signed
+
+    final BigInteger serialNumber = BigInteger.valueOf(System.currentTimeMillis());
+    final Instant now = Instant.now();
+    final Date startDate = Date.from(now);
+    final Date endDate = Date.from(now.plus(Duration.ofDays(365)));
+
+    final JcaX509v3CertificateBuilder certBuilder =
+        new JcaX509v3CertificateBuilder(
+            issuerName, serialNumber, startDate, endDate, subjectName, keyPair.getPublic());
+
+    final ContentSigner signer =
+        new JcaContentSignerBuilder(signatureAlgorithm).build(keyPair.getPrivate());
+
+    final X509CertificateHolder certHolder = certBuilder.build(signer);
+    return new JcaX509CertificateConverter().getCertificate(certHolder);
+  }
+}
