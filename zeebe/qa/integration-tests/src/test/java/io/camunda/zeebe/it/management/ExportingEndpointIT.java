da254a47b3386ba338dbc10ec12a8cad637d4ece
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2024-04-08 15:26:05 +0200

    test: add test for soft pause exporting in the integration tests.
    (cherry picked from commit 75168493d3dc2df412326324f9258748931204dd)

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/management/ExportingEndpointIT.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/management/ExportingEndpointIT.java
@@ -10,6 +10,7 @@ package io.camunda.zeebe.it.management;
 import static io.camunda.zeebe.test.StableValuePredicate.hasStableValue;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.model.bpmn.Bpmn;
@@ -22,6 +23,8 @@ import io.camunda.zeebe.test.util.junit.AutoCloseResources;
 import io.camunda.zeebe.test.util.junit.AutoCloseResources.AutoCloseResource;
 import io.camunda.zeebe.test.util.record.RecordingExporter;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -184,6 +187,16 @@ final class ExportingEndpointIT {
     }
   }
 
+  private void allPartitionsSoftPausedExporting() {
+    for (final var broker : CLUSTER.brokers().values()) {
+      assertThat(PartitionsActuator.of(broker).query().values())
+          .allMatch(
+              status ->
+                  status.exporterPhase() == null || status.exporterPhase().equals("SOFT_PAUSED"),
+              "All exporters should be soft paused");
+    }
+  }
+
   private void allPartitionsExporting() {
     for (final var broker : CLUSTER.brokers().values()) {
       assertThat(PartitionsActuator.of(broker).query().values())
@@ -192,5 +205,182 @@ final class ExportingEndpointIT {
                   status.exporterPhase() == null || status.exporterPhase().equals("EXPORTING"),
               "All exporters should be running");
     }
+  }
+
+  @Test
+  void shouldSoftPauseExporting() {
+
+    final Map<Integer, Long> exportedPositionPerPartition = new HashMap<>();
+    final Map<Integer, Long> secondExportedPositionPerPartition = new HashMap<>();
+    // given
+    client
+        .newPublishMessageCommand()
+        .messageName("Test")
+        .correlationKey("7")
+        .messageId("7")
+        .send()
+        .join();
+
+    final var recordsBeforePause =
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(30))
+            .during(Duration.ofSeconds(5))
+            .until(RecordingExporter.getRecords()::size, hasStableValue());
+
+    for (final var broker : CLUSTER.brokers().values()) {
+      PartitionsActuator.of(broker)
+          .query()
+          .forEach(
+              (partitionId, partitionStatus) -> {
+                exportedPositionPerPartition.put(partitionId, partitionStatus.exportedPosition());
+              });
+    }
+
+    // when
+    getActuator().softPause();
+
+    // given
+    client
+        .newPublishMessageCommand()
+        .messageName("Test")
+        .correlationKey("8")
+        .messageId("8")
+        .send()
+        .join();
+
+    final var recordsAfterSoftPause =
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(30))
+            .during(Duration.ofSeconds(5))
+            .until(RecordingExporter.getRecords()::size, hasStableValue());
+
+    for (final var broker : CLUSTER.brokers().values()) {
+      PartitionsActuator.of(broker)
+          .query()
+          .forEach(
+              (partitionId, partitionStatus) -> {
+                secondExportedPositionPerPartition.put(
+                    partitionId, partitionStatus.exportedPosition());
+              });
+    }
+
+    assertThat(recordsAfterSoftPause).isGreaterThan(recordsBeforePause);
+    assertThat(exportedPositionPerPartition.equals(secondExportedPositionPerPartition)).isTrue();
+  }
+
+  @Test
+  void shouldResumeAfterSoftPauseExporting() {
+
+    final Map<Integer, Long> exportedPositionPerPartition = new HashMap<>();
+    final Map<Integer, Long> secondExportedPositionPerPartition = new HashMap<>();
+    // given
+    client
+        .newPublishMessageCommand()
+        .messageName("Test")
+        .correlationKey("9")
+        .messageId("9")
+        .send()
+        .join();
+
+    final var recordsBeforePause =
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(30))
+            .during(Duration.ofSeconds(5))
+            .until(RecordingExporter.getRecords()::size, hasStableValue());
+
+    for (final var broker : CLUSTER.brokers().values()) {
+      PartitionsActuator.of(broker)
+          .query()
+          .forEach(
+              (partitionId, partitionStatus) -> {
+                exportedPositionPerPartition.put(partitionId, partitionStatus.exportedPosition());
+              });
+    }
+
+    // when
+    getActuator().softPause();
+
+    // given
+    client
+        .newPublishMessageCommand()
+        .messageName("Test")
+        .correlationKey("10")
+        .messageId("10")
+        .send()
+        .join();
+
+    final var recordsAfterSoftPause =
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(30))
+            .during(Duration.ofSeconds(5))
+            .until(RecordingExporter.getRecords()::size, hasStableValue());
+
+    getActuator().resume();
+    try {
+      Thread.sleep(1000);
+    } catch (final InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    for (final var broker : CLUSTER.brokers().values()) {
+      PartitionsActuator.of(broker)
+          .query()
+          .forEach(
+              (partitionId, partitionStatus) -> {
+                secondExportedPositionPerPartition.put(
+                    partitionId, partitionStatus.exportedPosition());
+              });
+    }
+
+    assertThat(recordsAfterSoftPause).isGreaterThan(recordsBeforePause);
+    // at least one partition should have a higher exported position
+    assertTrue(
+        exportedPositionPerPartition.entrySet().stream()
+            .anyMatch(
+                exportedPosition ->
+                    secondExportedPositionPerPartition.get(exportedPosition.getKey())
+                        > exportedPosition.getValue()));
+  }
+
+  @Test
+  void shouldStaySoftPausedAfterRestart() {
+    // given
+    getActuator().resume();
+    client
+        .newPublishMessageCommand()
+        .messageName("Test")
+        .correlationKey("11")
+        .messageId("11")
+        .send()
+        .join();
+
+    final var recordsBeforePause =
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(30))
+            .during(Duration.ofSeconds(5))
+            .until(RecordingExporter.getRecords()::size, hasStableValue());
+
+    // when
+    getActuator().softPause();
+    CLUSTER.shutdown();
+    CLUSTER.start();
+
+    client
+        .newPublishMessageCommand()
+        .messageName("Test")
+        .correlationKey("12")
+        .messageId("12")
+        .send()
+        .join();
+
+    final var recordsAfterRestart =
+        Awaitility.await()
+            .atMost(Duration.ofSeconds(30))
+            .during(Duration.ofSeconds(5))
+            .until(RecordingExporter.getRecords()::size, hasStableValue());
+
+    // then
+    assertThat(recordsAfterRestart).isGreaterThan(recordsBeforePause);
+    Awaitility.await().untilAsserted(this::allPartitionsSoftPausedExporting);
   }
 }
