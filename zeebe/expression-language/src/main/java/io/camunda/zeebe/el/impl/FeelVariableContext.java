3178300e5430475b92747ab9bd685242f3fe7ef5
Ole Schönburg <ole.schoenburg@gmail.com>
2024-04-08 17:00:05 +0200

    refactor: rewrite `FeelVariableContext` in Java

---
+++ zeebe/expression-language/src/main/java/io/camunda/zeebe/el/impl/FeelVariableContext.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.el.impl;
+
+import io.camunda.zeebe.el.EvaluationContext;
+import org.camunda.feel.context.CustomContext;
+import org.camunda.feel.context.VariableProvider;
+import scala.Option;
+import scala.collection.Iterable;
+import scala.collection.immutable.List$;
+
+final class FeelVariableContext extends CustomContext {
+  private final EvaluationContext context;
+
+  FeelVariableContext(final EvaluationContext context) {
+    this.context = context;
+  }
+
+  @Override
+  public VariableProvider variableProvider() {
+    return new EvaluationContextWrapper();
+  }
+
+  private final class EvaluationContextWrapper implements VariableProvider {
+
+    @Override
+    public Option<Object> getVariable(final String name) {
+      return Option.apply(context.getVariable(name))
+          .filter(variable -> variable.capacity() > 0)
+          .map(variable -> variable);
+    }
+
+    @Override
+    public Iterable<String> keys() {
+      return List$.MODULE$.empty();
+    }
+  }
+}
