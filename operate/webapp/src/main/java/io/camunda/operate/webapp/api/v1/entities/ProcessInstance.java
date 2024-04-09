4a5461dacd2d99a74787b2197b6358c37554e07d
Mihail <122792941+mihail-ca@users.noreply.github.com>
2024-04-09 12:54:44 +0200

    fix: INCIDENT state is not returned in Public API

--- operate/webapp/src/main/java/io/camunda/operate/webapp/api/v1/entities/ProcessInstance.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/api/v1/entities/ProcessInstance.java
@@ -162,9 +162,13 @@ public class ProcessInstance {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    ProcessInstance that = (ProcessInstance) o;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final ProcessInstance that = (ProcessInstance) o;
     return Objects.equals(key, that.key)
         && Objects.equals(processVersion, that.processVersion)
         && Objects.equals(bpmnProcessId, that.bpmnProcessId)
