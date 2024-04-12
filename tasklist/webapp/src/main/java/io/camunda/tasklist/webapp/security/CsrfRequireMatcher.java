b0abcd6f97a8c397e3040d7220e168c009a3714a
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-04-12 14:43:55 +0200

    feat: csrf token implementation (#4839)

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/security/CsrfRequireMatcher.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/security/CsrfRequireMatcher.java
@@ -16,59 +16,55 @@
  */
 package io.camunda.tasklist.webapp.security;
 
-import static io.camunda.tasklist.webapp.rest.ClientConfigRestService.CLIENT_CONFIG_RESOURCE;
+import static io.camunda.tasklist.webapp.security.TasklistURIs.LOGIN_RESOURCE;
+import static io.camunda.tasklist.webapp.security.TasklistURIs.LOGOUT_RESOURCE;
 
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-public final class TasklistURIs {
+public class CsrfRequireMatcher implements RequestMatcher {
+  private static final Pattern ALLOWED_METHODS = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
 
-  public static final String ROOT_URL = "/";
-  public static final String ROOT = ROOT_URL;
-  public static final String ERROR_URL = "/error";
-  public static final String GRAPHQL_URL = "/graphql";
-  public static final String REST_V1_API = "/v1/";
-  public static final String REST_V1_EXTERNAL_API = "/v1/external/**";
-  public static final String NEW_FORM = "/new/**";
-  public static final String ALL_REST_V1_API = "/v1/**";
-  public static final String TASKS_URL_V1 = "/v1/tasks";
-  public static final String VARIABLES_URL_V1 = "/v1/variables";
-  public static final String FORMS_URL_V1 = "/v1/forms";
-  public static final String USERS_URL_V1 = "/v1/internal/users";
-  public static final String DEV_UTIL_URL_V1 = "/v1/external/devUtil";
-  public static final String PROCESSES_URL_V1 = "/v1/internal/processes";
-  public static final String EXTERNAL_PROCESS_URL_V1 = "/v1/external/process";
+  private static final Pattern ALLOWED_PATHS =
+      Pattern.compile(LOGIN_RESOURCE + "|" + LOGOUT_RESOURCE);
 
-  public static final String LOGIN_RESOURCE = "/api/login";
-  public static final String LOGOUT_RESOURCE = "/api/logout";
-  public static final String SSO_CALLBACK = "/sso-callback";
-  public static final String NO_PERMISSION = "/noPermission";
-  public static final String IDENTITY_CALLBACK_URI = "/identity-callback";
-  public static final String REQUESTED_URL = "requestedUrl";
-  public static final String COOKIE_JSESSIONID = "TASKLIST-SESSION";
-  public static final String START_PUBLIC_PROCESS = "/new/";
-  public static final String RESPONSE_CHARACTER_ENCODING = "UTF-8";
-  public static final String X_CSRF_TOKEN = "X-CSRF-TOKEN";
+  @Override
+  public boolean matches(final HttpServletRequest request) {
+    // If request is from Allowed Methods, Login and Logout
+    if (ALLOWED_METHODS.matcher(request.getMethod()).matches()) {
+      return false;
+    }
+    if (ALLOWED_PATHS.matcher(request.getServletPath()).matches()) {
+      return false;
+    }
 
-  private TasklistURIs() {}
+    // Check if the user is authenticated
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    final boolean isUserAuthenticated = authentication != null && authentication.isAuthenticated();
 
-  public static final RequestMatcher[] getAuthWhitelist(
-      final HandlerMappingIntrospector introspector) {
-    final RequestMatcher[] requestMatchers = {
-      AntPathRequestMatcher.antMatcher("/webjars/**"),
-      AntPathRequestMatcher.antMatcher(CLIENT_CONFIG_RESOURCE),
-      new MvcRequestMatcher(introspector, ERROR_URL),
-      AntPathRequestMatcher.antMatcher(NO_PERMISSION),
-      AntPathRequestMatcher.antMatcher(LOGIN_RESOURCE),
-      AntPathRequestMatcher.antMatcher(LOGOUT_RESOURCE),
-      AntPathRequestMatcher.antMatcher(REST_V1_EXTERNAL_API),
-      new MvcRequestMatcher(introspector, NEW_FORM)
-    };
-    return requestMatchers;
+    if (!isUserAuthenticated) {
+      return false;
+    }
+
+    final String referer = request.getHeader("Referer");
+
+    // If request is from Swagger UI
+    if (referer != null && referer.matches(".*/swagger-ui.*")) {
+      return false;
+    }
+
+    // If is authenticated from as API user using Barer Token
+    final String authorizationHeader = request.getHeader("Authorization");
+    final boolean isAuthorizationHeaderPresent =
+        authorizationHeader != null && authorizationHeader.startsWith("Bearer ");
+    if (isAuthorizationHeaderPresent) {
+      return false;
+    }
+
+    // otherwise, CSRF is required
+    return true;
   }
-
-  // Used as constants class
-
 }
