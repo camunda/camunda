d3976f923da1bcfd18587605b95ffca2e10db320
Michał Konopski <58469680+misiekhardcore@users.noreply.github.com>
2024-04-12 11:26:39 +0200

    chore(gha): migrate import static dataset performance to gha (#7737)
    Co-authored-by: Josh Windels <joshua.windels@camunda.com>

---
+++ .github/podSpecs/performanceTests/wait-for-import-to-finish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[[ "$#" -eq 1 ]] || die "1 argument required [NAMESPACE], $# provided"
+
+NAMESPACE=$1
+
+#Monitoring Import of optimize-import (Should be true till data got imported)
+IMPORTING="true"
+until [[ ("$IMPORTING" == "false") ]]; do
+    curl -s "http://elasticsearch.$NAMESPACE:9200/_cat/indices?v" || true
+    IMPORTING=$(curl "http://optimize.$NAMESPACE:8090/api/status" | jq '.engineStatus."camunda-bpm".isImporting') || true
+
+    sleep 60
+done
