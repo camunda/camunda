6a6e03cd5f702150a5991f0bb3a2c0c360f686e6
Mihail <122792941+mihail-ca@users.noreply.github.com>
2024-04-17 14:40:53 +0200

    fix: Handle index value of null in BatchRequest
    fix: Handle index value of null in BatchRequest
    
    Closes #6653
    
    fix: Handle index value of null in BatchRequest

--- operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/sync/RichOpenSearchClient.java
+++ operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/sync/RichOpenSearchClient.java
@@ -18,6 +18,7 @@ package io.camunda.operate.store.opensearch.client.sync;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.operate.conditions.OpensearchCondition;
+import io.camunda.operate.property.OperateProperties;
 import io.camunda.operate.store.opensearch.client.async.OpenSearchAsyncDocumentOperations;
 import io.camunda.operate.store.opensearch.client.async.OpenSearchAsyncIndexOperations;
 import io.camunda.operate.store.opensearch.client.async.OpenSearchAsyncSnapshotOperations;
@@ -51,12 +52,13 @@ public class RichOpenSearchClient {
       BeanFactory beanFactory,
       OpenSearchClient openSearchClient,
       OpenSearchAsyncClient openSearchAsyncClient,
+      OperateProperties operateProperties,
       ObjectMapper objectMapper) {
     this.beanFactory = beanFactory;
     this.openSearchClient = openSearchClient;
     async = new Async(openSearchAsyncClient);
     openSearchBatchOperations =
-        new OpenSearchBatchOperations(LOGGER, openSearchClient, beanFactory);
+        new OpenSearchBatchOperations(LOGGER, openSearchClient, beanFactory, operateProperties.getOpensearch());
     openSearchClusterOperations = new OpenSearchClusterOperations(LOGGER, openSearchClient);
     openSearchDocumentOperations = new OpenSearchDocumentOperations(LOGGER, openSearchClient);
     openSearchIndexOperations =
