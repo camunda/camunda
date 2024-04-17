93fdd802427001b9a1f63ff29895b757dfde9db9
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-04-17 13:40:07 +0200

    fix: allow reusing the same builder
    (cherry picked from commit 0a9702fb8f0f5add27d0f8ca9743c263d2af3da6)

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/command/ActivateJobsCommandImpl.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/command/ActivateJobsCommandImpl.java
@@ -113,7 +113,7 @@ public final class ActivateJobsCommandImpl
 
   @Override
   public ZeebeFuture<ActivateJobsResponse> send() {
-
+    builder.clearTenantIds();
     if (customTenantIds.isEmpty()) {
       builder.addAllTenantIds(defaultTenantIds);
     } else {
