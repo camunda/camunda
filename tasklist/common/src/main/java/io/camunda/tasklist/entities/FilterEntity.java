f5e2b92ddf6a3c797b57feac1aa9b2d3bdc72659
marcosgvieira <marcos.vieira@camunda.com>
2024-04-17 13:43:49 +0100

    feat: adding Filter Index structure

---
+++ tasklist/common/src/main/java/io/camunda/tasklist/entities/FilterEntity.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.tasklist.entities;
+
+import java.util.List;
+
+public class FilterEntity extends TenantAwareTasklistEntity<FilterEntity> {
+
+  private String name;
+  private String createdBy;
+  private String filter;
+  private List<String> candidateUsers;
+  private List<String> candidateGroups;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(final String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public String getFilter() {
+    return filter;
+  }
+
+  public void setFilter(final String filter) {
+    this.filter = filter;
+  }
+
+  public List<String> getCandidateUsers() {
+    return candidateUsers;
+  }
+
+  public void setCandidateUsers(final List<String> candidateUsers) {
+    this.candidateUsers = candidateUsers;
+  }
+
+  public List<String> getCandidateGroups() {
+    return candidateGroups;
+  }
+
+  public void setCandidateGroups(final List<String> candidateGroups) {
+    this.candidateGroups = candidateGroups;
+  }
+
+}
