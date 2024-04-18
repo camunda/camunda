21b34c46a4596e1d50dd4102b85668cc03628b74
ana.vinogradova <ana.vinogradova@camunda.com>
2024-04-18 17:05:31 +0100

    feat: adding tests for saas and oidc auth config

---
+++ spring-boot-starter-camunda-sdk/src/test/java/io/camunda/zeebe/spring/client/config/AuthenticationConfigurationOidcTest.java
@@ -0,0 +1,63 @@
+package io.camunda.zeebe.spring.client.config;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import io.camunda.zeebe.spring.client.configuration.AuthenticationConfiguration;
+import io.camunda.zeebe.spring.common.auth.Authentication;
+import io.camunda.zeebe.spring.common.auth.Product;
+import io.camunda.zeebe.spring.common.auth.selfmanaged.SelfManagedAuthentication;
+import java.time.Instant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import wiremock.com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+@SpringBootTest(
+    classes = {AuthenticationConfiguration.class},
+    properties = {
+      "camunda.client.mode=oidc",
+      "camunda.client.auth.client-id=my-client-id",
+      "camunda.client.auth.client-secret=my-client-secret",
+      "camunda.client.auth.issuer=http://localhost:14682/auth-server"
+    })
+@WireMockTest(httpPort = 14682)
+public class AuthenticationConfigurationOidcTest {
+  private static final String ACCESS_TOKEN =
+      JWT.create().withExpiresAt(Instant.now().plusSeconds(300)).sign(Algorithm.none());
+  @Autowired Authentication authentication;
+
+  @Test
+  void shouldBeSelfManaged() {
+    assertThat(authentication).isExactlyInstanceOf(SelfManagedAuthentication.class);
+  }
+
+  @Test
+  void shouldHaveZeebeAuth() {
+    final String accessToken = ACCESS_TOKEN;
+    stubFor(
+        post("/auth-server/protocol/openid-connect/token")
+            .willReturn(
+                ok().withJsonBody(
+                        JsonNodeFactory.instance
+                            .objectNode()
+                            .put("access_token", accessToken)
+                            .put("expires_in", 300))));
+    assertThat(authentication.getTokenHeader(Product.ZEEBE))
+        .isNotNull()
+        .isEqualTo(entry("Authorization", "Bearer " + accessToken));
+    verify(
+        postRequestedFor(urlEqualTo("/auth-server/protocol/openid-connect/token"))
+            .withHeader("Content-Type", equalTo("application/x-www-form-urlencoded")));
+  }
+}
---
+++ spring-boot-starter-camunda-sdk/src/test/java/io/camunda/zeebe/spring/client/config/AuthenticationConfigurationSaasTest.java
@@ -0,0 +1,61 @@
+package io.camunda.zeebe.spring.client.config;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import io.camunda.zeebe.spring.client.configuration.AuthenticationConfiguration;
+import io.camunda.zeebe.spring.common.auth.Authentication;
+import io.camunda.zeebe.spring.common.auth.Product;
+import io.camunda.zeebe.spring.common.auth.saas.SaaSAuthentication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import wiremock.com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+@SpringBootTest(
+    classes = {AuthenticationConfiguration.class},
+    properties = {
+      "camunda.client.mode=saas",
+      "camunda.client.cluster-id=12345",
+      "camunda.client.region=bru-2",
+      "camunda.client.auth.client-id=my-client-id",
+      "camunda.client.auth.client-secret=my-client-secret",
+      "camunda.client.auth.issuer=http://localhost:14682/auth-server"
+    })
+@WireMockTest(httpPort = 14682)
+public class AuthenticationConfigurationSaasTest {
+  private static final String ACCESS_TOKEN =
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlFVVXdPVFpDUTBVM01qZEVRME0wTkRFelJrUkJORFk0T0RZeE1FRTBSa1pFUlVWRVF6bERNZyJ9.eyJodHRwczovL2FwaS5jbG91ZC5jYW11bmRhLmlvL2NsaWVudC9vcmdJZCI6IjM2YmQ4MDAxLTUyZGItNGIzZS05ZWZiLWViMzBhZTQyMDM3YiIsImh0dHBzOi8vYXBpLmNsb3VkLmNhbXVuZGEuaW8vY2xpZW50L3V1aWQiOiJjNDJlMjY3Yy03YWE2LTQ5ZmItYTgyMi0zYmQ0NjgxOGUzNzUiLCJpc3MiOiJodHRwczovL3dlYmxvZ2luLmNsb3VkLmNhbXVuZGEuaW8vIiwic3ViIjoiNGdGMmdvc2xWOEN6TWUyZ0cwVFB1bTNZeDF5TWRld3FAY2xpZW50cyIsImF1ZCI6ImFwaS5jbG91ZC5jYW11bmRhLmlvIiwiaWF0IjoxNzA5MTIxNTIzLCJleHAiOjE3MDkyMDc5MjMsImF6cCI6IjRnRjJnb3NsVjhDek1lMmdHMFRQdW0zWXgxeU1kZXdxIiwic2NvcGUiOiJHZXRXaGl0ZWxpc3RzIFVwZGF0ZVdoaXRlbGlzdHMiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.VlPxXPFFK3Oc8qfYKnPNmTKB29AdLnFRIDz0lmRJ2rDKdd-91D90-qM4Br11HW9tka2BYyJZG3BGkLugV_W0RgycrjGG4kWMAlI22axtU5h8BGiVHFYmudb8eZSYxDk7SN-872sJ1CSSeYkADuGZ_FoTJFEudunta8dVivtaoNXOvIaY8qS8w0Rmyd5plA6oia96pynJYWnwYELim8AAPNX24zWSZW6N16tPRxXT8YPBhg4a-XHu9gM_Q_8modroCFc8q5xw_aApWpbNxFaffMr2lSB9mK7AqXKBbrDzpL4bhZ7lSZVrF3lWVI0lGoROyQXUBYNvy24i-NHVDU0olA";
+  @Autowired Authentication authentication;
+
+  @Test
+  void shouldBeSaas() {
+    assertThat(authentication).isExactlyInstanceOf(SaaSAuthentication.class);
+  }
+
+  @Test
+  void shouldHaveZeebeAuth() {
+    stubFor(
+        post("/auth-server")
+            .willReturn(
+                ok().withJsonBody(
+                        JsonNodeFactory.instance
+                            .objectNode()
+                            .put("access_token", ACCESS_TOKEN)
+                            .put("expires_in", 300))));
+    assertThat(authentication.getTokenHeader(Product.ZEEBE))
+        .isNotNull()
+        .isEqualTo(entry("Authorization", "Bearer " + ACCESS_TOKEN));
+    verify(
+        postRequestedFor(urlEqualTo("/auth-server"))
+            .withHeader("Content-Type", equalTo("application/json")));
+  }
+}
