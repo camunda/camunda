f8eb22d0e7a79a5674bfb6b274881ab52808d180
Nicola Puppa <nicola.puppa@camunda.com>
2024-04-18 15:15:38 +0200

    feat: implementing bounded cache
    (cherry picked from commit f56a5de9798f6578f7d12711b71c7d02c9b03b49)

--- broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/EngineCfg.java
+++ broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/EngineCfg.java
@@ -77,6 +77,7 @@ public final class EngineCfg implements ConfigurationEntry {
         .setMessagesTtlCheckerBatchLimit(messages.getTtlCheckerBatchLimit())
         .setMessagesTtlCheckerInterval(messages.getTtlCheckerInterval())
         .setDrgCacheCapacity(caches.getDrgCacheCapacity())
+        .setFormCacheCapacity(caches.getFormCacheCapacity())
         .setJobsTimeoutCheckerPollingInterval(jobs.getTimeoutCheckerPollingInterval())
         .setJobsTimeoutCheckerBatchLimit(jobs.getTimeoutCheckerBatchLimit())
         .setValidatorsResultsOutputMaxSize(validators.getResultsOutputMaxSize());
