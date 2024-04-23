4fc863f507aaff1a8da3df2710b8b11266f879e6
Roman <roman.smirnov@camunda.com>
2024-04-23 20:31:22 +0200

    build: overwrite static-locations setting in Tasklist
    * Package frontend build to "META-INF/resources/tasklist"
    * Set "spring.web.resources.static-locations" to
      "META-INF/resources/tasklist" that way it will use Tasklist's
      index.html as welcome page
    * Forward to "tasklist/index" when serving the index.html

--- tasklist/webapp/src/main/java/io/camunda/tasklist/Application.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/Application.java
@@ -68,6 +68,8 @@ public class Application {
     System.setProperty(
         "spring.config.location",
         "optional:classpath:/,optional:classpath:/config/,optional:file:./,optional:file:./config/");
+    System.setProperty(
+        "spring.web.resources.static-locations", "classpath:/META-INF/resources/tasklist/");
     final SpringApplication springApplication = new SpringApplication(Application.class);
     // use fully qualified names as bean name, as we have classes with same names for different
     // versions of importer
