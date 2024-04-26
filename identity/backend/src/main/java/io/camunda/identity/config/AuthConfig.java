28c9a7d7b41e9a9162e739b54262dcaca4bbd39f
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2024-04-26 15:24:35 +0200

    fix: remove security filter chain

--- identity/backend/src/main/java/io/camunda/identity/config/AuthConfig.java
+++ identity/backend/src/main/java/io/camunda/identity/config/AuthConfig.java
@@ -9,11 +9,9 @@ package io.camunda.identity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -21,16 +19,5 @@ public class AuthConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
-  }
-
-  @Bean
-  public SecurityFilterChain defaultSecurityFilterChainLocal(final HttpSecurity http)
-      throws Exception {
-    return http.csrf()
-        .disable()
-        .authorizeHttpRequests(
-            (authorize) ->
-                authorize.requestMatchers("/**").permitAll().anyRequest().authenticated())
-        .build();
   }
 }
