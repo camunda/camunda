f71edb09f3fb4e72a4da06403e705c697d1e5daf
Ralf Puchert <ralf.puchert@camunda.com>
2024-04-26 11:11:52 +0200

    fix: ignore missing attributes in processing UserTask
    Related with #17611

--- operate/importer-8_5/src/main/java/io/camunda/operate/zeebeimport/v8_5/processors/UserTaskZeebeRecordProcessor.java
+++ operate/importer-8_5/src/main/java/io/camunda/operate/zeebeimport/v8_5/processors/UserTaskZeebeRecordProcessor.java
@@ -95,7 +95,11 @@ public class UserTaskZeebeRecordProcessor {
         Map.of(
             "candidateUserList",
             new Tuple<>(UserTaskTemplate.CANDIDATE_USERS, userTaskEntity::getCandidateUsers),
+            "candidateUsersList",
+            new Tuple<>(UserTaskTemplate.CANDIDATE_USERS, userTaskEntity::getCandidateUsers),
             "candidateGroupList",
+            new Tuple<>(UserTaskTemplate.CANDIDATE_GROUPS, userTaskEntity::getCandidateGroups),
+            "candidateGroupsList",
             new Tuple<>(UserTaskTemplate.CANDIDATE_GROUPS, userTaskEntity::getCandidateGroups),
             "dueDate",
             new Tuple<>(UserTaskTemplate.DUE_DATE, userTaskEntity::getDueDate),
@@ -108,7 +112,14 @@ public class UserTaskZeebeRecordProcessor {
     map.put(UserTaskTemplate.ACTION, userTaskEntity.getAction());
     for (final var changedAttribute : changedAttributes) {
       final var fieldAndValueSupplier = changedAttributesToUserEntitySupplier.get(changedAttribute);
-      map.put(fieldAndValueSupplier.getLeft(), fieldAndValueSupplier.getRight().get());
+      if (fieldAndValueSupplier != null) {
+        map.put(fieldAndValueSupplier.getLeft(), fieldAndValueSupplier.getRight().get());
+      } else {
+        LOGGER.warn(
+            "Could not find attribute {} from changed attributes {}. This will be ignored.",
+            changedAttribute,
+            changedAttributes);
+      }
     }
     return map;
   }
