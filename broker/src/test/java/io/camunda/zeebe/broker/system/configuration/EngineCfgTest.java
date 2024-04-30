ff98d40d2b7273ad4e0f2ac7f986b044a610cd7c
Mustafa Dagher <mustafa.dagher@camunda.com>
2024-04-30 13:29:06 +0200

    test: verify form and process cache capacities are being read correctly from configs
    (cherry picked from commit 35d5a4130dd82ca971af87b8707aad1477efbd57)

--- broker/src/test/java/io/camunda/zeebe/broker/system/configuration/EngineCfgTest.java
+++ broker/src/test/java/io/camunda/zeebe/broker/system/configuration/EngineCfgTest.java
@@ -36,6 +36,8 @@ final class EngineCfgTest {
     assertThat(configuration.getJobsTimeoutCheckerBatchLimit()).isEqualTo(Integer.MAX_VALUE);
     assertThat(configuration.getValidatorsResultsOutputMaxSize())
         .isEqualTo(EngineConfiguration.DEFAULT_VALIDATORS_RESULTS_OUTPUT_MAX_SIZE);
+    assertThat(configuration.getProcessCacheCapacity())
+        .isEqualTo(EngineConfiguration.DEFAULT_PROCESS_CACHE_CAPACITY);
   }
 
   @Test
@@ -54,5 +56,7 @@ final class EngineCfgTest {
         .isEqualTo(Duration.ofSeconds(15));
     assertThat(configuration.getJobsTimeoutCheckerBatchLimit()).isEqualTo(1000);
     assertThat(configuration.getValidatorsResultsOutputMaxSize()).isEqualTo(2000);
+    assertThat(configuration.getDrgCacheCapacity()).isEqualTo(2000L);
+    assertThat(configuration.getDrgCacheCapacity()).isEqualTo(2000L);
   }
 }
