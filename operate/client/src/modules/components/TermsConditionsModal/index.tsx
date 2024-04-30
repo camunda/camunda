93a751e21bd593ba1a6db02375a372f1b9f83e31
Roman <roman.smirnov@camunda.com>
2024-04-30 10:53:51 +0200

    refactor: update license headers in Operate's frontend

--- operate/client/src/modules/components/TermsConditionsModal/index.tsx
+++ operate/client/src/modules/components/TermsConditionsModal/index.tsx
@@ -1,18 +1,9 @@
 /*
- * Copyright Camunda Services GmbH
- *
- * BY INSTALLING, DOWNLOADING, ACCESSING, USING, OR DISTRIBUTING THE SOFTWARE ("USE"), YOU INDICATE YOUR ACCEPTANCE TO AND ARE ENTERING INTO A CONTRACT WITH, THE LICENSOR ON THE TERMS SET OUT IN THIS AGREEMENT. IF YOU DO NOT AGREE TO THESE TERMS, YOU MUST NOT USE THE SOFTWARE. IF YOU ARE RECEIVING THE SOFTWARE ON BEHALF OF A LEGAL ENTITY, YOU REPRESENT AND WARRANT THAT YOU HAVE THE ACTUAL AUTHORITY TO AGREE TO THE TERMS AND CONDITIONS OF THIS AGREEMENT ON BEHALF OF SUCH ENTITY.
- * "Licensee" means you, an individual, or the entity on whose behalf you receive the Software.
- *
- * Permission is hereby granted, free of charge, to the Licensee obtaining a copy of this Software and associated documentation files to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject in each case to the following conditions:
- * Condition 1: If the Licensee distributes the Software or any derivative works of the Software, the Licensee must attach this Agreement.
- * Condition 2: Without limiting other conditions in this Agreement, the grant of rights is solely for non-production use as defined below.
- * "Non-production use" means any use of the Software that is not directly related to creating products, services, or systems that generate revenue or other direct or indirect economic benefits.  Examples of permitted non-production use include personal use, educational use, research, and development. Examples of prohibited production use include, without limitation, use for commercial, for-profit, or publicly accessible systems or use for commercial or revenue-generating purposes.
- *
- * If the Licensee is in breach of the Conditions, this Agreement, including the rights granted under it, will automatically terminate with immediate effect.
- *
- * SUBJECT AS SET OUT BELOW, THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- * NOTHING IN THIS AGREEMENT EXCLUDES OR RESTRICTS A PARTY’S LIABILITY FOR (A) DEATH OR PERSONAL INJURY CAUSED BY THAT PARTY’S NEGLIGENCE, (B) FRAUD, OR (C) ANY OTHER LIABILITY TO THE EXTENT THAT IT CANNOT BE LAWFULLY EXCLUDED OR RESTRICTED.
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
 
 import {observer} from 'mobx-react';
@@ -36,71 +27,101 @@ const TermsConditionsModal: React.FC<Props> = observer(
           >
             <p>Copyright Camunda Services GmbH</p>
 
+            <p>Camunda License 1.0</p>
+
             <p>
-              BY INSTALLING, DOWNLOADING, ACCESSING, USING, OR DISTRIBUTING THE
-              SOFTWARE ("USE"), YOU INDICATE YOUR ACCEPTANCE TO AND ARE ENTERING
-              INTO A CONTRACT WITH, THE LICENSOR ON THE TERMS SET OUT IN THIS
-              AGREEMENT. IF YOU DO NOT AGREE TO THESE TERMS, YOU MUST NOT USE
-              THE SOFTWARE. IF YOU ARE RECEIVING THE SOFTWARE ON BEHALF OF A
-              LEGAL ENTITY, YOU REPRESENT AND WARRANT THAT YOU HAVE THE ACTUAL
-              AUTHORITY TO AGREE TO THE TERMS AND CONDITIONS OF THIS AGREEMENT
-              ON BEHALF OF SUCH ENTITY. “Licensee” means you, an individual, or
-              the entity on whose behalf you receive the Software.
+              The Camunda License (the “License”) sets forth the terms and
+              conditions under which Camunda Services GmbH ("the Licensor")
+              grants You a license solely to the source code in this repository
+              ("the Software").
             </p>
 
             <p>
-              Permission is hereby granted, free of charge, to the Licensee
-              obtaining a copy of this Software and associated documentation
-              files to deal in the Software without restriction, including
-              without limitation the rights to use, copy, modify, merge,
-              publish, distribute, sublicense, and/or sell copies of the
-              Software, and to permit persons to whom the Software is furnished
-              to do so, subject in each case to the following conditions:
+              Acceptance By Using the Software, You agree to all the terms and
+              conditions below. If Your Use of the Software does not comply with
+              the terms and conditions described in this License, You must
+              purchase a commercial license from the Licensor, its affiliated
+              entities, or authorized resellers, or You must refrain from Using
+              the Software. If You receive the Software in original or modified
+              form from a third party, the terms and conditions outlined in this
+              License apply to Your Use of that Software. You should have
+              received a copy of this License in this case.
+            </p>
+
+            <p>
+              Copyright License Subject to the terms and conditions of this
+              License, the Licensor hereby grants You the non-exclusive,
+              royalty-free, worldwide, non-sublicensable, non-transferable right
+              to Use the Software in any way or manner that would otherwise
+              infringe the Licensor’s copyright as long and insofar as You Use
+              the Software only and limited to the Use in or for the purpose of
+              Using the Software in Non-Production Environment. Each time you
+              distribute or make otherwise publicly available the Software or
+              Derivative Works thereof, the recipient automatically receives a
+              license from the original Licensor to the respective Software or
+              Derivative Works thereof under the terms of this License.
+            </p>
+
+            <p>
+              Conditions and Restrictions All Use of the Software is explicitly
+              made subject to the following conditions:
             </p>
 
             <UnorderedList>
               <ListItem>
-                Condition 1: If the Licensee distributes the Software or any
-                derivative works of the Software, the Licensee must attach this
-                Agreement.
+                You may not move, change, disable, or circumvent the license key
+                functionality in the Software, and You may not remove or obscure
+                any functionality in the Software that is protected by the
+                license key.
               </ListItem>
               <ListItem>
-                Condition 2: Without limiting other conditions in this
-                Agreement, the grant of rights is solely for non-production use
-                as defined below.
+                If You distribute or make available the Software or any
+                modification or Derivative Works thereof (including compiled
+                versions), You must conspicuously display and attach this
+                License on each original or modified copy of the Software and
+                enable the recipient to obtain the source code if You have
+                distributed a compiled version
               </ListItem>
             </UnorderedList>
 
             <p>
-              "Non-production use" means any use of the Software that is not
-              directly related to creating products, services, or systems that
-              generate revenue or other direct or indirect economic benefits.
-              Examples of permitted non-production use include personal use,
-              educational use, research, and development. Examples of prohibited
-              production use include, without limitation, use for commercial,
-              for-profit, or publicly accessible systems or use for commercial
-              or revenue-generating purposes.
+              Patent License Patent and trademark rights are not licensed under
+              this Public License.
             </p>
 
             <p>
-              If the Licensee is in breach of the Conditions, this Agreement,
-              including the rights granted under it, will automatically
-              terminate with immediate effect.
+              No Liability EXCEPT FOR DAMAGES CAUSED BY INTENT OR FRAUDULENTLY
+              CONCEALED DEFECTS, AND EXCEPT FOR DAMAGES RESULTING FROM BREACH OF
+              ANY WARRANTY OR GUARANTEE EXPRESSLY GIVEN BY LICENSOR IN THIS
+              LICENCE, IN NO EVENT WILL LICENSOR BE LIABLE TO YOU ON ANY LEGAL
+              THEORY FOR ANY DAMAGES ARISING OUT OF THIS LICENSE OR THE USE OF
+              THE WORK. ANY MANDATORY STATUTORY LIABILITY UNDER APPLICABLE LAW
+              REMAINS UNAFFECTED.
             </p>
 
             <p>
-              SUBJECT AS SET OUT BELOW, THE SOFTWARE IS PROVIDED “AS IS”,
-              WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-              NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-              PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-              OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-              OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-              OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. NOTHING IN THIS
-              AGREEMENT EXCLUDES OR RESTRICTS A PARTY’S LIABILITY FOR (A) DEATH
-              OR PERSONAL INJURY CAUSED BY THAT PARTY’S NEGLIGENCE, (B) FRAUD,
-              OR (C) ANY OTHER LIABILITY TO THE EXTENT THAT IT CANNOT BE
-              LAWFULLY EXCLUDED OR RESTRICTED.
+              No Warranty EXCEPT AS EXPRESSLY STATED IN THIS LICENSE OR REQUIRED
+              BY APPLICABLE LAW, THE WORKS ARE PROVIDED ON AN "AS IS" BASIS,
+              WITHOUT WARRANTIES OF ANY KIND INCLUDING WITHOUT LIMITATION, ANY
+              WARRANTIES REGARDING THE CONTENTS, ACCURACY, OR FITNESS FOR A
+              PARTICULAR PURPOSE.
+            </p>
+
+            <p>
+              Definitions You refer to the individual or entity agreeing to
+              these terms. Use means any action concerning the Software that,
+              without permission, would make Youliable for infringement under
+              applicable copyright law. Use within the meaning of this License
+              includes, but is not limited to, copying, distribution (with or
+              without modification), making available to the public, and
+              modifying the Software.
+            </p>
+
+            <p>
+              Non-Production Environment means a setting in which the Software
+              is used for development, staging, testing, quality assurance,
+              demonstration, or evaluation purposes, and not for any live or
+              production systems.
             </p>
           </Modal>
         )}
