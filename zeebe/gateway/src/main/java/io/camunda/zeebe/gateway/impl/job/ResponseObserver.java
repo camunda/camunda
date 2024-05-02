8f28b6283590e76e24c8b13cc17077c061e9f50d
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2024-05-02 14:43:48 +0200

    feat: abstract job activation code in gateway
    This abtracts job activation handler code to be protocol-agnostic.
    The functionality is independent from REST/HTTP and gRPC.

---
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/job/ResponseObserver.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.impl.job;
+
+public interface ResponseObserver<T> {
+  void onCompleted();
+
+  void onNext(T element);
+
+  boolean isCancelled();
+
+  void onError(Throwable throwable);
+}
