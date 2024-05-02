44f93c719bebc7ca003fd32ffdb13a383266f65d
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2024-05-02 14:37:11 +0200

    feat: extract common gateway classes into common module
    Moves and generalizes common gateway code into the gateway module.
    Those classes are used by the gRPC and REST gateway modules alike.
    This includes (not exhaustive):
    - Broker command request classes
    - Loggers
    - Polling for activatable jobs, including long-polling
    - Gateway configuration
    - Gateway exceptions
    
    The remainder moves to the gRPC gateway module.

--- zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerUserTaskUpdateRequest.java
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerUserTaskUpdateRequest.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.gateway.rest.impl.broker.request;
+package io.camunda.zeebe.gateway.impl.broker.request;
 
 import io.camunda.zeebe.broker.client.api.dto.BrokerExecuteCommand;
 import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
