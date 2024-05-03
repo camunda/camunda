a67b879ce588c386373e545409286f1ecb713b04
marcosgvieira <marcos.vieira@camunda.com>
2024-05-03 11:54:12 +0100

    refactor: improving code around IdentityAuthorization

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/security/identity/IdentityAuthorizationService.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/security/identity/IdentityAuthorizationService.java
@@ -15,6 +15,7 @@ import io.camunda.tasklist.webapp.security.sso.TokenAuthentication;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,15 +41,11 @@ public class IdentityAuthorizationService {
     // Extract access token based on authentication type
     if (authentication instanceof IdentityAuthentication) {
       accessToken = ((IdentityAuthentication) authentication).getTokens().getAccessToken();
-      return identity.authentication().verifyToken(accessToken).getUserDetails().getGroups();
+      return identity.authentication().getGroups(accessToken);
     } else if (authentication instanceof TokenAuthentication) {
       accessToken = ((TokenAuthentication) authentication).getAccessToken();
       final String organization = ((TokenAuthentication) authentication).getOrganization();
-      return identity
-          .authentication()
-          .verifyToken(accessToken, organization)
-          .getUserDetails()
-          .getGroups();
+      return identity.authentication().getGroups(accessToken, organization);
     }
 
     // Fallback groups if authentication type is unrecognized or access token is null
@@ -71,67 +68,54 @@ public class IdentityAuthorizationService {
     return getFromAuthorization(IdentityAuthorization.PROCESS_PERMISSION_START);
   }
 
-  private List<String> getFromAuthorization(final String type) {
-    if (tasklistProperties.getIdentity().isResourcePermissionsEnabled()
-        && tasklistProperties.getIdentity().getBaseUrl() != null) {
-      final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-      if (authentication instanceof IdentityAuthentication) {
-        final IdentityAuthentication identityAuthentication =
-            (IdentityAuthentication) authentication;
-        return switch (type) {
-          case IdentityAuthorization.PROCESS_PERMISSION_READ ->
-              identityAuthentication.getAuthorizations().getProcessesAllowedToRead();
-          case IdentityAuthorization.PROCESS_PERMISSION_START ->
-              identityAuthentication.getAuthorizations().getProcessesAllowedToStart();
-          default -> null;
-        };
-      } else if (authentication instanceof JwtAuthenticationToken) {
-        final JwtAuthenticationToken jwtAuthenticationToken =
-            (JwtAuthenticationToken) authentication;
-        final Identity identity = SpringContextHolder.getBean(Identity.class);
-
-        return switch (type) {
-          case IdentityAuthorization.PROCESS_PERMISSION_READ ->
-              new IdentityAuthorization(
-                      identity
-                          .authorizations()
-                          .forToken(jwtAuthenticationToken.getToken().getTokenValue()))
-                  .getProcessesAllowedToRead();
-          case IdentityAuthorization.PROCESS_PERMISSION_START ->
-              new IdentityAuthorization(
-                      identity
-                          .authorizations()
-                          .forToken(jwtAuthenticationToken.getToken().getTokenValue()))
-                  .getProcessesAllowedToStart();
-          default -> null;
-        };
-
-      } else if (authentication instanceof TokenAuthentication) {
-        final Identity identity = SpringContextHolder.getBean(Identity.class);
-
-        return switch (type) {
-          case IdentityAuthorization.PROCESS_PERMISSION_READ ->
-              new IdentityAuthorization(
-                      identity
-                          .authorizations()
-                          .forToken(
-                              ((TokenAuthentication) authentication).getAccessToken(),
-                              ((TokenAuthentication) authentication).getOrganization()))
-                  .getProcessesAllowedToRead();
-          case IdentityAuthorization.PROCESS_PERMISSION_START ->
-              new IdentityAuthorization(
-                      identity
-                          .authorizations()
-                          .forToken(
-                              ((TokenAuthentication) authentication).getAccessToken(),
-                              ((TokenAuthentication) authentication).getOrganization()))
-                  .getProcessesAllowedToStart();
-          default -> null;
-        };
-      }
+  private Optional<IdentityAuthorization> getIdentityAuthorization() {
+    if (!tasklistProperties.getIdentity().isResourcePermissionsEnabled()
+        || tasklistProperties.getIdentity().getBaseUrl() == null) {
+      return Optional.empty();
     }
-    final List<String> result = new ArrayList<String>();
-    result.add(IdentityProperties.ALL_RESOURCES);
-    return result;
+
+    final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    return switch (authentication) {
+      case final IdentityAuthentication identityAuthentication ->
+          Optional.of(identityAuthentication.getAuthorizations());
+      case final JwtAuthenticationToken jwtAuthenticationToken -> {
+        final Identity identity = SpringContextHolder.getBean(Identity.class);
+        yield Optional.of(
+            new IdentityAuthorization(
+                identity
+                    .authorizations()
+                    .forToken(jwtAuthenticationToken.getToken().getTokenValue())));
+      }
+
+      case final TokenAuthentication tokenAuthentication -> {
+        final Identity identity = SpringContextHolder.getBean(Identity.class);
+        yield Optional.of(
+            new IdentityAuthorization(
+                identity
+                    .authorizations()
+                    .forToken(
+                        tokenAuthentication.getAccessToken(),
+                        tokenAuthentication.getOrganization())));
+      }
+      default -> Optional.empty();
+    };
+  }
+
+  private List<String> getFromAuthorization(final String type) {
+    final Optional<IdentityAuthorization> optIdentityAuthorization = getIdentityAuthorization();
+    if (optIdentityAuthorization.isEmpty()) {
+      return Collections.singletonList(IdentityProperties.ALL_RESOURCES);
+    }
+
+    final IdentityAuthorization identityAuthorization = optIdentityAuthorization.get();
+
+    return switch (type) {
+      case IdentityAuthorization.PROCESS_PERMISSION_READ ->
+          identityAuthorization.getProcessesAllowedToRead();
+      case IdentityAuthorization.PROCESS_PERMISSION_START ->
+          identityAuthorization.getProcessesAllowedToStart();
+      default -> Collections.emptyList();
+    };
   }
 }
