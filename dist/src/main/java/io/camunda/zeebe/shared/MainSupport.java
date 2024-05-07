85e61cf510c17a2c30931d68bef6a4a6b36d2bfb
Euro Lew <euro.lew@camunda.com>
2024-05-07 12:05:52 +0100

    docs: typo
    (cherry picked from commit ed55efa56a5f928dbda24aeba7d557c669a5ee68)

--- dist/src/main/java/io/camunda/zeebe/shared/MainSupport.java
+++ dist/src/main/java/io/camunda/zeebe/shared/MainSupport.java
@@ -41,7 +41,7 @@ public final class MainSupport {
     // simplify bounding direct memory usage of Netty by ensuring all servers use the same allocator
     putSystemPropertyIfAbsent("io.grpc.netty.useCustomAllocator", Boolean.toString(false));
 
-    // Logging adapter to Log4
+    // Logging adapter to Log4j
     putSystemPropertyIfAbsent(
         "java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager");
   }
