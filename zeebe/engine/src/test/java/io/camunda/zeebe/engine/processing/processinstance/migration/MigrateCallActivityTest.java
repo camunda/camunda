6879d841b54be073139638321c8b5c4253acc923
Nico Korthout <nico.korthout@camunda.com>
2024-05-07 16:55:59 +0200

    test: continue flow of migrated call activity
    After migrating a call activity, completing the child process instance
    should continue the flow after the call activity in the parent process
    instance according to the target process definition.
    
    Co-authored-by: berkaycanbc <berkay.can@camunda.com>
    (cherry picked from commit a292a90637a7a5cda2371c09884d582a5f15e8b3)

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/processinstance/migration/MigrateCallActivityTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/processinstance/migration/MigrateCallActivityTest.java
@@ -94,4 +94,78 @@ public class MigrateCallActivityTest {
         .describedAs("Expect that version number did not change")
         .hasVersion(1);
   }
+
+  @Test
+  public void shouldContinueFlowAfterMigratingParentProcessInstance() {
+    // given
+    final String processId = helper.getBpmnProcessId() + "_source";
+    final String childProcessId = helper.getBpmnProcessId() + "_child";
+    final String targetProcessId = helper.getBpmnProcessId() + "_target";
+    final var deployment =
+        ENGINE
+            .deployment()
+            .withXmlResource(
+                Bpmn.createExecutableProcess(processId)
+                    .startEvent("start")
+                    .callActivity("A", c -> c.zeebeProcessId(childProcessId))
+                    .endEvent("end")
+                    .done())
+            .withXmlResource(
+                Bpmn.createExecutableProcess(targetProcessId)
+                    .startEvent("start")
+                    .callActivity("B", c -> c.zeebeProcessId(childProcessId))
+                    .userTask("C")
+                    .endEvent("end")
+                    .done())
+            .withXmlResource(
+                Bpmn.createExecutableProcess(childProcessId)
+                    .startEvent("start")
+                    .userTask("task", AbstractUserTaskBuilder::zeebeUserTask)
+                    .endEvent("end")
+                    .done())
+            .deploy();
+    final long parentProcessInstanceKey =
+        ENGINE.processInstance().ofBpmnProcessId(processId).create();
+
+    final long targetProcessDefinitionKey =
+        extractProcessDefinitionKeyByProcessId(deployment, targetProcessId);
+
+    final var childUserTask =
+        RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
+            .withParentProcessInstanceKey(parentProcessInstanceKey)
+            .withElementType(BpmnElementType.USER_TASK)
+            .withElementId("task")
+            .getFirst();
+    final var childProcessInstanceKey = childUserTask.getValue().getProcessInstanceKey();
+
+    // when
+    ENGINE
+        .processInstance()
+        .withInstanceKey(parentProcessInstanceKey)
+        .migration()
+        .withTargetProcessDefinitionKey(targetProcessDefinitionKey)
+        .addMappingInstruction("A", "B")
+        .migrate();
+
+    // then
+    ENGINE.userTask().ofInstance(childProcessInstanceKey).complete();
+
+    assertThat(
+            RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_COMPLETED)
+                .withProcessInstanceKey(childProcessInstanceKey)
+                .withElementType(BpmnElementType.PROCESS)
+                .getFirst())
+        .describedAs("Expect that the child process instance completed")
+        .isNotNull();
+
+    assertThat(
+            RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
+                .withProcessInstanceKey(parentProcessInstanceKey)
+                .withElementType(BpmnElementType.USER_TASK)
+                .withElementId("C")
+                .getFirst())
+        .describedAs(
+            "Expect that the parent process instance continues in the target process after migrating")
+        .isNotNull();
+  }
 }
