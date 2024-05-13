58b6c08ba36860a7d0854804e149cd925085f6fc
ana.vinogradova <ana.vinogradova@camunda.com>
2024-05-13 14:38:31 +0100

    refactor: renaming the test
    Renaming the CamundaClientPropertiesOidcTest to
    CamundaClientPropertiesSelfManagedTest as it reflects the actual usage
    of this profile

--- spring-boot-starter-camunda-sdk/src/test/java/io/camunda/zeebe/spring/client/properties/CamundaClientPropertiesSelfManagedTest.java
+++ spring-boot-starter-camunda-sdk/src/test/java/io/camunda/zeebe/spring/client/properties/CamundaClientPropertiesSelfManagedTest.java
@@ -25,7 +25,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(
     classes = CamundaClientPropertiesTestConfig.class,
     properties = "camunda.client.mode=oidc")
-public class CamundaClientPropertiesOidcTest {
+public class CamundaClientPropertiesSelfManagedTest {
   @Autowired CamundaClientProperties properties;
 
   @Test
