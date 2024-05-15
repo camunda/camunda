0a9d249af669c985547f3ab8cdea3a174f84250a
Euro Lew <euro.lew@camunda.com>
2024-05-15 17:44:14 +0100

    feat: added ability to generate Sfvchecksums with full file checksums

--- zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/impl/SnapshotChecksum.java
+++ zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/impl/SnapshotChecksum.java
@@ -7,16 +7,22 @@
  */
 package io.camunda.zeebe.snapshots.impl;
 
+import io.camunda.zeebe.snapshots.ChecksumProvider;
 import io.camunda.zeebe.snapshots.ImmutableChecksumsSFV;
 import io.camunda.zeebe.snapshots.MutableChecksumsSFV;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 final class SnapshotChecksum {
@@ -42,9 +48,23 @@ final class SnapshotChecksum {
   }
 
   public static MutableChecksumsSFV calculate(final Path snapshotDirectory) throws IOException {
+    return createChecksumForSnapshot(snapshotDirectory, SnapshotChecksum::createCombinedChecksum);
+  }
+
+  public static MutableChecksumsSFV calculateWithFullFileChecksums(final Path snapshotDirectory)
+      throws IOException {
+    return createChecksumForSnapshot(
+        snapshotDirectory, SnapshotChecksum::createCombinedChecksumWithFullFileChecksums);
+  }
+
+  private static MutableChecksumsSFV createChecksumForSnapshot(
+      final Path snapshotDirectory,
+      final BiFunction<Stream<Path>, Path, MutableChecksumsSFV> checksumFunction)
+      throws IOException {
+
     try (final var fileStream =
         Files.list(snapshotDirectory).filter(SnapshotChecksum::isNotMetadataFile).sorted()) {
-      final var sfvChecksum = createCombinedChecksum(fileStream);
+      final var sfvChecksum = checksumFunction.apply(fileStream, snapshotDirectory);
 
       // While persisting transient snapshot, the checksum of metadata file is added at the end.
       // Hence when we recalculate the checksum, we must follow the same order. Otherwise base on
@@ -82,16 +102,37 @@ final class SnapshotChecksum {
    *
    * @return the SfvChecksum object
    */
-  private static SfvChecksumImpl createCombinedChecksum(final Stream<Path> files) {
+  private static SfvChecksumImpl createCombinedChecksum(
+      final Stream<Path> files, final Path snapshotDirectory) {
     final SfvChecksumImpl checksum = new SfvChecksumImpl();
-    files.forEachOrdered(
-        path -> {
-          try {
-            checksum.updateFromFile(path);
-          } catch (final IOException e) {
-            throw new UncheckedIOException(e);
-          }
-        });
+    files.forEachOrdered(path -> updateChecksum(checksum, Collections.emptyMap(), path));
     return checksum;
+  }
+
+  private static SfvChecksumImpl createCombinedChecksumWithFullFileChecksums(
+      final Stream<Path> files, final Path snapshotDirectory) {
+    final SfvChecksumImpl checksum = new SfvChecksumImpl();
+    final Map<String, byte[]> fullFileChecksums =
+        ChecksumProvider.getSnapshotChecksums(snapshotDirectory);
+    files.forEachOrdered(path -> updateChecksum(checksum, fullFileChecksums, path));
+    return checksum;
+  }
+
+  private static void updateChecksum(
+      final MutableChecksumsSFV checksum,
+      final Map<String, byte[]> fullFileChecksums,
+      final Path file) {
+    if (fullFileChecksums.containsKey(file.getFileName().toString())) {
+      final String fileName = file.getFileName().toString();
+      final Integer sstChecksum =
+          ByteBuffer.wrap(fullFileChecksums.get(fileName)).order(ByteOrder.BIG_ENDIAN).getInt();
+      checksum.updateFromChecksum(file, Integer.toUnsignedLong(sstChecksum));
+    } else {
+      try {
+        checksum.updateFromFile(file);
+      } catch (final IOException e) {
+        throw new UncheckedIOException(e);
+      }
+    }
   }
 }
