5355976c5dccd48cdf15b7af474e72a4524fb303
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-05-21 16:38:40 +0200

    feat: Add schema difference methos for Open search

--- tasklist/els-schema/src/main/java/io/camunda/tasklist/os/IndexSchemaValidatorOpenSearch.java
+++ tasklist/els-schema/src/main/java/io/camunda/tasklist/os/IndexSchemaValidatorOpenSearch.java
@@ -9,13 +9,18 @@ package io.camunda.tasklist.os;
 
 import static io.camunda.tasklist.util.CollectionUtil.map;
 
+import com.google.common.collect.Maps;
 import io.camunda.tasklist.data.conditionals.OpenSearchCondition;
 import io.camunda.tasklist.exceptions.TasklistRuntimeException;
 import io.camunda.tasklist.property.TasklistProperties;
+import io.camunda.tasklist.schema.IndexMapping;
 import io.camunda.tasklist.schema.IndexMapping.IndexMappingProperty;
+import io.camunda.tasklist.schema.IndexMappingDifference;
 import io.camunda.tasklist.schema.IndexSchemaValidator;
 import io.camunda.tasklist.schema.SemanticVersion;
 import io.camunda.tasklist.schema.indices.IndexDescriptor;
+import io.camunda.tasklist.schema.manager.SchemaManager;
+import java.io.IOException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -41,6 +46,9 @@ public class IndexSchemaValidatorOpenSearch implements IndexSchemaValidator {
   @Autowired TasklistProperties tasklistProperties;
 
   @Autowired RetryOpenSearchClient retryOpenSearchClient;
+
+  @Autowired
+  SchemaManager schemaManager;
 
   private Set<String> getAllIndexNamesForIndex(final String index) {
     final String indexPattern = String.format("%s-%s*", getIndexPrefix(), index);
@@ -130,14 +138,50 @@ public class IndexSchemaValidatorOpenSearch implements IndexSchemaValidator {
 
   @Override
   public void validateIndexVersions() {
-    //TODO: implement in the scope of Open Search
+    if (!hasAnyTasklistIndices()) {
+      return;
+    }
+    final Set<String> errors = new HashSet<>();
+    indexDescriptors.forEach(
+        indexDescriptor -> {
+          final Set<String> oldVersions = olderVersionsForIndex(indexDescriptor);
+          final Set<String> newerVersions = newerVersionsForIndex(indexDescriptor);
+          if (oldVersions.size() > 1) {
+            errors.add(
+                String.format(
+                    "More than one older version for %s (%s) found: %s",
+                    indexDescriptor.getIndexName(), indexDescriptor.getVersion(), oldVersions));
+          }
+          if (!newerVersions.isEmpty()) {
+            errors.add(
+                String.format(
+                    "Newer version(s) for %s (%s) already exists: %s",
+                    indexDescriptor.getIndexName(), indexDescriptor.getVersion(), newerVersions));
+          }
+        });
+    if (!errors.isEmpty()) {
+      throw new TasklistRuntimeException("Error(s) in index schema: " + String.join(";", errors));
+    }
 
   }
 
   @Override
-  public Map<IndexDescriptor, Set<IndexMappingProperty>> validateIndexMappings() {
-    //TODO: implement in the scope of Open Search
-    return Map.of();
+  public Map<IndexDescriptor, Set<IndexMappingProperty>> validateIndexMappings()
+      throws IOException {
+    final Map<IndexDescriptor, Set<IndexMappingProperty>> newFields = new HashMap<>();
+    final Map<String, IndexMapping> indexMappings =
+        schemaManager.getIndexMappings(schemaManager.getIndexPrefix() + "*");
+    for (final IndexDescriptor indexDescriptor : indexDescriptors) {
+      final Map<String, IndexMapping> indexMappingsGroup =
+          filterIndexMappings(indexMappings, indexDescriptor);
+      // we don't check indices that were not yet created
+      if (!indexMappingsGroup.isEmpty()) {
+        final IndexMappingDifference difference =
+            getDifference(indexDescriptor, indexMappingsGroup);
+        validateDifferenceAndCollectNewFields(indexDescriptor, difference, newFields);
+      }
+    }
+    return newFields;
   }
 
   private Set<String> versionsForIndex(final IndexDescriptor indexDescriptor) {
@@ -169,5 +213,86 @@ public class IndexSchemaValidatorOpenSearch implements IndexSchemaValidator {
       }
     }
     return true;
+  }
+
+  /**
+   * Leave only runtime and dated indices that correspond to the given IndexDescriptor.
+   *
+   * @param indexMappings
+   * @param indexDescriptor
+   * @return
+   */
+  private Map<String, IndexMapping> filterIndexMappings(
+      final Map<String, IndexMapping> indexMappings, final IndexDescriptor indexDescriptor) {
+    return Maps.filterEntries(
+        indexMappings,
+        e -> e.getKey().matches(indexDescriptor.getAllVersionsIndexNameRegexPattern()));
+  }
+
+  private IndexMappingDifference getDifference(
+      final IndexDescriptor indexDescriptor, final Map<String, IndexMapping> indexMappingsGroup) {
+    return getIndexMappingDifference(indexDescriptor, indexMappingsGroup);
+  }
+
+  private IndexMappingDifference getIndexMappingDifference(
+      final IndexDescriptor indexDescriptor, final Map<String, IndexMapping> indexMappingsGroup) {
+    final IndexMapping indexMappingMustBe = schemaManager.getExpectedIndexFields(indexDescriptor);
+
+    IndexMappingDifference difference = null;
+    // compare every index in group
+    for (final Map.Entry<String, IndexMapping> singleIndexMapping : indexMappingsGroup.entrySet()) {
+      final IndexMappingDifference currentDifference =
+          new IndexMappingDifference.IndexMappingDifferenceBuilder()
+              .setLeft(indexMappingMustBe)
+              .setRight(singleIndexMapping.getValue())
+              .build();
+      if (!currentDifference.isEqual()) {
+        if (difference == null) {
+          difference = currentDifference;
+          // If there is a difference between the template and the existing runtime/data indices,
+          // all those indices should have the same difference. Compare based only on the
+          // differences (exclude the IndexMapping fields in the comparison)
+        } else if (!difference.checkEqualityForDifferences(currentDifference)) {
+          throw new TasklistRuntimeException(
+              "Ambiguous schema update. First bring runtime and date indices to one schema. Difference 1: "
+                  + difference
+                  + ". Difference 2: "
+                  + currentDifference);
+        }
+      }
+    }
+    return difference;
+  }
+
+  private void validateDifferenceAndCollectNewFields(
+      final IndexDescriptor indexDescriptor,
+      final IndexMappingDifference difference,
+      final Map<IndexDescriptor, Set<IndexMappingProperty>> newFields) {
+    if (difference != null && !difference.isEqual()) {
+      LOGGER.debug(
+          String.format(
+              "Index fields differ from expected. Index name: %s. Difference: %s.",
+              indexDescriptor.getIndexName(), difference));
+      if (!difference.getEntriesDiffering().isEmpty()) {
+        final String errorMsg =
+            String.format(
+                "Index name: %s. Not supported index changes are introduced. Data migration is required. Changes found: %s",
+                indexDescriptor.getIndexName(), difference.getEntriesDiffering());
+        LOGGER.error(errorMsg);
+        throw new TasklistRuntimeException(errorMsg);
+      } else if (!difference.getEntriesOnlyOnRight().isEmpty()) {
+        final String message =
+            String.format(
+                "Index name: %s. Field deletion is requested, will be ignored. Fields: %s",
+                indexDescriptor.getIndexName(), difference.getEntriesOnlyOnRight());
+        LOGGER.info(message);
+      } else if (!difference.getEntriesOnlyOnLeft().isEmpty()) {
+        newFields.put(indexDescriptor, difference.getEntriesOnlyOnLeft());
+      }
+    } else {
+      LOGGER.debug(
+          String.format(
+              "Index fields are up to date. Index name: %s.", indexDescriptor.getIndexName()));
+    }
   }
 }
