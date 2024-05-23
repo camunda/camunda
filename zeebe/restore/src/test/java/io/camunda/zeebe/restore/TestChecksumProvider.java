05b7469730b28e6f040334739e5a9076f28090bc
Euro Lew <euro.lew@camunda.com>
2024-05-23 15:23:25 +0100

    test: fix broke partition restore service tests
    (cherry picked from commit 43fe60ab6e8699f728673604bf894b8228ad5d1b)

---
+++ zeebe/restore/src/test/java/io/camunda/zeebe/restore/TestChecksumProvider.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.restore;
+
+import io.camunda.zeebe.db.ChecksumProvider;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Map;
+
+public class TestChecksumProvider implements ChecksumProvider {
+  @Override
+  public Map<String, byte[]> getSnapshotChecksums(final Path snapshotPath) {
+    return Collections.emptyMap();
+  }
+}
