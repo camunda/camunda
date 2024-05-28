686d7bc5e7099610748b9dd836f8a61ec7b3b039
Euro Lew <euro.lew@camunda.com>
2024-05-28 11:28:19 +0100

    refactor: hide implementation details of rocksDB file checksum
    (cherry picked from commit 4f7148d43a1a5399ce5fc9ee3f527a7ca841fa5d)

--- zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/ChecksumProviderRocksDBImpl.java
+++ zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/ChecksumProviderRocksDBImpl.java
@@ -8,6 +8,8 @@
 package io.camunda.zeebe.db.impl.rocksdb;
 
 import io.camunda.zeebe.snapshots.ChecksumProvider;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -18,11 +20,11 @@ import org.rocksdb.RocksDBException;
 public class ChecksumProviderRocksDBImpl implements ChecksumProvider {
 
   @Override
-  public Map<String, byte[]> getSnapshotChecksums(final Path snapshotPath) {
+  public Map<String, Long> getSnapshotChecksums(final Path snapshotPath) {
     try (final var db = RocksDB.openReadOnly(snapshotPath.toString())) {
       return db.getLiveFilesMetaData().stream()
           .filter(fileMetaData -> fileMetaData.fileChecksum().length != 0)
-          .collect(Collectors.toMap(this::getMetadataName, LiveFileMetaData::fileChecksum));
+          .collect(Collectors.toMap(this::getMetadataName, this::rocksDBChecksumAsLong));
     } catch (final RocksDBException e) {
       throw new RuntimeException(e);
     }
@@ -32,5 +34,10 @@ public class ChecksumProviderRocksDBImpl implements ChecksumProvider {
     return fileMetaData.fileName().substring(1);
     //        there is a leading '/' which breaks interactions with the Java Path.getFileName which
     //     returns the file name without a leading '/'
+  }
+
+  private Long rocksDBChecksumAsLong(final LiveFileMetaData fileMetaData) {
+    final var checksum = fileMetaData.fileChecksum();
+    return Integer.toUnsignedLong(ByteBuffer.wrap(checksum).order(ByteOrder.BIG_ENDIAN).getInt());
   }
 }
