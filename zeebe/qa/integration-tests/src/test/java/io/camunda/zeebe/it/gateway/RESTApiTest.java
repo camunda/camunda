5d1d9fce8a4f10dcdae3125cca70f690c2f465bf
Christopher Zell <zelldon91@googlemail.com>
2024-05-29 16:29:08 +0200

    test: add when/then
    When - send request via HTTP client
    Then - result should contain PI

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/gateway/RESTApiTest.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/gateway/RESTApiTest.java
@@ -7,9 +7,20 @@
  */
 package io.camunda.zeebe.it.gateway;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.model.bpmn.Bpmn;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneCamunda;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 
 @ZeebeIntegration
@@ -19,9 +30,29 @@ public class RESTApiTest {
 
   @Test
   public void shouldAcceptRequest() {
-
     // given
+    final ZeebeClient zeebeClient = testStandaloneCamunda.newClientBuilder().build();
 
+    zeebeClient
+        .newDeployResourceCommand()
+        .addProcessModel(
+            Bpmn.createExecutableProcess("process")
+                .startEvent()
+                .serviceTask("test")
+                .zeebeJobType("type")
+                .endEvent()
+                .done(),
+            "simple.bpmn")
+        .send()
+        .join();
+
+    final ProcessInstanceEvent processInstanceEvent =
+        zeebeClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId("process")
+            .latestVersion()
+            .send()
+            .join();
     // given
     //
     // 1. Elastic - Testcontainer?
@@ -29,6 +60,37 @@ public class RESTApiTest {
     //
     // 5. Deployment of process model
     // 6. Create instance
+
+    // when
+    final URI restAddress = testStandaloneCamunda.restAddress();
+    final HttpClient httpClient = HttpClient.newHttpClient();
+
+    Awaitility.await("should receive data from ES")
+        .untilAsserted(
+            () -> {
+              final HttpRequest request =
+                  HttpRequest.newBuilder()
+                      .uri(new URI(String.format("%sv1/process-instances/search1", restAddress)))
+                      .header("content-type", "application/json")
+                      .POST(
+                          HttpRequest.BodyPublishers.ofString(
+                              String.format(
+                                  "{\"filter\":{\"key\":%d}}",
+                                  processInstanceEvent.getProcessInstanceKey())))
+                      .build();
+
+              final HttpResponse<String> response =
+                  httpClient.send(request, BodyHandlers.ofString());
+
+              assertThat(response.statusCode())
+                  .withFailMessage(
+                      () ->
+                          "Expect success response, got "
+                              + response.body()
+                              + " with code: "
+                              + response.statusCode())
+                  .isEqualTo(200);
+            });
 
     // when
     // Query REST API
