19281ba3b80e295929829dbdbb99a4eb7700176c
Michał Konopski <58469680+misiekhardcore@users.noreply.github.com>
2024-05-31 11:37:38 +0200

    ci: add alpha suffix to release branch (#13342)

--- .github/scripts/get-next-development-version.sh
+++ .github/scripts/get-next-development-version.sh
@@ -13,19 +13,25 @@ if [[ -z "$RELEASE_TYPE" ]]; then
   exit 1
 fi
 
+if [[ "$RELEASE_TYPE" == "alpha" && -z "$RELEASE_ALPHA_NUMBER" ]]; then
+  echo "Alpha release number is required"
+  exit 1
+fi
+
 # Set the Internal Field Separator to '.'
 IFS='.' read -r -a numbers <<<"$RELEASE_VERSION"
+suffix=''
 
 if [[ "$RELEASE_TYPE" == "major" ]]; then
   numbers[0]=$((numbers[0] + 1))
 elif [[ "$RELEASE_TYPE" == "minor" ]]; then
   numbers[1]=$((numbers[1] + 1))
 elif [[ "$RELEASE_TYPE" == "alpha" ]]; then
-  # here we don't do anything
+  suffix="-alpha$RELEASE_ALPHA_NUMBER"
   continue
 else
   echo "Invalid release type"
   exit 1
 fi
 
-echo "next_development_version=${numbers[0]}.${numbers[1]}.${numbers[2]}" >>$GITHUB_OUTPUT
+echo "next_development_version=${numbers[0]}.${numbers[1]}.${numbers[2]}${suffix}" >>$GITHUB_OUTPUT
