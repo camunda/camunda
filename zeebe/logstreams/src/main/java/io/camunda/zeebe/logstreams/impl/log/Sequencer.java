097f8ce4796c6fe7be01aba5fa934da9e815e915
jawaharbabugit <jawaharbabu199914@gmail.com>
2024-06-10 19:30:16 -0400

    perf: Optimize isEntryValid method and record validation

--- zeebe/logstreams/src/main/java/io/camunda/zeebe/logstreams/impl/log/Sequencer.java
+++ zeebe/logstreams/src/main/java/io/camunda/zeebe/logstreams/impl/log/Sequencer.java
@@ -123,9 +123,6 @@ final class Sequencer implements LogStreamWriter, Closeable {
   }
 
   private static boolean isEntryValid(final LogAppendEntry entry) {
-    return entry.recordValue() != null
-        && entry.recordValue().getLength() > 0
-        && entry.recordMetadata() != null
-        && entry.recordMetadata().getLength() > 0;
+    return entry.recordValue() != null && entry.recordMetadata() != null;
   }
 }
