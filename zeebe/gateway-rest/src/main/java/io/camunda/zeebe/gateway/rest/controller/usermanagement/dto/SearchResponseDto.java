434e8aef8a315bd26b64bcd841e75d57897aad71
Steffen Pade <steffen.pade@camunda.com>
2024-06-11 08:42:23 +0200

    fix: using openapi-defined types for unified controllers;

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/usermanagement/dto/SearchResponseDto.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/usermanagement/dto/SearchResponseDto.java
@@ -5,6 +5,19 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.gateway.rest.dto.search;
+package io.camunda.zeebe.gateway.rest.controller.usermanagement.dto;
 
-public class PageDto {}
+import java.util.List;
+
+public class SearchResponseDto<T> {
+  private List<T> items;
+  private PageResponseDto page;
+
+  public List<T> getItems() {
+    return items;
+  }
+
+  public void setItems(final List<T> items) {
+    this.items = items;
+  }
+}
