08ec7afa417e3be7435a505b9860fb3c15dacf5e
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2024-06-11 14:03:07 +0200

    feat: add endpoints for enabling an exporter

--- dist/src/main/java/io/camunda/zeebe/shared/management/ExportersEndpoint.java
+++ dist/src/main/java/io/camunda/zeebe/shared/management/ExportersEndpoint.java
@@ -8,7 +8,9 @@
 package io.camunda.zeebe.shared.management;
 
 import io.camunda.zeebe.dynamic.config.api.ClusterConfigurationManagementRequest.ExporterDisableRequest;
+import io.camunda.zeebe.dynamic.config.api.ClusterConfigurationManagementRequest.ExporterEnableRequest;
 import io.camunda.zeebe.dynamic.config.api.ClusterConfigurationManagementRequestSender;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
@@ -16,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Component
@@ -38,4 +41,19 @@ public class ExportersEndpoint {
         .disableExporter(new ExporterDisableRequest(exporterId, dryRun))
         .handle(ClusterApiUtils::mapOperationResponse);
   }
+
+  @PostMapping(path = "/{exporterId}/enable")
+  public CompletableFuture<ResponseEntity<?>> enableExporter(
+      @PathVariable("exporterId") final String exporterId,
+      @RequestBody(required = false) final InitializationInfo initializeInfo,
+      @RequestParam(defaultValue = "false") final boolean dryRun) {
+
+    final var initializeFrom = initializeInfo != null ? initializeInfo.initializeFrom() : null;
+    return requestSender
+        .enableExporter(
+            new ExporterEnableRequest(exporterId, Optional.ofNullable(initializeFrom), dryRun))
+        .handle(ClusterApiUtils::mapOperationResponse);
+  }
+
+  private record InitializationInfo(String initializeFrom) {}
 }
