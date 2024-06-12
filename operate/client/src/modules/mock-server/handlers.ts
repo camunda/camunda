cbaf06026befe12e8019f13f579bd260b7903c13
Patrick Dehn <patrick.dehn@camunda.com>
2024-06-12 14:04:59 +0200

    Revert "refactor: add feature flag, mock response for process instance request"
    This reverts commit 0bd91d80cd19f1e11e0130391d4261e3af5190ca.

--- operate/client/src/modules/mock-server/handlers.ts
+++ operate/client/src/modules/mock-server/handlers.ts
@@ -6,44 +6,8 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {IS_LATEST_MIGRATION_DATE_ENABLED} from 'modules/feature-flags';
-import {RequestHandler, rest} from 'msw';
+import {RequestHandler} from 'msw';
 
-const processInstanceHandler = rest.get(
-  '/api/process-instances/:id',
-  async (req, res, ctx) => {
-    const id = req.params.id as string;
-
-    if (id.match(/^[0-9]{16,19}$/)) {
-      const response = await ctx.fetch(req);
-      const instance: ProcessInstanceEntity = await response.json();
-
-      const {operations} = instance;
-
-      const instanceWithCompletedDate: ProcessInstanceEntity = {
-        ...instance,
-        operations: [
-          ...operations,
-          {
-            id: '07b22091-bd87-47dd-81c1-bd09ab81e141',
-            batchOperationId: 'a7928686-7e34-4d23-89e0-6497f783fe18',
-            type: 'MIGRATE_PROCESS_INSTANCE',
-            state: 'COMPLETED',
-            errorMessage: null,
-            completedDate: '2024-06-10T09:14:02.660+0000',
-          },
-        ],
-      };
-
-      return res(ctx.json(instanceWithCompletedDate));
-    }
-
-    req.passthrough();
-  },
-);
-
-const handlers: RequestHandler[] = IS_LATEST_MIGRATION_DATE_ENABLED
-  ? [processInstanceHandler]
-  : [];
+const handlers: RequestHandler[] = [];
 
 export {handlers};
