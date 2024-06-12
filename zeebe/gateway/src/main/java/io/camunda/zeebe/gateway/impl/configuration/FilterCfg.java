55eef7c3ceab3c994d724e796dea05a568bbb150
Nikola Koevski <nikola.koevski@camunda.com>
2024-06-12 15:12:18 +0200

    feat: add a new filter config class for the zeebe gateway
    Adds a new FilterCfg class for the Zeebe Gateway. The
    class will be used to provide the configuration for
    loading a single custom implementation of a Jakarta
    Servlet Filter. These filters will be used as the
    custom middleware for the Zeebe REST API.
    
    The List of FilterCfg instances is contained within
    the GatewayCfg class, which will provide these config
    properties to the Zeebe REST API for instantiation.
    
    The FilterCfg class is a copy of the InterceptorCfg
    class since it provides the same functionality but
    for Filters instead of (gRPC) Interceptors. Re-using
    the same code was considered to avoid duplication,
    but this might hurt us in the future if we decide to
    expand the Filter configuration properties and not
    the Interceptor properties.

--- zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/configuration/FilterCfg.java
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/configuration/FilterCfg.java
@@ -8,25 +8,17 @@
 package io.camunda.zeebe.gateway.impl.configuration;
 
 /**
- * Configuration to load a single extra interceptor. The {@link #className} property is required,
- * and must be a fully qualified name referring to an implementation of {@link
- * io.grpc.ServerInterceptor}.
+ * Configuration to load a single extra filter. The {@link #className} property is required, and
+ * must be a fully qualified name referring to an implementation of {@link jakarta.servlet.Filter}.
  *
  * <p>Optionally, a {@link #jarPath} can be supplied, and the class will be looked up there. Note
  * that the JAR is loaded within an isolated class loader to avoid dependency conflicts, so you must
  * make sure that all dependencies are available in the JAR, or via the gateway itself.
  */
-public final class InterceptorCfg extends BaseExternalCodeCfg {
+public final class FilterCfg extends BaseExternalCodeCfg {
 
   @Override
   public String toString() {
-    return "InterceptorCfg{"
-        + ", jarPath='"
-        + jarPath
-        + '\''
-        + ", className='"
-        + className
-        + '\''
-        + '}';
+    return "FilterCfg{" + ", jarPath='" + jarPath + '\'' + ", className='" + className + '\'' + '}';
   }
 }
