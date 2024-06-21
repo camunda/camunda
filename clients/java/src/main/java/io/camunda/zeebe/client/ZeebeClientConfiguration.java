8a760836c30f4a1175b15c5fb9b51552c42ef8e8
Nicola Puppa <nicola.puppa@camunda.com>
2024-06-21 13:46:01 +0200

    refactor: deprecate zeebe client configuration class

--- clients/java/src/main/java/io/camunda/zeebe/client/ZeebeClientConfiguration.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/ZeebeClientConfiguration.java
@@ -23,38 +23,39 @@ import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
+@Deprecated
 public interface ZeebeClientConfiguration {
 
   /**
    * @deprecated since 8.5 for removal with 8.8, replaced by {@link
-   *     ZeebeClientConfiguration#getGrpcAddress()}
-   * @see ZeebeClientBuilder#grpcAddress(URI)
+   *     CamundaClientConfiguration#getGrpcAddress()}
+   * @see CamundaClientBuilder#grpcAddress(URI)
    */
   @Deprecated
   String getGatewayAddress();
 
   /**
-   * @see ZeebeClientBuilder#restAddress(URI)
+   * @see CamundaClientBuilder#restAddress(URI)
    */
   URI getRestAddress();
 
   /**
-   * @see ZeebeClientBuilder#grpcAddress(URI)
+   * @see CamundaClientBuilder#grpcAddress(URI)
    */
   URI getGrpcAddress();
 
   /**
-   * @see ZeebeClientBuilder#defaultTenantId(String)
+   * @see CamundaClientBuilder#defaultTenantId(String)
    */
   String getDefaultTenantId();
 
   /**
-   * @see ZeebeClientBuilder#defaultJobWorkerTenantIds(List)
+   * @see CamundaClientBuilder#defaultJobWorkerTenantIds(List)
    */
   List<String> getDefaultJobWorkerTenantIds();
 
   /**
-   * @see ZeebeClientBuilder#numJobWorkerExecutionThreads(int)
+   * @see CamundaClientBuilder#numJobWorkerExecutionThreads(int)
    */
   int getNumJobWorkerExecutionThreads();
 
