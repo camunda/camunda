f207eacde1fb38be9816ccdb3cd911ccf49e4c20
Steffen Pade <steffen.pade@camunda.com>
2024-06-24 09:04:51 +0200

    fix: exclude jpa autoconfiguration from ErrorMapperTest

--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/ErrorMapperTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/ErrorMapperTest.java
@@ -34,6 +34,7 @@ import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -261,7 +262,8 @@ public class ErrorMapperTest {
     Mockito.verifyNoInteractions(brokerClient);
   }
 
-  @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+  @SpringBootApplication(
+      exclude = {SecurityAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
   static class TestErrorMapperApplication {
     // required to provide the web server context
   }
