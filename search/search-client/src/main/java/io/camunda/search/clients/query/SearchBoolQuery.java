c7c435e0ce5930e99d43cd4acccf9e757b0816aa
Christopher Zell <zelldon91@googlemail.com>
2024-06-25 15:48:29 +0200

    refactor: simplify SearchBoolQuery
    Remove indirection and use factory in SearchQueryBuilders
    Remove unused methods

--- search/search-client/src/main/java/io/camunda/search/clients/query/SearchBoolQuery.java
+++ search/search-client/src/main/java/io/camunda/search/clients/query/SearchBoolQuery.java
@@ -14,18 +14,13 @@ import io.camunda.util.ObjectBuilder;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Function;
 
-public final record SearchBoolQuery(
+public record SearchBoolQuery(
     List<SearchQuery> filter,
     List<SearchQuery> must,
     List<SearchQuery> mustNot,
     List<SearchQuery> should)
     implements SearchQueryOption {
-
-  static SearchBoolQuery of(final Function<Builder, ObjectBuilder<SearchBoolQuery>> fn) {
-    return SearchQueryBuilders.bool(fn);
-  }
 
   public static final class Builder implements ObjectBuilder<SearchBoolQuery> {
 
@@ -43,10 +38,6 @@ public final record SearchBoolQuery(
       return filter(collectValues(query, queries));
     }
 
-    public Builder filter(final Function<SearchQuery.Builder, ObjectBuilder<SearchQuery>> fn) {
-      return filter(SearchQueryBuilders.query(fn));
-    }
-
     public Builder must(final List<SearchQuery> queries) {
       must = addValuesToList(must, queries);
       return this;
@@ -56,21 +47,9 @@ public final record SearchBoolQuery(
       return must(collectValues(query, queries));
     }
 
-    public Builder must(final Function<SearchQuery.Builder, ObjectBuilder<SearchQuery>> fn) {
-      return must(SearchQueryBuilders.query(fn));
-    }
-
     public Builder mustNot(final List<SearchQuery> queries) {
       mustNot = addValuesToList(mustNot, queries);
       return this;
-    }
-
-    public Builder mustNot(final SearchQuery query, final SearchQuery... queries) {
-      return mustNot(collectValues(query, queries));
-    }
-
-    public Builder mustNot(final Function<SearchQuery.Builder, ObjectBuilder<SearchQuery>> fn) {
-      return mustNot(SearchQueryBuilders.query(fn));
     }
 
     public Builder should(final List<SearchQuery> queries) {
@@ -80,10 +59,6 @@ public final record SearchBoolQuery(
 
     public Builder should(final SearchQuery query, final SearchQuery... queries) {
       return should(collectValues(query, queries));
-    }
-
-    public Builder should(final Function<SearchQuery.Builder, ObjectBuilder<SearchQuery>> fn) {
-      return should(SearchQueryBuilders.query(fn));
     }
 
     @Override
