2298bf8bdfafe5518c3450baea58b6c510474787
Steffen Pade <steffen.pade@camunda.com>
2024-06-27 13:19:55 +0200

    fix: unused imports

--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/UserControllerTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/UserControllerTest.java
@@ -22,13 +22,11 @@ import io.camunda.zeebe.gateway.rest.controller.usermanagement.UserController;
 import java.net.URI;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
-import org.springframework.test.web.reactive.server.WebTestClient;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest extends RestControllerTest {
