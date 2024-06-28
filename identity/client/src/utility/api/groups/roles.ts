c47a4ef5dbc319e02715939ceb42967971e1d5ac
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2024-06-28 17:52:57 +0200

    fix: apply review comments

--- identity/client/src/utility/api/groups/roles.ts
+++ identity/client/src/utility/api/groups/roles.ts
@@ -14,6 +14,10 @@ import {
 } from "src/utility/api/request.ts";
 import { Role } from "src/utility/api/roles";
 import { GROUPS_ENDPOINT } from "src/utility/api/groups/index.ts";
+import {
+  AssignRoleParams,
+  UnassignRoleParams,
+} from "src/utility/api/roles/assign";
 
 export type GetGroupRolesParams = { id: string };
 
@@ -23,20 +27,12 @@ export const getGroupRoles: ApiDefinition<Role[], GetGroupRolesParams> = ({
   id,
 }) => apiGet(path(id, "roles"));
 
-export type AssignGroupRoleParams = GetGroupRolesParams & {
-  roleId: Role["id"];
-};
+export const assignGroupRole: ApiDefinition<undefined, AssignRoleParams> = ({
+  id,
+  roleId,
+}) => apiPost(path(id, "roles"), { roleId: roleId });
 
-export const assignGroupRole: ApiDefinition<
-  undefined,
-  AssignGroupRoleParams
-> = ({ id, roleId }) => apiPost(path(id, "roles"), { roleId: roleId });
-
-export type RemoveGroupRoleParams = GetGroupRolesParams & {
-  roleId: Role["id"];
-};
-
-export const removeGroupRole: ApiDefinition<
-  undefined,
-  RemoveGroupRoleParams
-> = ({ id, roleId }) => apiDelete(path(id, "roles", roleId));
+export const removeGroupRole: ApiDefinition<undefined, UnassignRoleParams> = ({
+  id,
+  roleId,
+}) => apiDelete(path(id, "roles", roleId));
