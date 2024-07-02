f5de554437b344b6e6601c2346c1f4dc3e82144f
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-07-02 15:35:11 +0200

    refactor: move standalone camunda to zeebe root directory

--- zeebe/qa/util/src/main/java/io/camunda/zeebe/qa/util/cluster/TestCamundaApplication.java
+++ zeebe/qa/util/src/main/java/io/camunda/zeebe/qa/util/cluster/TestCamundaApplication.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.qa.util.cluster;
+package io.camunda.zeebe.qa.util.cluster;
 
 import io.camunda.commons.CommonsModuleConfiguration;
 import io.camunda.zeebe.qa.util.cluster.TestSpringApplication;
