13fa6094269595cf20fd2e7e8a9d56fbb94078a8
Euro Lew <euro.lew@camunda.com>
2024-07-04 14:10:35 +0100

    test: fixed changed classes to align with 8.2

--- snapshot/src/test/java/io/camunda/zeebe/snapshots/impl/FileBasedSnapshotStoreTest.java
+++ snapshot/src/test/java/io/camunda/zeebe/snapshots/impl/FileBasedSnapshotStoreTest.java
@@ -19,10 +19,14 @@ import io.camunda.zeebe.util.FileUtil;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.CRC32C;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,6 +37,8 @@ public class FileBasedSnapshotStoreTest {
   private static final String PENDING_DIRECTORY = "pending";
 
   private static final String SNAPSHOT_CONTENT_FILE_NAME = "file1.txt";
+  private static final String SNAPSHOT_CONTENT = "this is the content";
+  private static final Integer PARTITION_ID = 1;
 
   @Rule public TemporaryFolder temporaryFolder = new TemporaryFolder();
   @Rule public ActorSchedulerRule scheduler = new ActorSchedulerRule();
@@ -364,12 +370,52 @@ public class FileBasedSnapshotStoreTest {
     assertThat(newSnapshot.getPath()).exists();
   }
 
+  @Test
+  public void shouldRestartWithAReceivedSnapshot() throws IOException {
+    // given
+    final Map<String, Long> fileChecksums = new HashMap<>();
+    final var fileContentChecksum = new CRC32C();
+    fileContentChecksum.update(SNAPSHOT_CONTENT.getBytes(StandardCharsets.UTF_8));
+    fileChecksums.put(SNAPSHOT_CONTENT_FILE_NAME, fileContentChecksum.getValue());
+    final var receiverStorePath = temporaryFolder.newFolder("receiver").toPath();
+
+    final var store =
+        new FileBasedSnapshotStore(
+            1, new SnapshotMetrics(1 + "-" + 1), snapshotsDir, pendingSnapshotsDir);
+    scheduler.submitActor(store);
+
+    // when
+    final var persistedSnapshot = takeTransientSnapshot().persist().join();
+    final var receivedSnapshot = store.newReceivedSnapshot(persistedSnapshot.getId());
+    try (final var reader = persistedSnapshot.newChunkReader()) {
+      while (reader.hasNext()) {
+        receivedSnapshot.apply(reader.next()).join();
+      }
+    }
+
+    receivedSnapshot.persist().join();
+
+    // restart store will attempt to update the latest snapshot to the most recent one and check for
+    // corruption.
+    store.close();
+
+    final var restartedStore =
+        new FileBasedSnapshotStore(
+            1, new SnapshotMetrics(1 + "-" + 1), snapshotsDir, pendingSnapshotsDir);
+    scheduler.submitActor(restartedStore).join();
+
+    assertThat(restartedStore.getLatestSnapshot())
+        .describedAs(
+            "The latest snapshot is not detected as corrupted and should be loaded after restart")
+        .hasValueSatisfying(s -> assertThat(s.getId()).isEqualTo(persistedSnapshot.getId()));
+  }
+
   private boolean createSnapshotDir(final Path path) {
     try {
       FileUtil.ensureDirectoryExists(path);
       Files.write(
           path.resolve(SNAPSHOT_CONTENT_FILE_NAME),
-          "This is the content".getBytes(),
+          SNAPSHOT_CONTENT.getBytes(),
           CREATE_NEW,
           StandardOpenOption.WRITE);
     } catch (final IOException e) {
