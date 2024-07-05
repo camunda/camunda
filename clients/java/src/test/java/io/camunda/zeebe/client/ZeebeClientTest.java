e073344a618729d177e6e848048ea205c77a5ebe
Nicola Puppa <nicola.puppa@camunda.com>
2024-07-05 08:33:27 +0200

    refactor: copied tests into new package

--- clients/java/src/test/java/io/camunda/zeebe/client/ZeebeClientTest.java
+++ clients/java/src/test/java/io/camunda/zeebe/client/ZeebeClientTest.java
@@ -70,7 +70,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public final class CamundaClientTest extends ClientTest {
+public final class ZeebeClientTest extends ClientTest {
   @Rule public final EnvironmentRule environmentRule = new EnvironmentRule();
   @Rule public ExpectedException thrown = ExpectedException.none();
 
