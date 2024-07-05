65d016197129d5c0717516399e49571a13203d17
Jesse Simpson <jesse.simpson@camunda.com>
2024-07-05 11:26:17 -0400

    test: login sanity check for operate and tasklist

---
+++ c8run/e2e_tests/tests/operate_login.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+const playwright = require('playwright');
+
+test('test', async ({ page }) => {
+  test.setTimeout(60000);
+  await page.goto('http://localhost:8080/operate/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('demo');
+  await page.getByPlaceholder('Username').press('Tab');
+  await page.getByPlaceholder('Password').fill('demo');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('link', { name: 'Processes' }).click();
+});
---
+++ c8run/e2e_tests/tests/tasklist_login.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+const playwright = require('playwright');
+
+test('test', async ({ page }) => {
+  test.setTimeout(60000);
+  await page.goto('http://localhost:8080/tasklist/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('demo');
+  await page.getByPlaceholder('Username').press('Tab');
+  await page.getByPlaceholder('Password').fill('demo');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByLabel('Expand to show filters', { exact: true }).click();
+  await page.getByRole('link', { name: 'Assigned to me' }).click();
+});
