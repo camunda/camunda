e1ec5ec967487c14faaae487cef02a25c4e4053c
PHWaechtler <helene.waechtler@camunda.com>
2024-07-22 16:20:52 +0200

    Remove c7 workflows and adjust master to main

--- .github/scripts/get-engine-snapshot-version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ARTIFACTORY_METADATA_XML_URL='https://artifacts.camunda.com/artifactory/camunda-bpm-snapshots/org/camunda/bpm/camunda-engine/maven-metadata.xml'
-
-XML_CONTENT=$(curl -s "$ARTIFACTORY_METADATA_XML_URL")
-ENGINE_SNAPSHOT_VERSION=$(echo "$XML_CONTENT" | xmllint --xpath "string(/metadata/versioning/latest)" -)
-
-echo "$ENGINE_SNAPSHOT_VERSION"
