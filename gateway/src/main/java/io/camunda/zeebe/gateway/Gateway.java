36d5598a5f7809fe425bb5ee5a56980be2632474
Euro Lew <euro.lew@camunda.com>
2024-07-23 17:01:45 +0100

    style: removed unneeded import

--- gateway/src/main/java/io/camunda/zeebe/gateway/Gateway.java
+++ gateway/src/main/java/io/camunda/zeebe/gateway/Gateway.java
@@ -2,14 +2,13 @@
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
  * one or more contributor license agreements. See the NOTICE file distributed
  * with this work for additional information regarding copyright ownership.
- * Licensed under the Zeebe Community License 1.1. You may not use this file
- * except in compliance with the Zeebe Community License 1.1.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
 package io.camunda.zeebe.gateway;
 
 import static java.util.concurrent.Executors.newThreadPerTaskExecutor;
 
-import com.google.rpc.Code;
 import io.camunda.identity.sdk.IdentityConfiguration;
 import io.camunda.zeebe.gateway.health.GatewayHealthManager;
 import io.camunda.zeebe.gateway.health.Status;
