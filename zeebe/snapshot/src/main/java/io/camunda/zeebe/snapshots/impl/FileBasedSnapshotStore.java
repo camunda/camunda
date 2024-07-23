a555206015c2ae0f8f324fbba0937b413972c5fe
Euro Lew <euro.lew@camunda.com>
2024-07-23 09:41:16 +0100

    feat: checksum provider cannot be null.
    (cherry picked from commit f370177096ff5d06e6d6a74ff22a95a8d2e23a88)

--- zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/impl/FileBasedSnapshotStore.java
+++ zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/impl/FileBasedSnapshotStore.java
@@ -37,6 +37,7 @@ import java.util.Collections;
 import java.util.ConcurrentModificationException;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -104,7 +105,7 @@ public final class FileBasedSnapshotStore extends Actor
     listeners = new CopyOnWriteArraySet<>();
     actorName = buildActorName("SnapshotStore", partitionId);
     this.partitionId = partitionId;
-    this.checksumProvider = checksumProvider;
+    this.checksumProvider = Objects.requireNonNull(checksumProvider);
   }
 
   @Override
