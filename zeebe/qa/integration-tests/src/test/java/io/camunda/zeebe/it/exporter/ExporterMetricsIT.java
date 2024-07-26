5eeb6f559d32a1c40e11154b5a0bc8b3027752f5
Euro Lew <euro.lew@camunda.com>
2024-07-26 04:37:47 +0100

    test: added integration test for exporter context

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/exporter/ExporterMetricsIT.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/exporter/ExporterMetricsIT.java
@@ -7,13 +7,19 @@
  */
 package io.camunda.zeebe.it.exporter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.camunda.zeebe.qa.util.actuator.PrometheusActuator;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
 import io.prometheus.client.CollectorRegistry;
-import io.prometheus.metrics.exporter.common.PrometheusHttpResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import org.hawkular.agent.prometheus.text.TextPrometheusMetricsProcessor;
+import org.hawkular.agent.prometheus.types.MetricFamily;
+import org.hawkular.agent.prometheus.walkers.CollectorPrometheusMetricsWalker;
 import org.junit.jupiter.api.Test;
 
 @ZeebeIntegration
@@ -28,7 +34,25 @@ public class ExporterMetricsIT {
 
   @Test
   public void shouldAddMeterToExporterMetrics() throws IOException {
+    // given
     final var actuator = PrometheusActuator.of(zeebe);
-    final PrometheusHttpResponse response = actuator.metrics();
+
+    // when
+    final var metrics = collectMetrics(actuator.metrics().body().asInputStream());
+    final var addedCounter =
+        metrics.stream()
+            .filter(
+                metric -> metric.getName().equals(ExporterMetricsTestExporter.REGISTERED_COUNTER))
+            .findFirst();
+
+    // then
+    assertThat(addedCounter).isPresent();
+  }
+
+  private List<MetricFamily> collectMetrics(final InputStream metricsPlainText) {
+    final var walker = new CollectorPrometheusMetricsWalker();
+    final var processor = new TextPrometheusMetricsProcessor(metricsPlainText, walker);
+    processor.walk();
+    return walker.getAllMetricFamilies();
   }
 }
