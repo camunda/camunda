381e587ea783d5ff8c5aa680889b8d951d1426f8
Roman <roman.smirnov@camunda.com>
2024-07-29 17:10:59 +0200

    fix: resolve compile issues by using the correct package name
    (cherry picked from commit 667391bf0300066d93d1b5ec6d6c322d94bfc72f)

--- dist/src/test/java/io/camunda/zeebe/broker/StandaloneBrokerRestDisabledTest.java
+++ dist/src/test/java/io/camunda/zeebe/broker/StandaloneBrokerRestDisabledTest.java
@@ -9,7 +9,7 @@ package io.camunda.zeebe.broker;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.camunda.commons.CommonsModuleConfiguration;
+import io.camunda.application.commons.CommonsModuleConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
