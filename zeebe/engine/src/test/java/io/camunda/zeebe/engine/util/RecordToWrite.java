88cbcbe08ad51321b44f002e796899f488391cd1
Lena Schönburg <lena@fairburg.xyz>
2024-07-29 17:00:21 +0200

    fix: allow engine to process scale records
    Co-authored-by: Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/util/RecordToWrite.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/util/RecordToWrite.java
@@ -18,6 +18,7 @@ import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstan
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceMigrationRecord;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceModificationRecord;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
+import io.camunda.zeebe.protocol.impl.record.value.scale.ScaleRecord;
 import io.camunda.zeebe.protocol.impl.record.value.timer.TimerRecord;
 import io.camunda.zeebe.protocol.impl.record.value.variable.VariableDocumentRecord;
 import io.camunda.zeebe.protocol.record.RecordType;
@@ -30,6 +31,7 @@ import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceMigrationIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceModificationIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessMessageSubscriptionIntent;
+import io.camunda.zeebe.protocol.record.intent.ScaleIntent;
 import io.camunda.zeebe.protocol.record.intent.TimerIntent;
 import io.camunda.zeebe.protocol.record.intent.VariableDocumentIntent;
 import io.camunda.zeebe.protocol.record.value.JobRecordValue;
@@ -153,6 +155,12 @@ public final class RecordToWrite implements LogAppendEntry {
         .valueType(ValueType.PROCESS_INSTANCE_MIGRATION)
         .intent(ProcessInstanceMigrationIntent.MIGRATE);
     unifiedRecordValue = (ProcessInstanceMigrationRecord) value;
+    return this;
+  }
+
+  public RecordToWrite scale(final ScaleIntent intent, final ScaleRecord value) {
+    recordMetadata.valueType(ValueType.SCALE).intent(intent);
+    unifiedRecordValue = value;
     return this;
   }
 
