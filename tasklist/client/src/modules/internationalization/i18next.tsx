3a8128e11cac004a9823e5b3ab92458b714667c4
Valentin Serra <valentin.serra@camunda.com>
2024-07-29 11:01:21 +0200

    feat: tasklist i18n dropdown and configuration
    Related to #20287

---
+++ tasklist/client/src/modules/internationalization/i18next.tsx
@@ -0,0 +1,39 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import {initReactI18next} from 'react-i18next';
+import {IS_INTERNATIONALIZATION_ENABLED} from 'modules/featureFlags';
+import {translationResources} from '.';
+
+const detection = {
+  order: ['localStorage', 'navigator'],
+  lookupLocalStorage: 'language',
+  caches: ['localStorage'],
+  htmlTag: document.documentElement,
+  checkWhitelist: true,
+};
+
+function initI18next() {
+  if (IS_INTERNATIONALIZATION_ENABLED) {
+    i18n
+      .use(LanguageDetector)
+      .use(initReactI18next)
+      .init({
+        detection,
+        fallbackLng: 'en',
+        resources: translationResources,
+        interpolation: {
+          escapeValue: false,
+        },
+      });
+  } else {
+    i18n.use(initReactI18next).init({
+      lng: 'en',
+      resources: translationResources,
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+  }
+}
+
+initI18next();
