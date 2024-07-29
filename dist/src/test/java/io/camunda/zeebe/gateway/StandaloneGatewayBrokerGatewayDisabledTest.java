7f8b206538e031d97b628963b85b1f4981407b8c
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2024-07-29 15:49:09 +0200

    test: verify REST API configuration properties
    Ensure the configuration properties relevant to the REST API enable and disable the
    REST API and the REST Query API as expected.
    
    !!THIS IS WORK IN PROGRESS!!
    * The tests only succeed when run manually, one after the other. Running them in a test suite
      collides in the Spring context, trying to register Micrometer metrics multiple times.

---
+++ dist/src/test/java/io/camunda/zeebe/gateway/StandaloneGatewayBrokerGatewayDisabledTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.commons.CommonsModuleConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(
+    classes = {CommonsModuleConfiguration.class, GatewayModuleConfiguration.class},
+    webEnvironment = WebEnvironment.RANDOM_PORT,
+    properties = {"zeebe.broker.gateway.enable=false"})
+@ActiveProfiles(profiles = {"gateway", "standalone"})
+@DirtiesContext
+class StandaloneGatewayBrokerGatewayDisabledTest {
+
+  @LocalServerPort private int port;
+
+  @Autowired private TestRestTemplate restTemplate;
+
+  @Test
+  void shouldStartWithRestDisabled() {
+    // when/then
+    assertThat(restTemplate.getForObject("http://localhost:" + port + "/v2/topology", String.class))
+        .contains("clusterSize");
+  }
+
+  @Test
+  void shouldStartWithRestQueryDisabled() {
+    // when/then
+    assertThat(
+            restTemplate.postForObject(
+                "http://localhost:" + port + "/v2/process-instances/search", "{}", String.class))
+        .contains("404");
+  }
+}
