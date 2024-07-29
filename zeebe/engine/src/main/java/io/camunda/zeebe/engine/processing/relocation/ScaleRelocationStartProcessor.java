0e474744288fa35d96ca632befb43c11a6c9f8e0
Lena Schönburg <lena@fairburg.xyz>
2024-07-29 15:52:32 +0200

    feat: distribute command to start new relocation
    Co-authored-by: Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/relocation/ScaleRelocationStartProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/relocation/ScaleRelocationStartProcessor.java
@@ -7,6 +7,7 @@
  */
 package io.camunda.zeebe.engine.processing.relocation;
 
+import io.camunda.zeebe.engine.processing.distribution.CommandDistributionBehavior;
 import io.camunda.zeebe.engine.processing.streamprocessor.DistributedTypedRecordProcessor;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
@@ -14,20 +15,31 @@ import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.protocol.impl.record.value.scale.ScaleRecord;
 import io.camunda.zeebe.protocol.record.intent.ScaleIntent;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
+import io.camunda.zeebe.stream.api.state.KeyGenerator;
 
 public class ScaleRelocationStartProcessor implements DistributedTypedRecordProcessor<ScaleRecord> {
 
+  private final KeyGenerator keyGenerator;
   private final TypedCommandWriter commandWriter;
   private final StateWriter stateWriter;
+  private final CommandDistributionBehavior commandDistributionBehavior;
 
-  public ScaleRelocationStartProcessor(final Writers writers) {
+  public ScaleRelocationStartProcessor(
+      final KeyGenerator keyGenerator,
+      final Writers writers,
+      final CommandDistributionBehavior commandDistributionBehavior) {
+    this.keyGenerator = keyGenerator;
     commandWriter = writers.command();
     stateWriter = writers.state();
+    this.commandDistributionBehavior = commandDistributionBehavior;
   }
 
   @Override
   public void processNewCommand(final TypedRecord<ScaleRecord> command) {
-    // TODO: Distribute to other partitions
+    final var distributionKey = keyGenerator.nextKey();
+    // TODO: Use distribution key locally too, as unique key to identify this relocation?
+    processDistributedCommand(command);
+    commandDistributionBehavior.distributeCommand(distributionKey, command);
   }
 
   @Override
