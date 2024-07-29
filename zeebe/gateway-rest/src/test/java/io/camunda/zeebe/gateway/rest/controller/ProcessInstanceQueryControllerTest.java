4d88b3b879d64292d523e16995eb0cb7642d3552
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2024-07-29 20:18:27 +0200

    test: enable REST query API for respective tests
    Enables the REST Query API for ProcessInstanceQueryControllerTests by setting the respective
    property to true.
    
    Removes an unused test dependency from the dist module.
    
    (cherry picked from commit 9997a2242b35a570520e7a8aa617a3a94ad4ddc6)

--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/ProcessInstanceQueryControllerTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/ProcessInstanceQueryControllerTest.java
@@ -26,7 +26,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
-@WebMvcTest(ProcessInstanceQueryController.class)
+@WebMvcTest(
+    value = ProcessInstanceQueryController.class,
+    properties = "camunda.rest.query.enabled=true")
 public class ProcessInstanceQueryControllerTest extends RestControllerTest {
 
   static final String EXPECTED_SEARCH_RESPONSE =
