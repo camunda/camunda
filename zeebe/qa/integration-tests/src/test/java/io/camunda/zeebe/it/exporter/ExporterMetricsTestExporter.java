dcbdf03655d02918ce425cabe752468e6f1c15f5
Euro Lew <euro.lew@camunda.com>
2024-07-31 09:29:34 +0100

    style: cleaned up unused methods.

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/exporter/ExporterMetricsTestExporter.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/exporter/ExporterMetricsTestExporter.java
@@ -9,7 +9,6 @@ package io.camunda.zeebe.it.exporter;
 
 import io.camunda.zeebe.exporter.api.Exporter;
 import io.camunda.zeebe.exporter.api.context.Context;
-import io.camunda.zeebe.exporter.api.context.Controller;
 import io.camunda.zeebe.protocol.record.Record;
 import io.micrometer.core.instrument.Counter;
 
@@ -23,16 +22,6 @@ public class ExporterMetricsTestExporter implements Exporter {
   public void configure(final Context context) throws Exception {
     Exporter.super.configure(context);
     Counter.builder(REGISTERED_COUNTER).register(context.getMeterRegistry());
-  }
-
-  @Override
-  public void open(final Controller controller) {
-    Exporter.super.open(controller);
-  }
-
-  @Override
-  public void close() {
-    Exporter.super.close();
   }
 
   @Override
