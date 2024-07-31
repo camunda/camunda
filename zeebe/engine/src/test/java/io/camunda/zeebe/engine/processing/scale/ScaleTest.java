21d09a520acec88f66937a29bc1120df5412c299
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2024-07-31 11:23:51 +0200

    test: verify moving messages
    Co-authored-by: Lena Schönburg <lena@fairburg.xyz>

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/scale/ScaleTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/scale/ScaleTest.java
@@ -8,8 +8,8 @@
 package io.camunda.zeebe.engine.processing.scale;
 
 import static io.camunda.zeebe.protocol.Protocol.START_PARTITION_ID;
+import static io.camunda.zeebe.test.util.MsgPackUtil.asMsgPack;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 import static org.assertj.core.data.MapEntry.entry;
 
 import io.camunda.zeebe.engine.util.EngineRule;
@@ -20,8 +20,11 @@ import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
 import io.camunda.zeebe.protocol.impl.record.value.scale.ScaleRecord;
 import io.camunda.zeebe.protocol.record.intent.ScaleIntent;
 import io.camunda.zeebe.test.util.collection.Maps;
+import io.camunda.zeebe.test.util.record.ProcessInstances;
 import io.camunda.zeebe.test.util.record.RecordingExporter;
 import io.camunda.zeebe.test.util.record.RecordingExporterTestWatcher;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import org.junit.Rule;
 import org.junit.Test;
@@ -72,15 +75,18 @@ public class ScaleTest {
     // create instance with intermediate message catch event
     final ProcessInstanceCreationClient processInstanceCreationClient =
         engine.processInstance().ofBpmnProcessId(PROCESS_ID);
-    processInstanceCreationClient
-        .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID))
-        .create();
-    processInstanceCreationClient
-        .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID + 1))
-        .create();
-    processInstanceCreationClient
-        .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID + 2))
-        .create();
+    final var processInstanceKey1 =
+        processInstanceCreationClient
+            .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID))
+            .create();
+    final var processInstanceKey2 =
+        processInstanceCreationClient
+            .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID + 1))
+            .create();
+    final var processInstanceKey3 =
+        processInstanceCreationClient
+            .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID + 2))
+            .create();
 
     // scale
     final var scaleRecord = new ScaleRecord();
@@ -98,6 +104,83 @@ public class ScaleTest {
         .describedAs("Expect relocation to be completed")
         .isNotEmpty();
 
-    fail();
+    // when
+    // publish messages
+
+    engine.forEachPartition(
+        partitionId ->
+            engine
+                .message()
+                .onPartition(partitionId)
+                .withName("message")
+                .withCorrelationKey(CORRELATION_KEYS.get(partitionId))
+                .withVariables(asMsgPack("p", "p" + partitionId))
+                .publish());
+
+    // then
+    final List<String> correlatedValues =
+        Arrays.asList(
+            ProcessInstances.getCurrentVariables(processInstanceKey1).get("p"),
+            ProcessInstances.getCurrentVariables(processInstanceKey2).get("p"),
+            ProcessInstances.getCurrentVariables(processInstanceKey3).get("p"));
+
+    assertThat(correlatedValues).contains("\"p1\"", "\"p2\"", "\"p3\"");
+  }
+
+  @Test
+  public void shouldMoveMessages() {
+    // given
+    engine.forEachPartition(
+        partitionId ->
+            engine
+                .message()
+                .onPartition(1)
+                .withName("message")
+                .withCorrelationKey(CORRELATION_KEYS.get(partitionId))
+                .withVariables(asMsgPack("p", "p" + partitionId))
+                .publish());
+
+    // scale
+    final var scaleRecord = new ScaleRecord();
+    scaleRecord.setRoutingInfo(1, 3);
+
+    // when
+    engine.writeRecords(
+        RecordToWrite.command().key(-1).scale(ScaleIntent.RELOCATION_START, scaleRecord));
+
+    // then
+    assertThat(
+            RecordingExporter.records()
+                .filter(r -> r.getIntent().equals(ScaleIntent.RELOCATION_COMPLETED))
+                .limit(3))
+        .describedAs("Expect relocation to be completed")
+        .isNotEmpty();
+
+    // when
+    engine.deployment().withXmlResource(PROCESS).deploy();
+    // create instance with intermediate message catch event
+    final ProcessInstanceCreationClient processInstanceCreationClient =
+        engine.processInstance().ofBpmnProcessId(PROCESS_ID);
+    final var processInstanceKey1 =
+        processInstanceCreationClient
+            .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID))
+            .create();
+    final var processInstanceKey2 =
+        processInstanceCreationClient
+            .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID + 1))
+            .create();
+    final var processInstanceKey3 =
+        processInstanceCreationClient
+            .withVariable("key", CORRELATION_KEYS.get(START_PARTITION_ID + 2))
+            .create();
+
+    // then
+    final List<String> correlatedValues =
+        Arrays.asList(
+            ProcessInstances.getCurrentVariables(processInstanceKey1).get("p"),
+            ProcessInstances.getCurrentVariables(processInstanceKey2).get("p"),
+            ProcessInstances.getCurrentVariables(processInstanceKey3).get("p"));
+
+    assertThat(correlatedValues).contains("\"p1\"", "\"p2\"", "\"p3\"");
   }
 }
