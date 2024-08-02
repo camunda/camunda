948fc7feb1ccbc56b799b809210d1aee70887c4c
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2024-08-02 15:44:15 +0200

    feat: add tests for state and processor

---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/CreateAuthorizationTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.authorization;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.engine.util.EngineRule;
+import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationOwnerType;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.test.util.record.RecordingExporterTestWatcher;
+import java.util.List;
+import java.util.UUID;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class CreateAuthorizationTest {
+
+  @ClassRule public static final EngineRule ENGINE = EngineRule.singlePartition();
+
+  @Rule
+  public final RecordingExporterTestWatcher recordingExporterTestWatcher =
+      new RecordingExporterTestWatcher();
+
+  @DisplayName(
+      "should create authorization if no authorization for owner and resource does not exist")
+  @Test
+  public void shouldCreateAuthorization() {
+    // when
+    final var owner = "owner" + UUID.randomUUID();
+    final var permissions = List.of("write:*");
+    final var createdAuthorizationRecord =
+        ENGINE
+            .authorization()
+            .newAuthorization(1)
+            .withOwnerKey(owner)
+            .withOwnerType(AuthorizationOwnerType.USER)
+            .withResourceKey("resource")
+            .withResourceType("bpmn-id")
+            .withPermissions(permissions)
+            .create();
+
+    // then
+    final var createdAuthorization = createdAuthorizationRecord.getValue();
+    assertThat(createdAuthorization)
+        .isNotNull()
+        .hasFieldOrPropertyWithValue("ownerKey", owner)
+        .hasFieldOrPropertyWithValue("ownerType", AuthorizationOwnerType.USER.toString())
+        .hasFieldOrPropertyWithValue("resourceKey", "resource")
+        .hasFieldOrPropertyWithValue("resourceType", "bpmn-id")
+        .hasFieldOrPropertyWithValue("permissions", permissions);
+  }
+
+  @DisplayName("should create user throws exception when username already exists")
+  @Test
+  public void shouldNotDuplicate() {
+    // given
+    final var owner = "owner" + UUID.randomUUID();
+    final var permissions = List.of("write:*");
+    final var createdAuthorizationRecord =
+        ENGINE
+            .authorization()
+            .newAuthorization(System.currentTimeMillis())
+            .withOwnerKey(owner)
+            .withOwnerType(AuthorizationOwnerType.USER)
+            .withResourceKey("resource")
+            .withResourceType("bpmn-id")
+            .withPermissions(permissions)
+            .create();
+    // when
+    final var duplicatedAuthorizationRecord =
+        ENGINE
+            .authorization()
+            .newAuthorization(System.currentTimeMillis() + 1)
+            .withOwnerKey(owner)
+            .withOwnerType(AuthorizationOwnerType.USER)
+            .withResourceKey("resource")
+            .withResourceType("bpmn-id")
+            .withPermissions(permissions)
+            .expectRejection()
+            .create();
+
+    final var createdAuthorization = createdAuthorizationRecord.getValue();
+    assertThat(createdAuthorization).isNotNull().hasFieldOrPropertyWithValue("ownerKey", owner);
+
+    io.camunda.zeebe.protocol.record.Assertions.assertThat(duplicatedAuthorizationRecord)
+        .hasRejectionType(RejectionType.ALREADY_EXISTS);
+  }
+}
---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/util/client/AuthorizationClient.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.util.client;
+
+import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationOwnerType;
+import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.intent.AuthorizationIntent;
+import io.camunda.zeebe.protocol.record.value.AuthorizationRecordValue;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import java.util.List;
+import java.util.function.Function;
+
+public final class AuthorizationClient {
+
+  private final CommandWriter writer;
+
+  public AuthorizationClient(final CommandWriter writer) {
+    this.writer = writer;
+  }
+
+  public AuthorizationCreationClient newAuthorization(final long authorizationKey) {
+    return new AuthorizationCreationClient(writer, authorizationKey);
+  }
+
+  public static class AuthorizationCreationClient {
+
+    private static final Function<Long, Record<AuthorizationRecordValue>> SUCCESS_SUPPLIER =
+        (position) ->
+            RecordingExporter.authorizationRecords().withSourceRecordPosition(position).getFirst();
+    private static final Function<Long, Record<AuthorizationRecordValue>> REJECTION_SUPPLIER =
+        (position) ->
+            RecordingExporter.authorizationRecords()
+                .onlyCommandRejections()
+                .withSourceRecordPosition(position)
+                .getFirst();
+    private final CommandWriter writer;
+    private final AuthorizationRecord authorizationCreationRecord;
+    private Function<Long, Record<AuthorizationRecordValue>> expectation = SUCCESS_SUPPLIER;
+
+    public AuthorizationCreationClient(final CommandWriter writer, final long authorizationKey) {
+      this.writer = writer;
+      authorizationCreationRecord = new AuthorizationRecord();
+      authorizationCreationRecord.setAuthorizationKey(authorizationKey);
+    }
+
+    public AuthorizationCreationClient withOwnerKey(final String ownerKey) {
+      authorizationCreationRecord.setOwnerKey(ownerKey);
+      return this;
+    }
+
+    public AuthorizationCreationClient withOwnerType(final AuthorizationOwnerType ownerType) {
+      authorizationCreationRecord.setOwnerType(ownerType);
+      return this;
+    }
+
+    public AuthorizationCreationClient withResourceKey(final String resourceKey) {
+      authorizationCreationRecord.setResourceKey(resourceKey);
+      return this;
+    }
+
+    public AuthorizationCreationClient withResourceType(final String resourceType) {
+      authorizationCreationRecord.setResourceType(resourceType);
+      return this;
+    }
+
+    public AuthorizationCreationClient withPermissions(final List<String> permissions) {
+      authorizationCreationRecord.setPermissions(permissions);
+      return this;
+    }
+
+    public Record<AuthorizationRecordValue> create() {
+      final long position =
+          writer.writeCommand(AuthorizationIntent.CREATE, authorizationCreationRecord);
+      return expectation.apply(position);
+    }
+
+    public AuthorizationCreationClient expectRejection() {
+      expectation = REJECTION_SUPPLIER;
+      return this;
+    }
+  }
+}
