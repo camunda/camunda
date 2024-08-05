45bc4a28a424a2c959e002eede29697852b860a4
Igor Petrov <igor.petrov-ext@camunda.com>
2024-08-05 20:38:00 +0300

    feat: prototype ES custom header plugin for ES exporter

--- zeebe/exporters/elasticsearch-exporter/src/main/java/io/camunda/zeebe/exporter/RestClientFactory.java
+++ zeebe/exporters/elasticsearch-exporter/src/main/java/io/camunda/zeebe/exporter/RestClientFactory.java
@@ -7,7 +7,14 @@
  */
 package io.camunda.zeebe.exporter;
 
+import io.camunda.plugin.search.header.DatabaseCustomHeaderSupplier;
+import io.camunda.zeebe.exporter.ElasticsearchExporterConfiguration.InterceptorPlugin;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Paths;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -16,8 +23,12 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 final class RestClientFactory {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RestClientFactory.class);
+
   private static final RestClientFactory INSTANCE = new RestClientFactory();
 
   private RestClientFactory() {}
@@ -49,11 +60,63 @@ final class RestClientFactory {
     // use single thread for rest client
     builder.setDefaultIOReactorConfig(IOReactorConfig.custom().setIoThreadCount(1).build());
 
+    LOGGER.error("IGPETROV Attempt to load plugins");
+    if (config.getInterceptorPlugin() != null) {
+      LOGGER.error("IGPETROV Plugins detected to be not empty {}", config.getInterceptorPlugin());
+
+      final InterceptorPlugin interceptor = config.getInterceptorPlugin();
+      LOGGER.error("IGPETROV Attempting to register {}", interceptor.getId());
+      final var interceptorClazz = createInterceptorClass(interceptor);
+      if (interceptorClazz != null) {
+        // TODO: only default constructor
+        final Constructor<?> constructor = interceptorClazz.getConstructors()[0];
+        try {
+          final var createdInterceptor = constructor.newInstance();
+          if (createdInterceptor instanceof final DatabaseCustomHeaderSupplier dchs) {
+            LOGGER.error(
+                "IGPETROV Plugin {} appears to be a DB Header Provider. Registering with interceptor",
+                interceptor.getId());
+            builder.addInterceptorLast(
+                (HttpRequestInterceptor)
+                    (httpRequest, httpContext) -> {
+                      httpRequest.addHeader(
+                          dchs.getElasticsearchCustomHeader().key(),
+                          dchs.getElasticsearchCustomHeader().value());
+                    });
+          }
+        } catch (final InstantiationException
+            | IllegalAccessException
+            | InvocationTargetException e) {
+          LOGGER.error(
+              "IGPETROV Plugin {} failed to register due to exception. Ignoring",
+              interceptor.getId(),
+              e);
+        }
+      }
+    }
+
     if (config.hasAuthenticationPresent()) {
       setupBasicAuthentication(config, builder);
     }
 
     return builder;
+  }
+
+  Class<?> createInterceptorClass(final InterceptorPlugin interceptorPlugin) {
+    // File type and path are checked by class loader
+    try (final var classLoader =
+        ExternalJarClassLoaderMaybeRemove.ofPath(Paths.get(interceptorPlugin.getJarPath()))) {
+      return classLoader.loadClass(interceptorPlugin.getClassName());
+    } catch (final IOException | ClassNotFoundException e) {
+      LOGGER.error(
+          "IGPETROV Plugin {} failed to register due to exception. Ignoring",
+          interceptorPlugin.getId(),
+          e);
+      return null;
+    }
+
+    // TODO other validations?
+
   }
 
   private void setupBasicAuthentication(
