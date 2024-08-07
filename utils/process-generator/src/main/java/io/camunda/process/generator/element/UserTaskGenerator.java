a13b65e52b55aa869a0a07a257971c91cbb86691
Philipp Ossler <philipp.ossler@gmail.com>
2024-08-07 10:04:14 +0200

    feat: add user task generator
    Changes by nicolapuppa

---
+++ utils/process-generator/src/main/java/io/camunda/process/generator/element/UserTaskGenerator.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.process.generator.element;
+
+import io.camunda.process.generator.GeneratorContext;
+import io.camunda.zeebe.model.bpmn.builder.AbstractFlowNodeBuilder;
+import io.camunda.zeebe.model.bpmn.builder.UserTaskBuilder;
+
+public class UserTaskGenerator implements BpmnElementGenerator {
+
+  private final GeneratorContext generatorContext;
+
+  public UserTaskGenerator(final GeneratorContext generatorContext) {
+    this.generatorContext = generatorContext;
+  }
+
+  @Override
+  public AbstractFlowNodeBuilder<?, ?> addElement(
+      final AbstractFlowNodeBuilder<?, ?> processBuilder) {
+    final String elementId = generatorContext.createNewId();
+
+    final UserTaskBuilder userTaskBuilder = processBuilder.userTask();
+    userTaskBuilder.id(elementId).name(elementId);
+
+    return userTaskBuilder;
+  }
+}
