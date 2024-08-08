9fcb0a46ee7dcce514232c9a2204d5c193c757b5
remco.westerhoud <remcowesterhoud@msn.com>
2024-08-08 16:21:47 +0200

    feat: parallelize randomized test

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/randomized/NewProcessExecutionRandomizePropertyTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/randomized/NewProcessExecutionRandomizePropertyTest.java
@@ -14,14 +14,24 @@ import io.camunda.zeebe.engine.util.EngineRule;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.protocol.record.value.BpmnElementType;
 import io.camunda.zeebe.test.util.record.RecordingExporter;
+import java.util.Collection;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestWatcher;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class NewProcessExecutionRandomizePropertyTest {
+
+  public static final int AMOUNT_OF_PROCESSES = 1000;
+  private static final BpmnGenerator BPMN_GENERATOR = new BpmnGenerator();
   @Rule public final EngineRule engineRule = EngineRule.singlePartition();
-  private final BpmnGenerator bpmnGenerator = new BpmnGenerator();
-  private GeneratedProcess generatedProcess;
+  @Parameter public GeneratedProcess generatedProcess;
 
   @Rule
   public TestWatcher failedTestPrinter =
@@ -34,7 +44,6 @@ public class NewProcessExecutionRandomizePropertyTest {
   @Test
   public void shouldExecuteProcessToEnd() {
     // given - generate and deploy a process
-    generatedProcess = bpmnGenerator.generateProcess();
     final var engineRuleProcessExecutor = new EngineRuleProcessExecutor(engineRule);
     engineRule.deployment().withXmlResource(generatedProcess.process()).deploy();
 
@@ -51,5 +60,13 @@ public class NewProcessExecutionRandomizePropertyTest {
         .withElementType(BpmnElementType.PROCESS)
         .withBpmnProcessId(generatedProcess.processId())
         .await();
+  }
+
+  @Parameters(name = "{0}")
+  public static Collection<GeneratedProcess> processes() {
+    return IntStream.range(0, AMOUNT_OF_PROCESSES)
+        .mapToObj(i -> BPMN_GENERATOR.generateProcess(ThreadLocalRandom.current().nextLong()))
+        .toList();
+    //    return List.of(BPMN_GENERATOR.generateProcess(123L));
   }
 }
