7a9ac36c1c730317852d51d48b6ea15eb52d562f
Euro Lew <euro.lew@camunda.com>
2024-08-16 12:18:46 +0100

    test: to check TLS configuration with pkcs12 file is secure

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/network/SecureClusteredMessagingIT.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/network/SecureClusteredMessagingIT.java
@@ -15,34 +15,47 @@ import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
 import io.camunda.zeebe.qa.util.cluster.TestCluster;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
-import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
 import io.camunda.zeebe.test.util.asserts.SslAssert;
 import io.camunda.zeebe.test.util.asserts.TopologyAssert;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.nio.file.Files;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.Test;
 
 @ZeebeIntegration
 final class SecureClusteredMessagingIT {
   private final SelfSignedCertificate certificate = newCertificate();
+  private final String pkcs12Password = "password";
+  private final File pkcs12 = createPKCS12File(certificate);
 
-  @TestZeebe
-  private final TestCluster cluster =
-      TestCluster.builder()
-          .withPartitionsCount(1)
-          .withReplicationFactor(2)
-          .withBrokersCount(2)
-          .withGatewaysCount(1)
-          .withEmbeddedGateway(false)
-          .withBrokerConfig(broker -> broker.withBrokerConfig(this::configureBroker))
-          .withGatewayConfig(gateway -> gateway.withGatewayConfig(this::configureGateway))
-          .build();
+  private static TestCluster createCluster(
+      final Consumer<GatewayCfg> gateCfgChanges, final Consumer<BrokerCfg> brokerCfgChanges) {
+    final var cluster =
+        TestCluster.builder()
+            .withPartitionsCount(1)
+            .withReplicationFactor(2)
+            .withBrokersCount(2)
+            .withGatewaysCount(1)
+            .withEmbeddedGateway(false)
+            .withBrokerConfig(broker -> broker.withBrokerConfig(brokerCfgChanges::accept))
+            .withGatewayConfig(gateway -> gateway.withGatewayConfig(gateCfgChanges))
+            .build();
+    cluster.start();
+    cluster.awaitCompleteTopology();
+    return cluster;
+  }
 
   @Test
   void shouldFormAClusterWithTls() {
+    final var cluster = createCluster(this::configureGateway, this::configureBroker);
     // given - a cluster with 2 standalone brokers, and 1 standalone gateway
 
     // when - note the client is using plaintext since we only care about inter-cluster TLS
@@ -53,7 +66,24 @@ final class SecureClusteredMessagingIT {
     // are secured using the expected certificate
     TopologyAssert.assertThat(topology).hasBrokersCount(2).isComplete(2, 1, 2);
     cluster.brokers().values().forEach(this::assertBrokerMessagingServicesAreSecured);
-    assertAddressIsSecured("gateway", getGatewayAddress());
+    assertAddressIsSecured("gateway", getGatewayAddress(cluster));
+  }
+
+  @Test
+  void shouldFormAClusterWithTlsWithPKCS12File() {
+    final var cluster =
+        createCluster(this::configureGatewayWithPkcs12, this::configureBrokerWithPkcs12);
+    // given - a cluster with 2 standalone brokers, and 1 standalone gateway
+
+    // when - note the client is using plaintext since we only care about inter-cluster TLS
+    final Topology topology =
+        cluster.newClientBuilder().build().newTopologyRequest().send().join(15, TimeUnit.SECONDS);
+
+    // then - ensure the cluster is formed correctly and all inter-cluster communication endpoints
+    // are secured using the expected certificate
+    TopologyAssert.assertThat(topology).hasBrokersCount(2).isComplete(2, 1, 2);
+    cluster.brokers().values().forEach(this::assertBrokerMessagingServicesAreSecured);
+    assertAddressIsSecured("gateway", getGatewayAddress(cluster));
   }
 
   /** Verifies that both the command and internal APIs of the broker are correctly secured. */
@@ -67,7 +97,7 @@ final class SecureClusteredMessagingIT {
     assertAddressIsSecured(broker.brokerConfig().getCluster().getNodeId(), internalApiAddress);
   }
 
-  private InetSocketAddress getGatewayAddress() {
+  private InetSocketAddress getGatewayAddress(final TestCluster cluster) {
     final ClusterCfg clusterConfig = cluster.availableGateway().gatewayConfig().getCluster();
     final var address =
         Address.from(clusterConfig.getAdvertisedHost(), clusterConfig.getAdvertisedPort());
@@ -98,5 +128,40 @@ final class SecureClusteredMessagingIT {
     config.getNetwork().getSecurity().setEnabled(true);
     config.getNetwork().getSecurity().setCertificateChainPath(certificate.certificate());
     config.getNetwork().getSecurity().setPrivateKeyPath(certificate.privateKey());
+  }
+
+  private void configureGatewayWithPkcs12(final GatewayCfg config) {
+    config.getCluster().getSecurity().setEnabled(true);
+    config.getCluster().getSecurity().getPkcs12().setFilePath(pkcs12);
+    config.getCluster().getSecurity().getPkcs12().setPassword(pkcs12Password);
+  }
+
+  private void configureBrokerWithPkcs12(final BrokerCfg config) {
+    config.getNetwork().getSecurity().setEnabled(true);
+    config.getNetwork().getSecurity().getPkcs12().setFilePath(pkcs12);
+    config.getNetwork().getSecurity().getPkcs12().setPassword(pkcs12Password);
+  }
+
+  private File createPKCS12File(final SelfSignedCertificate cert) {
+    try {
+      final KeyStore store = KeyStore.getInstance("PKCS12");
+      store.load(null, null);
+
+      final Certificate[] chain = new Certificate[] {cert.cert()};
+
+      store.setKeyEntry("key", cert.key(), null, chain);
+
+      final var file = Files.createTempFile("id", ".p12").toFile();
+      final FileOutputStream fOut = new FileOutputStream(file);
+
+      store.store(fOut, pkcs12Password.toCharArray());
+
+      fOut.close();
+
+      return file;
+
+    } catch (final Exception e) {
+      throw new RuntimeException("Failed to create PKCS12 file", e);
+    }
   }
 }
