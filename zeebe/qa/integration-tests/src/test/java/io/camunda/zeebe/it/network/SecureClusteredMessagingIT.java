baf27bd9271a359adca0714aa1e245d692f893c2
Euro Lew <euro.lew@camunda.com>
2024-08-16 16:58:17 +0100

    refactor: SecureClusteredMessagingIT now uses test cases to avoid duplication

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/network/SecureClusteredMessagingIT.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/network/SecureClusteredMessagingIT.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.it.network;
 
 import io.atomix.utils.net.Address;
+import io.camunda.application.commons.configuration.BrokerBasedConfiguration.BrokerBasedProperties;
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.client.api.response.Topology;
 import io.camunda.zeebe.gateway.impl.configuration.ClusterCfg;
@@ -15,6 +16,7 @@ import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
 import io.camunda.zeebe.qa.util.cluster.TestCluster;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
 import io.camunda.zeebe.test.util.asserts.SslAssert;
 import io.camunda.zeebe.test.util.asserts.TopologyAssert;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
@@ -28,39 +30,38 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @ZeebeIntegration
 final class SecureClusteredMessagingIT {
-  private final SelfSignedCertificate certificate = newCertificate();
-  private final String keyStorePassword = "password";
-  private final File keyStore = createPKCS12File(certificate);
+  private static final SelfSignedCertificate CERTIFICATE = newCertificate();
 
-  private static TestCluster createCluster(
-      final Consumer<GatewayCfg> gateCfgChanges, final Consumer<BrokerCfg> brokerCfgChanges) {
-    final var cluster =
-        TestCluster.builder()
-            .withPartitionsCount(1)
-            .withReplicationFactor(2)
-            .withBrokersCount(2)
-            .withGatewaysCount(1)
-            .withEmbeddedGateway(false)
-            .withBrokerConfig(broker -> broker.withBrokerConfig(brokerCfgChanges::accept))
-            .withGatewayConfig(gateway -> gateway.withGatewayConfig(gateCfgChanges))
-            .build();
-    cluster.start();
-    cluster.awaitCompleteTopology();
-    return cluster;
-  }
+  @TestZeebe(autoStart = false)
+  private final TestCluster cluster =
+      TestCluster.builder()
+          .withPartitionsCount(1)
+          .withReplicationFactor(2)
+          .withBrokersCount(2)
+          .withGatewaysCount(1)
+          .withEmbeddedGateway(false)
+          .build();
 
-  @Test
-  void shouldFormAClusterWithTls() {
-    final var cluster = createCluster(this::configureGateway, this::configureBroker);
+  @ParameterizedTest
+  @MethodSource("provideTestCases")
+  void shouldFormAClusterWithTlsWithCertChain(final TestCase testCase) {
     // given - a cluster with 2 standalone brokers, and 1 standalone gateway
+    cluster.brokers().values().forEach(node -> node.withBrokerConfig(testCase.brokerConfig));
+    cluster.gateways().values().forEach(node -> node.withGatewayConfig(testCase.gatewayConfig));
+    cluster.start().awaitCompleteTopology();
 
     // when - note the client is using plaintext since we only care about inter-cluster TLS
-    final Topology topology =
-        cluster.newClientBuilder().build().newTopologyRequest().send().join(15, TimeUnit.SECONDS);
+    final Topology topology;
+    try (final var client = cluster.newClientBuilder().build()) {
+      topology = client.newTopologyRequest().send().join(15, TimeUnit.SECONDS);
+    }
 
     // then - ensure the cluster is formed correctly and all inter-cluster communication endpoints
     // are secured using the expected certificate
@@ -69,21 +70,20 @@ final class SecureClusteredMessagingIT {
     assertAddressIsSecured("gateway", getGatewayAddress(cluster));
   }
 
-  @Test
-  void shouldFormAClusterWithTlsWithPKCS12File() {
-    final var cluster =
-        createCluster(this::configureGatewayWithPkcs12, this::configureBrokerWithPkcs12);
-    // given - a cluster with 2 standalone brokers, and 1 standalone gateway
+  private static Stream<Named<TestCase>> provideTestCases() {
+    final var pkcs12File = createPKCS12File();
 
-    // when - note the client is using plaintext since we only care about inter-cluster TLS
-    final Topology topology =
-        cluster.newClientBuilder().build().newTopologyRequest().send().join(15, TimeUnit.SECONDS);
-
-    // then - ensure the cluster is formed correctly and all inter-cluster communication endpoints
-    // are secured using the expected certificate
-    TopologyAssert.assertThat(topology).hasBrokersCount(2).isComplete(2, 1, 2);
-    cluster.brokers().values().forEach(this::assertBrokerMessagingServicesAreSecured);
-    assertAddressIsSecured("gateway", getGatewayAddress(cluster));
+    return Stream.of(
+        Named.of(
+            "cert/key pair",
+            new TestCase(
+                SecureClusteredMessagingIT::configureCertChainBroker,
+                SecureClusteredMessagingIT::configureCertChainGateway)),
+        Named.of(
+            "key store",
+            new TestCase(
+                config -> configureKeyStoreBroker(config, pkcs12File),
+                config -> configureKeyStoreGateway(config, pkcs12File))));
   }
 
   /** Verifies that both the command and internal APIs of the broker are correctly secured. */
@@ -107,10 +107,10 @@ final class SecureClusteredMessagingIT {
   private void assertAddressIsSecured(final Object nodeId, final SocketAddress address) {
     SslAssert.assertThat(address)
         .as("node %s is not secured correctly at address %s", nodeId, address)
-        .isSecuredBy(certificate);
+        .isSecuredBy(CERTIFICATE);
   }
 
-  private SelfSignedCertificate newCertificate() {
+  private static SelfSignedCertificate newCertificate() {
     try {
       return new SelfSignedCertificate();
     } catch (final CertificateException e) {
@@ -118,48 +118,49 @@ final class SecureClusteredMessagingIT {
     }
   }
 
-  private void configureGateway(final GatewayCfg config) {
-    config.getCluster().getSecurity().setEnabled(true);
-    config.getCluster().getSecurity().setCertificateChainPath(certificate.certificate());
-    config.getCluster().getSecurity().setPrivateKeyPath(certificate.privateKey());
-  }
-
-  private void configureBroker(final BrokerCfg config) {
-    config.getNetwork().getSecurity().setEnabled(true);
-    config.getNetwork().getSecurity().setCertificateChainPath(certificate.certificate());
-    config.getNetwork().getSecurity().setPrivateKeyPath(certificate.privateKey());
-  }
-
-  private void configureGatewayWithPkcs12(final GatewayCfg config) {
-    config.getCluster().getSecurity().setEnabled(true);
-    config.getCluster().getSecurity().getKeyStore().setFilePath(keyStore);
-    config.getCluster().getSecurity().getKeyStore().setPassword(keyStorePassword);
-  }
-
-  private void configureBrokerWithPkcs12(final BrokerCfg config) {
-    config.getNetwork().getSecurity().setEnabled(true);
-    config.getNetwork().getSecurity().getKeyStore().setFilePath(keyStore);
-    config.getNetwork().getSecurity().getKeyStore().setPassword(keyStorePassword);
-  }
-
-  private File createPKCS12File(final SelfSignedCertificate cert) {
+  private static File createPKCS12File() {
     try {
-      final KeyStore store = KeyStore.getInstance("PKCS12");
-      store.load(null, null);
-
-      final Certificate[] chain = new Certificate[] {cert.cert()};
-
-      store.setKeyEntry("key", cert.key(), keyStorePassword.toCharArray(), chain);
-
+      final var store = KeyStore.getInstance("PKCS12");
+      final var chain = new Certificate[] {CERTIFICATE.cert()};
       final var file = Files.createTempFile("id", ".p12").toFile();
+
+      store.load(null, null);
+      store.setKeyEntry("key", CERTIFICATE.key(), "password".toCharArray(), chain);
+
       try (final var fOut = new FileOutputStream(file)) {
-        store.store(fOut, keyStorePassword.toCharArray());
+        store.store(fOut, "password".toCharArray());
       }
 
       return file;
-
     } catch (final Exception e) {
       throw new RuntimeException("Failed to create PKCS12 file", e);
     }
   }
+
+  private static void configureCertChainGateway(final GatewayCfg gatewayConfig) {
+    gatewayConfig.getCluster().getSecurity().setEnabled(true);
+    gatewayConfig.getCluster().getSecurity().setCertificateChainPath(CERTIFICATE.certificate());
+    gatewayConfig.getCluster().getSecurity().setPrivateKeyPath(CERTIFICATE.privateKey());
+  }
+
+  private static void configureCertChainBroker(final BrokerCfg config) {
+    config.getNetwork().getSecurity().setEnabled(true);
+    config.getNetwork().getSecurity().setCertificateChainPath(CERTIFICATE.certificate());
+    config.getNetwork().getSecurity().setPrivateKeyPath(CERTIFICATE.privateKey());
+  }
+
+  private static void configureKeyStoreGateway(final GatewayCfg config, final File pkcs12) {
+    config.getCluster().getSecurity().setEnabled(true);
+    config.getCluster().getSecurity().getKeyStore().setFilePath(pkcs12);
+    config.getCluster().getSecurity().getKeyStore().setPassword("password");
+  }
+
+  private static void configureKeyStoreBroker(final BrokerCfg config, final File pkcs12) {
+    config.getNetwork().getSecurity().setEnabled(true);
+    config.getNetwork().getSecurity().getKeyStore().setFilePath(pkcs12);
+    config.getNetwork().getSecurity().getKeyStore().setPassword("password");
+  }
+
+  private record TestCase(
+      Consumer<BrokerBasedProperties> brokerConfig, Consumer<GatewayCfg> gatewayConfig) {}
 }
