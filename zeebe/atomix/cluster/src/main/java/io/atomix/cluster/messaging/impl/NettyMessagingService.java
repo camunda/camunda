a9fa4d1ff8325db6bf1031bcbb799cf97bdd90bf
Euro Lew <euro.lew@camunda.com>
2024-08-16 16:25:42 +0100

    feat: human understandable error message for invalid key store file

--- zeebe/atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
+++ zeebe/atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
@@ -559,9 +559,17 @@ public final class NettyMessagingService implements ManagedMessagingService {
   }
 
   private KeyStore getKeyStore(final File keyStoreFile, final String password)
-      throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
+      throws KeyStoreException {
     final var keyStore = KeyStore.getInstance("PKCS12");
-    keyStore.load(new FileInputStream(keyStoreFile), password.toCharArray());
+    try {
+      keyStore.load(new FileInputStream(keyStoreFile), password.toCharArray());
+    } catch (final Exception e) {
+      throw new IllegalStateException(
+          String.format(
+              "Keystore failed to load file: %s, please ensure it is a valid PKCS12 keystore",
+              keyStoreFile.toPath()),
+          e);
+    }
 
     return keyStore;
   }
