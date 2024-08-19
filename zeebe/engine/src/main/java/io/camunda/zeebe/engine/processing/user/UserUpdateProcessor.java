460d986d677ff8cc94c8027b6e0c9ac74e2f4991
Ben Sheppard <ben.sheppard@camunda.com>
2024-08-19 09:06:44 +0100

    feat: support processing user update intent

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/user/UserUpdateProcessor.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.user;
+
+import io.camunda.zeebe.engine.processing.distribution.CommandDistributionBehavior;
+import io.camunda.zeebe.engine.processing.streamprocessor.DistributedTypedRecordProcessor;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedResponseWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
+import io.camunda.zeebe.engine.state.immutable.ProcessingState;
+import io.camunda.zeebe.engine.state.immutable.UserState;
+import io.camunda.zeebe.protocol.impl.record.value.user.UserRecord;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.intent.UserIntent;
+import io.camunda.zeebe.stream.api.records.TypedRecord;
+
+public class UserUpdateProcessor implements DistributedTypedRecordProcessor<UserRecord> {
+
+  private final UserState userState;
+  private final StateWriter stateWriter;
+  private final TypedRejectionWriter rejectionWriter;
+  private final TypedResponseWriter responseWriter;
+  private final CommandDistributionBehavior distributionBehavior;
+
+  public UserUpdateProcessor(
+      final ProcessingState state,
+      final Writers writers,
+      final CommandDistributionBehavior distributionBehavior) {
+    userState = state.getUserState();
+    stateWriter = writers.state();
+    rejectionWriter = writers.rejection();
+    responseWriter = writers.response();
+    this.distributionBehavior = distributionBehavior;
+  }
+
+  @Override
+  public void processNewCommand(final TypedRecord<UserRecord> command) {
+    final var username = command.getValue().getUsernameBuffer();
+    final var user = userState.getUser(username);
+
+    if (user == null) {
+      final var rejectionMessage =
+          "Expected to update user with username %s, but a user with this username does not exist"
+              .formatted(command.getValue().getUsername());
+
+      rejectionWriter.appendRejection(command, RejectionType.NOT_FOUND, rejectionMessage);
+      responseWriter.writeRejectionOnCommand(command, RejectionType.NOT_FOUND, rejectionMessage);
+      return;
+    }
+
+    stateWriter.appendFollowUpEvent(command.getKey(), UserIntent.UPDATED, command.getValue());
+    responseWriter.writeEventOnCommand(
+        command.getKey(), UserIntent.UPDATED, command.getValue(), command);
+
+    distributionBehavior.distributeCommand(command.getKey(), command);
+  }
+
+  @Override
+  public void processDistributedCommand(final TypedRecord<UserRecord> command) {
+    stateWriter.appendFollowUpEvent(command.getKey(), UserIntent.UPDATED, command.getValue());
+
+    distributionBehavior.acknowledgeCommand(command);
+  }
+}
