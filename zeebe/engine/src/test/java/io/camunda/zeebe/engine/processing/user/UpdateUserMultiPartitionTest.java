dfaaf12c68b7b2302d48dfac9b220c7f0a3e3360
Ben Sheppard <ben.sheppard@camunda.com>
2024-08-20 12:14:46 +0100

    style: remove unnecessary when statement

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/user/UpdateUserMultiPartitionTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/user/UpdateUserMultiPartitionTest.java
@@ -34,7 +34,6 @@ public class UpdateUserMultiPartitionTest {
 
   @Test
   public void shouldDistributeUserUpdateCommand() {
-    // when
     final var username = UUID.randomUUID().toString();
 
     ENGINE
