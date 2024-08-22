a484726fbad09799b6d7550d35e67a75313730a1
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2024-08-22 17:02:23 +0200

    refactor: move test to camunda qa

--- qa/integration-tests/src/test/java/io/camunda/it/auth/BasicAuthIT.java
+++ qa/integration-tests/src/test/java/io/camunda/it/auth/BasicAuthIT.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.it.auth;
+package io.camunda.it.auth;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
