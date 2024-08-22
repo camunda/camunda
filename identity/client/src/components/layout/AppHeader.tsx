3e8b7b6649cae1a19f9fa50dbd5747eed47c7e3c
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2024-08-22 14:38:05 +0200

    feat: show license status in identity UI

--- identity/client/src/components/layout/AppHeader.tsx
+++ identity/client/src/components/layout/AppHeader.tsx
@@ -1,10 +1,20 @@
 import { C3Navigation } from "@camunda/camunda-composite-components";
 import { useGlobalRoutes } from "src/components/global/useGlobalRoutes";
 import { useNavigate } from "react-router";
+import { useApi } from "src/utility/api";
+import { checkLicense } from "src/utility/api/headers";
 
 const AppHeader = () => {
   const routes = useGlobalRoutes();
   const navigate = useNavigate();
+  const { data: license } = useApi(checkLicense);
+  const showLicense: boolean =
+    license == null
+      ? false
+      : license.licenseType !== undefined &&
+        license.licenseType == "self-managed";
+  const isProductionLicense: boolean =
+    license == null ? false : license.validLicense;
 
   return (
     <C3Navigation
@@ -26,6 +36,10 @@ const AppHeader = () => {
             onClick: () => navigate(route.key),
           },
         })),
+        licenseTag: {
+          show: showLicense,
+          isProductionLicense: isProductionLicense,
+        },
       }}
     />
   );
