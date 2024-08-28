f5bc870f09f5e23cf801c5b035c8cb677f98830a
Ralf Puchert <ralf.puchert@camunda.com>
2024-08-28 10:05:49 +0200

    feat: add documentation for IncidentFilter
    Related with #20661

--- clients/java/src/main/java/io/camunda/zeebe/client/api/search/filter/IncidentFilter.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/api/search/filter/IncidentFilter.java
@@ -19,23 +19,83 @@ import io.camunda.zeebe.client.api.search.query.TypedSearchQueryRequest.SearchRe
 
 public interface IncidentFilter extends SearchRequestFilter {
 
+  /**
+   * Filters incidents by the specified key.
+   *
+   * @param value the key of the incidents
+   * @return the updated filter
+   */
   IncidentFilter key(final Long value);
 
+  /**
+   * Filters incidents by the process definition key.
+   *
+   * @param value the key of the process definition
+   * @return the updated filter
+   */
   IncidentFilter processDefinitionKey(final Long value);
 
+  /**
+   * Filters incidents by the process instance key.
+   *
+   * @param value the key of the process instance
+   * @return the updated filter
+   */
   IncidentFilter processInstanceKey(final Long value);
 
+  /**
+   * Filters incidents by the type.
+   *
+   * @param value the type of incident
+   * @return the updated filter
+   */
   IncidentFilter type(final String type);
 
+  /**
+   * Filters incidents by the flow node id.
+   *
+   * @param value the id of flow node id.
+   * @return the updated filter
+   */
   IncidentFilter flowNodeId(final String value);
 
+  /**
+   * Filters incidents by the flow node instance id.
+   *
+   * @param value the id of flow node instance.
+   * @return the updated filter
+   */
   IncidentFilter flowNodeInstanceId(final String value);
 
+  /**
+   * Filters incidents by the state of incident.
+   *
+   * @param value the state of incident
+   * @return the updated filter
+   */
   IncidentFilter state(final String value);
 
+  /**
+   * Filters incidents by job key.
+   *
+   * @param value the key of the job.
+   * @return the updated filter
+   */
   IncidentFilter jobKey(final Long value);
 
+  /**
+   * Filters incidents by tenant id.
+   *
+   * @param value the id of tenant
+   * @return the updated filter
+   */
   IncidentFilter tenantId(final String value);
 
+  /**
+   * Filters incidents by having an active operation.
+   *
+   * @param value whether the incident has an active operation.
+   * @return the updated filter
+   */
   IncidentFilter hasActiveOperation(final Boolean value);
 }
