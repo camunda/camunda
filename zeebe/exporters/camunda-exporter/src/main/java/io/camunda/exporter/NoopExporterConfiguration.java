3d34fef041ff79573d1b1bbb5f710029dab4b349
Euro Lew <euro.lew@camunda.com>
2024-08-29 15:05:53 +0100

    feat: skeleton for creating initial resources based on exporter config

---
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/NoopExporterConfiguration.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class NoopExporterConfiguration {
+  public final ZeebeConfig zeebe = new ZeebeConfig();
+  public final ElasticsearchConfig elasticsearch = new ElasticsearchConfig();
+
+  public static final class ElasticsearchConfig {
+    public String url;
+    public IndexSpecificSettings defaultSettings = new IndexSpecificSettings();
+    public Map<String, IndexSpecificSettings> indexSettingsByName = new HashMap<>();
+
+    public static final class IndexSpecificSettings {
+      public String numberOfShards;
+      public String numberOfReplicas;
+    }
+  }
+
+  public static final class ZeebeConfig {}
+}
