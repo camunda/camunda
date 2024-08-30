ca7de97b0b5c0e184c5e234006103f86e35fe0a7
Nicola Puppa <nicola.puppa@camunda.com>
2024-08-30 14:54:20 +0200

    test: verify start process instance endpoint behaviour

--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/ProcessInstanceControllerTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/ProcessInstanceControllerTest.java
@@ -7,12 +7,14 @@
  */
 package io.camunda.zeebe.gateway.rest.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import io.camunda.service.ProcessInstanceServices;
+import io.camunda.service.ProcessInstanceServices.ProcessInstanceStartRequest;
 import io.camunda.service.entities.ProcessInstanceEntity;
 import io.camunda.service.search.query.ProcessInstanceQuery;
 import io.camunda.service.search.query.SearchQueryResult;
@@ -20,9 +22,14 @@ import io.camunda.service.search.query.SearchQueryResult.Builder;
 import io.camunda.service.search.sort.ProcessInstanceSort;
 import io.camunda.service.security.auth.Authentication;
 import io.camunda.zeebe.gateway.rest.RestControllerTest;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceResultRecord;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -59,6 +66,18 @@ public class ProcessInstanceControllerTest extends RestControllerTest {
       }""";
   private static final String PROCESS_INSTANCES_SEARCH_URL = "/v2/process-instances/search";
   private static final SearchQueryResult<ProcessInstanceEntity> SEARCH_QUERY_RESULT =
+
+  static final String EXPECTED_START_RESPONSE =
+      """
+      {
+         "processKey":123,
+         "bpmnProcessId":"bpmnProcessId",
+         "version":-1,
+         "processInstanceKey":123,
+         "tenantId":"tenantId"
+      }""";
+
+  static final SearchQueryResult<ProcessInstanceEntity> SEARCH_QUERY_RESULT =
       new Builder<ProcessInstanceEntity>()
           .total(1L)
           .items(
@@ -82,7 +101,11 @@ public class ProcessInstanceControllerTest extends RestControllerTest {
                       null)))
           .sortValues(new Object[] {"v"})
           .build();
+
+  static final String PROCESS_INSTANCES_START_URL = "/v2/process-instances";
+
   @MockBean ProcessInstanceServices processInstanceServices;
+  @Captor ArgumentCaptor<ProcessInstanceStartRequest> requestCaptor;
 
   @BeforeEach
   void setupServices() {
@@ -344,5 +367,279 @@ public class ProcessInstanceControllerTest extends RestControllerTest {
         .json(expectedResponse);
 
     verify(processInstanceServices, never()).search(any(ProcessInstanceQuery.class));
+  }
+
+  @Test
+  void shouldStartProcessInstancesWithProcessDefinitionKey() {
+    // given
+    final var mockResponse =
+        new ProcessInstanceCreationRecord()
+            .setProcessDefinitionKey(123L)
+            .setBpmnProcessId("bpmnProcessId")
+            .setProcessInstanceKey(123L)
+            .setTenantId("tenantId");
+
+    when(processInstanceServices.startProcessInstance(any(ProcessInstanceStartRequest.class)))
+        .thenReturn(CompletableFuture.completedFuture(mockResponse));
+
+    final var request =
+        """
+        {
+            "processDefinitionKey": 123
+        }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .json(EXPECTED_START_RESPONSE);
+
+    verify(processInstanceServices).startProcessInstance(requestCaptor.capture());
+    final var capturedRequest = requestCaptor.getValue();
+    assertThat(capturedRequest.processDefinitionKey()).isEqualTo(123L);
+  }
+
+  @Test
+  void shouldStartProcessInstancesWithBpmnProcessIdAndVersion() {
+    // given
+    final var mockResponse =
+        new ProcessInstanceCreationRecord()
+            .setProcessDefinitionKey(123L)
+            .setBpmnProcessId("bpmnProcessId")
+            .setProcessInstanceKey(123L)
+            .setTenantId("tenantId");
+
+    when(processInstanceServices.startProcessInstance(any(ProcessInstanceStartRequest.class)))
+        .thenReturn(CompletableFuture.completedFuture(mockResponse));
+
+    final var request =
+        """
+        {
+            "bpmnProcessId": "bpmnProcessId",
+            "version": 1
+        }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .json(EXPECTED_START_RESPONSE);
+
+    verify(processInstanceServices).startProcessInstance(requestCaptor.capture());
+    final var capturedRequest = requestCaptor.getValue();
+    assertThat(capturedRequest.bpmnProcessId()).isEqualTo("bpmnProcessId");
+    assertThat(capturedRequest.version()).isEqualTo(1);
+  }
+
+  @Test
+  void shouldStartProcessInstancesWithResultWithProcessDefinitionKey() {
+    // given
+    final var mockResponse =
+        new ProcessInstanceResultRecord()
+            .setProcessDefinitionKey(123L)
+            .setBpmnProcessId("bpmnProcessId")
+            .setProcessInstanceKey(123L)
+            .setTenantId("tenantId");
+
+    when(processInstanceServices.startProcessInstanceWithResult(
+            any(ProcessInstanceStartRequest.class)))
+        .thenReturn(CompletableFuture.completedFuture(mockResponse));
+
+    final var request =
+        """
+        {
+            "processDefinitionKey": 123,
+            "awaitCompletion": true
+        }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .json(EXPECTED_START_RESPONSE);
+
+    verify(processInstanceServices).startProcessInstanceWithResult(requestCaptor.capture());
+    final var capturedRequest = requestCaptor.getValue();
+    assertThat(capturedRequest.processDefinitionKey()).isEqualTo(123L);
+    assertThat(capturedRequest.awaitCompletion()).isTrue();
+  }
+
+  @Test
+  void shouldStartProcessInstancesWithResultWithBpmnProcessIdAndVersion() {
+    // given
+    final var mockResponse =
+        new ProcessInstanceResultRecord()
+            .setProcessDefinitionKey(123L)
+            .setBpmnProcessId("bpmnProcessId")
+            .setProcessInstanceKey(123L)
+            .setTenantId("tenantId");
+
+    when(processInstanceServices.startProcessInstanceWithResult(
+            any(ProcessInstanceStartRequest.class)))
+        .thenReturn(CompletableFuture.completedFuture(mockResponse));
+
+    final var request =
+        """
+        {
+            "bpmnProcessId": "bpmnProcessId",
+            "version": 1,
+            "awaitCompletion": true
+        }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_JSON)
+        .expectBody()
+        .json(EXPECTED_START_RESPONSE);
+
+    verify(processInstanceServices).startProcessInstanceWithResult(requestCaptor.capture());
+    final var capturedRequest = requestCaptor.getValue();
+    assertThat(capturedRequest.bpmnProcessId()).isEqualTo("bpmnProcessId");
+    assertThat(capturedRequest.version()).isEqualTo(1);
+  }
+
+  @Test
+  void shouldRejectStartProcessInstancesIfNoBpmnProcessIdOrProcessDefinitionKeyAreProvided() {
+    // given
+    final var request =
+        """
+        {
+            "version": 1,
+            "awaitCompletion": true
+        }""";
+
+    final var expectedBody =
+        """
+        {
+            "type":"about:blank",
+            "title":"INVALID_ARGUMENT",
+            "status":400,
+            "detail":"At least one of [bpmnProcessId, processDefinitionKey] is required.",
+            "instance":"/v2/process-instances"
+         }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isBadRequest()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+        .expectBody()
+        .json(expectedBody);
+  }
+
+  @Test
+  void shouldRejectStartProcessInstancesIfBothBpmnProcessIdOrProcessDefinitionKeyAreProvided() {
+    // given
+    final var request =
+        """
+        {
+            "bpmnProcessId": "bpmnProcessId",
+            "processDefinitionKey": 123,
+            "version": 1
+        }""";
+
+    final var expectedBody =
+        """
+        {
+            "type":"about:blank",
+            "title":"INVALID_ARGUMENT",
+            "status":400,
+            "detail":"Only one of [bpmnProcessId, processDefinitionKey] is required.",
+            "instance":"/v2/process-instances"
+         }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isBadRequest()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+        .expectBody()
+        .json(expectedBody);
+  }
+
+  @Test
+  void shouldRejectStartProcessInstancesIfBpmnProcessIdIsProvidedButNoVersion() {
+    // given
+    final var request =
+        """
+        {
+            "bpmnProcessId": "bpmnProcessId",
+            "awaitCompletion": true
+        }""";
+
+    final var expectedBody =
+        """
+        {
+            "type":"about:blank",
+            "title":"INVALID_ARGUMENT",
+            "status":400,
+            "detail":"If bpmnProcessId is provided, version is required.",
+            "instance":"/v2/process-instances"
+         }""";
+
+    // when / then
+    webClient
+        .post()
+        .uri(PROCESS_INSTANCES_START_URL)
+        .accept(MediaType.APPLICATION_JSON)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange()
+        .expectStatus()
+        .isBadRequest()
+        .expectHeader()
+        .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+        .expectBody()
+        .json(expectedBody);
   }
 }
