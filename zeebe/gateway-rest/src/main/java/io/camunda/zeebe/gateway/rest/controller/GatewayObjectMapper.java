9244c02d7b5bb87313ecbcdad7e2f925daf91dab
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2024-09-03 09:47:17 +0300

    refactor: copy autowired objectMapper to include global options

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/GatewayObjectMapper.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/GatewayObjectMapper.java
@@ -7,17 +7,14 @@
  */
 package io.camunda.zeebe.gateway.rest.controller;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import io.camunda.zeebe.gateway.protocol.rest.UserTaskSearchQueryRequest;
 import io.camunda.zeebe.gateway.protocol.rest.UserTaskUpdateRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
@@ -25,19 +22,9 @@ public class GatewayObjectMapper {
 
   private static ObjectMapper strictIntegerMapper;
 
-  private ObjectMapper strictIntegerObjectMapper() {
+  private ObjectMapper strictIntegerObjectMapper(final ObjectMapper objectMapper) {
     if (strictIntegerMapper == null) {
-      strictIntegerMapper =
-          Jackson2ObjectMapperBuilder.json()
-              .featuresToDisable(
-                  DeserializationFeature.ACCEPT_FLOAT_AS_INT,
-                  SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
-                  DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE,
-                  DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
-                  DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES)
-              .featuresToEnable(
-                  JsonParser.Feature.ALLOW_COMMENTS, SerializationFeature.INDENT_OUTPUT)
-              .build();
+      strictIntegerMapper = objectMapper.copy().disable(DeserializationFeature.ACCEPT_FLOAT_AS_INT);
     }
     return strictIntegerMapper;
   }
@@ -50,10 +37,10 @@ public class GatewayObjectMapper {
 
     bean.registerObjectMappersForType(
         UserTaskUpdateRequest.class,
-        m -> m.put(MediaType.APPLICATION_JSON, strictIntegerObjectMapper()));
+        m -> m.put(MediaType.APPLICATION_JSON, strictIntegerObjectMapper(objectMapper)));
     bean.registerObjectMappersForType(
         UserTaskSearchQueryRequest.class,
-        m -> m.put(MediaType.APPLICATION_JSON, strictIntegerObjectMapper()));
+        m -> m.put(MediaType.APPLICATION_JSON, strictIntegerObjectMapper(objectMapper)));
     return bean;
   }
 }
