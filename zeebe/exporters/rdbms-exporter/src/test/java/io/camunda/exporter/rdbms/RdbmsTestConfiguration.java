67fbf8eb585f617ff89ecd86a4e7ffbac48f06da
Patrick Schalk <patrick.wunderlich@holisticon.de>
2024-09-05 10:05:28 +0200

    feat: Init rdbms modul (incl liquibase setup) #21983
    - Initial insert of ProcessInstance into database with mybatis

---
+++ zeebe/exporters/rdbms-exporter/src/test/java/io/camunda/exporter/rdbms/RdbmsTestConfiguration.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.rdbms;
+
+import io.camunda.db.rdbms.RdbmsConfiguration;
+import io.camunda.zeebe.scheduler.ActorScheduler;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@EnableAutoConfiguration
+@Import({
+    RdbmsConfiguration.class
+})
+public class RdbmsTestConfiguration {
+
+  @MockBean
+  private ActorScheduler actorScheduler;
+
+}
