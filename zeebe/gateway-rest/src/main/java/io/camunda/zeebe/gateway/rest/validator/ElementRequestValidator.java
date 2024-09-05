aa25e255c50c5ed9ad61cdaa12b7b6c533dfd696
Nicola Puppa <nicola.puppa@camunda.com>
2024-09-05 10:55:16 +0200

    feat: add request validator

---
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/validator/ElementRequestValidator.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.rest.validator;
+
+import static io.camunda.zeebe.gateway.rest.validator.ErrorMessages.ERROR_MESSAGE_EMPTY_ATTRIBUTE;
+import static io.camunda.zeebe.gateway.rest.validator.ErrorMessages.ERROR_MESSAGE_INVALID_ATTRIBUTE_VALUE;
+import static io.camunda.zeebe.gateway.rest.validator.RequestValidator.validate;
+
+import io.camunda.zeebe.gateway.protocol.rest.VariableRequest;
+import java.util.Optional;
+import org.springframework.http.ProblemDetail;
+
+public class ElementRequestValidator {
+
+  public static Optional<ProblemDetail> validateVariableRequest(
+      final VariableRequest variableRequest) {
+    return validate(
+        violations -> {
+          if (variableRequest.getVariables() == null || variableRequest.getVariables().isEmpty()) {
+            violations.add(ERROR_MESSAGE_EMPTY_ATTRIBUTE.formatted("variables"));
+          }
+          if (variableRequest.getOperationReference() != null
+              && variableRequest.getOperationReference() < 1) {
+            violations.add(
+                ERROR_MESSAGE_INVALID_ATTRIBUTE_VALUE.formatted(
+                    "operationReference", variableRequest.getOperationReference(), "> 0"));
+          }
+        });
+  }
+}
