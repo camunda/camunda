fafdad932e62bc80b640a04431ed8f11e4d7804b
Ivan Karpov <ivan.karpov-ext@camunda.com>
2024-09-09 17:44:27 +0200

    feat: Implement Upgrade Foundation with code refactoring for ElasticSearch

---
+++ optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/db/DatabaseQueryWrapper.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.optimize.service.db;
+
+import org.elasticsearch.index.query.AbstractQueryBuilder;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
+
+// TODO this should eventully be non-nullable fields to guarantee that we always have both queries
+public record DatabaseQueryWrapper(AbstractQueryBuilder<?> esQuery, Query osQuery) {}
