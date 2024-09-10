f6ee09c5e9b2ada8f449cce5e70f44b7e54e035d
Houssain Barouni <houssain.barouni@camunda.com>
2024-09-10 16:32:53 +0200

    fix: support Bearer token with typ AT+JWT

--- tasklist/webapp/src/test/java/io/camunda/tasklist/webapp/security/oauth/IdentityOAuth2WebConfigurerTest.java
+++ tasklist/webapp/src/test/java/io/camunda/tasklist/webapp/security/oauth/IdentityOAuth2WebConfigurerTest.java
@@ -9,16 +9,25 @@ package io.camunda.tasklist.webapp.security.oauth;
 
 import static io.camunda.tasklist.webapp.security.oauth.IdentityOAuth2WebConfigurer.SPRING_SECURITY_OAUTH_2_RESOURCESERVER_JWT_JWK_SET_URI;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.*;
 
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
 import io.camunda.identity.sdk.IdentityConfiguration;
+import java.util.function.BiConsumer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.test.util.ReflectionTestUtils;
 
 class IdentityOAuth2WebConfigurerTest {
@@ -26,6 +35,8 @@ class IdentityOAuth2WebConfigurerTest {
   @Mock private Environment environment;
 
   @Mock private IdentityConfiguration identityConfiguration;
+
+  @Mock private IdentityJwt2AuthenticationTokenConverter jwtConverter;
 
   @InjectMocks private IdentityOAuth2WebConfigurer webConfigurer;
 
@@ -50,7 +61,47 @@ class IdentityOAuth2WebConfigurerTest {
     webConfigurer.configure(httpSecurity);
 
     // then
-    verify(httpSecurity, times(1)).oauth2ResourceServer(any());
+    // then
+    final var oauth2ResourceServerCustomizer =
+        captureFrom(
+            httpSecurity,
+            Customizer.class,
+            (o, t) -> {
+              try {
+                o.oauth2ResourceServer(t);
+              } catch (final Exception e) {
+                fail(e);
+              }
+            });
+    final var serverConfigurer = mock(OAuth2ResourceServerConfigurer.class);
+    when(serverConfigurer.authenticationEntryPoint(any())).thenReturn(serverConfigurer);
+    oauth2ResourceServerCustomizer.customize(serverConfigurer);
+    final var jwtCustomizer =
+        captureFrom(serverConfigurer, Customizer.class, OAuth2ResourceServerConfigurer::jwt);
+    final OAuth2ResourceServerConfigurer<HttpSecurity>.JwtConfigurer jwtConfigurer =
+        mock(OAuth2ResourceServerConfigurer.JwtConfigurer.class);
+    when(jwtConfigurer.jwtAuthenticationConverter(jwtConverter)).thenReturn(jwtConfigurer);
+    jwtCustomizer.customize(jwtConfigurer);
+
+    final JwtDecoder jwtDecoder =
+        captureFrom(
+            jwtConfigurer, JwtDecoder.class, OAuth2ResourceServerConfigurer.JwtConfigurer::decoder);
+    assertThat(jwtDecoder).isInstanceOf(NimbusJwtDecoder.class);
+
+    final NimbusJwtDecoder nimbusJwtDecoder = (NimbusJwtDecoder) jwtDecoder;
+
+    // Use reflection to verify JWSTypeVerifier configuration
+    final var processor = ReflectionTestUtils.getField(nimbusJwtDecoder, "jwtProcessor");
+    final var jwsTypeVerifier = ReflectionTestUtils.invokeMethod(processor, "getJWSTypeVerifier");
+
+    assertThat(jwsTypeVerifier).isNotNull();
+    assertThat(jwsTypeVerifier).isInstanceOf(DefaultJOSEObjectTypeVerifier.class);
+
+    final var joseVerifier = (DefaultJOSEObjectTypeVerifier<?>) jwsTypeVerifier;
+
+    // Ensure that both jwt and at+jwt types are being verified
+    assertThat(joseVerifier.getAllowedTypes())
+        .containsExactlyInAnyOrder(new JOSEObjectType("jwt"), new JOSEObjectType("at+jwt"));
   }
 
   @Test
@@ -92,5 +143,12 @@ class IdentityOAuth2WebConfigurerTest {
         (String) (ReflectionTestUtils.invokeGetterMethod(webConfigurer, "getJwkSetUriProperty"));
 
     assertThat(result).isEqualTo("http://localhost:1111");
+  }
+
+  private <O, T> T captureFrom(
+      final O from, final Class<T> tClass, final BiConsumer<O, T> consumer) {
+    final var argumentCaptor = ArgumentCaptor.forClass(tClass);
+    consumer.accept(verify(from, times(1)), argumentCaptor.capture());
+    return argumentCaptor.getValue();
   }
 }
