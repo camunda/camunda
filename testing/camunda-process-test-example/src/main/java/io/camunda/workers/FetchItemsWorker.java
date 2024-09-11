eeac7a99a0be8486e6d51bb9de1880359c6b3bb0
Philipp Ossler <philipp.ossler@gmail.com>
2024-09-11 14:53:15 +0200

    feat: Add example project for CPT
    Add a new example project to demonstrate the usage of CPT. Focus on the integration with the Spring SDK. Include
    different test cases for unit + integration testing and connectors.

---
+++ testing/camunda-process-test-example/src/main/java/io/camunda/workers/FetchItemsWorker.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.workers;
+
+import io.camunda.services.InventoryService;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.spring.client.annotation.JobWorker;
+import io.camunda.zeebe.spring.client.annotation.Variable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FetchItemsWorker {
+
+  private final InventoryService service;
+
+  public FetchItemsWorker(final InventoryService service) {
+    this.service = service;
+  }
+
+  @JobWorker(type = "fetch-items")
+  public void handleJob(final ActivatedJob job, @Variable("order_id") final String orderId) {
+    service.fetchItems(orderId);
+  }
+}
