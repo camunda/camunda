54e27205607739ac4b40ca9ef4320961c262d2d0
esraagamal6 <esraa-gamal@hotmail.com>
2024-09-12 09:12:05 +0200

    fix:Update testcase

--- tasklist/client/e2e/tests/task-details.spec.ts
+++ tasklist/client/e2e/tests/task-details.spec.ts
@@ -9,7 +9,6 @@
 import {expect} from '@playwright/test';
 import {test} from '@/test-fixtures';
 import {createInstances, deploy} from '@/utils/zeebeClient';
-import {sleep} from '@/utils/sleep';
 
 test.afterAll(async ({resetData}) => {
   await resetData();
@@ -566,15 +565,8 @@ test.describe('task details page', () => {
     await tasksPage.openTask('Employee Details');
     await expect(tasksPage.detailsNav).toBeVisible();
     await tasksPage.assignToMeButton.click();
-    // try {
-    //   await expect(taskFormView.nameInput).toBeVisible({timeout: 60000});
-    // } catch (error) {
-    //   console.error('Form not loaded error:', error);
-    //   await sleep(5000);
     await page.reload();
     await expect(taskFormView.nameInput).toBeVisible();
-    //}
-
     await taskFormView.nameInput.fill('Ben');
     await taskFormView.selectDropdownValue('marketing');
     await tasksPage.completeTaskButton.click();
