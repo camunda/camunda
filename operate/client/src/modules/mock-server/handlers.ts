162d9bea78dc0d13eea5e0eb95cef50bf6710cce
Patrick Dehn <patrick.dehn@camunda.com>
2024-09-13 15:45:24 +0200

    refactor: remove mocked version tag handlers

--- operate/client/src/modules/mock-server/handlers.ts
+++ operate/client/src/modules/mock-server/handlers.ts
@@ -6,48 +6,8 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {IS_VERSION_TAG_ENABLED} from 'modules/feature-flags';
-import {RequestHandler, rest} from 'msw';
+import {RequestHandler} from 'msw';
 
-const processVersionTagHandler = [
-  rest.get('/api/processes/:processId', async (req, res, ctx) => {
-    const response = await ctx.fetch(req);
-    const body = await response.json();
-
-    return res(
-      ctx.json({
-        ...body,
-        versionTag: 'myVersionTag',
-      }),
-    );
-  }),
-];
-
-const processInstancesVersionTagHandler = [
-  rest.post('/api/processes/grouped', async (req, res, ctx) => {
-    const response = await ctx.fetch(req);
-    const processDefinitions = await response.json();
-
-    const processesDefinitionsWithVersionTags = processDefinitions.map(
-      (processDefinition: any) => {
-        const processesWithVersionTag = processDefinition.processes.map(
-          (process: any) => {
-            if (process.bpmnProcessId === 'complexProcess') {
-              return {...process, versionTag: 'myVersionTag'};
-            } else {
-              return {...process, versionTag: null};
-            }
-          },
-        );
-        return {...processDefinition, processes: processesWithVersionTag};
-      },
-    );
-    return res(ctx.json(processesDefinitionsWithVersionTags));
-  }),
-];
-
-const handlers: RequestHandler[] = IS_VERSION_TAG_ENABLED
-  ? [...processVersionTagHandler, ...processInstancesVersionTagHandler]
-  : [];
+const handlers: RequestHandler[] = [];
 
 export {handlers};
