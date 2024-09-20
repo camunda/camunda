4742c3f76fe608f0e6f35b43ecbcb29c373d7afa
Ralf Puchert <ralf.puchert@camunda.com>
2024-09-20 12:12:10 +0200

    feat: add Process definition sort
    Related with #20652

--- search/search-domain/src/main/java/io/camunda/search/sort/ProcessDefinitionSort.java
+++ search/search-domain/src/main/java/io/camunda/search/sort/ProcessDefinitionSort.java
@@ -20,6 +20,42 @@ public record ProcessDefinitionSort(List<FieldSorting> orderings) implements Sor
   public static final class Builder
       extends SortOption.AbstractBuilder<ProcessDefinitionSort.Builder>
       implements ObjectBuilder<ProcessDefinitionSort> {
+
+    public Builder processDefinitionKey() {
+      currentOrdering = new FieldSorting("key", null);
+      return this;
+    }
+
+    public Builder processName() {
+      currentOrdering = new FieldSorting("name", null);
+      return this;
+    }
+
+    public Builder processVersion() {
+      currentOrdering = new FieldSorting("version", null);
+      return this;
+    }
+
+    public Builder processVersionTag() {
+      currentOrdering = new FieldSorting("versionTag", null);
+      return this;
+    }
+
+    public Builder bpmnProcessId() {
+      currentOrdering = new FieldSorting("bpmnProcessId", null);
+      return this;
+    }
+
+    public Builder tenantId() {
+      currentOrdering = new FieldSorting("tenantId", null);
+      return this;
+    }
+
+    public Builder formKey() {
+      currentOrdering = new FieldSorting("formKey", null);
+      return this;
+    }
+
     @Override
     protected ProcessDefinitionSort.Builder self() {
       return this;
