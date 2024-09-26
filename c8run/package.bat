c725aebc3af59c8455f8be803aea475675286ed1
Jesse Simpson <jesse.simpson@camunda.com>
2024-09-26 09:05:48 -0400

    fix: the change was meant to be on windows package.bat, not package.sh

--- c8run/package.bat
+++ c8run/package.bat
@@ -33,7 +33,7 @@ if not exist "%connectorsFileName%" (
 
 go build -C windows -trimpath -o ..\c8run.exe
 
-tar -czf camunda8-run-%CAMUNDA_VERSION%-windows-x86_64.tar.gz ^
+tar -a -cf camunda8-run-%CAMUNDA_VERSION%-windows-x86_64.tar.gz ^
   -C ..\ ^
   c8run\README.md ^
   c8run\connectors-application.properties ^
