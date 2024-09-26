78cb2c5deab1507b9dab8c5a5dbbafa81bd89a4a
SvetlanaDorokhova <svetlana.dorokhova@camunda.com>
2024-09-26 10:47:19 +0200

    feat: copy templates and indices and ipmlement configurator class

---
+++ webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/template/AbstractTemplateDescriptor.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.webapps.schema.descriptors.operate.template;
+
+import io.camunda.webapps.schema.descriptors.IndexTemplateDescriptor;
+import io.camunda.webapps.schema.descriptors.operate.index.AbstractIndexDescriptor;
+import java.util.List;
+
+public abstract class AbstractTemplateDescriptor extends AbstractIndexDescriptor
+    implements IndexTemplateDescriptor {
+
+  private static final String SCHEMA_CREATE_TEMPLATE_JSON_ELASTICSEARCH =
+      SCHEMA_FOLDER_ELASTICSEARCH + "/template/operate-%s.json";
+  private static final String SCHEMA_CREATE_TEMPLATE_JSON_OPENSEARCH =
+      SCHEMA_FOLDER_OPENSEARCH + "/template/operate-%s.json";
+
+  public AbstractTemplateDescriptor(final String indexPrefix, final boolean isElasticsearch) {
+    super(indexPrefix, isElasticsearch);
+  }
+
+  @Override
+  public String getIndexPattern() {
+    return getFullQualifiedName() + "*";
+  }
+
+  @Override
+  public String getTemplateName() {
+    return getFullQualifiedName() + "template";
+  }
+
+  @Override
+  public List<String> getComposedOf() {
+    return List.of(String.format("%s_template", indexPrefix));
+  }
+
+  @Override
+  public String getMappingsClasspathFilename() {
+    if (isElasticsearch) {
+      return String.format(SCHEMA_CREATE_TEMPLATE_JSON_ELASTICSEARCH, getIndexName());
+    } else {
+      return String.format(SCHEMA_CREATE_TEMPLATE_JSON_OPENSEARCH, getIndexName());
+    }
+  }
+
+  @Override
+  public String getAllVersionsIndexNameRegexPattern() {
+    return String.format("%s-%s-\\d.*", indexPrefix, getIndexName());
+  }
+
+}
