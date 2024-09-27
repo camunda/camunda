94c6ea67c0d3627b89688d010db9d70c25f8a080
SvetlanaDorokhova <svetlana.dorokhova@camunda.com>
2024-09-27 13:11:04 +0200

    fix: different fixes for schema creation from exporter

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/schema/ElasticsearchEngineClient.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/schema/ElasticsearchEngineClient.java
@@ -250,7 +250,7 @@ public class ElasticsearchEngineClient implements SearchEngineClient {
   }
 
   private InputStream getResourceAsStream(final String classpathFileName) {
-    return Thread.currentThread().getContextClassLoader().getResourceAsStream(classpathFileName);
+    return getClass().getResourceAsStream(classpathFileName);
   }
 
   private PutIndexTemplateRequest putIndexTemplateRequest(
