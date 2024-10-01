76aba4b15c2880ee880a87ce8e8711db98f66fbb
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-10-01 15:45:23 +0200

    feat: adjust filters

--- search/search-domain/src/main/java/io/camunda/search/filter/VariableFilter.java
+++ search/search-domain/src/main/java/io/camunda/search/filter/VariableFilter.java
@@ -20,20 +20,20 @@ public final record VariableFilter(
     List<VariableValueFilter> variableFilters,
     List<Long> scopeKeys,
     List<Long> processInstanceKeys,
-    boolean orConditions,
-    boolean onlyRuntimeVariables)
+    List<Long> variableKeys,
+    List<String> tenantIds,
+    boolean isTruncated)
     implements FilterBase {
 
   public static final class Builder implements ObjectBuilder<VariableFilter> {
     private List<VariableValueFilter> variableFilters;
     private List<Long> scopeKeys;
     private List<Long> processInstanceKeys;
-    private boolean orConditions;
-    private boolean
-        onlyRuntimeVariables; // internally used to restric the search to variables for running
+    private List<Long> variableKeys;
+    private List<String> tenantIds;
+    private boolean isTruncated;
 
-    // tasks/processes to improve the performance
-
+    // Builder for variable filters
     public Builder variable(final List<VariableValueFilter> values) {
       variableFilters = addValuesToList(variableFilters, values);
       return this;
@@ -48,6 +48,7 @@ public final record VariableFilter(
       return variable(fn.apply(new VariableValueFilter.Builder()).build());
     }
 
+    // Builder for scope keys
     public Builder scopeKeys(final Long value, final Long... values) {
       return scopeKeys(collectValues(value, values));
     }
@@ -57,6 +58,7 @@ public final record VariableFilter(
       return this;
     }
 
+    // Builder for process instance keys
     public Builder processInstanceKeys(final Long value, final Long... values) {
       return processInstanceKeys(collectValues(value, values));
     }
@@ -66,13 +68,29 @@ public final record VariableFilter(
       return this;
     }
 
-    public Builder orConditions(final boolean orConditions) {
-      this.orConditions = orConditions;
+    // Builder for variable keys
+    public Builder variableKeys(final Long value, final Long... values) {
+      return variableKeys(collectValues(value, values));
+    }
+
+    public Builder variableKeys(final List<Long> values) {
+      variableKeys = addValuesToList(variableKeys, values);
       return this;
     }
 
-    public Builder onlyRuntimeVariables(final boolean onlyRuntimeVariables) {
-      this.onlyRuntimeVariables = onlyRuntimeVariables;
+    // Builder for tenant IDs
+    public Builder tenantIds(final String value, final String... values) {
+      return tenantIds(collectValues(value, values));
+    }
+
+    public Builder tenantIds(final List<String> values) {
+      tenantIds = addValuesToList(tenantIds, values);
+      return this;
+    }
+
+    // Builder for isTruncated
+    public Builder isTruncated(final boolean value) {
+      isTruncated = value;
       return this;
     }
 
@@ -82,8 +100,10 @@ public final record VariableFilter(
           Objects.requireNonNullElseGet(variableFilters, Collections::emptyList),
           Objects.requireNonNullElseGet(scopeKeys, Collections::emptyList),
           Objects.requireNonNullElseGet(processInstanceKeys, Collections::emptyList),
-          orConditions,
-          onlyRuntimeVariables);
+          Objects.requireNonNullElseGet(variableKeys, Collections::emptyList),
+          Objects.requireNonNullElseGet(tenantIds, Collections::emptyList),
+          isTruncated
+      );
     }
   }
 }
