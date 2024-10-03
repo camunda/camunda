85d379f690216587e7b5e21a3b15cdf18cf39d04
remco.westerhoud <remcowesterhoud@msn.com>
2024-10-03 10:13:35 +0200

    refactor: remove unused fields
    These fields are not used as the rejection/response is abstracted away by the CommandProcessor this class implements.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/streamprocessor/AuthorizableCommandProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/streamprocessor/AuthorizableCommandProcessor.java
@@ -13,9 +13,6 @@ import io.camunda.zeebe.engine.processing.identity.AuthorizationCheckBehavior.Au
 import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessor.ProcessingError;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
-import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
-import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedResponseWriter;
-import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.immutable.ProcessingState;
 import io.camunda.zeebe.protocol.impl.record.UnifiedRecordValue;
 import io.camunda.zeebe.protocol.record.RejectionType;
@@ -36,20 +33,15 @@ public class AuthorizableCommandProcessor<T extends UnifiedRecordValue, Resource
   public static final String UNAUTHORIZED_ERROR_MESSAGE =
       "Unauthorized to perform operation '%s' on resource '%s'";
   private final AuthorizationCheckBehavior authorizationCheckBehavior;
-  private final TypedRejectionWriter rejectionWriter;
-  private final TypedResponseWriter responseWriter;
   private final Authorizable<T, Resource> delegate;
 
   public AuthorizableCommandProcessor(
       final ProcessingState processingState,
-      final Writers writers,
       final EngineConfiguration engineConfig,
       final Authorizable<T, Resource> delegate) {
     authorizationCheckBehavior =
         new AuthorizationCheckBehavior(
             processingState.getAuthorizationState(), processingState.getUserState(), engineConfig);
-    rejectionWriter = writers.rejection();
-    responseWriter = writers.response();
     this.delegate = delegate;
   }
 
