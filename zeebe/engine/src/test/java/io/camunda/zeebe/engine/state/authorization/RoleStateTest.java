434381ba3387fe7744931e51871b22148077449d
Nicola Puppa <nicola.puppa@camunda.com>
2024-10-11 11:10:49 +0200

    test: verify the behaviour of adding an entity into a role record

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/authorization/RoleStateTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/authorization/RoleStateTest.java
@@ -13,6 +13,7 @@ import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
 import io.camunda.zeebe.engine.state.mutable.MutableRoleState;
 import io.camunda.zeebe.engine.util.ProcessingStateExtension;
 import io.camunda.zeebe.protocol.impl.record.value.authorization.RoleRecord;
+import io.camunda.zeebe.protocol.record.value.EntityType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -84,5 +85,28 @@ public class RoleStateTest {
 
     final var roleKeyByName = roleState.getRoleKeyByName(updatedName);
     assertThat(roleKeyByName).isEqualTo(roleKey);
+  }
+
+  @Test
+  void shouldAddEntity() {
+    // given
+    final long roleKey = 1L;
+    final String roleName = "foo";
+    final var roleRecord = new RoleRecord().setRoleKey(roleKey).setName(roleName);
+    roleState.createRole(roleRecord);
+
+    // when
+    roleRecord.setEntityKey(1L).setEntityType(EntityType.USER);
+    roleState.addEntity(roleRecord);
+
+    // then
+    final var persistedRole = roleState.getRole(roleKey).get();
+    assertThat(persistedRole.getRoleKey()).isEqualTo(roleKey);
+    assertThat(persistedRole.getName()).isEqualTo(roleName);
+    assertThat(persistedRole.getEntityKey()).isEqualTo(1L);
+    assertThat(persistedRole.getEntityType()).isEqualTo(EntityType.USER);
+
+    final var entityType = roleState.getEntityType(roleKey, 1L);
+    assertThat(entityType).isEqualTo(EntityType.USER);
   }
 }
