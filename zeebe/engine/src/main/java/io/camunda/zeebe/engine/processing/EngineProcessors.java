40860c923421db8d4b538825e869c15f528721ec
remco.westerhoud <remcowesterhoud@msn.com>
2024-10-15 15:03:12 +0200

    feat: check authorization on clock pin/reset

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/EngineProcessors.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/EngineProcessors.java
@@ -227,7 +227,12 @@ public final class EngineProcessors {
         config);
 
     ClockProcessors.addClockProcessors(
-        typedRecordProcessors, writers, keyGenerator, clock, commandDistributionBehavior);
+        typedRecordProcessors,
+        writers,
+        keyGenerator,
+        clock,
+        commandDistributionBehavior,
+        authCheckBehavior);
 
     AuthorizationProcessors.addAuthorizationProcessors(
         keyGenerator,
