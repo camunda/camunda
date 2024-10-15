fe21702e6dc35eba30bc8b3ec07eb351bc7cf62e
Oleksandr Kriuchenko <oleksandr.kriuchenko-ext@camunda.com>
2024-10-15 16:27:51 +0200

    feat: Implement GroupByIncidentFlowNode for OS #12918

--- optimize/backend/src/main/java/io/camunda/optimize/service/db/os/report/interpreter/groupby/process/flownode/GroupByIncidentFlowNodeInterpreterOS.java
+++ optimize/backend/src/main/java/io/camunda/optimize/service/db/os/report/interpreter/groupby/process/flownode/GroupByIncidentFlowNodeInterpreterOS.java
@@ -5,63 +5,69 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.optimize.service.db.es.report.interpreter.groupby.process.flownode;
+package io.camunda.optimize.service.db.os.report.interpreter.groupby.process.flownode;
 
-import static io.camunda.optimize.service.db.es.filter.util.IncidentFilterQueryUtilES.createIncidentAggregationFilter;
+import static io.camunda.optimize.service.db.os.externalcode.client.dsl.AggregationDSL.termAggregation;
 import static io.camunda.optimize.service.db.report.plan.process.ProcessGroupBy.PROCESS_GROUP_BY_INCIDENT_FLOW_NODE;
 import static io.camunda.optimize.service.db.schema.index.ProcessInstanceIndex.INCIDENTS;
 import static io.camunda.optimize.service.db.schema.index.ProcessInstanceIndex.INCIDENT_ACTIVITY_ID;
 
-import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
-import co.elastic.clients.elasticsearch._types.aggregations.FilterAggregate;
-import co.elastic.clients.elasticsearch._types.aggregations.NestedAggregate;
-import co.elastic.clients.elasticsearch._types.aggregations.StringTermsAggregate;
-import co.elastic.clients.elasticsearch._types.aggregations.StringTermsBucket;
-import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
-import co.elastic.clients.elasticsearch.core.search.ResponseBody;
 import io.camunda.optimize.dto.optimize.query.report.single.process.ProcessReportDataDto;
 import io.camunda.optimize.service.DefinitionService;
-import io.camunda.optimize.service.db.es.report.interpreter.distributedby.process.ProcessDistributedByInterpreterFacadeES;
-import io.camunda.optimize.service.db.es.report.interpreter.view.process.ProcessViewInterpreterFacadeES;
+import io.camunda.optimize.service.db.os.report.filter.util.IncidentFilterQueryUtilOS;
+import io.camunda.optimize.service.db.os.report.interpreter.RawResult;
+import io.camunda.optimize.service.db.os.report.interpreter.distributedby.process.ProcessDistributedByInterpreterFacadeOS;
+import io.camunda.optimize.service.db.os.report.interpreter.view.process.ProcessViewInterpreterFacadeOS;
 import io.camunda.optimize.service.db.report.ExecutionContext;
 import io.camunda.optimize.service.db.report.groupby.flownode.GroupByIncidentFlowNodeInterpreterHelper;
 import io.camunda.optimize.service.db.report.plan.process.ProcessExecutionPlan;
 import io.camunda.optimize.service.db.report.plan.process.ProcessGroupBy;
 import io.camunda.optimize.service.db.report.result.CompositeCommandResult;
 import io.camunda.optimize.service.util.configuration.ConfigurationService;
-import io.camunda.optimize.service.util.configuration.condition.ElasticSearchCondition;
+import io.camunda.optimize.service.util.configuration.condition.OpenSearchCondition;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import org.opensearch.client.opensearch._types.aggregations.Aggregation;
+import org.opensearch.client.opensearch._types.aggregations.FilterAggregate;
+import org.opensearch.client.opensearch._types.aggregations.NestedAggregate;
+import org.opensearch.client.opensearch._types.aggregations.StringTermsAggregate;
+import org.opensearch.client.opensearch._types.aggregations.StringTermsBucket;
+import org.opensearch.client.opensearch._types.query_dsl.Query;
+import org.opensearch.client.opensearch.core.SearchResponse;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
 @Component
-@Conditional(ElasticSearchCondition.class)
-public class GroupByIncidentFlowNodeInterpreterES extends AbstractGroupByFlowNodeInterpreterES {
-
+@Conditional(OpenSearchCondition.class)
+public class GroupByIncidentFlowNodeInterpreterOS extends AbstractGroupByFlowNodeInterpreterOS {
   private static final String NESTED_INCIDENT_AGGREGATION = "nestedIncidentAggregation";
   private static final String GROUPED_BY_FLOW_NODE_ID_AGGREGATION =
       "groupedByFlowNodeIdAggregation";
   private static final String FILTERED_INCIDENT_AGGREGATION = "filteredIncidentAggregation";
-  final ProcessDistributedByInterpreterFacadeES distributedByInterpreter;
-  final ProcessViewInterpreterFacadeES viewInterpreter;
+  final ProcessDistributedByInterpreterFacadeOS distributedByInterpreter;
+  final ProcessViewInterpreterFacadeOS viewInterpreter;
   private final ConfigurationService configurationService;
   private final GroupByIncidentFlowNodeInterpreterHelper helper;
   private final DefinitionService definitionService;
 
-  public GroupByIncidentFlowNodeInterpreterES(
+  public GroupByIncidentFlowNodeInterpreterOS(
+      final ProcessDistributedByInterpreterFacadeOS distributedByInterpreter,
+      final ProcessViewInterpreterFacadeOS viewInterpreter,
       final ConfigurationService configurationService,
       final GroupByIncidentFlowNodeInterpreterHelper helper,
-      final DefinitionService definitionService,
-      final ProcessDistributedByInterpreterFacadeES distributedByInterpreter,
-      final ProcessViewInterpreterFacadeES viewInterpreter) {
+      final DefinitionService definitionService) {
+    this.distributedByInterpreter = distributedByInterpreter;
+    this.viewInterpreter = viewInterpreter;
     this.configurationService = configurationService;
     this.helper = helper;
     this.definitionService = definitionService;
-    this.distributedByInterpreter = distributedByInterpreter;
-    this.viewInterpreter = viewInterpreter;
+  }
+
+  @Override
+  protected DefinitionService getDefinitionService() {
+    return definitionService;
   }
 
   @Override
@@ -70,45 +76,35 @@ public class GroupByIncidentFlowNodeInterpreterES extends AbstractGroupByFlowNod
   }
 
   @Override
-  public Map<String, Aggregation.Builder.ContainerBuilder> createAggregation(
-      final BoolQuery boolQuery,
+  public Map<String, Aggregation> createAggregation(
+      final Query query,
       final ExecutionContext<ProcessReportDataDto, ProcessExecutionPlan> context) {
-    final Aggregation.Builder.ContainerBuilder builder =
-        new Aggregation.Builder().nested(n -> n.path(INCIDENTS));
-    builder.aggregations(
-        FILTERED_INCIDENT_AGGREGATION,
-        Aggregation.of(
-            a ->
-                a.filter(
-                        f ->
-                            f.bool(
-                                createIncidentAggregationFilter(
-                                        context.getReportData(), definitionService)
-                                    .build()))
-                    .aggregations(
-                        GROUPED_BY_FLOW_NODE_ID_AGGREGATION,
-                        Aggregation.of(
-                            a1 -> {
-                              final Aggregation.Builder.ContainerBuilder terms =
-                                  a1.terms(
-                                      t ->
-                                          t.field(INCIDENTS + "." + INCIDENT_ACTIVITY_ID)
-                                              .size(
-                                                  configurationService
-                                                      .getElasticSearchConfiguration()
-                                                      .getAggregationBucketLimit()));
-                              getDistributedByInterpreter()
-                                  .createAggregations(context, boolQuery)
-                                  .forEach((k, v) -> terms.aggregations(k, v.build()));
-                              return terms;
-                            }))));
-    return Map.of(NESTED_INCIDENT_AGGREGATION, builder);
+    final int size =
+        configurationService.getElasticSearchConfiguration().getAggregationBucketLimit();
+    final Aggregation groupedByFlowNodeIdAggregation =
+        new Aggregation.Builder()
+            .terms(termAggregation(INCIDENTS + "." + INCIDENT_ACTIVITY_ID, size))
+            .aggregations(getDistributedByInterpreter().createAggregations(context, query))
+            .build();
+    final Aggregation filteredIncidentAggregation =
+        new Aggregation.Builder()
+            .filter(
+                IncidentFilterQueryUtilOS.createIncidentAggregationFilterQuery(
+                    context.getReportData(), definitionService))
+            .aggregations(GROUPED_BY_FLOW_NODE_ID_AGGREGATION, groupedByFlowNodeIdAggregation)
+            .build();
+    final Aggregation aggregation =
+        new Aggregation.Builder()
+            .nested(n -> n.path(INCIDENTS))
+            .aggregations(FILTERED_INCIDENT_AGGREGATION, filteredIncidentAggregation)
+            .build();
+    return Map.of(NESTED_INCIDENT_AGGREGATION, aggregation);
   }
 
   @Override
   public void addQueryResult(
       final CompositeCommandResult compositeCommandResult,
-      final ResponseBody<?> response,
+      final SearchResponse<RawResult> response,
       final ExecutionContext<ProcessReportDataDto, ProcessExecutionPlan> context) {
     final NestedAggregate nestedAgg =
         response.aggregations().get(NESTED_INCIDENT_AGGREGATION).nested();
@@ -120,7 +116,7 @@ public class GroupByIncidentFlowNodeInterpreterES extends AbstractGroupByFlowNod
     final Map<String, String> flowNodeNames = helper.getFlowNodeNames(context.getReportData());
     final List<CompositeCommandResult.GroupByResult> groupedData = new ArrayList<>();
     for (final StringTermsBucket flowNodeBucket : groupedByFlowNodeId.buckets().array()) {
-      final String flowNodeKey = flowNodeBucket.key().stringValue();
+      final String flowNodeKey = flowNodeBucket.key();
       if (flowNodeNames.containsKey(flowNodeKey)) {
         final List<CompositeCommandResult.DistributedByResult> singleResult =
             getDistributedByInterpreter()
@@ -138,17 +134,12 @@ public class GroupByIncidentFlowNodeInterpreterES extends AbstractGroupByFlowNod
   }
 
   @Override
-  public ProcessDistributedByInterpreterFacadeES getDistributedByInterpreter() {
+  protected ProcessDistributedByInterpreterFacadeOS getDistributedByInterpreter() {
     return distributedByInterpreter;
   }
 
   @Override
-  public ProcessViewInterpreterFacadeES getViewInterpreter() {
+  protected ProcessViewInterpreterFacadeOS getViewInterpreter() {
     return viewInterpreter;
-  }
-
-  @Override
-  public DefinitionService getDefinitionService() {
-    return definitionService;
   }
 }
