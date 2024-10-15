849075ae919cd999d0d06cfa68d08f9e74b6fe21
Kristin Komschow <162317489+kristinkomschow@users.noreply.github.com>
2024-10-15 09:32:11 +0200

    fix: allow for setting custom CSP headers in Operate SM
    - removed DEFAULT_SAAS_SECURITY_POLICY as default for the contentSecurity
    to allow for more complex logic when setting it
    - BaseWebConfigurer now sets the contentSecurity to either default values for SaaS or SM (depending on environment)
    or uses a custom CSP header if one is configured (for both SaaS and SM)
    
    (cherry picked from commit 4eaaa23693bb122bfd022a605b04d8daafefb860)

--- operate/webapp/src/main/java/io/camunda/operate/webapp/security/BaseWebConfigurer.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/security/BaseWebConfigurer.java
@@ -7,7 +7,6 @@
  */
 package io.camunda.operate.webapp.security;
 
-import static io.camunda.operate.property.WebSecurityProperties.DEFAULT_SM_SECURITY_POLICY;
 import static io.camunda.operate.webapp.security.OperateURIs.*;
 import static org.apache.http.entity.ContentType.APPLICATION_JSON;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
@@ -25,7 +24,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.actuate.logging.LoggersEndpoint;
 import org.springframework.context.annotation.Bean;
@@ -118,11 +116,7 @@ public abstract class BaseWebConfigurer {
   protected void applySecurityHeadersSettings(final HttpSecurity http) throws Exception {
     final WebSecurityProperties webSecurityConfig = operateProperties.getWebSecurity();
 
-    // Only SaaS has CloudProperties
-    final String policyDirectives =
-        (operateProperties.getCloud().getClusterId() == null)
-            ? DEFAULT_SM_SECURITY_POLICY
-            : webSecurityConfig.getContentSecurityPolicy();
+    final String policyDirectives = getContentSecurityPolicy();
 
     http.headers(
         headers -> {
@@ -139,6 +133,17 @@ public abstract class BaseWebConfigurer {
                             webSecurityConfig.getHttpStrictTransportSecurityIncludeSubDomains());
                   });
         });
+  }
+
+  protected String getContentSecurityPolicy() {
+    if (operateProperties.getCloud().getClusterId() == null) {
+      return (webSecurityProperties.getContentSecurityPolicy() == null)
+          ? webSecurityProperties.DEFAULT_SM_SECURITY_POLICY
+          : webSecurityProperties.getContentSecurityPolicy();
+    }
+    return (webSecurityProperties.getContentSecurityPolicy() == null)
+        ? webSecurityProperties.DEFAULT_SAAS_SECURITY_POLICY
+        : webSecurityProperties.getContentSecurityPolicy();
   }
 
   protected void applySecurityFilterSettings(final HttpSecurity http) throws Exception {
