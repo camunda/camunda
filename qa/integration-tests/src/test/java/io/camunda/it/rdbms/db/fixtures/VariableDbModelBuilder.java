b94f848315b5f08499fff2481b3b26536bdd0432
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2024-10-17 17:27:53 +0200

    feat: Implement support for variables to rdbms module #23273

---
+++ qa/integration-tests/src/test/java/io/camunda/it/rdbms/db/fixtures/VariableDbModelBuilder.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.rdbms.db.fixtures;
+
+import io.camunda.db.rdbms.write.domain.VariableDbModel;
+
+public class VariableDbModelBuilder {
+
+  private Long key;
+  private String name;
+  private String value;
+  private Long scopeKey;
+  private Long processInstanceKey;
+  private String tenantId;
+
+  // Public constructor to initialize the builder
+  public VariableDbModelBuilder() {}
+
+  // Builder methods for each field
+  public VariableDbModelBuilder key(final Long key) {
+    this.key = key;
+    return this;
+  }
+
+  public VariableDbModelBuilder value(final String value) {
+    this.value = value;
+    return this;
+  }
+
+  public VariableDbModelBuilder name(final String name) {
+    this.name = name;
+    return this;
+  }
+
+  public VariableDbModelBuilder scopeKey(final Long scopeKey) {
+    this.scopeKey = scopeKey;
+    return this;
+  }
+
+  public VariableDbModelBuilder processInstanceKey(final Long processInstanceKey) {
+    this.processInstanceKey = processInstanceKey;
+    return this;
+  }
+
+  public VariableDbModelBuilder tenantId(final String tenantId) {
+    this.tenantId = tenantId;
+    return this;
+  }
+
+  // Build method to create the record
+  public VariableDbModel build() {
+    return new VariableDbModel(key, name, value, scopeKey, processInstanceKey, tenantId);
+  }
+}
