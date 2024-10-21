276af30f7af37c120c45b93f1d336adae29d4f84
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-10-21 16:28:50 +0200

    feat: add the conversion to taskResponse

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/mapper/TaskMapper.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/mapper/TaskMapper.java
@@ -117,7 +117,11 @@ public class TaskMapper {
         .setCompletionDate(taskDTO.getCompletionTime())
         .setAssignee(taskDTO.getAssignee())
         .setTaskState(taskDTO.getTaskState())
-        .setFormKey(taskDTO.getFormKey())
+        .setFormKey(
+            taskDTO.getExternalFormReference() == null
+                    || taskDTO.getExternalFormReference().isBlank()
+                ? taskDTO.getFormKey()
+                : taskDTO.getExternalFormReference())
         .setFormId(taskDTO.getFormId())
         .setFormVersion(taskDTO.getFormVersion())
         .setIsFormEmbedded(taskDTO.getIsFormEmbedded())
