8cb6077bb6d465a71f7d7a6125cc57fa6b84b70b
berkaycanbc <berkay.can@camunda.com>
2024-10-24 15:58:48 +0300

    refactor: simplify if check
    (cherry picked from commit 1129c2063e3e581962ab24ec87abfc7889786e81)

--- engine/src/main/java/io/camunda/zeebe/engine/Engine.java
+++ engine/src/main/java/io/camunda/zeebe/engine/Engine.java
@@ -133,7 +133,7 @@ public class Engine implements RecordProcessor {
               || intent == ProcessInstanceIntent.TERMINATE_ELEMENT
               || intent == ProcessInstanceBatchIntent.TERMINATE;
 
-      if (noBanCheckNeeded || (banned && commandAllowedForBanned) || !banned) {
+      if (noBanCheckNeeded || !banned || commandAllowedForBanned) {
         currentProcessor.processRecord(record);
       }
     }
