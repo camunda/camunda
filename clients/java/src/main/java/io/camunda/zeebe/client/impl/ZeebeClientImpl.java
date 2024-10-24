c4a04f9d600187038e08edb18a462213e92c2de4
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-10-24 15:09:11 +0200

    feat: implement client get form for process

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/ZeebeClientImpl.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/ZeebeClientImpl.java
@@ -64,6 +64,7 @@ import io.camunda.zeebe.client.api.fetch.DecisionRequirementsGetXmlRequest;
 import io.camunda.zeebe.client.api.fetch.DocumentContentGetRequest;
 import io.camunda.zeebe.client.api.fetch.FlowNodeInstanceGetRequest;
 import io.camunda.zeebe.client.api.fetch.IncidentGetRequest;
+import io.camunda.zeebe.client.api.fetch.ProcessDefinitionGetFormRequest;
 import io.camunda.zeebe.client.api.fetch.ProcessDefinitionGetRequest;
 import io.camunda.zeebe.client.api.fetch.ProcessDefinitionGetXmlRequest;
 import io.camunda.zeebe.client.api.fetch.ProcessInstanceGetRequest;
@@ -121,6 +122,7 @@ import io.camunda.zeebe.client.impl.fetch.DecisionRequirementsGetXmlRequestImpl;
 import io.camunda.zeebe.client.impl.fetch.DocumentContentGetRequestImpl;
 import io.camunda.zeebe.client.impl.fetch.FlowNodeInstanceGetRequestImpl;
 import io.camunda.zeebe.client.impl.fetch.IncidentGetRequestImpl;
+import io.camunda.zeebe.client.impl.fetch.ProcessDefinitionGetFormRequestImpl;
 import io.camunda.zeebe.client.impl.fetch.ProcessDefinitionGetRequestImpl;
 import io.camunda.zeebe.client.impl.fetch.ProcessDefinitionGetXmlRequestImpl;
 import io.camunda.zeebe.client.impl.fetch.ProcessInstanceGetRequestImpl;
@@ -603,6 +605,12 @@ public final class ZeebeClientImpl implements ZeebeClient {
   public ProcessDefinitionGetXmlRequest newProcessDefinitionGetXmlRequest(
       final long processDefinitionKey) {
     return new ProcessDefinitionGetXmlRequestImpl(httpClient, processDefinitionKey);
+  }
+
+  @Override
+  public ProcessDefinitionGetFormRequest newProcessDefinitionGetFormRequest(
+      final long processDefinitionKey) {
+    return new ProcessDefinitionGetFormRequestImpl(httpClient, processDefinitionKey);
   }
 
   @Override
