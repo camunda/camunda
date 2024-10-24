34b9bdc9de9092bc4996eb9bf1983e2a0d410ffb
Christian Thiel <christian.thiel@holisticon.de>
2024-10-24 11:11:01 +0200

    feat: add processDefinition to RdbmsSearchClient #23263
    - exporter was already in place, but added missing fields
    - added versatile search and sort options
    - added integration tests

--- db/rdbms/src/main/java/io/camunda/db/rdbms/read/service/ProcessDefinitionReader.java
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/read/service/ProcessDefinitionReader.java
@@ -7,11 +7,17 @@
  */
 package io.camunda.db.rdbms.read.service;
 
+import io.camunda.db.rdbms.read.domain.ProcessDefinitionDbQuery;
 import io.camunda.db.rdbms.sql.ProcessDefinitionMapper;
-import io.camunda.db.rdbms.write.domain.ProcessDefinitionDbModel;
+import io.camunda.search.entities.ProcessDefinitionEntity;
+import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProcessDefinitionReader {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProcessDefinitionReader.class);
 
   private final ProcessDefinitionMapper processDefinitionMapper;
 
@@ -19,7 +25,24 @@ public class ProcessDefinitionReader {
     this.processDefinitionMapper = processDefinitionMapper;
   }
 
-  public Optional<ProcessDefinitionDbModel> findOne(final long processDefinitionKey) {
-    return Optional.ofNullable(processDefinitionMapper.findOne(processDefinitionKey));
+  public Optional<ProcessDefinitionEntity> findOne(final long processDefinitionKey) {
+    final var result =
+        search(
+            ProcessDefinitionDbQuery.of(
+                b -> b.filter(f -> f.processDefinitionKeys(processDefinitionKey))));
+    if (result.hits == null || result.hits.isEmpty()) {
+      return Optional.empty();
+    } else {
+      return Optional.of(result.hits.getFirst());
+    }
   }
+
+  public SearchResult search(final ProcessDefinitionDbQuery filter) {
+    LOG.trace("[RDBMS DB] Search for process instance with filter {}", filter);
+    final var totalHits = processDefinitionMapper.count(filter);
+    final var hits = processDefinitionMapper.search(filter);
+    return new SearchResult(hits, totalHits);
+  }
+
+  public record SearchResult(List<ProcessDefinitionEntity> hits, Integer total) {}
 }
--- db/rdbms/src/main/java/io/camunda/db/rdbms/sql/ProcessDefinitionMapper.java
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/sql/ProcessDefinitionMapper.java
@@ -7,11 +7,16 @@
  */
 package io.camunda.db.rdbms.sql;
 
+import io.camunda.db.rdbms.read.domain.ProcessDefinitionDbQuery;
 import io.camunda.db.rdbms.write.domain.ProcessDefinitionDbModel;
+import io.camunda.search.entities.ProcessDefinitionEntity;
+import java.util.List;
 
 public interface ProcessDefinitionMapper {
 
   void insert(ProcessDefinitionDbModel processDeployment);
 
-  ProcessDefinitionDbModel findOne(long processDefinitionKey);
+  Integer count(ProcessDefinitionDbQuery filter);
+
+  List<ProcessDefinitionEntity> search(ProcessDefinitionDbQuery filter);
 }
