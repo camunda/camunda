b8e9685cc71b1f03091a36dcf53c7d4a3735a661
Mustafa Dagher <mustafa.dagher@camunda.com>
2024-10-31 13:11:26 +0100

    feat: configure spring beans for OperationTemplate and BatchOperationTemplate

--- operate/schema/src/main/java/io/camunda/operate/schema/IndexTemplateDescriptorsConfigurator.java
+++ operate/schema/src/main/java/io/camunda/operate/schema/IndexTemplateDescriptorsConfigurator.java
@@ -13,6 +13,7 @@ import io.camunda.webapps.schema.descriptors.operate.index.DecisionIndex;
 import io.camunda.webapps.schema.descriptors.operate.index.DecisionRequirementsIndex;
 import io.camunda.webapps.schema.descriptors.operate.index.MetricIndex;
 import io.camunda.webapps.schema.descriptors.operate.index.ProcessIndex;
+import io.camunda.webapps.schema.descriptors.operate.template.BatchOperationTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.DecisionInstanceTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.EventTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.FlowNodeInstanceTemplate;
@@ -20,6 +21,7 @@ import io.camunda.webapps.schema.descriptors.operate.template.IncidentTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.JobTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.ListViewTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.MessageTemplate;
+import io.camunda.webapps.schema.descriptors.operate.template.OperationTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.PostImporterQueueTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.SequenceFlowTemplate;
 import io.camunda.webapps.schema.descriptors.operate.template.UserTaskTemplate;
@@ -149,6 +151,22 @@ public class IndexTemplateDescriptorsConfigurator {
       final OperateProperties operateProperties, final DatabaseInfo databaseInfo) {
     return new VariableTemplate(
         operateProperties.getIndexPrefix(databaseInfo.getCurrent()),
+        databaseInfo.isElasticsearchDb());
+  }
+
+  @Bean
+  public OperationTemplate getOperationTemplate(
+      final OperateProperties operateProperties, final DatabaseInfo databaseInfo) {
+    return new OperationTemplate(
+        operateProperties.getIndexPrefix(DatabaseInfo.getCurrent()),
+        databaseInfo.isElasticsearchDb());
+  }
+
+  @Bean
+  public BatchOperationTemplate getBatchOperationTemplate(
+      final OperateProperties operateProperties, final DatabaseInfo databaseInfo) {
+    return new BatchOperationTemplate(
+        operateProperties.getIndexPrefix(DatabaseInfo.getCurrent()),
         databaseInfo.isElasticsearchDb());
   }
 }
