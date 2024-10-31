78d41824a417a015fd8e6ce1fb54132ba1e01d93
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2024-10-31 17:00:45 +0200

    test: add ITest to verify rejection of TL job completion with variables
    This test demonstrates that when a Task Listener job with variables is rejected, it is retried, following the normal
    engine behavior to retry failed jobs. Once the job reaches the configured retry count, a `JOB_NO_RETRIES` incident is
    created with a message indicating that the incident was raised due to variables provided during the Task Listener job
    completion.

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/UserTaskListenersTest.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/UserTaskListenersTest.java
@@ -7,15 +7,22 @@
  */
 package io.camunda.zeebe.it.client.command;
 
+import static io.camunda.zeebe.protocol.record.Assertions.assertThat;
 import static io.camunda.zeebe.test.util.TestUtil.waitUntil;
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.awaitility.Awaitility.await;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobHandler;
 import io.camunda.zeebe.client.api.worker.JobWorker;
 import io.camunda.zeebe.it.util.RecordingJobHandler;
 import io.camunda.zeebe.it.util.ZeebeAssertHelper;
 import io.camunda.zeebe.it.util.ZeebeResourcesHelper;
+import io.camunda.zeebe.protocol.record.value.ErrorType;
+import io.camunda.zeebe.protocol.record.value.IncidentRecordValue;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
@@ -24,6 +31,7 @@ import io.camunda.zeebe.test.util.junit.AutoCloseResources.AutoCloseResource;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -102,5 +110,65 @@ public class UserTaskListenersTest {
           assertThat(userTask.getVariables()).isEmpty();
           assertThat(userTask.getAction()).isEqualTo("complete");
         });
+  }
+
+  @Test
+  void shouldRejectCompleteTaskListenerJobCompletionWhenVariablesAreSetAndCreateIncident() {
+    // given
+    final int jobRetries = 2;
+    final var listenerType = "complete_with_variables";
+    final var userTaskKey =
+        resourcesHelper.createSingleUserTask(
+            task ->
+                task.zeebeTaskListener(
+                    listener ->
+                        listener
+                            .complete()
+                            .type(listenerType)
+                            .retries(String.valueOf(jobRetries))));
+
+    final JobHandler completeJobWithVariableHandler =
+        (jobClient, job) ->
+            jobClient.newCompleteCommand(job.getKey()).variable("my_variable", 123).send().join();
+
+    final var recordingHandler = new RecordingJobHandler(completeJobWithVariableHandler);
+    workers.add(client.newWorker().jobType(listenerType).handler(recordingHandler).open());
+
+    // when
+    client.newUserTaskCompleteCommand(userTaskKey).send();
+    await("until all retries are exhausted")
+        .untilAsserted(
+            () ->
+                assertThat(recordingHandler.getHandledJobs())
+                    .describedAs(
+                        "TL job should retry until reaching the final attempt with retries set to 1")
+                    .last()
+                    .extracting(ActivatedJob::getRetries)
+                    .isEqualTo(1));
+
+    // then
+    final var handledJobs = recordingHandler.getHandledJobs();
+    assertThat(handledJobs)
+        .hasSize(jobRetries)
+        .allSatisfy(job -> assertThat(job.getType()).isEqualTo(listenerType));
+
+    assertThat(handledJobs.getFirst())
+        .describedAs("Job attempts should have same field values except 'retries' and 'deadline'")
+        .usingRecursiveComparison()
+        .ignoringFields("retries", "deadline")
+        .isEqualTo(handledJobs.getLast());
+
+    // assert that an incident was created due to the rejection of TL job completion with variables
+    final var expectedErrorMessageWithRejectionReason =
+        "Command 'COMPLETE' rejected with code 'INVALID_ARGUMENT': Task Listener job completion with variables is not supported";
+    ZeebeAssertHelper.assertIncidentCreated(
+        incident ->
+            assertThat(incident)
+                .hasJobKey(handledJobs.getLast().getKey())
+                .hasErrorType(ErrorType.JOB_NO_RETRIES)
+                .extracting(
+                    IncidentRecordValue::getErrorMessage, as(InstanceOfAssertFactories.STRING))
+                .startsWith("io.camunda.zeebe.client.api.command.ClientStatusException:")
+                .contains(expectedErrorMessageWithRejectionReason));
   }
 }
