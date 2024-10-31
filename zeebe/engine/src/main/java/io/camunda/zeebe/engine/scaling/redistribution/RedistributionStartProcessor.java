8494df5c4d88edb628ef6903a91300cb6c06fb37
Lena Schönburg <lena@fairburg.xyz>
2024-10-31 14:20:50 +0100

    feat: define the processors for the redistribution lifecycle

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/scaling/redistribution/RedistributionStartProcessor.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.scaling.redistribution;
+
+import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessor;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
+import io.camunda.zeebe.protocol.impl.record.value.scaling.RedistributionRecord;
+import io.camunda.zeebe.protocol.record.intent.scaling.RedistributionIntent;
+import io.camunda.zeebe.stream.api.records.TypedRecord;
+
+public class RedistributionStartProcessor implements TypedRecordProcessor<RedistributionRecord> {
+  private final StateWriter stateWriter;
+
+  public RedistributionStartProcessor(final Writers writers) {
+    stateWriter = writers.state();
+  }
+
+  @Override
+  public void processRecord(final TypedRecord<RedistributionRecord> record) {
+    stateWriter.appendFollowUpEvent(
+        record.getKey(), RedistributionIntent.STARTED, record.getValue());
+  }
+}
