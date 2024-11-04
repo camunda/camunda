4a4eaa40d66b8573988def41006c41b435d07945
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2024-11-04 19:04:25 +0100

    fix: change session flag to false by default

--- authentication/src/main/java/io/camunda/authentication/handler/session/ConditionalOnSessionPersistence.java
+++ authentication/src/main/java/io/camunda/authentication/handler/session/ConditionalOnSessionPersistence.java
@@ -24,5 +24,5 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplicat
         + " or "
         + "${camunda.tasklist.persistentSessionsEnabled:false}"
         + " or "
-        + "${camunda.persistent.sessions.enabled:true}")
+        + "${camunda.persistent.sessions.enabled:false}")
 public @interface ConditionalOnSessionPersistence {}
