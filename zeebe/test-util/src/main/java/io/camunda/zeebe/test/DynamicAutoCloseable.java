e5b57606e96d8a7df6160a683c86df493c3bbec5
Carlo Sana <carlo.sana@camunda.com>
2024-11-04 09:52:01 +0100

    refactor: extract logic from AutoCloseableRule to be be used without JUnit 4

---
+++ zeebe/test-util/src/main/java/io/camunda/zeebe/test/DynamicAutoCloseable.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that can managed AutoCloseable added dynamically. AutoCloseable are closed in the opposite
+ * order they were added.
+ */
+public class DynamicAutoCloseable implements AutoCloseable {
+
+  final List<AutoCloseable> thingsToClose = new ArrayList<>();
+
+  public <A extends AutoCloseable> A manage(final A closeable) {
+    thingsToClose.add(closeable);
+    return closeable;
+  }
+
+  @Override
+  public void close() {
+    RuntimeException firstException = null;
+    final int size = thingsToClose.size();
+    for (int i = size - 1; i >= 0; i--) {
+      try {
+        thingsToClose.remove(i).close();
+      } catch (final Exception e) {
+        if (firstException == null) {
+          firstException = new RuntimeException(e);
+        }
+      }
+    }
+    if (firstException != null) {
+      throw firstException;
+    }
+  }
+}
