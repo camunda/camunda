f1d8e4e2c28ce7ff19b323e80597f54e2bdfc445
Nikola Koevski <nikola.koevski@camunda.com>
2024-11-04 14:26:21 +0100

    feat: allow distribution of group records
    Enable the command distribution mechanism to work
    with group records by adding the mapping for group
    value-type:group record to the appropriate classes.

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/TypedEventRegistry.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/TypedEventRegistry.java
@@ -23,6 +23,7 @@ import io.camunda.zeebe.protocol.impl.record.value.deployment.ProcessRecord;
 import io.camunda.zeebe.protocol.impl.record.value.distribution.CommandDistributionRecord;
 import io.camunda.zeebe.protocol.impl.record.value.error.ErrorRecord;
 import io.camunda.zeebe.protocol.impl.record.value.escalation.EscalationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.group.GroupRecord;
 import io.camunda.zeebe.protocol.impl.record.value.incident.IncidentRecord;
 import io.camunda.zeebe.protocol.impl.record.value.job.JobBatchRecord;
 import io.camunda.zeebe.protocol.impl.record.value.job.JobRecord;
@@ -108,6 +109,7 @@ public final class TypedEventRegistry {
     registry.put(ValueType.ROLE, RoleRecord.class);
     registry.put(ValueType.SCALE, ScaleRecord.class);
     registry.put(ValueType.MAPPING, MappingRecord.class);
+    registry.put(ValueType.GROUP, GroupRecord.class);
 
     EVENT_REGISTRY = Collections.unmodifiableMap(registry);
 
