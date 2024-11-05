6f3b08618b79da07441cfc086bdf67bd0e636d74
Nikola Koevski <nikola.koevski@camunda.com>
2024-11-05 10:51:04 +0100

    test: coverage for add group add entity events
    Add test coverage for applying Group.ADD_ENTITY
    events to the GroupState.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/group/GroupStateTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/group/GroupStateTest.java
@@ -13,6 +13,8 @@ import io.camunda.zeebe.engine.state.mutable.MutableGroupState;
 import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
 import io.camunda.zeebe.engine.util.ProcessingStateExtension;
 import io.camunda.zeebe.protocol.impl.record.value.group.GroupRecord;
+import io.camunda.zeebe.protocol.record.value.EntityType;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -110,5 +112,48 @@ public class GroupStateTest {
     final var groupKeyByName = groupState.getGroupKeyByName(updatedGroupName);
     assertThat(groupKeyByName.isPresent()).isTrue();
     assertThat(groupKeyByName.get()).isEqualTo(groupKey);
+  }
+
+  @Test
+  void shouldAddEntity() {
+    // given
+    final var groupKey = 1L;
+    final var groupName = "group";
+    final var groupRecord = new GroupRecord().setGroupKey(groupKey).setName(groupName);
+    groupState.create(groupKey, groupRecord);
+
+    // when
+    final var userKey = 2L;
+    final var userEntityType = EntityType.USER;
+    groupRecord.setEntityKey(userKey).setEntityType(userEntityType);
+    groupState.addEntity(groupKey, groupRecord);
+
+    // then
+    final var entityType = groupState.getEntityType(groupKey, userKey);
+    assertThat(entityType.isPresent()).isTrue();
+    assertThat(entityType.get()).isEqualTo(userEntityType);
+  }
+
+  @Test
+  void shouldReturnEntitiesByType() {
+    // given
+    final var groupKey = 1L;
+    final var groupName = "group";
+    final var groupRecord = new GroupRecord().setGroupKey(groupKey).setName(groupName);
+    groupState.create(groupKey, groupRecord);
+    final var userKey = 2L;
+    groupRecord.setEntityKey(2L).setEntityType(EntityType.USER);
+    groupState.addEntity(groupKey, groupRecord);
+    final var mappingKey = 3L;
+    groupRecord.setEntityKey(mappingKey).setEntityType(EntityType.MAPPING);
+    groupState.addEntity(groupKey, groupRecord);
+
+    // when
+    final var entities = groupState.getEntitiesByType(groupKey);
+
+    // then
+    assertThat(entities)
+        .containsEntry(EntityType.USER, List.of(userKey))
+        .containsEntry(EntityType.MAPPING, List.of(mappingKey));
   }
 }
