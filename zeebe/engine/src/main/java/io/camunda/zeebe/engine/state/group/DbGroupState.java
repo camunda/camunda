24cae6cf71345800e5fa2741c968a3e87a889bb4
Nikola Koevski <nikola.koevski@camunda.com>
2024-11-05 13:21:55 +0100

    feat: add group state delete method
    Define and implement a delete method for
    the GroupState.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/group/DbGroupState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/group/DbGroupState.java
@@ -109,6 +109,24 @@ public class DbGroupState implements MutableGroupState {
   }
 
   @Override
+  public void delete(final long groupKey) {
+    this.groupKey.wrapLong(groupKey);
+
+    // remove record from GROUP_BY_NAME cf
+    groupName.wrapString(persistedGroup.getName());
+    groupByNameColumnFamily.deleteExisting(groupName);
+
+    // remove entries from ENTITY_BY_GROUP cf
+    entityTypeByGroupColumnFamily.whileEqualPrefix(
+        fkGroupKey,
+        (compositeKey, value) -> {
+          entityTypeByGroupColumnFamily.deleteExisting(compositeKey);
+        });
+
+    groupColumnFamily.deleteExisting(this.groupKey);
+  }
+
+  @Override
   public Optional<PersistedGroup> get(final long groupKey) {
     this.groupKey.wrapLong(groupKey);
     final var persistedGroup = groupColumnFamily.get(this.groupKey);
