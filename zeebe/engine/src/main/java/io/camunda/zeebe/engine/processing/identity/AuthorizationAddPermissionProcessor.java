a2e44781e3de62cb2a7f1f8a8d3f09dcadc107cb
remco.westerhoud <remcowesterhoud@msn.com>
2024-11-07 11:50:35 +0100

    refactor: extract common logic to behavior class
    A lot of the logic in this class will be shared when removing permissions. By extracting this to a separate class we can
     easily reuse it later.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/AuthorizationAddPermissionProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/AuthorizationAddPermissionProcessor.java
@@ -7,24 +7,16 @@
  */
 package io.camunda.zeebe.engine.processing.identity;
 
-import static io.camunda.zeebe.engine.processing.identity.AuthorizationCheckBehavior.UNAUTHORIZED_ERROR_MESSAGE;
-
 import io.camunda.zeebe.engine.processing.distribution.CommandDistributionBehavior;
-import io.camunda.zeebe.engine.processing.identity.AuthorizationCheckBehavior.AuthorizationRequest;
 import io.camunda.zeebe.engine.processing.streamprocessor.DistributedTypedRecordProcessor;
-import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessor.ProcessingError;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedResponseWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.distribution.DistributionQueue;
-import io.camunda.zeebe.engine.state.immutable.AuthorizationState;
 import io.camunda.zeebe.engine.state.immutable.ProcessingState;
 import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
-import io.camunda.zeebe.protocol.record.RejectionType;
 import io.camunda.zeebe.protocol.record.intent.AuthorizationIntent;
-import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
-import io.camunda.zeebe.protocol.record.value.PermissionType;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
 import io.camunda.zeebe.stream.api.state.KeyGenerator;
 
@@ -32,12 +24,11 @@ public final class AuthorizationAddPermissionProcessor
     implements DistributedTypedRecordProcessor<AuthorizationRecord> {
 
   private final KeyGenerator keyGenerator;
-  private final AuthorizationState authorizationState;
   private final CommandDistributionBehavior distributionBehavior;
   private final StateWriter stateWriter;
   private final TypedResponseWriter responseWriter;
   private final TypedRejectionWriter rejectionWriter;
-  private final AuthorizationCheckBehavior authCheckBehavior;
+  private final PermissionsBehavior permissionsBehavior;
 
   public AuthorizationAddPermissionProcessor(
       final Writers writers,
@@ -46,44 +37,24 @@ public final class AuthorizationAddPermissionProcessor
       final CommandDistributionBehavior distributionBehavior,
       final AuthorizationCheckBehavior authCheckBehavior) {
     this.keyGenerator = keyGenerator;
-    authorizationState = processingState.getAuthorizationState();
     this.distributionBehavior = distributionBehavior;
     stateWriter = writers.state();
     responseWriter = writers.response();
     rejectionWriter = writers.rejection();
-    this.authCheckBehavior = authCheckBehavior;
+    permissionsBehavior = new PermissionsBehavior(processingState, authCheckBehavior);
   }
 
   @Override
   public void processNewCommand(final TypedRecord<AuthorizationRecord> command) {
-    final var authorizationRequest =
-        new AuthorizationRequest(
-            command, AuthorizationResourceType.AUTHORIZATION, PermissionType.UPDATE);
-    if (!authCheckBehavior.isAuthorized(authorizationRequest)) {
-      final var errorMessage =
-          UNAUTHORIZED_ERROR_MESSAGE.formatted(
-              authorizationRequest.getPermissionType(), authorizationRequest.getResourceType());
-      rejectionWriter.appendRejection(command, RejectionType.UNAUTHORIZED, errorMessage);
-      responseWriter.writeRejectionOnCommand(command, RejectionType.UNAUTHORIZED, errorMessage);
-      return;
-    }
-
-    final var authorizationRecord = command.getValue();
-
-    final var ownerType =
-        authorizationState
-            .getOwnerType(authorizationRecord.getOwnerKey())
-            .orElseThrow(() -> new OwnerNotFoundException(authorizationRecord.getOwnerKey()));
-    authorizationRecord.setOwnerType(ownerType);
-
-    final long key = keyGenerator.nextKey();
-    stateWriter.appendFollowUpEvent(key, AuthorizationIntent.PERMISSION_ADDED, authorizationRecord);
-    distributionBehavior
-        .withKey(key)
-        .inQueue(DistributionQueue.IDENTITY.getQueueId())
-        .distribute(command);
-    responseWriter.writeEventOnCommand(
-        key, AuthorizationIntent.PERMISSION_ADDED, authorizationRecord, command);
+    permissionsBehavior
+        .isAuthorized(command)
+        .flatMap(permissionsBehavior::ownerExists)
+        .ifRightOrLeft(
+            authorizationRecord -> writeEventAndDistribute(command, authorizationRecord),
+            (rejection) -> {
+              rejectionWriter.appendRejection(command, rejection.type(), rejection.reason());
+              responseWriter.writeRejectionOnCommand(command, rejection.type(), rejection.reason());
+            });
   }
 
   @Override
@@ -93,26 +64,16 @@ public final class AuthorizationAddPermissionProcessor
     distributionBehavior.acknowledgeCommand(command);
   }
 
-  @Override
-  public ProcessingError tryHandleError(
-      final TypedRecord<AuthorizationRecord> command, final Throwable error) {
-    if (error instanceof final OwnerNotFoundException exception) {
-      rejectionWriter.appendRejection(command, RejectionType.NOT_FOUND, exception.getMessage());
-      responseWriter.writeRejectionOnCommand(
-          command, RejectionType.NOT_FOUND, exception.getMessage());
-      return ProcessingError.EXPECTED_ERROR;
-    }
-
-    return ProcessingError.UNEXPECTED_ERROR;
-  }
-
-  private static final class OwnerNotFoundException extends RuntimeException {
-
-    public static final String OWNER_NOT_FOUND_MESSAGE =
-        "Expected to find owner with key: '%d', but none was found";
-
-    public OwnerNotFoundException(final long ownerKey) {
-      super(OWNER_NOT_FOUND_MESSAGE.formatted(ownerKey));
-    }
+  private void writeEventAndDistribute(
+      final TypedRecord<AuthorizationRecord> command,
+      final AuthorizationRecord authorizationRecord) {
+    final long key = keyGenerator.nextKey();
+    stateWriter.appendFollowUpEvent(key, AuthorizationIntent.PERMISSION_ADDED, authorizationRecord);
+    responseWriter.writeEventOnCommand(
+        key, AuthorizationIntent.PERMISSION_ADDED, authorizationRecord, command);
+    distributionBehavior
+        .withKey(key)
+        .inQueue(DistributionQueue.IDENTITY.getQueueId())
+        .distribute(command);
   }
 }
