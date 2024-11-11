607e8f3d67088dcb77bbd377fab7dc7e1c61d1e4
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-11-11 14:27:00 +0100

    feat: add support for adding and removing role members
    closes #24579 and #24583

--- service/src/main/java/io/camunda/service/RoleServices.java
+++ service/src/main/java/io/camunda/service/RoleServices.java
@@ -18,10 +18,12 @@ import io.camunda.security.auth.Authorization;
 import io.camunda.service.search.core.SearchQueryService;
 import io.camunda.service.security.SecurityContextProvider;
 import io.camunda.zeebe.broker.client.api.BrokerClient;
+import io.camunda.zeebe.gateway.impl.broker.request.BrokerRoleEntityRequest;
 import io.camunda.zeebe.gateway.impl.broker.request.BrokerRoleUpdateRequest;
 import io.camunda.zeebe.gateway.impl.broker.request.role.BrokerRoleCreateRequest;
 import io.camunda.zeebe.gateway.impl.broker.request.role.BrokerRoleDeleteRequest;
 import io.camunda.zeebe.protocol.impl.record.value.authorization.RoleRecord;
+import io.camunda.zeebe.protocol.record.value.EntityType;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
@@ -76,5 +78,19 @@ public class RoleServices extends SearchQueryService<RoleServices, RoleQuery, Ro
 
   public CompletableFuture<RoleRecord> deleteRole(final long roleKey) {
     return sendBrokerRequest(new BrokerRoleDeleteRequest(roleKey));
+  }
+
+  public void addMember(final Long roleKey, final EntityType entityType, final long entityKey) {
+    sendBrokerRequest(
+        BrokerRoleEntityRequest.createAddRequest()
+            .setRoleKey(roleKey)
+            .setEntity(entityType, entityKey));
+  }
+
+  public void removeMember(final Long roleKey, final EntityType entityType, final long entityKey) {
+    sendBrokerRequest(
+        BrokerRoleEntityRequest.createRemoveRequest()
+            .setRoleKey(roleKey)
+            .setEntity(entityType, entityKey));
   }
 }
