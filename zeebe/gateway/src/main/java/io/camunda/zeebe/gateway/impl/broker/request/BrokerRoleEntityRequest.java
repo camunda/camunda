11892fbc5d196516ae0e6b3ff6fbf0171aee68a6
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-11-11 14:27:00 +0100

    feat: add support for adding and removing role members
    closes #24579 and #24583

---
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerRoleEntityRequest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.impl.broker.request;
+
+import com.google.common.base.Preconditions;
+import io.camunda.zeebe.broker.client.api.dto.BrokerExecuteCommand;
+import io.camunda.zeebe.protocol.impl.record.value.authorization.RoleRecord;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.RoleIntent;
+import io.camunda.zeebe.protocol.record.value.EntityType;
+import org.agrona.DirectBuffer;
+
+public final class BrokerRoleEntityRequest extends BrokerExecuteCommand<RoleRecord> {
+  private final RoleRecord roleDto = new RoleRecord();
+
+  private BrokerRoleEntityRequest(final RoleIntent intent) {
+    super(ValueType.ROLE, intent);
+  }
+
+  public static BrokerRoleEntityRequest createAddRequest() {
+    return new BrokerRoleEntityRequest(RoleIntent.ADD_ENTITY);
+  }
+
+  public static BrokerRoleEntityRequest createRemoveRequest() {
+    return new BrokerRoleEntityRequest(RoleIntent.REMOVE_ENTITY);
+  }
+
+  public BrokerRoleEntityRequest setRoleKey(final long roleKey) {
+    roleDto.setRoleKey(roleKey);
+    return this;
+  }
+
+  public BrokerRoleEntityRequest setEntity(final EntityType entityType, final long entityKey) {
+    Preconditions.checkArgument(
+        entityType == EntityType.USER || entityType == EntityType.MAPPING,
+        "For now, roles can only be granted to users and mappings");
+    roleDto.setEntityType(entityType);
+    roleDto.setEntityKey(entityKey);
+    return this;
+  }
+
+  @Override
+  public RoleRecord getRequestWriter() {
+    return roleDto;
+  }
+
+  @Override
+  protected RoleRecord toResponseDto(final DirectBuffer buffer) {
+    final var response = new RoleRecord();
+    response.wrap(buffer);
+    return response;
+  }
+}
