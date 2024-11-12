b942642cfd362489185c5735aaa5ffc40c473c11
Ivan Karpov <ivan.karpov-ext@camunda.com>
2024-11-12 14:15:43 +0100

    feat: remove DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES

--- optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/util/mapper/ObjectMapperFactory.java
+++ optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/util/mapper/ObjectMapperFactory.java
@@ -34,11 +34,13 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class ObjectMapperFactory {
-  public static final ObjectMapper OPTIMIZE_MAPPER =
+  private static final ObjectMapperFactory FACTORY =
       new ObjectMapperFactory(
-              new OptimizeDateTimeFormatterFactory().getObject(),
-              ConfigurationServiceBuilder.createDefaultConfiguration())
-          .createOptimizeMapper();
+          new OptimizeDateTimeFormatterFactory().getObject(),
+          ConfigurationServiceBuilder.createDefaultConfiguration());
+  public static final ObjectMapper OPTIMIZE_MAPPER = FACTORY.createOptimizeMapper();
+  public static final ObjectMapper OPTIMIZE_MAPPER_UNKNOWN_FAIL_DISABLED =
+      FACTORY.createOptimizeMapper(false);
 
   private final DateTimeFormatter optimizeDateTimeFormatter;
   private final ConfigurationService configurationService;
@@ -54,21 +56,27 @@ public class ObjectMapperFactory {
   @Bean
   @Primary
   public ObjectMapper createOptimizeMapper() {
-    return buildObjectMapper(optimizeDateTimeFormatter);
+    return buildObjectMapper(optimizeDateTimeFormatter, true);
+  }
+
+  public ObjectMapper createOptimizeMapper(final boolean unknownPropsEnable) {
+    return buildObjectMapper(optimizeDateTimeFormatter, unknownPropsEnable);
   }
 
   @Qualifier("engineMapper")
   @Bean
   public ObjectMapper createEngineMapper() {
-    return buildObjectMapper(createEngineDateTimeFormatter());
+    return buildObjectMapper(createEngineDateTimeFormatter(), true);
   }
 
   private DateTimeFormatter createEngineDateTimeFormatter() {
     return DateTimeFormatter.ofPattern(configurationService.getEngineDateFormat());
   }
 
-  private ObjectMapper buildObjectMapper(final DateTimeFormatter deserializationDateTimeFormatter) {
-    JavaTimeModule javaTimeModule = new JavaTimeModule();
+  private ObjectMapper buildObjectMapper(
+      final DateTimeFormatter deserializationDateTimeFormatter,
+      final boolean unknownPropsFailEnable) {
+    final JavaTimeModule javaTimeModule = new JavaTimeModule();
     javaTimeModule.addSerializer(
         OffsetDateTime.class, new CustomOffsetDateTimeSerializer(optimizeDateTimeFormatter));
     javaTimeModule.addSerializer(
@@ -77,24 +85,28 @@ public class ObjectMapperFactory {
         OffsetDateTime.class,
         new CustomOffsetDateTimeDeserializer(deserializationDateTimeFormatter));
 
-    ObjectMapper mapper =
+    final Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder =
         Jackson2ObjectMapperBuilder.json()
             .modules(new Jdk8Module(), javaTimeModule)
             .featuresToDisable(
                 SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,
                 DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE,
-                DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
                 DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES,
                 DeserializationFeature.FAIL_ON_MISSING_EXTERNAL_TYPE_ID_PROPERTY,
                 SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS)
             .featuresToEnable(
                 JsonParser.Feature.ALLOW_COMMENTS,
-                //                SerializationFeature.INDENT_OUTPUT,
                 DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY,
-                MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS)
-            .build();
+                MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
 
-    SimpleModule module = new SimpleModule();
+    if (unknownPropsFailEnable) {
+      jackson2ObjectMapperBuilder.featuresToDisable(
+          DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    final ObjectMapper mapper = jackson2ObjectMapperBuilder.build();
+
+    final SimpleModule module = new SimpleModule();
     module.addDeserializer(
         DefinitionOptimizeResponseDto.class, new CustomDefinitionDeserializer(mapper));
     module.addDeserializer(
