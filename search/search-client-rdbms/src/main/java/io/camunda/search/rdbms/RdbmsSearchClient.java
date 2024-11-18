f786ef279d069148eee34075ca7c4e27f1eedc81
Christian Thiel <christian.thiel@holisticon.de>
2024-11-18 17:28:28 +0100

    feat: added keyset pagination to flowNodeInstance search in RDBMS #24002

--- search/search-client-rdbms/src/main/java/io/camunda/search/rdbms/RdbmsSearchClient.java
+++ search/search-client-rdbms/src/main/java/io/camunda/search/rdbms/RdbmsSearchClient.java
@@ -8,7 +8,6 @@
 package io.camunda.search.rdbms;
 
 import io.camunda.db.rdbms.RdbmsService;
-import io.camunda.db.rdbms.read.domain.FlowNodeInstanceDbQuery;
 import io.camunda.db.rdbms.read.domain.UserTaskDbQuery;
 import io.camunda.search.clients.AuthorizationSearchClient;
 import io.camunda.search.clients.DecisionDefinitionSearchClient;
@@ -129,14 +128,7 @@ public class RdbmsSearchClient
   @Override
   public SearchQueryResult<FlowNodeInstanceEntity> searchFlowNodeInstances(
       final FlowNodeInstanceQuery query) {
-    final var searchResult =
-        rdbmsService
-            .getFlowNodeInstanceReader()
-            .search(
-                FlowNodeInstanceDbQuery.of(
-                    b -> b.filter(query.filter()).sort(query.sort()).page(query.page())));
-
-    return new SearchQueryResult<>(searchResult.total(), searchResult.hits(), null);
+    return rdbmsService.getFlowNodeInstanceReader().search(query);
   }
 
   @Override
