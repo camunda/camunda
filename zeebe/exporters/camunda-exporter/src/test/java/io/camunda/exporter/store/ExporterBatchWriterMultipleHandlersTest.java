42b135e0dff60819cb446dcdab903c2dd192384f
Christopher Zell <zelldon91@googlemail.com>
2024-11-19 16:22:16 +0100

    test: verify flush and caching
    Verify that flushing can happen on several indices
    Verify that flushing can happen on same index with different entities
    Verify that flushing can happen on same index with same entities multiple times

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/store/ExporterBatchWriterMultipleHandlersTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/store/ExporterBatchWriterMultipleHandlersTest.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import io.camunda.exporter.exceptions.PersistenceException;
@@ -19,11 +20,11 @@ import io.camunda.exporter.store.ExporterBatchWriter.Builder;
 import io.camunda.webapps.schema.entities.ExporterEntity;
 import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.ValueType;
-import io.camunda.zeebe.protocol.record.intent.Intent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.protocol.record.value.ProcessInstanceRecordValue;
 import io.camunda.zeebe.test.broker.protocol.ProtocolFactory;
 import java.util.List;
+import java.util.function.Function;
 import org.junit.jupiter.api.Test;
 
 /** Integration test with actual exporter handlers */
@@ -32,12 +33,32 @@ public class ExporterBatchWriterMultipleHandlersTest {
   private final ProtocolFactory factory = new ProtocolFactory();
 
   @Test
-  public void shouldCacheEntitiesForMultipleHandlers() {
+  public void shouldCacheMultipleHandlersWithSameEntityOnce() {
     // given
     final var writer =
         Builder.begin()
-            .withHandler(new TestExportHandler("indexA"))
-            .withHandler(new TestExportHandler("indexB"))
+            .withHandler(TestExportHandler.defaultHandler())
+            .withHandler(TestExportHandler.defaultHandler())
+            .build();
+
+    // when
+    writer.addRecord(
+        factory.generateRecord(
+            ValueType.PROCESS_INSTANCE,
+            builder -> builder.withIntent(ProcessInstanceIntent.ELEMENT_COMPLETED)));
+
+    // then
+    assertThat(writer.getBatchSize()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldCacheMultipleDifferentEntitiesForMultipleHandlers() {
+    // given
+    final var writer =
+        Builder.begin()
+            .withHandler(
+                TestExportHandler.handlerForEntity(OtherTestEntity.class, OtherTestEntity::new))
+            .withHandler(TestExportHandler.defaultHandler())
             .build();
 
     // when
@@ -51,12 +72,39 @@ public class ExporterBatchWriterMultipleHandlersTest {
   }
 
   @Test
-  public void shouldFlushIntoSeparateIndicesForMultipleHandlers() throws PersistenceException {
+  public void shouldFlushMultipleHandlersWithSameEntity() throws PersistenceException {
     // given
     final var writer =
         Builder.begin()
-            .withHandler(new TestExportHandler("indexA"))
-            .withHandler(new TestExportHandler("indexB"))
+            .withHandler(TestExportHandler.defaultHandler())
+            .withHandler(TestExportHandler.defaultHandler())
+            .build();
+
+    final var record =
+        factory.generateRecord(
+            ValueType.PROCESS_INSTANCE,
+            builder -> builder.withIntent(ProcessInstanceIntent.ELEMENT_COMPLETED));
+    writer.addRecord(record);
+
+    final BatchRequest batchRequest = mock(BatchRequest.class);
+
+    // when
+    writer.flush(batchRequest);
+
+    // then
+    verify(batchRequest, times(2))
+        .update(eq("indexA"), eq(Long.toString(record.getKey())), any(TestEntity.class));
+    verify(batchRequest).execute();
+  }
+
+  @Test
+  public void shouldFlushIntoSeparateIndicesForMultipleHandlersWithSameEntity()
+      throws PersistenceException {
+    // given
+    final var writer =
+        Builder.begin()
+            .withHandler(TestExportHandler.handlerForIndex("indexA"))
+            .withHandler(TestExportHandler.handlerForIndex("indexB"))
             .build();
 
     final var record =
@@ -78,12 +126,101 @@ public class ExporterBatchWriterMultipleHandlersTest {
     verify(batchRequest).execute();
   }
 
-  private class TestExportHandler implements ExportHandler<TestEntity, ProcessInstanceRecordValue> {
+  @Test
+  public void shouldFlushMultipleDifferentEntitiesForMultipleHandlers()
+      throws PersistenceException {
+    // given
+    final var writer =
+        Builder.begin()
+            .withHandler(
+                TestExportHandler.handlerForEntity(OtherTestEntity.class, OtherTestEntity::new))
+            .withHandler(TestExportHandler.defaultHandler())
+            .build();
+
+    final var record =
+        factory.generateRecord(
+            ValueType.PROCESS_INSTANCE,
+            builder -> builder.withIntent(ProcessInstanceIntent.ELEMENT_COMPLETED));
+    writer.addRecord(record);
+
+    final BatchRequest batchRequest = mock(BatchRequest.class);
+
+    // when
+    writer.flush(batchRequest);
+
+    // then
+    verify(batchRequest)
+        .update(eq("indexA"), eq(Long.toString(record.getKey())), any(TestEntity.class));
+    verify(batchRequest)
+        .update(eq("indexA"), eq(Long.toString(record.getKey())), any(OtherTestEntity.class));
+    verify(batchRequest).execute();
+  }
+
+  private static class TestEntity implements ExporterEntity<TestEntity> {
+
+    private String id;
+
+    TestEntity(final String id) {
+      this.id = id;
+    }
+
+    @Override
+    public String getId() {
+      return id;
+    }
+
+    @Override
+    public TestEntity setId(final String id) {
+      this.id = id;
+      return this;
+    }
+  }
+
+  private static class OtherTestEntity implements ExporterEntity<OtherTestEntity> {
+
+    private String id;
+
+    OtherTestEntity(final String id) {
+      this.id = id;
+    }
+
+    @Override
+    public String getId() {
+      return id;
+    }
+
+    @Override
+    public OtherTestEntity setId(final String id) {
+      this.id = id;
+      return this;
+    }
+  }
+
+  private static class TestExportHandler<T extends ExporterEntity<T>>
+      implements ExportHandler<T, ProcessInstanceRecordValue> {
 
     private final String indexName;
+    private final Class<T> clazz;
+    private final Function<String, T> constructor;
 
-    private TestExportHandler(final String indexName) {
+    public TestExportHandler(
+        final String indexName, final Class<T> clazz, final Function<String, T> constructor) {
       this.indexName = indexName;
+      this.clazz = clazz;
+      this.constructor = constructor;
+    }
+
+    public static TestExportHandler<TestEntity> defaultHandler() {
+      return new TestExportHandler<>("indexA", TestEntity.class, TestEntity::new);
+    }
+
+    public static <T extends ExporterEntity<T>> TestExportHandler<T> handlerForEntity(
+        final Class<T> clazz, final Function<String, T> constructor) {
+      return new TestExportHandler<>("indexA", clazz, constructor);
+    }
+
+    public static TestExportHandler<TestEntity> handlerForIndex(final String indexName) {
+      return new TestExportHandler<>(indexName, TestEntity.class, TestEntity::new);
     }
 
     @Override
@@ -92,8 +229,8 @@ public class ExporterBatchWriterMultipleHandlersTest {
     }
 
     @Override
-    public Class<TestEntity> getEntityType() {
-      return TestEntity.class;
+    public Class<T> getEntityType() {
+      return clazz;
     }
 
     @Override
@@ -107,53 +244,23 @@ public class ExporterBatchWriterMultipleHandlersTest {
     }
 
     @Override
-    public TestEntity createNewEntity(final String id) {
-      return new TestEntity(id);
+    public T createNewEntity(final String id) {
+      return constructor.apply(id);
     }
 
     @Override
-    public void updateEntity(final Record record, final TestEntity entity) {
-      entity.setId(Long.toString(record.getKey())).setIntent(record.getIntent());
+    public void updateEntity(final Record<ProcessInstanceRecordValue> record, final T entity) {
+      entity.setId(Long.toString(record.getKey()));
     }
 
     @Override
-    public void flush(final TestEntity entity, final BatchRequest batchRequest)
-        throws PersistenceException {
+    public void flush(final T entity, final BatchRequest batchRequest) throws PersistenceException {
       batchRequest.update(indexName, entity.getId(), entity);
     }
 
     @Override
     public String getIndexName() {
       return null;
-    }
-  }
-
-  private class TestEntity implements ExporterEntity<TestEntity> {
-
-    private String id;
-    private Intent intent;
-
-    private TestEntity(final String id) {
-      this.id = id;
-    }
-
-    public Intent getIntent() {
-      return intent;
-    }
-
-    public void setIntent(final Intent intent) {
-      this.intent = intent;
-    }
-
-    @Override
-    public String getId() {
-      return id;
-    }
-
-    @Override
-    public TestEntity setId(final String id) {
-      this.id = id;
-      return this;
     }
   }
 }
