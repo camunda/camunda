26f4e45ca54b325c30a2f0234c30943bfec19b01
Carlo Sana <carlo.sana@camunda.com>
2024-11-21 17:46:39 +0100

    refactor: OpenSearchGetSnapshotResponse is converted to a record
    relates #24869

--- webapps-backup/src/main/java/io/camunda/webapps/backup/repository/opensearch/OpenSearchGetSnapshotResponse.java
+++ webapps-backup/src/main/java/io/camunda/webapps/backup/repository/opensearch/OpenSearchGetSnapshotResponse.java
@@ -8,48 +8,13 @@
 package io.camunda.webapps.backup.repository.opensearch;
 
 import java.util.List;
-import java.util.Objects;
 import org.opensearch.client.opensearch.snapshot.GetSnapshotResponse;
 
-public class OpenSearchGetSnapshotResponse {
-
-  private List<OpenSearchSnapshotInfo> snapshotInfos = List.of();
-
-  public OpenSearchGetSnapshotResponse() {}
-
-  public OpenSearchGetSnapshotResponse(final List<OpenSearchSnapshotInfo> snapshotInfos) {
-    this.snapshotInfos = snapshotInfos;
-  }
-
-  public List<OpenSearchSnapshotInfo> snapshots() {
-    return snapshotInfos;
-  }
+public record OpenSearchGetSnapshotResponse(List<OpenSearchSnapshotInfo> snapshots) {
 
   public static OpenSearchGetSnapshotResponse fromResponse(final GetSnapshotResponse response) {
     final var snapshotInfos =
         response.snapshots().stream().map(OpenSearchSnapshotInfo::fromResponse).toList();
     return new OpenSearchGetSnapshotResponse(snapshotInfos);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(snapshotInfos);
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    final OpenSearchGetSnapshotResponse that = (OpenSearchGetSnapshotResponse) o;
-    return Objects.equals(snapshotInfos, that.snapshotInfos);
-  }
-
-  @Override
-  public String toString() {
-    return "OpenSearchGetSnapshotResponse{" + "snapshotInfos=" + snapshotInfos + '}';
   }
 }
