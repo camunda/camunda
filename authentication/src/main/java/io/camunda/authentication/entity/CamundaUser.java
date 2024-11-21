d8335c7b728e4a3d2780149fd4a8419fbfd22964
Ben Sheppard <ben.sheppard@camunda.com>
2024-11-21 09:43:18 +0100

    refactor: add webapp user fields to central user object to support auth transition

--- authentication/src/main/java/io/camunda/authentication/entity/CamundaUser.java
+++ authentication/src/main/java/io/camunda/authentication/entity/CamundaUser.java
@@ -7,8 +7,13 @@
  */
 package io.camunda.authentication.entity;
 
+import io.camunda.search.entities.TenantEntity;
+import io.camunda.security.entity.ClusterMetadata;
+import io.camunda.security.entity.Permission;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -17,8 +22,13 @@ public class CamundaUser extends User {
 
   private final Long userKey;
   private final String displayName;
-
-  private boolean canLogout = true;
+  private List<Permission> permissions = List.of();
+  private List<TenantEntity> tenants = List.of();
+  private List<String> groups = List.of();
+  private String salesPlanType;
+  private Map<ClusterMetadata.AppName, String> c8Links = new HashMap<>();
+  private boolean canLogout;
+  private boolean apiUser;
 
   public CamundaUser(
       final Long userKey, final String displayName, final String username, final String password) {
@@ -35,6 +45,31 @@ public class CamundaUser extends User {
     super(username, password, prepareAuthorities(roles));
     userKey = null;
     this.displayName = displayName;
+  }
+
+  public CamundaUser(
+      final Long userKey,
+      final String displayName,
+      final String username,
+      final String password,
+      final List<String> roles,
+      final List<Permission> permissions,
+      final List<TenantEntity> tenants,
+      final List<String> groups,
+      final String salesPlanType,
+      final Map<ClusterMetadata.AppName, String> c8Links,
+      final boolean canLogout,
+      final boolean apiUser) {
+    super(username, password, prepareAuthorities(roles));
+    this.userKey = userKey;
+    this.displayName = displayName;
+    this.permissions = permissions;
+    this.tenants = tenants;
+    this.groups = groups;
+    this.salesPlanType = salesPlanType;
+    this.c8Links = c8Links;
+    this.canLogout = canLogout;
+    this.apiUser = apiUser;
   }
 
   public Long getUserKey() {
@@ -57,7 +92,23 @@ public class CamundaUser extends User {
     return getAuthorities().stream().map(GrantedAuthority::getAuthority).toList();
   }
 
-  public boolean isCanLogout() {
+  public List<Permission> getPermissions() {
+    return permissions;
+  }
+
+  public List<TenantEntity> getTenants() {
+    return tenants;
+  }
+
+  public String getSalesPlanType() {
+    return salesPlanType;
+  }
+
+  public Map<ClusterMetadata.AppName, String> getC8Links() {
+    return c8Links;
+  }
+
+  public boolean canLogout() {
     return canLogout;
   }
 
@@ -70,6 +121,14 @@ public class CamundaUser extends User {
     private String name;
     private String username;
     private String password;
+    private List<String> roles;
+    private List<Permission> permissions;
+    private List<TenantEntity> tenants;
+    private List<String> groups;
+    private String salesPlanType;
+    private Map<ClusterMetadata.AppName, String> c8Links;
+    private boolean canLogout;
+    private boolean apiUser;
 
     private CamundaUserBuilder() {}
 
@@ -97,8 +156,60 @@ public class CamundaUser extends User {
       return this;
     }
 
+    public CamundaUserBuilder withRoles(final List<String> roles) {
+      this.roles = roles;
+      return this;
+    }
+
+    public CamundaUserBuilder withPermissions(final List<Permission> permissions) {
+      this.permissions = permissions;
+      return this;
+    }
+
+    public CamundaUserBuilder withTenants(final List<TenantEntity> tenants) {
+      this.tenants = tenants;
+      return this;
+    }
+
+    public CamundaUserBuilder withGroups(final List<String> groups) {
+      this.groups = groups;
+      return this;
+    }
+
+    public CamundaUserBuilder withSalesPlanType(final String salesPlanType) {
+      this.salesPlanType = salesPlanType;
+      return this;
+    }
+
+    public CamundaUserBuilder withC8Links(final Map<ClusterMetadata.AppName, String> c8Links) {
+      this.c8Links = c8Links;
+      return this;
+    }
+
+    public CamundaUserBuilder withCanLogout(final boolean canLogout) {
+      this.canLogout = canLogout;
+      return this;
+    }
+
+    public CamundaUserBuilder withApiUser(final boolean apiUser) {
+      this.apiUser = apiUser;
+      return this;
+    }
+
     public CamundaUser build() {
-      return new CamundaUser(userKey, name, username, password);
+      return new CamundaUser(
+          userKey,
+          name,
+          username,
+          password,
+          roles,
+          permissions,
+          tenants,
+          groups,
+          salesPlanType,
+          c8Links,
+          canLogout,
+          apiUser);
     }
   }
 }
