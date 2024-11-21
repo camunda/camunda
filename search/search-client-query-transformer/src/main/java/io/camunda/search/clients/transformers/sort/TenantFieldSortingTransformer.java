94d81da40969ff857aaa74699eca7190cc46537b
Oleksii Ivanov <oleksii.ivanov-ext@camunda.com>
2024-11-21 18:56:10 +0200

    feat: add missing sorting and query Transformers

---
+++ search/search-client-query-transformer/src/main/java/io/camunda/search/clients/transformers/sort/TenantFieldSortingTransformer.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.search.clients.transformers.sort;
+
+import static io.camunda.webapps.schema.descriptors.usermanagement.index.TenantIndex.KEY;
+import static io.camunda.webapps.schema.descriptors.usermanagement.index.TenantIndex.NAME;
+import static io.camunda.webapps.schema.descriptors.usermanagement.index.TenantIndex.TENANT_ID;
+
+public class TenantFieldSortingTransformer implements FieldSortingTransformer {
+
+  @Override
+  public String apply(final String domainField) {
+    return switch (domainField) {
+      case "key" -> KEY;
+      case "tenantId" -> TENANT_ID;
+      case "name" -> NAME;
+      default -> throw new IllegalArgumentException("Unknown sortField: " + domainField);
+    };
+  }
+}
