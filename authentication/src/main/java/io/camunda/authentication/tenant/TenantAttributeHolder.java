dac068e6ed1cd9ee68cc923d1cf814c2347efbf3
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-11-22 10:53:09 +0100

    feat: populate tenant request attribute
    Instead of fetching tenants for the principal from the old identity
    service we now read them from the exported zeebe state.

--- authentication/src/main/java/io/camunda/authentication/tenant/TenantAttributeHolder.java
+++ authentication/src/main/java/io/camunda/authentication/tenant/TenantAttributeHolder.java
@@ -7,41 +7,32 @@
  */
 package io.camunda.authentication.tenant;
 
-import static io.camunda.zeebe.protocol.record.value.TenantOwned.DEFAULT_TENANT_IDENTIFIER;
 import static org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import org.springframework.web.context.request.RequestAttributes;
+import java.util.Objects;
+import javax.annotation.Nonnull;
 import org.springframework.web.context.request.RequestContextHolder;
 
+/**
+ * Provides static typed access to a thread-local request attribute that holds the list of
+ * authenticated tenant ids for the request.
+ */
 public final class TenantAttributeHolder {
   private static final String ATTRIBUTE_KEY = "io.camunda.zeebe.gateway.rest.tenantIds";
 
   private TenantAttributeHolder() {}
 
-  public static List<String> tenantIds() {
-    final var requestAttributes = getCurrentRequestAttributes();
-    return getTenantsOrDefaultTenant(requestAttributes);
+  public static List<String> getTenantIds() {
+    final Object tenantAttributeValue =
+        RequestContextHolder.currentRequestAttributes().getAttribute(ATTRIBUTE_KEY, SCOPE_REQUEST);
+    return (List<String>) tenantAttributeValue;
   }
 
-  public static void withTenantIds(final List<String> tenantIds) {
-    final var requestAttributes = getCurrentRequestAttributes();
-    final var tenantsOptional = Optional.ofNullable(tenantIds);
-    requestAttributes.setAttribute(ATTRIBUTE_KEY, tenantsOptional, SCOPE_REQUEST);
-  }
-
-  private static RequestAttributes getCurrentRequestAttributes() {
-    return RequestContextHolder.currentRequestAttributes();
-  }
-
-  private static List<String> getTenantsOrDefaultTenant(final RequestAttributes requestAttributes) {
-    final var tenants = requestAttributes.getAttribute(ATTRIBUTE_KEY, SCOPE_REQUEST);
-
-    if (tenants != null) {
-      return ((Optional<List<String>>) tenants).orElse(null);
-    } else {
-      return List.of(DEFAULT_TENANT_IDENTIFIER);
-    }
+  public static void setTenantIds(@Nonnull final Collection<String> tenantIds) {
+    Objects.requireNonNull(tenantIds, "tenantIds must not be null");
+    final var requestAttributes = RequestContextHolder.currentRequestAttributes();
+    requestAttributes.setAttribute(ATTRIBUTE_KEY, tenantIds.stream().toList(), SCOPE_REQUEST);
   }
 }
