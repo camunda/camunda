a0eb874734f325f1d8e702fa786f4c6367265296
Lena Schönburg <lena@fairburg.xyz>
2024-11-22 11:41:30 +0100

    feat: include decisions in reconstructed deployments

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/deployment/DeploymentResourceUtil.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/deployment/DeploymentResourceUtil.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.engine.state.deployment;
 
 import io.camunda.zeebe.engine.processing.deployment.ChecksumGenerator;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.DecisionRecord;
 import io.camunda.zeebe.protocol.impl.record.value.deployment.FormMetadataRecord;
 import io.camunda.zeebe.protocol.impl.record.value.deployment.ProcessMetadata;
 import io.camunda.zeebe.util.buffer.BufferUtil;
@@ -35,5 +36,16 @@ public final class DeploymentResourceUtil {
     target.setTenantId(source.getTenantId());
     target.setDeploymentKey(source.getDeploymentKey());
     target.setChecksum(checksumGenerator.checksum(source.getResource()));
+  }
+
+  public void applyDecisionMetadata(final PersistedDecision source, final DecisionRecord target) {
+    target.setDecisionKey(source.getDecisionKey());
+    target.setDecisionId(BufferUtil.bufferAsString(source.getDecisionId()));
+    target.setDecisionName(BufferUtil.bufferAsString(source.getDecisionName()));
+    target.setVersion(source.getVersion());
+    target.setDecisionRequirementsKey(source.getDecisionRequirementsKey());
+    target.setDecisionRequirementsId(BufferUtil.bufferAsString(source.getDecisionRequirementsId()));
+    target.setTenantId(source.getTenantId());
+    target.setDeploymentKey(source.getDeploymentKey());
   }
 }
