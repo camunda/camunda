8e14cb45ec57763aba7ee7e22a4530f2f9938abf
Ivan Karpov <ivan.karpov-ext@camunda.com>
2024-11-22 10:57:15 +0100

    feat: fix comments

--- optimize/backend/src/test/java/io/camunda/optimize/service/util/BpmnModelUtilTest.java
+++ optimize/backend/src/test/java/io/camunda/optimize/service/util/BpmnModelUtilTest.java
@@ -7,6 +7,8 @@
  */
 package io.camunda.optimize.service.util;
 
+import static io.camunda.optimize.util.ZeebeBpmnModels.END_EVENT;
+import static io.camunda.optimize.util.ZeebeBpmnModels.SERVICE_TASK;
 import static io.camunda.optimize.util.ZeebeBpmnModels.START_EVENT;
 import static io.camunda.optimize.util.ZeebeBpmnModels.USER_TASK;
 import static io.camunda.optimize.util.ZeebeBpmnModels.createSimpleServiceTaskProcess;
@@ -25,6 +27,8 @@ import org.junit.jupiter.api.Test;
 public class BpmnModelUtilTest {
 
   public static final String CUSTOMER_ONBOARDING = "CustomerOnboarding";
+  public static final String END_EVENT_TYPE = "endEvent";
+  public static final String SERVICE_TASK_TYPE = "serviceTask";
 
   public static final String SIMPLE_SERVICE_TASK_PROCESS =
       Bpmn.convertToString(createSimpleServiceTaskProcess(CUSTOMER_ONBOARDING));
@@ -42,9 +46,22 @@ public class BpmnModelUtilTest {
         BpmnModelUtil.extractFlowNodeData(SIMPLE_SERVICE_TASK_PROCESS);
     assertThat(flowNodeData).isNotNull();
     assertThat(flowNodeData.isEmpty()).isFalse();
-    for (FlowNodeDataDto node : flowNodeData) {
-      assertThat(node.getId()).isNotNull();
-    }
+    FlowNodeDataDto endEventflowNodeDataDto = flowNodeData.get(0);
+    assertThat(endEventflowNodeDataDto.getId()).isNotNull();
+    assertThat(endEventflowNodeDataDto.getId()).isEqualTo(END_EVENT);
+    assertThat(endEventflowNodeDataDto.getType()).isEqualTo(END_EVENT_TYPE);
+
+    FlowNodeDataDto serviceTaskflowNodeDataDto = flowNodeData.get(1);
+    assertThat(serviceTaskflowNodeDataDto.getId()).isNotNull();
+    assertThat(serviceTaskflowNodeDataDto.getId()).isEqualTo(SERVICE_TASK);
+    assertThat(serviceTaskflowNodeDataDto.getName()).isEqualTo(SERVICE_TASK);
+    assertThat(serviceTaskflowNodeDataDto.getType()).isEqualTo(SERVICE_TASK_TYPE);
+
+    FlowNodeDataDto startEventflowNodeDataDto = flowNodeData.get(2);
+    assertThat(startEventflowNodeDataDto.getId()).isNotNull();
+    assertThat(startEventflowNodeDataDto.getId()).isEqualTo(START_EVENT);
+    assertThat(startEventflowNodeDataDto.getName()).isEqualTo(START_EVENT);
+    assertThat(startEventflowNodeDataDto.getType()).isEqualTo(START_EVENT);
   }
 
   @Test
