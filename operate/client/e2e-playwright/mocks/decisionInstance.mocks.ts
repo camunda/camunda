b2a1fbac58d4319a229cf6fd4a9dc62b19e7fbef
Patrick Dehn <patrick.dehn@camunda.com>
2024-11-26 17:53:08 +0100

    refactor: remove global read/write permissions

--- operate/client/e2e-playwright/mocks/decisionInstance.mocks.ts
+++ operate/client/e2e-playwright/mocks/decisionInstance.mocks.ts
@@ -159,7 +159,6 @@ function mockResponses({
           userId: 'demo',
           displayName: 'demo',
           canLogout: true,
-          permissions: ['read', 'write'],
           roles: null,
           salesPlanType: null,
           c8Links: {},
