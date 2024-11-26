45f1d750b5cdfc61cb763efe5e890383ce3c5b89
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-11-26 10:55:27 +0100

    refactor: make use of SearchQueryRequest::toBuilder

--- webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/index/DecisionRequirementsIndex.java
+++ webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/index/DecisionRequirementsIndex.java
@@ -9,7 +9,6 @@ package io.camunda.webapps.schema.descriptors.operate.index;
 
 import io.camunda.webapps.schema.descriptors.backup.Prio5Backup;
 import io.camunda.webapps.schema.descriptors.operate.OperateIndexDescriptor;
-import java.util.Optional;
 
 public class DecisionRequirementsIndex extends OperateIndexDescriptor implements Prio5Backup {
 
@@ -30,11 +29,6 @@ public class DecisionRequirementsIndex extends OperateIndexDescriptor implements
   @Override
   public String getIndexName() {
     return INDEX_NAME;
-  }
-
-  @Override
-  public Optional<String> getTenantIdField() {
-    return Optional.of(TENANT_ID);
   }
 
   @Override
--- webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/template/EventTemplate.java
+++ webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/template/EventTemplate.java
@@ -10,7 +10,6 @@ package io.camunda.webapps.schema.descriptors.operate.template;
 import io.camunda.webapps.schema.descriptors.backup.Prio4Backup;
 import io.camunda.webapps.schema.descriptors.operate.OperateTemplateDescriptor;
 import io.camunda.webapps.schema.descriptors.operate.ProcessInstanceDependant;
-import java.util.Optional;
 
 public class EventTemplate extends OperateTemplateDescriptor
     implements ProcessInstanceDependant, Prio4Backup {
@@ -56,11 +55,6 @@ public class EventTemplate extends OperateTemplateDescriptor
   @Override
   public String getIndexName() {
     return INDEX_NAME;
-  }
-
-  @Override
-  public Optional<String> getTenantIdField() {
-    return Optional.of(TENANT_ID);
   }
 
   @Override
