cd5eea20a34a79088b694bb09e57d4f449b61901
Oleksii Ivanov <oleksii.ivanov-ext@camunda.com>
2024-11-28 00:58:42 +0200

    test: add tests for deleting tenant

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/util/ZeebeAssertHelper.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/util/ZeebeAssertHelper.java
@@ -422,6 +422,19 @@ public final class ZeebeAssertHelper {
     consumer.accept(tenantRecordValue);
   }
 
+  public static void assertTenantDeleted(
+      final String tenantId, final Consumer<TenantRecordValue> consumer) {
+    final TenantRecordValue tenantRecordValue =
+        RecordingExporter.tenantRecords()
+            .withIntent(TenantIntent.DELETED)
+            .withTenantId(tenantId)
+            .getFirst()
+            .getValue();
+
+    assertThat(tenantRecordValue).isNotNull();
+    consumer.accept(tenantRecordValue);
+  }
+
   public static void assertMappingCreated(final String claimName, final String claimValue) {
     final MappingRecordValue mapping =
         RecordingExporter.mappingRecords()
