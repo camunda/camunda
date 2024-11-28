7caf6090ec189f84ec73cf1a3e10ac3169bbe63e
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2024-11-28 13:16:19 +0200

    fix: upsert variables to handle migrated intent

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/VariableHandler.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/VariableHandler.java
@@ -10,12 +10,16 @@ package io.camunda.exporter.handlers;
 import static io.camunda.exporter.utils.ExporterUtil.tenantOrDefault;
 
 import io.camunda.exporter.store.BatchRequest;
+import io.camunda.webapps.schema.descriptors.operate.template.VariableTemplate;
 import io.camunda.webapps.schema.entities.operate.VariableEntity;
 import io.camunda.webapps.schema.entities.operate.listview.VariableForListViewEntity;
 import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.VariableIntent;
 import io.camunda.zeebe.protocol.record.value.VariableRecordValue;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class VariableHandler implements ExportHandler<VariableEntity, VariableRecordValue> {
 
@@ -69,6 +73,34 @@ public class VariableHandler implements ExportHandler<VariableEntity, VariableRe
         .setName(recordValue.getName())
         .setTenantId(tenantOrDefault(recordValue.getTenantId()))
         .setPosition(record.getPosition());
+
+    if (!record.getIntent().name().equals(VariableIntent.MIGRATED.name())) {
+      setVariableValues(recordValue, entity);
+    }
+  }
+
+  @Override
+  public void flush(final VariableEntity entity, final BatchRequest batchRequest) {
+    final Map<String, Object> updateFields = new HashMap<>();
+
+    if (!entity.getValue().equals("null")) {
+      updateFields.put(VariableTemplate.VALUE, entity.getValue());
+      updateFields.put(VariableTemplate.FULL_VALUE, entity.getFullValue());
+      updateFields.put(VariableTemplate.IS_PREVIEW, entity.getIsPreview());
+    }
+    updateFields.put(VariableTemplate.PROCESS_DEFINITION_KEY, entity.getProcessDefinitionKey());
+    updateFields.put(VariableTemplate.BPMN_PROCESS_ID, entity.getBpmnProcessId());
+
+    batchRequest.upsert(indexName, entity.getId(), entity, updateFields);
+  }
+
+  @Override
+  public String getIndexName() {
+    return indexName;
+  }
+
+  private void setVariableValues(
+      final VariableRecordValue recordValue, final VariableEntity entity) {
     if (recordValue.getValue().length() > variableSizeThreshold) {
       entity.setValue(recordValue.getValue().substring(0, variableSizeThreshold));
       entity.setFullValue(recordValue.getValue());
@@ -78,15 +110,5 @@ public class VariableHandler implements ExportHandler<VariableEntity, VariableRe
       entity.setFullValue(null);
       entity.setIsPreview(false);
     }
-  }
-
-  @Override
-  public void flush(final VariableEntity entity, final BatchRequest batchRequest) {
-    batchRequest.add(indexName, entity);
-  }
-
-  @Override
-  public String getIndexName() {
-    return indexName;
   }
 }
