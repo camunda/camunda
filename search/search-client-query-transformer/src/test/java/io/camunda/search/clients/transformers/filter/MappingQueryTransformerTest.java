b522a27e05eaa096d862e8cf907c336208f39d10
Ben Sheppard <ben.sheppard@camunda.com>
2024-11-29 10:54:50 +0000

    test: refactor test to be simpler

--- search/search-client-query-transformer/src/test/java/io/camunda/search/clients/transformers/filter/MappingQueryTransformerTest.java
+++ search/search-client-query-transformer/src/test/java/io/camunda/search/clients/transformers/filter/MappingQueryTransformerTest.java
@@ -9,10 +9,7 @@ package io.camunda.search.clients.transformers.filter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.camunda.search.clients.query.SearchQueryOption;
-import io.camunda.search.clients.query.SearchTermQuery;
-import io.camunda.search.clients.query.SearchTermsQuery;
-import io.camunda.search.clients.types.TypedValue;
+import io.camunda.search.clients.query.SearchQuery;
 import io.camunda.search.filter.FilterBuilders;
 import io.camunda.search.filter.MappingFilter;
 import io.camunda.search.filter.MappingFilter.Builder;
@@ -29,29 +26,13 @@ public class MappingQueryTransformerTest extends AbstractTransformerTest {
   @ParameterizedTest
   @MethodSource("queryFilterParameters")
   public void shouldQueryByField(
-      final Function<Builder, ObjectBuilder<MappingFilter>> fn,
-      final String column,
-      final Object value) {
+      final Function<Builder, ObjectBuilder<MappingFilter>> fn, final SearchQuery expected) {
     // given
     final var filter = FilterBuilders.mapping(fn);
-
     // when
     final var searchRequest = transformQuery(filter);
 
-    // then
-    final var queryVariant = searchRequest.queryOption();
-
-    if (value instanceof final List<?> values) {
-      assertListValues(column, queryVariant, values);
-    } else {
-      assertThat(queryVariant)
-          .isInstanceOfSatisfying(
-              SearchTermQuery.class,
-              t -> {
-                assertThat(t.field()).isEqualTo(column);
-                assertThat(t.value().value()).isEqualTo(value);
-              });
-    }
+    assertThat(searchRequest).isEqualTo(expected);
   }
 
   public static Stream<Arguments> queryFilterParameters() {
@@ -59,27 +40,13 @@ public class MappingQueryTransformerTest extends AbstractTransformerTest {
         Arguments.of(
             (Function<Builder, ObjectBuilder<MappingFilter>>)
                 f -> f.claimNames(List.of("foo", "bar")),
-            "claimName",
-            List.of("foo", "bar")),
+            SearchQuery.of(
+                q -> q.terms(t -> t.field("claimName").stringTerms(List.of("foo", "bar"))))),
         Arguments.of(
             (Function<Builder, ObjectBuilder<MappingFilter>>) f -> f.claimName("barfoo"),
-            "claimName",
-            "barfoo"),
+            SearchQuery.of(q -> q.term(t -> t.field("claimName").value("barfoo")))),
         Arguments.of(
             (Function<Builder, ObjectBuilder<MappingFilter>>) f -> f.claimValue("foobar"),
-            "claimValue",
-            "foobar"));
-  }
-
-  private void assertListValues(
-      final String column, final SearchQueryOption queryVariant, final List<?> values) {
-    assertThat(queryVariant)
-        .isInstanceOfSatisfying(
-            SearchTermsQuery.class,
-            t -> {
-              assertThat(t.field()).isEqualTo(column);
-              assertThat(t.values().stream().map(TypedValue::stringValue).toList())
-                  .isEqualTo(values);
-            });
+            SearchQuery.of(q -> q.term(t -> t.field("claimValue").value("foobar")))));
   }
 }
