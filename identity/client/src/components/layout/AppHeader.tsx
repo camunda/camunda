36223e1f4c0954ce05ebbd7bed8a377c01123ab3
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-12-03 17:39:54 +0100

    feat: add user menu

--- identity/client/src/components/layout/AppHeader.tsx
+++ identity/client/src/components/layout/AppHeader.tsx
@@ -1,14 +1,22 @@
-import { C3Navigation } from "@camunda/camunda-composite-components";
-import { useGlobalRoutes } from "src/components/global/useGlobalRoutes";
-import { useNavigate } from "react-router";
-import { useApi } from "src/utility/api";
-import { checkLicense, License } from "src/utility/api/headers";
+import {C3Navigation} from "@camunda/camunda-composite-components";
+import {useGlobalRoutes} from "src/components/global/useGlobalRoutes";
+import {useNavigate} from "react-router";
+import {useApi} from "src/utility/api";
+import {checkLicense, License} from "src/utility/api/headers";
 import {baseUrl} from "src/configuration";
+import {getAuthentication} from "src/utility/api/authentication";
+import {useCallback} from "react";
+import {ArrowRight} from "@carbon/react/icons";
 
 const AppHeader = () => {
   const routes = useGlobalRoutes();
   const navigate = useNavigate();
   const { data: license } = useApi(checkLicense);
+  const { data: camundaUser } = useApi(getAuthentication);
+
+  const logout = useCallback(() => {
+    console.log("logout");
+  }, []);
 
   return (
     <C3Navigation
@@ -31,6 +39,56 @@ const AppHeader = () => {
           },
         })),
         licenseTag: getLicenseTag(license),
+      }}
+      userSideBar={{
+        version: import.meta.env.VITE_APP_VERSION,
+        ariaLabel: "Settings",
+        customElements: {
+          profile: {
+            label: "Profile",
+            user: {
+              name: camundaUser?.displayName ?? "",
+              email: "",
+            },
+          },
+        },
+        elements: [
+          {
+            key: "terms",
+            label: "Terms of use",
+            onClick: () => {
+              window.open(
+                "https://camunda.com/legal/terms/camunda-platform/camunda-platform-8-saas-trial/",
+                "_blank",
+              );
+            },
+          },
+          {
+            key: "privacy",
+            label: "Privacy policy",
+            onClick: () => {
+              window.open("https://camunda.com/legal/privacy/", "_blank");
+            },
+          },
+          {
+            key: "imprint",
+            label: "Imprint",
+            onClick: () => {
+              window.open("https://camunda.com/legal/imprint/", "_blank");
+            },
+          },
+        ],
+        bottomElements: camundaUser?.canLogout
+          ? [
+              {
+                key: "logout",
+                label: "Log out",
+                renderIcon: ArrowRight,
+                kind: "ghost",
+                onClick: logout,
+              },
+            ]
+          : undefined,
       }}
     />
   );
