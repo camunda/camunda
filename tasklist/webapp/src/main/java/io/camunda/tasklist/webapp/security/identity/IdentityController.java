a077547ef170159bec61db8f9666fdebb8187651
Houssain Barouni <houssain.barouni@camunda.com>
2024-12-03 17:32:33 +0100

    fix: add context path to Tasklist redirection urls #25534
    (cherry picked from commit 9979846b874540e46a4adb292589b135b4991c9a)

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/security/identity/IdentityController.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/security/identity/IdentityController.java
@@ -107,9 +107,9 @@ public class IdentityController {
       throws IOException {
     final Object originalRequestUrl = req.getSession().getAttribute(REQUESTED_URL);
     if (originalRequestUrl != null) {
-      res.sendRedirect(originalRequestUrl.toString());
+      res.sendRedirect(req.getContextPath() + originalRequestUrl);
     } else {
-      res.sendRedirect(ROOT);
+      res.sendRedirect(req.getContextPath() + ROOT);
     }
   }
 
