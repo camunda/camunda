9ca7fa56aa327d7dbc0caad797213cb6604223f9
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-12-04 13:06:56 +0100

    feat: annotate index descriptors with tenant id fields
    Given an index descriptor, we need to know whether queries must apply
    tenant filtering. For now, we return a field name. In the future, we
    might change this to return a filter given a collection of tenant ids.

--- webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/index/DecisionIndex.java
+++ webapps-schema/src/main/java/io/camunda/webapps/schema/descriptors/operate/index/DecisionIndex.java
@@ -9,6 +9,7 @@ package io.camunda.webapps.schema.descriptors.operate.index;
 
 import io.camunda.webapps.schema.descriptors.backup.Prio5Backup;
 import io.camunda.webapps.schema.descriptors.operate.OperateIndexDescriptor;
+import java.util.Optional;
 
 public class DecisionIndex extends OperateIndexDescriptor implements Prio5Backup {
 
@@ -28,6 +29,11 @@ public class DecisionIndex extends OperateIndexDescriptor implements Prio5Backup
   @Override
   public String getIndexName() {
     return INDEX_NAME;
+  }
+
+  @Override
+  public Optional<String> getTenantIdField() {
+    return Optional.of(TENANT_ID);
   }
 
   @Override
