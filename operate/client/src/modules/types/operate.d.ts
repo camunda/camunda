9b2cf4fb6d0d9b736c417e6c9613dd2c0ce8c9e5
vitorwtavares <vitor.tavares@camunda.com>
2024-12-05 20:49:53 -0300

    feat: add event type values to listeners entity on frontend

--- operate/client/src/modules/types/operate.d.ts
+++ operate/client/src/modules/types/operate.d.ts
@@ -106,7 +106,13 @@ interface ListenerEntity {
   listenerKey: string;
   state: 'ACTIVE' | 'COMPLETED' | 'FAILED';
   jobType: string;
-  event: 'UNSPECIFIED' | 'START' | 'END';
+  event:
+    | 'START'
+    | 'END'
+    | 'COMPLETE'
+    | 'ASSIGNMENT'
+    | 'UNKNOWN'
+    | 'UNSPECIFIED';
   time: string;
   sortValues: ReadonlyArray<string>;
 }
