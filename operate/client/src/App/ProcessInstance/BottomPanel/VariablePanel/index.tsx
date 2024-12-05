dcb1907580698d19070b1ae37836c2c215d563f7
vitorwtavares <vitor.tavares@camunda.com>
2024-12-05 20:22:22 -0300

    test: add e2e test case to cover listeners on processes and participants

--- operate/client/src/App/ProcessInstance/BottomPanel/VariablePanel/index.tsx
+++ operate/client/src/App/ProcessInstance/BottomPanel/VariablePanel/index.tsx
@@ -36,8 +36,6 @@ const VariablePanel = observer(function VariablePanel() {
   const {listeners, listenerTypeFilter} = state;
 
   const shouldUseFlowNodeId = !flowNodeInstanceId && flowNodeId;
-  const shouldUseFlowNodeInstanceId =
-    flowNodeInstanceId && !flowNodeSelectionStore.isRootNodeSelected;
 
   useEffect(() => {
     reset();
@@ -61,7 +59,7 @@ const VariablePanel = observer(function VariablePanel() {
           ...(listenerTypeFilter && {listenerTypeFilter}),
         },
       });
-    } else if (shouldUseFlowNodeInstanceId) {
+    } else if (flowNodeInstanceId) {
       fetchListeners({
         fetchType: 'initial',
         processInstanceId: processInstanceId,
@@ -77,7 +75,6 @@ const VariablePanel = observer(function VariablePanel() {
     flowNodeId,
     flowNodeInstanceId,
     shouldUseFlowNodeId,
-    shouldUseFlowNodeInstanceId,
     listenerTypeFilter,
   ]);
 
