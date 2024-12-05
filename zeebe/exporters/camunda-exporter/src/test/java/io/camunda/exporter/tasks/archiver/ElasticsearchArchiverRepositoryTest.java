9b68ba0e9b571d898a8f089221f97a807917a92f
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2024-12-05 16:23:42 +0100

    fix: adjust tests.

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
@@ -15,7 +15,6 @@ import co.elastic.clients.transport.rest_client.RestClientTransport;
 import io.camunda.exporter.config.ExporterConfiguration.ArchiverConfiguration;
 import io.camunda.exporter.config.ExporterConfiguration.RetentionConfiguration;
 import io.camunda.exporter.metrics.CamundaExporterMetrics;
-import io.camunda.search.connect.configuration.ConnectConfiguration;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import java.time.Duration;
 import org.apache.http.HttpHost;
@@ -68,7 +67,7 @@ final class ElasticsearchArchiverRepositoryTest {
         1,
         new ArchiverConfiguration(),
         retention,
-        new ConnectConfiguration(),
+        "testPrefix",
         "instance",
         "batch",
         client,
