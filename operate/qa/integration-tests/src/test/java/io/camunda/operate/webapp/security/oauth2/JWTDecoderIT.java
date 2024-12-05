63792ebc8b99e989842f4568c72b46416ef0ad84
Kristin Komschow <162317489+kristinkomschow@users.noreply.github.com>
2024-12-05 16:55:11 +0100

    test: add wiremock for authserver and attempt to remove alg field

--- operate/qa/integration-tests/src/test/java/io/camunda/operate/webapp/security/oauth2/JWTDecoderIT.java
+++ operate/qa/integration-tests/src/test/java/io/camunda/operate/webapp/security/oauth2/JWTDecoderIT.java
@@ -1,5 +1,11 @@
 package io.camunda.operate.webapp.security.oauth2;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JOSEObjectType;
 import com.nimbusds.jose.JWSAlgorithm;
@@ -8,12 +14,15 @@ import com.nimbusds.jose.JWSSigner;
 import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
+import com.nimbusds.jose.util.Base64;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import io.camunda.identity.sdk.IdentityConfiguration;
 import java.util.Date;
 import java.util.List;
+import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.env.MockEnvironment;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -21,47 +30,40 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 // TODO: Shhould this be IT or Test?
 public class JWTDecoderIT {
 
+  private static final int mockAuthServerPort = 19602;
+  @Rule public WireMockRule wireMockRule = new WireMockRule(mockAuthServerPort);
+
   @Test
   public void testDecodeJwt() throws JOSEException {
+
     // given
-    MockEnvironment environment = new MockEnvironment();
-    IdentityConfiguration identityConfiguration =
-        new IdentityConfiguration(
-            null, "http://localhost:18080/auth/realms/camunda-platform", null, null, null);
+    final String authServerMockUrl = urlFor("/mockAuthServer");
 
-    JwtDecoder decoder = IdentityOAuth2WebConfigurer.jwtDecoder(environment, identityConfiguration);
+    final MockEnvironment environment = new MockEnvironment();
 
-    // // JWK jwk = new RSAKey.Builder(publicKey).privateKey(privateKey).build();
-    // // JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-    // JwtEncoder encoder = new NimbusJwtEncoder(new
-    // ImmutableSecret<>("bar".getBytes()));
-    //
-    // JwsHeader jwsHeader = JwsHeader.with(SignatureAlgorithm.RS256).build();
-    // JwtClaimsSet claimsSet = JwtClaimsSet.builder().claim("foo", "bar").build();
-    // JwtEncoderParameters parameters = JwtEncoderParameters.from(jwsHeader,
-    // claimsSet);
-    //
-    // Jwt encodedJwt = encoder.encode(parameters);
-    //
-    // Jwt token = buildJWT();
+    final IdentityConfiguration identityConfiguration =
+        new IdentityConfiguration(null, authServerMockUrl, null, null, null);
+
+    final JwtDecoder decoder =
+        IdentityOAuth2WebConfigurer.jwtDecoder(environment, identityConfiguration);
 
     // RSA signatures require a public and private RSA key pair, the public key
     // must be made known to the JWS recipient in order to verify the signatures
-    RSAKey rsaJWK = new RSAKeyGenerator(2048).keyID("123").generate();
-    RSAKey rsaPublicJWK = rsaJWK.toPublicJWK();
+    final RSAKey rsaJWK = new RSAKeyGenerator(2048).keyID("123").generate();
+    final RSAKey rsaPublicJWK = rsaJWK.toPublicJWK();
 
     // Create RSA-signer with the private key
-    JWSSigner signer = new RSASSASigner(rsaJWK);
+    final JWSSigner signer = new RSASSASigner(rsaJWK);
 
     // Prepare JWT with claims set
-    JWTClaimsSet claimsSet =
+    final JWTClaimsSet claimsSet =
         new JWTClaimsSet.Builder()
             .subject("alice")
             .issuer("https://c2id.com")
             .expirationTime(new Date(new Date().getTime() + 60 * 1000))
             .build();
 
-    SignedJWT signedJWT =
+    final SignedJWT signedJWT =
         new SignedJWT(
             new JWSHeader.Builder(JWSAlgorithm.RS256)
                 .type(JOSEObjectType.JWT)
@@ -70,13 +72,23 @@ public class JWTDecoderIT {
             claimsSet);
 
     signedJWT.sign(signer);
-    String serializedJwt = signedJWT.serialize();
+    signedJWT.getHeader().getAlgorithm();
+    final String serializedJwt = signedJWT.serialize();
     System.out.println(serializedJwt);
 
-    // JwtEncoder encoder =
+    final String authServerResponseBody = "{\"keys\":[" + rsaPublicJWK.toJSONString() + "]}";
+    stubFor(
+        get(urlMatching(".*/mockAuthServer/protocol/openid-connect/certs"))
+            .willReturn(
+                jsonResponse(authServerResponseBody, HttpStatus.OK.value())
+                    .withHeader("TODO", "test"))); // TODO ?
 
+    final String header = "{\"kid\": \"123\", \"typ\": \"JWT\"}"; // TODO generalize maybe
+    final String withoutAlg =
+        serializedJwt.replaceFirst("[^.]*", Base64.encode(header).toString()).replaceAll("=", "");
+    System.out.println(withoutAlg);
     // when
-    decoder.decode(serializedJwt);
+    decoder.decode(serializedJwt); // TODO use withoutAlg once it works
   }
 
   private Jwt buildJWT() {
@@ -85,5 +97,9 @@ public class JWTDecoderIT {
         .header("alg", "HS256")
         .claim("scope", "scope")
         .build();
+  }
+
+  private String urlFor(final String path) {
+    return String.format("http://localhost:%d%s", mockAuthServerPort, path);
   }
 }
