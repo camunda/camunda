8a50540f9642ce2f767db7d7604520c6d9660787
Nico Korthout <nico.korthout@camunda.com>
2024-12-05 11:14:01 +0100

    refactor: use enhanced switch statement
    We have access to enhanced switch statements in the engine code, which
    provide multiple improvements over the legacy switch statements. For
    example, we don't have to explicitly return at the end of a block, as
    the cases don't fall through.
    
    This refactoring was applied by IntelliJ-IDEA. It may be hard to review
    by a person. Please note that I did not make any other changes in this
    commit other than the automated replacement between switch case styles.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/job/JobCompleteProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/job/JobCompleteProcessor.java
@@ -83,58 +83,55 @@ public final class JobCompleteProcessor implements CommandProcessor<JobRecord> {
     }
 
     switch (value.getJobKind()) {
-      case EXECUTION_LISTENER:
-        {
-          // to store the variable for merge, to handle concurrent commands
-          eventHandle.triggeringProcessEvent(value);
+      case EXECUTION_LISTENER -> {
+        // to store the variable for merge, to handle concurrent commands
+        eventHandle.triggeringProcessEvent(value);
 
+        commandWriter.appendFollowUpCommand(
+            elementInstanceKey,
+            ProcessInstanceIntent.COMPLETE_EXECUTION_LISTENER,
+            elementInstance.getValue());
+        return;
+      }
+      case TASK_LISTENER -> {
+        /*
+         We retrieve the intermediate user task state rather than the regular user task record
+         because the intermediate state captures the exact data provided during the original
+         user task command (e.g., COMPLETE, ASSIGN). This data includes variables, actions,
+         and other command-related details that may not yet be reflected in the persisted user
+         task record, that can be accessed via `userTaskState.getUserTask`.
+
+         When task listeners are involved, it's essential to preserve this original state
+         until all task listeners have been executed. Retrieving the intermediate state here
+         ensures that the finalization of the user task command uses the correct, unmodified
+         data as originally intended by the user. Once all task listeners have been processed
+         and the original user task command is finalized, the intermediate state is cleared.
+        */
+        final var userTask =
+            userTaskState.getIntermediateState(elementInstance.getUserTaskKey()).getRecord();
+
+        if (value.getResult().isDenied()) {
+          commandWriter.appendFollowUpCommand(
+              userTask.getUserTaskKey(), UserTaskIntent.DENY_TASK_LISTENER, userTask);
+        } else {
+          commandWriter.appendFollowUpCommand(
+              userTask.getUserTaskKey(), UserTaskIntent.COMPLETE_TASK_LISTENER, userTask);
+        }
+
+        return;
+      }
+      default -> {
+        final long scopeKey = elementInstance.getValue().getFlowScopeKey();
+        final ElementInstance scopeInstance = elementInstanceState.getInstance(scopeKey);
+
+        if (scopeInstance != null && scopeInstance.isActive()) {
+          eventHandle.triggeringProcessEvent(value);
           commandWriter.appendFollowUpCommand(
               elementInstanceKey,
-              ProcessInstanceIntent.COMPLETE_EXECUTION_LISTENER,
+              ProcessInstanceIntent.COMPLETE_ELEMENT,
               elementInstance.getValue());
-          return;
         }
-      case TASK_LISTENER:
-        {
-          /*
-           We retrieve the intermediate user task state rather than the regular user task record
-           because the intermediate state captures the exact data provided during the original
-           user task command (e.g., COMPLETE, ASSIGN). This data includes variables, actions,
-           and other command-related details that may not yet be reflected in the persisted user
-           task record, that can be accessed via `userTaskState.getUserTask`.
-
-           When task listeners are involved, it's essential to preserve this original state
-           until all task listeners have been executed. Retrieving the intermediate state here
-           ensures that the finalization of the user task command uses the correct, unmodified
-           data as originally intended by the user. Once all task listeners have been processed
-           and the original user task command is finalized, the intermediate state is cleared.
-          */
-          final var userTask =
-              userTaskState.getIntermediateState(elementInstance.getUserTaskKey()).getRecord();
-
-          if (value.getResult().isDenied()) {
-            commandWriter.appendFollowUpCommand(
-                userTask.getUserTaskKey(), UserTaskIntent.DENY_TASK_LISTENER, userTask);
-          } else {
-            commandWriter.appendFollowUpCommand(
-                userTask.getUserTaskKey(), UserTaskIntent.COMPLETE_TASK_LISTENER, userTask);
-          }
-
-          return;
-        }
-      default:
-        {
-          final long scopeKey = elementInstance.getValue().getFlowScopeKey();
-          final ElementInstance scopeInstance = elementInstanceState.getInstance(scopeKey);
-
-          if (scopeInstance != null && scopeInstance.isActive()) {
-            eventHandle.triggeringProcessEvent(value);
-            commandWriter.appendFollowUpCommand(
-                elementInstanceKey,
-                ProcessInstanceIntent.COMPLETE_ELEMENT,
-                elementInstance.getValue());
-          }
-        }
+      }
     }
   }
 
