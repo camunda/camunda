477b9348e8bb16ad57f94b07ee19c28792964312
berkaycanbc <berkay.can@camunda.com>
2024-12-06 12:29:40 +0100

    feat: add parallel gateway to supported BPMN elements
    In order to support migration for active parallel gateways we need to add parallel gateway to
    supported element types. No other change is needed for incident migration because it is already
    handled as a part of the incident migration implementation.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceMigrationPreconditions.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceMigrationPreconditions.java
@@ -57,7 +57,8 @@ public final class ProcessInstanceMigrationPreconditions {
           BpmnElementType.BUSINESS_RULE_TASK,
           BpmnElementType.SCRIPT_TASK,
           BpmnElementType.SEND_TASK,
-          BpmnElementType.MULTI_INSTANCE_BODY);
+          BpmnElementType.MULTI_INSTANCE_BODY,
+          BpmnElementType.PARALLEL_GATEWAY);
   private static final Set<BpmnElementType> UNSUPPORTED_ELEMENT_TYPES =
       EnumSet.complementOf(SUPPORTED_ELEMENT_TYPES);
   private static final Set<BpmnEventType> SUPPORTED_INTERMEDIATE_CATCH_EVENT_TYPES =
