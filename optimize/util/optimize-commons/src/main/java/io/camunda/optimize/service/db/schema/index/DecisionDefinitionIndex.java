5f94944f976a928ade2249b09bc8ff0a006e9812
Carlo Sana <carlo.sana@camunda.com>
2024-12-06 13:36:08 +0100

    feat: Optimize static indices extends Prio{1,6}backup interface
    closes #24464

--- optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/db/schema/index/DecisionDefinitionIndex.java
+++ optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/db/schema/index/DecisionDefinitionIndex.java
@@ -9,8 +9,10 @@ package io.camunda.optimize.service.db.schema.index;
 
 import co.elastic.clients.elasticsearch._types.mapping.TypeMapping;
 import io.camunda.optimize.service.db.DatabaseConstants;
+import io.camunda.webapps.schema.descriptors.backup.Prio6Backup;
 
-public abstract class DecisionDefinitionIndex<TBuilder> extends AbstractDefinitionIndex<TBuilder> {
+public abstract class DecisionDefinitionIndex<TBuilder> extends AbstractDefinitionIndex<TBuilder>
+    implements Prio6Backup {
 
   public static final int VERSION = 5;
 
