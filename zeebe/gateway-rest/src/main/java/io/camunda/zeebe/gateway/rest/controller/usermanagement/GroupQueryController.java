cc964553f1b300fc65c5736f7c86077c6aea255a
Nikola Koevski <nikola.koevski@camunda.com>
2024-12-06 11:54:53 +0100

    feat: add endpoint to retrieve group-assigned users
    Add a REST API endpoint to retrieve users assigned
    to a group with a given group key.

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/usermanagement/GroupQueryController.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/usermanagement/GroupQueryController.java
@@ -7,14 +7,22 @@
  */
 package io.camunda.zeebe.gateway.rest.controller.usermanagement;
 
+import io.camunda.search.entities.GroupEntity;
 import io.camunda.search.query.GroupQuery;
+import io.camunda.search.query.SearchQueryBuilders;
+import io.camunda.search.query.UserQuery;
 import io.camunda.service.GroupServices;
+import io.camunda.service.UserServices;
 import io.camunda.zeebe.gateway.protocol.rest.GroupSearchQueryRequest;
 import io.camunda.zeebe.gateway.protocol.rest.GroupSearchQueryResponse;
+import io.camunda.zeebe.gateway.protocol.rest.UserSearchQueryRequest;
+import io.camunda.zeebe.gateway.protocol.rest.UserSearchResponse;
 import io.camunda.zeebe.gateway.rest.RestErrorMapper;
 import io.camunda.zeebe.gateway.rest.SearchQueryRequestMapper;
 import io.camunda.zeebe.gateway.rest.SearchQueryResponseMapper;
 import io.camunda.zeebe.gateway.rest.controller.CamundaRestController;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,9 +36,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class GroupQueryController {
 
   private final GroupServices groupServices;
+  private final UserServices userServices;
 
-  public GroupQueryController(final GroupServices groupServices) {
+  public GroupQueryController(final GroupServices groupServices, final UserServices userServices) {
     this.groupServices = groupServices;
+    this.userServices = userServices;
   }
 
   @GetMapping(
@@ -55,10 +65,39 @@ public class GroupQueryController {
         .fold(RestErrorMapper::mapProblemToResponse, this::search);
   }
 
+  @PostMapping(
+      path = "/{groupKey}/users/search",
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE},
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<UserSearchResponse> searchUsersInGroup(
+      @PathVariable final long groupKey,
+      @RequestBody(required = false) final UserSearchQueryRequest query) {
+    final var groupMemberKeys =
+        groupServices
+            .search(
+                SearchQueryBuilders.groupSearchQuery().filter(f -> f.groupKey(groupKey)).build())
+            .items()
+            .stream()
+            .map(GroupEntity::assignedMemberKeys)
+            .flatMap(Set::stream)
+            .collect(Collectors.toSet());
+    return SearchQueryRequestMapper.toUserQuery(query, groupMemberKeys)
+        .fold(RestErrorMapper::mapProblemToResponse, this::searchUsersInGroup);
+  }
+
   private ResponseEntity<GroupSearchQueryResponse> search(final GroupQuery query) {
     try {
       final var result = groupServices.search(query);
       return ResponseEntity.ok(SearchQueryResponseMapper.toGroupSearchQueryResponse(result));
+    } catch (final Exception e) {
+      return RestErrorMapper.mapErrorToResponse(e);
+    }
+  }
+
+  private ResponseEntity<UserSearchResponse> searchUsersInGroup(final UserQuery query) {
+    try {
+      final var result = userServices.search(query);
+      return ResponseEntity.ok(SearchQueryResponseMapper.toUserSearchQueryResponse(result));
     } catch (final Exception e) {
       return RestErrorMapper.mapErrorToResponse(e);
     }
