84a31b4b70a9ff206ecef80062f800cea6ccde00
Mustafa Dagher <mustafa.dagher@camunda.com>
2024-12-06 17:24:30 +0100

    fix: bring back `createSchema` to SearchTestRuleProviders

--- operate/qa/integration-tests/src/test/java/io/camunda/operate/util/ElasticsearchTestRuleProvider.java
+++ operate/qa/integration-tests/src/test/java/io/camunda/operate/util/ElasticsearchTestRuleProvider.java
@@ -16,6 +16,7 @@ import io.camunda.operate.conditions.ElasticsearchCondition;
 import io.camunda.operate.exceptions.PersistenceException;
 import io.camunda.operate.property.OperateElasticsearchProperties;
 import io.camunda.operate.property.OperateProperties;
+import io.camunda.operate.schema.SchemaManager;
 import io.camunda.operate.zeebe.ImportValueType;
 import io.camunda.operate.zeebeimport.RecordsReader;
 import io.camunda.operate.zeebeimport.RecordsReaderHolder;
@@ -111,6 +112,7 @@ public class ElasticsearchTestRuleProvider implements SearchTestRuleProvider {
   @Autowired private DecisionInstanceTemplate decisionInstanceTemplate;
   @Autowired private DecisionRequirementsIndex decisionRequirementsIndex;
   @Autowired private DecisionIndex decisionIndex;
+  @Autowired private SchemaManager schemaManager;
   @Autowired private IndexPrefixHolder indexPrefixHolder;
 
   @Autowired private ObjectMapper objectMapper;
@@ -129,6 +131,12 @@ public class ElasticsearchTestRuleProvider implements SearchTestRuleProvider {
       indexPrefix =
           Optional.ofNullable(indexPrefixHolder.createNewIndexPrefix()).orElse(indexPrefix);
       operateProperties.getElasticsearch().setIndexPrefix(indexPrefix);
+    }
+    if (operateProperties.getElasticsearch().isCreateSchema()) {
+      schemaManager.createSchema();
+      assertThat(areIndicesCreatedAfterChecks(indexPrefix, 5, 5 * 60 /*sec*/))
+          .describedAs("Elasticsearch %s (min %d) indices are created", indexPrefix, 5)
+          .isTrue();
     }
   }
 
