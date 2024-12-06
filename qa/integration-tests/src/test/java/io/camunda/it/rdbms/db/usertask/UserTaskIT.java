23cf55892b34730d6f69b7bcce9c9c8e585a81a9
Nathanyel Sandi <nathan.sandi@gmail.com>
2024-12-06 09:52:02 -0300

    feat: add rdms test

--- qa/integration-tests/src/test/java/io/camunda/it/rdbms/db/usertask/UserTaskIT.java
+++ qa/integration-tests/src/test/java/io/camunda/it/rdbms/db/usertask/UserTaskIT.java
@@ -174,7 +174,7 @@ public class UserTaskIT {
   }
 
   @TestTemplate
-  public void shouldFindUserTaskByVariableName(final CamundaRdbmsTestApplication testApplication) {
+  public void shouldFindUserTaskByProcessInstanceVariableName(final CamundaRdbmsTestApplication testApplication) {
     final RdbmsService rdbmsService = testApplication.getRdbmsService();
 
     final UserTaskDbModel randomizedUserTask = UserTaskFixtures.createRandomized();
@@ -194,6 +194,41 @@ public class UserTaskIT {
                 new UserTaskDbQuery(
                     new UserTaskFilter.Builder()
                         .processInstanceVariables(
+                            List.of(
+                                new VariableValueFilter.Builder()
+                                    .name(randomizedVariable.name())
+                                    .build()))
+                        .build(),
+                    UserTaskSort.of(b -> b),
+                    SearchQueryPage.of(b -> b.from(0).size(10))));
+
+    assertThat(searchResult).isNotNull();
+    assertThat(searchResult.total()).isEqualTo(1);
+    assertThat(searchResult.hits()).hasSize(1);
+    assertUserTaskEntity(searchResult.hits().getFirst(), randomizedUserTask);
+  }
+
+  @TestTemplate
+  public void shouldFindUserTaskByLocalVariableName(final CamundaRdbmsTestApplication testApplication) {
+    final RdbmsService rdbmsService = testApplication.getRdbmsService();
+
+    final UserTaskDbModel randomizedUserTask = UserTaskFixtures.createRandomized();
+    createAndSaveUserTask(rdbmsService, randomizedUserTask);
+
+    final VariableDbModel randomizedVariable =
+        VariableFixtures.createRandomized(
+            b -> b.processInstanceKey(randomizedUserTask.processInstanceKey()));
+    final RdbmsWriter writer = rdbmsService.createWriter(1L);
+    writer.getVariableWriter().create(randomizedVariable);
+    writer.flush();
+
+    final var searchResult =
+        rdbmsService
+            .getUserTaskReader()
+            .search(
+                new UserTaskDbQuery(
+                    new UserTaskFilter.Builder()
+                        .localVariables(
                             List.of(
                                 new VariableValueFilter.Builder()
                                     .name(randomizedVariable.name())
