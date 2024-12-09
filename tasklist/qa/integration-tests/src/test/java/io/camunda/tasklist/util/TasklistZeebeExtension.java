1b4036f62d7f6052ba19120966d343f45d9bc3d6
Christopher Zell <zelldon91@googlemail.com>
2024-12-09 13:58:23 +0100

    refactor: adjust prefix and schema creation
     * Prefixes are correctly set once, and also set in properties
     * Prefixholder is used to reclaim prefix
     * Schema is not created separatelty - it is done by exporter

--- tasklist/qa/integration-tests/src/test/java/io/camunda/tasklist/util/TasklistZeebeExtension.java
+++ tasklist/qa/integration-tests/src/test/java/io/camunda/tasklist/util/TasklistZeebeExtension.java
@@ -10,6 +10,7 @@ package io.camunda.tasklist.util;
 import io.camunda.client.CamundaClient;
 import io.camunda.tasklist.property.TasklistProperties;
 import io.camunda.tasklist.qa.util.ContainerVersionsUtil;
+import io.camunda.tasklist.qa.util.TasklistIndexPrefixHolder;
 import io.camunda.tasklist.webapp.security.TasklistProfileService;
 import io.zeebe.containers.ZeebeContainer;
 import java.net.URI;
@@ -41,6 +42,7 @@ public abstract class TasklistZeebeExtension
   private static ContainerPoolManager<ZeebeContainer> zeebeContainerContainerPoolManager;
 
   @Autowired protected TasklistProperties tasklistProperties;
+  @Autowired protected TasklistIndexPrefixHolder indexPrefixHolder;
 
   protected ZeebeContainer zeebeContainer;
 
@@ -122,7 +124,7 @@ public abstract class TasklistZeebeExtension
     final String zeebeVersion =
         ContainerVersionsUtil.readProperty(
             ContainerVersionsUtil.ZEEBE_CURRENTVERSION_DOCKER_PROPERTY_NAME);
-    final String indexPrefix = tasklistProperties.getElasticsearch().getIndexPrefix();
+    final String indexPrefix = indexPrefixHolder.getIndexPrefix();
     LOGGER.info(
         "************ Starting Zeebe:{}, indexPrefix={} ************", zeebeVersion, indexPrefix);
     final ZeebeContainer zContainer =
