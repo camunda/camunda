e05ef0bd8019accb4412b43cdd371ff083a6e451
Mattia Buccarella <mattia.buccarella@camunda.com>
2024-12-10 12:12:00 +0100

    build: LogUnit no more used

--- optimize/util/optimize-commons/src/main/java/io/camunda/optimize/util/LogUtil.java
+++ optimize/util/optimize-commons/src/main/java/io/camunda/optimize/util/LogUtil.java
@@ -7,46 +7,9 @@
  */
 package io.camunda.optimize.util;
 
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
-
-public class LogUtil extends LogEventPatternConverter {
-
-  protected LogUtil(final String name, final String style) {
-    super(name, style);
-  }
-
-  public static LogUtil newInstance() {
-    return new LogUtil("LogUtil", "custom");
-  }
-
-  @Override
-  public void format(final LogEvent event, final StringBuilder output) {
-    final String message = event.getMessage().getFormattedMessage();
-    output.append(transformString(message));
-  }
+public class LogUtil {
 
   public static String sanitizeLogMessage(final String input) {
     return input.replaceAll("[\n|\r|\t]", "_");
-  }
-
-  private static String transformString(final String s) {
-    return s
-        // newline
-        .replace("\n", "\\n")
-        // carriage return
-        .replace("\r", "\\r")
-        // backspace
-        .replace("\b", "\\b")
-        // next line
-        .replace("\u0085", "\\u0085")
-        // vertical tab
-        .replace("\u000B", "\\u000B")
-        // form feed
-        .replace("\u000C", "\\u000C")
-        // line separator
-        .replace("\u2028", "\\u2028")
-        // paragraph separator
-        .replace("\u2029", "\\u2029");
   }
 }
