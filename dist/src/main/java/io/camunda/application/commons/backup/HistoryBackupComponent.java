06a3dc70588b30886da4c26a09ba14d4e2add3c7
Carlo Sana <carlo.sana@camunda.com>
2024-12-10 18:08:28 +0100

    feat: add unit test that checks Prio$NBackup interfaces are all injected
    closes #25848

--- dist/src/main/java/io/camunda/application/commons/backup/HistoryBackupComponent.java
+++ dist/src/main/java/io/camunda/application/commons/backup/HistoryBackupComponent.java
@@ -12,15 +12,7 @@ import io.camunda.webapps.backup.BackupService;
 import io.camunda.webapps.backup.BackupServiceImpl;
 import io.camunda.webapps.backup.repository.BackupRepositoryProps;
 import io.camunda.webapps.profiles.ProfileOperateTasklist;
-import io.camunda.webapps.schema.descriptors.backup.Prio1Backup;
-import io.camunda.webapps.schema.descriptors.backup.Prio2Backup;
-import io.camunda.webapps.schema.descriptors.backup.Prio3Backup;
-import io.camunda.webapps.schema.descriptors.backup.Prio4Backup;
-import io.camunda.webapps.schema.descriptors.backup.Prio5Backup;
-import io.camunda.webapps.schema.descriptors.backup.Prio6Backup;
-import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.camunda.webapps.schema.descriptors.backup.BackupPriorities;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,41 +23,19 @@ import org.springframework.stereotype.Component;
 @Configuration
 @ProfileOperateTasklist
 public class HistoryBackupComponent {
-  private static final Logger LOG = LoggerFactory.getLogger(HistoryBackupComponent.class);
 
   private final ThreadPoolTaskExecutor threadPoolTaskExecutor;
-  private final List<Prio1Backup> prio1BackupIndices;
-  private final List<Prio2Backup> prio2BackupTemplates;
-  private final List<Prio3Backup> prio3BackupTemplates;
-  private final List<Prio4Backup> prio4BackupTemplates;
-  private final List<Prio5Backup> prio5BackupIndices;
+  private final BackupPriorities backupPriorities;
   private final BackupRepositoryProps backupRepositoryProps;
   private final BackupRepository backupRepository;
-  private final List<Prio6Backup> prio6BackupIndices;
 
   public HistoryBackupComponent(
       @Qualifier("backupThreadPoolExecutor") final ThreadPoolTaskExecutor threadPoolTaskExecutor,
-      final List<Prio1Backup> prio1BackupIndices,
-      final List<Prio2Backup> prio2BackupTemplates,
-      final List<Prio3Backup> prio3BackupTemplates,
-      final List<Prio4Backup> prio4BackupTemplates,
-      final List<Prio5Backup> prio5BackupIndices,
-      final List<Prio6Backup> prio6BackupIndices,
+      final BackupPriorities backupPriorities,
       final BackupRepositoryProps backupRepositoryProps,
       final BackupRepository backupRepository) {
-    LOG.debug("Prio1BackupIndices are {}", prio1BackupIndices);
-    LOG.debug("Prio2BackupTemplates are {}", prio2BackupTemplates);
-    LOG.debug("Prio3BackupTemplates are {}", prio3BackupTemplates);
-    LOG.debug("Prio4BackupIndices are {}", prio4BackupTemplates);
-    LOG.debug("Prio5BackupIndices are {}", prio5BackupIndices);
-    LOG.debug("Prio6BackupIndices are {}", prio6BackupIndices);
     this.threadPoolTaskExecutor = threadPoolTaskExecutor;
-    this.prio1BackupIndices = prio1BackupIndices;
-    this.prio2BackupTemplates = prio2BackupTemplates;
-    this.prio3BackupTemplates = prio3BackupTemplates;
-    this.prio4BackupTemplates = prio4BackupTemplates;
-    this.prio5BackupIndices = prio5BackupIndices;
-    this.prio6BackupIndices = prio6BackupIndices;
+    this.backupPriorities = backupPriorities;
     this.backupRepositoryProps = backupRepositoryProps;
     this.backupRepository = backupRepository;
   }
@@ -73,14 +43,6 @@ public class HistoryBackupComponent {
   @Bean
   public BackupService backupService() {
     return new BackupServiceImpl(
-        threadPoolTaskExecutor,
-        prio1BackupIndices,
-        prio2BackupTemplates,
-        prio3BackupTemplates,
-        prio4BackupTemplates,
-        prio5BackupIndices,
-        prio6BackupIndices,
-        backupRepositoryProps,
-        backupRepository);
+        threadPoolTaskExecutor, backupPriorities, backupRepositoryProps, backupRepository);
   }
 }
---
+++ dist/src/test/java/io/camunda/application/commons/backup/BackupPrioritiesTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.application.commons.backup;
+
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.implement;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import io.camunda.operate.conditions.DatabaseInfo;
+import io.camunda.operate.property.OperateProperties;
+import io.camunda.operate.schema.IndexTemplateDescriptorsConfigurator;
+import io.camunda.tasklist.property.TasklistProperties;
+import io.camunda.tasklist.schema.TasklistIndexTemplateDescriptorsConfigurator;
+import io.camunda.webapps.schema.descriptors.backup.BackupPriority;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(
+    classes = {
+      BackupPrioritiesTest.TestBackupPriorityConfiguration.class,
+      BackupPriorityConfiguration.class,
+      // Classes that provide the Beans for the indices
+      TasklistIndexTemplateDescriptorsConfigurator.class,
+      IndexTemplateDescriptorsConfigurator.class
+    })
+public class BackupPrioritiesTest {
+
+  @Autowired BackupPriorityConfiguration backupPriorityConfiguration;
+
+  @Test
+  public void onlyOneInterfaceIsImplemented() {
+    final var implementations =
+        new ClassFileImporter()
+                .importPackages("io.camunda.webapps.schema")
+                .that(implement(BackupPriority.class))
+                .stream()
+                .toList();
+
+    assertThat(implementations)
+        .allSatisfy(
+            clazz ->
+                assertThat(
+                        clazz.getInterfaces().stream()
+                            .filter(i -> i.getName().matches(".*.Prio\\d+Backup"))
+                            .count())
+                    .isEqualTo(1));
+  }
+
+  @Test
+  public void testBackupPriorities() {
+    final var priorities = backupPriorityConfiguration.backupPriorities();
+
+    System.out.println(priorities);
+    final Set<String> allPriorities =
+        priorities.allPriorities().map(obj -> obj.getClass().getName()).collect(Collectors.toSet());
+
+    final var classes =
+        new ClassFileImporter()
+            .importPackages("io.camunda.webapps.schema")
+            .that(implement(BackupPriority.class));
+
+    final var missingInBackup = new HashSet<String>();
+    classes.forEach(
+        clzz -> {
+          if (!allPriorities.contains(clzz.getName())) {
+            missingInBackup.add(clzz.getName());
+          }
+        });
+    assertThat(missingInBackup).isEmpty();
+  }
+
+  @TestConfiguration
+  static class TestBackupPriorityConfiguration {
+    @Primary
+    @Bean
+    public OperateProperties properties() {
+      return new OperateProperties();
+    }
+
+    @Primary
+    @Bean
+    public TasklistProperties tasklistProperties() {
+      return new TasklistProperties();
+    }
+
+    @Primary
+    @Bean
+    DatabaseInfo databaseInfo() {
+      return new DatabaseInfo();
+    }
+  }
+}
