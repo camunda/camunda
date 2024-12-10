85fcfed6a0f60052a736ff4561c5c6ed9aa1b423
Euro Lew <euro.lew@camunda.com>
2024-12-10 11:42:12 +0000

    test: disable broken tests until fix

--- operate/qa/integration-tests/src/test/java/io/camunda/operate/operation/ProcessInstanceMigrationIT.java
+++ operate/qa/integration-tests/src/test/java/io/camunda/operate/operation/ProcessInstanceMigrationIT.java
@@ -20,7 +20,7 @@ import io.camunda.webapps.schema.entities.operate.FlowNodeType;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
@@ -32,7 +32,7 @@ class ProcessInstanceMigrationIT extends OperateZeebeSearchAbstractIT {
 
   @Autowired private MigrateProcessInstanceHandler migrateProcessInstanceHandler;
 
-  @Test
+  @Disabled("To be re-enabled with the fix in https://github.com/camunda/camunda/issues/24084")
   void shouldMigrateSubprocessToSubprocess() throws Exception {
     // given
     // process instances that are running
