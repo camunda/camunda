b782c2c4cf131f7d8fb9803ca7c3dc9923dfce0a
Kostya Zraitel <kos.zrt@gmail.com>
2024-12-12 17:35:59 +0200

    feat: add test & test support for Resource
    (cherry picked from commit aa027eaa2e86b293998327a1848761139318031b)

--- zeebe/test-util/src/main/java/io/camunda/zeebe/test/util/record/RecordingExporter.java
+++ zeebe/test-util/src/main/java/io/camunda/zeebe/test/util/record/RecordingExporter.java
@@ -74,6 +74,7 @@ import io.camunda.zeebe.protocol.record.value.deployment.DecisionRecordValue;
 import io.camunda.zeebe.protocol.record.value.deployment.DecisionRequirementsRecordValue;
 import io.camunda.zeebe.protocol.record.value.deployment.Form;
 import io.camunda.zeebe.protocol.record.value.deployment.Process;
+import io.camunda.zeebe.protocol.record.value.deployment.Resource;
 import java.time.Duration;
 import java.util.Collection;
 import java.util.Iterator;
@@ -411,6 +412,10 @@ public final class RecordingExporter implements Exporter {
 
   public static FormRecordStream formRecords() {
     return new FormRecordStream(records(ValueType.FORM, Form.class));
+  }
+
+  public static ResourceRecordStream resourceRecords() {
+    return new ResourceRecordStream(records(ValueType.RESOURCE, Resource.class));
   }
 
   public static ErrorRecordStream errorRecords() {
