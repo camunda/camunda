1a54d03afbde5054a89c73bc6a6583e30bfabbc5
Johannes Dollinger <johannes.dollinger@camunda.com>
2024-12-12 10:56:45 +0100

    feat: map OIDC tokens to Camunda principals

---
+++ authentication/src/main/java/io/camunda/authentication/entity/AuthenticationContext.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.authentication.entity;
+
+import io.camunda.search.entities.RoleEntity;
+import io.camunda.service.TenantServices.TenantDTO;
+import java.util.List;
+
+public record AuthenticationContext(
+    List<RoleEntity> roles,
+    List<String> authorizedApplications,
+    List<TenantDTO> tenants,
+    List<String> groups) {}
