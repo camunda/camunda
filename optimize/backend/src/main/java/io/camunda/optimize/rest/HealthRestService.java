e967f0329da756c30ec5fa5fdf429f4160d0e69e
Oleksandr Kriuchenko <oleksandr.kriuchenko-ext@camunda.com>
2024-12-13 05:59:56 +0200

    feat: Translate controller responses to Spring MVC #25377

--- optimize/backend/src/main/java/io/camunda/optimize/rest/HealthRestService.java
+++ optimize/backend/src/main/java/io/camunda/optimize/rest/HealthRestService.java
@@ -11,7 +11,8 @@ import static io.camunda.optimize.rest.HealthRestService.READYZ_PATH;
 import static io.camunda.optimize.tomcat.OptimizeResourceConstants.REST_API_PATH;
 
 import io.camunda.optimize.service.status.StatusCheckingService;
-import jakarta.ws.rs.core.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,10 +30,10 @@ public class HealthRestService {
   }
 
   @GetMapping
-  public Response getConnectionStatus() {
+  public ResponseEntity<String> getConnectionStatus() {
     if (statusCheckingService.isConnectedToDatabase()) {
-      return Response.ok().build();
+      return ResponseEntity.ok().build();
     }
-    return Response.status(Response.Status.SERVICE_UNAVAILABLE).build();
+    return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
   }
 }
