b2906fed2314f61622ed545f38f612ba87b036fd
Lena Schönburg <lena@fairburg.xyz>
2024-12-16 11:39:16 +0100

    fix: identity migration app connects to Zeebe via REST

--- migration/identity-migration/src/main/java/io/camunda/migration/identity/config/ZeebeConnector.java
+++ migration/identity-migration/src/main/java/io/camunda/migration/identity/config/ZeebeConnector.java
@@ -31,7 +31,7 @@ public class ZeebeConnector {
 
   public ZeebeClient newZeebeClient(final ZeebeProperties zeebeProperties) {
     final ZeebeClientBuilder builder =
-        ZeebeClient.newClientBuilder().grpcAddress(URI.create(zeebeProperties.getGatewayAddress()));
+        ZeebeClient.newClientBuilder().restAddress(URI.create(zeebeProperties.getGatewayAddress()));
     if (zeebeProperties.isSecure()) {
       builder.caCertificatePath(zeebeProperties.getCertificatePath());
       LOGGER.info("Use TLS connection to zeebe");
