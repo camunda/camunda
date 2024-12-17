49a6aa5035b7e2421397a78a0c4bb4e7a5415813
remco.westerhoud <remcowesterhoud@msn.com>
2024-12-17 09:45:25 +0100

    feat: verify configured mappings are not empty/null

--- security/security-core/src/main/java/io/camunda/security/configuration/ConfiguredMapping.java
+++ security/security-core/src/main/java/io/camunda/security/configuration/ConfiguredMapping.java
@@ -7,12 +7,16 @@
  */
 package io.camunda.security.configuration;
 
+import static io.camunda.security.util.ArgumentUtil.ensureNotNullOrEmpty;
+
 public class ConfiguredMapping {
 
   private String claimName;
   private String claimValue;
 
   public ConfiguredMapping(final String claimName, final String claimValue) {
+    ensureNotNullOrEmpty("claimName", claimName);
+    ensureNotNullOrEmpty("claimValue", claimValue);
     this.claimName = claimName;
     this.claimValue = claimValue;
   }
@@ -22,6 +26,7 @@ public class ConfiguredMapping {
   }
 
   public void setClaimName(final String claimName) {
+    ensureNotNullOrEmpty("claimName", claimName);
     this.claimName = claimName;
   }
 
@@ -30,6 +35,7 @@ public class ConfiguredMapping {
   }
 
   public void setClaimValue(final String claimValue) {
+    ensureNotNullOrEmpty("claimValue", claimValue);
     this.claimValue = claimValue;
   }
 }
