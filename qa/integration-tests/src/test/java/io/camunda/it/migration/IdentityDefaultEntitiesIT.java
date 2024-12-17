32a844e5f9e06913961fa8b3c85be9120db9af88
Lena Schönburg <lena@fairburg.xyz>
2024-12-17 09:06:40 +0100

    style: capitalize static members

--- qa/integration-tests/src/test/java/io/camunda/it/migration/IdentityDefaultEntitiesIT.java
+++ qa/integration-tests/src/test/java/io/camunda/it/migration/IdentityDefaultEntitiesIT.java
@@ -23,15 +23,15 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers(parallel = true)
 public class IdentityDefaultEntitiesIT {
   @Container
-  private static final GenericContainer<?> postgres = IdentityMigrationTestUtil.getPostgres();
+  private static final GenericContainer<?> POSTGRES = IdentityMigrationTestUtil.getPostgres();
 
   @Container
-  private static final GenericContainer<?> keycloak =
-      IdentityMigrationTestUtil.getKeycloak(postgres);
+  private static final GenericContainer<?> KEYCLOAK =
+      IdentityMigrationTestUtil.getKeycloak(POSTGRES);
 
   @Container
-  private static final GenericContainer<?> identity =
-      IdentityMigrationTestUtil.getManagementIdentity(postgres, keycloak);
+  private static final GenericContainer<?> IDENTITY =
+      IdentityMigrationTestUtil.getManagementIdentity(POSTGRES, KEYCLOAK);
 
   @TestZeebe(autoStart = false)
   final TestStandaloneCamunda camunda = new TestStandaloneCamunda();
@@ -45,10 +45,10 @@ public class IdentityDefaultEntitiesIT {
         .withAdditionalProfile(Profile.IDENTITY_MIGRATION)
         .withProperty("camunda.migration.identity.zeebe.gatewayAddress", camunda.restAddress())
         .withProperty(
-            "camunda.migration.identity.managementIdentity.baseUrl", externalIdentityUrl(identity))
+            "camunda.migration.identity.managementIdentity.baseUrl", externalIdentityUrl(IDENTITY))
         .withProperty(
             "camunda.migration.identity.managementIdentity.m2mToken",
-            getIdentityAccessToken(keycloak))
+            getIdentityAccessToken(KEYCLOAK))
         .start();
 
     // then -- the migration did not fail
