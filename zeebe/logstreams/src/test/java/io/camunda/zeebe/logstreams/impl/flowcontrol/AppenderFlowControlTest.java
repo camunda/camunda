3ffef51de3434c6a50378da2b7b6978ce183e3f4
Filipe Campos <filipe.campos@camunda.com>
2024-12-23 17:59:46 +0000

    test: reproduces concurrency issue updating inFlight counter above limit

--- zeebe/logstreams/src/test/java/io/camunda/zeebe/logstreams/impl/flowcontrol/AppenderFlowControlTest.java
+++ zeebe/logstreams/src/test/java/io/camunda/zeebe/logstreams/impl/flowcontrol/AppenderFlowControlTest.java
@@ -7,8 +7,21 @@
  */
 package io.camunda.zeebe.logstreams.impl.flowcontrol;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.netflix.concurrency.limits.Limit;
+import com.netflix.concurrency.limits.Limiter.Listener;
+import com.netflix.concurrency.limits.limit.SettableLimit;
 import java.time.Duration;
+import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
@@ -76,5 +89,44 @@ final class AppenderFlowControlTest {
 
     // then
     Awaitility.await("Eventually accepts appends again").until(() -> flow.tryAcquire().isPresent());
+  }
+
+  @Test
+  void shouldNotAllowInFlightHigherThanLimit() throws InterruptedException {
+    // given
+    final int numThreads = 3000;
+    final int poolSize = 300;
+    final int limit = 100;
+    final Limit myLimit = new SettableLimit(limit);
+    final AppenderMetrics appenderMetrics = new AppenderMetrics(1);
+    appenderMetrics.setInflightLimit(limit);
+    final AppendLimiter myRateLimiter =
+        AppendLimiter.builder().limit(myLimit).metrics(appenderMetrics).build();
+    final ExecutorService threadPool = Executors.newFixedThreadPool(poolSize);
+    final Optional<Listener>[] listeners = new Optional[numThreads];
+
+    final Collection<Callable<Object>> tasks =
+        IntStream.range(0, numThreads)
+            .mapToObj(
+                i ->
+                    (Callable<Object>)
+                        () -> {
+                          final int sleepTime = ThreadLocalRandom.current().nextInt(limit);
+                          listeners[i] = myRateLimiter.acquire(null);
+                          Thread.sleep(sleepTime);
+                          assertThat(myRateLimiter.getInflight()).isLessThanOrEqualTo(limit);
+                          listeners[i].get().onSuccess();
+                          return null;
+                        })
+            .collect(Collectors.toList());
+    try {
+      threadPool.invokeAll(tasks);
+    } finally {
+      // when
+      threadPool.shutdown();
+    }
+
+    // then
+    assertThat(myRateLimiter.getInflight()).isEqualTo(0);
   }
 }
