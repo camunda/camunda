032b2f2477ef258c23f4af06f1a2c566b1ad0af6
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-12-23 10:42:52 +0100

    feat: moved ZeebeClientPropertiesNoProfileTest

---
+++ clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/properties/ZeebeClientPropertiesNoProfileTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.spring.client.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CamundaClientPropertiesTestConfig.class)
+public class ZeebeClientPropertiesNoProfileTest {
+  @Autowired CamundaClientProperties camundaClientProperties;
+
+  @Test
+  void shouldWork() {}
+}
