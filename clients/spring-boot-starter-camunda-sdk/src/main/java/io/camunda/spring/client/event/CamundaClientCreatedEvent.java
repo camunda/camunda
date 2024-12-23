fa326bd75a11bb46826c7cb818ed6b60633527da
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-12-23 07:16:29 +0100

    feat: moved ZeebeClientCreatedEvent to CamundaClientCreatedEvent

--- clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/spring/client/event/CamundaClientCreatedEvent.java
+++ clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/spring/client/event/CamundaClientCreatedEvent.java
@@ -1,19 +1,11 @@
 /*
- * Copyright © 2017 camunda services GmbH (info@camunda.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.spring.client.event;
+package io.camunda.spring.client.event;
 
 import io.camunda.client.CamundaClient;
 import org.springframework.context.ApplicationEvent;
@@ -28,13 +20,13 @@ import org.springframework.context.ApplicationEvent;
  * created (at least logically, as the CamundaClient Spring bean might simply be a proxy always
  * pointing to the right client automatically to avoid problems with @Autowire).
  *
- * <p>Furthermore, when `zeebe.client.enabled=false`, the event might not be fired ever
+ * <p>Furthermore, when `camunda.client.enabled=false`, the event might not be fired ever
  */
-public class ZeebeClientCreatedEvent extends ApplicationEvent {
+public class CamundaClientCreatedEvent extends ApplicationEvent {
 
   public final CamundaClient client;
 
-  public ZeebeClientCreatedEvent(final Object source, final CamundaClient client) {
+  public CamundaClientCreatedEvent(final Object source, final CamundaClient client) {
     super(source);
     this.client = client;
   }
