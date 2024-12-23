43de90646a199b3f68c35a36d16ddf02c8854d80
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-12-23 07:08:35 +0100

    feat: moved ZeebeBpmnError to CamundaBpmnError

--- clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/spring/client/exception/CamundaBpmnError.java
+++ clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/spring/client/exception/CamundaBpmnError.java
@@ -1,19 +1,11 @@
 /*
- * Copyright © 2017 camunda services GmbH (info@camunda.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.spring.common.exception;
+package io.camunda.spring.client.exception;
 
 import java.util.Map;
 
@@ -21,13 +13,13 @@ import java.util.Map;
  * Indicates an error in sense of BPMN occured, that should be handled by the BPMN process, see
  * https://docs.camunda.io/docs/reference/bpmn-processes/error-events/error-events/
  */
-public class ZeebeBpmnError extends RuntimeException {
+public class CamundaBpmnError extends RuntimeException {
 
   private final String errorCode;
   private final String errorMessage;
   private final Map<String, Object> variables;
 
-  public ZeebeBpmnError(
+  public CamundaBpmnError(
       final String errorCode, final String errorMessage, final Map<String, Object> variables) {
     super("[" + errorCode + "] " + errorMessage);
     this.errorCode = errorCode;
