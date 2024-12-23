e9061118fac100838c37f86dc8eb85af69953b99
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-12-23 10:34:28 +0100

    feat: moved PropertiesPostProcessor

--- clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/spring/client/properties/PropertiesPostProcessor.java
+++ clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/spring/client/properties/PropertiesPostProcessor.java
@@ -1,21 +1,13 @@
 /*
- * Copyright © 2017 camunda services GmbH (info@camunda.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.spring.client.properties;
+package io.camunda.spring.client.properties;
 
-import io.camunda.zeebe.spring.client.properties.CamundaClientProperties.ClientMode;
+import io.camunda.spring.client.properties.CamundaClientProperties.ClientMode;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
@@ -50,10 +42,10 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
   private String determinePropertiesFile(final ClientMode clientMode) {
     switch (clientMode) {
       case selfManaged -> {
-        return "application-camunda-self-managed.yaml";
+        return "modes/self-managed.yaml";
       }
       case saas -> {
-        return "application-camunda-saas.yaml";
+        return "modes/saas.yaml";
       }
       default -> {
         throw new IllegalStateException("Unknown client mode " + clientMode);
