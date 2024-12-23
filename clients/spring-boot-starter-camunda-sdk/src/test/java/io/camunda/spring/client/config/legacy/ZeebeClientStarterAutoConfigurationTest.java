71e427fd0d54dd7357930fc8b413dcbcc65ee8ed
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-12-23 10:44:35 +0100

    feat: moved ZeebeClientStarterAutoConfigurationTest

--- clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/config/legacy/ZeebeClientStarterAutoConfigurationTest.java
+++ clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/config/legacy/ZeebeClientStarterAutoConfigurationTest.java
@@ -1,19 +1,11 @@
 /*
- * Copyright © 2017 camunda services GmbH (info@camunda.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.spring.client.config.legacy;
+package io.camunda.spring.client.config.legacy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -21,10 +13,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.JsonMapper;
-import io.camunda.zeebe.spring.client.configuration.CamundaAutoConfiguration;
-import io.camunda.zeebe.spring.client.configuration.JsonMapperConfiguration;
-import io.camunda.zeebe.spring.client.configuration.ZeebeClientProdAutoConfiguration;
-import io.camunda.zeebe.spring.client.properties.CamundaClientProperties;
+import io.camunda.spring.client.configuration.CamundaAutoConfiguration;
+import io.camunda.spring.client.configuration.CamundaClientProdAutoConfiguration;
+import io.camunda.spring.client.configuration.JsonMapperConfiguration;
+import io.camunda.spring.client.properties.CamundaClientProperties;
 import java.time.Duration;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -65,7 +57,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 public class ZeebeClientStarterAutoConfigurationTest {
 
   @Autowired private JsonMapper jsonMapper;
-  @Autowired private ZeebeClientProdAutoConfiguration autoConfiguration;
+  @Autowired private CamundaClientProdAutoConfiguration autoConfiguration;
   @Autowired private ApplicationContext applicationContext;
 
   @Test
