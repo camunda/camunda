00d8d916b68648aa7efb19411336e6e312a51845
Mihail <122792941+mihail-ca@users.noreply.github.com>
2025-01-02 16:18:40 +0100

    feat: Document available sort fields in REST API v2

--- clients/java/src/main/java/io/camunda/client/impl/search/SearchQuerySortRequestMapper.java
+++ clients/java/src/main/java/io/camunda/client/impl/search/SearchQuerySortRequestMapper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.camunda.zeebe.client.impl.search;
+package io.camunda.client.impl.search;
 
 import io.camunda.client.protocol.rest.*;
 import java.util.List;
