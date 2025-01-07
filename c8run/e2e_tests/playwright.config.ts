43be483252f617ee2ae65454f6b0ab668eff91d5
Jesse Simpson <jesse.simpson@camunda.com>
2025-01-07 15:17:12 -0500

    fix: tests adjusted for http basic auth

--- c8run/e2e_tests/playwright.config.ts
+++ c8run/e2e_tests/playwright.config.ts
@@ -28,6 +28,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    httpCredentials: {
+      username: 'demo',
+      password: 'demo',
+    }
   },
 
   /* Configure projects for major browsers */
