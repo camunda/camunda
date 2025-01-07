de45126a47ad10b079baec931edbe15a115885d2
Jesse Simpson <jesse.simpson@camunda.com>
2025-01-07 15:42:54 -0500

    fix: add version bump to c8run since it's necessary for tasklist to function

--- c8run/main.go
+++ c8run/main.go
@@ -197,7 +197,7 @@ func main() {
 	baseDir, _ := os.Getwd()
 	parentDir := baseDir
 	elasticsearchVersion := "8.13.4"
-	camundaVersion := "8.7.0-alpha2"
+	camundaVersion := "8.7.0-alpha3-rc4"
 	connectorsVersion := "8.7.0-alpha2.1"
 	composeTag := "8.7-alpha2"
 	composeExtractedFolder := "camunda-platform-8.7-alpha2"
