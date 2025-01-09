f0bd33f143f3917667e55d7c01242f1b14eeba0a
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-01-09 17:25:14 +0100

    fix: remove invalid call to deprecated junit helper
    (cherry picked from commit 55b016306787a1427b144076f6759973948298b6)
    
     BACKPORT-CONFLICT

--- util/src/main/java/io/camunda/zeebe/util/ReflectUtil.java
+++ util/src/main/java/io/camunda/zeebe/util/ReflectUtil.java
@@ -7,7 +7,11 @@
  */
 package io.camunda.zeebe.util;
 
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+import java.util.stream.Stream;
 
 public final class ReflectUtil {
 
@@ -25,5 +29,54 @@ public final class ReflectUtil {
               "Failed to instantiate class %s with the default constructor", clazz.getName()),
           e);
     }
+  }
+
+  /**
+   * Sets the field to be accessible via reflection if it is not currently for the given instance
+   * object. This replaces the old Junit 5 `ReflectUtils.makeAccessible` which was removed from
+   * their platform.
+   *
+   * @param member the field to make accessible
+   * @param instance the instance on which we check accessibility
+   * @return the field, accessible
+   * @param <M> the type of the member
+   * @param <U> the type of the instance, typically just {@code Object}
+   */
+  public static <M extends AccessibleObject & Member, U> M makeAccessible(
+      final M member, final U instance) {
+    var canAccess = false;
+    try {
+      // this throws an IllegalArgumentException if member is not a field/method of Instance
+      canAccess = member.canAccess(instance);
+    } catch (final IllegalArgumentException e) {
+      final var modifiers = member.getModifiers();
+      if (Modifier.isPublic(modifiers)
+          && Modifier.isPublic(member.getDeclaringClass().getModifiers())) {
+        canAccess = true;
+      }
+    }
+    if (!canAccess) {
+      member.setAccessible(true);
+    }
+
+    return member;
+  }
+
+  /**
+   * Returns a stream of all non-interface classes that are a subtypes of the given sealed class.
+   */
+  public static <T> Stream<Class<T>> implementationsOfSealedInterface(final Class<T> clazz) {
+    if (!clazz.isSealed()) {
+      throw new IllegalArgumentException(String.format("Class %s is not sealed", clazz.getName()));
+    }
+    return Stream.of(clazz.getPermittedSubclasses())
+        .flatMap(
+            c -> {
+              if (c.isSealed()) {
+                return implementationsOfSealedInterface((Class<T>) c);
+              } else {
+                return Stream.of((Class<T>) c);
+              }
+            });
   }
 }
