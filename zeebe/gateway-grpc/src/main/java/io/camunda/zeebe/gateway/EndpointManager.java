cd5683b5f0d1e205ad9beb6638c124161dbd7d31
remco.westerhoud <remcowesterhoud@msn.com>
2025-01-13 14:19:58 +0100

    feat: GRPC put username on command instead of user key
    With this change we will now put the username on the command. This will be used by the broker to determine
    authorizations.

--- zeebe/gateway-grpc/src/main/java/io/camunda/zeebe/gateway/EndpointManager.java
+++ zeebe/gateway-grpc/src/main/java/io/camunda/zeebe/gateway/EndpointManager.java
@@ -500,10 +500,10 @@ public final class EndpointManager {
           });
     }
 
-    // retrieve the user key from the context and add it to the authorization if present
-    final Long userKey = Context.current().call(AuthenticationInterceptor.USER_KEY::get);
-    if (userKey != null) {
-      claims.put(Authorization.AUTHORIZED_USER_KEY, userKey);
+    // retrieve the username from the context and add it to the authorization if present
+    final String username = Context.current().call(AuthenticationInterceptor.USERNAME::get);
+    if (username != null) {
+      claims.put(Authorization.AUTHORIZED_USERNAME, username);
     }
 
     brokerRequest.setAuthorization(claims);
