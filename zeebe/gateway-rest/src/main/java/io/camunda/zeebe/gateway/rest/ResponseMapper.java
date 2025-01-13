0ddc54342d15250ed24e4342e33e49e5b4051938
Kostya Zraitel <kos.zrt@gmail.com>
2025-01-13 13:29:33 +0200

    fix: better naming for Resource

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/ResponseMapper.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/ResponseMapper.java
@@ -31,8 +31,8 @@ import io.camunda.zeebe.gateway.protocol.rest.DeploymentDecisionRequirements;
 import io.camunda.zeebe.gateway.protocol.rest.DeploymentForm;
 import io.camunda.zeebe.gateway.protocol.rest.DeploymentMetadata;
 import io.camunda.zeebe.gateway.protocol.rest.DeploymentProcess;
+import io.camunda.zeebe.gateway.protocol.rest.DeploymentResource;
 import io.camunda.zeebe.gateway.protocol.rest.DeploymentResponse;
-import io.camunda.zeebe.gateway.protocol.rest.DeploymentRpa;
 import io.camunda.zeebe.gateway.protocol.rest.DocumentCreationBatchResponse;
 import io.camunda.zeebe.gateway.protocol.rest.DocumentCreationFailureDetail;
 import io.camunda.zeebe.gateway.protocol.rest.DocumentMetadata;
@@ -305,7 +305,7 @@ public final class ResponseMapper {
     addDeployedDecision(response, brokerResponse.decisionsMetadata());
     addDeployedDecisionRequirements(response, brokerResponse.decisionRequirementsMetadata());
     addDeployedForm(response, brokerResponse.formMetadata());
-    addDeployedRPA(response, brokerResponse.resourceMetadata());
+    addDeployedResource(response, brokerResponse.resourceMetadata());
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 
@@ -334,16 +334,16 @@ public final class ResponseMapper {
         .forEach(response::addDeploymentsItem);
   }
 
-  private static void addDeployedRPA(
+  private static void addDeployedResource(
       final DeploymentResponse response,
       final ValueArray<ResourceMetadataRecord> resourceMetadataRecords) {
     resourceMetadataRecords.stream()
         .map(
             resource ->
-                new DeploymentRpa()
+                new DeploymentResource()
                     .resourceId(resource.getResourceId())
                     .version(resource.getVersion())
-                    .resourceKey(String.valueOf(resource.getResourceKey()))
+                    .resourceKey(resource.getResourceKey())
                     .resourceName(resource.getResourceName())
                     .tenantId(resource.getTenantId()))
         .map(deploymentForm -> new DeploymentMetadata().resource(deploymentForm))
