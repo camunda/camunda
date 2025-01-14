4e0e65e8591cc43936feb88facbf2f1b154d84a1
Nicola Puppa <nicola.puppa@camunda.com>
2025-01-14 15:58:52 +0100

    feat: create broker request class to send request to the broker

---
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerAuthorizationCreateRequest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.impl.broker.request;
+
+import io.camunda.zeebe.broker.client.api.dto.BrokerExecuteCommand;
+import io.camunda.zeebe.protocol.Protocol;
+import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.AuthorizationIntent;
+import io.camunda.zeebe.protocol.record.value.AuthorizationOwnerType;
+import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
+import io.camunda.zeebe.protocol.record.value.PermissionType;
+import io.camunda.zeebe.util.buffer.BufferWriter;
+import java.util.Set;
+import org.agrona.DirectBuffer;
+
+public class BrokerAuthorizationCreateRequest extends BrokerExecuteCommand<AuthorizationRecord> {
+  private final AuthorizationRecord requestDto = new AuthorizationRecord();
+
+  public BrokerAuthorizationCreateRequest() {
+    super(ValueType.AUTHORIZATION, AuthorizationIntent.CREATE);
+    setPartitionId(Protocol.DEPLOYMENT_PARTITION);
+  }
+
+  public BrokerAuthorizationCreateRequest setOwnerId(final String ownerId) {
+    // TODO: add set method https://github.com/camunda/camunda/issues/26883
+    return this;
+  }
+
+  public BrokerAuthorizationCreateRequest setOwnerType(final AuthorizationOwnerType ownerType) {
+    requestDto.setOwnerType(ownerType);
+    return this;
+  }
+
+  public BrokerAuthorizationCreateRequest setResourceId(final String resourceId) {
+    // TODO: add set method https://github.com/camunda/camunda/issues/26883
+    return this;
+  }
+
+  public BrokerAuthorizationCreateRequest setResourceType(
+      final AuthorizationResourceType resourceType) {
+    requestDto.setResourceType(resourceType);
+    return this;
+  }
+
+  public BrokerAuthorizationCreateRequest setPermissions(final Set<PermissionType> permissions) {
+    // TODO: add set method https://github.com/camunda/camunda/issues/26883
+    return this;
+  }
+
+  @Override
+  public BufferWriter getRequestWriter() {
+    return requestDto;
+  }
+
+  @Override
+  protected AuthorizationRecord toResponseDto(final DirectBuffer buffer) {
+    final var response = new AuthorizationRecord();
+    response.wrap(buffer);
+    return response;
+  }
+}
