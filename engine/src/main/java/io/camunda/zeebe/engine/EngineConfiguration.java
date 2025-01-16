9a5e4b51d50ce683c3d45dbdc43f5eaa91d66803
Nico Korthout <nico.korthout@camunda.com>
2025-01-16 17:55:15 +0100

    feat: allow configuring maxProcessDepth
    Adds zeebe.broker.experimental.engine.maxProcessDepth as a new config
    property. This allows users to configure it.

--- engine/src/main/java/io/camunda/zeebe/engine/EngineConfiguration.java
+++ engine/src/main/java/io/camunda/zeebe/engine/EngineConfiguration.java
@@ -111,7 +111,8 @@ public final class EngineConfiguration {
     return maxProcessDepth;
   }
 
-  public void setMaxProcessDepth(final int maxProcessDepth) {
+  public EngineConfiguration setMaxProcessDepth(final int maxProcessDepth) {
     this.maxProcessDepth = maxProcessDepth;
+    return this;
   }
 }
