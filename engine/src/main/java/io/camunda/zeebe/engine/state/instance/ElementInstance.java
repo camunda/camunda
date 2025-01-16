188912196dc0d43eb2c670d2ccdede538f479226
Nico Korthout <nico.korthout@camunda.com>
2025-01-16 15:50:05 +0100

    perf: correctly declare number of properties
    This helps avoid unnecessary allocations and memory copies for the
    declared properties.
    
    Co-authored-by: Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
    Co-authored-by: Remco <remco@westerhoud.nl>
    Co-authored-by: Stephan Epping <stephan.epping@camunda.com>
    Co-authored-by: berkaycanbc <berkay.can@camunda.com>
    (cherry picked from commit 18a73e46433806806a942db06c4a2d114a3c3900)
    (cherry picked from commit c690f5230325efa4b9b30be770c628526b7318d1)

--- engine/src/main/java/io/camunda/zeebe/engine/state/instance/ElementInstance.java
+++ engine/src/main/java/io/camunda/zeebe/engine/state/instance/ElementInstance.java
@@ -50,7 +50,7 @@ public final class ElementInstance extends UnpackedObject implements DbValue {
   private final IntegerProperty processDepth = new IntegerProperty("processDepth", 1);
 
   public ElementInstance() {
-    super(12);
+    super(13);
     declareProperty(parentKeyProp)
         .declareProperty(childCountProp)
         .declareProperty(childActivatedCountProp)
