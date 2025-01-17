62d7f2e0ca891fbe00c5748c6009bdbfbfc4e7bc
Filipe Campos <filipe.campos@camunda.com>
2025-01-17 17:59:05 +0000

    feat: duplicate counter in TopologyMetrics

--- zeebe/dynamic-config/src/main/java/io/camunda/zeebe/dynamic/config/GatewayClusterConfigurationService.java
+++ zeebe/dynamic-config/src/main/java/io/camunda/zeebe/dynamic/config/GatewayClusterConfigurationService.java
@@ -14,6 +14,7 @@ import io.camunda.zeebe.dynamic.config.gossip.ClusterConfigurationGossiperConfig
 import io.camunda.zeebe.dynamic.config.serializer.ProtoBufSerializer;
 import io.camunda.zeebe.dynamic.config.state.ClusterConfiguration;
 import io.camunda.zeebe.scheduler.Actor;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,7 +36,8 @@ public class GatewayClusterConfigurationService extends Actor
   public GatewayClusterConfigurationService(
       final ClusterCommunicationService communicationService,
       final ClusterMembershipService memberShipService,
-      final ClusterConfigurationGossiperConfig config) {
+      final ClusterConfigurationGossiperConfig config,
+      final MeterRegistry meterRegistry) {
     clusterConfigurationGossiper =
         new ClusterConfigurationGossiper(
             this,
@@ -43,7 +45,8 @@ public class GatewayClusterConfigurationService extends Actor
             memberShipService,
             new ProtoBufSerializer(),
             config,
-            this::updateClusterTopology);
+            this::updateClusterTopology,
+            meterRegistry);
   }
 
   private void updateClusterTopology(final ClusterConfiguration clusterConfiguration) {
