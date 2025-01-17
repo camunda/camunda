a300f943f7321b58e5ebfb6e557648cd5a5a3090
Houssain Barouni <houssain.barouni@camunda.com>
2025-01-17 14:29:20 +0100

    fix: fix breaking change in deploy resources API response

--- clients/java/src/test/java/io/camunda/zeebe/client/resource/rest/DeployResourceRestTest.java
+++ clients/java/src/test/java/io/camunda/zeebe/client/resource/rest/DeployResourceRestTest.java
@@ -323,7 +323,7 @@ public class DeployResourceRestTest extends ClientRestTest {
                     .decisionRequirements(
                         new DeploymentDecisionRequirements()
                             .decisionRequirementsId(DMN_DECISION_REQUIREMENTS_ID)
-                            .name(DMN_DECISION_REQUIREMENTS_NAME)
+                            .decisionRequirementsName(DMN_DECISION_REQUIREMENTS_NAME)
                             .version(version)
                             .decisionRequirementsKey(decisionRequirementsKey)
                             .resourceName(filename)
