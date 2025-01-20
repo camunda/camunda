2cd2d3252fdc2d4bbad22a2543680f214552c504
Nicola Puppa <nicola.puppa@camunda.com>
2025-01-20 18:08:51 +0100

    test: add missing idempotency check

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/DeleteAuthorizationTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/DeleteAuthorizationTest.java
@@ -41,7 +41,6 @@ public class DeleteAuthorizationTest {
             .create()
             .getValue()
             .getAuthorizationKey();
-    ;
 
     // when
     final var deletedAuthorization =
