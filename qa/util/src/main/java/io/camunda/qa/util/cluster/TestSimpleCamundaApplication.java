60a51cf763cf1f4f0d2ce18defca82840594f0cf
Christopher Zell <zelldon91@googlemail.com>
2025-01-22 12:18:30 +0100

    refactor: simplify NewCamundaTestApplication
     * Reduce several code parts that are not needed
     * Remove exporter configuration completelty
     * Rename to TestSimpleCamundaApplication

--- qa/util/src/main/java/io/camunda/qa/util/cluster/TestSimpleCamundaApplication.java
+++ qa/util/src/main/java/io/camunda/qa/util/cluster/TestSimpleCamundaApplication.java
@@ -14,16 +14,10 @@ import io.camunda.application.commons.configuration.BrokerBasedConfiguration.Bro
 import io.camunda.application.commons.security.CamundaSecurityConfiguration.CamundaSecurityProperties;
 import io.camunda.application.initializers.WebappsConfigurationInitializer;
 import io.camunda.client.CamundaClientBuilder;
-import io.camunda.exporter.CamundaExporter;
 import io.camunda.operate.OperateModuleConfiguration;
-import io.camunda.operate.property.OperateOpensearchProperties;
-import io.camunda.operate.property.OperateProperties;
-import io.camunda.search.connect.configuration.DatabaseType;
 import io.camunda.security.configuration.ConfiguredUser;
 import io.camunda.security.configuration.InitializationConfiguration;
 import io.camunda.tasklist.TasklistModuleConfiguration;
-import io.camunda.tasklist.property.TasklistOpenSearchProperties;
-import io.camunda.tasklist.property.TasklistProperties;
 import io.camunda.webapps.WebappsModuleConfiguration;
 import io.camunda.zeebe.broker.BrokerModuleConfiguration;
 import io.camunda.zeebe.broker.system.configuration.ExporterCfg;
@@ -33,36 +27,31 @@ import io.camunda.zeebe.qa.util.actuator.GatewayHealthActuator;
 import io.camunda.zeebe.qa.util.actuator.HealthActuator;
 import io.camunda.zeebe.qa.util.cluster.TestGateway;
 import io.camunda.zeebe.qa.util.cluster.TestSpringApplication;
+import io.camunda.zeebe.qa.util.cluster.TestStandaloneApplication;
 import io.camunda.zeebe.qa.util.cluster.TestZeebePort;
-import io.camunda.zeebe.test.util.record.RecordingExporter;
 import io.camunda.zeebe.test.util.socket.SocketUtil;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 import java.util.function.Consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.util.unit.DataSize;
 
-/** Represents an instance of the {@link BrokerModuleConfiguration} Spring application. */
+/**
+ * Represents an instance of the a Camunda Application, without any extras (like ES containers,
+ * etc.).
+ */
 @SuppressWarnings("UnusedReturnValue")
-public final class NewCamundaTestApplication
-    extends TestSpringApplication<NewCamundaTestApplication>
-    implements TestGateway<NewCamundaTestApplication> {
+public final class TestSimpleCamundaApplication
+    extends TestSpringApplication<TestSimpleCamundaApplication>
+    implements TestGateway<TestSimpleCamundaApplication>,
+        TestStandaloneApplication<TestSimpleCamundaApplication> {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(NewCamundaTestApplication.class);
-  private static final String RECORDING_EXPORTER_ID = "recordingExporter";
-  private static final String ELASTICSEARCH_EXPORTER_ID = "elasticsearchExporter";
-  private static final String CAMUNDA_EXPORTER_ID = "camundaExporter";
+  private static final Logger LOGGER = LoggerFactory.getLogger(TestSimpleCamundaApplication.class);
   private final BrokerBasedProperties brokerProperties;
-  private final OperateProperties operateProperties;
-  private final TasklistProperties tasklistProperties;
   private final CamundaSecurityProperties securityConfig;
-  private final Map<String, Consumer<ExporterCfg>> registeredExporters = new HashMap<>();
 
-  public NewCamundaTestApplication() {
+  public TestSimpleCamundaApplication() {
     super(
         CommonsModuleConfiguration.class,
         OperateModuleConfiguration.class,
@@ -94,8 +83,6 @@ public final class NewCamundaTestApplication
     brokerProperties.getExperimental().getConsistencyChecks().setEnableForeignKeyChecks(true);
     brokerProperties.getExperimental().getConsistencyChecks().setEnablePreconditions(true);
 
-    operateProperties = new OperateProperties();
-    tasklistProperties = new TasklistProperties();
     securityConfig = new CamundaSecurityProperties();
     securityConfig
         .getInitialization()
@@ -109,8 +96,6 @@ public final class NewCamundaTestApplication
 
     //noinspection resource
     withBean("config", brokerProperties, BrokerBasedProperties.class)
-        .withBean("operate-config", operateProperties, OperateProperties.class)
-        .withBean("tasklist-config", tasklistProperties, TasklistProperties.class)
         .withBean("security-config", securityConfig, CamundaSecurityProperties.class)
         .withAdditionalProfile(Profile.BROKER)
         .withAdditionalProfile(Profile.OPERATE)
@@ -119,13 +104,7 @@ public final class NewCamundaTestApplication
   }
 
   @Override
-  public NewCamundaTestApplication start() {
-    setExportersConfig();
-    return super.start();
-  }
-
-  @Override
-  public NewCamundaTestApplication stop() {
+  public TestSimpleCamundaApplication stop() {
     // clean up ES/OS indices
     LOGGER.info("Stopping standalone camunda test...");
     return super.stop();
@@ -151,7 +130,7 @@ public final class NewCamundaTestApplication
   }
 
   @Override
-  public NewCamundaTestApplication self() {
+  public TestSimpleCamundaApplication self() {
     return this;
   }
 
@@ -182,7 +161,7 @@ public final class NewCamundaTestApplication
           "Expected to get the gateway address for this broker, but the embedded gateway is not enabled");
     }
 
-    return TestGateway.super.grpcAddress();
+    return TestStandaloneApplication.super.grpcAddress();
   }
 
   @Override
@@ -191,7 +170,7 @@ public final class NewCamundaTestApplication
   }
 
   @Override
-  public NewCamundaTestApplication withGatewayConfig(final Consumer<GatewayCfg> modifier) {
+  public TestSimpleCamundaApplication withGatewayConfig(final Consumer<GatewayCfg> modifier) {
     modifier.accept(brokerProperties.getGateway());
     return this;
   }
@@ -208,49 +187,17 @@ public final class NewCamundaTestApplication
           "Cannot create a new client for this broker, as it does not have an embedded gateway");
     }
 
-    return TestGateway.super.newClientBuilder();
-  }
-
-  /** Returns the broker configuration */
-  public BrokerBasedProperties brokerConfig() {
-    return brokerProperties;
-  }
-
-  /**
-   * Modifies the broker configuration. Will still mutate the configuration if the broker is
-   * started, but likely has no effect until it's restarted.
-   */
-  public NewCamundaTestApplication withBrokerConfig(
-      final Consumer<BrokerBasedProperties> modifier) {
-    modifier.accept(brokerProperties);
-    return this;
+    return TestStandaloneApplication.super.newClientBuilder();
   }
 
   /**
    * Modifies the security configuration. Will still mutate the configuration if the broker is
    * started, but likely has no effect until it's restarted.
    */
-  public NewCamundaTestApplication withSecurityConfig(
+  public TestSimpleCamundaApplication withSecurityConfig(
       final Consumer<CamundaSecurityProperties> modifier) {
     modifier.accept(securityConfig);
     return this;
-  }
-
-  /**
-   * Enables/disables usage of the recording exporter using {@link #RECORDING_EXPORTER_ID} as its
-   * unique ID.
-   *
-   * @param useRecordingExporter if true, will enable the exporter; if false, will remove it from
-   *     the config
-   * @return itself for chaining
-   */
-  public NewCamundaTestApplication withRecordingExporter(final boolean useRecordingExporter) {
-    if (!useRecordingExporter) {
-      registeredExporters.remove(RECORDING_EXPORTER_ID);
-      return this;
-    }
-    return withExporter(
-        RECORDING_EXPORTER_ID, cfg -> cfg.setClassName(RecordingExporter.class.getName()));
   }
 
   /**
@@ -261,124 +208,23 @@ public final class NewCamundaTestApplication
    * @param modifier a configuration function
    * @return itself for chaining
    */
-  public NewCamundaTestApplication withExporter(
+  @Override
+  public TestSimpleCamundaApplication withExporter(
       final String id, final Consumer<ExporterCfg> modifier) {
-    registeredExporters.merge(id, modifier, (key, cfg) -> cfg.andThen(modifier));
+    final var cfg = new ExporterCfg();
+    modifier.accept(cfg);
+    brokerProperties.getExporters().put(id, cfg);
     return this;
   }
 
-  public NewCamundaTestApplication withElasticsearchSupport(
-      final String elasticSearchUrl, final String prefix) {
-    operateProperties.getElasticsearch().setUrl(elasticSearchUrl);
-    operateProperties.getElasticsearch().setIndexPrefix(prefix);
-    operateProperties.getZeebeElasticsearch().setUrl(elasticSearchUrl);
-    tasklistProperties.getElasticsearch().setUrl(elasticSearchUrl);
-    tasklistProperties.getElasticsearch().setIndexPrefix(prefix);
-    tasklistProperties.getZeebeElasticsearch().setUrl(elasticSearchUrl);
-
-    withExporter(
-        "CamundaExporter",
-        cfg -> {
-          cfg.setClassName(CamundaExporter.class.getName());
-          cfg.setArgs(
-              Map.of(
-                  "connect",
-                  Map.of(
-                      "url",
-                      elasticSearchUrl,
-                      "indexPrefix",
-                      prefix,
-                      "type",
-                      DatabaseType.ELASTICSEARCH),
-                  "index",
-                  Map.of("prefix", prefix),
-                  "bulk",
-                  Map.of("size", 1)));
-        });
-
-    withProperty("camunda.database.type", DatabaseType.ELASTICSEARCH);
-    withProperty("camunda.database.indexPrefix", prefix);
-    withProperty("camunda.database.url", elasticSearchUrl);
+  /**
+   * Modifies the broker configuration. Will still mutate the configuration if the broker is
+   * started, but likely has no effect until it's restarted.
+   */
+  @Override
+  public TestSimpleCamundaApplication withBrokerConfig(
+      final Consumer<BrokerBasedProperties> modifier) {
+    modifier.accept(brokerProperties);
     return this;
-  }
-
-  public NewCamundaTestApplication withOpensearchSupport(
-      final String opensearchUrl,
-      final String prefix,
-      final String adminUser,
-      final String adminPassword) {
-    final OperateOpensearchProperties operateOpensearch = operateProperties.getOpensearch();
-    operateOpensearch.setUrl(opensearchUrl);
-    operateOpensearch.setIndexPrefix(prefix);
-    operateOpensearch.setPassword(adminPassword);
-    operateOpensearch.setUsername(adminUser);
-
-    tasklistProperties.setDatabase("opensearch");
-    final TasklistOpenSearchProperties tasklistOpensearch = tasklistProperties.getOpenSearch();
-    tasklistOpensearch.setUrl(opensearchUrl);
-    tasklistOpensearch.setIndexPrefix(prefix);
-    tasklistOpensearch.setPassword(adminPassword);
-    tasklistOpensearch.setUsername(adminUser);
-
-    withExporter(
-        "CamundaExporter",
-        cfg -> {
-          cfg.setClassName(CamundaExporter.class.getName());
-          cfg.setArgs(
-              Map.of(
-                  "connect",
-                  Map.of(
-                      "url",
-                      opensearchUrl,
-                      "indexPrefix",
-                      prefix,
-                      "type",
-                      DatabaseType.OPENSEARCH,
-                      "username",
-                      adminUser,
-                      "password",
-                      adminPassword),
-                  "index",
-                  Map.of("prefix", prefix),
-                  "bulk",
-                  Map.of("size", 1)));
-        });
-
-    withProperty("camunda.database.type", DatabaseType.OPENSEARCH);
-    withProperty("camunda.operate.database", "opensearch");
-    withProperty("camunda.tasklist.database", "opensearch");
-    withProperty("camunda.database.indexPrefix", prefix);
-    withProperty("camunda.database.username", adminUser);
-    withProperty("camunda.database.password", adminPassword);
-    withProperty("camunda.database.url", opensearchUrl);
-    return this;
-  }
-
-  public NewCamundaTestApplication withRdbmsExporter() {
-    withProperty("camunda.database.type", DatabaseType.RDBMS);
-    withProperty(
-        "spring.datasource.url",
-        "jdbc:h2:mem:testdb+" + UUID.randomUUID() + ";DB_CLOSE_DELAY=-1;MODE=PostgreSQL");
-    withProperty("spring.datasource.driver-class-name", "org.h2.Driver");
-    withProperty("spring.datasource.username", "sa");
-    withProperty("spring.datasource.password", "");
-    withProperty("logging.level.io.camunda.db.rdbms", "DEBUG");
-    withProperty("logging.level.org.mybatis", "DEBUG");
-    withExporter(
-        "rdbms",
-        cfg -> {
-          cfg.setClassName("-");
-          cfg.setArgs(Map.of("flushInterval", "0"));
-        });
-    return this;
-  }
-
-  private void setExportersConfig() {
-    registeredExporters.forEach(
-        (id, exporterModifier) -> {
-          final var cfg = new ExporterCfg();
-          exporterModifier.accept(cfg);
-          brokerProperties.getExporters().put(id, cfg);
-        });
   }
 }
