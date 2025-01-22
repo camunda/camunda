a8ca081249fceca2993fa2fdfe38ae22a3f35f75
Jesse Simpson <jesse.simpson@camunda.com>
2025-01-22 16:53:15 -0500

    test: use login page to login rather than auth-basic

--- c8run/e2e_tests/tests/operate_login.spec.ts
+++ c8run/e2e_tests/tests/operate_login.spec.ts
@@ -3,6 +3,11 @@ const playwright = require('playwright');
 
 test('test', async ({ page }) => {
   test.setTimeout(60000);
-  await page.goto('http://localhost:8080/operate');
+  await page.goto('http://localhost:8080/operate/login');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('demo');
+  await page.getByPlaceholder('Username').press('Tab');
+  await page.getByPlaceholder('Password').fill('demo');
+  await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'Processes' }).click();
 });
