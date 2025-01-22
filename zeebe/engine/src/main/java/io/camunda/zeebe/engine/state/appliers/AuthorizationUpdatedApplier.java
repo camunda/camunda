8ced0c6b8c739ec1ed9c11d9c1acd1f7f2887e35
Nicola Puppa <nicola.puppa@camunda.com>
2025-01-22 15:29:04 +0100

    feat: add authorization updated applier

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/AuthorizationUpdatedApplier.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.state.appliers;
+
+import io.camunda.zeebe.engine.state.TypedEventApplier;
+import io.camunda.zeebe.engine.state.mutable.MutableAuthorizationState;
+import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
+import io.camunda.zeebe.protocol.record.intent.AuthorizationIntent;
+
+public class AuthorizationUpdatedApplier
+    implements TypedEventApplier<AuthorizationIntent, AuthorizationRecord> {
+
+  private final MutableAuthorizationState authorizationState;
+
+  public AuthorizationUpdatedApplier(final MutableAuthorizationState state) {
+    authorizationState = state;
+  }
+
+  @Override
+  public void applyState(final long key, final AuthorizationRecord value) {
+    // TODO: add update method call once https://github.com/camunda/camunda/issues/27203 is
+    // implemented
+  }
+}
