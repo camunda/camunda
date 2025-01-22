bd3189af28b25eec2194e3bc95dbc9da0d3c19dc
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2025-01-22 14:19:02 +0100

    feat: FineGrainedColumnFamilyMetrics replace prometheus metric with micrometer metric.

--- zeebe/zb-db/src/main/java/io/camunda/zeebe/db/impl/FineGrainedColumnFamilyMetrics.java
+++ zeebe/zb-db/src/main/java/io/camunda/zeebe/db/impl/FineGrainedColumnFamilyMetrics.java
@@ -9,53 +9,73 @@ package io.camunda.zeebe.db.impl;
 
 import io.camunda.zeebe.db.ColumnFamilyMetrics;
 import io.camunda.zeebe.protocol.EnumValue;
-import io.prometheus.client.Histogram;
-import io.prometheus.client.Histogram.Child;
-import io.prometheus.client.Histogram.Timer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
+import java.time.Duration;
+import java.util.Arrays;
 
 public final class FineGrainedColumnFamilyMetrics implements ColumnFamilyMetrics {
+  private static final MeterRegistry METER_REGISTRY = Metrics.globalRegistry;
 
-  private static final Histogram LATENCY =
-      Histogram.build()
-          .namespace("zeebe")
-          .name("rocksdb_latency")
-          .exponentialBuckets(0.00001, 2, 15)
-          .labelNames("partition", "columnFamily", "operation")
-          .help("Latency of RocksDB operations per column family")
-          .register();
+  private static final Duration[] LATENCY_DURATION_BUCKETS =
+      Arrays.stream(exponentialBuckets(0.00001, 2, 15))
+          .mapToObj(Duration::ofMillis)
+          .toArray(Duration[]::new);
 
-  private final Child getLatency;
-  private final Child putLatency;
-  private final Child deleteLatency;
-  private final Child iterateLatency;
+  private final Timer getLatency;
+  private final Timer putLatency;
+  private final Timer deleteLatency;
+  private final Timer iterateLatency;
 
   public <ColumnFamilyNames extends Enum<? extends EnumValue> & EnumValue>
       FineGrainedColumnFamilyMetrics(final int partitionId, final ColumnFamilyNames columnFamily) {
     final var partitionLabel = String.valueOf(partitionId);
     final var columnFamilyLabel = columnFamily.name();
-    getLatency = LATENCY.labels(partitionLabel, columnFamilyLabel, "get");
-    putLatency = LATENCY.labels(partitionLabel, columnFamilyLabel, "put");
-    deleteLatency = LATENCY.labels(partitionLabel, columnFamilyLabel, "delete");
-    iterateLatency = LATENCY.labels(partitionLabel, columnFamilyLabel, "iterate");
+    getLatency = createTimer(METER_REGISTRY, "get", partitionLabel, columnFamilyLabel);
+    putLatency = createTimer(METER_REGISTRY, "put", partitionLabel, columnFamilyLabel);
+    deleteLatency = createTimer(METER_REGISTRY, "delete", partitionLabel, columnFamilyLabel);
+    iterateLatency = createTimer(METER_REGISTRY, "iterate", partitionLabel, columnFamilyLabel);
+  }
+
+  private Timer createTimer(
+      final MeterRegistry registry,
+      final String operation,
+      final String partition,
+      final String columnFamily) {
+    return io.micrometer.core.instrument.Timer.builder("zeebe.rocksdb.latency")
+        .description("Latency of RocksDB operations per column family")
+        .tags("partition", partition, "columnFamily", columnFamily, "operation", operation)
+        .publishPercentileHistogram() // Enables histogram generation
+        .serviceLevelObjectives(LATENCY_DURATION_BUCKETS)
+        .register(registry);
+  }
+
+  private static long[] exponentialBuckets(final double start, final int factor, final int count) {
+    final long[] buckets = new long[count];
+    for (int i = 0; i < count; i++) {
+      buckets[i] = (long) (start * Math.pow(factor, i));
+    }
+    return buckets;
   }
 
   @Override
   public Timer measureGetLatency() {
-    return getLatency.startTimer();
+    return getLatency;
   }
 
   @Override
   public Timer measurePutLatency() {
-    return putLatency.startTimer();
+    return putLatency;
   }
 
   @Override
   public Timer measureDeleteLatency() {
-    return deleteLatency.startTimer();
+    return deleteLatency;
   }
 
   @Override
   public Timer measureIterateLatency() {
-    return iterateLatency.startTimer();
+    return iterateLatency;
   }
 }
