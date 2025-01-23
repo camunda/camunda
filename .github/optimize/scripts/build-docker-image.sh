a72c8620123076f5b7cc43f8ae6ce99984ef6c55
Christian Nicolai <christian.nicolai@camunda.com>
2025-01-23 11:35:11 +0100

    ci: publish Optimize 8.7.0-SNAPSHOTs to DockerHub

--- .github/optimize/scripts/build-docker-image.sh
+++ .github/optimize/scripts/build-docker-image.sh
@@ -12,6 +12,11 @@ if [ "${IS_MAIN}" = "true" ]; then
     tags+=("${DOCKER_IMAGE_DOCKER_HUB}:8-SNAPSHOT")
 fi
 
+# temporarily needed for https://github.com/camunda/product-hub/issues/2618
+if [ "${IS_STABLE_87}" = "true" ]; then
+    tags+=("${DOCKER_IMAGE_DOCKER_HUB}:8.7.0-SNAPSHOT")
+fi
+
 printf -v tag_arguments -- "-t %s " "${tags[@]}"
 docker buildx create --use
 
