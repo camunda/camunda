fae7f680cf00cfc5e9c49fa5d6c6eea992ed1d3a
Johannes Dollinger <johannes.dollinger@camunda.com>
2025-01-24 10:00:51 +0100

    refactor: handle security config updates for all property overrides

--- authentication/src/main/java/io/camunda/authentication/config/AuthenticationProperties.java
+++ authentication/src/main/java/io/camunda/authentication/config/AuthenticationProperties.java
@@ -7,6 +7,9 @@
  */
 package io.camunda.authentication.config;
 
+import io.camunda.security.configuration.SecurityConfiguration;
+import io.camunda.security.entity.AuthenticationMethod;
+
 public final class AuthenticationProperties {
   public static final String METHOD = "camunda.security.authentication.method";
   public static final String API_UNPROTECTED =
@@ -16,5 +19,20 @@ public final class AuthenticationProperties {
 
   public static String getAllowUnauthenticatedApiAccessEnvVar() {
     return API_UNPROTECTED.replace(".", "_").replace("-", "").toUpperCase();
+  }
+
+  public static void applyToSecurityConfig(
+      final SecurityConfiguration securityConfig, final String key, final Object value) {
+    switch (key) {
+      case METHOD ->
+          AuthenticationMethod.parse(String.valueOf(value))
+              .ifPresent(securityConfig.getAuthentication()::setMethod);
+      case API_UNPROTECTED ->
+          securityConfig
+              .getAuthentication()
+              .getBasic()
+              .setAllowUnauthenticatedApiAccess(Boolean.parseBoolean(String.valueOf(value)));
+      default -> {}
+    }
   }
 }
