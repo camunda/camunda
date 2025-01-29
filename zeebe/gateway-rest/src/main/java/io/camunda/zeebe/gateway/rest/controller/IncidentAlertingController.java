ca27244389cacbda2b7ed68f81a4b76eaf89592a
ana.vinogradova <ana.vinogradova@camunda.com>
2025-01-29 14:12:36 +0100

    feat: basic IncidentAlertingController

---
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/IncidentAlertingController.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.rest.controller;
+
+import io.camunda.zeebe.gateway.rest.annotation.CamundaGetMapping;
+import io.camunda.zeebe.gateway.rest.annotation.CamundaPostMapping;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CamundaRestController
+@RequestMapping(path = {"/v2/incident-alerting"})
+public class IncidentAlertingController {
+
+  private static final Map<String, AlertingChannel> storage = new ConcurrentHashMap<>();
+
+  @CamundaPostMapping(path = "/config")
+  public ResponseEntity<Object> storeConfig(@RequestBody final IncidentAlertingConfig config) {
+    storage.put(config.getFilter().getProcessDefinitionKey(), config.getChannel());
+    return ResponseEntity.ok().build();
+  }
+
+  @CamundaGetMapping()
+  public ResponseEntity<Object> getConfig() {
+    return ResponseEntity.ok(storage);
+  }
+}
+
+class IncidentAlertingConfig {
+  private Filter filter;
+  private AlertingChannel channel;
+
+  public AlertingChannel getChannel() {
+    return channel;
+  }
+
+  public void setChannel(final AlertingChannel channel) {
+    this.channel = channel;
+  }
+
+  public Filter getFilter() {
+    return filter;
+  }
+
+  public void setFilter(final Filter filter) {
+    this.filter = filter;
+  }
+}
+
+class Filter {
+  private String processDefinitionKey;
+
+  public String getProcessDefinitionKey() {
+    return processDefinitionKey;
+  }
+
+  public void setProcessDefinitionKey(final String processDefinitionKey) {
+    this.processDefinitionKey = processDefinitionKey;
+  }
+}
+
+class AlertingChannel {
+  private String email;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(final String email) {
+    this.email = email;
+  }
+}
