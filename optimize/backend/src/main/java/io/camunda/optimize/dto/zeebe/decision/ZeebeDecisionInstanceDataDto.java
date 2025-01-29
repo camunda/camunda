e3d1c738ee13edf8a43ac7d465afe5947d4283f4
Matt Bear <matthew.bear@camunda.com>
2025-01-29 13:15:28 -0600

    feat: hackathon hacks

---
+++ optimize/backend/src/main/java/io/camunda/optimize/dto/zeebe/decision/ZeebeDecisionInstanceDataDto.java
@@ -0,0 +1,354 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.optimize.dto.zeebe.decision;
+
+import io.camunda.zeebe.protocol.record.RecordValue;
+import java.util.List;
+import java.util.Map;
+
+public class ZeebeDecisionInstanceDataDto implements RecordValue {
+  private String evaluationFailureMessage;
+  private String decisionName;
+  private int decisionVersion;
+  private String decisionRequirementsId;
+  private long decisionRequirementsKey;
+  private String decisionOutput;
+  private String failedDecisionId;
+  private Map<String, Object> variables;
+  private String tenantId;
+  private String bpmnProcessId;
+  private long processInstanceKey;
+  private String decisionId;
+  private long decisionKey;
+  private long elementInstanceKey;
+  private long processDefinitionKey;
+  private String elementId;
+  private List<EvaluatedDecision> evaluatedDecisions;
+
+  // Getters and setters
+
+  public String getEvaluationFailureMessage() {
+    return evaluationFailureMessage;
+  }
+
+  // Getters and setters for the main class
+
+  public void setEvaluationFailureMessage(final String evaluationFailureMessage) {
+    this.evaluationFailureMessage = evaluationFailureMessage;
+  }
+
+  public String getDecisionName() {
+    return decisionName;
+  }
+
+  public void setDecisionName(final String decisionName) {
+    this.decisionName = decisionName;
+  }
+
+  public int getDecisionVersion() {
+    return decisionVersion;
+  }
+
+  public void setDecisionVersion(final int decisionVersion) {
+    this.decisionVersion = decisionVersion;
+  }
+
+  public String getDecisionRequirementsId() {
+    return decisionRequirementsId;
+  }
+
+  public void setDecisionRequirementsId(final String decisionRequirementsId) {
+    this.decisionRequirementsId = decisionRequirementsId;
+  }
+
+  public long getDecisionRequirementsKey() {
+    return decisionRequirementsKey;
+  }
+
+  public void setDecisionRequirementsKey(final long decisionRequirementsKey) {
+    this.decisionRequirementsKey = decisionRequirementsKey;
+  }
+
+  public String getDecisionOutput() {
+    return decisionOutput;
+  }
+
+  public void setDecisionOutput(final String decisionOutput) {
+    this.decisionOutput = decisionOutput;
+  }
+
+  public String getFailedDecisionId() {
+    return failedDecisionId;
+  }
+
+  public void setFailedDecisionId(final String failedDecisionId) {
+    this.failedDecisionId = failedDecisionId;
+  }
+
+  public Map<String, Object> getVariables() {
+    return variables;
+  }
+
+  public void setVariables(final Map<String, Object> variables) {
+    this.variables = variables;
+  }
+
+  public String getTenantId() {
+    return tenantId;
+  }
+
+  public void setTenantId(final String tenantId) {
+    this.tenantId = tenantId;
+  }
+
+  public String getBpmnProcessId() {
+    return bpmnProcessId;
+  }
+
+  public void setBpmnProcessId(final String bpmnProcessId) {
+    this.bpmnProcessId = bpmnProcessId;
+  }
+
+  public long getProcessInstanceKey() {
+    return processInstanceKey;
+  }
+
+  public void setProcessInstanceKey(final long processInstanceKey) {
+    this.processInstanceKey = processInstanceKey;
+  }
+
+  public String getDecisionId() {
+    return decisionId;
+  }
+
+  public void setDecisionId(final String decisionId) {
+    this.decisionId = decisionId;
+  }
+
+  public long getDecisionKey() {
+    return decisionKey;
+  }
+
+  public void setDecisionKey(final long decisionKey) {
+    this.decisionKey = decisionKey;
+  }
+
+  public long getElementInstanceKey() {
+    return elementInstanceKey;
+  }
+
+  public void setElementInstanceKey(final long elementInstanceKey) {
+    this.elementInstanceKey = elementInstanceKey;
+  }
+
+  public long getProcessDefinitionKey() {
+    return processDefinitionKey;
+  }
+
+  public void setProcessDefinitionKey(final long processDefinitionKey) {
+    this.processDefinitionKey = processDefinitionKey;
+  }
+
+  public String getElementId() {
+    return elementId;
+  }
+
+  public void setElementId(final String elementId) {
+    this.elementId = elementId;
+  }
+
+  public List<EvaluatedDecision> getEvaluatedDecisions() {
+    return evaluatedDecisions;
+  }
+
+  public void setEvaluatedDecisions(final List<EvaluatedDecision> evaluatedDecisions) {
+    this.evaluatedDecisions = evaluatedDecisions;
+  }
+
+  public static class EvaluatedDecision {
+    private String decisionName;
+    private int decisionVersion;
+    private String decisionType;
+    private String decisionOutput;
+    private String tenantId;
+    private String decisionId;
+    private long decisionKey;
+    private List<EvaluatedInput> evaluatedInputs;
+    private List<MatchedRule> matchedRules;
+
+    // Getters and setters
+
+    public String getDecisionName() {
+      return decisionName;
+    }
+
+    public void setDecisionName(final String decisionName) {
+      this.decisionName = decisionName;
+    }
+
+    public int getDecisionVersion() {
+      return decisionVersion;
+    }
+
+    public void setDecisionVersion(final int decisionVersion) {
+      this.decisionVersion = decisionVersion;
+    }
+
+    public String getDecisionType() {
+      return decisionType;
+    }
+
+    public void setDecisionType(final String decisionType) {
+      this.decisionType = decisionType;
+    }
+
+    public String getDecisionOutput() {
+      return decisionOutput;
+    }
+
+    public void setDecisionOutput(final String decisionOutput) {
+      this.decisionOutput = decisionOutput;
+    }
+
+    public String getTenantId() {
+      return tenantId;
+    }
+
+    public void setTenantId(final String tenantId) {
+      this.tenantId = tenantId;
+    }
+
+    public String getDecisionId() {
+      return decisionId;
+    }
+
+    public void setDecisionId(final String decisionId) {
+      this.decisionId = decisionId;
+    }
+
+    public long getDecisionKey() {
+      return decisionKey;
+    }
+
+    public void setDecisionKey(final long decisionKey) {
+      this.decisionKey = decisionKey;
+    }
+
+    public List<EvaluatedInput> getEvaluatedInputs() {
+      return evaluatedInputs;
+    }
+
+    public void setEvaluatedInputs(final List<EvaluatedInput> evaluatedInputs) {
+      this.evaluatedInputs = evaluatedInputs;
+    }
+
+    public List<MatchedRule> getMatchedRules() {
+      return matchedRules;
+    }
+
+    public void setMatchedRules(final List<MatchedRule> matchedRules) {
+      this.matchedRules = matchedRules;
+    }
+
+    public static class EvaluatedInput {
+      private String inputId;
+      private String inputName;
+      private String inputValue;
+
+      // Getters and setters
+
+      public String getInputId() {
+        return inputId;
+      }
+
+      public void setInputId(final String inputId) {
+        this.inputId = inputId;
+      }
+
+      public String getInputName() {
+        return inputName;
+      }
+
+      public void setInputName(final String inputName) {
+        this.inputName = inputName;
+      }
+
+      public String getInputValue() {
+        return inputValue;
+      }
+
+      public void setInputValue(final String inputValue) {
+        this.inputValue = inputValue;
+      }
+    }
+
+    public static class MatchedRule {
+      private String ruleId;
+      private int ruleIndex;
+      private List<EvaluatedOutput> evaluatedOutputs;
+
+      // Getters and setters
+
+      public String getRuleId() {
+        return ruleId;
+      }
+
+      public void setRuleId(final String ruleId) {
+        this.ruleId = ruleId;
+      }
+
+      public int getRuleIndex() {
+        return ruleIndex;
+      }
+
+      public void setRuleIndex(final int ruleIndex) {
+        this.ruleIndex = ruleIndex;
+      }
+
+      public List<EvaluatedOutput> getEvaluatedOutputs() {
+        return evaluatedOutputs;
+      }
+
+      public void setEvaluatedOutputs(final List<EvaluatedOutput> evaluatedOutputs) {
+        this.evaluatedOutputs = evaluatedOutputs;
+      }
+
+      public static class EvaluatedOutput {
+        private String outputId;
+        private String outputName;
+        private String outputValue;
+
+        // Getters and setters
+
+        public String getOutputId() {
+          return outputId;
+        }
+
+        public void setOutputId(final String outputId) {
+          this.outputId = outputId;
+        }
+
+        public String getOutputName() {
+          return outputName;
+        }
+
+        public void setOutputName(final String outputName) {
+          this.outputName = outputName;
+        }
+
+        public String getOutputValue() {
+          return outputValue;
+        }
+
+        public void setOutputValue(final String outputValue) {
+          this.outputValue = outputValue;
+        }
+      }
+    }
+  }
+}
---
+++ optimize/backend/src/main/java/io/camunda/optimize/service/importing/engine/service/zeebe/ZeebeDecisionInstanceImportService.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.optimize.service.importing.engine.service.zeebe;
+
+import io.camunda.optimize.dto.optimize.importing.DecisionInstanceDto;
+import io.camunda.optimize.dto.optimize.importing.InputInstanceDto;
+import io.camunda.optimize.dto.optimize.importing.OutputInstanceDto;
+import io.camunda.optimize.dto.optimize.query.variable.VariableType;
+import io.camunda.optimize.dto.zeebe.decision.ZeebeDecisionInstanceDataDto;
+import io.camunda.optimize.dto.zeebe.decision.ZeebeDecisionInstanceDataDto.EvaluatedDecision;
+import io.camunda.optimize.dto.zeebe.decision.ZeebeDecisionInstanceDataDto.EvaluatedDecision.EvaluatedInput;
+import io.camunda.optimize.dto.zeebe.decision.ZeebeDecisionInstanceDataDto.EvaluatedDecision.MatchedRule;
+import io.camunda.optimize.dto.zeebe.decision.ZeebeDecisionInstanceDataDto.EvaluatedDecision.MatchedRule.EvaluatedOutput;
+import io.camunda.optimize.dto.zeebe.decision.ZeebeDecisionInstanceRecordDto;
+import io.camunda.optimize.service.db.DatabaseClient;
+import io.camunda.optimize.service.db.writer.DecisionInstanceWriter;
+import io.camunda.optimize.service.importing.DatabaseImportJob;
+import io.camunda.optimize.service.importing.DatabaseImportJobExecutor;
+import io.camunda.optimize.service.importing.engine.service.ImportService;
+import io.camunda.optimize.service.importing.job.DecisionInstanceDatabaseImportJob;
+import io.camunda.optimize.service.util.IdGenerator;
+import io.camunda.optimize.service.util.configuration.ConfigurationService;
+import io.camunda.zeebe.protocol.record.intent.DecisionEvaluationIntent;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+
+public class ZeebeDecisionInstanceImportService
+    implements ImportService<ZeebeDecisionInstanceRecordDto> {
+
+  public static final Set<DecisionEvaluationIntent> INTENTS_TO_IMPORT =
+      Set.of(DecisionEvaluationIntent.EVALUATED);
+  private static final Logger LOG =
+      org.slf4j.LoggerFactory.getLogger(ZeebeDecisionInstanceImportService.class);
+
+  private final DatabaseImportJobExecutor databaseImportJobExecutor;
+  private final ConfigurationService configurationService;
+  private final DecisionInstanceWriter decisionInstanceWriter;
+  private final int partitionId;
+  private final DatabaseClient databaseClient;
+
+  public ZeebeDecisionInstanceImportService(
+      final ConfigurationService configurationService,
+      final DecisionInstanceWriter decisionInstanceWriter,
+      final int partitionId,
+      // final ProcessDefinitionReader processDefinitionReader,
+      final DatabaseClient databaseClient) {
+    databaseImportJobExecutor =
+        new DatabaseImportJobExecutor(getClass().getSimpleName(), configurationService);
+    this.configurationService = configurationService;
+    this.decisionInstanceWriter = decisionInstanceWriter;
+    this.partitionId = partitionId;
+    this.databaseClient = databaseClient;
+  }
+
+  @Override
+  public void executeImport(
+      final List<ZeebeDecisionInstanceRecordDto> pageOfDecisionInstancess,
+      final Runnable importCompleteCallback) {
+    LOG.trace("Importing decision instances from zeebe records...");
+
+    final boolean newDataIsAvailable = !pageOfDecisionInstancess.isEmpty();
+    if (newDataIsAvailable) {
+      final List<DecisionInstanceDto> newOptimizeEntities =
+          filterAndMapZeebeRecordsToOptimizeEntities(pageOfDecisionInstancess);
+      final DatabaseImportJob<DecisionInstanceDto> databaseImportJob =
+          createDatabaseImportJob(newOptimizeEntities, importCompleteCallback);
+      addDatabaseImportJobToQueue(databaseImportJob);
+    }
+  }
+
+  @Override
+  public DatabaseImportJobExecutor getDatabaseImportJobExecutor() {
+    return databaseImportJobExecutor;
+  }
+
+  private List<DecisionInstanceDto> filterAndMapZeebeRecordsToOptimizeEntities(
+      final List<ZeebeDecisionInstanceRecordDto> zeebeRecords) {
+    final List<DecisionInstanceDto> optimizeDtos =
+        zeebeRecords.stream()
+            .filter(zeebeRecord -> INTENTS_TO_IMPORT.contains(zeebeRecord.getIntent()))
+            .map(this::mapZeebeRecordsToOptimizeEntities)
+            .collect(Collectors.toList());
+    LOG.debug(
+        "Processing {} fetched zeebe decision instance records, of which {} are relevant to Optimize and will be imported.",
+        zeebeRecords.size(),
+        optimizeDtos.size());
+    return optimizeDtos;
+  }
+
+  private DecisionInstanceDto mapZeebeRecordsToOptimizeEntities(
+      final ZeebeDecisionInstanceRecordDto zeebeDecisionInstanceRecord) {
+    final ZeebeDecisionInstanceDataDto recordData = zeebeDecisionInstanceRecord.getValue();
+    final DecisionInstanceDto instanceDto = new DecisionInstanceDto();
+    instanceDto.setDecisionInstanceId(String.valueOf(recordData.getDecisionKey()));
+    instanceDto.setProcessDefinitionId(String.valueOf(recordData.getProcessDefinitionKey()));
+    instanceDto.setProcessDefinitionKey(recordData.getBpmnProcessId());
+    instanceDto.setDecisionDefinitionId(String.valueOf(recordData.getDecisionRequirementsKey()));
+    instanceDto.setDecisionDefinitionKey(recordData.getDecisionRequirementsId());
+    instanceDto.setDecisionDefinitionVersion(String.valueOf(recordData.getDecisionVersion()));
+    instanceDto.setEvaluationDateTime(zeebeDecisionInstanceRecord.getDateForTimestamp());
+    instanceDto.setProcessInstanceId(String.valueOf(recordData.getProcessInstanceKey()));
+    // test.setRootProcessInstanceId();
+    instanceDto.setActivityId(recordData.getElementId());
+    // test.setCollectResultValue();
+    // test.setRootDecisionInstanceId();
+    instanceDto.setEngine(configurationService.getConfiguredZeebe().getName());
+    instanceDto.setTenantId(recordData.getTenantId());
+
+    final List<InputInstanceDto> inputs = new ArrayList<>();
+    final List<OutputInstanceDto> outputs = new ArrayList<>();
+    final Set<String> matchedRuleIds = new HashSet<>();
+    for (final EvaluatedDecision evaluatedDecision : recordData.getEvaluatedDecisions()) {
+      final String decisionId = evaluatedDecision.getDecisionId();
+      for (final EvaluatedInput evaluatedInput : evaluatedDecision.getEvaluatedInputs()) {
+        final InputInstanceDto inputInstanceDto =
+            new InputInstanceDto(
+                IdGenerator.getNextId(),
+                evaluatedInput.getInputId(),
+                evaluatedInput.getInputName(),
+                VariableType.OBJECT,
+                evaluatedInput.getInputValue());
+        inputs.add(inputInstanceDto);
+      }
+
+      for (final MatchedRule matchedRule : evaluatedDecision.getMatchedRules()) {
+        matchedRuleIds.add(matchedRule.getRuleId());
+        for (final EvaluatedOutput evaluatedOutput : matchedRule.getEvaluatedOutputs()) {
+          final OutputInstanceDto output =
+              new OutputInstanceDto(
+                  IdGenerator.getNextId(),
+                  evaluatedOutput.getOutputId(),
+                  evaluatedOutput.getOutputName(),
+                  matchedRule.getRuleId(),
+                  matchedRule.getRuleIndex(),
+                  evaluatedOutput.getOutputName(),
+                  VariableType.OBJECT,
+                  evaluatedOutput.getOutputValue());
+          outputs.add(output);
+        }
+      }
+    }
+    instanceDto.setInputs(inputs);
+    instanceDto.setOutputs(outputs);
+    instanceDto.setMatchedRules(matchedRuleIds);
+
+    return instanceDto;
+  }
+
+  private DatabaseImportJob<DecisionInstanceDto> createDatabaseImportJob(
+      final List<DecisionInstanceDto> instanceDefinitions, final Runnable importCompleteCallback) {
+    final DecisionInstanceDatabaseImportJob decInstantImportJob =
+        new DecisionInstanceDatabaseImportJob(
+            decisionInstanceWriter, importCompleteCallback, databaseClient);
+    decInstantImportJob.setEntitiesToImport(instanceDefinitions);
+    return decInstantImportJob;
+  }
+
+  private void addDatabaseImportJobToQueue(
+      final DatabaseImportJob<DecisionInstanceDto> databaseImportJob) {
+    databaseImportJobExecutor.executeImportJob(databaseImportJob);
+  }
+}
