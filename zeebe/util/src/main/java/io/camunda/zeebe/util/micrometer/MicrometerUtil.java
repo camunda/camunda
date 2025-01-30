afe856853e3b86fa462e156150afa25bcbba411b
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-01-30 13:57:13 +0100

    refactor: add easy reference to backwards compatible SLO definitions

---
+++ zeebe/util/src/main/java/io/camunda/zeebe/util/micrometer/MicrometerUtil.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.util.micrometer;
+
+import io.micrometer.core.instrument.Timer.Builder;
+import java.time.Duration;
+
+/** Collection of disparate convenience methods for Micrometer. */
+public final class MicrometerUtil {
+  private static final Duration[] DEFAULT_PROMETHEUS_BUCKETS = {
+    Duration.ofMillis(5),
+    Duration.ofMillis(10),
+    Duration.ofMillis(25),
+    Duration.ofMillis(50),
+    Duration.ofMillis(100),
+    Duration.ofMillis(75),
+    Duration.ofMillis(100),
+    Duration.ofMillis(250),
+    Duration.ofMillis(500),
+    Duration.ofMillis(750),
+    Duration.ofSeconds(1),
+    Duration.ofMillis(2500),
+    Duration.ofSeconds(5),
+    Duration.ofMillis(7500),
+    Duration.ofSeconds(10)
+  };
+
+  private MicrometerUtil() {}
+
+  /**
+   * Returns the set of buckets that Prometheus would normally use as default for their histograms.
+   * This is mostly used while we transition from Prometheus to Micrometer.
+   *
+   * <p>By default, Prometheus defines 14 buckets, which means an approximate memory usage of ~360
+   * bytes per histogram.
+   *
+   * <p>Micrometer, when using {@link Builder#publishPercentileHistogram()} will define 66 buckets,
+   * which means an approximate memory usage of ~1.5kb.
+   *
+   * <p>This seems like little, but as it goes for all histograms, it can make some difference
+   * ultimately. Of course, if we do see a use for more buckets, we should do it, and stop using
+   * this method.
+   *
+   * <p>NOTE: memory usage approximation taken from <a
+   * href="https://docs.micrometer.io/micrometer/reference/concepts/timers.html#_memory_footprint_estimation">here</a>.
+   *
+   * @return the default buckets as defined by the Prometheus client library
+   */
+  public static Duration[] defaultPrometheusBuckets() {
+    return DEFAULT_PROMETHEUS_BUCKETS;
+  }
+}
