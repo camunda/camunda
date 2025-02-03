05363d7e67788520c8477804f58774adbbf7952a
Nicola Puppa <nicola.puppa@camunda.com>
2025-02-03 15:48:46 +0100

    fix: partial authorization update is not supported
    Zeebe expects from the client the full updated record

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/AuthorizationUpdateProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/AuthorizationUpdateProcessor.java
@@ -15,7 +15,6 @@ import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedResponseWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
-import io.camunda.zeebe.engine.state.authorization.PersistedAuthorization;
 import io.camunda.zeebe.engine.state.distribution.DistributionQueue;
 import io.camunda.zeebe.engine.state.immutable.ProcessingState;
 import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
@@ -55,9 +54,6 @@ public class AuthorizationUpdateProcessor
             authorizationRecord ->
                 permissionsBehavior.authorizationExists(
                     authorizationRecord, AUTHORIZATION_DOES_NOT_EXIST_ERROR_MESSAGE_UPDATE))
-        .map(
-            persistedAuthorization ->
-                overlayAuthorization(command.getValue(), persistedAuthorization))
         .flatMap(
             record ->
                 permissionsBehavior.hasValidPermissionTypes(
@@ -98,28 +94,5 @@ public class AuthorizationUpdateProcessor
         .distribute(command);
     responseWriter.writeEventOnCommand(
         key, AuthorizationIntent.UPDATED, authorizationRecord, command);
-  }
-
-  // Create the new record with the persisted values if not changed by the user
-  private AuthorizationRecord overlayAuthorization(
-      final AuthorizationRecord newAuthorization,
-      final PersistedAuthorization persistedAuthorization) {
-    final var changeset = newAuthorization.getChangedAttributes();
-    if (!changeset.contains(AuthorizationRecord.OWNER_ID)) {
-      newAuthorization.setOwnerId(persistedAuthorization.getOwnerId());
-    }
-    if (!changeset.contains(AuthorizationRecord.OWNER_TYPE)) {
-      newAuthorization.setOwnerType(persistedAuthorization.getOwnerType());
-    }
-    if (!changeset.contains(AuthorizationRecord.RESOURCE_ID)) {
-      newAuthorization.setResourceId(persistedAuthorization.getResourceId());
-    }
-    if (!changeset.contains(AuthorizationRecord.RESOURCE_TYPE)) {
-      newAuthorization.setResourceType(persistedAuthorization.getResourceType());
-    }
-    if (!changeset.contains(AuthorizationRecord.PERMISSIONS)) {
-      newAuthorization.setAuthorizationPermissions(persistedAuthorization.getPermissions());
-    }
-    return newAuthorization;
   }
 }
