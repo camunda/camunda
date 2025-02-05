b146174004b570410104194a35e1a33d5467839a
Carlo Sana <carlo.sana@camunda.com>
2025-02-05 17:07:03 +0100

    feat: migrate RaftReplicationMetrics to micrometer
    relates #26708

---
+++ zeebe/atomix/cluster/src/main/java/io/atomix/raft/metrics/RaftReplicationMetricsDoc.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.atomix.raft.metrics;
+
+import io.camunda.zeebe.util.micrometer.ExtendedMeterDocumentation;
+import io.micrometer.core.instrument.Meter.Type;
+
+public enum RaftReplicationMetricsDoc implements ExtendedMeterDocumentation {
+  COMMIT_INDEX {
+    @Override
+    public String getName() {
+      return "partition.raft.commit.index";
+    }
+
+    @Override
+    public Type getType() {
+      return Type.GAUGE;
+    }
+
+    @Override
+    public String getDescription() {
+      return "The commit index";
+    }
+  },
+  APPEND_INDEX {
+    @Override
+    public String getName() {
+      return "partition.raft.append.index";
+    }
+
+    @Override
+    public Type getType() {
+      return Type.GAUGE;
+    }
+
+    @Override
+    public String getDescription() {
+      return "The index of last entry appended to the log";
+    }
+  }
+}
