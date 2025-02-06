dd8a9ae26abcf16f7c2ea879103a881e6f9026db
Carlo Sana <carlo.sana@camunda.com>
2025-02-06 10:30:59 +0100

    feat: migrate MetaStoreMetrics to prometheus
    relates #26708
    
    (cherry picked from commit b0cbc7b64c610f243468471dd866ac2b045d662c)

---
+++ zeebe/atomix/cluster/src/main/java/io/atomix/raft/metrics/MetaStoreMetricsDoc.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.atomix.raft.metrics;
+
+import io.camunda.zeebe.util.micrometer.ExtendedMeterDocumentation;
+import io.micrometer.core.instrument.Meter.Type;
+
+public enum MetaStoreMetricsDoc implements ExtendedMeterDocumentation {
+  /** Time it takes to update the last flushed index */
+  LAST_FLUSHED_INDEX {
+    @Override
+    public String getName() {
+      return "last_flushed_index_update";
+    }
+
+    @Override
+    public Type getType() {
+      return Type.TIMER;
+    }
+
+    @Override
+    public String getDescription() {
+      return "Time it takes to update the last flushed index";
+    }
+
+    @Override
+    public String getBaseUnit() {
+      return "ms";
+    }
+  }
+}
