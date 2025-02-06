f71cb1e4ec3bbefd0387a39cd4fa2c4c593efc06
Kostya Zraitel <kos.zrt@gmail.com>
2025-02-06 17:04:44 +0200

    feat: add LinkedResource transformation
    (cherry picked from commit 48fd2f43d11c4c56433db03cf1399b5690459c9b)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnJobBehavior.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnJobBehavior.java
@@ -44,6 +44,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.agrona.DirectBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
