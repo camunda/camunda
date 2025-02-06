54d22ed8b27f2b97234e633645384235187bbdb8
Christian Thiel <christian.thiel@holisticon.de>
2025-02-06 16:42:57 +0100

    feat: support processInstance migration #24057

--- db/rdbms/src/main/java/io/camunda/db/rdbms/sql/UserTaskMapper.java
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/sql/UserTaskMapper.java
@@ -9,6 +9,7 @@ package io.camunda.db.rdbms.sql;
 
 import io.camunda.db.rdbms.read.domain.UserTaskDbQuery;
 import io.camunda.db.rdbms.write.domain.UserTaskDbModel;
+import io.camunda.util.ObjectBuilder;
 import java.util.List;
 
 public interface UserTaskMapper {
@@ -25,7 +26,61 @@ public interface UserTaskMapper {
 
   void deleteCandidateGroups(Long key);
 
+  void migrateToProcess(MigrateToProcessDto dto);
+
   Long count(UserTaskDbQuery filter);
 
   List<UserTaskDbModel> search(UserTaskDbQuery filter);
+
+  record MigrateToProcessDto(
+      Long userTaskKey,
+      Long processDefinitionKey,
+      String processDefinitionId,
+      String elementId,
+      Integer processDefinitionVersion) {
+
+    public static class Builder implements ObjectBuilder<MigrateToProcessDto> {
+
+      private Long userTaskKey;
+      private Long processDefinitionKey;
+      private String processDefinitionId;
+      private String elementId;
+      private Integer processDefinitionVersion;
+
+      public Builder userTaskKey(Long userTaskKey) {
+        this.userTaskKey = userTaskKey;
+        return this;
+      }
+
+      public Builder processDefinitionKey(Long processDefinitionKey) {
+        this.processDefinitionKey = processDefinitionKey;
+        return this;
+      }
+
+      public Builder processDefinitionId(String processDefinitionId) {
+        this.processDefinitionId = processDefinitionId;
+        return this;
+      }
+
+      public Builder elementId(String elementId) {
+        this.elementId = elementId;
+        return this;
+      }
+
+      public Builder processDefinitionVersion(Integer processDefinitionVersion) {
+        this.processDefinitionVersion = processDefinitionVersion;
+        return this;
+      }
+
+      @Override
+      public MigrateToProcessDto build() {
+        return new MigrateToProcessDto(
+            userTaskKey,
+            processDefinitionKey,
+            processDefinitionId,
+            elementId,
+            processDefinitionVersion);
+      }
+    }
+  }
 }
