43e7f4178d23e3021c087a1a9dfb5febd964557e
Nico Korthout <nico.korthout@camunda.com>
2025-02-07 18:17:30 +0100

    fix: resiliently transform execution listeners
    While a simple try catch would be sufficient, I wanted to separate the
    expected errors from the unexpected in logging, i.e. debug vs warn.
    
    For the expected errors, we can use an either to improve the readability
    over a bunch of if-else blocks with their own log statements.
    
    (cherry picked from commit 22a22dd01f517627f0d18d4643928536d3dfc3b4)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/model/transformer/zeebe/ExecutionListenerTransformer.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/model/transformer/zeebe/ExecutionListenerTransformer.java
@@ -10,9 +10,16 @@ package io.camunda.zeebe.engine.processing.deployment.model.transformer.zeebe;
 import io.camunda.zeebe.el.ExpressionLanguage;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableFlowNode;
 import io.camunda.zeebe.model.bpmn.instance.zeebe.ZeebeExecutionListener;
+import io.camunda.zeebe.util.Either;
+import io.camunda.zeebe.util.buffer.BufferUtil;
 import java.util.Collection;
+import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class ExecutionListenerTransformer {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExecutionListenerTransformer.class);
 
   public void transform(
       final ExecutableFlowNode executableFlowNode,
@@ -20,17 +27,55 @@ public final class ExecutionListenerTransformer {
       final ExpressionLanguage expressionLanguage) {
 
     executionListeners.forEach(
-        listener -> addListenerToFlowNode(listener, executableFlowNode, expressionLanguage));
+        listener -> {
+          try {
+            addListenerToFlowNode(listener, executableFlowNode, expressionLanguage);
+          } catch (final Exception e) {
+            // This is an additional safety, as our transformers currently require additional
+            // safeguards to avoid breaking the partition on replay
+            LOGGER.warn(
+                """
+                Failed to transform execution listener for flow node '{}', \
+                caught an unexpected exception: '{}': {}. Ignoring this listener definition.""",
+                BufferUtil.bufferAsString(executableFlowNode.getId()),
+                e.getClass().getSimpleName(),
+                e.getMessage());
+          }
+        });
   }
 
   private void addListenerToFlowNode(
       final ZeebeExecutionListener listener,
       final ExecutableFlowNode flowNode,
       final ExpressionLanguage expressionLanguage) {
-
-    flowNode.addListener(
-        listener.getEventType(),
-        expressionLanguage.parseExpression(listener.getType()),
-        expressionLanguage.parseExpression(listener.getRetries()));
+    Either.<Error, ZeebeExecutionListener>right(listener)
+        .flatMap(l -> requireNotNull(l, () -> l, "listener"))
+        .flatMap(l -> requireNotNull(l, l::getEventType, "eventType"))
+        .flatMap(l -> requireNotNull(l, l::getType, "type"))
+        .flatMap(l -> requireNotNull(l, l::getRetries, "retries"))
+        .ifRightOrLeft(
+            ok ->
+                flowNode.addListener(
+                    listener.getEventType(),
+                    expressionLanguage.parseExpression(listener.getType()),
+                    expressionLanguage.parseExpression(listener.getRetries())),
+            error ->
+                LOGGER.debug(
+                    """
+                    Failed to transform execution listener for flow node '%s', \
+                    because %s. Ignoring this listener definition."""
+                        .formatted(BufferUtil.bufferAsString(flowNode.getId()), error.reason())));
   }
+
+  private Either<Error, ZeebeExecutionListener> requireNotNull(
+      final ZeebeExecutionListener listener,
+      final Supplier<?> attributeSupplier,
+      final String attributeName) {
+    if (attributeSupplier.get() == null) {
+      return Either.left(new Error("'%s' is null".formatted(attributeName)));
+    }
+    return Either.right(listener);
+  }
+
+  private record Error(String reason) {}
 }
