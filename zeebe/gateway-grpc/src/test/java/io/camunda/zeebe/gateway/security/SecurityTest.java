d2d93365e9a9e93142d53ce5c0574f633a5d94d3
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-02-09 23:17:21 +0100

    refactor: migrate gRPC gateway metrics to micrometer

--- zeebe/gateway-grpc/src/test/java/io/camunda/zeebe/gateway/security/SecurityTest.java
+++ zeebe/gateway-grpc/src/test/java/io/camunda/zeebe/gateway/security/SecurityTest.java
@@ -27,6 +27,7 @@ import io.camunda.zeebe.scheduler.ActorScheduler;
 import io.camunda.zeebe.scheduler.future.ActorFuture;
 import io.camunda.zeebe.test.util.asserts.SslAssert;
 import io.camunda.zeebe.test.util.socket.SocketUtil;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import java.io.File;
 import java.io.IOException;
@@ -190,6 +191,7 @@ final class SecurityTest {
         actorScheduler,
         jobStreamClient.streamer(),
         mock(UserServices.class),
-        mock(PasswordEncoder.class));
+        mock(PasswordEncoder.class),
+        new SimpleMeterRegistry());
   }
 }
