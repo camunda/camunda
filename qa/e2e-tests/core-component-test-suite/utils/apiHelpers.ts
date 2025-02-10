5630100f485b731e50d7474cec1eb3cd8f0a2521
esraagamal6 <esraa-gamal@hotmail.com>
2025-02-10 11:48:21 +0100

    Update API tests for 8.7

--- qa/e2e-tests/core-component-test-suite/utils/apiHelpers.ts
+++ qa/e2e-tests/core-component-test-suite/utils/apiHelpers.ts
@@ -1,21 +1,24 @@
-import {APIRequestContext, expect, request, Response} from '@playwright/test';
+import {
+  APIRequestContext,
+  expect,
+  request,
+  APIResponse,
+} from '@playwright/test';
 
 export async function auhtAPI(name: string, password: string): Promise<void> {
   const apiRequestContext: APIRequestContext = await request.newContext();
-  await apiRequestContext.post(
-    '/api/login?username=' + name + '&password=' + password,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
+  await apiRequestContext.post('/api/login', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-  );
+    form: {
+      username: name,
+      password: password,
+    },
+  });
   await apiRequestContext.storageState({path: 'utils/.auth'});
 }
 
-export async function assertResponseStatus(
-  response: Response,
-  status: number,
-): Promise<void> {
+export function assertResponseStatus(response: APIResponse, status: number) {
   expect(response.status()).toBe(status);
 }
