c1a47b3a8c3072ce895f9f2d93b13730c11b7a81
Carlo Sana <carlo.sana@camunda.com>
2025-02-10 16:23:06 +0100

    feat: migrate JournalMetrics to micrometer

--- zeebe/journal/src/test/java/io/camunda/zeebe/journal/file/SegmentLoaderTest.java
+++ zeebe/journal/src/test/java/io/camunda/zeebe/journal/file/SegmentLoaderTest.java
@@ -8,9 +8,12 @@
 package io.camunda.zeebe.journal.file;
 
 import io.camunda.zeebe.journal.util.PosixPathAssert;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.api.parallel.Execution;
@@ -19,6 +22,8 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 @SuppressWarnings("resource")
 @Execution(ExecutionMode.CONCURRENT)
 final class SegmentLoaderTest {
+  @AutoClose private final MeterRegistry meterRegistry = new SimpleMeterRegistry();
+
   @Test
   void shouldPreallocateNewFileIfUnusedSegmentAlreadyExists(final @TempDir Path tmpDir)
       throws IOException {
@@ -26,7 +31,7 @@ final class SegmentLoaderTest {
     final var segmentSize = 4 * 1024 * 1024;
     final var descriptor =
         SegmentDescriptor.builder().withId(1).withIndex(1).withMaxSegmentSize(segmentSize).build();
-    final var segmentLoader = new SegmentLoader(segmentSize * 2, new JournalMetrics("1"));
+    final var segmentLoader = new SegmentLoader(segmentSize * 2, new JournalMetrics(meterRegistry));
     final var segmentFile = tmpDir.resolve("segment.log");
 
     // when - "unused" segment can happen if we crashed in the middle of creating the new segment
