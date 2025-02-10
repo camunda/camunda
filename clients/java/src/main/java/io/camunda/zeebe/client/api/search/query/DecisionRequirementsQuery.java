e0b7fbf8ba60967a3225171c1c3e9374cc9a719d
Houssain Barouni <houssain.barouni@camunda.com>
2025-02-10 17:10:23 +0100

    docs: update java client deprecation versions

--- clients/java/src/main/java/io/camunda/zeebe/client/api/search/query/DecisionRequirementsQuery.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/api/search/query/DecisionRequirementsQuery.java
@@ -21,7 +21,7 @@ import io.camunda.zeebe.client.api.search.response.DecisionRequirements;
 import io.camunda.zeebe.client.api.search.sort.DecisionRequirementsSort;
 
 /**
- * @deprecated since 8.7 for removal in 8.8, replaced by {@link
+ * @deprecated since 8.8 for removal in 8.9, replaced by {@link
  *     io.camunda.client.api.search.query.DecisionRequirementsQuery}
  */
 @Deprecated
--- clients/java/src/main/java/io/camunda/zeebe/client/api/search/query/UserTaskQuery.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/api/search/query/UserTaskQuery.java
@@ -21,7 +21,7 @@ import io.camunda.zeebe.client.api.search.response.UserTask;
 import io.camunda.zeebe.client.api.search.sort.UserTaskSort;
 
 /**
- * @deprecated since 8.7 for removal in 8.8, replaced by {@link
+ * @deprecated since 8.8 for removal in 8.9, replaced by {@link
  *     io.camunda.client.api.search.query.UserTaskQuery}
  */
 @Deprecated
