5843d2fbdac7c4c248867f0adac9a3d47e33d6cc
OmranAbazid <9063094+OmranAbazid@users.noreply.github.com>
2025-02-10 21:23:29 +0100

    refactor: align users page design
    related to #27395

--- identity/client/src/pages/users/List.tsx
+++ identity/client/src/pages/users/List.tsx
@@ -6,25 +6,25 @@
  * except in compliance with the Camunda License 1.0.
  */
 import { FC, useState } from "react";
+import { useNavigate } from "react-router";
+import { Stack } from "@carbon/react";
+import { Add, Edit, TrashCan } from "@carbon/react/icons";
+import { spacing04, spacing06 } from "@carbon/elements";
+import { C3EmptyState } from "@camunda/camunda-composite-components";
 import useTranslate from "src/utility/localization";
 import { useApi } from "src/utility/api/hooks";
-import Page, { PageTitle } from "src/components/layout/Page";
-import EntityList, {
-  DocumentationDescription,
-} from "src/components/entityList";
+import Page, { PageTitle, PageSubTitle } from "src/components/layout/Page";
+import EntityList from "src/components/entityList";
 import {
   documentationHref,
   DocumentationLink,
 } from "src/components/documentation";
 import { searchUser, User } from "src/utility/api/users";
-import { useNavigate } from "react-router";
 import { TranslatedErrorInlineNotification } from "src/components/notifications/InlineNotification";
 import useModal, { useEntityModal } from "src/components/modal/useModal";
 import AddModal from "src/pages/users/modals/AddModal";
 import EditModal from "src/pages/users/modals/EditModal";
 import DeleteModal from "src/pages/users/modals/DeleteModal";
-import { Edit, TrashCan } from "@carbon/react/icons";
-import { C3EmptyState } from "@camunda/camunda-composite-components";
 
 const List: FC = () => {
   const { t, Translate } = useTranslate();
@@ -42,26 +42,49 @@ const List: FC = () => {
 
   const showDetails = ({ username }: User) => navigate(`${username}`);
 
+  const header = (
+    <Stack gap={spacing04}>
+      <PageTitle>
+        <Translate>Users</Translate>
+      </PageTitle>
+      {success && (
+        <PageSubTitle>
+          <Translate>For more information, see documentation on</Translate>{" "}
+          <DocumentationLink path="/concepts/access-control/users">
+            users
+          </DocumentationLink>
+        </PageSubTitle>
+      )}
+    </Stack>
+  );
+
   if (success && !userSearchResults?.items.length) {
     return (
       <Page>
-        <PageTitle>
-          <Translate>Users</Translate>
-        </PageTitle>
-        <C3EmptyState
-          heading={t("You don’t have any users yet")}
-          description={t(
-            "Roles, permissions, and authorizations can be applied to a user.",
-          )}
-          button={{
-            label: t("Create a user"),
-            onClick: addUser,
-          }}
-          link={{
-            href: documentationHref("/concepts/access-control/users", ""),
-            label: t("Learn more about groups"),
-          }}
-        />
+        <Stack gap={spacing06}>
+          {header}
+          <C3EmptyState
+            heading={t("No users created yet")}
+            description={
+              <>
+                <Translate>Start by</Translate>{" "}
+                <DocumentationLink path="/concepts/access-control/users">
+                  creating a new user
+                </DocumentationLink>{" "}
+                <Translate>to get started</Translate>
+              </>
+            }
+            button={{
+              label: t("Create a user"),
+              onClick: addUser,
+              icon: Add,
+            }}
+            link={{
+              href: documentationHref("/concepts/access-control/users", ""),
+              label: t("Learn more about users"),
+            }}
+          />
+        </Stack>
         {addUserModal}
       </Page>
     );
@@ -69,40 +92,37 @@ const List: FC = () => {
 
   return (
     <Page>
-      <EntityList
-        title={t("Users")}
-        data={userSearchResults == null ? [] : userSearchResults.items}
-        headers={[
-          { header: t("Name"), key: "name" },
-          { header: t("Username"), key: "username" },
-          { header: t("Email"), key: "email" },
-        ]}
-        menuItems={[
-          {
-            label: t("Edit user"),
-            onClick: editUser,
-            icon: Edit,
-          },
-          {
-            label: t("Delete user"),
-            icon: TrashCan,
-            onClick: deleteUser,
-            isDangerous: true,
-          },
-        ]}
-        sortProperty="username"
-        onEntityClick={showDetails}
-        addEntityLabel={t("Create user")}
-        onAddEntity={addUser}
-        onSearch={setSearch}
-        loading={loading}
-      />
-      {success && (
-        <DocumentationDescription>
-          <Translate>Learn more about users in our</Translate>{" "}
-          <DocumentationLink path="/concepts/access-control/users" />.
-        </DocumentationDescription>
-      )}
+      <Stack gap={spacing06}>
+        {header}
+        <EntityList
+          data={userSearchResults == null ? [] : userSearchResults.items}
+          headers={[
+            { header: t("Username"), key: "username" },
+            { header: t("Name"), key: "name" },
+            { header: t("Email"), key: "email" },
+          ]}
+          menuItems={[
+            {
+              label: t("Edit user"),
+              onClick: editUser,
+              icon: Edit,
+            },
+            {
+              label: t("Delete"),
+              icon: TrashCan,
+              onClick: deleteUser,
+              isDangerous: true,
+            },
+          ]}
+          sortProperty="username"
+          onEntityClick={showDetails}
+          addEntityLabel={t("Create user")}
+          onAddEntity={addUser}
+          onSearch={setSearch}
+          loading={loading}
+          searchPlaceholder={t("Search by username")}
+        />
+      </Stack>
       {!loading && !success && (
         <TranslatedErrorInlineNotification
           title="The list of users could not be loaded."
