b6ff78ea92805e9f18395f4eb6ada0b89f499388
Jesse Simpson <jesse.simpson@camunda.com>
2025-02-10 15:20:29 -0500

    test: fix playwright tests to use new auth mechanism

--- c8run/e2e_tests/tests/operate_login.spec.ts
+++ c8run/e2e_tests/tests/operate_login.spec.ts
@@ -4,5 +4,10 @@ const playwright = require('playwright');
 test('test', async ({ page }) => {
   test.setTimeout(60000);
   await page.goto('http://localhost:8080/operate');
+  await page.getByPlaceholder('Username').click();
+  await page.getByPlaceholder('Username').fill('demo');
+  await page.getByPlaceholder('Username').press('Tab');
+  await page.getByPlaceholder('Password').fill('demo');
+  await page.getByRole('button', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'Processes' }).click();
 });
