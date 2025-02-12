70a9414a3e36e1e1eb905dfa53377df4982f8b50
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-02-12 18:32:49 +0100

    refactor: migrate job metrics to micrometer
    (cherry picked from commit b3da6eab8502d562ab334149b54a935e940a9e02)

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/metrics/JobProcessingMetrics.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Zeebe Community License 1.1. You may not use this file
+ * except in compliance with the Zeebe Community License 1.1.
+ */
+package io.camunda.zeebe.engine.metrics;
+
+import static io.camunda.zeebe.engine.metrics.EngineMetricsDoc.JOB_EVENTS;
+
+import io.camunda.zeebe.engine.metrics.EngineMetricsDoc.EngineKeyNames;
+import io.camunda.zeebe.engine.metrics.EngineMetricsDoc.JobAction;
+import io.camunda.zeebe.protocol.record.value.JobKind;
+import io.camunda.zeebe.util.collection.Table;
+import io.camunda.zeebe.util.micrometer.BoundedMeterCache;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import net.jcip.annotations.NotThreadSafe;
+
+/**
+ * This class is not thread safe, and is meant to be used only in the context of the engine actor.
+ *
+ * <p>If you need to use it from the scheduling actor, or from somewhere else, then you need to
+ * first make this thread safe.
+ */
+@NotThreadSafe
+public final class JobProcessingMetrics {
+
+  private final Table<JobAction, JobKind, BoundedMeterCache<Counter>> jobEvents = Table.simple();
+  private final MeterRegistry registry;
+
+  public JobProcessingMetrics(final MeterRegistry registry) {
+    this.registry = registry;
+  }
+
+  public void countJobEvent(final JobAction action, final JobKind kind, final String type) {
+    countJobEvent(action, kind, type, 1);
+  }
+
+  public void countJobEvent(
+      final JobAction action, final JobKind kind, final String type, final int amount) {
+    jobEvents
+        .computeIfAbsent(action, kind, this::registerJobEventCounter)
+        .get(type)
+        .increment(amount);
+  }
+
+  private BoundedMeterCache<Counter> registerJobEventCounter(
+      final JobAction jobAction, final JobKind kind) {
+    final var provider =
+        Counter.builder(JOB_EVENTS.getName())
+            .description(JOB_EVENTS.getDescription())
+            .tag(EngineKeyNames.JOB_ACTION.asString(), jobAction.toString())
+            .tag(EngineKeyNames.JOB_KIND.asString(), kind.name())
+            .withRegistry(registry);
+
+    return BoundedMeterCache.of(registry, provider, EngineKeyNames.JOB_TYPE);
+  }
+}
