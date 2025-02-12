c77bb2160fc20fad51e55243968879f744b1fcc2
githubEshan <66063996+githubEshan@users.noreply.github.com>
2025-02-12 06:08:16 -0500

    feat: Check usage of methods annotated with @VisibleForTesting

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/ArchitectureTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/ArchitectureTest.java
@@ -7,12 +7,17 @@
  */
 package io.camunda.zeebe.engine;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+import io.camunda.zeebe.util.VisibleForTesting;
 
 @AnalyzeClasses(packages = "io.camunda.zeebe", importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchitectureTest {
@@ -34,4 +39,42 @@ public class ArchitectureTest {
           .should()
           .dependOnClassesThat()
           .resideInAPackage("io.camunda.zeebe.scheduler..");
+
+  private static final DescribedPredicate<? super JavaMethod> CALLED_WITHIN_CONTAINING_CLASS =
+      new DescribedPredicate<>("Annotation") {
+
+        @Override
+        public boolean test(final JavaMethod javaMethod) {
+          if (!javaMethod.isAnnotatedWith(VisibleForTesting.class)) {
+            return true;
+          }
+
+          final JavaClass declaringClass = javaMethod.getOwner();
+
+          return declaringClass.getMethodCallsFromSelf().stream()
+              .anyMatch(call -> call.getTarget().equals(declaringClass));
+        }
+      };
+  private static final DescribedPredicate<? super JavaMethod> CALLED_FROM_TEST_CLASS =
+      new DescribedPredicate<>("Called from a test class") {
+        @Override
+        public boolean test(final JavaMethod method) {
+          if (!method.isAnnotatedWith(VisibleForTesting.class)) {
+            return true;
+          }
+
+          return method.getOwner().getDirectDependenciesFromSelf().stream()
+              .anyMatch(dep -> dep.getTargetClass().getSimpleName().endsWith("Test"));
+        }
+      };
+
+  @ArchTest
+  public static final ArchRule RULE_ENGINE_CLASSES_VISIBLE_FOR_TESTING_METHODS_USAGE =
+      classes()
+          .that()
+          .resideInAPackage("io.camunda.zeebe.engine..")
+          .should()
+          .onlyCallMethodsThat(CALLED_WITHIN_CONTAINING_CLASS)
+          .orShould()
+          .onlyCallMethodsThat(CALLED_FROM_TEST_CLASS);
 }
