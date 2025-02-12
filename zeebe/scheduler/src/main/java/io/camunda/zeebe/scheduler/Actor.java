19d47b31eba0b148032518bfb16849e6bb4fd553
Carlo Sana <carlo.sana@camunda.com>
2025-02-12 10:55:26 +0100

    feat: close the ActorMetrics in ActorTask.onClosed()

--- zeebe/scheduler/src/main/java/io/camunda/zeebe/scheduler/Actor.java
+++ zeebe/scheduler/src/main/java/io/camunda/zeebe/scheduler/Actor.java
@@ -101,9 +101,6 @@ public abstract class Actor implements AutoCloseable, AsyncClosable, Concurrency
 
   @Override
   public ActorFuture<Void> closeAsync() {
-    if (metrics != null) {
-      metrics.close();
-    }
     return actor.close();
   }
 
