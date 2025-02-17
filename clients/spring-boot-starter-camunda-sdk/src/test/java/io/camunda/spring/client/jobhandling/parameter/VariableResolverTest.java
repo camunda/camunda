6f4ca214626256afc3240911bb4250ef461215d8
ana.vinogradova <ana.vinogradova@camunda.com>
2025-02-17 08:55:53 +0000

    fix: `VariableResolver` can return `null`
    (cherry picked from commit 03bd8adcb957dfd2bc91d87c18830e57f335ed4a)

---
+++ clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/jobhandling/parameter/VariableResolverTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.spring.client.jobhandling.parameter;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import io.camunda.client.api.response.ActivatedJob;
+import io.camunda.client.api.worker.JobClient;
+import io.camunda.client.impl.CamundaObjectMapper;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class VariableResolverTest {
+
+  private VariableResolver resolver;
+  @Mock private JobClient jobClient;
+  @Mock private ActivatedJob job;
+
+  @BeforeEach
+  void setUp() {
+    resolver = new VariableResolver("testVar", String.class, new CamundaObjectMapper());
+  }
+
+  @Test
+  void shouldResolveVariableNotPresent() {
+    when(job.getVariablesAsMap()).thenReturn(Map.of("anotherVar", "another value"));
+
+    final Object resolvedValue = resolver.resolve(jobClient, job);
+
+    assertNull(resolvedValue);
+  }
+
+  @Test
+  void shouldResolveVariableIsPresent() {
+    when(job.getVariablesAsMap()).thenReturn(Map.of("testVar", "test value"));
+
+    final Object resolvedValue = resolver.resolve(jobClient, job);
+
+    assertEquals("test value", resolvedValue);
+  }
+}
