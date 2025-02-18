cce083b00882400eabe53cb5518d2486d3d827f7
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-02-18 18:39:08 +0100

    docs: document new interfaces

--- zeebe/broker-client/src/main/java/io/camunda/zeebe/broker/client/api/BrokerClientRequestMetrics.java
+++ zeebe/broker-client/src/main/java/io/camunda/zeebe/broker/client/api/BrokerClientRequestMetrics.java
@@ -10,18 +10,26 @@ package io.camunda.zeebe.broker.client.api;
 import io.camunda.zeebe.broker.client.impl.BrokerClientMetricsImpl;
 import io.micrometer.core.instrument.MeterRegistry;
 
+/**
+ * Metrics related to gateway to broker clients, such as request count, number of failed requests,
+ * etc. See {@link BrokerClientMetricsDoc} for documentation on the actual metrics.
+ */
 public interface BrokerClientRequestMetrics {
   Noop NOOP = new Noop();
 
+  /** Returns an implementation which will register and updates metrics on the given registry */
   static BrokerClientRequestMetrics of(final MeterRegistry meterRegistry) {
     return new BrokerClientMetricsImpl(meterRegistry);
   }
 
+  /** Increments the count of successful requests for the given parameters, using these as tags. */
   void registerSuccessfulRequest(
       final int partitionId, final String requestType, final long latencyMs);
 
+  /** Increments the count of failed requests for the given parameters, using these as tags. */
   void registerFailedRequest(final int partitionId, final String requestType, final Enum<?> error);
 
+  /** An implementation which simply does nothing, mostly useful for testing. */
   final class Noop implements BrokerClientRequestMetrics {
 
     @Override
