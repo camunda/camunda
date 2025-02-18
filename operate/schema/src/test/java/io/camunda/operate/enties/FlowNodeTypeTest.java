9d6318f15412380cffa14625adc07043c4733425
Philipp Ossler <philipp.ossler@gmail.com>
2025-02-18 14:21:25 +0100

    test: Verify flow-node type mapping
    Add a test case to verify that every BPMN element type is mapped.

---
+++ operate/schema/src/test/java/io/camunda/operate/enties/FlowNodeTypeTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.operate.enties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.operate.entities.FlowNodeType;
+import io.camunda.zeebe.protocol.record.value.BpmnElementType;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+public class FlowNodeTypeTest {
+
+  @ParameterizedTest
+  @EnumSource(BpmnElementType.class)
+  void shouldContainBpmnElementType(final BpmnElementType bpmnElementType) {
+    // when
+    final FlowNodeType flowNodeType = FlowNodeType.fromZeebeBpmnElementType(bpmnElementType.name());
+
+    // then
+    assertThat(flowNodeType)
+        .describedAs(
+            """
+            The enum FlowNodeType should contain a value for: %s. \
+            Probably, the BPMN element is new and need to be added.""",
+            bpmnElementType)
+        .isNotNull()
+        .isNotEqualTo(FlowNodeType.UNKNOWN);
+  }
+}
