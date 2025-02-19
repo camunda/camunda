camunda-cloud/zeebe

feat: fix and enhance execution order in rdbms statement queue #28101

db/rdbms/src/main/java/io/camunda/db/rdbms/write/queue/QueueItem.java
@@ -9,13 +9,19 @@ package io.camunda.db.rdbms.write.queue;
 
 import java.util.function.Function;
 
-public record QueueItem(ContextType contextType, Object id, String statementId, Object parameter) {
+public record QueueItem(
+    ContextType contextType,
+    StatementType statementType,
+    Object id,
+    String statementId,
+    Object parameter) {
 
   public QueueItem copy(final Function<QueueItemBuilder, QueueItemBuilder> builderFunction) {
     return builderFunction
         .apply(
             new QueueItemBuilder()
                 .contextType(contextType)
+                .statementType(statementType)
                 .id(id)
                 .statementId(statementId)
                 .parameter(parameter))
@@ -26,12 +32,18 @@ public record QueueItem(ContextType contextType, Object id, String statementId, 
   public static class QueueItemBuilder {
 
     private ContextType contextType;
+    private StatementType statementType;
     private Object id;
     private String statementId;
     private Object parameter;
 
     public QueueItemBuilder contextType(final ContextType contextType) {
       this.contextType = contextType;
+      return this;
+    }
+
+    public QueueItemBuilder statementType(final StatementType statementType) {
+      this.statementType = statementType;
       return this;
     }
 
@@ -51,7 +63,7 @@ public record QueueItem(ContextType contextType, Object id, String statementId, 
     }
 
     public QueueItem build() {
-      return new QueueItem(contextType, id, statementId, parameter);
+      return new QueueItem(contextType, statementType, id, statementId, parameter);
     }
   }
 }

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/DecisionDefinitionWriter.java
@@ -11,6 +11,7 @@ import io.camunda.db.rdbms.write.domain.DecisionDefinitionDbModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.StatementType;
 
 public class DecisionDefinitionWriter {
 
@@ -24,6 +25,7 @@ public class DecisionDefinitionWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.DECISION_DEFINITION,
+            StatementType.INSERT,
             decisionDefinition.decisionDefinitionKey(),
             "io.camunda.db.rdbms.sql.DecisionDefinitionMapper.insert",
             decisionDefinition));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/ExporterPositionService.java
@@ -12,6 +12,7 @@ import io.camunda.db.rdbms.write.domain.ExporterPositionModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.StatementType;
 
 public class ExporterPositionService {
 
@@ -28,6 +29,7 @@ public class ExporterPositionService {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.EXPORTER_POSITION,
+            StatementType.INSERT,
             variable.partitionId(),
             "io.camunda.db.rdbms.sql.ExporterPositionMapper.insert",
             variable));
@@ -41,6 +43,7 @@ public class ExporterPositionService {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.EXPORTER_POSITION,
+            StatementType.UPDATE,
             variable.partitionId(),
             "io.camunda.db.rdbms.sql.ExporterPositionMapper.update",
             variable));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/FlowNodeInstanceWriter.java
@@ -14,6 +14,7 @@ import io.camunda.db.rdbms.write.domain.FlowNodeInstanceDbModel.FlowNodeInstance
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.StatementType;
 import io.camunda.db.rdbms.write.queue.UpsertMerger;
 import io.camunda.search.entities.FlowNodeInstanceEntity.FlowNodeState;
 import java.time.OffsetDateTime;
@@ -31,6 +32,7 @@ public class FlowNodeInstanceWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.FLOW_NODE,
+            StatementType.INSERT,
             flowNode.flowNodeInstanceKey(),
             "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.insert",
             flowNode));
@@ -40,6 +42,7 @@ public class FlowNodeInstanceWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.FLOW_NODE,
+            StatementType.UPDATE,
             flowNode.flowNodeInstanceKey(),
             "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.update",
             flowNode));
@@ -53,6 +56,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              StatementType.UPDATE,
               key,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.updateStateAndEndDate",
               dto));
@@ -76,6 +80,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              StatementType.UPDATE,
               flowNodeInstanceKey,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.incrementSubprocessIncidentCount",
               flowNodeInstanceKey));
@@ -91,6 +96,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              StatementType.UPDATE,
               flowNodeInstanceKey,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.decrementSubprocessIncidentCount",
               flowNodeInstanceKey));
@@ -105,6 +111,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              StatementType.UPDATE,
               flowNodeInstanceKey,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.updateIncident",
               dto));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/MappingWriter.java
@@ -11,6 +11,7 @@ import io.camunda.db.rdbms.write.domain.MappingDbModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.StatementType;
 
 public class MappingWriter {
 
@@ -24,6 +25,7 @@ public class MappingWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.MAPPING,
+            StatementType.INSERT,
             mapping.mappingKey(),
             "io.camunda.db.rdbms.sql.MappingMapper.insert",
             mapping));
@@ -33,6 +35,7 @@ public class MappingWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.MAPPING,
+            StatementType.DELETE,
             mappingKey,
             "io.camunda.db.rdbms.sql.MappingMapper.delete",
             mappingKey));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/ProcessDefinitionWriter.java
@@ -11,6 +11,7 @@ import io.camunda.db.rdbms.write.domain.ProcessDefinitionDbModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.StatementType;
 
 public class ProcessDefinitionWriter {
 
@@ -24,6 +25,7 @@ public class ProcessDefinitionWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.PROCESS_DEFINITION,
+            StatementType.INSERT,
             processDefinition.processDefinitionKey(),
             "io.camunda.db.rdbms.sql.ProcessDefinitionMapper.insert",
             processDefinition));

camunda/camunda

db/rdbms/src/main/java/io/camunda/db/rdbms/write/queue/QueueItem.java
@@ -9,13 +9,19 @@ package io.camunda.db.rdbms.write.queue;
 
 import java.util.function.Function;
 
-public record QueueItem(ContextType contextType, Object id, String statementId, Object parameter) {
+public record QueueItem(
+    ContextType contextType,
+    WriteStatementType statementType,
+    Object id,
+    String statementId,
+    Object parameter) {
 
   public QueueItem copy(final Function<QueueItemBuilder, QueueItemBuilder> builderFunction) {
     return builderFunction
         .apply(
             new QueueItemBuilder()
                 .contextType(contextType)
+                .statementType(statementType)
                 .id(id)
                 .statementId(statementId)
                 .parameter(parameter))
@@ -26,12 +32,18 @@ public record QueueItem(ContextType contextType, Object id, String statementId, 
   public static class QueueItemBuilder {
 
     private ContextType contextType;
+    private WriteStatementType statementType;
     private Object id;
     private String statementId;
     private Object parameter;
 
     public QueueItemBuilder contextType(final ContextType contextType) {
       this.contextType = contextType;
+      return this;
+    }
+
+    public QueueItemBuilder statementType(final WriteStatementType statementType) {
+      this.statementType = statementType;
       return this;
     }
 
@@ -51,7 +63,7 @@ public record QueueItem(ContextType contextType, Object id, String statementId, 
     }
 
     public QueueItem build() {
-      return new QueueItem(contextType, id, statementId, parameter);
+      return new QueueItem(contextType, statementType, id, statementId, parameter);
     }
   }
 }

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/DecisionDefinitionWriter.java
@@ -11,6 +11,7 @@ import io.camunda.db.rdbms.write.domain.DecisionDefinitionDbModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
 
 public class DecisionDefinitionWriter {
 
@@ -24,6 +25,7 @@ public class DecisionDefinitionWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.DECISION_DEFINITION,
+            WriteStatementType.INSERT,
             decisionDefinition.decisionDefinitionKey(),
             "io.camunda.db.rdbms.sql.DecisionDefinitionMapper.insert",
             decisionDefinition));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/ExporterPositionService.java
@@ -12,6 +12,7 @@ import io.camunda.db.rdbms.write.domain.ExporterPositionModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
 
 public class ExporterPositionService {
 
@@ -28,6 +29,7 @@ public class ExporterPositionService {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.EXPORTER_POSITION,
+            WriteStatementType.INSERT,
             variable.partitionId(),
             "io.camunda.db.rdbms.sql.ExporterPositionMapper.insert",
             variable));
@@ -41,6 +43,7 @@ public class ExporterPositionService {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.EXPORTER_POSITION,
+            WriteStatementType.UPDATE,
             variable.partitionId(),
             "io.camunda.db.rdbms.sql.ExporterPositionMapper.update",
             variable));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/FlowNodeInstanceWriter.java
@@ -15,6 +15,7 @@ import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
 import io.camunda.db.rdbms.write.queue.UpsertMerger;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
 import io.camunda.search.entities.FlowNodeInstanceEntity.FlowNodeState;
 import java.time.OffsetDateTime;
 import java.util.function.Function;
@@ -31,6 +32,7 @@ public class FlowNodeInstanceWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.FLOW_NODE,
+            WriteStatementType.INSERT,
             flowNode.flowNodeInstanceKey(),
             "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.insert",
             flowNode));
@@ -40,6 +42,7 @@ public class FlowNodeInstanceWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.FLOW_NODE,
+            WriteStatementType.UPDATE,
             flowNode.flowNodeInstanceKey(),
             "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.update",
             flowNode));
@@ -53,6 +56,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              WriteStatementType.UPDATE,
               key,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.updateStateAndEndDate",
               dto));
@@ -76,6 +80,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              WriteStatementType.UPDATE,
               flowNodeInstanceKey,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.incrementSubprocessIncidentCount",
               flowNodeInstanceKey));
@@ -91,6 +96,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              WriteStatementType.UPDATE,
               flowNodeInstanceKey,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.decrementSubprocessIncidentCount",
               flowNodeInstanceKey));
@@ -105,6 +111,7 @@ public class FlowNodeInstanceWriter {
       executionQueue.executeInQueue(
           new QueueItem(
               ContextType.FLOW_NODE,
+              WriteStatementType.UPDATE,
               flowNodeInstanceKey,
               "io.camunda.db.rdbms.sql.FlowNodeInstanceMapper.updateIncident",
               dto));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/MappingWriter.java
@@ -11,6 +11,7 @@ import io.camunda.db.rdbms.write.domain.MappingDbModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
 
 public class MappingWriter {
 
@@ -24,6 +25,7 @@ public class MappingWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.MAPPING,
+            WriteStatementType.INSERT,
             mapping.mappingKey(),
             "io.camunda.db.rdbms.sql.MappingMapper.insert",
             mapping));
@@ -33,6 +35,7 @@ public class MappingWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.MAPPING,
+            WriteStatementType.DELETE,
             mappingKey,
             "io.camunda.db.rdbms.sql.MappingMapper.delete",
             mappingKey));

db/rdbms/src/main/java/io/camunda/db/rdbms/write/service/ProcessDefinitionWriter.java
@@ -11,6 +11,7 @@ import io.camunda.db.rdbms.write.domain.ProcessDefinitionDbModel;
 import io.camunda.db.rdbms.write.queue.ContextType;
 import io.camunda.db.rdbms.write.queue.ExecutionQueue;
 import io.camunda.db.rdbms.write.queue.QueueItem;
+import io.camunda.db.rdbms.write.queue.WriteStatementType;
 
 public class ProcessDefinitionWriter {
 
@@ -24,6 +25,7 @@ public class ProcessDefinitionWriter {
     executionQueue.executeInQueue(
         new QueueItem(
             ContextType.PROCESS_DEFINITION,
+            WriteStatementType.INSERT,
             processDefinition.processDefinitionKey(),
             "io.camunda.db.rdbms.sql.ProcessDefinitionMapper.insert",
             processDefinition));