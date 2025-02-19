6f1a9a08a66e976391b80dc000cb30f8bdbc5328
berkaycanbc <berkay.can@camunda.com>
2025-02-19 18:45:18 +0300

    feat: set multi-instance body type explicitly during migration
    The expected element type for multi-instance body needs to be set
    explicitly because inner activities has the same element id.
    
    If we do not set the expected element type, the inner activity of the
    multi instance body is returned. As a result, event subscriptions are
    handled for wrong target element.
    
    (cherry picked from commit 82dd66f8ecbcfc657ca6ed51dfb0e9e14531b511)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceMigrationCatchEventBehaviour.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceMigrationCatchEventBehaviour.java
@@ -12,6 +12,7 @@ import static io.camunda.zeebe.engine.processing.processinstance.ProcessInstance
 import io.camunda.zeebe.engine.processing.bpmn.BpmnElementContextImpl;
 import io.camunda.zeebe.engine.processing.common.CatchEventBehavior;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableCatchEventSupplier;
+import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableMultiInstanceBody;
 import io.camunda.zeebe.engine.processing.distribution.CommandDistributionBehavior;
 import io.camunda.zeebe.engine.processing.processinstance.ProcessInstanceMigrationPreconditions.ProcessInstanceMigrationPreconditionFailedException;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
@@ -87,10 +88,14 @@ public class ProcessInstanceMigrationCatchEventBehaviour {
       final String elementId) {
     final var context = new BpmnElementContextImpl();
     context.init(elementInstance.getKey(), elementInstanceRecord, elementInstance.getState());
-    final var targetElement =
-        targetProcessDefinition
-            .getProcess()
-            .getElementById(targetElementId, ExecutableCatchEventSupplier.class);
+
+    // set type explicitly for multi-instance body because inner activities has the same element id
+    final var expectedType =
+        elementInstanceRecord.getBpmnElementType() == BpmnElementType.MULTI_INSTANCE_BODY
+            ? ExecutableMultiInstanceBody.class
+            : ExecutableCatchEventSupplier.class;
+    final ExecutableCatchEventSupplier targetElement =
+        targetProcessDefinition.getProcess().getElementById(targetElementId, expectedType);
 
     // UNSUBSCRIBE FROM CATCH EVENTS
     final List<ProcessMessageSubscription> processMessageSubscriptionsToMigrate =
