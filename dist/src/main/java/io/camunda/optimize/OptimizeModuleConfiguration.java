bb06b8ba783b9648d866314a9f22304d1798ec32
Mattia Buccarella <mattia.buccarella@camunda.com>
2025-02-19 21:10:00 +0100

    build: spotless

--- dist/src/main/java/io/camunda/optimize/OptimizeModuleConfiguration.java
+++ dist/src/main/java/io/camunda/optimize/OptimizeModuleConfiguration.java
@@ -9,7 +9,6 @@ package io.camunda.optimize;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 
 /**
