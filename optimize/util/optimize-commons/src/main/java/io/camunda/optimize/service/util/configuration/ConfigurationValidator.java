688dd3c4a2d312458ff5de9414a947ff0a6aa311
Alex Bremard <alex.bremard@camunda.com>
2025-02-19 17:23:17 +0700

    refactor: remove deleted-config logic from Optimize C8

--- optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/util/configuration/ConfigurationValidator.java
+++ optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/util/configuration/ConfigurationValidator.java
@@ -7,101 +7,22 @@
  */
 package io.camunda.optimize.service.util.configuration;
 
-import static io.camunda.optimize.service.util.configuration.ConfigurationParser.parseConfigFromLocations;
-import static io.camunda.optimize.service.util.configuration.ConfigurationUtil.getLocationsAsInputStream;
-import static java.util.stream.Collectors.toMap;
-
 import com.google.common.collect.Lists;
-import com.jayway.jsonpath.Configuration;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import com.jayway.jsonpath.Option;
-import com.jayway.jsonpath.ReadContext;
 import io.camunda.optimize.service.exceptions.OptimizeConfigurationException;
-import io.camunda.optimize.util.SuppressionConstants;
-import java.io.InputStream;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ConfigurationValidator {
 
-  public static final String DOC_URL =
-      "https://docs.camunda.io/optimize/next/self-managed/optimize-deployment/configuration/system-configuration/";
-  private static final String[] DEFAULT_DELETED_CONFIG_LOCATIONS = {"deleted-config.yaml"};
+  public ConfigurationValidator() {}
 
-  private final Map<String, String> deletedConfigKeys;
-
-  public ConfigurationValidator() {
-    this(null);
-  }
-
-  @SuppressWarnings(SuppressionConstants.UNCHECKED_CAST)
-  public ConfigurationValidator(String[] deletedConfigLocations) {
-    final List<InputStream> deletedConfigStreams =
-        getLocationsAsInputStream(
-            deletedConfigLocations == null
-                ? DEFAULT_DELETED_CONFIG_LOCATIONS
-                : deletedConfigLocations);
-
-    this.deletedConfigKeys =
-        (Map<String, String>)
-            parseConfigFromLocations(deletedConfigStreams)
-                .map(ReadContext::json)
-                .orElse(Collections.emptyMap());
-  }
-
-  public void validate(ConfigurationService configurationService) {
-    validateNoDeletedConfigKeysUsed(configurationService.getConfigJsonContext());
+  public void validate(final ConfigurationService configurationService) {
     configurationService.getEmailAuthenticationConfiguration().validate();
     validateWebhooks(configurationService);
-  }
-
-  public static ConfigurationValidator createValidatorWithoutDeletions() {
-    return new ConfigurationValidator(new String[] {});
-  }
-
-  private void validateNoDeletedConfigKeysUsed(ReadContext configJsonContext) {
-    final Configuration conf =
-        Configuration.defaultConfiguration().addOptions(Option.SUPPRESS_EXCEPTIONS);
-    final DocumentContext failsafeConfigurationJsonContext =
-        JsonPath.using(conf).parse((Object) configJsonContext.json());
-
-    final Map<String, String> usedDeletionKeysWithNewDocumentationPath =
-        deletedConfigKeys.entrySet().stream()
-            .filter(
-                entry ->
-                    Optional.ofNullable(
-                            failsafeConfigurationJsonContext.read("$." + entry.getKey()))
-                        // in case of array structures we always a list as result, thus we need to
-                        // check if it contains actual results
-                        .flatMap(
-                            object ->
-                                object instanceof Collection && ((Collection<?>) object).isEmpty()
-                                    ? Optional.empty()
-                                    : Optional.of(object))
-                        .isPresent())
-            // We now set the URL to be the generic docs URL because we don't have consistent
-            // versioning across docs
-            .peek(keyAndUrl -> keyAndUrl.setValue(DOC_URL))
-            .peek(
-                keyAndUrl ->
-                    log.error(
-                        "Deleted setting used with key {}, please see the updated documentation {}",
-                        keyAndUrl.getKey(),
-                        DOC_URL))
-            .collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-    if (!usedDeletionKeysWithNewDocumentationPath.isEmpty()) {
-      throw new OptimizeConfigurationException(
-          "Configuration contains deleted entries", usedDeletionKeysWithNewDocumentationPath);
-    }
   }
 
   private void validateWebhooks(final ConfigurationService configurationService) {
@@ -111,7 +32,7 @@ public class ConfigurationValidator {
     final List<String> webhooksWithoutPlaceholder = Lists.newArrayList();
     final List<String> webhooksWithoutUrl = Lists.newArrayList();
 
-    for (Map.Entry<String, WebhookConfiguration> webhookConfigEntry : webhookMap.entrySet()) {
+    for (final Map.Entry<String, WebhookConfiguration> webhookConfigEntry : webhookMap.entrySet()) {
       final String defaultPayload = webhookConfigEntry.getValue().getDefaultPayload();
       final String url = webhookConfigEntry.getValue().getUrl();
       final boolean usesPlaceholderString =
