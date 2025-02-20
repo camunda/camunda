71b0ca6545d2104631e1a1a183a71fbe553a3ce4
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-02-21 00:38:37 +0100

    feat: redirect to a custom error page when user doesn't have access

--- identity/client/src/App.tsx
+++ identity/client/src/App.tsx
@@ -4,12 +4,14 @@ import { getBaseUrl } from "./configuration";
 import AppRoot from "./components/global/AppRoot";
 import GlobalRoutes from "src/components/global/GlobalRoutes";
 import { LoginPage } from "src/pages/login/LoginPage.tsx";
+import Error403 from "src/pages/errors/403.tsx";
 
 const App: FC = () => (
   <BrowserRouter basename={getBaseUrl()}>
     <StrictMode>
       <Routes>
         <Route key="login" path="/login" Component={LoginPage} />
+        <Route path="/403" element={<Error403 />} />
         <Route
           key="identity-ui"
           path="*"
