fc0fc4f63eda4a073057e6ec27ae407dfeaca5ee
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-02-21 11:59:28 +0200

    test: update `UserTaskUpdateAuthorizationTest` to use new `update(String)` method
    Refactored `UserTaskUpdateAuthorizationTest` to leverage the newly introduced
    `UserTaskClient#update(String username)` method.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/usertask/UserTaskUpdateAuthorizationTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/usertask/UserTaskUpdateAuthorizationTest.java
@@ -12,7 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.camunda.security.configuration.ConfiguredUser;
 import io.camunda.zeebe.engine.util.EngineRule;
 import io.camunda.zeebe.model.bpmn.Bpmn;
-import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
 import io.camunda.zeebe.protocol.record.Assertions;
 import io.camunda.zeebe.protocol.record.RejectionType;
 import io.camunda.zeebe.protocol.record.intent.UserTaskIntent;
@@ -69,10 +68,7 @@ public class UserTaskUpdateAuthorizationTest {
     final var processInstanceKey = createProcessInstance();
 
     // when
-    engine
-        .userTask()
-        .ofInstance(processInstanceKey)
-        .update(new UserTaskRecord(), DEFAULT_USER.getUsername());
+    engine.userTask().ofInstance(processInstanceKey).update(DEFAULT_USER.getUsername());
 
     // then
     assertThat(
@@ -94,10 +90,7 @@ public class UserTaskUpdateAuthorizationTest {
         PROCESS_ID);
 
     // when
-    engine
-        .userTask()
-        .ofInstance(processInstanceKey)
-        .update(new UserTaskRecord(), user.getUsername());
+    engine.userTask().ofInstance(processInstanceKey).update(user.getUsername());
 
     // then
     assertThat(
@@ -119,7 +112,7 @@ public class UserTaskUpdateAuthorizationTest {
             .userTask()
             .ofInstance(processInstanceKey)
             .expectRejection()
-            .update(new UserTaskRecord(), user.getUsername());
+            .update(user.getUsername());
 
     // then
     Assertions.assertThat(rejection)
