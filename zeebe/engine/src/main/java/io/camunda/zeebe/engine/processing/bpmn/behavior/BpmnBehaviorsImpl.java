0fa1b3859f3bcbf308f54ddb95b804aa223bc8bd
Mustafa Dagher <mustafa.dagher@camunda.com>
2025-02-21 12:01:20 +0100

    feat: populate tree-path properties for `ACTIVATING` triggered events

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnBehaviorsImpl.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnBehaviorsImpl.java
@@ -87,11 +87,15 @@ public final class BpmnBehaviorsImpl implements BpmnBehaviors {
             clock,
             transientProcessMessageSubscriptionState);
 
+    stateBehavior = new BpmnStateBehavior(processingState, variableBehavior);
+
     eventTriggerBehavior =
         new EventTriggerBehavior(
-            processingState.getKeyGenerator(), catchEventBehavior, writers, processingState);
-
-    stateBehavior = new BpmnStateBehavior(processingState, variableBehavior);
+            processingState.getKeyGenerator(),
+            catchEventBehavior,
+            writers,
+            processingState,
+            stateBehavior);
 
     bpmnDecisionBehavior =
         new BpmnDecisionBehavior(
