48c1714f65da4242217d52ea8dec8a3790aecdbe
vitorwtavares <vitor.tavares@camunda.com>
2025-02-24 19:03:43 -0300

    fix: general style fixes on reusable page components

--- identity/client/src/components/layout/PageHeadline.tsx
+++ identity/client/src/components/layout/PageHeadline.tsx
@@ -6,12 +6,11 @@
  * except in compliance with the Camunda License 1.0.
  */
 import styled from "styled-components";
-import { spacing04, spacing05 } from "@carbon/elements";
+import { spacing04 } from "@carbon/elements";
 
 const PageHeadline = styled.h1`
   display: flex;
   gap: ${spacing04};
-  margin-left: ${spacing05};
 `;
 
 export default PageHeadline;
