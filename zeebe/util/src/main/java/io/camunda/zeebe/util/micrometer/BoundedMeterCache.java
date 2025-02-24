28325efc93e5079eca0e548fb4dc5c3796822911
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-02-24 17:31:43 +0100

    refactor: remove builder in favor of static method

--- zeebe/util/src/main/java/io/camunda/zeebe/util/micrometer/BoundedMeterCache.java
+++ zeebe/util/src/main/java/io/camunda/zeebe/util/micrometer/BoundedMeterCache.java
@@ -7,7 +7,6 @@
  */
 package io.camunda.zeebe.util.micrometer;
 
-import com.github.benmanes.caffeine.cache.CacheLoader;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.github.benmanes.caffeine.cache.RemovalListener;
@@ -15,10 +14,7 @@ import io.micrometer.common.docs.KeyName;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.Meter.MeterProvider;
 import io.micrometer.core.instrument.MeterRegistry;
-import java.time.Duration;
 import java.util.AbstractMap;
-import java.util.Set;
-import java.util.function.Consumer;
 
 /**
  * A wrapper around {@link Caffeine} for when you want to cache metrics with high cardinality tags
@@ -34,7 +30,7 @@ import java.util.function.Consumer;
  *
  * @param <T> the expected meter type
  */
-public final class BoundedMeterCache<T extends Meter> extends AbstractMap<String, T> {
+public final class BoundedMeterCache<T extends Meter> {
 
   private final LoadingCache<String, T> meters;
 
@@ -43,20 +39,54 @@ public final class BoundedMeterCache<T extends Meter> extends AbstractMap<String
   }
 
   /**
-   * Returns the meter for given tag value. Note that if it doesn't exist, it's loaded using the
-   * provided cache loader at construction time.
+   * Returns a bounded cache which will load the meter from the given provider based on the tag key
+   * given here, and the value given when accessing the cache via {@link #get(String)}.
+   *
+   * <p>By default, the cache is bounded to a maximum of 500 entries.
+   *
+   * @param registry the registry to load from
+   * @param provider the meter provider which gets or creates a meter when not in the cache
+   * @param tagKey the tag key to pass to the meter provider
+   * @return a new bounded cache
+   * @param <T> the expected meter type
+   */
+  public static <T extends Meter> BoundedMeterCache<T> of(
+      final MeterRegistry registry, final MeterProvider<T> provider, final KeyName tagKey) {
+    return of(registry, provider, tagKey, 500);
+  }
+
+  /**
+   * Returns a bounded cache which will load the meter from the given provider based on the tag key
+   * given here, and the value given when accessing the cache via {@link #get(String)}.
+   *
+   * <p>Note that the entries are evicted using a least-frequently-used
+   *
+   * @param registry the registry to load from
+   * @param provider the meter provider which gets or creates a meter when not in the cache
+   * @param tagKey the tag key to pass to the meter provider
+   * @param maxSize the maximum number of keys in this cache before eviction occurs
+   * @return a new bounded cache
+   * @param <T> the expected meter type
+   */
+  public static <T extends Meter> BoundedMeterCache<T> of(
+      final MeterRegistry registry,
+      final MeterProvider<T> provider,
+      final KeyName tagKey,
+      final int maxSize) {
+    final var builder =
+        Caffeine.newBuilder().maximumSize(maxSize).evictionListener(removeEvictedMeter(registry));
+
+    return new BoundedMeterCache<>(builder.build(v -> provider.withTag(tagKey.asString(), v)));
+  }
+
+  /**
+   * Returns the meter for given tag value.
    *
    * @param tagValue the tag value
    * @return the matching meter
    */
   public T get(final String tagValue) {
     return meters.get(tagValue);
-  }
-
-  @SuppressWarnings("NullableProblems")
-  @Override
-  public Set<Entry<String, T>> entrySet() {
-    return meters.asMap().entrySet();
   }
 
   private static <K, T extends Meter> RemovalListener<K, T> removeEvictedMeter(
@@ -68,79 +98,6 @@ public final class BoundedMeterCache<T extends Meter> extends AbstractMap<String
       final MeterRegistry registry, final T value) {
     if (value != null) {
       registry.remove(value);
-    }
-  }
-
-  /**
-   * Builds a bounded meter cache. By default, it sets the maximum size to 100, and expires/removes
-   * meters that haven't been accessed in the last.
-   *
-   * <p>The entries are automatically loaded on access based on the given {@link MeterProvider}
-   *
-   * <p>NOTE: this can be overridden and customized via {@link #config(Consumer)}
-   *
-   * @param <T> the expected meter type
-   */
-  public static final class Builder<T extends Meter> {
-    private final CacheLoader<String, T> loader;
-    private final Caffeine<String, T> builder;
-
-    private int maxSize = 500;
-    private Duration expiry = Duration.ofMinutes(5);
-
-    public Builder(
-        final MeterRegistry registry, final MeterProvider<T> provider, final KeyName tagKey) {
-      this(registry, provider, tagKey.asString());
-    }
-
-    public Builder(
-        final MeterRegistry registry, final MeterProvider<T> provider, final String tagKey) {
-      loader = tagValue -> provider.withTag(tagKey, tagValue);
-      builder = Caffeine.newBuilder().evictionListener(removeEvictedMeter(registry));
-    }
-
-    /**
-     * Sets the maximum number of entries for this cache, after which entries are evicted based on
-     * least-frequent usage.
-     *
-     * <p>Defaults to 500.
-     *
-     * @param maxSize the maximum number of entries in the cache
-     * @return this builder for chaining
-     */
-    public Builder<T> maxSize(final int maxSize) {
-      this.maxSize = maxSize;
-      return this;
-    }
-
-    /**
-     * Sets the TTL on a given cache entry, from the time it was last accessed.
-     *
-     * <p>Defaults to 5 minutes.
-     *
-     * @param expiry the duration after which an entry that hasn't been accessed should be removed
-     * @return this builder for chaining
-     */
-    public Builder<T> ttl(final Duration expiry) {
-      this.expiry = expiry;
-      return this;
-    }
-
-    /**
-     * Customizes the underlying {@link Caffeine} cache, e.g. setting a maximum size, settings a
-     * TTL, etc.
-     *
-     * @param modifier the callback modifying the cache
-     * @return this builder for chaining
-     */
-    public Builder<T> config(final Consumer<Caffeine<String, T>> modifier) {
-      modifier.accept(builder);
-      return this;
-    }
-
-    public BoundedMeterCache<T> build() {
-      return new BoundedMeterCache<>(
-          builder.maximumSize(maxSize).expireAfterAccess(expiry).build(loader));
     }
   }
 }
