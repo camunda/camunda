f12b9255aa79cb8cd04750cdab5194259c08a23a
vitorwtavares <vitor.tavares@camunda.com>
2025-02-24 19:07:41 -0300

    fix: style, structure of details page for tenants, add users structure

--- identity/client/src/utility/localization/en/index.ts
+++ identity/client/src/utility/localization/en/index.ts
@@ -1,9 +1,11 @@
 import components from "./components.json";
 import permissions from "./permissions.json";
 import authorizations from "./authorizations.json";
+import tenants from "./tenants.json";
 
 export default {
   components,
   permissions,
   authorizations,
+  tenants,
 };
