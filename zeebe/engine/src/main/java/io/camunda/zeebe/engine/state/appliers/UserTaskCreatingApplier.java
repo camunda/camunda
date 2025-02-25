ccc282819f009c5889520b86ac7c72705bac8fad
Nico Korthout <nico.korthout@camunda.com>
2025-02-25 17:12:43 +0100

    fix: store and cleanup creating transition

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/UserTaskCreatingApplier.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/UserTaskCreatingApplier.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.engine.state.appliers;
 
 import io.camunda.zeebe.engine.state.TypedEventApplier;
+import io.camunda.zeebe.engine.state.immutable.UserTaskState.LifecycleState;
 import io.camunda.zeebe.engine.state.instance.ElementInstance;
 import io.camunda.zeebe.engine.state.mutable.MutableElementInstanceState;
 import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
@@ -28,7 +29,9 @@ public final class UserTaskCreatingApplier
 
   @Override
   public void applyState(final long key, final UserTaskRecord value) {
+    // todo: should we actually store the user task fully already, or wait until we finalize?
     userTaskState.create(value);
+    userTaskState.storeIntermediateState(value, LifecycleState.CREATING);
 
     final long elementInstanceKey = value.getElementInstanceKey();
     if (elementInstanceKey > 0) {
