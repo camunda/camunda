62e2da4fcfcef76eb9a5d8c45310061c04ee80fb
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-02-25 11:15:42 +0100

    fix: rename 403 and apply new style

--- identity/client/src/App.tsx
+++ identity/client/src/App.tsx
@@ -4,14 +4,14 @@ import { getBaseUrl } from "./configuration";
 import AppRoot from "./components/global/AppRoot";
 import GlobalRoutes from "src/components/global/GlobalRoutes";
 import { LoginPage } from "src/pages/login/LoginPage.tsx";
-import Error403 from "src/pages/errors/403.tsx";
+import Forbidden from "src/pages/forbidden/index.tsx";
 
 const App: FC = () => (
   <BrowserRouter basename={getBaseUrl()}>
     <StrictMode>
       <Routes>
         <Route key="login" path="/login" Component={LoginPage} />
-        <Route path="/403" element={<Error403 />} />
+        <Route path="/forbidden" element={<Forbidden />} />
         <Route
           key="identity-ui"
           path="*"
