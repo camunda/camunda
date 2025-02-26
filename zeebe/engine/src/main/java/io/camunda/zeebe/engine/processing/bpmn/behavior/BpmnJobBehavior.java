1de82638c4ee6b74bdcc233bd04496e34b561dd1
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-02-26 20:40:17 +0100

    refactor: remove debugging leftovers

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnJobBehavior.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnJobBehavior.java
@@ -515,7 +515,6 @@ public final class BpmnJobBehavior {
       // it there as well.
       stateWriter.appendFollowUpEvent(jobKey, JobIntent.CANCELED, job);
       jobMetrics.countJobEvent(JobAction.CANCELED, job.getJobKind(), job.getType());
-      System.out.println("Canceled job " + jobKey);
     }
   }
 
