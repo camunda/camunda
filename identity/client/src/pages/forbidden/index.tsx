0f625f07d7711eec36fd968d2584d44ed72119c9
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-02-26 08:46:05 +0100

    fix: make prettier

--- identity/client/src/pages/forbidden/index.tsx
+++ identity/client/src/pages/forbidden/index.tsx
@@ -9,26 +9,24 @@
 // src/pages/Error403.tsx
 import { FC } from "react";
 import useTranslate from "src/utility/localization";
-import {C3EmptyState} from "@camunda/camunda-composite-components";
-
-
+import { C3EmptyState } from "@camunda/camunda-composite-components";
 
 const Forbidden: FC = () => {
-  const { t} = useTranslate();
+  const { t } = useTranslate();
 
   return (
-      <C3EmptyState
-          icon={{
-            path: "modules/components/Icon/permission-denied.svg",
-            altText: "Permission Denied Icon", // Add a meaningful description for accessibility
-          }}
-          heading={t("403 - You do not have access to this component")}
-          description="It looks like you don't have the necessary permissions to access this component. Please contact your cluster admin to get access."
-          link={{
-            label: 'Learn more about permissions',
-            href: '',
-          }}
-      />
+    <C3EmptyState
+      icon={{
+        path: "modules/components/Icon/permission-denied.svg",
+        altText: "Permission Denied Icon", // Add a meaningful description for accessibility
+      }}
+      heading={t("403 - You do not have access to this component")}
+      description="It looks like you don't have the necessary permissions to access this component. Please contact your cluster admin to get access."
+      link={{
+        label: "Learn more about permissions",
+        href: "",
+      }}
+    />
   );
 };
 
