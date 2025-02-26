d1fd2ca9e24e2716cf7935a8fd9e76e1d1004268
Gareth Steyn <gareth.steyn@camunda.com>
2025-02-26 17:25:29 +0100

    feat: implement processor to process AdHocSubProcessActivityActivate commands

---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/adhocsubprocess/ActivateAdHocSubProcessActivityTest.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.adhocsubprocess;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
+
+import io.camunda.zeebe.engine.util.EngineRule;
+import io.camunda.zeebe.engine.util.RecordToWrite;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import io.camunda.zeebe.model.bpmn.builder.AdHocSubProcessBuilder;
+import io.camunda.zeebe.protocol.impl.record.value.adhocsubprocess.AdHocSubProcessActivityActivationRecord;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.intent.AdHocSubProcessActivityActivationIntent;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
+import io.camunda.zeebe.protocol.record.value.BpmnElementType;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import io.camunda.zeebe.test.util.record.RecordingExporterTestWatcher;
+import java.util.List;
+import java.util.function.Consumer;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ActivateAdHocSubProcessActivityTest {
+  @ClassRule public static final EngineRule ENGINE = EngineRule.singlePartition();
+
+  private static final String PROCESS_ID = "process";
+  private static final String AD_HOC_SUB_PROCESS_ELEMENT_ID = "ad-hoc";
+
+  @Rule public final RecordingExporterTestWatcher watcher = new RecordingExporterTestWatcher();
+
+  private long processInstanceKey;
+  private long adHocSubProcessInstanceKey;
+
+  @Before
+  public void setUp() {
+    final BpmnModelInstance processInstance =
+        process(
+            adHocSubProcess -> {
+              adHocSubProcess.task("A");
+              adHocSubProcess.task("B");
+              adHocSubProcess.task("C");
+            });
+    ENGINE.deployment().withXmlResource(processInstance).deploy();
+    processInstanceKey = ENGINE.processInstance().ofBpmnProcessId(PROCESS_ID).create();
+
+    adHocSubProcessInstanceKey =
+        RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
+            .withProcessInstanceKey(processInstanceKey)
+            .withElementType(BpmnElementType.AD_HOC_SUB_PROCESS)
+            .map(Record::getKey)
+            .limit(1)
+            .toList()
+            .getFirst();
+  }
+
+  private BpmnModelInstance process(final Consumer<AdHocSubProcessBuilder> modifier) {
+    return Bpmn.createExecutableProcess(PROCESS_ID)
+        .startEvent()
+        .adHocSubProcess(AD_HOC_SUB_PROCESS_ELEMENT_ID, modifier)
+        .endEvent()
+        .done();
+  }
+
+  @Test
+  public void
+      givenRunningAdhocSubProcessInstanceWhenActivatingExistingFlowNodeThenTheFlowNodeIsActivated() {
+    final var adHocSubProcessActivityActivationRecord =
+        new AdHocSubProcessActivityActivationRecord()
+            .setAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey));
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("A");
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    assertThat(
+            RecordingExporter.processInstanceRecords().withProcessInstanceKey(processInstanceKey))
+        // todo add short-circuit `limit` call?
+        .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .contains(
+            tuple("A", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+            tuple("A", ProcessInstanceIntent.ELEMENT_ACTIVATED))
+        .doesNotContainAnyElementsOf(
+            List.of(
+                tuple("B", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+                tuple("C", ProcessInstanceIntent.ACTIVATE_ELEMENT)));
+  }
+
+  @Test
+  public void
+      givenRunningAdhocSubProcessInstanceWhenSuccessfullyActivatingAFlowNodeThenSubsequentActivatedEventIsSent() {
+    final var adHocSubProcessActivityActivationRecord =
+        new AdHocSubProcessActivityActivationRecord()
+            .setAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey));
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("A");
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    assertThat(
+            RecordingExporter.adHocSubProcessActivityActivationRecords()
+                .withAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey)))
+        // todo add short-circuit `limit` call?
+        .extracting(r -> r.getValue().getFlowNodes().getFirst().getFlowNodeId(), Record::getIntent)
+        .contains(tuple("A", AdHocSubProcessActivityActivationIntent.ACTIVATED));
+  }
+
+  @Test
+  public void
+      givenRunningAdhocSubProcessInstanceWhenActivatingMoreThanOneFlowNodeThenAllGivenFlowNodesAreActivated() {
+    final var adHocSubProcessActivityActivationRecord =
+        new AdHocSubProcessActivityActivationRecord()
+            .setAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey));
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("A");
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("B");
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    assertThat(
+            RecordingExporter.processInstanceRecords().withProcessInstanceKey(processInstanceKey))
+        // todo add short-circuit `limit` call?
+        .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .contains(
+            tuple("A", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+            tuple("A", ProcessInstanceIntent.ELEMENT_ACTIVATED),
+            tuple("B", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+            tuple("B", ProcessInstanceIntent.ELEMENT_ACTIVATED))
+        .doesNotContainAnyElementsOf(List.of(tuple("C", ProcessInstanceIntent.ACTIVATE_ELEMENT)));
+  }
+
+  @Test
+  public void
+      givenRunningAdhocSubProcessInstanceWhenActivatingFlowNodeThatDoesNotExistThenTheActivationIsRejected() {
+    final var adHocSubProcessActivityActivationRecord =
+        new AdHocSubProcessActivityActivationRecord()
+            .setAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey));
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("does-not-exist");
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    assertThat(
+            RecordingExporter.adHocSubProcessActivityActivationRecords()
+                .withAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey))
+                .onlyCommandRejections()
+                .limit(1))
+        .extracting(r -> r.getValue().getFlowNodes().getFirst().getFlowNodeId(), Record::getIntent)
+        .describedAs("Expected flow node that doesn't exist to be rejected.")
+        .contains(tuple("does-not-exist", AdHocSubProcessActivityActivationIntent.ACTIVATE));
+  }
+
+  @Test
+  public void
+      givenAdhocSubProcessInATerminalStateWhenActivatingFlowNodesThenTheActivationIsRejected() {
+    final var adHocSubProcessActivityActivationRecord =
+        new AdHocSubProcessActivityActivationRecord()
+            .setAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey));
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("A");
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    assertThat(
+            RecordingExporter.adHocSubProcessActivityActivationRecords()
+                .withAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey))
+                .onlyCommandRejections()
+                .limit(1))
+        .describedAs(
+            "Expected activation to be rejected because the adhoc subprocess has completed.")
+        .isNotEmpty();
+  }
+
+  @Test
+  public void
+      givenRunningAdhocSubProcessWhenAttemptingToActivateDuplicateFlowNodesThenTheActivationIsRejected() {
+    final var adHocSubProcessActivityActivationRecord =
+        new AdHocSubProcessActivityActivationRecord()
+            .setAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey));
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("A");
+    adHocSubProcessActivityActivationRecord.flowNodes().add().setFlowNodeId("A");
+
+    ENGINE.writeRecords(
+        RecordToWrite.command()
+            .adHocSubProcessActivityActivation(adHocSubProcessActivityActivationRecord)
+            .key(adHocSubProcessInstanceKey));
+
+    assertThat(
+            RecordingExporter.adHocSubProcessActivityActivationRecords()
+                .withAdHocSubProcessInstanceKey(String.valueOf(adHocSubProcessInstanceKey))
+                .onlyCommandRejections()
+                .limit(1))
+        .describedAs(
+            "Expected activation to be rejected because duplicate flow nodes are provided.")
+        .isNotEmpty();
+  }
+}
