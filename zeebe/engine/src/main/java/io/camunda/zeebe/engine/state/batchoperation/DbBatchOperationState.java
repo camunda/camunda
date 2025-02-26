a48b7e186aa049bf70beeae924a4730e7d78e3d7
Christian Thiel <christian.thiel@holisticon.de>
2025-02-26 16:14:21 +0100

    feat: first working batch

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/batchoperation/DbBatchOperationState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/batchoperation/DbBatchOperationState.java
@@ -11,17 +11,14 @@ import io.camunda.zeebe.db.ColumnFamily;
 import io.camunda.zeebe.db.TransactionContext;
 import io.camunda.zeebe.db.ZeebeDb;
 import io.camunda.zeebe.db.impl.DbLong;
-import io.camunda.zeebe.engine.state.authorization.PersistedAuthorization;
 import io.camunda.zeebe.engine.state.mutable.MutableBatchOperationState;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationCreationRecord;
-import io.camunda.zeebe.protocol.record.value.AuthorizationOwnerType;
-import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
-import io.camunda.zeebe.protocol.record.value.PermissionType;
+import java.util.ArrayList;
 import java.util.Optional;
-import java.util.Set;
 
 public class DbBatchOperationState implements MutableBatchOperationState {
+
   private static final String KEY = "BATCH_OPERATION";
 
   private final DbLong batchKey = new DbLong();
@@ -37,14 +34,14 @@ public class DbBatchOperationState implements MutableBatchOperationState {
   public void create(final long batchKey, final BatchOperationCreationRecord record) {
     this.batchKey.wrapLong(batchKey);
     final var itemKeys = new ItemKeys();
-    itemKeys.setKeys(record.getKeys());
+    itemKeys.setKeys(new ArrayList<>(record.getKeys())); // todo record also as list?
     columnFamily.insert(this.batchKey, itemKeys);
   }
 
   @Override
-  public Optional<PersistedAuthorization> get(final long key) {
+  public Optional<ItemKeys> get(final long key) {
     batchKey.wrapLong(key);
-    return null;
+    return Optional.ofNullable(columnFamily.get(batchKey));
   }
 
 }
