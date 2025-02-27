448ad1919bf00ca8b217059784a6087ab5b027bc
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-02-27 17:16:20 +0100

    refactor: use new prometheus registry, not simpleclient based

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/observability/MetricsConfigurationIT.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/observability/MetricsConfigurationIT.java
@@ -2,8 +2,8 @@
  * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
  * one or more contributor license agreements. See the NOTICE file distributed
  * with this work for additional information regarding copyright ownership.
- * Licensed under the Zeebe Community License 1.1. You may not use this file
- * except in compliance with the Zeebe Community License 1.1.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
  */
 package io.camunda.zeebe.it.observability;
 
@@ -15,7 +15,7 @@ import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry;
 import io.micrometer.registry.otlp.OtlpMeterRegistry;
 import java.time.Duration;
 import java.util.ArrayList;
