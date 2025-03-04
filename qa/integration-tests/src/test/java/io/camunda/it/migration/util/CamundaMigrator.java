c5eec7619874852d3781e4731149f839d213660e
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2025-03-04 21:24:02 +0100

    test: migration extension

---
+++ qa/integration-tests/src/test/java/io/camunda/it/migration/util/CamundaMigrator.java
@@ -0,0 +1,310 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.migration.util;
+
+import io.camunda.application.Profile;
+import io.camunda.client.CamundaClient;
+import io.camunda.qa.util.cluster.TestRestOperateClient;
+import io.camunda.qa.util.cluster.TestRestTasklistClient;
+import io.camunda.qa.util.cluster.TestSimpleCamundaApplication;
+import io.camunda.qa.util.multidb.MultiDbConfigurator;
+import io.camunda.search.clients.DocumentBasedSearchClient;
+import io.camunda.search.connect.configuration.ConnectConfiguration;
+import io.camunda.search.connect.configuration.DatabaseType;
+import io.camunda.webapps.schema.descriptors.IndexDescriptor;
+import io.camunda.webapps.schema.descriptors.IndexDescriptors;
+import io.camunda.zeebe.exporter.ElasticsearchExporter;
+import io.camunda.zeebe.exporter.opensearch.OpensearchExporter;
+import io.camunda.zeebe.it.util.SearchClientsUtil;
+import io.camunda.zeebe.qa.util.actuator.PartitionsActuator;
+import io.camunda.zeebe.qa.util.cluster.TestZeebePort;
+import io.camunda.zeebe.util.FileUtil;
+import io.zeebe.containers.ZeebeContainer;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import org.testcontainers.containers.Network;
+import org.testcontainers.utility.DockerImageName;
+
+public class CamundaMigrator extends ApiCallable {
+  private static final String CAMUNDA_OLD_VERSION = "8.7.0-SNAPSHOT";
+  private static final String OS_USER = "admin";
+  private static final String OS_PASSWORD = "yourStrongPassword123!";
+  private static final String URL = "http://%s:%d";
+  private static final String RPC_URL = "http://%s:%d";
+  private String databaseUrl;
+  private final Network network;
+  private final String indexPrefix;
+  private final CamundaVolume volume;
+  private final Path zeebeDataPath;
+  private CamundaClient camundaClient;
+  private ZeebeContainer camundaContainer;
+  private TestSimpleCamundaApplication broker;
+  private TestRestTasklistClient tasklistClient;
+  private TestRestOperateClient operateClient;
+  private DocumentBasedSearchClient searchClients;
+
+  public CamundaMigrator(final Network network, final String indexPrefix) {
+    this.network = network;
+    this.indexPrefix = indexPrefix;
+    volume = CamundaVolume.newCamundaVolume();
+    zeebeDataPath = Path.of(System.getProperty("user.dir") + "/zeebe-data" + volume.getName());
+  }
+
+  public CamundaMigrator initialize(final DatabaseType databaseType, final String databaseUrl) {
+    return initialize(databaseType, databaseUrl, new HashMap<>());
+  }
+
+  public CamundaMigrator initialize(
+      final DatabaseType databaseType,
+      final String databaseUrl,
+      final Map<String, String> envOverrides) {
+    final String image = "camunda/camunda:" + CAMUNDA_OLD_VERSION;
+    this.databaseUrl = databaseUrl;
+    camundaContainer =
+        new ZeebeContainer(DockerImageName.parse(image))
+            .withExposedPorts(26500, 9600, 8080)
+            .withNetwork(network)
+            .withNetworkAliases("camunda");
+
+    final Map<String, String> env =
+        databaseType.equals(DatabaseType.ELASTICSEARCH)
+            ? elasticsearchConfiguration87()
+            : opensearchConfiguration87();
+    if (envOverrides != null) {
+      env.putAll(envOverrides);
+    }
+    env.forEach(camundaContainer::withEnv);
+
+    camundaContainer.withCreateContainerCmdModifier(
+        cmd ->
+            cmd.withUser("1001:0")
+                .getHostConfig()
+                /* On camunda/camunda image the default Zeebe dataDir is /usr/local/camunda/data */
+                .withBinds(volume.asBind("/usr/local/camunda/data")));
+
+    camundaContainer.start();
+    url =
+        URL.formatted(
+            camundaContainer.getHost(), camundaContainer.getMappedPort(TestZeebePort.REST.port()));
+    camundaClient =
+        CamundaClient.newClientBuilder()
+            .grpcAddress(
+                URI.create(
+                    RPC_URL.formatted(
+                        camundaContainer.getHost(),
+                        camundaContainer.getMappedPort(TestZeebePort.GATEWAY.port()))))
+            .restAddress(URI.create(url))
+            .usePlaintext()
+            .build();
+
+    try {
+      login();
+    } catch (final IOException | InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    return this;
+  }
+
+  public CamundaMigrator update(final DatabaseType databaseType) {
+    return update(databaseType, new HashMap<>());
+  }
+
+  public CamundaMigrator update(
+      final DatabaseType databaseType, final Map<String, String> envOverrides) {
+    /* Trigger snapshot of Zeebe's data to force flush ExporterMetadata */
+    PartitionsActuator.of(camundaContainer).takeSnapshot();
+
+    camundaContainer.close();
+    extractVolume();
+    broker =
+        new TestSimpleCamundaApplication()
+            .withBasicAuth()
+            .withAuthenticatedAccess()
+            .withAdditionalProfile(Profile.PROCESS_MIGRATION)
+            .withBrokerConfig(
+                cfg -> {
+                  cfg.getExperimental().setVersionCheckRestrictionEnabled(false);
+                  cfg.getGateway().setEnable(true);
+                })
+            .withWorkingDirectory(zeebeDataPath.resolve("usr/local/zeebe"));
+
+    final var multiDbConfigurator = new MultiDbConfigurator(broker);
+    if (databaseType.equals(DatabaseType.ELASTICSEARCH)) {
+      multiDbConfigurator.configureElasticsearchSupport(databaseUrl, indexPrefix);
+    } else {
+      multiDbConfigurator.configureOpenSearchSupport(
+          databaseUrl, indexPrefix, OS_USER, OS_PASSWORD);
+    }
+    final Map<String, String> env = new HashMap<>();
+    env.put("camunda.migration.process.importerFinishedTimeout", "PT2S");
+    // Reduce importer intervals to speed up tests
+    env.put("camunda.operate.importer.importPositionUpdateInterval", "200");
+    env.put("camunda.operate.importer.readerBackoff", "200");
+    env.put("camunda.tasklist.importer.importPositionUpdateInterval", "200");
+    env.put("camunda.tasklist.importer.readerBackoff", "200");
+
+    env.putAll(envOverrides);
+    env.forEach(broker::withProperty);
+
+    broker.start();
+    broker.awaitCompleteTopology();
+    camundaClient = broker.newClientBuilder().build();
+    url = URL.formatted(broker.host(), broker.mappedPort(TestZeebePort.REST));
+    final var uri = URI.create(url + "/");
+    tasklistClient = new TestRestTasklistClient(uri, databaseUrl);
+    operateClient = new TestRestOperateClient(uri, "demo", "demo");
+    searchClients =
+        databaseType.isElasticSearch()
+            ? SearchClientsUtil.createLowLevelElasticsearchSearchClient(
+                getConnectConfiguration(databaseType))
+            : SearchClientsUtil.createLowLevelOpensearchSearchClient(
+                getConnectConfiguration(databaseType));
+    return this;
+  }
+
+  public void close() throws Exception {
+    if (camundaContainer != null) {
+      camundaContainer.stop();
+      camundaContainer = null;
+    }
+    if (broker != null) {
+      broker.stop();
+      broker = null;
+    }
+    if (tasklistClient != null) {
+      tasklistClient.close();
+      tasklistClient = null;
+    }
+    if (operateClient != null) {
+      operateClient.close();
+      operateClient = null;
+    }
+  }
+
+  public void cleanup() throws IOException {
+    if (Files.exists(zeebeDataPath)) {
+      FileUtil.deleteFolder(zeebeDataPath);
+    }
+  }
+
+  public String getWebappsUrl() {
+    return url + "/v1";
+  }
+
+  public CamundaClient getCamundaClient() {
+    return camundaClient;
+  }
+
+  /// Returns the tasklist client. This is only usable on 8.8 version
+  public TestRestTasklistClient getTasklistClient() {
+    return tasklistClient;
+  }
+
+  /// Returns the operate client. This is only usable on 8.8 version
+  public TestRestOperateClient getOperateClient() {
+    return operateClient;
+  }
+
+  /// Returns the search client. This is only usable on 8.8 version
+  public DocumentBasedSearchClient getSearchClient() {
+    return searchClients;
+  }
+
+  public IndexDescriptor indexFor(final Class<? extends IndexDescriptor> clazz) {
+    return new IndexDescriptors(indexPrefix, true).get(clazz);
+  }
+
+  private Map<String, String> elasticsearchConfiguration87() {
+    return new HashMap<>() {
+      {
+        put("SPRING_PROFILES_ACTIVE", "tasklist,broker,auth,operate");
+        put("ZEEBE_BROKER_CLUSTER_PARTITIONSCOUNT", "1");
+        put(
+            "ZEEBE_BROKER_EXPORTERS_ELASTICSEARCH_CLASSNAME",
+            ElasticsearchExporter.class.getName());
+        put("ZEEBE_BROKER_EXPORTERS_ELASTICSEARCH_ARGS_URL", "http://elasticsearch:9200");
+        put("ZEEBE_BROKER_EXPORTERS_ELASTICSEARCH_ARGS_BULKSIZE", "1");
+        put("ZEEBE_BROKER_EXPORTERS_ELASTICSEARCH_ARGS_INDEX_PREFIX", indexPrefix);
+        put("CAMUNDA_TASKLIST_ZEEBE_COMPATIBILITYMODE", "true");
+        put("CAMUNDA_TASKLIST_ELASTICSEARCH_URL", "http://elasticsearch:9200");
+        put("CAMUNDA_TASKLIST_ZEEBEELASTICSEARCH_URL", "http://elasticsearch:9200");
+        put("CAMUNDA_TASKLIST_ZEEBE_GATEWAYADDRESS", "camunda:26500");
+        put("CAMUNDA_TASKLIST_ZEEBE_RESTADDRESS", "http://camunda:8080");
+        put("CAMUNDA_TASKLIST_ELASTICSEARCH_INDEXPREFIX", indexPrefix + "-tasklist");
+        put("CAMUNDA_TASKLIST_ZEEBEELASTICSEARCH_PREFIX", indexPrefix);
+        put("CAMUNDA_OPERATE_ELASTICSEARCH_URL", "http://elasticsearch:9200");
+        put("CAMUNDA_OPERATE_ELASTICSEARCH_INDEXPREFIX", indexPrefix + "-operate");
+        put("CAMUNDA_OPERATE_ZEEBEELASTICSEARCH_PREFIX", indexPrefix);
+        put("CAMUNDA_OPERATE_ZEEBEELASTICSEARCH_URL", "http://elasticsearch:9200");
+        put("CAMUNDA_OPERATE_ZEEBE_GATEWAYADDRESS", "camunda:26500");
+        put("ZEEBE_BROKER_GATEWAY_ENABLE", "true");
+        put("CAMUNDA_DATABASE_URL", "http://elasticsearch:9200");
+      }
+    };
+  }
+
+  private Map<String, String> opensearchConfiguration87() {
+    return new HashMap<>() {
+      {
+        put("SPRING_PROFILES_ACTIVE", "tasklist,broker,auth,operate");
+        put("ZEEBE_BROKER_CLUSTER_PARTITIONSCOUNT", "1");
+        put("ZEEBE_BROKER_EXPORTERS_OPENSEARCH_CLASSNAME", OpensearchExporter.class.getName());
+        put("ZEEBE_BROKER_EXPORTERS_OPENSEARCH_ARGS_URL", "http://opensearch:9200");
+        put("ZEEBE_BROKER_EXPORTERS_OPENSEARCH_ARGS_BULKSIZE", "1");
+        put("ZEEBE_BROKER_EXPORTERS_OPENSEARCH_ARGS_INDEX_PREFIX", indexPrefix);
+        put("CAMUNDA_TASKLIST_ZEEBE_COMPATIBILITYMODE", "true");
+        put("CAMUNDA_TASKLIST_DATABASE", "opensearch");
+        put("CAMUNDA_TASKLIST_OPENSEARCH_URL", "http://opensearch:9200");
+        put("CAMUNDA_TASKLIST_OPENSEARCH_INDEXPREFIX", indexPrefix + "-tasklist");
+        put("CAMUNDA_TASKLIST_ZEEBEOPENSEARCH_PREFIX", indexPrefix);
+        put("CAMUNDA_TASKLIST_ZEEBEOPENSEARCH_URL", "http://opensearch:9200");
+        put("CAMUNDA_TASKLIST_ZEEBE_GATEWAYADDRESS", "camunda:26500");
+        put("CAMUNDA_TASKLIST_ZEEBE_RESTADDRESS", "http://camunda:8080");
+        put("CAMUNDA_OPERATE_OPENSEARCH_URL", "http://opensearch:9200");
+        put("CAMUNDA_OPERATE_DATABASE", "opensearch");
+        put("CAMUNDA_OPERATE_ZEEBEOPENSEARCH_URL", "http://opensearch:9200");
+        put("CAMUNDA_OPERATE_ZEEBE_GATEWAYADDRESS", "camunda:26500");
+        put("CAMUNDA_OPERATE_OPENSEARCH_INDEXPREFIX", indexPrefix + "-operate");
+        put("CAMUNDA_OPERATE_ZEEBEOPENSEARCH_PREFIX", indexPrefix);
+        put("ZEEBE_BROKER_GATEWAY_ENABLE", "true");
+        put("CAMUNDA_DATABASE_URL", "http://opensearch:9200");
+        // Reduce importer intervals to speed up tests
+        put("CAMUNDA_OPERATE_IMPORTER_IMPORTPOSITIONUPDATEINTERVAL", "200");
+        put("CAMUNDA_OPERATE_IMPORTER_READERBACKOFF", "200");
+        put("CAMUNDA_TASKLIST_IMPORTER_IMPORTPOSITIONUPDATEINTERVAL", "200");
+        put("CAMUNDA_TASKLIST_IMPORTER_READERBACKOFF", "200");
+      }
+    };
+  }
+
+  private void extractVolume() {
+    try {
+      volume.extract(zeebeDataPath);
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private ConnectConfiguration getConnectConfiguration(final DatabaseType databaseType) {
+    final ConnectConfiguration connectConfiguration = new ConnectConfiguration();
+    connectConfiguration.setType(databaseType.name());
+    connectConfiguration.setClusterName("elasticsearch");
+    connectConfiguration.setUrl(databaseUrl);
+    connectConfiguration.setIndexPrefix(indexPrefix);
+    if (databaseType.isOpenSearch()) {
+      connectConfiguration.setUsername(OS_USER);
+      connectConfiguration.setPassword(OS_PASSWORD);
+    }
+    return connectConfiguration;
+  }
+}
---
+++ qa/integration-tests/src/test/java/io/camunda/it/migration/util/MigrationITExtension.java
@@ -0,0 +1,289 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.migration.util;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.search.clients.DocumentBasedSearchClient;
+import io.camunda.search.connect.configuration.DatabaseType;
+import io.camunda.webapps.schema.descriptors.IndexDescriptors;
+import io.camunda.zeebe.test.util.testcontainers.TestSearchContainers;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
+import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
+import org.junit.platform.commons.support.ModifierSupport;
+import org.opensearch.testcontainers.OpensearchContainer;
+import org.testcontainers.containers.Network;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+
+public class MigrationITExtension
+    implements AfterAllCallback,
+        BeforeAllCallback,
+        AfterEachCallback,
+        TestTemplateInvocationContextProvider {
+
+  private static final String TASKLIST = "tasklist";
+  private static final String OPERATE = "operate";
+  private static final Map<DatabaseType, CamundaMigrator> MIGRATORS = new HashMap<>();
+  private static final List<AutoCloseable> DB_CLOSABLES = new ArrayList<>();
+  private static final Map<DatabaseType, Network> NETWORKS = new HashMap<>();
+  private static final Set<DatabaseType> DATABASE_TYPES =
+      Set.of(/*DatabaseType.ELASTICSEARCH,*/ DatabaseType.OPENSEARCH);
+  private static final Map<DatabaseType, String> DATABASE_EXTERNAL_URLS = new HashMap<>();
+  private static final Map<DatabaseType, MigrationDatabaseChecks> DATABASE_CHECKS = new HashMap<>();
+  private static final Map<DatabaseType, String> INDEX_PREFIXES = new HashMap<>();
+  private static final Map<DatabaseType, Boolean> HAS_87_DATA =
+      new HashMap<>() {
+        {
+          put(DatabaseType.ELASTICSEARCH, false);
+          put(DatabaseType.OPENSEARCH, false);
+        }
+      };
+
+  @Override
+  public void afterAll(final ExtensionContext context) {
+    final var clazz = context.getTestClass().get();
+    if (!isNestedClass(clazz)) {
+      DB_CLOSABLES.parallelStream()
+          .forEach(
+              c -> {
+                try {
+                  c.close();
+                } catch (final Exception e) {
+                  throw new RuntimeException(e);
+                }
+              });
+    }
+  }
+
+  @Override
+  public void afterEach(final ExtensionContext context) {
+    final String indexPrefix = context.getTestMethod().get().getName().toLowerCase();
+
+    final DatabaseType db = DatabaseType.valueOf(context.getDisplayName());
+    try {
+      MIGRATORS.get(db).close();
+      MIGRATORS.get(db).cleanup();
+    } catch (final Exception e) {
+      throw new RuntimeException(e);
+    }
+    DATABASE_CHECKS.get(db).cleanup(indexPrefix);
+    DATABASE_CHECKS.get(db).close();
+    INDEX_PREFIXES.remove(db);
+    HAS_87_DATA.put(db, false);
+  }
+
+  @Override
+  public void beforeAll(final ExtensionContext context) throws Exception {
+    final var clazz = context.getTestClass().get();
+    if (!isNestedClass(clazz)) {
+      DATABASE_TYPES.parallelStream()
+          .forEach(
+              db -> {
+                final Network network = Network.newNetwork();
+                startDatabaseContainer(db, network);
+                NETWORKS.put(db, network);
+              });
+    }
+  }
+
+  @Override
+  public boolean supportsTestTemplate(final ExtensionContext context) {
+    return true;
+  }
+
+  @Override
+  public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(
+      final ExtensionContext context) {
+    return DATABASE_TYPES.stream()
+        .flatMap(
+            db -> {
+              final String indexPrefix = context.getTestMethod().get().getName().toLowerCase();
+              INDEX_PREFIXES.put(db, indexPrefix);
+              final CamundaMigrator migrator = new CamundaMigrator(NETWORKS.get(db), indexPrefix);
+              migrator.initialize(db, DATABASE_EXTERNAL_URLS.get(db));
+              final var expectedDescriptors =
+                  new IndexDescriptors(indexPrefix, db.equals(DatabaseType.ELASTICSEARCH)).all();
+              DATABASE_CHECKS.put(
+                  db,
+                  new MigrationDatabaseChecks(DATABASE_EXTERNAL_URLS.get(db), expectedDescriptors));
+              MIGRATORS.put(db, migrator);
+              return Stream.of(invocationContext(db));
+            });
+  }
+
+  public void upgrade(final DatabaseType databaseType) {
+    final String indexPrefix = INDEX_PREFIXES.get(databaseType);
+    if (HAS_87_DATA.get(databaseType)) {
+      awaitImportersFlushed(databaseType, indexPrefix);
+    }
+
+    MIGRATORS.get(databaseType).update(databaseType);
+
+    awaitExporterReadiness(databaseType, indexPrefix);
+
+    /* Ingest an 8.8 Record in order to trigger importers empty batch counting */
+    ingestRecordToTriggerImporters(databaseType);
+
+    awaitImportersFinished(databaseType, indexPrefix);
+  }
+
+  private boolean isNestedClass(final Class<?> currentClass) {
+    return !ModifierSupport.isStatic(currentClass) && currentClass.isMemberClass();
+  }
+
+  public CamundaClient getCamundaClient(final DatabaseType databaseType) {
+    return MIGRATORS.get(databaseType).getCamundaClient();
+  }
+
+  /**
+   * If the test case creates data on 8.7 version, it should be marked as such in order to wait for
+   * the Tasklist & Operate import-position indices to be flushed
+   */
+  public void has87Data(final DatabaseType databaseType) {
+    HAS_87_DATA.put(databaseType, true);
+  }
+
+  public CamundaMigrator getMigrator(final DatabaseType databaseType) {
+    return MIGRATORS.get(databaseType);
+  }
+
+  private TestTemplateInvocationContext invocationContext(final DatabaseType databaseType) {
+    return new TestTemplateInvocationContext() {
+
+      @Override
+      public String getDisplayName(final int invocationIndex) {
+        return databaseType.name();
+      }
+
+      @Override
+      public List<Extension> getAdditionalExtensions() {
+        return List.of(
+            new ParameterResolver() {
+              @Override
+              public boolean supportsParameter(
+                  final ParameterContext parameterContext, final ExtensionContext extensionContext)
+                  throws ParameterResolutionException {
+                return Set.of(
+                        DatabaseType.class,
+                        CamundaClient.class,
+                        CamundaMigrator.class,
+                        DocumentBasedSearchClient.class)
+                    .contains(parameterContext.getParameter().getType());
+              }
+
+              @Override
+              public Object resolveParameter(
+                  final ParameterContext parameterContext, final ExtensionContext extensionContext)
+                  throws ParameterResolutionException {
+                if (parameterContext.getParameter().getType().equals(CamundaClient.class)) {
+                  return MIGRATORS.get(databaseType).getCamundaClient();
+                } else if (parameterContext
+                    .getParameter()
+                    .getType()
+                    .equals(CamundaMigrator.class)) {
+                  return MIGRATORS.get(databaseType);
+                } else if (parameterContext.getParameter().getType().equals(DatabaseType.class)) {
+                  return databaseType;
+                } else if (parameterContext
+                    .getParameter()
+                    .getType()
+                    .equals(DocumentBasedSearchClient.class)) {
+                  return MIGRATORS.get(databaseType).getSearchClient();
+                }
+                throw new ParameterResolutionException("Unsupported parameter type");
+              }
+            });
+      }
+    };
+  }
+
+  private void startDatabaseContainer(final DatabaseType db, final Network network) {
+    switch (db) {
+      case ELASTICSEARCH:
+        final ElasticsearchContainer elasticsearchContainer =
+            TestSearchContainers.createDefeaultElasticsearchContainer()
+                .withNetwork(network)
+                .withNetworkAliases("elasticsearch");
+        elasticsearchContainer.setPortBindings(List.of("9200:9200"));
+        elasticsearchContainer.start();
+        DB_CLOSABLES.add(elasticsearchContainer);
+        DATABASE_EXTERNAL_URLS.put(db, "http://" + elasticsearchContainer.getHttpHostAddress());
+        break;
+      case OPENSEARCH:
+        final OpensearchContainer opensearchContainer =
+            TestSearchContainers.createDefaultOpensearchContainer()
+                .withNetwork(network)
+                .withNetworkAliases("opensearch");
+        opensearchContainer.setPortBindings(List.of("9210:9200"));
+        opensearchContainer.start();
+        DATABASE_EXTERNAL_URLS.put(db, opensearchContainer.getHttpHostAddress());
+        DB_CLOSABLES.add(opensearchContainer);
+        break;
+      default:
+        throw new IllegalArgumentException("Unsupported database type: " + db);
+    }
+  }
+
+  private static void awaitExporterReadiness(
+      final DatabaseType databaseType, final String indexPrefix) {
+    Awaitility.await("Await database and exporter readiness")
+        .timeout(Duration.of(1, ChronoUnit.MINUTES))
+        .pollInterval(Duration.of(1, ChronoUnit.MILLIS))
+        .untilAsserted(() -> DATABASE_CHECKS.get(databaseType).validateSchemaCreation(indexPrefix));
+  }
+
+  private static void awaitImportersFinished(
+      final DatabaseType databaseType, final String indexPrefix) {
+    Awaitility.await("Await Importers finished")
+        .timeout(Duration.of(1, ChronoUnit.MINUTES))
+        .pollInterval(Duration.of(1, ChronoUnit.SECONDS))
+        .until(
+            () ->
+                DATABASE_CHECKS.get(databaseType).checkImportersFinished(indexPrefix, OPERATE)
+                    && DATABASE_CHECKS
+                        .get(databaseType)
+                        .checkImportersFinished(indexPrefix, TASKLIST));
+  }
+
+  private static void awaitImportersFlushed(
+      final DatabaseType databaseType, final String indexPrefix) {
+    Awaitility.await("Await Import Positions have been flushed")
+        .timeout(Duration.of(1, ChronoUnit.MINUTES))
+        .pollInterval(Duration.of(500, ChronoUnit.MILLIS))
+        .until(
+            () ->
+                DATABASE_CHECKS.get(databaseType).checkImportPositionsFlushed(indexPrefix, OPERATE)
+                    && DATABASE_CHECKS
+                        .get(databaseType)
+                        .checkImportPositionsFlushed(indexPrefix, TASKLIST));
+  }
+
+  private void ingestRecordToTriggerImporters(final DatabaseType databaseType) {
+    getCamundaClient(databaseType)
+        .newDeployResourceCommand()
+        .addResourceFromClasspath("process/error-end-event.bpmn")
+        .send()
+        .join();
+  }
+}
