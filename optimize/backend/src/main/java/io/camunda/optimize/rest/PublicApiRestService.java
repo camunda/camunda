724f07db0cf13af4b3aa9e8c249f2ade670798e4
PHWaechtler <helene.waechtler@camunda.com>
2025-03-05 07:57:21 +0100

    feat(publicAPI): add undocumented API to create collections for consulting

--- optimize/backend/src/main/java/io/camunda/optimize/rest/PublicApiRestService.java
+++ optimize/backend/src/main/java/io/camunda/optimize/rest/PublicApiRestService.java
@@ -11,8 +11,11 @@ import static io.camunda.optimize.rest.SharingRestService.SHARE_PATH;
 
 import co.elastic.clients.elasticsearch._types.ElasticsearchException;
 import io.camunda.optimize.dto.optimize.SettingsDto;
+import io.camunda.optimize.dto.optimize.UserDto;
 import io.camunda.optimize.dto.optimize.query.EntityIdResponseDto;
 import io.camunda.optimize.dto.optimize.query.IdResponseDto;
+import io.camunda.optimize.dto.optimize.query.collection.CollectionScopeEntryDto;
+import io.camunda.optimize.dto.optimize.query.collection.PartialCollectionDefinitionRequestDto;
 import io.camunda.optimize.dto.optimize.query.variable.DefinitionVariableLabelsDto;
 import io.camunda.optimize.dto.optimize.rest.export.OptimizeEntityExportDto;
 import io.camunda.optimize.dto.optimize.rest.export.report.ReportDefinitionExportDto;
@@ -20,10 +23,14 @@ import io.camunda.optimize.dto.optimize.rest.pagination.PaginatedDataExportDto;
 import io.camunda.optimize.dto.optimize.rest.pagination.PaginationScrollableDto;
 import io.camunda.optimize.dto.optimize.rest.pagination.PaginationScrollableRequestDto;
 import io.camunda.optimize.service.SettingsService;
+import io.camunda.optimize.service.collection.CollectionScopeService;
+import io.camunda.optimize.service.collection.CollectionService;
 import io.camunda.optimize.service.dashboard.DashboardService;
 import io.camunda.optimize.service.entities.EntityExportService;
 import io.camunda.optimize.service.entities.EntityImportService;
+import io.camunda.optimize.service.exceptions.OptimizeUserOrGroupIdNotFoundException;
 import io.camunda.optimize.service.export.JsonReportResultExportService;
+import io.camunda.optimize.service.identity.AbstractIdentityService;
 import io.camunda.optimize.service.report.ReportService;
 import io.camunda.optimize.service.variable.ProcessVariableLabelService;
 import jakarta.validation.Valid;
@@ -33,6 +40,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -60,6 +68,10 @@ public class PublicApiRestService {
 
   public static final String EXPORT_SUB_PATH = "/export";
   public static final String IMPORT_SUB_PATH = "/import";
+  public static final String COLLECTION_SUB_PATH = "/collection";
+  public static final String COLLECTION_CREATE_SUB_PATH = COLLECTION_SUB_PATH + "/create";
+  public static final String COLLECTION_SCOPE_SUB_PATH =
+      COLLECTION_SUB_PATH + "/collection/{id}/scope";
   public static final String REPORT_SUB_PATH = "/report";
   public static final String DASHBOARD_SUB_PATH = "/dashboard";
   public static final String LABELS_SUB_PATH = "/variables/labels";
@@ -81,6 +93,9 @@ public class PublicApiRestService {
   private final DashboardService dashboardService;
   private final ProcessVariableLabelService processVariableLabelService;
   private final SettingsService settingsService;
+  private final CollectionService collectionService;
+  private final CollectionScopeService collectionScopeService;
+  private final AbstractIdentityService identityService;
 
   @GET
   @Path(REPORT_SUB_PATH)
@@ -212,5 +227,32 @@ public class PublicApiRestService {
   public void disableShare() {
     SettingsDto settings = SettingsDto.builder().sharingEnabled(false).build();
     settingsService.setSettings(settings);
+  }
+
+  // Undocumented API used in consulting to programmatically set up/"import" collections
+  @POST
+  @Path(COLLECTION_CREATE_SUB_PATH)
+  public IdResponseDto createCollection(
+      final @RequestBody PartialCollectionDefinitionRequestDto
+              partialCollectionDefinitionCreationRequestDto) {
+    if (!identityService.doesIdentityExist(
+        new UserDto(partialCollectionDefinitionCreationRequestDto.getOwnerId()))) {
+      throw new OptimizeUserOrGroupIdNotFoundException(
+          "No Optimize user exists for the given ownerId.");
+    }
+
+    return collectionService.createNewCollectionAndReturnId(
+        partialCollectionDefinitionCreationRequestDto.getOwnerId(),
+        new PartialCollectionDefinitionRequestDto(
+            partialCollectionDefinitionCreationRequestDto.getName()));
+  }
+
+  // Undocumented API used in consulting to programmatically set up/"import" collections
+  @PUT
+  @Path(COLLECTION_SCOPE_SUB_PATH)
+  public void addScopeEntriesToCollection(
+      final @PathParam("id") String collectionId,
+      final @RequestBody List<CollectionScopeEntryDto> scopeEntries) {
+    collectionScopeService.addScopeEntriesToCollectionAsAService(collectionId, scopeEntries);
   }
 }
