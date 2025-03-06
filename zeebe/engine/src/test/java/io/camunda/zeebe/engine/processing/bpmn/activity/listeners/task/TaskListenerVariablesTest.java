d0b06d1eda9baade9f1510c5d9020bc0d80c55f0
Nico Korthout <nico.korthout@camunda.com>
2025-03-06 16:29:28 +0100

    refactor: set up new test classes
    This will allow us to easily move the test cases from TaskListenerTest
    to the individual classes.

---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/bpmn/activity/listeners/task/TaskListenerVariablesTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.bpmn.activity.listeners.task;
+
+import io.camunda.zeebe.engine.util.EngineRule;
+import io.camunda.zeebe.test.util.record.RecordingExporterTestWatcher;
+import java.util.UUID;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.TestWatcher;
+
+/** Tests verifying that a task listener can access and provide variables. */
+public class TaskListenerVariablesTest {
+  @ClassRule public static final EngineRule ENGINE = EngineRule.singlePartition();
+  @Rule public final TestWatcher watcher = new RecordingExporterTestWatcher();
+
+  private String listenerType;
+
+  @Before
+  public void setup() {
+    listenerType = "my_listener_" + UUID.randomUUID();
+  }
+}
