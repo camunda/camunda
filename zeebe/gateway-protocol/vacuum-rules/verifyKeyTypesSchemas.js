1792eac5be00ac485263ed6469132ab06bb11761
Steven Hicks <steven.hicks@camunda.com>
2025-03-06 16:24:16 -0600

    fix: accept allOf types for key fields

--- zeebe/gateway-protocol/vacuum-rules/verifyKeyTypesSchemas.js
+++ zeebe/gateway-protocol/vacuum-rules/verifyKeyTypesSchemas.js
@@ -1,13 +1,17 @@
 function runRule(input) {
   // `input` is an entire schema object with all properties.
-  //   Identify all properties that end with Key, and are not strings.
+  //   Identify all properties that end with Key, and are not strings or allOfs.
   return Object.entries(input)
     .filter(([name, schema]) => {
-      return /Key$/.test(name) && schema.type !== "string";
+      return (
+        /Key$/.test(name) &&
+        schema.type !== "string" &&
+        !Array.isArray(schema.allOf)
+      );
     })
     .map(([name]) => {
       return {
-        message: `\`${name}\` property must be of type \`string\`.`,
+        message: `\`${name}\` property must be of type \`string\` or \`BasicStringFilterProperty\`.`,
       };
     });
 }
