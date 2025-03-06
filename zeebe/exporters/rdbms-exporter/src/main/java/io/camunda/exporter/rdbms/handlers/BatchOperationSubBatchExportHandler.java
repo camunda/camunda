1da4de0c66abf1f89f3fcb802cd2ddfa7f9c8457
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-03-06 14:40:55 +0100

    feat: batch operations / rdbms - use new subBatch Record for appending items

---
+++ zeebe/exporters/rdbms-exporter/src/main/java/io/camunda/exporter/rdbms/handlers/BatchOperationSubBatchExportHandler.java
@@ -0,0 +1,45 @@
+package io.camunda.exporter.rdbms.handlers;
+
+import io.camunda.db.rdbms.write.domain.BatchOperationDbModel;
+import io.camunda.db.rdbms.write.service.BatchOperationWriter;
+import io.camunda.exporter.rdbms.RdbmsExportHandler;
+import io.camunda.search.entities.BatchOperationEntity.BatchOperationState;
+import io.camunda.zeebe.protocol.Protocol;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.BatchOperationIntent;
+import io.camunda.zeebe.protocol.record.value.BatchOperationCreationRecordValue;
+import io.camunda.zeebe.protocol.record.value.BatchOperationSubbatchRecordValue;
+import io.camunda.zeebe.util.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Exports a batch operation creation record to the database. This is only done if the batch was
+ * also created on this partition!
+ */
+public class BatchOperationSubBatchExportHandler
+    implements RdbmsExportHandler<BatchOperationSubbatchRecordValue> {
+
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(BatchOperationSubBatchExportHandler.class);
+
+  private final BatchOperationWriter batchOperationWriter;
+
+  public BatchOperationSubBatchExportHandler(final BatchOperationWriter batchOperationWriter) {
+    this.batchOperationWriter = batchOperationWriter;
+  }
+
+  @Override
+  public boolean canExport(final Record<BatchOperationSubbatchRecordValue> record) {
+    return record.getValueType() == ValueType.BATCH_OPERATION_SUBBATCH
+        && record.getIntent().equals(BatchOperationIntent.CREATED_SUBBATCH);
+  }
+
+  @Override
+  public void export(final Record<BatchOperationSubbatchRecordValue> record) {
+    final var value = record.getValue();
+    batchOperationWriter.updateBatchAndInsertItems(value.getBatchOperationKey(), value.getKeys());
+  }
+
+}
