d1bd1f810f440004a3d601df54b198a673b2df86
Christopher Kujawa <zelldon91@googlemail.com>
2025-03-06 15:33:20 +0100

    refactor: clean up
     Remove dead and obsolete code
    
    (cherry picked from commit fc8612e2cfd0ffe8e4d1a364e8022f183cae0dc6)

--- dist/src/main/java/io/camunda/application/StandaloneBackupManager.java
+++ dist/src/main/java/io/camunda/application/StandaloneBackupManager.java
@@ -7,8 +7,6 @@
  */
 package io.camunda.application;
 
-import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
-
 import io.camunda.application.StandaloneSchemaManager.SchemaManagerConfiguration.BrokerBasedProperties;
 import io.camunda.application.commons.sources.DefaultObjectMapperConfiguration;
 import io.camunda.application.listeners.ApplicationErrorListener;
@@ -19,14 +17,12 @@ import io.camunda.tasklist.property.TasklistProperties;
 import io.camunda.tasklist.webapp.es.backup.BackupManager;
 import io.camunda.tasklist.webapp.management.dto.BackupStateDto;
 import io.camunda.tasklist.webapp.management.dto.TakeBackupRequestDto;
-import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
@@ -81,18 +77,11 @@ import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGe
 public class StandaloneBackupManager implements CommandLineRunner {
 
   private static final Logger LOG = LoggerFactory.getLogger(StandaloneBackupManager.class);
-  private static final String SPRING_PROFILES_ACTIVE_PROPERTY = ACTIVE_PROFILES_PROPERTY_NAME;
-  private static final String DEFAULT_CAMUNDA_PROFILES =
-      String.join(",", Profile.OPERATE.getId(), Profile.TASKLIST.getId());
-  private final BrokerBasedProperties brokerProperties;
   private final BackupManager tasklistBackupManager;
   private final BackupService operateBackupManager;
 
   public StandaloneBackupManager(
-      final BrokerBasedProperties brokerProperties,
-      final BackupManager tasklistBackupManager,
-      final BackupService operateBackupManager) {
-    this.brokerProperties = brokerProperties;
+      final BackupManager tasklistBackupManager, final BackupService operateBackupManager) {
     this.tasklistBackupManager = tasklistBackupManager;
     this.operateBackupManager = operateBackupManager;
   }
@@ -112,16 +101,11 @@ public class StandaloneBackupManager implements CommandLineRunner {
 
     LOG.info("Creating/updating Elasticsearch schema for Camunda ...");
 
-    //    final var defaultActiveProfiles = getDefaultActiveProfiles();
     MainSupport.createDefaultApplicationBuilder()
         .web(WebApplicationType.NONE)
         .logStartupInfo(true)
         .sources(
             BackupManagerConfiguration.class,
-            //            CommonsModuleConfiguration.class,
-            //            OperateModuleConfiguration.class,
-            //            TasklistModuleConfiguration.class,
-            //            WebappModuleConfiguration.class,
             StandaloneBackupManager.class,
             io.camunda.tasklist.JacksonConfig.class,
             io.camunda.operate.JacksonConfig.class)
@@ -134,13 +118,6 @@ public class StandaloneBackupManager implements CommandLineRunner {
     // blocking shutdown.
     System.exit(0);
   }
-
-  //
-  //  public static Map<String, Object> getDefaultActiveProfiles() {
-  //    final var defaultProperties = new HashMap<String, Object>();
-  //    defaultProperties.put(SPRING_PROFILES_ACTIVE_PROPERTY, DEFAULT_CAMUNDA_PROFILES);
-  //    return defaultProperties;
-  //  }
 
   @Override
   public void run(final String... args) throws Exception {
@@ -204,16 +181,6 @@ public class StandaloneBackupManager implements CommandLineRunner {
         }
       } while (snapshotObservation);
 
-      //      final var elasticsearchConfig =
-      //          new ExporterConfiguration(
-      //                  "elasticsearch",
-      // brokerProperties.getExporters().get("elasticsearch").getArgs())
-      //              .instantiate(ElasticsearchExporterConfiguration.class);
-
-      //      tasklistBackupManager.getBackupState()
-      // Not needed
-      //      new io.camunda.zeebe.exporter.SchemaManager(elasticsearchConfig).createSchema();
-      //      operateUserDetailsService.initializeUsers();
     } catch (final Exception e) {
       LOG.error("Expected to trigger ES snapshots for backupId {}, but failed", backupId, e);
       throw e;
@@ -222,30 +189,18 @@ public class StandaloneBackupManager implements CommandLineRunner {
     LOG.info("... triggered Elasticsearch snapshot based on Camunda's backup procedure");
   }
 
-  // LEARNING - basePackageClasses - defining a class causes to scan the whole package with * doing
-  // a traversal to include all childs
-  // This approach doesnt work pretty good as we include a lot of other things
-
   @SpringBootConfiguration
   @EnableConfigurationProperties(BrokerBasedProperties.class)
   @ConfigurationPropertiesScan
   @ComponentScan(
-      //      basePackages = {"io.camunda.operate.*"}
+      // LEARNING - basePackageClasses - defining a class causes to scan the whole package with *
+      // doing
+      // a traversal to include all childs
       basePackageClasses = {
-        //        OperateUserDetailsService.class, - Not needed
-        // Schema startup - to have access on index descriptors
-        // TODO: Find a better way
-        //        io.camunda.tasklist.schema.SchemaStartup.class, // - Not needed
         io.camunda.tasklist.schema.indices.IndexDescriptor.class,
         io.camunda.tasklist.schema.templates.TemplateDescriptor.class,
         io.camunda.operate.schema.indices.IndexDescriptor.class,
         io.camunda.operate.schema.templates.TemplateDescriptor.class,
-        // we need this metrics for the ElasticsearchImportStore; todo: why do we need it?
-        // WE DONT WANT THIS
-        //        io.camunda.operate.Metrics.class,
-        //        io.camunda.tasklist.Metrics.class,
-
-        //        io.camunda.operate.schema.SchemaStartup.class, // - Not needed
         // Backup services from Operate / Tasklist - to be used to create backups
         io.camunda.operate.webapp.backup.BackupService.class,
         io.camunda.operate.webapp.elasticsearch.backup.ElasticsearchBackupRepository.class,
@@ -258,18 +213,9 @@ public class StandaloneBackupManager implements CommandLineRunner {
         io.camunda.operate.connect.ElasticsearchConnector
             .class, // we need this to find the right clients for Operate
         ElasticsearchConnector.class,
-        // WE DONT WANT THIS
-        //        io.camunda.operate.tenant.TenantAwareElasticsearchClient
-        //            .class, // needed for decision store - that is pulled in from somwhere TODO;
-        // clarify
-        //        io.camunda.operate.store.elasticsearch.RetryElasticsearchClient.class,
-        //        io.camunda.tasklist.es.RetryElasticsearchClient.class,
         // Object mapper used by other components
         DefaultObjectMapperConfiguration.class,
       },
       nameGenerator = FullyQualifiedAnnotationBeanNameGenerator.class)
-  public static class BackupManagerConfiguration {
-    @ConfigurationProperties("zeebe.broker")
-    public static final class BrokerBasedProperties extends BrokerCfg {}
-  }
+  public static class BackupManagerConfiguration {}
 }
