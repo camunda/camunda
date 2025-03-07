d9f4c6846362135584bdd7d79c1c6996e679c50a
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-03-07 15:46:08 +0200

    feat: create job for first `updating` listener on variable update
    Enhanced `VariableDocumentUpdateProcessor` to create a job for the first `updating` listener
    when a task-scoped variable update is triggered.
    
    Added a test to verify that the job was created and to assert the activated job:
    - Custom headers include `"variables"` as a changed attribute.
    - Variables include both updated and existing process instance variables.
    
    Introduced `expectPartialUpdate` in `VariableClient` to accommodate the logic split
    in `VariableDocumentUpdateProcessor` due to the creation of the job for the first `updating` listener.
    
    Co-authored-by: Ana Vinogradova <ana.vinogradova@camunda.com>

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/BpmnProcessors.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/BpmnProcessors.java
@@ -221,11 +221,7 @@ public final class BpmnProcessors {
         ValueType.VARIABLE_DOCUMENT,
         VariableDocumentIntent.UPDATE,
         new VariableDocumentUpdateProcessor(
-            processingState,
-            keyGenerator,
-            bpmnBehaviors.variableBehavior(),
-            writers,
-            authCheckBehavior));
+            processingState, keyGenerator, bpmnBehaviors, writers, authCheckBehavior));
   }
 
   private static void addProcessInstanceCreationStreamProcessors(
