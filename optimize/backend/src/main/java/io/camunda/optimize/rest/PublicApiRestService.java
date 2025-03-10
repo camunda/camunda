b164ad1ecf8c4d6f6d1436928907066d3418d266
Christophe <christophe.dame@camunda.com>
2025-03-10 17:27:19 +0100

    feat: add undocumented API to list collections

--- optimize/backend/src/main/java/io/camunda/optimize/rest/PublicApiRestService.java
+++ optimize/backend/src/main/java/io/camunda/optimize/rest/PublicApiRestService.java
@@ -14,6 +14,7 @@ import io.camunda.optimize.dto.optimize.SettingsDto;
 import io.camunda.optimize.dto.optimize.UserDto;
 import io.camunda.optimize.dto.optimize.query.EntityIdResponseDto;
 import io.camunda.optimize.dto.optimize.query.IdResponseDto;
+import io.camunda.optimize.dto.optimize.query.collection.CollectionDefinitionDto;
 import io.camunda.optimize.dto.optimize.query.collection.CollectionScopeEntryDto;
 import io.camunda.optimize.dto.optimize.query.collection.PartialCollectionDefinitionRequestDto;
 import io.camunda.optimize.dto.optimize.query.variable.DefinitionVariableLabelsDto;
@@ -41,6 +42,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -63,6 +65,7 @@ public class PublicApiRestService {
   public static final String EXPORT_SUB_PATH = "/export";
   public static final String IMPORT_SUB_PATH = "/import";
   public static final String COLLECTION_SUB_PATH = "/collection";
+  public static final String COLLECTION_BY_ID_PATH = COLLECTION_SUB_PATH + "/{collectionId}";
   public static final String COLLECTION_CREATE_SUB_PATH = COLLECTION_SUB_PATH + "/create";
   public static final String COLLECTION_SCOPE_SUB_PATH =
       COLLECTION_SUB_PATH + "/collection/{id}/scope";
@@ -198,6 +201,21 @@ public class PublicApiRestService {
   public void disableShare() {
     final SettingsDto settings = SettingsDto.builder().sharingEnabled(false).build();
     settingsService.setSettings(settings);
+  }
+
+  // Undocumented API used in consulting to programmatically list collections
+  @GetMapping(COLLECTION_SUB_PATH)
+  public List<IdResponseDto> getCollectionIds() {
+    return collectionService.getAllCollections().stream()
+        .map(collectionDef -> new IdResponseDto(collectionDef.getId()))
+        .collect(Collectors.toList());
+  }
+
+  // Undocumented API used in consulting to read a collection
+  @GetMapping(COLLECTION_BY_ID_PATH)
+  public CollectionDefinitionDto getCollection(
+      final @PathVariable("collectionId") String collectionId) {
+    return collectionService.getCollectionDefinition(collectionId);
   }
 
   // Undocumented API used in consulting to programmatically set up/"import" collections
