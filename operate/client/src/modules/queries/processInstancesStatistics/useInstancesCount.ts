4e4711dfaf45ffb4d33e2146b6b3b68e69672ea3
Alex Bremard <alex.bremard@camunda.com>
2025-03-11 08:14:43 +0700

    feat: migrate BatchModificationNotification component to v2

---
+++ operate/client/src/modules/queries/processInstancesStatistics/useInstancesCount.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {ProcessInstancesStatisticsDto} from 'modules/api/v2/processInstances/fetchProcessInstancesStatistics';
+import {useProcessInstancesStatistics} from './useProcessInstancesStatistics';
+
+function getInstancesCount(
+  data: ProcessInstancesStatisticsDto[],
+  flowNodeId: string,
+) {
+  const flowNodeStatistics = data.find(
+    (statistics) => statistics.flowNodeId === flowNodeId,
+  );
+
+  if (flowNodeStatistics === undefined) {
+    return 0;
+  }
+
+  return flowNodeStatistics.active + flowNodeStatistics.incidents;
+}
+
+function instancesCountParser(
+  flowNodeId: string,
+): (data: ProcessInstancesStatisticsDto[]) => number {
+  return (data: ProcessInstancesStatisticsDto[]) => {
+    return getInstancesCount(data, flowNodeId);
+  };
+}
+
+function useInstancesCount(flowNodeId: string) {
+  return useProcessInstancesStatistics<number>(
+    {flowNodeId: flowNodeId},
+    instancesCountParser(flowNodeId),
+    flowNodeId !== '',
+  );
+}
+
+export {getInstancesCount, useInstancesCount};
