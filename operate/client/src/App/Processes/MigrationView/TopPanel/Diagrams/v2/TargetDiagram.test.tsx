bb35139de01ac92e4e3455f081e562b86027205d
Alex Bremard <alex.bremard@camunda.com>
2025-03-17 09:50:57 +0700

    feat: use zod endpoint

--- operate/client/src/App/Processes/MigrationView/TopPanel/Diagrams/v2/TargetDiagram.test.tsx
+++ operate/client/src/App/Processes/MigrationView/TopPanel/Diagrams/v2/TargetDiagram.test.tsx
@@ -23,6 +23,7 @@ import {TargetDiagram} from './TargetDiagram';
 import {processInstanceMigrationStore} from 'modules/stores/processInstanceMigration';
 import {Wrapper} from '../tests/mocks';
 import * as filterModule from 'modules/hooks/useProcessInstancesFilters';
+import * as processIdModule from 'modules/hooks/useProcessId';
 import {mockFetchProcessInstancesStatistics} from 'modules/mocks/api/v2/processInstances/fetchProcessInstancesStatistics';
 import {mockFetchProcessDefinitionXml} from 'modules/mocks/api/v2/processDefinitions/fetchProcessDefinitionXml';
 import {processInstancesSelectionStore} from 'modules/stores/processInstancesSelection';
@@ -33,6 +34,7 @@ jest.mock('modules/hooks/useProcessInstancesFilters');
 describe('Target Diagram', () => {
   beforeEach(() => {
     jest.spyOn(filterModule, 'useProcessInstanceFilters').mockReturnValue({});
+    jest.spyOn(processIdModule, 'useProcessId').mockReturnValue('123');
   });
 
   afterEach(() => {
