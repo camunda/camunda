b96a30a9c486db441f3c6fa31b5ffa465c3f18ea
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-03-17 12:43:21 +0100

    fix: fix PDF preview on Chrome

--- tasklist/common/src/main/java/io/camunda/tasklist/property/SecurityProperties.java
+++ tasklist/common/src/main/java/io/camunda/tasklist/property/SecurityProperties.java
@@ -9,7 +9,7 @@ package io.camunda.tasklist.property;
 
 public class SecurityProperties {
   public static final String CONTENT_SECURITY_POLICY =
-      "default-src 'self';connect-src 'self' https: *.mixpanel.com cloudflareinsights.com *.appcues.net wss://api.appcues.net;script-src 'self' https: *.chargebee.com *.mixpanel.com ajax.cloudflare.com static.cloudflareinsights.com;style-src 'self' https: 'unsafe-inline' *.googleapis.com *.chargebee.com;img-src * data: ;font-src 'self' data: https://fonts.gstatic.com https://fonts.camunda.io;frame-ancestors 'self';frame-src 'self' https: *.chargebee.com ;child-src;worker-src 'self' blob:;base-uri 'self';form-action 'self';object-src 'self' blob:;script-src-attr 'none';";
+      "default-src 'self';connect-src 'self' https: *.mixpanel.com cloudflareinsights.com *.appcues.net wss://api.appcues.net;script-src 'self' https: *.chargebee.com *.mixpanel.com ajax.cloudflare.com static.cloudflareinsights.com;style-src 'self' https: 'unsafe-inline' *.googleapis.com *.chargebee.com;img-src * data: ;font-src 'self' data: https://fonts.gstatic.com https://fonts.camunda.io;frame-ancestors 'self';frame-src 'self' https: *.chargebee.com blob:;child-src;worker-src 'self' blob:;base-uri 'self';form-action 'self';object-src 'self' blob:;script-src-attr 'none';";
 
   private String contentSecurityPolicy = CONTENT_SECURITY_POLICY;
 
