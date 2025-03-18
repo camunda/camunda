17746c170eca2b422af171a4a81edf56de8eeb35
Mathias Geat <mathias.geat@camunda.com>
2025-03-18 11:49:16 +0100

    test: add tests verifying interaction of ad-hoc completion API with completion condition an cancel remaining instances

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/ActivateAdHocSubprocessActivityTest.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/client/command/ActivateAdHocSubprocessActivityTest.java
@@ -16,17 +16,21 @@ import io.camunda.client.api.command.ProblemException;
 import io.camunda.client.api.response.ProcessInstanceEvent;
 import io.camunda.zeebe.it.util.ZeebeResourcesHelper;
 import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.builder.AdHocSubProcessBuilder;
 import io.camunda.zeebe.protocol.impl.record.value.signal.SignalRecord;
 import io.camunda.zeebe.protocol.record.Record;
-import io.camunda.zeebe.protocol.record.RecordValue;
+import io.camunda.zeebe.protocol.record.intent.JobIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.protocol.record.intent.SignalIntent;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
+import io.camunda.zeebe.test.util.record.ProcessInstanceRecordStream;
 import io.camunda.zeebe.test.util.record.RecordingExporter;
 import java.time.Duration;
-import java.util.function.Predicate;
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -48,27 +52,29 @@ public class ActivateAdHocSubprocessActivityTest {
   private ProcessInstanceEvent processInstance;
 
   @BeforeEach
-  public void init(final TestInfo testInfo) {
+  public void init() {
     client = zeebe.newClientBuilder().defaultRequestTimeout(Duration.ofSeconds(15)).build();
     resourcesHelper = new ZeebeResourcesHelper(client);
-
-    deploy(testInfo);
-    processInstance =
-        client.newCreateInstanceCommand().bpmnProcessId(processId).latestVersion().send().join();
   }
 
   @Test
-  void shouldActivateAdHocSubprocessActivities(final TestInfo testInfo) {
-    // allows us to wait for the signal to be broadcasted
-    client.newBroadcastSignalCommand().signalName("signal").send().join();
+  void shouldActivateActivitiesAndCompleteProcess(final TestInfo testInfo) {
+    // given
+    deployAndStartInstance(
+        testInfo,
+        adHocSubProcess -> {
+          adHocSubProcess.task("A");
+          adHocSubProcess.task("B");
+          adHocSubProcess.task("C");
+        });
 
-    assertThat(
-            RecordingExporter.records()
-                .limit(signalBroadcasted("signal"))
-                .processInstanceRecords()
-                .withProcessInstanceKey(processInstance.getProcessInstanceKey()))
+    client.newBroadcastSignalCommand().signalName("setup_signal").send().join();
+
+    assertThat(recordsUpToSignal("setup_signal"))
         .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .describedAs("Expect ad-hoc subprocess to be activated")
         .contains(tuple(AD_HOC_SUB_PROCESS_ELEMENT_ID, ProcessInstanceIntent.ELEMENT_ACTIVATED))
+        .describedAs("Expect no activities to be activated")
         .doesNotContain(
             tuple("A", ProcessInstanceIntent.ACTIVATE_ELEMENT),
             tuple("B", ProcessInstanceIntent.ACTIVATE_ELEMENT),
@@ -95,6 +101,7 @@ public class ActivateAdHocSubprocessActivityTest {
                 .withProcessInstanceKey(processInstance.getProcessInstanceKey())
                 .limitToProcessInstanceCompleted())
         .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .describedAs("Expect activated activities and whole process to be completed")
         .contains(
             tuple("A", ProcessInstanceIntent.ELEMENT_COMPLETED),
             tuple("C", ProcessInstanceIntent.ELEMENT_COMPLETED),
@@ -103,13 +110,157 @@ public class ActivateAdHocSubprocessActivityTest {
   }
 
   @Test
-  void shouldReturnErrorOnCommandRejection(final TestInfo testInfo) {
+  void shouldActivateActivitiesAndCompleteProcessOnlyWhenCompletionConditionIsMet(
+      final TestInfo testInfo) {
+    // given
+    deployAndStartInstance(
+        testInfo,
+        adHocSubProcess -> {
+          adHocSubProcess.completionCondition("condition");
+          adHocSubProcess.task("A");
+          adHocSubProcess.task("B");
+          adHocSubProcess.serviceTask("ServiceTask", b -> b.zeebeJobType("testType"));
+        },
+        Map.of("condition", false));
+
     final var activatedAdHocSubprocess =
         RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
             .withProcessInstanceKey(processInstance.getProcessInstanceKey())
             .withElementId(AD_HOC_SUB_PROCESS_ELEMENT_ID)
             .getFirst();
 
+    // when1
+    client
+        .newActivateAdHocSubprocessActivitiesCommand(
+            String.valueOf(activatedAdHocSubprocess.getKey()))
+        .activateElements("A", "B")
+        .send()
+        .join();
+
+    client.newBroadcastSignalCommand().signalName("signal1").send().join();
+
+    // then1
+    assertThat(recordsUpToSignal("signal1"))
+        .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .describedAs(
+            "Expect ad-hoc process instance not to be completed until completion condition is not met")
+        .contains(
+            tuple("A", ProcessInstanceIntent.ELEMENT_COMPLETED),
+            tuple("B", ProcessInstanceIntent.ELEMENT_COMPLETED))
+        .doesNotContain(
+            tuple(AD_HOC_SUB_PROCESS_ELEMENT_ID, ProcessInstanceIntent.ELEMENT_COMPLETED));
+
+    // when2
+    client
+        .newActivateAdHocSubprocessActivitiesCommand(
+            String.valueOf(activatedAdHocSubprocess.getKey()))
+        .activateElements("ServiceTask")
+        .send()
+        .join();
+
+    final var createdJob =
+        RecordingExporter.jobRecords(JobIntent.CREATED)
+            .withProcessInstanceKey(processInstance.getProcessInstanceKey())
+            .getFirst();
+
+    // complete service task with completion condition variable changing to true
+    client.newCompleteCommand(createdJob.getKey()).variable("condition", true).send().join();
+
+    // then2
+    assertThat(
+            RecordingExporter.processInstanceRecords()
+                .withProcessInstanceKey(processInstance.getProcessInstanceKey())
+                .limitToProcessInstanceCompleted())
+        .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .describedAs("Expect activated activities and whole process to be completed")
+        .contains(
+            tuple("ServiceTask", ProcessInstanceIntent.ELEMENT_COMPLETED),
+            tuple(AD_HOC_SUB_PROCESS_ELEMENT_ID, ProcessInstanceIntent.ELEMENT_COMPLETED),
+            tuple(processId, ProcessInstanceIntent.ELEMENT_COMPLETED));
+  }
+
+  @Test
+  void shouldCancelRemainingInstancesWhenCompletionConditionIsMetAfterActivatingActivities(
+      final TestInfo testInfo) {
+    // given
+    deployAndStartInstance(
+        testInfo,
+        adHocSubProcess -> {
+          adHocSubProcess.completionCondition("condition");
+          adHocSubProcess.cancelRemainingInstances(true);
+          adHocSubProcess.task("A");
+          adHocSubProcess.task("B");
+          adHocSubProcess.serviceTask("ServiceTask", b -> b.zeebeJobType("testType")).task("C");
+        },
+        Map.of("condition", true));
+
+    client.newBroadcastSignalCommand().signalName("setup_signal").send().join();
+
+    assertThat(recordsUpToSignal("setup_signal"))
+        .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .describedAs("Expect ad-hoc subprocess to be activated")
+        .contains(tuple(AD_HOC_SUB_PROCESS_ELEMENT_ID, ProcessInstanceIntent.ELEMENT_ACTIVATED))
+        .describedAs("Expect no activities to be activated")
+        .doesNotContain(
+            tuple("A", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+            tuple("B", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+            tuple("ServiceTask", ProcessInstanceIntent.ACTIVATE_ELEMENT),
+            tuple(AD_HOC_SUB_PROCESS_ELEMENT_ID, ProcessInstanceIntent.ELEMENT_COMPLETED));
+
+    final var activatedAdHocSubprocess =
+        RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
+            .withProcessInstanceKey(processInstance.getProcessInstanceKey())
+            .withElementId(AD_HOC_SUB_PROCESS_ELEMENT_ID)
+            .getFirst();
+
+    // when
+    client
+        .newActivateAdHocSubprocessActivitiesCommand(
+            String.valueOf(activatedAdHocSubprocess.getKey()))
+        .activateElements("A", "ServiceTask")
+        .send()
+        .join();
+
+    // then
+    assertThat(
+            RecordingExporter.processInstanceRecords()
+                .withProcessInstanceKey(processInstance.getProcessInstanceKey())
+                .limitToProcessInstanceCompleted())
+        .extracting(r -> r.getValue().getElementId(), Record::getIntent)
+        .describedAs("Expect service task to be terminated after completion of A")
+        .contains(
+            tuple("A", ProcessInstanceIntent.ELEMENT_ACTIVATED),
+            tuple("ServiceTask", ProcessInstanceIntent.ELEMENT_ACTIVATED),
+            tuple("A", ProcessInstanceIntent.ELEMENT_COMPLETED),
+            tuple("ServiceTask", ProcessInstanceIntent.TERMINATE_ELEMENT),
+            tuple("ServiceTask", ProcessInstanceIntent.ELEMENT_TERMINATED),
+            tuple(AD_HOC_SUB_PROCESS_ELEMENT_ID, ProcessInstanceIntent.ELEMENT_COMPLETED),
+            tuple(processId, ProcessInstanceIntent.ELEMENT_COMPLETED))
+        .describedAs(
+            "Expect service task to never complete and task depending on service task to never be activated")
+        .doesNotContain(
+            tuple("ServiceTask", ProcessInstanceIntent.ELEMENT_COMPLETED),
+            tuple("C", ProcessInstanceIntent.ELEMENT_ACTIVATED));
+  }
+
+  @Test
+  void shouldReturnErrorOnCommandRejection(final TestInfo testInfo) {
+    // given
+    deployAndStartInstance(
+        testInfo,
+        adHocSubProcess -> {
+          adHocSubProcess.task("A");
+          adHocSubProcess.task("B");
+          adHocSubProcess.task("C");
+        });
+
+    final var activatedAdHocSubprocess =
+        RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
+            .withProcessInstanceKey(processInstance.getProcessInstanceKey())
+            .withElementId(AD_HOC_SUB_PROCESS_ELEMENT_ID)
+            .getFirst();
+
+    // when/then
     assertThatThrownBy(
             () ->
                 client
@@ -125,26 +276,41 @@ public class ActivateAdHocSubprocessActivityTest {
                 .formatted(activatedAdHocSubprocess.getKey()));
   }
 
-  private void deploy(final TestInfo testInfo) {
+  private void deployAndStartInstance(
+      final TestInfo testInfo, final Consumer<AdHocSubProcessBuilder> modifier) {
+    deployAndStartInstance(testInfo, modifier, Collections.emptyMap());
+  }
+
+  private void deployAndStartInstance(
+      final TestInfo testInfo,
+      final Consumer<AdHocSubProcessBuilder> modifier,
+      final Map<String, Object> variables) {
     processId = "process-" + testInfo.getTestMethod().get().getName();
 
     resourcesHelper.deployProcess(
         Bpmn.createExecutableProcess(processId)
             .startEvent()
-            .adHocSubProcess(
-                AD_HOC_SUB_PROCESS_ELEMENT_ID,
-                adHocSubProcess -> {
-                  adHocSubProcess.task("A");
-                  adHocSubProcess.task("B");
-                  adHocSubProcess.task("C");
-                })
+            .adHocSubProcess(AD_HOC_SUB_PROCESS_ELEMENT_ID, modifier)
             .endEvent()
             .done());
+
+    processInstance =
+        client
+            .newCreateInstanceCommand()
+            .bpmnProcessId(processId)
+            .latestVersion()
+            .variables(variables)
+            .send()
+            .join();
   }
 
-  private static Predicate<Record<RecordValue>> signalBroadcasted(final String signalName) {
-    return r ->
-        r.getIntent() == SignalIntent.BROADCASTED
-            && ((SignalRecord) r.getValue()).getSignalName().equals(signalName);
+  private ProcessInstanceRecordStream recordsUpToSignal(final String signalName) {
+    return RecordingExporter.records()
+        .limit(
+            r ->
+                r.getIntent() == SignalIntent.BROADCASTED
+                    && ((SignalRecord) r.getValue()).getSignalName().equals(signalName))
+        .processInstanceRecords()
+        .withProcessInstanceKey(processInstance.getProcessInstanceKey());
   }
 }
