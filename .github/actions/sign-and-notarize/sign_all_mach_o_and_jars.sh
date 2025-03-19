bb9dcec2331cb846e7be35069d7955d43334533b
Jesse Simpson <jesse.simpson@camunda.com>
2025-03-19 19:34:15 -0400

    fix: don't replace manifest.mf

--- .github/actions/sign-and-notarize/sign_all_mach_o_and_jars.sh
+++ .github/actions/sign-and-notarize/sign_all_mach_o_and_jars.sh
@@ -151,7 +151,7 @@ while IFS= read -r -d '' jar_file; do
   (
     cd "$tmpdir"
     echo "  -> Re-zipping into $jar_abs_path"
-    jar -uf "$jar_abs_path" ./
+    jar -M -uf "$jar_abs_path" ./
   )
 
   rm -rf "$tmpdir"
