72ab785796fe9946bbc65144cf439730dbbbaec3
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-03-20 11:25:55 +0100

    feat: add broker requests to cancel, pause, resume batch operations #29712

---
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerResumeBatchOperationRequest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.impl.broker.request;
+
+import io.camunda.zeebe.broker.client.api.dto.BrokerExecuteCommand;
+import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationExecutionRecord;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.BatchOperationExecutionIntent;
+import org.agrona.DirectBuffer;
+
+public class BrokerResumeBatchOperationRequest
+    extends BrokerExecuteCommand<BatchOperationExecutionRecord> {
+
+  BatchOperationExecutionRecord requestDto = new BatchOperationExecutionRecord();
+
+  public BrokerResumeBatchOperationRequest() {
+    super(ValueType.BATCH_OPERATION_EXECUTION, BatchOperationExecutionIntent.RESUME);
+  }
+
+  public BrokerResumeBatchOperationRequest setBatchOperationKey(final long batchOperationKey) {
+    requestDto.setBatchOperationKey(batchOperationKey);
+    return this;
+  }
+
+  @Override
+  public BatchOperationExecutionRecord getRequestWriter() {
+    return requestDto;
+  }
+
+  @Override
+  protected BatchOperationExecutionRecord toResponseDto(final DirectBuffer buffer) {
+    final BatchOperationExecutionRecord responseDto = new BatchOperationExecutionRecord();
+    responseDto.wrap(buffer);
+    return responseDto;
+  }
+}
