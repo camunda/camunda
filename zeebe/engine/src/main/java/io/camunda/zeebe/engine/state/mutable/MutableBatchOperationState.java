f508b04b5a434792937bef59680cf35ad47e94fb
Christian Thiel <christian.thiel@holisticon.de>
2025-03-20 07:35:39 +0100

    feat: add batch operation processor, state and applier for CREATE and CREATED #29695

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/mutable/MutableBatchOperationState.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.state.mutable;
+
+import io.camunda.zeebe.engine.state.immutable.BatchOperationState;
+import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationCreationRecord;
+
+public interface MutableBatchOperationState extends BatchOperationState {
+
+  /**
+   * Stores the batch operation in the state.
+   *
+   * @param record the batch operation creation record to create
+   */
+  void create(final long batchKey, final BatchOperationCreationRecord record);
+}
