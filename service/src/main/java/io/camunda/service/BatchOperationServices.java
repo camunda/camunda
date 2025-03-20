26b06158b90ba8e31659415cb96968ac7f7b4bc7
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-03-20 11:26:47 +0100

    feat: add new endpoints + service to pause cancel and resume batch operation

--- service/src/main/java/io/camunda/service/BatchOperationServices.java
+++ service/src/main/java/io/camunda/service/BatchOperationServices.java
@@ -20,7 +20,12 @@ import io.camunda.security.auth.Authorization.Builder;
 import io.camunda.service.search.core.SearchQueryService;
 import io.camunda.service.security.SecurityContextProvider;
 import io.camunda.zeebe.broker.client.api.BrokerClient;
+import io.camunda.zeebe.gateway.impl.broker.request.BrokerCancelBatchOperationRequest;
+import io.camunda.zeebe.gateway.impl.broker.request.BrokerPauseBatchOperationRequest;
+import io.camunda.zeebe.gateway.impl.broker.request.BrokerResumeBatchOperationRequest;
+import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationExecutionRecord;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,8 +56,7 @@ public final class BatchOperationServices
     return batchOperationSearchClient
         .withSecurityContext(
             securityContextProvider.provideSecurityContext(
-                authentication,
-                Authorization.of(Builder::read))) // TODO do we need an own auth category?
+                authentication, Authorization.of(Builder::read)))
         .searchBatchOperations(query);
   }
 
@@ -65,5 +69,31 @@ public final class BatchOperationServices
 
   public List<BatchOperationItemEntity> getItemsByKey(final Long key) {
     return batchOperationSearchClient.getBatchOperationItems(key);
+  }
+
+  public CompletableFuture<BatchOperationExecutionRecord> cancel(final long batchKey) {
+    LOGGER.debug("Cancelling batch operation with key '{}'", batchKey);
+
+    final var brokerRequest =
+        new BrokerCancelBatchOperationRequest().setBatchOperationKey(batchKey);
+
+    return sendBrokerRequest(brokerRequest);
+  }
+
+  public CompletableFuture<BatchOperationExecutionRecord> pause(final long batchKey) {
+    LOGGER.debug("Pausing batch operation with key '{}'", batchKey);
+
+    final var brokerRequest = new BrokerPauseBatchOperationRequest().setBatchOperationKey(batchKey);
+
+    return sendBrokerRequest(brokerRequest);
+  }
+
+  public CompletableFuture<BatchOperationExecutionRecord> resume(final long batchKey) {
+    LOGGER.debug("Resuming batch operation with key '{}'", batchKey);
+
+    final var brokerRequest =
+        new BrokerResumeBatchOperationRequest().setBatchOperationKey(batchKey);
+
+    return sendBrokerRequest(brokerRequest);
   }
 }
