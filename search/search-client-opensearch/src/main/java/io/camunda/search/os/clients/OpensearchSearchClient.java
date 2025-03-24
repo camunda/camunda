6d7ab9137152f52a7f980bdd11ca6073307f8a74
Mihail <122792941+mihail-ca@users.noreply.github.com>
2025-03-24 21:58:19 +0100

    BACKPORT-CONFLICT

--- search/search-client-opensearch/src/main/java/io/camunda/search/os/clients/OpensearchSearchClient.java
+++ search/search-client-opensearch/src/main/java/io/camunda/search/os/clients/OpensearchSearchClient.java
@@ -15,6 +15,14 @@ import io.camunda.search.os.transformers.search.SearchResponseTransformer;
 import io.camunda.search.transformers.SearchTransfomer;
 import io.camunda.zeebe.util.Either;
 import java.io.IOException;
+<<<<<<< HEAD
+=======
+import java.net.ConnectException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+>>>>>>> 36c1918d (fix: Align API logging levels to C8 recommendations)
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch._types.OpenSearchException;
 import org.opensearch.client.opensearch.core.SearchRequest;
@@ -43,6 +51,7 @@ public final class OpensearchSearchClient implements CamundaSearchClient {
       final var request = requestTransformer.apply(searchRequest);
       final SearchResponse<T> rawSearchResponse = client.search(request, documentClass);
       final SearchResponseTransformer<T> searchResponseTransformer = getSearchResponseTransformer();
+<<<<<<< HEAD
       final SearchQueryResponse<T> response = searchResponseTransformer.apply(rawSearchResponse);
       return Either.right(response);
     } catch (final IOException ioe) {
@@ -54,6 +63,115 @@ public final class OpensearchSearchClient implements CamundaSearchClient {
 
   private SearchTransfomer<SearchQueryRequest, SearchRequest> getSearchRequestTransformer() {
     return transformers.getTransformer(SearchQueryRequest.class);
+=======
+      return searchResponseTransformer.apply(rawSearchResponse);
+    } catch (final IOException | OpenSearchException e) {
+      logException(ErrorMessages.ERROR_FAILED_SEARCH_QUERY, e);
+      throw new CamundaSearchException(
+          ErrorMessages.ERROR_FAILED_SEARCH_QUERY, e, searchExceptionToReason(e));
+    }
+  }
+
+  @Override
+  public <T> List<T> findAll(final SearchQueryRequest searchRequest, final Class<T> documentClass) {
+    final List<T> result = new ArrayList<>();
+    String scrollId = null;
+    try {
+      final var request =
+          getSearchRequestTransformer()
+              .toSearchRequestBuilder(searchRequest)
+              .scroll(s -> s.time(SCROLL_KEEP_ALIVE_TIME))
+              .build();
+      final SearchResponse<T> rawSearchResponse = client.search(request, documentClass);
+      scrollId = rawSearchResponse.scrollId();
+      var items = rawSearchResponse.hits().hits().stream().map(Hit::source).toList();
+      result.addAll(items);
+      final int pageSize = Optional.ofNullable(searchRequest.size()).orElse(items.size());
+      while (!items.isEmpty() && pageSize == items.size()) {
+        final ScrollResponse<T> scrollResponse = scroll(scrollId, documentClass);
+        scrollId = scrollResponse.scrollId();
+        items = scrollResponse.hits().hits().stream().map(Hit::source).toList();
+        result.addAll(items);
+      }
+    } catch (final IOException | OpenSearchException e) {
+      logException(ErrorMessages.ERROR_FAILED_FIND_ALL_QUERY, e);
+      throw new CamundaSearchException(
+          ErrorMessages.ERROR_FAILED_FIND_ALL_QUERY, e, searchExceptionToReason(e));
+    } finally {
+      clearScroll(scrollId);
+    }
+    return result;
+  }
+
+  @Override
+  public <T> SearchGetResponse<T> get(
+      final SearchGetRequest getRequest, final Class<T> documentClass) {
+    try {
+      final var requestTransformer = getSearchGetRequestTransformer();
+      final var request = requestTransformer.apply(getRequest);
+      final var rawSearchResponse = client.get(request, documentClass);
+      final SearchGetResponseTransformer<T> searchResponseTransformer =
+          getSearchGetResponseTransformer();
+      return searchResponseTransformer.apply(rawSearchResponse);
+    } catch (final IOException | OpenSearchException e) {
+      logException(ErrorMessages.ERROR_FAILED_GET_REQUEST, e);
+      throw new CamundaSearchException(
+          ErrorMessages.ERROR_FAILED_GET_REQUEST, e, searchExceptionToReason(e));
+    }
+  }
+
+  @Override
+  public <T> SearchWriteResponse index(final SearchIndexRequest<T> indexRequest) {
+    try {
+      final SearchIndexRequestTransformer<T> requestTransformer =
+          getSearchIndexRequestTransformer();
+      final var request = requestTransformer.apply(indexRequest);
+      final var rawIndexResponse = client.index(request);
+      final var indexResponseTransformer = getSearchWriteResponseTransformer();
+      return indexResponseTransformer.apply(rawIndexResponse);
+    } catch (final IOException | OpenSearchException e) {
+      LOGGER.error(ErrorMessages.ERROR_FAILED_INDEX_REQUEST, e);
+      throw new CamundaSearchException(
+          ErrorMessages.ERROR_FAILED_INDEX_REQUEST, e, searchExceptionToReason(e));
+    }
+  }
+
+  @Override
+  public SearchWriteResponse delete(final SearchDeleteRequest deleteRequest) {
+    try {
+      final var requestTransformer = getSearchDeleteRequestTransformer();
+      final var request = requestTransformer.apply(deleteRequest);
+      final var rawDeleteRequest = client.delete(request);
+      final var deleteResponseTransformer = getSearchWriteResponseTransformer();
+      return deleteResponseTransformer.apply(rawDeleteRequest);
+    } catch (final IOException | OpenSearchException e) {
+      logException(ErrorMessages.ERROR_FAILED_DELETE_REQUEST, e);
+      throw new CamundaSearchException(
+          ErrorMessages.ERROR_FAILED_DELETE_REQUEST, e, searchExceptionToReason(e));
+    }
+  }
+
+  private <T> ScrollResponse<T> scroll(final String scrollId, final Class<T> documentClass)
+      throws IOException {
+    return client.scroll(
+        r -> r.scrollId(scrollId).scroll(s -> s.time(SCROLL_KEEP_ALIVE_TIME)), documentClass);
+  }
+
+  private void clearScroll(final String scrollId) {
+    if (scrollId != null) {
+      try {
+        client.clearScroll(r -> r.scrollId(scrollId));
+      } catch (final IOException | OpenSearchException e) {
+        logException("Failed to clear scroll.", e);
+      }
+    }
+  }
+
+  private SearchRequestTransformer getSearchRequestTransformer() {
+    final SearchTransfomer<SearchQueryRequest, SearchRequest> transformer =
+        transformers.getTransformer(SearchQueryRequest.class);
+    return (SearchRequestTransformer) transformer;
+>>>>>>> 36c1918d (fix: Align API logging levels to C8 recommendations)
   }
 
   private <T> SearchResponseTransformer<T> getSearchResponseTransformer() {
@@ -70,4 +188,25 @@ public final class OpensearchSearchClient implements CamundaSearchClient {
       }
     }
   }
+<<<<<<< HEAD
+=======
+
+  private static CamundaSearchException.Reason searchExceptionToReason(final Exception e) {
+    if (e instanceof ConnectException) {
+      return CamundaSearchException.Reason.CONNECTION_FAILED;
+    }
+    if (e instanceof OpenSearchException) {
+      return CamundaSearchException.Reason.SEARCH_SERVER_FAILED;
+    }
+    return CamundaSearchException.Reason.SEARCH_CLIENT_FAILED;
+  }
+
+  static void logException(String msg, Exception exception) {
+    if (exception instanceof SocketTimeoutException) {
+      LOGGER.warn("{}: {}", msg, exception.getMessage());
+    } else {
+      LOGGER.error(msg, exception);
+    }
+  }
+>>>>>>> 36c1918d (fix: Align API logging levels to C8 recommendations)
 }
