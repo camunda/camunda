ed3385c431aea9c1a502fedee13f1ea9657924be
Nathanyel Sandi <nathan.sandi@gmail.com>
2025-03-25 08:01:49 -0300

    fix: remove yamlObjectMapper

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/JacksonConfig.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/JacksonConfig.java
@@ -9,14 +9,11 @@ package io.camunda.zeebe.gateway.rest.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.camunda.zeebe.gateway.rest.serializer.LongSerializer;
 import java.util.function.Consumer;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 public class JacksonConfig {
@@ -33,20 +30,5 @@ public class JacksonConfig {
     final var builder = Jackson2ObjectMapperBuilder.json();
     gatewayRestObjectMapperCustomizer().accept(builder);
     return builder.build();
-  }
-
-  @Bean("yamlObjectMapper")
-  public ObjectMapper yamlObjectMapper() {
-    final var builder = Jackson2ObjectMapperBuilder.yaml();
-    builder.factory(new YAMLFactory());
-    gatewayRestObjectMapperCustomizer().accept(builder);
-    return builder.build();
-  }
-
-  // To avoid conflict between the default ObjectMapper and the custom one (yamlObjectMapper)
-  @Bean
-  public MappingJackson2HttpMessageConverter gatewayRestMessageConverter(
-      final @Qualifier("gatewayRestObjectMapper") ObjectMapper gatewayRestObjectMapper) {
-    return new MappingJackson2HttpMessageConverter(gatewayRestObjectMapper);
   }
 }
