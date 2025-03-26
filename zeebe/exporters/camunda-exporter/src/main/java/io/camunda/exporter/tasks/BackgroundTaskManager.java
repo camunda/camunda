camunda-cloud/zeebe

feat: graceful shutdown of archiver tasks if possible
When BackgroundTaskManager.close() is called, ReschedulingTask will not
reschedule themselves, but will have some time to finish before the
executor is forcibly closed with "shutdownNow()".

zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/tasks/BackgroundTaskManager.java
@@ -12,9 +12,11 @@ import io.camunda.exporter.tasks.batchoperations.BatchOperationUpdateRepository;
 import io.camunda.exporter.tasks.incident.IncidentUpdateRepository;
 import io.camunda.zeebe.util.CloseableSilently;
 import io.camunda.zeebe.util.VisibleForTesting;
+import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.WillCloseWhenClosed;
 import org.agrona.CloseHelper;
 import org.slf4j.Logger;
@@ -26,7 +28,8 @@ public final class BackgroundTaskManager implements CloseableSilently {
   private final BatchOperationUpdateRepository batchOperationUpdateRepository;
   private final Logger logger;
   private final ScheduledThreadPoolExecutor executor;
-  private final List<Runnable> tasks;
+  private final List<RunnableTask> tasks;
+  private final Duration closeTimeout;
 
   private int submittedTasks = 0;
 
@@ -38,7 +41,8 @@ public final class BackgroundTaskManager implements CloseableSilently {
       final @WillCloseWhenClosed BatchOperationUpdateRepository batchOperationUpdateRepository,
       final Logger logger,
       final @WillCloseWhenClosed ScheduledThreadPoolExecutor executor,
-      final List<Runnable> tasks) {
+      final List<RunnableTask> tasks,
+      final Duration closeTimeout) {
     this.partitionId = partitionId;
     this.archiverRepository =
         Objects.requireNonNull(archiverRepository, "must specify an archiver repository");
@@ -50,18 +54,23 @@ public final class BackgroundTaskManager implements CloseableSilently {
     this.logger = Objects.requireNonNull(logger, "must specify a logger");
     this.executor = Objects.requireNonNull(executor, "must specify an executor");
     this.tasks = Objects.requireNonNull(tasks, "must specify tasks");
+    this.closeTimeout = closeTimeout;
   }
 
   @Override
   public void close() {
-    // Close executor first before anything else; this will ensure any callbacks are not triggered
-    // in case we close any underlying resource (e.g. repository) and would want to perform
-    // unnecessary error handling in any of these callbacks
-    //
-    // avoid calling executor.close, which will await 1d (!) until termination
-    // we also don't need to wait for the jobs to fully finish, as we should be able to handle
-    // partial jobs (e.g. node crash/restart)
-    executor.shutdownNow();
+    // Stop all running tasks gracefully, so they don't reschedule themselves, but they can still
+    // finish the current run
+    CloseHelper.closeAll(tasks);
+    executor.shutdown();
+    try {
+      executor.awaitTermination(closeTimeout.toMillis(), TimeUnit.MILLISECONDS);
+    } catch (final InterruptedException e) {
+    }
+    if (!executor.isTerminated()) {
+      executor.shutdownNow();
+    }
+
     CloseHelper.closeAll(
         error -> logger.warn("Failed to close resource for partition {}", partitionId, error),
         archiverRepository,

zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/BackgroundTaskManagerTest.java
@@ -13,10 +13,12 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import io.camunda.exporter.tasks.archiver.ArchiverRepository.NoopArchiverRepository;
 import io.camunda.exporter.tasks.batchoperations.BatchOperationUpdateRepository.NoopBatchOperationUpdateRepository;
 import io.camunda.exporter.tasks.incident.IncidentUpdateRepository.NoopIncidentUpdateRepository;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import org.agrona.collections.MutableInteger;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -39,7 +41,8 @@ final class BackgroundTaskManagerTest {
             LoggerFactory.getLogger(BackgroundTaskManagerTest.class),
             executor,
             // return unfinished futures to have a deterministic count of submitted tasks
-            List.of(CompletableFuture::new, CompletableFuture::new));
+            List.of(CompletableFuture::new, CompletableFuture::new),
+            Duration.ofMillis(100));
 
     @Test
     void shouldNotResubmitTasksOnStart() {
@@ -94,6 +97,27 @@ final class BackgroundTaskManagerTest {
         new CloseableIncidentRepository();
     private final CloseableBatchOperationUpdateRepository batchOperationUpdateRepository =
         new CloseableBatchOperationUpdateRepository();
+    private volatile boolean taskClosed = false;
+    private volatile boolean keepRunningEvenIfClosed = false;
+    private final RunnableTask task =
+        new RunnableTask() {
+          @Override
+          public void run() {
+            try {
+              while (!taskClosed || keepRunningEvenIfClosed) {
+                Thread.sleep(1);
+              }
+            } catch (final InterruptedException e) {
+
+            }
+          }
+
+          @Override
+          public void close() {
+            taskClosed = true;
+          }
+        };
+    private final List<RunnableTask> tasks = List.of(task);
     private final BackgroundTaskManager taskManager =
         new BackgroundTaskManager(
             1,
@@ -102,7 +126,18 @@ final class BackgroundTaskManagerTest {
             batchOperationUpdateRepository,
             LoggerFactory.getLogger(BackgroundTaskManagerTest.class),
             executor,
-            List.of());
+            tasks,
+            Duration.ofMillis(100));
+
+    public CloseTest() {
+      taskManager.start();
+    }
+
+    @Test
+    void shouldCloseTasksOnClose() {
+      taskManager.close();
+      assertThat(taskClosed).isTrue();
+    }
 
     @Test
     void shouldCloseExecutorOnClose() {
@@ -110,7 +145,20 @@ final class BackgroundTaskManagerTest {
       taskManager.close();
 
       // then
-      assertThat(executor.isTerminated()).isTrue();
+      Awaitility.await("Executor is terminated")
+          .untilAsserted(() -> assertThat(executor.isTerminated()).isTrue());
+    }
+
+    @Test
+    void shouldCloseExecutorOnCloseWhenTasksDoNotTerminate() {
+      // given
+      keepRunningEvenIfClosed = true;
+      // when
+      taskManager.close();
+
+      // then
+      Awaitility.await("Executor is terminated")
+          .untilAsserted(() -> assertThat(executor.isTerminated()).isTrue());
     }
 
     @Test

camunda/camunda

zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/tasks/BackgroundTaskManager.java
@@ -12,9 +12,11 @@ import io.camunda.exporter.tasks.batchoperations.BatchOperationUpdateRepository;
 import io.camunda.exporter.tasks.incident.IncidentUpdateRepository;
 import io.camunda.zeebe.util.CloseableSilently;
 import io.camunda.zeebe.util.VisibleForTesting;
+import java.time.Duration;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.WillCloseWhenClosed;
 import org.agrona.CloseHelper;
 import org.slf4j.Logger;
@@ -26,7 +28,8 @@ public final class BackgroundTaskManager implements CloseableSilently {
   private final BatchOperationUpdateRepository batchOperationUpdateRepository;
   private final Logger logger;
   private final ScheduledThreadPoolExecutor executor;
-  private final List<Runnable> tasks;
+  private final List<RunnableTask> tasks;
+  private final Duration closeTimeout;
 
   private int submittedTasks = 0;
 
@@ -38,7 +41,8 @@ public final class BackgroundTaskManager implements CloseableSilently {
       final @WillCloseWhenClosed BatchOperationUpdateRepository batchOperationUpdateRepository,
       final Logger logger,
       final @WillCloseWhenClosed ScheduledThreadPoolExecutor executor,
-      final List<Runnable> tasks) {
+      final List<RunnableTask> tasks,
+      final Duration closeTimeout) {
     this.partitionId = partitionId;
     this.archiverRepository =
         Objects.requireNonNull(archiverRepository, "must specify an archiver repository");
@@ -50,18 +54,24 @@ public final class BackgroundTaskManager implements CloseableSilently {
     this.logger = Objects.requireNonNull(logger, "must specify a logger");
     this.executor = Objects.requireNonNull(executor, "must specify an executor");
     this.tasks = Objects.requireNonNull(tasks, "must specify tasks");
+    this.closeTimeout = closeTimeout;
   }
 
   @Override
   public void close() {
-    // Close executor first before anything else; this will ensure any callbacks are not triggered
-    // in case we close any underlying resource (e.g. repository) and would want to perform
-    // unnecessary error handling in any of these callbacks
-    //
-    // avoid calling executor.close, which will await 1d (!) until termination
-    // we also don't need to wait for the jobs to fully finish, as we should be able to handle
-    // partial jobs (e.g. node crash/restart)
-    executor.shutdownNow();
+    // Stop all running tasks gracefully, so they don't reschedule themselves, but they can still
+    // finish the current run
+    CloseHelper.closeAll(tasks);
+    executor.shutdown();
+    try {
+      executor.awaitTermination(closeTimeout.toMillis(), TimeUnit.MILLISECONDS);
+    } catch (final InterruptedException ignored) {
+      // interrupted, will close the executor later
+    }
+    if (!executor.isTerminated()) {
+      executor.shutdownNow();
+    }
+
     CloseHelper.closeAll(
         error -> logger.warn("Failed to close resource for partition {}", partitionId, error),
         archiverRepository,

zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/BackgroundTaskManagerTest.java
@@ -13,10 +13,12 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import io.camunda.exporter.tasks.archiver.ArchiverRepository.NoopArchiverRepository;
 import io.camunda.exporter.tasks.batchoperations.BatchOperationUpdateRepository.NoopBatchOperationUpdateRepository;
 import io.camunda.exporter.tasks.incident.IncidentUpdateRepository.NoopIncidentUpdateRepository;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import org.agrona.collections.MutableInteger;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -39,7 +41,8 @@ final class BackgroundTaskManagerTest {
             LoggerFactory.getLogger(BackgroundTaskManagerTest.class),
             executor,
             // return unfinished futures to have a deterministic count of submitted tasks
-            List.of(CompletableFuture::new, CompletableFuture::new));
+            List.of(CompletableFuture::new, CompletableFuture::new),
+            Duration.ofMillis(100));
 
     @Test
     void shouldNotResubmitTasksOnStart() {
@@ -94,6 +97,27 @@ final class BackgroundTaskManagerTest {
         new CloseableIncidentRepository();
     private final CloseableBatchOperationUpdateRepository batchOperationUpdateRepository =
         new CloseableBatchOperationUpdateRepository();
+    private volatile boolean taskClosed = false;
+    private volatile boolean keepRunningEvenIfClosed = false;
+    private final RunnableTask task =
+        new RunnableTask() {
+          @Override
+          public void run() {
+            while (!taskClosed || keepRunningEvenIfClosed) {
+              try {
+                Thread.sleep(1);
+              } catch (final InterruptedException ignored) {
+                break;
+              }
+            }
+          }
+
+          @Override
+          public void close() {
+            taskClosed = true;
+          }
+        };
+    private final List<RunnableTask> tasks = List.of(task);
     private final BackgroundTaskManager taskManager =
         new BackgroundTaskManager(
             1,
@@ -102,7 +126,18 @@ final class BackgroundTaskManagerTest {
             batchOperationUpdateRepository,
             LoggerFactory.getLogger(BackgroundTaskManagerTest.class),
             executor,
-            List.of());
+            tasks,
+            Duration.ofMillis(100));
+
+    public CloseTest() {
+      taskManager.start();
+    }
+
+    @Test
+    void shouldCloseTasksOnClose() {
+      taskManager.close();
+      assertThat(taskClosed).isTrue();
+    }
 
     @Test
     void shouldCloseExecutorOnClose() {
@@ -110,7 +145,20 @@ final class BackgroundTaskManagerTest {
       taskManager.close();
 
       // then
-      assertThat(executor.isTerminated()).isTrue();
+      Awaitility.await("Executor is terminated")
+          .untilAsserted(() -> assertThat(executor.isTerminated()).isTrue());
+    }
+
+    @Test
+    void shouldCloseExecutorOnCloseWhenTasksDoNotTerminate() {
+      // given
+      keepRunningEvenIfClosed = true;
+      // when
+      taskManager.close();
+
+      // then
+      Awaitility.await("Executor is terminated")
+          .untilAsserted(() -> assertThat(executor.isTerminated()).isTrue());
     }
 
     @Test