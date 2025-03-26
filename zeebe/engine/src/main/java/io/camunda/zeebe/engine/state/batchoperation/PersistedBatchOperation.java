a88433d4e3aa3694436fe96ce7fe5a33ca35b2b0
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-03-26 10:55:29 +0100

    feat: extend PersistedBatchOperationChunk to query next chunk block keys #29697
    Co-authored-by: Christian Thiel <christian.thiel@holisticon.de>

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/batchoperation/PersistedBatchOperation.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/batchoperation/PersistedBatchOperation.java
@@ -9,11 +9,14 @@ package io.camunda.zeebe.engine.state.batchoperation;
 
 import io.camunda.zeebe.db.DbValue;
 import io.camunda.zeebe.msgpack.UnpackedObject;
+import io.camunda.zeebe.msgpack.property.ArrayProperty;
 import io.camunda.zeebe.msgpack.property.BinaryProperty;
 import io.camunda.zeebe.msgpack.property.EnumProperty;
 import io.camunda.zeebe.msgpack.property.LongProperty;
+import io.camunda.zeebe.msgpack.value.LongValue;
 import io.camunda.zeebe.protocol.impl.encoding.MsgPackConverter;
 import io.camunda.zeebe.protocol.record.value.BatchOperationType;
+import java.util.Comparator;
 import org.agrona.DirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 
@@ -26,13 +29,16 @@ public class PersistedBatchOperation extends UnpackedObject implements DbValue {
   private final EnumProperty<BatchOperationStatus> statusProp =
       new EnumProperty<>("status", BatchOperationStatus.class);
   private final BinaryProperty entityFilterProp = new BinaryProperty("entityFilter");
+  private final ArrayProperty<LongValue> chunkKeysProp =
+      new ArrayProperty<>("chunkKeys", LongValue::new);
 
   public PersistedBatchOperation() {
-    super(4);
+    super(5);
     declareProperty(keyProp)
         .declareProperty(batchOperationTypeProp)
         .declareProperty(statusProp)
-        .declareProperty(entityFilterProp);
+        .declareProperty(entityFilterProp)
+        .declareProperty(chunkKeysProp);
   }
 
   public long getKey() {
@@ -70,6 +76,44 @@ public class PersistedBatchOperation extends UnpackedObject implements DbValue {
   public PersistedBatchOperation setEntityFilter(final DirectBuffer filter) {
     entityFilterProp.setValue(new UnsafeBuffer(filter));
     return this;
+  }
+
+  public <T> T getEntityFilter(final Class<T> clazz) {
+    return MsgPackConverter.convertToObject(entityFilterProp.getValue(), clazz);
+  }
+
+  public long nextBlockKey() {
+    final var nextKey = getMaxBlockKey() + 1;
+    chunkKeysProp.add().setValue(nextKey);
+
+    return nextKey;
+  }
+
+  public PersistedBatchOperation removeBlockKey(final Long blockKey) {
+    final var newKeys =
+        chunkKeysProp.stream().map(LongValue::getValue).filter(k -> !k.equals(blockKey)).toList();
+
+    chunkKeysProp.reset();
+
+    for (final var key : newKeys) {
+      chunkKeysProp.add().setValue(key);
+    }
+
+    return this;
+  }
+
+  public long getMinBlockKey() {
+    return chunkKeysProp.stream()
+        .min(Comparator.comparing(LongValue::getValue))
+        .map(LongValue::getValue)
+        .orElse(-1L);
+  }
+
+  public long getMaxBlockKey() {
+    return chunkKeysProp.stream()
+        .max(Comparator.comparing(LongValue::getValue))
+        .map(LongValue::getValue)
+        .orElse(-1L);
   }
 
   public enum BatchOperationStatus {
