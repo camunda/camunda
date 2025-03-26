8d3ab382f74bc9d84d13c89f0eb54034268d1c6b
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-03-26 11:05:57 +0100

    feat: add new scheduler for batch operations to query the second db async
    * This also appends the keys as chunk
    
    Co-authored-by: Christian Thiel <christian.thiel@holisticon.de>

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationExecutionScheduler.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.batchoperation;
+
+import com.google.common.collect.Lists;
+import io.camunda.search.clients.SearchClientsProxy;
+import io.camunda.search.entities.ProcessInstanceEntity;
+import io.camunda.search.filter.ProcessInstanceFilter;
+import io.camunda.search.page.SearchQueryPageBuilders;
+import io.camunda.search.query.SearchQueryBuilders;
+import io.camunda.zeebe.engine.state.batchoperation.PersistedBatchOperation;
+import io.camunda.zeebe.engine.state.immutable.BatchOperationState;
+import io.camunda.zeebe.engine.state.immutable.ScheduledTaskState;
+import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationChunkRecord;
+import io.camunda.zeebe.protocol.record.intent.BatchOperationChunkIntent;
+import io.camunda.zeebe.stream.api.ReadonlyStreamProcessorContext;
+import io.camunda.zeebe.stream.api.StreamProcessorLifecycleAware;
+import io.camunda.zeebe.stream.api.scheduling.AsyncTaskGroup;
+import io.camunda.zeebe.stream.api.scheduling.TaskResult;
+import io.camunda.zeebe.stream.api.scheduling.TaskResultBuilder;
+import io.camunda.zeebe.stream.api.state.KeyGenerator;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BatchOperationExecutionScheduler implements StreamProcessorLifecycleAware {
+
+  private static final Logger LOG = LoggerFactory.getLogger(BatchOperationExecutionScheduler.class);
+  private static final int BATCH_SIZE = 10;
+  private final Duration pollingInterval;
+  private final KeyGenerator keyGenerator;
+
+  private final BatchOperationState batchOperationState;
+  private ReadonlyStreamProcessorContext processingContext;
+  private final SearchClientsProxy queryService;
+
+  /** Marks if this scheduler is currently executing or not. */
+  private final AtomicBoolean executing = new AtomicBoolean(false);
+
+  public BatchOperationExecutionScheduler(
+      final Supplier<ScheduledTaskState> scheduledTaskStateFactory,
+      final SearchClientsProxy searchClientsProxy,
+      final KeyGenerator keyGenerator,
+      final Duration pollingInterval) {
+    batchOperationState = scheduledTaskStateFactory.get().getBatchOperationState();
+    this.keyGenerator = keyGenerator;
+    queryService = searchClientsProxy;
+    this.pollingInterval = pollingInterval;
+  }
+
+  @Override
+  public void onRecovered(final ReadonlyStreamProcessorContext context) {
+    processingContext = context;
+    scheduleExecution();
+  }
+
+  @Override
+  public void onResumed() {
+    scheduleExecution();
+  }
+
+  private void scheduleExecution() {
+    if (!executing.get()) {
+      processingContext
+          .getScheduleService()
+          .runDelayedAsync(pollingInterval, this::execute, AsyncTaskGroup.BATCH_OPERATIONS);
+    } else {
+      LOG.warn("Execution is already in progress, skipping scheduling.");
+    }
+  }
+
+  private TaskResult execute(final TaskResultBuilder taskResultBuilder) {
+    try {
+      LOG.trace("Looking for pending batch operations to execute (scheduled).");
+      executing.set(true);
+      batchOperationState.foreachPendingBatchOperation(
+          bo -> executeBatchOperation(bo, taskResultBuilder));
+      return taskResultBuilder.build();
+    } finally {
+      executing.set(false);
+      scheduleExecution();
+    }
+  }
+
+  private void executeBatchOperation(
+      final PersistedBatchOperation batchOperation, final TaskResultBuilder taskResultBuilder) {
+    final var keys = queryAllKeys(batchOperation);
+
+    Lists.partition(new ArrayList<>(keys), BATCH_SIZE)
+        .forEach(partition -> appendChunk(batchOperation, taskResultBuilder, partition));
+  }
+
+  private void appendChunk(
+      final PersistedBatchOperation batchOperation,
+      final TaskResultBuilder taskResultBuilder,
+      final List<Long> keys) {
+    final var chunkKey = keyGenerator.nextKey();
+    final var command = new BatchOperationChunkRecord();
+    command.setBatchOperationKey(batchOperation.getKey());
+    command.setItemKeys(keys);
+    command.setChunkKey(chunkKey);
+
+    LOG.debug(
+        "Appending batch operation {} subbatch with key {}", batchOperation.getKey(), chunkKey);
+    taskResultBuilder.appendCommandRecord(chunkKey, BatchOperationChunkIntent.CREATE, command);
+  }
+
+  private List<Long> queryAllKeys(final PersistedBatchOperation batchOperation) {
+    return switch (batchOperation.getBatchOperationType()) {
+      case PROCESS_CANCELLATION -> queryAllProcessInstanceKeys(batchOperation);
+      default ->
+          throw new IllegalArgumentException(
+              "Unexpected batch operation type: " + batchOperation.getBatchOperationType());
+    };
+  }
+
+  private List<Long> queryAllProcessInstanceKeys(final PersistedBatchOperation batchOperation) {
+    final ProcessInstanceFilter filter =
+        batchOperation.getEntityFilter(ProcessInstanceFilter.class);
+
+    final var itemKeys = new ArrayList<Long>();
+
+    Object[] searchValues = null;
+    final int batchSize = 400000;
+    while (true) {
+
+      final var page =
+          SearchQueryPageBuilders.page().size(batchSize).searchAfter(searchValues).build();
+      final var query =
+          SearchQueryBuilders.processInstanceSearchQuery()
+              .filter(filter)
+              .page(page)
+              .resultConfig(c -> c.onlyKey(true))
+              .build();
+      final var result = queryService.searchProcessInstances(query);
+      itemKeys.addAll(
+          result.items().stream()
+              .map(ProcessInstanceEntity::processInstanceKey)
+              .collect(Collectors.toSet()));
+      searchValues = result.lastSortValues();
+
+      if (itemKeys.size() >= result.total() || result.items().isEmpty()) {
+        break;
+      }
+    }
+
+    return itemKeys;
+  }
+}
