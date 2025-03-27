8a38d8d176bf12528e30e90ee680537b21e9d64d
vitorwtavares <vitor.tavares@camunda.com>
2025-03-27 14:59:18 -0300

    fix: rename env variable to match other web apps

--- identity/client/src/configuration/env.ts
+++ identity/client/src/configuration/env.ts
@@ -6,10 +6,10 @@
  */
 
 interface AppWindow {
-  env: Record<string, string>;
+  clientConfig: Record<string, string>;
 }
 
-const { env } = window as unknown as AppWindow;
+const { clientConfig } = window as unknown as AppWindow;
 
 export type GetEnv<R = string | undefined> = (
   key: string,
@@ -22,8 +22,8 @@ const getEnv = <R>(
   parser: (value: string) => R,
 ): R => {
   const viteKey = `VITE_${key}`;
-  if (env && env[viteKey] !== undefined) {
-    return parser(env[viteKey]);
+  if (clientConfig && clientConfig[viteKey] !== undefined) {
+    return parser(clientConfig[viteKey]);
   }
   if (import.meta.env[viteKey] !== undefined) {
     return parser(import.meta.env[viteKey] as string);
