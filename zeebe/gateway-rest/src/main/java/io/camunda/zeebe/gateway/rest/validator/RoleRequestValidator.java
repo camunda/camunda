0a46da3145def512850aed72e3c3230d3431a352
remco.westerhoud <remcowesterhoud@msn.com>
2025-04-01 14:04:50 +0200

    feat: validate role id
    We need to ensure that role id is set and it matches the pattern we use for ids.

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/validator/RoleRequestValidator.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/validator/RoleRequestValidator.java
@@ -8,30 +8,47 @@
 package io.camunda.zeebe.gateway.rest.validator;
 
 import static io.camunda.zeebe.gateway.rest.validator.ErrorMessages.ERROR_MESSAGE_EMPTY_ATTRIBUTE;
+import static io.camunda.zeebe.gateway.rest.validator.ErrorMessages.ERROR_MESSAGE_ILLEGAL_CHARACTER;
+import static io.camunda.zeebe.gateway.rest.validator.ErrorMessages.ERROR_MESSAGE_TOO_MANY_CHARACTERS;
+import static io.camunda.zeebe.gateway.rest.validator.IdentifierPatterns.ID_PATTERN;
+import static io.camunda.zeebe.gateway.rest.validator.IdentifierPatterns.ID_REGEX;
+import static io.camunda.zeebe.gateway.rest.validator.IdentifierPatterns.MAX_LENGTH;
 import static io.camunda.zeebe.gateway.rest.validator.RequestValidator.validate;
 
 import io.camunda.zeebe.gateway.protocol.rest.RoleCreateRequest;
 import io.camunda.zeebe.gateway.protocol.rest.RoleUpdateRequest;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.http.ProblemDetail;
 
 public final class RoleRequestValidator {
   private RoleRequestValidator() {}
 
-  public static Optional<ProblemDetail> validateRoleName(final String name) {
-    return validate(
-        violations -> {
-          if (name == null || name.isBlank()) {
-            violations.add(ERROR_MESSAGE_EMPTY_ATTRIBUTE.formatted("name"));
-          }
-        });
+  public static void validateRoleName(final String name, final List<String> violations) {
+    if (name == null || name.isBlank()) {
+      violations.add(ERROR_MESSAGE_EMPTY_ATTRIBUTE.formatted("name"));
+    }
+  }
+
+  public static void validateRoleId(final String id, final List<String> violations) {
+    if (id == null || id.isBlank()) {
+      violations.add(ERROR_MESSAGE_EMPTY_ATTRIBUTE.formatted("roleId"));
+    } else if (id.length() > MAX_LENGTH) {
+      violations.add(ERROR_MESSAGE_TOO_MANY_CHARACTERS.formatted("roleId", MAX_LENGTH));
+    } else if (!ID_PATTERN.matcher(id).matches()) {
+      violations.add(ERROR_MESSAGE_ILLEGAL_CHARACTER.formatted("roleId", ID_REGEX));
+    }
   }
 
   public static Optional<ProblemDetail> validateUpdateRequest(final RoleUpdateRequest request) {
-    return validateRoleName(request.getChangeset().getName());
+    return validate(violations -> validateRoleName(request.getChangeset().getName(), violations));
   }
 
   public static Optional<ProblemDetail> validateCreateRequest(final RoleCreateRequest request) {
-    return validateRoleName(request.getName());
+    return validate(
+        violations -> {
+          validateRoleId(request.getRoleId(), violations);
+          validateRoleName(request.getName(), violations);
+        });
   }
 }
