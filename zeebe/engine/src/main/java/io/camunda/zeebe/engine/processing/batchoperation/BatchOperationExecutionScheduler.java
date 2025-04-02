18551c5836888baee0da904de26416d5327b6cb5
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-04-02 10:45:49 +0200

    feat: append started / failed event in BatchOperationExecutionScheduler
    * Started will be appended before start working on the chunks
    * Currently the failed event will be appended when a EntityFilterDeserializeException occurs

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationExecutionScheduler.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationExecutionScheduler.java
@@ -12,13 +12,16 @@ import io.camunda.search.entities.ProcessInstanceEntity;
 import io.camunda.search.filter.ProcessInstanceFilter;
 import io.camunda.search.page.SearchQueryPageBuilders;
 import io.camunda.search.query.SearchQueryBuilders;
+import io.camunda.zeebe.engine.state.batchoperation.EntityFilterDeserializeException;
 import io.camunda.zeebe.engine.state.batchoperation.PersistedBatchOperation;
 import io.camunda.zeebe.engine.state.immutable.BatchOperationState;
 import io.camunda.zeebe.engine.state.immutable.ScheduledTaskState;
 import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationChunkRecord;
+import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationCreationRecord;
 import io.camunda.zeebe.protocol.impl.record.value.batchoperation.BatchOperationExecutionRecord;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationChunkIntent;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationExecutionIntent;
+import io.camunda.zeebe.protocol.record.intent.BatchOperationIntent;
 import io.camunda.zeebe.stream.api.ReadonlyStreamProcessorContext;
 import io.camunda.zeebe.stream.api.StreamProcessorLifecycleAware;
 import io.camunda.zeebe.stream.api.scheduling.AsyncTaskGroup;
@@ -93,14 +96,53 @@ public class BatchOperationExecutionScheduler implements StreamProcessorLifecycl
 
   private void executeBatchOperation(
       final PersistedBatchOperation batchOperation, final TaskResultBuilder taskResultBuilder) {
-    final var keys = queryAllKeys(batchOperation);
-    for (int i = 0; i < keys.size(); i += CHUNK_SIZE_IN_RECORD) {
-      final Set<Long> chunkKeys =
-          keys.stream().skip(i).limit(CHUNK_SIZE_IN_RECORD).collect(Collectors.toSet());
-      appendChunk(batchOperation.getKey(), taskResultBuilder, chunkKeys);
+    // First fire a start event
+    appendStartedCommand(taskResultBuilder, batchOperation);
+
+    try {
+      // Then append the chunks
+      final var keys = queryAllKeys(batchOperation);
+      for (int i = 0; i < keys.size(); i += CHUNK_SIZE_IN_RECORD) {
+        final Set<Long> chunkKeys =
+            keys.stream().skip(i).limit(CHUNK_SIZE_IN_RECORD).collect(Collectors.toSet());
+        appendChunk(batchOperation.getKey(), taskResultBuilder, chunkKeys);
+      }
+    } catch (final EntityFilterDeserializeException e) {
+      LOG.error(
+          "Failed to deserialize entity filter, batch operation with key {} will be removed from queue",
+          batchOperation.getKey(),
+          e);
+      appendFailedCommand(taskResultBuilder, batchOperation);
+    } catch (final IllegalArgumentException e) {
+      LOG.error(
+          "Failed to execute batch operation with key {} because operation type '{}' is not known",
+          batchOperation.getKey(),
+          batchOperation.getBatchOperationType(),
+          e);
+      appendFailedCommand(taskResultBuilder, batchOperation);
     }
 
     appendExecution(batchOperation.getKey(), taskResultBuilder);
+  }
+
+  private void appendStartedCommand(
+      final TaskResultBuilder taskResultBuilder, final PersistedBatchOperation batchOperation) {
+    final var batchOperationKey = batchOperation.getKey();
+    final var command = new BatchOperationCreationRecord();
+    command.setBatchOperationKey(batchOperationKey);
+    command.setBatchOperationType(batchOperation.getBatchOperationType());
+    LOG.debug("Appending batch operation {} started event", batchOperationKey);
+    taskResultBuilder.appendCommandRecord(batchOperationKey, BatchOperationIntent.START, command);
+  }
+
+  private void appendFailedCommand(
+      final TaskResultBuilder taskResultBuilder, final PersistedBatchOperation batchOperation) {
+    final var batchOperationKey = batchOperation.getKey();
+    final var command = new BatchOperationCreationRecord();
+    command.setBatchOperationKey(batchOperationKey);
+    command.setBatchOperationType(batchOperation.getBatchOperationType());
+    LOG.debug("Appending batch operation {} failed event", batchOperationKey);
+    taskResultBuilder.appendCommandRecord(batchOperationKey, BatchOperationIntent.FAIL, command);
   }
 
   private void appendChunk(
@@ -127,7 +169,8 @@ public class BatchOperationExecutionScheduler implements StreamProcessorLifecycl
         batchOperationKey, BatchOperationExecutionIntent.EXECUTE, command, batchOperationKey);
   }
 
-  private Set<Long> queryAllKeys(final PersistedBatchOperation batchOperation) {
+  private Set<Long> queryAllKeys(final PersistedBatchOperation batchOperation)
+      throws EntityFilterDeserializeException, IllegalArgumentException {
     return switch (batchOperation.getBatchOperationType()) {
       case PROCESS_CANCELLATION -> queryAllProcessInstanceKeys(batchOperation);
       default ->
@@ -136,7 +179,8 @@ public class BatchOperationExecutionScheduler implements StreamProcessorLifecycl
     };
   }
 
-  private Set<Long> queryAllProcessInstanceKeys(final PersistedBatchOperation batchOperation) {
+  private Set<Long> queryAllProcessInstanceKeys(final PersistedBatchOperation batchOperation)
+      throws EntityFilterDeserializeException {
     final ProcessInstanceFilter filter =
         batchOperation.getEntityFilter(ProcessInstanceFilter.class);
 
