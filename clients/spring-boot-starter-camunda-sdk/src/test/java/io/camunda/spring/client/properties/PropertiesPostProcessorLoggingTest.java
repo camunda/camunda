fd87cb1a7d72fcd2da2ef027922d3b36e73e5c42
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-04-03 11:58:00 +0200

    fix: remove unused import

--- clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/properties/PropertiesPostProcessorLoggingTest.java
+++ clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/properties/PropertiesPostProcessorLoggingTest.java
@@ -17,7 +17,6 @@ package io.camunda.spring.client.properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
