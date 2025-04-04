da558cd009bd34883bc0a4937df0198fcac6d313
Nicola Puppa <nicola.puppa@camunda.com>
2025-04-04 16:12:31 +0200

    test: add engine test to verify group removal from tenant

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/tenant/RemoveEntityTenantTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/tenant/RemoveEntityTenantTest.java
@@ -145,4 +145,111 @@ public class RemoveEntityTenantTest {
             "Expected to remove user with ID '%s' from tenant with ID '%s', but the user is not assigned to this tenant."
                 .formatted(username, tenantId));
   }
+
+  @Test
+  public void shouldRemoveGroupFromTenant() {
+    final var tenantId = UUID.randomUUID().toString();
+    engine
+        .tenant()
+        .newTenant()
+        .withTenantId(tenantId)
+        .withName("name")
+        .create()
+        .getValue()
+        .getTenantKey();
+    final var name = "foo";
+    final var groupId = "123";
+    engine.group().newGroup(name).withGroupId(groupId).create();
+    engine
+        .tenant()
+        .addEntity(tenantId)
+        .withEntityId(groupId)
+        .withEntityType(EntityType.GROUP)
+        .add();
+    final var removedEntity =
+        engine
+            .tenant()
+            .removeEntity(tenantId)
+            .withEntityId(groupId)
+            .withEntityType(EntityType.GROUP)
+            .remove()
+            .getValue();
+
+    Assertions.assertThat(removedEntity)
+        .isNotNull()
+        .hasFieldOrPropertyWithValue("tenantId", tenantId)
+        .hasFieldOrPropertyWithValue("entityId", groupId)
+        .hasFieldOrPropertyWithValue("entityType", EntityType.GROUP);
+  }
+
+  @Test
+  public void shouldRejectIfTenantIsNotPresentGroupRemoval() {
+    final var notPresentTenantId = UUID.randomUUID().toString();
+    final var notPresentUpdateRecord =
+        engine.tenant().removeEntity(notPresentTenantId).expectRejection().remove();
+
+    assertThat(notPresentUpdateRecord)
+        .hasRejectionType(RejectionType.NOT_FOUND)
+        .hasRejectionReason(
+            "Expected to remove entity from tenant '"
+                + notPresentTenantId
+                + "', but no tenant with this id exists.");
+  }
+
+  @Test
+  public void shouldRejectIfEntityIsNotPresentGroupRemoval() {
+    // given
+    final var tenantId = UUID.randomUUID().toString();
+    final var name = UUID.randomUUID().toString();
+    final var groupId = "123";
+    final var tenantRecord = engine.tenant().newTenant().withTenantId(tenantId).create();
+    engine.group().newGroup(name).withGroupId(groupId).create();
+
+    // when
+    final var createdTenant = tenantRecord.getValue();
+    final var notPresentUpdateRecord =
+        engine
+            .tenant()
+            .removeEntity(tenantId)
+            .withEntityId(groupId)
+            .withEntityType(EntityType.GROUP)
+            .expectRejection()
+            .remove();
+
+    Assertions.assertThat(createdTenant)
+        .isNotNull()
+        .hasFieldOrPropertyWithValue("tenantId", tenantId);
+
+    assertThat(notPresentUpdateRecord)
+        .hasRejectionType(RejectionType.NOT_FOUND)
+        .hasRejectionReason(
+            "Expected to remove group with ID '%s' from tenant with ID '%s', but the group is not assigned to this tenant."
+                .formatted(groupId, tenantId));
+  }
+
+  @Test
+  public void shouldRejectIfGroupIsNotAssigned() {
+    // given
+    final var tenantId = UUID.randomUUID().toString();
+    engine.tenant().newTenant().withTenantId(tenantId).create();
+
+    // when
+    final var name = "foo";
+    final var groupId = "123";
+    engine.group().newGroup(name).withGroupId(groupId).create();
+    final var notAssignedUpdateRecord =
+        engine
+            .tenant()
+            .removeEntity(tenantId)
+            .withEntityId(groupId)
+            .withEntityType(EntityType.GROUP)
+            .expectRejection()
+            .remove();
+
+    assertThat(notAssignedUpdateRecord)
+        .hasRejectionType(RejectionType.NOT_FOUND)
+        .hasRejectionReason(
+            "Expected to remove group with ID '%s' from tenant with ID '%s', but the group is not assigned to this tenant."
+                .formatted(groupId, tenantId));
+  }
 }
