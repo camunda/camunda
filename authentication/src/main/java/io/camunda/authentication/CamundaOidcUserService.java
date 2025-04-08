e53da65895debe4ed82ea400a41aa676d9834391
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-04-08 13:56:48 +0200

    feat: resolve mapping authorizations Search authorization layer

--- authentication/src/main/java/io/camunda/authentication/CamundaOidcUserService.java
+++ authentication/src/main/java/io/camunda/authentication/CamundaOidcUserService.java
@@ -7,26 +7,9 @@
  */
 package io.camunda.authentication;
 
-import io.camunda.authentication.entity.AuthenticationContext;
 import io.camunda.authentication.entity.CamundaOidcUser;
-import io.camunda.search.entities.GroupEntity;
-import io.camunda.search.entities.MappingEntity;
-import io.camunda.security.configuration.SecurityConfiguration;
 import io.camunda.security.entity.AuthenticationMethod;
-import io.camunda.service.AuthorizationServices;
-import io.camunda.service.GroupServices;
-import io.camunda.service.MappingServices;
-import io.camunda.service.RoleServices;
-import io.camunda.service.TenantServices;
-import io.camunda.service.TenantServices.TenantDTO;
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -36,73 +19,17 @@ import org.springframework.stereotype.Service;
 @Service
 @ConditionalOnAuthenticationMethod(AuthenticationMethod.OIDC)
 public class CamundaOidcUserService extends OidcUserService {
-  private static final Logger LOG = LoggerFactory.getLogger(CamundaOidcUserService.class);
-  private final MappingServices mappingServices;
-  private final TenantServices tenantServices;
-  private final RoleServices roleServices;
-  private final GroupServices groupServices;
-  private final AuthorizationServices authorizationServices;
-  private final String usernameClaim;
 
-  public CamundaOidcUserService(
-      final MappingServices mappingServices,
-      final TenantServices tenantServices,
-      final RoleServices roleServices,
-      final GroupServices groupServices,
-      final AuthorizationServices authorizationServices,
-      final SecurityConfiguration securityConfiguration) {
-    this.mappingServices = mappingServices;
-    this.tenantServices = tenantServices;
-    this.roleServices = roleServices;
-    this.groupServices = groupServices;
-    this.authorizationServices = authorizationServices;
-    usernameClaim = securityConfiguration.getAuthentication().getOidc().getUsernameClaim();
+  private final CamundaOAuthPrincipalService camundaOAuthPrincipalService;
+
+  public CamundaOidcUserService(final CamundaOAuthPrincipalService camundaOAuthPrincipalService) {
+    this.camundaOAuthPrincipalService = camundaOAuthPrincipalService;
   }
 
   @Override
   public OidcUser loadUser(final OidcUserRequest userRequest) throws OAuth2AuthenticationException {
     final OidcUser oidcUser = super.loadUser(userRequest);
-
     final Map<String, Object> claims = userRequest.getIdToken().getClaims();
-    final List<MappingEntity> mappings = mappingServices.getMatchingMappings(claims);
-    final Set<Long> mappingKeys =
-        mappings.stream().map(MappingEntity::mappingKey).collect(Collectors.toSet());
-    final Set<String> mappingIds =
-        mappings.stream().map(MappingEntity::mappingId).collect(Collectors.toSet());
-    if (mappingKeys.isEmpty()) {
-      LOG.debug("No mappings found for these claims: {}", claims);
-    }
-
-    final var assignedRoles = roleServices.getRolesByMemberKeys(mappingKeys);
-
-    return new CamundaOidcUser(
-        oidcUser,
-        mappingKeys,
-        mappingIds,
-        new AuthenticationContext(
-            getUsernameFromClaims(claims),
-            assignedRoles,
-            authorizationServices.getAuthorizedApplications(
-                Stream.concat(
-                        assignedRoles.stream().map(r -> r.roleKey().toString()),
-                        mappingKeys.stream()
-                            .map(String::valueOf)) // TODO remove mapping when refactoring to IDs
-                    .collect(Collectors.toSet())),
-            tenantServices.getTenantsByMemberIds(mappingIds).stream()
-                .map(TenantDTO::fromEntity)
-                .toList(),
-            groupServices.getGroupsByMemberKeys(mappingKeys).stream()
-                .map(GroupEntity::name)
-                .toList()));
-  }
-
-  private String getUsernameFromClaims(final Map<String, Object> claims) {
-    return Optional.ofNullable(claims.get(usernameClaim))
-        .map(Object::toString)
-        .orElseThrow(
-            () ->
-                new IllegalArgumentException(
-                    "Configured username claim %s not found in claims. Please check your OIDC configuration."
-                        .formatted(usernameClaim)));
+    return new CamundaOidcUser(oidcUser, camundaOAuthPrincipalService.loadOAuthContext(claims));
   }
 }
