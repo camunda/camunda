261ce2c8779564d3814b506e8a5beaddb97105e3
esraagamal6 <esraa-gamal@hotmail.com>
2025-04-08 09:13:36 +0200

    fix: update flaky API tests in the core application test suite

--- qa/core-application-e2e-test-suite/utils/apiHelpers.ts
+++ qa/core-application-e2e-test-suite/utils/apiHelpers.ts
@@ -7,6 +7,7 @@
  */
 
 import {APIRequestContext, request} from '@playwright/test';
+import {sleep} from './sleep';
 
 export async function authAPI(
   name: string,
@@ -14,15 +15,16 @@ export async function authAPI(
   application: string,
 ): Promise<void> {
   let baseURL: string;
-
+  let authFilePath: string; // Define separate storage path for each app
   if (application === 'tasklist') {
     baseURL = process.env.CORE_APPLICATION_TASKLIST_URL as string;
+    authFilePath = 'utils/.auth_tasklist'; // Tasklist-specific auth file
   } else if (application === 'operate') {
     baseURL = process.env.CORE_APPLICATION_OPERATE_URL as string;
+    authFilePath = 'utils/.auth_operate'; // Tasklist-specific auth file
   } else {
     throw new Error(`Unsupported application: ${application}`);
   }
-
   const apiRequestContext: APIRequestContext = await request.newContext({
     baseURL,
   });
@@ -35,5 +37,6 @@ export async function authAPI(
       password: password,
     },
   });
-  await apiRequestContext.storageState({path: 'utils/.auth'});
+  // Save authentication session in the app-specific file
+  await apiRequestContext.storageState({path: authFilePath});
 }
