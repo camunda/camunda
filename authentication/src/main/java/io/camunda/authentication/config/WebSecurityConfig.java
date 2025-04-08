e97dc4c648cc2c4b0292be59901f230f4eef3024
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-04-08 13:56:48 +0200

    feat: resolve mapping authorizations Search authorization layer

--- authentication/src/main/java/io/camunda/authentication/config/WebSecurityConfig.java
+++ authentication/src/main/java/io/camunda/authentication/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@
  */
 package io.camunda.authentication.config;
 
+import io.camunda.authentication.CamundaJwtAuthenticationConverter;
 import io.camunda.authentication.CamundaUserDetailsService;
 import io.camunda.authentication.ConditionalOnAuthenticationMethod;
 import io.camunda.authentication.ConditionalOnUnprotectedApi;
@@ -305,7 +306,8 @@ public class WebSecurityConfig {
     public SecurityFilterChain oidcApiSecurity(
         final HttpSecurity httpSecurity,
         final AuthFailureHandler authFailureHandler,
-        final JwtDecoder jwtDecoder)
+        final JwtDecoder jwtDecoder,
+        final CamundaJwtAuthenticationConverter converter)
         throws Exception {
       return httpSecurity
           .securityMatcher(API_PATHS.toArray(new String[0]))
@@ -324,7 +326,10 @@ public class WebSecurityConfig {
           .formLogin(AbstractHttpConfigurer::disable)
           .anonymous(AbstractHttpConfigurer::disable)
           .oauth2ResourceServer(
-              oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder)))
+              oauth2 ->
+                  oauth2.jwt(
+                      jwtConfigurer ->
+                          jwtConfigurer.decoder(jwtDecoder).jwtAuthenticationConverter(converter)))
           .oauth2Login(AbstractHttpConfigurer::disable)
           .oidcLogout(AbstractHttpConfigurer::disable)
           .logout(AbstractHttpConfigurer::disable)
@@ -338,7 +343,8 @@ public class WebSecurityConfig {
         final AuthFailureHandler authFailureHandler,
         final ClientRegistrationRepository clientRegistrationRepository,
         final WebApplicationAuthorizationCheckFilter webApplicationAuthorizationCheckFilter,
-        final JwtDecoder jwtDecoder)
+        final JwtDecoder jwtDecoder,
+        final CamundaJwtAuthenticationConverter converter)
         throws Exception {
       return httpSecurity
           .securityMatcher(WEBAPP_PATHS.toArray(new String[0]))
@@ -357,7 +363,10 @@ public class WebSecurityConfig {
           .formLogin(AbstractHttpConfigurer::disable)
           .anonymous(AbstractHttpConfigurer::disable)
           .oauth2ResourceServer(
-              oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder)))
+              oauth2 ->
+                  oauth2.jwt(
+                      jwtConfigurer ->
+                          jwtConfigurer.decoder(jwtDecoder).jwtAuthenticationConverter(converter)))
           .oauth2Login(
               oauthLoginConfigurer -> {
                 oauthLoginConfigurer
