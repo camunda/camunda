45aee3ecb4fb7835a2f0fa90f4a3a916b757d1f0
Carlo Sana <carlo.sana@camunda.com>
2025-04-09 10:17:33 +0200

    feat: add function to extract Metadata from backup name

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/es/backup/Metadata.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/es/backup/Metadata.java
@@ -7,7 +7,9 @@
  */
 package io.camunda.tasklist.webapp.es.backup;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.tasklist.exceptions.TasklistRuntimeException;
+import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,6 +21,11 @@ public class Metadata {
   private static final String SNAPSHOT_NAME_PREFIX_PATTERN = SNAPSHOT_NAME_PREFIX + "{backupId}_";
   private static final Pattern BACKUPID_PATTERN =
       Pattern.compile(SNAPSHOT_NAME_PREFIX + "(\\d*)_.*");
+  private static final Pattern METADATA_PATTERN =
+      Pattern.compile(
+          SNAPSHOT_NAME_PREFIX
+              + "(?<backupId>\\d+)_(?<version>[^_]+)_part_(?<index>\\d+)_of_(?<count>\\d+)");
+
   private Long backupId;
   private String version;
   private Integer partNo;
@@ -28,7 +35,7 @@ public class Metadata {
     return backupId;
   }
 
-  public Metadata setBackupId(Long backupId) {
+  public Metadata setBackupId(final Long backupId) {
     this.backupId = backupId;
     return this;
   }
@@ -37,7 +44,7 @@ public class Metadata {
     return version;
   }
 
-  public Metadata setVersion(String version) {
+  public Metadata setVersion(final String version) {
     this.version = version;
     return this;
   }
@@ -46,7 +53,7 @@ public class Metadata {
     return partNo;
   }
 
-  public Metadata setPartNo(Integer partNo) {
+  public Metadata setPartNo(final Integer partNo) {
     this.partNo = partNo;
     return this;
   }
@@ -55,7 +62,7 @@ public class Metadata {
     return partCount;
   }
 
-  public Metadata setPartCount(Integer partCount) {
+  public Metadata setPartCount(final Integer partCount) {
     this.partCount = partCount;
     return this;
   }
@@ -68,12 +75,16 @@ public class Metadata {
         .replace("{count}", partCount + "");
   }
 
-  public static String buildSnapshotNamePrefix(Long backupId) {
+  public boolean isInitialized() {
+    return partCount != null && partNo != null && version != null;
+  }
+
+  public static String buildSnapshotNamePrefix(final Long backupId) {
     return SNAPSHOT_NAME_PREFIX_PATTERN.replace("{backupId}", String.valueOf(backupId));
   }
 
   // backward compatibility with v. 8.1
-  public static Long extractBackupIdFromSnapshotName(String snapshotName) {
+  public static Long extractBackupIdFromSnapshotName(final String snapshotName) {
     final Matcher matcher = BACKUPID_PATTERN.matcher(snapshotName);
     if (matcher.matches()) {
       return Long.valueOf(matcher.group(1));
@@ -83,8 +94,45 @@ public class Metadata {
     }
   }
 
+  public static Metadata extractMetadataFromSnapshotName(final String snapshotName) {
+    Objects.requireNonNull(snapshotName, "Snapshot name cannot be null");
+    final Matcher matcher = METADATA_PATTERN.matcher(snapshotName);
+    if (matcher.matches()) {
+      final Long backupId = Long.parseLong(matcher.group("backupId"));
+      final String version = matcher.group("version");
+      final Integer index = Integer.parseInt(matcher.group("index"));
+      final Integer count = Integer.parseInt(matcher.group("count"));
+
+      return new Metadata()
+          .setBackupId(backupId)
+          .setPartCount(count)
+          .setPartNo(index)
+          .setVersion(version);
+    } else {
+      throw new IllegalArgumentException(
+          "Unable to extract metadata. Snapshot name: " + snapshotName);
+    }
+  }
+
+  public static Metadata extractFromMetadataOrName(
+      final ObjectMapper objectMapper,
+      final Map<String, Object> metadata,
+      final String snapshotName) {
+    final Metadata fromMetadata = objectMapper.convertValue(metadata, Metadata.class);
+    if (fromMetadata != null && fromMetadata.isInitialized()) {
+      return fromMetadata;
+    } else {
+      return Metadata.extractMetadataFromSnapshotName(snapshotName);
+    }
+  }
+
   @Override
-  public boolean equals(Object o) {
+  public int hashCode() {
+    return Objects.hash(version, partNo, partCount);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
     if (this == o) {
       return true;
     }
@@ -95,10 +143,5 @@ public class Metadata {
     return Objects.equals(version, metadata.version)
         && Objects.equals(partNo, metadata.partNo)
         && Objects.equals(partCount, metadata.partCount);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(version, partNo, partCount);
   }
 }
---
+++ tasklist/webapp/src/test/java/io/camunda/tasklist/webapp/es/backup/MetadataTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.tasklist.webapp.es.backup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class MetadataTest {
+  @Test
+  public void shouldExtractMetadataFromSnapshotname() {
+    final Metadata metadata =
+        new Metadata().setBackupId(23L).setVersion("8.7.1").setPartCount(6).setPartNo(2);
+    final var name = metadata.buildSnapshotName();
+    final var fromName = Metadata.extractMetadataFromSnapshotName(name);
+    assertThat(metadata).isEqualTo(fromName);
+    assertThat(Metadata.extractFromMetadataOrName(new ObjectMapper(), Map.of(), name))
+        .isEqualTo(fromName);
+  }
+
+  @Test
+  public void shouldPreferExtractingFromMetadataField() {
+    final Metadata metadata =
+        new Metadata().setBackupId(23L).setVersion("8.7.1").setPartCount(6).setPartNo(2);
+    final var extracted =
+        Metadata.extractFromMetadataOrName(
+            new ObjectMapper(),
+            Map.of(
+                "backupId",
+                metadata.getBackupId(),
+                "partNo",
+                metadata.getPartNo(),
+                "partCount",
+                metadata.getPartCount(),
+                "version",
+                metadata.getVersion()),
+            "");
+    assertThat(extracted).isEqualTo(metadata);
+  }
+}
