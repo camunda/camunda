a2d5f21497367c7a2d3c6cd5a625425e8ee7eb2b
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-04-14 16:44:45 +0200

    feat: add RSS and NMT metrics via JFR

---
+++ dist/src/main/java/io/camunda/application/commons/metrics/JfrMetricRecorder.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.application.commons.metrics;
+
+import io.camunda.application.commons.metrics.jfr.NativeMemoryMetrics;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.Objects;
+import jdk.jfr.consumer.RecordingStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Allows registering and tracking metrics based on JFR events. Although there should be no overhead
+ * according to the JVM, we still allow disabling this via an experimental flag {@code
+ * camunda.flags.jfr.metrics = false}.
+ *
+ * <p>Defaults to being enabled.
+ *
+ * <p>NOTE: the stream is not closed, as this is a singleton component; it will live until the
+ * application is closed. Since it's not using a daemon thread, this will not prevent the
+ * application from stopping.
+ */
+@Component
+@ConditionalOnProperty(
+    prefix = "camunda.flags.jfr",
+    name = "metrics",
+    havingValue = "true",
+    matchIfMissing = true)
+@Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
+public final class JfrMetricRecorder {
+  private final RecordingStream jfrStream;
+  private final MeterRegistry registry;
+
+  @Autowired
+  public JfrMetricRecorder(final MeterRegistry registry) {
+    this.registry = Objects.requireNonNull(registry, "must specify a meter registry");
+    jfrStream = new RecordingStream();
+    jfrStream.setMaxSize(8 * 1024 * 1024);
+  }
+
+  @EventListener(classes = {ApplicationStartedEvent.class})
+  public void onStart(final ApplicationStartedEvent ignored) {
+    new NativeMemoryMetrics().registerEvents(jfrStream, registry);
+    jfrStream.startAsync();
+  }
+}
---
+++ dist/src/main/java/io/camunda/application/commons/metrics/jfr/NativeMemoryMetrics.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.application.commons.metrics.jfr;
+
+import static io.camunda.application.commons.metrics.jfr.NativeMemoryMetricsDoc.NMT_USAGE;
+import static io.camunda.application.commons.metrics.jfr.NativeMemoryMetricsDoc.NMT_USAGE_TOTAL;
+import static io.camunda.application.commons.metrics.jfr.NativeMemoryMetricsDoc.RSS;
+import static io.camunda.application.commons.metrics.jfr.NativeMemoryMetricsDoc.RSS_PEAK;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tag;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+import jdk.jfr.consumer.RecordedEvent;
+import jdk.jfr.consumer.RecordingStream;
+
+/**
+ * Tracks RSS and native memory usage (as provided by the JVM's Native Memory Tracking) capabilities
+ * and registers equivalent gauges to Micrometer.
+ *
+ * <p>See {@link NativeMemoryMetricsDoc} for documentation.
+ *
+ * <p>Note that the native memory metrics are only available if NMT is enabled via {@code
+ * -XX:NativeMemoryTracking} (e.g. {@code -XX:NativeMemoryTracking=summary}).
+ */
+public final class NativeMemoryMetrics {
+  private final MemoryUsage residentSetSize = new MemoryUsage();
+  private final ConcurrentMap<String, NativeMemoryUsage> nativeMemoryUsage =
+      new ConcurrentHashMap<>();
+  private final NativeMemoryUsage nativeMemoryUsageTotal = new NativeMemoryUsage();
+
+  public void registerEvents(final RecordingStream stream, final MeterRegistry registry) {
+    stream.enable(RSS.getJfrEventName()).withoutStackTrace();
+    stream.onEvent(RSS.getJfrEventName(), residentSetSize::onEvent);
+    residentSetSize.register(registry);
+
+    stream.enable(NMT_USAGE_TOTAL.getJfrEventName()).withoutStackTrace();
+    stream.onEvent(NMT_USAGE_TOTAL.getJfrEventName(), nativeMemoryUsageTotal::onEvent);
+    nativeMemoryUsageTotal.register(NMT_USAGE_TOTAL, registry);
+
+    stream.enable(NMT_USAGE.getJfrEventName()).withoutStackTrace();
+    stream.onEvent(
+        NMT_USAGE.getJfrEventName(),
+        event ->
+            nativeMemoryUsage
+                .computeIfAbsent(event.getString("type"), type -> registerNmtGauge(type, registry))
+                .onEvent(event));
+  }
+
+  private NativeMemoryUsage registerNmtGauge(final String type, final MeterRegistry registry) {
+    final var usage = new NativeMemoryUsage();
+    usage.register(
+        NMT_USAGE,
+        registry,
+        Tag.of(NativeMemoryMetricsDoc.NativeMemoryUsageKeys.TYPE.asString(), type));
+
+    return usage;
+  }
+
+  private record MemoryUsage(AtomicLong size, AtomicLong peak) {
+    private MemoryUsage() {
+      this(new AtomicLong(), new AtomicLong());
+    }
+
+    private void onEvent(final RecordedEvent event) {
+      size.set(event.getLong("size"));
+      peak.set(event.getLong("peak"));
+    }
+
+    private void register(final MeterRegistry registry) {
+      Gauge.builder(RSS.getName(), size, AtomicLong::get)
+          .description(RSS.getDescription())
+          .baseUnit("bytes")
+          .register(registry);
+      Gauge.builder(RSS_PEAK.getName(), peak, AtomicLong::get)
+          .description(RSS_PEAK.getDescription())
+          .baseUnit("bytes")
+          .register(registry);
+    }
+  }
+
+  private record NativeMemoryUsage(AtomicLong reserved, AtomicLong committed) {
+    private NativeMemoryUsage() {
+      this(new AtomicLong(), new AtomicLong());
+    }
+
+    private void onEvent(final RecordedEvent event) {
+      reserved.set(event.getLong("reserved"));
+      committed.set(event.getLong("committed"));
+    }
+
+    private void register(
+        final NativeMemoryMetricsDoc doc, final MeterRegistry registry, final Tag... tags) {
+      Gauge.builder(doc.getName(), reserved, AtomicLong::get)
+          .description(doc.getDescription())
+          .baseUnit("bytes")
+          .tags(List.of(tags))
+          .register(registry);
+      Gauge.builder(doc.getName(), committed, AtomicLong::get)
+          .description(doc.getDescription())
+          .baseUnit("bytes")
+          .tags(List.of(tags))
+          .register(registry);
+    }
+  }
+}
