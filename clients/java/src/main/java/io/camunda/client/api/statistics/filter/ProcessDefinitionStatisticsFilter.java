08d8a2c16af8ad5280489ccf4f44ebbc468c39f8
Daniel Kelemen <daniel.kelemen@camunda.com>
2025-04-15 15:12:15 +0200

    feat: implement RDBMS changes for PD statistics or filters
    related to https://github.com/camunda/camunda/issues/30945

--- clients/java/src/main/java/io/camunda/client/api/statistics/filter/ProcessDefinitionStatisticsFilter.java
+++ clients/java/src/main/java/io/camunda/client/api/statistics/filter/ProcessDefinitionStatisticsFilter.java
@@ -18,103 +18,135 @@ package io.camunda.client.api.statistics.filter;
 import io.camunda.client.api.search.enums.ElementInstanceState;
 import io.camunda.client.api.search.enums.ProcessInstanceState;
 import io.camunda.client.api.search.filter.VariableValueFilter;
+import io.camunda.client.api.search.filter.ProcessInstanceFilterBase;
 import io.camunda.client.api.search.filter.builder.BasicLongProperty;
 import io.camunda.client.api.search.filter.builder.DateTimeProperty;
 import io.camunda.client.api.search.filter.builder.ElementInstanceStateProperty;
 import io.camunda.client.api.search.filter.builder.ProcessInstanceStateProperty;
 import io.camunda.client.api.search.filter.builder.StringProperty;
-import io.camunda.client.api.statistics.request.StatisticsRequest.StatisticsRequestFilter;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
-public interface ProcessDefinitionStatisticsFilter extends StatisticsRequestFilter {
+public interface ProcessDefinitionStatisticsFilter extends ProcessDefinitionStatisticsFilterBase {
 
   /** Filter by processInstanceKey */
+  @Override
   ProcessDefinitionStatisticsFilter processInstanceKey(final Long processInstanceKey);
 
   /** Filter by processInstanceKey using {@link BasicLongProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter processInstanceKey(final Consumer<BasicLongProperty> fn);
 
   /** Filter by parentProcessInstanceKey */
+  @Override
   ProcessDefinitionStatisticsFilter parentProcessInstanceKey(final Long parentProcessInstanceKey);
 
   /** Filter by parentProcessInstanceKey using {@link BasicLongProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter parentProcessInstanceKey(final Consumer<BasicLongProperty> fn);
 
   /** Filter by parentElementInstanceKey */
+  @Override
   ProcessDefinitionStatisticsFilter parentElementInstanceKey(final Long parentElementInstanceKey);
 
   /** Filter by parentElementInstanceKey using {@link BasicLongProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter parentElementInstanceKey(final Consumer<BasicLongProperty> fn);
 
   /** Filter by startDate */
+  @Override
   ProcessDefinitionStatisticsFilter startDate(final OffsetDateTime startDate);
 
   /** Filter by startDate using {@link DateTimeProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter startDate(final Consumer<DateTimeProperty> fn);
 
   /** Filter by endDate */
+  @Override
   ProcessDefinitionStatisticsFilter endDate(final OffsetDateTime endDate);
 
   /** Filter by endDate using {@link DateTimeProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter endDate(final Consumer<DateTimeProperty> fn);
 
   /** Filter by state */
+  @Override
   ProcessDefinitionStatisticsFilter state(final ProcessInstanceState state);
 
   /** Filter by state using {@link ProcessInstanceStateProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter state(final Consumer<ProcessInstanceStateProperty> fn);
 
   /** Filter by hasIncident */
+  @Override
   ProcessDefinitionStatisticsFilter hasIncident(final Boolean hasIncident);
 
   /** Filter by tenantId */
+  @Override
   ProcessDefinitionStatisticsFilter tenantId(final String tenantId);
 
   /** Filter by tenantId using {@link StringProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter tenantId(final Consumer<StringProperty> fn);
 
   /** Filter by variables */
+  @Override
   ProcessDefinitionStatisticsFilter variables(
       final List<Consumer<VariableValueFilter>> variableValueFilters);
 
   /** Filter by variables map */
+  @Override
   ProcessDefinitionStatisticsFilter variables(final Map<String, Object> variableValueFilters);
 
   /** Filter by batchOperationId */
+  @Override
   ProcessDefinitionStatisticsFilter batchOperationId(final String batchOperationId);
 
   /** Filter by batchOperationId using {@link StringProperty} */
+  @Override
   ProcessDefinitionStatisticsFilter batchOperationId(final Consumer<StringProperty> fn);
 
   /** Filter by error message */
+  @Override
   ProcessDefinitionStatisticsFilter errorMessage(final String errorMessage);
 
   /** Filter by error message using {@link StringProperty} consumer */
+  @Override
   ProcessDefinitionStatisticsFilter errorMessage(final Consumer<StringProperty> fn);
 
   /** Filter by hasRetriesLeft */
+  @Override
   ProcessDefinitionStatisticsFilter hasRetriesLeft(final Boolean hasRetriesLeft);
 
   /** Filter by elementId */
+  @Override
   ProcessDefinitionStatisticsFilter elementId(final String elementId);
 
   /** Filter by elementId using {@link StringProperty} */
+  @Override
   ProcessDefinitionStatisticsFilter elementId(final Consumer<StringProperty> fn);
 
   /** Filter by elementInstanceState */
+  @Override
   ProcessDefinitionStatisticsFilter elementInstanceState(final ElementInstanceState state);
 
   /** Filter by elementInstanceState using {@link ElementInstanceStateProperty} */
+  @Override
   ProcessDefinitionStatisticsFilter elementInstanceState(
       final Consumer<ElementInstanceStateProperty> fn);
 
   /** Filter by hasElementInstanceIncident */
+  @Override
   ProcessDefinitionStatisticsFilter hasElementInstanceIncident(
       final Boolean hasElementInstanceIncident);
 
   /** Filter by incidentErrorHashCode */
+  @Override
   ProcessDefinitionStatisticsFilter incidentErrorHashCode(final Integer incidentErrorHashCode);
+
+  /** Filter by or conjunction using {@link ProcessInstanceFilterBase} consumer */
+  ProcessDefinitionStatisticsFilter orFilters(
+      List<Consumer<ProcessDefinitionStatisticsFilterBase>> filters);
 }
