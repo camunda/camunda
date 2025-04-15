92555a2d71a76c9e20dd76f861dd3debe9f38a99
Ferhat Ayaz <ferhat.ayaz@holisticon.de>
2025-04-15 16:14:48 +0200

    feat: Mock job workers

---
+++ testing/camunda-process-test-java/src/test/java/io/camunda/process/test/impl/extensions/CamundaProcessTestContextTest.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.process.test.impl.extensions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.DeploymentEvent;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.process.test.api.CamundaAssert;
+import io.camunda.process.test.api.CamundaProcessTest;
+import io.camunda.process.test.api.CamundaProcessTestContext;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Future;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+
+@CamundaProcessTest
+public class CamundaProcessTestContextTest {
+  private static final int TIMEOUT = 40;
+  private CamundaProcessTestContext processTestContext;
+  private CamundaClient client;
+
+  @Test
+  void shouldThrowBpmnErrorWithoutVariables() {
+    // Given
+    processTestContext.mockJobWorker("test").thenThrowBpmnError("bpmn-error");
+    final long processDefinitionKey = deployProcessModel();
+
+    // When
+    final ProcessInstanceEvent processInstanceEvent =
+        client.newCreateInstanceCommand().processDefinitionKey(processDefinitionKey).send().join();
+
+    // Then
+    CamundaAssert.assertThat(processInstanceEvent).hasCompletedElements("error-end");
+  }
+
+  @Test
+  void shouldThrowBpmnErrorWithVariables() {
+    // Given
+    final Map<String, Object> variables = new HashMap<>();
+    variables.put("abc", 123);
+    processTestContext.mockJobWorker("test").thenThrowBpmnError("bpmn-error", variables);
+    final long processDefinitionKey = deployProcessModel();
+
+    // When
+    final ProcessInstanceEvent processInstanceEvent =
+        client.newCreateInstanceCommand().processDefinitionKey(processDefinitionKey).send().join();
+
+    // Then
+    CamundaAssert.assertThat(processInstanceEvent).hasCompletedElements("error-end");
+    CamundaAssert.assertThat(processInstanceEvent).hasVariables(variables);
+  }
+
+  @Test
+  void shouldMockJobWorkerWithoutVariables() {
+    // Given
+    processTestContext.mockJobWorker("test").thenComplete();
+    final long processDefinitionKey = deployProcessModel();
+
+    // When
+    final ProcessInstanceEvent processInstanceEvent =
+        client.newCreateInstanceCommand().processDefinitionKey(processDefinitionKey).send().join();
+
+    // Then
+    CamundaAssert.assertThat(processInstanceEvent).isCompleted();
+    CamundaAssert.assertThat(processInstanceEvent).hasCompletedElements("success-end");
+  }
+
+  @Test
+  void shouldMockJobWorkerWithVariables() {
+    // Given
+    final Map<String, Object> variables = new HashMap<>();
+    variables.put("abc", 123);
+    processTestContext.mockJobWorker("test").thenComplete(variables);
+    final long processDefinitionKey = deployProcessModel();
+
+    // When
+    final ProcessInstanceEvent processInstanceEvent =
+        client.newCreateInstanceCommand().processDefinitionKey(processDefinitionKey).send().join();
+
+    // Then
+    CamundaAssert.assertThat(processInstanceEvent).isCompleted();
+    CamundaAssert.assertThat(processInstanceEvent).hasCompletedElements("success-end");
+    CamundaAssert.assertThat(processInstanceEvent).hasVariables(variables);
+  }
+
+  @Test
+  void shouldMockJobWorkerWithJobHandlerBpmnError() {
+    // Given
+    processTestContext
+        .mockJobWorker("test")
+        .withHandler(
+            (jobClient, job) -> {
+              jobClient.newThrowErrorCommand(job).errorCode("bpmn-error").send().join();
+            });
+    final long processDefinitionKey = deployProcessModel();
+
+    // When
+    final ProcessInstanceEvent processInstanceEvent =
+        client.newCreateInstanceCommand().processDefinitionKey(processDefinitionKey).send().join();
+
+    // Then
+    CamundaAssert.assertThat(processInstanceEvent).isCompleted();
+    CamundaAssert.assertThat(processInstanceEvent).hasCompletedElements("error-end");
+  }
+
+  @Test
+  void shouldMockJobWorkerWithJobHandlerSuccess() {
+    // Given
+    processTestContext
+        .mockJobWorker("test")
+        .withHandler(
+            (jobClient, job) -> {
+              jobClient.newCompleteCommand(job).send().join();
+            });
+    final long processDefinitionKey = deployProcessModel();
+
+    // When
+    final ProcessInstanceEvent processInstanceEvent =
+        client.newCreateInstanceCommand().processDefinitionKey(processDefinitionKey).send().join();
+
+    // Then
+    CamundaAssert.assertThat(processInstanceEvent).isCompleted();
+    CamundaAssert.assertThat(processInstanceEvent).hasCompletedElements("success-end");
+  }
+
+  /**
+   * Deploys a process model and waits until it is accessible via the API.
+   *
+   * @return the process definition key
+   */
+  private long deployProcessModel() {
+    final BpmnModelInstance processModel =
+        Bpmn.createExecutableProcess("test-process")
+            .startEvent()
+            .serviceTask("service-task-1")
+            .zeebeJobType("test")
+            .boundaryEvent("error-boundary-event")
+            .error("bpmn-error")
+            .endEvent("error-end")
+            .moveToActivity("service-task-1")
+            .endEvent("success-end")
+            .done();
+
+    final DeploymentEvent deploymentEvent =
+        client
+            .newDeployResourceCommand()
+            .addProcessModel(processModel, "test-process.bpmn")
+            .send()
+            .join();
+    final long processDefinitionKey =
+        deploymentEvent.getProcesses().stream().findFirst().get().getProcessDefinitionKey();
+    Awaitility.await("until process model is accessible via API")
+        .atMost(Duration.ofSeconds(2 * TIMEOUT))
+        .untilAsserted(
+            () -> {
+              final Future<?> futureRequest =
+                  client.newProcessDefinitionGetRequest(processDefinitionKey).send();
+              assertThat(futureRequest).succeedsWithin(Duration.ofSeconds(TIMEOUT));
+            });
+    return processDefinitionKey;
+  }
+}
