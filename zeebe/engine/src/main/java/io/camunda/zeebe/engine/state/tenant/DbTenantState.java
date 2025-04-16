c92b0b3080e1a242ff73104dac6d93ea83b74f74
Lena Schönburg <lena@fairburg.xyz>
2025-04-16 18:58:41 +0200

    refactor: remove membership tracking from tenant state

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/tenant/DbTenantState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/tenant/DbTenantState.java
@@ -10,18 +10,10 @@ package io.camunda.zeebe.engine.state.tenant;
 import io.camunda.zeebe.db.ColumnFamily;
 import io.camunda.zeebe.db.TransactionContext;
 import io.camunda.zeebe.db.ZeebeDb;
-import io.camunda.zeebe.db.impl.DbCompositeKey;
-import io.camunda.zeebe.db.impl.DbForeignKey;
 import io.camunda.zeebe.db.impl.DbString;
-import io.camunda.zeebe.engine.state.authorization.EntityTypeValue;
 import io.camunda.zeebe.engine.state.mutable.MutableTenantState;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import io.camunda.zeebe.protocol.impl.record.value.tenant.TenantRecord;
-import io.camunda.zeebe.protocol.record.value.EntityType;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -31,25 +23,11 @@ public class DbTenantState implements MutableTenantState {
   private final PersistedTenant persistedTenant = new PersistedTenant();
   private final ColumnFamily<DbString, PersistedTenant> tenantsColumnFamily;
 
-  private final DbForeignKey<DbString> fkTenantId;
-  private final DbString entityId = new DbString();
-  private final EntityTypeValue entityType = new EntityTypeValue();
-  private final DbCompositeKey<DbForeignKey<DbString>, DbString> entityIdByTenantId;
-  private final ColumnFamily<DbCompositeKey<DbForeignKey<DbString>, DbString>, EntityTypeValue>
-      entityByTenantColumnFamily;
-
   public DbTenantState(
       final ZeebeDb<ZbColumnFamilies> zeebeDb, final TransactionContext transactionContext) {
     tenantsColumnFamily =
         zeebeDb.createColumnFamily(
             ZbColumnFamilies.TENANTS, transactionContext, tenantId, new PersistedTenant());
-
-    fkTenantId = new DbForeignKey<>(tenantId, ZbColumnFamilies.TENANTS);
-
-    entityIdByTenantId = new DbCompositeKey<>(fkTenantId, entityId);
-    entityByTenantColumnFamily =
-        zeebeDb.createColumnFamily(
-            ZbColumnFamilies.ENTITY_BY_TENANT, transactionContext, entityIdByTenantId, entityType);
   }
 
   @Override
@@ -69,62 +47,9 @@ public class DbTenantState implements MutableTenantState {
   }
 
   @Override
-  public void addEntity(final TenantRecord tenantRecord) {
-    tenantId.wrapString(tenantRecord.getTenantId());
-    entityId.wrapString(tenantRecord.getEntityId());
-    entityType.setEntityType(tenantRecord.getEntityType());
-    entityByTenantColumnFamily.insert(entityIdByTenantId, entityType);
-  }
-
-  @Override
-  public void removeEntity(final TenantRecord tenantRecord) {
-    tenantId.wrapString(tenantRecord.getTenantId());
-    entityId.wrapString(tenantRecord.getEntityId());
-    entityByTenantColumnFamily.deleteExisting(entityIdByTenantId);
-  }
-
-  @Override
   public void delete(final TenantRecord tenantRecord) {
     tenantId.wrapString(tenantRecord.getTenantId());
-
-    entityByTenantColumnFamily.whileEqualPrefix(
-        fkTenantId,
-        (compositeKey, entityTypeValue) -> {
-          entityByTenantColumnFamily.deleteExisting(compositeKey);
-        });
-
     tenantsColumnFamily.deleteExisting(tenantId);
-  }
-
-  @Override
-  public Optional<EntityType> getEntityType(final String tenantId, final String entityId) {
-    this.tenantId.wrapString(tenantId);
-    this.entityId.wrapString(entityId);
-
-    final var entityTypeValue = entityByTenantColumnFamily.get(entityIdByTenantId);
-
-    if (entityTypeValue == null) {
-      return Optional.empty();
-    }
-
-    return Optional.of(entityTypeValue.getEntityType());
-  }
-
-  @Override
-  public Map<EntityType, List<String>> getEntitiesByType(final String tenantId) {
-    final Map<EntityType, List<String>> entitiesMap = new HashMap<>();
-    this.tenantId.wrapString(tenantId);
-
-    entityByTenantColumnFamily.whileEqualPrefix(
-        fkTenantId,
-        (compositeKey, entityTypeValue) -> {
-          final var entityType = entityTypeValue.getEntityType();
-          final var entityId = compositeKey.second().toString();
-          entitiesMap.putIfAbsent(entityType, new ArrayList<>());
-          entitiesMap.get(entityType).add(entityId);
-        });
-
-    return entitiesMap;
   }
 
   @Override
