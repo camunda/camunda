0d64c912dbb9bec8c25451a098702b3c5614548c
Nikola Koevski <nikola.koevski@camunda.com>
2025-04-17 14:46:16 +0200

    feat: introduce operate adapter interface for zeebe-based services
    Introduce an adapter interfaces for Operate to enable
    switching between using the CamundaClient for compatibility
    mode, and the CamundaServices layer, for single app mode.
    
    This commit also introduces the Client and Service implementations
    of the adapter. The implementations add the CamundaClient and the
    service layer Zeebe services used by Operate.

---
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/zeebe/operation/adapter/ServicesBasedAdapter.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.operate.webapp.zeebe.operation.adapter;
+
+import io.camunda.operate.util.ConditionalOnOperateCompatibility;
+import io.camunda.operate.webapp.security.permission.PermissionsService;
+import io.camunda.operate.webapp.security.tenant.TenantService;
+import io.camunda.service.IncidentServices;
+import io.camunda.service.ProcessInstanceServices;
+import io.camunda.service.ResourceServices;
+import io.camunda.service.VariableServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnOperateCompatibility(enabled = "false", matchIfMissing = true)
+public class ServicesBasedAdapter implements OperateServicesAdapter {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ServicesBasedAdapter.class);
+
+  private final ProcessInstanceServices processInstanceServices;
+  private final ResourceServices resourceServices;
+  private final VariableServices variableServices;
+  private final IncidentServices incidentServices;
+  private final PermissionsService permissionsService;
+  private final TenantService tenantService;
+
+  public ServicesBasedAdapter(
+      final ProcessInstanceServices processInstanceServices,
+      final ResourceServices resourceServices,
+      final VariableServices variableServices,
+      final IncidentServices incidentServices,
+      final PermissionsService permissionsService,
+      final TenantService tenantService) {
+    this.processInstanceServices = processInstanceServices;
+    this.resourceServices = resourceServices;
+    this.variableServices = variableServices;
+    this.incidentServices = incidentServices;
+    this.permissionsService = permissionsService;
+    this.tenantService = tenantService;
+  }
+}
