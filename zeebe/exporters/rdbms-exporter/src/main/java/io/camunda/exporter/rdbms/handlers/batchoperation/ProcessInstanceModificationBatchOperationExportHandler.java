5b82346c9b2557bd1aefb4a7fc7788362a5e8720
Christian Thiel <christian.thiel@holisticon.de>
2025-04-17 11:29:15 +0200

    feat: extend rdbms exporter to monitor MODIFY_PROCESS_INSTANCE batch operations #31169

---
+++ zeebe/exporters/rdbms-exporter/src/main/java/io/camunda/exporter/rdbms/handlers/batchoperation/ProcessInstanceModificationBatchOperationExportHandler.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.rdbms.handlers.batchoperation;
+
+import io.camunda.db.rdbms.write.service.BatchOperationWriter;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceModificationIntent;
+import io.camunda.zeebe.protocol.record.value.ProcessInstanceModificationRecordValue;
+
+/** This aggregates the batch operation status of process instance modify tasks */
+public class ProcessInstanceModificationBatchOperationExportHandler
+    extends RdbmsBatchOperationStatusExportHandler<ProcessInstanceModificationRecordValue> {
+
+  public ProcessInstanceModificationBatchOperationExportHandler(
+      final BatchOperationWriter batchOperationWriter) {
+    super(batchOperationWriter);
+  }
+
+  @Override
+  long getItemKey(final Record<ProcessInstanceModificationRecordValue> record) {
+    return record.getValue().getProcessInstanceKey();
+  }
+
+  @Override
+  protected boolean isCompleted(final Record<ProcessInstanceModificationRecordValue> record) {
+    return record.getIntent().equals(ProcessInstanceModificationIntent.MODIFIED);
+  }
+
+  @Override
+  protected boolean isFailed(final Record<ProcessInstanceModificationRecordValue> record) {
+    return record.getIntent().equals(ProcessInstanceModificationIntent.MODIFY)
+        && record.getRejectionType() != RejectionType.NULL_VAL;
+  }
+}
