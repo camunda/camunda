6a6d3b3bbbed91306f7d59a4dee493b49447ae77
Alex Bremard <alex.bremard@camunda.com>
2025-04-21 16:50:54 +0700

    feat: add operations methods

---
+++ operate/client/src/modules/queries/processInstance/deprecated/useProcessInstanceDeprecated.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {skipToken, useQuery, UseQueryResult} from '@tanstack/react-query';
+import {RequestError} from 'modules/request';
+import {useProcessInstancePageParams} from 'App/ProcessInstance/useProcessInstancePageParams';
+import {fetchProcessInstanceDeprecated} from 'modules/api/processInstances/fetchProcessInstance';
+
+function getQueryKey(processInstanceKey?: string) {
+  return ['processInstanceDeprecated', processInstanceKey];
+}
+
+const useProcessInstanceDeprecated = <T = ProcessInstanceEntity>(
+  select?: (data: ProcessInstanceEntity) => T,
+): UseQueryResult<T, RequestError> => {
+  const {processInstanceId} = useProcessInstancePageParams();
+
+  return useQuery({
+    queryKey: getQueryKey(processInstanceId),
+    queryFn: !!processInstanceId
+      ? async () => {
+          const {response, error} =
+            await fetchProcessInstanceDeprecated(processInstanceId);
+
+          if (response !== null) {
+            return response;
+          }
+
+          throw error;
+        }
+      : skipToken,
+    select,
+  });
+};
+
+export {useProcessInstanceDeprecated};
