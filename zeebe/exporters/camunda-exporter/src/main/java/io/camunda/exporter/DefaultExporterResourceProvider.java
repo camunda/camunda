2dc0e00378dadfa8ab24300d79999b8a2e79a1a4
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-04-22 15:09:06 +0200

    feat: add batch operation created and started handler for camunda exporter

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/DefaultExporterResourceProvider.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/DefaultExporterResourceProvider.java
@@ -65,6 +65,8 @@ import io.camunda.exporter.handlers.UserTaskJobBasedHandler;
 import io.camunda.exporter.handlers.UserTaskProcessInstanceHandler;
 import io.camunda.exporter.handlers.UserTaskVariableHandler;
 import io.camunda.exporter.handlers.VariableHandler;
+import io.camunda.exporter.handlers.batchoperation.BatchOperationCreatedHandler;
+import io.camunda.exporter.handlers.batchoperation.BatchOperationStartedHandler;
 import io.camunda.exporter.handlers.operation.OperationFromIncidentHandler;
 import io.camunda.exporter.handlers.operation.OperationFromProcessInstanceHandler;
 import io.camunda.exporter.handlers.operation.OperationFromVariableDocumentHandler;
@@ -86,6 +88,7 @@ import io.camunda.webapps.schema.descriptors.index.RoleIndex;
 import io.camunda.webapps.schema.descriptors.index.TasklistMetricIndex;
 import io.camunda.webapps.schema.descriptors.index.TenantIndex;
 import io.camunda.webapps.schema.descriptors.index.UserIndex;
+import io.camunda.webapps.schema.descriptors.template.BatchOperationTemplate;
 import io.camunda.webapps.schema.descriptors.template.DecisionInstanceTemplate;
 import io.camunda.webapps.schema.descriptors.template.EventTemplate;
 import io.camunda.webapps.schema.descriptors.template.FlowNodeInstanceTemplate;
@@ -271,7 +274,12 @@ public class DefaultExporterResourceProvider implements ExporterResourceProvider
                 indexDescriptors.get(MetricIndex.class).getFullQualifiedName()),
             new JobHandler(indexDescriptors.get(JobTemplate.class).getFullQualifiedName()),
             new MigratedVariableHandler(
-                indexDescriptors.get(VariableTemplate.class).getFullQualifiedName()));
+                indexDescriptors.get(VariableTemplate.class).getFullQualifiedName()),
+            // Batch Operation Handler
+            new BatchOperationCreatedHandler(
+                indexDescriptors.get(BatchOperationTemplate.class).getFullQualifiedName()),
+            new BatchOperationStartedHandler(
+                indexDescriptors.get(BatchOperationTemplate.class).getFullQualifiedName()));
 
     indicesWithCustomErrorHandlers =
         Map.of(
---
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/batchoperation/BatchOperationStartedHandler.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.handlers.batchoperation;
+
+import io.camunda.exporter.exceptions.PersistenceException;
+import io.camunda.exporter.handlers.ExportHandler;
+import io.camunda.exporter.store.BatchRequest;
+import io.camunda.webapps.schema.descriptors.template.BatchOperationTemplate;
+import io.camunda.webapps.schema.entities.operation.BatchOperationEntity;
+import io.camunda.webapps.schema.entities.operation.BatchOperationEntity.BatchOperationState;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.BatchOperationIntent;
+import io.camunda.zeebe.protocol.record.value.BatchOperationCreationRecordValue;
+import io.camunda.zeebe.util.DateUtil;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BatchOperationStartedHandler
+    implements ExportHandler<BatchOperationEntity, BatchOperationCreationRecordValue> {
+
+  private final String indexName;
+
+  public BatchOperationStartedHandler(final String indexName) {
+    this.indexName = indexName;
+  }
+
+  @Override
+  public ValueType getHandledValueType() {
+    return ValueType.BATCH_OPERATION_CREATION;
+  }
+
+  @Override
+  public Class<BatchOperationEntity> getEntityType() {
+    return BatchOperationEntity.class;
+  }
+
+  @Override
+  public boolean handlesRecord(final Record<BatchOperationCreationRecordValue> record) {
+    return record.getIntent().equals(BatchOperationIntent.STARTED);
+  }
+
+  @Override
+  public List<String> generateIds(final Record<BatchOperationCreationRecordValue> record) {
+    return List.of(String.valueOf(record.getValue().getBatchOperationKey()));
+  }
+
+  @Override
+  public BatchOperationEntity createNewEntity(final String id) {
+    return new BatchOperationEntity().setId(id);
+  }
+
+  @Override
+  public void updateEntity(
+      final Record<BatchOperationCreationRecordValue> record, final BatchOperationEntity entity) {
+    entity
+        .setStartDate(DateUtil.toOffsetDateTime(record.getTimestamp()))
+        .setState(BatchOperationState.ACTIVE);
+  }
+
+  @Override
+  public void flush(final BatchOperationEntity entity, final BatchRequest batchRequest)
+      throws PersistenceException {
+    final Map<String, Object> updateFields = new HashMap<>();
+    updateFields.put(BatchOperationTemplate.STATE, entity.getState());
+    updateFields.put(BatchOperationTemplate.START_DATE, entity.getStartDate());
+    batchRequest.update(indexName, entity.getId(), updateFields);
+  }
+
+  @Override
+  public String getIndexName() {
+    return indexName;
+  }
+}
