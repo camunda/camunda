064cf629cbbe1ae7c09a2707888add696106c8bc
Daniel Kelemen <daniel.kelemen@camunda.com>
2025-04-25 18:36:15 +0200

    feat: implement PI sequence-flows RDBMS
    related to https://github.com/camunda/camunda/issues/31020

---
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/read/mapper/SequenceFlowEntityMapper.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.db.rdbms.read.mapper;
+
+import io.camunda.db.rdbms.write.domain.SequenceFlowDbModel;
+import io.camunda.search.entities.SequenceFlowEntity;
+
+public class SequenceFlowEntityMapper {
+
+  public static SequenceFlowEntity toEntity(final SequenceFlowDbModel dbModel) {
+    return new SequenceFlowEntity(
+        dbModel.sequenceFlowId(),
+        dbModel.flowNodeId(),
+        dbModel.processInstanceKey(),
+        dbModel.processDefinitionKey(),
+        dbModel.processDefinitionId(),
+        dbModel.tenantId());
+  }
+}
