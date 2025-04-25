ed63e835e83e053f453eb5b4927634a616c58793
vitorwtavares <vitor.tavares@camunda.com>
2025-04-25 16:07:42 -0300

    BACKPORT-CONFLICT

--- operate/client/src/modules/stores/flowNodeInstance/index.tsx
+++ operate/client/src/modules/stores/flowNodeInstance/index.tsx
@@ -51,11 +51,13 @@ type State = {
     | 'fetching-next'
     | 'fetching-prev';
   flowNodeInstances: FlowNodeInstances;
+  flowNodesWithSubProcesses: FlowNodeInstanceType[];
 };
 
 const DEFAULT_STATE: State = {
   status: 'initial',
   flowNodeInstances: {},
+  flowNodesWithSubProcesses: [],
 };
 
 class FlowNodeInstance extends NetworkReconnectionHandler {
@@ -361,6 +363,10 @@ class FlowNodeInstance extends NetworkReconnectionHandler {
   handleFetchSuccess = (flowNodeInstances: FlowNodeInstances) => {
     Object.entries(flowNodeInstances).forEach(
       ([treePath, flowNodeInstance]) => {
+        this.state.flowNodesWithSubProcesses = flowNodeInstance.children.filter(
+          (instance) => instance.type === 'SUB_PROCESS',
+        );
+
         if (
           !isEqual(this.state.flowNodeInstances[treePath], flowNodeInstance)
         ) {
@@ -379,6 +385,9 @@ class FlowNodeInstance extends NetworkReconnectionHandler {
 
     Object.entries(flowNodeInstances).forEach(
       ([treePath, flowNodeInstance]) => {
+        this.state.flowNodesWithSubProcesses = flowNodeInstance.children.filter(
+          (instance) => instance.type === 'SUB_PROCESS',
+        );
         // don't create new trees (this prevents showing a tree when the user collapsed it earlier)
         if (
           this.state.flowNodeInstances[treePath] !== undefined &&
