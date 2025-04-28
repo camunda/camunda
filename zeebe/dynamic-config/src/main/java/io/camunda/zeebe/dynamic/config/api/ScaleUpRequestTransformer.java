8976bdc2e991f23a76331451bba83a098cf23e97
Carlo Sana <carlo.sana@camunda.com>
2025-04-28 13:43:59 +0200

    feat: validate members are registered in the configuration when scaling up

--- zeebe/dynamic-config/src/main/java/io/camunda/zeebe/dynamic/config/api/ScaleUpRequestTransformer.java
+++ zeebe/dynamic-config/src/main/java/io/camunda/zeebe/dynamic/config/api/ScaleUpRequestTransformer.java
@@ -32,40 +32,43 @@ public class ScaleUpRequestTransformer implements ConfigurationChangeRequest {
   @Override
   public Either<Exception, List<ClusterConfigurationChangeOperation>> operations(
       final ClusterConfiguration clusterConfiguration) {
-    if (desiredPartitionCount > clusterConfiguration.partitionCount()) {
-      if (clusterConfiguration.partitionCount() + bootstrappedPartitions.size()
-          != desiredPartitionCount) {
-        return Either.left(
-            new InvalidRequest(
-                String.format(
-                    "Cannot start scale up: clusterConfiguration.partitionCount(%d) + bootstrappedPartitions(%d) != desiredPartitionCount(%d)",
-                    clusterConfiguration.partitionCount(),
-                    bootstrappedPartitions.size(),
-                    desiredPartitionCount)));
-      }
-      final var operations =
-          // Generate the operations for the "coordinator" node, which is the node with the lowest
-          // id.
-          // See ClusterConfigurationCoordinatorSupplier
-          clusterConfiguration.members().entrySet().stream().min(Entry.comparingByKey()).stream()
-              .map(Entry::getKey)
-              .flatMap(
-                  id ->
-                      Stream.of(
-                          new StartPartitionScaleUp(id, desiredPartitionCount),
-                          new AwaitRedistributionCompletion(
-                              id, desiredPartitionCount, bootstrappedPartitions),
-                          new AwaitRelocationCompletion(
-                              id, desiredPartitionCount, bootstrappedPartitions)))
-              .map(ClusterConfigurationChangeOperation.class::cast)
-              .toList();
-      return Either.right(operations);
-    } else {
+    if (desiredPartitionCount <= clusterConfiguration.partitionCount()) {
       return Either.left(
           new InvalidRequest(
               String.format(
-                  "Cannot start scale up: desiredPartitionCount(%d) <= clusterConfiguration.partitionCount (%d)",
+                  "Cannot scale up when desiredPartitionCount %d is less than the current partition count %d",
                   desiredPartitionCount, clusterConfiguration.partitionCount())));
     }
+    if (clusterConfiguration.partitionCount() + bootstrappedPartitions.size()
+        != desiredPartitionCount) {
+      return Either.left(
+          new InvalidRequest(
+              String.format(
+                  "Cannot start scale up: clusterConfiguration.partitionCount(%d) + bootstrappedPartitions(%d) != desiredPartitionCount(%d)",
+                  clusterConfiguration.partitionCount(),
+                  bootstrappedPartitions.size(),
+                  desiredPartitionCount)));
+    }
+    final var operations =
+        // Generate the operations for the "coordinator" node, which is the node with the lowest
+        // id.
+        // See ClusterConfigurationCoordinatorSupplier
+        clusterConfiguration.members().entrySet().stream().min(Entry.comparingByKey()).stream()
+            .map(Entry::getKey)
+            .flatMap(
+                id ->
+                    Stream.of(
+                        new StartPartitionScaleUp(id, desiredPartitionCount),
+                        new AwaitRedistributionCompletion(
+                            id, desiredPartitionCount, bootstrappedPartitions),
+                        new AwaitRelocationCompletion(
+                            id, desiredPartitionCount, bootstrappedPartitions)))
+            .map(ClusterConfigurationChangeOperation.class::cast)
+            .toList();
+    if (operations.isEmpty()) {
+      return Either.left(
+          new InvalidRequest("Cannot scale up: no members registered in the configuration."));
+    }
+    return Either.right(operations);
   }
 }
