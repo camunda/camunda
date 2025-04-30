38b10c4e19d6be2cf428f46e8cf71abd4994add1
Carlo Sana <carlo.sana@camunda.com>
2025-04-30 15:08:15 +0200

    test: add test case when broker returns exception

--- zeebe/broker/src/test/java/io/camunda/zeebe/broker/partitioning/scaling/BrokerClientPartitionScalingExecutorTest.java
+++ zeebe/broker/src/test/java/io/camunda/zeebe/broker/partitioning/scaling/BrokerClientPartitionScalingExecutorTest.java
@@ -14,13 +14,16 @@ import static org.mockito.Mockito.verify;
 
 import io.camunda.zeebe.broker.client.api.BrokerClient;
 import io.camunda.zeebe.broker.client.api.BrokerResponseConsumer;
+import io.camunda.zeebe.broker.client.api.BrokerResponseException;
 import io.camunda.zeebe.broker.client.api.dto.BrokerRequest;
 import io.camunda.zeebe.gateway.impl.broker.request.GetScaleUpProgress;
 import io.camunda.zeebe.protocol.impl.record.value.scaling.ScaleRecord;
 import io.camunda.zeebe.scheduler.testing.TestConcurrencyControl;
+import io.camunda.zeebe.util.Either;
 import java.time.Duration;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -43,9 +46,7 @@ public class BrokerClientPartitionScalingExecutorTest {
   @Test
   public void shouldFailFutureWhenInvalidDesiredPartitionCount() {
     final var scaleRecord = new ScaleRecord().setDesiredPartitionCount(2);
-    final var future = awaitRedistributionWith(scaleRecord);
-
-    assertThat(future)
+    assertThat(awaitRedistributionWith(Either.right(scaleRecord)))
         .completesExceptionallyWithin(Duration.ZERO)
         .withThrowableThat()
         .withMessageContaining(
@@ -56,10 +57,9 @@ public class BrokerClientPartitionScalingExecutorTest {
   public void shouldFailFutureResponseWhenNotAllPartitionsAreReady() {
     final var scaleRecord =
         new ScaleRecord().setDesiredPartitionCount(5).setRedistributedPartitionsProperty(Set.of(4));
-    final var future = awaitRedistributionWith(scaleRecord);
 
     // then
-    assertThat(future)
+    assertThat(awaitRedistributionWith(Either.right(scaleRecord)))
         .completesExceptionallyWithin(Duration.ZERO)
         .withThrowableThat()
         .withMessageContaining(
@@ -74,15 +74,25 @@ public class BrokerClientPartitionScalingExecutorTest {
             .setRedistributedPartitionsProperty(Set.of(4, 5));
 
     // then
-    assertThat(awaitRedistributionWith(scaleRecord)).isCompleted();
+    assertThat(awaitRedistributionWith(Either.right(scaleRecord))).isCompleted();
+  }
+
+  @Test
+  public void shoulFailFutureWhenBrokerReturnsError() {
+    assertThat(awaitRedistributionWith(Either.left(new BrokerResponseException("expected"))))
+        .completesExceptionallyWithin(Duration.ZERO)
+        .withThrowableThat()
+        .withCause(new BrokerResponseException("expected"));
   }
 
   @SuppressWarnings("unchecked")
-  private CompletableFuture<Void> awaitRedistributionWith(final ScaleRecord scaleRecord) {
+  private CompletableFuture<Void> awaitRedistributionWith(
+      final Either<Throwable, ScaleRecord> scaleRecord) {
     // given
     doNothing().when(brokerClient).sendRequestWithRetry(any(), any(), any());
     final var responseConsumerCaptor = ArgumentCaptor.forClass(BrokerResponseConsumer.class);
     final var requestCaptor = ArgumentCaptor.forClass(BrokerRequest.class);
+    final var throwableCaptor = ArgumentCaptor.forClass(Consumer.class);
 
     // when
     final var future =
@@ -90,7 +100,8 @@ public class BrokerClientPartitionScalingExecutorTest {
             .awaitRedistributionCompletion(5, Set.of(4, 5), Duration.ofSeconds(5))
             .toCompletableFuture();
     verify(brokerClient)
-        .sendRequestWithRetry(requestCaptor.capture(), responseConsumerCaptor.capture(), any());
+        .sendRequestWithRetry(
+            requestCaptor.capture(), responseConsumerCaptor.capture(), throwableCaptor.capture());
     final var request = requestCaptor.getValue();
     assertThat(request).isInstanceOf(GetScaleUpProgress.class);
     final var getScaleUpProgress = (GetScaleUpProgress) request;
@@ -102,7 +113,9 @@ public class BrokerClientPartitionScalingExecutorTest {
               assertThat(requestRecord.getDesiredPartitionCount()).isEqualTo(5);
             });
 
-    responseConsumerCaptor.getValue().accept(1L, scaleRecord);
+    scaleRecord.ifRightOrLeft(
+        record -> responseConsumerCaptor.getValue().accept(1L, record),
+        ex -> throwableCaptor.getValue().accept(ex));
     return future;
   }
 }
