81a06afe8c431dc7501b2b452bbc58b3eaa92e5a
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-05-06 14:03:37 +0200

    style: apply licensing/formatting

--- zeebe/benchmarks/project/src/main/java/io/camunda/zeebe/ProcessInstanceMonitor.java
+++ zeebe/benchmarks/project/src/main/java/io/camunda/zeebe/ProcessInstanceMonitor.java
@@ -1,9 +1,17 @@
 /*
- * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
- * one or more contributor license agreements. See the NOTICE file distributed
- * with this work for additional information regarding copyright ownership.
- * Licensed under the Camunda License 1.0. You may not use this file
- * except in compliance with the Camunda License 1.0.
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package io.camunda.zeebe;
 
@@ -16,7 +24,6 @@ import static io.camunda.zeebe.ProcessInstanceMonitor.MetricsDoc.LATENCY;
 import static io.camunda.zeebe.ProcessInstanceMonitor.MetricsDoc.MONITORING;
 
 import io.camunda.zeebe.OperateClient.HttpResponseException;
-import io.camunda.zeebe.OperateClient.ProcessInstance;
 import io.camunda.zeebe.util.micrometer.ExtendedMeterDocumentation;
 import io.micrometer.common.docs.KeyName;
 import io.micrometer.core.instrument.Counter;
