adb39e64ceea75403f8c700d5b78425ec92227c1
Matt Bear <matthew.bear@camunda.com>
2025-05-12 15:39:48 -0500

    test: use junit5 to properly skip test

--- operate/common/src/test/java/io/camunda/operate/util/StackdriverJSONLayoutTest.java
+++ operate/common/src/test/java/io/camunda/operate/util/StackdriverJSONLayoutTest.java
@@ -18,8 +18,8 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.test.appender.ListAppender;
 import org.apache.logging.log4j.core.test.junit.LoggerContextRule;
-import org.junit.Ignore;
 import org.junit.Rule;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -36,7 +36,7 @@ public class StackdriverJSONLayoutTest {
   private final ObjectReader jsonReader = new ObjectMapper().reader();
 
   @Test
-  @Ignore
+  @Disabled
   public void testLayout() throws Exception {
     // having Stackdriver appender activated
     final LoggerContext ctx = loggerRule.getLoggerContext();
