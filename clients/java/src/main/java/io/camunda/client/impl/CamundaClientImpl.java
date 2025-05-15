464547c560bb29cd134ec1687a61ef3263671f86
ana.vinogradova <ana.vinogradova@camunda.com>
2025-05-15 09:21:00 +0100

    feat: adding support for unassigning role from group in the `CamundaClient`

--- clients/java/src/main/java/io/camunda/client/impl/CamundaClientImpl.java
+++ clients/java/src/main/java/io/camunda/client/impl/CamundaClientImpl.java
@@ -73,6 +73,7 @@ import io.camunda.client.api.command.TopologyRequestStep1;
 import io.camunda.client.api.command.UnassignGroupFromTenantCommandStep1;
 import io.camunda.client.api.command.UnassignMappingFromGroupStep1;
 import io.camunda.client.api.command.UnassignRoleFromTenantCommandStep1;
+import io.camunda.client.api.command.UnassignRoleFromGroupCommandStep1;
 import io.camunda.client.api.command.UnassignUserFromGroupCommandStep1;
 import io.camunda.client.api.command.UnassignUserTaskCommandStep1;
 import io.camunda.client.api.command.UpdateAuthorizationCommandStep1;
@@ -177,6 +178,7 @@ import io.camunda.client.impl.command.TopologyRequestImpl;
 import io.camunda.client.impl.command.UnassignGroupFromTenantCommandImpl;
 import io.camunda.client.impl.command.UnassignMappingFromGroupCommandImpl;
 import io.camunda.client.impl.command.UnassignRoleFromTenantCommandImpl;
+import io.camunda.client.impl.command.UnassignRoleFromGroupCommandImpl;
 import io.camunda.client.impl.command.UnassignUserFromGroupCommandImpl;
 import io.camunda.client.impl.command.UnassignUserTaskCommandImpl;
 import io.camunda.client.impl.command.UpdateAuthorizationCommandImpl;
@@ -856,6 +858,11 @@ public final class CamundaClientImpl implements CamundaClient {
   @Override
   public RolesByTenantSearchRequest newRolesByTenantSearchRequest(final String tenantId) {
     return new RolesByTenantSearchRequestImpl(httpClient, jsonMapper, tenantId);
+  }
+
+  @Override
+  public UnassignRoleFromGroupCommandStep1 newUnassignRoleFromGroupCommand() {
+    return new UnassignRoleFromGroupCommandImpl(httpClient);
   }
 
   @Override
