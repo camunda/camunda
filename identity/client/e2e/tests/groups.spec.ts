be56b7b673c566a2bb3eda36651d21aa7c3dfb36
vitorwtavares <vitor.tavares@camunda.com>
2025-05-16 10:22:57 -0300

    test: unskip groups tests and fix locators and utils

--- identity/client/e2e/tests/groups.spec.ts
+++ identity/client/e2e/tests/groups.spec.ts
@@ -13,8 +13,6 @@ import { relativizePath } from "../utils/relativizePaths";
 import { LOGIN_CREDENTIALS } from "../utils/constants";
 import { waitForItemInList } from "../utils/waitForItemInList";
 
-import { IS_GROUPS_BASE_PAGE_INTEGRATED } from "../../src/feature-flags";
-
 const NEW_GROUP = {
   groupId: "testgroupid",
   name: "Test Group",
@@ -34,63 +32,60 @@ test.beforeEach(async ({ page, loginPage, groupsPage }) => {
   await expect(page).toHaveURL(relativizePath(Paths.groups()));
 });
 
-test.describe[IS_GROUPS_BASE_PAGE_INTEGRATED ? "serial" : "skip"](
-  "groups CRUD",
-  () => {
-    test("creates a group", async ({ page, groupsPage }) => {
-      await expect(
-        groupsPage.groupsList.getByRole("cell", { name: NEW_GROUP.name }),
-      ).not.toBeVisible();
+test.describe.serial("groups CRUD", () => {
+  test("creates a group", async ({ page, groupsPage }) => {
+    await expect(
+      groupsPage.groupsList.getByRole("cell", { name: NEW_GROUP.name }),
+    ).not.toBeVisible();
 
-      await groupsPage.createGroupButton.click();
-      await expect(groupsPage.createGroupModal).toBeVisible();
-      await groupsPage.createGroupIdField.fill(NEW_GROUP.groupId);
-      await groupsPage.createNameField.fill(NEW_GROUP.name);
-      await groupsPage.createDescriptionField.fill(NEW_GROUP.description);
-      await groupsPage.createGroupModalCreateButton.click();
-      await expect(groupsPage.createGroupModal).not.toBeVisible();
+    await groupsPage.createGroupButton.click();
+    await expect(groupsPage.createGroupModal).toBeVisible();
+    await groupsPage.createGroupIdField.fill(NEW_GROUP.groupId);
+    await groupsPage.createNameField.fill(NEW_GROUP.name);
+    await groupsPage.createDescriptionField.fill(NEW_GROUP.description);
+    await groupsPage.createGroupModalCreateButton.click();
+    await expect(groupsPage.createGroupModal).not.toBeVisible();
 
-      const item = groupsPage.groupsList.getByRole("cell", {
-        name: NEW_GROUP.name,
-      });
-
-      await waitForItemInList(page, item);
+    const item = groupsPage.groupsList.getByRole("cell", {
+      name: NEW_GROUP.name,
     });
 
-    test("edits a group", async ({ page, groupsPage }) => {
-      await expect(
-        groupsPage.groupsList.getByRole("cell", { name: NEW_GROUP.name }),
-      ).toBeVisible();
+    await waitForItemInList(page, item);
+  });
 
-      await groupsPage.editGroupButton(NEW_GROUP.name).click();
-      await expect(groupsPage.editGroupModal).toBeVisible();
-      await groupsPage.editNameField.fill(EDITED_GROUP.name);
-      await groupsPage.editDescriptionField.fill(EDITED_GROUP.description);
-      await groupsPage.editGroupModalUpdateButton.click();
-      await expect(groupsPage.editGroupModal).not.toBeVisible();
+  test("edits a group", async ({ page, groupsPage }) => {
+    await expect(
+      groupsPage.groupsList.getByRole("cell", { name: NEW_GROUP.name }),
+    ).toBeVisible();
 
-      const item = groupsPage.groupsList.getByRole("cell", {
-        name: EDITED_GROUP.name,
-      });
+    await groupsPage.editGroupButton(NEW_GROUP.name).click();
+    await expect(groupsPage.editGroupModal).toBeVisible();
+    await groupsPage.editNameField.fill(EDITED_GROUP.name);
+    await groupsPage.editDescriptionField.fill(EDITED_GROUP.description);
+    await groupsPage.editGroupModalUpdateButton.click();
+    await expect(groupsPage.editGroupModal).not.toBeVisible();
 
-      await waitForItemInList(page, item);
+    const item = groupsPage.groupsList.getByRole("cell", {
+      name: EDITED_GROUP.name,
     });
 
-    test("deletes a group", async ({ page, groupsPage }) => {
-      await expect(
-        groupsPage.groupsList.getByRole("cell", { name: EDITED_GROUP.name }),
-      ).toBeVisible();
+    await waitForItemInList(page, item);
+  });
 
-      await groupsPage.deleteGroupButton(EDITED_GROUP.name).click();
-      await expect(groupsPage.deleteGroupModal).toBeVisible();
-      await groupsPage.deleteGroupModalDeleteButton.click();
-      await expect(groupsPage.deleteGroupModal).not.toBeVisible();
+  test("deletes a group", async ({ page, groupsPage }) => {
+    await expect(
+      groupsPage.groupsList.getByRole("cell", { name: EDITED_GROUP.name }),
+    ).toBeVisible();
 
-      const item = groupsPage.groupsList.getByRole("cell", {
-        name: EDITED_GROUP.name,
-      });
+    await groupsPage.deleteGroupButton(EDITED_GROUP.name).click();
+    await expect(groupsPage.deleteGroupModal).toBeVisible();
+    await groupsPage.deleteGroupModalDeleteButton.click();
+    await expect(groupsPage.deleteGroupModal).not.toBeVisible();
 
-      await waitForItemInList(page, item, { shouldBeVisible: false });
+    const item = groupsPage.groupsList.getByRole("cell", {
+      name: EDITED_GROUP.name,
     });
-  },
-);
+
+    await waitForItemInList(page, item, { shouldBeVisible: false });
+  });
+});
