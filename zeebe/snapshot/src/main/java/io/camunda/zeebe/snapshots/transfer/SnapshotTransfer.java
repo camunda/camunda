64553303490f1db6ac8b3feab9f68256c851f8dd
Carlo Sana <carlo.sana@camunda.com>
2025-05-19 10:18:27 +0200

    docs: javadocs in SnapshotTransfer

--- zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/transfer/SnapshotTransfer.java
+++ zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/transfer/SnapshotTransfer.java
@@ -17,6 +17,13 @@ import io.camunda.zeebe.snapshots.SnapshotChunk;
 import io.camunda.zeebe.util.collection.Tuple;
 import java.util.UUID;
 
+/**
+ * Receive a complete snapshot from a {@link SnapshotTransferService} by repeatedly asking for the
+ * next chunk until all chunks are received. No retry is done on the futures, if you want support
+ * for retry, wrap {@param service} with retries.
+ *
+ * <p>Snapshots are received in the {@param snapshotStore}.
+ */
 public class SnapshotTransfer {
 
   private final SnapshotTransferService service;
