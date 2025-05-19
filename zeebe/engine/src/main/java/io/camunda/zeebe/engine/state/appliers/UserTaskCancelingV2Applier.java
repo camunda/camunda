228a9288fbe84bf0b3fdbe5526b9b6985816c8d7
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-05-19 09:21:17 +0300

    feat: adjust UserTaskCancelingApplier to clean request metadata
    that may have been persisted by a previous transition

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/UserTaskCancelingV2Applier.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/UserTaskCancelingV2Applier.java
@@ -8,13 +8,17 @@
 package io.camunda.zeebe.engine.state.appliers;
 
 import io.camunda.zeebe.engine.state.TypedEventApplier;
+import io.camunda.zeebe.engine.state.immutable.RequestMetadataState.RequestMetadata;
 import io.camunda.zeebe.engine.state.immutable.UserTaskState.LifecycleState;
 import io.camunda.zeebe.engine.state.mutable.MutableElementInstanceState;
 import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
+import io.camunda.zeebe.engine.state.mutable.MutableRequestMetadataState;
 import io.camunda.zeebe.engine.state.mutable.MutableUserTaskState;
 import io.camunda.zeebe.engine.state.mutable.MutableVariableState;
 import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
+import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.UserTaskIntent;
+import java.util.Set;
 
 public final class UserTaskCancelingV2Applier
     implements TypedEventApplier<UserTaskIntent, UserTaskRecord> {
@@ -22,11 +26,13 @@ public final class UserTaskCancelingV2Applier
   private final MutableUserTaskState userTaskState;
   private final MutableVariableState variableState;
   private final MutableElementInstanceState elementInstanceState;
+  private final MutableRequestMetadataState requestMetadataState;
 
   public UserTaskCancelingV2Applier(final MutableProcessingState processingState) {
     userTaskState = processingState.getUserTaskState();
     variableState = processingState.getVariableState();
     elementInstanceState = processingState.getElementInstanceState();
+    requestMetadataState = processingState.getRequestMetadataState();
   }
 
   @Override
@@ -36,7 +42,7 @@ public final class UserTaskCancelingV2Applier
     // Clean up data that may have been persisted by a previous transition
     variableState.removeVariableDocumentState(value.getElementInstanceKey());
     userTaskState.deleteIntermediateStateIfExists(key);
-    userTaskState.deleteRecordRequestMetadata(key);
+    removeRequestMetadata(value);
     resetTaskListenerIndices(value);
 
     // Persist new data related to "canceling" user task transition
@@ -52,5 +58,14 @@ public final class UserTaskCancelingV2Applier
       userTaskInstance.resetTaskListenerIndices();
       elementInstanceState.updateInstance(userTaskInstance);
     }
+  }
+
+  private void removeRequestMetadata(final UserTaskRecord value) {
+    final var eligibleValueTypes = Set.of(ValueType.USER_TASK, ValueType.VARIABLE_DOCUMENT);
+    requestMetadataState
+        .findAllByScopeKey(value.getElementInstanceKey())
+        .filter(m -> eligibleValueTypes.contains(m.valueType()))
+        .map(RequestMetadata::record)
+        .forEach(requestMetadataState::remove);
   }
 }
