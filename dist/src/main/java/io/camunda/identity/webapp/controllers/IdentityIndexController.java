549a53d7d43ff531445b163854a24fb10593546a
Ben Sheppard <ben.sheppard@camunda.com>
2025-05-20 19:33:22 +0100

    feat: pass config into the identity client code from the backend to handle feature toggles

--- dist/src/main/java/io/camunda/identity/webapp/controllers/IdentityIndexController.java
+++ dist/src/main/java/io/camunda/identity/webapp/controllers/IdentityIndexController.java
@@ -7,11 +7,15 @@
  */
 package io.camunda.identity.webapp.controllers;
 
+import io.camunda.security.configuration.SecurityConfiguration;
+import io.camunda.security.entity.AuthenticationMethod;
 import io.camunda.webapps.controllers.WebappsRequestForwardManager;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.security.SecureRandom;
+import java.util.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,14 +23,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IdentityIndexController {
+  private static final String CONTENT_SECURITY_POLICY_HEADER = "Content-Security-Policy";
 
-  @Autowired private ServletContext context;
+  private final ServletContext context;
 
-  @Autowired private WebappsRequestForwardManager webappsRequestForwardManager;
+  private final WebappsRequestForwardManager webappsRequestForwardManager;
+
+  private final SecurityConfiguration securityConfiguration;
+
+  public IdentityIndexController(
+      final ServletContext context,
+      final WebappsRequestForwardManager webappsRequestForwardManager,
+      final SecurityConfiguration securityConfiguration) {
+    this.context = context;
+    this.webappsRequestForwardManager = webappsRequestForwardManager;
+    this.securityConfiguration = securityConfiguration;
+  }
 
   @GetMapping("/identity")
-  public String identity(final Model model) throws IOException {
+  public String identity(final Model model, final HttpServletResponse response) throws IOException {
+    final var envJsNonce = generateNonce();
     model.addAttribute("contextPath", context.getContextPath() + "/identity/");
+    model.addAttribute("clientConfigNonce", envJsNonce);
+    model.addAttribute(
+        "isOidc",
+        securityConfiguration.getAuthentication().getMethod() == AuthenticationMethod.OIDC);
+    setContentSecurePolicyHeader(response, envJsNonce);
     return "identity/index";
   }
 
@@ -34,5 +56,27 @@ public class IdentityIndexController {
       value = {"/identity/", "/identity/{regex:[\\w-]+}", "/identity/**/{regex:[\\w-]+}"})
   public String forwardToIdentity(final HttpServletRequest request) {
     return webappsRequestForwardManager.forward(request, "identity");
+  }
+
+  private void setContentSecurePolicyHeader(
+      final HttpServletResponse response, final String envJsNonce) {
+    response.addHeader(
+        CONTENT_SECURITY_POLICY_HEADER,
+        "default-src 'self';"
+            + "script-src 'self' 'nonce-"
+            + envJsNonce
+            + "';"
+            + "style-src 'self' 'unsafe-inline';"
+            + "frame-src 'none';"
+            + "object-src 'none';"
+            + "media-src 'none';");
+  }
+
+  private String generateNonce() {
+    final var secureRandom = new SecureRandom();
+    final var secureBytes = new byte[16];
+    secureRandom.nextBytes(secureBytes);
+
+    return Base64.getEncoder().encodeToString(secureBytes);
   }
 }
