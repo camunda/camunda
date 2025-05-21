c5df058b4749e3d8606758843f2a51c79db30cb2
Sebastian Stamm <sebastian.stamm@camunda.com>
2025-05-21 11:49:11 +0200

    feat: define mock response for jobs search endpoint

--- operate/client/src/modules/mock-server/handlers.ts
+++ operate/client/src/modules/mock-server/handlers.ts
@@ -6,8 +6,47 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {RequestHandler} from 'msw';
+import {GetJobsResponseBody} from '@vzeta/camunda-api-zod-schemas/operate';
+import {RequestHandler, rest} from 'msw';
 
-const handlers: RequestHandler[] = [];
+const mockEndpoints = [
+  rest.post('/v2/jobs/search', async (req, res, ctx) => {
+    const mockResponse: GetJobsResponseBody = {
+      items: [
+        {
+          jobKey: '2251799813916032',
+          type: 'type',
+          worker: 'worker',
+          state: 'CREATED',
+          kind: 'EXECUTION_LISTENER',
+          listenerEventType: 'UNSPECIFIED',
+          retries: 3,
+          isDenied: false,
+          deniedReason: 'deniedReason',
+          hasFailedWithRetriesLeft: false,
+          errorCode: 'errorCode',
+          errorMessage: 'errorMessage',
+          deadline: 'deadline',
+          endTime: 'endTime',
+          processDefinitionId: 'processDefinitionId',
+          processDefinitionKey: 'processDefinitionKey',
+          processInstanceKey: 'processInstanceKey',
+          elementId: 'elementId',
+          elementInstanceKey: 'elementInstanceKey',
+          tenantId: 'tenantId',
+        },
+      ],
+      page: {
+        totalItems: 1,
+        firstSortValues: [0, 1],
+        lastSortValues: [0, 1],
+      },
+    };
+
+    return res(ctx.json(mockResponse));
+  }),
+];
+
+const handlers: RequestHandler[] = [...mockEndpoints];
 
 export {handlers};
