ccd34b6753cfd911b89e6c0316701b43232cb1a1
ana.vinogradova <ana.vinogradova@camunda.com>
2025-05-21 15:13:07 +0100

    test: adding negative scenario integration tests

---
+++ qa/acceptance-tests/src/test/java/io/camunda/it/client/ClientsByRoleSearchIntegrationTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.command.ProblemException;
+import io.camunda.qa.util.multidb.MultiDbTest;
+import io.camunda.zeebe.test.util.Strings;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+@MultiDbTest
+public class ClientsByRoleSearchIntegrationTest {
+
+  private static CamundaClient camundaClient;
+
+  private static final String EXISTING_ROLE_ID = Strings.newRandomValidIdentityId();
+
+  @BeforeAll
+  static void setup() {
+    createRole(EXISTING_ROLE_ID, "ARoleName", "description");
+
+    Awaitility.await("Role is created and exported")
+        .ignoreExceptionsInstanceOf(ProblemException.class)
+        .untilAsserted(
+            () -> {
+              final var role = camundaClient.newRoleGetRequest(EXISTING_ROLE_ID).send().join();
+              assertThat(role).isNotNull();
+              assertThat(role.getRoleId()).isEqualTo(EXISTING_ROLE_ID);
+              assertThat(role.getName()).isEqualTo("ARoleName");
+              assertThat(role.getDescription()).isEqualTo("description");
+            });
+  }
+
+  @Test
+  void shouldSearchClientsByRoleId() {}
+
+  @Test
+  void shouldRejectSearchClientsByRoleIdIfEmptyRoleId() {
+    // when / then
+    assertThatThrownBy(() -> camundaClient.newClientsByRoleSearchRequest("").send().join())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("roleId must not be empty");
+  }
+
+  @Test
+  void shouldRejectSearchClientsByRoleIdIfNullRoleId() {
+    // when / then
+    assertThatThrownBy(() -> camundaClient.newClientsByRoleSearchRequest(null).send().join())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("roleId must not be null");
+  }
+
+  private static void createRole(
+      final String roleId, final String roleName, final String description) {
+    camundaClient
+        .newCreateRoleCommand()
+        .roleId(roleId)
+        .name(roleName)
+        .description(description)
+        .send()
+        .join();
+  }
+}
