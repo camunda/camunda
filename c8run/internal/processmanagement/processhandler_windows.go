3afe333da05d8079d71d312810fbed130c94a420
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-05-22 21:05:52 +0100

    fix: change the windows test for is a process running

--- c8run/internal/processmanagement/processhandler_windows.go
+++ c8run/internal/processmanagement/processhandler_windows.go
@@ -4,14 +4,24 @@
 package processmanagement
 
 import (
-	"os"
-	"syscall"
+	"errors"
+
+	"golang.org/x/sys/windows"
 )
 
 func (p *ProcessHandler) IsPidRunning(pid int) bool {
-	process, err := os.FindProcess(pid)
+	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
 	if err != nil {
+		if errors.Is(err, windows.ERROR_INVALID_PARAMETER) {
+			return false // PID doesn't exist
+		}
 		return false
 	}
-	return process.Signal(syscall.Signal(0)) == nil
+	defer windows.CloseHandle(h)
+
+	var code uint32
+	if err = windows.GetExitCodeProcess(h, &code); err != nil {
+		return false
+	}
+	return code == uint32(259)
 }
