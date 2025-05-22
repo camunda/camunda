4bdc048d7c43511a6b584a85e9743ad0df4d8db8
Carlo Sana <carlo.sana@camunda.com>
2025-05-22 14:51:54 +0200

    feat: add informations about migrations in PartitionTransitionContext
    (cherry picked from commit a66ceb575ed939872c807b16a11d88d37f3a8333)

--- zeebe/broker/src/test/java/io/camunda/zeebe/broker/system/partitions/TestPartitionTransitionContext.java
+++ zeebe/broker/src/test/java/io/camunda/zeebe/broker/system/partitions/TestPartitionTransitionContext.java
@@ -23,6 +23,7 @@ import io.camunda.zeebe.broker.partitioning.PartitionAdminAccess;
 import io.camunda.zeebe.broker.partitioning.topology.TopologyManager;
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.broker.system.monitoring.DiskSpaceUsageMonitor;
+import io.camunda.zeebe.broker.system.monitoring.HealthTreeMetrics;
 import io.camunda.zeebe.broker.system.partitions.impl.AsyncSnapshotDirector;
 import io.camunda.zeebe.broker.transport.adminapi.AdminApiRequestHandler;
 import io.camunda.zeebe.broker.transport.backupapi.BackupApiRequestHandler;
@@ -42,6 +43,7 @@ import io.camunda.zeebe.snapshots.PersistedSnapshotStore;
 import io.camunda.zeebe.stream.api.StreamClock.ControllableStreamClock;
 import io.camunda.zeebe.stream.impl.StreamProcessor;
 import io.camunda.zeebe.transport.impl.AtomixServerTransport;
+import io.camunda.zeebe.util.health.ComponentTreeListener;
 import io.camunda.zeebe.util.health.HealthMonitor;
 import io.camunda.zeebe.util.micrometer.MicrometerUtil;
 import io.camunda.zeebe.util.micrometer.MicrometerUtil.PartitionKeyNames;
@@ -82,9 +84,12 @@ public class TestPartitionTransitionContext implements PartitionTransitionContex
   private ControllableStreamClock clock;
   private MeterRegistry partitionMeterRegistry;
   private MeterRegistry transitionMeterRegistry;
+  private boolean migrationsPerformed;
+  private final ComponentTreeListener healthMetrics;
 
   public TestPartitionTransitionContext() {
     transitionMeterRegistry = MicrometerUtil.wrap(startupMeterRegistry, PartitionKeyNames.tags(1));
+    healthMetrics = new HealthTreeMetrics(transitionMeterRegistry);
   }
 
   @Override
@@ -331,6 +336,21 @@ public class TestPartitionTransitionContext implements PartitionTransitionContex
   @Override
   public void setPartitionTransitionMeterRegistry(final MeterRegistry transitionMeterRegistry) {
     this.transitionMeterRegistry = transitionMeterRegistry;
+  }
+
+  @Override
+  public void setMigrationsPerformed(final boolean migrationsPerformed) {
+    this.migrationsPerformed = migrationsPerformed;
+  }
+
+  @Override
+  public boolean areMigrationsPerformed() {
+    return migrationsPerformed;
+  }
+
+  @Override
+  public ComponentTreeListener getComponentTreeListener() {
+    return healthMetrics;
   }
 
   public void setGatewayBrokerTransport(final AtomixServerTransport gatewayBrokerTransport) {
