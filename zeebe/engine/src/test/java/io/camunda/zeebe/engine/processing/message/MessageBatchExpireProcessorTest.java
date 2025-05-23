28a7dab5415b79e89451349492f59d4e8bb7a665
berkaycanbc <berkay.can@camunda.com>
2025-05-23 13:16:18 +0200

    test: add message batch expire unit tests
    Three unit tests are added:
    - Messages should be exported with message body
      - This is the main requirement of the PR.
    - Messages that are not found in the state should be skipped
      - There could be a case where `MessageTimeToLiveChecker` could pick up
       expired messages and write a batch of messages to expire into the log
       stream. But before processing expire command batch, a message can
       already be correlated and removed from the state. In that case, we
       should not append any `EXPIRED` events because it is invalid.
    - If no `EXPIRED` events appended for the batch, we should reject the
    command
      - As a rule of thumb, every command must produce at least one
      follow-up record, so we know that the command was processed.
      Therefore, if no `EXPIRED` event is written, we should at least
      append a rejection to know what happened in the engine.
    
    (cherry picked from commit 7dfa4a1670eb8264854ee65d288527f8b1730fba)

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/message/MessageBatchExpireProcessorTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/message/MessageBatchExpireProcessorTest.java
@@ -7,19 +7,26 @@
  */
 package io.camunda.zeebe.engine.processing.message;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
+import io.camunda.zeebe.engine.state.immutable.MessageState;
+import io.camunda.zeebe.engine.state.message.StoredMessage;
 import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
 import io.camunda.zeebe.protocol.impl.record.value.message.MessageBatchRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageRecord;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.intent.MessageIntent;
 import io.camunda.zeebe.stream.api.records.ExceededBatchRecordSizeException;
 import io.camunda.zeebe.stream.impl.records.RecordBatchEntry;
 import io.camunda.zeebe.stream.impl.records.UnwrittenRecord;
@@ -29,8 +36,10 @@ import org.mockito.Mockito;
 public final class MessageBatchExpireProcessorTest {
 
   private final StateWriter stateWriter = Mockito.mock(StateWriter.class);
+  private final TypedRejectionWriter rejectionWriter = Mockito.mock(TypedRejectionWriter.class);
+  private final MessageState messageState = Mockito.mock(MessageState.class);
   final MessageBatchExpireProcessor messageBatchExpireProcessor =
-      new MessageBatchExpireProcessor(stateWriter);
+      new MessageBatchExpireProcessor(stateWriter, rejectionWriter, messageState);
 
   @Test
   public void shouldStopProcessingWhenExceedingBatchLimit() {
@@ -43,6 +52,9 @@ public final class MessageBatchExpireProcessorTest {
             .addMessageKey(3)
             .addMessageKey(4);
 
+    doReturn(mock(StoredMessage.class)).when(messageState).getMessage(1);
+    doReturn(mock(StoredMessage.class)).when(messageState).getMessage(2);
+    doReturn(mock(StoredMessage.class)).when(messageState).getMessage(3);
     doNothing().when(stateWriter).appendFollowUpEvent(eq(1L), any(), any());
     doNothing().when(stateWriter).appendFollowUpEvent(eq(2L), any(), any());
 
@@ -58,5 +70,130 @@ public final class MessageBatchExpireProcessorTest {
 
     // then
     verify(stateWriter, times(3)).appendFollowUpEvent(anyLong(), any(), any());
+  }
+
+  @Test
+  public void shouldExportWithMessageBody() {
+    // given
+    final var messageBatchRecord = new MessageBatchRecord().addMessageKey(1).addMessageKey(2);
+
+    doReturn(
+            new StoredMessage()
+                .setMessage(
+                    new MessageRecord()
+                        .setName("message1")
+                        .setCorrelationKey("correlationKey1")
+                        .setTimeToLive(100L)))
+        .when(messageState)
+        .getMessage(1L);
+    doReturn(
+            new StoredMessage()
+                .setMessage(
+                    new MessageRecord()
+                        .setName("message2")
+                        .setCorrelationKey("correlationKey2")
+                        .setTimeToLive(101L)))
+        .when(messageState)
+        .getMessage(2L);
+    doNothing().when(stateWriter).appendFollowUpEvent(eq(1L), any(), any());
+    doNothing().when(stateWriter).appendFollowUpEvent(eq(2L), any(), any());
+
+    // when
+    messageBatchExpireProcessor.processRecord(
+        new UnwrittenRecord(-1, 1, messageBatchRecord, new RecordMetadata()));
+
+    // then
+    verify(stateWriter, times(1))
+        .appendFollowUpEvent(
+            eq(1L),
+            eq(MessageIntent.EXPIRED),
+            argThat(
+                record -> {
+                  final var messageRecord = (MessageRecord) record;
+                  return messageRecord.getName().equals("message1")
+                      && messageRecord.getCorrelationKey().equals("correlationKey1")
+                      && messageRecord.getTimeToLive() == 100L;
+                }));
+
+    verify(stateWriter, times(1))
+        .appendFollowUpEvent(
+            eq(2L),
+            eq(MessageIntent.EXPIRED),
+            argThat(
+                record -> {
+                  final var messageRecord = (MessageRecord) record;
+                  return messageRecord.getName().equals("message2")
+                      && messageRecord.getCorrelationKey().equals("correlationKey2")
+                      && messageRecord.getTimeToLive() == 101L;
+                }));
+  }
+
+  @Test
+  public void shouldSkipNotFoundMessages() {
+    // given
+    final var messageBatchRecord = new MessageBatchRecord().addMessageKey(1).addMessageKey(2);
+
+    doReturn(
+            new StoredMessage()
+                .setMessage(
+                    new MessageRecord()
+                        .setName("message1")
+                        .setCorrelationKey("correlationKey1")
+                        .setTimeToLive(100L)))
+        .when(messageState)
+        .getMessage(1L);
+    doReturn(null).when(messageState).getMessage(2L);
+    doNothing().when(stateWriter).appendFollowUpEvent(eq(1L), any(), any());
+
+    // when
+    messageBatchExpireProcessor.processRecord(
+        new UnwrittenRecord(-1, 1, messageBatchRecord, new RecordMetadata()));
+
+    // then
+    verify(stateWriter, times(1))
+        .appendFollowUpEvent(
+            eq(1L),
+            eq(MessageIntent.EXPIRED),
+            argThat(
+                record -> {
+                  final var messageRecord = (MessageRecord) record;
+                  return messageRecord.getName().equals("message1")
+                      && messageRecord.getCorrelationKey().equals("correlationKey1")
+                      && messageRecord.getTimeToLive() == 100L;
+                }));
+
+    verify(stateWriter, times(0)).appendFollowUpEvent(eq(2L), eq(MessageIntent.EXPIRED), any());
+  }
+
+  @Test
+  public void shouldRejectIfNoMessagesFound() {
+    // given
+    final var messageBatchRecord = new MessageBatchRecord().addMessageKey(1).addMessageKey(2);
+
+    doReturn(
+            new StoredMessage()
+                .setMessage(
+                    new MessageRecord()
+                        .setName("message1")
+                        .setCorrelationKey("correlationKey1")
+                        .setTimeToLive(100L)))
+        .when(messageState)
+        .getMessage(1L);
+    doReturn(null).when(messageState).getMessage(1L);
+    doReturn(null).when(messageState).getMessage(2L);
+
+    // when
+    final UnwrittenRecord record =
+        new UnwrittenRecord(-1, 1, messageBatchRecord, new RecordMetadata());
+    messageBatchExpireProcessor.processRecord(record);
+
+    // then
+    verify(rejectionWriter, times(1))
+        .appendRejection(
+            record,
+            RejectionType.NOT_FOUND,
+            "Expected to expire 2 messages in a batch, but none of the messages were found in the state.");
+    verify(stateWriter, times(0)).appendFollowUpEvent(eq(1L), eq(MessageIntent.EXPIRED), any());
+    verify(stateWriter, times(0)).appendFollowUpEvent(eq(2L), eq(MessageIntent.EXPIRED), any());
   }
 }
--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/util/RecordToWrite.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/util/RecordToWrite.java
@@ -12,6 +12,7 @@ import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
 import io.camunda.zeebe.protocol.impl.record.UnifiedRecordValue;
 import io.camunda.zeebe.protocol.impl.record.value.job.JobBatchRecord;
 import io.camunda.zeebe.protocol.impl.record.value.job.JobRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageBatchRecord;
 import io.camunda.zeebe.protocol.impl.record.value.message.MessageRecord;
 import io.camunda.zeebe.protocol.impl.record.value.message.ProcessMessageSubscriptionRecord;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationRecord;
@@ -24,6 +25,7 @@ import io.camunda.zeebe.protocol.record.RecordType;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.JobBatchIntent;
 import io.camunda.zeebe.protocol.record.intent.JobIntent;
+import io.camunda.zeebe.protocol.record.intent.MessageBatchIntent;
 import io.camunda.zeebe.protocol.record.intent.MessageIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceCreationIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
@@ -33,6 +35,7 @@ import io.camunda.zeebe.protocol.record.intent.ProcessMessageSubscriptionIntent;
 import io.camunda.zeebe.protocol.record.intent.TimerIntent;
 import io.camunda.zeebe.protocol.record.intent.VariableDocumentIntent;
 import io.camunda.zeebe.protocol.record.value.JobRecordValue;
+import io.camunda.zeebe.protocol.record.value.MessageBatchRecordValue;
 import io.camunda.zeebe.protocol.record.value.MessageRecordValue;
 import io.camunda.zeebe.protocol.record.value.ProcessInstanceCreationRecordValue;
 import io.camunda.zeebe.protocol.record.value.ProcessInstanceMigrationRecordValue;
@@ -156,6 +159,30 @@ public final class RecordToWrite implements LogAppendEntry {
     return this;
   }
 
+<<<<<<< HEAD
+=======
+  public RecordToWrite messageBatch(final MessageBatchRecordValue value) {
+    recordMetadata.valueType(ValueType.MESSAGE_BATCH).intent(MessageBatchIntent.EXPIRE);
+    unifiedRecordValue = (MessageBatchRecord) value;
+    return this;
+  }
+
+  public RecordToWrite scale(final ScaleIntent intent, final ScaleRecord value) {
+    recordMetadata.valueType(ValueType.SCALE).intent(intent);
+    unifiedRecordValue = value;
+    return this;
+  }
+
+  public RecordToWrite adHocSubProcessActivityActivation(
+      final AdHocSubProcessActivityActivationRecordValue value) {
+    recordMetadata
+        .valueType(ValueType.AD_HOC_SUB_PROCESS_ACTIVITY_ACTIVATION)
+        .intent(AdHocSubProcessActivityActivationIntent.ACTIVATE);
+    unifiedRecordValue = (AdHocSubProcessActivityActivationRecord) value;
+    return this;
+  }
+
+>>>>>>> b2a9e061 (test: add more message batch expire tests)
   /**
    * Used to refer to the record that caused this record to be written. For example, when you want
    * to write a Job Created event that was the result of the processing of a Service Task
