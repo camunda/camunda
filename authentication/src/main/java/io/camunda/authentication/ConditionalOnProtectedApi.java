386a1dbb91bc4603ec1d32e1d6f0913a10ca993f
romansmirnov <roman.smirnov@camunda.com>
2025-05-23 16:50:54 +0200

    fix: resolve security filter chain conflict

--- authentication/src/main/java/io/camunda/authentication/ConditionalOnProtectedApi.java
+++ authentication/src/main/java/io/camunda/authentication/ConditionalOnProtectedApi.java
@@ -7,34 +7,28 @@
  */
 package io.camunda.authentication;
 
+import io.camunda.authentication.ConditionalOnProtectedApi.OnProtectedApiCondition;
 import io.camunda.authentication.ConditionalOnUnprotectedApi.ApiUnprotectedCondition;
-import io.camunda.authentication.config.AuthenticationProperties;
-import io.camunda.security.configuration.AuthenticationConfiguration;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.util.Optional;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
-import org.springframework.core.env.Environment;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Documented
-@Conditional(ApiUnprotectedCondition.class)
-public @interface ConditionalOnUnprotectedApi {
+@Conditional(OnProtectedApiCondition.class)
+public @interface ConditionalOnProtectedApi {
 
-  class ApiUnprotectedCondition implements Condition {
+  class OnProtectedApiCondition implements Condition {
     @Override
     public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
-      final Environment env = context.getEnvironment();
-      return Optional.ofNullable(
-              env.getProperty(AuthenticationProperties.API_UNPROTECTED, Boolean.class))
-          .orElse(AuthenticationConfiguration.DEFAULT_UNPROTECTED_API);
+      return !new ApiUnprotectedCondition().matches(context, metadata);
     }
   }
 }
