639dfcf54be10c55b6f4bac0d4e91d0c0831000e
Kristin Komschow <162317489+kristinkomschow@users.noreply.github.com>
2025-05-26 17:16:03 +0200

    fix: use correct logger class in DecisionType
    (cherry picked from commit b09f9e7d04836985dad3032b1c9e3ed757a937ad)

--- operate/schema/src/main/java/io/camunda/operate/entities/dmn/DecisionType.java
+++ operate/schema/src/main/java/io/camunda/operate/entities/dmn/DecisionType.java
@@ -16,7 +16,6 @@
  */
 package io.camunda.operate.entities.dmn;
 
-import io.camunda.operate.entities.FlowNodeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +26,7 @@ public enum DecisionType {
   UNSPECIFIED,
   UNKNOWN;
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(FlowNodeType.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(DecisionType.class);
 
   public static DecisionType fromString(final String decisionType) {
     if (decisionType == null) {
