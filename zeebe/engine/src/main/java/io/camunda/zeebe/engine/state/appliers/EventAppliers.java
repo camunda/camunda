dd42ff195d0da694e6366978977cd872d18b3b15
Christian Thiel <christian.thiel@holisticon.de>
2025-05-27 15:19:55 +0200

    feat: add event appliers for BatchOperationPartitionLifecycleRecordValue #32512

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
@@ -621,6 +621,9 @@ public final class EventAppliers implements EventApplier {
     register(
         BatchOperationExecutionIntent.COMPLETED,
         new BatchOperationCompletedApplier(state.getBatchOperationState()));
+    register(
+        BatchOperationIntent.PARTITION_COMPLETED,
+        new BatchOperationPartitionCompletedApplier(state.getBatchOperationState()));
   }
 
   private void registerIdentitySetupAppliers() {
