6e74971b5dc1858537c746e4a1b964c589b4bdf4
Christian Thiel <christian.thiel@holisticon.de>
2025-05-27 15:18:08 +0200

    feat: register BatchOperationPartitionLifecycleRecordValue as distributable #32512

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationSetupProcessors.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationSetupProcessors.java
@@ -19,6 +19,7 @@ import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessors;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.immutable.ProcessingState;
 import io.camunda.zeebe.engine.state.immutable.ScheduledTaskState;
+import io.camunda.zeebe.engine.state.routing.RoutingInfo;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationChunkIntent;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationExecutionIntent;
@@ -40,7 +41,8 @@ public final class BatchOperationSetupProcessors {
       final SearchClientsProxy searchClientsProxy,
       final ProcessingState processingState,
       final EngineConfiguration engineConfiguration,
-      final int partitionId) {
+      final int partitionId,
+      final RoutingInfo routingInfo) {
     final var batchExecutionHandlers =
         Map.of(
             BatchOperationType.CANCEL_PROCESS_INSTANCE,
@@ -60,7 +62,11 @@ public final class BatchOperationSetupProcessors {
             ValueType.BATCH_OPERATION_CREATION,
             BatchOperationIntent.CREATE,
             new BatchOperationCreateProcessor(
-                writers, keyGenerator, commandDistributionBehavior, authorizationCheckBehavior))
+                writers,
+                keyGenerator,
+                commandDistributionBehavior,
+                authorizationCheckBehavior,
+                routingInfo))
         .onCommand(
             ValueType.BATCH_OPERATION_CREATION,
             BatchOperationIntent.START,
