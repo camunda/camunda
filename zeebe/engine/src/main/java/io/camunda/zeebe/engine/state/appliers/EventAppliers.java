dc497305d03118d8ae954d44cfb60b922ca6ad17
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-05-28 23:23:35 +0300

    feat: register noop appliers for `AsyncRequestMetadata` intents
    Registered NOOP event appliers for `AsyncRequestMetadataIntent.RECEIVED` and `PROCESSED`.
    Full state handling will be implemented in follow-up commits.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
@@ -19,6 +19,7 @@ import io.camunda.zeebe.engine.state.mutable.MutableProcessingState;
 import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
 import io.camunda.zeebe.protocol.record.RecordValue;
 import io.camunda.zeebe.protocol.record.intent.AdHocSubProcessActivityActivationIntent;
+import io.camunda.zeebe.protocol.record.intent.AsyncRequestMetadataIntent;
 import io.camunda.zeebe.protocol.record.intent.AuthorizationIntent;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationChunkIntent;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationExecutionIntent;
@@ -139,6 +140,7 @@ public final class EventAppliers implements EventApplier {
     registerMappingAppliers(state);
     registerBatchOperationAppliers(state);
     registerIdentitySetupAppliers();
+    registerAsyncRequestMetadataAppliers();
 
     return this;
   }
@@ -629,6 +631,11 @@ public final class EventAppliers implements EventApplier {
 
   private void registerIdentitySetupAppliers() {
     register(IdentitySetupIntent.INITIALIZED, NOOP_EVENT_APPLIER);
+  }
+
+  private void registerAsyncRequestMetadataAppliers() {
+    register(AsyncRequestMetadataIntent.RECEIVED, NOOP_EVENT_APPLIER);
+    register(AsyncRequestMetadataIntent.PROCESSED, NOOP_EVENT_APPLIER);
   }
 
   private <I extends Intent> void register(final I intent, final TypedEventApplier<I, ?> applier) {
