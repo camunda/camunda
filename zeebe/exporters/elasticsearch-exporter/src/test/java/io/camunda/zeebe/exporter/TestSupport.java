4ba16a91b8f28f9be95fdc0de8dc8ceb36998ec1
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-05-28 22:27:52 +0300

    feat: add ElasticSearch exporter support for new record value
    Added necessary configurations and index template to enable exporting
    `AsyncRequestMetadata` records by Camunda ElasticSearch exporter.

--- zeebe/exporters/elasticsearch-exporter/src/test/java/io/camunda/zeebe/exporter/TestSupport.java
+++ zeebe/exporters/elasticsearch-exporter/src/test/java/io/camunda/zeebe/exporter/TestSupport.java
@@ -69,6 +69,7 @@ final class TestSupport {
       case BATCH_OPERATION_LIFECYCLE_MANAGEMENT -> config.batchOperationLifecycleManagement = value;
       case AD_HOC_SUB_PROCESS_ACTIVITY_ACTIVATION ->
           config.adHocSubProcessActivityActivation = value;
+      case ASYNC_REQUEST_METADATA -> config.asyncRequestMetadata = value;
       default ->
           throw new IllegalArgumentException(
               "No known indexing configuration option for value type " + valueType);
