674099a1294023fcf3340e9ce6450c620fd69cb1
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-05-28 22:14:29 +0300

    feat: register Jackson mixin and randomizer for new record value
    Registered mixin to support correct (de)serialization of `ImmutableAsyncRequestMetadataRecordValue` via Jackson.
    This enables resolving the `intent` field to its proper enum type based on the associated `valueType`.
    
    Also registered a custom randomizer in `ProtocolFactory#registerRandomizers` to generate randomized
    `ImmutableAsyncRequestMetadataRecordValue` instances for testing purposes with `valueType` and relevant `intent`.

--- zeebe/protocol-jackson/src/main/java/io/camunda/zeebe/protocol/jackson/ZeebeProtocolModule.java
+++ zeebe/protocol-jackson/src/main/java/io/camunda/zeebe/protocol/jackson/ZeebeProtocolModule.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.camunda.zeebe.protocol.record.ImmutableRecord;
 import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.value.ImmutableAsyncRequestMetadataRecordValue;
 import io.camunda.zeebe.protocol.record.value.ImmutableCommandDistributionRecordValue;
 
 /**
@@ -36,6 +37,7 @@ public final class ZeebeProtocolModule extends SimpleModule {
   public ZeebeProtocolModule() {
     setMixInAnnotation(ImmutableRecord.Builder.class, RecordMixin.class);
     setMixInAnnotation(ImmutableCommandDistributionRecordValue.Builder.class, RecordMixin.class);
+    setMixInAnnotation(ImmutableAsyncRequestMetadataRecordValue.Builder.class, RecordMixin.class);
   }
 
   @Override
