a3e67c8377d8f67f04da280044c6d6f2bf2af362
Alex Bremard <alex.bremard@camunda.com>
2025-05-30 14:19:11 +0700

    feat: migrate full variable value logic

--- operate/client/src/modules/queries/variables/useVariable.ts
+++ operate/client/src/modules/queries/variables/useVariable.ts
@@ -6,32 +6,37 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {useQuery, UseQueryResult} from '@tanstack/react-query';
+import {skipToken, useQuery, UseQueryResult} from '@tanstack/react-query';
 import {Variable} from '@vzeta/camunda-api-zod-schemas/process-management';
 import {RequestError} from 'modules/request';
 import {getVariable} from 'modules/api/v2/variables/getVariable';
 
 const VARIABLES_GET_QUERY_KEY = 'variablesGet';
 
-function getQueryKey(variableKey: string) {
+function getQueryKey(variableKey?: string) {
   return [VARIABLES_GET_QUERY_KEY, variableKey];
 }
 
-function useVariable<T = Variable>(
-  variableKey: string,
-  select?: (data: Variable) => T,
-): UseQueryResult<T, RequestError> {
+function useVariable<T = Variable>({
+  variableKey,
+  select,
+}: {
+  variableKey?: string;
+  select?: (data: Variable) => T;
+}): UseQueryResult<T, RequestError> {
   return useQuery({
     queryKey: getQueryKey(variableKey),
-    queryFn: async () => {
-      const {response, error} = await getVariable(variableKey);
+    queryFn: !!variableKey
+      ? async () => {
+          const {response, error} = await getVariable(variableKey);
 
-      if (response !== null) {
-        return response;
-      }
+          if (response !== null) {
+            return response;
+          }
 
-      throw error;
-    },
+          throw error;
+        }
+      : skipToken,
     select,
   });
 }
