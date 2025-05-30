65f3cf465a2a6fdd5a03aaf74e89eac933807bec
ana.vinogradova <ana.vinogradova@camunda.com>
2025-05-30 14:47:09 +0100

    feat: adding corresponding authorization services methods + tests

--- service/src/main/java/io/camunda/service/AuthorizationServices.java
+++ service/src/main/java/io/camunda/service/AuthorizationServices.java
@@ -9,6 +9,8 @@ package io.camunda.service;
 
 import io.camunda.search.clients.AuthorizationSearchClient;
 import io.camunda.search.entities.AuthorizationEntity;
+import io.camunda.search.exception.CamundaSearchException;
+import io.camunda.search.exception.ErrorMessages;
 import io.camunda.search.query.AuthorizationQuery;
 import io.camunda.search.query.SearchQueryBuilders;
 import io.camunda.search.query.SearchQueryResult;
@@ -26,6 +28,7 @@ import io.camunda.zeebe.protocol.record.value.AuthorizationOwnerType;
 import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
 import io.camunda.zeebe.protocol.record.value.PermissionType;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -132,6 +135,25 @@ public class AuthorizationServices
             .setResourceId(request.resourceId())
             .setPermissionTypes(request.permissionTypes());
     return sendBrokerRequest(brokerRequest);
+  }
+
+  public AuthorizationEntity getAuthorization(final long authorizationKey) {
+    return findAuthorization(authorizationKey)
+        .orElseThrow(
+            () ->
+                new CamundaSearchException(
+                    ErrorMessages.ERROR_NOT_FOUND_AUTHORIZATION_BY_KEY.formatted(authorizationKey),
+                    CamundaSearchException.Reason.NOT_FOUND));
+  }
+
+  public Optional<AuthorizationEntity> findAuthorization(final long authorizationKey) {
+    return search(
+            SearchQueryBuilders.authorizationSearchQuery()
+                .filter(f -> f.authorizationKey(authorizationKey))
+                .build())
+        .items()
+        .stream()
+        .findFirst();
   }
 
   public CompletableFuture<AuthorizationRecord> deleteAuthorization(final long authorizationKey) {
