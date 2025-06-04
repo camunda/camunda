fb7373c50d7ffaad998c340c2ea31982c53bf227
maryam.nematigavzan <maryam.nematigavzan@camunda.com>
2025-06-04 12:34:32 +0200

    fix: apply spotless

--- authentication/src/main/java/io/camunda/authentication/CamundaOAuthPrincipalService.java
+++ authentication/src/main/java/io/camunda/authentication/CamundaOAuthPrincipalService.java
@@ -25,7 +25,6 @@ import io.camunda.zeebe.protocol.record.value.EntityType;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
