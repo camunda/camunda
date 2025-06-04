75471c7c40589a6d171174eaf93aa6fd1d97abc1
Lena Schönburg <lena@fairburg.xyz>
2025-06-04 15:23:24 +0200

    perf: engine maintains a cache of compiled mapping rules

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/AuthorizationCheckBehavior.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/identity/AuthorizationCheckBehavior.java
@@ -50,6 +50,8 @@ public final class AuthorizationCheckBehavior {
   private final AuthorizationState authorizationState;
   private final MappingState mappingState;
   private final MembershipState membershipState;
+  private final MappingRuleMatcher.CompilationCache compiledMappingRulesCache =
+      new MappingRuleMatcher.CompilationCache();
 
   private final boolean authorizationsEnabled;
   private final boolean multiTenancyEnabled;
@@ -395,7 +397,8 @@ public final class AuthorizationCheckBehavior {
     final var claims =
         (Map<String, Object>)
             command.getAuthorizations().getOrDefault(Authorization.USER_TOKEN_CLAIMS, Map.of());
-    return MappingRuleMatcher.matchingRules(mappingState.getAll().stream(), claims);
+    return MappingRuleMatcher.matchingRules(
+        compiledMappingRulesCache, mappingState.getAll().stream(), claims);
   }
 
   public static final class AuthorizationRequest {
