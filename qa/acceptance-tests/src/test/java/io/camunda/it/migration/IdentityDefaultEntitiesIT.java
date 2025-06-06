56f8f7edf771a64c432a9a36ba21cf3ad5fbed53
Nicola Puppa <nicola.puppa@camunda.com>
2025-06-06 17:34:03 +0200

    test: add IT test to verify groups migration
    The test verify that the groups are migrated from old identity management to the OC.
    With groups also user group memberships are migrated.

--- qa/acceptance-tests/src/test/java/io/camunda/it/migration/IdentityDefaultEntitiesIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/migration/IdentityDefaultEntitiesIT.java
@@ -10,53 +10,76 @@ package io.camunda.it.migration;
 import static io.camunda.it.migration.IdentityMigrationTestUtil.CAMUNDA_IDENTITY_RESOURCE_SERVER;
 import static io.camunda.it.migration.IdentityMigrationTestUtil.IDENTITY_CLIENT;
 import static io.camunda.it.migration.IdentityMigrationTestUtil.IDENTITY_CLIENT_SECRET;
+import static io.camunda.it.migration.IdentityMigrationTestUtil.ZEEBE_CLIENT_AUDIENCE;
 import static io.camunda.it.migration.IdentityMigrationTestUtil.externalIdentityUrl;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 import io.camunda.application.Profile;
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.search.response.Group;
+import io.camunda.client.impl.oauth.OAuthCredentialsProviderBuilder;
 import io.camunda.qa.util.cluster.TestCamundaApplication;
-import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
-import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
-import io.camunda.zeebe.test.util.testcontainers.TestSearchContainers;
-import org.junit.jupiter.api.Disabled;
+import io.camunda.qa.util.multidb.MultiDbTest;
+import io.camunda.qa.util.multidb.MultiDbTestApplication;
+import java.time.Duration;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@ZeebeIntegration
+@MultiDbTest
 @Testcontainers(parallel = true)
-@Disabled
 public class IdentityDefaultEntitiesIT {
 
-  @Container
-  private static final GenericContainer<?> POSTGRES = IdentityMigrationTestUtil.getPostgres();
-
-  @Container
-  private static final GenericContainer<?> KEYCLOAK =
-      IdentityMigrationTestUtil.getKeycloak(POSTGRES);
-
-  @Container
-  private static final GenericContainer<?> IDENTITY =
-      IdentityMigrationTestUtil.getManagementIdentity(POSTGRES, KEYCLOAK);
-
-  @Container
-  private static final ElasticsearchContainer ELASTICSEARCH =
-      TestSearchContainers.createDefeaultElasticsearchContainer();
-
-  @TestZeebe(autoStart = false)
-  final TestCamundaApplication camunda =
+  @MultiDbTestApplication(managedLifecycle = false)
+  static final TestCamundaApplication CAMUNDA =
       new TestCamundaApplication()
           .withSecurityConfig(cfg -> cfg.getAuthorizations().setEnabled(true));
 
-  @Test
-  void canMigrateOldDefaultEntities() {
+  static CamundaClient client;
 
-    // when -- Camunda is started with identity migration profile
-    camunda
+  @Container
+  private static final KeycloakContainer KEYCLOAK = IdentityMigrationTestUtil.getKeycloak();
+
+  @Container
+  private static final GenericContainer<?> IDENTITY =
+      IdentityMigrationTestUtil.getManagementIdentity(KEYCLOAK)
+          // create groups
+          .withEnv("KEYCLOAK_GROUPS_0_NAME", "groupA")
+          .withEnv("KEYCLOAK_GROUPS_1_NAME", "groupB")
+          .withEnv("KEYCLOAK_GROUPS_2_NAME", "groupC")
+          // create users and assign them to groups
+          .withEnv("KEYCLOAK_USERS_0_EMAIL", "user0@email.com")
+          .withEnv("KEYCLOAK_USERS_0_FIRST-NAME", "user0")
+          .withEnv("KEYCLOAK_USERS_0_LAST-NAME", "user0")
+          .withEnv("KEYCLOAK_USERS_0_USERNAME", "user0")
+          .withEnv("KEYCLOAK_USERS_0_PASSWORD", "password")
+          .withEnv("KEYCLOAK_USERS_0_GROUPS_0", "groupA")
+          .withEnv("KEYCLOAK_USERS_0_GROUPS_1", "groupB")
+          .withEnv("KEYCLOAK_USERS_1_EMAIL", "user1@email.com")
+          .withEnv("KEYCLOAK_USERS_1_FIRST-NAME", "user1")
+          .withEnv("KEYCLOAK_USERS_1_LAST-NAME", "user1")
+          .withEnv("KEYCLOAK_USERS_1_USERNAME", "user1")
+          .withEnv("KEYCLOAK_USERS_1_PASSWORD", "password")
+          .withEnv("KEYCLOAK_USERS_1_GROUPS_0", "groupC")
+          .waitingFor(
+              new HttpWaitStrategy()
+                  .forPort(8082)
+                  .forPath("/actuator/health/readiness")
+                  .allowInsecure()
+                  .forStatusCode(200)
+                  .withReadTimeout(Duration.ofSeconds(10))
+                  .withStartupTimeout(Duration.ofMinutes(5)));
+
+  @BeforeAll
+  public static void setup() {
+    CAMUNDA
         .withAdditionalProfile(Profile.IDENTITY_MIGRATION)
-        .withProperty("camunda.database.url", "http://" + ELASTICSEARCH.getHttpHostAddress())
-        .withProperty("camunda.migration.identity.zeebe.gatewayAddress", camunda.restAddress())
         .withProperty(
             "camunda.migration.identity.managementIdentity.baseUrl", externalIdentityUrl(IDENTITY))
         .withProperty(
@@ -68,9 +91,49 @@ public class IdentityDefaultEntitiesIT {
             "camunda.migration.identity.management-identity.client-secret", IDENTITY_CLIENT_SECRET)
         .withProperty(
             "camunda.migration.identity.management-identity.audience",
-            CAMUNDA_IDENTITY_RESOURCE_SERVER)
-        .start();
+            CAMUNDA_IDENTITY_RESOURCE_SERVER);
+    CAMUNDA.start();
 
-    // then -- the migration did not fail
+    client =
+        CAMUNDA
+            .newClientBuilder()
+            .credentialsProvider(
+                new OAuthCredentialsProviderBuilder()
+                    .clientId(IDENTITY_CLIENT)
+                    .clientSecret(IDENTITY_CLIENT_SECRET)
+                    .audience(ZEEBE_CLIENT_AUDIENCE)
+                    .authorizationServerUrl(
+                        "http://localhost:%d%s/protocol/openid-connect/token"
+                            .formatted(KEYCLOAK.getFirstMappedPort(), "/realms/camunda-platform"))
+                    .build())
+            .defaultRequestTimeout(Duration.ofSeconds(10))
+            .build();
+  }
+
+  @Test
+  void canMigrateGroups() {
+    Awaitility.await()
+        .atMost(Duration.ofSeconds(10))
+        .untilAsserted(
+            () -> {
+              final var groups = client.newGroupsSearchRequest().send().join();
+              assertThat(groups.items().size()).isEqualTo(3);
+            });
+
+    final var groups = client.newGroupsSearchRequest().send().join();
+    assertThat(groups.items().size()).isEqualTo(3);
+    assertThat(groups.items())
+        .map(Group::getGroupId, Group::getName)
+        .containsExactly(
+            tuple("groupa", "groupA"), tuple("groupb", "groupB"), tuple("groupc", "groupC"));
+    final var userA = client.newUsersByGroupSearchRequest("groupa").send().join();
+    assertThat(userA.items().size()).isEqualTo(1);
+    assertThat(userA.items().getFirst().getUsername()).isEqualTo("user0@email.com");
+    final var userB = client.newUsersByGroupSearchRequest("groupb").send().join();
+    assertThat(userB.items().size()).isEqualTo(1);
+    assertThat(userB.items().getFirst().getUsername()).isEqualTo("user0@email.com");
+    final var userC = client.newUsersByGroupSearchRequest("groupc").send().join();
+    assertThat(userC.items().size()).isEqualTo(1);
+    assertThat(userC.items().getFirst().getUsername()).isEqualTo("user1@email.com");
   }
 }
