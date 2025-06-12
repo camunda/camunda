d101d475388e6535a51c1e22ca2ef7c62e6e25bc
megglos <sebastian.bathke@camunda.com>
2025-06-12 11:26:48 +0200

    feat: await cluster availbility for identity migration
    The identity migration makes use of the broker client and thus
    depends on a successful cluster join before starting the migration.

--- migration/identity-migration/src/main/java/io/camunda/migration/identity/IdentityMigrator.java
+++ migration/identity-migration/src/main/java/io/camunda/migration/identity/IdentityMigrator.java
@@ -9,22 +9,35 @@ package io.camunda.migration.identity;
 
 import io.camunda.migration.api.Migrator;
 import io.camunda.migration.identity.config.IdentityMigrationProperties;
+import io.camunda.migration.identity.config.cluster.ClusterProperties;
+import io.camunda.zeebe.broker.client.api.BrokerTopologyManager;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @EnableConfigurationProperties(IdentityMigrationProperties.class)
 @Component("identity-migrator")
 public class IdentityMigrator implements Migrator {
+  private static final Logger LOG = LoggerFactory.getLogger(IdentityMigrator.class);
 
   private final List<MigrationHandler<?>> handlers;
+  private final BrokerTopologyManager brokerTopologyManager;
+  private final IdentityMigrationProperties identityMigrationProperties;
 
-  public IdentityMigrator(final List<MigrationHandler<?>> handlers) {
+  public IdentityMigrator(
+      final List<MigrationHandler<?>> handlers,
+      final BrokerTopologyManager brokerTopologyManager,
+      final IdentityMigrationProperties identityMigrationProperties) {
     this.handlers = handlers;
+    this.brokerTopologyManager = brokerTopologyManager;
+    this.identityMigrationProperties = identityMigrationProperties;
   }
 
   @Override
   public Void call() {
+    awaitClusterJoin();
     migrate();
     return null;
   }
@@ -32,6 +45,35 @@ public class IdentityMigrator implements Migrator {
   private void migrate() {
     for (final MigrationHandler<?> handler : handlers) {
       handler.migrate();
+    }
+  }
+
+  private void awaitClusterJoin() {
+    final ClusterProperties clusterConfig = identityMigrationProperties.getCluster();
+    int remainingClusterStateAttempts = clusterConfig.getAwaitClusterJoinMaxAttempts();
+    boolean joinedCluster = false;
+    do {
+      joinedCluster = brokerTopologyManager.getTopology().isInitialized();
+      LOG.info("Waiting for Orchestration Cluster Broker availability...");
+      try {
+        Thread.sleep(clusterConfig.getAwaitClusterJoinRetryInterval().toMillis());
+      } catch (final InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      remainingClusterStateAttempts--;
+    } while (!joinedCluster && remainingClusterStateAttempts > 0);
+
+    if (joinedCluster) {
+      LOG.info("Successfully connected to Orchestration Cluster, starting migration.");
+    } else {
+      final String message =
+          "Failed to connect to Orchestration Cluster within %s."
+              .formatted(
+                  clusterConfig
+                      .getAwaitClusterJoinRetryInterval()
+                      .multipliedBy(clusterConfig.getAwaitClusterJoinMaxAttempts()));
+      LOG.error(message);
+      throw new RuntimeException(message);
     }
   }
 }
