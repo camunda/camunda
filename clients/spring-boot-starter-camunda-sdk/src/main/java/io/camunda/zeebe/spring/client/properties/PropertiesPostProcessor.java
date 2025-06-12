c24fcdd32198517300bf8a926aa25bf746b370a3
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-06-12 12:43:35 +0200

    feat: copied over properties post processor from main and adjusted to match 8.7

--- clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/zeebe/spring/client/properties/PropertiesPostProcessor.java
+++ clients/spring-boot-starter-camunda-sdk/src/main/java/io/camunda/zeebe/spring/client/properties/PropertiesPostProcessor.java
@@ -16,6 +16,7 @@
 package io.camunda.zeebe.spring.client.properties;
 
 import io.camunda.zeebe.spring.client.properties.CamundaClientProperties.ClientMode;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +35,8 @@ import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 
 public class PropertiesPostProcessor implements EnvironmentPostProcessor {
+  public static final String CAMUNDA_CLIENT_MODE = "camunda.client.mode";
+
   private static final String OVERRIDE_PREFIX = "camunda.client.zeebe.override.";
   private static final List<String> LEGACY_OVERRIDE_PREFIX =
       List.of("zeebe.client.worker.override.");
@@ -52,34 +55,14 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
   }
 
   private void mapLegacyOverrides(final ConfigurableEnvironment environment) {
-    final Map<String, Object> properties = new HashMap<>();
     environment.getPropertySources().stream()
         .filter(o -> EnumerablePropertySource.class.isAssignableFrom(o.getClass()))
         .map(EnumerablePropertySource.class::cast)
+        .flatMap(propertySource -> mapLegacyOverrideFromSource(environment, propertySource))
         .forEach(
-            propertySource -> {
-              for (final String propertyName : propertySource.getPropertyNames()) {
-                for (final String prefix : LEGACY_OVERRIDE_PREFIX) {
-                  final String normalizedPropertyName =
-                      propertyName.replaceAll("_", ".").toLowerCase();
-                  if (normalizedPropertyName.startsWith(prefix)) {
-                    final String newPropertyName =
-                        OVERRIDE_PREFIX + normalizedPropertyName.substring(prefix.length());
-                    if (!properties.containsKey(newPropertyName)
-                        && !environment.containsProperty(newPropertyName)) {
-                      log.debug(
-                          String.format(
-                              "Mapping worker override from '%s' to '%s'",
-                              propertyName, newPropertyName));
-                      properties.put(newPropertyName, propertySource.getProperty(propertyName));
-                    }
-                  }
-                }
-              }
-            });
-    final PropertySource<?> propertySource =
-        new MapPropertySource("zeebe-client-legacy-worker-overrides.properties", properties);
-    environment.getPropertySources().addFirst(propertySource);
+            propertySource ->
+                addMapPropertySourceFirst(
+                    propertySource.sourceName(), propertySource.properties(), environment));
   }
 
   private Stream<MappedPropertySource> mapLegacyOverrideFromSource(
@@ -112,7 +95,6 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
   }
 
   private void mapLegacyProperties(final ConfigurableEnvironment environment) {
-    final Map<String, Object> properties = new HashMap<>();
     final List<CamundaClientLegacyPropertiesMapping> mappings =
         CamundaClientLegacyPropertiesMappingsLoader.load();
     mappings.stream()
@@ -125,14 +107,6 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
                     mappedPropertySource.sourceName(),
                     mappedPropertySource.properties(),
                     environment));
-  }
-
-  private void addMapPropertySourceFirst(
-      final String sourceName,
-      final Map<String, Object> properties,
-      final ConfigurableEnvironment environment) {
-    final PropertySource<?> propertySource = new MapPropertySource(sourceName, properties);
-    environment.getPropertySources().addFirst(propertySource);
   }
 
   private Optional<MappedPropertySource> detectPropertyValue(
@@ -189,7 +163,7 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
 
   private void processClientMode(final ConfigurableEnvironment environment) {
     try {
-      ClientMode clientMode = environment.getProperty("camunda.client.mode", ClientMode.class);
+      ClientMode clientMode = environment.getProperty(CAMUNDA_CLIENT_MODE, ClientMode.class);
       if (clientMode == null) {
         if (isImplicitSaas(environment)) {
           clientMode = ClientMode.saas;
@@ -197,36 +171,47 @@ public class PropertiesPostProcessor implements EnvironmentPostProcessor {
           return;
         }
       }
-      final YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+
       final String propertiesFile = determinePropertiesFile(clientMode);
-      final ClassPathResource resource = new ClassPathResource(propertiesFile);
-      final List<PropertySource<?>> props = loader.load(propertiesFile, resource);
-      for (final PropertySource<?> prop : props) {
-        environment.getPropertySources().addLast(prop); // lowest priority
-      }
+      addYamlPropertySourceLast(propertiesFile, environment);
     } catch (final Exception e) {
       throw new IllegalStateException("Error while post processing camunda properties", e);
     }
   }
 
+  private void addMapPropertySourceFirst(
+      final String sourceName,
+      final Map<String, Object> properties,
+      final ConfigurableEnvironment environment) {
+    final PropertySource<?> propertySource = new MapPropertySource(sourceName, properties);
+    environment.getPropertySources().addFirst(propertySource);
+  }
+
+  private void addYamlPropertySourceLast(
+      final String propertiesFile, final ConfigurableEnvironment environment) throws IOException {
+    final YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+    final ClassPathResource resource = new ClassPathResource(propertiesFile);
+    final List<PropertySource<?>> props = loader.load(propertiesFile, resource);
+    for (final PropertySource<?> prop : props) {
+      environment.getPropertySources().addLast(prop); // lowest priority
+    }
+  }
+
   private boolean isImplicitSaas(final ConfigurableEnvironment environment) {
     if (environment.containsProperty("camunda.client.cloud.cluster-id")) {
-      log.debug("Cluster id detected, setting implicit saas mode");
+      log.info(
+          String.format(
+              "Implicit '%s' '%s' detected, will be used", CAMUNDA_CLIENT_MODE, ClientMode.saas));
       return true;
     }
     return false;
   }
 
   private String determinePropertiesFile(final ClientMode clientMode) {
-    switch (clientMode) {
-      case selfManaged -> {
-        return "modes/self-managed.yaml";
-      }
-      case saas -> {
-        return "modes/saas.yaml";
-      }
-      default -> throw new IllegalStateException("Unknown client mode " + clientMode);
-    }
+    return switch (clientMode) {
+      case selfManaged -> "modes/self-managed.yaml";
+      case saas -> "modes/saas.yaml";
+    };
   }
 
   private record MappedPropertySource(String sourceName, Map<String, Object> properties) {}
