9a6aef2968a4eda522034f30aaaf4d98773c9809
remco.westerhoud <remcowesterhoud@msn.com>
2025-06-13 09:23:56 +0200

    fix: gracefully handle non-existing parent
    If the parent is terminated the PI batch would result in an NPE. This results in a banned process instance. Obviously
    that's not desirable and we should be able to handle this case gracefully.
    By adding a simple null-check we can determine that the parent doesn't exist anymore, and simply reject this command.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceBatchActivateProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceBatchActivateProcessor.java
@@ -11,11 +11,14 @@ import io.camunda.zeebe.engine.EngineConfiguration;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableMultiInstanceBody;
 import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessor;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.immutable.ElementInstanceState;
 import io.camunda.zeebe.engine.state.immutable.ProcessState;
+import io.camunda.zeebe.engine.state.instance.ElementInstance;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceBatchRecord;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
+import io.camunda.zeebe.protocol.record.RejectionType;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceBatchIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
@@ -23,10 +26,14 @@ import io.camunda.zeebe.stream.api.state.KeyGenerator;
 
 public final class ProcessInstanceBatchActivateProcessor
     implements TypedRecordProcessor<ProcessInstanceBatchRecord> {
+
+  public static final String PARENT_NOT_FOUND_ERROR_MESSAGE =
+      "Expected to activate child for batch element instance, but no parent element instance found for key '%s'. The parent was likely terminated before processing this batch activation.";
   private final TypedCommandWriter commandWriter;
   private final KeyGenerator keyGenerator;
   private final ElementInstanceState elementInstanceState;
   private final ProcessState processState;
+  private final TypedRejectionWriter rejectionWriter;
 
   public ProcessInstanceBatchActivateProcessor(
       final Writers writers,
@@ -37,13 +44,25 @@ public final class ProcessInstanceBatchActivateProcessor
     this.keyGenerator = keyGenerator;
     this.elementInstanceState = elementInstanceState;
     this.processState = processState;
+    rejectionWriter = writers.rejection();
   }
 
   @Override
   public void processRecord(final TypedRecord<ProcessInstanceBatchRecord> record) {
     final var recordValue = record.getValue();
 
-    final ProcessInstanceRecord childInstanceRecord = createChildInstanceRecord(recordValue);
+    final var batchElementInstanceKey = record.getValue().getBatchElementInstanceKey();
+    final var parentElementInstance = elementInstanceState.getInstance(batchElementInstanceKey);
+    if (parentElementInstance == null) {
+      rejectionWriter.appendRejection(
+          record,
+          RejectionType.INVALID_STATE,
+          PARENT_NOT_FOUND_ERROR_MESSAGE.formatted(batchElementInstanceKey));
+      return;
+    }
+
+    final ProcessInstanceRecord childInstanceRecord =
+        createChildInstanceRecord(parentElementInstance);
     var amountOfChildInstancesToActivate = recordValue.getIndex();
     while (amountOfChildInstancesToActivate > 0) {
       if (canWriteCommands(record, childInstanceRecord)) {
@@ -59,9 +78,7 @@ public final class ProcessInstanceBatchActivateProcessor
   }
 
   private ProcessInstanceRecord createChildInstanceRecord(
-      final ProcessInstanceBatchRecord recordValue) {
-    final var parentElementInstance =
-        elementInstanceState.getInstance(recordValue.getBatchElementInstanceKey());
+      final ElementInstance parentElementInstance) {
     final var processDefinition =
         processState
             .getProcessByKeyAndTenant(
