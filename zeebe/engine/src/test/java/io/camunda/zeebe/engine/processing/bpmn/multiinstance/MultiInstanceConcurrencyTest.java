6889c1586eb2226bf31aa967a07e74a2bc2571f5
remco.westerhoud <remcowesterhoud@msn.com>
2025-06-13 09:22:56 +0200

    test: verify PI batch activate gets rejected if parent doesn't exist
    If the parent is terminated the PI batch would result in an NPE. This results in a banned process instance. Obviously
    that's not desirable and we should be able to handle this case gracefully. This test reproduces this scenario. By
    writing commands/events manually we can ensure we have a deterministic ordering. We make sure we have activated the MI
    body. After this we write a command to terminate the MI body, as well as another command to activate the PI batch. When
    processing the PI batch activate command the MI body will be terminated and we will reach our NPE.
    Instead of the NPE we expect that the command gets rejected gracefully, with a proper error message.
    
    (cherry picked from commit 1706ec211c781d39a248572f6695d0ad340e0c2f)

---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/bpmn/multiinstance/MultiInstanceConcurrencyTest.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.bpmn.multiinstance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.engine.util.EngineRule;
+import io.camunda.zeebe.engine.util.RecordToWrite;
+import io.camunda.zeebe.engine.util.Records;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
+import io.camunda.zeebe.protocol.record.intent.JobIntent;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceBatchIntent;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
+import io.camunda.zeebe.protocol.record.value.BpmnElementType;
+import io.camunda.zeebe.test.util.BrokerClassRuleHelper;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MultiInstanceConcurrencyTest {
+  @ClassRule
+  public static final EngineRule ENGINE = EngineRule.singlePartition().maxCommandsInBatch(1);
+
+  private static final String PROCESS_ID = "processId";
+  @Rule public final BrokerClassRuleHelper helper = new BrokerClassRuleHelper();
+
+  @Test
+  public void shouldRejectPiBatchActivateWhenBodyIsTerminated() {
+    // given
+    ENGINE
+        .deployment()
+        .withXmlResource(
+            Bpmn.createExecutableProcess(PROCESS_ID)
+                .startEvent()
+                .serviceTask("serviceTask1", t -> t.zeebeJobType("jobType1"))
+                .sequenceFlowId("task1-to-task2")
+                .serviceTask("serviceTask2", t -> t.zeebeJobType("jobType2"))
+                .multiInstance(
+                    mi ->
+                        mi.zeebeInputCollectionExpression("[1,2,3]")
+                            .zeebeInputElement("item")
+                            .parallel())
+                .endEvent()
+                .done())
+        .deploy();
+    final var processInstanceKey = ENGINE.processInstance().ofBpmnProcessId(PROCESS_ID).create();
+    final var serviceTask =
+        RecordingExporter.processInstanceRecords(ProcessInstanceIntent.ELEMENT_ACTIVATED)
+            .withProcessInstanceKey(processInstanceKey)
+            .withElementType(BpmnElementType.SERVICE_TASK)
+            .getFirst();
+    final var task1Job =
+        RecordingExporter.jobRecords(JobIntent.CREATED)
+            .withProcessInstanceKey(processInstanceKey)
+            .getFirst();
+    final var procDefKey = serviceTask.getValue().getProcessDefinitionKey();
+    final ProcessInstanceRecord sequenceFlow =
+        Records.processInstance(processInstanceKey, PROCESS_ID)
+            .setBpmnElementType(BpmnElementType.SEQUENCE_FLOW)
+            .setElementId("task1-to-task2")
+            .setFlowScopeKey(processInstanceKey)
+            .setProcessDefinitionKey(procDefKey);
+    final var multiInstance =
+        Records.processInstance(processInstanceKey, PROCESS_ID)
+            .setBpmnElementType(BpmnElementType.MULTI_INSTANCE_BODY)
+            .setElementId("serviceTask2")
+            .setFlowScopeKey(processInstanceKey)
+            .setProcessDefinitionKey(procDefKey);
+
+    // when
+    ENGINE.stop();
+    RecordingExporter.reset();
+    final var multiInstanceKey = task1Job.getKey() + 10000;
+    final var batchActivateKey = multiInstanceKey + 1;
+    // We need to ensure that the multi-instance body is terminated before we try to activate the
+    // first batch. In order to achieve we have to write our events and commands manually. If we
+    // don't do this the test won't be deterministic.
+    ENGINE.writeRecords(
+        RecordToWrite.event().key(task1Job.getKey()).job(JobIntent.COMPLETED, task1Job.getValue()),
+        RecordToWrite.event()
+            .key(serviceTask.getKey())
+            .processInstance(ProcessInstanceIntent.ELEMENT_COMPLETING, serviceTask.getValue()),
+        RecordToWrite.event()
+            .key(serviceTask.getKey())
+            .processInstance(ProcessInstanceIntent.ELEMENT_COMPLETED, serviceTask.getValue()),
+        RecordToWrite.event()
+            .key(-1L)
+            .processInstance(ProcessInstanceIntent.SEQUENCE_FLOW_TAKEN, sequenceFlow),
+        RecordToWrite.event()
+            .key(multiInstanceKey)
+            .processInstance(ProcessInstanceIntent.ELEMENT_ACTIVATING, multiInstance),
+        RecordToWrite.event()
+            .key(multiInstanceKey)
+            .processInstance(ProcessInstanceIntent.ELEMENT_ACTIVATED, multiInstance),
+        RecordToWrite.command()
+            .key(multiInstanceKey)
+            .processInstance(ProcessInstanceIntent.TERMINATE_ELEMENT, multiInstance),
+        RecordToWrite.command()
+            .key(batchActivateKey)
+            .processInstanceBatch(
+                ProcessInstanceBatchIntent.ACTIVATE,
+                Records.processInstanceBatch(processInstanceKey, multiInstanceKey)));
+    ENGINE.start();
+
+    // then
+    assertThat(
+            RecordingExporter.processInstanceBatchRecords(ProcessInstanceBatchIntent.ACTIVATE)
+                .onlyCommandRejections()
+                .withProcessInstanceKey(processInstanceKey)
+                .withRecordKey(batchActivateKey)
+                .getFirst()
+                .getRejectionReason())
+        .isEqualTo(
+            "Expected to activate child for batch element instance, but no parent element instance found for key '%s'. The parent was likely terminated before processing this batch activation."
+                .formatted(multiInstanceKey));
+  }
+}
