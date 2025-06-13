1b499e10bbe255dea6ee107ab5765b120be5b0ee
Nicola Puppa <nicola.puppa@camunda.com>
2025-06-13 17:11:30 +0200

    feat: add client permission authorization requests

--- migration/identity-migration/src/main/java/io/camunda/migration/identity/config/saas/StaticEntities.java
+++ migration/identity-migration/src/main/java/io/camunda/migration/identity/config/saas/StaticEntities.java
@@ -22,8 +22,14 @@ public class StaticEntities {
   public static final String TASK_USER_ROLE_ID = "taskuser";
   public static final String VISITOR_ROLE_ID = "visitor";
 
+  public static final String ZEEBE_CLIENT_ID = "Zeebe";
+  public static final String OPERATE_CLIENT_ID = "Operate";
+  public static final String TASKLIST_CLIENT_ID = "Tasklist";
+
   public static final Set<String> ROLE_IDS =
       Set.of(DEVELOPER_ROLE_ID, OPERATIONS_ENGINEER_ROLE_ID, TASK_USER_ROLE_ID, VISITOR_ROLE_ID);
+  public static final Set<String> CLIENT_IDS =
+      Set.of(ZEEBE_CLIENT_ID, OPERATE_CLIENT_ID, TASKLIST_CLIENT_ID);
 
   public static final List<CreateRoleRequest> ROLES =
       List.of(
@@ -152,4 +158,113 @@ public class StaticEntities {
               Set.of(PermissionType.READ))
           // TODO: add document permissions once implemented
           );
+
+  public static final List<CreateAuthorizationRequest> CLIENT_PERMISSIONS =
+      List.of(
+          // ZEEBE
+          new CreateAuthorizationRequest(
+              ZEEBE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.MESSAGE,
+              Set.of(PermissionType.CREATE)),
+          new CreateAuthorizationRequest(
+              ZEEBE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.SYSTEM,
+              AuthorizationResourceType.SYSTEM.getSupportedPermissionTypes()),
+          new CreateAuthorizationRequest(
+              ZEEBE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.RESOURCE,
+              Set.of(
+                  PermissionType.CREATE,
+                  PermissionType.DELETE_FORM,
+                  PermissionType.DELETE_PROCESS,
+                  PermissionType.DELETE_DRD,
+                  PermissionType.DELETE_RESOURCE)),
+          new CreateAuthorizationRequest(
+              ZEEBE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.PROCESS_DEFINITION,
+              Set.of(
+                  PermissionType.UPDATE_PROCESS_INSTANCE,
+                  PermissionType.UPDATE_USER_TASK,
+                  PermissionType.CREATE_PROCESS_INSTANCE,
+                  PermissionType.DELETE_PROCESS_INSTANCE)),
+          new CreateAuthorizationRequest(
+              ZEEBE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.DECISION_DEFINITION,
+              Set.of(
+                  PermissionType.CREATE_DECISION_INSTANCE,
+                  PermissionType.DELETE_DECISION_INSTANCE)),
+          new CreateAuthorizationRequest(
+              ZEEBE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.DECISION_REQUIREMENTS_DEFINITION,
+              Set.of(PermissionType.UPDATE, PermissionType.DELETE)),
+          // OPERATE
+          new CreateAuthorizationRequest(
+              OPERATE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.MESSAGE,
+              Set.of(PermissionType.READ)),
+          new CreateAuthorizationRequest(
+              OPERATE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.BATCH_OPERATION,
+              Set.of(PermissionType.READ, PermissionType.CREATE, PermissionType.UPDATE)),
+          new CreateAuthorizationRequest(
+              OPERATE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.RESOURCE,
+              Set.of(PermissionType.READ)),
+          new CreateAuthorizationRequest(
+              OPERATE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.PROCESS_DEFINITION,
+              Set.of(
+                  PermissionType.READ_PROCESS_DEFINITION,
+                  PermissionType.READ_PROCESS_INSTANCE,
+                  PermissionType.DELETE_PROCESS_INSTANCE)),
+          new CreateAuthorizationRequest(
+              OPERATE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.DECISION_DEFINITION,
+              Set.of(
+                  PermissionType.READ_DECISION_DEFINITION, PermissionType.READ_DECISION_INSTANCE)),
+          new CreateAuthorizationRequest(
+              OPERATE_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.DECISION_REQUIREMENTS_DEFINITION,
+              Set.of(PermissionType.READ)),
+          // TASKLIST
+          new CreateAuthorizationRequest(
+              TASKLIST_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.RESOURCE,
+              Set.of(PermissionType.READ)),
+          new CreateAuthorizationRequest(
+              TASKLIST_CLIENT_ID,
+              AuthorizationOwnerType.CLIENT,
+              "*",
+              AuthorizationResourceType.PROCESS_DEFINITION,
+              Set.of(
+                  // TODO: add document permissions once implemented
+                  PermissionType.READ_PROCESS_DEFINITION,
+                  PermissionType.READ_USER_TASK,
+                  PermissionType.UPDATE_USER_TASK)));
 }
