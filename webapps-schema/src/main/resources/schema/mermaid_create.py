e868e350fbc77062ecf5cf0088c684ef5f670324
Euro Lew <euro.lew@camunda.com>
2025-06-16 14:03:26 +0800

    wip: add styling

--- webapps-schema/src/main/resources/schema/mermaid_create.py
+++ webapps-schema/src/main/resources/schema/mermaid_create.py
@@ -2,6 +2,23 @@ import json
 import sys
 import os
 
+
+styling = """
+%%{init: {
+  "theme": "base",
+  "themeVariables": {
+    "primaryColor": "#1f77b4",
+    "secondaryColor": "#ff7f0e",
+    "tertiaryColor": "#2ca02c",
+    "background": "#ffffff",
+    "nodeBorder": "2px",
+    "primaryBorderColor": "#1f77b4",
+    "edgeLabelBackground": "#f0f0f0",
+    "fontSize": "14px"
+  }
+}}%%
+"""
+
 def json_mapping_to_mermaid(mapping_json, class_name):
   properties = mapping_json.get("mappings", {}).get("properties", {})
 
@@ -38,22 +55,17 @@ if __name__ == "__main__":
 
   prefix = sys.argv[1]
 
-  files = find_files_with_prefix("elasticsearch",  prefix)
-
-  index_diagram = []
-  index_diagram.append("classDiagram")
-  for file in files:
+  diagram_for_indices_with_prefix = [styling,"classDiagram"]
+  for file in find_files_with_prefix("elasticsearch", prefix):
     with open(file) as f:
-      mapping_json = json.load(f)
       class_name = os.path.splitext(os.path.basename(file))[0]
-      diagram_code = json_mapping_to_mermaid(mapping_json, class_name)
-      index_diagram.append(diagram_code)
+      diagram_for_file = json_mapping_to_mermaid(json.load(f), class_name)
+      diagram_for_indices_with_prefix.append(diagram_for_file)
 
 
-  print("\n".join(index_diagram))
-  output_filename = "index_diagram.mmd"
+  output_filename = f"{prefix}-diagrams.mmd"
 
   with open(output_filename, "w") as f:
-    f.write("\n".join(index_diagram))
+    f.write("\n".join(diagram_for_indices_with_prefix))
 
-  print(f"Mermaid diagram generated and saved as '{output_filename}'")
+  print(f"Mermaid diagram for prefix '{prefix}' generated and saved as '{output_filename}'")
