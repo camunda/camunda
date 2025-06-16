33cfe63b772e6e8dd385f7a0f79cbce9a4e15ab6
Slava Akaevich <vjachyslav@gmail.com>
2025-06-16 21:02:00 +0300

    BACKPORT-CONFLICT

--- clients/java/src/main/java/io/camunda/zeebe/client/impl/oauth/OAuthCredentialsProvider.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/oauth/OAuthCredentialsProvider.java
@@ -16,13 +16,17 @@
 package io.camunda.zeebe.client.impl.oauth;
 
 import static java.lang.Math.toIntExact;
+import static java.util.UUID.randomUUID;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
 import io.camunda.zeebe.client.CredentialsProvider;
 import io.camunda.zeebe.client.impl.ZeebeClientCredentials;
 import io.camunda.zeebe.client.impl.util.VersionUtil;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -33,7 +37,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.MessageDigest;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Base64;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -50,6 +62,8 @@ import org.slf4j.LoggerFactory;
 @ThreadSafe
 public final class OAuthCredentialsProvider implements CredentialsProvider {
   private static final String HEADER_AUTH_KEY = "Authorization";
+  private static final String JWT_ASSERTION_TYPE =
+      "urn:ietf:params:oauth:client-assertion-type:jwt-bearer";
 
   private static final ObjectMapper JSON_MAPPER =
       new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
@@ -114,8 +128,20 @@ public final class OAuthCredentialsProvider implements CredentialsProvider {
 
   private static String createParams(final OAuthCredentialsProviderBuilder builder) {
     final Map<String, String> payload = new HashMap<>();
+    if (builder.sslClientCertConfigurationProvided()) {
+      payload.put(
+          "client_assertion",
+          getClientAssertion(
+              builder.getSslClientCertPath().toAbsolutePath().toString(),
+              builder.getSslClientCertPassword(),
+              builder.getClientId(),
+              builder.getAudience()));
+      payload.put("client_assertion_type", JWT_ASSERTION_TYPE);
+    } else {
+      payload.put("client_secret", builder.getClientSecret());
+    }
+
     payload.put("client_id", builder.getClientId());
-    payload.put("client_secret", builder.getClientSecret());
     payload.put("audience", builder.getAudience());
     payload.put("grant_type", "client_credentials");
     final String scope = builder.getScope();
@@ -126,6 +152,56 @@ public final class OAuthCredentialsProvider implements CredentialsProvider {
     return payload.entrySet().stream()
         .map(e -> encode(e.getKey()) + "=" + encode(e.getValue()))
         .collect(Collectors.joining("&"));
+  }
+
+  private static String getClientAssertion(
+      String certPath, String certStorePassword, String clientId, String audience) {
+    final X509Certificate certificate;
+    final Algorithm algorithm;
+    try (FileInputStream stream = new FileInputStream(certPath)) {
+      final KeyStore keyStore = KeyStore.getInstance("PKCS12");
+      final char[] password = certStorePassword.toCharArray();
+      keyStore.load(stream, password);
+
+      final String alias = keyStore.aliases().nextElement();
+      final RSAPrivateKey privateKey = (RSAPrivateKey) keyStore.getKey(alias, password);
+      final X509Certificate keyStoreCertificate = (X509Certificate) keyStore.getCertificate(alias);
+      final RSAPublicKey publicKey = (RSAPublicKey) keyStoreCertificate.getPublicKey();
+
+      certificate = (X509Certificate) keyStore.getCertificate(alias);
+      algorithm = Algorithm.RSA256(publicKey, privateKey);
+    } catch (IOException | GeneralSecurityException e) {
+      throw new RuntimeException("Failed to create client assertion", e);
+    }
+
+    final Date now = new Date();
+    final String x5t = generateX5tThumbprint(certificate);
+
+    final Map<String, Object> header = new HashMap<>();
+    header.put("alg", "RSA256");
+    header.put("typ", "JWT");
+    header.put("x5t", x5t);
+
+    return JWT.create()
+        .withHeader(header)
+        .withIssuer(clientId)
+        .withSubject(clientId)
+        .withAudience(audience)
+        .withIssuedAt(now)
+        .withNotBefore(now)
+        .withExpiresAt(new Date(now.getTime() + 5 * 60 * 1000))
+        .withJWTId(randomUUID().toString())
+        .sign(algorithm);
+  }
+
+  private static String generateX5tThumbprint(X509Certificate certificate) {
+    try {
+      final MessageDigest digest = MessageDigest.getInstance("SHA-1");
+      final byte[] encoded = digest.digest(certificate.getEncoded());
+      return Base64.getUrlEncoder().withoutPadding().encodeToString(encoded);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to generate x5t thumbprint", e);
+    }
   }
 
   private static String encode(final String param) {
--- clients/java/src/main/java/io/camunda/zeebe/client/impl/oauth/OAuthCredentialsProviderBuilder.java
+++ clients/java/src/main/java/io/camunda/zeebe/client/impl/oauth/OAuthCredentialsProviderBuilder.java
@@ -28,12 +28,33 @@ import io.camunda.zeebe.client.impl.util.Environment;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.time.Duration;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 public final class OAuthCredentialsProviderBuilder {
   public static final String INVALID_ARGUMENT_MSG = "Expected valid %s but none was provided.";
+<<<<<<< HEAD
+=======
+  public static final String OAUTH_ENV_CLIENT_ID = "ZEEBE_CLIENT_ID";
+  public static final String OAUTH_ENV_CLIENT_SECRET = "ZEEBE_CLIENT_SECRET";
+  public static final String OAUTH_ENV_TOKEN_AUDIENCE = "ZEEBE_TOKEN_AUDIENCE";
+  public static final String OAUTH_ENV_TOKEN_SCOPE = "ZEEBE_TOKEN_SCOPE";
+  public static final String OAUTH_ENV_AUTHORIZATION_SERVER = "ZEEBE_AUTHORIZATION_SERVER_URL";
+  public static final String OAUTH_ENV_CACHE_PATH = "ZEEBE_CLIENT_CONFIG_PATH";
+  public static final String OAUTH_ENV_CONNECT_TIMEOUT = "ZEEBE_AUTH_CONNECT_TIMEOUT";
+  public static final String OAUTH_ENV_READ_TIMEOUT = "ZEEBE_AUTH_READ_TIMEOUT";
+  public static final String OAUTH_ENV_SSL_CLIENT_CERT_PATH = "OAUTH_SSL_CLIENT_CERT_PATH";
+  public static final String OAUTH_ENV_SSL_CLIENT_CERT_PASSWORD = "OAUTH_SSL_CLIENT_CERT_PASSWORD";
+>>>>>>> 3e731140 (feat: add ms entra support)
   private static final String DEFAULT_AUTHZ_SERVER = "https://login.cloud.camunda.io/oauth/token/";
   private static final Duration DEFAULT_CONNECT_TIMEOUT = Duration.ofSeconds(5);
   private static final Duration DEFAULT_READ_TIMEOUT = DEFAULT_CONNECT_TIMEOUT;
@@ -48,6 +69,12 @@ public final class OAuthCredentialsProviderBuilder {
   private File credentialsCache;
   private Duration connectTimeout;
   private Duration readTimeout;
+<<<<<<< HEAD
+=======
+  private boolean applyEnvironmentOverrides = true;
+  private Path sslClientCertPath;
+  private String sslClientCertPassword;
+>>>>>>> 3e731140 (feat: add ms entra support)
 
   /** Client id to be used when requesting access token from OAuth authorization server. */
   public OAuthCredentialsProviderBuilder clientId(final String clientId) {
@@ -169,9 +196,15 @@ public final class OAuthCredentialsProviderBuilder {
   public OAuthCredentialsProvider build() {
     checkEnvironmentOverrides();
     applyDefaults();
+    applySSLClientCertConfiguration();
 
     validate();
     return new OAuthCredentialsProvider(this);
+  }
+
+  private void applySSLClientCertConfiguration() {
+    applyEnvironmentValueIfNotNull(this::sslClientCertPath, OAUTH_ENV_SSL_CLIENT_CERT_PATH);
+    applyEnvironmentValueIfNotNull(this::sslClientCertPassword, OAUTH_ENV_SSL_CLIENT_CERT_PASSWORD);
   }
 
   private void checkEnvironmentOverrides() {
@@ -241,7 +274,15 @@ public final class OAuthCredentialsProviderBuilder {
   private void validate() {
     try {
       Objects.requireNonNull(clientId, String.format(INVALID_ARGUMENT_MSG, "client id"));
-      Objects.requireNonNull(clientSecret, String.format(INVALID_ARGUMENT_MSG, "client secret"));
+      if (sslClientCertConfigurationProvided()) {
+        // loading the certificate from the provided path to ensure it exists and is valid
+        final KeyStore keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(
+            Files.newInputStream(Paths.get(sslClientCertPath.toAbsolutePath().toString())),
+            sslClientCertPassword.toCharArray());
+      } else {
+        Objects.requireNonNull(clientSecret, String.format(INVALID_ARGUMENT_MSG, "client secret"));
+      }
       Objects.requireNonNull(audience, String.format(INVALID_ARGUMENT_MSG, "audience"));
       Objects.requireNonNull(
           authorizationServerUrl, String.format(INVALID_ARGUMENT_MSG, "authorization server URL"));
@@ -255,7 +296,11 @@ public final class OAuthCredentialsProviderBuilder {
       }
       validateTimeout(connectTimeout, "ConnectTimeout");
       validateTimeout(readTimeout, "ReadTimeout");
-    } catch (final NullPointerException | IOException e) {
+    } catch (final NullPointerException
+        | IOException
+        | KeyStoreException
+        | NoSuchAlgorithmException
+        | CertificateException e) {
       throw new IllegalArgumentException(e);
     }
   }
@@ -268,4 +313,56 @@ public final class OAuthCredentialsProviderBuilder {
               timeoutName, timeout.toMillis(), Integer.MAX_VALUE));
     }
   }
+<<<<<<< HEAD
+=======
+
+  public OAuthCredentialsProviderBuilder sslClientCertPath(final String entraCertificatePath) {
+    if (entraCertificatePath != null) {
+      this.sslClientCertPath = Paths.get(entraCertificatePath);
+    }
+    return this;
+  }
+
+  public Path getSslClientCertPath() {
+    return sslClientCertPath;
+  }
+
+  public OAuthCredentialsProviderBuilder sslClientCertPassword(
+      final String entraCertificatePassword) {
+    this.sslClientCertPassword = entraCertificatePassword;
+    return this;
+  }
+
+  public String getSslClientCertPassword() {
+    return sslClientCertPassword;
+  }
+
+  public boolean sslClientCertConfigurationProvided() {
+    return sslClientCertPassword != null
+        && !sslClientCertPassword.isEmpty()
+        && sslClientCertPath != null
+        && sslClientCertPath.toFile().exists();
+  }
+
+  public OAuthCredentialsProviderBuilder applyEnvironmentOverrides(
+      final boolean applyEnvironmentOverrides) {
+    this.applyEnvironmentOverrides = applyEnvironmentOverrides;
+    return this;
+  }
+
+  private static Optional<String> getEnvironmentVariableValue(final String envName) {
+    return Optional.ofNullable(Environment.system().get(envName));
+  }
+
+  private static void applyEnvironmentValueIfNotNull(
+      final Consumer<String> action, final String... envNames) {
+    for (final String envName : envNames) {
+      final Optional<String> value = getEnvironmentVariableValue(envName);
+      value.ifPresent(action);
+      if (value.isPresent()) {
+        break;
+      }
+    }
+  }
+>>>>>>> 3e731140 (feat: add ms entra support)
 }
