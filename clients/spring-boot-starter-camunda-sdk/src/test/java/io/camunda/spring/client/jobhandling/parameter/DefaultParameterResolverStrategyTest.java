9aaa6a1ab294e6bd8a3e16b3500b85791c4c920a
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-06-18 12:47:54 +0200

    test: document can be resolved

--- clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/jobhandling/parameter/DefaultParameterResolverStrategyTest.java
+++ clients/spring-boot-starter-camunda-sdk/src/test/java/io/camunda/spring/client/jobhandling/parameter/DefaultParameterResolverStrategyTest.java
@@ -15,36 +15,21 @@
  */
 package io.camunda.spring.client.jobhandling.parameter;
 
+import static io.camunda.spring.client.testsupport.ClassInfoUtil.parameterInfos;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 import io.camunda.client.api.response.ActivatedJob;
 import io.camunda.client.api.worker.JobClient;
 import io.camunda.client.impl.CamundaObjectMapper;
-import io.camunda.spring.client.bean.ClassInfo;
+import io.camunda.spring.client.annotation.Document;
 import io.camunda.spring.client.bean.ParameterInfo;
+import io.camunda.spring.client.jobhandling.DocumentContext;
 import io.camunda.zeebe.client.ZeebeClient;
-import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class DefaultParameterResolverStrategyTest {
-  private List<ParameterInfo> resolveParameters(final String methodName) {
-    return ClassInfo.builder()
-        .bean(this)
-        .beanName("testBean")
-        .build()
-        .toMethodInfo(findMethod(methodName))
-        .getParameters();
-  }
-
-  private static Method findMethod(final String name) {
-    return Arrays.stream(DefaultParameterResolverStrategyTest.class.getMethods())
-        .filter(m -> m.getName().equals(name))
-        .findFirst()
-        .get();
-  }
 
   @Test
   void shouldResolveLegacyParameter() {
@@ -52,7 +37,7 @@ public class DefaultParameterResolverStrategyTest {
     final DefaultParameterResolverStrategy strategy =
         new DefaultParameterResolverStrategy(new CamundaObjectMapper(), zeebeClient);
 
-    final List<ParameterInfo> parameters = resolveParameters("legacyMethod");
+    final List<ParameterInfo> parameters = parameterInfos(this, "legacyMethod");
     assertThat(parameters).hasSize(2);
     final ParameterResolver jobClientResolver = strategy.createResolver(parameters.get(0));
     assertThat(jobClientResolver).isInstanceOf(CompatJobClientParameterResolver.class);
@@ -66,7 +51,7 @@ public class DefaultParameterResolverStrategyTest {
   void shouldResolveCurrentParameter() {
     final DefaultParameterResolverStrategy strategy =
         new DefaultParameterResolverStrategy(new CamundaObjectMapper());
-    final List<ParameterInfo> parameters = resolveParameters("currentMethod");
+    final List<ParameterInfo> parameters = parameterInfos(this, "currentMethod");
     assertThat(parameters).hasSize(2);
     final ParameterResolver jobClientResolver = strategy.createResolver(parameters.get(0));
     assertThat(jobClientResolver).isInstanceOf(JobClientParameterResolver.class);
@@ -74,9 +59,21 @@ public class DefaultParameterResolverStrategyTest {
     assertThat(activatedJobResolver).isInstanceOf(ActivatedJobParameterResolver.class);
   }
 
+  @Test
+  void shouldResolveDocument() {
+    final DefaultParameterResolverStrategy strategy =
+        new DefaultParameterResolverStrategy(new CamundaObjectMapper());
+    final List<ParameterInfo> parameters = parameterInfos(this, "documentMethod");
+    assertThat(parameters).hasSize(1);
+    final ParameterResolver parameterResolver = strategy.createResolver(parameters.get(0));
+    assertThat(parameterResolver).isInstanceOf(DocumentResolver.class);
+  }
+
   public void legacyMethod(
       final io.camunda.zeebe.client.api.worker.JobClient jobClient,
       final io.camunda.zeebe.client.api.response.ActivatedJob job) {}
 
   public void currentMethod(final JobClient jobClient, final ActivatedJob job) {}
+
+  public void documentMethod(@Document final DocumentContext myDoc) {}
 }
