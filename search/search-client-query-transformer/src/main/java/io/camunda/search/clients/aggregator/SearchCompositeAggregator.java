db4e76da3a5419bda411ea235cc5863f2f1b3772
Jonathan Roques <jonathan.roques@camunda.com>
2025-06-19 16:47:25 +0200

    feat: add support for Composite aggregations that will enable pagination

---
+++ search/search-client-query-transformer/src/main/java/io/camunda/search/clients/aggregator/SearchCompositeAggregator.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.search.clients.aggregator;
+
+import io.camunda.util.ObjectBuilder;
+import java.util.List;
+import java.util.Objects;
+
+public record SearchCompositeAggregator(
+    String name, Integer size, List<SearchAggregator> aggregations, List<SearchAggregator> sources)
+    implements SearchAggregator {
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public List<SearchAggregator> getAggregations() {
+    return aggregations;
+  }
+
+  public static final class Builder extends AbstractBuilder<SearchCompositeAggregator.Builder>
+      implements ObjectBuilder<SearchCompositeAggregator> {
+    private Integer size = 10000;
+    private List<SearchAggregator> sources;
+
+    @Override
+    protected SearchCompositeAggregator.Builder self() {
+      return this;
+    }
+
+    public SearchCompositeAggregator.Builder size(final Integer value) {
+      // Validate size to ensure it's a positive integer
+      if (value != null && value < 0) {
+        throw new IllegalArgumentException("Size must be a positive integer.");
+      }
+      size = value;
+      return this;
+    }
+
+    public SearchCompositeAggregator.Builder sources(final List<SearchAggregator> sources) {
+      this.sources = sources;
+      return this;
+    }
+
+    @Override
+    public SearchCompositeAggregator build() {
+      return new SearchCompositeAggregator(
+          Objects.requireNonNull(name, "Expected non-null field for name."),
+          size,
+          aggregations,
+          sources);
+    }
+  }
+}
