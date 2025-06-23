15b6aef0a6c0a72d81c5800fd8e93adbf092929d
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-06-23 11:03:25 +0200

    feat: added a convenient way to retrieve a document reference to ActivatedJob

--- clients/java/src/main/java/io/camunda/client/api/response/ActivatedJob.java
+++ clients/java/src/main/java/io/camunda/client/api/response/ActivatedJob.java
@@ -17,6 +17,7 @@ package io.camunda.client.api.response;
 
 import io.camunda.client.api.ExperimentalApi;
 import io.camunda.client.api.command.ClientException;
+import java.util.List;
 import io.camunda.client.api.search.enums.JobKind;
 import io.camunda.client.api.search.enums.ListenerEventType;
 import java.util.Map;
@@ -102,7 +103,8 @@ public interface ActivatedJob {
 
   /**
    * @return de-serialized variable value or null if the provided variable name is present among the
-   *     available variables, otherwise throw a {@link ClientException}
+   *     available variables
+   * @throws ClientException if the variable is missing
    */
   Object getVariable(String name);
 
@@ -133,4 +135,12 @@ public interface ActivatedJob {
    */
   @ExperimentalApi("https://github.com/camunda/camunda/issues/13560")
   String getTenantId();
+
+  /**
+   * @return de-serialized document references if the provided variable name is present among the
+   *     available variables and can be parsed as document reference
+   * @throws ClientException if the variable is missing or if the variable cannot be parsed * as
+   *     document reference list
+   */
+  List<DocumentReferenceResponse> getDocumentReferences(String name);
 }
