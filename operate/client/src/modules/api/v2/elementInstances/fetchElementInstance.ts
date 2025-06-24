5afdc5bc96a3816ad16683e924001fac1867ad45
Yuliia <yuliia.saienko-ext@camunda.com>
2025-06-24 12:16:29 +0300

    feat: v2 element instances queries

---
+++ operate/client/src/modules/api/v2/elementInstances/fetchElementInstance.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {
+  endpoints,
+  GetElementInstanceResponseBody,
+} from '@vzeta/camunda-api-zod-schemas';
+import {requestWithThrow, RequestResult} from 'modules/request';
+
+const fetchElementInstance = async (params: {
+  elementInstanceKey: string;
+}): RequestResult<GetElementInstanceResponseBody> => {
+  return requestWithThrow<GetElementInstanceResponseBody>({
+    url: (
+      endpoints.getElementInstance.getUrl as (params: {
+        elementInstanceKey: string;
+      }) => string
+    )(params),
+    method: endpoints.getElementInstance.method,
+  });
+};
+
+export {fetchElementInstance};
