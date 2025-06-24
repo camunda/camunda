ead22757c5ea92b1669490a6ca91b4bd78f2b2c5
Yuliia <yuliia.saienko-ext@camunda.com>
2025-06-24 12:16:29 +0300

    feat: v2 element instances queries

--- operate/client/src/modules/queries/elementInstances/useElementInstancesSearch.ts
+++ operate/client/src/modules/queries/elementInstances/useElementInstancesSearch.ts
@@ -6,9 +6,16 @@
  * except in compliance with the Camunda License 1.0.
  */
 
+<<<<<<< HEAD
 import {useQuery} from '@tanstack/react-query';
 import {searchElementInstances} from 'modules/api/v2/elementInstances/searchElementInstances';
 import {QueryElementInstancesRequestBody} from '@vzeta/camunda-api-zod-schemas';
+=======
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { searchElementInstances } from 'modules/api/v2/elementInstances/searchElementInstances';
+import { QueryElementInstancesRequestBody, QueryElementInstancesResponseBody } from '@vzeta/camunda-api-zod-schemas/8.8';
+import { RequestError } from 'modules/request';
+>>>>>>> 57964e33e19 (feat: v2 element instances queries)
 
 const ELEMENT_INSTANCES_SEARCH_QUERY_KEY = 'elementInstancesSearch';
 
@@ -17,6 +24,7 @@ function getQueryKey(params: QueryElementInstancesRequestBody) {
 }
 
 const useElementInstancesSearch = (
+<<<<<<< HEAD
   params: QueryElementInstancesRequestBody,
 ) => {
   return useQuery({
@@ -26,8 +34,22 @@ const useElementInstancesSearch = (
         if (response !== null) return response;
         throw error;
       }),
+=======
+    params: QueryElementInstancesRequestBody,
+): UseQueryResult<QueryElementInstancesResponseBody, RequestError> => {
+  return useQuery({
+    queryKey: getQueryKey(params),
+    queryFn: () => searchElementInstances(params).then(({ response, error }) => {
+      if (response !== null) return response;
+      throw error;
+    }),
+>>>>>>> 57964e33e19 (feat: v2 element instances queries)
     enabled: !!params,
   });
 };
 
+<<<<<<< HEAD
 export {useElementInstancesSearch, ELEMENT_INSTANCES_SEARCH_QUERY_KEY};
+=======
+export { useElementInstancesSearch, ELEMENT_INSTANCES_SEARCH_QUERY_KEY };
+>>>>>>> 57964e33e19 (feat: v2 element instances queries)
