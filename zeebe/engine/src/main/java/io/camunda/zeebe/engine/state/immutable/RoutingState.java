cf4927411a2d5cc69897c03e5eb8cfe02ca135c3
Carlo Sana <carlo.sana@camunda.com>
2025-06-25 16:19:28 +0200

    refactor: include partitionCount in error message from RoutingState

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/immutable/RoutingState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/immutable/RoutingState.java
@@ -32,7 +32,8 @@ public interface RoutingState {
     record HashMod(int partitionCount) implements MessageCorrelation {
       public HashMod {
         if (partitionCount <= 0) {
-          throw new IllegalArgumentException("Partition count must be positive");
+          throw new IllegalArgumentException(
+              "Partition count must be positive, was " + partitionCount);
         }
       }
     }
