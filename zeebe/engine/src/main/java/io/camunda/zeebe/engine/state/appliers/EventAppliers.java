05fbb2476ff7145b919d65928a66e16600f3d688
Pavel Kotelevsky <pavel.kotelevskii@camunda.com>
2025-06-26 17:54:08 +0200

    feat: add ProcessInstanceCreationCreatedApplierV2

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/appliers/EventAppliers.java
@@ -265,7 +265,13 @@ public final class EventAppliers implements EventApplier {
 
     register(
         ProcessInstanceCreationIntent.CREATED,
+        1,
         new ProcessInstanceCreationCreatedApplier(processState, elementInstanceState));
+
+    register(
+        ProcessInstanceCreationIntent.CREATED,
+        2,
+        new ProcessInstanceCreationCreatedApplierV2(processState, elementInstanceState));
   }
 
   private void registerProcessInstanceModificationAppliers(final MutableProcessingState state) {
