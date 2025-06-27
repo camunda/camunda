c4423101d8a43ffb0ad52696df09d40be611b714
Jonathan Roques <jonathan.roques@camunda.com>
2025-06-27 16:01:33 +0200

    feat: add Opensearch implementation

--- search/search-client-opensearch/src/main/java/io/camunda/search/os/transformers/aggregator/SearchAggregationResultTransformer.java
+++ search/search-client-opensearch/src/main/java/io/camunda/search/os/transformers/aggregator/SearchAggregationResultTransformer.java
@@ -7,22 +7,45 @@
  */
 package io.camunda.search.os.transformers.aggregator;
 
+import io.camunda.search.clients.aggregator.SearchAggregator;
+import io.camunda.search.clients.aggregator.SearchTopHitsAggregator;
 import io.camunda.search.clients.core.AggregationResult;
 import io.camunda.search.clients.core.AggregationResult.Builder;
+import io.camunda.search.clients.core.SearchQueryHit;
 import io.camunda.search.clients.transformers.SearchTransfomer;
+import io.camunda.search.clients.transformers.query.Cursor;
+import io.camunda.search.os.transformers.OpensearchTransformers;
+import io.camunda.search.os.transformers.search.SearchQueryHitTransformer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.opensearch.client.json.JsonData;
 import org.opensearch.client.opensearch._types.aggregations.Aggregate;
+import org.opensearch.client.opensearch._types.aggregations.CompositeAggregate;
+import org.opensearch.client.opensearch._types.aggregations.CompositeBucket;
 import org.opensearch.client.opensearch._types.aggregations.LongTermsBucket;
 import org.opensearch.client.opensearch._types.aggregations.MultiBucketAggregateBase;
 import org.opensearch.client.opensearch._types.aggregations.MultiBucketBase;
 import org.opensearch.client.opensearch._types.aggregations.SingleBucketAggregateBase;
 import org.opensearch.client.opensearch._types.aggregations.StringTermsBucket;
+import org.opensearch.client.opensearch._types.aggregations.TopHitsAggregate;
+import org.opensearch.client.opensearch.core.search.Hit;
 
-public class SearchAggregationResultTransformer
+public class SearchAggregationResultTransformer<T>
     implements SearchTransfomer<Map<String, Aggregate>, Map<String, AggregationResult>> {
+
+  private static final String COMPOSITE_KEY_DELIMITER = "__";
+  private final OpensearchTransformers transformers;
+  private final List<SearchAggregator> aggregators;
+
+  public SearchAggregationResultTransformer(
+      final OpensearchTransformers transformers, final List<SearchAggregator> aggregators) {
+    this.transformers = transformers;
+    this.aggregators = aggregators;
+  }
 
   @Override
   public Map<String, AggregationResult> apply(final Map<String, Aggregate> value) {
@@ -37,10 +60,46 @@ public class SearchAggregationResultTransformer
         .build();
   }
 
+  private SearchTopHitsAggregator findTopHitsAggregatorRecursively(
+      final List<SearchAggregator> aggregators, final String key) {
+    if (aggregators == null || aggregators.isEmpty()) {
+      return null;
+    }
+
+    // First try to find direct match
+    final Optional<SearchTopHitsAggregator> directMatch =
+        aggregators.stream()
+            .filter(SearchTopHitsAggregator.class::isInstance)
+            .map(SearchTopHitsAggregator.class::cast)
+            .filter(aggregator -> aggregator.getName().equals(key))
+            .findFirst();
+
+    // If not found, search recursively in sub-aggregations
+    return directMatch.orElseGet(
+        () ->
+            aggregators.stream()
+                .map(
+                    aggregator ->
+                        findTopHitsAggregatorRecursively(aggregator.getAggregations(), key))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null));
+  }
+
+  private AggregationResult transformTopHitsAggregate(
+      final String key, final TopHitsAggregate aggregate) {
+    final var hits = aggregate.hits().hits();
+
+    final var topHitAggregator = findTopHitsAggregatorRecursively(aggregators, key);
+
+    return new Builder().hits(toSearchQueryHits(hits, topHitAggregator.documentClass())).build();
+  }
+
   private <B extends MultiBucketBase> AggregationResult transformMultiBucketAggregate(
       final MultiBucketAggregateBase<B> aggregate) {
     final var map = new HashMap<String, AggregationResult>();
     final var buckets = aggregate.buckets();
+    final var searchAfter = extractSearchAfter(aggregate);
     if (buckets.isKeyed()) {
       buckets
           .keyed()
@@ -61,6 +120,10 @@ public class SearchAggregationResultTransformer
                 switch (bucket) {
                   case final StringTermsBucket b -> b.key();
                   case final LongTermsBucket b -> b.keyAsString();
+                  case final CompositeBucket b ->
+                      b.key().values().stream()
+                          .map(JsonData::toString)
+                          .collect(Collectors.joining(COMPOSITE_KEY_DELIMITER));
                   default ->
                       throw new IllegalStateException(
                           "Unsupported bucket type: " + bucket.getClass());
@@ -73,7 +136,20 @@ public class SearchAggregationResultTransformer
             map.put(key, result);
           });
     }
-    return new Builder().aggregations(map).build();
+    return new Builder().aggregations(map).endCursor(Cursor.encode(searchAfter)).build();
+  }
+
+  private <B extends MultiBucketBase> Object[] extractSearchAfter(
+      final MultiBucketAggregateBase<B> aggregate) {
+    if (aggregate instanceof final CompositeAggregate compositeAggregate) {
+      return compositeAggregate.afterKey() != null
+          ? compositeAggregate.afterKey().entrySet().stream()
+              .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().toString()))
+              .entrySet()
+              .toArray()
+          : null;
+    }
+    return null;
   }
 
   private Map<String, AggregationResult> transformAggregation(
@@ -92,6 +168,8 @@ public class SearchAggregationResultTransformer
             case Filter -> res = transformSingleBucketAggregate(aggregate.filter());
             case Filters -> res = transformMultiBucketAggregate(aggregate.filters());
             case Sterms -> res = transformMultiBucketAggregate(aggregate.sterms());
+            case Composite -> res = transformMultiBucketAggregate(aggregate.composite());
+            case TopHits -> res = transformTopHitsAggregate(key, aggregate.topHits());
             default ->
                 throw new IllegalStateException(
                     "Unsupported aggregation type: " + aggregate._kind());
@@ -99,5 +177,25 @@ public class SearchAggregationResultTransformer
           result.put(key, res);
         });
     return result;
+  }
+
+  private List<SearchQueryHit> toSearchQueryHits(
+      final List<Hit<JsonData>> hits, final Class<T> documentClass) {
+    if (hits != null) {
+      final var hitTransformer = new SearchQueryHitTransformer<T>(transformers);
+      return hits.stream()
+          .filter(hit -> Objects.nonNull(hit.source()))
+          .map(
+              hit ->
+                  new Hit.Builder<T>()
+                      .index(hit.index())
+                      .id(hit.id())
+                      .score(hit.score())
+                      .source(hit.source().to(documentClass))
+                      .build())
+          .map(hitTransformer::apply)
+          .collect(Collectors.toList());
+    }
+    return List.of();
   }
 }
