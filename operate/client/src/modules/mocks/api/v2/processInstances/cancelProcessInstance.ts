2ffff9c880e62618f23406bc570d2494c1483d34
Patrick Dehn <patrick.dehn@camunda.com>
2025-06-27 09:46:36 +0200

    refactor: import endpoint URL in cancel Process instance API mock

--- operate/client/src/modules/mocks/api/v2/processInstances/cancelProcessInstance.ts
+++ operate/client/src/modules/mocks/api/v2/processInstances/cancelProcessInstance.ts
@@ -6,19 +6,12 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-/*
- * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
- * one or more contributor license agreements. See the NOTICE file distributed
- * with this work for additional information regarding copyright ownership.
- * Licensed under the Camunda License 1.0. You may not use this file
- * except in compliance with the Camunda License 1.0.
- */
-
+import {endpoints} from '@vzeta/camunda-api-zod-schemas';
 import {mockPostRequest} from '../../mockRequest';
 
 const mockCancelProcessInstance = (contextPath = '') =>
   mockPostRequest(
-    `${contextPath}/v2/process-instances/:processInstanceKey/cancellation`,
+    `${contextPath}${endpoints.cancelProcessInstance.getUrl({processInstanceKey: ':processInstanceKey'})}`,
   );
 
 export {mockCancelProcessInstance};
