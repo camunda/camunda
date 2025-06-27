0b40f21ecb41ac1fadf9e8ca15a36379327c54c0
Yuliia <yuliia.saienko-ext@camunda.com>
2025-06-27 16:16:23 +0300

    feat: v2 user tasks query

---
+++ operate/client/src/modules/api/v2/userTasks/searchUserTasks.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {
+  endpoints,
+  QueryUserTasksResponseBody,
+  QueryUserTasksRequestBody,
+} from '@vzeta/camunda-api-zod-schemas';
+import {requestWithThrow, RequestResult} from 'modules/request';
+
+const searchUserTasks = async (
+  payload: QueryUserTasksRequestBody,
+): RequestResult<QueryUserTasksResponseBody> => {
+  return requestWithThrow<QueryUserTasksResponseBody>({
+    url: endpoints.queryUserTasks.getUrl(),
+    method: endpoints.queryUserTasks.method,
+    body: payload,
+  });
+};
+
+export {searchUserTasks};
---
+++ operate/client/src/modules/queries/userTasks/useElementInstancesSearch.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {useQuery} from '@tanstack/react-query';
+import {searchUserTasks} from 'modules/api/v2/userTasks/searchUserTasks';
+import {QueryUserTasksRequestBody} from '@vzeta/camunda-api-zod-schemas';
+
+const USER_TASKS_SEARCH_QUERY_KEY = 'userTasksSearch';
+
+function getQueryKey(params: QueryUserTasksRequestBody) {
+  return [USER_TASKS_SEARCH_QUERY_KEY, params];
+}
+
+const useUserTasksSearch = (params: QueryUserTasksRequestBody) => {
+  return useQuery({
+    queryKey: getQueryKey(params),
+    queryFn: () =>
+      searchUserTasks(params).then(({response, error}) => {
+        if (response !== null) return response;
+        throw error;
+      }),
+    enabled: !!params,
+  });
+};
+
+export {useUserTasksSearch, USER_TASKS_SEARCH_QUERY_KEY};
