779ef66491590aed59574cf6a99546ee24d10faf
Carlo Sana <carlo.sana@camunda.com>
2025-06-27 11:55:24 +0200

    fix: SnapshotCopy opens a SnapshotOnlyDb to copy from

--- zeebe/zb-db/src/main/java/io/camunda/zeebe/db/SnapshotCopy.java
+++ zeebe/zb-db/src/main/java/io/camunda/zeebe/db/SnapshotCopy.java
@@ -7,9 +7,7 @@
  */
 package io.camunda.zeebe.db;
 
-import io.camunda.zeebe.db.impl.rocksdb.transaction.ZeebeTransactionDb;
 import io.camunda.zeebe.protocol.ColumnFamilyScope;
-import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import java.nio.file.Path;
 import java.util.Set;
 
@@ -18,19 +16,6 @@ import java.util.Set;
  * also method to compare two snapshots that can be useful when testing.
  */
 public interface SnapshotCopy {
-
-  /**
-   * Helper function that can be used to compare or copy two snapshots. Particularly useful for
-   * testing.
-   *
-   * @param fromPath path of the readonly snapshot that will be associated to "from" arguments in
-   *     {@link CopyContextConsumer}
-   * @param toDBPath path of the snapshot that will be associated to "to" arguments in {@link
-   *     CopyContextConsumer}
-   * @param consumer that can access both DB instances.
-   */
-  void withContexts(final Path fromPath, final Path toDBPath, final CopyContextConsumer consumer);
-
   /**
    * Copies the content of a snapshot from a path to another path, copying only the columns with a
    * certain scope
@@ -40,22 +25,4 @@ public interface SnapshotCopy {
    * @param scope the scope of the columns to copy
    */
   void copySnapshot(Path fromPath, Path toPath, Set<ColumnFamilyScope> scope);
-
-  interface CopyContextConsumer {
-
-    /**
-     * Callback that will be invoked when the two DBs are opened. The arguments of this function
-     * cannot escape this method, as they will be closed once this method returns.
-     *
-     * @param fromDB DB located at fromPath
-     * @param fromCtx transaction context at fromPath
-     * @param toDB DB located at toPath
-     * @param toCtx transaction context at toPath
-     */
-    void accept(
-        ZeebeTransactionDb<ZbColumnFamilies> fromDB,
-        TransactionContext fromCtx,
-        ZeebeTransactionDb<ZbColumnFamilies> toDB,
-        TransactionContext toCtx);
-  }
 }
