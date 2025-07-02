2fda1cfea2b42e8fa4d214849132a48b98b939eb
Nikola Koevski <nikola.koevski@camunda.com>
2025-07-02 15:02:04 +0200

    refactor: adjust tenant exporter handler tests with new id format

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/handlers/TenantDeletedHandlerTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/handlers/TenantDeletedHandlerTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.verify;
 
 import io.camunda.exporter.exceptions.PersistenceException;
 import io.camunda.exporter.store.BatchRequest;
+import io.camunda.webapps.schema.descriptors.index.TenantIndex;
 import io.camunda.webapps.schema.entities.usermanagement.TenantEntity;
 import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.ValueType;
@@ -52,12 +53,14 @@ public class TenantDeletedHandlerTest {
     // given
     final Record<TenantRecordValue> tenantRecord =
         factory.generateRecordWithIntent(ValueType.TENANT, TenantIntent.DELETED);
+    final String tenantId = tenantRecord.getValue().getTenantId();
+    final String uniqueExporterId = TenantIndex.JOIN_RELATION_FACTORY.createParentId(tenantId);
 
     // when
     final var idList = underTest.generateIds(tenantRecord);
 
     // then
-    assertThat(idList).containsExactly(String.valueOf(tenantRecord.getValue().getTenantId()));
+    assertThat(idList).containsExactly(uniqueExporterId);
   }
 
   @Test
