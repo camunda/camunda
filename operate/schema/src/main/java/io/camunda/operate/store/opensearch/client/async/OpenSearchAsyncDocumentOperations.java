9f96f2637acaab6bd2ed711650107d7792184e94
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2025-07-04 12:24:14 +0300

    refactor: remove unused opensearch client code
    (cherry picked from commit ad8cdcaf891dfb506dca339bfdb586d8c6e5901a)
    
    # Conflicts:
    #	operate/qa/integration-tests/src/test/java/io/camunda/operate/opensearch/client/async/OpenSearchAsyncDocumentOperationsIT.java
    #	operate/qa/integration-tests/src/test/java/io/camunda/operate/opensearch/client/async/OpenSearchAsyncIndexOperationsIT.java
    #	operate/qa/integration-tests/src/test/java/io/camunda/operate/opensearch/client/async/OpenSearchAsyncTaskOperationsIT.java
    #	operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/async/OpenSearchAsyncIndexOperations.java
    #	operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/async/OpenSearchAsyncTaskOperations.java
    #	operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/sync/RichOpenSearchClient.java

--- operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/async/OpenSearchAsyncDocumentOperations.java
+++ operate/schema/src/main/java/io/camunda/operate/store/opensearch/client/async/OpenSearchAsyncDocumentOperations.java
@@ -19,38 +19,20 @@ package io.camunda.operate.store.opensearch.client.async;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 import org.opensearch.client.opensearch.OpenSearchAsyncClient;
-import org.opensearch.client.opensearch.core.DeleteByQueryRequest;
-import org.opensearch.client.opensearch.core.DeleteByQueryResponse;
-import org.opensearch.client.opensearch.core.ScrollRequest;
-import org.opensearch.client.opensearch.core.ScrollResponse;
 import org.opensearch.client.opensearch.core.SearchRequest;
 import org.opensearch.client.opensearch.core.SearchResponse;
 import org.slf4j.Logger;
 
 public class OpenSearchAsyncDocumentOperations extends OpenSearchAsyncOperation {
   public OpenSearchAsyncDocumentOperations(
-      Logger logger, OpenSearchAsyncClient openSearchAsyncClient) {
+      final Logger logger, final OpenSearchAsyncClient openSearchAsyncClient) {
     super(logger, openSearchAsyncClient);
   }
 
-  // TODO check unsued
-  private CompletableFuture<ScrollResponse<Object>> scrollAsync(
-      final ScrollRequest scrollRequest, Function<Exception, String> errorMessageSupplier) {
-    return safe(
-        () -> openSearchAsyncClient.scroll(scrollRequest, Object.class), errorMessageSupplier);
-  }
-
-  public CompletableFuture<DeleteByQueryResponse> delete(
-      DeleteByQueryRequest.Builder requestBuilder,
-      Function<Exception, String> errorMessageSupplier) {
-    return safe(
-        () -> openSearchAsyncClient.deleteByQuery(requestBuilder.build()), errorMessageSupplier);
-  }
-
   public <R> CompletableFuture<SearchResponse<R>> search(
-      SearchRequest.Builder searchRequestBuilder,
-      Class<R> clazz,
-      Function<Exception, String> errorMessageSupplier) {
+      final SearchRequest.Builder searchRequestBuilder,
+      final Class<R> clazz,
+      final Function<Exception, String> errorMessageSupplier) {
     return safe(
         () -> openSearchAsyncClient.search(searchRequestBuilder.build(), clazz),
         errorMessageSupplier);
