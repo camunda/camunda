5ca7583369e8432399c04820cff8d1eb925dd550
Nicola Puppa <nicola.puppa@camunda.com>
2025-07-04 15:36:03 +0200

    test: verify the handler behaviour

---
+++ migration/identity-migration/src/test/java/io/camunda/migration/identity/handler/sm/UserRoleMigrationHandlerTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.migration.identity.handler.sm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.camunda.identity.sdk.users.dto.User;
+import io.camunda.migration.identity.client.ManagementIdentityClient;
+import io.camunda.migration.identity.dto.Role;
+import io.camunda.security.auth.CamundaAuthentication;
+import io.camunda.service.RoleServices;
+import io.camunda.service.RoleServices.RoleMemberRequest;
+import io.camunda.zeebe.broker.client.api.BrokerRejectionException;
+import io.camunda.zeebe.broker.client.api.dto.BrokerRejection;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.intent.RoleIntent;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UserRoleMigrationHandlerTest {
+
+  private final ManagementIdentityClient managementIdentityClient;
+  private final RoleServices roleServices;
+  private final UserRoleMigrationHandler userRoleMigrationHandler;
+
+  public UserRoleMigrationHandlerTest(
+      @Mock final ManagementIdentityClient managementIdentityClient,
+      @Mock(answer = Answers.RETURNS_SELF) final RoleServices roleServices) {
+    this.managementIdentityClient = managementIdentityClient;
+    this.roleServices = roleServices;
+    userRoleMigrationHandler =
+        new UserRoleMigrationHandler(
+            CamundaAuthentication.none(), managementIdentityClient, roleServices);
+  }
+
+  @Test
+  public void shouldMigrateUserRoles() {
+    // given
+    when(managementIdentityClient.fetchUsers(any(Integer.class)))
+        .thenReturn(
+            List.of(
+                new User("user1", "username1", "name", "email@email.com"),
+                new User("user2", "username2", "name", "email@email.com")))
+        .thenReturn(List.of());
+    when(managementIdentityClient.fetchUserRoles(any()))
+        .thenReturn(
+            List.of(
+                new Role("Role 1", "Description for Role 1"),
+                new Role(
+                    "Role@Name#With$Special%Chars", "Description for Role with special chars")))
+        .thenReturn(
+            List.of(
+                new Role("Role 2", "Description for Role 2"),
+                new Role("Role 3", "Description for Role 3")));
+    when(roleServices.addMember(any())).thenReturn(CompletableFuture.completedFuture(null));
+
+    // when
+    userRoleMigrationHandler.migrate();
+
+    // then
+    final var roleMembershipCaptor = ArgumentCaptor.forClass(RoleMemberRequest.class);
+    verify(roleServices, times(4)).addMember(roleMembershipCaptor.capture());
+    final var requests = roleMembershipCaptor.getAllValues();
+    assertThat(requests)
+        .extracting(RoleMemberRequest::roleId, RoleMemberRequest::entityId)
+        .containsExactlyInAnyOrder(
+            tuple("role_1", "username1"),
+            tuple("role@name_with_special_chars", "username1"),
+            tuple("role_2", "username2"),
+            tuple("role_3", "username2"));
+  }
+
+  @Test
+  public void shouldNotBlockTheMigrationIfTheMembershipAlreadyExists() {
+    // given
+    when(managementIdentityClient.fetchUsers(any(Integer.class)))
+        .thenReturn(
+            List.of(
+                new User("user1", "username1", "name", "email@email.com"),
+                new User("user2", "username2", "name", "email@email.com")))
+        .thenReturn(List.of());
+    when(managementIdentityClient.fetchUserRoles(any()))
+        .thenReturn(
+            List.of(
+                new Role("Role 1", "Description for Role 1"),
+                new Role(
+                    "Role@Name#With$Special%Chars", "Description for Role with special chars")))
+        .thenReturn(
+            List.of(
+                new Role("Role 2", "Description for Role 2"),
+                new Role("Role 3", "Description for Role 3")));
+    when(roleServices.addMember(any()))
+        .thenReturn(
+            CompletableFuture.failedFuture(
+                new BrokerRejectionException(
+                    new BrokerRejection(
+                        RoleIntent.ADD_ENTITY,
+                        -1,
+                        RejectionType.ALREADY_EXISTS,
+                        "role membership exists"))));
+
+    // when
+    userRoleMigrationHandler.migrate();
+
+    // then
+    verify(roleServices, times(4)).addMember(any());
+  }
+}
--- qa/acceptance-tests/src/test/java/io/camunda/it/migration/KeycloakIdentityMigrationIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/migration/KeycloakIdentityMigrationIT.java
@@ -26,6 +26,7 @@ import io.camunda.client.api.search.enums.ResourceType;
 import io.camunda.client.api.search.response.Group;
 import io.camunda.client.api.search.response.GroupUser;
 import io.camunda.client.api.search.response.Role;
+import io.camunda.client.api.search.response.RoleUser;
 import io.camunda.migration.identity.config.IdentityMigrationProperties;
 import io.camunda.migration.identity.config.IdentityMigrationProperties.Mode;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
@@ -104,6 +105,7 @@ public class KeycloakIdentityMigrationIT {
           .withEnv("KEYCLOAK_USERS_1_USERNAME", "user1")
           .withEnv("KEYCLOAK_USERS_1_PASSWORD", "password")
           .withEnv("KEYCLOAK_USERS_1_GROUPS_0", "groupC")
+          .withEnv("KEYCLOAK_USERS_1_ROLES_0", "Zeebe")
           // assign authorizations to groups
           .withEnv("IDENTITY_AUTHORIZATIONS_0_GROUP_NAME", "groupA")
           .withEnv("IDENTITY_AUTHORIZATIONS_0_RESOURCE_KEY", "*")
@@ -367,6 +369,34 @@ public class KeycloakIdentityMigrationIT {
                     PermissionType.DELETE_DECISION_INSTANCE,
                     PermissionType.READ_DECISION_INSTANCE,
                     PermissionType.READ_DECISION_DEFINITION)));
+  }
+
+  @Test
+  public void canMigrateUsersRolesMembership() {
+    // when
+    migration.start();
+
+    Awaitility.await()
+        .atMost(Duration.ofSeconds(5))
+        .ignoreExceptions()
+        .untilAsserted(
+            () -> {
+              final var users = client.newUsersByRoleSearchRequest("zeebe").send().join();
+              assertThat(users.items()).hasSize(2);
+            });
+
+    // then
+    assertThat(migration.getExitCode()).isEqualTo(0);
+    final var zeebeUsers = client.newUsersByRoleSearchRequest("zeebe").send().join().items();
+    assertThat(zeebeUsers)
+        .extracting(RoleUser::getUsername)
+        .containsExactlyInAnyOrder("user0", "user1");
+    final var operateUsers = client.newUsersByRoleSearchRequest("operate").send().join().items();
+    assertThat(operateUsers).extracting(RoleUser::getUsername).containsExactlyInAnyOrder("user0");
+    final var tasklistUsers = client.newUsersByRoleSearchRequest("tasklist").send().join().items();
+    assertThat(tasklistUsers).extracting(RoleUser::getUsername).containsExactlyInAnyOrder("user0");
+    final var identityUsers = client.newUsersByRoleSearchRequest("identity").send().join().items();
+    assertThat(identityUsers).extracting(RoleUser::getUsername).containsExactlyInAnyOrder("user0");
   }
 
   // TODO: refactor this once https://github.com/camunda/camunda/issues/32721 is implemented
