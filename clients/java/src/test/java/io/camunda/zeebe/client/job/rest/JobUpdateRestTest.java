93a550c224aa821884d2cfe18bc012a8c57ba649
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-07-04 16:14:26 +0300

    fix: set operationRef on REST job update requests for ZeebeClient
    Ensure that `operationReference` is correctly set on REST request objects
    in `JobUpdateRetriesCommandImpl` and `JobUpdateTimeoutCommandImpl` of ZeebeClient.
    
    Added missing test coverage to verify that the field is properly propagated
    in both REST and gRPC request implementations.

--- clients/java/src/test/java/io/camunda/zeebe/client/job/rest/JobUpdateRestTest.java
+++ clients/java/src/test/java/io/camunda/zeebe/client/job/rest/JobUpdateRestTest.java
@@ -57,6 +57,24 @@ public class JobUpdateRestTest extends ClientRestTest {
   }
 
   @Test
+  public void shouldSetOperationReferenceForUpdateRetriesCommand() {
+    // given
+    final long operationReference = 456;
+
+    // when
+    client
+        .newUpdateRetriesCommand(123)
+        .retries(3)
+        .operationReference(operationReference)
+        .send()
+        .join();
+
+    // then
+    final JobUpdateRequest request = gatewayService.getLastRequest(JobUpdateRequest.class);
+    assertThat(request.getOperationReference()).isEqualTo(operationReference);
+  }
+
+  @Test
   public void shouldUpdateTimeoutByKeyMillis() {
     // given
     final long jobKey = 12;
@@ -112,6 +130,24 @@ public class JobUpdateRestTest extends ClientRestTest {
     // then
     final JobUpdateRequest request = gatewayService.getLastRequest(JobUpdateRequest.class);
     assertThat(request.getChangeset().getTimeout()).isEqualTo(timeout.toMillis());
+  }
+
+  @Test
+  public void shouldSetOperationReferenceForUpdateTimeoutCommand() {
+    // given
+    final long operationReference = 456;
+
+    // when
+    client
+        .newUpdateTimeoutCommand(123)
+        .timeout(100)
+        .operationReference(operationReference)
+        .send()
+        .join();
+
+    // then
+    final JobUpdateRequest request = gatewayService.getLastRequest(JobUpdateRequest.class);
+    assertThat(request.getOperationReference()).isEqualTo(operationReference);
   }
 
   @Test
