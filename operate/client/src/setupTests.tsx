2127e95d1221cc96b40903082434d56a14d47b80
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-07-06 19:18:11 +0200

    test: Fix setup

--- operate/client/src/setupTests.tsx
+++ operate/client/src/setupTests.tsx
@@ -9,19 +9,95 @@
 import '@testing-library/jest-dom';
 import {mockServer} from 'modules/mock-server/node';
 import {configure} from 'modules/testing-library';
+import React from 'react';
 
-vi.mock('modules/utils/date/formatDate');
+vi.mock('@devbookhq/splitter', async () => {
+  const actual = await vi.importActual('@devbookhq/splitter');
+  const mockModule = await import('./__mocks__/@devbookhq/splitter');
+  return {
+    ...actual,
+    default: mockModule.default,
+    SplitDirection: mockModule.SplitDirection,
+  };
+});
 
-beforeAll(() => mockServer.listen());
+vi.mock('modules/components/MonacoEditor');
+
+vi.mock('modules/components/InfiniteScroller', () => {
+  const InfiniteScroller: React.FC<{children?: React.ReactNode}> = ({
+    children,
+  }) => {
+    return <>{children}</>;
+  };
+  return {InfiniteScroller};
+});
+
+vi.mock('modules/stores/licenseTag', () => ({
+  licenseTagStore: {
+    fetchLicense: vi.fn(),
+    state: {isTagVisible: false},
+  },
+}));
+
+vi.mock('bpmn-js/lib/features/outline', () => {
+  return () => {};
+});
+
+vi.mock('@floating-ui/react-dom', () => {
+  const originalModule = vi.importActual('@floating-ui/react-dom');
+
+  return {
+    ...originalModule,
+    hide: () => {},
+  };
+});
+
+const localStorageMock = (function () {
+  let store: {[key: string]: string} = {};
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+    setItem(key: string, value: string) {
+      store[key] = value;
+    },
+    clear() {
+      store = {};
+    },
+    removeItem: vi.fn(),
+  };
+})();
+
+beforeAll(() =>
+  mockServer.listen({
+    onUnhandledRequest: 'error',
+  }),
+);
 afterEach(() => mockServer.resetHandlers());
 afterAll(() => mockServer.close());
-beforeEach(() => {
-  Object.defineProperty(window, 'clientConfig', {
-    value: {
-      canLogout: true,
-    },
-    writable: true,
+beforeEach(async () => {
+  vi.stubGlobal(
+    'ResizeObserver',
+    (await import('resize-observer-polyfill')).default,
+  );
+  vi.stubGlobal('localStorage', localStorageMock);
+  vi.stubGlobal('MutationObserver', MutationObserver);
+  vi.stubGlobal('clientConfig', {
+    canLogout: true,
   });
+  vi.stubGlobal(
+    'matchMedia',
+    vi.fn().mockImplementation(() => ({
+      matches: false,
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+    })),
+  );
+
+  // Clear localStorage for each test
+  localStorage.clear();
 });
 
 configure({
