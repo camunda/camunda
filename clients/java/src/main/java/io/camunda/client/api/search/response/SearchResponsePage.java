1c0cc0c1dd9db816b652ee02320547957d898c75
Konstantinos Mylosis <konstantinos.mylosis@camunda.com>
2025-07-07 14:22:17 +0300

    refactor: avoid mentioning default limitation from ES/OS results

--- clients/java/src/main/java/io/camunda/client/api/search/response/SearchResponsePage.java
+++ clients/java/src/main/java/io/camunda/client/api/search/response/SearchResponsePage.java
@@ -21,17 +21,20 @@ public interface SearchResponsePage {
   Long totalItems();
 
   /**
-   * Whether more than 10,000 results exist in Elasticsearch (ES) or OpenSearch (OS) searches.
+   * Whether the total items count exceeds the maximum limit in Elasticsearch (ES) or OpenSearch
+   * (OS).
    *
-   * <p>In ES or OS, total hits are capped at 10,000. If the result set is greater than or equal to
-   * this, this method returns {@code true}; otherwise, it returns {@code false}.
+   * <p>In ES or OS, total items are often capped by a predefined configurable limit. If the result
+   * set is greater than or equal to this, this method returns {@code true}; otherwise, it returns
+   * {@code false}.
    *
    * <p>For RDBMS-backed searches, this is always {@code false} because there is no such limitation.
    *
    * <p>This helps clients understand when total item counts may be incomplete due to ES or OS
    * limits.
    *
-   * @return {@code true} if more than 10,000 items exist in ES or OS; {@code false} otherwise.
+   * @return {@code true} if the total result count exceeds the cap in ES or OS; {@code false}
+   *     otherwise.
    */
   default Boolean hasMoreTotalItems() {
     return false;
