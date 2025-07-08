124fdf6dcd696da1793c7dc9d3f1b95a1727ab8c
Alex Bremard <alex.bremard@camunda.com>
2025-07-08 11:32:26 +0700

    fix: use NoopSearchClientsProxy instead of disabling all services when database.type=none
    - Add NoopSearchClientsProxy bean when database.type=none
    - Remove DatabaseNotNoneCondition and allow all services to load
    - Services that don't depend on search clients remain functional
    - Services that depend on search clients get noop implementations
    - This preserves useful functionality while preventing startup errors
    
    Co-authored-by: abremard <48785808+abremard@users.noreply.github.com>

--- dist/src/main/java/io/camunda/application/commons/search/SearchClientDatabaseConfiguration.java
+++ dist/src/main/java/io/camunda/application/commons/search/SearchClientDatabaseConfiguration.java
@@ -10,6 +10,7 @@ package io.camunda.application.commons.search;
 import io.camunda.db.rdbms.RdbmsService;
 import io.camunda.search.clients.DocumentBasedSearchClient;
 import io.camunda.search.clients.DocumentBasedSearchClients;
+import io.camunda.search.clients.SearchClientsProxy;
 import io.camunda.search.connect.configuration.ConnectConfiguration;
 import io.camunda.search.connect.configuration.DatabaseConfig;
 import io.camunda.search.connect.es.ElasticsearchConnector;
@@ -71,5 +72,14 @@ public class SearchClientDatabaseConfiguration {
             connectConfiguration.getIndexPrefix(),
             connectConfiguration.getTypeEnum().isElasticSearch());
     return new DocumentBasedSearchClients(searchClient, indexDescriptors);
+  }
+
+  @Bean
+  @ConditionalOnProperty(
+      prefix = "camunda.database",
+      name = "type",
+      havingValue = "none")
+  public SearchClientsProxy noopSearchClientsProxy() {
+    return SearchClientsProxy.noop();
   }
 }
