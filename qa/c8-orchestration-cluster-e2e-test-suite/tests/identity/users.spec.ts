ee4431377462e854e5b2b94062695b4bc3a15643
YuliiaKorsun <yuliia.korsun@camunda.com>
2025-07-11 17:49:01 +0200

    test: Admin user can delete user
    Updated test and changed deleteUser method to use 2 arguments, so it search for the uniq locator
     async deleteUser(user: {username: string; email: string}) {

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/identity/users.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/identity/users.spec.ts
@@ -12,7 +12,7 @@ import {expect} from '@playwright/test';
 import {captureScreenshot, captureFailureVideo} from '@setup';
 import {relativizePath, Paths} from 'utils/relativizePath';
 
-test.describe('Admin user can delete user', () => {
+test.describe('Users Page Tests', () => {
   test.beforeEach(async ({loginPage, page}) => {
     await navigateToApp(page, 'identity');
     await loginPage.login('demo', 'demo');
@@ -23,41 +23,45 @@ test.describe('Admin user can delete user', () => {
     await captureFailureVideo(page, testInfo);
   });
 
-  test('Deleted user cannot login anymore', async ({
+  const TEST_USER = {
+    username: 'yuliia',
+    password: 'yuliia',
+    email: 'yuliia@example.com',
+  };
+
+  test('Admin user can delete user', async ({
     page,
     loginPage,
     identityUsersPage,
     identityHeader,
   }) => {
     await expect(identityUsersPage.usersList).toBeVisible();
-    await expect(identityUsersPage.usersList).toContainText('yuliia');
-    await identityUsersPage.deleteUser('yuliia');
+    await expect(identityUsersPage.usersList).toContainText(TEST_USER.username);
+    await identityUsersPage.deleteUser(TEST_USER);
     await identityHeader.logout();
     await expect(page).toHaveURL(
       `${relativizePath(Paths.login('identity'))}?next=/identity/`,
     );
 
-    await page.reload();
-
-    await test.step(`Deleted user cannot access /identity`, async () => {
+    await test.step(`Deleted user cannot access Identity`, async () => {
       await navigateToApp(page, `identity`);
-      await loginPage.login('yuliia', 'yuliia');
+      await loginPage.login(TEST_USER.username, TEST_USER.password);
       await expect(page).toHaveURL(new RegExp(`identity`));
       await expect(loginPage.errorMessage).toContainText(
-        /Username and password do(?: not|n't) match/,
+        /Username and [Pp]assword do(?: not|n't) match/,
       );
     });
 
-    await test.step(`Deleted user cannot access /tasklist`, async () => {
+    await test.step(`Deleted user cannot access Tasklist`, async () => {
       await navigateToApp(page, `tasklist`);
       await loginPage.login('yuliia', 'yuliia');
       await expect(page).toHaveURL(new RegExp(`tasklist`));
       await expect(loginPage.errorMessage).toContainText(
-        /Username and password do(?: not|n't) match/,
+        /Username and [Pp]assword do(?: not|n't) match/,
       );
     });
 
-    await test.step(`Deleted user cannot access /operate`, async () => {
+    await test.step(`Deleted user cannot access Operate`, async () => {
       await navigateToApp(page, `operate`);
       await loginPage.login('yuliia', 'yuliia');
       await expect(page).toHaveURL(new RegExp(`operate`));
