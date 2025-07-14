7100359a6099b6496ee6b67f4cb578d71162fbb3
Patrick Dehn <patrick.dehn@camunda.com>
2025-07-14 17:05:09 +0200

    feat: add batch operation API support for items and incident resolution

---
+++ operate/client/src/modules/mocks/api/v2/processInstances/createIncidentResolutionBatchOperation.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {endpoints} from '@vzeta/camunda-api-zod-schemas';
+import {mockPostRequest} from '../../mockRequest';
+
+const mockCreateIncidentResolutionBatchOperation = (contextPath = '') =>
+  mockPostRequest(
+    `${contextPath}${endpoints.createIncidentResolutionBatchOperation.getUrl()}`,
+  );
+
+export {mockCreateIncidentResolutionBatchOperation};
