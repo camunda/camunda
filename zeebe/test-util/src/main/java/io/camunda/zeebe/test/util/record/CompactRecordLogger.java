553d360c7f2405a5e870b152fafc610b2eb6b43f
Nico Korthout <nico.korthout@camunda.com>
2025-07-14 17:57:36 +0200

    feat: summarize compensation subscription
    This adds a summary for compensation subscriptions.
    
    The summary is different for compensation subscriptions that have not
    yet been triggered, and those that have.
    
    In case the compensation subscription has not yet been triggered the
    summary describes which compensation handler is registered for the
    compensable activity. This makes it clear what would happen when the
    compensation is triggered, i.e. the compensation handler is triggered to
    compensate the compensable activity.
    
    In case the compensation subscription has been triggered, the summary
    describes which event triggered it, and what effect it had. i.e.
    executing the compensation handler. Only in this case can there be
    variables present.
    
    It's not useful to present the process information again on the events
    where the subscription has already been triggered. It's easy to infer
    from the other keys in use.

--- zeebe/test-util/src/main/java/io/camunda/zeebe/test/util/record/CompactRecordLogger.java
+++ zeebe/test-util/src/main/java/io/camunda/zeebe/test/util/record/CompactRecordLogger.java
@@ -204,6 +204,11 @@ public class CompactRecordLogger {
     valueLoggers.put(ValueType.GROUP, this::summarizeGroup);
     valueLoggers.put(ValueType.MAPPING_RULE, this::summarizeMappingRule);
     valueLoggers.put(ValueType.ASYNC_REQUEST, this::summarizeAsyncRequest);
+    valueLoggers.put(ValueType.USER, this::summarizeUser);
+    valueLoggers.put(ValueType.AUTHORIZATION, this::summarizeAuthorization);
+    valueLoggers.put(ValueType.RESOURCE, this::summarizeResource);
+    valueLoggers.put(ValueType.RESOURCE_DELETION, this::summarizeResourceDeletion);
+    valueLoggers.put(ValueType.COMPENSATION_SUBSCRIPTION, this::summarizeCompensationSubscription);
     valueLoggers.put(ValueType.MULTI_INSTANCE, this::summarizeMultiInstance);
     valueLoggers.put(ValueType.RUNTIME_INSTRUCTION, this::summarizeRuntimeInstruction);
     valueLoggers.put(ValueType.BATCH_OPERATION_CREATION, this::summarizeBatchOperationCreation);
@@ -1173,6 +1178,57 @@ public class CompactRecordLogger {
     if (value.getElementId() != null) {
       result.append(" interrupted by \"").append(value.getElementId()).append("\"");
     }
+    return result.toString();
+  }
+
+  private String summarizeCompensationSubscription(final Record<?> record) {
+    final var result = new StringBuilder();
+    final var value = (CompensationSubscriptionRecordValue) record.getValue();
+    if (value.getThrowEventInstanceKey() < 0) {
+      // compensation subscription has not been triggered yet, for example:
+      // E COMP_SUB CREATED #28->#22 K11 "CompHandler" → "TaskToCompensate"[K08] in <process
+      // K03[K04]>
+      // explains that the compensation handler is registered for the compensable activity
+      result
+          .append("\"")
+          .append(StringUtils.abbreviateMiddle(value.getCompensationHandlerId(), "..", 20))
+          .append("\"")
+          .append("[")
+          .append(shortenKey(value.getCompensationHandlerInstanceKey()))
+          .append("]")
+          .append(" → ")
+          .append("\"")
+          .append(StringUtils.abbreviateMiddle(value.getCompensableActivityId(), "..", 20))
+          .append("\"")
+          .append("[")
+          .append(shortenKey(value.getCompensableActivityInstanceKey()))
+          .append("]")
+          .append(
+              summarizeProcessInformation(
+                  value.getProcessDefinitionKey(), value.getProcessInstanceKey()));
+    } else {
+      // an event was thrown triggering the compensation, for example:
+      // E COMP_SUB TRIGGERED #39->#22 K11 "CompThrowEvent"[K13] → "CompensationHandler"[K15] (no
+      // vars)
+      // explains that the throw event has triggered the compensation handler without vars
+      result
+          .append("\"")
+          .append(StringUtils.abbreviateMiddle(value.getThrowEventId(), "..", 20))
+          .append("\"")
+          .append("[")
+          .append(shortenKey(value.getThrowEventInstanceKey()))
+          .append("]")
+          .append(" → ")
+          .append("\"")
+          .append(StringUtils.abbreviateMiddle(value.getCompensationHandlerId(), "..", 20))
+          .append("\"")
+          .append("[")
+          .append(shortenKey(value.getCompensationHandlerInstanceKey()))
+          .append("]")
+          .append(formatVariables(value));
+    }
+
+    result.append(formatTenant(value));
     return result.toString();
   }
 
