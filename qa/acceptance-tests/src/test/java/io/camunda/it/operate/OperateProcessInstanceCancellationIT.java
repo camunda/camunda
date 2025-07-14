2fb4a10a71bb9dc487ea6e39726ef942f7e3fa0b
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2025-07-14 17:23:08 +0200

    WIP

--- qa/acceptance-tests/src/test/java/io/camunda/it/operate/OperateProcessInstanceCancellationIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/operate/OperateProcessInstanceCancellationIT.java
@@ -14,14 +14,22 @@ import static io.camunda.client.api.search.enums.PermissionType.READ_PROCESS_INS
 import static io.camunda.client.api.search.enums.PermissionType.UPDATE_PROCESS_INSTANCE;
 import static io.camunda.client.api.search.enums.ResourceType.PROCESS_DEFINITION;
 import static io.camunda.client.api.search.enums.ResourceType.RESOURCE;
+import static io.camunda.it.util.TestHelper.waitForBatchOperationWithCorrectTotalCount;
+import static io.camunda.qa.util.multidb.CamundaMultiDBExtension.TIMEOUT_DATA_AVAILABILITY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.response.DeploymentEvent;
 import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.client.api.search.enums.BatchOperationState;
 import io.camunda.client.api.search.enums.ElementInstanceState;
 import io.camunda.client.api.search.enums.ElementInstanceType;
+import io.camunda.client.api.search.enums.ProcessInstanceState;
 import io.camunda.client.api.search.response.ElementInstance;
+import io.camunda.it.util.TestHelper;
+import io.camunda.operate.webapp.rest.dto.listview.ListViewQueryDto;
+import io.camunda.operate.webapp.rest.dto.listview.ListViewRequestDto;
+import io.camunda.operate.webapp.rest.dto.listview.ProcessInstanceStateDto;
 import io.camunda.qa.util.auth.Authenticated;
 import io.camunda.qa.util.auth.Permissions;
 import io.camunda.qa.util.auth.TestUser;
@@ -38,7 +46,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 @MultiDbTest
-public class OperateProcessInstanceMigrationIT {
+public class OperateProcessInstanceCancellationIT {
   @MultiDbTestApplication
   static final TestCamundaApplication CAMUNDA_APPLICATION =
       new TestCamundaApplication().withAuthorizationsEnabled().withBasicAuth();
@@ -67,61 +75,36 @@ public class OperateProcessInstanceMigrationIT {
   }
 
   @Test
-  void shouldMigrateSubprocessToSubprocess(
+  void shouldCancelProcess(
       @Authenticated(SUPER_USER_USERNAME) final CamundaClient client) {
     // given
     // process instances that are running
-    deployProcess(client, "process/migration-subprocess.bpmn");
-    final long processInstanceKey = createProcessInstance(client, "prWithSubprocess");
-    completeJob(client, "taskA");
-    final var processDefinitionTo = deployProcess(client, "process/migration-subprocess2.bpmn");
-
-    // we have the given migration plan
-    final var migrationPlan =
-        MigrationPlan.newBuilder()
-            .withTargetProcessDefinitionKey(processDefinitionTo)
-            .addMappingInstruction("taskA", "taskA")
-            .addMappingInstruction("subprocess", "subprocess2")
-            .addMappingInstruction("innerSubprocess", "innerSubprocess2")
-            .addMappingInstruction("taskB", "taskB")
-            .build();
+    deployProcess(client, "process/service_tasks_v1.bpmn");
+    final long processInstanceKey = createProcessInstance(client, "service_tasks_v1");
 
     // when
-    // execute MIGRATE_PROCESS_INSTANCE
-    final var batchOperationEntity =
-        operateClient.migrateProcessInstanceBatchOperationWith(processInstanceKey, migrationPlan);
+    // execute CANCEL_PROCESS_INSTANCE
+    final var batchOperationEntity = operateClient.cancelProcessInstancesBatchOperationRequest(
+        new ListViewQueryDto().setIds(List.of(String.valueOf(processInstanceKey)))
+    );
 
     // then
     // This should:
     //
     //   * Create a batch operation
-    //   * Create an operation for the migration
-    //   * Trigger the migration via Command on Zeebe
+    //   * Create an operation for the cancellation
+    //   * Trigger the cancellation via Command on Zeebe
     //   * Zeebe should process and export the respective events
-    //   * The process should be migrated
+    //   * The process should be cancelled
     //   * Operation should be marked completed by exporter at the end
     assertThat(batchOperationEntity.isRight())
         .withFailMessage("Expected batch operation to be created.")
         .isTrue();
-    waitForFNIState(client, processInstanceKey, "subProcess2");
+    final var batchOperationId = batchOperationEntity.get().getId();
 
-    final var elementInstanceSearchQueryResponse =
-        client
-            .newElementInstanceSearchRequest()
-            .filter(
-                flownodeInstanceFilter ->
-                    flownodeInstanceFilter.processInstanceKey(processInstanceKey))
-            .filter(
-                flownodeInstanceFilter ->
-                    flownodeInstanceFilter.type(ElementInstanceType.SUB_PROCESS))
-            .send()
-            .join();
+    waitForBatchOperationWithCorrectTotalCount(client, batchOperationId, 1);
 
-    final var items = elementInstanceSearchQueryResponse.items();
-    assertThat(items).hasSize(2);
-    assertMigratedFieldsByFlowNodeId(items, "subprocess2", processInstanceKey, processDefinitionTo);
-    assertMigratedFieldsByFlowNodeId(
-        items, "innerSubprocess2", processInstanceKey, processDefinitionTo);
+    waitForProcessCancelledState(client, processInstanceKey);
   }
 
   private static long deployProcess(final CamundaClient client, final String path) {
@@ -148,8 +131,8 @@ public class OperateProcessInstanceMigrationIT {
     return processDefinitionKey;
   }
 
-  private static long waitForFNIState(
-      final CamundaClient client, final long processInstanceKey, final String elementId) {
+  private static long waitForProcessCancelledState(
+      final CamundaClient client, final long processInstanceKey) {
     Awaitility.await("should wait until flow node instance is in given state")
         .atMost(Duration.ofSeconds(60))
         .ignoreExceptions() // Ignore exceptions and continue retrying
@@ -157,15 +140,11 @@ public class OperateProcessInstanceMigrationIT {
             () -> {
               final var result =
                   client
-                      .newElementInstanceSearchRequest()
-                      .filter(
-                          flownodeInstanceFilter ->
-                              flownodeInstanceFilter.processInstanceKey(processInstanceKey))
-                      .filter(flownodeInstanceFilter -> flownodeInstanceFilter.elementId(elementId))
+                      .newProcessInstanceGetRequest(processInstanceKey)
                       .send()
                       .join();
 
-              assertThat(result.items()).isNotEmpty();
+              assertThat(result.getState()).isEqualTo(ProcessInstanceState.TERMINATED);
             });
 
     return processInstanceKey;
@@ -183,63 +162,60 @@ public class OperateProcessInstanceMigrationIT {
             .join();
     final long processInstanceKey = processInstanceEvent.getProcessInstanceKey();
 
-    Awaitility.await("should wait until flow node instances are available")
-        .atMost(Duration.ofSeconds(60))
+    TestHelper.waitForProcessInstancesToStart(
+        client,
+        b -> b
+            .processInstanceKey(processInstanceKey)
+            .state(ProcessInstanceState.ACTIVE),
+        1
+    );
+
+    Awaitility.await("operate should find the process instance")
+        .atMost(TIMEOUT_DATA_AVAILABILITY)
         .ignoreExceptions() // Ignore exceptions and continue retrying
         .untilAsserted(
             () -> {
-              final var result = client.newElementInstanceSearchRequest().send().join();
-              final var any =
-                  result.items().stream()
-                      .filter(event -> event.getProcessInstanceKey() == processInstanceKey)
-                      .findAny();
-              assertThat(any).isPresent();
+              // and
+              final var processEither = operateClient.getProcessInstanceWith(processInstanceKey);
+              assertThat(processEither.isRight()).isTrue();
+              assertThat(processEither.get().processInstances().getFirst().getState()).isEqualTo("ACTIVE");
+            });
+
+    try {
+      Thread.sleep(30000);
+    } catch (final InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+
+    Awaitility.await("operate should find the process instance")
+        .atMost(TIMEOUT_DATA_AVAILABILITY)
+        .ignoreExceptions() // Ignore exceptions and continue retrying
+        .untilAsserted(
+            () -> {
+              // and
+              final var processEither = operateClient.getProcessInstanceWith(processInstanceKey);
+              assertThat(processEither.isRight()).isTrue();
+              assertThat(processEither.get().processInstances().getFirst().getState()).isEqualTo("ACTIVE");
+            });
+
+    Awaitility.await("operate should find the process instance")
+        .atMost(TIMEOUT_DATA_AVAILABILITY)
+        .ignoreExceptions() // Ignore exceptions and continue retrying
+        .untilAsserted(
+            () -> {
+              // and
+              final var processEither = operateClient.searchProcessInstances(
+                  new ListViewRequestDto().setQuery(
+                      new ListViewQueryDto()
+                          .setIds(List.of(String.valueOf(processInstanceKey)))
+                  )
+              );
+              assertThat(processEither.isRight()).isTrue();
+              assertThat(processEither.get().getProcessInstances().getFirst().getState()).isEqualTo(
+                  ProcessInstanceStateDto.ACTIVE);
             });
 
     return processInstanceKey;
   }
 
-  private static long completeJob(final CamundaClient client, final String jobType) {
-    final var jobsResponse =
-        client
-            .newActivateJobsCommand()
-            .jobType(jobType)
-            .maxJobsToActivate(1)
-            .workerName("this")
-            .send()
-            .join();
-    assertThat(jobsResponse.getJobs()).isNotEmpty();
-
-    final var job = jobsResponse.getJobs().getFirst();
-    client.newCompleteCommand(job).send().join();
-
-    Awaitility.await("should wait until flow node instances are available")
-        .atMost(Duration.ofSeconds(60))
-        .ignoreExceptions() // Ignore exceptions and continue retrying
-        .untilAsserted(
-            () -> {
-              final var result = client.newElementInstanceSearchRequest().send().join();
-              final var any =
-                  result.items().stream()
-                      .filter(
-                          event ->
-                              event.getElementInstanceKey() == job.getElementInstanceKey()
-                                  && event.getState().equals(ElementInstanceState.COMPLETED))
-                      .findAny();
-              assertThat(any).isPresent();
-            });
-
-    return job.getKey();
-  }
-
-  private void assertMigratedFieldsByFlowNodeId(
-      final List<ElementInstance> fnis,
-      final String flowNodeId,
-      final Long instanceKey,
-      final Long processDefinitionTo) {
-    final var flowNode =
-        fnis.stream().filter(fn -> fn.getElementId().equals(flowNodeId)).findFirst().orElseThrow();
-    assertThat(flowNode.getProcessInstanceKey()).isEqualTo(instanceKey);
-    assertThat(flowNode.getProcessDefinitionKey()).isEqualTo(processDefinitionTo);
-  }
 }
