256d4e855abd0a140526df5337b8050f1b5ad034
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2025-07-14 16:40:16 +0200

    feat: add active profiles to payload.

--- dist/src/main/java/io/camunda/application/commons/console/ping/PingConsoleRunner.java
+++ dist/src/main/java/io/camunda/application/commons/console/ping/PingConsoleRunner.java
@@ -20,6 +20,7 @@ import io.camunda.zeebe.util.retry.RetryConfiguration;
 import java.net.URI;
 import java.time.Duration;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -31,6 +32,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -42,13 +44,16 @@ public class PingConsoleRunner implements ApplicationRunner {
   private final ConsolePingConfiguration pingConfiguration;
   private final ManagementServices managementServices;
   private final Either<Exception, String> licensePayload;
+  private final ApplicationContext applicationContext;
 
   @Autowired
   public PingConsoleRunner(
       final ConsolePingConfiguration pingConfigurationProperties,
-      final ManagementServices managementServices) {
+      final ManagementServices managementServices,
+      final ApplicationContext applicationContext) {
     pingConfiguration = pingConfigurationProperties;
     this.managementServices = managementServices;
+    this.applicationContext = applicationContext;
     licensePayload = getLicensePayload();
   }
 
@@ -150,6 +155,7 @@ public class PingConsoleRunner implements ApplicationRunner {
             pingConfiguration.clusterId(),
             pingConfiguration.clusterName(),
             VersionUtil.getVersion(),
+            Arrays.stream(applicationContext.getEnvironment().getActiveProfiles()).toList(),
             pingConfiguration.properties());
     try {
       return Either.right(objectMapper.writeValueAsString(payload));
