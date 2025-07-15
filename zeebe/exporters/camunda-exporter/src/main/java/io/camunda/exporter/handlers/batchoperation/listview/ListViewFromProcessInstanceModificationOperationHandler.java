20722febb2ad6eae5ccebf845a5acc4bdf47a791
Mustafa Dagher <mustafa.dagher@camunda.com>
2025-07-16 00:03:40 +0200

    feat: added handler to link a process instance to its completed modification batch operation

---
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/batchoperation/listview/ListViewFromProcessInstanceModificationOperationHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.handlers.batchoperation.listview;
+
+import io.camunda.webapps.schema.entities.operation.OperationType;
+import io.camunda.zeebe.exporter.common.cache.ExporterEntityCache;
+import io.camunda.zeebe.exporter.common.cache.batchoperation.CachedBatchOperationEntity;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.ProcessInstanceModificationIntent;
+import io.camunda.zeebe.protocol.record.value.ProcessInstanceRecordValue;
+
+public class ListViewFromProcessInstanceModificationOperationHandler
+    extends AbstractProcessInstanceFromOperationItemHandler<ProcessInstanceRecordValue> {
+
+  public ListViewFromProcessInstanceModificationOperationHandler(
+      final String indexName,
+      final ExporterEntityCache<String, CachedBatchOperationEntity> batchOperationCache) {
+    super(indexName, batchOperationCache, OperationType.MODIFY_PROCESS_INSTANCE);
+  }
+
+  @Override
+  public ValueType getHandledValueType() {
+    return ValueType.PROCESS_INSTANCE_MODIFICATION;
+  }
+
+  @Override
+  protected boolean isFailed(final Record<ProcessInstanceRecordValue> record) {
+    return record.getIntent().equals(ProcessInstanceModificationIntent.MODIFY)
+        && record.getRejectionType() != RejectionType.NULL_VAL;
+  }
+
+  @Override
+  protected boolean isCompleted(final Record<ProcessInstanceRecordValue> record) {
+    return record.getIntent() == ProcessInstanceModificationIntent.MODIFIED;
+  }
+}
