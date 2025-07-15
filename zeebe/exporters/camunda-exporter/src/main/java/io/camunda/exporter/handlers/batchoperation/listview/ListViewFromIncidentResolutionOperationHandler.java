5b9a9179e6c469bc6fa697265595bf6b5c13ba5b
Mustafa Dagher <mustafa.dagher@camunda.com>
2025-07-16 00:05:23 +0200

    feat: added handler to link a process instance to its incident resolution batch operation

---
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/batchoperation/listview/ListViewFromIncidentResolutionOperationHandler.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.handlers.batchoperation.listview;
+
+import io.camunda.webapps.schema.entities.operation.OperationType;
+import io.camunda.zeebe.exporter.common.cache.ExporterEntityCache;
+import io.camunda.zeebe.exporter.common.cache.batchoperation.CachedBatchOperationEntity;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.RejectionType;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.IncidentIntent;
+import io.camunda.zeebe.protocol.record.value.IncidentRecordValue;
+
+public class ListViewFromIncidentResolutionOperationHandler
+    extends AbstractProcessInstanceFromOperationItemHandler<IncidentRecordValue> {
+
+  public ListViewFromIncidentResolutionOperationHandler(
+      final String indexName,
+      final ExporterEntityCache<String, CachedBatchOperationEntity> batchOperationCache) {
+    super(indexName, batchOperationCache, OperationType.RESOLVE_INCIDENT);
+  }
+
+  @Override
+  public ValueType getHandledValueType() {
+    return ValueType.INCIDENT;
+  }
+
+  @Override
+  protected boolean isFailed(final Record<IncidentRecordValue> record) {
+    return record.getIntent().equals(IncidentIntent.RESOLVE)
+        && record.getRejectionType() != RejectionType.NULL_VAL;
+  }
+
+  @Override
+  protected boolean isCompleted(final Record<IncidentRecordValue> record) {
+    return record.getIntent() == IncidentIntent.RESOLVED;
+  }
+}
