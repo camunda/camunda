59de4b2b76ee8194f30fdddd0aa786c8f4612424
Nico Korthout <nico.korthout@camunda.com>
2025-07-17 19:13:45 +0200

    feat: transform task headers on execution listener
    These changes adds task headers to the job worker properties of
    executable flow nodes with execution listeners.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/model/transformer/zeebe/ExecutionListenerTransformer.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/model/transformer/zeebe/ExecutionListenerTransformer.java
@@ -9,11 +9,16 @@ package io.camunda.zeebe.engine.processing.deployment.model.transformer.zeebe;
 
 import io.camunda.zeebe.el.ExpressionLanguage;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableFlowNode;
+import io.camunda.zeebe.model.bpmn.instance.BaseElement;
 import io.camunda.zeebe.model.bpmn.instance.zeebe.ZeebeExecutionListener;
+import io.camunda.zeebe.model.bpmn.instance.zeebe.ZeebeHeader;
+import io.camunda.zeebe.model.bpmn.instance.zeebe.ZeebeTaskHeaders;
 import io.camunda.zeebe.util.Either;
 import io.camunda.zeebe.util.buffer.BufferUtil;
 import java.util.Collection;
+import java.util.Map;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,14 +27,22 @@ public final class ExecutionListenerTransformer {
   private static final Logger LOGGER = LoggerFactory.getLogger(ExecutionListenerTransformer.class);
 
   public void transform(
+      final BaseElement element,
       final ExecutableFlowNode executableFlowNode,
       final Collection<ZeebeExecutionListener> executionListeners,
       final ExpressionLanguage expressionLanguage) {
 
+    if (executionListeners.isEmpty()) {
+      // No execution listeners defined, nothing to do
+      return;
+    }
+
+    final var taskHeaders = transformTaskHeaders(element);
+
     executionListeners.forEach(
         listener -> {
           try {
-            addListenerToFlowNode(listener, executableFlowNode, expressionLanguage);
+            addListenerToFlowNode(listener, executableFlowNode, expressionLanguage, taskHeaders);
           } catch (final Exception e) {
             // This is an additional safety, as our transformers currently require additional
             // safeguards to avoid breaking the partition on replay
@@ -44,10 +57,21 @@ public final class ExecutionListenerTransformer {
         });
   }
 
+  private Map<String, String> transformTaskHeaders(final BaseElement element) {
+    final var zeebeTaskHeaders = element.getSingleExtensionElement(ZeebeTaskHeaders.class);
+    if (zeebeTaskHeaders == null) {
+      return null;
+    }
+    return zeebeTaskHeaders.getHeaders().stream()
+        .filter(this::isValidHeader)
+        .collect(Collectors.toMap(ZeebeHeader::getKey, ZeebeHeader::getValue));
+  }
+
   private void addListenerToFlowNode(
       final ZeebeExecutionListener listener,
       final ExecutableFlowNode flowNode,
-      final ExpressionLanguage expressionLanguage) {
+      final ExpressionLanguage expressionLanguage,
+      final Map<String, String> taskHeaders) {
     Either.<Error, ZeebeExecutionListener>right(listener)
         .flatMap(l -> requireNotNull(l, () -> l, "listener"))
         .flatMap(l -> requireNotNull(l, l::getEventType, "eventType"))
@@ -58,7 +82,8 @@ public final class ExecutionListenerTransformer {
                 flowNode.addListener(
                     listener.getEventType(),
                     expressionLanguage.parseExpression(listener.getType()),
-                    expressionLanguage.parseExpression(listener.getRetries())),
+                    expressionLanguage.parseExpression(listener.getRetries()),
+                    taskHeaders),
             error ->
                 LOGGER.debug(
                     """
@@ -75,6 +100,14 @@ public final class ExecutionListenerTransformer {
       return Either.left(new Error("'%s' is null".formatted(attributeName)));
     }
     return Either.right(listener);
+  }
+
+  public boolean isValidHeader(final ZeebeHeader header) {
+    return header != null
+        && header.getValue() != null
+        && !header.getValue().isEmpty()
+        && header.getKey() != null
+        && !header.getKey().isEmpty();
   }
 
   private record Error(String reason) {}
