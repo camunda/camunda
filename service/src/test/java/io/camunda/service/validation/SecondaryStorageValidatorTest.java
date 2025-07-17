260b0cfaaaaacd81f2569a8fb8097a76dcec7341
Alex Bremard <alex.bremard@camunda.com>
2025-07-17 17:54:42 +0700

    test: Add unit tests for new exception-based validation approach
    Co-authored-by: abremard <48785808+abremard@users.noreply.github.com>

---
+++ service/src/test/java/io/camunda/service/validation/SecondaryStorageValidatorTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.service.validation;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.camunda.service.exception.SecondaryStorageUnavailableException;
+import org.junit.jupiter.api.Test;
+
+public class SecondaryStorageValidatorTest {
+
+  @Test
+  public void shouldThrowExceptionWhenDatabaseTypeIsNone() {
+    // Given
+    final SecondaryStorageValidator validator = new SecondaryStorageValidator("none");
+
+    // When/Then
+    assertThatThrownBy(() -> validator.validateSecondaryStorageEnabled())
+        .isInstanceOf(SecondaryStorageUnavailableException.class)
+        .hasMessageContaining("This endpoint requires secondary storage");
+  }
+
+  @Test
+  public void shouldNotThrowExceptionWhenDatabaseTypeIsElasticsearch() {
+    // Given
+    final SecondaryStorageValidator validator = new SecondaryStorageValidator("elasticsearch");
+
+    // When/Then - should not throw
+    validator.validateSecondaryStorageEnabled();
+  }
+
+  @Test
+  public void shouldNotThrowExceptionWhenDatabaseTypeIsOpensearch() {
+    // Given
+    final SecondaryStorageValidator validator = new SecondaryStorageValidator("opensearch");
+
+    // When/Then - should not throw
+    validator.validateSecondaryStorageEnabled();
+  }
+}
