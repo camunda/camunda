f8a661c6859a7a6c447ec16b73fd3430fd001efc
Christopher Kujawa <zelldon91@googlemail.com>
2025-07-18 15:09:39 +0200

    refactor: check for container path
     * Add extra variable for container path
     * Validate container path and adjust path if necessary
       For old images necessary

--- zeebe/benchmarks/docs/scripts/executeProfiling.sh
+++ zeebe/benchmarks/docs/scripts/executeProfiling.sh
@@ -10,6 +10,15 @@ if [ -z "$1" ]; then
 fi
 node=$1
 
+# Determine right container path
+containerPath=/usr/local/camunda/data
+if ! kubectl exec ck-profiling-tests-zeebe-0 -- ls -la "$containerPath";
+then
+  # Old container path
+  containerPath=/usr/local/zeebe/data
+fi
+
+
 # Download and extract latest async profiler
 if [ ! -d "async-profiler-4.0-linux-x64/" ];
 then
@@ -20,21 +29,21 @@ fi
 if ! kubectl exec "$node" -- test -f data/libasyncProfiler.so;
 then
   # Copy async profiler to pod
-  kubectl cp async-profiler-4.0-linux-x64/bin/asprof "$node":/usr/local/camunda/data/asprof
-  kubectl cp async-profiler-4.0-linux-x64/lib/libasyncProfiler.so "$node":/usr/local/camunda/data/libasyncProfiler.so
-  kubectl exec "$node" -- chmod +x /usr/local/camunda/data/asprof
+  kubectl cp async-profiler-4.0-linux-x64/bin/asprof "$node":"$containerPath/asprof"
+  kubectl cp async-profiler-4.0-linux-x64/lib/libasyncProfiler.so "$node":"$containerPath/libasyncProfiler.so"
+  kubectl exec "$node" -- chmod +x "$containerPath/asprof"
 fi
 
 # Run profiling
 filename=flamegraph-$(date +%Y-%m-%d_%H-%M-%S).html
 PID=$(kubectl exec "$node" -- jps | grep Standalone | cut -d " " -f 1)
-kubectl exec "$node" -- ./data/asprof -e itimer -d 100 -t -f "/usr/local/camunda/data/$filename" --libpath /usr/local/camunda/data/libasyncProfiler.so "$PID"
+kubectl exec "$node" -- ./data/asprof -e itimer -d 100 -t -f "$containerPath/$filename" --libpath "$containerPath/libasyncProfiler.so" "$PID"
 
 # Copy result
-kubectl cp "$node:/usr/local/camunda/data/$filename" "$node-$filename"
+kubectl cp "$node:$containerPath/$filename" "$node-$filename"
 
 # Clean up
 # Comment out the following lines to make exeuction faster next time
-kubectl exec "$node" -- rm ./data/asprof data/libasyncProfiler.so "data/$filename"
+kubectl exec "$node" -- rm "$containerPath/asprof" "$containerPath/libasyncProfiler.so" "$containerPath/$filename"
 rm profiler.tar.gz
 rm -r async-profiler-4.0-linux-x64/
