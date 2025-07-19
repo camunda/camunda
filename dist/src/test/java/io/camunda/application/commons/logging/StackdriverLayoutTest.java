4216a27c25a2ad94efe48a41a3df41bfe30852eb
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-07-19 18:44:01 +0200

    feat: add new StackdriverLayout JSON schema
    Adds a new StackdriverLayout.json template which can be used by Log4j2's
    built in JsonTemplateLayout. This will allow us to completely remove our
    old Stackdriver layout, and make better use of garbage-free logging.
    
    The commit also adds including the Spring application version via
    resource filtering, since this can then be used to have as the default
    service version.
    
    This should be split into two commits, but as it's quite small, the
    overhead of doing so is likely more than it costs the reviewer.

---
+++ dist/src/test/java/io/camunda/application/commons/logging/StackdriverLayoutTest.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.application.commons.logging;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.pattern.PlainTextRenderer;
+import org.apache.logging.log4j.core.pattern.TextRenderer;
+import org.apache.logging.log4j.core.test.appender.EncodingListAppender;
+import org.apache.logging.log4j.core.test.appender.ListAppender;
+import org.apache.logging.log4j.layout.template.json.JsonTemplateLayout;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+@SuppressWarnings("LoggingSimilarMessage")
+final class StackdriverLayoutTest {
+
+  private static final StackTraceElement DUMMY_LOCATION =
+      new StackTraceElement("Foo", "Bar", "Foo.java", 1);
+  private static final String LOG_MESSAGE = "Hello World!";
+
+  private Logger logger;
+  private ListAppender appender;
+  private ListAppender encodingAppender;
+
+  @BeforeEach
+  void beforeEach() throws IOException {
+    logger = (Logger) LogManager.getLogger();
+
+    final var configuration = logger.getContext().getConfiguration();
+    final var layout =
+        JsonTemplateLayout.newBuilder()
+            .setConfiguration(configuration)
+            .setEventTemplateUri("classpath:logging/StackdriverLayout.json")
+            .setLocationInfoEnabled(true)
+            .setStackTraceEnabled(true)
+            .build();
+
+    appender = new ListAppender("event", null, null, true, false);
+    appender.start();
+    logger.addAppender(appender);
+
+    encodingAppender = new EncodingListAppender("json", null, layout, true, false);
+    encodingAppender.start();
+    logger.addAppender(encodingAppender);
+
+    logger.setLevel(Level.TRACE);
+  }
+
+  @AfterEach
+  void afterEach() {
+    if (encodingAppender != null) {
+      encodingAppender.stop();
+      if (logger != null) {
+        logger.removeAppender(encodingAppender);
+      }
+    }
+
+    if (appender != null) {
+      appender.stop();
+      if (logger != null) {
+        logger.removeAppender(appender);
+      }
+    }
+  }
+
+  @Test
+  void shouldLogReportLocationOnErrorWithoutException() {
+    // given
+
+    // when
+    logger.atError().withLocation(DUMMY_LOCATION).log(LOG_MESSAGE);
+
+    // then
+    assertMessageMatchesWithReportLocation();
+  }
+
+  @Test
+  void shouldLogException() {
+    // given
+    final var exception = new RuntimeException("Test Exception");
+
+    // when
+    logger.atError().withLocation(DUMMY_LOCATION).withThrowable(exception).log(LOG_MESSAGE);
+
+    // then
+    assertMessageMatchesWithException(exception);
+  }
+
+  @ParameterizedTest
+  @MethodSource("severityProvider")
+  void shouldMapSeverity(final String expectedSeverity, final Level actualLevel) {
+    // given
+
+    // when
+    logger.atLevel(actualLevel).withLocation(DUMMY_LOCATION).log(LOG_MESSAGE);
+
+    // then
+    assertMessageMatches(expectedSeverity);
+  }
+
+  @SuppressWarnings("unused")
+  @Test
+  void shouldLogContextMap() {
+    // given
+
+    // when
+    try (final var bar = MDC.putCloseable("foo", "bar");
+        final var baz = MDC.putCloseable("baz", "qux")) {
+      logger.atDebug().withLocation(DUMMY_LOCATION).log(LOG_MESSAGE);
+    }
+
+    // then - note that Log4j2 will print out the context map with keys alphabetically sorted
+    assertMessageMatchesWithContext(
+        "DEBUG",
+        """
+        "contextMap":{"baz":"qux","foo":"bar"}""");
+  }
+
+  @Test
+  void shouldLog() {
+    // given
+
+    // when
+    logger.atDebug().withLocation(DUMMY_LOCATION).log(LOG_MESSAGE);
+
+    // then
+    assertMessageMatches("DEBUG");
+  }
+
+  private static Stream<Arguments> severityProvider() {
+    return Stream.of(
+        Arguments.of("DEBUG", Level.TRACE),
+        Arguments.of("DEBUG", Level.DEBUG),
+        Arguments.of("INFO", Level.INFO),
+        Arguments.of("WARNING", Level.WARN));
+  }
+
+  private void assertMessageMatches(final String expectedSeverity) {
+    assertMessageMatchesWithContext(expectedSeverity, null);
+  }
+
+  private void assertMessageMatchesWithContext(
+      final String expectedSeverity, final String expectedContext) {
+    final var message = appender.getEvents().getFirst();
+    final var jsonMessage = encodingAppender.getMessages().getFirst();
+
+    assertThat(jsonMessage)
+        .isEqualTo(
+            """
+        {"timestampSeconds":%d,"timestampNanos":%d,"severity":"%s","message":"Hello World!",\
+        "logging.googleapis.com/sourceLocation":{"file":"Foo.java","line":1,\
+        "function":"Foo.Bar"},%s\
+        "staticContext":{"threadId":1,"threadName":"main","threadPriority":5,\
+        "loggerName":"io.camunda.application.commons.logging.StackdriverLayoutTest"},\
+        "serviceContext":{"service":"","version":""}}""",
+            message.getInstant().getEpochSecond(),
+            message.getInstant().getNanoOfSecond(),
+            expectedSeverity,
+            expectedContext == null ? "" : expectedContext + ",");
+  }
+
+  private void assertMessageMatchesWithReportLocation() {
+    final var message = appender.getEvents().getFirst();
+    final var jsonMessage = encodingAppender.getMessages().getFirst();
+
+    assertThat(jsonMessage)
+        .isEqualTo(
+            """
+        {"timestampSeconds":%d,"timestampNanos":%d,"severity":"ERROR","message":"Hello World!",\
+        "logging.googleapis.com/sourceLocation":{"file":"Foo.java","line":1,\
+        "function":"Foo.Bar"},\
+        "staticContext":{"threadId":1,"threadName":"main","threadPriority":5,\
+        "loggerName":"io.camunda.application.commons.logging.StackdriverLayoutTest"},\
+        "serviceContext":{"service":"","version":""},\
+        "@type":"type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent",\
+        "reportLocation":{"filePath":"Foo.java","functionName":"Bar","lineNumber":1}}""",
+            message.getInstant().getEpochSecond(), message.getInstant().getNanoOfSecond());
+  }
+
+  private void assertMessageMatchesWithException(final RuntimeException exception) {
+    final var message = appender.getEvents().getFirst();
+    final var jsonMessage = encodingAppender.getMessages().getFirst();
+
+    // it's hard to generate the actual stack trace in the same format without breaking
+    // abstractions, so we just check that the exception message is present in the JSON
+    assertThat(jsonMessage)
+        .matches(
+            Pattern.quote(
+                    """
+        {"timestampSeconds":%d,"timestampNanos":%d,"severity":"ERROR","message":"Hello World!",\
+        "logging.googleapis.com/sourceLocation":{"file":"Foo.java","line":1,\
+        "function":"Foo.Bar"},\
+        "staticContext":{"threadId":1,"threadName":"main","threadPriority":5,\
+        "loggerName":"io.camunda.application.commons.logging.StackdriverLayoutTest"},\
+        "serviceContext":{"service":"","version":""},\
+        "@type":"type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent",\
+        "exception":"""
+                        .formatted(
+                            message.getInstant().getEpochSecond(),
+                            message.getInstant().getNanoOfSecond()))
+                + ".+\"}")
+        .contains(exception.getMessage());
+  }
+}
