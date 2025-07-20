77d8e6f45ebc6eba18b56812c0bf097084a85ae4
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-07-20 21:19:37 +0200

    fix: do not configure grpc but our messaging service instead
    (cherry picked from commit e3d2d4b126722e1b03fce872ad57e490a7b6233d)

--- dist/src/main/java/io/camunda/application/commons/configuration/GatewayBasedConfiguration.java
+++ dist/src/main/java/io/camunda/application/commons/configuration/GatewayBasedConfiguration.java
@@ -157,12 +157,15 @@ public final class GatewayBasedConfiguration {
 
   private MessagingConfig messagingConfig(final GatewayCfg config) {
     final var cluster = config.getCluster();
+    final var network = config.getNetwork();
 
     final var messaging =
         new MessagingConfig()
             .setCompressionAlgorithm(cluster.getMessageCompression())
             .setInterfaces(Collections.singletonList(cluster.getHost()))
-            .setPort(cluster.getPort());
+            .setPort(cluster.getPort())
+            .setSocketSendBuffer((int) network.getSocketSendBuffer().toBytes())
+            .setSocketReceiveBuffer((int) network.getSocketReceiveBuffer().toBytes());
 
     final var security = cluster.getSecurity();
     if (security.isEnabled()) {
