cc16a3e55b84bbbf53f7a70c17897d7209e83dd4
Lilian Cavalet <liliancavalet@users.noreply.github.com>
2025-07-21 15:23:00 -0300

    ci: enforce flakyness

---
+++ clients/java/src/test/java/io/camunda/zeebe/client/FlakySearchTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.client;
+
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FlakySearchTest {
+
+    @Test
+    public void testRandomFailure() {
+        // This test will fail randomly about 30% of the time
+        Random random = new Random();
+        boolean shouldPass = random.nextDouble() > 0.3;
+
+        // Add some delay to make it more realistic
+        try {
+            Thread.sleep(100 + random.nextInt(200));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        assertTrue(shouldPass, "This test failed randomly to simulate flakiness");
+    }
+
+    @Test
+    public void testTimingDependent() {
+        // This test depends on system timing and might be flaky
+        long startTime = System.currentTimeMillis();
+
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        // This assertion might fail depending on system load
+        assertTrue(duration < 100, "Test took too long: " + duration + "ms");
+    }
+}
