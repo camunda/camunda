07a4d3997654dc0911acd100ebf008a7e183d21f
Christian Thiel <christian.thiel@holisticon.de>
2025-07-22 09:58:41 +0200

    test: refactored the batch operation scheduler to use the new item provider #35435

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationSetupProcessors.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/batchoperation/BatchOperationSetupProcessors.java
@@ -14,6 +14,7 @@ import io.camunda.zeebe.engine.processing.batchoperation.handlers.CancelProcessI
 import io.camunda.zeebe.engine.processing.batchoperation.handlers.MigrateProcessInstanceBatchOperationExecutor;
 import io.camunda.zeebe.engine.processing.batchoperation.handlers.ModifyProcessInstanceBatchOperationExecutor;
 import io.camunda.zeebe.engine.processing.batchoperation.handlers.ResolveIncidentBatchOperationExecutor;
+import io.camunda.zeebe.engine.processing.batchoperation.itemprovider.ItemProviderFactory;
 import io.camunda.zeebe.engine.processing.distribution.CommandDistributionBehavior;
 import io.camunda.zeebe.engine.processing.identity.AuthorizationCheckBehavior;
 import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessors;
@@ -144,8 +145,7 @@ public final class BatchOperationSetupProcessors {
         .withListener(
             new BatchOperationExecutionScheduler(
                 scheduledTaskStateFactory,
-                new BatchOperationItemProvider(
-                    searchClientsProxy, engineConfiguration, batchOperationMetrics),
+                new ItemProviderFactory(searchClientsProxy, batchOperationMetrics, partitionId),
                 engineConfiguration,
                 partitionId,
                 batchOperationMetrics));
