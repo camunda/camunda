220c3b7dda979cd14c9359ecb5f9a039d976e14e
vitorwtavares <vitor.tavares@camunda.com>
2025-07-23 11:03:09 -0300

    fix: bump camunda api types library and update usage of user task state type

--- tasklist/client/src/v2/TaskDetailsLayout/AssignButton/index.tsx
+++ tasklist/client/src/v2/TaskDetailsLayout/AssignButton/index.tsx
@@ -9,7 +9,7 @@
 import {t as _t} from 'i18next';
 import {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
-import type {UserTaskState} from '@vzeta/camunda-api-zod-schemas/8.8';
+import type {UserTask} from '@vzeta/camunda-api-zod-schemas/8.8';
 import {AsyncActionButton} from 'common/components/AsyncActionButton';
 import {notificationsStore} from 'common/notifications/notifications.store';
 import {tracking} from 'common/tracking';
@@ -37,7 +37,7 @@ const getAssignmentToggleLabels = () =>
 type Props = {
   id: string;
   assignee: string | undefined;
-  taskState: UserTaskState;
+  taskState: UserTask['state'];
   currentUser: string;
 };
 
