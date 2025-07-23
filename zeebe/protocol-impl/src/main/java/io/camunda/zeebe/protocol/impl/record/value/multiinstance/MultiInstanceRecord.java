89bc64ed9dcae81f2c7874fe1ef0bc9b25bbc9ba
remco.westerhoud <remcowesterhoud@msn.com>
2025-07-23 14:28:47 +0200

    feat: add MultiInstance record and intent
    This new record will be used to write an event and store the evaluated input expression result in state. This will later
     be used by the processor instead of reevaluating the expression.

---
+++ zeebe/protocol-impl/src/main/java/io/camunda/zeebe/protocol/impl/record/value/multiinstance/MultiInstanceRecord.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.protocol.impl.record.value.multiinstance;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.camunda.zeebe.msgpack.property.ArrayProperty;
+import io.camunda.zeebe.msgpack.value.BinaryValue;
+import io.camunda.zeebe.msgpack.value.StringValue;
+import io.camunda.zeebe.protocol.impl.encoding.MsgPackConverter;
+import io.camunda.zeebe.protocol.impl.record.UnifiedRecordValue;
+import io.camunda.zeebe.protocol.record.value.MultiInstanceRecordValue;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.agrona.DirectBuffer;
+
+public class MultiInstanceRecord extends UnifiedRecordValue implements MultiInstanceRecordValue {
+  private static final StringValue INPUT_COLLECTION = new StringValue("inputCollection");
+  private final ArrayProperty<BinaryValue> inputCollectionProperty =
+      new ArrayProperty<>(INPUT_COLLECTION, BinaryValue::new);
+
+  public MultiInstanceRecord() {
+    super(1);
+    declareProperty(inputCollectionProperty);
+  }
+
+  @JsonIgnore
+  public List<DirectBuffer> getInputCollectionBuffers() {
+    return inputCollectionProperty.stream().map(BinaryValue::getValue).collect(Collectors.toList());
+  }
+
+  @Override
+  public List<String> getInputCollection() {
+    return getInputCollectionBuffers().stream().map(MsgPackConverter::convertToJson).toList();
+  }
+
+  public MultiInstanceRecord setInputCollection(final List<DirectBuffer> inputCollection) {
+    inputCollection.forEach(
+        inputElement -> {
+          inputCollectionProperty.add().wrap(inputElement, 0, inputElement.capacity());
+        });
+    return this;
+  }
+}
--- zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/record/ValueTypeMapping.java
+++ zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/record/ValueTypeMapping.java
@@ -44,6 +44,7 @@ import io.camunda.zeebe.protocol.record.intent.MessageCorrelationIntent;
 import io.camunda.zeebe.protocol.record.intent.MessageIntent;
 import io.camunda.zeebe.protocol.record.intent.MessageStartEventSubscriptionIntent;
 import io.camunda.zeebe.protocol.record.intent.MessageSubscriptionIntent;
+import io.camunda.zeebe.protocol.record.intent.MultiInstanceIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessEventIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceBatchIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceCreationIntent;
@@ -94,6 +95,7 @@ import io.camunda.zeebe.protocol.record.value.MessageCorrelationRecordValue;
 import io.camunda.zeebe.protocol.record.value.MessageRecordValue;
 import io.camunda.zeebe.protocol.record.value.MessageStartEventSubscriptionRecordValue;
 import io.camunda.zeebe.protocol.record.value.MessageSubscriptionRecordValue;
+import io.camunda.zeebe.protocol.record.value.MultiInstanceRecordValue;
 import io.camunda.zeebe.protocol.record.value.ProcessEventRecordValue;
 import io.camunda.zeebe.protocol.record.value.ProcessInstanceBatchRecordValue;
 import io.camunda.zeebe.protocol.record.value.ProcessInstanceCreationRecordValue;
@@ -307,6 +309,9 @@ public final class ValueTypeMapping {
     mapping.put(
         ValueType.USAGE_METRIC,
         new Mapping<>(UsageMetricRecordValue.class, UsageMetricIntent.class));
+    mapping.put(
+        ValueType.MULTI_INSTANCE,
+        new Mapping<>(MultiInstanceRecordValue.class, MultiInstanceIntent.class));
     return mapping;
   }
 
