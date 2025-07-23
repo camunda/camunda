30048402c993841ce2e1c242edb4a639e4b4f481
remco.westerhoud <remcowesterhoud@msn.com>
2025-07-23 14:51:34 +0200

    feat: define input collection behavior
    This behavior will be responsible for managing the input collection. It'll evaluate the expression and ensure it gets
    stored in state. It's also responsible for retrieving the collection from the state when necessary.
    
    A future expansion will be removing the input collection from state when the MI body completes.
    
    (cherry picked from commit b4eb19aea1111ceb990507a15886eda51e65987e)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnBehaviorsImpl.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/BpmnBehaviorsImpl.java
@@ -41,6 +41,7 @@ public final class BpmnBehaviorsImpl implements BpmnBehaviors {
   private final BpmnProcessResultSenderBehavior processResultSenderBehavior;
   private final BpmnBufferedMessageStartEventBehavior bufferedMessageStartEventBehavior;
   private final BpmnJobBehavior jobBehavior;
+  private final MultiInstanceInputCollectionBehavior multiInstanceInputCollectionBehavior;
   private final MultiInstanceOutputCollectionBehavior multiInstanceOutputCollectionBehavior;
   private final CatchEventBehavior catchEventBehavior;
   private final EventTriggerBehavior eventTriggerBehavior;
@@ -144,6 +145,8 @@ public final class BpmnBehaviorsImpl implements BpmnBehaviors {
             jobMetrics,
             clock);
 
+    multiInstanceInputCollectionBehavior =
+        new MultiInstanceInputCollectionBehavior(expressionBehavior, stateBehavior);
     multiInstanceOutputCollectionBehavior =
         new MultiInstanceOutputCollectionBehavior(stateBehavior, expressionBehavior());
 
@@ -249,6 +252,11 @@ public final class BpmnBehaviorsImpl implements BpmnBehaviors {
   @Override
   public BpmnSignalBehavior signalBehavior() {
     return signalBehavior;
+  }
+
+  @Override
+  public MultiInstanceInputCollectionBehavior inputCollectionBehavior() {
+    return multiInstanceInputCollectionBehavior;
   }
 
   @Override
