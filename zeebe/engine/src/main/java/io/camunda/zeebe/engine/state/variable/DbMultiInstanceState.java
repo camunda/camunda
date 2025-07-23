4b25672fcd4d607f1a3d7a9a22d9c0705df1f0da
remco.westerhoud <remcowesterhoud@msn.com>
2025-07-23 15:25:20 +0200

    feat: add CF and implement multi-instance state class
    Adds a CF to store the input collection. Also introduces 3 methods for the basic create, read, delete operations.
    
    (cherry picked from commit 7533ae4ddb617537ccf3bd846658393a6cc9cbfa)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/variable/DbMultiInstanceState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/variable/DbMultiInstanceState.java
@@ -7,21 +7,58 @@
  */
 package io.camunda.zeebe.engine.state.variable;
 
+import io.camunda.zeebe.db.ColumnFamily;
+import io.camunda.zeebe.db.TransactionContext;
+import io.camunda.zeebe.db.ZeebeDb;
+import io.camunda.zeebe.db.impl.DbLong;
+import io.camunda.zeebe.engine.state.multiinstance.PersistedInputCollection;
 import io.camunda.zeebe.engine.state.mutable.MutableMultiInstanceState;
+import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import java.util.List;
+import java.util.Optional;
 import org.agrona.DirectBuffer;
 
 public class DbMultiInstanceState implements MutableMultiInstanceState {
 
+  private final DbLong multiInstanceKey;
+  private final PersistedInputCollection inputCollection;
+  private final ColumnFamily<DbLong, PersistedInputCollection> inputCollectionColumnFamily;
+
+  public DbMultiInstanceState(
+      final ZeebeDb<ZbColumnFamilies> zeebeDb, final TransactionContext transactionContext) {
+    multiInstanceKey = new DbLong();
+    inputCollection = new PersistedInputCollection();
+    inputCollectionColumnFamily =
+        zeebeDb.createColumnFamily(
+            ZbColumnFamilies.MULTI_INSTANCE_INPUT_COLLECTION,
+            transactionContext,
+            multiInstanceKey,
+            inputCollection);
+  }
+
   @Override
   public void insertInputCollection(
-      final long multiInstanceKey, final List<DirectBuffer> inputCollection) {}
+      final long multiInstanceKey, final List<DirectBuffer> inputCollection) {
+    this.multiInstanceKey.wrapLong(multiInstanceKey);
+    this.inputCollection.setInputCollection(inputCollection);
+    inputCollectionColumnFamily.insert(this.multiInstanceKey, this.inputCollection);
+  }
 
   @Override
-  public void deleteInputCollection(final long multiInstanceKey) {}
+  public void deleteInputCollection(final long multiInstanceKey) {
+    this.multiInstanceKey.wrapLong(multiInstanceKey);
+    inputCollectionColumnFamily.deleteIfExists(this.multiInstanceKey);
+  }
 
   @Override
-  public List<DirectBuffer> getInputCollection(final long multiInstanceKey) {
-    return List.of();
+  public Optional<List<DirectBuffer>> getInputCollection(final long multiInstanceKey) {
+    this.multiInstanceKey.wrapLong(multiInstanceKey);
+    final var persistedInputCollection = inputCollectionColumnFamily.get(this.multiInstanceKey);
+
+    if (persistedInputCollection == null) {
+      return Optional.empty();
+    }
+
+    return Optional.of(persistedInputCollection.getInputCollection());
   }
 }
