095e76f48e14b3fa897d73092d26392fe7d4db75
ana.vinogradova <ana.vinogradova@camunda.com>
2025-07-23 10:48:37 +0100

    test: add AUTH tests

--- qa/acceptance-tests/src/test/java/io/camunda/it/auth/TenantAuthorizationIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/auth/TenantAuthorizationIT.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.client.CamundaClient;
 import io.camunda.client.api.command.ProblemException;
+import io.camunda.client.api.search.response.Client;
 import io.camunda.client.api.search.response.SearchResponse;
 import io.camunda.client.api.search.response.Tenant;
 import io.camunda.client.api.search.response.TenantUser;
@@ -30,12 +31,22 @@ import io.camunda.qa.util.multidb.MultiDbTest;
 import io.camunda.qa.util.multidb.MultiDbTestApplication;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
 import io.camunda.zeebe.test.util.Strings;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 import org.assertj.core.api.Assertions;
 import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
@@ -57,6 +68,8 @@ class TenantAuthorizationIT {
   private static final String RESTRICTED = "restrictedUser";
   private static final String UNAUTHORIZED = "unauthorizedUser";
   private static final Duration AWAIT_TIMEOUT = Duration.ofSeconds(15);
+  private static final String TENANT_ID_1 = "tenant1";
+  private static final String TENANT_ID_2 = "tenant2";
 
   @UserDefinition
   private static final TestUser ADMIN_USER =
@@ -74,15 +87,17 @@ class TenantAuthorizationIT {
       new TestUser(
           RESTRICTED,
           PASSWORD,
-          List.of(new Permissions(TENANT, READ, List.of("tenant1", "tenant2"))));
+          List.of(new Permissions(TENANT, READ, List.of(TENANT_ID_1, TENANT_ID_2))));
 
   @UserDefinition
   private static final TestUser UNAUTHORIZED_USER = new TestUser(UNAUTHORIZED, PASSWORD, List.of());
 
+  @AutoClose private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+
   @BeforeAll
   static void setUp(@Authenticated(ADMIN) final CamundaClient adminClient) {
-    createTenant(adminClient, "tenant1");
-    createTenant(adminClient, "tenant2");
+    createTenant(adminClient, TENANT_ID_1);
+    createTenant(adminClient, TENANT_ID_2);
 
     Awaitility.await("should create tenants and import in ES")
         .atMost(AWAIT_TIMEOUT)
@@ -92,7 +107,7 @@ class TenantAuthorizationIT {
               final var tenantsSearchResponse = adminClient.newTenantsSearchRequest().send().join();
               Assertions.assertThat(
                       tenantsSearchResponse.items().stream().map(Tenant::getTenantId).toList())
-                  .containsAll(Arrays.asList("tenant1", "tenant2"));
+                  .containsAll(Arrays.asList(TENANT_ID_1, TENANT_ID_2));
             });
   }
 
@@ -107,7 +122,7 @@ class TenantAuthorizationIT {
     assertThat(tenantSearchResponse.items())
         .hasSize(2)
         .map(Tenant::getTenantId)
-        .containsExactlyInAnyOrder("tenant1", "tenant2");
+        .containsExactlyInAnyOrder(TENANT_ID_1, TENANT_ID_2);
   }
 
   @Test
@@ -125,17 +140,17 @@ class TenantAuthorizationIT {
   void getByIdShouldReturnAuthorizedTenant(
       @Authenticated(RESTRICTED) final CamundaClient userClient) {
     // when
-    final Tenant tenant = userClient.newTenantGetRequest("tenant1").send().join();
+    final Tenant tenant = userClient.newTenantGetRequest(TENANT_ID_1).send().join();
 
     // then
-    assertThat(tenant.getTenantId()).isEqualTo("tenant1");
+    assertThat(tenant.getTenantId()).isEqualTo(TENANT_ID_1);
   }
 
   @Test
   void getByIdShouldReturnForbiddenForUnauthorizedTenantId(
       @Authenticated(UNAUTHORIZED) final CamundaClient userClient) {
     // when/then
-    assertThatThrownBy(() -> userClient.newTenantGetRequest("tenant1").send().join())
+    assertThatThrownBy(() -> userClient.newTenantGetRequest(TENANT_ID_1).send().join())
         .isInstanceOf(ProblemException.class)
         .hasMessageContaining("Unauthorized to perform operation 'READ' on resource 'TENANT'");
   }
@@ -144,7 +159,7 @@ class TenantAuthorizationIT {
   void searchUsersByTenantShouldReturnEmptyListIfUnauthorized(
       @Authenticated(UNAUTHORIZED) final CamundaClient camundaClient) {
     final SearchResponse<TenantUser> response =
-        camundaClient.newUsersByTenantSearchRequest("tenant1").send().join();
+        camundaClient.newUsersByTenantSearchRequest(TENANT_ID_1).send().join();
     Assertions.assertThat(response.items()).isEmpty();
   }
 
@@ -163,7 +178,12 @@ class TenantAuthorizationIT {
         .send()
         .join();
 
-    adminClient.newAssignUserToTenantCommand().username(userName).tenantId("tenant1").send().join();
+    adminClient
+        .newAssignUserToTenantCommand()
+        .username(userName)
+        .tenantId(TENANT_ID_1)
+        .send()
+        .join();
 
     // when/then
     Awaitility.await("Search returns correct users")
@@ -171,10 +191,61 @@ class TenantAuthorizationIT {
         .untilAsserted(
             () -> {
               final SearchResponse<TenantUser> response =
-                  adminClient.newUsersByTenantSearchRequest("tenant1").send().join();
+                  adminClient.newUsersByTenantSearchRequest(TENANT_ID_1).send().join();
               Assertions.assertThat(response.items().stream().map(TenantUser::getUsername).toList())
                   .contains(userName);
             });
+  }
+
+  @Test
+  void shouldSearchClientsByTenantIfAuthorized(
+      @Authenticated(ADMIN) final CamundaClient adminClient)
+      throws URISyntaxException, IOException, InterruptedException {
+    // given
+    final String clientId = Strings.newRandomValidIdentityId();
+    assignClientToTenant(
+        adminClient.getConfiguration().getRestAddress().toString(), ADMIN, TENANT_ID_1, clientId);
+
+    // when/then
+    Awaitility.await("Search returns clients by tenant")
+        .ignoreExceptionsInstanceOf(ProblemException.class)
+        .untilAsserted(
+            () -> {
+              final SearchResponse<Client> response =
+                  adminClient.newClientsByTenantSearchRequest(TENANT_ID_1).send().join();
+              Assertions.assertThat(response.items().stream().map(Client::getClientId))
+                  .contains(clientId);
+            });
+  }
+
+  @Test
+  void searchClientsByTenantShouldReturnEmptyListIfUnauthorized(
+      @Authenticated(UNAUTHORIZED) final CamundaClient camundaClient) {
+    final SearchResponse<Client> response =
+        camundaClient.newClientsByTenantSearchRequest(TENANT_ID_1).send().join();
+    Assertions.assertThat(response.items()).isEmpty();
+  }
+
+  // TODO once available in #35767, this test should use the client to make the request
+  private static void assignClientToTenant(
+      final String restAddress, final String username, final String tenantId, final String clientId)
+      throws URISyntaxException, IOException, InterruptedException {
+    final var encodedCredentials =
+        Base64.getEncoder().encodeToString("%s:%s".formatted(username, PASSWORD).getBytes());
+    final HttpRequest request =
+        HttpRequest.newBuilder()
+            .uri(
+                new URI(
+                    "%s%s%s%s%s"
+                        .formatted(restAddress, "v2/tenants/", tenantId, "/clients/", clientId)))
+            .PUT(BodyPublishers.ofString(""))
+            .header("Authorization", "Basic %s".formatted(encodedCredentials))
+            .build();
+
+    // Send the request and get the response
+    final HttpResponse<String> response = HTTP_CLIENT.send(request, BodyHandlers.ofString());
+
+    assertThat(response.statusCode()).isEqualTo(204);
   }
 
   private static void createTenant(final CamundaClient adminClient, final String tenantId) {
