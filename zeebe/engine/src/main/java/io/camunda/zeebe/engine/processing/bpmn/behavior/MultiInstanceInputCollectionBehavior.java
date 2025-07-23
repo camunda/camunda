1414b16456078f66959c07eed48dec0aca328262
remco.westerhoud <remcowesterhoud@msn.com>
2025-07-23 16:06:16 +0200

    feat: implement the MI input collection behavior class
    Implements the writing of the input collection record and retrieving the input collection from the state.
    
    (cherry picked from commit 39b862e810b3e976c4c540bf5166a96fe08661e1)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/MultiInstanceInputCollectionBehavior.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/bpmn/behavior/MultiInstanceInputCollectionBehavior.java
@@ -11,6 +11,9 @@ import io.camunda.zeebe.engine.processing.bpmn.BpmnElementContext;
 import io.camunda.zeebe.engine.processing.common.ExpressionProcessor;
 import io.camunda.zeebe.engine.processing.common.Failure;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableMultiInstanceBody;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
+import io.camunda.zeebe.protocol.impl.record.value.multiinstance.MultiInstanceRecord;
+import io.camunda.zeebe.protocol.record.intent.MultiInstanceIntent;
 import io.camunda.zeebe.util.Either;
 import java.util.List;
 import org.agrona.DirectBuffer;
@@ -18,25 +21,43 @@ import org.agrona.DirectBuffer;
 public class MultiInstanceInputCollectionBehavior {
   private final ExpressionProcessor expressionProcessor;
   private final BpmnStateBehavior stateBehavior;
+  private final StateWriter stateWriter;
 
   public MultiInstanceInputCollectionBehavior(
-      final ExpressionProcessor expressionProcessor, final BpmnStateBehavior stateBehavior) {
+      final ExpressionProcessor expressionProcessor,
+      final BpmnStateBehavior stateBehavior,
+      final StateWriter stateWriter) {
     this.expressionProcessor = expressionProcessor;
     this.stateBehavior = stateBehavior;
+    this.stateWriter = stateWriter;
   }
 
   public Either<Failure, List<DirectBuffer>> initializeInputCollection(
       final ExecutableMultiInstanceBody element, final BpmnElementContext context) {
-    final var inputCollectionExpression = element.getLoopCharacteristics().getInputCollection();
-    // TODO write the event before returning the input collection
-    return expressionProcessor.evaluateArrayExpression(
-        inputCollectionExpression, context.getElementInstanceKey());
+    return evaluateInputCollection(element, context)
+        .map(
+            inputCollection -> {
+              final var record = new MultiInstanceRecord().setInputCollection(inputCollection);
+              stateWriter.appendFollowUpEvent(
+                  context.getElementInstanceKey(),
+                  MultiInstanceIntent.INPUT_COLLECTION_EVALUATED,
+                  record);
+              return inputCollection;
+            });
   }
 
-  public Either<Failure, List<DirectBuffer>> getInputCollection() {
-    // TODO read input collection from state
-    // TODO as a fallback, if the input collection cannot be found in the state we should still
-    //  evaluate the input collection so we remain backwards compatible.
-    return Either.right(List.of());
+  public Either<Failure, List<DirectBuffer>> getInputCollection(
+      final ExecutableMultiInstanceBody element, final BpmnElementContext context) {
+    return stateBehavior
+        .getInputCollection(context.getElementInstanceKey())
+        .map(Either::<Failure, List<DirectBuffer>>right)
+        .orElseGet(() -> evaluateInputCollection(element, context));
+  }
+
+  private Either<Failure, List<DirectBuffer>> evaluateInputCollection(
+      final ExecutableMultiInstanceBody element, final BpmnElementContext context) {
+    final var inputCollectionExpression = element.getLoopCharacteristics().getInputCollection();
+    return expressionProcessor.evaluateArrayExpression(
+        inputCollectionExpression, context.getElementInstanceKey());
   }
 }
