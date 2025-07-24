728eab8a0ea88061e4bb54329f3e38d0b215148e
Daniel Kelemen <daniel.kelemen@camunda.com>
2025-07-24 14:26:03 +0200

    feat: add ES/OS sum aggregation support

--- search/search-client-elasticsearch/src/main/java/io/camunda/search/es/transformers/ElasticsearchTransformers.java
+++ search/search-client-elasticsearch/src/main/java/io/camunda/search/es/transformers/ElasticsearchTransformers.java
@@ -13,6 +13,7 @@ import io.camunda.search.clients.aggregator.SearchCompositeAggregator;
 import io.camunda.search.clients.aggregator.SearchFilterAggregator;
 import io.camunda.search.clients.aggregator.SearchFiltersAggregator;
 import io.camunda.search.clients.aggregator.SearchParentAggregator;
+import io.camunda.search.clients.aggregator.SearchSumAggregator;
 import io.camunda.search.clients.aggregator.SearchTermsAggregator;
 import io.camunda.search.clients.aggregator.SearchTopHitsAggregator;
 import io.camunda.search.clients.core.SearchDeleteRequest;
@@ -48,6 +49,7 @@ import io.camunda.search.es.transformers.aggregator.SearchCompositeAggregatorTra
 import io.camunda.search.es.transformers.aggregator.SearchFilterAggregatorTransformer;
 import io.camunda.search.es.transformers.aggregator.SearchFiltersAggregatorTransformer;
 import io.camunda.search.es.transformers.aggregator.SearchParentAggregatorTransformer;
+import io.camunda.search.es.transformers.aggregator.SearchSumAggregatorTransformer;
 import io.camunda.search.es.transformers.aggregator.SearchTermsAggregatorTransformer;
 import io.camunda.search.es.transformers.aggregator.SearchTopHitsAggregatorTransformer;
 import io.camunda.search.es.transformers.query.BoolQueryTransformer;
@@ -147,6 +149,7 @@ public final class ElasticsearchTransformers {
     mappers.put(SearchCompositeAggregator.class, new SearchCompositeAggregatorTransformer(mappers));
     mappers.put(SearchChildrenAggregator.class, new SearchChildrenAggregatorTransformer(mappers));
     mappers.put(SearchParentAggregator.class, new SearchParentAggregatorTransformer(mappers));
+    mappers.put(SearchSumAggregator.class, new SearchSumAggregatorTransformer(mappers));
 
     // sort
     mappers.put(SearchSortOptions.class, new SortOptionsTransformer(mappers));
