179ef781f540bf6865bdeade671228024152931b
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-07-29 13:52:32 +0300

    refactor: rename method to clarify it waits for CREATED task state
    (cherry picked from commit f4a6aa12d81516fd49e1c10516bd2a3b7c34e770)

--- qa/acceptance-tests/src/test/java/io/camunda/it/migration/usertask/CompleteUserTaskMigrationIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/migration/usertask/CompleteUserTaskMigrationIT.java
@@ -59,7 +59,7 @@ public class CompleteUserTaskMigrationIT extends UserTaskMigrationHelper {
         startProcessInstance(
             migrator.getCamundaClient(),
             PROCESS_DEFINITION_KEYS.get(TaskImplementation.ZEEBE_USER_TASK));
-    final var taskKey = waitFor88TaskToBeImportedReturningId(migrator, piKey);
+    final var taskKey = waitFor88CreatedTaskToBeImportedReturningId(migrator, piKey);
 
     final var res =
         migrator.getTasklistClient().withAuthentication("demo", "demo").completeUserTask(taskKey);
@@ -75,7 +75,7 @@ public class CompleteUserTaskMigrationIT extends UserTaskMigrationHelper {
         startProcessInstance(
             migrator.getCamundaClient(),
             PROCESS_DEFINITION_KEYS.get(TaskImplementation.ZEEBE_USER_TASK));
-    final var taskKey = waitFor88TaskToBeImportedReturningId(migrator, piKey);
+    final var taskKey = waitFor88CreatedTaskToBeImportedReturningId(migrator, piKey);
 
     migrator.getCamundaClient().newUserTaskCompleteCommand(taskKey).send().join();
 
@@ -101,7 +101,7 @@ public class CompleteUserTaskMigrationIT extends UserTaskMigrationHelper {
         startProcessInstance(
             migrator.getCamundaClient(),
             PROCESS_DEFINITION_KEYS.get(TaskImplementation.JOB_WORKER));
-    final var taskKey = waitFor88TaskToBeImportedReturningId(migrator, piKey);
+    final var taskKey = waitFor88CreatedTaskToBeImportedReturningId(migrator, piKey);
 
     final var res =
         migrator.getTasklistClient().withAuthentication("demo", "demo").completeUserTask(taskKey);
