7fd032e93786cabf4d9259d77d2d01d0e34e77fd
Martin Stamm <martin.stamm@camunda.com>
2025-07-29 10:54:07 +0200

    feat: add search to DropdownSearch component

--- identity/client/src/pages/tenants/detail/members/AssignMembersModal.tsx
+++ identity/client/src/pages/tenants/detail/members/AssignMembersModal.tsx
@@ -6,7 +6,7 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import { FC, useEffect, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { Tag } from "@carbon/react";
 import { UseEntityModalCustomProps } from "src/components/modal";
 import { assignTenantMember } from "src/utility/api/membership";
@@ -33,21 +33,30 @@ const AssignMembersModal: FC<
   const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
   const [loadingAssignUser, setLoadingAssignUser] = useState(false);
 
+  const [search, setSearch] = useState<Record<string, unknown>>({});
+  const handleSearchChange = (search: string) => {
+    if (search === "") {
+      setSearch({});
+      return;
+    }
+    setSearch({ filter: { username: { $like: `*${search}*` } } });
+  };
+
   const {
     data: userSearchResults,
     loading,
     reload,
     error,
-  } = useApi(searchUser);
+  } = useApi(searchUser, search);
 
   const [callAssignUser] = useApiCall(assignTenantMember);
 
-  const unassignedUsers =
-    userSearchResults?.items.filter(
-      ({ username }) =>
-        !assignedUsers.some((user) => user.username === username) &&
-        !selectedUsers.some((user) => user.username === username),
-    ) || [];
+  const unassignedFilter = useCallback(
+    ({ username }: User) =>
+      !assignedUsers.some((user) => user.username === username) &&
+      !selectedUsers.some((user) => user.username === username),
+    [assignedUsers, selectedUsers],
+  );
 
   const onSelectUser = (user: User) => {
     setSelectedUsers([...selectedUsers, user]);
@@ -117,11 +126,13 @@ const AssignMembersModal: FC<
       )}
       <DropdownSearch
         autoFocus
-        items={unassignedUsers}
+        items={userSearchResults?.items || []}
         itemTitle={({ username }) => username}
         itemSubTitle={({ email }) => email}
         placeholder={t("searchByNameOrEmail")}
         onSelect={onSelectUser}
+        onChange={handleSearchChange}
+        filter={unassignedFilter}
       />
       {!loading && error && (
         <TranslatedErrorInlineNotification
