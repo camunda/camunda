d8f7b1480642d6c72266d808bfcf6727d16eb3ec
romansmirnov <roman.smirnov@camunda.com>
2025-07-29 17:52:23 +0200

    refactor: convert UsernamePasswordAuthentication accordingly

--- authentication/src/main/java/io/camunda/authentication/converter/UnprotectedCamundaAuthenticationConverter.java
+++ authentication/src/main/java/io/camunda/authentication/converter/UnprotectedCamundaAuthenticationConverter.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.authentication;
+package io.camunda.authentication.converter;
 
 import io.camunda.security.auth.CamundaAuthentication;
 import io.camunda.security.auth.CamundaAuthenticationConverter;
