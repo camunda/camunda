4c50902f00cebdadce01427287343717a9c03bae
Nico Korthout <nico.korthout@camunda.com>
2025-07-30 18:56:27 +0200

    test: ensure default admin role is created
    This tests the properties of the default admin role's authorizations.
    
    The test is setup using some uncommon usages related to the
    RecordingExporter that help in preserving the recorded records for
    individual test cases while only starting the engine once. This helps
    speed up the testcases. These are:
    - ENGINE resets the recording exporter ONLY_BEFORE_AND_AFTER_ALL_TESTS
      to avoid side effects to other tests
    - ENGINE additionally uses NO_RESET_AFTER_IDENTITY_SETUP
    
    The assertions themselves are structured using
    satisfiesExactlyInAnyOrder to ensure that adding a new authorization to
    the default role will fail the test, without the need for an explicit
    size assertion.
    
    The common properties between all authorizations can be tested in one go
    using allSatisfy.

---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/IdentitySetupInitializeDefaultsTest.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.processing.authorization;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.zeebe.engine.processing.identity.AuthorizationCheckBehavior;
+import io.camunda.zeebe.engine.util.EngineRule;
+import io.camunda.zeebe.engine.util.EngineRule.ResetRecordingExporterMode;
+import io.camunda.zeebe.engine.util.EngineRule.ResetRecordingExporterTestWatcherMode;
+import io.camunda.zeebe.protocol.record.Assertions;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.intent.IdentitySetupIntent;
+import io.camunda.zeebe.protocol.record.value.AuthorizationOwnerType;
+import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
+import io.camunda.zeebe.protocol.record.value.DefaultRole;
+import io.camunda.zeebe.protocol.record.value.PermissionType;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class IdentitySetupInitializeDefaultsTest {
+
+  @ClassRule
+  public static final EngineRule ENGINE =
+      EngineRule.singlePartition()
+          .withResetRecordingExporterTestWatcherMode(
+              ResetRecordingExporterTestWatcherMode.ONLY_BEFORE_AND_AFTER_ALL_TESTS)
+          .withIdentitySetup(ResetRecordingExporterMode.NO_RESET_AFTER_IDENTITY_SETUP);
+
+  @Test
+  public void shouldCreateAdminRoleByDefault() {
+    // then
+    assertThat(
+            RecordingExporter.records()
+                .limit(r -> r.getIntent() == IdentitySetupIntent.INITIALIZED)
+                .authorizationRecords()
+                .withOwnerId(DefaultRole.ADMIN.getId()))
+        .extracting(Record::getValue)
+        .describedAs("Expect all admin role authorizations to be owned by the admin role")
+        .allSatisfy(
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasOwnerId(DefaultRole.ADMIN.getId())
+                    .hasOwnerType(AuthorizationOwnerType.ROLE))
+        .describedAs("Expect all admin role authorizations to have the wildcard resource ID")
+        .allSatisfy(
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceId(AuthorizationCheckBehavior.WILDCARD_PERMISSION))
+        .describedAs("Expect the admin role authorizations to have specific resource permissions")
+        .satisfiesExactlyInAnyOrder(
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.AUTHORIZATION)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.READ,
+                        PermissionType.UPDATE,
+                        PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.MAPPING_RULE)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.READ,
+                        PermissionType.UPDATE,
+                        PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.MESSAGE)
+                    .hasOnlyPermissionTypes(PermissionType.CREATE, PermissionType.READ),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.APPLICATION)
+                    .hasOnlyPermissionTypes(PermissionType.ACCESS),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.SYSTEM)
+                    .hasOnlyPermissionTypes(PermissionType.READ, PermissionType.UPDATE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.TENANT)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.READ,
+                        PermissionType.UPDATE,
+                        PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.RESOURCE)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.READ,
+                        PermissionType.CREATE,
+                        PermissionType.DELETE_FORM,
+                        PermissionType.DELETE_PROCESS,
+                        PermissionType.DELETE_DRD,
+                        PermissionType.DELETE_RESOURCE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.READ_PROCESS_DEFINITION,
+                        PermissionType.READ_PROCESS_INSTANCE,
+                        PermissionType.READ_USER_TASK,
+                        PermissionType.UPDATE_PROCESS_INSTANCE,
+                        PermissionType.UPDATE_USER_TASK,
+                        PermissionType.CREATE_PROCESS_INSTANCE,
+                        PermissionType.DELETE_PROCESS_INSTANCE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.DECISION_REQUIREMENTS_DEFINITION)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.READ, PermissionType.UPDATE, PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.DECISION_DEFINITION)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.READ_DECISION_DEFINITION,
+                        PermissionType.READ_DECISION_INSTANCE,
+                        PermissionType.CREATE_DECISION_INSTANCE,
+                        PermissionType.DELETE_DECISION_INSTANCE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.GROUP)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.READ,
+                        PermissionType.UPDATE,
+                        PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.USER)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.READ,
+                        PermissionType.UPDATE,
+                        PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.ROLE)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.READ,
+                        PermissionType.UPDATE,
+                        PermissionType.DELETE),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.BATCH)
+                    .hasOnlyPermissionTypes(
+                        PermissionType.CREATE,
+                        PermissionType.CREATE_BATCH_OPERATION_CANCEL_PROCESS_INSTANCE,
+                        PermissionType.CREATE_BATCH_OPERATION_DELETE_PROCESS_INSTANCE,
+                        PermissionType.CREATE_BATCH_OPERATION_MIGRATE_PROCESS_INSTANCE,
+                        PermissionType.CREATE_BATCH_OPERATION_MODIFY_PROCESS_INSTANCE,
+                        PermissionType.CREATE_BATCH_OPERATION_RESOLVE_INCIDENT,
+                        PermissionType.CREATE_BATCH_OPERATION_DELETE_DECISION_INSTANCE,
+                        PermissionType.CREATE_BATCH_OPERATION_DELETE_DECISION_DEFINITION,
+                        PermissionType.CREATE_BATCH_OPERATION_DELETE_PROCESS_DEFINITION,
+                        PermissionType.UPDATE,
+                        PermissionType.READ),
+            auth ->
+                Assertions.assertThat(auth)
+                    .hasResourceType(AuthorizationResourceType.USAGE_METRIC)
+                    .hasOnlyPermissionTypes(PermissionType.READ));
+  }
+}
