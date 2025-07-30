fbdc866dab4e5bbb27e80635cad32ab63f6e9fb4
Nicola Puppa <nicola.puppa@camunda.com>
2025-07-30 10:22:39 +0200

    feat: add generic method to handle backpressure on the handlers
    The handlers should retry with a delay until succeed while facing backpressure

--- migration/identity-migration/src/main/java/io/camunda/migration/identity/handler/MigrationHandler.java
+++ migration/identity-migration/src/main/java/io/camunda/migration/identity/handler/MigrationHandler.java
@@ -7,16 +7,17 @@
  */
 package io.camunda.migration.identity.handler;
 
+import io.camunda.migration.api.MigrationException;
 import io.camunda.service.exception.ServiceException;
 import io.camunda.service.exception.ServiceException.Status;
 import java.util.List;
 import java.util.concurrent.CompletionException;
+import java.util.function.Supplier;
 import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class MigrationHandler<T> {
-  static final int SIZE = 100;
   protected final Logger logger = LoggerFactory.getLogger(getClass());
 
   protected boolean isConflictError(final Throwable e) {
@@ -28,6 +29,11 @@ public abstract class MigrationHandler<T> {
 
   protected boolean isNotImplementedError(final Throwable e) {
     return e instanceof final NotImplementedException exception;
+  }
+
+  protected static boolean isBackpressureError(final Throwable e) {
+    return (e instanceof final CompletionException ce && isBackpressureError(ce.getCause()))
+        || (e instanceof final ServiceException se && se.getStatus() == Status.RESOURCE_EXHAUSTED);
   }
 
   public void migrate() {
@@ -50,5 +56,27 @@ public abstract class MigrationHandler<T> {
 
   protected void logSummary() {
     logger.info("Completed {}", getName());
+  }
+
+  protected <T> void retryOnBackpressure(
+      final Supplier<T> operation, final String contextDescription) {
+
+    while (true) {
+      try {
+        operation.get();
+        return;
+      } catch (final Exception e) {
+        if (!isBackpressureError(e)) {
+          throw e;
+        }
+        logger.warn("Backpressure during {}. Retrying in 5 seconds...", contextDescription);
+        try {
+          Thread.sleep(5000);
+        } catch (final InterruptedException ie) {
+          Thread.currentThread().interrupt();
+          throw new MigrationException("Retry interrupted during backpressure handling.", ie);
+        }
+      }
+    }
   }
 }
