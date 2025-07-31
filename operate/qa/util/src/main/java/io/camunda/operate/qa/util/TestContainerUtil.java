49f45236477af4b398c81ca712eb450da22a60bd
Houssain Barouni <houssain.barouni@camunda.com>
2025-07-31 13:40:58 +0200

    test: fix legacy operate migration ci
    (cherry picked from commit b598d4fbb26b168db605a2430e9e2d9002780e64)

--- operate/qa/util/src/main/java/io/camunda/operate/qa/util/TestContainerUtil.java
+++ operate/qa/util/src/main/java/io/camunda/operate/qa/util/TestContainerUtil.java
@@ -15,6 +15,7 @@ import static org.testcontainers.images.PullPolicy.alwaysPull;
 import io.camunda.operate.exceptions.OperateRuntimeException;
 import io.camunda.operate.schema.migration.SemanticVersion;
 import io.camunda.operate.util.RetryOperation;
+import io.camunda.zeebe.util.SemanticVersion;
 import io.zeebe.containers.ZeebeContainer;
 import io.zeebe.containers.ZeebePort;
 import jakarta.annotation.PreDestroy;
@@ -27,6 +28,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -491,6 +493,20 @@ public class TestContainerUtil {
       broker =
           new ZeebeContainer(DockerImageName.parse(String.format("%s:%s", dockerRepo, version)));
       broker.withLogConsumer(new Slf4jLogConsumer(LOGGER));
+      final var exposedPorts =
+          new ArrayList<>(
+              List.of(
+                  ZeebePort.GATEWAY_GRPC.getPort(),
+                  ZeebePort.COMMAND.getPort(),
+                  ZeebePort.INTERNAL.getPort(),
+                  ZeebePort.MONITORING.getPort()));
+      // Expose REST port only for versions >= 8.5.0
+      if (SemanticVersion.parse(version)
+          .map(v -> v.compareTo(SemanticVersion.parse("8.5.0").get()) >= 0)
+          .orElse(true)) {
+        exposedPorts.add(ZeebePort.GATEWAY_REST.getPort());
+      }
+      broker.setExposedPorts(exposedPorts);
       if (testContext.getNetwork() != null) {
         broker.withNetwork(testContext.getNetwork());
       }
