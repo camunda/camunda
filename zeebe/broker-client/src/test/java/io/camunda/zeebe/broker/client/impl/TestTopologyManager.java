29e079be027d745417a0c6456d4422cde932229b
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2025-08-01 15:52:05 +0200

    test: update cluster configuration in broker client tests

--- zeebe/broker-client/src/test/java/io/camunda/zeebe/broker/client/impl/TestTopologyManager.java
+++ zeebe/broker-client/src/test/java/io/camunda/zeebe/broker/client/impl/TestTopologyManager.java
@@ -11,28 +11,32 @@ import io.camunda.zeebe.broker.client.api.BrokerClusterState;
 import io.camunda.zeebe.broker.client.api.BrokerTopologyListener;
 import io.camunda.zeebe.broker.client.api.BrokerTopologyManager;
 import io.camunda.zeebe.dynamic.config.state.ClusterConfiguration;
+import io.camunda.zeebe.protocol.record.PartitionHealthStatus;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 final class TestTopologyManager implements BrokerTopologyManager {
-  private final BrokerClusterStateImpl topology;
+  private final TestBrokerClusterState topology;
   private ClusterConfiguration clusterConfiguration = ClusterConfiguration.uninitialized();
 
   TestTopologyManager() {
-    this(new BrokerClusterStateImpl());
+    topology = new TestBrokerClusterState();
   }
 
-  TestTopologyManager(final BrokerClusterStateImpl topology) {
+  TestTopologyManager(final TestBrokerClusterState topology) {
     this.topology = topology;
   }
 
   TestTopologyManager addPartition(final int id, final int leaderId) {
     if (leaderId != BrokerClusterState.NODE_ID_NULL) {
       topology.addBrokerIfAbsent(leaderId);
-      topology.setPartitionLeader(id, leaderId, 1);
-      topology.setClusterSize(topology.getBrokers().size());
+      topology.setPartitionLeader(id, leaderId);
     }
 
     topology.addPartitionIfAbsent(id);
-    topology.setPartitionsCount(topology.getPartitions().size());
     return this;
   }
 
@@ -64,5 +68,99 @@ final class TestTopologyManager implements BrokerTopologyManager {
   @Override
   public void onClusterConfigurationUpdated(final ClusterConfiguration clusterConfiguration) {
     throw new UnsupportedOperationException();
+  }
+
+  private static class TestBrokerClusterState implements BrokerClusterState {
+
+    private final List<Integer> brokers = new ArrayList<>();
+    private final Map<Integer, Integer> partitionLeaders = new HashMap<>();
+    private final List<Integer> partitions = new ArrayList<>();
+
+    @Override
+    public boolean isInitialized() {
+      return true;
+    }
+
+    @Override
+    public int getClusterSize() {
+      return brokers.size();
+    }
+
+    @Override
+    public int getPartitionsCount() {
+      return partitions.size();
+    }
+
+    @Override
+    public int getReplicationFactor() {
+      return 0;
+    }
+
+    @Override
+    public int getLeaderForPartition(final int partition) {
+      return partitionLeaders.getOrDefault(partition, BrokerClusterState.NODE_ID_NULL);
+    }
+
+    @Override
+    public Set<Integer> getFollowersForPartition(final int partition) {
+      return Set.of();
+    }
+
+    @Override
+    public Set<Integer> getInactiveNodesForPartition(final int partition) {
+      return Set.of();
+    }
+
+    @Override
+    public int getRandomBroker() {
+      return 0;
+    }
+
+    @Override
+    public List<Integer> getPartitions() {
+      return partitions;
+    }
+
+    @Override
+    public List<Integer> getBrokers() {
+      return brokers;
+    }
+
+    @Override
+    public String getBrokerAddress(final int brokerId) {
+      return "";
+    }
+
+    @Override
+    public int getPartition(final int index) {
+      return partitions.get(index % partitions.size());
+    }
+
+    @Override
+    public String getBrokerVersion(final int brokerId) {
+      return "";
+    }
+
+    @Override
+    public PartitionHealthStatus getPartitionHealth(final int brokerId, final int partition) {
+      return null;
+    }
+
+    @Override
+    public long getLastCompletedChangeId() {
+      return 0;
+    }
+
+    public void addBrokerIfAbsent(final int leaderId) {
+      brokers.add(leaderId);
+    }
+
+    public void setPartitionLeader(final int id, final int leaderId) {
+      partitionLeaders.put(id, leaderId);
+    }
+
+    public void addPartitionIfAbsent(final int id) {
+      partitions.add(id);
+    }
   }
 }
