c422415b5c47054212ecf100f13c019b29f328ab
Mathias Geat <mathias.geat@camunda.com>
2025-08-01 08:26:54 +0200

    test: update ad-hoc activation controller tests to cover all validation cases
    With that, we can remove the tests for RequestMapper, so be in line with
    other implementations. Controller tests cover:
    
    - validation
    - request mapping
    - calling the service with the expected request

--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/RequestMapperTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/RequestMapperTest.java
@@ -9,11 +9,8 @@ package io.camunda.zeebe.gateway.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.camunda.service.AdHocSubProcessActivityServices.AdHocSubProcessActivateActivitiesRequest;
 import io.camunda.service.ProcessInstanceServices.ProcessInstanceMigrateBatchOperationRequest;
 import io.camunda.service.ProcessInstanceServices.ProcessInstanceModifyBatchOperationRequest;
-import io.camunda.zeebe.gateway.protocol.rest.AdHocSubProcessActivateActivitiesInstruction;
-import io.camunda.zeebe.gateway.protocol.rest.AdHocSubProcessActivateActivityReference;
 import io.camunda.zeebe.gateway.protocol.rest.MigrateProcessInstanceMappingInstruction;
 import io.camunda.zeebe.gateway.protocol.rest.ProcessInstanceFilter;
 import io.camunda.zeebe.gateway.protocol.rest.ProcessInstanceMigrationBatchOperationPlan;
@@ -22,11 +19,7 @@ import io.camunda.zeebe.gateway.protocol.rest.ProcessInstanceModificationBatchOp
 import io.camunda.zeebe.gateway.protocol.rest.ProcessInstanceModificationMoveBatchOperationInstruction;
 import io.camunda.zeebe.util.Either;
 import java.util.List;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.http.ProblemDetail;
 
 class RequestMapperTest {
@@ -138,137 +131,5 @@ class RequestMapperTest {
     final var problemDetail = result.getLeft();
     assertThat(problemDetail.getStatus()).isEqualTo(400);
     assertThat(problemDetail.getDetail()).isEqualTo("No targetElementId provided.");
-  }
-
-  @Test
-  void shouldMapToAdHocSubProcessActivateActivitiesRequest() {
-    // given
-    final var element1 = new AdHocSubProcessActivateActivityReference();
-    element1.setElementId("activity1");
-    element1.setVariables(Map.of("var1", "value1", "count", 42));
-
-    final var element2 = new AdHocSubProcessActivateActivityReference();
-    element2.setElementId("activity2");
-    element2.setVariables(Map.of("var2", "value2"));
-
-    final var instruction = new AdHocSubProcessActivateActivitiesInstruction();
-    instruction.setElements(List.of(element1, element2));
-    instruction.setCancelRemainingInstances(true);
-
-    // when
-    final Either<ProblemDetail, AdHocSubProcessActivateActivitiesRequest> result =
-        RequestMapper.toAdHocSubProcessActivateActivitiesRequest("123456", instruction);
-
-    // then
-    assertThat(result.isRight()).isTrue();
-
-    final var request = result.get();
-    assertThat(request.adHocSubProcessInstanceKey()).isEqualTo("123456");
-    assertThat(request.cancelRemainingInstances()).isTrue();
-    assertThat(request.elements())
-        .hasSize(2)
-        .satisfies(
-            elements -> {
-              assertThat(elements.get(0).elementId()).isEqualTo("activity1");
-              assertThat(elements.get(0).variables())
-                  .containsExactlyInAnyOrderEntriesOf(Map.of("var1", "value1", "count", 42));
-              assertThat(elements.get(1).elementId()).isEqualTo("activity2");
-              assertThat(elements.get(1).variables())
-                  .containsExactlyInAnyOrderEntriesOf(Map.of("var2", "value2"));
-            });
-  }
-
-  @ParameterizedTest
-  @CsvSource({"true,true", "false,false", ",false"})
-  void shouldMapToAdHocSubProcessActivateActivitiesRequestHandlingCancelRemainingInstances(
-      final Boolean cancelRemainingInstances, final boolean expectedValue) {
-    // given
-    final var element = new AdHocSubProcessActivateActivityReference();
-    element.setElementId("activity1");
-
-    final var instruction = new AdHocSubProcessActivateActivitiesInstruction();
-    instruction.setElements(List.of(element));
-
-    if (cancelRemainingInstances != null) {
-      instruction.setCancelRemainingInstances(cancelRemainingInstances);
-    }
-
-    // when
-    final Either<ProblemDetail, AdHocSubProcessActivateActivitiesRequest> result =
-        RequestMapper.toAdHocSubProcessActivateActivitiesRequest("123456", instruction);
-
-    // then
-    assertThat(result.isRight()).isTrue();
-    assertThat(result.get().cancelRemainingInstances()).isEqualTo(expectedValue);
-  }
-
-  @ParameterizedTest
-  @NullAndEmptySource
-  void shouldMapToAdHocSubProcessActivateActivitiesRequestWithEmptyVariables(
-      final Map<String, Object> variables) {
-    // given
-    final var element = new AdHocSubProcessActivateActivityReference();
-    element.setElementId("activity1");
-    element.setVariables(variables);
-
-    final var instruction = new AdHocSubProcessActivateActivitiesInstruction();
-    instruction.setElements(List.of(element));
-    instruction.setCancelRemainingInstances(true);
-
-    // when
-    final Either<ProblemDetail, AdHocSubProcessActivateActivitiesRequest> result =
-        RequestMapper.toAdHocSubProcessActivateActivitiesRequest("123456", instruction);
-
-    // then
-    assertThat(result.isRight()).isTrue();
-    assertThat(result.get().elements())
-        .hasSize(1)
-        .first()
-        .satisfies(
-            e -> {
-              assertThat(e.elementId()).isEqualTo("activity1");
-              assertThat(e.variables()).isEmpty();
-            });
-  }
-
-  @Test
-  void shouldReturnProblemDetailForAdHocSubProcessActivationRequestWithMissingElementId() {
-    // given
-    final var element = new AdHocSubProcessActivateActivityReference();
-    element.setElementId(null); // Invalid - null element ID
-
-    final var instruction = new AdHocSubProcessActivateActivitiesInstruction();
-    instruction.setElements(List.of(element));
-    instruction.setCancelRemainingInstances(true);
-
-    // when
-    final Either<ProblemDetail, AdHocSubProcessActivateActivitiesRequest> result =
-        RequestMapper.toAdHocSubProcessActivateActivitiesRequest("123456", instruction);
-
-    // then
-    assertThat(result.isLeft()).isTrue();
-    final var problemDetail = result.getLeft();
-    assertThat(problemDetail.getStatus()).isEqualTo(400);
-    assertThat(problemDetail.getTitle()).isEqualTo("INVALID_ARGUMENT");
-    assertThat(problemDetail.getDetail()).isEqualTo("No elements[0].elementId provided.");
-  }
-
-  @Test
-  void shouldReturnProblemDetailForAdHocSubProcessActivationRequestWithEmptyElementsToActivate() {
-    // given
-    final var instruction = new AdHocSubProcessActivateActivitiesInstruction();
-    instruction.setElements(List.of());
-    instruction.setCancelRemainingInstances(true);
-
-    // when
-    final Either<ProblemDetail, AdHocSubProcessActivateActivitiesRequest> result =
-        RequestMapper.toAdHocSubProcessActivateActivitiesRequest("123456", instruction);
-
-    // then
-    assertThat(result.isLeft()).isTrue();
-    final var problemDetail = result.getLeft();
-    assertThat(problemDetail.getStatus()).isEqualTo(400);
-    assertThat(problemDetail.getTitle()).isEqualTo("INVALID_ARGUMENT");
-    assertThat(problemDetail.getDetail()).isEqualTo("No elements provided.");
   }
 }
--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/AdHocSubProcessActivityControllerTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/AdHocSubProcessActivityControllerTest.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.gateway.rest.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.assertArg;
@@ -19,10 +20,8 @@ import io.camunda.security.auth.CamundaAuthentication;
 import io.camunda.security.auth.CamundaAuthenticationProvider;
 import io.camunda.service.AdHocSubProcessActivityServices;
 import io.camunda.service.AdHocSubProcessActivityServices.AdHocSubProcessActivateActivitiesRequest;
-import io.camunda.service.AdHocSubProcessActivityServices.AdHocSubProcessActivateActivitiesRequest.AdHocSubProcessActivateActivityReference;
 import io.camunda.zeebe.gateway.rest.RestControllerTest;
 import io.camunda.zeebe.protocol.impl.record.value.adhocsubprocess.AdHocSubProcessInstructionRecord;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +30,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -58,8 +58,9 @@ class AdHocSubProcessActivityControllerTest extends RestControllerTest {
 
     private static final String AD_HOC_SUBPROCESS_INSTANCE_KEY = "123456789";
 
-    @Test
-    void shouldActivateActivities() {
+    @ParameterizedTest
+    @ValueSource(booleans = {true, false})
+    void shouldActivateActivities(final boolean cancelRemainingInstances) {
       when(adHocSubProcessActivityServices.activateActivities(
               any(AdHocSubProcessActivateActivitiesRequest.class)))
           .thenReturn(CompletableFuture.completedFuture(new AdHocSubProcessInstructionRecord()));
@@ -71,20 +72,24 @@ class AdHocSubProcessActivityControllerTest extends RestControllerTest {
           .contentType(MediaType.APPLICATION_JSON)
           .bodyValue(
               """
-            {
-              "elements": [
-                {
-                   "elementId": "A",
-                   "variables": {
-                      "key": "value"
-                   }
-                },
-                {"elementId": "B"},
-                {"elementId": "C"}
-              ],
-              "cancelRemainingInstances": true
-            }
-            """)
+              {
+                "elements": [
+                  {
+                     "elementId": "A",
+                     "variables": {
+                        "key": "value"
+                     }
+                  },
+                  {
+                      "elementId": "B",
+                      "variables":  {}
+                  },
+                  {"elementId": "C"}
+                ],
+                "cancelRemainingInstances": %b
+              }
+              """
+                  .formatted(cancelRemainingInstances))
           .exchange()
           .expectStatus()
           .isNoContent();
@@ -95,16 +100,64 @@ class AdHocSubProcessActivityControllerTest extends RestControllerTest {
                   request -> {
                     assertThat(request.adHocSubProcessInstanceKey())
                         .isEqualTo(AD_HOC_SUBPROCESS_INSTANCE_KEY);
-                    assertThat(request.cancelRemainingInstances()).isTrue();
+                    assertThat(request.cancelRemainingInstances())
+                        .isEqualTo(cancelRemainingInstances);
 
                     assertThat(request.elements())
-                        .extracting(AdHocSubProcessActivateActivityReference::elementId)
-                        .containsExactly("A", "B", "C");
+                        .satisfiesExactly(
+                            element -> {
+                              assertThat(element.elementId()).isEqualTo("A");
+                              assertThat(element.variables())
+                                  .containsExactly(entry("key", "value"));
+                            },
+                            element -> {
+                              assertThat(element.elementId()).isEqualTo("B");
+                              assertThat(element.variables()).isEmpty();
+                            },
+                            element -> {
+                              assertThat(element.elementId()).isEqualTo("C");
+                              assertThat(element.variables()).isEmpty();
+                            });
+                  }));
+    }
+
+    @Test
+    void shouldActivateActivitiesWithMissingCancelRemainingActivitiesFlag() {
+      when(adHocSubProcessActivityServices.activateActivities(
+              any(AdHocSubProcessActivateActivitiesRequest.class)))
+          .thenReturn(CompletableFuture.completedFuture(new AdHocSubProcessInstructionRecord()));
+
+      webClient
+          .post()
+          .uri(ACTIVATE_ACTIVITIES_URL, AD_HOC_SUBPROCESS_INSTANCE_KEY)
+          .accept(MediaType.APPLICATION_JSON)
+          .contentType(MediaType.APPLICATION_JSON)
+          .bodyValue(
+              """
+                  {
+                    "elements": [
+                      {"elementId": "A"}
+                    ]
+                  }
+                  """)
+          .exchange()
+          .expectStatus()
+          .isNoContent();
+
+      verify(adHocSubProcessActivityServices)
+          .activateActivities(
+              assertArg(
+                  request -> {
+                    assertThat(request.adHocSubProcessInstanceKey())
+                        .isEqualTo(AD_HOC_SUBPROCESS_INSTANCE_KEY);
+                    assertThat(request.cancelRemainingInstances()).isFalse();
 
                     assertThat(request.elements())
-                        .filteredOn(AdHocSubProcessActivateActivityReference::elementId, "A")
-                        .extracting(AdHocSubProcessActivateActivityReference::variables)
-                        .containsExactly(Map.of("key", "value"));
+                        .satisfiesExactly(
+                            element -> {
+                              assertThat(element.elementId()).isEqualTo("A");
+                              assertThat(element.variables()).isEmpty();
+                            });
                   }));
     }
 
