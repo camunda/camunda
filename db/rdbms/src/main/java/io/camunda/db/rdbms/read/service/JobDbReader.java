e6fae5ab9a33f19e4993c1a2e51815e806350852
Stefan Zilske <stefan.zilske@holisticon.de>
2025-08-06 09:09:00 +0200

    feat: Add auth checks to job

--- db/rdbms/src/main/java/io/camunda/db/rdbms/read/service/JobDbReader.java
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/read/service/JobDbReader.java
@@ -16,6 +16,7 @@ import io.camunda.search.entities.JobEntity;
 import io.camunda.search.query.JobQuery;
 import io.camunda.search.query.SearchQueryResult;
 import io.camunda.security.reader.ResourceAccessChecks;
+import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,9 +36,25 @@ public class JobDbReader extends AbstractEntityReader<JobEntity> implements JobR
   public SearchQueryResult<JobEntity> search(
       final JobQuery query, final ResourceAccessChecks resourceAccessChecks) {
     final var dbSort = convertSort(query.sort(), JobSearchColumn.JOB_KEY);
+
+    // If the authorization check is enabled and no resource IDs are authorized, return an empty
+    // result
+    // If the tenant check is enabled and no tenant IDs are authorized, return an empty result
+    if ((resourceAccessChecks.authorizationCheck().enabled()
+            && resourceAccessChecks.getAuthorizedResourceIds().isEmpty())
+        || (resourceAccessChecks.tenantCheck().enabled()
+            && resourceAccessChecks.getAuthorizedTenantIds().isEmpty())) {
+      return buildSearchQueryResult(0, List.of(), dbSort);
+    }
+
     final var dbQuery =
         JobDbQuery.of(
-            b -> b.filter(query.filter()).sort(dbSort).page(convertPaging(dbSort, query.page())));
+            b ->
+                b.filter(query.filter())
+                    .authorizedResourceIds(resourceAccessChecks.getAuthorizedResourceIds())
+                    .authorizedTenantIds(resourceAccessChecks.getAuthorizedTenantIds())
+                    .sort(dbSort)
+                    .page(convertPaging(dbSort, query.page())));
 
     LOG.trace("[RDBMS DB] Search for jobs with filter {}", dbQuery);
     final var totalHits = jobMapper.count(dbQuery);
