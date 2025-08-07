e80c67928a102ac75ced26865ff5bf228a3d27ce
Eamonn Moloney <eamonn.moloney@camunda.com>
2025-08-07 07:30:20 +0100

    fix: e2e tests are failing for c8run. this fixes them

--- c8run/e2e_tests/api_tests.sh
+++ c8run/e2e_tests/api_tests.sh
@@ -3,9 +3,9 @@
 printf "\nTest: Operate process instance api\n"
 
 curl -f -L -X POST 'http://localhost:8080/v2/process-instances/search' \
--H 'Content-Type: application/json' \
--H 'Accept: application/json' \
---data-raw '{
+        -H 'Content-Type: application/json' \
+        -H 'Accept: application/json' \
+        --data-raw '{
   "filter": {
     "running": true,
     "active": true
@@ -15,48 +15,45 @@ curl -f -L -X POST 'http://localhost:8080/v2/process-instances/search' \
 returnCode=$?
 
 if [[ "$returnCode" != 0 ]]; then
-   echo "test failed"
-   exit 1
+        echo "test failed"
+        exit 1
 fi
 
 printf "\nTest: Tasklist user task\n"
 curl -f -L -X POST 'http://localhost:8080/v2/user-tasks/search' \
--H 'Content-Type: application/json' \
--H 'Accept: application/json' \
---data-raw '{}'
+        -H 'Content-Type: application/json' \
+        -H 'Accept: application/json' \
+        --data-raw '{}'
 
 returnCode=$?
 
 if [[ "$returnCode" != 0 ]]; then
-   echo "test failed"
-   exit 1
+        echo "test failed"
+        exit 1
 fi
-
 
 printf "\nTest: Zeebe topology endpoint\n"
 curl localhost:8080/v2/topology
 
 returnCode=$?
 if [[ "$returnCode" != 0 ]]; then
-   echo "test failed"
-   exit 1
+        echo "test failed"
+        exit 1
 fi
 printf "\nTest: test --config flag\n"
 
-PREFIX="$( curl localhost:9600/actuator/configprops | jq '.contexts.Camunda.beans.["io.camunda.tasklist.property.TasklistProperties"].properties.zeebeElasticsearch.prefix' )"
+PREFIX="$(curl localhost:9600/actuator/configprops | jq '.contexts.camunda.beans.["camunda.tasklist-io.camunda.configuration.beanoverrides.LegacyTasklistProperties"].properties.zeebeElasticsearch.prefix')"
 echo $PREFIX
 if [[ "$PREFIX" != "\"extra-prefix-zeebe-record\"" ]]; then
-   echo "test failed"
-   exit 1
+        echo "test failed"
+        exit 1
 fi
-
 
 printf "\nTest: connectors api \n"
 
-STATUS="$( curl localhost:8085/actuator/health | jq '.status' )"
+STATUS="$(curl localhost:8085/actuator/health | jq '.status')"
 echo $STATUS
 if [[ "$STATUS" != "\"UP\"" ]]; then
-   echo "test failed"
-   exit 1
+        echo "test failed"
+        exit 1
 fi
-
