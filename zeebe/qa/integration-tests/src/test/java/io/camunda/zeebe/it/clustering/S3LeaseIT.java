27f751a998ecf14a4dca4637f20e18628d1ebb27
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2025-08-08 12:28:32 +0300

    test: set test for AWS S3

--- zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/clustering/S3LeaseIT.java
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/clustering/S3LeaseIT.java
@@ -14,23 +14,14 @@ import io.camunda.zeebe.broker.clustering.mapper.S3LeaseConfig;
 import io.camunda.zeebe.qa.util.cluster.TestCluster;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
 import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
-import java.net.URI;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
@@ -40,27 +31,13 @@ import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
 @Testcontainers
 public class S3LeaseIT {
 
-  public static final String ACCESS_KEY = "letmein";
-  public static final String SECRET_KEY = "letmein1234";
-  public static final int S3_PORT = 9000;
+  // TODO: Provide you AWS S3 credentials
+  // Neither LocalStack S3 nor MinIO have strong consistence guarantees on atomic file operations
+  public static final String ACCESS_KEY = "";
+  public static final String SECRET_KEY = "";
+  public static final Region REGION = Region.EU_NORTH_1;
   public static S3AsyncClient s3Client;
-  private static final Logger LOG = LoggerFactory.getLogger(S3LeaseIT.class);
-  private static final String BUCKET_NAME = RandomStringUtils.randomAlphabetic(10).toLowerCase();
-
-  @SuppressWarnings("resource")
-  @Container
-  private static final GenericContainer<?> S3 =
-      new GenericContainer<>(DockerImageName.parse("minio/minio"))
-          .withCommand("server /data")
-          .withExposedPorts(S3_PORT)
-          .withEnv("MINIO_ACCESS_KEY", ACCESS_KEY)
-          .withEnv("MINIO_SECRET_KEY", SECRET_KEY)
-          .withEnv("MINIO_DOMAIN", "localhost")
-          .waitingFor(
-              new HttpWaitStrategy()
-                  .forPath("/minio/health/ready")
-                  .forPort(S3_PORT)
-                  .withStartupTimeout(Duration.ofMinutes(1)));
+  private static final String BUCKET_NAME = "";
 
   @TestZeebe(autoStart = false)
   private final TestCluster cluster =
@@ -74,22 +51,24 @@ public class S3LeaseIT {
 
   @BeforeEach
   void beforeAll() {
-    final String s3Uri = "http://%s:%d".formatted(S3.getHost(), S3.getMappedPort(S3_PORT));
     final S3LeaseConfig config = new S3LeaseConfig();
     config.setBucketName(BUCKET_NAME);
-    config.setEndpoint(Optional.of(s3Uri));
     config.setAccessKey(ACCESS_KEY);
     config.setSecretKey(SECRET_KEY);
-    config.setRegion(Optional.of(Region.US_EAST_1.id()));
+    config.setRegion(Optional.of(REGION.id()));
     s3Client =
         S3AsyncClient.builder()
-            .endpointOverride(URI.create(s3Uri))
-            .region(Region.US_EAST_1)
+            .region(Region.of(config.getRegion().get()))
             .credentialsProvider(
                 () -> AwsBasicCredentials.create(config.getAccessKey(), config.getSecretKey()))
             .build();
-
-    s3Client.createBucket(CreateBucketRequest.builder().bucket(config.bucketName()).build()).join();
+    if (s3Client.listBuckets().join().buckets().stream()
+        .noneMatch(b -> b.name().equals(BUCKET_NAME))) {
+      // Create bucket if it does not exist
+      s3Client
+          .createBucket(CreateBucketRequest.builder().bucket(config.bucketName()).build())
+          .join();
+    }
     cluster.brokers().values().forEach(broker -> broker.brokerConfig().setLeaseConfig(config));
     cluster.start().awaitCompleteTopology();
   }
