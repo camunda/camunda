5d3be9765a918f44f2f321e67cfabd6c4b26f4b1
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-08-08 12:55:41 +0200

    feat: extracted job worker factory from job worker manager

--- clients/camunda-spring-boot-starter/src/main/java/io/camunda/client/spring/configuration/CamundaClientAllAutoConfiguration.java
+++ clients/camunda-spring-boot-starter/src/main/java/io/camunda/client/spring/configuration/CamundaClientAllAutoConfiguration.java
@@ -24,6 +24,7 @@ import io.camunda.client.jobhandling.CommandExceptionHandlingStrategy;
 import io.camunda.client.jobhandling.DefaultCommandExceptionHandlingStrategy;
 import io.camunda.client.jobhandling.DefaultJobExceptionHandlingStrategy;
 import io.camunda.client.jobhandling.JobExceptionHandlingStrategy;
+import io.camunda.spring.client.jobhandling.JobWorkerFactory;
 import io.camunda.client.jobhandling.JobWorkerManager;
 import io.camunda.client.jobhandling.parameter.DefaultParameterResolverStrategy;
 import io.camunda.client.jobhandling.parameter.ParameterResolverStrategy;
@@ -109,6 +110,12 @@ public class CamundaClientAllAutoConfiguration {
   }
 
   @Bean
+  public JobWorkerFactory jobWorkerFactory(
+      final BackoffSupplier backoffSupplier, final MetricsRecorder metricsRecorder) {
+    return new JobWorkerFactory(backoffSupplier, metricsRecorder);
+  }
+
+  @Bean
   public JobWorkerManager jobWorkerManager(
       final CommandExceptionHandlingStrategy commandExceptionHandlingStrategy,
       final MetricsRecorder metricsRecorder,
@@ -116,15 +123,16 @@ public class CamundaClientAllAutoConfiguration {
       final ResultProcessorStrategy resultProcessorStrategy,
       final BackoffSupplier backoffSupplier,
       final JobExceptionHandlingStrategy jobExceptionHandlingStrategy,
-      final List<JobWorkerValueCustomizer> jobWorkerValueCustomizers) {
+      final List<JobWorkerValueCustomizer> jobWorkerValueCustomizers,
+      final JobWorkerFactory jobWorkerFactory) {
     return new JobWorkerManager(
         commandExceptionHandlingStrategy,
         metricsRecorder,
         parameterResolverStrategy,
         resultProcessorStrategy,
-        backoffSupplier,
         jobExceptionHandlingStrategy,
-        jobWorkerValueCustomizers);
+        jobWorkerValueCustomizers,
+        jobWorkerFactory);
   }
 
   @Bean
