9a034b4767a80ae88db3238379398ba736bc1105
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-08-08 19:17:30 +0200

    fix: fix active operation polling

--- operate/client/src/modules/mutations/processInstance/useCreateIncidentResolutionBatchOperation.tsx
+++ operate/client/src/modules/mutations/processInstance/useCreateIncidentResolutionBatchOperation.tsx
@@ -11,20 +11,9 @@ import {
   useQueryClient,
   type UseMutationOptions,
 } from '@tanstack/react-query';
-import {
-  type CreateIncidentResolutionBatchOperationResponseBody,
-  type QueryBatchOperationsRequestBody,
-} from '@vzeta/camunda-api-zod-schemas/8.8';
-import {queryBatchOperations} from 'modules/api/v2/batchOperations/queryBatchOperations';
+import {type CreateIncidentResolutionBatchOperationResponseBody} from '@vzeta/camunda-api-zod-schemas/8.8';
+import {getBatchOperation} from 'modules/api/v2/batchOperations/getBatchOperation';
 import {createIncidentResolutionBatchOperation} from 'modules/api/v2/processInstances/createIncidentResolutionBatchOperation';
-
-const getMutationKey = (processInstanceKey: string) => {
-  return ['mutateBatchOperations', processInstanceKey, 'incidentResolution'];
-};
-
-function getQueryKey(payload: QueryBatchOperationsRequestBody) {
-  return ['queryBatchOperations', ...Object.values(payload)];
-}
 
 const useCreateIncidentResolutionBatchOperation = (
   processInstanceKey: string,
@@ -35,41 +24,39 @@ const useCreateIncidentResolutionBatchOperation = (
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationKey: getMutationKey(processInstanceKey),
+    mutationKey: [
+      'mutateBatchOperations',
+      processInstanceKey,
+      'incidentResolution',
+    ],
     mutationFn: async () => {
       const {response, error} = await createIncidentResolutionBatchOperation({
         processInstanceKey,
       });
-      if (error) {
+
+      if (response === null) {
         throw new Error(error.response?.statusText);
       }
 
-      const payload = {filter: {batchOperationKey: response.batchOperationKey}};
-      const OPERATION_PENDING = 'Batch operation is still pending';
+      const {batchOperationKey} = response;
 
       await queryClient.fetchQuery({
-        queryKey: getQueryKey(payload),
+        queryKey: ['queryBatchOperations', batchOperationKey],
         queryFn: async () => {
-          const {response, error} = await queryBatchOperations(payload);
+          const {response, error} = await getBatchOperation({
+            batchOperationKey,
+          });
           if (error) {
             throw new Error(error.response?.statusText);
           }
 
-          const hasActiveOperation = response.items.some(
-            (item) => item.state === 'ACTIVE',
-          );
-
-          if (response.page.totalItems === 0 || hasActiveOperation) {
-            throw new Error(OPERATION_PENDING);
+          if (response.state !== 'COMPLETED') {
+            throw new Error('Batch operation is still pending');
           }
+
           return response;
         },
-        retry: (_, error) => {
-          if (error.message === OPERATION_PENDING) {
-            return true;
-          }
-          return false;
-        },
+        retry: true,
         retryDelay: 5000,
       });
 
@@ -79,4 +66,4 @@ const useCreateIncidentResolutionBatchOperation = (
   });
 };
 
-export {useCreateIncidentResolutionBatchOperation, getMutationKey};
+export {useCreateIncidentResolutionBatchOperation};
