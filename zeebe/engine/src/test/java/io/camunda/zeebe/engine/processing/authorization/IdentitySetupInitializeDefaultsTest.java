822a901c85df67bad2d6d0664c38b6e02a052aa8
Nico Korthout <nico.korthout@camunda.com>
2025-08-08 17:19:29 +0200

    fix: align drg permissions to docs
    Resource type DECISION_REQUIREMENTS_DEFINITION should only have
    PermissionType.READ. Creating and deleting happens through the RESOURCE
    resource type. Updating is not supported at all.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/IdentitySetupInitializeDefaultsTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/IdentitySetupInitializeDefaultsTest.java
@@ -123,8 +123,7 @@ public class IdentitySetupInitializeDefaultsTest {
             auth ->
                 Assertions.assertThat(auth)
                     .hasResourceType(AuthorizationResourceType.DECISION_REQUIREMENTS_DEFINITION)
-                    .hasOnlyPermissionTypes(
-                        PermissionType.READ, PermissionType.UPDATE, PermissionType.DELETE),
+                    .hasOnlyPermissionTypes(PermissionType.READ),
             auth ->
                 Assertions.assertThat(auth)
                     .hasResourceType(AuthorizationResourceType.DECISION_DEFINITION)
