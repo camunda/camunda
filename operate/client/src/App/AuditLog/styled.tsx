6a0e94644e21f0990a7a166908bb7320603cea65
volodymyr-melnykc <volodymyr.melnyk@camunda.com>
2025-08-08 08:34:57 +0200

    Add the audit log page with comment editing

---
+++ operate/client/src/App/AuditLog/styled.tsx
@@ -0,0 +1,17 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export {Container};
