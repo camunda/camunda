135021e131903dcf92980fd02f18ef4cfabd21b5
ana.vinogradova <ana.vinogradova@camunda.com>
2025-08-09 10:59:25 +0100

    fix: add a wrapper for `ClientRegistrationRepository`
    The main purpose of the wrapper is to ensure `ClientRegistrationRepository`
    is created rather on a startup or later during retries:
    - on a startup it will attempt to create a repository
    - if this fails, it'll create `SafeInitProxy` (see previous commits) and pass 2 arguments:
     a supplier to create a `ClientRegistrationRepository` and en error listener (log entry will be present)
    - startup will succeed even when IdP is not available
    - created proxy will keep attempting to create the repository in the background
    - at the same time, if client makes a call (any), `findByRegistrationId` will attempt to get a value
    from the proxy (if successful that means the IdP is available). If the value is not present
    (meaning IdP is still not available) it'll throw an exception.

---
+++ authentication/src/main/java/io/camunda/authentication/config/SafeInitClientRegistrationRepository.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.authentication.config;
+
+import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+/**
+ * Wrapper around ClientRegistrationRepository that ensures repository is created rather at a
+ * startup or during later retries.
+ */
+public class SafeInitClientRegistrationRepository implements ClientRegistrationRepository {
+
+  private static final Logger LOG =
+      LoggerFactory.getLogger(SafeInitClientRegistrationRepository.class);
+
+  /**
+   * Helper that ensures ClientRegistrationRepository is created rather on startup or after retries
+   * if startup initialization failed.
+   */
+  private final SafeInitProxy<ClientRegistrationRepository> proxy;
+
+  public SafeInitClientRegistrationRepository(
+      final Supplier<ClientRegistrationRepository> repositoryFactory) {
+    // Try to create a repository. In case of failure - schedule async retries of creation.
+    proxy =
+        new SafeInitProxy<>(
+            repositoryFactory,
+            e -> LOG.error("Failed to initialize ClientRegistrationRepository. Retrying.", e));
+  }
+
+  @Override
+  public ClientRegistration findByRegistrationId(final String registrationId) {
+    // if repository is initialized, then delegate method call to it. If not, throw an exception
+    // that will be propagated to client.
+    return proxy
+        .orElseThrow(
+            () ->
+                new RuntimeException(
+                    "Authentication service unavailable: Unable to connect to the configured Identity Provider (OIDC). "
+                        + "Please try again later or contact your administrator."))
+        .findByRegistrationId(registrationId);
+  }
+}
