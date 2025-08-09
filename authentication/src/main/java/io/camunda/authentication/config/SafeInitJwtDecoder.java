084f548cf1dfa46031b90d3427f9fb3c411d1487
ana.vinogradova <ana.vinogradova@camunda.com>
2025-08-09 11:06:19 +0100

    fix: add a wrapper for `JwtDecoder`
    Same as for the previous wrapper,the main purpose of this is to ensure `JwtDecoder`
    is created rather on a startup or later during retries:
    
    - on a startup it will attempt to create a decoder
    - if this fails, it'll create `SafeInitProxy` instance (see previous commits) and pass 2 arguments:
     a supplier to create a `JwtDecoder` and en error listener (log entry will be present)
    - this will schedule async retries of creation
    - startup will succeed
    - created proxy will keep attempting to create the decoder in the background
    - at the same time, if client makes a call (any), `decode` will attempt to get a value
    from the proxy (if successful that means the IdP is available). If the value is not present
    (meaning IdP is still not available) it'll throw an exception that will be returned to the client.
    
    Open question: what the error message should be here?
    JwtDecoder relies on ClientRegistrationRepository so I guess the error here should
    probably be the same?

---
+++ authentication/src/main/java/io/camunda/authentication/config/SafeInitJwtDecoder.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.authentication.config;
+
+import java.util.function.Supplier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+
+/**
+ * Wrapper around JwtDecoder that ensures decoder is created rather at a startup or during later
+ * retries.
+ */
+public class SafeInitJwtDecoder implements JwtDecoder {
+  private static final Logger LOG = LoggerFactory.getLogger(SafeInitJwtDecoder.class);
+
+  private final SafeInitProxy<JwtDecoder> proxy;
+
+  public SafeInitJwtDecoder(final Supplier<JwtDecoder> decoderSupplier) {
+    // Try to create a decoder. In case of failure - schedule async retries of creation.
+    proxy =
+        new SafeInitProxy<>(
+            decoderSupplier, e -> LOG.error("Failed to initialize JWT Decoder. Retrying.", e));
+  }
+
+  @Override
+  public Jwt decode(final String token) throws JwtException {
+    // if JWT Decoder is initialized, then delegate method call to it. If not, throw an exception
+    // that will be propagated to client.
+    return proxy
+        .orElseThrow(
+            () ->
+                new AuthenticationCredentialsNotFoundException(
+                    "Authentication service unavailable: Unable to connect to the configured Identity Provider (OIDC). "
+                        + "Please try again later or contact your administrator."))
+        .decode(token);
+  }
+}
