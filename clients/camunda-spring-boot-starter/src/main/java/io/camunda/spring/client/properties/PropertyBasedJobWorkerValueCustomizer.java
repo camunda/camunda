64c591b7b3977001635975f98a3a58b0f32d21d0
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-08-09 06:21:52 +0200

    fix: adjusted imports

--- clients/camunda-spring-boot-starter/src/main/java/io/camunda/spring/client/properties/PropertyBasedJobWorkerValueCustomizer.java
+++ clients/camunda-spring-boot-starter/src/main/java/io/camunda/spring/client/properties/PropertyBasedJobWorkerValueCustomizer.java
@@ -28,7 +28,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
