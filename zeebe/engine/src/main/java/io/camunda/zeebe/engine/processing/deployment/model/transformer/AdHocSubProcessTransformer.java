0217cc2191f3c5357e09c4fd83d23dc601737f4b
Matt Bear <matthew.bear@camunda.com>
2025-08-11 10:30:55 -0500

    feat: Have Transformer set Output Collection and Element

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/model/transformer/AdHocSubProcessTransformer.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/model/transformer/AdHocSubProcessTransformer.java
@@ -72,6 +72,7 @@ public final class AdHocSubProcessTransformer implements ModelElementTransformer
     setInnerInstance(executableAdHocSubProcess, childElements, process);
     setJobWorkerProperties(executableAdHocSubProcess, context, element);
     setAdHocActivitiesMetadata(executableAdHocSubProcess);
+    setOutputCollectionAndElement(executableAdHocSubProcess, element, context);
   }
 
   private static void setActiveElementsCollection(
@@ -200,6 +201,23 @@ public final class AdHocSubProcessTransformer implements ModelElementTransformer
         parameter.type(),
         parameter.schema(),
         parameter.options());
+  }
+
+  private void setOutputCollectionAndElement(
+      final ExecutableAdHocSubProcess executableAdHocSubProcess,
+      final AdHocSubProcess element,
+      final TransformContext context) {
+    Optional.ofNullable(element.getSingleExtensionElement(ZeebeAdHoc.class))
+        .map(ZeebeAdHoc::getOutputCollection)
+        .filter(e -> !e.isEmpty())
+        .map(BufferUtil::wrapString)
+        .ifPresent(executableAdHocSubProcess::setOutputCollection);
+
+    Optional.ofNullable(element.getSingleExtensionElement(ZeebeAdHoc.class))
+        .map(ZeebeAdHoc::getOutputElement)
+        .filter(e -> !e.isEmpty())
+        .map(e -> context.getExpressionLanguage().parseExpression(e))
+        .ifPresent(executableAdHocSubProcess::setOutputElement);
   }
 
   /**
