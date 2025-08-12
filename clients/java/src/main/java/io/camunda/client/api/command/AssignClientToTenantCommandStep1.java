710f02678c964026855baf0e39841f84817be5d4
ana.vinogradova <ana.vinogradova@camunda.com>
2025-08-12 07:41:56 +0100

    feat: add a command steps to assign client to tenant

---
+++ clients/java/src/main/java/io/camunda/client/api/command/AssignClientToTenantCommandStep1.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.client.api.command;
+
+import io.camunda.client.api.response.AssignClientToTenantResponse;
+
+public interface AssignClientToTenantCommandStep1 {
+
+  /**
+   * Sets the ID of the client to be assigned to a tenant.
+   *
+   * @param clientId the ID of the client
+   * @return the builder for this command.
+   */
+  AssignClientToTenantCommandStep2 clientId(String clientId);
+
+  interface AssignClientToTenantCommandStep2
+      extends FinalCommandStep<AssignClientToTenantResponse> {
+
+    /**
+     * Sets the tenant ID.
+     *
+     * @param tenantId the ID of the tenant
+     * @return the builder for this command. Call {@link #send()} to complete the command and send
+     *     it to the broker.
+     */
+    AssignClientToTenantCommandStep2 tenantId(String tenantId);
+  }
+}
