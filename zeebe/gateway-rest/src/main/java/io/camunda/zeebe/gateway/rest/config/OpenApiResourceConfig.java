b9f00acc76deb271693c09c64d102eb8bade83c5
Alex Bremard <alex.bremard@camunda.com>
2025-08-14 14:17:25 +0700

    feat: move description and security schemes to OpenApiResourceConfig

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/OpenApiResourceConfig.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/OpenApiResourceConfig.java
@@ -9,13 +9,22 @@ package io.camunda.zeebe.gateway.rest.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.zeebe.gateway.rest.util.YamlToJsonResourceTransformer;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class OpenApiResourceConfig implements WebMvcConfigurer {
+
+  public static final String BEARER_SECURITY_SCHEMA_NAME = "bearerAuth";
+  public static final SecurityScheme BEARER_SECURITY_SCHEMA =
+      new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
 
   @Autowired private ObjectMapper objectMapper;
 
@@ -26,5 +35,27 @@ public class OpenApiResourceConfig implements WebMvcConfigurer {
         .addResourceLocations("classpath:/apidoc/")
         .resourceChain(true)
         .addTransformer(new YamlToJsonResourceTransformer(objectMapper));
+  }
+
+  @Bean
+  public GroupedOpenApi restApiV1() {
+    return GroupedOpenApi.builder()
+        .group("Orchestration Cluster API")
+        .addOpenApiCustomizer(
+            openApi -> {
+              openApi
+                  .info(new Info().description(getApiDescription()))
+                  .getComponents()
+                  .addSecuritySchemes(BEARER_SECURITY_SCHEMA_NAME, BEARER_SECURITY_SCHEMA);
+
+              openApi.addSecurityItem(
+                  new SecurityRequirement().addList(BEARER_SECURITY_SCHEMA_NAME));
+            })
+        .pathsToMatch("/**")
+        .build();
+  }
+
+  private String getApiDescription() {
+    return "API for communicating with a Camunda 8 cluster.";
   }
 }
