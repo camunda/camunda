0d9da80783e85331a34041fd139bae98839af41b
Ben Sheppard <ben.sheppard@camunda.com>
2025-08-14 13:37:18 +0100

    feat: apply permission checks to V1 API to get a form

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/api/rest/v1/controllers/FormController.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/api/rest/v1/controllers/FormController.java
@@ -7,9 +7,13 @@
  */
 package io.camunda.tasklist.webapp.api.rest.v1.controllers;
 
+import static io.camunda.tasklist.webapp.permission.TasklistPermissionServices.WILDCARD_RESOURCE;
+
 import io.camunda.tasklist.store.FormStore;
 import io.camunda.tasklist.webapp.api.rest.v1.entities.FormResponse;
+import io.camunda.tasklist.webapp.permission.TasklistPermissionServices;
 import io.camunda.tasklist.webapp.rest.exception.Error;
+import io.camunda.tasklist.webapp.rest.exception.ForbiddenActionException;
 import io.camunda.tasklist.webapp.security.TasklistURIs;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -32,6 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FormController extends ApiErrorController {
 
   @Autowired private FormStore formStore;
+  @Autowired private TasklistPermissionServices tasklistPermissionServices;
 
   @Operation(
       summary = "Get a form",
@@ -49,19 +54,33 @@ public class FormController extends ApiErrorController {
             content =
                 @Content(
                     mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                    schema = @Schema(implementation = Error.class))),
+        @ApiResponse(
+            description =
+                "A forbidden error is returned when user does not have permission read user tasks.",
+            responseCode = "403",
+            content =
+                @Content(
+                    mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
                     schema = @Schema(implementation = Error.class)))
       })
   @GetMapping("{formId}")
   public ResponseEntity<FormResponse> getForm(
-      @PathVariable @Parameter(description = "The ID of the form.", required = true) String formId,
+      @PathVariable @Parameter(description = "The ID of the form.", required = true)
+          final String formId,
       @RequestParam
           @Parameter(description = "Reference to the process definition.", required = true)
-          String processDefinitionKey,
+          final String processDefinitionKey,
       @RequestParam(required = false)
           @Parameter(
               description = "The version of the form. Valid only for deployed forms.",
               required = false)
-          Long version) {
+          final Long version) {
+    if (!tasklistPermissionServices.hasPermissionToReadProcessDefinition(WILDCARD_RESOURCE)) {
+      throw new ForbiddenActionException(
+          "User does not have permission to read resource. Please check your permissions.");
+    }
+
     final var form = formStore.getForm(formId, processDefinitionKey, version);
 
     // This is to set processDefinitionKey when the form is deployed
