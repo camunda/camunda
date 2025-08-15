6ca387cfb20ad5fd418b1c2ef40f170d1c24c099
Matt Bear <matthew.bear@camunda.com>
2025-08-15 12:31:19 -0500

    feat: Avoid AHSP element activation if the completion condition is fulfilled

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/bpmn/activity/JobBasedAdHocSubProcessTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/bpmn/activity/JobBasedAdHocSubProcessTest.java
@@ -878,6 +878,29 @@ public class JobBasedAdHocSubProcessTest {
         .hasErrorMessage("The output collection has the wrong type. Expected ARRAY but was NIL.");
   }
 
+  @Test
+  public void shouldRejectWhenCompletionConditionIsFullfilledAndActivatingElements() {
+    // given
+    final var jobType = UUID.randomUUID().toString();
+    final BpmnModelInstance process =
+        process(
+            jobType,
+            adHocSubProcess -> {
+              adHocSubProcess.task("A");
+              adHocSubProcess.userTask("B");
+              adHocSubProcess.userTask("C");
+            });
+    ENGINE.deployment().withXmlResource(process).deploy();
+
+    // when
+    completeJob(jobType, true, false, activateElement("A"), activateElement("B"));
+
+    // then
+    Assertions.assertThat(RecordingExporter.jobRecords().onlyCommandRejections().getFirst())
+        .extracting(Record::getRejectionType, Record::getIntent)
+        .containsOnly(RejectionType.INVALID_ARGUMENT, JobIntent.COMPLETE);
+  }
+
   private void completeJob(
       final String jobType,
       final boolean completionConditionFulfilled,
@@ -891,6 +914,7 @@ public class JobBasedAdHocSubProcessTest {
             .setCompletionConditionFulfilled(completionConditionFulfilled)
             .setCancelRemainingInstances(cancelRemainingInstances);
     ENGINE.job().withKey(jobKey).withResult(jobResult).complete();
+    System.out.println("this is the job key: " + jobKey);
   }
 
   private JobResultActivateElement activateElement(final String elementId) {
