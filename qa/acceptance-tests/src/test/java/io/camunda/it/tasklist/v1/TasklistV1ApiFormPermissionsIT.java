ded69218cb51b410e1a0bebf4c4ce072d6935f98
Ben Sheppard <ben.sheppard@camunda.com>
2025-08-18 15:30:28 +0100

    test: implement ITs for form v1 GET API endpoint

---
+++ qa/acceptance-tests/src/test/java/io/camunda/it/tasklist/v1/TasklistV1ApiFormPermissionsIT.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.tasklist.v1;
+
+import static io.camunda.client.api.search.enums.PermissionType.CREATE;
+import static io.camunda.client.api.search.enums.PermissionType.CREATE_PROCESS_INSTANCE;
+import static io.camunda.client.api.search.enums.PermissionType.READ_USER_TASK;
+import static io.camunda.client.api.search.enums.ResourceType.AUTHORIZATION;
+import static io.camunda.client.api.search.enums.ResourceType.PROCESS_DEFINITION;
+import static io.camunda.client.api.search.enums.ResourceType.RESOURCE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.camunda.client.CamundaClient;
+import io.camunda.qa.util.auth.Authenticated;
+import io.camunda.qa.util.auth.Permissions;
+import io.camunda.qa.util.auth.TestUser;
+import io.camunda.qa.util.auth.UserDefinition;
+import io.camunda.qa.util.cluster.TestCamundaApplication;
+import io.camunda.qa.util.cluster.TestRestTasklistClient;
+import io.camunda.qa.util.multidb.CamundaMultiDBExtension;
+import io.camunda.qa.util.multidb.MultiDbTest;
+import io.camunda.qa.util.multidb.MultiDbTestApplication;
+import io.camunda.tasklist.webapp.api.rest.v1.entities.FormResponse;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.test.util.JsonUtil;
+import java.net.http.HttpClient;
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.AutoClose;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.springframework.http.HttpStatus;
+
+@MultiDbTest
+@DisabledIfSystemProperty(named = "test.integration.camunda.database.type", matches = "rdbms")
+@DisabledIfSystemProperty(named = "test.integration.camunda.database.type", matches = "AWS_OS")
+public class TasklistV1ApiFormPermissionsIT {
+  @MultiDbTestApplication
+  static final TestCamundaApplication STANDALONE_CAMUNDA =
+      new TestCamundaApplication().withAuthorizationsEnabled().withBasicAuth();
+
+  private static final String PROCESS_ID = "processId";
+  private static final String ADMIN_USERNAME = "admin";
+  private static final String UNAUTHORIZED_USERNAME = "unauthorized";
+  private static long processDefinitionKey;
+  private static long taskKey;
+  private static String formId;
+  private static TestRestTasklistClient AUTHORIZED_CLIENT;
+  private static TestRestTasklistClient UNAUTHORIZED_CLIENT;
+
+  @AutoClose private static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+
+  @UserDefinition
+  private static final TestUser ADMIN =
+      new TestUser(
+          ADMIN_USERNAME,
+          ADMIN_USERNAME,
+          List.of(
+              new Permissions(AUTHORIZATION, CREATE, List.of("*")),
+              new Permissions(RESOURCE, CREATE, List.of("*")),
+              new Permissions(PROCESS_DEFINITION, CREATE_PROCESS_INSTANCE, List.of("*")),
+              new Permissions(PROCESS_DEFINITION, READ_USER_TASK, List.of("*"))));
+
+  @UserDefinition
+  private static final TestUser UNAUTHORIZED =
+      new TestUser(UNAUTHORIZED_USERNAME, UNAUTHORIZED_USERNAME, List.of());
+
+  @BeforeAll
+  public static void beforeAll(@Authenticated(ADMIN_USERNAME) final CamundaClient adminClient)
+      throws Exception {
+    await()
+        .atMost(CamundaMultiDBExtension.TIMEOUT_DATA_AVAILABILITY)
+        .ignoreExceptions()
+        .untilAsserted(
+            () -> {
+              final var form =
+                  adminClient
+                      .newDeployResourceCommand()
+                      .addResourceFromClasspath("form/form.form")
+                      .send()
+                      .join()
+                      .getForm()
+                      .getFirst();
+              formId = form.getFormId();
+            });
+
+    final var deployment =
+        adminClient
+            .newDeployResourceCommand()
+            .addProcessModel(
+                Bpmn.createExecutableProcess(PROCESS_ID)
+                    .startEvent()
+                    .userTask()
+                    .zeebeFormId(formId)
+                    .zeebeUserTask()
+                    .endEvent()
+                    .done(),
+                "process.bpmn")
+            .send()
+            .join();
+    processDefinitionKey = deployment.getProcesses().getFirst().getProcessDefinitionKey();
+
+    final String json = JsonUtil.toJson(Collections.singletonMap("key", "val"));
+    final var processInstanceEvent =
+        adminClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId(PROCESS_ID)
+            .latestVersion()
+            .variables(json)
+            .send()
+            .join();
+    await()
+        .atMost(CamundaMultiDBExtension.TIMEOUT_DATA_AVAILABILITY)
+        .ignoreExceptions()
+        .untilAsserted(
+            () -> {
+              final var tasks =
+                  adminClient
+                      .newUserTaskSearchRequest()
+                      .filter(
+                          t -> t.processInstanceKey(processInstanceEvent.getProcessInstanceKey()))
+                      .send()
+                      .join()
+                      .items();
+              assertThat(tasks).describedAs("Wait until the task exists").hasSize(1);
+              taskKey = tasks.getFirst().getUserTaskKey();
+            });
+
+    AUTHORIZED_CLIENT =
+        STANDALONE_CAMUNDA
+            .newTasklistClient()
+            .withAuthentication(ADMIN.username(), ADMIN.password());
+    UNAUTHORIZED_CLIENT =
+        STANDALONE_CAMUNDA
+            .newTasklistClient()
+            .withAuthentication(UNAUTHORIZED.username(), UNAUTHORIZED.password());
+  }
+
+  @Test
+  void shouldBeAuthorizedToGetForm() throws JsonProcessingException {
+    final var response =
+        AUTHORIZED_CLIENT.getRequest(
+            "v1/forms/%s?processDefinitionKey=%s".formatted(formId, processDefinitionKey));
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    final var form =
+        TestRestTasklistClient.OBJECT_MAPPER.readValue(response.body(), FormResponse.class);
+    assertThat(form).isNotNull();
+    assertThat(form.getId()).isEqualTo(formId);
+  }
+
+  @Test
+  void shouldBeUnauthorizedToGetForm() throws JsonProcessingException {
+    final var response =
+        UNAUTHORIZED_CLIENT.getRequest(
+            "v1/forms/%s?processDefinitionKey=%s".formatted(formId, processDefinitionKey));
+    assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+    assertThat(response.body())
+        .contains("User does not have permission to read resource. Please check your permissions.");
+  }
+}
