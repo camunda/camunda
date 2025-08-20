95075ddd034916575810387a52cf9965b1d07509
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-08-20 23:10:13 +0200

    deps: add prettier

---
+++ client-components/.prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  singleQuote: true,
+  bracketSpacing: false,
+  useTabs: true,
+  tabWidth: 2,
+  printWidth: 120,
+  trailingComma: "all",
+};
