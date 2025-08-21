e4d48f46f83ee4b6d90697bcb09b57a7dfed1113
Nico Korthout <nico.korthout@camunda.com>
2025-08-21 08:08:57 +0200

    refactor: reflect codebase structure in ArchTests
    This change moves the RequireKebabCaseInConditionalOnPropertyArchTest to
    the io.camunda package as it covers all those classes.
    
    This makes it easier for other ArchTests to also align to the structure
    of the codebase that they work with. The end goal is a module that
    organizes its test in way where it's easy to see at a glance what code
    is covered by these tests.
    
    (cherry picked from commit 42ce60f902b70c02de9c8d2d3eb4e0f66614bcc4)

--- qa/archunit-tests/src/test/java/io/camunda/RequireKebabCaseInConditionalOnPropertyArchTest.java
+++ qa/archunit-tests/src/test/java/io/camunda/RequireKebabCaseInConditionalOnPropertyArchTest.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.archunit;
+package io.camunda;
 
 import com.tngtech.archunit.core.domain.JavaAnnotation;
 import com.tngtech.archunit.core.domain.JavaClass;
