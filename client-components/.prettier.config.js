21254a2b8d2fad6100b6e09195974491359adcfd
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-08-21 15:17:09 +0200

    fix: fix prettier

--- client-components/.prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('prettier').Config} */
-export default {
-  singleQuote: true,
-  bracketSpacing: false,
-  useTabs: true,
-  tabWidth: 2,
-  printWidth: 120,
-  trailingComma: "all",
-};
--- client-components/eslint.config.ts
+++ client-components/eslint.config.ts
@@ -1,22 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import {defineConfig} from 'eslint/config';
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: {
-      globals: globals.browser,
-    },
-  },
-  tseslint.configs.recommended,
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-    },
-  },
+	{
+		files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+		plugins: {js},
+		extends: ['js/recommended'],
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+	tseslint.configs.recommended,
+	{
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+		},
+	},
 ]);
