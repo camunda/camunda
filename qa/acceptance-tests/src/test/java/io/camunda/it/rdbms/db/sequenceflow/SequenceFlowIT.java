a8372718cb2ea50226260b399fc4e8eb45ee61c4
Christian Thiel <christian.thiel@holisticon.de>
2025-08-21 11:41:23 +0200

    feat: add auth checks for sequenceFlow entity #28725
    Co-authored-by: Stefan Zilske <stefan.zilske@holisticon.de>

--- qa/acceptance-tests/src/test/java/io/camunda/it/rdbms/db/sequenceflow/SequenceFlowIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/rdbms/db/sequenceflow/SequenceFlowIT.java
@@ -7,10 +7,12 @@
  */
 package io.camunda.it.rdbms.db.sequenceflow;
 
+import static io.camunda.it.rdbms.db.fixtures.SequenceFlowFixtures.createAndSaveRandomSequenceFlow;
+import static io.camunda.it.rdbms.db.fixtures.SequenceFlowFixtures.createAndSaveRandomSequenceFlows;
+import static io.camunda.it.rdbms.db.fixtures.SequenceFlowFixtures.createRandomized;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.camunda.db.rdbms.write.domain.SequenceFlowDbModel;
-import io.camunda.db.rdbms.write.domain.SequenceFlowDbModel.Builder;
+import io.camunda.it.rdbms.db.fixtures.CommonFixtures;
 import io.camunda.it.rdbms.db.util.CamundaRdbmsInvocationContextProviderExtension;
 import io.camunda.it.rdbms.db.util.CamundaRdbmsTestApplication;
 import io.camunda.search.entities.SequenceFlowEntity;
@@ -33,35 +35,26 @@ public class SequenceFlowIT {
     final var rdbmsService = testApplication.getRdbmsService();
     final var rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
     final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
-    final var sequenceFlowWriter = rdbmsWriter.getSequenceFlowWriter();
 
     // when
-    sequenceFlowWriter.create(
-        new SequenceFlowDbModel.Builder()
-            .flowNodeId("node1")
-            .processInstanceKey(1L)
-            .processDefinitionKey(11L)
-            .processDefinitionId("bpmn1")
-            .tenantId("tenant1")
-            .partitionId(PARTITION_ID.intValue())
-            .historyCleanupDate(NOW)
-            .build());
-    rdbmsWriter.flush();
+    final var sequenceFlow = createAndSaveRandomSequenceFlow(rdbmsWriter, b -> b);
 
     // then
     final var items =
         sequenceFlowReader
-            .search(SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(1L))))
+            .search(
+                SequenceFlowQuery.of(
+                    q -> q.filter(f -> f.processInstanceKey(sequenceFlow.processInstanceKey()))))
             .items();
     assertThat(items)
         .containsExactly(
             new SequenceFlowEntity.Builder()
-                .sequenceFlowId("1_node1")
-                .flowNodeId("node1")
-                .processInstanceKey(1L)
-                .processDefinitionKey(11L)
-                .processDefinitionId("bpmn1")
-                .tenantId("tenant1")
+                .sequenceFlowId(sequenceFlow.processInstanceKey() + "_" + sequenceFlow.flowNodeId())
+                .flowNodeId(sequenceFlow.flowNodeId())
+                .processInstanceKey(sequenceFlow.processInstanceKey())
+                .processDefinitionKey(sequenceFlow.processDefinitionKey())
+                .processDefinitionId(sequenceFlow.processDefinitionId())
+                .tenantId(sequenceFlow.tenantId())
                 .build());
   }
 
@@ -71,46 +64,77 @@ public class SequenceFlowIT {
     final var rdbmsService = testApplication.getRdbmsService();
     final var rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
     final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
-    final var sequenceFlowWriter = rdbmsWriter.getSequenceFlowWriter();
-    sequenceFlowWriter.create(
-        new Builder()
-            .flowNodeId("node21")
-            .processInstanceKey(21L)
-            .processDefinitionKey(11L)
-            .processDefinitionId("bpmn21")
-            .tenantId("tenant1")
-            .partitionId(PARTITION_ID.intValue())
-            .historyCleanupDate(NOW)
-            .build());
-    sequenceFlowWriter.create(
-        new Builder()
-            .flowNodeId("node22")
-            .processInstanceKey(22L)
-            .processDefinitionKey(11L)
-            .processDefinitionId("bpmn22")
-            .tenantId("tenant1")
-            .partitionId(PARTITION_ID.intValue())
-            .historyCleanupDate(NOW)
-            .build());
-    rdbmsWriter.flush();
+
+    final var sequenceFlow = createAndSaveRandomSequenceFlow(rdbmsWriter, b -> b);
+    createAndSaveRandomSequenceFlows(rdbmsWriter);
 
     // when
     final var actual =
         sequenceFlowReader
-            .search(SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(22L))))
+            .search(
+                SequenceFlowQuery.of(
+                    q -> q.filter(f -> f.processInstanceKey(sequenceFlow.processInstanceKey()))))
             .items();
 
     // then
     assertThat(actual)
         .containsExactly(
             new SequenceFlowEntity.Builder()
-                .sequenceFlowId("22_node22")
-                .flowNodeId("node22")
-                .processInstanceKey(22L)
-                .processDefinitionKey(11L)
-                .processDefinitionId("bpmn22")
-                .tenantId("tenant1")
+                .sequenceFlowId(sequenceFlow.processInstanceKey() + "_" + sequenceFlow.flowNodeId())
+                .flowNodeId(sequenceFlow.flowNodeId())
+                .processInstanceKey(sequenceFlow.processInstanceKey())
+                .processDefinitionKey(sequenceFlow.processDefinitionKey())
+                .processDefinitionId(sequenceFlow.processDefinitionId())
+                .tenantId(sequenceFlow.tenantId())
                 .build());
+  }
+
+  @TestTemplate
+  public void shouldFindSequenceFlowByAuthorizedResourceId(
+      final CamundaRdbmsTestApplication testApplication) {
+    // given
+    final var rdbmsService = testApplication.getRdbmsService();
+    final var rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
+    final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
+
+    final var sequenceFlow =
+        createAndSaveRandomSequenceFlow(rdbmsWriter, b -> b.processInstanceKey(42L));
+    createAndSaveRandomSequenceFlows(rdbmsWriter, b -> b.processInstanceKey(42L));
+
+    // when
+    final var searchResult =
+        sequenceFlowReader.search(
+            SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(42L))),
+            CommonFixtures.resourceAccessChecksFromResourceIds(sequenceFlow.processDefinitionId()));
+
+    assertThat(searchResult.total()).isEqualTo(1);
+    assertThat(searchResult.items()).hasSize(1);
+    assertThat(searchResult.items().getFirst().processInstanceKey())
+        .isEqualTo(sequenceFlow.processInstanceKey());
+  }
+
+  @TestTemplate
+  public void shouldFindSequenceFlowByAuthorizedTenantId(
+      final CamundaRdbmsTestApplication testApplication) {
+    // given
+    final var rdbmsService = testApplication.getRdbmsService();
+    final var rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
+    final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
+
+    final var sequenceFlow =
+        createAndSaveRandomSequenceFlow(rdbmsWriter, b -> b.processInstanceKey(42L));
+    createAndSaveRandomSequenceFlows(rdbmsWriter, b -> b.processInstanceKey(42L));
+
+    // when
+    final var searchResult =
+        sequenceFlowReader.search(
+            SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(42L))),
+            CommonFixtures.resourceAccessChecksFromTenantIds(sequenceFlow.tenantId()));
+
+    assertThat(searchResult.total()).isEqualTo(1);
+    assertThat(searchResult.items()).hasSize(1);
+    assertThat(searchResult.items().getFirst().processInstanceKey())
+        .isEqualTo(sequenceFlow.processInstanceKey());
   }
 
   @TestTemplate
@@ -122,34 +146,19 @@ public class SequenceFlowIT {
     final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
     final var sequenceFlowWriter = rdbmsWriter.getSequenceFlowWriter();
 
+    final var sequenceFlow = createRandomized(b -> b);
     // when
-    sequenceFlowWriter.createIfNotExists(
-        new SequenceFlowDbModel.Builder()
-            .flowNodeId("node3")
-            .processInstanceKey(3L)
-            .processDefinitionKey(11L)
-            .processDefinitionId("bpmn3")
-            .tenantId("tenant1")
-            .partitionId(PARTITION_ID.intValue())
-            .historyCleanupDate(NOW)
-            .build());
+    sequenceFlowWriter.createIfNotExists(sequenceFlow);
     rdbmsWriter.flush();
 
     // then
     final var items =
         sequenceFlowReader
-            .search(SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(3L))))
+            .search(
+                SequenceFlowQuery.of(
+                    q -> q.filter(f -> f.processInstanceKey(sequenceFlow.processInstanceKey()))))
             .items();
-    assertThat(items)
-        .containsExactly(
-            new SequenceFlowEntity.Builder()
-                .sequenceFlowId("3_node3")
-                .flowNodeId("node3")
-                .processInstanceKey(3L)
-                .processDefinitionKey(11L)
-                .processDefinitionId("bpmn3")
-                .tenantId("tenant1")
-                .build());
+    assertThat(items.getFirst().processInstanceKey()).isEqualTo(sequenceFlow.processInstanceKey());
   }
 
   @TestTemplate
@@ -160,16 +169,7 @@ public class SequenceFlowIT {
     final var rdbmsWriter = rdbmsService.createWriter(PARTITION_ID);
     final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
     final var sequenceFlowWriter = rdbmsWriter.getSequenceFlowWriter();
-    final var dbModel =
-        new Builder()
-            .flowNodeId("node4")
-            .processInstanceKey(4L)
-            .processDefinitionKey(11L)
-            .processDefinitionId("bpmn4")
-            .tenantId("tenant1")
-            .partitionId(PARTITION_ID.intValue())
-            .historyCleanupDate(NOW)
-            .build();
+    final var dbModel = createRandomized(b -> b);
     sequenceFlowWriter.create(dbModel);
     rdbmsWriter.flush();
 
@@ -180,18 +180,11 @@ public class SequenceFlowIT {
     // then
     final var items =
         sequenceFlowReader
-            .search(SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(4L))))
+            .search(
+                SequenceFlowQuery.of(
+                    q -> q.filter(f -> f.processInstanceKey(dbModel.processInstanceKey()))))
             .items();
-    assertThat(items)
-        .containsExactly(
-            new SequenceFlowEntity.Builder()
-                .sequenceFlowId("4_node4")
-                .flowNodeId("node4")
-                .processInstanceKey(4L)
-                .processDefinitionKey(11L)
-                .processDefinitionId("bpmn4")
-                .tenantId("tenant1")
-                .build());
+    assertThat(items.getFirst().processInstanceKey()).isEqualTo(dbModel.processInstanceKey());
   }
 
   @TestTemplate
@@ -202,22 +195,15 @@ public class SequenceFlowIT {
     final var sequenceFlowReader = rdbmsService.getSequenceFlowReader();
     final var sequenceFlowWriter = rdbmsWriter.getSequenceFlowWriter();
 
-    final var dbModel =
-        new Builder()
-            .flowNodeId("nodeDel")
-            .processInstanceKey(99L)
-            .processDefinitionKey(9L)
-            .processDefinitionId("bpmnDel")
-            .tenantId("tenantX")
-            .partitionId(PARTITION_ID.intValue())
-            .historyCleanupDate(NOW)
-            .build();
+    final var dbModel = createRandomized(b -> b);
     sequenceFlowWriter.create(dbModel);
     rdbmsWriter.flush();
 
     assertThat(
             sequenceFlowReader
-                .search(SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(99L))))
+                .search(
+                    SequenceFlowQuery.of(
+                        q -> q.filter(f -> f.processInstanceKey(dbModel.processInstanceKey()))))
                 .items())
         .isNotEmpty();
 
@@ -228,7 +214,9 @@ public class SequenceFlowIT {
     // then
     final var itemsAfterDelete =
         sequenceFlowReader
-            .search(SequenceFlowQuery.of(q -> q.filter(f -> f.processInstanceKey(99L))))
+            .search(
+                SequenceFlowQuery.of(
+                    q -> q.filter(f -> f.processInstanceKey(dbModel.processInstanceKey()))))
             .items();
     assertThat(itemsAfterDelete).isEmpty();
   }
