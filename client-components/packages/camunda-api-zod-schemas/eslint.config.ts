b413db0856de5698c4623bcd4ff33279c9dc04d0
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-08-21 15:28:40 +0200

    refactor: move eslint to sub-package

--- client-components/packages/camunda-api-zod-schemas/eslint.config.ts
+++ client-components/packages/camunda-api-zod-schemas/eslint.config.ts
@@ -10,6 +10,9 @@ export default defineConfig([
 		extends: ['js/recommended'],
 		languageOptions: {
 			globals: globals.browser,
+			parserOptions: {
+				tsconfigRootDir: import.meta.dirname,
+			},
 		},
 	},
 	tseslint.configs.recommended,
