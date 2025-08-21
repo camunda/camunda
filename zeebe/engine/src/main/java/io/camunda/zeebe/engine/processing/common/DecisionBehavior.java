4db2d29b30ff4692d2b77acca84929cb3e6a52f4
Nicola Puppa <nicola.puppa@camunda.com>
2025-08-21 11:20:53 +0200

    refactor: extract the generation og decisionEvaluationInstanceKey into a separate generation class

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/common/DecisionBehavior.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/common/DecisionBehavior.java
@@ -153,21 +153,17 @@ public class DecisionBehavior {
                     persistedDecision -> bufferAsString(persistedDecision.getDecisionId()),
                     DecisionInfo::new));
 
-    final var evaluatedDecisions = decisionResult.getEvaluatedDecisions();
-    for (int index = 0; index < evaluatedDecisions.size(); index++) {
-      final EvaluatedDecision evaluatedDecision = evaluatedDecisions.get(index);
-      // The decisionEvaluationInstanceKey is used to uniquely identify the evaluation of a
-      // decision within a decision evaluation event. It is constructed by appending the decision
-      // evaluation key to the index of the evaluated decision (starting from 1).
-      final var decisionEvaluationInstanceKey =
-          String.format("%d-%d", decisionEvaluationKey, index + 1);
-      addDecisionToEvaluationEvent(
-          evaluatedDecision,
-          decisionKeysByDecisionId.getOrDefault(
-              evaluatedDecision.decisionId(), UNKNOWN_DECISION_INFO),
-          decisionEvaluationEvent,
-          decisionEvaluationInstanceKey);
-    }
+    final var generator = new DecisionEvaluationInstanceKeyGenerator(decisionEvaluationKey);
+    decisionResult
+        .getEvaluatedDecisions()
+        .forEach(
+            evaluatedDecision ->
+                addDecisionToEvaluationEvent(
+                    evaluatedDecision,
+                    decisionKeysByDecisionId.getOrDefault(
+                        evaluatedDecision.decisionId(), UNKNOWN_DECISION_INFO),
+                    decisionEvaluationEvent,
+                    generator.next()));
 
     final DecisionEvaluationIntent decisionEvaluationIntent;
     if (decisionResult.isFailure()) {
