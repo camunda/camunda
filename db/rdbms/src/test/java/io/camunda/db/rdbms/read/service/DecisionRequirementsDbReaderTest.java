3e7ec939f7130ff5f957a9e9beb0228a80485e3a
Christian Thiel <christian.thiel@holisticon.de>
2025-08-22 09:54:33 +0200

    feat: Add auth checks to decision requirements entity #28725
    Co-authored-by: Stefan Zilske <stefan.zilske@holisticon.de>

---
+++ db/rdbms/src/test/java/io/camunda/db/rdbms/read/service/DecisionRequirementsDbReaderTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.db.rdbms.read.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import io.camunda.db.rdbms.sql.DecisionRequirementsMapper;
+import io.camunda.search.query.DecisionRequirementsQuery;
+import io.camunda.security.auth.Authorization;
+import io.camunda.security.reader.AuthorizationCheck;
+import io.camunda.security.reader.ResourceAccessChecks;
+import io.camunda.security.reader.TenantCheck;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class DecisionRequirementsDbReaderTest {
+  private final DecisionRequirementsMapper decisionRequirementsMapper =
+      mock(DecisionRequirementsMapper.class);
+  private final DecisionRequirementsDbReader decisionRequirementsDbReader =
+      new DecisionRequirementsDbReader(decisionRequirementsMapper);
+
+  @Test
+  void shouldReturnEmptyResultWhenAuthorizedResourceIdsIsNull() {
+    final DecisionRequirementsQuery query = DecisionRequirementsQuery.of(b -> b);
+    final ResourceAccessChecks resourceAccessChecks =
+        ResourceAccessChecks.of(
+            AuthorizationCheck.enabled(
+                Authorization.of(a -> a.decisionRequirementsDefinition().read())),
+            TenantCheck.disabled());
+
+    final var items = decisionRequirementsDbReader.search(query, resourceAccessChecks).items();
+    assertThat(items).isEmpty();
+  }
+
+  @Test
+  void shouldReturnEmptyListWhenAuthorizedTenantIdsIsNull() {
+    final DecisionRequirementsQuery query = DecisionRequirementsQuery.of(b -> b);
+    final ResourceAccessChecks resourceAccessChecks =
+        ResourceAccessChecks.of(AuthorizationCheck.disabled(), TenantCheck.enabled(List.of()));
+
+    final var items = decisionRequirementsDbReader.search(query, resourceAccessChecks).items();
+    assertThat(items).isEmpty();
+  }
+}
