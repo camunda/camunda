c183606ae67a34f6ff19feccbf55a3bbc1e94512
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-08-23 17:40:15 +0200

    refactor: do not print out record in error responses

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingStateMachine.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingStateMachine.java
@@ -15,6 +15,7 @@ import io.camunda.zeebe.logstreams.log.LogStreamReader;
 import io.camunda.zeebe.logstreams.log.LogStreamWriter;
 import io.camunda.zeebe.logstreams.log.LoggedEvent;
 import io.camunda.zeebe.logstreams.log.WriteContext;
+import io.camunda.zeebe.protocol.Protocol;
 import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
 import io.camunda.zeebe.protocol.impl.record.value.error.ErrorRecord;
 import io.camunda.zeebe.protocol.record.RecordType;
@@ -463,20 +464,26 @@ public final class ProcessingStateMachine {
   }
 
   private boolean tryExitOutOfErrorLoop(final Throwable error) {
+    final var rejectionReason =
+        """
+            Expected to process command %d (%s.%s), but caught an exception. Check broker logs \
+            (partition %s) for details, or ask your operator to do so."""
+            .formatted(
+                Protocol.encodePartitionId(context.getPartitionId(), currentRecord.getKey()),
+                metadata.getValueType(),
+                metadata.getIntent(),
+                context.getPartitionId());
     try {
       // If in error loop and the processing record is a user command
       if (errorHandlingPhase == ErrorHandlingPhase.USER_COMMAND_PROCESSING_ERROR_FAILED) {
         // First try to reject with proper error message
         LOG.debug(ERROR_MESSAGE_HANDLING_PROCESSING_ERROR_FAILED, currentRecord, metadata, error);
-        tryRejectingIfUserCommand(error.getMessage());
+        tryRejectingIfUserCommand(rejectionReason);
         return true;
       } else if (errorHandlingPhase == ErrorHandlingPhase.USER_COMMAND_REJECT_FAILED) {
         LOG.warn(ERROR_MESSAGE_HANDLING_PROCESSING_ERROR_FAILED, currentRecord, metadata, error);
         // try to reject with a generic error message
-        tryRejectingIfUserCommand(
-            String.format(
-                "Expected to process command, but caught an exception. Check broker logs (partition %s) for details.",
-                context.getPartitionId()));
+        tryRejectingIfUserCommand(rejectionReason);
         return true;
       }
     } catch (final Exception e) {
