4fd4adc02f3d8e9bdce5ee113c1c67bf0d64d3dd
Houssain Barouni <houssain.barouni@camunda.com>
2025-08-25 17:25:25 +0200

    fix: Reduce the noise in schema-manager logs for nominal cases

--- schema-manager/src/main/java/io/camunda/search/schema/opensearch/OpensearchEngineClient.java
+++ schema-manager/src/main/java/io/camunda/search/schema/opensearch/OpensearchEngineClient.java
@@ -253,7 +253,7 @@ public class OpensearchEngineClient implements SearchEngineClient {
           String.format(
               "Failed to search documents in the import position index for partition [%s]",
               partitionId);
-      LOG.error(errMsg, e);
+      LOG.warn(errMsg, e);
       return false;
     }
   }
