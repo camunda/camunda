1bbf6b7a9ab30a8c51fffeb302826bce0897c590
volodymyr-melnykc <volodymyr.melnyk@camunda.com>
2025-08-25 13:30:36 +0200

    feat: Add missing links to documentation in Identity UI

--- identity/client/src/pages/roles/detail/members/index.tsx
+++ identity/client/src/pages/roles/detail/members/index.tsx
@@ -68,7 +68,7 @@ const Members: FC<MembersProps> = ({ roleId }) => {
           childResourceTypeTranslationKey={"user"}
           parentResourceTypeTranslationKey={"role"}
           handleClick={openAssignModal}
-          docsLinkPath=""
+          docsLinkPath="/docs/next/components/identity/user/"
         />
         {assignUsersModal}
       </>
