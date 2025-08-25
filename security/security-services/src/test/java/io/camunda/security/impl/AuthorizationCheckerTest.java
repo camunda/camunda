cd80320c5e77aa6936e1bab36089b6be749b85e9
Nico Korthout <nico.korthout@camunda.com>
2025-08-25 15:13:31 +0200

    test: check isAuthorized edge cases
    This adds a unit test for AuthorizationChecker that verifies the correct
    functioning of the isAuthorized method.
    
    It uses parameterized tests for scenarios with anonymous users and
    authenticated users, clients and mapping rules, where it considers
    whether they can access a resource. Cases cover all reasonable
    combinations of authentication, authorization, and resource to access.
    An expected enum is used to easily clarify the expected outcome: Access
    allowed, Access denied. Some scenarios overlap, but that's fine as this
    test is cheap to maintain.
    
    The AuthorizationChecker depends AuthorizationReader for which a fake
    implementation is provided. This is done rather than using Mockito to
    mock it, as using a fake leads to tests that are less dependent on the
    implementation of AuthorizationChecker and how it interacts with
    AuthorizationReader, but rather depends on a basic implementation of the
    reader. These tests thus run fast and are cheap to maintain (they don't
    depend on the implementation but on the interface).
    
    The fake reader is implemented as an in-memory list of authorizations
    that can be created so they can be accessed using getByKey and search.
    The search is implemented as simple as possible, only considering filter
    options that are not null. The fake is closed after all tests (clearing
    the stored authorizations), which is not strictly necessary. However, I
    felt this highlighted how this data store is available throughout all
    tests.
    
    Note that an alternative way to test the AuthorizationChecker would have
    been to set up Elasticsearch using Testcontainers. However, I found this
    to be slow and brittle. The current tests using the fake reader run in a
    fraction of a second instead.

--- security/security-services/src/test/java/io/camunda/security/impl/AuthorizationCheckerTest.java
+++ security/security-services/src/test/java/io/camunda/security/impl/AuthorizationCheckerTest.java
@@ -13,12 +13,29 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import io.camunda.search.clients.reader.AuthorizationReader;
+import io.camunda.search.entities.AuthorizationEntity;
 import io.camunda.security.auth.Authorization;
 import io.camunda.security.auth.CamundaAuthentication;
 import io.camunda.security.auth.SecurityContext;
+import io.camunda.zeebe.protocol.Protocol;
+import io.camunda.zeebe.protocol.record.value.AuthorizationOwnerType;
+import io.camunda.zeebe.protocol.record.value.AuthorizationResourceMatcher;
+import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
 import io.camunda.zeebe.protocol.record.value.AuthorizationScope;
+import io.camunda.zeebe.protocol.record.value.PermissionType;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 
 public class AuthorizationCheckerTest {
@@ -75,5 +92,348 @@ public class AuthorizationCheckerTest {
 
     // then
     assertThat(result).isFalse();
+  }
+
+  @Nested
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  class IsAuthorizedTests {
+
+    public final String WILDCARD_RESOURCE_ID = AuthorizationScope.WILDCARD.getResourceId();
+    public final String AUTHORIZED_USERNAME =
+        io.camunda.zeebe.auth.Authorization.AUTHORIZED_USERNAME;
+
+    private static final String RESOURCE_ID = "id";
+
+    private AuthorizationChecker authorizationChecker;
+    private FakeAuthorizationReader authorizationReader;
+
+    @BeforeAll
+    void beforeAll() {
+      authorizationReader = new FakeAuthorizationReader();
+      authorizationChecker = new AuthorizationChecker(authorizationReader);
+      // user based authorizations
+      authorizationReader.create(
+          new AuthorizationEntity(
+              Protocol.encodePartitionId(1, 1),
+              "userWithWildcard",
+              AuthorizationOwnerType.USER.name(),
+              AuthorizationResourceType.PROCESS_DEFINITION.name(),
+              AuthorizationResourceMatcher.ANY.value(),
+              WILDCARD_RESOURCE_ID,
+              Set.of(PermissionType.READ_PROCESS_INSTANCE)));
+      authorizationReader.create(
+          new AuthorizationEntity(
+              Protocol.encodePartitionId(1, 2),
+              "userWithResourceId",
+              AuthorizationOwnerType.USER.name(),
+              AuthorizationResourceType.PROCESS_DEFINITION.name(),
+              AuthorizationResourceMatcher.ID.value(),
+              RESOURCE_ID,
+              Set.of(PermissionType.READ_PROCESS_INSTANCE)));
+      // client based authorizations
+      authorizationReader.create(
+          new AuthorizationEntity(
+              Protocol.encodePartitionId(1, 3),
+              "clientWildcard",
+              AuthorizationOwnerType.CLIENT.name(),
+              AuthorizationResourceType.PROCESS_DEFINITION.name(),
+              AuthorizationResourceMatcher.ANY.value(),
+              WILDCARD_RESOURCE_ID,
+              Set.of(PermissionType.READ_PROCESS_INSTANCE)));
+      authorizationReader.create(
+          new AuthorizationEntity(
+              Protocol.encodePartitionId(1, 4),
+              "clientWithResourceId",
+              AuthorizationOwnerType.CLIENT.name(),
+              AuthorizationResourceType.PROCESS_DEFINITION.name(),
+              AuthorizationResourceMatcher.ID.value(),
+              RESOURCE_ID,
+              Set.of(PermissionType.READ_PROCESS_INSTANCE)));
+      // mapping rule based authorizations
+      authorizationReader.create(
+          new AuthorizationEntity(
+              Protocol.encodePartitionId(1, 5),
+              "mrWildcard",
+              AuthorizationOwnerType.MAPPING_RULE.name(),
+              AuthorizationResourceType.PROCESS_DEFINITION.name(),
+              AuthorizationResourceMatcher.ANY.value(),
+              WILDCARD_RESOURCE_ID,
+              Set.of(PermissionType.READ_PROCESS_INSTANCE)));
+      authorizationReader.create(
+          new AuthorizationEntity(
+              Protocol.encodePartitionId(1, 6),
+              "mrWithResourceId",
+              AuthorizationOwnerType.MAPPING_RULE.name(),
+              AuthorizationResourceType.PROCESS_DEFINITION.name(),
+              AuthorizationResourceMatcher.ID.value(),
+              RESOURCE_ID,
+              Set.of(PermissionType.READ_PROCESS_INSTANCE)));
+    }
+
+    @AfterAll
+    void afterAll() {
+      authorizationReader.close();
+    }
+
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("anonymousAuthorizationScenarios")
+    void anonymousAuthorizationScenariosTest(
+        final String displayName,
+        final CamundaAuthentication authentication,
+        final Authorization<?> authorization,
+        final AuthorizationScope attemptedScope,
+        final Expected expected) {
+      // given
+      final var securityContext =
+          SecurityContext.of(
+              c -> c.withAuthentication(authentication).withAuthorization(authorization));
+
+      // when
+      final boolean isAuthorized =
+          authorizationChecker.isAuthorized(attemptedScope, securityContext);
+
+      // then
+      Assertions.assertThat(isAuthorized)
+          .describedAs(displayName)
+          .isEqualTo(expected == Expected.ACCESS_ALLOWED);
+    }
+
+    Stream<Arguments> anonymousAuthorizationScenarios() {
+      return Stream.of(
+          Arguments.of(
+              "anonymous ID:id accessing id -> denied",
+              CamundaAuthentication.anonymous(),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_DENIED),
+          Arguments.of(
+              "anonymous ID:id accessing * -> denied",
+              CamundaAuthentication.anonymous(),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.WILDCARD,
+              Expected.ACCESS_DENIED),
+          Arguments.of(
+              "anonymous ANY:* accessing id -> denied",
+              CamundaAuthentication.anonymous(),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_DENIED),
+          Arguments.of(
+              "anonymous ANY:* accessing * -> denied",
+              CamundaAuthentication.anonymous(),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.WILDCARD,
+              Expected.ACCESS_DENIED));
+    }
+
+    @ParameterizedTest(name = "{index} {0}")
+    @MethodSource("authenticatedAuthorizationScenarios")
+    void authenticatedAuthorizationScenariosTest(
+        final String displayName,
+        final CamundaAuthentication authentication,
+        final Authorization<?> authorization,
+        final AuthorizationScope attemptedScope,
+        final Expected expected) {
+      // given
+      final var securityContext =
+          SecurityContext.of(
+              c -> c.withAuthentication(authentication).withAuthorization(authorization));
+
+      // when
+      final boolean isAuthorized =
+          authorizationChecker.isAuthorized(attemptedScope, securityContext);
+
+      // then
+      Assertions.assertThat(isAuthorized)
+          .describedAs(displayName)
+          .isEqualTo(expected == Expected.ACCESS_ALLOWED);
+    }
+
+    Stream<Arguments> authenticatedAuthorizationScenarios() {
+      return Stream.of(
+          // user based
+          Arguments.of(
+              "auth userWithWildcard ANY:* accessing * -> allowed",
+              CamundaAuthentication.of(
+                  authorization ->
+                      authorization
+                          .user("userWithWildcard")
+                          .claims(Map.of(AUTHORIZED_USERNAME, "userWithWildcard"))),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.WILDCARD,
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth userWithWildcard ANY:* accessing id -> allowed",
+              CamundaAuthentication.of(
+                  authorization ->
+                      authorization
+                          .user("userWithWildcard")
+                          .claims(Map.of(AUTHORIZED_USERNAME, "userWithWildcard"))),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth userWithResourceId ID:id accessing id -> allowed",
+              CamundaAuthentication.of(
+                  authorization ->
+                      authorization
+                          .user("userWithResourceId")
+                          .claims(Map.of(AUTHORIZED_USERNAME, "userWithResourceId"))),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth userWithResourceId ID:id accessing different id -> denied",
+              CamundaAuthentication.of(
+                  authorization ->
+                      authorization
+                          .user("userWithResourceId")
+                          .claims(Map.of(AUTHORIZED_USERNAME, "userWithResourceId"))),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id("a_different_id"),
+              Expected.ACCESS_DENIED),
+          Arguments.of(
+              "auth userWithResourceId ID:id accessing * -> denied",
+              CamundaAuthentication.of(
+                  authorization ->
+                      authorization
+                          .user("userWithResourceId")
+                          .claims(Map.of(AUTHORIZED_USERNAME, "userWithResourceId"))),
+              Authorization.of(
+                  authorization ->
+                      authorization
+                          .resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.WILDCARD,
+              Expected.ACCESS_DENIED),
+          // client based
+          Arguments.of(
+              "auth clientWildcard ANY:* accessing * -> allowed",
+              CamundaAuthentication.of(a -> a.clientId("clientWildcard")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.WILDCARD,
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth clientWildcard ANY:* accessing id -> allowed",
+              CamundaAuthentication.of(a -> a.clientId("clientWildcard")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth clientWithResourceId ID:id accessing id -> allowed",
+              CamundaAuthentication.of(a -> a.clientId("clientWithResourceId")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth clientWithResourceId ID:id accessing other id -> denied",
+              CamundaAuthentication.of(a -> a.clientId("clientWithResourceId")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id("different"),
+              Expected.ACCESS_DENIED),
+          // mapping rule based
+          Arguments.of(
+              "auth mapping rule mrWildcard ANY:* accessing * -> allowed",
+              CamundaAuthentication.of(a -> a.mappingRule("mrWildcard")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.WILDCARD,
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth mapping rule mrWildcard ANY:* accessing id -> allowed",
+              CamundaAuthentication.of(a -> a.mappingRule("mrWildcard")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(WILDCARD_RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth mapping rule mrWithResourceId ID:id accessing id -> allowed",
+              CamundaAuthentication.of(a -> a.mappingRule("mrWithResourceId")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id(RESOURCE_ID),
+              Expected.ACCESS_ALLOWED),
+          Arguments.of(
+              "auth mapping rule mrWithResourceId ID:id accessing other id -> denied",
+              CamundaAuthentication.of(a -> a.mappingRule("mrWithResourceId")),
+              Authorization.of(
+                  a ->
+                      a.resourceType(AuthorizationResourceType.PROCESS_DEFINITION)
+                          .permissionType(PermissionType.READ_PROCESS_INSTANCE)
+                          .resourceId(RESOURCE_ID)),
+              AuthorizationScope.id("different"),
+              Expected.ACCESS_DENIED));
+    }
+
+    private enum Expected {
+      ACCESS_ALLOWED,
+      ACCESS_DENIED
+    }
   }
 }
