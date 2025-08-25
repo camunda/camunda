10d34886cedab9bcb6b10b24e733b327627ec14c
esraagamal6 <esraa-gamal@hotmail.com>
2025-08-25 08:48:41 +0200

    fix: fix test runs on 8.7

--- qa/c8-orchestration-cluster-e2e-test-suite/utils/zeebeClient.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/utils/zeebeClient.ts
@@ -7,10 +7,16 @@
  */
 
 import {Camunda8} from '@camunda8/sdk';
-import {JSONDoc} from '@camunda8/sdk/dist/zeebe/types';
+import {JSONDoc} from '@camunda8/sdk/dist/zeebe/types.js';
 
 const c8 = new Camunda8({
-  CAMUNDA_AUTH_STRATEGY: process.env.CAMUNDA_AUTH_STRATEGY,
+  CAMUNDA_AUTH_STRATEGY: process.env.CAMUNDA_AUTH_STRATEGY as
+    | 'BASIC'
+    | 'OAUTH'
+    | 'BEARER'
+    | 'COOKIE'
+    | 'NONE'
+    | undefined,
   CAMUNDA_BASIC_AUTH_USERNAME: process.env.CAMUNDA_BASIC_AUTH_USERNAME,
   CAMUNDA_BASIC_AUTH_PASSWORD: process.env.CAMUNDA_BASIC_AUTH_PASSWORD,
   ZEEBE_REST_ADDRESS: process.env.ZEEBE_REST_ADDRESS,
