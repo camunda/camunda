520748607cc99792bfe6b9dcd013b325121420bb
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2025-08-27 10:34:31 +0200

    feat: temp enable actor metrics

--- zeebe/util/src/main/java/io/camunda/zeebe/util/FeatureFlags.java
+++ zeebe/util/src/main/java/io/camunda/zeebe/util/FeatureFlags.java
@@ -47,7 +47,7 @@ public record FeatureFlags(
   //  protected static final boolean FOO_DEFAULT = false;
 
   private static final boolean YIELDING_DUE_DATE_CHECKER = true;
-  private static final boolean ENABLE_ACTOR_METRICS = false;
+  private static final boolean ENABLE_ACTOR_METRICS = true;
 
   private static final boolean ENABLE_MSG_TTL_CHECKER_ASYNC = false;
   private static final boolean ENABLE_DUE_DATE_CHECKER_ASYNC = false;
