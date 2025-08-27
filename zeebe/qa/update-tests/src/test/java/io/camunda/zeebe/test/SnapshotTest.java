f669d7a23f3b0c831f6a96134027384093cc983d
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2025-08-27 20:02:44 +0300

    test: force all partitions to snapshot after update

--- zeebe/qa/update-tests/src/test/java/io/camunda/zeebe/test/SnapshotTest.java
+++ zeebe/qa/update-tests/src/test/java/io/camunda/zeebe/test/SnapshotTest.java
@@ -10,8 +10,14 @@ package io.camunda.zeebe.test;
 import static io.camunda.zeebe.test.ContainerStateAssert.assertThat;
 import static io.camunda.zeebe.test.UpdateTestCaseProvider.PROCESS_ID;
 
+import io.camunda.zeebe.protocol.Protocol;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -64,25 +70,54 @@ final class SnapshotTest {
   void takesSnapshotAfterUpdate(final ContainerState state) {
     // given - we take an initial snapshot on the old version
     state.withNetwork(network).withOldBroker().start(true);
-    state
-        .client()
-        .newPublishMessageCommand()
-        .messageName("test")
-        .correlationKey("key")
-        .send()
-        .join();
+    activateAllPartitions(state);
     state.getPartitionsActuator().takeSnapshot();
-    assertThat(state).eventuallyHasSnapshotAvailable(1);
-    final var oldSnapshotId = state.getPartitionsActuator().query().get(1).snapshotId();
+
+    for (int i = 1; i <= ContainerState.PARTITION_COUNT; i++) {
+      assertThat(state).eventuallyHasSnapshotAvailable(i);
+    }
+    final Map<Integer, String> oldSnapshotIds = getPartitionSnapshotIds(state);
 
     // when - we update to the new version without any new processing
     state.close();
     state.withNewBroker().start(true);
 
     // then - the new version takes a new snapshot to persist migrations and version marker
-    Awaitility.await()
+    for (int i = 1; i <= ContainerState.PARTITION_COUNT; i++) {
+      assertThat(state).eventuallyHasNewSnapshotAvailable(i, oldSnapshotIds.get(i));
+    }
+  }
+
+  private void activateAllPartitions(final ContainerState state) {
+    final var partitionIdsActivated = new HashSet<>();
+    final AtomicInteger correlationKey = new AtomicInteger(0);
+    Awaitility.await("Await all partitions to be activated")
+        .atMost(Duration.ofSeconds(30))
+        .ignoreExceptions()
+        .pollInterval(Duration.ofSeconds(1))
         .until(
-            () -> state.getPartitionsActuator().query().get(1).snapshotId(),
-            newSnapshotId -> !newSnapshotId.equals(oldSnapshotId));
+            () -> {
+              final var messageKey =
+                  state
+                      .client()
+                      .newPublishMessageCommand()
+                      .messageName("test")
+                      .correlationKey(String.valueOf(correlationKey.incrementAndGet()))
+                      .send()
+                      .join()
+                      .getMessageKey();
+              partitionIdsActivated.add(Protocol.decodePartitionId(messageKey));
+              return partitionIdsActivated;
+            },
+            (partitionsActivated) -> partitionsActivated.size() == ContainerState.PARTITION_COUNT);
+  }
+
+  private Map<Integer, String> getPartitionSnapshotIds(final ContainerState state) {
+    final var partitionStatuses = state.getPartitionsActuator().query();
+    return partitionStatuses.keySet().stream()
+        .collect(
+            HashMap::new,
+            (m, partitionId) -> m.put(partitionId, partitionStatuses.get(partitionId).snapshotId()),
+            HashMap::putAll);
   }
 }
