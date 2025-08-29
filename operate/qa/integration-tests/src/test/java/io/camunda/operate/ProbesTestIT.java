68f5471acb4c4c4712555e887c847cc577eee6b5
Mattia Buccarella <mattia.buccarella@camunda.com>
2025-08-29 18:01:19 +0200

    fix: ProbesTest for operate and tasklist

--- operate/qa/integration-tests/src/test/java/io/camunda/operate/ProbesTestIT.java
+++ operate/qa/integration-tests/src/test/java/io/camunda/operate/ProbesTestIT.java
@@ -13,7 +13,9 @@ import io.camunda.operate.management.IndicesCheck;
 import io.camunda.operate.qa.util.TestSchemaManager;
 import io.camunda.operate.util.TestApplication;
 import io.camunda.operate.util.TestUtil;
+import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,8 +26,16 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 class ProbesTestIT {
-  private static final String INDEX_PREFIX_CONFIG =
-      "camunda.data.secondary-storage.elasticsearch.index-prefix";
+
+  private static String indexPrefixConfig;
+
+  @BeforeAll
+  public static void init() {
+    final String dbType =
+        (Optional.ofNullable("camunda.data.secondary-storage.type").orElse("elasticsearch"))
+            .toLowerCase();
+    indexPrefixConfig = "camunda.data.secondary-storage." + dbType + ".index-prefix";
+  }
 
   @Nested
   @ExtendWith(SpringExtension.class)
@@ -41,7 +51,7 @@ class ProbesTestIT {
 
     @DynamicPropertySource
     static void setProperties(final DynamicPropertyRegistry registry) {
-      registry.add(INDEX_PREFIX_CONFIG, () -> TestUtil.createRandomString(10));
+      registry.add(indexPrefixConfig, () -> TestUtil.createRandomString(10));
       registry.add("camunda.database.schema-manager.createSchema", () -> true);
       registry.add("camunda.operate.zeebe.compatibility.enabled", () -> true);
     }
@@ -67,7 +77,7 @@ class ProbesTestIT {
 
     @DynamicPropertySource
     static void setProperties(final DynamicPropertyRegistry registry) {
-      registry.add(INDEX_PREFIX_CONFIG, () -> TestUtil.createRandomString(10));
+      registry.add(indexPrefixConfig, () -> TestUtil.createRandomString(10));
       registry.add("camunda.database.schema-manager.createSchema", () -> false);
       registry.add("camunda.operate.zeebe.compatibility.enabled", () -> true);
     }
