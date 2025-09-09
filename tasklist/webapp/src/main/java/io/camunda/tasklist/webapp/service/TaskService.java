a1a7601b7d7e168b58c737667f5470639a1efdc4
Nico Korthout <nico.korthout@camunda.com>
2025-09-09 14:10:57 +0200

    style: use final keyword for parameter
    Auto applied by save-actions plugin.
    
    (cherry picked from commit be3b692ee29cd19cc3ac2ae85d505b03eec653ca)

--- tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/service/TaskService.java
+++ tasklist/webapp/src/main/java/io/camunda/tasklist/webapp/service/TaskService.java
@@ -190,7 +190,7 @@ public class TaskService {
     return TaskDTO.createFrom(claimedTask, objectMapper);
   }
 
-  public void updateTaskAssignedMetric(TaskEntity task) {
+  public void updateTaskAssignedMetric(final TaskEntity task) {
     // Only write metrics when completing a Job-based User Tasks. With 8.7,
     // metrics for completed (not Job-based) User Tasks are written by the
     // handler "TaskCompletedMetricHandler" in the camunda-exporter
