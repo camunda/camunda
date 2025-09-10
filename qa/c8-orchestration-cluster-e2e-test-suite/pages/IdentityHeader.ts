32547f5fd1aad1646e76dffaedc1ed74aca0f1ee
esraagamal6 <esraa-gamal@hotmail.com>
2025-09-10 13:20:21 +0200

    BACKPORT-CONFLICT

--- qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityHeader.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityHeader.ts
@@ -13,6 +13,12 @@ export class IdentityHeader {
   readonly logoutButton: Locator;
   readonly rolesTab: Locator;
   readonly tenantsTab: Locator;
+<<<<<<< HEAD
+=======
+  readonly authorizationsTab: Locator;
+  readonly usersTab: Locator;
+  readonly groupsTab: Locator;
+>>>>>>> f5f87de1 (test: Identity e2e As an Admin user I can unassign role from user)
 
   constructor(page: Page) {
     this.openSettingsButton = page.getByRole('button', {
@@ -21,6 +27,14 @@ export class IdentityHeader {
     this.logoutButton = page.getByRole('button', {name: 'Log out'});
     this.rolesTab = page.locator('nav a').filter({hasText: /^Roles$/});
     this.tenantsTab = page.locator('nav a').filter({hasText: /^Tenants$/});
+<<<<<<< HEAD
+=======
+    this.authorizationsTab = page
+      .locator('nav a')
+      .filter({hasText: /^Authorizations$/});
+    this.usersTab = page.locator('nav a').filter({hasText: /^Users$/});
+    this.groupsTab = page.locator('nav a').filter({hasText: /^Groups$/});
+>>>>>>> f5f87de1 (test: Identity e2e As an Admin user I can unassign role from user)
   }
 
   async logout() {
@@ -35,4 +49,19 @@ export class IdentityHeader {
   async navigateToTenants() {
     await this.tenantsTab.click();
   }
+<<<<<<< HEAD
+=======
+
+  async navigateToAuthorizations() {
+    await this.authorizationsTab.click();
+  }
+
+  async navigateToUsers() {
+    await this.usersTab.click();
+  }
+
+  async navigateToGroups() {
+    await this.groupsTab.click();
+  }
+>>>>>>> f5f87de1 (test: Identity e2e As an Admin user I can unassign role from user)
 }
