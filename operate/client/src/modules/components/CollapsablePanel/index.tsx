e913f1ce8235e65ab14f53312300e22d29980100
Patrick Dehn <patrick.dehn@camunda.com>
2025-09-10 14:25:14 +0200

    fix: remove title from collapsable component to remove tooltip on hover
    (cherry picked from commit 7c63497dce60522c6a86cbf48a774bb446444330)

--- operate/client/src/modules/components/CollapsablePanel/index.tsx
+++ operate/client/src/modules/components/CollapsablePanel/index.tsx
@@ -56,7 +56,6 @@ const CollapsablePanel = forwardRef<HTMLDivElement, Props>(
       <Collapsable
         {...props}
         aria-label={label}
-        title={label}
         $isCollapsed={isCollapsed}
         $panelPosition={panelPosition}
         $isOverlay={isOverlay}
