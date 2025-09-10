133a8ba1b4fe86ca245b07bf8c04c953bfb1a99b
Patrick Dehn <patrick.dehn@camunda.com>
2025-09-10 14:57:39 +0200

    fix: handle PARTIALLY_COMPLETED and FAILED batch operation states
    (cherry picked from commit 9cec0f9dc12f72cdb8507893aaa5d442068e20cb)

--- operate/client/src/modules/components/OperationsPanel/OperationsEntry/OperationEntryStatus/index.tsx
+++ operate/client/src/modules/components/OperationsPanel/OperationsEntry/OperationEntryStatus/index.tsx
@@ -6,14 +6,21 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import type {BatchOperationType} from '@camunda/camunda-api-zod-schemas/8.8';
+import type {
+  BatchOperationState,
+  BatchOperationType,
+} from '@camunda/camunda-api-zod-schemas/8.8';
 import {CheckmarkFilled, StatusContainer, Text, WarningFilled} from './styled';
 import pluralSuffix from 'modules/utils/pluralSuffix';
+import {Stack} from '@carbon/react';
+import {PartiallyCompleted} from './PartiallyCompleted';
+import {Failed} from './Failed';
 
 type Props = {
   type: BatchOperationType;
   failedCount?: number;
   completedCount?: number;
+  state: BatchOperationState;
 };
 
 const getSuccessMessage = (
@@ -50,23 +57,28 @@ const OperationEntryStatus: React.FC<Props> = ({
   type,
   failedCount = 0,
   completedCount = 0,
+  state,
 }) => {
   return (
-    <StatusContainer>
-      {completedCount > 0 ? (
-        <>
-          <CheckmarkFilled />
-          <Text>{getSuccessMessage(type, completedCount)}</Text>
-        </>
-      ) : null}
-      {completedCount > 0 && failedCount > 0 ? ' / ' : null}
-      {failedCount ? (
-        <>
-          <WarningFilled />
-          <Text>{getFailureMessage(type, failedCount, completedCount)}</Text>
-        </>
-      ) : null}
-    </StatusContainer>
+    <Stack gap={3}>
+      {state === 'PARTIALLY_COMPLETED' && <PartiallyCompleted />}
+      {state === 'FAILED' && <Failed />}
+      <StatusContainer>
+        {completedCount > 0 ? (
+          <>
+            <CheckmarkFilled />
+            <Text>{getSuccessMessage(type, completedCount)}</Text>
+          </>
+        ) : null}
+        {completedCount > 0 && failedCount > 0 ? ' / ' : null}
+        {failedCount ? (
+          <>
+            <WarningFilled />
+            <Text>{getFailureMessage(type, failedCount, completedCount)}</Text>
+          </>
+        ) : null}
+      </StatusContainer>
+    </Stack>
   );
 };
 
