cc06725475df8970d6695b0cb71678a54f3cb213
Philipp Ossler <philipp.ossler@gmail.com>
2025-09-11 11:23:14 +0200

    refactor: Delete dead code
    Delete unused method.

--- testing/camunda-process-test-java/src/main/java/io/camunda/process/test/impl/assertions/CamundaDataSource.java
+++ testing/camunda-process-test-java/src/main/java/io/camunda/process/test/impl/assertions/CamundaDataSource.java
@@ -159,10 +159,6 @@ public class CamundaDataSource {
     return client.newDecisionInstanceGetRequest(decisionInstanceId).send().join();
   }
 
-  public List<MessageSubscription> findMessageSubscriptions() {
-    return findMessageSubscriptions(filter -> {});
-  }
-
   public List<MessageSubscription> findMessageSubscriptions(
       final Consumer<MessageSubscriptionFilter> filter) {
     return client
