385b79eccd51e4132ad9d0ce98b2c28b7de5a98c
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2025-09-11 09:00:30 +0200

    test: remove final modifiers

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/CamundaExporterIT.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/CamundaExporterIT.java
@@ -88,10 +88,10 @@ import org.testcontainers.containers.GenericContainer;
 @TestInstance(Lifecycle.PER_CLASS)
 final class CamundaExporterIT {
 
-  @RegisterExtension private static final SearchDBExtension searchDB = SearchDBExtension.create();
+  @RegisterExtension private static SearchDBExtension searchDB = SearchDBExtension.create();
 
   @RegisterExtension
-  private static final CamundaExporterITTemplateExtension templateExtension =
+  private static CamundaExporterITTemplateExtension templateExtension =
       new CamundaExporterITTemplateExtension(searchDB);
 
   private final ProtocolFactory factory = new ProtocolFactory();
