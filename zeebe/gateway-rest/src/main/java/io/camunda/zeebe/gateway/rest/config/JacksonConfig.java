e9c6378c054261a8cbf351d3dfc720fec2456084
megglos <sebastian.bathke@camunda.com>
2025-09-11 15:48:10 +0200

    fix: disable type coercion for v2 only
    Change of course in accepting changing behavior for v1.
    This isolated disabled type coercion only to v2 classes.
    
    (cherry picked from commit 4da6f40fe58310b421b8714f0718f4ce5d9e0b05)

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/JacksonConfig.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/JacksonConfig.java
@@ -8,8 +8,14 @@
 package io.camunda.zeebe.gateway.rest.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.cfg.CoercionAction;
+import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.camunda.zeebe.gateway.protocol.rest.BasicStringFilterProperty;
 import io.camunda.zeebe.gateway.protocol.rest.BatchOperationItemStateFilterProperty;
 import io.camunda.zeebe.gateway.protocol.rest.BatchOperationStateFilterProperty;
@@ -84,9 +90,18 @@ public class JacksonConfig {
   @Bean("gatewayRestObjectMapper")
   public ObjectMapper objectMapper() {
     final var builder = Jackson2ObjectMapperBuilder.json();
+    builder
+        .featuresToDisable(MapperFeature.ALLOW_COERCION_OF_SCALARS)
+        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .featuresToEnable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+        .modulesToInstall(new JavaTimeModule(), new Jdk8Module())
+        .postConfigurer(
+            om -> {
+              // this also prevents coercion for string target types
+              om.coercionConfigDefaults()
+                  .setCoercion(CoercionInputShape.String, CoercionAction.Fail);
+            });
     gatewayRestObjectMapperCustomizer().accept(builder);
-    final var mapper = builder.build();
-    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-    return mapper;
+    return builder.build();
   }
 }
