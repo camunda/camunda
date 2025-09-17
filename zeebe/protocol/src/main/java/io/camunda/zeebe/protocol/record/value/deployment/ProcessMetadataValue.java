65eac0749e328b3c658df1cf1f55d3981684baab
Fabio Paini <fabio.paini@camunda.com>
2025-09-17 09:53:28 +0200

    refactor: make metadata record values extend a common interface to improve maintainability

--- zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/record/value/deployment/ProcessMetadataValue.java
+++ zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/record/value/deployment/ProcessMetadataValue.java
@@ -16,51 +16,29 @@
 package io.camunda.zeebe.protocol.record.value.deployment;
 
 import io.camunda.zeebe.protocol.record.ImmutableProtocol;
-import io.camunda.zeebe.protocol.record.RecordValue;
-import io.camunda.zeebe.protocol.record.value.TenantOwned;
 import org.immutables.value.Value;
 
-/** Represents deployed process meta data, so all important properties of an deployed process. */
+/** Represents deployed process metadata, so all important properties of a deployed process. */
 @Value.Immutable
 @ImmutableProtocol(builder = ImmutableProcessMetadataValue.Builder.class)
-public interface ProcessMetadataValue extends RecordValue, TenantOwned {
+public interface ProcessMetadataValue extends ExtendedMetadataValue {
   /**
    * @return the bpmn process ID of this process
    */
   String getBpmnProcessId();
 
   /**
-   * @return the version of this process
-   */
-  int getVersion();
-
-  /**
-   * @return the custom version tag of this process
-   */
-  String getVersionTag();
-
-  /**
    * @return the key of this process
    */
   long getProcessDefinitionKey();
 
-  /**
-   * @return the name of the resource through which this process was deployed
-   */
-  String getResourceName();
+  @Override
+  default String entityId() {
+    return getBpmnProcessId();
+  }
 
-  /**
-   * @return the checksum of the process (MD5)
-   */
-  byte[] getChecksum();
-
-  /**
-   * return true if the process is a duplicate (and has been deployed previously), false otherwise
-   */
-  boolean isDuplicate();
-
-  /**
-   * @return the key of the deployment this process was deployed with
-   */
-  long getDeploymentKey();
+  @Override
+  default long entityKey() {
+    return getProcessDefinitionKey();
+  }
 }
