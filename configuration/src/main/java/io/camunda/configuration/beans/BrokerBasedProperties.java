0e3792a1842818e9c1e9441a765daad4175be1bc
Stefan Zilske <stefan.zilske@holisticon.de>
2025-09-23 13:43:48 +0200

    feat: Move rdbms history config to unified configuration

--- configuration/src/main/java/io/camunda/configuration/beans/BrokerBasedProperties.java
+++ configuration/src/main/java/io/camunda/configuration/beans/BrokerBasedProperties.java
@@ -9,6 +9,8 @@ package io.camunda.configuration.beans;
 
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.broker.system.configuration.ExporterCfg;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 // NOTE: This class has been moved away from dist. The reason for this is that as, for now,
 //  we're storing the unified configuration objects and beans in the configuration module,
@@ -29,5 +31,24 @@ public class BrokerBasedProperties extends BrokerCfg {
 
   public ExporterCfg getRdbmsExporter() {
     return getExporters().get("rdbms");
+  }
+
+  public ExporterCfg getExporterConfig(final String exporterClassName, final String exporterName) {
+    final List<ExporterCfg> exporters =
+        getExporters().values().stream()
+            .filter(e -> e.getClassName().equals(exporterClassName))
+            .toList();
+
+    final ExporterCfg exporter;
+    if (exporters.isEmpty()) {
+      exporter = new ExporterCfg();
+      exporter.setClassName(exporterClassName);
+      exporter.setArgs(new LinkedHashMap<>());
+      getExporters().put(exporterName, exporter);
+    } else {
+      exporter = exporters.getFirst();
+    }
+
+    return exporter;
   }
 }
