9341668b37d8e11bd024af607c4a0851e656c9f4
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-09-24 11:52:57 +0200

    fix: reverted interceptor change

--- configuration/src/main/java/io/camunda/configuration/Interceptor.java
+++ configuration/src/main/java/io/camunda/configuration/Interceptor.java
@@ -9,45 +9,7 @@ package io.camunda.configuration;
 
 import io.camunda.zeebe.gateway.impl.configuration.InterceptorCfg;
 
-public class Interceptor {
-  /** Sets the identifier of the implementation. */
-  private String id;
-
-  /**
-   * Sets the path to the class JAR. Can be null if the implementation class can be found on the
-   * class path.
-   */
-  private String jarPath;
-
-  /**
-   * Sets a new class name. Note that this must be a fully qualified class name to avoid any
-   * collisions.
-   */
-  private String className;
-
-  public String getId() {
-    return id;
-  }
-
-  public void setId(final String id) {
-    this.id = id;
-  }
-
-  public String getJarPath() {
-    return jarPath;
-  }
-
-  public void setJarPath(final String jarPath) {
-    this.jarPath = jarPath;
-  }
-
-  public String getClassName() {
-    return className;
-  }
-
-  public void setClassName(final String className) {
-    this.className = className;
-  }
+public class Interceptor extends BaseExternalCodeConfiguration {
 
   public InterceptorCfg toInterceptorCfg() {
     final var interceptorCfg = new InterceptorCfg();
