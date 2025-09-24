e8795e41b5329507f8acace778b7dd08705f7dae
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-09-24 11:46:16 +0200

    feat: introduced mapping mode

--- configuration/src/main/java/io/camunda/configuration/processor/CamundaLegacyPropertiesMapping.java
+++ configuration/src/main/java/io/camunda/configuration/processor/CamundaLegacyPropertiesMapping.java
@@ -7,9 +7,31 @@
  */
 package io.camunda.configuration.processor;
 
+import java.time.Duration;
 import java.util.Set;
+import java.util.function.Function;
 
 public record CamundaLegacyPropertiesMapping(
-    String newProperty, Set<Set<LegacyProperty>> legacyProperties) {
-  public record LegacyProperty(String name, Set<String> profiles, String mapper) {}
+    String newProperty, Set<Set<LegacyProperty>> legacyProperties, Mode mode) {
+  public record LegacyProperty(String name, Set<String> profiles, Mapper mapper) {
+    public enum Mapper {
+      secondsToDuration(s -> Duration.ofSeconds(Long.parseLong((String) s)));
+
+      private final Function<Object, Object> mapper;
+
+      Mapper(final Function<Object, Object> mapperFunction) {
+        mapper = mapperFunction;
+      }
+
+      public Function<Object, Object> getMapper() {
+        return mapper;
+      }
+    }
+  }
+
+  public enum Mode {
+    notSupported,
+    supportedOnlyIfValuesMatch,
+    supported
+  }
 }
