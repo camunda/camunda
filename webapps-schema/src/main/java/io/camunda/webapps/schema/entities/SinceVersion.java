b031b307eb046aef3887154cf7c8255873837a6e
euro.lew <euro.lew@camunda.com>
2025-09-24 20:00:23 +0100

    docs: for @SinceVersion annotation usage

--- webapps-schema/src/main/java/io/camunda/webapps/schema/entities/SinceVersion.java
+++ webapps-schema/src/main/java/io/camunda/webapps/schema/entities/SinceVersion.java
@@ -12,6 +12,29 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * New fields in entities stored at {@link io.camunda.webapps.schema.entities} must have the
+ * annotation {@link io.camunda.webapps.schema.entities.SinceVersion} with the value being the
+ * semantic version the field was introduced.
+ *
+ * <p>In addition, all fields with this annotation must have a non-null default value. This is to
+ * maintain backwards compatability. However, if the nullable field is set to true then it is
+ * acceptable for the field to have a null default.
+ *
+ * <p>Example usage:
+ *
+ * <pre><code>
+ *   class Foo {
+ *     private String newField; // This will fail
+ *
+ *    {@literal @SinceVersion("8.8.0")}
+ *     private String newField = "bar" // This will pass
+ *
+ *    {@literal @SinceVersion(value = "8.8.0", nullable = true)}
+ *     private String newField; // This will pass
+ *   }
+ * </code></pre>
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SinceVersion {
