ffac96528977f08ff635768fd13c4c76c4531c7a
Stephan Epping <stephan.epping@camunda.com>
2025-09-26 11:55:41 +0200

    test: split zeebe integration tests into engine, cluster and shared
    (cherry picked from commit 9314f99adc65f15769cc6ea2f68618764147fbba)

---
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/cluster/network/NetworkCompressionTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.it.cluster.network;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.atomix.cluster.messaging.MessagingConfig.CompressionAlgorithm;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
+import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
+import io.camunda.zeebe.it.cluster.clustering.ClusteringRule;
+import io.camunda.zeebe.it.util.BrokerClassRuleHelper;
+import io.camunda.zeebe.it.util.GrpcClientRule;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.Timeout;
+
+public class NetworkCompressionTest {
+  public final Timeout testTimeout = Timeout.seconds(120);
+
+  public final ClusteringRule clusteringRule =
+      new ClusteringRule(
+          1, 3, 3, this::configureClusterWithCompression, this::configureGatewayWithCompression);
+
+  public final GrpcClientRule clientRule = new GrpcClientRule(clusteringRule);
+
+  @Rule
+  public RuleChain ruleChain =
+      RuleChain.outerRule(testTimeout).around(clusteringRule).around(clientRule);
+
+  @Test
+  public void shouldCommunicateWhenCompressionEnabled() {
+    // given
+    final String processId = new BrokerClassRuleHelper().getBpmnProcessId();
+
+    // when
+    clientRule.deployProcess(
+        Bpmn.createExecutableProcess(processId).startEvent("start").endEvent("end").done());
+
+    final ProcessInstanceEvent processInstance =
+        clientRule
+            .getClient()
+            .newCreateInstanceCommand()
+            .bpmnProcessId(processId)
+            .latestVersion()
+            .send()
+            .join();
+
+    // then - gateway to broker and broker to broker communication was successful
+    assertThat(processInstance.getBpmnProcessId()).isEqualTo(processId);
+  }
+
+  private void configureGatewayWithCompression(final GatewayCfg gatewayCfg) {
+    gatewayCfg.getCluster().setMessageCompression(CompressionAlgorithm.GZIP);
+  }
+
+  private void configureClusterWithCompression(final BrokerCfg brokerCfg) {
+    brokerCfg.getCluster().setMessageCompression(CompressionAlgorithm.GZIP);
+  }
+}
---
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/engine/client/command/UnassignUserFromGroupTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.it.engine.client.command;
+
+import static io.camunda.zeebe.protocol.record.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.command.ProblemException;
+import io.camunda.zeebe.it.util.ZeebeAssertHelper;
+import io.camunda.zeebe.protocol.record.value.EntityType;
+import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
+import io.camunda.zeebe.test.util.Strings;
+import java.time.Duration;
+import org.junit.jupiter.api.AutoClose;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+@ZeebeIntegration
+public class UnassignUserFromGroupTest {
+  @TestZeebe
+  private final TestStandaloneBroker zeebe =
+      new TestStandaloneBroker().withRecordingExporter(true).withUnauthenticatedAccess();
+
+  @AutoClose private CamundaClient client;
+
+  private String groupId;
+  private String username;
+
+  @BeforeEach
+  void initClientAndInstances() {
+    client = zeebe.newClientBuilder().defaultRequestTimeout(Duration.ofSeconds(15)).build();
+    username =
+        client
+            .newCreateUserCommand()
+            .name("User Name")
+            .username(Strings.newRandomValidUsername())
+            .email("foo@example.com")
+            .password("******")
+            .send()
+            .join()
+            .getUsername();
+
+    groupId =
+        client
+            .newCreateGroupCommand()
+            .groupId(Strings.newRandomValidIdentityId())
+            .name("groupName")
+            .send()
+            .join()
+            .getGroupId();
+    client.newAssignUserToGroupCommand().username(username).groupId(groupId).send().join();
+  }
+
+  @Test
+  void shouldUnassignUserFromGroup() {
+    // when
+    client.newUnassignUserFromGroupCommand().username(username).groupId(groupId).send().join();
+
+    // then
+    ZeebeAssertHelper.assertEntityUnassignedFromGroup(
+        groupId,
+        username,
+        group -> {
+          assertThat(group).hasEntityType(EntityType.USER);
+        });
+  }
+
+  @Test
+  void shouldRejectIfGroupDoesNotExist() {
+    // given
+    final String nonExistentGroupId = Strings.newRandomValidIdentityId();
+
+    // when / then
+    assertThatThrownBy(
+            () ->
+                client
+                    .newUnassignUserFromGroupCommand()
+                    .username(username)
+                    .groupId(nonExistentGroupId)
+                    .send()
+                    .join())
+        .isInstanceOf(ProblemException.class)
+        .hasMessageContaining("Failed with code 404: 'Not Found'")
+        .hasMessageContaining(
+            "Expected to update group with ID '%s', but a group with this ID does not exist."
+                .formatted(nonExistentGroupId));
+  }
+
+  @Test
+  void shouldRejectIfMissingGroupId() {
+    // when / then
+    assertThatThrownBy(
+            () ->
+                client
+                    .newUnassignUserFromGroupCommand()
+                    .username("username")
+                    .groupId(null)
+                    .send()
+                    .join())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("groupId must not be null");
+  }
+
+  @Test
+  void shouldRejectIfEmptyGroupId() {
+    // when / then
+    assertThatThrownBy(
+            () ->
+                client
+                    .newUnassignUserFromGroupCommand()
+                    .username("username")
+                    .groupId("")
+                    .send()
+                    .join())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("groupId must not be empty");
+  }
+
+  @Test
+  void shouldRejectIfMissingUsername() {
+    // when / then
+    assertThatThrownBy(
+            () ->
+                client
+                    .newUnassignUserFromGroupCommand()
+                    .username(null)
+                    .groupId(groupId)
+                    .send()
+                    .join())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("username must not be null");
+  }
+
+  @Test
+  void shouldRejectIfEmptyUsername() {
+    // when / then
+    assertThatThrownBy(
+            () ->
+                client
+                    .newUnassignUserFromGroupCommand()
+                    .username("")
+                    .groupId(groupId)
+                    .send()
+                    .join())
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessageContaining("username must not be empty");
+  }
+}
