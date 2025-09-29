1c168ec5d344212d67587cf6a3632827e143c7f6
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-09-29 10:57:16 +0200

    fix: default to Tasklist V2

--- tasklist/client/src/common/tasks/available-tasks/CollapsiblePanel/CustomFiltersModal/index.v2.test.tsx
+++ tasklist/client/src/common/tasks/available-tasks/CollapsiblePanel/CustomFiltersModal/index.v2.test.tsx
@@ -25,19 +25,6 @@ const definitionsMock = getQueryProcessDefinitionsResponseMock([
   getProcessDefinitionMock(),
 ]);
 
-vi.mock('common/config/getClientConfig', async (importOriginal) => {
-  const actual =
-    await importOriginal<typeof import('common/config/getClientConfig')>();
-  return {
-    getClientConfig() {
-      return {
-        ...actual.getClientConfig(),
-        clientMode: 'v2',
-      };
-    },
-  };
-});
-
 const getWrapper = () => {
   const mockClient = getMockQueryClient();
 
