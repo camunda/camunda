5c4a1cbd365ec800de7e771a4ca8bc3513932bc8
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-09-30 17:48:22 +0200

    test: add missing acceptance tests for filesystem store
    (cherry picked from commit 73c7a3e7549e5665484721f3e7bfb1e6824e502a)

---
+++ zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/backup/FilesystemRestoreAcceptanceIT.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.it.backup;
+
+import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
+import io.camunda.zeebe.broker.system.configuration.backup.BackupStoreCfg.BackupStoreType;
+import io.camunda.zeebe.broker.system.configuration.backup.FilesystemBackupStoreConfig;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
+import java.nio.file.Path;
+import java.util.UUID;
+import org.junit.jupiter.api.io.TempDir;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@ZeebeIntegration
+final class FilesystemRestoreAcceptanceIT implements RestoreAcceptance {
+  private static @TempDir Path tempDir;
+
+  private final Path basePath = tempDir.resolve(UUID.randomUUID().toString());
+
+  @Override
+  public void configureBackupStore(final BrokerCfg cfg) {
+    final var backup = cfg.getData().getBackup();
+    backup.setStore(BackupStoreType.FILESYSTEM);
+
+    final var config = new FilesystemBackupStoreConfig();
+    config.setBasePath(basePath.toAbsolutePath().toString());
+    backup.setFilesystem(config);
+  }
+}
