da4b6208ce2e51bcc4f5f5f938a44074bf027dc3
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-10-02 12:17:55 +0200

    refactor: remove unused imports

--- operate/client/src/modules/queries/messageSubscriptions/useSearchMessageSubscriptions.ts
+++ operate/client/src/modules/queries/messageSubscriptions/useSearchMessageSubscriptions.ts
@@ -7,13 +7,8 @@
  */
 
 import {useQuery} from '@tanstack/react-query';
-import {
-  type QueryMessageSubscriptionsRequestBody,
-  type QueryUserTasksRequestBody,
-} from '@camunda/camunda-api-zod-schemas/8.8';
+import {type QueryMessageSubscriptionsRequestBody} from '@camunda/camunda-api-zod-schemas/8.8';
 import {searchMessageSubscriptions} from 'modules/api/v2/messageSubscriptions/searchMessageSubscriptions';
-
-const MESSAGE_SUBSCRIPTION_SEARCH_QUERY_KEY = 'incidentsSearch';
 
 const useSearchMessageSubscriptions = (
   payload: QueryMessageSubscriptionsRequestBody,
@@ -22,7 +17,7 @@ const useSearchMessageSubscriptions = (
   const {enabled = true} = options ?? {};
 
   return useQuery({
-    queryKey: [MESSAGE_SUBSCRIPTION_SEARCH_QUERY_KEY, payload],
+    queryKey: ['incidentsSearch', payload],
     queryFn: async () => {
       const {response, error} = await searchMessageSubscriptions(payload);
       if (response !== null) {
