f7e425bc2b798dd82ba78e1b77a2dc8814fc9c72
Houssain Barouni <houssain.barouni@camunda.com>
2025-10-07 08:37:40 +0200

    refactor: clean-up tasklist properties

--- tasklist/common/src/main/java/io/camunda/tasklist/property/TasklistProperties.java
+++ tasklist/common/src/main/java/io/camunda/tasklist/property/TasklistProperties.java
@@ -7,7 +7,6 @@
  */
 package io.camunda.tasklist.property;
 
-import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
@@ -26,32 +25,12 @@ public class TasklistProperties {
   public static final String OPEN_SEARCH = "opensearch";
   private static final String UNKNOWN_VERSION = "unknown-version";
 
-  private boolean importerEnabled = false;
-  private boolean webappEnabled = true;
-
-  private boolean persistentSessionsEnabled = false;
-  private boolean csrfPreventionEnabled = true;
-  private boolean fixUsernames = true;
-  private String userId = "demo";
-  private String displayName = "demo";
-  private String password = "demo";
-  private String operatorUserId = "act";
-  private String operatorPassword = "act";
-  private String operatorDisplayName = "act";
-  private String readerUserId = "view";
-  private String readerPassword = "view";
-  private String readerDisplayName = "view";
   private String database = ELASTIC_SEARCH;
-
-  private List<String> roles = List.of("OWNER");
 
   private boolean enterprise = false;
 
   @Value("${camunda.tasklist.internal.version.current}")
   private String version = UNKNOWN_VERSION;
-
-  @NestedConfigurationProperty
-  private SecurityProperties securityProperties = new SecurityProperties();
 
   @NestedConfigurationProperty
   private TasklistElasticsearchProperties elasticsearch = new TasklistElasticsearchProperties();
@@ -70,9 +49,6 @@ public class TasklistProperties {
   @NestedConfigurationProperty
   private FeatureFlagProperties featureFlag = new FeatureFlagProperties();
 
-  @NestedConfigurationProperty
-  private ClusterNodeProperties clusterNode = new ClusterNodeProperties();
-
   @NestedConfigurationProperty private Auth0Properties auth0 = new Auth0Properties();
 
   @NestedConfigurationProperty private IdentityProperties identity = new IdentityProperties();
@@ -82,36 +58,8 @@ public class TasklistProperties {
   @NestedConfigurationProperty
   private TasklistDocumentationProperties documentation = new TasklistDocumentationProperties();
 
-  public boolean isImporterEnabled() {
-    return importerEnabled;
-  }
-
-  public void setImporterEnabled(final boolean importerEnabled) {
-    this.importerEnabled = importerEnabled;
-  }
-
-  public boolean isWebappEnabled() {
-    return webappEnabled;
-  }
-
-  public void setWebappEnabled(final boolean webappEnabled) {
-    this.webappEnabled = webappEnabled;
-  }
-
-  public boolean isAlphaVersion() {
-    return getVersion().toLowerCase().contains(TasklistProperties.ALPHA_RELEASES_SUFIX);
-  }
-
   public boolean isSelfManaged() {
     return getCloud().getClusterId() == null;
-  }
-
-  public boolean isCsrfPreventionEnabled() {
-    return csrfPreventionEnabled;
-  }
-
-  public void setCsrfPreventionEnabled(final boolean csrfPreventionEnabled) {
-    this.csrfPreventionEnabled = csrfPreventionEnabled;
   }
 
   public TasklistElasticsearchProperties getElasticsearch() {
@@ -130,44 +78,12 @@ public class TasklistProperties {
     this.zeebe = zeebe;
   }
 
-  public String getUserId() {
-    return userId;
-  }
-
-  public void setUserId(final String userId) {
-    this.userId = userId;
-  }
-
-  public String getDisplayName() {
-    return displayName;
-  }
-
-  public void setDisplayName(final String displayName) {
-    this.displayName = displayName;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(final String password) {
-    this.password = password;
-  }
-
   public ImportProperties getImporter() {
     return importer;
   }
 
   public void setImporter(final ImportProperties importer) {
     this.importer = importer;
-  }
-
-  public ClusterNodeProperties getClusterNode() {
-    return clusterNode;
-  }
-
-  public void setClusterNode(final ClusterNodeProperties clusterNode) {
-    this.clusterNode = clusterNode;
   }
 
   public boolean isEnterprise() {
@@ -186,15 +102,6 @@ public class TasklistProperties {
     this.client = client;
   }
 
-  public boolean isPersistentSessionsEnabled() {
-    return persistentSessionsEnabled;
-  }
-
-  public TasklistProperties setPersistentSessionsEnabled(final boolean persistentSessionsEnabled) {
-    this.persistentSessionsEnabled = persistentSessionsEnabled;
-    return this;
-  }
-
   public Auth0Properties getAuth0() {
     return auth0;
   }
@@ -210,14 +117,6 @@ public class TasklistProperties {
 
   public void setIdentity(final IdentityProperties identity) {
     this.identity = identity;
-  }
-
-  public List<String> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(final List<String> roles) {
-    this.roles = roles;
   }
 
   public CloudProperties getCloud() {
@@ -246,83 +145,12 @@ public class TasklistProperties {
     return this;
   }
 
-  public boolean isFixUsernames() {
-    return fixUsernames;
-  }
-
-  public void setFixUsernames(final boolean fixUsernames) {
-    this.fixUsernames = fixUsernames;
-  }
-
-  public SecurityProperties getSecurityProperties() {
-    return securityProperties;
-  }
-
-  public TasklistProperties setSecurityProperties(final SecurityProperties securityProperties) {
-    this.securityProperties = securityProperties;
-    return this;
-  }
-
   public FeatureFlagProperties getFeatureFlag() {
     return featureFlag;
   }
 
   public TasklistProperties setFeatureFlag(final FeatureFlagProperties featureFlag) {
     this.featureFlag = featureFlag;
-    return this;
-  }
-
-  public String getOperatorUserId() {
-    return operatorUserId;
-  }
-
-  public TasklistProperties setOperatorUserId(final String operatorUserId) {
-    this.operatorUserId = operatorUserId;
-    return this;
-  }
-
-  public String getOperatorPassword() {
-    return operatorPassword;
-  }
-
-  public TasklistProperties setOperatorPassword(final String operatorPassword) {
-    this.operatorPassword = operatorPassword;
-    return this;
-  }
-
-  public String getOperatorDisplayName() {
-    return operatorDisplayName;
-  }
-
-  public TasklistProperties setOperatorDisplayName(final String operatorDisplayName) {
-    this.operatorDisplayName = operatorDisplayName;
-    return this;
-  }
-
-  public String getReaderUserId() {
-    return readerUserId;
-  }
-
-  public TasklistProperties setReaderUserId(final String readerUserId) {
-    this.readerUserId = readerUserId;
-    return this;
-  }
-
-  public String getReaderPassword() {
-    return readerPassword;
-  }
-
-  public TasklistProperties setReaderPassword(final String readerPassword) {
-    this.readerPassword = readerPassword;
-    return this;
-  }
-
-  public String getReaderDisplayName() {
-    return readerDisplayName;
-  }
-
-  public TasklistProperties setReaderDisplayName(final String readerDisplayName) {
-    this.readerDisplayName = readerDisplayName;
     return this;
   }
 
