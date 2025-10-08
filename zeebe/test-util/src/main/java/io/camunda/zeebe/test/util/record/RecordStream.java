51a68198de27adcc8976f9b8bf92d98b0fe32fbe
remco.westerhoud <remcowesterhoud@msn.com>
2025-10-08 09:46:27 +0200

    test: verify no events are written on message correlate command if user is unauthorized
    (cherry picked from commit 6d3f3a49c7da2d3cdd8cb157c59be1f383b131cb)

--- zeebe/test-util/src/main/java/io/camunda/zeebe/test/util/record/RecordStream.java
+++ zeebe/test-util/src/main/java/io/camunda/zeebe/test/util/record/RecordStream.java
@@ -106,6 +106,16 @@ public final class RecordStream extends ExporterRecordStream<RecordValue, Record
         filter(r -> r.getValueType() == ValueType.INCIDENT).map(Record.class::cast));
   }
 
+  public MessageRecordStream messageRecords() {
+    return new MessageRecordStream(
+        filter(r -> r.getValueType() == ValueType.MESSAGE).map(Record.class::cast));
+  }
+
+  public MessageCorrelationRecordStream messageCorrelationRecords() {
+    return new MessageCorrelationRecordStream(
+        filter(r -> r.getValueType() == ValueType.MESSAGE_CORRELATION).map(Record.class::cast));
+  }
+
   public MessageSubscriptionRecordStream messageSubscriptionRecords() {
     return new MessageSubscriptionRecordStream(
         filter(r -> r.getValueType() == ValueType.MESSAGE_SUBSCRIPTION).map(Record.class::cast));
