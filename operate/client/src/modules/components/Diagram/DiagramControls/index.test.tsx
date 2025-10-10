1fcd66acba55f311ea2a55cc1bd59bd9e46752c1
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-10-10 17:45:22 +0200

    test: remove wrong mocks

--- operate/client/src/modules/components/Diagram/DiagramControls/index.test.tsx
+++ operate/client/src/modules/components/Diagram/DiagramControls/index.test.tsx
@@ -8,18 +8,18 @@
 
 import {render, screen} from 'modules/testing-library';
 import DiagramControls from './index';
-import {vi} from 'vitest';
-
-vi.mock('modules/queries/processDefinitions/useProcessDefinitionXml', () => ({
-  useProcessDefinitionXml: () => ({isLoading: false, data: '<xml />'}),
-}));
-vi.mock('modules/queries/processDefinitions/useProcessDefinition', () => ({
-  useProcessDefinition: () => ({
-    data: {name: 'Test', processDefinitionId: 'test-id', version: 1},
-  }),
-}));
+import {QueryClientProvider} from '@tanstack/react-query';
+import {getMockQueryClient} from 'modules/react-query/mockQueryClient';
 
 const MOCK_PROCESS_DEFINITION_KEY = '123';
+
+const Wrapper: React.FC<{children?: React.ReactNode}> = ({children}) => {
+  return (
+    <QueryClientProvider client={getMockQueryClient()}>
+      {children}
+    </QueryClientProvider>
+  );
+};
 
 describe('<DiagramControls />', () => {
   it('should render diagram controls', async () => {
@@ -33,6 +33,7 @@ describe('<DiagramControls />', () => {
         handleZoomReset={handleZoomReset}
         processDefinitionKey={MOCK_PROCESS_DEFINITION_KEY}
       />,
+      {wrapper: Wrapper},
     );
 
     await user.click(
