9e2b3e88858dd3eed62d69549197cfc96f59604c
euro.lew <euro.lew@camunda.com>
2025-10-10 13:38:11 +0100

    fix: field which was not added to 8.8.0 release
    (cherry picked from commit 9a24500fc58ca0cb8d39620da1182326fd85680f)

--- webapps-schema/src/main/java/io/camunda/webapps/schema/entities/usermanagement/TenantMemberEntity.java
+++ webapps-schema/src/main/java/io/camunda/webapps/schema/entities/usermanagement/TenantMemberEntity.java
@@ -8,6 +8,7 @@
 package io.camunda.webapps.schema.entities.usermanagement;
 
 import io.camunda.webapps.schema.entities.AbstractExporterEntity;
+import io.camunda.webapps.schema.entities.SinceVersion;
 import io.camunda.zeebe.protocol.record.value.EntityType;
 
 public class TenantMemberEntity extends AbstractExporterEntity<TenantMemberEntity> {
