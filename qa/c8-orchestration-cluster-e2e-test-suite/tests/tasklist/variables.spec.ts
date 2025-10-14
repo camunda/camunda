0d563caae73ef04f24ce3642fa90e1fcb64f54f1
slolatte <sara.lolatte@camunda.com>
2025-10-14 11:54:28 +0200

    test: additional timeout increase

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/tasklist/variables.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/tasklist/variables.spec.ts
@@ -171,7 +171,7 @@ test.describe('variables page', () => {
     await taskPanelPage.openTask('usertask_with_variables');
 
     await expect(taskDetailsPage.addVariableButton).toBeHidden();
-    await expect(taskDetailsPage.assignToMeButton).toBeVisible({timeout: 30000});
+    await expect(taskDetailsPage.assignToMeButton).toBeVisible({timeout: 60000});
     await expect(taskDetailsPage.completeTaskButton).toBeDisabled();
     await taskDetailsPage.clickAssignToMeButton();
 
