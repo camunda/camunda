d1ae07169e8119dea3160e03f6d7e1217d8215e1
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-10-14 09:55:04 +0200

    feat: Introducing DbClusterVariableKey for readability
    The key is managed in a centralized place, makes it simpler for development

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/variable/DbClusterVariableState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/variable/DbClusterVariableState.java
@@ -10,9 +10,7 @@ package io.camunda.zeebe.engine.state.variable;
 import io.camunda.zeebe.db.ColumnFamily;
 import io.camunda.zeebe.db.TransactionContext;
 import io.camunda.zeebe.db.ZeebeDb;
-import io.camunda.zeebe.db.impl.DbCompositeKey;
-import io.camunda.zeebe.db.impl.DbEnumValue;
-import io.camunda.zeebe.db.impl.DbString;
+import io.camunda.zeebe.db.impl.DbClusterVariableKey;
 import io.camunda.zeebe.engine.state.mutable.MutableClusterVariableState;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import io.camunda.zeebe.protocol.impl.record.value.variable.ClusterVariableRecord;
@@ -21,112 +19,73 @@ import org.agrona.DirectBuffer;
 
 public class DbClusterVariableState implements MutableClusterVariableState {
 
-  private final ColumnFamily<
-          DbCompositeKey<DbString, DbCompositeKey<DbEnumValue<Scope>, DbString>>,
-          ClusterVariableInstance>
+  private final DbClusterVariableKey clusterVariableKey;
+  private final ColumnFamily<DbClusterVariableKey, ClusterVariableInstance>
       clusterVariablesColumnFamily;
-  private final DbCompositeKey<DbString, DbCompositeKey<DbEnumValue<Scope>, DbString>>
-      clusterVariableNameScopeTenantIdCompositeKey;
-
-  private final DbEnumValue<Scope> scope;
-  private final DbString name;
-  private final DbString tenantId;
-
   private final ClusterVariableInstance clusterVariableInstance;
 
   public DbClusterVariableState(
       final ZeebeDb<ZbColumnFamilies> zeebeDb, final TransactionContext transactionContext) {
     clusterVariableInstance = new ClusterVariableInstance();
-
-    name = new DbString();
-    tenantId = new DbString();
-    scope = new DbEnumValue<>(Scope.class);
-
-    clusterVariableNameScopeTenantIdCompositeKey =
-        new DbCompositeKey<>(name, new DbCompositeKey<>(scope, tenantId));
+    clusterVariableKey = new DbClusterVariableKey();
     clusterVariablesColumnFamily =
         zeebeDb.createColumnFamily(
             ZbColumnFamilies.CLUSTER_VARIABLES,
             transactionContext,
-            clusterVariableNameScopeTenantIdCompositeKey,
+            clusterVariableKey,
             clusterVariableInstance);
   }
 
   @Override
   public void setClusterVariable(final ClusterVariableRecord clusterVariableRecord) {
-    name.wrapBuffer(clusterVariableRecord.getNameBuffer());
     if (clusterVariableRecord.getTenantId() == null
         || clusterVariableRecord.getTenantId().isBlank()) {
-      tenantId.wrapString("");
-      scope.setValue(Scope.GLOBAL);
+      clusterVariableKey.globalKey(clusterVariableRecord.getNameBuffer());
     } else {
-      tenantId.wrapString(clusterVariableRecord.getTenantId());
-      scope.setValue(Scope.TENANT);
+      clusterVariableKey.tenantKey(
+          clusterVariableRecord.getNameBuffer(), clusterVariableRecord.getTenantId());
     }
-
     clusterVariableInstance.setRecord(clusterVariableRecord);
-
-    clusterVariablesColumnFamily.upsert(
-        clusterVariableNameScopeTenantIdCompositeKey, clusterVariableInstance);
+    clusterVariablesColumnFamily.upsert(clusterVariableKey, clusterVariableInstance);
   }
 
   @Override
   public void deleteTenantScopedClusterVariable(
       final DirectBuffer variableNameBuffer, final String tenantId) {
-    name.wrapBuffer(variableNameBuffer);
-    scope.setValue(Scope.TENANT);
-    this.tenantId.wrapString(tenantId);
-    clusterVariablesColumnFamily.deleteExisting(clusterVariableNameScopeTenantIdCompositeKey);
+    clusterVariablesColumnFamily.deleteExisting(
+        clusterVariableKey.tenantKey(variableNameBuffer, tenantId));
   }
 
   @Override
   public void deleteGloballyScopedClusterVariable(final DirectBuffer variableNameBuffer) {
-    name.wrapBuffer(variableNameBuffer);
-    scope.setValue(Scope.GLOBAL);
-    tenantId.wrapString("");
-    clusterVariablesColumnFamily.deleteExisting(clusterVariableNameScopeTenantIdCompositeKey);
+    clusterVariablesColumnFamily.deleteExisting(clusterVariableKey.globalKey(variableNameBuffer));
   }
 
   @Override
   public Optional<ClusterVariableInstance> getTenantScopedClusterVariable(
       final DirectBuffer variableNameBuffer, final String tenantId) {
-    name.wrapBuffer(variableNameBuffer);
-    this.tenantId.wrapString(tenantId);
-    scope.setValue(Scope.TENANT);
     return Optional.ofNullable(
         clusterVariablesColumnFamily.get(
-            clusterVariableNameScopeTenantIdCompositeKey, ClusterVariableInstance::new));
+            clusterVariableKey.tenantKey(variableNameBuffer, tenantId),
+            ClusterVariableInstance::new));
   }
 
   @Override
   public Optional<ClusterVariableInstance> getGloballyScopedClusterVariable(
       final DirectBuffer variableNameBuffer) {
-    name.wrapBuffer(variableNameBuffer);
-    tenantId.wrapString("");
-    scope.setValue(Scope.GLOBAL);
     return Optional.ofNullable(
         clusterVariablesColumnFamily.get(
-            clusterVariableNameScopeTenantIdCompositeKey, ClusterVariableInstance::new));
+            clusterVariableKey.globalKey(variableNameBuffer), ClusterVariableInstance::new));
   }
 
   @Override
   public boolean existsAtTenantScope(final DirectBuffer variableNameBuffer, final String tenantId) {
-    name.wrapBuffer(variableNameBuffer);
-    this.tenantId.wrapString(tenantId);
-    scope.setValue(Scope.TENANT);
-    return clusterVariablesColumnFamily.exists(clusterVariableNameScopeTenantIdCompositeKey);
+    return clusterVariablesColumnFamily.exists(
+        clusterVariableKey.tenantKey(variableNameBuffer, tenantId));
   }
 
   @Override
   public boolean existsAtGlobalScope(final DirectBuffer variableNameBuffer) {
-    name.wrapBuffer(variableNameBuffer);
-    tenantId.wrapString("");
-    scope.setValue(Scope.GLOBAL);
-    return clusterVariablesColumnFamily.exists(clusterVariableNameScopeTenantIdCompositeKey);
-  }
-
-  enum Scope {
-    GLOBAL,
-    TENANT
+    return clusterVariablesColumnFamily.exists(clusterVariableKey.globalKey(variableNameBuffer));
   }
 }
