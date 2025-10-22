81ddd465b3e725d63b5ecf0a942f1316144dd8f6
Patrick Dehn <patrick.dehn@camunda.com>
2025-10-22 16:45:35 +0200

    fix: include filters for batch operation requests
    (cherry picked from commit 41e3d7f52a1488eef9ae74d7c20c5ab9faa3e84a)

--- operate/client/src/App/Processes/ListView/InstancesTable/Toolbar/buildMutationRequestBody.ts
+++ operate/client/src/App/Processes/ListView/InstancesTable/Toolbar/buildMutationRequestBody.ts
@@ -12,25 +12,30 @@ import type {
   CreateIncidentResolutionBatchOperationRequestBody,
 } from '@camunda/camunda-api-zod-schemas/8.8';
 import {buildProcessInstanceKeyCriterion} from 'modules/mutations/processes/buildProcessInstanceKeyCriterion';
+import {formatToISO} from 'modules/utils/date/formatDate';
 
 type BuildMutationRequestBodyParams = {
   baseFilter: RequestFilters;
   includeIds: string[];
   excludeIds: string[];
-  processDefinitionKey?: string;
 };
 
 const buildMutationRequestBody = ({
   baseFilter,
   includeIds,
   excludeIds,
-  processDefinitionKey,
 }: BuildMutationRequestBodyParams) => {
   const requestBody:
     | CreateIncidentResolutionBatchOperationRequestBody
     | CreateCancellationBatchOperationRequestBody = {
     filter: {
       elementId: baseFilter.activityId,
+      errorMessage: baseFilter.errorMessage,
+      tenantId: baseFilter.tenantId,
+      batchOperationKey: baseFilter.batchOperationId,
+      parentProcessInstanceKey: baseFilter.parentInstanceId,
+      hasRetriesLeft: baseFilter.retriesLeft,
+      incidentErrorHashCode: baseFilter.incidentErrorHashCode,
     },
   };
 
@@ -42,13 +47,54 @@ const buildMutationRequestBody = ({
     requestBody.filter.state = {$eq: 'ACTIVE'};
   }
 
+  if (baseFilter.completed && baseFilter.canceled) {
+    requestBody.filter.state = {$in: ['COMPLETED', 'TERMINATED']};
+  } else if (baseFilter.completed) {
+    requestBody.filter.state = {$eq: 'COMPLETED'};
+  } else if (baseFilter.canceled) {
+    requestBody.filter.state = {$eq: 'TERMINATED'};
+  }
+
+  if (baseFilter.processIds && baseFilter.processIds.length > 0) {
+    requestBody.filter.processDefinitionKey = {
+      $in: baseFilter.processIds,
+    };
+  }
+
+  if (baseFilter.startDateAfter || baseFilter.startDateBefore) {
+    requestBody.filter.startDate = {
+      ...(baseFilter.startDateAfter && {
+        $gt: formatToISO(baseFilter.startDateAfter),
+      }),
+      ...(baseFilter.startDateBefore && {
+        $lt: formatToISO(baseFilter.startDateBefore),
+      }),
+    };
+  }
+
+  if (baseFilter.endDateAfter || baseFilter.endDateBefore) {
+    requestBody.filter.endDate = {
+      ...(baseFilter.endDateAfter && {
+        $gt: formatToISO(baseFilter.endDateAfter),
+      }),
+      ...(baseFilter.endDateBefore && {
+        $lt: formatToISO(baseFilter.endDateBefore),
+      }),
+    };
+  }
+
+  if (baseFilter.variable?.name && baseFilter.variable.values) {
+    requestBody.filter.variables = baseFilter.variable.values.map((value) => {
+      return {
+        name: baseFilter.variable!.name,
+        value,
+      };
+    });
+  }
+
   const keyCriterion = buildProcessInstanceKeyCriterion(includeIds, excludeIds);
   if (keyCriterion) {
     requestBody.filter.processInstanceKey = keyCriterion;
-  }
-
-  if (processDefinitionKey) {
-    requestBody.filter.processDefinitionKey = processDefinitionKey;
   }
 
   return requestBody;
