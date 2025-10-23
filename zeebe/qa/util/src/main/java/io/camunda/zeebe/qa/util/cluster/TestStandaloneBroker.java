c16007bcad6077dc398772db0524b882fc920e34
Lena Schönburg <lena@fairburg.xyz>
2025-10-23 11:36:54 +0200

    test: disable raft flush to save IO
    Flushing mostly helps with crash resiliency - something that we don't
    need during tests because we won't recover from a crashed machine.
    Without a crash, we can rely on the page cache of the kernel, and don't
    need to care whether data was flushed or not.
    
    The only exception to this would be micro-benchmarks but those should
    customize carefully control the entire configuration and, if relevant,
    enable flush anyway.
    
    (cherry picked from commit ad57cea41b265dd2d78e78b6b351d7ce648c6766)

--- zeebe/qa/util/src/main/java/io/camunda/zeebe/qa/util/cluster/TestStandaloneBroker.java
+++ zeebe/qa/util/src/main/java/io/camunda/zeebe/qa/util/cluster/TestStandaloneBroker.java
@@ -27,6 +27,7 @@ import io.camunda.security.configuration.InitializationConfiguration;
 import io.camunda.security.entity.AuthenticationMethod;
 import io.camunda.zeebe.broker.BrokerModuleConfiguration;
 import io.camunda.zeebe.broker.system.configuration.ExporterCfg;
+import io.camunda.zeebe.broker.system.configuration.RaftCfg.FlushConfig;
 import io.camunda.zeebe.gateway.impl.configuration.GatewayCfg;
 import io.camunda.zeebe.qa.util.actuator.BrokerHealthActuator;
 import io.camunda.zeebe.qa.util.actuator.GatewayHealthActuator;
@@ -34,6 +35,7 @@ import io.camunda.zeebe.qa.util.actuator.HealthActuator;
 import io.camunda.zeebe.test.util.record.RecordingExporter;
 import io.camunda.zeebe.test.util.socket.SocketUtil;
 import java.net.URI;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -77,6 +79,9 @@ public final class TestStandaloneBroker extends TestSpringApplication<TestStanda
     config.getData().getDisk().getFreeSpace().setReplication(DataSize.ofMegabytes(64));
     // Disable pre-allocation of segment files - many tests won't even fill up their first segment
     config.getExperimental().getRaft().setPreallocateSegmentFiles(false);
+    // Disable flushes to reduce test runtime - we don't need perfect durability in tests, if the
+    // machine crashes the test fails anyway.
+    config.getCluster().getRaft().setFlush(new FlushConfig(false, Duration.ZERO));
 
     config.getExperimental().getConsistencyChecks().setEnableForeignKeyChecks(true);
     config.getExperimental().getConsistencyChecks().setEnablePreconditions(true);
