4f0a738fec14bd4076c2593c0038ca2a66f55f43
Fabio Paini <fabio.paini@camunda.com>
2025-10-23 14:18:10 +0200

    feat: first implementation of global user task listeners

---
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/ListenerCfg.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.broker.system.configuration.engine;
+
+import io.camunda.zeebe.broker.system.configuration.ConfigurationEntry;
+import io.camunda.zeebe.engine.ListenerConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListenerCfg implements ConfigurationEntry {
+
+  private static final String DEFAULT_RETRIES = "3";
+
+  private List<String> eventTypes = new ArrayList<>();
+  private String type;
+  private String retries = DEFAULT_RETRIES;
+
+  public List<String> getEventTypes() {
+    return eventTypes;
+  }
+
+  public void setEventTypes(final List<String> eventTypes) {
+    this.eventTypes = eventTypes;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(final String type) {
+    this.type = type;
+  }
+
+  public String getRetries() {
+    return retries;
+  }
+
+  public void setRetries(final String retries) {
+    this.retries = retries;
+  }
+
+  public ListenerConfiguration createListenerConfiguration() {
+    return new ListenerConfiguration(eventTypes, type, retries);
+  }
+}
