de08465b0d2d32bba51908952ecd19ecca2d8d06
Jonathan Lukas <jonathan.lukas@camunda.com>
2025-10-29 15:13:39 +0100

    feat: added javadoc comments

--- clients/camunda-spring-boot-starter/src/main/java/io/camunda/client/exception/CamundaError.java
+++ clients/camunda-spring-boot-starter/src/main/java/io/camunda/client/exception/CamundaError.java
@@ -22,7 +22,14 @@ public abstract class CamundaError extends RuntimeException {
   protected CamundaError(final String message, final Throwable cause) {
     super(message, cause);
   }
-
+  /**
+   * Creates a bpmn error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newThrowErrorCommand(long)}
+   * @param errorCode the error code for the throw error command
+   * @param errorMessage the error message for the throw error command
+   * @param variables the variables for the throw error command
+   * @param cause the cause of the issue
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static BpmnError bpmnError(
       final String errorCode,
       final String errorMessage,
@@ -31,28 +38,63 @@ public abstract class CamundaError extends RuntimeException {
     return new BpmnError(errorCode, errorMessage, variables, cause);
   }
 
+  /**
+   * Creates a bpmn error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newThrowErrorCommand(long)}
+   * @param errorCode the error code for the throw error command
+   * @param errorMessage the error message for the throw error command
+   * @param variables the variables for the throw error command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static BpmnError bpmnError(
       final String errorCode, final String errorMessage, final Object variables) {
     return bpmnError(errorCode, errorMessage, variables, null);
   }
 
+  /**
+   * Creates a bpmn error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newThrowErrorCommand(long)}
+   * @param errorCode the error code for the throw error command
+   * @param errorMessage the error message for the throw error command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static BpmnError bpmnError(final String errorCode, final String errorMessage) {
     return bpmnError(errorCode, errorMessage, null, null);
   }
-
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(final String errorMessage) {
     return jobError(errorMessage, null, null, (Duration) null, null);
   }
-
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @param variables the variables for the fail job command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(final String errorMessage, final Object variables) {
     return jobError(errorMessage, variables, null, (Duration) null, null);
   }
-
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @param variables the variables for the fail job command
+   * @param retries the retries for the fail job command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(
       final String errorMessage, final Object variables, final Integer retries) {
     return jobError(errorMessage, variables, retries, (Duration) null, null);
   }
-
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @param variables the variables for the fail job command
+   * @param retries the retries for the fail job command
+   * @param retryBackoff the retry backoff for the fail job command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(
       final String errorMessage,
       final Object variables,
@@ -60,7 +102,15 @@ public abstract class CamundaError extends RuntimeException {
       final Duration retryBackoff) {
     return jobError(errorMessage, variables, retries, retryBackoff, null);
   }
-
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @param variables the variables for the fail job command
+   * @param retries the retries for the fail job command
+   * @param retryBackoff the retry backoff for the fail job command
+   * @param cause the cause of the issue, will be appended to the stacktrace
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(
       final String errorMessage,
       final Object variables,
@@ -69,7 +119,14 @@ public abstract class CamundaError extends RuntimeException {
       final Throwable cause) {
     return new JobError(errorMessage, variables, retries, r -> retryBackoff, cause);
   }
-
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @param variables the variables for the fail job command
+   * @param retries the retries for the fail job command
+   * @param retryBackoff a function that supplies the new retries as parameter and expects the retry backoff for the fail job command
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(
       final String errorMessage,
       final Object variables,
@@ -78,6 +135,15 @@ public abstract class CamundaError extends RuntimeException {
     return jobError(errorMessage, variables, retries, retryBackoff, null);
   }
 
+  /**
+   * Creates a job error from the provided parameters. For more information, see {@link io.camunda.client.CamundaClient#newFailCommand(long)}
+   * @param errorMessage the message for the fail job command
+   * @param variables the variables for the fail job command
+   * @param retries the retries for the fail job command
+   * @param retryBackoff a function that supplies the new retries as parameter and expects the retry backoff for the fail job command
+   * @param cause the cause of the issue
+   * @return the error that can be thrown from inside a {@link io.camunda.client.annotation.JobWorker}
+   */
   public static JobError jobError(
       final String errorMessage,
       final Object variables,
