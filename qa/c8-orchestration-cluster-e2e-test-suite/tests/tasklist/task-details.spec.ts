9ba04ee7441f11f82f28345ac393119bf1a23994
esraagamal6 <esraa-gamal@hotmail.com>
2025-10-30 11:51:04 +0100

    test: add task completion with large variable form e2e test
    (cherry picked from commit 3201221eb4df810b3b281bc34155e52a0daece68)

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/tasklist/task-details.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/tasklist/task-details.spec.ts
@@ -41,6 +41,8 @@ test.beforeAll(async () => {
     './resources/processWithDeployedForm.bpmn',
     './resources/create_invoice.form',
     './resources/zeebe_and_job_worker_process.bpmn',
+    './resources/bigVariableProcessWithForm.bpmn',
+    './resources/bigVariableForm.form',
   ]);
   await sleep(1000);
 
@@ -73,6 +75,7 @@ test.beforeAll(async () => {
     }),
     createInstances('processWithDeployedForm', 1, 1),
     createInstances('zeebe_and_job_worker_process', 1, 1),
+    createInstances('bigVariableProcessWithForm', 1, 1),
   ]);
 
   await sleep(1000);
@@ -574,5 +577,28 @@ test.describe('task details page', () => {
     await taskDetailsPage.processTab.click();
 
     await expect(taskDetailsPage.bpmnDiagram).toBeVisible();
+  });
+
+  test('task completion with large variable form', async ({
+    taskPanelPage,
+    taskDetailsPage,
+    page,
+  }) => {
+    await taskPanelPage.filterBy('Unassigned');
+    await taskPanelPage.openTask('Big Variable Usertask');
+
+    await taskDetailsPage.clickAssignToMeButton();
+    await taskDetailsPage.assertFieldValue(
+      'Process input variable',
+      'name:"Adeel Solangi"',
+      {contains: true},
+    );
+    await taskDetailsPage.assertFieldValue(
+      'Process input variable',
+      'Maecenas quis nisi nunc.", version:2.69',
+      {contains: true},
+    );
+    await taskDetailsPage.clickCompleteTaskButton();
+    await expect(taskDetailsPage.taskCompletedBanner).toBeVisible();
   });
 });
