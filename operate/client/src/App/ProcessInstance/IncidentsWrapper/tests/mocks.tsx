280aca9f64e8aabc2f507dd334182069055f06f0
Christoph Fricke <christoph.fricke@holisticon.de>
2025-11-10 10:13:59 +0100

    test: rework IncidentsWrapper tests for incidents table v2

--- operate/client/src/App/ProcessInstance/IncidentsWrapper/tests/mocks.tsx
+++ operate/client/src/App/ProcessInstance/IncidentsWrapper/tests/mocks.tsx
@@ -8,50 +8,27 @@
 
 import {Route, MemoryRouter, Routes} from 'react-router-dom';
 import {LocationLog} from 'modules/utils/LocationLog';
-import {createIncident} from 'modules/testUtils';
+import {createIncident, createIncidentV2} from 'modules/testUtils';
 import {Paths} from 'modules/Routes';
 import {ProcessDefinitionKeyContext} from 'App/Processes/ListView/processDefinitionKeyContext';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {getMockQueryClient} from 'modules/react-query/mockQueryClient';
+import type {QueryProcessInstanceIncidentsResponseBody} from '@camunda/camunda-api-zod-schemas/8.8';
 
-const mockIncidents = {
-  count: 2,
-  incidents: [
-    createIncident({
-      errorType: {name: 'Condition errortype', id: 'CONDITION_ERROR'},
-      flowNodeId: 'flowNodeId_exclusiveGateway',
-      creationTime: '2022-03-01T14:26:19',
-    }),
-    createIncident({
-      errorType: {name: 'Extract value errortype', id: 'EXTRACT_VALUE_ERROR'},
-      flowNodeId: 'flowNodeId_alwaysFailingTask',
-    }),
-  ],
-  errorTypes: [
-    {
-      id: 'CONDITION_ERROR',
-      name: 'Condition errortype',
-      count: 1,
-    },
-    {
-      id: 'EXTRACT_VALUE_ERROR',
-      name: 'Extract value errortype',
-      count: 1,
-    },
-  ],
+const firstIncident = createIncidentV2({
+  errorType: 'CONDITION_ERROR',
+  creationTime: '2022-03-01T14:26:19',
+  elementId: 'flowNodeId_exclusiveGateway',
+});
 
-  flowNodes: [
-    {
-      id: 'flowNodeId_exclusiveGateway',
-      name: 'flowNodeName_exclusiveGateway',
-      count: 1,
-    },
-    {
-      id: 'flowNodeId_alwaysFailingTask',
-      name: 'flowNodeName_alwaysFailingTask',
-      count: 1,
-    },
-  ],
+const secondIncident = createIncidentV2({
+  errorType: 'EXTRACT_VALUE_ERROR',
+  elementId: 'flowNodeId_alwaysFailingTask',
+});
+
+const mockIncidents: QueryProcessInstanceIncidentsResponseBody = {
+  page: {totalItems: 2},
+  items: [firstIncident, secondIncident],
 };
 
 const mockResolvedIncidents = {
@@ -100,4 +77,10 @@ const Wrapper: React.FC<{children?: React.ReactNode}> = ({children}) => {
   );
 };
 
-export {mockIncidents, mockResolvedIncidents, Wrapper};
+export {
+  mockIncidents,
+  mockResolvedIncidents,
+  firstIncident,
+  secondIncident,
+  Wrapper,
+};
