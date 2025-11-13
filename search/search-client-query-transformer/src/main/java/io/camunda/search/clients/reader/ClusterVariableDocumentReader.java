2ef19f2ab1a3a55abf5714fe8be2f521d5751767
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-11-13 17:52:07 +0100

    feat: Introduce camunda cluster variable in the search client query transformer

---
+++ search/search-client-query-transformer/src/main/java/io/camunda/search/clients/reader/ClusterVariableDocumentReader.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.search.clients.reader;
+
+import io.camunda.search.clients.SearchClientBasedQueryExecutor;
+import io.camunda.search.entities.ClusterVariableEntity;
+import io.camunda.search.query.ClusterVariableQuery;
+import io.camunda.search.query.SearchQueryResult;
+import io.camunda.security.reader.ResourceAccessChecks;
+import io.camunda.webapps.schema.descriptors.IndexDescriptor;
+
+public class ClusterVariableDocumentReader extends DocumentBasedReader
+    implements ClusterVariableReader {
+
+  public ClusterVariableDocumentReader(
+      final SearchClientBasedQueryExecutor executor, final IndexDescriptor indexDescriptor) {
+    super(executor, indexDescriptor);
+  }
+
+  @Override
+  public SearchQueryResult<ClusterVariableEntity> search(
+      final ClusterVariableQuery query, final ResourceAccessChecks resourceAccessChecks) {
+    return getSearchExecutor()
+        .search(
+            query,
+            io.camunda.webapps.schema.entities.clustervariable.ClusterVariableEntity.class,
+            resourceAccessChecks);
+  }
+
+  @Override
+  public ClusterVariableEntity getTenantScopedClusterVariable(
+      final String tenant, final String name) {
+    return getSearchExecutor()
+        .getByQuery(
+            ClusterVariableQuery.of(
+                b -> b.filter(f -> f.names(name).resourceIds(tenant)).singleResult()),
+            io.camunda.webapps.schema.entities.clustervariable.ClusterVariableEntity.class);
+  }
+
+  @Override
+  public ClusterVariableEntity getGloballyScopedClusterVariable(final String name) {
+    return getSearchExecutor()
+        .getByQuery(
+            ClusterVariableQuery.of(b -> b.filter(f -> f.names(name)).singleResult()),
+            io.camunda.webapps.schema.entities.clustervariable.ClusterVariableEntity.class);
+  }
+}
---
+++ search/search-client-query-transformer/src/main/java/io/camunda/search/clients/transformers/filter/ClusterVariableFilterTransformer.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.search.clients.transformers.filter;
+
+import static io.camunda.search.clients.query.SearchQueryBuilders.and;
+import static io.camunda.search.clients.query.SearchQueryBuilders.stringOperations;
+import static io.camunda.search.clients.query.SearchQueryBuilders.stringTerms;
+import static io.camunda.search.clients.query.SearchQueryBuilders.term;
+import static io.camunda.search.clients.query.SearchQueryBuilders.variableOperations;
+import static io.camunda.webapps.schema.descriptors.index.ClusterVariableIndex.IS_PREVIEW;
+import static io.camunda.webapps.schema.descriptors.index.ClusterVariableIndex.NAME;
+
+import io.camunda.search.clients.query.SearchQuery;
+import io.camunda.search.filter.ClusterVariableFilter;
+import io.camunda.search.filter.Operation;
+import io.camunda.search.filter.UntypedOperation;
+import io.camunda.security.auth.Authorization;
+import io.camunda.webapps.schema.descriptors.IndexDescriptor;
+import io.camunda.webapps.schema.descriptors.index.ClusterVariableIndex;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public final class ClusterVariableFilterTransformer
+    extends IndexFilterTransformer<ClusterVariableFilter> {
+
+  public ClusterVariableFilterTransformer(final IndexDescriptor indexDescriptor) {
+    super(indexDescriptor);
+  }
+
+  @Override
+  protected SearchQuery toAuthorizationCheckSearchQuery(final Authorization<?> authorization) {
+    return stringTerms(ClusterVariableIndex.NAME, authorization.resourceIds());
+  }
+
+  @Override
+  public SearchQuery toSearchQuery(final ClusterVariableFilter filter) {
+    final var queries = new ArrayList<SearchQuery>();
+    queries.addAll(stringOperations(NAME, filter.nameOperations()));
+    queries.addAll(getVariablesQuery(filter.valueOperations()));
+    queries.addAll(getScopeQuery(filter.scopeOperations()));
+    queries.addAll(getResourceIdQuery(filter.resourceIdOperations()));
+    return and(queries);
+  }
+
+  private Collection<SearchQuery> getResourceIdQuery(final List<Operation<String>> operations) {
+    return stringOperations(ClusterVariableIndex.SCOPE, operations);
+  }
+
+  private Collection<SearchQuery> getScopeQuery(final List<Operation<String>> operations) {
+    return stringOperations(ClusterVariableIndex.SCOPE, operations);
+  }
+
+  private List<SearchQuery> getVariablesQuery(final List<UntypedOperation> variableFilters) {
+    return variableOperations(ClusterVariableIndex.VALUE, variableFilters);
+  }
+
+  private SearchQuery getIsTruncatedQuery(final Boolean isTruncated) {
+    if (isTruncated == null) {
+      return null;
+    }
+    return term(IS_PREVIEW, isTruncated);
+  }
+}
