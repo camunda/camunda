53c03a6370fe06835ad151fa0b5ff0750ada0ccb
Nikola Koevski <nikola.koevski@camunda.com>
2025-11-13 18:58:47 +0100

    refactor: job complete processor implements typed record processor
    Remove usages of the CommandProcessor interface so that
    it can be removed. The CommandProcessor interface is
    is difficult to understand and is a frequent source of
    processor-related bugs.
    
    This commit also removes the DefaultJobPreconditionGuard
    class and JobAccept interfaces as they aren't used anymore.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/job/JobCompleteProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/job/JobCompleteProcessor.java
@@ -10,57 +10,83 @@ package io.camunda.zeebe.engine.processing.job;
 import io.camunda.zeebe.engine.metrics.EngineMetricsDoc.JobAction;
 import io.camunda.zeebe.engine.metrics.JobProcessingMetrics;
 import io.camunda.zeebe.engine.processing.common.EventHandle;
-import io.camunda.zeebe.engine.processing.streamprocessor.CommandProcessor;
+import io.camunda.zeebe.engine.processing.streamprocessor.TypedRecordProcessor;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.StateWriter;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedCommandWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedRejectionWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.TypedResponseWriter;
+import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.immutable.ElementInstanceState;
 import io.camunda.zeebe.engine.state.immutable.JobState;
+import io.camunda.zeebe.engine.state.immutable.JobState.State;
 import io.camunda.zeebe.engine.state.immutable.ProcessingState;
 import io.camunda.zeebe.engine.state.instance.ElementInstance;
 import io.camunda.zeebe.protocol.impl.record.value.job.JobRecord;
-import io.camunda.zeebe.protocol.record.RejectionType;
-import io.camunda.zeebe.protocol.record.intent.Intent;
 import io.camunda.zeebe.protocol.record.intent.JobIntent;
 import io.camunda.zeebe.protocol.record.intent.ProcessInstanceIntent;
 import io.camunda.zeebe.protocol.record.value.JobKind;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
+import java.util.List;
 
-public final class JobCompleteProcessor implements CommandProcessor<JobRecord> {
-
-  private static final String NO_JOB_FOUND_MESSAGE =
-      "Expected to update retries for job with key '%d', but no such job was found";
+public final class JobCompleteProcessor implements TypedRecordProcessor<JobRecord> {
 
   private final JobState jobState;
   private final ElementInstanceState elementInstanceState;
-  private final DefaultJobCommandPreconditionGuard defaultProcessor;
+  private final JobCommandPreconditionChecker preconditionChecker;
   private final JobProcessingMetrics jobMetrics;
   private final EventHandle eventHandle;
 
+  private final StateWriter stateWriter;
+  private final TypedCommandWriter commandWriter;
+  private final TypedResponseWriter responseWriter;
+  private final TypedRejectionWriter rejectionWriter;
+
   public JobCompleteProcessor(
       final ProcessingState state,
+      final Writers writers,
       final JobProcessingMetrics jobMetrics,
       final EventHandle eventHandle) {
     jobState = state.getJobState();
     elementInstanceState = state.getElementInstanceState();
-    defaultProcessor =
-        new DefaultJobCommandPreconditionGuard("complete", jobState, this::acceptCommand);
+    commandWriter = writers.command();
+    stateWriter = writers.state();
+    responseWriter = writers.response();
+    rejectionWriter = writers.rejection();
+    preconditionChecker =
+        new JobCommandPreconditionChecker("complete", List.of(State.ACTIVATABLE, State.ACTIVATED));
     this.jobMetrics = jobMetrics;
     this.eventHandle = eventHandle;
   }
 
   @Override
-  public boolean onCommand(
-      final TypedRecord<JobRecord> command, final CommandControl<JobRecord> commandControl) {
-    return defaultProcessor.onCommand(command, commandControl);
+  public void processRecord(final TypedRecord<JobRecord> record) {
+    final long jobKey = record.getKey();
+    final JobState.State state = jobState.getState(jobKey);
+
+    preconditionChecker
+        .check(state, jobKey)
+        .ifRightOrLeft(
+            ignored -> completeJob(record),
+            rejection -> {
+              rejectionWriter.appendRejection(record, rejection.getLeft(), rejection.getRight());
+              responseWriter.writeRejectionOnCommand(
+                  record, rejection.getLeft(), rejection.getRight());
+            });
   }
 
-  @Override
-  public void afterAccept(
-      final TypedCommandWriter commandWriter,
-      final StateWriter stateWriter,
-      final long key,
-      final Intent intent,
-      final JobRecord value) {
+  private void completeJob(final TypedRecord<JobRecord> command) {
+    final var job = jobState.getJob(command.getKey(), command.getAuthorizations());
+    job.setVariables(command.getValue().getVariablesBuffer());
+
+    stateWriter.appendFollowUpEvent(command.getKey(), JobIntent.COMPLETED, job);
+    responseWriter.writeEventOnCommand(command.getKey(), JobIntent.COMPLETED, job, command);
+
+    jobMetrics.countJobEvent(JobAction.COMPLETED, job.getJobKind(), job.getType());
+
+    postCompleteActions(job);
+  }
+
+  private void postCompleteActions(final JobRecord value) {
 
     final var serviceTaskKey = value.getElementInstanceKey();
 
@@ -87,22 +113,5 @@ public final class JobCompleteProcessor implements CommandProcessor<JobRecord> {
             serviceTaskKey, ProcessInstanceIntent.COMPLETE_ELEMENT, serviceTask.getValue());
       }
     }
-  }
-
-  private void acceptCommand(
-      final TypedRecord<JobRecord> command, final CommandControl<JobRecord> commandControl) {
-
-    final long jobKey = command.getKey();
-
-    final JobRecord job = jobState.getJob(jobKey, command.getAuthorizations());
-    if (job == null) {
-      commandControl.reject(RejectionType.NOT_FOUND, String.format(NO_JOB_FOUND_MESSAGE, jobKey));
-      return;
-    }
-
-    job.setVariables(command.getValue().getVariablesBuffer());
-
-    commandControl.accept(JobIntent.COMPLETED, job);
-    jobMetrics.countJobEvent(JobAction.COMPLETED, job.getJobKind(), job.getType());
   }
 }
