36541ac0db4be8ab1cf428c976a8879d76446852
Mathias Vandaele <mathias.vandaele@camunda.com>
2025-11-13 17:49:22 +0100

    feat: Introduce camunda cluster variable in schema manager

--- schema-manager/src/test/java/io/camunda/search/schema/SchemaManagerIT.java
+++ schema-manager/src/test/java/io/camunda/search/schema/SchemaManagerIT.java
@@ -1000,6 +1000,7 @@ public class SchemaManagerIT {
         // to make sure no index will be accidentally dropped, names are changed or added
         .containsExactlyInAnyOrder(
             newPrefix + "-camunda-authorization-8.8.0_",
+            newPrefix + "-camunda-cluster-variable-8.9.0_",
             newPrefix + "-camunda-correlated-message-subscription-8.8.0_",
             newPrefix + "-camunda-group-8.8.0_",
             newPrefix + "-camunda-mapping-rule-8.8.0_",
