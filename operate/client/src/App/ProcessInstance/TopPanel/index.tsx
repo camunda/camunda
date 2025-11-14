f315180144416c05e2d49b9f940627cad019c4d2
OmranAbazid <9063094+OmranAbazid@users.noreply.github.com>
2025-11-14 10:32:01 +0100

    feat: add support for flownodes in adhoc modification

--- operate/client/src/App/ProcessInstance/TopPanel/index.tsx
+++ operate/client/src/App/ProcessInstance/TopPanel/index.tsx
@@ -273,7 +273,6 @@ const TopPanel: React.FC = observer(() => {
     return 'content';
   };
 
-  console.log(modificationsStore.state.status);
   const {data: totalRunningInstancesByFlowNode} =
     useTotalRunningInstancesByFlowNode();
 
