2fed1172ded843d712b9ace6157159087582b8c9
Stefan Zilske <stefan.zilske@holisticon.de>
2025-11-17 16:20:59 +0100

    feat: Terminate connections before drop

--- .github/actions/rdbms/drop-aurora-database.sh
+++ .github/actions/rdbms/drop-aurora-database.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Terminate all connections to the database
+psql postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '${PGDATABASE}' AND pid <> pg_backend_pid();"
+
+# Drop the database
 psql postgres -c "DROP DATABASE IF EXISTS \"${PGDATABASE}\";"
