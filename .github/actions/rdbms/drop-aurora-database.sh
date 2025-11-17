3ee0ffb841486bd2c7013e48e481b9427abb2945
Stefan Zilske <stefan.zilske@holisticon.de>
2025-11-17 17:01:04 +0100

    feat: force drop database

--- .github/actions/rdbms/drop-aurora-database.sh
+++ .github/actions/rdbms/drop-aurora-database.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Terminate all connections to the database
-psql postgres -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '${PGDATABASE}' AND pid <> pg_backend_pid();"
+# Drop the database, forcefully terminating any connections
+psql postgres -c "DROP DATABASE IF EXISTS \"${PGDATABASE}\" WITH (FORCE);"
 
-# Drop the database
-psql postgres -c "DROP DATABASE IF EXISTS \"${PGDATABASE}\";"
