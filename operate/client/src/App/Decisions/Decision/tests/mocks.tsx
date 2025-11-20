9966fa9685f11ec1900196129dc2c9223270d6ab
Christoph Fricke <christoph.fricke@holisticon.de>
2025-11-20 14:19:03 +0100

    test: update unit tests for migrated decision definitions

--- operate/client/src/App/Decisions/Decision/tests/mocks.tsx
+++ operate/client/src/App/Decisions/Decision/tests/mocks.tsx
@@ -6,25 +6,13 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {groupedDecisionsStore} from 'modules/stores/groupedDecisions';
 import {MemoryRouter} from 'react-router-dom';
-import {useEffect} from 'react';
-import {authenticationStore} from 'modules/stores/authentication';
 import {Paths} from 'modules/Routes';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {getMockQueryClient} from 'modules/react-query/mockQueryClient';
 
 function createWrapper(initialPath: string = Paths.dashboard()) {
   const Wrapper: React.FC<{children?: React.ReactNode}> = ({children}) => {
-    useEffect(() => {
-      groupedDecisionsStore.fetchDecisions();
-
-      return () => {
-        groupedDecisionsStore.reset();
-        authenticationStore.reset();
-      };
-    }, []);
-
     return (
       <QueryClientProvider client={getMockQueryClient()}>
         <MemoryRouter initialEntries={[initialPath]}>{children}</MemoryRouter>
