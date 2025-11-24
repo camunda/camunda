5f001a6f0cd28eb0dc5fc282925403db7fec264c
Vinicius Goulart <vinicius.goulart@camunda.com>
2025-11-25 00:58:05 +0100

    feat: add latest migration date to element instance history v2

--- operate/client/src/App/ProcessInstance/FlowNodeInstanceLog/FlowNodeInstancesTree/v2/index.new.tsx
+++ operate/client/src/App/ProcessInstance/FlowNodeInstanceLog/FlowNodeInstancesTree/v2/index.new.tsx
@@ -33,6 +33,7 @@ import {modificationsStore} from 'modules/stores/modifications';
 import type {FlowNodeInstance} from 'modules/stores/flowNodeInstance';
 import {VirtualBar} from './VirtualBar';
 import {useBusinessObjects} from 'modules/queries/processDefinitions/useBusinessObjects';
+import {useBatchOperations} from 'modules/queries/batch-operations/useBatchOperations';
 
 const TREE_NODE_HEIGHT = 32;
 const FOLDABLE_ELEMENT_TYPES: ElementInstance['type'][] = [
@@ -74,6 +75,7 @@ const ElementInstanceHistoryTree = createContext<{
   processInstance: ProcessInstance;
   scrollableContainerRef: React.RefObject<HTMLDivElement | null>;
   businessObjects: BusinessObjects;
+  latestMigrationDate: string | undefined;
 } | null>(null);
 
 const useElementInstanceHistoryTree = () => {
@@ -113,7 +115,7 @@ const NonFoldableElementInstancesNode: React.FC<NonFoldableElementInstancesNodeP
       ...rest
     }) => {
       const rowRef = useRef<HTMLDivElement>(null);
-      const latestMigrationDate = undefined;
+      const {latestMigrationDate} = useElementInstanceHistoryTree();
       const isRoot = elementType === 'PROCESS';
 
       const rootNode = useRootNode();
@@ -422,8 +424,12 @@ const FoldableElementInstancesNode: React.FC<FoldableElementInstancesNodeProps> 
       ...carbonTreeNodeProps
     }) => {
       const rowRef = useRef<HTMLDivElement>(null);
-      const {scrollableContainerRef, businessObjects, processInstance} =
-        useElementInstanceHistoryTree();
+      const {
+        scrollableContainerRef,
+        businessObjects,
+        processInstance,
+        latestMigrationDate,
+      } = useElementInstanceHistoryTree();
       const {refetch: fetchFirstChild} = useSearchElementInstancesByScope(
         {
           elementInstanceScopeKey: scopeKey,
@@ -432,7 +438,6 @@ const FoldableElementInstancesNode: React.FC<FoldableElementInstancesNodeProps> 
         },
         {enabled: false},
       );
-      const latestMigrationDate = undefined;
       const isRoot = elementType === 'PROCESS';
 
       const rootNode = useRootNode();
@@ -685,6 +690,15 @@ const ElementInstancesTree: React.FC<ElementInstancesTreeProps> = observer(
   (props) => {
     const {processInstance, scrollableContainerRef, ...rest} = props;
     const {data: businessObjects} = useBusinessObjects();
+    const {data: {items: operations} = {items: []}} = useBatchOperations({
+      filter: {
+        operationType: 'MIGRATE_PROCESS_INSTANCE',
+      },
+      sort: [{field: 'endDate', order: 'desc'}],
+      page: {limit: 1, from: 0},
+    });
+    const latestMigrationDate =
+      operations.length > 0 ? operations[0].endDate : undefined;
     const rootElementInstance = useMemo<ElementInstance>(() => {
       const {
         processInstanceKey,
@@ -715,6 +729,7 @@ const ElementInstancesTree: React.FC<ElementInstancesTreeProps> = observer(
           processInstance,
           scrollableContainerRef,
           businessObjects,
+          latestMigrationDate,
         }}
       >
         <ElementInstanceSubTreeRoot
