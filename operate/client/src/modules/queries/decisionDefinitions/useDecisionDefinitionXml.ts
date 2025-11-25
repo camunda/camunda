4a21262df3ef953c75941b855030edca19e1c20a
Christoph Fricke <christoph.fricke@holisticon.de>
2025-11-25 11:57:54 +0100

    refactor: change decision-definition-xml hook to use skipToken and wrapped useQuery

--- operate/client/src/modules/queries/decisionDefinitions/useDecisionDefinitionXml.ts
+++ operate/client/src/modules/queries/decisionDefinitions/useDecisionDefinitionXml.ts
@@ -6,28 +6,40 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {fetchDecisionDefinitionXml} from 'modules/api/v2/decisionDefinitions/fetchDecisionDefinitionXml';
-import {genericQueryOptions} from '../genericQuery';
+import {skipToken, useQuery} from '@tanstack/react-query';
+import {queryKeys} from '../queryKeys';
 import {type DecisionDefinition} from '@camunda/camunda-api-zod-schemas/8.8';
+import {fetchDecisionDefinitionXml} from 'modules/api/v2/decisionDefinitions/fetchDecisionDefinitionXml';
+import type {RequestError} from 'modules/request';
 
-function useDecisionDefinitionXmlOptions({
-  decisionDefinitionKey,
-  enabled,
-}: {
-  decisionDefinitionKey: DecisionDefinition['decisionDefinitionKey'];
+type QueryOptions<T> = {
+  decisionDefinitionKey?: DecisionDefinition['decisionDefinitionKey'];
   enabled?: boolean;
-}) {
-  const queryKey = ['DecisionDefinitionXml', decisionDefinitionKey];
+  select?: (result: string) => T;
+};
 
-  return genericQueryOptions(
-    queryKey,
-    () => fetchDecisionDefinitionXml(decisionDefinitionKey),
-    {
-      queryKey,
-      enabled,
-      staleTime: 'static',
-    },
-  );
+function useDecisionDefinitionXml<T = string>(options?: QueryOptions<T>) {
+  const decisionDefinitionKey = options?.decisionDefinitionKey;
+
+  return useQuery<string, RequestError, T>({
+    queryKey: queryKeys.decisionDefinitionXml.get(decisionDefinitionKey),
+    staleTime: 'static',
+    enabled: options?.enabled,
+    select: options?.select,
+    queryFn: !decisionDefinitionKey
+      ? skipToken
+      : async () => {
+          const {response, error} = await fetchDecisionDefinitionXml(
+            decisionDefinitionKey,
+          );
+
+          if (response !== null) {
+            return response;
+          }
+
+          throw error;
+        },
+  });
 }
 
-export {useDecisionDefinitionXmlOptions};
+export {useDecisionDefinitionXml};
