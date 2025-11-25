f65adcfc0276c4d2bd688118611b9994369cbb40
Carlo Sana <carlo.sana@camunda.com>
2025-11-25 12:41:48 +0100

    feat: client/server can be configured w/o sending payload for hearbeats

--- zeebe/atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
+++ zeebe/atomix/cluster/src/main/java/io/atomix/cluster/messaging/impl/NettyMessagingService.java
@@ -147,6 +147,7 @@ public final class NettyMessagingService implements ManagedMessagingService {
 
   // flag for passing heartbeats down the pipeline
   private boolean forwardHeartbeats = false;
+  private boolean sendHeartbeatPayload = true;
   private boolean heartbeatsEnabled = true;
 
   public NettyMessagingService(
@@ -955,6 +956,11 @@ public final class NettyMessagingService implements ManagedMessagingService {
     heartbeatsEnabled = false;
   }
 
+  @VisibleForTesting
+  void noHeartbeatPayload() {
+    sendHeartbeatPayload = false;
+  }
+
   /** Channel initializer for basic connections. */
   private class BasicClientChannelInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -1157,7 +1163,8 @@ public final class NettyMessagingService implements ManagedMessagingService {
                     advertisedAddress,
                     config.getHeartbeatTimeout(),
                     config.getHeartbeatInterval(),
-                    forwardHeartbeats));
+                    forwardHeartbeats,
+                    sendHeartbeatPayload));
       }
       future.complete(context.channel());
     }
@@ -1206,7 +1213,8 @@ public final class NettyMessagingService implements ManagedMessagingService {
             .addBefore(
                 MESSAGE_DISPATCHER_NAME,
                 "heartbeat-setup",
-                new HeartbeatSetupHandler.Server("decoder", log, forwardHeartbeats));
+                new HeartbeatSetupHandler.Server(
+                    "decoder", log, forwardHeartbeats, sendHeartbeatPayload));
       }
     }
   }
