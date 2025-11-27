85d614ac4d530282af92027cb24e16351bad1b69
Stefan Zilske <stefan.zilske@holisticon.de>
2025-11-27 08:56:49 +0100

    docs: Fix description

--- .github/actions/rdbms/create-aurora-database.sh
+++ .github/actions/rdbms/create-aurora-database.sh
@@ -3,7 +3,6 @@
 psql postgres -c "DROP DATABASE IF EXISTS \"${PGDATABASE}\";" \
   -c "CREATE DATABASE \"${PGDATABASE}\";"
 
-# see https://github.com/camunda/infra-core/tree/opensearch-cluster/camunda-opensearch#user-setup
 psql postgres -c "CREATE USER \"${PGUSER_IRSA}\";" \
   -c "GRANT rds_iam TO \"${PGUSER_IRSA}\";" \
   -c "GRANT rds_superuser TO \"${PGUSER_IRSA}\";" \
