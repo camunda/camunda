fad0355c34a6aca379e1e1943dea0087841d518f
berkaycanbc <berkay.can@camunda.com>
2025-11-27 17:14:51 +0100

    feat: create subscription for root-level conditional start events

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/StartEventSubscriptionManager.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/deployment/StartEventSubscriptionManager.java
@@ -8,6 +8,7 @@
 package io.camunda.zeebe.engine.processing.deployment;
 
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableCatchEventElement;
+import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableConditional;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableMessage;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableProcess;
 import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableSignal;
@@ -18,10 +19,12 @@ import io.camunda.zeebe.engine.state.immutable.MessageStartEventSubscriptionStat
 import io.camunda.zeebe.engine.state.immutable.ProcessState;
 import io.camunda.zeebe.engine.state.immutable.ProcessingState;
 import io.camunda.zeebe.engine.state.immutable.SignalSubscriptionState;
+import io.camunda.zeebe.protocol.impl.record.value.conditional.ConditionalSubscriptionRecord;
 import io.camunda.zeebe.protocol.impl.record.value.deployment.DeploymentRecord;
 import io.camunda.zeebe.protocol.impl.record.value.deployment.ProcessMetadata;
 import io.camunda.zeebe.protocol.impl.record.value.message.MessageStartEventSubscriptionRecord;
 import io.camunda.zeebe.protocol.impl.record.value.signal.SignalSubscriptionRecord;
+import io.camunda.zeebe.protocol.record.intent.ConditionalSubscriptionIntent;
 import io.camunda.zeebe.protocol.record.intent.MessageStartEventSubscriptionIntent;
 import io.camunda.zeebe.protocol.record.intent.SignalSubscriptionIntent;
 import io.camunda.zeebe.stream.api.state.KeyGenerator;
@@ -34,6 +37,8 @@ public class StartEventSubscriptionManager {
   private final MessageStartEventSubscriptionRecord messageSubscriptionRecord =
       new MessageStartEventSubscriptionRecord();
   private final SignalSubscriptionRecord signalSubscriptionRecord = new SignalSubscriptionRecord();
+  private final ConditionalSubscriptionRecord conditionalSubscriptionRecord =
+      new ConditionalSubscriptionRecord();
 
   private final ProcessState processState;
   private final MessageStartEventSubscriptionState messageStartEventSubscriptionState;
@@ -150,6 +155,8 @@ public class StartEventSubscriptionManager {
         openMessageStartEventSubscription(processDefinition, startEvent);
       } else if (startEvent.isSignal()) {
         openSignalStartEventSubscription(processDefinition, startEvent);
+      } else if (startEvent.isConditional()) {
+        openConditionalStartEventSubscription(processDefinition, startEvent);
       }
     }
   }
@@ -165,6 +172,8 @@ public class StartEventSubscriptionManager {
                 openMessageStartEventSubscription(deployedProcess, startEvent);
               } else if (startEvent.isSignal()) {
                 openSignalStartEventSubscription(deployedProcess, startEvent);
+              } else if (startEvent.isConditional()) {
+                openConditionalStartEventSubscription(deployedProcess, startEvent);
               }
             });
   }
@@ -215,5 +224,22 @@ public class StartEventSubscriptionManager {
               stateWriter.appendFollowUpEvent(
                   subscriptionKey, SignalSubscriptionIntent.CREATED, signalSubscriptionRecord);
             });
+  }
+
+  private void openConditionalStartEventSubscription(
+      final DeployedProcess processDefinition, final ExecutableStartEvent startEvent) {
+    final ExecutableConditional conditional = startEvent.getConditional();
+
+    conditionalSubscriptionRecord.reset();
+    conditionalSubscriptionRecord
+        .setProcessDefinitionKey(processDefinition.getKey())
+        .setCatchEventId(startEvent.getId())
+        .setCondition(BufferUtil.wrapString(conditional.getCondition()))
+        .setVariableNames(conditional.getVariableNames())
+        .setTenantId(processDefinition.getTenantId());
+
+    final var subscriptionKey = keyGenerator.nextKey();
+    stateWriter.appendFollowUpEvent(
+        subscriptionKey, ConditionalSubscriptionIntent.CREATED, conditionalSubscriptionRecord);
   }
 }
