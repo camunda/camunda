3e0c4c2372dd8acaabd87f54da4bca9275c4b496
OmranAbazid <9063094+OmranAbazid@users.noreply.github.com>
2025-11-28 01:53:30 +0100

    feat: add support for move modifications in multi-instance scenarios
    closes #41514

--- operate/client/src/App/ProcessInstance/TopPanel/index.tsx
+++ operate/client/src/App/ProcessInstance/TopPanel/index.tsx
@@ -46,6 +46,7 @@ import {
   selectFlowNode,
 } from 'modules/utils/flowNodeSelection';
 import {
+  useTotalRunningInstancesByFlowNode,
   useTotalRunningInstancesForFlowNode,
   useTotalRunningInstancesVisibleForFlowNode,
 } from 'modules/queries/flownodeInstancesStatistics/useTotalRunningInstancesForFlowNode';
@@ -72,6 +73,7 @@ import {isRequestError} from 'modules/request';
 import {useProcessInstanceIncidentsCount} from 'modules/queries/incidents/useProcessInstanceIncidentsCount';
 import {incidentsPanelStore} from 'modules/stores/incidentsPanel';
 import {isInstanceRunning} from 'modules/utils/instance';
+import {hasMultipleScopes} from 'modules/utils/processInstanceDetailsDiagram';
 
 const OVERLAY_TYPE_STATE = 'flowNodeState';
 const OVERLAY_TYPE_MODIFICATIONS_BADGE = 'modificationsBadge';
@@ -106,6 +108,8 @@ const TopPanel: React.FC = observer(() => {
   const {data: totalRunningInstances} = useTotalRunningInstancesForFlowNode(
     currentSelection?.flowNodeId,
   );
+  const {data: totalRunningInstancesByFlowNode} =
+    useTotalRunningInstancesByFlowNode();
   const {data: businessObjects} = useBusinessObjects();
   const {data: totalMoveOperationRunningInstances} =
     useTotalRunningInstancesForFlowNode(
@@ -291,6 +295,10 @@ const TopPanel: React.FC = observer(() => {
           isOpen={isIncidentBarOpen}
         />
       )}
+
+      {modificationsStore.state.status === 'requires-ancestor-selection' && (
+        <ModificationInfoBanner text="Target flow node has multiple parent scopes. Please select parent node from Instance History to move." />
+      )}
       {modificationsStore.state.status === 'moving-token' &&
         businessObjects && (
           <ModificationInfoBanner
@@ -347,6 +355,11 @@ const TopPanel: React.FC = observer(() => {
                 }
                 onFlowNodeSelection={(flowNodeId, isMultiInstance) => {
                   if (modificationsStore.state.status === 'moving-token') {
+                    const ancestorSelectionRequired = hasMultipleScopes(
+                      businessObjects[flowNodeId ?? ''],
+                      totalRunningInstancesByFlowNode,
+                    );
+
                     clearSelection(rootNode);
                     finishMovingToken(
                       affectedTokenCount,
@@ -354,6 +367,7 @@ const TopPanel: React.FC = observer(() => {
                       businessObjects,
                       processInstance?.processDefinitionId,
                       flowNodeId,
+                      ancestorSelectionRequired,
                     );
                   } else {
                     if (modificationsStore.state.status !== 'adding-token') {
