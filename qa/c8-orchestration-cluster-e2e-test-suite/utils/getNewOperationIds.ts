662dc774292f9c761b4f6a52680aabd0efb330b2
Catherina Prokudina <catherina.prokudina@camunda.com>
2025-11-28 15:38:23 +0100

    test: update search for new operation in auto mapping test

---
+++ qa/c8-orchestration-cluster-e2e-test-suite/utils/getNewOperationIds.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+export type OperationEntry = {id: string; type: string};
+
+export function getNewOperationIds(
+  before: OperationEntry[],
+  after: OperationEntry[],
+): string[] {
+  const beforeIds = new Set(before.map((entry) => entry.id));
+
+  return after
+    .filter((entry) => !beforeIds.has(entry.id))
+    .map((entry) => entry.id);
+}
