94658242dfa08f2fb8132924086b1dcf74f508a1
Carlo Sana <carlo.sana@camunda.com>
2025-12-03 11:08:49 +0100

    feat: add factory method for records in UnifiedRecorValue
    Moves the logic to map from a ValueType to a Record in
    UnifiedRecordValue.
    This makes adding a new record just limited to the protocol-impl module,
    avoiding the codeowners of zeebe/broker to review the change.
    
    There are other advantages:
    - this logic is centralized in case it needs to be used by other modules
    - using a switch we get a compiler warning if a record is missing
    - a tests verifies that every value type is mapped correctly
    - we verify that the naming is consistent across all value types
    
    (cherry picked from commit cab50db9e3f3c000fe8ec2da9167b863a5b6b7cf)

--- zeebe/protocol-impl/src/main/java/io/camunda/zeebe/protocol/impl/record/UnifiedRecordValue.java
+++ zeebe/protocol-impl/src/main/java/io/camunda/zeebe/protocol/impl/record/UnifiedRecordValue.java
@@ -10,7 +10,48 @@ package io.camunda.zeebe.protocol.impl.record;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.camunda.zeebe.msgpack.UnpackedObject;
 import io.camunda.zeebe.protocol.impl.encoding.MsgPackConverter;
+import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.clock.ClockRecord;
+import io.camunda.zeebe.protocol.impl.record.value.compensation.CompensationSubscriptionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.decision.DecisionEvaluationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.DecisionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.DecisionRequirementsRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.DeploymentDistributionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.DeploymentRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.FormRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.ProcessRecord;
+import io.camunda.zeebe.protocol.impl.record.value.deployment.ResourceRecord;
+import io.camunda.zeebe.protocol.impl.record.value.distribution.CommandDistributionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.error.ErrorRecord;
+import io.camunda.zeebe.protocol.impl.record.value.escalation.EscalationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.incident.IncidentRecord;
+import io.camunda.zeebe.protocol.impl.record.value.job.JobBatchRecord;
+import io.camunda.zeebe.protocol.impl.record.value.job.JobRecord;
+import io.camunda.zeebe.protocol.impl.record.value.management.CheckpointRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageBatchRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageCorrelationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageStartEventSubscriptionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.MessageSubscriptionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.message.ProcessMessageSubscriptionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.multiinstance.MultiInstanceRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessEventRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceBatchRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceMigrationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceModificationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceResultRecord;
+import io.camunda.zeebe.protocol.impl.record.value.resource.ResourceDeletionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.signal.SignalRecord;
+import io.camunda.zeebe.protocol.impl.record.value.signal.SignalSubscriptionRecord;
+import io.camunda.zeebe.protocol.impl.record.value.timer.TimerRecord;
+import io.camunda.zeebe.protocol.impl.record.value.user.UserRecord;
+import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
+import io.camunda.zeebe.protocol.impl.record.value.variable.VariableDocumentRecord;
+import io.camunda.zeebe.protocol.impl.record.value.variable.VariableRecord;
 import io.camunda.zeebe.protocol.record.RecordValue;
+import io.camunda.zeebe.protocol.record.ValueType;
 
 public class UnifiedRecordValue extends UnpackedObject implements RecordValue {
 
@@ -45,5 +86,52 @@ public class UnifiedRecordValue extends UnpackedObject implements RecordValue {
   @Override
   public String toJson() {
     return MsgPackConverter.convertJsonSerializableObjectToJson(this);
+  }
+
+  public static UnifiedRecordValue fromValueType(final ValueType valueType) {
+    return switch (valueType) {
+      case ValueType.DEPLOYMENT -> new DeploymentRecord();
+      case ValueType.JOB -> new JobRecord();
+      case ValueType.PROCESS_INSTANCE -> new ProcessInstanceRecord();
+      case ValueType.MESSAGE -> new MessageRecord();
+      case ValueType.MESSAGE_BATCH -> new MessageBatchRecord();
+      case PROCESS_MESSAGE_SUBSCRIPTION -> new ProcessMessageSubscriptionRecord();
+      case ValueType.JOB_BATCH -> new JobBatchRecord();
+      case ValueType.INCIDENT -> new IncidentRecord();
+      case TIMER -> new TimerRecord();
+      case MESSAGE_START_EVENT_SUBSCRIPTION -> new MessageStartEventSubscriptionRecord();
+      case VARIABLE -> new VariableRecord();
+      case ValueType.VARIABLE_DOCUMENT -> new VariableDocumentRecord();
+      case ValueType.PROCESS_INSTANCE_CREATION -> new ProcessInstanceCreationRecord();
+      case ERROR -> new ErrorRecord();
+      case PROCESS_INSTANCE_RESULT -> new ProcessInstanceResultRecord();
+      case PROCESS -> new ProcessRecord();
+      case DEPLOYMENT_DISTRIBUTION -> new DeploymentDistributionRecord();
+      case PROCESS_EVENT -> new ProcessEventRecord();
+      case DECISION -> new DecisionRecord();
+      case DECISION_REQUIREMENTS -> new DecisionRequirementsRecord();
+      case ValueType.DECISION_EVALUATION -> new DecisionEvaluationRecord();
+      case ValueType.PROCESS_INSTANCE_MODIFICATION -> new ProcessInstanceModificationRecord();
+      case ESCALATION -> new EscalationRecord();
+      case SIGNAL_SUBSCRIPTION -> new SignalSubscriptionRecord();
+      case ValueType.SIGNAL -> new SignalRecord();
+      case ValueType.COMMAND_DISTRIBUTION -> new CommandDistributionRecord();
+      case ValueType.PROCESS_INSTANCE_BATCH -> new ProcessInstanceBatchRecord();
+      case ValueType.RESOURCE_DELETION -> new ResourceDeletionRecord();
+      case FORM -> new FormRecord();
+      case ValueType.USER_TASK -> new UserTaskRecord();
+      case ValueType.PROCESS_INSTANCE_MIGRATION -> new ProcessInstanceMigrationRecord();
+      case ValueType.COMPENSATION_SUBSCRIPTION -> new CompensationSubscriptionRecord();
+      case ValueType.MESSAGE_CORRELATION -> new MessageCorrelationRecord();
+      case ValueType.USER -> new UserRecord();
+      case ValueType.CLOCK -> new ClockRecord();
+      case ValueType.AUTHORIZATION -> new AuthorizationRecord();
+      case ValueType.RESOURCE -> new ResourceRecord();
+      case ValueType.MULTI_INSTANCE -> new MultiInstanceRecord();
+      case ValueType.CHECKPOINT -> new CheckpointRecord();
+      case ValueType.MESSAGE_SUBSCRIPTION -> new MessageSubscriptionRecord();
+      case ValueType.SBE_UNKNOWN -> null;
+      case ValueType.NULL_VAL -> null;
+    };
   }
 }
