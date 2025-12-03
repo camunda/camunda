ed8c08182c62307a4f727a814b52e1da934a7356
Carlo Sana <carlo.sana@camunda.com>
2025-12-03 11:08:49 +0100

    feat: add factory method for records in UnifiedRecorValue
    Moves the logic to map from a ValueType to a Record in
    UnifiedRecordValue.
    This makes adding a new record just limited to the protocol-impl module,
    avoiding the codeowners of zeebe/broker to review the change.
    
    There are other advantages:
    - this logic is centralized in case it needs to be used by other modules
    - using a switch we get a compiler warning if a record is missing
    - a tests verifies that every value type is mapped correctly
    - we verify that the naming is consistent across all value types
    
    (cherry picked from commit cab50db9e3f3c000fe8ec2da9167b863a5b6b7cf)

--- zeebe/broker/src/main/java/io/camunda/zeebe/broker/transport/commandapi/CommandApiRequestReader.java
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/transport/commandapi/CommandApiRequestReader.java
@@ -14,67 +14,11 @@ import io.camunda.zeebe.broker.transport.RequestReaderException;
 import io.camunda.zeebe.protocol.impl.encoding.AuthInfo;
 import io.camunda.zeebe.protocol.impl.record.RecordMetadata;
 import io.camunda.zeebe.protocol.impl.record.UnifiedRecordValue;
-import io.camunda.zeebe.protocol.impl.record.value.authorization.AuthorizationRecord;
-import io.camunda.zeebe.protocol.impl.record.value.clock.ClockRecord;
-import io.camunda.zeebe.protocol.impl.record.value.compensation.CompensationSubscriptionRecord;
-import io.camunda.zeebe.protocol.impl.record.value.decision.DecisionEvaluationRecord;
-import io.camunda.zeebe.protocol.impl.record.value.deployment.DeploymentRecord;
-import io.camunda.zeebe.protocol.impl.record.value.deployment.ResourceRecord;
-import io.camunda.zeebe.protocol.impl.record.value.distribution.CommandDistributionRecord;
-import io.camunda.zeebe.protocol.impl.record.value.incident.IncidentRecord;
-import io.camunda.zeebe.protocol.impl.record.value.job.JobBatchRecord;
-import io.camunda.zeebe.protocol.impl.record.value.job.JobRecord;
-import io.camunda.zeebe.protocol.impl.record.value.message.MessageBatchRecord;
-import io.camunda.zeebe.protocol.impl.record.value.message.MessageCorrelationRecord;
-import io.camunda.zeebe.protocol.impl.record.value.message.MessageRecord;
-import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceBatchRecord;
-import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationRecord;
-import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceMigrationRecord;
-import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceModificationRecord;
-import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceRecord;
-import io.camunda.zeebe.protocol.impl.record.value.resource.ResourceDeletionRecord;
-import io.camunda.zeebe.protocol.impl.record.value.signal.SignalRecord;
-import io.camunda.zeebe.protocol.impl.record.value.user.UserRecord;
-import io.camunda.zeebe.protocol.impl.record.value.usertask.UserTaskRecord;
-import io.camunda.zeebe.protocol.impl.record.value.variable.VariableDocumentRecord;
 import io.camunda.zeebe.protocol.record.ExecuteCommandRequestDecoder;
 import io.camunda.zeebe.protocol.record.MessageHeaderDecoder;
-import io.camunda.zeebe.protocol.record.ValueType;
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.function.Supplier;
 import org.agrona.DirectBuffer;
 
 public class CommandApiRequestReader implements RequestReader<ExecuteCommandRequestDecoder> {
-  static final Map<ValueType, Supplier<UnifiedRecordValue>> RECORDS_BY_TYPE =
-      new EnumMap<>(ValueType.class);
-
-  static {
-    RECORDS_BY_TYPE.put(ValueType.DEPLOYMENT, DeploymentRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.JOB, JobRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.PROCESS_INSTANCE, ProcessInstanceRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.MESSAGE, MessageRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.MESSAGE_BATCH, MessageBatchRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.JOB_BATCH, JobBatchRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.INCIDENT, IncidentRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.VARIABLE_DOCUMENT, VariableDocumentRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.PROCESS_INSTANCE_CREATION, ProcessInstanceCreationRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.DECISION_EVALUATION, DecisionEvaluationRecord::new);
-    RECORDS_BY_TYPE.put(
-        ValueType.PROCESS_INSTANCE_MODIFICATION, ProcessInstanceModificationRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.SIGNAL, SignalRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.COMMAND_DISTRIBUTION, CommandDistributionRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.PROCESS_INSTANCE_BATCH, ProcessInstanceBatchRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.RESOURCE_DELETION, ResourceDeletionRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.USER_TASK, UserTaskRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.PROCESS_INSTANCE_MIGRATION, ProcessInstanceMigrationRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.COMPENSATION_SUBSCRIPTION, CompensationSubscriptionRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.MESSAGE_CORRELATION, MessageCorrelationRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.USER, UserRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.CLOCK, ClockRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.AUTHORIZATION, AuthorizationRecord::new);
-    RECORDS_BY_TYPE.put(ValueType.RESOURCE, ResourceRecord::new);
-  }
 
   private UnifiedRecordValue value;
   private final RecordMetadata metadata = new RecordMetadata();
@@ -113,12 +57,12 @@ public class CommandApiRequestReader implements RequestReader<ExecuteCommandRequ
         messageHeaderDecoder.version());
 
     metadata.protocolVersion(messageHeaderDecoder.version());
-    final var recordSupplier = RECORDS_BY_TYPE.get(commandRequestDecoder.valueType());
-    if (recordSupplier != null) {
+    final var record = UnifiedRecordValue.fromValueType(commandRequestDecoder.valueType());
+    if (record != null) {
       final int valueOffset =
           commandRequestDecoder.limit() + ExecuteCommandRequestDecoder.valueHeaderLength();
       final int valueLength = commandRequestDecoder.valueLength();
-      value = recordSupplier.get();
+      value = record;
       value.wrap(buffer, valueOffset, valueLength);
     }
 
