36997120fac8a75dcdcd66b9e507917a4f319a5c
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2025-12-04 21:58:00 +0100

    refactor: move test to correct package

--- clients/java/src/test/java/io/camunda/client/tenant/MappingRulesByTenantSearchRequestTest.java
+++ clients/java/src/test/java/io/camunda/client/tenant/MappingRulesByTenantSearchRequestTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.camunda.client.role;
+package io.camunda.client.tenant;
 
 import static io.camunda.client.impl.http.HttpClientFactory.REST_API_PATH;
 import static org.assertj.core.api.Assertions.assertThat;
