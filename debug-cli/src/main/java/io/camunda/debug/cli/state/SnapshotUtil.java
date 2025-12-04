aa6ed97167c5b5f1768aca68b4326d09dc2e7753
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2025-12-04 07:18:32 +0100

    feat: allow debug-cli to set a key and max key in the state
    (cherry picked from commit 05c455a1799403d92fe5dcaa489c041b421b3520)

---
+++ debug-cli/src/main/java/io/camunda/debug/cli/state/SnapshotUtil.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.debug.cli.state;
+
+import io.camunda.zeebe.db.AccessMetricsConfiguration;
+import io.camunda.zeebe.db.AccessMetricsConfiguration.Kind;
+import io.camunda.zeebe.db.ConsistencyChecksSettings;
+import io.camunda.zeebe.db.ZeebeDb;
+import io.camunda.zeebe.db.impl.rocksdb.ChecksumProviderRocksDBImpl;
+import io.camunda.zeebe.db.impl.rocksdb.RocksDbConfiguration;
+import io.camunda.zeebe.db.impl.rocksdb.ZeebeRocksDbFactory;
+import io.camunda.zeebe.scheduler.testing.TestConcurrencyControl;
+import io.camunda.zeebe.snapshots.PersistedSnapshot;
+import io.camunda.zeebe.snapshots.impl.FileBasedSnapshotId;
+import io.camunda.zeebe.snapshots.impl.FileBasedSnapshotMetadata;
+import io.camunda.zeebe.snapshots.impl.FileBasedSnapshotStoreImpl;
+import io.camunda.zeebe.snapshots.impl.SnapshotMetrics;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SnapshotUtil {
+
+  private final ZeebeRocksDbFactory zeebeDbFactory;
+
+  public SnapshotUtil() {
+    zeebeDbFactory =
+        new ZeebeRocksDbFactory<>(
+            new RocksDbConfiguration().setWalDisabled(false),
+            new ConsistencyChecksSettings(true, true),
+            new AccessMetricsConfiguration(Kind.NONE, 1),
+            SimpleMeterRegistry::new);
+  }
+
+  public ZeebeDb openSnapshot(final Path snapshotPath, final Path runtimePath) {
+    try (final var db = zeebeDbFactory.openSnapshotOnlyDb(snapshotPath.toFile())) {
+      db.createSnapshot(runtimePath.toFile());
+    } catch (final Exception e) {
+      throw new RuntimeException("Failed to open and copy snapshot", e);
+    }
+    final var runtimeDb = zeebeDbFactory.createDb(runtimePath.toFile());
+    return runtimeDb;
+  }
+
+  public PersistedSnapshot takeSnapshot(
+      final ZeebeDb runtime,
+      final Path rootDirectory,
+      final String stringSnapshotId,
+      final long lastFollowupEventPosition) {
+    final var snapshotStore =
+        new FileBasedSnapshotStoreImpl(
+            0,
+            rootDirectory,
+            new ChecksumProviderRocksDBImpl(),
+            new TestConcurrencyControl(),
+            new SnapshotMetrics(new SimpleMeterRegistry()));
+
+    final var snapshotId = FileBasedSnapshotId.ofFileName(stringSnapshotId).getOrThrow();
+
+    final var transientSnapshot =
+        snapshotStore
+            .newTransientSnapshot(
+                snapshotId.getIndex(),
+                snapshotId.getTerm(),
+                snapshotId.getProcessedPosition(),
+                snapshotId.getExportedPosition(),
+                true)
+            .get();
+
+    transientSnapshot
+        .withLastFollowupEventPosition(lastFollowupEventPosition)
+        .take(
+            path -> {
+              runtime.createSnapshot(path.toFile());
+            })
+        .join();
+
+    return transientSnapshot.persist().join();
+  }
+
+  public static long getLastFollowupEventPosition(final Path snapshotPath) throws IOException {
+    final var snapshotMetadata =
+        Files.readAllBytes(snapshotPath.resolve(FileBasedSnapshotStoreImpl.METADATA_FILE_NAME));
+    final var metadata = FileBasedSnapshotMetadata.decode(snapshotMetadata);
+
+    final var lastFollowupEventPosition = metadata.lastFollowupEventPosition();
+    return lastFollowupEventPosition;
+  }
+}
