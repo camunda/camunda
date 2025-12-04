34219cf59d6c461a9d157d5ee9af53f2ada2a97a
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2025-12-04 09:37:33 +0100

    feat: allow overwriting next key in the state and set a max key
    This is not required during normal operation, but only required
    in incidents where we want to manually recover the snapshot by overwriting
    the key and the max key.
    
    A max key is required so that, during replay it doesn't overwrite the new key
    that we set for recovery.
    
    A typical use case is as follows: Due to a bug or data corruption, the key in
    the partition overflowed in to the next partition's key range. As a result, this
    partition is dead and cannot process new commands any more. In this case, a manual
    recovery using debug-cli to ovewrite the key is required to unblock the partition.
    If we only set the key in the snapshot, during the replay it might overwrite it back
    to the higher value thus blocking the partition again. To prevent that, we also use a
    max key. During replay, the key is not updated if it is greater than the max key.

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/state/DbKeyGenerator.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/state/DbKeyGenerator.java
@@ -7,15 +7,22 @@
  */
 package io.camunda.zeebe.stream.impl.state;
 
+import io.camunda.zeebe.db.ColumnFamily;
 import io.camunda.zeebe.db.TransactionContext;
 import io.camunda.zeebe.db.ZeebeDb;
+import io.camunda.zeebe.db.impl.DbLong;
+import io.camunda.zeebe.db.impl.DbString;
 import io.camunda.zeebe.protocol.Protocol;
 import io.camunda.zeebe.protocol.ZbColumnFamilies;
 import io.camunda.zeebe.stream.api.state.KeyGeneratorControls;
 import io.camunda.zeebe.util.VisibleForTesting;
 import io.camunda.zeebe.util.exception.UnrecoverableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class DbKeyGenerator implements KeyGeneratorControls {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DbKeyGenerator.class);
 
   private static final long INITIAL_VALUE = 0;
 
@@ -24,6 +31,11 @@ public final class DbKeyGenerator implements KeyGeneratorControls {
   private final long keyStartValue;
   private final NextValueManager nextValueManager;
   private final int partitionId;
+
+  private final DbString maxKeyValueKey;
+  private final DbLong maxKeyValueValue = new DbLong();
+  private final ColumnFamily<DbString, DbLong> maxValueColumnFamily;
+  private long maxKeyValue;
 
   /**
    * Initializes the key state with the corresponding partition id, so that unique keys are
@@ -39,6 +51,13 @@ public final class DbKeyGenerator implements KeyGeneratorControls {
         new NextValueManager(
             keyStartValue, zeebeDb, transactionContext, ZbColumnFamilies.KEY, LATEST_KEY);
 
+    maxKeyValueKey = new DbString();
+    maxKeyValueKey.wrapString("maxKeyValue");
+    // Reuse the column family of next key, but use a different key name.
+    maxValueColumnFamily =
+        zeebeDb.createColumnFamily(
+            ZbColumnFamilies.KEY, transactionContext, maxKeyValueKey, maxKeyValueValue);
+
     final var currentKey = nextValueManager.getCurrentValue();
     if (Protocol.decodePartitionId(currentKey) != partitionId) {
       throw new UnrecoverableException(
@@ -46,6 +65,16 @@ public final class DbKeyGenerator implements KeyGeneratorControls {
               String.format(
                   "Current key in the state %d does not belong to partition %d, but belongs to %d. Potential data corruption suspected.",
                   currentKey, partitionId, Protocol.decodePartitionId(currentKey))));
+    }
+
+    final var max = maxValueColumnFamily.get(maxKeyValueKey);
+    if (max != null) {
+      maxKeyValue = max.getValue();
+    } else {
+      // We deliberately leave 1 extra bit to give a buffer space before hitting the actual max key
+      // range possible for this partition. This allows us to detect potential issues before we
+      // exhaust the entire key space and provide more options for recovery.
+      maxKeyValue = Protocol.encodePartitionId(partitionId, 1L << (Protocol.KEY_BITS - 1));
     }
   }
 
@@ -58,6 +87,14 @@ public final class DbKeyGenerator implements KeyGeneratorControls {
               String.format(
                   "Generated key %d does not belong to partition %d, but belongs to %d",
                   nextKey, partitionId, Protocol.decodePartitionId(nextKey))));
+    }
+
+    if (nextKey > maxKeyValue) {
+      throw new UnrecoverableException(
+          new IllegalStateException(
+              String.format(
+                  "Generated key %d exceeds the maximum allowed key value %d for partition %d",
+                  nextKey, maxKeyValue, partitionId)));
     }
     return nextKey;
   }
@@ -85,7 +122,54 @@ public final class DbKeyGenerator implements KeyGeneratorControls {
                     "Provided key %d does not belong to partition %d, it belongs to %d",
                     key, partitionId, Protocol.decodePartitionId(key))));
       }
+
+      if (key > maxKeyValue) {
+        // skip - we don't want to throw here to avoid stopping the processing of a partition due to
+        // an invalid key that was generated already. Instead, we log a warning.
+        LOGGER.warn(
+            "Provided key {} is higher than the maximum allowed key value {} for partition {}, skipping setKeyIfHigher.",
+            key,
+            maxKeyValue,
+            partitionId);
+        return;
+      }
       nextValueManager.setValue(LATEST_KEY, key);
     }
+  }
+
+  /**
+   * Overwrite the current key in the state. Since this is a dangerous operation, use it with
+   * caution. It is indended to be used on to recover from specific failures manually..
+   */
+  public void overwriteKey(final long key) {
+    if (Protocol.decodePartitionId(key) != partitionId) {
+      throw new UnrecoverableException(
+          new IllegalArgumentException(
+              String.format(
+                  "Provided key %d does not belong to partition %d, it belongs to %d",
+                  key, partitionId, Protocol.decodePartitionId(key))));
+    }
+
+    nextValueManager.setValue(LATEST_KEY, key);
+  }
+
+  public Long getMaxKeyValue() {
+    final var readValue = maxValueColumnFamily.get(maxKeyValueKey);
+    if (readValue != null) {
+      return readValue.getValue();
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * This operation is intended to be used only to manually recover from specific failures.
+   *
+   * @param maxValue the max value allowed for the key
+   */
+  public void setMaxKeyValue(final long maxValue) {
+    maxKeyValueValue.wrapLong(maxValue);
+    maxValueColumnFamily.upsert(maxKeyValueKey, maxKeyValueValue);
+    maxKeyValue = maxValue;
   }
 }
