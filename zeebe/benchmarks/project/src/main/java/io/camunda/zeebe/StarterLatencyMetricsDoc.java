ab4e9ea6663835a0ae45fa98d9da108ddf2dcd21
Christopher Kujawa <zelldon91@googlemail.com>
2025-12-04 13:45:31 +0100

    feat: add data availability metric
     * Measure the time from instance has been created to instance is available via API
     * Create class to record and measure process instance availability
     * Abstract away how the instances are queried or checked/found
     * Allows to write easy sort-of unit-tests, validating our metric logic
    
    How measurement works:
    
     * We create an instance async - we take the time (nanos)
     * On response (async), we collect the corresponding key
     * We collect the key and start time (ns) as a record and put it into a list
     * We have a regular job running every 250 ms (separate pool)
     * We query/search the process instance API - filter by the keys we know
     * If they exist, we get the corresponding PIs back, with that we can remove
       our PIs from the list and take the endTime, calculate the duration,
       and register the corresponding time as a metric
    
    (cherry picked from commit b51a789317e7eda09e81449736498ce9c3ecb4e2)
    (cherry picked from commit 106fe610ac30dbce678e33b135bc20e527d52398)

---
+++ zeebe/benchmarks/project/src/main/java/io/camunda/zeebe/StarterLatencyMetricsDoc.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe;
+
+import io.camunda.zeebe.util.micrometer.ExtendedMeterDocumentation;
+import io.micrometer.core.instrument.Meter.Type;
+import java.time.Duration;
+
+public enum StarterLatencyMetricsDoc implements ExtendedMeterDocumentation {
+  /**
+   * The data availability latency when starting process instances. It measures the time from
+   * instance creation to the time the instance can be queried.
+   */
+  DATA_AVAILABILITY_LATENCY {
+    private static final Duration[] BUCKETS = {
+      Duration.ofMillis(500),
+      Duration.ofSeconds(1),
+      Duration.ofMillis(2500),
+      Duration.ofSeconds(5),
+      Duration.ofSeconds(10),
+      Duration.ofSeconds(15),
+      Duration.ofSeconds(30),
+      Duration.ofSeconds(45),
+      Duration.ofSeconds(60),
+      Duration.ofSeconds(90),
+    };
+
+    @Override
+    public String getDescription() {
+      return "The data availability latency when starting process instances. It measures the time from instance creation to the time the instance can be queried.";
+    }
+
+    @Override
+    public String getName() {
+      return "starter.data.availability.latency";
+    }
+
+    @Override
+    public Type getType() {
+      return Type.TIMER;
+    }
+
+    @Override
+    public Duration[] getTimerSLOs() {
+      return BUCKETS;
+    }
+  },
+
+  /**
+   * The response latency when starting process instances. It measures the time from sending the
+   * request to receiving the response.
+   */
+  RESPONSE_LATENCY {
+    private static final Duration[] BUCKETS = {
+      Duration.ofMillis(10),
+      Duration.ofMillis(25),
+      Duration.ofMillis(50),
+      Duration.ofMillis(75),
+      Duration.ofMillis(100),
+      Duration.ofMillis(250),
+      Duration.ofMillis(500),
+      Duration.ofMillis(750),
+      Duration.ofSeconds(1),
+      Duration.ofMillis(2500),
+      Duration.ofSeconds(5)
+    };
+
+    @Override
+    public String getDescription() {
+      return "The response latency when starting process instances. It measures the time from sending the request to receiving the response.";
+    }
+
+    @Override
+    public String getName() {
+      return "starter.response.latency";
+    }
+
+    @Override
+    public Type getType() {
+      return Type.TIMER;
+    }
+
+    @Override
+    public Duration[] getTimerSLOs() {
+      return BUCKETS;
+    }
+  };
+}
