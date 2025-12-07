5dcac6569f20d4af855896cd689c7d08c29b2ac3
Bojan Mudric <bojan.mudric@camunda.com>
2025-12-07 17:50:32 +0100

    refactor: move CamundaUserDetailsService from authentication to zeebe dist

--- dist/src/test/java/io/camunda/application/commons/identity/CamundaUserDetailsServiceTest.java
+++ dist/src/test/java/io/camunda/application/commons/identity/CamundaUserDetailsServiceTest.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.authentication;
+package io.camunda.application.commons.identity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
