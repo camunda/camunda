6d3b623a510094d02e67557dd13a1204d094f9ad
euro.lew <euro.lew@camunda.com>
2025-12-08 18:07:12 +0700

    feat: ElasticsearchDecisionDefinitionDao now uses java client

--- operate/webapp/src/main/java/io/camunda/operate/webapp/api/v1/dao/elasticsearch/ElasticsearchDecisionDefinitionDao.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/api/v1/dao/elasticsearch/ElasticsearchDecisionDefinitionDao.java
@@ -7,9 +7,10 @@
  */
 package io.camunda.operate.webapp.api.v1.dao.elasticsearch;
 
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
-import static org.elasticsearch.index.query.QueryBuilders.termsQuery;
-
+import co.elastic.clients.elasticsearch._types.FieldValue;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchRequest.Builder;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import io.camunda.operate.conditions.ElasticsearchCondition;
 import io.camunda.operate.util.ElasticsearchUtil;
 import io.camunda.operate.webapp.api.v1.dao.DecisionDefinitionDao;
@@ -23,14 +24,8 @@ import io.camunda.operate.webapp.api.v1.exceptions.ResourceNotFoundException;
 import io.camunda.operate.webapp.api.v1.exceptions.ServerException;
 import io.camunda.webapps.schema.descriptors.index.DecisionIndex;
 import io.camunda.webapps.schema.descriptors.index.DecisionRequirementsIndex;
-import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
@@ -51,8 +46,7 @@ public class ElasticsearchDecisionDefinitionDao extends ElasticsearchDao<Decisio
   public DecisionDefinition byKey(final Long key) throws APIException {
     final List<DecisionDefinition> decisionDefinitions;
     try {
-      decisionDefinitions =
-          searchFor(new SearchSourceBuilder().query(termQuery(DecisionIndex.KEY, key)));
+      decisionDefinitions = searchFor(ElasticsearchUtil.termsQuery(DecisionIndex.KEY, key));
     } catch (final Exception e) {
       throw new ServerException(
           String.format("Error in reading decision definition for key %s", key), e);
@@ -79,68 +73,108 @@ public class ElasticsearchDecisionDefinitionDao extends ElasticsearchDao<Decisio
   public Results<DecisionDefinition> search(final Query<DecisionDefinition> query)
       throws APIException {
 
-    final SearchSourceBuilder searchSourceBuilder =
-        buildQueryOn(query, DecisionDefinition.KEY, new SearchSourceBuilder());
+    final var searchRequestBuilder =
+        buildQueryOn(query, DecisionDefinition.KEY, new Builder(), true);
     try {
-      final SearchRequest searchRequest =
-          new SearchRequest().indices(decisionIndex.getAlias()).source(searchSourceBuilder);
-      final SearchResponse searchResponse = tenantAwareClient.search(searchRequest);
-      final SearchHits searchHits = searchResponse.getHits();
-      final SearchHit[] searchHitArray = searchHits.getHits();
-      if (searchHitArray != null && searchHitArray.length > 0) {
-        final Object[] sortValues = searchHitArray[searchHitArray.length - 1].getSortValues();
-        final List<DecisionDefinition> decisionDefinitions =
-            ElasticsearchUtil.mapSearchHits(searchHitArray, objectMapper, DecisionDefinition.class);
-        populateDecisionRequirementsNameAndVersion(decisionDefinitions);
-        return new Results<DecisionDefinition>()
-            .setTotal(searchHits.getTotalHits().value)
-            .setItems(decisionDefinitions)
-            .setSortValues(sortValues);
-      } else {
-        return new Results<DecisionDefinition>().setTotal(searchHits.getTotalHits().value);
+      final var searchReq = searchRequestBuilder.index(decisionIndex.getAlias()).build();
+      final var res = es8Client.search(searchReq, DecisionDefinition.class);
+
+      final var hits = res.hits().hits();
+
+      if (hits.isEmpty()) {
+        return new Results<DecisionDefinition>().setTotal(res.hits().total().value());
       }
+
+      final Object[] sortValues = hits.getLast().sort().stream().map(FieldValue::_get).toArray();
+
+      final var decisionDefinitions = hits.stream().map(Hit::source).toList();
+
+      populateDecisionRequirementsNameAndVersion(decisionDefinitions);
+
+      return new Results<DecisionDefinition>()
+          .setTotal(res.hits().total().value())
+          .setItems(decisionDefinitions)
+          .setSortValues(sortValues);
+
     } catch (final Exception e) {
       throw new ServerException("Error in reading decision definitions", e);
     }
   }
 
-  protected List<DecisionDefinition> searchFor(final SearchSourceBuilder searchSource)
-      throws IOException {
-    final SearchRequest searchRequest =
-        new SearchRequest(decisionIndex.getAlias()).source(searchSource);
-    return tenantAwareClient.search(
-        searchRequest,
-        () -> {
-          return ElasticsearchUtil.scroll(
-              searchRequest, DecisionDefinition.class, objectMapper, elasticsearch);
-        });
+  protected List<DecisionDefinition> searchFor(
+      final co.elastic.clients.elasticsearch._types.query_dsl.Query query) {
+    final var tenantAwareQuery = tenantHelper.makeQueryTenantAware(query);
+
+    final var searchRequestBuilder =
+        new SearchRequest.Builder().index(decisionIndex.getAlias()).query(tenantAwareQuery);
+
+    return ElasticsearchUtil.scrollAllStream(
+            es8Client, searchRequestBuilder, DecisionDefinition.class)
+        .flatMap(res -> res.hits().hits().stream())
+        .map(Hit::source)
+        .toList();
   }
 
   @Override
   protected void buildFiltering(
-      final Query<DecisionDefinition> query, final SearchSourceBuilder searchSourceBuilder) {
-    final DecisionDefinition filter = query.getFilter();
-    if (filter != null) {
-      final List<QueryBuilder> queryBuilders = new ArrayList<>();
-      queryBuilders.add(buildTermQuery(DecisionDefinition.ID, filter.getId()));
-      queryBuilders.add(buildTermQuery(DecisionDefinition.KEY, filter.getKey()));
-      queryBuilders.add(buildTermQuery(DecisionDefinition.DECISION_ID, filter.getDecisionId()));
-      queryBuilders.add(buildTermQuery(DecisionDefinition.TENANT_ID, filter.getTenantId()));
-      queryBuilders.add(buildTermQuery(DecisionDefinition.NAME, filter.getName()));
-      queryBuilders.add(buildTermQuery(DecisionDefinition.VERSION, filter.getVersion()));
-      queryBuilders.add(
-          buildTermQuery(
-              DecisionDefinition.DECISION_REQUIREMENTS_ID, filter.getDecisionRequirementsId()));
-      queryBuilders.add(
-          buildTermQuery(
-              DecisionDefinition.DECISION_REQUIREMENTS_KEY, filter.getDecisionRequirementsKey()));
-      queryBuilders.add(
-          buildFilteringBy(
-              filter.getDecisionRequirementsName(), filter.getDecisionRequirementsVersion()));
+      final Query<DecisionDefinition> query, final SearchSourceBuilder searchSourceBuilder) {}
 
-      searchSourceBuilder.query(
-          ElasticsearchUtil.joinWithAnd(queryBuilders.toArray(new QueryBuilder[] {})));
+  @Override
+  protected void buildFiltering(
+      final Query<DecisionDefinition> query,
+      final SearchRequest.Builder searchRequestBuilder,
+      final boolean isTenantAware) {
+    final DecisionDefinition filter = query.getFilter();
+    if (filter == null) {
+      return;
     }
+
+    final var idQ =
+        filter.getId() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(DecisionDefinition.ID, filter.getId());
+    final var keyQ =
+        filter.getKey() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(DecisionDefinition.KEY, filter.getKey());
+    final var decIdQ =
+        filter.getDecisionId() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(DecisionDefinition.DECISION_ID, filter.getDecisionId());
+    final var tenantIdQ =
+        filter.getTenantId() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(DecisionDefinition.TENANT_ID, filter.getTenantId());
+    final var nameQ =
+        filter.getName() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(DecisionDefinition.NAME, filter.getName());
+    final var verQ =
+        filter.getVersion() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(DecisionDefinition.VERSION, filter.getVersion());
+    final var decReqIdQ =
+        filter.getDecisionRequirementsId() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(
+                DecisionDefinition.DECISION_REQUIREMENTS_ID, filter.getDecisionRequirementsId());
+    final var decReqKeyQ =
+        filter.getDecisionRequirementsKey() == null
+            ? null
+            : ElasticsearchUtil.termsQuery(
+                DecisionDefinition.DECISION_REQUIREMENTS_KEY, filter.getDecisionRequirementsKey());
+    final var filteringQ =
+        buildFilteringByEs8(
+            filter.getDecisionRequirementsName(), filter.getDecisionRequirementsVersion());
+
+    final var andOfAllQueries =
+        ElasticsearchUtil.joinWithAnd(
+            idQ, keyQ, decIdQ, tenantIdQ, nameQ, verQ, decReqIdQ, decReqKeyQ, filteringQ);
+
+    final var finalQuery =
+        isTenantAware ? tenantHelper.makeQueryTenantAware(andOfAllQueries) : andOfAllQueries;
+
+    searchRequestBuilder.query(finalQuery);
   }
 
   /**
@@ -149,44 +183,52 @@ public class ElasticsearchDecisionDefinitionDao extends ElasticsearchDao<Decisio
    * @return the query to filter decision definitions by decisionRequirementsName and
    *     decisionRequirementsVersion, or null if no filter is needed
    */
-  private QueryBuilder buildFilteringBy(
+  private co.elastic.clients.elasticsearch._types.query_dsl.Query buildFilteringByEs8(
       final String decisionRequirementsName, final Integer decisionRequirementsVersion) {
 
-    final List<QueryBuilder> queryBuilders = new ArrayList<>();
-    queryBuilders.add(buildTermQuery(DecisionRequirementsIndex.NAME, decisionRequirementsName));
-    queryBuilders.add(
-        buildTermQuery(DecisionRequirementsIndex.VERSION, decisionRequirementsVersion));
+    final var nameQuery =
+        decisionRequirementsName == null
+            ? null
+            : ElasticsearchUtil.termsQuery(
+                DecisionRequirementsIndex.NAME, decisionRequirementsName);
+    final var versionQuery =
+        decisionRequirementsVersion == null
+            ? null
+            : ElasticsearchUtil.termsQuery(
+                DecisionRequirementsIndex.VERSION, decisionRequirementsVersion);
 
-    final QueryBuilder query =
-        ElasticsearchUtil.joinWithAnd(queryBuilders.toArray(new QueryBuilder[] {}));
+    final var query = ElasticsearchUtil.joinWithAnd(nameQuery, versionQuery);
+
     if (query == null) {
       return null;
     }
 
-    final SearchSourceBuilder searchSourceBuilder =
-        new SearchSourceBuilder().query(query).fetchSource(DecisionRequirementsIndex.KEY, null);
-    final SearchRequest searchRequest =
-        new SearchRequest(decisionRequirementsIndex.getAlias()).source(searchSourceBuilder);
-    try {
-      final List<DecisionRequirements> decisionRequirements =
-          tenantAwareClient.search(
-              searchRequest,
-              () -> {
-                return ElasticsearchUtil.scroll(
-                    searchRequest, DecisionRequirements.class, objectMapper, elasticsearch);
-              });
-      final List<Long> nonNullKeys =
-          decisionRequirements.stream()
-              .map(DecisionRequirements::getKey)
-              .filter(Objects::nonNull)
-              .toList();
-      if (nonNullKeys.isEmpty()) {
-        return ElasticsearchUtil.createMatchNoneQuery();
-      }
-      return termsQuery(DecisionDefinition.DECISION_REQUIREMENTS_KEY, nonNullKeys);
-    } catch (final Exception e) {
-      throw new ServerException("Error in reading decision requirements by name and version", e);
+    final var tenantAwareQuery = tenantHelper.makeQueryTenantAware(query);
+
+    final var searchRequestBuilder =
+        new SearchRequest.Builder()
+            .index(decisionRequirementsIndex.getAlias())
+            .query(tenantAwareQuery)
+            .source(s -> s.filter(f -> f.includes(DecisionRequirementsIndex.KEY)));
+
+    final var streamResult =
+        ElasticsearchUtil.scrollAllStream(
+                es8Client, searchRequestBuilder, DecisionRequirements.class)
+            .flatMap(res -> res.hits().hits().stream())
+            .toList();
+    final var sourceRes = streamResult.stream().map(Hit::source).toList();
+    final var nonNullKeys =
+        sourceRes.stream()
+            .filter(Objects::nonNull)
+            .map(DecisionRequirements::getKey)
+            .filter(Objects::nonNull)
+            .toList();
+
+    if (nonNullKeys.isEmpty()) {
+      return ElasticsearchUtil.createMatchNoneQueryEs8().build()._toQuery();
     }
+
+    return ElasticsearchUtil.termsQuery(DecisionDefinition.DECISION_REQUIREMENTS_KEY, nonNullKeys);
   }
 
   /**
