e41272eddf9f5c22d38c9a11f795ea099406cfa2
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2025-12-08 08:22:51 +0100

    test: extend test cases for DbKeyGenerator
    (cherry picked from commit d31b01c2da1829564b62d7d60dc7b692ce4d6a3f)

--- zeebe/stream-platform/src/test/java/io/camunda/zeebe/stream/impl/state/DbKeyGeneratorTest.java
+++ zeebe/stream-platform/src/test/java/io/camunda/zeebe/stream/impl/state/DbKeyGeneratorTest.java
@@ -95,4 +95,131 @@ class DbKeyGeneratorTest {
     // then
     assertThat(dbKeyGenerator.getCurrentKey()).isEqualTo(currentKey);
   }
+
+  @Test
+  void shouldGenerateSequentialKeys() {
+    // given
+    final long firstKey = dbKeyGenerator.nextKey();
+
+    // when
+    final long secondKey = dbKeyGenerator.nextKey();
+    final long thirdKey = dbKeyGenerator.nextKey();
+
+    // then
+    assertThat(secondKey).isEqualTo(firstKey + 1);
+    assertThat(thirdKey).isEqualTo(secondKey + 1);
+    assertThat(Protocol.decodePartitionId(firstKey)).isEqualTo(PARTITION_ID);
+    assertThat(Protocol.decodePartitionId(secondKey)).isEqualTo(PARTITION_ID);
+    assertThat(Protocol.decodePartitionId(thirdKey)).isEqualTo(PARTITION_ID);
+  }
+
+  @Test
+  void shouldSetKeyIfHigher() {
+    // given
+    dbKeyGenerator.nextKey(); // current key is 1
+    final long higherKey = Protocol.encodePartitionId(PARTITION_ID, 100L);
+
+    // when
+    dbKeyGenerator.setKeyIfHigher(higherKey);
+
+    // then
+    assertThat(dbKeyGenerator.getCurrentKey()).isEqualTo(higherKey);
+    assertThat(dbKeyGenerator.nextKey()).isEqualTo(higherKey + 1);
+  }
+
+  @Test
+  void shouldNotSetKeyIfLower() {
+    // given
+    final long currentKey = Protocol.encodePartitionId(PARTITION_ID, 100L);
+    dbKeyGenerator.overwriteKey(currentKey);
+    final long lowerKey = Protocol.encodePartitionId(PARTITION_ID, 50L);
+
+    // when
+    dbKeyGenerator.setKeyIfHigher(lowerKey);
+
+    // then
+    assertThat(dbKeyGenerator.getCurrentKey()).isEqualTo(currentKey);
+  }
+
+  @Test
+  void shouldFailOverwriteKeyWithWrongPartition() {
+    // given
+    final long keyOfAnotherPartition = Protocol.encodePartitionId(PARTITION_ID + 1, 100);
+
+    // when - then
+    assertThatException()
+        .isThrownBy(() -> dbKeyGenerator.overwriteKey(keyOfAnotherPartition))
+        .havingCause()
+        .isInstanceOf(IllegalArgumentException.class)
+        .withMessageContaining(
+            String.format(
+                "Provided key %d does not belong to partition %d",
+                keyOfAnotherPartition, PARTITION_ID));
+  }
+
+  @Test
+  void shouldFailOverwriteKeyExceedingMaxValue() {
+    // given
+    dbKeyGenerator.setMaxKeyValue(Protocol.encodePartitionId(PARTITION_ID, 1000L));
+    final long keyExceedingMax = Protocol.encodePartitionId(PARTITION_ID, 2000L);
+
+    // when - then
+    assertThatException()
+        .isThrownBy(() -> dbKeyGenerator.overwriteKey(keyExceedingMax))
+        .havingCause()
+        .isInstanceOf(IllegalArgumentException.class)
+        .withMessageContaining(
+            String.format(
+                "Provided key %d exceeds the maximum allowed key value", keyExceedingMax));
+  }
+
+  @Test
+  void shouldReturnNullWhenMaxKeyNotSet() {
+    // when
+    final Long maxKeyValue = dbKeyGenerator.getMaxKeyValue();
+
+    // then
+    assertThat(maxKeyValue).isNull();
+  }
+
+  @Test
+  void shouldPersistMaxKeyValueAcrossInstances() {
+    // given
+    final long maxValue = Protocol.encodePartitionId(PARTITION_ID, 5000L);
+    dbKeyGenerator.setMaxKeyValue(maxValue);
+
+    // when - create new instance
+    final DbKeyGenerator newGenerator = new DbKeyGenerator(PARTITION_ID, db, db.createContext());
+
+    // then
+    assertThat(newGenerator.getMaxKeyValue()).isEqualTo(maxValue);
+  }
+
+  @Test
+  void shouldGenerateKeysUpToMaxValue() {
+    // given
+    final long maxValue = Protocol.encodePartitionId(PARTITION_ID, 10L);
+    dbKeyGenerator.setMaxKeyValue(maxValue);
+    dbKeyGenerator.overwriteKey(Protocol.encodePartitionId(PARTITION_ID, 8L));
+
+    // when - then
+    assertThat(dbKeyGenerator.nextKey()).isEqualTo(Protocol.encodePartitionId(PARTITION_ID, 9L));
+    assertThat(dbKeyGenerator.nextKey()).isEqualTo(Protocol.encodePartitionId(PARTITION_ID, 10L));
+    assertThatException().isThrownBy(() -> dbKeyGenerator.nextKey());
+  }
+
+  @Test
+  void shouldSetKeyIfHigherRespectingMaxValue() {
+    // given
+    dbKeyGenerator.setMaxKeyValue(Protocol.encodePartitionId(PARTITION_ID, 1000L));
+    final long currentKey = Protocol.encodePartitionId(PARTITION_ID, 50L);
+    dbKeyGenerator.overwriteKey(currentKey);
+    final long validHigherKey = Protocol.encodePartitionId(PARTITION_ID, 500L);
+
+    // when
+    dbKeyGenerator.setKeyIfHigher(validHigherKey);
+
+    // then
+    assertThat(dbKeyGenerator.getCurrentKey()).isEqualTo(validHigherKey);
+  }
 }
