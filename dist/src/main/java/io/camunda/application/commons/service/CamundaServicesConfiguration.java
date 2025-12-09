6434eeb4f6fcdf55b5bbc59b4dc144d2ce7d8d5a
georgios-goulos <georgios.goulos@camunda.com>
2025-12-09 18:52:32 +0000

    feat: REST API for evaluating conditional start events

--- dist/src/main/java/io/camunda/application/commons/service/CamundaServicesConfiguration.java
+++ dist/src/main/java/io/camunda/application/commons/service/CamundaServicesConfiguration.java
@@ -42,6 +42,7 @@ import io.camunda.service.AuthorizationServices;
 import io.camunda.service.BatchOperationServices;
 import io.camunda.service.ClockServices;
 import io.camunda.service.ClusterVariableServices;
+import io.camunda.service.ConditionalEventServices;
 import io.camunda.service.DecisionDefinitionServices;
 import io.camunda.service.DecisionInstanceServices;
 import io.camunda.service.DecisionRequirementsServices;
@@ -547,6 +548,20 @@ public class CamundaServicesConfiguration {
         brokerClient,
         securityContextProvider,
         messageSubscriptionSearchClient,
+        null,
+        executorProvider,
+        brokerRequestAuthorizationConverter);
+  }
+
+  @Bean
+  public ConditionalEventServices conditionalEventServices(
+      final BrokerClient brokerClient,
+      final SecurityContextProvider securityContextProvider,
+      final ApiServicesExecutorProvider executorProvider,
+      final BrokerRequestAuthorizationConverter brokerRequestAuthorizationConverter) {
+    return new ConditionalEventServices(
+        brokerClient,
+        securityContextProvider,
         null,
         executorProvider,
         brokerRequestAuthorizationConverter);
