5abb2c6f41fc2f8afce81dbc8cf0dab40488a6d8
Ben Sheppard <ben.sheppard@camunda.com>
2025-12-09 16:46:39 +0000

    refactor: accurately reflect that the authorization migration is always run because of default permissions
    (cherry picked from commit ef1481bcb13ced9d9330cf407ecb5e3c8aa7d9e5)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/migration/to_8_8/PermissionStateCorrectionMigration.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/migration/to_8_8/PermissionStateCorrectionMigration.java
@@ -10,7 +10,6 @@ package io.camunda.zeebe.engine.state.migration.to_8_8;
 import io.camunda.zeebe.engine.state.migration.MigrationTask;
 import io.camunda.zeebe.engine.state.migration.MigrationTaskContext;
 import io.camunda.zeebe.engine.state.migration.MutableMigrationTaskContext;
-import io.camunda.zeebe.protocol.ZbColumnFamilies;
 
 public class PermissionStateCorrectionMigration implements MigrationTask {
   @Override
@@ -20,14 +19,7 @@ public class PermissionStateCorrectionMigration implements MigrationTask {
 
   @Override
   public boolean needsToRun(final MigrationTaskContext context) {
-    final var processingState = context.processingState();
-
-    /*
-     * We need to correct any instances where a permission does not exist in the permissions CF but
-     * does exist in a record in the authorizations CF under permissionType. We only need to do this
-     * if we have authorizations present.
-     */
-    return !processingState.isEmpty(ZbColumnFamilies.AUTHORIZATIONS);
+    return true;
   }
 
   @Override
