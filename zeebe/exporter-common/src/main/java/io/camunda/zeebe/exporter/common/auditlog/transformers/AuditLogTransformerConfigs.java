9a99b728fcf86ead0af27c635e7beb985f07e73e
Igor Petrov <igor.petrov-ext@camunda.com>
2025-12-09 14:51:41 +0200

    test: audit: added audit handlers registration tests

--- zeebe/exporter-common/src/main/java/io/camunda/zeebe/exporter/common/auditlog/transformers/AuditLogTransformerConfigs.java
+++ zeebe/exporter-common/src/main/java/io/camunda/zeebe/exporter/common/auditlog/transformers/AuditLogTransformerConfigs.java
@@ -12,16 +12,24 @@ import static io.camunda.zeebe.protocol.record.ValueType.PROCESS_INSTANCE_MODIFI
 import static io.camunda.zeebe.protocol.record.intent.ProcessInstanceModificationIntent.MODIFIED;
 
 import io.camunda.zeebe.exporter.common.auditlog.transformers.AuditLogTransformer.TransformerConfig;
+import io.camunda.zeebe.protocol.record.RejectionType;
 import io.camunda.zeebe.protocol.record.intent.BatchOperationIntent;
+import java.util.Set;
 
 public class AuditLogTransformerConfigs {
   public static final TransformerConfig PROCESS_INSTANCE_MODIFICATION_CONFIG =
       TransformerConfig.with(PROCESS_INSTANCE_MODIFICATION).withIntents(MODIFIED);
 
   public static final TransformerConfig BATCH_OPERATION_LIFECYCLE_MANAGEMENT_CONFIG =
-      TransformerConfig.with(BATCH_OPERATION_LIFECYCLE_MANAGEMENT)
-          .withIntents(
+      new TransformerConfig(
+          BATCH_OPERATION_LIFECYCLE_MANAGEMENT,
+          Set.of(
               BatchOperationIntent.RESUMED,
               BatchOperationIntent.SUSPENDED,
-              BatchOperationIntent.CANCELED);
+              BatchOperationIntent.CANCELED),
+          Set.of(
+              BatchOperationIntent.RESUME,
+              BatchOperationIntent.SUSPEND,
+              BatchOperationIntent.CANCEL),
+          Set.of(RejectionType.INVALID_STATE));
 }
