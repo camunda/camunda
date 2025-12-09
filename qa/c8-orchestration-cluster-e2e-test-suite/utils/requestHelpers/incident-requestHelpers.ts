8a6545bef960051984b48c36c459da0ed6f3bce5
Catherina Prokudina <catherina.prokudina@camunda.com>
2025-12-09 16:24:32 +0100

    test: incident-search-api test

---
+++ qa/c8-orchestration-cluster-e2e-test-suite/utils/requestHelpers/incident-requestHelpers.ts
@@ -0,0 +1,134 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import type {APIRequestContext} from 'playwright-core';
+import {expect, test} from '@playwright/test';
+import {assertStatusCode, buildUrl, jsonHeaders} from '../http';
+import {defaultAssertionOptions} from '../constants';
+import {validateResponse} from '../../json-body-assertions';
+import {createInstances, createSingleInstance} from '../zeebeClient';
+
+const INCIDENT_SEARCH_ENDPOINT = '/incidents/search';
+
+export async function searchIncidentByPIK(
+    request: APIRequestContext,
+    {processInstanceKey}:{processInstanceKey: string}
+) {
+    const localState: Record<string, unknown> = {};
+
+    await expect(async () => {
+        const res = await request.post(buildUrl(INCIDENT_SEARCH_ENDPOINT), {
+            headers: jsonHeaders(),
+            data: {
+                page: {
+                    from: 0,
+                    limit: 10,
+                },
+                filter: {
+                    processInstanceKey,
+                },
+            },
+        });
+
+        await assertStatusCode(res, 200);
+        await validateResponse(
+            {
+                path: INCIDENT_SEARCH_ENDPOINT,
+                method: 'POST',
+                status: '200',
+            },
+            res,
+        );
+        const json = await res.json();
+        expect(json.items.length).toBeGreaterThan(0);
+        localState['incidents'] = json.items;
+    }).toPass(defaultAssertionOptions);
+    
+    return localState['incidents'] as {
+        incidentKey: string,
+        errorMessage: string,
+        processDefinitionId: string,
+        processDefinitionKey: string,
+        processInstanceKey: string,
+        elementInstanceKey: string,
+        elementId: string,
+        creationTime: string,
+        tenantId: string,
+        jobKey?: string,
+    }[];
+}
+
+/**
+ * Create Instance of one process with two incidents of the same type
+ * @param localState 
+ * @param request 
+ */
+export async function setupIncidentTest (
+  localState: Record<string, unknown>,
+  request: APIRequestContext,
+) {
+    await test.step('Create process instance with an incident', async () => {
+        const instances = await createInstances('processWithAnError', 1, 1);
+        localState['processInstanceKey'] = instances[0].processInstanceKey;
+    });
+
+    await test.step('Search incident to get incidentKey', async () => {
+        const incidents = await searchIncidentByPIK(request, {
+          processInstanceKey: localState['processInstanceKey'] as string,
+        });
+        localState['incidentKeys'] = incidents.map(incident => incident.incidentKey);
+      });
+}
+
+/**
+ * Create two process instances with incidents of different type
+ */
+export async function setupMultipleIncidentsTest(
+  localState: Record<string, unknown>,
+  request: APIRequestContext,
+) {
+    await test.step('Create process instances with incidents', async () => {
+        const instances = [];
+        const processWithAnErrorInstance = await createSingleInstance('processWithAnError', 1,);
+        instances.push(processWithAnErrorInstance.processInstanceKey);
+
+        const loanApprovalProcessInstance = await createSingleInstance('loanApprovalProcess', 1,);
+        instances.push(loanApprovalProcessInstance.processInstanceKey);
+        localState['processInstanceKey'] = instances;
+    });
+
+    await test.step('Search incident to get incidentKey', async () => {
+        const allKeys: Array<number | string> = [];
+        for (const key of localState['processInstanceKey'] as string[]) {
+            const incidents = await searchIncidentByPIK(request, { processInstanceKey: key });
+            allKeys.push(...(incidents ?? []).map(i => i.incidentKey));
+        }
+
+        localState['incidentKeys'] = allKeys;
+      });
+}
+
+/**
+ * Create one process instance with two incidents of different type
+ */
+export async function setupMultipleTypeIncidentsTest(
+  localState: Record<string, unknown>,
+  request: APIRequestContext,
+) {
+    await test.step('Create process instance with two different incidents', async () => {
+        const instance = await createSingleInstance('MultipleErrorTypesProcess', 1,);
+        localState['processInstanceKey'] = instance.processInstanceKey;
+    });
+
+    await test.step('Search incident to get incidentKey', async () => {
+        const incidents = await searchIncidentByPIK(request, {
+          processInstanceKey: localState['processInstanceKey'] as string,
+        });
+        localState['incidentKeys'] = incidents.map(incident => incident.incidentKey);
+      });
+}
