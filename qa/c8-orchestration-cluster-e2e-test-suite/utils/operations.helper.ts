126b9bfae7f8a189f49309530a5c08343ffb54e3
esraagamal6 <esraa-gamal@hotmail.com>
2025-12-10 13:57:15 +0100

    test: migrate operation.spec.ts to OC test suite

---
+++ qa/c8-orchestration-cluster-e2e-test-suite/utils/operations.helper.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {expect, APIRequestContext} from '@playwright/test';
+import {credentials, authHeaders} from './http';
+
+export async function createDemoOperations(
+  request: APIRequestContext,
+  processInstanceKey: string,
+  count: number,
+): Promise<void> {
+  const requestHeaders = {
+    headers: {
+      'Content-Type': 'application/json',
+      ...authHeaders(credentials.accessToken),
+    },
+  };
+
+  await Promise.all(
+    [...new Array(count)].map(async () => {
+      const response = await request.post(
+        `http://localhost:8080/api/process-instances/${processInstanceKey}/operation`,
+        {
+          ...requestHeaders,
+          data: {
+            operationType: 'RESOLVE_INCIDENT',
+          },
+        },
+      );
+      expect(response.ok()).toBeTruthy();
+      return response;
+    }),
+  );
+
+  await expect
+    .poll(
+      async () => {
+        const response = await request.post(
+          `http://localhost:8080/api/batch-operations`,
+          {
+            ...requestHeaders,
+            data: {
+              pageSize: count,
+            },
+          },
+        );
+        const operations = await response.json();
+        return operations.length;
+      },
+      {timeout: 30000},
+    )
+    .toBeGreaterThanOrEqual(count);
+}
