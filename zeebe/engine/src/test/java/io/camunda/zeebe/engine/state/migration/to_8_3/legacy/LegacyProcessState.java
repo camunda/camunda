25fe884c91778be7e12d84a0a85a9c285c6ade26
Fabio Paini <fabio.paini@camunda.com>
2025-12-10 09:55:40 +0100

    feat: do not include global listeners in ExecutableUserTask
    Global listeners to be used when processing a user task lifecycle events
    are the ones pinned at the start of the lifecycle event, thus they are
    not necessarily the same for all tasks related to the same
    ExecutableUserTask.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/migration/to_8_3/legacy/LegacyProcessState.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/state/migration/to_8_3/legacy/LegacyProcessState.java
@@ -83,7 +83,7 @@ public final class LegacyProcessState {
       final ZeebeDb<ZbColumnFamilies> zeebeDb,
       final TransactionContext transactionContext,
       final InstantSource clock) {
-    transformer = BpmnFactory.createTransformer(clock, null);
+    transformer = BpmnFactory.createTransformer(clock);
     processDefinitionKey = new DbLong();
     persistedProcess = new PersistedProcess();
     processColumnFamily =
