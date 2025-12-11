809e3b3e38c2c090225f15947588c44563a6290f
Lena Schönburg <lena@fairburg.xyz>
2025-12-11 13:59:03 +0100

    fix: pending writes and responses can't be modified after processing
    (cherry picked from commit 6541253be55c16a46027d85a6785656e199eb0e5)

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingStateMachine.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/ProcessingStateMachine.java
@@ -394,6 +394,9 @@ public final class ProcessingStateMachine {
       processedCommandsCount++;
       processingMetrics.commandsProcessed();
     }
+    // We are done with processing, no new writes or responses will be added.
+    pendingWrites = Collections.unmodifiableList(pendingWrites);
+    pendingResponses = Collections.unmodifiableCollection(pendingResponses);
   }
 
   /**
