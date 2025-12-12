c9257b4a5b3c4019b16969e3eee2bda99b2b2f26
Christopher Kujawa <zelldon91@googlemail.com>
2025-12-12 11:32:39 +0100

    refactor: revert 'refactor: remove manual way of set up'
    Brings back manual way (still broken)
    
    This reverts commit a6f2d5f5e2c0ab5828d84c9853245090acc06913.

---
+++ load-tests/setup/newLoadTest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Contains OS specific sed function
+. utils.sh
+
+set -exo pipefail
+
+if [ -z $1 ]
+then
+  echo "Please provide an namespace name!"
+  exit 1
+fi
+
+### Load test helper script
+### First parameter is used as namespace name
+### For a new namespace a new folder will be created
+
+
+namespace=$1
+
+kubectl create namespace $namespace
+cp -rv default/ $namespace
+cd $namespace
+
+# calls OS specific sed inplace function
+sed_inplace "s/default/$namespace/g" Makefile
+
+# get latest updates from zeebe repo
+helm repo add zeebe-benchmark https://camunda.github.io/zeebe-benchmark-helm # skips if already exists
+helm repo update
