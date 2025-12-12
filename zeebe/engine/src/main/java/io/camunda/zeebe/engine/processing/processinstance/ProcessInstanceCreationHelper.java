ce03cd9b6b0eb942bfc0c6dd4c13e99841eda16c
Mustafa Dagher <mustafa.dagher@camunda.com>
2025-12-12 17:31:17 +0100

    refactor: extract `validateCommand()` from `ProcessInstanceCreationCreateProcessor`
    to be reused between the two process instance creation processors

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceCreationHelper.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/processinstance/ProcessInstanceCreationHelper.java
@@ -8,19 +8,42 @@
 package io.camunda.zeebe.engine.processing.processinstance;
 
 import static io.camunda.zeebe.util.buffer.BufferUtil.bufferAsString;
+import static io.camunda.zeebe.util.buffer.BufferUtil.wrapString;
 
 import io.camunda.zeebe.engine.processing.Rejection;
+import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableFlowElement;
+import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableFlowNode;
+import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableProcess;
+import io.camunda.zeebe.engine.processing.deployment.model.element.ExecutableSequenceFlow;
 import io.camunda.zeebe.engine.processing.identity.authorization.AuthorizationCheckBehavior;
 import io.camunda.zeebe.engine.processing.identity.authorization.request.AuthorizationRequest;
 import io.camunda.zeebe.engine.state.deployment.DeployedProcess;
+import io.camunda.zeebe.msgpack.property.ArrayProperty;
 import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationRecord;
+import io.camunda.zeebe.protocol.impl.record.value.processinstance.ProcessInstanceCreationStartInstruction;
 import io.camunda.zeebe.protocol.record.RejectionType;
 import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
+import io.camunda.zeebe.protocol.record.value.BpmnElementType;
 import io.camunda.zeebe.protocol.record.value.PermissionType;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
 import io.camunda.zeebe.util.Either;
+import io.camunda.zeebe.util.TagUtil;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ProcessInstanceCreationHelper {
+  private static final String ERROR_MESSAGE_NO_NONE_START_EVENT =
+      "Expected to create instance of process with none start event, but there is no such event";
+  private static final Set<BpmnElementType> UNSUPPORTED_ELEMENT_TYPES =
+      Set.of(
+          BpmnElementType.START_EVENT,
+          BpmnElementType.SEQUENCE_FLOW,
+          BpmnElementType.BOUNDARY_EVENT,
+          BpmnElementType.UNSPECIFIED);
+  private static final Either<Rejection, Object> VALID = Either.right(null);
+
   private final AuthorizationCheckBehavior authCheckBehavior;
 
   public ProcessInstanceCreationHelper(final AuthorizationCheckBehavior authCheckBehavior) {
@@ -55,4 +78,211 @@ public class ProcessInstanceCreationHelper {
             : rejection.reason();
     return Either.left(new Rejection(rejection.type(), errorMessage));
   }
+
+  public Either<Rejection, DeployedProcess> validateCommand(
+      final ProcessInstanceCreationRecord command, final DeployedProcess deployedProcess) {
+    final var process = deployedProcess.getProcess();
+    final var startInstructions = command.startInstructions();
+    final var tags = command.getTags();
+
+    return validateHasNoneStartEventOrStartInstructions(process, startInstructions)
+        .flatMap(valid -> validateElementsExist(process, startInstructions))
+        .flatMap(valid -> validateElementsNotInsideMultiInstance(process, startInstructions))
+        .flatMap(valid -> validateElementsNotInsideAdHocSubProcess(process, startInstructions))
+        .flatMap(valid -> validateTargetsSupportedElementType(process, startInstructions))
+        .flatMap(
+            valid -> validateElementNotBelongingToEventBasedGateway(process, startInstructions))
+        .flatMap(valid -> validateTags(tags))
+        .map(valid -> deployedProcess);
+  }
+
+  private Either<Rejection, ?> validateHasNoneStartEventOrStartInstructions(
+      final ExecutableProcess process,
+      final ArrayProperty<ProcessInstanceCreationStartInstruction> startInstructions) {
+
+    if (process.getNoneStartEvent() != null || !startInstructions.isEmpty()) {
+      return VALID;
+    } else {
+      return Either.left(
+          new Rejection(RejectionType.INVALID_STATE, ERROR_MESSAGE_NO_NONE_START_EVENT));
+    }
+  }
+
+  private Either<Rejection, ?> validateElementsExist(
+      final ExecutableProcess process,
+      final ArrayProperty<ProcessInstanceCreationStartInstruction> startInstructions) {
+
+    return startInstructions.stream()
+        .map(ProcessInstanceCreationStartInstruction::getElementId)
+        .filter(elementId -> !isElementOfProcess(process, elementId))
+        .findAny()
+        .map(
+            elementId ->
+                Either.left(
+                    new Rejection(
+                        RejectionType.INVALID_ARGUMENT,
+                        "Expected to create instance of process with start instructions but no element found with id '%s'."
+                            .formatted(elementId))))
+        .orElse(VALID);
+  }
+
+  private boolean isElementOfProcess(final ExecutableProcess process, final String elementId) {
+    return process.getElementById(wrapString(elementId)) != null;
+  }
+
+  private Either<Rejection, ?> validateElementsNotInsideMultiInstance(
+      final ExecutableProcess process,
+      final ArrayProperty<ProcessInstanceCreationStartInstruction> startInstructions) {
+
+    return startInstructions.stream()
+        .map(ProcessInstanceCreationStartInstruction::getElementId)
+        .filter(elementId -> isElementInsideMultiInstance(process, elementId))
+        .findAny()
+        .map(
+            elementId ->
+                Either.left(
+                    new Rejection(
+                        RejectionType.INVALID_ARGUMENT,
+                        "Expected to create instance of process with start instructions but the element with id '%s' is inside a multi-instance subprocess. The creation of elements inside a multi-instance subprocess is not supported."
+                            .formatted(elementId))))
+        .orElse(VALID);
+  }
+
+  private boolean isElementInsideMultiInstance(
+      final ExecutableProcess process, final String elementId) {
+    final var element = process.getElementById(wrapString(elementId));
+    return element != null && hasMultiInstanceScope(element);
+  }
+
+  private boolean hasMultiInstanceScope(final ExecutableFlowElement flowElement) {
+    final var flowScope = flowElement.getFlowScope();
+    if (flowScope == null) {
+      return false;
+    }
+
+    if (flowScope.getElementType() == BpmnElementType.MULTI_INSTANCE_BODY) {
+      return true;
+    } else {
+      return hasMultiInstanceScope(flowScope);
+    }
+  }
+
+  private Either<Rejection, ?> validateElementsNotInsideAdHocSubProcess(
+      final ExecutableProcess process,
+      final ArrayProperty<ProcessInstanceCreationStartInstruction> startInstructions) {
+
+    return startInstructions.stream()
+        .map(ProcessInstanceCreationStartInstruction::getElementId)
+        .filter(elementId -> isElementInsideAdHocSubProcess(process, elementId))
+        .findAny()
+        .map(
+            elementId ->
+                Either.left(
+                    new Rejection(
+                        RejectionType.INVALID_ARGUMENT,
+                        "Expected to create instance of process with start instructions but the element with id '%s' is inside an ad-hoc subprocess. The creation of elements inside an ad-hoc subprocess is not supported."
+                            .formatted(elementId))))
+        .orElse(VALID);
+  }
+
+  private boolean isElementInsideAdHocSubProcess(
+      final ExecutableProcess process, final String elementId) {
+    final var element = process.getElementById(wrapString(elementId));
+    return element != null && hasAdHocSubProcessScope(element);
+  }
+
+  private boolean hasAdHocSubProcessScope(final ExecutableFlowElement flowElement) {
+    final var flowScope = flowElement.getFlowScope();
+    if (flowScope == null) {
+      return false;
+    }
+
+    if (flowScope.getElementType() == BpmnElementType.AD_HOC_SUB_PROCESS) {
+      return true;
+    } else {
+      return hasAdHocSubProcessScope(flowScope);
+    }
+  }
+
+  private Either<Rejection, ?> validateTargetsSupportedElementType(
+      final ExecutableProcess process,
+      final ArrayProperty<ProcessInstanceCreationStartInstruction> startInstructions) {
+
+    return startInstructions.stream()
+        .map(
+            instruction ->
+                new ElementIdAndType(
+                    instruction.getElementId(),
+                    process.getElementById(instruction.getElementIdBuffer()).getElementType()))
+        .filter(
+            elementIdAndType -> UNSUPPORTED_ELEMENT_TYPES.contains(elementIdAndType.elementType))
+        .findAny()
+        .map(
+            elementIdAndType ->
+                Either.left(
+                    new Rejection(
+                        RejectionType.INVALID_ARGUMENT,
+                        ("Expected to create instance of process with start instructions but the element with id '%s' targets unsupported element type '%s'. "
+                                + "Supported element types are: %s")
+                            .formatted(
+                                elementIdAndType.elementId,
+                                elementIdAndType.elementType,
+                                Arrays.stream(BpmnElementType.values())
+                                    .filter(
+                                        elementType ->
+                                            !UNSUPPORTED_ELEMENT_TYPES.contains(elementType))
+                                    .collect(Collectors.toSet())))))
+        .orElse(VALID);
+  }
+
+  private Either<Rejection, ?> validateElementNotBelongingToEventBasedGateway(
+      final ExecutableProcess process,
+      final ArrayProperty<ProcessInstanceCreationStartInstruction> startInstructions) {
+
+    return startInstructions.stream()
+        .map(ProcessInstanceCreationStartInstruction::getElementId)
+        .filter(elementId -> doesElementBelongToAnEventBasedGateway(process, elementId))
+        .findAny()
+        .map(
+            elementId ->
+                Either.left(
+                    new Rejection(
+                        RejectionType.INVALID_ARGUMENT,
+                        "Expected to create instance of process with start instructions but the element with id '%s' belongs to an event-based gateway. The creation of elements belonging to an event-based gateway is not supported."
+                            .formatted(elementId))))
+        .orElse(VALID);
+  }
+
+  private Either<Rejection, ?> validateTags(final Set<String> tags) {
+    if (tags.size() > TagUtil.MAX_NUMBER_OF_TAGS) {
+      return Either.left(
+          new Rejection(
+              RejectionType.INVALID_ARGUMENT,
+              String.format(
+                  "Expected to create instance of process with tags, but the number of tags exceeds the limit of %s.",
+                  TagUtil.MAX_NUMBER_OF_TAGS)));
+    }
+
+    final List<String> invalidTags = tags.stream().filter(tag -> !TagUtil.isValidTag(tag)).toList();
+    if (!invalidTags.isEmpty()) {
+      return Either.left(
+          new Rejection(
+              RejectionType.INVALID_ARGUMENT,
+              "Expected to create instance of process with tags, but the tags '%s' are invalid. %s"
+                  .formatted(String.join("', '", invalidTags), TagUtil.TAG_FORMAT_DESCRIPTION)));
+    }
+
+    return VALID;
+  }
+
+  private boolean doesElementBelongToAnEventBasedGateway(
+      final ExecutableProcess process, final String elementId) {
+    final ExecutableFlowNode element = process.getElementById(elementId, ExecutableFlowNode.class);
+    return element.getIncoming().stream()
+        .map(ExecutableSequenceFlow::getSource)
+        .anyMatch(
+            flowNode -> flowNode.getElementType().equals(BpmnElementType.EVENT_BASED_GATEWAY));
+  }
+
+  private record ElementIdAndType(String elementId, BpmnElementType elementType) {}
 }
