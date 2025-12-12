7ec083f2ac9a97725ef16f4a5efe8659df02429c
John Montgomery <john.montgomery@camunda.com>
2025-12-12 20:25:49 +0000

    test: ensure form readable in ES/OS before calling endpoint in tests
    Otherwise we end up with a 404 and the tests fail

---
+++ tasklist/qa/integration-tests/src/test/java/io/camunda/tasklist/util/TestChecks.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.tasklist.util;
+
+import static io.camunda.tasklist.util.TestCheck.FORM_EXISTS_CHECK;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.tasklist.exceptions.NotFoundException;
+import io.camunda.tasklist.store.FormStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestChecks {
+  @Autowired private FormStore formStore;
+
+  @Bean(name = FORM_EXISTS_CHECK)
+  public TestCheck getFormExistsCheck() {
+    return new TestCheck() {
+      @Override
+      public String getName() {
+        return FORM_EXISTS_CHECK;
+      }
+
+      @Override
+      public boolean test(final Object[] objects) {
+        assertThat(objects).hasSize(3);
+        assertThat(objects[0]).isInstanceOf(String.class);
+        assertThat(objects[1]).isInstanceOf(String.class);
+        if (objects[2] != null) {
+          assertThat(objects[2]).isInstanceOf(Long.class);
+        }
+        final String processDefinitionId = (String) objects[0];
+        final String formId = (String) objects[1];
+        final Long versionId = (Long) objects[2];
+        try {
+          return formStore.getForm(formId, processDefinitionId, versionId) != null;
+        } catch (final NotFoundException ex) {
+          return false;
+        }
+      }
+    };
+  }
+}
