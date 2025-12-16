6b382072d313d15eed10809621f57265b1c45550
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2025-12-16 13:15:14 +0100

    refactor: adhere to coding guidelines

--- gateways/gateway-mcp/src/main/java/io/camunda/zeebe/gateway/mcp/model/IncidentSearchQuerySortRequest.java
+++ gateways/gateway-mcp/src/main/java/io/camunda/zeebe/gateway/mcp/model/IncidentSearchQuerySortRequest.java
@@ -13,6 +13,102 @@ import java.util.Objects;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class IncidentSearchQuerySortRequest {
 
+  private FieldEnum field;
+  private SortOrderEnum order = SortOrderEnum.ASC;
+
+  public IncidentSearchQuerySortRequest() {
+    super();
+  }
+
+  /** Constructor with only required parameters */
+  public IncidentSearchQuerySortRequest(final FieldEnum field) {
+    this.field = field;
+  }
+
+  public IncidentSearchQuerySortRequest field(final FieldEnum field) {
+    this.field = field;
+    return this;
+  }
+
+  /**
+   * The field to sort by.
+   *
+   * @return field
+   */
+  @NotNull
+  @Schema(
+      name = "field",
+      description = "The field to sort by.",
+      requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("field")
+  public FieldEnum getField() {
+    return field;
+  }
+
+  public void setField(final FieldEnum field) {
+    this.field = field;
+  }
+
+  public IncidentSearchQuerySortRequest order(final SortOrderEnum order) {
+    this.order = order;
+    return this;
+  }
+
+  /**
+   * Get order
+   *
+   * @return order
+   */
+  @Valid
+  @Schema(name = "order", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+  @JsonProperty("order")
+  public SortOrderEnum getOrder() {
+    return order;
+  }
+
+  public void setOrder(final SortOrderEnum order) {
+    this.order = order;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(field, order);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    final IncidentSearchQuerySortRequest incidentSearchQuerySortRequest =
+        (IncidentSearchQuerySortRequest) o;
+    return Objects.equals(field, incidentSearchQuerySortRequest.field)
+        && Objects.equals(order, incidentSearchQuerySortRequest.order);
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder();
+    sb.append("class IncidentSearchQuerySortRequest {\n");
+    sb.append("    field: ").append(toIndentedString(field)).append("\n");
+    sb.append("    order: ").append(toIndentedString(order)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(final Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
   /** The field to sort by. */
   public enum FieldEnum {
     INCIDENT_KEY("incidentKey"),
@@ -41,7 +137,7 @@ public class IncidentSearchQuerySortRequest {
 
     private final String value;
 
-    FieldEnum(String value) {
+    FieldEnum(final String value) {
       this.value = value;
     }
 
@@ -56,110 +152,13 @@ public class IncidentSearchQuerySortRequest {
     }
 
     @JsonCreator
-    public static FieldEnum fromValue(String value) {
-      for (FieldEnum b : FieldEnum.values()) {
+    public static FieldEnum fromValue(final String value) {
+      for (final FieldEnum b : FieldEnum.values()) {
         if (b.value.equalsIgnoreCase(value)) {
           return b;
         }
       }
       throw new IllegalArgumentException("Unexpected value '" + value + "'");
     }
-  }
-
-  private FieldEnum field;
-
-  private SortOrderEnum order = SortOrderEnum.ASC;
-
-  public IncidentSearchQuerySortRequest() {
-    super();
-  }
-
-  /** Constructor with only required parameters */
-  public IncidentSearchQuerySortRequest(FieldEnum field) {
-    this.field = field;
-  }
-
-  public IncidentSearchQuerySortRequest field(FieldEnum field) {
-    this.field = field;
-    return this;
-  }
-
-  /**
-   * The field to sort by.
-   *
-   * @return field
-   */
-  @NotNull
-  @Schema(
-      name = "field",
-      description = "The field to sort by.",
-      requiredMode = Schema.RequiredMode.REQUIRED)
-  @JsonProperty("field")
-  public FieldEnum getField() {
-    return field;
-  }
-
-  public void setField(FieldEnum field) {
-    this.field = field;
-  }
-
-  public IncidentSearchQuerySortRequest order(SortOrderEnum order) {
-    this.order = order;
-    return this;
-  }
-
-  /**
-   * Get order
-   *
-   * @return order
-   */
-  @Valid
-  @Schema(name = "order", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
-  @JsonProperty("order")
-  public SortOrderEnum getOrder() {
-    return order;
-  }
-
-  public void setOrder(SortOrderEnum order) {
-    this.order = order;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    IncidentSearchQuerySortRequest incidentSearchQuerySortRequest =
-        (IncidentSearchQuerySortRequest) o;
-    return Objects.equals(this.field, incidentSearchQuerySortRequest.field)
-        && Objects.equals(this.order, incidentSearchQuerySortRequest.order);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(field, order);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class IncidentSearchQuerySortRequest {\n");
-    sb.append("    field: ").append(toIndentedString(field)).append("\n");
-    sb.append("    order: ").append(toIndentedString(order)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
   }
 }
