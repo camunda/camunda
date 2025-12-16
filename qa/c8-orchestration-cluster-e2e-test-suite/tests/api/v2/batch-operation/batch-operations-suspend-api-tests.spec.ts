c0c8325f8400376462adf5d92cfb8efdf477f5b3
paulkdeuster <paul.deuster@holisticon.de>
2025-12-16 11:17:27 +0100

    test: Add suspend batch operation tests and enhance request helpers
    (cherry picked from commit 49b5a46ae23334a3499817c8263dea476347b862)

---
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/batch-operation/batch-operations-suspend-api-tests.spec.ts
@@ -0,0 +1,122 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {test} from '@playwright/test';
+import {deploy} from '../../../../utils/zeebeClient';
+import {
+  assertBadRequest,
+  assertConflictRequest,
+  assertNotFoundRequest,
+  assertStatusCode,
+  assertUnauthorizedRequest,
+  buildUrl,
+  jsonHeaders,
+} from '../../../../utils/http';
+import {
+  createCancellationBatch,
+  createCompletedBatchOperation,
+  expectBatchState,
+  notFoundDetail,
+  suspendBatchOperation,
+} from '@requestHelpers';
+
+/* eslint-disable playwright/expect-expect */
+test.describe.parallel('Suspend Batch Operation Tests', () => {
+  test.beforeAll(async () => {
+    await deploy(['./resources/batch_cancellation_process.bpmn']);
+  });
+
+  test('Suspend active batch operation returns 204 and status becomes SUSPENDED', async ({
+    request,
+  }) => {
+    const key =
+      await test.step('Create cancelable batch operation', async () => {
+        return createCancellationBatch(request);
+      });
+
+    await test.step('Send suspend request', async () => {
+      const res = await suspendBatchOperation(request, key);
+      await assertStatusCode(res, 204);
+    });
+
+    await test.step('Poll until batch operation is suspended', async () => {
+      await expectBatchState(request, key, 'SUSPENDED');
+    });
+  });
+
+  test('Suspend batch operation twice fails on second request', async ({
+    request,
+  }) => {
+    const key =
+      await test.step('Create cancelable batch operation', async () => {
+        return createCancellationBatch(request);
+      });
+
+    await test.step('Suspend batch operation once', async () => {
+      const res = await suspendBatchOperation(request, key);
+      await assertStatusCode(res, 204);
+    });
+
+    await test.step('Wait for suspended state', async () => {
+      await expectBatchState(request, key, 'SUSPENDED');
+    });
+
+    await test.step('Suspend already suspended batch operation', async () => {
+      const doubleRes = await suspendBatchOperation(request, key);
+      await assertConflictRequest(doubleRes, 'INVALID_STATE');
+    });
+  });
+
+  test('Suspend finished batch operation returns 404', async ({request}) => {
+    const key =
+      await test.step('Create completed batch operation', async () => {
+        return createCompletedBatchOperation(request);
+      });
+    const res = await suspendBatchOperation(request, key);
+    await assertNotFoundRequest(res, notFoundDetail(key));
+  });
+
+  test('Suspend batch operation with unknown key returns 404', async ({
+    request,
+  }) => {
+    const unknownKey = '2251799813999999';
+    const res = await suspendBatchOperation(request, unknownKey);
+    await assertNotFoundRequest(res, notFoundDetail(unknownKey));
+  });
+
+  test('Suspend batch operation with invalid key returns 400', async ({
+    request,
+  }) => {
+    const res = await request.post(
+      buildUrl('/batch-operations/not-a-valid-key/suspension'),
+      {
+        headers: jsonHeaders(),
+      },
+    );
+    await assertBadRequest(
+      res,
+      "Batch operation id 'not-a-valid-key' is not a valid number. Legacy Batch Operation IDs are not supported!",
+    );
+  });
+
+  test('Suspend batch operation without auth returns 401', async ({
+    request,
+  }) => {
+    const key =
+      await test.step('Create batch operation for auth test', async () => {
+        return createCancellationBatch(request);
+      });
+    const res = await request.post(
+      buildUrl(`/batch-operations/${key}/suspension`),
+      {
+        data: {},
+      },
+    );
+    await assertUnauthorizedRequest(res);
+  });
+});
