9a45628980fc3826e6de33d61a7a873da4d23e56
Catherina Prokudina <catherina.prokudina@camunda.com>
2025-12-16 19:10:10 +0100

    test: decision-requirement api test unhappy path

--- qa/c8-orchestration-cluster-e2e-test-suite/utils/requestHelpers/decision-requirement-requestHelper.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/utils/requestHelpers/decision-requirement-requestHelper.ts
@@ -12,7 +12,7 @@ import {assertStatusCode, buildUrl, jsonHeaders} from '../http';
 import {decisionDefinitionRequiredFields} from '../beans/requestBeans';
 import {validateResponse} from '../../json-body-assertions';
 import {deploy} from '../zeebeClient';
-import { DecisionDeployment, DecisionRequirementsDeployment } from '@camunda8/sdk/dist/c8/lib/C8Dto';
+import {DecisionRequirementsDeployment} from '@camunda8/sdk/dist/c8/lib/C8Dto';
 import { defaultAssertionOptions } from 'utils/constants';
 
 export async function deployMammalDecisionAndStoreResponse(
@@ -21,4 +21,12 @@ export async function deployMammalDecisionAndStoreResponse(
   const response = await deploy(['./resources/isMammal_.dmn',]);
   expect(response.decisionRequirements.length).toBe(1);
   decisionRequirements.push(response.decisionRequirements[0] as DecisionRequirementsDeployment);
+}
+
+export async function deployTwoSimpleDecisionsAndStoreResponse(decisionRequirements: DecisionRequirementsDeployment[]){
+  const response = await deploy(['./resources/simpleDecisionTable1.dmn','./resources/simpleDecisionTable2.dmn']);
+  expect(response.decisionRequirements.length).toBe(2);
+  for (const decisionRequirementElement of response.decisionRequirements) {
+    decisionRequirements.push(decisionRequirementElement as DecisionRequirementsDeployment);
+  }
 }
