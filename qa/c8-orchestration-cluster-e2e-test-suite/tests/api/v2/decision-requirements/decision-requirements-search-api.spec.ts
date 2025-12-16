ed3008acda369a597b6b7818a27bddbb72d41014
Catherina Prokudina <catherina.prokudina@camunda.com>
2025-12-16 19:10:10 +0100

    test: decision-requirement api test unhappy path

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/decision-requirements/decision-requirements-search-api.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/decision-requirements/decision-requirements-search-api.spec.ts
@@ -17,7 +17,7 @@ import {
   assertRequiredFields,
 } from '../../../../utils/http';
 import {defaultAssertionOptions} from '../../../../utils/constants';
-import {deployMammalDecisionAndStoreResponse} from '@requestHelpers';
+import {deployMammalDecisionAndStoreResponse, deployTwoSimpleDecisionsAndStoreResponse} from '@requestHelpers';
 import {DecisionRequirementsDeployment} from '@camunda8/sdk/dist/c8/lib/C8Dto';
 import {validateResponse} from '../../../../json-body-assertions';
 import { decisionRequirementRequiredFields } from 'utils/beans/requestBeans';
@@ -32,9 +32,10 @@ test.describe.parallel('Search Decision Requirements API Tests', () => {
     await deployMammalDecisionAndStoreResponse(
       decisionRequirements,
     );
+    await deployTwoSimpleDecisionsAndStoreResponse(decisionRequirements);
   });
 
-  test('Search decision requirements - 1 result - success', async ({request}) => {
+  test('Search decision requirements - multiple results - success', async ({request}) => {
     await expect(async () => {
       const res = await request.post(
         buildUrl(DECISION_REQUIREMENTS_SEARCH_ENDPOINT, {}),
@@ -55,19 +56,19 @@ test.describe.parallel('Search Decision Requirements API Tests', () => {
       );
 
       const body = await res.json();
-      expect(body.items.length).toBeGreaterThanOrEqual(1);
+      expect(body.items.length).toBeGreaterThanOrEqual(3);
       expect(Array.isArray(body.items)).toBe(true);
     }).toPass(defaultAssertionOptions);
   });
 
-  test('Search decision requirements by decisionRequirementsName success', async ({request}) => {
-    const decisionRequirementToSearch = decisionRequirements[0];
+  test('Search decision requirements by version success', async ({request}) => {
+    const decisionRequirementToSearch = decisionRequirements[1];
     await expect(async () => {
       const res = await request.post(buildUrl(DECISION_REQUIREMENTS_SEARCH_ENDPOINT), {
         headers: jsonHeaders(),
         data: {
           filter: {
-            decisionRequirementsName: decisionRequirementToSearch.decisionRequirementsName,
+            version: decisionRequirementToSearch.version,
           },
         },
       });
@@ -84,10 +85,99 @@ test.describe.parallel('Search Decision Requirements API Tests', () => {
       );
 
       const body = await res.json();
-      assertRequiredFields(body, ['items', 'page']);
+      expect(body.page.totalItems).toBeGreaterThanOrEqual(3);
+    }).toPass(defaultAssertionOptions);
+  });
+
+  test('Search decision requirements by decisionRequirementsName and version success', async ({request}) => {
+    const decisionRequirementToSearch = decisionRequirements[0];
+    await expect(async () => {
+      const res = await request.post(buildUrl(DECISION_REQUIREMENTS_SEARCH_ENDPOINT), {
+        headers: jsonHeaders(),
+        data: {
+          filter: {
+            decisionRequirementsName: decisionRequirementToSearch.decisionRequirementsName,
+            version: decisionRequirementToSearch.version,
+          },
+        },
+      });
+
+      await assertStatusCode(res, 200);
+
+      await validateResponse(
+        {
+        path: DECISION_REQUIREMENTS_SEARCH_ENDPOINT,
+        method: 'POST',
+        status: '200',
+        },
+        res,
+      );
+
+      const body = await res.json();
       expect(body.items.length).toEqual(1);
       expect(body.page.totalItems).toBeGreaterThanOrEqual(1);
       assertEqualsForKeys(decisionRequirementToSearch, body.items[0], decisionRequirementRequiredFields);
     }).toPass(defaultAssertionOptions);
   });
+
+  test('Search decision requirements - unauthorized request', async ({request}) => {
+    await expect(async () => {
+        const res = await request.post(buildUrl(DECISION_REQUIREMENTS_SEARCH_ENDPOINT), {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: {},
+        });
+
+        await assertUnauthorizedRequest(res);
+     }).toPass(defaultAssertionOptions);
+   });
+
+   test('Search decision requirements - empty result', async ({request}) => {
+    const someNotExistingDecisionRequirementsName = 'someRandomDecisionRequirementsName';
+    await expect(async () => {
+      const res = await request.post(buildUrl(DECISION_REQUIREMENTS_SEARCH_ENDPOINT), {
+        headers: jsonHeaders(),
+          data: {
+            filter: {
+              decisionRequirementsName: someNotExistingDecisionRequirementsName,
+            },
+          },
+      });
+
+      await assertStatusCode(res, 200);
+
+      await validateResponse(
+        {
+        path: DECISION_REQUIREMENTS_SEARCH_ENDPOINT,
+        method: 'POST',
+        status: '200',
+        },
+        res,
+      );
+
+      const body = await res.json();
+      expect(body.items.length).toEqual(0);
+      expect(body.page.totalItems).toEqual(0);
+     }).toPass(defaultAssertionOptions);
+   });
+
+   test('Search decision requirements - invalid filter', async ({request}) => {
+    const someRandomFilterValue = 'meow';
+    await expect(async () => {
+      const res = await request.post(buildUrl(DECISION_REQUIREMENTS_SEARCH_ENDPOINT), {
+        headers: jsonHeaders(),
+          data: {
+            filter: {
+              randomNotExistingFieldName: someRandomFilterValue,
+            },
+          },
+      });
+
+      await assertBadRequest(
+        res,
+        'Request property [filter.randomNotExistingFieldName] cannot be parsed',
+      );
+     }).toPass(defaultAssertionOptions);
+   });
 });
