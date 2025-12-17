dbf3f554ac97a99f1b512ba0bf8d2006ff069250
esraagamal6 <esraa-gamal@hotmail.com>
2025-12-17 13:53:09 +0100

    test: migrate dashboard.spec to OC test suite

--- qa/c8-orchestration-cluster-e2e-test-suite/pages/OperateDashboardPage.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/pages/OperateDashboardPage.ts
@@ -11,13 +11,39 @@ import {Page, Locator} from '@playwright/test';
 export class OperateDashboardPage {
   private page: Page;
   readonly metricPanel: Locator;
+  readonly incidentInstancesBadge: Locator;
+  readonly activeInstancesBadge: Locator;
+  readonly totalInstancesLink: Locator;
+  readonly activeInstancesLink: Locator;
+  readonly incidentInstancesLink: Locator;
+  readonly instancesByProcess: Locator;
+  readonly incidentsByError: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.metricPanel = page.getByTestId('metric-panel');
+    this.incidentInstancesBadge = page
+      .getByTestId('incident-instances-badge')
+      .first();
+    this.activeInstancesBadge = page
+      .getByTestId('active-instances-badge')
+      .first();
+    this.totalInstancesLink = page.getByTestId('total-instances-link');
+    this.activeInstancesLink = page.getByTestId('active-instances-link');
+    this.incidentInstancesLink = page.getByTestId('incident-instances-link');
+    this.instancesByProcess = page.getByTestId('instances-by-process');
+    this.incidentsByError = page.getByTestId('incident-byError');
   }
 
   async gotoDashboardPage(options?: Parameters<Page['goto']>[1]) {
     await this.page.goto('/operate', options);
+  }
+
+  getInstancesByProcessItem(index: number): Locator {
+    return this.page.getByTestId(`instances-by-process-${index}`);
+  }
+
+  getIncidentsByErrorItem(index: number): Locator {
+    return this.page.getByTestId(`incident-byError-${index}`);
   }
 }
