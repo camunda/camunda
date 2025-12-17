2a7ea7c7414c2f5bb08bedb2a2088368e0336610
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2025-12-17 09:47:37 +0100

    test: ensure topology functionality
    Verifies the topology functionality still works as expected.
    The REST controller tests adjust to mocking the service layer instead of implementation details.
    The former tests to ensure broker client and topology manager move to a new service layer test for topology services.

--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/configuration/RestApiBrokerGatewayDisabledTest.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/configuration/RestApiBrokerGatewayDisabledTest.java
@@ -44,6 +44,6 @@ public class RestApiBrokerGatewayDisabledTest extends RestApiConfigurationTest {
         .expectStatus()
         .isNotFound();
 
-    verifyNoInteractions(topologyManager);
+    verifyNoInteractions(topologyServices);
   }
 }
