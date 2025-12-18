1b35f6c0ab496f15b4e28a3c9122446494a92596
Carlo Sana <carlo.sana@camunda.com>
2025-12-18 13:55:28 +0100

    feat: include PreAllocationStrategy in `configuration` module
    (cherry picked from commit ff1008468e6ad526fe67c0820659dd29c86b22cf)
    (cherry picked from commit 1e1fad505c3c789fd9b07a4cc8cd52b2fdca4e21)

--- configuration/src/test/java/io/camunda/configuration/RaftTest.java
+++ configuration/src/test/java/io/camunda/configuration/RaftTest.java
@@ -12,117 +12,126 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.camunda.configuration.beanoverrides.BrokerBasedPropertiesOverride;
 import io.camunda.configuration.beans.BrokerBasedProperties;
 import io.camunda.zeebe.broker.system.configuration.ExperimentalRaftCfg.PreAllocateStrategy;
-import org.junit.jupiter.api.Nested;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 
-@SpringJUnitConfig({
-  UnifiedConfiguration.class,
-  BrokerBasedPropertiesOverride.class,
-  UnifiedConfigurationHelper.class
-})
-@ActiveProfiles("broker")
 public class RaftTest {
 
-  @Nested
-  @TestPropertySource(
-      properties = {
-        "camunda.cluster.raft.preallocate-segment-files=true",
-        "camunda.cluster.raft.segment-preallocation-strategy=POSIX"
-      })
-  class WithOnlyUnifiedConfigSet {
-    final BrokerBasedProperties brokerCfg;
+  private final ApplicationContextRunner brokerRunner =
+      new ApplicationContextRunner()
+          .withUserConfiguration(
+              UnifiedConfiguration.class,
+              BrokerBasedPropertiesOverride.class,
+              UnifiedConfigurationHelper.class)
+          .withPropertyValues("spring.profiles.active=broker");
 
-    WithOnlyUnifiedConfigSet(@Autowired final BrokerBasedProperties brokerCfg) {
-      this.brokerCfg = brokerCfg;
-    }
-
-    @Test
-    void shouldSetSegmentPreallocationStrategy() {
-      assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
-          .isEqualTo(PreAllocateStrategy.POSIX);
-    }
+  private static Stream<Arguments> segmentPreallocationStrategies() {
+    return Stream.of(PreAllocateStrategy.values()).map(s -> Arguments.of(s.name(), s));
   }
 
-  @Nested
-  @TestPropertySource(
-      properties = {
-        "zeebe.broker.experimental.raft.preallocateSegmentFiles=true",
-        "zeebe.broker.experimental.raft.segmentPreallocationStrategy=POSIX"
-      })
-  class WithOnlyLegacySet {
-    final BrokerBasedProperties brokerCfg;
-
-    WithOnlyLegacySet(@Autowired final BrokerBasedProperties brokerCfg) {
-      this.brokerCfg = brokerCfg;
-    }
-
-    @Test
-    void shouldSetSegmentPreallocationStrategyFromLegacy() {
-      assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
-          .isEqualTo(PreAllocateStrategy.POSIX);
-    }
+  @Test
+  void shouldSetSegmentPreallocationStrategyFromUnifiedConfig() {
+    brokerRunner
+        .withPropertyValues(
+            "camunda.cluster.raft.preallocate-segment-files=true",
+            "camunda.cluster.raft.segment-preallocation-strategy=POSIX")
+        .run(
+            context -> {
+              final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+              assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+                  .isEqualTo(PreAllocateStrategy.POSIX);
+            });
   }
 
-  @Nested
-  @TestPropertySource(
-      properties = {
-        // new
-        "camunda.cluster.raft.preallocate-segment-files=true",
-        "camunda.cluster.raft.segment-preallocation-strategy=FILL",
-        // legacy
-        "zeebe.broker.experimental.raft.preallocateSegmentFiles=true",
-        "zeebe.broker.experimental.raft.segmentPreallocationStrategy=POSIX"
-      })
-  class WithNewAndLegacySet {
-    final BrokerBasedProperties brokerCfg;
-
-    WithNewAndLegacySet(@Autowired final BrokerBasedProperties brokerCfg) {
-      this.brokerCfg = brokerCfg;
-    }
-
-    @Test
-    void shouldPreferNewSegmentPreallocationStrategy() {
-      assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
-          .isEqualTo(PreAllocateStrategy.FILL);
-    }
+  @Test
+  void shouldSetSegmentPreallocationStrategyFromLegacyConfig() {
+    brokerRunner
+        .withPropertyValues(
+            "zeebe.broker.experimental.raft.preallocateSegmentFiles=true",
+            "zeebe.broker.experimental.raft.segmentPreallocationStrategy=POSIX")
+        .run(
+            context -> {
+              final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+              assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+                  .isEqualTo(PreAllocateStrategy.POSIX);
+            });
   }
 
-  @Nested
-  @TestPropertySource(
-      properties = {
-        "camunda.cluster.raft.preallocate-segment-files=false",
-        "camunda.cluster.raft.segment-preallocation-strategy=POSIX"
-      })
-  class WithPreallocationDisabled {
-    final BrokerBasedProperties brokerCfg;
-
-    WithPreallocationDisabled(@Autowired final BrokerBasedProperties brokerCfg) {
-      this.brokerCfg = brokerCfg;
-    }
-
-    @Test
-    void shouldForceStrategyToNoop() {
-      assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
-          .isEqualTo(PreAllocateStrategy.NOOP);
-    }
+  @Test
+  void shouldPreferNewSegmentPreallocationStrategyWhenBothAreSet() {
+    brokerRunner
+        .withPropertyValues(
+            // new
+            "camunda.cluster.raft.preallocate-segment-files=true",
+            "camunda.cluster.raft.segment-preallocation-strategy=FILL",
+            // legacy
+            "zeebe.broker.experimental.raft.preallocateSegmentFiles=true",
+            "zeebe.broker.experimental.raft.segmentPreallocationStrategy=POSIX")
+        .run(
+            context -> {
+              final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+              assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+                  .isEqualTo(PreAllocateStrategy.FILL);
+            });
   }
 
-  @Nested
-  class WithoutNewAndLegacySet {
-    final BrokerBasedProperties brokerCfg;
+  @Test
+  void shouldForceStrategyToNoopWhenPreallocationDisabled() {
+    brokerRunner
+        .withPropertyValues(
+            "camunda.cluster.raft.preallocate-segment-files=false",
+            "camunda.cluster.raft.segment-preallocation-strategy=POSIX")
+        .run(
+            context -> {
+              final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+              assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+                  .isEqualTo(PreAllocateStrategy.NOOP);
+            });
+  }
 
-    WithoutNewAndLegacySet(@Autowired final BrokerBasedProperties brokerCfg) {
-      this.brokerCfg = brokerCfg;
-    }
+  @Test
+  void shouldUseDefaultSegmentPreallocationStrategy() {
+    brokerRunner.run(
+        context -> {
+          final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+          assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+              .isEqualTo(PreAllocateStrategy.FILL);
+        });
+  }
 
-    @Test
-    void shouldUseDefaultSegmentPreallocationStrategy() {
-      assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
-          .isEqualTo(PreAllocateStrategy.FILL);
-    }
+  @ParameterizedTest(name = "unified strategy '{0}' maps to {1}")
+  @MethodSource("segmentPreallocationStrategies")
+  void shouldMapUnifiedStrategyStringToEnum(
+      final String strategy, final PreAllocateStrategy expected) {
+    brokerRunner
+        .withPropertyValues(
+            "camunda.cluster.raft.preallocate-segment-files=true",
+            "camunda.cluster.raft.segment-preallocation-strategy=" + strategy)
+        .run(
+            context -> {
+              final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+              assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+                  .isEqualTo(expected);
+            });
+  }
+
+  @ParameterizedTest(name = "legacy strategy '{0}' maps to {1}")
+  @MethodSource("segmentPreallocationStrategies")
+  void shouldMapLegacyStrategyStringToEnum(
+      final String strategy, final PreAllocateStrategy expected) {
+    brokerRunner
+        .withPropertyValues(
+            "zeebe.broker.experimental.raft.preallocateSegmentFiles=true",
+            "zeebe.broker.experimental.raft.segmentPreallocationStrategy=" + strategy)
+        .run(
+            context -> {
+              final var brokerCfg = context.getBean(BrokerBasedProperties.class);
+              assertThat(brokerCfg.getExperimental().getRaft().getSegmentPreallocationStrategy())
+                  .isEqualTo(expected);
+            });
   }
 }
