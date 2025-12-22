b64fc296c0dca92d4eb2355c496eab5f3981ff14
Carlo Sana <carlo.sana@camunda.com>
2025-12-22 18:03:21 +0100

    build: move EnumParserBenchmark from zeebe/msgpack to microbenchmarks

--- microbenchmarks/src/main/java/io/camunda/microbenchmarks/msgpack/EnumParserBenchmark.java
+++ microbenchmarks/src/main/java/io/camunda/microbenchmarks/msgpack/EnumParserBenchmark.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.msgpack.perf;
+package io.camunda.microbenchmarks.msgpack;
 
 import io.camunda.zeebe.msgpack.util.EnumParser;
 import io.camunda.zeebe.msgpack.util.TrieEnumParser;
@@ -14,7 +14,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeUnit;
 import org.agrona.DirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -30,7 +39,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @State(Scope.Benchmark)
 @Warmup(iterations = 3, time = 2)
 @Measurement(iterations = 3, time = 3)
-@Fork(1)
+@Fork(3)
 public class EnumParserBenchmark {
 
   // Parsers
