683a646f44fa8ac5056a2fd6578d829c4065ec85
Fabio Paini <fabio.paini@camunda.com>
2025-12-22 16:53:26 +0100

    test: [#43183] add utilities to generate GLOBAL_LISTENER commands in tests

---
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/util/client/GlobalListenerClient.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.util.client;
+
+import io.camunda.zeebe.protocol.impl.record.value.globallistener.GlobalListenerRecord;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.intent.GlobalListenerIntent;
+import io.camunda.zeebe.protocol.record.value.GlobalListenerRecordValue;
+import io.camunda.zeebe.test.util.record.RecordingExporter;
+import java.util.function.BiFunction;
+
+public final class GlobalListenerClient {
+
+  private static final BiFunction<Long, GlobalListenerIntent, Record<GlobalListenerRecordValue>>
+      SUCCESS_SUPPLIER =
+          (sourceRecordPosition, intent) ->
+              RecordingExporter.globalListenerRecords()
+                  .withIntent(intent)
+                  .withSourceRecordPosition(sourceRecordPosition)
+                  .getFirst();
+  private static final BiFunction<Long, GlobalListenerIntent, Record<GlobalListenerRecordValue>>
+      REJECTION_SUPPLIER =
+          (sourceRecordPosition, intent) ->
+              RecordingExporter.globalListenerRecords()
+                  .onlyCommandRejections()
+                  .withSourceRecordPosition(sourceRecordPosition)
+                  .getFirst();
+
+  private final GlobalListenerRecord globalListenerRecord;
+  private final CommandWriter writer;
+  private BiFunction<Long, GlobalListenerIntent, Record<GlobalListenerRecordValue>> expectation =
+      SUCCESS_SUPPLIER;
+
+  public GlobalListenerClient(final CommandWriter writer) {
+    globalListenerRecord = new GlobalListenerRecord();
+    this.writer = writer;
+  }
+
+  public GlobalListenerClient withType(final String type) {
+    globalListenerRecord.setType(type);
+    return this;
+  }
+
+  public GlobalListenerClient withRetries(final int retries) {
+    globalListenerRecord.setRetries(retries);
+    return this;
+  }
+
+  public GlobalListenerClient withEventType(final String eventType) {
+    globalListenerRecord.addEventType(eventType);
+    return this;
+  }
+
+  public GlobalListenerClient withAfterNonGlobal(final boolean afterNonGlobal) {
+    globalListenerRecord.setAfterNonGlobal(afterNonGlobal);
+    return this;
+  }
+
+  public GlobalListenerClient afterNonGlobal() {
+    return withAfterNonGlobal(true);
+  }
+
+  public GlobalListenerClient beforeNonGlobal() {
+    return withAfterNonGlobal(false);
+  }
+
+  public GlobalListenerClient expectRejection() {
+    expectation = REJECTION_SUPPLIER;
+    return this;
+  }
+
+  public Record<GlobalListenerRecordValue> create() {
+    final long position = writer.writeCommand(GlobalListenerIntent.CREATE, globalListenerRecord);
+    return expectation.apply(position, GlobalListenerIntent.CREATED);
+  }
+
+  public Record<GlobalListenerRecordValue> update() {
+    final long position = writer.writeCommand(GlobalListenerIntent.UPDATE, globalListenerRecord);
+    return expectation.apply(position, GlobalListenerIntent.UPDATED);
+  }
+
+  public Record<GlobalListenerRecordValue> delete() {
+    final long position = writer.writeCommand(GlobalListenerIntent.DELETE, globalListenerRecord);
+    return expectation.apply(position, GlobalListenerIntent.DELETED);
+  }
+}
