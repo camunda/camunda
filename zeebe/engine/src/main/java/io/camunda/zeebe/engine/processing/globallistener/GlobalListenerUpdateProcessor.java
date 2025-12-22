e022715952060cd5ef28e8620ff44488dab80ae3
Fabio Paini <fabio.paini@camunda.com>
2025-12-22 14:12:07 +0100

    feat: [#43182] check authorization in global listeners commands

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/globallistener/GlobalListenerUpdateProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/globallistener/GlobalListenerUpdateProcessor.java
@@ -7,18 +7,19 @@
  */
 package io.camunda.zeebe.engine.processing.globallistener;
 
-import io.camunda.zeebe.engine.processing.ExcludeAuthorizationCheck;
 import io.camunda.zeebe.engine.processing.distribution.CommandDistributionBehavior;
 import io.camunda.zeebe.engine.processing.identity.authorization.AuthorizationCheckBehavior;
+import io.camunda.zeebe.engine.processing.identity.authorization.request.AuthorizationRequest;
 import io.camunda.zeebe.engine.processing.streamprocessor.DistributedTypedRecordProcessor;
 import io.camunda.zeebe.engine.processing.streamprocessor.writers.Writers;
 import io.camunda.zeebe.engine.state.distribution.DistributionQueue;
 import io.camunda.zeebe.protocol.impl.record.value.globallistener.GlobalListenerRecord;
 import io.camunda.zeebe.protocol.record.intent.GlobalListenerIntent;
+import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
+import io.camunda.zeebe.protocol.record.value.PermissionType;
 import io.camunda.zeebe.stream.api.records.TypedRecord;
 import io.camunda.zeebe.stream.api.state.KeyGenerator;
 
-@ExcludeAuthorizationCheck
 public final class GlobalListenerUpdateProcessor
     implements DistributedTypedRecordProcessor<GlobalListenerRecord> {
 
@@ -40,6 +41,20 @@ public final class GlobalListenerUpdateProcessor
 
   @Override
   public void processNewCommand(final TypedRecord<GlobalListenerRecord> command) {
+    final AuthorizationRequest authRequest =
+        AuthorizationRequest.builder()
+            .command(command)
+            .resourceType(AuthorizationResourceType.GLOBAL_LISTENER)
+            .permissionType(PermissionType.UPDATE_TASK_LISTENER)
+            .build();
+    final var isAuthorized = authCheckBehavior.isAuthorizedOrInternalCommand(authRequest);
+    if (isAuthorized.isLeft()) {
+      final var rejection = isAuthorized.getLeft();
+      writers.rejection().appendRejection(command, rejection.type(), rejection.reason());
+      writers.response().writeRejectionOnCommand(command, rejection.type(), rejection.reason());
+      return;
+    }
+
     final long key = keyGenerator.nextKey();
     executeCommand(key, command.getValue());
 
