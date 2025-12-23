f40f5af3e628c7cc76ef9d718949ed26f149df88
Konstantinos Mylosis <konstantinos.mylosis@camunda.com>
2025-12-23 12:56:32 +0200

    feat: history deletion common exporter configuration

---
+++ zeebe/exporter-common/src/main/java/io/camunda/zeebe/exporter/common/historydeletion/HistoryDeletionConfiguration.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.exporter.common.historydeletion;
+
+import java.time.Duration;
+
+public final class HistoryDeletionConfiguration {
+
+  private Duration delayBetweenRuns = Duration.ofSeconds(1);
+  private Duration maxDelayBetweenRuns = Duration.ofMinutes(5);
+  private int queueBatchSize = 100;
+  private int dependentRowLimit = 10000;
+
+  public Duration getDelayBetweenRuns() {
+    return delayBetweenRuns;
+  }
+
+  public void setDelayBetweenRuns(final Duration delayBetweenRuns) {
+    this.delayBetweenRuns = delayBetweenRuns;
+  }
+
+  public Duration getMaxDelayBetweenRuns() {
+    return maxDelayBetweenRuns;
+  }
+
+  public void setMaxDelayBetweenRuns(final Duration maxDelayBetweenRuns) {
+    this.maxDelayBetweenRuns = maxDelayBetweenRuns;
+  }
+
+  public int getQueueBatchSize() {
+    return queueBatchSize;
+  }
+
+  public void setQueueBatchSize(final int queueBatchSize) {
+    this.queueBatchSize = queueBatchSize;
+  }
+
+  public int getDependentRowLimit() {
+    return dependentRowLimit;
+  }
+
+  public void setDependentRowLimit(final int dependentRowLimit) {
+    this.dependentRowLimit = dependentRowLimit;
+  }
+
+  @Override
+  public String toString() {
+    return "HistoryDeletionConfiguration{"
+        + "delayBetweenRuns="
+        + delayBetweenRuns
+        + ", maxDelayBetweenRuns="
+        + maxDelayBetweenRuns
+        + ", queueBatchSize="
+        + queueBatchSize
+        + ", dependentRowLimit="
+        + dependentRowLimit
+        + '}';
+  }
+}
