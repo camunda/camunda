d92deece25ee234f7d11c89096d98e7853607ddc
Eddie Tsedeke <6949627+tsedekey@users.noreply.github.com>
2025-12-29 17:29:26 +0300

    feat: add rdbms parent endpoint

--- db/rdbms/src/main/java/io/camunda/db/rdbms/read/service/IncidentProcessInstanceStatisticsByErrorDbReader.java
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/read/service/IncidentProcessInstanceStatisticsByErrorDbReader.java
@@ -7,6 +7,7 @@
  */
 package io.camunda.db.rdbms.read.service;
 
+import io.camunda.db.rdbms.read.domain.IncidentProcessInstanceStatisticsByErrorDbQuery;
 import io.camunda.db.rdbms.sql.IncidentMapper;
 import io.camunda.db.rdbms.sql.columns.IncidentProcessInstanceStatisticsByErrorSearchColumn;
 import io.camunda.search.clients.reader.IncidentProcessInstanceStatisticsByErrorReader;
@@ -14,20 +15,61 @@ import io.camunda.search.entities.IncidentProcessInstanceStatisticsByErrorEntity
 import io.camunda.search.query.IncidentProcessInstanceStatisticsByErrorQuery;
 import io.camunda.search.query.SearchQueryResult;
 import io.camunda.security.reader.ResourceAccessChecks;
+import io.camunda.zeebe.protocol.record.value.AuthorizationResourceType;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IncidentProcessInstanceStatisticsByErrorDbReader
     extends AbstractEntityReader<IncidentProcessInstanceStatisticsByErrorEntity>
     implements IncidentProcessInstanceStatisticsByErrorReader {
 
-  // TODO: implement aggregation logic for rdbms reader - #42652
+  private static final Logger LOG =
+      LoggerFactory.getLogger(IncidentProcessInstanceStatisticsByErrorDbReader.class);
+
+  private final IncidentMapper incidentMapper;
+
   public IncidentProcessInstanceStatisticsByErrorDbReader(final IncidentMapper incidentMapper) {
     super(IncidentProcessInstanceStatisticsByErrorSearchColumn.values());
+    this.incidentMapper = incidentMapper;
   }
 
   @Override
   public SearchQueryResult<IncidentProcessInstanceStatisticsByErrorEntity> aggregate(
       final IncidentProcessInstanceStatisticsByErrorQuery query,
       final ResourceAccessChecks resourceAccessChecks) {
-    return SearchQueryResult.empty();
+
+    final var dbSort =
+        convertSort(
+            query.sort(),
+            IncidentProcessInstanceStatisticsByErrorSearchColumn.ACTIVE_INSTANCES_WITH_ERROR_COUNT);
+
+    if (shouldReturnEmptyResult(resourceAccessChecks)) {
+      return buildSearchQueryResult(0, List.of(), dbSort);
+    }
+
+    final var authorizedResourceIds =
+        resourceAccessChecks
+            .getAuthorizedResourceIdsByType()
+            .getOrDefault(AuthorizationResourceType.PROCESS_DEFINITION.name(), List.of());
+
+    final var dbQuery =
+        IncidentProcessInstanceStatisticsByErrorDbQuery.of(
+            builder ->
+                builder
+                    .authorizedResourceIds(authorizedResourceIds)
+                    .authorizedTenantIds(resourceAccessChecks.getAuthorizedTenantIds())
+                    .sort(dbSort)
+                    .page(convertPaging(dbSort, query.page())));
+
+    LOG.trace(
+        "[RDBMS DB] Aggregate for incident process instance statistics by error with query {}",
+        dbQuery);
+
+    return executePagedQuery(
+        () -> incidentMapper.processInstanceStatisticsByErrorCount(dbQuery),
+        () -> incidentMapper.processInstanceStatisticsByError(dbQuery),
+        dbQuery.page(),
+        dbSort);
   }
 }
