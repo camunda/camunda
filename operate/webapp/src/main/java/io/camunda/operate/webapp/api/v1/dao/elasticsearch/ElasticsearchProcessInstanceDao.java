3ef328f4af610621443ca84aae2a3b7f023fc780
euro.lew <euro.lew@camunda.com>
2025-12-30 16:50:04 +0000

    feat: ElasticsearchProcessInstanceDao now uses es8 java client

--- operate/webapp/src/main/java/io/camunda/operate/webapp/api/v1/dao/elasticsearch/ElasticsearchProcessInstanceDao.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/api/v1/dao/elasticsearch/ElasticsearchProcessInstanceDao.java
@@ -7,11 +7,12 @@
  */
 package io.camunda.operate.webapp.api.v1.dao.elasticsearch;
 
-import static io.camunda.operate.util.ElasticsearchUtil.joinWithAnd;
 import static io.camunda.webapps.schema.descriptors.template.ListViewTemplate.JOIN_RELATION;
 import static io.camunda.webapps.schema.descriptors.template.ListViewTemplate.PROCESS_INSTANCE_JOIN_RELATION;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchRequest.Builder;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import io.camunda.operate.conditions.ElasticsearchCondition;
 import io.camunda.operate.util.ElasticsearchUtil;
 import io.camunda.operate.webapp.api.v1.dao.ProcessInstanceDao;
@@ -22,14 +23,8 @@ import io.camunda.operate.webapp.api.v1.exceptions.ResourceNotFoundException;
 import io.camunda.operate.webapp.api.v1.exceptions.ServerException;
 import io.camunda.operate.webapp.writer.ProcessInstanceWriter;
 import io.camunda.webapps.schema.descriptors.template.ListViewTemplate;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.search.SearchHit;
-import org.elasticsearch.search.SearchHits;
+import java.util.Objects;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
@@ -44,41 +39,27 @@ public class ElasticsearchProcessInstanceDao extends ElasticsearchDao<ProcessIns
 
   @Autowired private ProcessInstanceWriter processInstanceWriter;
 
-  private List<ProcessInstance> mapSearchHits(final SearchHit[] searchHitArray) {
-    final List<ProcessInstance> processInstances =
-        ElasticsearchUtil.mapSearchHits(searchHitArray, objectMapper, ProcessInstance.class);
+  private ProcessInstance postProcessProcessInstance(final ProcessInstance pi) {
+    pi.setStartDate(dateTimeFormatter.convertGeneralToApiDateTime(pi.getStartDate()));
+    pi.setEndDate(dateTimeFormatter.convertGeneralToApiDateTime(pi.getEndDate()));
 
-    if (processInstances != null) {
-      for (final ProcessInstance pi : processInstances) {
-        pi.setStartDate(dateTimeFormatter.convertGeneralToApiDateTime(pi.getStartDate()));
-        pi.setEndDate(dateTimeFormatter.convertGeneralToApiDateTime(pi.getEndDate()));
-      }
-    }
-
-    return processInstances;
+    return pi;
   }
 
   @Override
   public Results<ProcessInstance> search(final Query<ProcessInstance> query) throws APIException {
     logger.debug("search {}", query);
-    final SearchSourceBuilder searchSourceBuilder =
-        buildQueryOn(query, ProcessInstance.KEY, new SearchSourceBuilder());
+
+    final var searchReqBuilder =
+        buildQueryOn(query, ProcessInstance.KEY, new SearchRequest.Builder(), true);
+
     try {
-      final SearchRequest searchRequest =
-          new SearchRequest().indices(processInstanceIndex.getAlias()).source(searchSourceBuilder);
-      final SearchResponse searchResponse = tenantAwareClient.search(searchRequest);
-      final SearchHits searchHits = searchResponse.getHits();
-      final SearchHit[] searchHitArray = searchHits.getHits();
-      if (searchHitArray != null && searchHitArray.length > 0) {
-        final Object[] sortValues = searchHitArray[searchHitArray.length - 1].getSortValues();
-        final List<ProcessInstance> processInstances = mapSearchHits(searchHitArray);
-        return new Results<ProcessInstance>()
-            .setTotal(searchHits.getTotalHits().value)
-            .setItems(processInstances)
-            .setSortValues(sortValues);
-      } else {
-        return new Results<ProcessInstance>().setTotal(searchHits.getTotalHits().value);
-      }
+      final var searchReq = searchReqBuilder.index(processInstanceIndex.getAlias()).build();
+
+      final var results = searchWithResultsReturn(searchReq, ProcessInstance.class);
+      results.getItems().forEach(this::postProcessProcessInstance);
+
+      return results;
     } catch (final Exception e) {
       throw new ServerException("Error in reading process instances", e);
     }
@@ -89,8 +70,21 @@ public class ElasticsearchProcessInstanceDao extends ElasticsearchDao<ProcessIns
     logger.debug("byKey {}", key);
     final List<ProcessInstance> processInstances;
     try {
+      final var query = ElasticsearchUtil.termsQuery(ListViewTemplate.KEY, key);
+      final var tenantAwareQuery = tenantHelper.makeQueryTenantAware(query);
+
+      final var searchReq =
+          new SearchRequest.Builder()
+              .index(processInstanceIndex.getAlias())
+              .query(tenantAwareQuery);
+
       processInstances =
-          searchFor(new SearchSourceBuilder().query(termQuery(ListViewTemplate.KEY, key)));
+          ElasticsearchUtil.scrollAllStream(es8Client, searchReq, ProcessInstance.class)
+              .flatMap(res -> res.hits().hits().stream())
+              .map(Hit::source)
+              .filter(Objects::nonNull)
+              .map(this::postProcessProcessInstance)
+              .toList();
     } catch (final Exception e) {
       throw new ServerException(
           String.format("Error in reading process instance for key %s", key), e);
@@ -127,50 +121,95 @@ public class ElasticsearchProcessInstanceDao extends ElasticsearchDao<ProcessIns
 
   @Override
   protected void buildFiltering(
-      final Query<ProcessInstance> query, final SearchSourceBuilder searchSourceBuilder) {
-    final ProcessInstance filter = query.getFilter();
-    final List<QueryBuilder> queryBuilders = new ArrayList<>();
-    queryBuilders.add(termQuery(JOIN_RELATION, PROCESS_INSTANCE_JOIN_RELATION));
-    if (filter != null) {
-      queryBuilders.add(buildTermQuery(ProcessInstance.KEY, filter.getKey()));
-      queryBuilders.add(
-          buildTermQuery(ProcessInstance.PROCESS_DEFINITION_KEY, filter.getProcessDefinitionKey()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.PARENT_KEY, filter.getParentKey()));
-      queryBuilders.add(
-          buildTermQuery(
-              ProcessInstance.PARENT_FLOW_NODE_INSTANCE_KEY,
-              filter.getParentFlowNodeInstanceKey()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.VERSION, filter.getProcessVersion()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.VERSION_TAG, filter.getProcessVersionTag()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.BPMN_PROCESS_ID, filter.getBpmnProcessId()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.STATE, filter.getState()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.INCIDENT, filter.getIncident()));
-      queryBuilders.add(buildTermQuery(ProcessInstance.TENANT_ID, filter.getTenantId()));
-      queryBuilders.add(buildMatchDateQuery(ProcessInstance.START_DATE, filter.getStartDate()));
-      queryBuilders.add(buildMatchDateQuery(ProcessInstance.END_DATE, filter.getEndDate()));
-    }
-    searchSourceBuilder.query(joinWithAnd(queryBuilders.toArray(new QueryBuilder[] {})));
-  }
+      final Query<ProcessInstance> query, final SearchSourceBuilder searchSourceBuilder) {}
 
-  protected List<ProcessInstance> searchFor(final SearchSourceBuilder searchSource)
-      throws IOException {
-    final SearchRequest searchRequest =
-        new SearchRequest(processInstanceIndex.getAlias()).source(searchSource);
-    final List<ProcessInstance> processInstances =
-        tenantAwareClient.search(
-            searchRequest,
-            () -> {
-              return ElasticsearchUtil.scroll(
-                  searchRequest, ProcessInstance.class, objectMapper, elasticsearch);
-            });
+  @Override
+  protected void buildFiltering(
+      final Query<ProcessInstance> query,
+      final Builder searchRequestBuilder,
+      final boolean isTenantAware) {
+    final var filter = query.getFilter();
 
-    if (processInstances != null) {
-      for (final ProcessInstance pi : processInstances) {
-        pi.setStartDate(dateTimeFormatter.convertGeneralToApiDateTime(pi.getStartDate()));
-        pi.setEndDate(dateTimeFormatter.convertGeneralToApiDateTime(pi.getEndDate()));
-      }
+    final var joinRelationQ =
+        ElasticsearchUtil.termsQuery(JOIN_RELATION, PROCESS_INSTANCE_JOIN_RELATION);
+
+    if (filter == null) {
+      searchRequestBuilder.query(joinRelationQ);
+      return;
     }
 
-    return processInstances;
+    final var keyQ =
+        buildIfPresent(ProcessInstance.KEY, filter.getKey(), ElasticsearchUtil::termsQuery);
+
+    final var procDefKeyQ =
+        buildIfPresent(
+            ProcessInstance.PROCESS_DEFINITION_KEY,
+            filter.getProcessDefinitionKey(),
+            ElasticsearchUtil::termsQuery);
+
+    final var parentKeyQ =
+        buildIfPresent(
+            ProcessInstance.PARENT_KEY, filter.getParentKey(), ElasticsearchUtil::termsQuery);
+
+    final var parentFlowNodeKeyQ =
+        buildIfPresent(
+            ProcessInstance.PARENT_FLOW_NODE_INSTANCE_KEY,
+            filter.getParentFlowNodeInstanceKey(),
+            ElasticsearchUtil::termsQuery);
+
+    final var versionQ =
+        buildIfPresent(
+            ProcessInstance.VERSION, filter.getProcessVersion(), ElasticsearchUtil::termsQuery);
+
+    final var versionTagQ =
+        buildIfPresent(
+            ProcessInstance.VERSION_TAG,
+            filter.getProcessVersionTag(),
+            ElasticsearchUtil::termsQuery);
+
+    final var bpmnProcessIdQ =
+        buildIfPresent(
+            ProcessInstance.BPMN_PROCESS_ID,
+            filter.getBpmnProcessId(),
+            ElasticsearchUtil::termsQuery);
+
+    final var stateQ =
+        buildIfPresent(ProcessInstance.STATE, filter.getState(), ElasticsearchUtil::termsQuery);
+
+    final var incidentQ =
+        buildIfPresent(
+            ProcessInstance.INCIDENT, filter.getIncident(), ElasticsearchUtil::termsQuery);
+
+    final var tenantIdQ =
+        buildIfPresent(
+            ProcessInstance.TENANT_ID, filter.getTenantId(), ElasticsearchUtil::termsQuery);
+
+    final var startDateQ =
+        buildIfPresent(
+            ProcessInstance.START_DATE, filter.getStartDate(), this::buildMatchDateQueryEs8);
+
+    final var endDateQ =
+        buildIfPresent(ProcessInstance.END_DATE, filter.getEndDate(), this::buildMatchDateQueryEs8);
+
+    final var andOfAllQueries =
+        ElasticsearchUtil.joinWithAnd(
+            joinRelationQ,
+            keyQ,
+            procDefKeyQ,
+            parentKeyQ,
+            parentFlowNodeKeyQ,
+            versionQ,
+            versionTagQ,
+            bpmnProcessIdQ,
+            stateQ,
+            incidentQ,
+            tenantIdQ,
+            startDateQ,
+            endDateQ);
+
+    final var finalQuery =
+        isTenantAware ? tenantHelper.makeQueryTenantAware(andOfAllQueries) : andOfAllQueries;
+
+    searchRequestBuilder.query(finalQuery);
   }
 }
