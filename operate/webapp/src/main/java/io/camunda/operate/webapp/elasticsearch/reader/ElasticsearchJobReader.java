b0d4ce7181b9847445b6e33c27348eabff48dd17
euro.lew <euro.lew@camunda.com>
2026-01-06 00:23:21 +0000

    feat: ElasticsearchJobReader now uses java client

--- operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/ElasticsearchJobReader.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/ElasticsearchJobReader.java
@@ -7,22 +7,19 @@
  */
 package io.camunda.operate.webapp.elasticsearch.reader;
 
-import static org.elasticsearch.index.query.QueryBuilders.constantScoreQuery;
-import static org.elasticsearch.index.query.QueryBuilders.termQuery;
+import static io.camunda.operate.util.ElasticsearchUtil.QueryType.ALL;
+import static io.camunda.operate.util.ElasticsearchUtil.whereToSearch;
 
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
 import io.camunda.operate.conditions.ElasticsearchCondition;
 import io.camunda.operate.exceptions.OperateRuntimeException;
-import io.camunda.operate.tenant.TenantAwareElasticsearchClient;
 import io.camunda.operate.util.ElasticsearchUtil;
 import io.camunda.operate.webapp.reader.JobReader;
 import io.camunda.webapps.schema.descriptors.template.JobTemplate;
 import io.camunda.webapps.schema.entities.JobEntity;
 import java.io.IOException;
 import java.util.Optional;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
 
@@ -31,32 +28,31 @@ import org.springframework.stereotype.Component;
 public class ElasticsearchJobReader extends AbstractReader implements JobReader {
 
   private final JobTemplate jobTemplate;
-  private final TenantAwareElasticsearchClient tenantAwareClient;
 
-  public ElasticsearchJobReader(
-      final JobTemplate jobTemplate, final TenantAwareElasticsearchClient tenantAwareClient) {
+  public ElasticsearchJobReader(final JobTemplate jobTemplate) {
     this.jobTemplate = jobTemplate;
-    this.tenantAwareClient = tenantAwareClient;
   }
 
   @Override
   public Optional<JobEntity> getJobByFlowNodeInstanceId(final String flowNodeInstanceId) {
-    final QueryBuilder query =
-        constantScoreQuery(termQuery(JobTemplate.FLOW_NODE_INSTANCE_ID, flowNodeInstanceId));
-    final SearchRequest request =
-        ElasticsearchUtil.createSearchRequest(jobTemplate)
-            .source(new SearchSourceBuilder().query(query).sort(JobTemplate.JOB_KEY));
+    final var query =
+        ElasticsearchUtil.termsQuery(JobTemplate.FLOW_NODE_INSTANCE_ID, flowNodeInstanceId);
+    final var tenantAwareQuery = tenantHelper.makeQueryTenantAware(query);
+    final var constantScoreQuery = ElasticsearchUtil.constantScoreQuery(tenantAwareQuery);
+
+    final var searchRequest =
+        new SearchRequest.Builder()
+            .index(whereToSearch(jobTemplate, ALL))
+            .query(constantScoreQuery)
+            .sort(s -> s.field(f -> f.field(JobTemplate.JOB_KEY).order(SortOrder.Asc)))
+            .size(1)
+            .build();
+
     try {
-      final SearchResponse response = tenantAwareClient.search(request);
-      final var hits = response.getHits();
-      final var totalHits =
-          (hits != null && hits.getTotalHits() != null) ? hits.getTotalHits().value : 0L;
+      final var response = es8client.search(searchRequest, JobEntity.class);
+      final var totalHits = response.hits().total().value();
       if (totalHits >= 1) {
-        // take the first job found
-        final JobEntity job =
-            ElasticsearchUtil.fromSearchHit(
-                hits.getHits()[0].getSourceAsString(), objectMapper, JobEntity.class);
-        return Optional.of(job);
+        return Optional.of(response.hits().hits().get(0).source());
       } else {
         return Optional.empty();
       }
