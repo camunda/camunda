98e20298a49731ece05acda6398ff51e260bcda3
euro.lew <euro.lew@camunda.com>
2026-01-07 17:28:19 +0000

    fix: stylecheck

--- operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/FlowNodeInstanceReader.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/FlowNodeInstanceReader.java
@@ -41,7 +41,6 @@ import io.camunda.operate.util.ElasticsearchUtil;
 import io.camunda.operate.util.ElasticsearchUtil.QueryType;
 import io.camunda.operate.webapp.data.IncidentDataHolder;
 import io.camunda.operate.webapp.rest.FlowNodeInstanceMetadataBuilder;
-import io.camunda.operate.webapp.rest.dto.FlowNodeStatisticsDto;
 import io.camunda.operate.webapp.rest.dto.activity.FlowNodeInstanceDto;
 import io.camunda.operate.webapp.rest.dto.activity.FlowNodeInstanceQueryDto;
 import io.camunda.operate.webapp.rest.dto.activity.FlowNodeInstanceRequestDto;
