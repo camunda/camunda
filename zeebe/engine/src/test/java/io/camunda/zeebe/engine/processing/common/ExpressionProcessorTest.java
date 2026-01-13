66053c71595c162176183615f8d6d6e3ec4ae6cb
Nico Korthout <nico.korthout@camunda.com>
2026-01-13 17:19:07 +0100

    test: verify error cases of expression processor
    The expression processor offers a lot of methods to evaluate different
    kinds of expressions. For each of these, we want to ensure that it
    handles errors correctly.
    
    We have some unexpected errors: interruption and error thrown by
    expression language.
    
    A timeout should be treated as an expected error -> returned as a
    Failure.
    
    These tests are set up as fast running unit tests of the expression
    processor, that cover all evaluateX methods that we currently support.
    
    (cherry picked from commit 67b13d4d402bd338d2aa31f68646fb605a95fc12)
    
    Conflicts:
    zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/common/ExpressionProcessorTest.java
    
    The expression processor on 8.9 supports more methods and requires
    passing a tenant along, while that's not the case on older versions.
    
    (cherry picked from commit 2cd4e699ff354b5e1fc51cc7096419484e7b1c66)

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/common/ExpressionProcessorTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/common/ExpressionProcessorTest.java
@@ -10,14 +10,22 @@ package io.camunda.zeebe.engine.processing.common;
 import static io.camunda.zeebe.test.util.asserts.EitherAssert.assertThat;
 
 import io.camunda.zeebe.el.EvaluationContext;
+import io.camunda.zeebe.el.EvaluationResult;
+import io.camunda.zeebe.el.Expression;
 import io.camunda.zeebe.el.ExpressionLanguage;
 import io.camunda.zeebe.el.ExpressionLanguageFactory;
 import io.camunda.zeebe.engine.processing.bpmn.clock.ZeebeFeelEngineClock;
 import io.camunda.zeebe.engine.processing.common.ExpressionProcessor.EvaluationContextLookup;
+import io.camunda.zeebe.engine.processing.common.ExpressionProcessor.EvaluationException;
 import io.camunda.zeebe.util.Either;
+import java.time.Duration;
 import java.time.InstantSource;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -221,6 +229,237 @@ class ExpressionProcessorTest {
               Expected result of the expression 'x' to be 'OBJECT', but was 'NULL'. \
               The evaluation reported the following warnings:
               [NO_VARIABLE_FOUND] No variable found with name 'x'""");
+    }
+  }
+
+  @Nested
+  @TestInstance(Lifecycle.PER_CLASS)
+  class EvaluationErrorsTest {
+
+    /**
+     * This test case uses an expression language implementation that blocks until released. It
+     * verifies that the processor returns a Failure (left) when the evaluation times out.
+     */
+    @ParameterizedTest(name = "{1} returns Failure on timeout")
+    @MethodSource("processorEvaluationMethods")
+    void shouldReturnFailureWhenEvaluationTimesOut(
+        final ProcessorMethod evaluationMethod, final String methodName) {
+      // given
+      final var blockingEL = new FakeBlockingExpressionLanguage();
+      final var processor =
+          new ExpressionProcessor(blockingEL, DEFAULT_CONTEXT_LOOKUP, Duration.ofMillis(1));
+      final var expression = EXPRESSION_LANGUAGE.parseExpression("foo");
+
+      try {
+        // when
+        final var result = evaluationMethod.evaluate(processor, expression);
+
+        // then
+        assertThat(result)
+            .as("should return failure for %s", methodName)
+            .isLeft()
+            .extracting(Either::getLeft)
+            .extracting(Failure::getMessage)
+            .isEqualTo("Expected to evaluate expression but timed out after 1 ms: 'foo'");
+      } finally {
+        // release the blocking EL to avoid leaking threads
+        blockingEL.release();
+      }
+    }
+
+    /**
+     * This test case uses an expression language implementation that always throws a specific
+     * exception when evaluating an expression. It verifies that the processor catches this
+     * exception and rethrows it as an EvaluationException with the expected message and cause.
+     */
+    @ParameterizedTest(name = "{1} throws EvaluationException on error")
+    @MethodSource("processorEvaluationMethods")
+    void shouldThrowEvaluationExceptionWhenExceptionThrown(final ProcessorMethod evaluationMethod) {
+      // given
+      final var processor =
+          new ExpressionProcessor(
+              new FakeThrowingExpressionLanguage(new CustomException("custom failure")),
+              DEFAULT_CONTEXT_LOOKUP);
+      final var expression = EXPRESSION_LANGUAGE.parseExpression("foo");
+
+      // when & then
+      Assertions.assertThatThrownBy(() -> evaluationMethod.evaluate(processor, expression))
+          .isInstanceOf(EvaluationException.class)
+          .hasMessage("Expected to evaluate expression 'foo', but an exception was thrown")
+          .hasCauseInstanceOf(CustomException.class)
+          .hasRootCauseMessage("custom failure");
+    }
+
+    /**
+     * This test case uses an expression language implementation that blocks indefinitely when
+     * evaluating an expression. The test starts the evaluation in a separate thread, waits until
+     * the evaluation has started, then interrupts the thread and verifies that the processor
+     * responds to the interruption by throwing an EvaluationException.
+     */
+    @ParameterizedTest(name = "{1} throws EvaluationException on interruption")
+    @MethodSource("processorEvaluationMethods")
+    void shouldThrowEvaluationExceptionWhenInterrupted(
+        final ProcessorMethod evaluationMethod, final String methodName) throws Exception {
+      // given
+      final var blockingEL = new FakeBlockingExpressionLanguage();
+      final var processor = new ExpressionProcessor(blockingEL, DEFAULT_CONTEXT_LOOKUP);
+      final var expression = EXPRESSION_LANGUAGE.parseExpression("foo");
+
+      final var failure = new AtomicReference<Throwable>();
+      final var evaluationThread =
+          new Thread(
+              () -> {
+                try {
+                  evaluationMethod.evaluate(processor, expression);
+                } catch (final Throwable t) {
+                  failure.set(t);
+                }
+              },
+              "expression-processor-interrupt-test");
+
+      // when
+      evaluationThread.start();
+
+      Assertions.assertThat(blockingEL.awaitEntered(5, TimeUnit.SECONDS))
+          .as("evaluation should start and block for %s", methodName)
+          .isTrue();
+
+      evaluationThread.interrupt();
+      blockingEL.release(); // unblock EL so it can notice interrupt/fail fast
+      evaluationThread.join(TimeUnit.SECONDS.toMillis(5));
+
+      // then
+      Assertions.assertThat(evaluationThread.isAlive())
+          .as("evaluation thread should terminate after interruption for %s", methodName)
+          .isFalse();
+
+      Assertions.assertThat(failure.get())
+          .isInstanceOf(EvaluationException.class)
+          .hasMessage("Expected to evaluate expression 'foo', but the evaluation was interrupted")
+          .hasCauseInstanceOf(InterruptedException.class);
+    }
+
+    Stream<Arguments> processorEvaluationMethods() {
+      return Stream.of(
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateStringExpression(expression, -1L),
+              "evaluateStringExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) ->
+                      processor.evaluateStringExpressionAsDirectBuffer(expression, -1L),
+              "evaluateStringExpressionAsDirectBuffer"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateLongExpression(expression, -1L),
+              "evaluateLongExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateBooleanExpression(expression, -1L),
+              "evaluateBooleanExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateIntervalExpression(expression, -1L),
+              "evaluateIntervalExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateDateTimeExpression(expression, -1L),
+              "evaluateDateTimeExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) ->
+                      processor.evaluateDateTimeExpression(expression, -1L, false),
+              "evaluateDateTimeExpression(nullableFlag)"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateAnyExpression(expression, -1L),
+              "evaluateAnyExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateArrayExpression(expression, -1L),
+              "evaluateArrayExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) ->
+                      processor.evaluateArrayOfStringsExpression(expression, -1L),
+              "evaluateArrayOfStringsExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) ->
+                      processor.evaluateMessageCorrelationKeyExpression(expression, -1L),
+              "evaluateMessageCorrelationKeyExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) -> processor.evaluateIntegerExpression(expression, -1L),
+              "evaluateIntegerExpression"),
+          Arguments.of(
+              (ProcessorMethod)
+                  (processor, expression) ->
+                      processor.evaluateVariableMappingExpression(expression, -1L),
+              "evaluateVariableMappingExpression"));
+    }
+
+    private static final class FakeBlockingExpressionLanguage implements ExpressionLanguage {
+      private final CountDownLatch entered = new CountDownLatch(1);
+      private final CountDownLatch release = new CountDownLatch(1);
+
+      @SuppressWarnings("SameParameterValue")
+      boolean awaitEntered(final long timeout, final TimeUnit unit) throws InterruptedException {
+        return entered.await(timeout, unit);
+      }
+
+      void release() {
+        release.countDown();
+      }
+
+      @Override
+      public Expression parseExpression(final String expression) {
+        // not used in these tests
+        return null;
+      }
+
+      @Override
+      public EvaluationResult evaluateExpression(
+          final Expression expression, final EvaluationContext context) {
+        entered.countDown();
+        try {
+          // deterministically block until the test releases; if interrupted while waiting,
+          // propagate
+          release.await();
+          return null; // not relevant for this test
+        } catch (final InterruptedException e) {
+          Thread.currentThread().interrupt();
+          throw new RuntimeException(e);
+        }
+      }
+    }
+
+    private static class CustomException extends RuntimeException {
+      public CustomException(final String message) {
+        super(message);
+      }
+    }
+
+    private record FakeThrowingExpressionLanguage(RuntimeException throwsException)
+        implements ExpressionLanguage {
+
+      @Override
+      public Expression parseExpression(final String expression) {
+        // not used in these tests
+        return null;
+      }
+
+      @Override
+      public EvaluationResult evaluateExpression(
+          final Expression expression, final EvaluationContext context) {
+        throw throwsException;
+      }
+    }
+
+    @FunctionalInterface
+    private interface ProcessorMethod {
+      Either<Failure, ?> evaluate(ExpressionProcessor processor, Expression expression);
     }
   }
 }
