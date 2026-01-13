c6fd2995e76e49b3ba35d0f34011815c32cb6a23
euro.lew <euro.lew@camunda.com>
2026-01-13 10:44:41 +0000

    refactor: formatting

--- operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/ElasticsearchJobReader.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/ElasticsearchJobReader.java
@@ -12,6 +12,7 @@ import static io.camunda.operate.util.ElasticsearchUtil.whereToSearch;
 
 import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.search.Hit;
 import io.camunda.operate.conditions.ElasticsearchCondition;
 import io.camunda.operate.exceptions.OperateRuntimeException;
 import io.camunda.operate.util.ElasticsearchUtil;
@@ -50,12 +51,7 @@ public class ElasticsearchJobReader extends AbstractReader implements JobReader 
 
     try {
       final var response = es8client.search(searchRequest, JobEntity.class);
-      final var totalHits = response.hits().total().value();
-      if (totalHits >= 1) {
-        return Optional.of(response.hits().hits().get(0).source());
-      } else {
-        return Optional.empty();
-      }
+      return response.hits().hits().stream().findFirst().map(Hit::source);
     } catch (final IOException e) {
       throw new OperateRuntimeException("Error reading job from Elasticsearch", e);
     }
