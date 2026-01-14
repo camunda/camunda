22c5b8a57b15b5d34485924908f369e1793dda6d
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-01-14 12:42:59 -0300

    feat: fetch task audit log data

--- tasklist/client/src/v2/TaskDetailsHistoryView/index.tsx
+++ tasklist/client/src/v2/TaskDetailsHistoryView/index.tsx
@@ -6,12 +6,75 @@
  * except in compliance with the Camunda License 1.0.
  */
 
+import {
+  type LoaderFunctionArgs,
+  useRevalidator,
+  useRouteError,
+} from 'react-router-dom';
+import {useTaskDetailsParams} from 'common/routing';
+import {reactQueryClient} from 'common/react-query/reactQueryClient';
+import {getUserTaskAuditLogsQueryOptions} from 'v2/api/useUserTaskAuditLogs.query';
+import {useSuspenseInfiniteQuery} from '@tanstack/react-query';
+import {SomethingWentWrong} from 'common/error-handling/SomethingWentWrong';
+import {Forbidden} from 'common/error-handling/Forbidden';
+import {useTranslation} from 'react-i18next';
+import {logger} from 'common/utils/logger';
+import {requestErrorSchema} from 'common/api/request';
+
+const HTTP_STATUS_FORBIDDEN = 403;
+
+async function loader({params}: LoaderFunctionArgs) {
+  const userTaskKey = params.id;
+  if (!userTaskKey) {
+    throw new Error('User task key is required');
+  }
+
+  await reactQueryClient.ensureInfiniteQueryData(
+    getUserTaskAuditLogsQueryOptions(userTaskKey),
+  );
+
+  return null;
+}
+
 const TaskDetailsHistoryView: React.FC = () => {
+  const {id} = useTaskDetailsParams();
+  const {data} = useSuspenseInfiniteQuery(getUserTaskAuditLogsQueryOptions(id));
+
   return (
     <div data-testid="task-details-history-view">
-      {/* Empty content - to be implemented with #40532 */}
+      <code>{JSON.stringify(data, null, 2)}</code>
     </div>
   );
 };
 
-export {TaskDetailsHistoryView as Component};
+const ErrorBoundary: React.FC = () => {
+  const error = useRouteError();
+  const {t} = useTranslation();
+  const revalidator = useRevalidator();
+
+  logger.error(error);
+
+  const {data: parsedError, success} = requestErrorSchema.safeParse(error);
+  if (success && parsedError.variant === 'failed-response') {
+    if (parsedError.response.status === HTTP_STATUS_FORBIDDEN) {
+      return (
+        <Forbidden
+          titleKey="taskDetailsHistoryForbiddenTitle"
+          descKey="taskDetailsHistoryForbiddenDesc"
+          linkLabelKey="taskDetailsHistoryForbiddenLinkLabel"
+        />
+      );
+    }
+  }
+
+  return (
+    <SomethingWentWrong
+      title={t('taskDetailsHistoryErrorTitle')}
+      message={t('taskDetailsHistoryErrorMessage')}
+      onRetryClick={revalidator.revalidate}
+    />
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export {TaskDetailsHistoryView as Component, loader, ErrorBoundary};
