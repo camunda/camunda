861268042b5b136df04d1e27ecb60594686a5bd7
Mathias Vandaele <mathias.vandaele@camunda.com>
2026-01-14 14:58:06 +0100

    feat: changes in configuration

--- zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/JobMetricsCfg.java
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/engine/JobMetricsCfg.java
@@ -13,6 +13,10 @@ import java.time.Duration;
 
 public class JobMetricsCfg implements ConfigurationEntry {
   private Duration exportInterval = EngineConfiguration.DEFAULT_JOB_METRICS_EXPORT_INTERVAL;
+  private int maxWorkerNameLength = EngineConfiguration.DEFAULT_MAX_WORKER_NAME_LENGTH;
+  private int maxJobTypeLength = EngineConfiguration.DEFAULT_MAX_JOB_TYPE_LENGTH;
+  private int maxTenantIdLength = EngineConfiguration.DEFAULT_MAX_TENANT_ID_LENGTH;
+  private int maxUniqueKeys = EngineConfiguration.DEFAULT_MAX_UNIQUE_JOB_METRICS_KEYS;
 
   public Duration getExportInterval() {
     return exportInterval;
@@ -25,5 +29,37 @@ public class JobMetricsCfg implements ConfigurationEntry {
   @Override
   public String toString() {
     return "JobMetricsCfg{" + "exportInterval=" + exportInterval + '}';
+  }
+
+  public int getMaxWorkerNameLength() {
+    return maxWorkerNameLength;
+  }
+
+  public void setMaxWorkerNameLength(final int maxWorkerNameLength) {
+    this.maxWorkerNameLength = maxWorkerNameLength;
+  }
+
+  public int getMaxJobTypeLength() {
+    return maxJobTypeLength;
+  }
+
+  public void setMaxJobTypeLength(final int maxJobTypeLength) {
+    this.maxJobTypeLength = maxJobTypeLength;
+  }
+
+  public int getMaxTenantIdLength() {
+    return maxTenantIdLength;
+  }
+
+  public void setMaxTenantIdLength(final int maxTenantIdLength) {
+    this.maxTenantIdLength = maxTenantIdLength;
+  }
+
+  public int getMaxUniqueKeys() {
+    return maxUniqueKeys;
+  }
+
+  public void setMaxUniqueKeys(final int maxUniqueKeys) {
+    this.maxUniqueKeys = maxUniqueKeys;
   }
 }
