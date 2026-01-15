6c195cb67300c1810ea92751524606c8a48e816a
rodrigolourencolopes <rodrigo.lopes@camunda.com>
2026-01-15 19:42:16 +0100

    feat: make auto strategy based of fixed percentage of ram.

--- zeebe/broker/src/main/java/io/camunda/zeebe/broker/partitioning/RocksDbSharedCache.java
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/partitioning/RocksDbSharedCache.java
@@ -10,15 +10,14 @@ package io.camunda.zeebe.broker.partitioning;
 import com.sun.management.OperatingSystemMXBean;
 import io.camunda.zeebe.broker.system.configuration.BrokerCfg;
 import io.camunda.zeebe.broker.system.configuration.RocksdbCfg;
+import io.camunda.zeebe.db.impl.rocksdb.RocksDbConfiguration.MemoryAllocationStrategy;
 import io.camunda.zeebe.db.impl.rocksdb.ZeebeRocksDbFactory.SharedRocksDbResources;
 import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RocksDbSharedCache {
   public static final long MINIMUM_PARTITION_MEMORY_LIMIT = 32 * 1024 * 1024L;
-  private static final double ROCKSDB_OVERHEAD_FACTOR = 0.15;
 
   private static final Logger LOGGER = LoggerFactory.getLogger(RocksDbSharedCache.class);
 
@@ -40,50 +39,16 @@ public class RocksDbSharedCache {
 
     return switch (rocksdbCfg.getMemoryAllocationStrategy()) {
       case BROKER -> rocksdbCfg.getMemoryLimit().toBytes();
-      case AUTO -> getAvailableMemoryCapacity();
+      case AUTO -> getFixedMemoryPercentage(rocksdbCfg.getMemoryFraction());
       // in case of PARTITION or null, we allocate per partition
       default -> rocksdbCfg.getMemoryLimit().toBytes() * partitionsCount;
     };
   }
 
-  static long getAvailableMemoryCapacity() {
+  static long getFixedMemoryPercentage(final double memoryFraction) {
     final long totalMemorySize =
         ((OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalMemorySize();
-    final MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
-
-    // This is the largest size the JVM Heap is allowed to grow to. -XX:MaxRAMPercentage.
-    long maxHeapBytes = memoryBean.getHeapMemoryUsage().getMax();
-    // Get Maximum Non-Heap Size, -XX:MaxMetaspaceSize
-    long maxNonHeapBytes = memoryBean.getNonHeapMemoryUsage().getMax();
-    // both return -1 if not defined.
-
-    // if not set, assume default to 25% of total memory
-    if (maxNonHeapBytes < 0) {
-      maxNonHeapBytes = Math.round(0.25 * totalMemorySize);
-      LOGGER.info(
-          "Warning: Non-Heap limit is not set. Using an assumption of 25% of "
-              + "total RAM :{} Bytes for safety, for RocksDB memory calculation."
-              + "Consider setting -XX:MaxMetaspaceSize explicitly.",
-          maxNonHeapBytes);
-    }
-    // -XX:MaxRAMPercentage is almost always set, but in case it is removed.
-    if (maxHeapBytes < 0) {
-      maxHeapBytes = Math.round(0.25 * totalMemorySize);
-      LOGGER.info(
-          "Warning: Heap limit is not set. Using an assumption of 25% of "
-              + "total RAM :{} Bytes for safety, for RocksDB memory calculation."
-              + "Consider setting -XX:MaxRAMPercentage explicitly.",
-          maxHeapBytes);
-    }
-
-    // Estimate Total JVM Memory Usage (Heap + Non-Heap)
-    final long maxJvmInternalBytes = maxHeapBytes + maxNonHeapBytes;
-
-    final long availableOffHeapBudget = totalMemorySize - maxJvmInternalBytes;
-
-    // We set the cache capacity to a percentage of the remaining budget and
-    // reserve space for native overheads.
-    return (long) (availableOffHeapBudget * (1.0 - ROCKSDB_OVERHEAD_FACTOR));
+    return (long) (totalMemorySize * memoryFraction);
   }
 
   public static void validateRocksDbMemory(final RocksdbCfg rocksdbCfg, final int partitionsCount) {
@@ -94,6 +59,15 @@ public class RocksDbSharedCache {
           String.format(
               "Expected the allocated memory for RocksDB per partition to be at least %s bytes, but was %s bytes.",
               MINIMUM_PARTITION_MEMORY_LIMIT, blockCacheBytes / partitionsCount));
+    }
+
+    // makes sure that memoryFraction is between [0 and 1] when using AUTO strategy
+    if (rocksdbCfg.getMemoryAllocationStrategy() == MemoryAllocationStrategy.AUTO
+        && (rocksdbCfg.getMemoryFraction() <= 0.0 || rocksdbCfg.getMemoryFraction() >= 1.0)) {
+      throw new IllegalArgumentException(
+          String.format(
+              "Expected the memoryFraction for RocksDB AUTO memory allocation strategy to be between 0 and 1, but was %s.",
+              rocksdbCfg.getMemoryFraction()));
     }
   }
 }
