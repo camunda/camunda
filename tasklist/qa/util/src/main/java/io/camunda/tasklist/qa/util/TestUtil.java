ff63e3acd77a95da1a747643496f651c601f2d75
euro.lew <euro.lew@camunda.com>
2026-01-19 08:27:05 +0000

    feat: remove rhlc from ElasticsearchUtil

--- tasklist/qa/util/src/main/java/io/camunda/tasklist/qa/util/TestUtil.java
+++ tasklist/qa/util/src/main/java/io/camunda/tasklist/qa/util/TestUtil.java
@@ -7,17 +7,15 @@
  */
 package io.camunda.tasklist.qa.util;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.DeleteIndexRequest;
+import co.elastic.clients.elasticsearch.indices.DeleteIndexTemplateRequest;
+import co.elastic.clients.elasticsearch.indices.GetIndexRequest;
+import co.elastic.clients.elasticsearch.indices.GetIndexTemplateRequest;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.elasticsearch.ElasticsearchStatusException;
-import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
-import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.DeleteComposableIndexTemplateRequest;
-import org.elasticsearch.client.indices.GetComposableIndexTemplateRequest;
-import org.elasticsearch.client.indices.GetIndexRequest;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,26 +58,27 @@ public final class TestUtil {
     }
   }
 
-  public static void removeAllIndices(RestHighLevelClient esClient, String prefix) {
+  public static void removeAllIndices(ElasticsearchClient esClient, String prefix) {
     try {
       LOGGER.info("Removing indices");
-      final var indexResponses =
-          esClient.indices().get(new GetIndexRequest(prefix + "*"), RequestOptions.DEFAULT);
-      for (String index : indexResponses.getIndices()) {
-        esClient.indices().delete(new DeleteIndexRequest(index), RequestOptions.DEFAULT);
+      final var indexResponse =
+          esClient.indices().get(GetIndexRequest.of(r -> r.index(prefix + "*")));
+
+      for (String index : indexResponse.result().keySet()) {
+        esClient.indices().delete(DeleteIndexRequest.of(r -> r.index(index)));
       }
-      final var templateResponses =
+
+      final var templateResponse =
           esClient
               .indices()
-              .getIndexTemplate(
-                  new GetComposableIndexTemplateRequest(prefix + "*"), RequestOptions.DEFAULT);
-      for (String template : templateResponses.getIndexTemplates().keySet()) {
+              .getIndexTemplate(GetIndexTemplateRequest.of(r -> r.name(prefix + "*")));
+
+      for (var template : templateResponse.indexTemplates()) {
         esClient
             .indices()
-            .deleteIndexTemplate(
-                new DeleteComposableIndexTemplateRequest(template), RequestOptions.DEFAULT);
+            .deleteIndexTemplate(DeleteIndexTemplateRequest.of(r -> r.name(template.name())));
       }
-    } catch (ElasticsearchStatusException | IOException ex) {
+    } catch (IOException ex) {
       LOGGER.error(ex.getMessage(), ex);
     }
   }
