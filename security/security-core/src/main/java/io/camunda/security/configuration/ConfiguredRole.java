4566519337a7f5fa9ffaca3bc46d0c0b4b63cb7a
Lukas Taake <lukas.taake@holisticon.de>
2026-01-19 10:09:07 +0100

    feat: add configuration object for preconfigured roles

---
+++ security/security-core/src/main/java/io/camunda/security/configuration/ConfiguredRole.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.security.configuration;
+
+import java.util.List;
+
+public record ConfiguredRole(
+    String roleId,
+    String name,
+    String description,
+    List<String> users,
+    List<String> clients,
+    List<String> mappingRules,
+    List<String> groups) {}
