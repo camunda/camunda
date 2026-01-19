3e7088da2362038d342a246630981c98d39c2657
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-01-19 16:44:49 -0300

    feat: render fetched data

--- identity/client/src/pages/operations-log/List.tsx
+++ identity/client/src/pages/operations-log/List.tsx
@@ -9,9 +9,36 @@
 import { FC } from "react";
 import useTranslate from "src/utility/localization";
 import Page, { PageHeader } from "src/components/layout/Page";
+import EntityList from "src/components/entityList";
+import { TranslatedErrorInlineNotification } from "src/components/notifications/InlineNotification";
+import { useApi } from "src/utility/api";
+import { searchAuditLogs } from "src/utility/api/audit-logs";
 
 const List: FC = () => {
   const { t } = useTranslate("operationsLog");
+  const { t: tComponents } = useTranslate();
+  const {
+    data: auditLogs,
+    loading,
+    success,
+    reload,
+  } = useApi(searchAuditLogs, {
+    sort: [
+      {
+        field: "timestamp",
+        order: "desc",
+      },
+    ],
+    filter: {
+      category: {
+        $eq: "ADMIN",
+      },
+    },
+    page: {
+      from: 0,
+      limit: 50,
+    },
+  });
 
   return (
     <Page>
@@ -20,6 +47,32 @@ const List: FC = () => {
         linkText={t("operationsLog").toLowerCase()}
         docsLinkPath="/docs/components/concepts/audit-log/"
       />
+      <EntityList
+        data={
+          auditLogs?.items.map((log) => ({
+            id: log.auditLogKey,
+            operationType: log.operationType,
+            entityType: log.entityType,
+            result: log.result,
+            actorId: log.actorId,
+            timestamp: new Date(log.timestamp).toLocaleString(),
+          })) || []
+        }
+        headers={[
+          { header: t("operation"), key: "operationType" },
+          { header: t("entity"), key: "entityType" },
+          { header: t("status"), key: "result" },
+          { header: t("actor"), key: "actorId" },
+          { header: t("time"), key: "timestamp" },
+        ]}
+        loading={loading}
+      />
+      {!loading && !success && (
+        <TranslatedErrorInlineNotification
+          title={t("operationsLogCouldNotLoad")}
+          actionButton={{ label: tComponents("retry"), onClick: reload }}
+        />
+      )}
     </Page>
   );
 };
