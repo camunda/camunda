0a184d523dc23ffcff9d80fb258017e2d3fdc849
Mattia Buccarella <mattia.buccarella@camunda.com>
2026-01-20 16:41:49 +0100

    feat: add launch modes

---
+++ run-all-in-one.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# clear terminal buffer
+clear && printf '\e[3J'
+
+CAMUNDA_SECURITY_INITIALIZATION_USERS_0_NAME=Demo \
+CAMUNDA_SECURITY_INITIALIZATION_USERS_0_USERNAME=demo \
+CAMUNDA_SECURITY_INITIALIZATION_USERS_0_EMAIL=demo@example.com \
+CAMUNDA_SECURITY_INITIALIZATION_USERS_0_PASSWORD=demo \
+CAMUNDA_SECURITY_INITIALIZATION_DEFAULTROLES_ADMIN_USERS_0_=demo \
+mvn -f dist/pom.xml exec:java -Dexec.mainClass="io.camunda.application.StandaloneCamunda" \
+-Dcamunda.mode=all-in-one \
+-Dcamunda.development=true
