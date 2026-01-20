7fa05301b10eca101e6f5d538ff03787ba097050
megglos <sebastian.bathke@camunda.com>
2026-01-20 15:47:22 +0100

    fix: read getPatternsCondition as we use ant patterns

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/CamundaRequestMappingHandlerMapping.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/CamundaRequestMappingHandlerMapping.java
@@ -9,9 +9,11 @@ package io.camunda.zeebe.gateway.rest.config;
 
 import io.camunda.zeebe.gateway.rest.controller.CamundaRestController;
 import java.lang.reflect.Method;
+import java.util.stream.Stream;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.util.pattern.PathPattern;
 
 public class CamundaRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
 
@@ -34,19 +36,43 @@ public class CamundaRequestMappingHandlerMapping extends RequestMappingHandlerMa
     final String basePath = controllerAnnotation.path();
     final String enginePath = ENGINE_PREFIX + basePath;
 
-    // Build dual-path mapping
-    final var patternsCondition = mapping.getPathPatternsCondition();
+    // Check for AntPathMatcher patterns (getPatternsCondition)
+    final var patternsCondition = mapping.getPatternsCondition();
     if (patternsCondition != null) {
       final var patterns = patternsCondition.getPatterns();
       final var dualPatterns =
           patterns.stream()
-              .flatMap(
-                  pattern -> {
-                    final String patternValue = pattern.getPatternString();
-                    // Replace base path with both variants
-                    final String withEngine = patternValue.replaceFirst("^" + basePath, enginePath);
-                    return java.util.stream.Stream.of(patternValue, withEngine);
-                  })
+              .flatMap(methodPath -> Stream.of(basePath + methodPath, enginePath + methodPath))
+              .distinct()
+              .toArray(String[]::new);
+
+      return RequestMappingInfo.paths(dualPatterns)
+          .methods(
+              mapping
+                  .getMethodsCondition()
+                  .getMethods()
+                  .toArray(new org.springframework.web.bind.annotation.RequestMethod[0]))
+          .params(mapping.getParamsCondition().getExpressions().toArray(new String[0]))
+          .headers(mapping.getHeadersCondition().getExpressions().toArray(new String[0]))
+          .consumes(
+              mapping.getConsumesCondition().getConsumableMediaTypes().stream()
+                  .map(Object::toString)
+                  .toArray(String[]::new))
+          .produces(
+              mapping.getProducesCondition().getProducibleMediaTypes().stream()
+                  .map(Object::toString)
+                  .toArray(String[]::new))
+          .build();
+    }
+
+    // Fallback: check PathPatternParser patterns
+    final var pathPatternsCondition = mapping.getPathPatternsCondition();
+    if (pathPatternsCondition != null) {
+      final var patterns = pathPatternsCondition.getPatterns();
+      final var dualPatterns =
+          patterns.stream()
+              .map(PathPattern::getPatternString)
+              .flatMap(methodPath -> Stream.of(basePath + methodPath, enginePath + methodPath))
               .distinct()
               .toArray(String[]::new);
 
