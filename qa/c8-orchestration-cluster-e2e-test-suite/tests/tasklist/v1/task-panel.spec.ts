3896bd93e226ef7fa981be12077b423a168f7b90
Catherina Prokudina <catherina.prokudina@camunda.com>
2026-01-20 20:57:12 +0100

    test: wip

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/tasklist/v1/task-panel.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/tasklist/v1/task-panel.spec.ts
@@ -159,7 +159,7 @@ test.describe('task panel page', () => {
     await taskPanelPageV1.scrollToFirstTask('usertask_for_scrolling_2');
 
     await expect(page.getByText('usertask_for_scrolling_1')).toHaveCount(isAdditionalTaskVisible ? 0 : 1);
-    await expect(page.getByText('usertask_for_scrolling_2')).toHaveCount(amountOfVisibleTasks);
+    await expect(page.getByText('usertask_for_scrolling_2')).toHaveCount(isAdditionalTaskVisible? amountOfVisibleTasks+1 : amountOfVisibleTasks);
     await expect(page.getByText('usertask_for_scrolling_3')).toHaveCount(0);
   });
 });
