175d2caa21f2268ba17f834e5b430ddec7230798
Catherina Prokudina <catherina.prokudina@camunda.com>
2026-01-21 17:15:43 +0100

    test: get auth tests
    (cherry picked from commit 2b41366eb1b382111f351884230c8480e2ebfbec)

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/authorization/get-authorization-api.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/authorization/get-authorization-api.spec.ts
@@ -16,6 +16,7 @@ import {
   assertNotFoundRequest,
   encode,
   assertStatusCode,
+  assertForbiddenRequest,
 } from '../../../../utils/http';
 import {defaultAssertionOptions} from '../../../../utils/constants';
 import {cleanupUsers} from '../../../../utils/usersCleanup';
@@ -28,6 +29,7 @@ import {
   expectAuthorizationCanBeFound,
   verifyAuthorizationFields,
   type Authorization,
+  grantUserResourceAuthorization,
 } from '@requestHelpers';
 import {CREATE_CUSTOM_AUTHORIZATION_BODY} from '../../../../utils/beans/requestBeans';
 import {waitForAssertion} from 'utils/waitForAssertion';
@@ -81,18 +83,162 @@ test.describe.parallel('Get Authorization API', () => {
         authorizationKey: userAuthorizationKey,
     };
 
-    await test.step('Get Authorization - verify authorization can be found', async () => {
+    await test.step('Get Authorization and assert results', async () => {
         await expect(async () => {
-            const statusRes = await request.get(
+            const res = await request.get(
               buildUrl(`/authorizations/${userAuthorizationKey}`),
               {
                 headers: jsonHeaders(),
               },
             );
-            await assertStatusCode(statusRes, 200);
-            const authBody = await statusRes.json();
+            await assertStatusCode(res, 200);
+            const authBody = await res.json();
             verifyAuthorizationFields(authBody, expectedUserAuthorization);
         }).toPass(defaultAssertionOptions);
+    });
+  });
+
+  test('Get existing Authorization - not found', async ({request}) => {
+    const nonExistentAuthorizationKey = '9999999999999999';
+    await test.step('Get Authorization and assert results', async () => {
+        await expect(async () => {
+            const res = await request.get(
+              buildUrl(`/authorizations/${nonExistentAuthorizationKey}`),
+              {
+                headers: jsonHeaders(),
+              },
+            );
+            await assertNotFoundRequest(
+              res,
+              `Authorization with key '${nonExistentAuthorizationKey}' not found`,
+            );
+        }).toPass(defaultAssertionOptions);
+    });
+  });
+
+  test('Get existing Authorization - unauthorized', async ({request}) => {
+    let user: {
+      username: string;
+      name: string;
+      email: string;
+      password: string;
+    };
+    let userAuthorizationKey: string = '';
+    let originalUserAuthorization: Authorization = {} as Authorization;
+    await test.step('Setup - Create user for authorization tests', async () => {
+      user = await createUser(request);
+      cleanups.push(async (request) => {
+        await cleanupUsers(request, [user.username]);
+      });
+    });
+
+    await test.step('Setup - Grant user necessary authorizations', async () => {
+      const authorizationBody = CREATE_CUSTOM_AUTHORIZATION_BODY(
+        user.username,
+        'USER',
+        '*',
+        'ROLE',
+        ['READ'],
+      );
+      userAuthorizationKey = await createComponentAuthorization(
+        request,
+        authorizationBody,
+      );
+      originalUserAuthorization = authorizationBody;
+    });
+
+    const expectedUserAuthorization = {
+      ...originalUserAuthorization,
+        authorizationKey: userAuthorizationKey,
+    };
+
+    await test.step('Get Authorization without authorization header', async () => {
+        await expect(async () => {
+            const res = await request.get(
+              buildUrl(`/authorizations/${userAuthorizationKey}`),
+              {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+              },
+            );
+            await assertUnauthorizedRequest(res);
+        }).toPass(defaultAssertionOptions);
+    });
+  });
+
+  test('Get existing Authorization - forbidden', async ({request}) => {
+    let userWithResourcesAuthorizationToSendRequest: {
+    username: string;
+    name: string;
+    email: string;
+    password: string;
+  } = {} as {
+    username: string;
+    name: string;
+    email: string;
+    password: string;
+};
+    let user: {
+      username: string;
+      name: string;
+      email: string;
+      password: string;
+    };
+    let userAuthorizationKey: string = '';
+    let originalUserAuthorization: Authorization = {} as Authorization;
+
+    await test.step('Setup - Create test user with Resource Authorization and user for granting Authorization', async () => {
+        userWithResourcesAuthorizationToSendRequest = await createUser(request);
+        await grantUserResourceAuthorization(
+            request,
+            userWithResourcesAuthorizationToSendRequest,
+        );
+        cleanups.push(async (request) => {
+          await cleanupUsers(request, [
+            userWithResourcesAuthorizationToSendRequest.username,
+          ]);
+        });
+   
+        user = await createUser(request);
+        cleanups.push(async (request) => {
+            await cleanupUsers(request, [user.username]);
+        });
+    });
+
+    await test.step('Setup - Grant user necessary authorizations', async () => {
+      const authorizationBody = CREATE_CUSTOM_AUTHORIZATION_BODY(
+        user.username,
+        'USER',
+        '*',
+        'ROLE',
+        ['READ'],
+      );
+      userAuthorizationKey = await createComponentAuthorization(
+        request,
+        authorizationBody,
+      );
+      originalUserAuthorization = authorizationBody;
+    });
+
+    await test.step('Get Authorization and assert results', async () => {
+        const token = encode(
+        `${userWithResourcesAuthorizationToSendRequest.username}:${userWithResourcesAuthorizationToSendRequest.password}`,
+      );
+      
+    await expect(async () => {
+        const authRes = await request.get(
+            buildUrl(`/authorizations/${userAuthorizationKey}`),
+            {
+            headers: jsonHeaders(token), // overrides default demo:demo
+            data: {},
+            },
+        );
+        await assertForbiddenRequest(
+            authRes,
+            "Unauthorized to perform operation 'READ' on resource 'AUTHORIZATION'",
+        );
+    }).toPass(defaultAssertionOptions);
     });
   });
 });
