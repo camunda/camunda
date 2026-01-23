2b0f47290d7ab8880edc5f0ceab39ac593281373
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2026-01-23 14:25:14 +0100

    feat: expose MCP call details on URI
    Adds the MCP call specifics to the `uri` tag of the default `http.server.request` metrics.
    This allows tracking which MCP tools and capabilities have been called, including their timings and details.

---
+++ dist/src/main/java/io/camunda/application/commons/metrics/ServerRequestObservationConfiguration.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.application.commons.metrics;
+
+import io.camunda.gateway.mcp.ConditionalOnMcpGatewayEnabled;
+import io.micrometer.common.KeyValues;
+import io.micrometer.observation.ObservationRegistry;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.function.Function;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.DefaultServerRequestObservationConvention;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.http.server.observation.ServerRequestObservationConvention;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.ServerHttpObservationFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnMcpGatewayEnabled
+public class ServerRequestObservationConfiguration {
+
+  @Bean
+  public ServerHttpObservationFilter serverHttpObservationFilter(
+      final ObservationRegistry observationRegistry,
+      final ServerRequestObservationConvention customRequestObservationConvention) {
+    // return the common request observation filter but wrap the request in a
+    // ContentCachingRequestWrapper to be able to read the body multiple times
+    return new ServerHttpObservationFilter(
+        observationRegistry, customRequestObservationConvention) {
+      @Override
+      protected void doFilterInternal(
+          final @NonNull HttpServletRequest request,
+          final @NonNull HttpServletResponse response,
+          final @NonNull FilterChain filterChain)
+          throws ServletException, IOException {
+        super.doFilterInternal(new ContentCachingRequestWrapper(request), response, filterChain);
+      }
+    };
+  }
+
+  @Bean
+  public ServerRequestObservationConvention customRequestObservationConvention(
+      final LowCardinalityKeyValuesMapper lowCardinalityKeyValuesMapper) {
+    // extend the default convention to add custom low cardinality key values
+    return new DefaultServerRequestObservationConvention() {
+      @Override
+      public @NonNull KeyValues getLowCardinalityKeyValues(
+          final @NonNull ServerRequestObservationContext context) {
+        final var lowCardinalityKeyValues = super.getLowCardinalityKeyValues(context);
+        final var newLowCardinalityKeyValues = lowCardinalityKeyValuesMapper.apply(context);
+        return lowCardinalityKeyValues.and(newLowCardinalityKeyValues);
+      }
+    };
+  }
+
+  public interface LowCardinalityKeyValuesMapper
+      extends Function<ServerRequestObservationContext, KeyValues> {}
+}
