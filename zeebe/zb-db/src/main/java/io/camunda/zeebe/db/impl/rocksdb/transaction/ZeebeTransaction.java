8c6e5558353575694bf2ad45bfb6259b0d7cc287
Fabio Paini <fabio.paini@camunda.com>
2026-01-29 10:09:25 +0100

    fix: clear ZeebeTransaction cache when transaction is stopped

--- zeebe/zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/transaction/ZeebeTransaction.java
+++ zeebe/zb-db/src/main/java/io/camunda/zeebe/db/impl/rocksdb/transaction/ZeebeTransaction.java
@@ -9,16 +9,12 @@ package io.camunda.zeebe.db.impl.rocksdb.transaction;
 
 import static io.camunda.zeebe.db.impl.rocksdb.transaction.RocksDbInternal.isRocksDbExceptionRecoverable;
 
-import com.google.common.primitives.Bytes;
 import io.camunda.zeebe.db.TransactionOperation;
 import io.camunda.zeebe.db.ZeebeDbException;
 import io.camunda.zeebe.db.ZeebeDbTransaction;
-
-import java.util.Arrays;
+import io.camunda.zeebe.util.buffer.BufferUtil;
 import java.util.HashMap;
 import java.util.Map;
-
-import io.camunda.zeebe.util.buffer.BufferUtil;
 import org.agrona.LangUtil;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.rocksdb.ColumnFamilyHandle;
@@ -58,9 +54,12 @@ public class ZeebeTransaction implements ZeebeDbTransaction, AutoCloseable {
       final int valueLength)
       throws Exception {
     try {
-        final CFKEy cfkEy = new CFKEy(columnFamilyHandle, new UnsafeBuffer(key, keyOffset, keyLength).hashCode());
-        cache.put(cfkEy, BufferUtil.cloneBuffer(new UnsafeBuffer(value, valueOffset, valueLength)).byteArray());
-        RocksDbInternal.putWithHandle.invokeExact(
+      final CFKEy cfkEy =
+          new CFKEy(columnFamilyHandle, new UnsafeBuffer(key, keyOffset, keyLength).hashCode());
+      cache.put(
+          cfkEy,
+          BufferUtil.cloneBuffer(new UnsafeBuffer(value, valueOffset, valueLength)).byteArray());
+      RocksDbInternal.putWithHandle.invokeExact(
           nativeHandle,
           key,
           keyOffset,
@@ -104,19 +103,26 @@ public class ZeebeTransaction implements ZeebeDbTransaction, AutoCloseable {
 
     final CFKEy cfkEy = new CFKEy(columnFamilyHandle, key, keyOffset, keyLength);
 
-    final byte[] cached = cache.computeIfAbsent(
-        cfkEy,
-        cfk -> {
-          try {
-            final byte[] value = (byte[])
-                RocksDbInternal.getWithHandle.invokeExact(
-                    nativeHandle, readOptionsHandle, key, keyOffset, keyLength, columnFamilyHandle);
-            return value != null ? value : NO_VALUE;
-          } catch (final Throwable e) {
-            LangUtil.rethrowUnchecked(e);
-            return null; // unreachable
-          }
-        });
+    final byte[] cached =
+        cache.computeIfAbsent(
+            cfkEy,
+            cfk -> {
+              try {
+                final byte[] value =
+                    (byte[])
+                        RocksDbInternal.getWithHandle.invokeExact(
+                            nativeHandle,
+                            readOptionsHandle,
+                            key,
+                            keyOffset,
+                            keyLength,
+                            columnFamilyHandle);
+                return value != null ? value : NO_VALUE;
+              } catch (final Throwable e) {
+                LangUtil.rethrowUnchecked(e);
+                return null; // unreachable
+              }
+            });
     return cached != NO_VALUE ? cached : null;
   }
 
@@ -187,24 +193,24 @@ public class ZeebeTransaction implements ZeebeDbTransaction, AutoCloseable {
   void commitInternal() throws RocksDBException {
     inCurrentTransaction = false;
     transaction.commit();
+    cache.clear();
   }
 
   void rollbackInternal() throws RocksDBException {
     inCurrentTransaction = false;
     transaction.rollback();
+    cache.clear();
   }
 
   @Override
   public void close() {
     transaction.close();
+    cache.clear();
   }
 
   record CFKEy(long columnFamilyHandle, long hash) {
     public CFKEy(
-        final long columnFamilyHandle,
-        final byte[] key,
-        final int keyOffset,
-        final int keyLengt) {
+        final long columnFamilyHandle, final byte[] key, final int keyOffset, final int keyLengt) {
       this(columnFamilyHandle, new UnsafeBuffer(key, keyOffset, keyLengt).hashCode());
     }
   }
