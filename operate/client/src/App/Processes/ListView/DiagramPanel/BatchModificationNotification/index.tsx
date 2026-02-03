295a50a650175593525b0560a6ef2796521b9920
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-02-03 16:40:34 +0100

    refactor: undo unnecessary import change

--- operate/client/src/App/Processes/ListView/DiagramPanel/BatchModificationNotification/index.tsx
+++ operate/client/src/App/Processes/ListView/DiagramPanel/BatchModificationNotification/index.tsx
@@ -10,7 +10,7 @@ import {observer} from 'mobx-react';
 import {useInstancesCount} from 'modules/queries/processInstancesStatistics/useInstancesCount';
 import pluralSuffix from 'modules/utils/pluralSuffix';
 import {Container, InlineNotification, Button} from './styled';
-import {useProcessDefinitionKeyContext} from '../../processDefinitionKeyContext';
+import {useProcessDefinitionKeyContext} from 'App/Processes/ListView/processDefinitionKeyContext';
 import {useListViewXml} from 'modules/queries/processDefinitions/useListViewXml';
 import {getFlowNodeName} from 'modules/utils/flowNodes';
 
