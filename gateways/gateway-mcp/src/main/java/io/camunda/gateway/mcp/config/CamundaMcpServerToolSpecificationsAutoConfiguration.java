be51a20bfc348ea2756d4d5e8e64c016f5d94222
Mathias Geat <mathias@ailoo.net>
2026-02-05 16:42:41 +0100

    feat: define our own annotation-based tool specifications
    Preparation to use a custom JSON schema creator

---
+++ gateways/gateway-mcp/src/main/java/io/camunda/gateway/mcp/config/CamundaMcpServerToolSpecificationsAutoConfiguration.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.gateway.mcp.config;
+
+import io.camunda.gateway.mcp.ConditionalOnMcpGatewayEnabled;
+import io.modelcontextprotocol.server.McpStatelessServerFeatures;
+import io.modelcontextprotocol.server.McpStatelessServerFeatures.SyncToolSpecification;
+import java.util.List;
+import org.springaicommunity.mcp.annotation.McpTool;
+import org.springframework.ai.mcp.annotation.spring.SyncMcpAnnotationProviders;
+import org.springframework.ai.mcp.server.common.autoconfigure.McpServerStatelessAutoConfiguration.EnabledStatelessServerCondition;
+import org.springframework.ai.mcp.server.common.autoconfigure.StatelessToolCallbackConverterAutoConfiguration.ToolCallbackConverterCondition;
+import org.springframework.ai.mcp.server.common.autoconfigure.annotations.McpServerAnnotationScannerAutoConfiguration;
+import org.springframework.ai.mcp.server.common.autoconfigure.annotations.McpServerAnnotationScannerAutoConfiguration.ServerMcpAnnotatedBeans;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+
+@AutoConfiguration(after = McpServerAnnotationScannerAutoConfiguration.class)
+@Conditional({EnabledStatelessServerCondition.class, ToolCallbackConverterCondition.class})
+@ConditionalOnMcpGatewayEnabled
+public class CamundaMcpServerToolSpecificationsAutoConfiguration {
+
+  @Bean
+  public List<SyncToolSpecification> mcpGatewayToolSpecifications(
+      ServerMcpAnnotatedBeans beansWithMcpMethodAnnotations) {
+    List<Object> beansByAnnotation =
+        beansWithMcpMethodAnnotations.getBeansByAnnotation(McpTool.class);
+    List<McpStatelessServerFeatures.SyncToolSpecification> syncToolSpecifications =
+        SyncMcpAnnotationProviders.statelessToolSpecifications(beansByAnnotation);
+    return syncToolSpecifications;
+  }
+}
