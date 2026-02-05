95b06808b3be4fab09145af83012dc918f4813a9
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-02-05 22:09:26 +0100

    BACKPORT-CONFLICT

--- tasklist/client/src/common/tasks/variables-editor/createVariableFieldName.tsx
+++ tasklist/client/src/common/tasks/variables-editor/createVariableFieldName.tsx
@@ -9,8 +9,12 @@
 import {VARIABLE_NAME_DOT_ESCAPE_CHAR} from './constants';
 
 const createVariableFieldName = (name: string) => {
+<<<<<<< HEAD:tasklist/client/src/common/tasks/variables-editor/createVariableFieldName.tsx
   const escapedName = name.replaceAll('.', VARIABLE_NAME_DOT_ESCAPE_CHAR);
   return `#${escapedName}`;
+=======
+  return `#${encodeURIComponent(name).replace(/\./g, '%2E')}`;
+>>>>>>> c857b356 (fix: variable form when variables have dots in the name):tasklist/client/src/Tasks/Task/Variables/createVariableFieldName.tsx
 };
 
 const createNewVariableFieldName = (prefix: string, suffix: string) => {
