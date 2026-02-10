e0202abe520fbfb3820faf6a101cbd263cdbe5e8
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2026-02-10 14:24:52 +0100

    test: unit test disabling user info endpoint
    (cherry picked from commit 8c500d8e04bbeb1048117dd7b12d9bb615803f5f)

--- authentication/src/test/java/io/camunda/authentication/config/OidcClientRegistrationTest.java
+++ authentication/src/test/java/io/camunda/authentication/config/OidcClientRegistrationTest.java
@@ -10,6 +10,7 @@ package io.camunda.authentication.config;
 import io.camunda.security.configuration.OidcAuthenticationConfiguration;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 
 class OidcClientRegistrationTest {
@@ -40,5 +41,61 @@ class OidcClientRegistrationTest {
             () -> ClientRegistrationFactory.createClientRegistration("foo", config))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessageContaining("unsupported client authentication method: does_not_exist");
+  }
+
+  @Test
+  void shouldEnableUserInfoEndpoint() {
+    // given
+    final var config = new OidcAuthenticationConfiguration();
+    final var builder =
+        ClientRegistration.withRegistrationId("foo").userInfoUri("http://localhost:8080");
+    config.setClientId("clientId");
+    config.setRedirectUri("redirectUri");
+    config.setAuthorizationUri("authorizationUri");
+    config.setTokenUri("tokenUri");
+    config.setClientAuthenticationMethod("client_secret_basic");
+    config.setUserInfoEnabled(true);
+
+    try (final var mockedRegistration = Mockito.mockStatic(ClientRegistration.class)) {
+      mockedRegistration
+          .when(() -> ClientRegistration.withRegistrationId(Mockito.anyString()))
+          .thenReturn(builder);
+
+      // when
+      final var clientRegistration =
+          ClientRegistrationFactory.createClientRegistration("foo", config);
+
+      // then
+      Assertions.assertThat(clientRegistration.getProviderDetails().getUserInfoEndpoint().getUri())
+          .isEqualTo("http://localhost:8080");
+    }
+  }
+
+  @Test
+  void shouldDisableUserInfoEndpoint() {
+    // given
+    final var config = new OidcAuthenticationConfiguration();
+    final var builder =
+        ClientRegistration.withRegistrationId("foo").userInfoUri("http://localhost:8080");
+    config.setClientId("clientId");
+    config.setRedirectUri("redirectUri");
+    config.setAuthorizationUri("authorizationUri");
+    config.setTokenUri("tokenUri");
+    config.setClientAuthenticationMethod("client_secret_basic");
+    config.setUserInfoEnabled(false);
+
+    try (final var mockedRegistration = Mockito.mockStatic(ClientRegistration.class)) {
+      mockedRegistration
+          .when(() -> ClientRegistration.withRegistrationId(Mockito.anyString()))
+          .thenReturn(builder);
+
+      // when
+      final var clientRegistration =
+          ClientRegistrationFactory.createClientRegistration("foo", config);
+
+      // then
+      Assertions.assertThat(clientRegistration.getProviderDetails().getUserInfoEndpoint().getUri())
+          .isNull();
+    }
   }
 }
