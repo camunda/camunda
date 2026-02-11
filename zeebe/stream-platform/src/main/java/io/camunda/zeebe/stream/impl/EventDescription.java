9ddad6606d1c3e7c15a41edbe5b55517dcf0efdd
Carlo Sana <carlo.sana@camunda.com>
2026-02-11 16:07:11 +0100

    perf: use a ReentrantLock as it's simpler and no contention is expected
    ReentrantLock is cheaper as it's just a atomic operation. Contention is
    almost non-existent on the happy path
    
    (cherry picked from commit 83963d4f24ffdb9ffe66be9d157450da0dcaa186)

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/EventDescription.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/EventDescription.java
@@ -9,44 +9,44 @@ package io.camunda.zeebe.stream.impl;
 
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.Intent;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class EventDescription {
   private long position;
   private Intent intent;
   private ValueType valueType;
   private String status;
-  private final ReadWriteLock rwLock = new ReentrantReadWriteLock();
+  private final Lock rwLock = new ReentrantLock();
 
   public EventDescription(final String status) {
     status(status);
   }
 
   public void status(final String status) {
-    rwLock.writeLock().lock();
+    rwLock.lock();
     // this cannot fail, try/finally is not needed
     this.status = status;
     position = 0;
     intent = null;
     valueType = null;
-    rwLock.writeLock().unlock();
+    rwLock.unlock();
   }
 
   public void set(
       final String status, final long position, final Intent intent, final ValueType valueType) {
     // this cannot fail, try/finally is not needed
-    rwLock.writeLock().lock();
+    rwLock.lock();
     this.status = status;
     this.position = position;
     this.intent = intent;
     this.valueType = valueType;
-    rwLock.writeLock().unlock();
+    rwLock.unlock();
   }
 
   @Override
   public String toString() {
-    rwLock.readLock().lock();
+    rwLock.lock();
     try {
       if (position == 0) {
         return status;
@@ -55,7 +55,7 @@ public class EventDescription {
             "%s @ position=%d, intent=%s, valueType=%s", status, position, intent, valueType);
       }
     } finally {
-      rwLock.readLock().unlock();
+      rwLock.unlock();
     }
   }
 }
