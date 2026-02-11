d69def89a92b236642cc38171cd2d35244a7589a
Josh Windels <joshua.windels@camunda.com>
2026-02-11 10:41:29 +0100

    fix: use jupiter assertion rather than junit4

--- tasklist/importer/src/test/java/io/camunda/tasklist/zeebeimport/ImportListenerElasticsearchTest.java
+++ tasklist/importer/src/test/java/io/camunda/tasklist/zeebeimport/ImportListenerElasticsearchTest.java
@@ -7,10 +7,10 @@
  */
 package io.camunda.tasklist.zeebeimport;
 
-import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
