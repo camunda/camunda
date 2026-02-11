66ef4789f99bf5ebed2d9cf590cd4af9e949038c
Carlo Sana <carlo.sana@camunda.com>
2026-02-11 17:06:23 +0100

    feat: acquire lock in a try/finally with a timeout
    The writers acquire the lock with a timeout of 1 milli, the readers with
    a 100 millis timeout.
    
    (cherry picked from commit 07efa786931d5bf377fd6944653b19018f2be303)

--- zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/EventDescription.java
+++ zeebe/stream-platform/src/main/java/io/camunda/zeebe/stream/impl/EventDescription.java
@@ -9,53 +9,74 @@ package io.camunda.zeebe.stream.impl;
 
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.protocol.record.intent.Intent;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class EventDescription {
+  private static final int WRITE_ACQUIRE_LOCK_TIMEOUT = 1;
+  private static final int READ_ACQUIRE_LOCK_TIMEOUT = 100;
+  private static final String FAILED_TO_ACQUIRE_LOCK = "failed to acquire lock";
   private long position;
   private Intent intent;
   private ValueType valueType;
   private String status;
-  private final Lock rwLock = new ReentrantLock();
+  private final Lock lock = new ReentrantLock();
 
   public EventDescription(final String status) {
     status(status);
   }
 
   public void status(final String status) {
-    rwLock.lock();
-    // this cannot fail, try/finally is not needed
-    this.status = status;
-    position = 0;
-    intent = null;
-    valueType = null;
-    rwLock.unlock();
+    try {
+      if (lock.tryLock(WRITE_ACQUIRE_LOCK_TIMEOUT, TimeUnit.MILLISECONDS)) {
+        // this cannot fail, try/finally is not needed
+        this.status = status;
+        position = 0;
+        intent = null;
+        valueType = null;
+      }
+    } catch (final InterruptedException e) {
+      // ignore it, it's just for reporting
+    } finally {
+      lock.unlock();
+    }
   }
 
   public void set(
       final String status, final long position, final Intent intent, final ValueType valueType) {
-    // this cannot fail, try/finally is not needed
-    rwLock.lock();
-    this.status = status;
-    this.position = position;
-    this.intent = intent;
-    this.valueType = valueType;
-    rwLock.unlock();
+    try {
+      if (lock.tryLock(WRITE_ACQUIRE_LOCK_TIMEOUT, TimeUnit.MILLISECONDS)) {
+        this.status = status;
+        this.position = position;
+        this.intent = intent;
+        this.valueType = valueType;
+      }
+    } catch (final InterruptedException e) {
+      // ignore it, it's just for reporting
+    } finally {
+      lock.unlock();
+    }
   }
 
   @Override
   public String toString() {
-    rwLock.lock();
     try {
-      if (position == 0) {
-        return status;
+      if (lock.tryLock(READ_ACQUIRE_LOCK_TIMEOUT, TimeUnit.MILLISECONDS)) {
+        if (position == 0) {
+          return status;
+        } else {
+          return String.format(
+              "%s @ position=%d, intent=%s, valueType=%s", status, position, intent, valueType);
+        }
       } else {
-        return String.format(
-            "%s @ position=%d, intent=%s, valueType=%s", status, position, intent, valueType);
+        return FAILED_TO_ACQUIRE_LOCK;
       }
+    } catch (final InterruptedException e) {
+      // ignore it, it's just for reporting
+      return FAILED_TO_ACQUIRE_LOCK;
     } finally {
-      rwLock.unlock();
+      lock.unlock();
     }
   }
 }
