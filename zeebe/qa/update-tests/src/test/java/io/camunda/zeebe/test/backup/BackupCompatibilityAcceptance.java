a050402e089ba5b14cb8103d3c31ec80b9e826a5
Lena Schönburg <lena@fairburg.xyz>
2026-02-12 14:54:32 +0100

    refactor: move backup compatibility tests to update-tests module
    The backup compatibility tests belong in the update-tests module since
    they test cross-version compatibility (restoring backups from 8.8.0 on
    the current version). This also adds the necessary dependencies to the
    update-tests pom.xml.

--- zeebe/qa/update-tests/src/test/java/io/camunda/zeebe/test/backup/BackupCompatibilityAcceptance.java
+++ zeebe/qa/update-tests/src/test/java/io/camunda/zeebe/test/backup/BackupCompatibilityAcceptance.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.it.cluster.backup;
+package io.camunda.zeebe.test.backup;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
--- zeebe/qa/update-tests/src/test/java/io/camunda/zeebe/test/backup/GcsBackupCompatibilityIT.java
+++ zeebe/qa/update-tests/src/test/java/io/camunda/zeebe/test/backup/GcsBackupCompatibilityIT.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.zeebe.it.cluster.backup;
+package io.camunda.zeebe.test.backup;
 
 import com.google.cloud.storage.BucketInfo;
 import io.camunda.configuration.Camunda;
