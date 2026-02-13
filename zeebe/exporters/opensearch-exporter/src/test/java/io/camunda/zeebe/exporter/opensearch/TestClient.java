4cd56a64db3e94c940c7b28054ecfaf31c3048f0
Yohan Fernando <yohan.fernando@camunda.com>
2026-02-13 11:25:46 +0000

    refactor: Switch Put ISM Policy to Java Client
    Switch ISM PUT and Delete to the java client and then build the
    Policy using the opensearch builder.

--- zeebe/exporters/opensearch-exporter/src/test/java/io/camunda/zeebe/exporter/opensearch/TestClient.java
+++ zeebe/exporters/opensearch-exporter/src/test/java/io/camunda/zeebe/exporter/opensearch/TestClient.java
@@ -18,14 +18,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.zeebe.exporter.opensearch.TestClient.ComponentTemplatesDto.ComponentTemplateWrapper;
 import io.camunda.zeebe.exporter.opensearch.TestClient.IndexTemplatesDto.IndexTemplateWrapper;
 import io.camunda.zeebe.exporter.opensearch.dto.GetIndexStateManagementPolicyResponse;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy.IsmTemplate;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy.State;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy.State.Action;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy.State.DeleteAction;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy.State.Transition;
-import io.camunda.zeebe.exporter.opensearch.dto.PutIndexStateManagementPolicyRequest.Policy.State.Transition.Conditions;
 import io.camunda.zeebe.exporter.opensearch.dto.Template;
 import io.camunda.zeebe.protocol.jackson.ZeebeProtocolModule;
 import io.camunda.zeebe.protocol.record.Record;
@@ -35,7 +27,6 @@ import io.camunda.zeebe.util.VersionUtil;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URISyntaxException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -44,9 +35,17 @@ import org.apache.hc.core5.http.HttpHost;
 import org.opensearch.client.Request;
 import org.opensearch.client.ResponseException;
 import org.opensearch.client.RestClient;
+import org.opensearch.client.json.JsonData;
 import org.opensearch.client.json.jackson.JacksonJsonpMapper;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch.core.GetResponse;
+import org.opensearch.client.opensearch.ism.Action;
+import org.opensearch.client.opensearch.ism.ActionDelete;
+import org.opensearch.client.opensearch.ism.IsmTemplate;
+import org.opensearch.client.opensearch.ism.Policy;
+import org.opensearch.client.opensearch.ism.PutPolicyRequest;
+import org.opensearch.client.opensearch.ism.States;
+import org.opensearch.client.opensearch.ism.Transition;
 import org.opensearch.client.transport.OpenSearchTransport;
 import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBuilder;
 
@@ -135,14 +134,18 @@ final class TestClient implements CloseableSilently {
 
   void putIndexStateManagementPolicy(final String minimumAge) {
     try {
-      final var request =
-          new Request("PUT", "/_plugins/_ism/policies/" + config.retention.getPolicyName());
-      decoratePolicyVersionSeq(request);
-      final var requestEntity = createPutIndexManagementPolicyRequest(minimumAge);
-      request.setJsonEntity(MAPPER.writeValueAsString(requestEntity));
-      restClient.performRequest(request);
+      final PutPolicyRequest request =
+          PutPolicyRequest.of(
+              req -> {
+                req.policyId(config.retention.getPolicyName());
+                req.policy(getIsmPolicy(minimumAge));
+                decoratePolicyVersionSeq(req);
+                return req;
+              });
+
+      osClient.ism().putPolicy(request);
     } catch (final IOException e) {
-      throw new UncheckedIOException(e);
+      throw new OpensearchExporterException("Failed to put index state management policy", e);
     }
   }
 
@@ -164,38 +167,6 @@ final class TestClient implements CloseableSilently {
   @Override
   public void close() {
     CloseHelper.quietCloseAll(osClient._transport());
-  }
-
-  private void decoratePolicyVersionSeq(final Request request) {
-    try {
-      final GetIndexStateManagementPolicyResponse policy = getIndexStateManagementPolicy();
-      request.addParameter("if_seq_no", String.valueOf(policy.seqNo()));
-      request.addParameter("if_primary_term", String.valueOf(policy.primaryTerm()));
-    } catch (final Exception e) {
-      if (!(e.getCause() instanceof final ResponseException responseException
-          && responseException.getResponse().getStatusLine().getStatusCode() == 404)) {
-        throw e;
-      }
-    }
-  }
-
-  private PutIndexStateManagementPolicyRequest createPutIndexManagementPolicyRequest(
-      final String minimumAge) {
-    final var initialState =
-        new State(
-            ISM_INITIAL_STATE,
-            Collections.emptyList(),
-            List.of(new Transition(ISM_DELETE_STATE, new Conditions(minimumAge))));
-    final var deleteState =
-        new State(
-            ISM_DELETE_STATE, List.of(new Action(new DeleteAction())), Collections.emptyList());
-    final var policy =
-        new Policy(
-            config.retention.getPolicyDescription(),
-            ISM_INITIAL_STATE,
-            List.of(initialState, deleteState),
-            new IsmTemplate(List.of(config.index.prefix + "*"), 1));
-    return new PutIndexStateManagementPolicyRequest(policy);
   }
 
   public Optional<IndexISMPolicyDto> explainIndex(final String index) {
@@ -256,15 +227,6 @@ final class TestClient implements CloseableSilently {
     }
   }
 
-  void deleteIndicesByPattern(final String pattern) {
-    try {
-      final var request = new Request("DELETE", pattern + "?expand_wildcards=all");
-      restClient.performRequest(request);
-    } catch (final IOException e) {
-      throw new UncheckedIOException(e);
-    }
-  }
-
   private OpenSearchTransport createTransport(final OpensearchExporterConfiguration config) {
     try {
       return ApacheHttpClient5TransportBuilder.builder(HttpHost.create(config.url))
@@ -273,6 +235,47 @@ final class TestClient implements CloseableSilently {
     } catch (final URISyntaxException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private void decoratePolicyVersionSeq(final PutPolicyRequest.Builder builder) {
+    try {
+      final GetIndexStateManagementPolicyResponse policy = getIndexStateManagementPolicy();
+      builder.ifSeqNo(policy.seqNo().longValue());
+      builder.ifPrimaryTerm(policy.primaryTerm());
+    } catch (final Exception e) {
+      if (!(e.getCause() instanceof final ResponseException responseException
+          && responseException.getResponse().getStatusLine().getStatusCode() == 404)) {
+        throw e;
+      }
+    }
+  }
+
+  private Policy getIsmPolicy(final String minimumAge) {
+    final States initialState =
+        States.builder()
+            .name(ISM_INITIAL_STATE)
+            .transitions(
+                Transition.builder()
+                    .stateName(ISM_DELETE_STATE)
+                    .conditions("min_index_age", JsonData.of(minimumAge))
+                    .build())
+            .build();
+    final States deleteState =
+        States.builder()
+            .name(ISM_DELETE_STATE)
+            .actions(Action.builder().delete(ActionDelete._INSTANCE).build())
+            .build();
+
+    final IsmTemplate ismTemplate =
+        IsmTemplate.builder().indexPatterns(config.index.prefix + "*").priority(1).build();
+
+    return Policy.builder()
+        .policyId(config.retention.getPolicyName())
+        .description(config.retention.getPolicyDescription())
+        .defaultState(ISM_INITIAL_STATE)
+        .states(List.of(initialState, deleteState))
+        .ismTemplate(ismTemplate)
+        .build();
   }
 
   record IndexTemplatesDto(@JsonProperty("index_templates") List<IndexTemplateWrapper> wrappers) {
