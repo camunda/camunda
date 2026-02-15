06e2f9eac796333027de61fece860f4dc6b245d5
Yohan Fernando <yohan.fernando@camunda.com>
2026-02-15 16:17:59 +0000

    feat: Change Get ISM Policy to Java Client
    Change the GET ISM Policy to Java client. There is a bug in the
    OpenSearch ISM client where the ISM policy lastUpdatedTime is set as
    Integer in the DTO rather than a Long. As a result when getting the
    policy, it throws a number overflow exception.
    
    In order to fix it, we use the ISM client's transport, and make the
    same call that it make internally to the ISM endpoint, then map the
    response to a custom DTO using a custom serializer.

--- zeebe/exporters/opensearch-exporter/src/test/java/io/camunda/zeebe/exporter/opensearch/TestClient.java
+++ zeebe/exporters/opensearch-exporter/src/test/java/io/camunda/zeebe/exporter/opensearch/TestClient.java
@@ -17,6 +17,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.zeebe.exporter.opensearch.TestClient.ComponentTemplatesDto.ComponentTemplateWrapper;
 import io.camunda.zeebe.exporter.opensearch.TestClient.IndexTemplatesDto.IndexTemplateWrapper;
+// import io.camunda.zeebe.exporter.opensearch.dto.GetIndexStateManagementPolicyResponseOLD;
 import io.camunda.zeebe.exporter.opensearch.dto.GetIndexStateManagementPolicyResponse;
 import io.camunda.zeebe.exporter.opensearch.dto.Template;
 import io.camunda.zeebe.protocol.jackson.ZeebeProtocolModule;
@@ -24,6 +25,7 @@ import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.ValueType;
 import io.camunda.zeebe.util.CloseableSilently;
 import io.camunda.zeebe.util.VersionUtil;
+import io.micrometer.core.instrument.Metrics;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URISyntaxException;
@@ -56,7 +58,7 @@ import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBui
 final class TestClient implements CloseableSilently {
   private static final ObjectMapper MAPPER =
       new ObjectMapper().registerModule(new ZeebeProtocolModule());
-
+  final OpensearchClient opensearchClient;
   private final OpensearchExporterConfiguration config;
   private final RestClient restClient;
   private final OpenSearchClient osClient;
@@ -74,6 +76,8 @@ final class TestClient implements CloseableSilently {
     this.indexRouter = indexRouter;
     this.restClient = restClient;
     osClient = new OpenSearchClient(createTransport(config));
+
+    opensearchClient = new OpensearchClient(config, Metrics.globalRegistry, osClient, restClient);
   }
 
   @SuppressWarnings("rawtypes")
@@ -120,16 +124,8 @@ final class TestClient implements CloseableSilently {
     }
   }
 
-  GetIndexStateManagementPolicyResponse getIndexStateManagementPolicy() {
-    try {
-      final var request =
-          new Request("GET", "/_plugins/_ism/policies/" + config.retention.getPolicyName());
-      final var response = restClient.performRequest(request);
-      return MAPPER.readValue(
-          response.getEntity().getContent(), GetIndexStateManagementPolicyResponse.class);
-    } catch (final IOException e) {
-      throw new UncheckedIOException(e);
-    }
+  Optional<GetIndexStateManagementPolicyResponse> getIndexStateManagementPolicy() {
+    return opensearchClient.getIndexStateManagementPolicy();
   }
 
   void putIndexStateManagementPolicy(final String minimumAge) {
@@ -143,7 +139,7 @@ final class TestClient implements CloseableSilently {
                 return req;
               });
 
-      osClient.ism().putPolicy(request);
+      opensearchClient.putIndexStateManagementPolicy(request);
     } catch (final IOException e) {
       throw new OpensearchExporterException("Failed to put index state management policy", e);
     }
@@ -239,7 +235,12 @@ final class TestClient implements CloseableSilently {
 
   private void decoratePolicyVersionSeq(final PutPolicyRequest.Builder builder) {
     try {
-      final GetIndexStateManagementPolicyResponse policy = getIndexStateManagementPolicy();
+      final Optional<GetIndexStateManagementPolicyResponse> maybePolicy =
+          getIndexStateManagementPolicy();
+      if (maybePolicy.isEmpty()) {
+        return;
+      }
+      final GetIndexStateManagementPolicyResponse policy = maybePolicy.get();
       builder.ifSeqNo(policy.seqNo().longValue());
       builder.ifPrimaryTerm(policy.primaryTerm());
     } catch (final Exception e) {
