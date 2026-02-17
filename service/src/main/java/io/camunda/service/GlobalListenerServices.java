b47ad00c7e70db5fde082031324ef30be66e2061
Fabio Paini <fabio.paini@camunda.com>
2026-02-18 00:51:06 +0100

    feat: implement global listener search API

--- service/src/main/java/io/camunda/service/GlobalListenerServices.java
+++ service/src/main/java/io/camunda/service/GlobalListenerServices.java
@@ -7,9 +7,16 @@
  */
 package io.camunda.service;
 
+import static io.camunda.service.authorization.Authorizations.GLOBAL_TASK_LISTENER_READ_AUTHORIZATION;
+
 import io.camunda.search.clients.GlobalListenerSearchClient;
+import io.camunda.search.entities.GlobalListenerEntity;
+import io.camunda.search.entities.GlobalListenerType;
+import io.camunda.search.query.GlobalListenerQuery;
+import io.camunda.search.query.SearchQueryResult;
 import io.camunda.security.auth.BrokerRequestAuthorizationConverter;
 import io.camunda.security.auth.CamundaAuthentication;
+import io.camunda.service.search.core.SearchQueryService;
 import io.camunda.service.security.SecurityContextProvider;
 import io.camunda.zeebe.broker.client.api.BrokerClient;
 import io.camunda.zeebe.gateway.impl.broker.request.BrokerCreateGlobalListenerRequest;
@@ -18,7 +25,8 @@ import io.camunda.zeebe.gateway.impl.broker.request.BrokerUpdateGlobalListenerRe
 import io.camunda.zeebe.protocol.impl.record.value.globallistener.GlobalListenerRecord;
 import java.util.concurrent.CompletableFuture;
 
-public final class GlobalListenerServices extends ApiServices<GlobalListenerServices> {
+public final class GlobalListenerServices
+    extends SearchQueryService<GlobalListenerServices, GlobalListenerQuery, GlobalListenerEntity> {
 
   private final GlobalListenerSearchClient globalListenerSearchClient;
 
@@ -54,6 +62,17 @@ public final class GlobalListenerServices extends ApiServices<GlobalListenerServ
     return sendBrokerRequest(new BrokerCreateGlobalListenerRequest(request));
   }
 
+  public GlobalListenerEntity getGlobalTaskListener(final GlobalListenerRecord request) {
+    return executeSearchRequest(
+        () ->
+            globalListenerSearchClient
+                .withSecurityContext(
+                    securityContextProvider.provideSecurityContext(
+                        authentication, GLOBAL_TASK_LISTENER_READ_AUTHORIZATION))
+                .getGlobalListener(
+                    request.getId(), GlobalListenerType.valueOf(request.getListenerType().name())));
+  }
+
   public CompletableFuture<GlobalListenerRecord> updateGlobalListener(
       final GlobalListenerRecord request) {
     return sendBrokerRequest(new BrokerUpdateGlobalListenerRequest(request));
@@ -62,5 +81,16 @@ public final class GlobalListenerServices extends ApiServices<GlobalListenerServ
   public CompletableFuture<GlobalListenerRecord> deleteGlobalListener(
       final GlobalListenerRecord request) {
     return sendBrokerRequest(new BrokerDeleteGlobalListenerRequest(request));
+  }
+
+  @Override
+  public SearchQueryResult<GlobalListenerEntity> search(final GlobalListenerQuery query) {
+    return executeSearchRequest(
+        () ->
+            globalListenerSearchClient
+                .withSecurityContext(
+                    securityContextProvider.provideSecurityContext(
+                        authentication, GLOBAL_TASK_LISTENER_READ_AUTHORIZATION))
+                .searchGlobalListeners(query));
   }
 }
