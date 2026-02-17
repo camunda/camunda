7c8def8b97bdbc55a6e0ad3e2b6008b062517c76
Fabio Paini <fabio.paini@camunda.com>
2026-02-17 10:45:29 +0100

    feat: add backup configuration for global listener exports

--- dist/src/main/java/io/camunda/application/commons/backup/BackupPriorityConfiguration.java
+++ dist/src/main/java/io/camunda/application/commons/backup/BackupPriorityConfiguration.java
@@ -24,6 +24,7 @@ import io.camunda.webapps.schema.descriptors.index.ClusterVariableIndex;
 import io.camunda.webapps.schema.descriptors.index.DecisionIndex;
 import io.camunda.webapps.schema.descriptors.index.DecisionRequirementsIndex;
 import io.camunda.webapps.schema.descriptors.index.FormIndex;
+import io.camunda.webapps.schema.descriptors.index.GlobalListenerIndex;
 import io.camunda.webapps.schema.descriptors.index.GroupIndex;
 import io.camunda.webapps.schema.descriptors.index.HistoryDeletionIndex;
 import io.camunda.webapps.schema.descriptors.index.MappingRuleIndex;
@@ -147,7 +148,8 @@ public class BackupPriorityConfiguration {
             new AuditLogTemplate(indexPrefix, isElasticsearch),
             // CAMUNDA
             new ClusterVariableIndex(indexPrefix, isElasticsearch),
-            new JobMetricsBatchTemplate(indexPrefix, isElasticsearch));
+            new JobMetricsBatchTemplate(indexPrefix, isElasticsearch),
+            new GlobalListenerIndex(indexPrefix, isElasticsearch));
 
     LOG.debug("Prio1 are {}", prio1);
     LOG.debug("Prio2 are {}", prio2);
