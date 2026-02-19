3dfb1c8ae8b28f7c3ef68222b64aeed5707e83e1
Jonathan Roques <jonathan.roques@camunda.com>
2026-02-19 16:40:32 +0100

    test: add JobTypeStatistics IT tests

--- clients/java/src/test/java/io/camunda/client/util/RestGatewayService.java
+++ clients/java/src/test/java/io/camunda/client/util/RestGatewayService.java
@@ -486,7 +486,7 @@ public class RestGatewayService {
         response);
   }
 
-  public void onJobStatisticsRequest(
+  public void onJobTypeStatisticsRequest(
       final io.camunda.client.protocol.rest.JobTypeStatisticsQueryResult response) {
     register(
         WireMock.post(WireMock.urlPathEqualTo(RestGatewayPaths.getJobTypeStatisticsUrl())),
