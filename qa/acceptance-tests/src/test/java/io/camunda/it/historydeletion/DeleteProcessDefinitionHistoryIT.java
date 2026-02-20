9137450a0d9a181a01962c64f5c90e19916fae32
remco.westerhoud <remcowesterhoud@msn.com>
2026-02-20 11:42:56 +0100

    refactor: rename class to indicate we verify history deletion
    (cherry picked from commit 37eaa7fac8e7ea9efb6938079ed3984d51167b28)

--- qa/acceptance-tests/src/test/java/io/camunda/it/historydeletion/DeleteProcessDefinitionHistoryIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/historydeletion/DeleteProcessDefinitionHistoryIT.java
@@ -32,7 +32,7 @@ import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 @MultiDbTest
 @DisabledIfSystemProperty(named = "test.integration.camunda.database.type", matches = "AWS_OS")
-public class DeleteProcessDefinitionIT {
+public class DeleteProcessDefinitionHistoryIT {
 
   @MultiDbTestApplication
   static final TestStandaloneBroker BROKER =
