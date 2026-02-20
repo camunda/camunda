e61756c4f356a650dcadfff31c0addcbfb98d7d9
Lena Schönburg <lena@fairburg.xyz>
2026-02-20 23:32:40 +0100

    test: update tests for CF-based backup range status
    Adapt BackupApiRequestHandlerTest to use CheckpointInfo instead of BackupStatus.
    Add mock ZeebeDb to BackupServiceTransitionStepTest so installBackupManager can
    create state instances. Pass null state params in BackupServiceImplTest,
    ConcurrentBackupCompactionTest, and PartitionRestoreServiceTest where range
    status is not exercised.

--- zeebe/restore/src/test/java/io/camunda/zeebe/restore/PartitionRestoreServiceTest.java
+++ zeebe/restore/src/test/java/io/camunda/zeebe/restore/PartitionRestoreServiceTest.java
@@ -122,7 +122,9 @@ class PartitionRestoreServiceTest {
             // RaftPartitions implements this interface, but the RaftServer is not started
             index -> CompletableFuture.completedFuture(journal.getTailSegments(index)),
             meterRegistry,
-            (context, entries, source) -> Either.left(WriteFailure.CLOSED));
+            (context, entries, source) -> Either.left(WriteFailure.CLOSED),
+            null,
+            null);
     actorScheduler.submitActor(backupService);
   }
 
