f935d35a0682b26abcdc2f61c11117587be32396
Nico Korthout <nico.korthout@camunda.com>
2026-02-20 16:04:55 +0100

    refactor: inject spring environment
    By injecting the spring Environment we'll be able to read
    spring.config.import and spring.config.location during configuration
    service building.
    
    Note that I've added some JavaDoc to the builder method to clarify the
    different usages of each method.

--- optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/util/configuration/ConfigurationServiceBuilder.java
+++ optimize/util/optimize-commons/src/main/java/io/camunda/optimize/service/util/configuration/ConfigurationServiceBuilder.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class ConfigurationServiceBuilder {
@@ -22,17 +23,55 @@ public class ConfigurationServiceBuilder {
   private String[] configLocations = DEFAULT_CONFIG_LOCATIONS.toArray(new String[] {});
   private ConfigurationValidator configurationValidator = new ConfigurationValidator();
 
+  /**
+   * Creates a ConfigurationService bean that is configured based on the default config locations
+   * and any additional config locations specified via spring.config.import and
+   * spring.config.location environment properties.
+   *
+   * @param environment the Spring Environment to read properties from
+   * @return the created ConfigurationService bean
+   */
   @Bean
   @Qualifier("configurationService")
+  public static ConfigurationService createConfigurationService(final Environment environment) {
+    return createConfiguration().build();
+  }
+
+  /**
+   * Creates a ConfigurationService based on the default config, the given additional config
+   * locations, and any additional config locations specified via spring.config.import and
+   * spring.config.location environment properties. The given config locations will be loaded on top
+   * of the default config, and then any additional config locations from the environment will be
+   * added on top of that.
+   *
+   * @param environment the Spring Environment to read properties from
+   * @param configLocations config locations to load configuration from
+   * @return the created ConfigurationService
+   */
+  public static ConfigurationService createConfigurationService(
+      final Environment environment, final String... configLocations) {
+    return createConfiguration().loadConfigurationFrom(configLocations).build();
+  }
+
+  /**
+   * Creates a ConfigurationService based on the default config locations. This does not take into
+   * account any additional config locations specified via spring.config.import and
+   * spring.config.location environment properties, so it should only be used in contexts where
+   * those properties are not expected to be set (e.g. in tests).
+   *
+   * @return the created ConfigurationService
+   */
   public static ConfigurationService createDefaultConfiguration() {
     return createConfiguration().build();
   }
 
-  public static ConfigurationService createConfigurationFromLocations(
-      final String... configLocations) {
-    return createConfiguration().loadConfigurationFrom(configLocations).build();
-  }
-
+  /**
+   * Creates a ConfigurationService based on the default config and the given additional config
+   * locations. The given config locations will be loaded on top of the default config.
+   *
+   * @param additionalLocations config locations to load configuration from
+   * @return the created ConfigurationService
+   */
   public static ConfigurationService createConfigurationWithDefaultAndAdditionalLocations(
       final String... additionalLocations) {
     return createConfiguration().addConfigurationLocations(additionalLocations).build();
