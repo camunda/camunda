596c53e2164a5d4a02c7386e8dada0b3eda09d5b
romansmirnov <roman.smirnov@camunda.com>
2026-02-23 13:58:19 +0100

    test: just for load testing

---
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/hubmetrics/ProcessMetricsValue.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.engine.state.hubmetrics;
+
+import io.camunda.zeebe.db.DbValue;
+import io.camunda.zeebe.msgpack.UnpackedObject;
+import io.camunda.zeebe.msgpack.property.IntegerProperty;
+import io.camunda.zeebe.msgpack.property.LongProperty;
+
+public class ProcessMetricsValue extends UnpackedObject implements DbValue {
+
+  private final LongProperty absoluteProp = new LongProperty("abs", 0);
+  private final IntegerProperty createdProp = new IntegerProperty("created", 0);
+  private final IntegerProperty completedProp = new IntegerProperty("completed", 0);
+
+  public ProcessMetricsValue() {
+    super(3);
+    declareProperty(absoluteProp).declareProperty(createdProp).declareProperty(completedProp);
+  }
+
+  public long getAbsolute() {
+    return absoluteProp.getValue();
+  }
+
+  public ProcessMetricsValue setAbsolute(final long value) {
+    absoluteProp.setValue(value);
+    return this;
+  }
+
+  public void incrementAbsolute() {
+    final var currentValue = getAbsolute();
+    setAbsolute(currentValue + 1);
+  }
+
+  public void decrementAbsolute() {
+    final var currentAbsoluteCounter = getAbsolute();
+    final var newValue = Math.max(currentAbsoluteCounter - 1, 0);
+    setAbsolute(newValue);
+  }
+
+  public int getCreated() {
+    return createdProp.getValue();
+  }
+
+  public ProcessMetricsValue setCreated(final int value) {
+    createdProp.setValue(value);
+    return this;
+  }
+
+  public void incrementCreated() {
+    final var currentCreatedCounter = getCreated();
+    setCreated(currentCreatedCounter + 1);
+  }
+
+  public int getCompleted() {
+    return completedProp.getValue();
+  }
+
+  public ProcessMetricsValue setCompleted(final int value) {
+    completedProp.setValue(value);
+    return this;
+  }
+
+  public void incrementCompleted() {
+    final var currentCompletedCounter = getCompleted();
+    setCompleted(currentCompletedCounter + 1);
+  }
+}
