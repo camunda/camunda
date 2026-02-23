7ff20b254d3fe90eeeccec011f13ff79fa437ae3
Jonathan Lukas <jonathan.lukas@camunda.com>
2026-02-23 10:29:17 +0100

    fix: job worker change sets reset tenant filter as well

--- clients/camunda-spring-boot-starter/src/test/java/io/camunda/client/jobhandling/JobWorkerChangeSetTest.java
+++ clients/camunda-spring-boot-starter/src/test/java/io/camunda/client/jobhandling/JobWorkerChangeSetTest.java
@@ -22,6 +22,7 @@ import io.camunda.client.annotation.value.SourceAware.Empty;
 import io.camunda.client.annotation.value.SourceAware.FromAnnotation;
 import io.camunda.client.annotation.value.SourceAware.FromOverrideProperty;
 import io.camunda.client.annotation.value.SourceAware.FromRuntimeOverride;
+import io.camunda.client.api.command.enums.TenantFilter;
 import io.camunda.client.jobhandling.JobWorkerChangeSet.FetchVariablesChangeSet;
 import io.camunda.client.jobhandling.JobWorkerChangeSet.ForceFetchAllVariablesChangeSet;
 import io.camunda.client.jobhandling.JobWorkerChangeSet.MaxJobsActiveChangeSet;
@@ -160,5 +161,17 @@ public class JobWorkerChangeSetTest {
     changeSet1.applyChanges(jobWorkerValue);
     assertThat(jobWorkerValue.getTenantIds())
         .isEqualTo(List.of(new FromRuntimeOverride<>("xyz", new FromAnnotation<>("abc"))));
+  }
+
+  @Test
+  void shouldResetTenantFilter() {
+    final JobWorkerValue jobWorkerValue = new JobWorkerValue();
+    jobWorkerValue.setTenantFilter(
+        new FromRuntimeOverride<>(
+            TenantFilter.ASSIGNED, new FromAnnotation<>(TenantFilter.PROVIDED)));
+    final ResetChangeSet resetChangeSet = new ResetChangeSet();
+    resetChangeSet.applyChanges(jobWorkerValue);
+    assertThat(jobWorkerValue.getTenantFilter())
+        .isEqualTo(new FromAnnotation<>(TenantFilter.PROVIDED));
   }
 }
