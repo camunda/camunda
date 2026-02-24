91f3fdf2521baf85ea4e7b32f0e4ac911ded8521
remco.westerhoud <remcowesterhoud@msn.com>
2026-02-24 17:00:32 +0100

    fix: set key on request
    The key is used to determine the partition that should receive the command. We need to ensure we set this properly,
    as if it's null the command would be distributed round-robin. As a result process instance existence checks would
    sometimes fail, and sometimes succeed, depending on the partition that received the command.

--- zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerDeleteHistoryRequest.java
+++ zeebe/gateway/src/main/java/io/camunda/zeebe/gateway/impl/broker/request/BrokerDeleteHistoryRequest.java
@@ -24,6 +24,7 @@ public class BrokerDeleteHistoryRequest extends BrokerExecuteCommand<HistoryDele
   }
 
   public BrokerDeleteHistoryRequest setResourceKey(final long resourceKey) {
+    request.setKey(resourceKey);
     requestDto.setResourceKey(resourceKey);
     return this;
   }
