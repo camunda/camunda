b4ab0254f5fc64a31ca9cd8813b4004db8120d4c
Eddie Tsedeke <6949627+tsedekey@users.noreply.github.com>
2026-02-24 12:13:16 +0300

    refactor: remove /api/decision-instances endpoint

--- operate/qa/util/src/main/java/io/camunda/operate/qa/util/RestAPITestUtil.java
+++ operate/qa/util/src/main/java/io/camunda/operate/qa/util/RestAPITestUtil.java
@@ -7,8 +7,6 @@
  */
 package io.camunda.operate.qa.util;
 
-import io.camunda.operate.webapp.rest.dto.dmn.list.DecisionInstanceListQueryDto;
-import io.camunda.operate.webapp.rest.dto.dmn.list.DecisionInstanceListRequestDto;
 import io.camunda.operate.webapp.rest.dto.listview.ListViewQueryDto;
 import io.camunda.operate.webapp.rest.dto.listview.ListViewRequestDto;
 import java.util.List;
@@ -84,37 +82,6 @@ public class RestAPITestUtil {
 
   public static ListViewRequestDto createGetAllProcessInstancesRequest() {
     return new ListViewRequestDto(createGetAllProcessInstancesQuery());
-  }
-
-  public static DecisionInstanceListQueryDto createDecisionInstanceQuery(
-      Consumer<DecisionInstanceListQueryDto> filtersSupplier) {
-    final DecisionInstanceListQueryDto query = new DecisionInstanceListQueryDto();
-    filtersSupplier.accept(query);
-    return query;
-  }
-
-  public static DecisionInstanceListRequestDto createDecisionInstanceRequest(
-      Consumer<DecisionInstanceListQueryDto> filtersSupplier) {
-    final DecisionInstanceListRequestDto request = new DecisionInstanceListRequestDto();
-    final DecisionInstanceListQueryDto query = new DecisionInstanceListQueryDto();
-    filtersSupplier.accept(query);
-    request.setQuery(query);
-    return request;
-  }
-
-  public static DecisionInstanceListQueryDto createGetAllDecisionInstancesQuery() {
-    return createDecisionInstanceQuery(q -> q.setFailed(true).setEvaluated(true));
-  }
-
-  public static DecisionInstanceListRequestDto createGetAllDecisionInstancesRequest() {
-    return new DecisionInstanceListRequestDto(createGetAllDecisionInstancesQuery());
-  }
-
-  public static DecisionInstanceListRequestDto createGetAllDecisionInstancesRequest(
-      Consumer<DecisionInstanceListQueryDto> filtersSupplier) {
-    final DecisionInstanceListQueryDto decisionInstanceQuery = createGetAllDecisionInstancesQuery();
-    filtersSupplier.accept(decisionInstanceQuery);
-    return new DecisionInstanceListRequestDto(decisionInstanceQuery);
   }
 
   public static ListViewRequestDto createGetAllProcessInstancesRequest(
