9d403a92f6a249919abf39a88d268efcd3973df6
Eddie Tsedeke <6949627+tsedekey@users.noreply.github.com>
2026-02-24 11:42:38 +0300

    refactor: remove /api/decisions/grouped endpoint

--- operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/DecisionReader.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/elasticsearch/reader/DecisionReader.java
@@ -7,29 +7,14 @@
  */
 package io.camunda.operate.webapp.elasticsearch.reader;
 
-import co.elastic.clients.elasticsearch._types.SortOrder;
-import co.elastic.clients.elasticsearch._types.aggregations.Aggregation;
-import co.elastic.clients.elasticsearch._types.aggregations.StringTermsAggregate;
-import co.elastic.clients.elasticsearch._types.aggregations.TopHitsAggregation;
-import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
-import co.elastic.clients.elasticsearch.core.search.Hit;
 import io.camunda.operate.conditions.ElasticsearchCondition;
 import io.camunda.operate.exceptions.OperateRuntimeException;
 import io.camunda.operate.util.ElasticsearchUtil;
-import io.camunda.operate.webapp.rest.dto.DecisionRequestDto;
 import io.camunda.operate.webapp.rest.exception.NotFoundException;
-import io.camunda.operate.webapp.security.permission.PermissionsService;
-import io.camunda.security.configuration.SecurityConfiguration;
 import io.camunda.webapps.schema.descriptors.index.DecisionIndex;
 import io.camunda.webapps.schema.entities.dmn.definition.DecisionDefinitionEntity;
-import io.camunda.zeebe.protocol.record.value.PermissionType;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,10 +29,6 @@ public class DecisionReader extends AbstractReader
   private static final Logger LOGGER = LoggerFactory.getLogger(DecisionReader.class);
 
   @Autowired private DecisionIndex decisionIndex;
-
-  @Autowired private PermissionsService permissionsService;
-
-  @Autowired private SecurityConfiguration securityConfiguration;
 
   /**
    * Gets the decision by key
@@ -88,111 +69,5 @@ public class DecisionReader extends AbstractReader
       LOGGER.error(message, e);
       throw new OperateRuntimeException(message, e);
     }
-  }
-
-  /**
-   * Returns map of Decision entities grouped by decisionId.
-   *
-   * @return
-   */
-  @Override
-  public Map<String, List<DecisionDefinitionEntity>> getDecisionsGrouped(
-      final DecisionRequestDto request) {
-    final var tenantsGroupsAggName = "group_by_tenantId";
-    final var groupsAggName = "group_by_decisionId";
-    final var decisionsAggName = "decisions";
-
-    final var decisionsAgg =
-        new TopHitsAggregation.Builder()
-            .source(
-                s ->
-                    s.filter(
-                        f ->
-                            f.includes(
-                                DecisionIndex.ID,
-                                DecisionIndex.NAME,
-                                DecisionIndex.VERSION,
-                                DecisionIndex.DECISION_ID,
-                                DecisionIndex.TENANT_ID)))
-            .size(ElasticsearchUtil.TOPHITS_AGG_SIZE)
-            .sort(s -> s.field(f -> f.field(DecisionIndex.VERSION).order(SortOrder.Desc)))
-            .build();
-
-    final var groupByDecisionIdAgg =
-        new Aggregation.Builder()
-            .terms(t -> t.field(DecisionIndex.DECISION_ID).size(ElasticsearchUtil.TERMS_AGG_SIZE))
-            .aggregations(Map.of(decisionsAggName, decisionsAgg._toAggregation()))
-            .build();
-
-    final var groupByTenantIdAgg =
-        new Aggregation.Builder()
-            .terms(t -> t.field(DecisionIndex.TENANT_ID).size(ElasticsearchUtil.TERMS_AGG_SIZE))
-            .aggregations(Map.of(groupsAggName, groupByDecisionIdAgg))
-            .build();
-
-    final var query = buildQuery(request.getTenantId());
-    final var tenantAwareQuery = tenantHelper.makeQueryTenantAware(query);
-    final var searchRequest =
-        new SearchRequest.Builder()
-            .index(decisionIndex.getAlias())
-            .query(tenantAwareQuery)
-            .aggregations(tenantsGroupsAggName, groupByTenantIdAgg)
-            .size(0)
-            .build();
-
-    try {
-      final var searchResponse = esClient.search(searchRequest, DecisionDefinitionEntity.class);
-      final var groups = searchResponse.aggregations().get(tenantsGroupsAggName).sterms();
-      final var result = new HashMap<String, List<DecisionDefinitionEntity>>();
-
-      groups.buckets().array().stream()
-          .forEach(
-              b -> {
-                final var groupTenantId = b.key().stringValue();
-                final StringTermsAggregate decisionGroups =
-                    b.aggregations().get(groupsAggName).sterms();
-
-                decisionGroups.buckets().array().stream()
-                    .forEach(
-                        tenantB -> {
-                          final var decisionId = tenantB.key().stringValue();
-                          final var groupKey = groupTenantId + "_" + decisionId;
-
-                          final var decisionHits =
-                              tenantB.aggregations().get(decisionsAggName).topHits();
-                          final List<DecisionDefinitionEntity> decisions =
-                              decisionHits.hits().hits().stream()
-                                  .map(Hit::source)
-                                  .filter(Objects::nonNull)
-                                  .map(source -> source.to(DecisionDefinitionEntity.class))
-                                  .toList();
-                          result.put(groupKey, new ArrayList<>(decisions));
-                        });
-              });
-
-      return result;
-    } catch (final IOException e) {
-      final var message =
-          String.format(
-              "Exception occurred, while obtaining grouped decisions: %s", e.getMessage());
-      throw new OperateRuntimeException(message, e);
-    }
-  }
-
-  private Query buildQuery(final String tenantId) {
-    final var allowed =
-        permissionsService.getDecisionsWithPermission(PermissionType.READ_DECISION_DEFINITION);
-
-    final var decisionIdQ =
-        allowed.isAll()
-            ? ElasticsearchUtil.matchAllQuery()
-            : ElasticsearchUtil.termsQuery(DecisionIndex.DECISION_ID, allowed.getIds());
-
-    Query tenantIdQ = null;
-    if (securityConfiguration.getMultiTenancy().isChecksEnabled()) {
-      tenantIdQ =
-          tenantId != null ? ElasticsearchUtil.termsQuery(DecisionIndex.TENANT_ID, tenantId) : null;
-    }
-    return ElasticsearchUtil.joinWithAnd(decisionIdQ, tenantIdQ);
   }
 }
--- operate/webapp/src/main/java/io/camunda/operate/webapp/opensearch/reader/OpensearchDecisionReader.java
+++ operate/webapp/src/main/java/io/camunda/operate/webapp/opensearch/reader/OpensearchDecisionReader.java
@@ -7,31 +7,15 @@
  */
 package io.camunda.operate.webapp.opensearch.reader;
 
-import static io.camunda.operate.store.opensearch.client.sync.OpenSearchDocumentOperations.TERMS_AGG_SIZE;
-import static io.camunda.operate.store.opensearch.client.sync.OpenSearchDocumentOperations.TOPHITS_AGG_SIZE;
-import static io.camunda.operate.store.opensearch.dsl.AggregationDSL.*;
 import static io.camunda.operate.store.opensearch.dsl.QueryDSL.*;
 import static io.camunda.operate.store.opensearch.dsl.RequestDSL.searchRequestBuilder;
 
 import io.camunda.operate.conditions.OpensearchCondition;
 import io.camunda.operate.store.opensearch.client.sync.RichOpenSearchClient;
 import io.camunda.operate.webapp.reader.DecisionReader;
-import io.camunda.operate.webapp.rest.dto.DecisionRequestDto;
 import io.camunda.operate.webapp.rest.exception.NotFoundException;
-import io.camunda.operate.webapp.security.permission.PermissionsService;
-import io.camunda.security.configuration.SecurityConfiguration;
 import io.camunda.webapps.schema.descriptors.index.DecisionIndex;
-import io.camunda.webapps.schema.descriptors.index.DecisionRequirementsIndex;
-import io.camunda.webapps.schema.descriptors.index.ProcessIndex;
 import io.camunda.webapps.schema.entities.dmn.definition.DecisionDefinitionEntity;
-import io.camunda.zeebe.protocol.record.value.PermissionType;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.opensearch.client.opensearch._types.SortOrder;
-import org.opensearch.client.opensearch._types.query_dsl.Query;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.stereotype.Component;
@@ -40,15 +24,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OpensearchDecisionReader implements DecisionReader {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(OpensearchDecisionReader.class);
-
   @Autowired private DecisionIndex decisionIndex;
-
-  @Autowired private DecisionRequirementsIndex decisionRequirementsIndex;
-
-  @Autowired private PermissionsService permissionsService;
-
-  @Autowired private SecurityConfiguration securityConfiguration;
 
   @Autowired private RichOpenSearchClient richOpenSearchClient;
 
@@ -68,88 +44,5 @@ public class OpensearchDecisionReader implements DecisionReader {
       throw new NotFoundException(
           String.format("Could not find decision with key '%s'.", decisionDefinitionKey));
     }
-  }
-
-  @Override
-  public Map<String, List<DecisionDefinitionEntity>> getDecisionsGrouped(
-      final DecisionRequestDto request) {
-    final var tenantsGroupsAggName = "group_by_tenantId";
-    final var groupsAggName = "group_by_decisionId";
-    final var decisionsAggName = "decisions";
-    final var sourceFields =
-        List.of(
-            DecisionIndex.ID,
-            DecisionIndex.NAME,
-            DecisionIndex.VERSION,
-            DecisionIndex.DECISION_ID,
-            DecisionIndex.TENANT_ID);
-
-    final var aggregationsRequest =
-        searchRequestBuilder(decisionIndex.getAlias())
-            .query(withTenantCheck(buildQuery(request.getTenantId())))
-            .size(0)
-            .aggregations(
-                tenantsGroupsAggName,
-                withSubaggregations(
-                    termAggregation(ProcessIndex.TENANT_ID, TERMS_AGG_SIZE),
-                    Map.of(
-                        groupsAggName,
-                        withSubaggregations(
-                            termAggregation(DecisionIndex.DECISION_ID, TERMS_AGG_SIZE),
-                            Map.of(
-                                decisionsAggName,
-                                topHitsAggregation(
-                                        sourceFields,
-                                        TOPHITS_AGG_SIZE,
-                                        sortOptions(DecisionIndex.VERSION, SortOrder.Desc))
-                                    .toAggregation())))));
-
-    final Map<String, List<DecisionDefinitionEntity>> result = new HashMap<>();
-    final var response = richOpenSearchClient.doc().search(aggregationsRequest, Object.class);
-
-    response
-        .aggregations()
-        .get(tenantsGroupsAggName)
-        .sterms()
-        .buckets()
-        .array()
-        .forEach(
-            tenantBucket ->
-                tenantBucket
-                    .aggregations()
-                    .get(groupsAggName)
-                    .sterms()
-                    .buckets()
-                    .array()
-                    .forEach(
-                        decisionIdBucket -> {
-                          final String key = tenantBucket.key() + "_" + decisionIdBucket.key();
-                          final List<DecisionDefinitionEntity> value =
-                              decisionIdBucket
-                                  .aggregations()
-                                  .get(decisionsAggName)
-                                  .topHits()
-                                  .hits()
-                                  .hits()
-                                  .stream()
-                                  .map(h -> h.source().to(DecisionDefinitionEntity.class))
-                                  .toList();
-                          result.put(key, value);
-                        }));
-
-    return result;
-  }
-
-  private Query buildQuery(final String tenantId) {
-    final var allowed =
-        permissionsService.getDecisionsWithPermission(PermissionType.READ_DECISION_DEFINITION);
-    final Query decisionIdQuery =
-        allowed.isAll() ? matchAll() : stringTerms(DecisionIndex.DECISION_ID, allowed.getIds());
-
-    Query tenantIdQuery = null;
-    if (securityConfiguration.getMultiTenancy().isChecksEnabled()) {
-      tenantIdQuery = tenantId != null ? term(DecisionIndex.TENANT_ID, tenantId) : null;
-    }
-    return and(decisionIdQuery, tenantIdQuery);
   }
 }
