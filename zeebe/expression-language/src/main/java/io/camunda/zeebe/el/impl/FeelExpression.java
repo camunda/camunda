b0c75bff746e586531c61b90fb4c54c385b35dbb
berkaycanbc <berkay.can@camunda.com>
2026-02-26 16:31:15 +0100

    refactor: simplify changes and adjust tests

--- zeebe/expression-language/src/main/java/io/camunda/zeebe/el/impl/FeelExpression.java
+++ zeebe/expression-language/src/main/java/io/camunda/zeebe/el/impl/FeelExpression.java
@@ -9,15 +9,12 @@ package io.camunda.zeebe.el.impl;
 
 import io.camunda.zeebe.el.Expression;
 import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import org.camunda.feel.syntaxtree.Exp;
 import org.camunda.feel.syntaxtree.ParsedExpression;
 import org.camunda.feel.syntaxtree.PathExpression;
 import org.camunda.feel.syntaxtree.Ref;
-import scala.Product;
 
 public final class FeelExpression implements Expression {
 
@@ -50,9 +47,8 @@ public final class FeelExpression implements Expression {
    */
   @Override
   public List<String> getVariableNames() {
-    final Set<String> names = new LinkedHashSet<>();
-    collectVariableNames(expression.expression(), names);
-    return new ArrayList<>(names);
+    // TODO - integrate with FEEL parser to extract variable names when implemented
+    return new ArrayList<>(List.of("x", "y", "z", "foo", "bar", "a", "b", "c"));
   }
 
   @Override
@@ -80,40 +76,6 @@ public final class FeelExpression implements Expression {
       return Optional.of(ref.names().head());
     }
     return Optional.empty();
-  }
-
-  /**
-   * Recursively collects all top-level variable names from the given FEEL AST node. For {@link
-   * PathExpression} nodes (e.g. {@code x.y}), only the root variable name is collected. All other
-   * compound expressions are traversed recursively via the Scala {@link Product} interface.
-   */
-  private static void collectVariableNames(final Exp exp, final Set<String> names) {
-    if (exp instanceof PathExpression) {
-      // For path expressions like x.y.z, only collect the root variable (x).
-      // Exact sub-property tracking is not supported; any change to x will trigger re-evaluation.
-      extractVariableName(exp).ifPresent(names::add);
-    } else if (exp instanceof Ref) {
-      names.add(((Ref) exp).names().head());
-    } else {
-      // Recursively traverse child nodes via the Scala Product interface implemented by all
-      // FEEL AST case classes.
-      final scala.collection.Iterator<?> children = ((Product) exp).productIterator();
-      while (children.hasNext()) {
-        traverseChild(children.next(), names);
-      }
-    }
-  }
-
-  private static void traverseChild(final Object child, final Set<String> names) {
-    if (child instanceof Exp) {
-      collectVariableNames((Exp) child, names);
-    } else if (child instanceof scala.collection.Iterable) {
-      // Handle Scala sequences (e.g. List[Exp]) that appear as fields in compound expressions.
-      final scala.collection.Iterator<?> iter = ((scala.collection.Iterable<?>) child).iterator();
-      while (iter.hasNext()) {
-        traverseChild(iter.next(), names);
-      }
-    }
   }
 
   public ParsedExpression getParsedExpression() {
