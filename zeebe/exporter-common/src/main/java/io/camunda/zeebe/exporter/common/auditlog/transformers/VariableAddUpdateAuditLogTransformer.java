050672517cef8ee4de27aa87f3b2071531c06747
Nikola Koevski <nikola.koevski@camunda.com>
2026-02-26 14:00:56 +0100

    feat: track only direct variable operations in the audit log
    Variable operations in the audit log are only tracked when
    directly requested through the gRPC/REST API. Variable
    operations triggered as follow-ups from other operations
    , like job complete, are not tracked (yet).

--- zeebe/exporter-common/src/main/java/io/camunda/zeebe/exporter/common/auditlog/transformers/VariableAddUpdateAuditLogTransformer.java
+++ zeebe/exporter-common/src/main/java/io/camunda/zeebe/exporter/common/auditlog/transformers/VariableAddUpdateAuditLogTransformer.java
@@ -9,6 +9,7 @@ package io.camunda.zeebe.exporter.common.auditlog.transformers;
 
 import io.camunda.zeebe.exporter.common.auditlog.AuditLogEntry;
 import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.value.VariableOperationType;
 import io.camunda.zeebe.protocol.record.value.VariableRecordValue;
 
 public class VariableAddUpdateAuditLogTransformer
@@ -23,5 +24,12 @@ public class VariableAddUpdateAuditLogTransformer
   public void transform(final Record<VariableRecordValue> record, final AuditLogEntry log) {
     final VariableRecordValue value = record.getValue();
     log.setEntityDescription(value.getName());
+  }
+
+  @Override
+  public boolean supports(final Record<?> record) {
+    final VariableRecordValue value = (VariableRecordValue) record.getValue();
+    return AuditLogTransformer.super.supports(record)
+        && VariableOperationType.API.equals(value.getSource().getType());
   }
 }
