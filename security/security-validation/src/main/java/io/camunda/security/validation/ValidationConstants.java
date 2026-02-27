c9bf8122b77b4225b033e1a379bdf62165458233
Christian Thiel <christian.thiel@holisticon.de>
2026-02-27 09:23:03 +0100

    feat: move length constant to separate class

---
+++ security/security-validation/src/main/java/io/camunda/security/validation/ValidationConstants.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.security.validation;
+
+public final class ValidationConstants {
+  public static final int MAX_FIELD_LENGTH = 256;
+
+  private ValidationConstants() {
+    // prevent instantiation
+  }
+}
