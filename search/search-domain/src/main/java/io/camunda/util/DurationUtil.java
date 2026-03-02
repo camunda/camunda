97a3f152c106d494d02f369d76d711426bc1e372
Jonathan Roques <jonathan.roques@camunda.com>
2026-03-02 14:29:29 +0100

    feat: add Date Histogram aggregation support
    (cherry picked from commit fc7d5ce936c66a21933568209fa24bd05a9c6d3c)

--- search/search-domain/src/main/java/io/camunda/util/DurationUtil.java
+++ search/search-domain/src/main/java/io/camunda/util/DurationUtil.java
@@ -8,6 +8,7 @@
 package io.camunda.util;
 
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 public final class DurationUtil {
 
@@ -21,5 +22,30 @@ public final class DurationUtil {
   /** Returns the smaller of two durations. */
   public static Duration min(final Duration a, final Duration b) {
     return a.compareTo(b) <= 0 ? a : b;
+  }
+
+  public static String toEsOsInterval(final Duration duration) {
+    final long seconds = duration.getSeconds();
+    if (seconds <= 0) {
+      throw new IllegalArgumentException("Duration must be greater than zero seconds: " + duration);
+    }
+
+    if (seconds % ChronoUnit.YEARS.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.YEARS.getDuration().getSeconds()) + "y";
+    } else if (seconds % ChronoUnit.MONTHS.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.MONTHS.getDuration().getSeconds()) + "M";
+    } else if (seconds % ChronoUnit.WEEKS.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.WEEKS.getDuration().getSeconds()) + "w";
+    } else if (seconds % ChronoUnit.DAYS.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.DAYS.getDuration().getSeconds()) + "d";
+    } else if (seconds % ChronoUnit.HOURS.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.HOURS.getDuration().getSeconds()) + "h";
+    } else if (seconds % ChronoUnit.MINUTES.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.MINUTES.getDuration().getSeconds()) + "m";
+    } else if (seconds % ChronoUnit.SECONDS.getDuration().getSeconds() == 0) {
+      return (seconds / ChronoUnit.SECONDS.getDuration().getSeconds()) + "s";
+    } else {
+      throw new IllegalArgumentException("Duration must be a whole number of seconds: " + duration);
+    }
   }
 }
