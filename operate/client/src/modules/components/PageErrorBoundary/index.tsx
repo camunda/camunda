b2cdd34b056b421d1dd5df7d3a1f59c29846a513
arienzIT <lucarienti@gmail.com>
2026-03-02 12:07:46 +0100

    fix: create react router page error boundary component

---
+++ operate/client/src/modules/components/PageErrorBoundary/index.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {isRouteErrorResponse, useRouteError} from 'react-router-dom';
+import {CodeSnippet} from '@carbon/react';
+import {ErrorWrapper} from './ErrorWrapper.tsx';
+
+const PageErrorBoundary: React.FC = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return (
+      <ErrorWrapper>
+        <p>
+          {error.status} {error.statusText}
+        </p>
+        <CodeSnippet type="multi">{error.data}</CodeSnippet>
+      </ErrorWrapper>
+    );
+  } else if (error instanceof Error) {
+    const stackTrace = error.stack ?? 'No stack trace available';
+    return (
+      <ErrorWrapper>
+        <CodeSnippet>{error.message}</CodeSnippet>
+        <h4>Error stack trace</h4>
+        <CodeSnippet type="multi">{stackTrace}</CodeSnippet>
+      </ErrorWrapper>
+    );
+  } else {
+    return (
+      <ErrorWrapper>
+        <CodeSnippet type="single">Unknown error</CodeSnippet>
+      </ErrorWrapper>
+    );
+  }
+};
+
+export {PageErrorBoundary};
---
+++ operate/client/src/modules/components/PageErrorBoundary/styled.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {Stack} from '@carbon/react';
+import styled from 'styled-components';
+
+export const StyledSection = styled('section')`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledStack = styled(Stack)`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
