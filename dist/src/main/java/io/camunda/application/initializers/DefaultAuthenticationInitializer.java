ca002a0904fb1160daf755a9d3dbe52415a3f590
Mattia Buccarella <mattia.buccarella@camunda.com>
2026-03-04 16:39:59 +0100

    build: transform application-insecure.yaml into code

--- dist/src/main/java/io/camunda/application/initializers/DefaultAuthenticationInitializer.java
+++ dist/src/main/java/io/camunda/application/initializers/DefaultAuthenticationInitializer.java
@@ -8,13 +8,17 @@
 package io.camunda.application.initializers;
 
 import static io.camunda.application.Profile.CONSOLIDATED_AUTH;
+import static io.camunda.application.Profile.INSECURE;
 import static io.camunda.application.Profile.getWebappProfiles;
 
 import io.camunda.authentication.config.AuthenticationProperties;
+import java.util.Map;
 import java.util.Set;
+import org.springframework.boot.env.DefaultPropertiesPropertySource;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
 
 /** Adds the "consolidated-auth" profile if it's not set */
 public class DefaultAuthenticationInitializer
@@ -25,6 +29,18 @@ public class DefaultAuthenticationInitializer
     final var env = context.getEnvironment();
     if (shouldApplyDefaultAuthenticationProfile(env)) {
       env.addActiveProfile(CONSOLIDATED_AUTH.getId());
+    }
+
+    final Set<String> activeProfiles = Set.of(env.getActiveProfiles());
+    if (activeProfiles.contains(INSECURE.getId())) {
+      final MutablePropertySources propertySources = env.getPropertySources();
+      final Map<String, Object> propertyMap =
+          Map.of(
+              "zeebe.broker.gateway.security.enabled", false, // embedded gateway
+              "zeebe.gateway.security.enabled", false, // dedicated gateway
+              "camunda.security.authentication.unprotected-api", true,
+              "camunda.security,authorizations.enabled", false);
+      DefaultPropertiesPropertySource.addOrMerge(propertyMap, propertySources);
     }
   }
 
