9bdf125862cf981c0f3a12747492f3c3fb43ec9e
Kristin Komschow <162317489+kristinkomschow@users.noreply.github.com>
2026-03-04 10:29:32 +0100

    Revert "feat: backport user task PIM frontend warnings to stable/8.8"

--- operate/client/src/App/Processes/MigrationView/BottomPanel/styled.tsx
+++ operate/client/src/App/Processes/MigrationView/BottomPanel/styled.tsx
@@ -7,11 +7,10 @@
  */
 
 import styled from 'styled-components';
-import {spacing03, supportSuccess, supportWarning} from '@carbon/elements';
+import {supportSuccess} from '@carbon/elements';
 import {
   CheckmarkFilled as BaseCheckmark,
   ArrowRight as BaseArrowRight,
-  WarningFilled as BaseWarningFilled,
 } from '@carbon/react/icons';
 import {DataTable as BaseDataTable} from 'modules/components/DataTable';
 import {Select as BaseSelect} from '@carbon/react';
@@ -64,16 +63,6 @@ const ErrorMessageContainer = styled.div`
   height: 100%;
 `;
 
-const WarningFilled = styled(BaseWarningFilled)`
-  fill: ${supportWarning};
-  margin-right: ${spacing03};
-
-  [data-icon-path='inner-path'] {
-    opacity: 1;
-    fill: black;
-  }
-`;
-
 const CheckmarkFilled = styled(BaseCheckmark)`
   color: ${supportSuccess};
 `;
@@ -99,5 +88,4 @@ export {
   Select,
   IconContainer,
   ToggleContainer,
-  WarningFilled,
 };
