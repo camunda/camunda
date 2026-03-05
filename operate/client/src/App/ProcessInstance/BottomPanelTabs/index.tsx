eec44c9d6a282fa0bef97c269fb44c3d1f506fa0
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-03-05 20:25:21 +0100

    refactor: remove useMemo
    (cherry picked from commit 481922d3bcd83e099a765e1d2d592f46bde583c6)

--- operate/client/src/App/ProcessInstance/BottomPanelTabs/index.tsx
+++ operate/client/src/App/ProcessInstance/BottomPanelTabs/index.tsx
@@ -6,7 +6,6 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {useMemo} from 'react';
 import {Outlet} from 'react-router-dom';
 import {Paths} from 'modules/Routes';
 import {Container} from './styled';
@@ -21,73 +20,66 @@ const BottomPanelTabs: React.FC = () => {
   const {data: processInstance} = useProcessInstance();
   const {processInstanceId} = useProcessInstancePageParams();
   const {currentPage} = useCurrentPage();
-  const tabItems = useMemo(() => {
-    return [
-      {
-        label: 'Details',
-        to: {pathname: Paths.processInstanceDetails({processInstanceId})},
-        key: 'details',
-        selected: currentPage === 'process-details-details',
-        title: 'Details',
-        visible: hasSelection,
+  const tabItems = [
+    {
+      label: 'Details',
+      to: {pathname: Paths.processInstanceDetails({processInstanceId})},
+      key: 'details',
+      selected: currentPage === 'process-details-details',
+      title: 'Details',
+      visible: hasSelection,
+    },
+    {
+      label: 'Variables',
+      to: {pathname: Paths.processInstance(processInstanceId)},
+      key: 'variables',
+      selected: currentPage === 'process-details',
+      title: 'Variables',
+      visible: true,
+    },
+    {
+      label: 'Incidents',
+      to: {pathname: Paths.processInstanceIncidents({processInstanceId})},
+      key: 'incidents',
+      selected: currentPage === 'process-details-incidents',
+      title: 'Incidents',
+      visible: processInstance?.hasIncident === true,
+    },
+    {
+      label: 'Input Mappings',
+      to: {pathname: Paths.processInstanceInputMappings({processInstanceId})},
+      key: 'input-mappings',
+      selected: currentPage === 'process-details-input-mappings',
+      title: 'Input Mappings',
+      visible: hasSelection,
+    },
+    {
+      label: 'Output Mappings',
+      to: {
+        pathname: Paths.processInstanceOutputMappings({processInstanceId}),
       },
-      {
-        label: 'Variables',
-        to: {pathname: Paths.processInstance(processInstanceId)},
-        key: 'variables',
-        selected: currentPage === 'process-details',
-        title: 'Variables',
-        visible: true,
-      },
-      {
-        label: 'Incidents',
-        to: {pathname: Paths.processInstanceIncidents({processInstanceId})},
-        key: 'incidents',
-        selected: currentPage === 'process-details-incidents',
-        title: 'Incidents',
-        visible: processInstance?.hasIncident === true,
-      },
-      {
-        label: 'Input Mappings',
-        to: {pathname: Paths.processInstanceInputMappings({processInstanceId})},
-        key: 'input-mappings',
-        selected: currentPage === 'process-details-input-mappings',
-        title: 'Input Mappings',
-        visible: hasSelection,
-      },
-      {
-        label: 'Output Mappings',
-        to: {
-          pathname: Paths.processInstanceOutputMappings({processInstanceId}),
-        },
-        key: 'output-mappings',
-        selected: currentPage === 'process-details-output-mappings',
-        title: 'Output Mappings',
-        visible: hasSelection,
-      },
-      {
-        label: 'Listeners',
-        to: {pathname: Paths.processInstanceListeners({processInstanceId})},
-        key: 'listeners',
-        selected: currentPage === 'process-details-listeners',
-        title: 'Listeners',
-        visible: true,
-      },
-      {
-        label: 'Operations Log',
-        to: {pathname: Paths.processInstanceOperationsLog({processInstanceId})},
-        key: 'operations-log',
-        selected: currentPage === 'process-details-operations-log',
-        title: 'Operations Log',
-        visible: true,
-      },
-    ] satisfies React.ComponentProps<typeof TabListNav>['items'];
-  }, [
-    processInstanceId,
-    currentPage,
-    hasSelection,
-    processInstance?.hasIncident,
-  ]);
+      key: 'output-mappings',
+      selected: currentPage === 'process-details-output-mappings',
+      title: 'Output Mappings',
+      visible: hasSelection,
+    },
+    {
+      label: 'Listeners',
+      to: {pathname: Paths.processInstanceListeners({processInstanceId})},
+      key: 'listeners',
+      selected: currentPage === 'process-details-listeners',
+      title: 'Listeners',
+      visible: true,
+    },
+    {
+      label: 'Operations Log',
+      to: {pathname: Paths.processInstanceOperationsLog({processInstanceId})},
+      key: 'operations-log',
+      selected: currentPage === 'process-details-operations-log',
+      title: 'Operations Log',
+      visible: true,
+    },
+  ] satisfies React.ComponentProps<typeof TabListNav>['items'];
 
   return (
     <Container>
