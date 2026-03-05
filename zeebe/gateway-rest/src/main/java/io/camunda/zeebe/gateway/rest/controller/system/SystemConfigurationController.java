b374cd481642f8df2d4eb9a12da470760b73ed16
Mathias Vandaele <mathias.vandaele@camunda.com>
2026-03-06 00:19:23 +0800

    feat: add the endpoint to read jobMetrics configuration
    (cherry picked from commit 19e29a358d3e114d91854ef98eb76f0a20bea24c)

---
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/system/SystemConfigurationController.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.rest.controller.system;
+
+import io.camunda.gateway.protocol.model.JobMetricsConfigurationResponse;
+import io.camunda.zeebe.gateway.rest.annotation.CamundaGetMapping;
+import io.camunda.zeebe.gateway.rest.config.GatewayRestConfiguration;
+import io.camunda.zeebe.gateway.rest.config.GatewayRestConfiguration.JobMetricsConfiguration;
+import io.camunda.zeebe.gateway.rest.controller.CamundaRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@CamundaRestController
+@RequestMapping("/v2/system/configuration")
+public class SystemConfigurationController {
+
+  private final GatewayRestConfiguration gatewayRestConfiguration;
+
+  public SystemConfigurationController(final GatewayRestConfiguration gatewayRestConfiguration) {
+    this.gatewayRestConfiguration = gatewayRestConfiguration;
+  }
+
+  @CamundaGetMapping(path = "/job-metrics")
+  public ResponseEntity<JobMetricsConfigurationResponse> getJobMetricsConfiguration() {
+    final JobMetricsConfiguration jobMetricsCfg = gatewayRestConfiguration.getJobMetrics();
+    final var response =
+        new JobMetricsConfigurationResponse()
+            .enabled(jobMetricsCfg.isEnabled())
+            .exportInterval(jobMetricsCfg.getExportInterval().toString())
+            .maxWorkerNameLength(jobMetricsCfg.getMaxWorkerNameLength())
+            .maxJobTypeLength(jobMetricsCfg.getMaxJobTypeLength())
+            .maxTenantIdLength(jobMetricsCfg.getMaxTenantIdLength())
+            .maxUniqueKeys(jobMetricsCfg.getMaxUniqueKeys());
+    return ResponseEntity.ok(response);
+  }
+}
