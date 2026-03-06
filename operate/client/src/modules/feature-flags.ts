49d565c9654d9c4544bd2c2e295ced93a6bb8f44
Yuliia <yuliia.saienko-ext@camunda.com>
2026-03-06 16:55:41 +0200

    fix: fixed for review comments

--- operate/client/src/modules/feature-flags.ts
+++ operate/client/src/modules/feature-flags.ts
@@ -6,6 +6,6 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-const IS_NEW_PROCESS_INSTANCE_PAGE = false;
+const IS_NEW_PROCESS_INSTANCE_PAGE = true;
 
 export {IS_NEW_PROCESS_INSTANCE_PAGE};
