8c6f57f29801bbc32716ae01f3c9e894a373a1e4
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2026-03-06 14:42:02 +0100

    feat: Define interface and test cases for conditional scenario API of CPT
    (cherry picked from commit 49a755ba827674d50e93b7b2c2b6071dcc192940)

---
+++ testing/camunda-process-test-java/src/main/java/io/camunda/process/test/api/ConditionalScenarioActionStep.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.process.test.api;
+
+/**
+ * A step in the conditional scenario builder that allows chaining additional actions or defining
+ * new scenarios.
+ */
+public interface ConditionalScenarioActionStep {
+
+  /**
+   * Chains an additional action for repeated condition matches. Actions are consumed in order:
+   * first match fires the first action, second match fires the second, and the last action repeats
+   * indefinitely.
+   *
+   * @param action the action to execute on the next condition match
+   * @return this step for further chaining
+   */
+  ConditionalScenarioActionStep then(Runnable action);
+
+  /**
+   * Defines a new conditional scenario with the given condition.
+   *
+   * @param condition the condition to evaluate
+   * @return the condition step for defining the action
+   */
+  ConditionalScenarioConditionStep when(Runnable condition);
+}
---
+++ testing/camunda-process-test-spring/src/test/java/io/camunda/process/test/api/SpringConditionalScenarioApiIT.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright © 2017 camunda services GmbH (info@camunda.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.camunda.process.test.api;
+
+import static io.camunda.process.test.api.CamundaAssert.assertThatProcessInstance;
+import static io.camunda.process.test.api.CamundaAssert.assertThatUserTask;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.ProcessInstanceEvent;
+import io.camunda.process.test.api.assertions.ProcessInstanceSelectors;
+import io.camunda.process.test.api.assertions.UserTaskSelectors;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = {SpringConditionalScenarioApiIT.class})
+@CamundaSpringProcessTest
+public class SpringConditionalScenarioApiIT {
+
+  @Autowired private CamundaClient client;
+  @Autowired private CamundaProcessTestContext processTestContext;
+
+  @Test
+  void shouldCompleteProcessWithConditionalScenarios() {
+    final Map<String, Object> unhappy = Collections.singletonMap("happy", false);
+    final Map<String, Object> happy = Collections.singletonMap("happy", true);
+    final Map<String, Object> exportVars = Collections.singletonMap("exportSuccess", true);
+
+    // Deploy
+    client
+        .newDeployResourceCommand()
+        .addResourceFromClasspath("conditionalScenarioApi/scenario-api-test.bpmn")
+        .addResourceFromClasspath("conditionalScenarioApi/satisfaction.form")
+        .send()
+        .join();
+
+    // Setup conditional scenarios
+    processTestContext
+        .when(
+            () -> assertThatUserTask(UserTaskSelectors.byElementId("State_Happiness")).isCreated())
+        .then(() -> processTestContext.completeUserTask("State_Happiness", unhappy))
+        .then(() -> processTestContext.completeUserTask("State_Happiness", happy))
+        .when(
+            () ->
+                assertThatProcessInstance(
+                        ProcessInstanceSelectors.byProcessId("user-happiness-check"))
+                    .hasActiveElements("Export_Happiness"))
+        .then(() -> processTestContext.completeJob("io.camunda:http-json:1", exportVars));
+
+    // Start the process
+    final ProcessInstanceEvent processInstanceEvent =
+        client
+            .newCreateInstanceCommand()
+            .bpmnProcessId("user-happiness-check")
+            .latestVersion()
+            .send()
+            .join();
+
+    // Assert
+    assertThatProcessInstance(processInstanceEvent)
+        .isCompleted()
+        .hasVariable("happy", true)
+        .hasVariable("exportSuccess", true);
+  }
+}
