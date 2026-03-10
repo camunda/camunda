1c4de357010f2011d8cf0d8e8959d8d01f99a548
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-03-10 01:20:23 +0100

    fix: fix scrolling on smaller screens

--- operate/client/src/App/ProcessInstance/BottomPanelTabs/styled.tsx
+++ operate/client/src/App/ProcessInstance/BottomPanelTabs/styled.tsx
@@ -9,6 +9,10 @@
 import {styled} from 'styled-components';
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
   background-color: var(--cds-layer);
 `;
 
