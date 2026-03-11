bf247afc015c67b3ac6c32769eff30195276196c
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2026-03-11 13:22:01 +0100

    refactor: Move classes from api to impl to reduce api surface
    (cherry picked from commit c476199961fdd35c99abe88d591097fdede0f0c9)

--- testing/camunda-process-test-langchain4j/src/main/java/io/camunda/process/test/impl/judge/AnthropicChatModelBuilder.java
+++ testing/camunda-process-test-langchain4j/src/main/java/io/camunda/process/test/impl/judge/AnthropicChatModelBuilder.java
@@ -13,12 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.camunda.process.test.api.judge;
+package io.camunda.process.test.impl.judge;
 
-import static io.camunda.process.test.api.judge.ModelBuilderSupport.require;
+import static io.camunda.process.test.impl.judge.ModelBuilderSupport.require;
 
 import dev.langchain4j.model.anthropic.AnthropicChatModel;
 import dev.langchain4j.model.chat.ChatModel;
+import io.camunda.process.test.api.judge.JudgeConfigBootstrapData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
--- testing/camunda-process-test-langchain4j/src/main/java/io/camunda/process/test/impl/judge/ModelBuilderSupport.java
+++ testing/camunda-process-test-langchain4j/src/main/java/io/camunda/process/test/impl/judge/ModelBuilderSupport.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.camunda.process.test.api.judge;
+package io.camunda.process.test.impl.judge;
 
 final class ModelBuilderSupport {
 
