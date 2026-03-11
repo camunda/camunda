9d7217f18790a7c6b6109322ab1bd331c97e2269
Lukas Taake <lukas.taake@holisticon.de>
2026-03-11 17:01:30 +0100

    fix: represent new behavior checking existence of users and groups in tests

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/group/GroupTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/group/GroupTest.java
@@ -157,7 +157,7 @@ public class GroupTest {
   }
 
   @Test
-  public void shouldNotRejectIfUserIsNotPresent() {
+  public void shouldRejectIfUserIsNotPresent() {
     // given
     final var groupId = Strings.newRandomValidIdentityId();
     final var name = UUID.randomUUID().toString();
@@ -171,11 +171,15 @@ public class GroupTest {
             .addEntity(groupId)
             .withEntityId(notPresentEntityId)
             .withEntityType(EntityType.USER)
-            .add()
-            .getValue();
+            .expectRejection()
+            .add();
 
     // then
-    assertThat(updatedGroup).hasEntityId(notPresentEntityId);
+    assertThat(updatedGroup)
+        .hasRejectionType(RejectionType.NOT_FOUND)
+        .hasRejectionReason(
+            "Expected to add an entity with ID '%s' and type '%s' to group with ID '%s', but the entity does not exist."
+                .formatted(notPresentEntityId, EntityType.USER, groupId));
   }
 
   @Test
