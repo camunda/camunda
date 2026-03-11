2b08e2963e588ad9299e44c9723cd99c142b8f14
tanjinyi <ambrose.tan@camunda.com>
2026-03-11 21:30:10 +0800

    fix: use PI perms instead of PD perms in pd element statistics
    # Conflicts:
    #	service/src/test/java/io/camunda/service/ProcessDefinitionServiceTest.java

---
+++ qa/acceptance-tests/src/test/java/io/camunda/it/auth/ProcessDefinitionStatisticsAuthorizationIT.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.auth;
+
+import static io.camunda.it.util.TestHelper.waitForProcessInstances;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.search.enums.PermissionType;
+import io.camunda.client.api.search.enums.ProcessInstanceState;
+import io.camunda.client.api.search.enums.ResourceType;
+import io.camunda.qa.util.auth.Authenticated;
+import io.camunda.qa.util.auth.Permissions;
+import io.camunda.qa.util.auth.TestUser;
+import io.camunda.qa.util.auth.UserDefinition;
+import io.camunda.qa.util.multidb.MultiDbTest;
+import io.camunda.qa.util.multidb.MultiDbTestApplication;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
+import java.util.List;
+import java.util.UUID;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
+@MultiDbTest
+@DisabledIfSystemProperty(named = "test.integration.camunda.database.type", matches = "AWS_OS")
+class ProcessDefinitionStatisticsAuthorizationIT {
+
+  @MultiDbTestApplication
+  static final TestStandaloneBroker BROKER =
+      new TestStandaloneBroker().withBasicAuth().withAuthorizationsEnabled();
+
+  private static final String TEST_USERNAME_READ_PROCESS_DEFINITION =
+      "testUserReadProcessDefinition";
+  private static final String TEST_USERNAME_READ_PROCESS_INSTANCE = "testUserReadProcessInstance";
+
+  @UserDefinition
+  private static final TestUser TEST_USER_WITH_READ_PROCESS_DEFINITION =
+      new TestUser(
+          TEST_USERNAME_READ_PROCESS_DEFINITION,
+          "password",
+          List.of(
+              new Permissions(
+                  ResourceType.PROCESS_DEFINITION,
+                  PermissionType.READ_PROCESS_DEFINITION,
+                  List.of("*"))));
+
+  @UserDefinition
+  private static final TestUser TEST_USER_WITH_READ_PROCESS_INSTANCE =
+      new TestUser(
+          TEST_USERNAME_READ_PROCESS_INSTANCE,
+          "password",
+          List.of(
+              new Permissions(
+                  ResourceType.PROCESS_DEFINITION,
+                  PermissionType.READ_PROCESS_INSTANCE,
+                  List.of("*"))));
+
+  private static CamundaClient camundaClient;
+
+  @Test
+  void shouldReturnEmptyStatisticsForReadProcessDefinitionPermission(
+      @Authenticated(TEST_USERNAME_READ_PROCESS_DEFINITION) final CamundaClient userClient) {
+    // given
+    final var processDefinitionKey = deployCompleteBPMN();
+    createInstance(processDefinitionKey);
+    createInstance(processDefinitionKey);
+    waitForProcessInstances(
+        camundaClient,
+        f -> f.processDefinitionKey(processDefinitionKey).state(ProcessInstanceState.COMPLETED),
+        2);
+
+    // when
+    final var actual =
+        userClient.newProcessDefinitionElementStatisticsRequest(processDefinitionKey).send().join();
+
+    // then
+    assertThat(actual).isEmpty();
+  }
+
+  @Test
+  void shouldAllowReadProcessInstancePermission(
+      @Authenticated(TEST_USERNAME_READ_PROCESS_INSTANCE) final CamundaClient userClient) {
+    // given
+    final var processDefinitionKey = deployCompleteBPMN();
+    createInstance(processDefinitionKey);
+    createInstance(processDefinitionKey);
+    waitForProcessInstances(
+        camundaClient,
+        f -> f.processDefinitionKey(processDefinitionKey).state(ProcessInstanceState.COMPLETED),
+        2);
+
+    // when
+    final var actual =
+        userClient.newProcessDefinitionElementStatisticsRequest(processDefinitionKey).send().join();
+
+    // then
+    assertThat(actual).hasSize(2);
+  }
+
+  private static long deployCompleteBPMN() {
+    final var processId = "process-auth-" + UUID.randomUUID();
+    final var processModel = Bpmn.createExecutableProcess(processId).startEvent().endEvent().done();
+    return deployResource(processModel, "complete-auth.bpmn")
+        .getProcesses()
+        .getFirst()
+        .getProcessDefinitionKey();
+  }
+
+  private static void createInstance(final long processDefinitionKey) {
+    camundaClient
+        .newCreateInstanceCommand()
+        .processDefinitionKey(processDefinitionKey)
+        .send()
+        .join();
+  }
+
+  private static io.camunda.client.api.response.DeploymentEvent deployResource(
+      final BpmnModelInstance processModel, final String resourceName) {
+    return camundaClient
+        .newDeployResourceCommand()
+        .addProcessModel(processModel, resourceName)
+        .send()
+        .join();
+  }
+}
