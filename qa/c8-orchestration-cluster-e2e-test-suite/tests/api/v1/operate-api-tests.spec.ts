510083ef129d55ee4ca5e53745a34e60efe05094
Hilal <hilal.bursali@camunda.com>
2026-03-12 12:27:05 +0100

    test: changed tests for rdbms

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v1/operate-api-tests.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v1/operate-api-tests.spec.ts
@@ -10,80 +10,82 @@ import {test} from 'fixtures';
 import {expect} from '@playwright/test';
 import {createInstances, deploy} from 'utils/zeebeClient';
 
-test.beforeAll(async () => {
-  await Promise.all([
-    deploy(['./resources/User_Task_Process_With_Form_API.bpmn']),
-  ]);
-  await createInstances('Form_User_Task_API', 1, 3);
-});
-
-test.describe('API tests', () => {
-  const auth = Buffer.from(`demo:demo`).toString('base64');
-  const requestHeaders = {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${auth}`,
-    },
-  };
-
-  test('Search for process definitions', async ({request}) => {
-    const processDefinition = await request.post(
-      '/v1/process-definitions/search',
-      requestHeaders,
-    );
-    expect(processDefinition.status()).toBe(200);
+if (process.env.DATABASE != 'RDBMS') {
+  test.beforeAll(async () => {
+    await Promise.all([
+      deploy(['./resources/User_Task_Process_With_Form_API.bpmn']),
+    ]);
+    await createInstances('Form_User_Task_API', 1, 3);
   });
 
-  test('Get a process definition via key', async ({request}) => {
-    let processDefinitions: {items: {key: number}[]} = {items: []};
-    await expect(async () => {
-      const response = await request.post(
+  test.describe('API tests', () => {
+    const auth = Buffer.from(`demo:demo`).toString('base64');
+    const requestHeaders = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${auth}`,
+      },
+    };
+
+    test('Search for process definitions', async ({request}) => {
+      const processDefinition = await request.post(
         '/v1/process-definitions/search',
         requestHeaders,
       );
-      expect(response.status()).toBe(200);
-      processDefinitions = await response.json();
-      expect(processDefinitions.items.length).toBeGreaterThan(0);
-    }).toPass({
-      intervals: [3_000, 4_000, 5_000],
-      timeout: 30_000,
+      expect(processDefinition.status()).toBe(200);
     });
-    const response = await request.get(
-      `/v1/process-definitions/${processDefinitions.items[0].key}`,
-      requestHeaders,
-    );
-    expect(response.status()).toBe(200);
-  });
 
-  test('Search for process instances', async ({request}) => {
-    const processInstancesList = await request.post(
-      'v1/process-instances/search',
-      requestHeaders,
-    );
-    expect(processInstancesList.status()).toBe(200);
-  });
+    test('Get a process definition via key', async ({request}) => {
+      let processDefinitions: {items: {key: number}[]} = {items: []};
+      await expect(async () => {
+        const response = await request.post(
+          '/v1/process-definitions/search',
+          requestHeaders,
+        );
+        expect(response.status()).toBe(200);
+        processDefinitions = await response.json();
+        expect(processDefinitions.items.length).toBeGreaterThan(0);
+      }).toPass({
+        intervals: [3_000, 4_000, 5_000],
+        timeout: 30_000,
+      });
+      const response = await request.get(
+        `/v1/process-definitions/${processDefinitions.items[0].key}`,
+        requestHeaders,
+      );
+      expect(response.status()).toBe(200);
+    });
 
-  test('Search for flownode-instances', async ({request}) => {
-    const flowNodeInstancesList = await request.post(
-      'v1/flownode-instances/search',
-      requestHeaders,
-    );
-    expect(flowNodeInstancesList.status()).toBe(200);
-  });
+    test('Search for process instances', async ({request}) => {
+      const processInstancesList = await request.post(
+        'v1/process-instances/search',
+        requestHeaders,
+      );
+      expect(processInstancesList.status()).toBe(200);
+    });
 
-  test('Search for variables for process instances', async ({request}) => {
-    const variablesInstancesList = await request.post(
-      'v1/variables/search',
-      requestHeaders,
-    );
-    expect(variablesInstancesList.status()).toBe(200);
-  });
+    test('Search for flownode-instances', async ({request}) => {
+      const flowNodeInstancesList = await request.post(
+        'v1/flownode-instances/search',
+        requestHeaders,
+      );
+      expect(flowNodeInstancesList.status()).toBe(200);
+    });
 
-  test('Search for incidents', async ({request}) => {
-    const incidentsList = await request.post(
-      'v1/incidents/search',
-      requestHeaders,
-    );
-    expect(incidentsList.status()).toBe(200);
+    test('Search for variables for process instances', async ({request}) => {
+      const variablesInstancesList = await request.post(
+        'v1/variables/search',
+        requestHeaders,
+      );
+      expect(variablesInstancesList.status()).toBe(200);
+    });
+
+    test('Search for incidents', async ({request}) => {
+      const incidentsList = await request.post(
+        'v1/incidents/search',
+        requestHeaders,
+      );
+      expect(incidentsList.status()).toBe(200);
+    });
   });
-});
+}
--- qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v1/tasklist-api-tests.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v1/tasklist-api-tests.spec.ts
@@ -10,45 +10,47 @@ import {test} from 'fixtures';
 import {expect} from '@playwright/test';
 import {createInstances, deploy} from 'utils/zeebeClient';
 
-test.beforeAll(async () => {
-  await Promise.all([
-    deploy([
-      './resources/User_Task_Process_With_Form_API.bpmn',
-      './resources/New Form.form',
-    ]),
-  ]);
-  await createInstances('Form_User_Task_API', 1, 3);
-});
-
-test.describe('API tests', () => {
-  const auth = Buffer.from(`demo:demo`).toString('base64');
-  const requestHeaders = {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Basic ${auth}`,
-    },
-  };
-
-  test('Search for tasks', async ({request}) => {
-    const response = await request.post('/v1/tasks/search', requestHeaders);
-    expect(response.status()).toBe(200);
+if (process.env.DATABASE != 'RDBMS') {
+  test.beforeAll(async () => {
+    await Promise.all([
+      deploy([
+        './resources/User_Task_Process_With_Form_API.bpmn',
+        './resources/New Form.form',
+      ]),
+    ]);
+    await createInstances('Form_User_Task_API', 1, 3);
   });
 
-  test('Get a task via ID', async ({request}) => {
-    let taskData: {id: string}[] = [];
-    await expect(async () => {
+  test.describe('API tests', () => {
+    const auth = Buffer.from(`demo:demo`).toString('base64');
+    const requestHeaders = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Basic ${auth}`,
+      },
+    };
+
+    test('Search for tasks', async ({request}) => {
       const response = await request.post('/v1/tasks/search', requestHeaders);
       expect(response.status()).toBe(200);
-      taskData = await response.json();
-      expect(taskData.length).toBeGreaterThan(0);
-    }).toPass({
-      intervals: [3_000, 4_000, 5_000],
-      timeout: 30_000,
     });
-    const response = await request.get(
-      `/v1/tasks/${taskData[0].id}`,
-      requestHeaders,
-    );
-    expect(response.status()).toBe(200);
+
+    test('Get a task via ID', async ({request}) => {
+      let taskData: {id: string}[] = [];
+      await expect(async () => {
+        const response = await request.post('/v1/tasks/search', requestHeaders);
+        expect(response.status()).toBe(200);
+        taskData = await response.json();
+        expect(taskData.length).toBeGreaterThan(0);
+      }).toPass({
+        intervals: [3_000, 4_000, 5_000],
+        timeout: 30_000,
+      });
+      const response = await request.get(
+        `/v1/tasks/${taskData[0].id}`,
+        requestHeaders,
+      );
+      expect(response.status()).toBe(200);
+    });
   });
-});
+}
