1915c825112f3f4f574760a5289cac745fb15bb9
Panagiotis Goutis <panagiotis.goutis@camunda.com>
2026-03-13 22:18:20 +0200

    refactor: skip obsolete null check
    (cherry picked from commit 0fd0df88af3d5dc8a8253a7666f6d78f23df3089)

--- zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/partitions/impl/steps/ExporterDirectorPartitionTransitionStep.java
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/partitions/impl/steps/ExporterDirectorPartitionTransitionStep.java
@@ -28,7 +28,6 @@ import io.camunda.zeebe.stream.impl.SkipPositionsFilter;
 import io.camunda.zeebe.util.VisibleForTesting;
 import io.camunda.zeebe.util.collection.Tuple;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.BiFunction;
@@ -113,8 +112,8 @@ public final class ExporterDirectorPartitionTransitionStep implements PartitionT
     final BrokerCfg brokerCfg = context.getBrokerCfg();
     final ExportingCfg exportingCfg = brokerCfg.getExporting();
     final int partitionId = context.getPartitionId();
-    final var exporterFilter =
-        SkipPositionsFilter.of(computeSkipPositions(exportingCfg, partitionId));
+    final var skipRecords = exportingCfg.skipRecords().getOrDefault(partitionId, Set.of());
+    final var exporterFilter = SkipPositionsFilter.of(skipRecords);
     final ExporterMode exporterMode =
         targetRole == Role.LEADER ? ExporterMode.ACTIVE : ExporterMode.PASSIVE;
     final ExporterDirectorContext exporterCtx =
@@ -159,12 +158,6 @@ public final class ExporterDirectorPartitionTransitionStep implements PartitionT
           }
         });
     return startFuture;
-  }
-
-  private static Set<Long> computeSkipPositions(
-      final ExportingCfg exportingCfg, final int partitionId) {
-    final var positions = exportingCfg.skipRecords().getOrDefault(partitionId, Set.of());
-    return positions == null ? Collections.emptySet() : positions;
   }
 
   private void deleteOrEnableExportersIfConfigChanged(
