9cf127fd7c8a4dfb03ab70095bb36b8a27476902
Hilal <hilal.bursali@camunda.com>
2026-03-16 19:26:22 +0100

    test: added sleep

--- qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityHeader.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityHeader.ts
@@ -46,7 +46,7 @@ export class IdentityHeader {
     await expect(this.page.getByText('logged out...')).not.toBeVisible({
       timeout: 15000,
     });
-    await sleep(3000);
+    await sleep(5000);
   }
 
   async navigateToRoles() {
