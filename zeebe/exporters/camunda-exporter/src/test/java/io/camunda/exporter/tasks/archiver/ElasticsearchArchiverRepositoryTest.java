4c76a395e8dc041a1f1bff0442cd82dd6367433f
Mattia Buccarella <mattia.buccarella@camunda.com>
2026-03-18 15:33:39 +0100

    build: adjust test - 4

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
@@ -184,6 +184,17 @@ final class ElasticsearchArchiverRepositoryTest extends AbstractArchiverReposito
     final var response = createResponse(List.of(hit1, hit2));
     when(client.search(any(SearchRequest.class), eq(ProcessInstanceForListViewEntity.class)))
         .thenReturn(CompletableFuture.completedFuture(response));
+    when(client.search(any(SearchRequest.class), eq(Void.class)))
+        .thenReturn(CompletableFuture.completedFuture(SearchResponse.<Void>of(
+            r ->
+                r.took(1)
+                    .timedOut(false)
+                    .shards(s -> s.total(1).successful(1).failed(0))
+                    .hits(h -> h.total(t -> t.value(0).relation(TotalHitsRelation.Eq)).hits(List.of()))
+                    .aggregations(Map.of(DATE_AGGREGATION_NAME, Aggregate.of(a -> a.dateHistogram(
+                        dh -> dh.buckets(b -> b.array(List.of()))
+                    ))))
+        )));
 
     // when
     final var batch = repository.getProcessInstancesNextBatch().join();
