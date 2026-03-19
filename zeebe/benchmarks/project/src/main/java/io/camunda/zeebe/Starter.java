02cf55bed84b0fa31d286f26bfed444dcaf27493
ck-claude-test <zelldon91@googlemail.com>
2026-03-19 11:22:56 +0100

    fix: make limit explicit
     * Set paging/limit for query explicity to 100
    
    (cherry picked from commit 51e0e6a0b1e38ea69cb8b74f19631e5ec1d02307)

--- zeebe/benchmarks/project/src/main/java/io/camunda/zeebe/Starter.java
+++ zeebe/benchmarks/project/src/main/java/io/camunda/zeebe/Starter.java
@@ -156,6 +156,7 @@ public class Starter extends App {
                       .newProcessInstanceSearchRequest()
                       .filter((f) -> f.processInstanceKey(key -> key.in(listOfStartedInstances)))
                       .sort(ProcessInstanceSort::startDate)
+                      .page(p -> p.limit(100))
                       .send();
 
               return send.thenApply(
