67a242ec32fceadca4eb56f7d75b5205213902b8
euro.lew <euro.lew@camunda.com>
2026-03-19 15:18:22 +0000

    style: fix spotless formatting in CamundaExporterIT

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/CamundaExporterIT.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/CamundaExporterIT.java
@@ -259,8 +259,7 @@ final class CamundaExporterIT {
     // given
     createSchemas(config);
     final var valueType = ValueType.USER;
-    final Record record =
-        generateRecordWithSupportedBrokerVersion(valueType, UserIntent.CREATED);
+    final Record record = generateRecordWithSupportedBrokerVersion(valueType, UserIntent.CREATED);
     final var resourceProvider = new DefaultExporterResourceProvider();
     resourceProvider.init(
         config,
