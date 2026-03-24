70e56c5f8d0f51314ad4ad5f138b0c40e2e695e2
Tobias Metzke-Bernstein <586643+tmetzke@users.noreply.github.com>
2026-03-24 11:03:26 +0100

    feat: add effective UT variable search REST layer

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/mapper/search/SearchQueryRequestMapper.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/mapper/search/SearchQueryRequestMapper.java
@@ -447,6 +447,24 @@ public final class SearchQueryRequestMapper {
     return buildSearchQuery(filter, sort, page, SearchQueryBuilders::variableSearchQuery);
   }
 
+  public static Either<ProblemDetail, VariableQuery> toUserTaskEffectiveVariableQuery(
+      final UserTaskEffectiveVariableSearchQueryRequest request) {
+    if (request == null) {
+      return Either.right(SearchQueryBuilders.variableSearchQuery().build());
+    }
+
+    final var filter = SearchQueryFilterMapper.toUserTaskVariableFilter(request.getFilter());
+    final var page = toSearchQueryPage(request.getPage());
+    final var sort =
+        SearchQuerySortRequestMapper.toSearchQuerySort(
+            SearchQuerySortRequestMapper.fromUserTaskVariableSearchQuerySortRequest(
+                request.getSort()),
+            SortOptionBuilders::variable,
+            SearchQuerySortRequestMapper::applyUserTaskVariableSortField);
+
+    return buildSearchQuery(filter, sort, page, SearchQueryBuilders::variableSearchQuery);
+  }
+
   public static Either<ProblemDetail, VariableQuery> toVariableQuery(
       final VariableSearchQuery request) {
 
