21bd4d6140cf115b2520d9df331e737a1a430524
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2026-03-24 10:15:46 +0100

    refactor: Rename ValueAssertjTest to ValueAssertTest

--- testing/camunda-process-test-java/src/test/java/io/camunda/process/test/api/ValueAssertTest.java
+++ testing/camunda-process-test-java/src/test/java/io/camunda/process/test/api/ValueAssertTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class ValueAssertjTest {
+public class ValueAssertTest {
 
   @AfterEach
   void resetJudgeConfig() {
