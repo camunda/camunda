f5e2be55c25437eaf846cddb066070a025ddb872
Carlo Sana <carlo.sana@camunda.com>
2026-03-25 09:11:39 +0100

    fix: whitelist JobIntent.TIME_OUT
    When a cluster is under load, the JOB_TIMEOUT commands can be rejected,
    which will lead to jobs staying in "ACTIVATED" state more than necessary
    and the background job to timeout them will issue the same commands
    again.
    Now that a job timeout is not pushed to the clients, it's safe to do so
    
    (cherry picked from commit d0811f10f856cf690a651e665017ed6f104d937b)

--- zeebe/logstreams/src/main/java/io/camunda/zeebe/logstreams/impl/flowcontrol/WhiteListedCommands.java
+++ zeebe/logstreams/src/main/java/io/camunda/zeebe/logstreams/impl/flowcontrol/WhiteListedCommands.java
@@ -23,6 +23,7 @@ public class WhiteListedCommands {
           JobIntent.COMPLETE,
           JobIntent.FAIL,
           JobIntent.YIELD,
+          JobIntent.TIME_OUT,
           ProcessInstanceIntent.CANCEL,
           DeploymentIntent.CREATE,
           DeploymentIntent.DISTRIBUTE,
