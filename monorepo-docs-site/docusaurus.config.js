1aae685979e1275d88e99d5413d8e525ff5c5942
Patrick Wunderlich <patrick.wunderlich@holisticon.de>
2026-03-25 14:01:42 +0100

    docs: made sidebar hideable in docusaurus

--- monorepo-docs-site/docusaurus.config.js
+++ monorepo-docs-site/docusaurus.config.js
@@ -96,6 +96,11 @@ const config = {
           },
         ],
       },
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       footer: {
         style: 'dark',
         links: [
