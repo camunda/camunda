13d6d74b0fd07ec7b598d0d9cf3ee74b253116a8
esraagamal6 <esraa-gamal@hotmail.com>
2026-03-30 14:31:13 +0200

    test: unskip audit log tests

--- qa/c8-orchestration-cluster-e2e-test-suite/pages/OperateOperationsLogPage.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/pages/OperateOperationsLogPage.ts
@@ -11,13 +11,39 @@ import type {Locator, Page} from '@playwright/test';
 export class OperateOperationsLogPage {
   private page: Page;
   readonly operationsLogTable: Locator;
+  readonly operationsLogTableRows: Locator;
+  readonly operationsLogHeading: Locator;
   readonly processInstanceKeyFilter: Locator;
+  readonly processInstanceCells: Locator;
+  readonly operationTypeColumnHeader: Locator;
+  readonly entityTypeColumnHeader: Locator;
+  readonly entityKeyColumnHeader: Locator;
+  readonly actorColumnHeader: Locator;
+  readonly dateColumnHeader: Locator;
   readonly resetFiltersButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.operationsLogTable = page.getByRole('table');
+    this.operationsLogTableRows = this.operationsLogTable.getByRole('row');
+    this.operationsLogHeading = page.getByRole('heading', {
+      name: /operations log/i,
+    });
     this.processInstanceKeyFilter = page.getByLabel('Process instance key');
+    this.processInstanceCells = this.operationsLogTable.getByRole('cell', {
+      name: /process instance/i,
+    });
+    this.operationTypeColumnHeader = page.getByRole('columnheader', {
+      name: 'Operation type',
+    });
+    this.entityTypeColumnHeader = page.getByRole('columnheader', {
+      name: 'Entity type',
+    });
+    this.entityKeyColumnHeader = page.getByRole('columnheader', {
+      name: 'Entity key',
+    });
+    this.actorColumnHeader = page.getByRole('columnheader', {name: 'Actor'});
+    this.dateColumnHeader = page.getByRole('columnheader', {name: 'Date'});
     this.resetFiltersButton = page.getByRole('button', {
       name: /reset filters/i,
     });
@@ -37,5 +63,9 @@ export class OperateOperationsLogPage {
         `${process.env.CORE_APPLICATION_URL}/operate/operations-log`,
       );
     }
+  }
+
+  async getRowCount(): Promise<number> {
+    return this.operationsLogTableRows.count();
   }
 }
