130ab30f69a765d79362acef25b8065adb644d35
Christian Thiel <christian.thiel@holisticon.de>
2026-04-02 11:53:36 +0200

    refactor: move rdbms typehandlers to singular package

--- db/rdbms/src/main/java/io/camunda/db/rdbms/sql/typehandler/NullToEmptyStringTypeHandler.java
+++ db/rdbms/src/main/java/io/camunda/db/rdbms/sql/typehandler/NullToEmptyStringTypeHandler.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.db.rdbms.typehandler;
+package io.camunda.db.rdbms.sql.typehandler;
 
 import static io.camunda.db.rdbms.read.NullSafeStrings.nullToEmpty;
 
