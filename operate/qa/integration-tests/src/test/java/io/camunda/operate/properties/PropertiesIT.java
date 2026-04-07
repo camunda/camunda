93b299f2feb0b4de4b264319b1baff24993e316a
Kristin Komschow <162317489+kristinkomschow@users.noreply.github.com>
2026-04-07 17:14:17 +0200

    feat: add Operate configuration for zeebe client request timeout

--- operate/qa/integration-tests/src/test/java/io/camunda/operate/properties/PropertiesIT.java
+++ operate/qa/integration-tests/src/test/java/io/camunda/operate/properties/PropertiesIT.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.operate.property.OperateProperties;
 import io.camunda.operate.util.apps.nobeans.TestApplicationWithNoBeans;
+import java.time.Duration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,7 @@ public class PropertiesIT {
     assertThat(operateProperties.getZeebeElasticsearch().getBatchSize()).isEqualTo(222);
     assertThat(operateProperties.getZeebeElasticsearch().getPrefix()).isEqualTo("somePrefix");
     assertThat(operateProperties.getZeebe().getGatewayAddress()).isEqualTo("someZeebeHost:999");
+    assertThat(operateProperties.getZeebe().getRequestTimeout()).isEqualTo(Duration.ofSeconds(50));
     assertThat(operateProperties.getOperationExecutor().getBatchSize()).isEqualTo(555);
     assertThat(operateProperties.getOperationExecutor().getWorkerId()).isEqualTo("someWorker");
     assertThat(operateProperties.getOperationExecutor().getLockTimeout()).isEqualTo(15000);
