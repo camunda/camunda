1fec906b8dbe72776ccfe73d6038f75cb9a99678
Catherina Prokudina <catherina.prokudina@camunda.com>
2026-04-09 12:09:49 +0200

    test: resolve related incident test

---
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/process-instance/process-instance-resolve-related-incident-api.spec.ts
@@ -0,0 +1,215 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {expect, test} from '@playwright/test';
+import {
+  cancelProcessInstance,
+  createInstances,
+  createSingleInstance,
+  deploy,
+} from '../../../../utils/zeebeClient';
+import {
+  assertBadRequest,
+  assertForbiddenRequest,
+  assertInvalidArgument,
+  assertNotFoundRequest,
+  assertStatusCode,
+  assertUnauthorizedRequest,
+  buildUrl,
+  encode,
+  jsonHeaders,
+} from '../../../../utils/http';
+import {defaultAssertionOptions} from '../../../../utils/constants';
+import {validateResponse} from '../../../../json-body-assertions';
+import {
+  createUser,
+  grantUserResourceAuthorization,
+  expectBatchState,
+} from '@requestHelpers';
+import {cleanupUsers} from 'utils/usersCleanup';
+
+
+test.describe.parallel('Resolve related incidents API Tests', () => {
+  let processInstanceKeyWithIncidentToResolve: string = '';
+  let userWithResourcesAuthorizationToSendRequest: {
+    username: string;
+    name: string;
+    email: string;
+    password: string;
+  } = {} as {
+    username: string;
+    name: string;
+    email: string;
+    password: string;
+  };
+
+  test.beforeAll(async ({request}) => {
+    await test.step('Setup - Deploy process and create instance to delete', async () => {
+      await deploy([
+        './resources/MultipleErrorTypesProcess.bpmn',
+      ]);
+      const createdInstance = await createSingleInstance('MultipleErrorTypesProcess', 1);
+      processInstanceKeyWithIncidentToResolve = createdInstance.processInstanceKey;
+    });
+
+    await test.step('Setup - Create test user with Resource Authorization and user for granting Authorization', async () => {
+    //   userWithResourcesAuthorizationToSendRequest = await createUser(request);
+    //   await grantUserResourceAuthorization(
+    //     request,
+    //     userWithResourcesAuthorizationToSendRequest,
+    //   );
+    });
+  });
+
+  test.afterAll(async ({request}) => {
+    await test.step('Cleanup - Delete test users', async () => {
+    //   await cleanupUsers(request, [
+    //     userWithResourcesAuthorizationToSendRequest.username,
+    //   ]);
+    });
+
+    await test.step('Cleanup - Cancel created process instances', async () => {
+      await cancelProcessInstance(processInstanceKeyWithIncidentToResolve);
+    });
+  });
+
+  test('Should resolve related incidents of a process instance', async ({request}) => {
+    let elementInstanceKey: string = '';
+    let batchOperationKey: string = '';
+    let incidentKeys: string[] = [];
+        await test.step('Search for incidents and verify the number of incidents and its state', async () => {
+          await expect(async () => {
+            const searchRes = await request.post(
+              buildUrl('/incidents/search'),
+              {
+                headers: jsonHeaders(),
+                data: {
+                  filter: {
+                    processInstanceKey: processInstanceKeyWithIncidentToResolve,
+                  },
+                },
+              },
+            );
+    
+            await assertStatusCode(searchRes, 200);
+            await validateResponse(
+              {
+                path: '/incidents/search',
+                method: 'POST',
+                status: '200',
+              },
+              searchRes,
+            );
+    
+            const body = await searchRes.json();
+            expect(body.page.totalItems).toEqual(2);
+            expect(body.items.length).toBeGreaterThan(0);
+            for (const incident of body.items) {
+                incidentKeys.push(incident.incidentKey);
+                expect(incident.state).toBe('ACTIVE');
+                if (incident.errorType === 'EXTRACT_VALUE_ERROR') {
+                    elementInstanceKey = incident.elementInstanceKey;
+                }
+            }
+          }).toPass(defaultAssertionOptions);
+        });
+    
+        await test.step('Update element instance variables', async () => {
+          const updateRes = await request.put(
+            buildUrl(`/element-instances/${elementInstanceKey}/variables`),
+            {
+              headers: jsonHeaders(),
+              data: {
+                variables: {
+                  goUp: 6,
+                },
+              },
+            },
+          );
+          await assertStatusCode(updateRes, 204);
+        });
+
+        await test.step('Resolve incidents', async () => {
+            const resolveRes = await request.post(buildUrl(`/process-instances/${processInstanceKeyWithIncidentToResolve}/incident-resolution`), {
+                headers: jsonHeaders(),
+            });
+            await assertStatusCode(resolveRes, 200);
+            await validateResponse({
+                path: `/process-instances/{processInstanceKey}/incident-resolution`,
+                method: 'POST',
+                status: '200',
+            }, resolveRes);
+            const body = await resolveRes.json();
+            batchOperationKey = body.batchOperationKey;
+            expect(body.batchOperationType).toBe('RESOLVE_INCIDENT');
+        });
+
+        await test.step('Poll batch operation until completion', async () => {
+            await expect(async () => {
+                const statusRes = await request.get(
+                    buildUrl(`/batch-operations/${batchOperationKey}`),
+                    {
+                        headers: jsonHeaders(),
+                    },
+                );
+                await assertStatusCode(statusRes, 200);
+                await validateResponse(
+                    {
+                        path: '/batch-operations/{batchOperationKey}',
+                        method: 'GET',
+                        status: '200',
+                    },
+                    statusRes,
+                );
+                const body = await statusRes.json();
+                expect(body.state).toBe('COMPLETED');
+                expect(body.operationsTotalCount).toBeGreaterThanOrEqual(2);
+            }).toPass({
+                intervals: [5_000, 10_000, 15_000, 25_000, 35_000],
+                timeout: 120_000,
+            });
+        });
+
+        await test.step('Search for incidents and verify the state is resolved', async () => {
+            await expect(async () => {
+            const searchRes = await request.post(
+              buildUrl('/incidents/search'),
+              {
+                headers: jsonHeaders(),
+                data: {
+                  filter: {
+                    processInstanceKey: processInstanceKeyWithIncidentToResolve,
+                  },
+                },
+              },
+            );
+    
+            await assertStatusCode(searchRes, 200);
+            await validateResponse(
+              {
+                path: '/incidents/search',
+                method: 'POST',
+                status: '200',
+              },
+              searchRes,
+            );
+    
+            const body = await searchRes.json();
+            expect(body.page.totalItems).toBeGreaterThanOrEqual(3);
+            expect(body.items.length).toBeGreaterThan(0);
+            for (const incident of body.items) {
+                if (incidentKeys.includes(incident.incidentKey)) {
+                    expect(incident.state).toBe('RESOLVED');
+                } else {
+                    expect(incident.state).toBe('ACTIVE');
+                }
+            }
+          }).toPass(defaultAssertionOptions);
+        });
+  });
+});
