3b2bf81884d4a279b5577fdeb386fc2c53e51a67
georgios-goulos <georgios.goulos@camunda.com>
2026-04-09 12:39:30 +0100

    test: fix flaky test
    (cherry picked from commit 5010dc7ff96e50788cfbc764a826d76ebea87318)

--- qa/acceptance-tests/src/test/java/io/camunda/it/cluster/ClusterMultiplePartitionsBatchOperationIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/cluster/ClusterMultiplePartitionsBatchOperationIT.java
@@ -8,37 +8,39 @@
 package io.camunda.it.cluster;
 
 import static io.camunda.it.util.TestHelper.deployResource;
-import static io.camunda.it.util.TestHelper.startProcessInstance;
+import static io.camunda.it.util.TestHelper.getScopedVariables;
+import static io.camunda.it.util.TestHelper.startScopedProcessInstance;
 import static io.camunda.it.util.TestHelper.waitForBatchOperationCompleted;
 import static io.camunda.it.util.TestHelper.waitForBatchOperationWithCorrectTotalCount;
-import static io.camunda.it.util.TestHelper.waitForElementInstances;
 import static io.camunda.it.util.TestHelper.waitForProcessInstanceToBeTerminated;
-import static io.camunda.it.util.TestHelper.waitForProcessInstancesToStart;
 import static io.camunda.it.util.TestHelper.waitForProcessesToBeDeployed;
+import static io.camunda.it.util.TestHelper.waitForScopedProcessInstancesToStart;
 import static io.camunda.qa.util.multidb.CamundaMultiDBExtension.TIMEOUT_DATA_AVAILABILITY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import io.camunda.client.CamundaClient;
-import io.camunda.client.api.response.Process;
 import io.camunda.client.api.response.ProcessInstanceEvent;
 import io.camunda.client.api.search.enums.BatchOperationItemState;
 import io.camunda.client.api.search.response.BatchOperationItems.BatchOperationItem;
-import io.camunda.client.impl.search.filter.ProcessInstanceFilterImpl;
-import io.camunda.qa.util.auth.Authenticated;
 import io.camunda.qa.util.multidb.MultiDbTest;
 import io.camunda.qa.util.multidb.MultiDbTestApplication;
 import io.camunda.zeebe.protocol.Protocol;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneApplication;
 import io.camunda.zeebe.qa.util.cluster.TestStandaloneBroker;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.stream.IntStream;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 @MultiDbTest
@@ -56,28 +58,38 @@ public class ClusterMultiplePartitionsBatchOperationIT {
                 b.setCluster(cluster);
               });
 
-  private static Process deployedProcess;
-  private static final List<ProcessInstanceEvent> ACTIVE_PROCESS_INSTANCES = new ArrayList<>();
+  private static CamundaClient camundaClient;
+
+  private String testScopeId;
+  private final List<ProcessInstanceEvent> activeProcessInstances = new ArrayList<>();
 
   @BeforeAll
-  public static void beforeAll(@Authenticated final CamundaClient camundaClient) {
+  static void beforeAll() {
     Objects.requireNonNull(camundaClient);
 
     // Deploy process definitions
-    deployedProcess =
+    final var deployedProcess =
         deployResource(camundaClient, "process/service_tasks_v1.bpmn").getProcesses().getFirst();
-    waitForProcessesToBeDeployed(camundaClient, 1);
+    waitForProcessesToBeDeployed(
+        camundaClient, f -> f.processDefinitionKey(deployedProcess.getProcessDefinitionKey()), 1);
+  }
 
-    // Start process instances
+  @BeforeEach
+  void beforeEach(final TestInfo testInfo) {
+    testScopeId =
+        testInfo.getTestMethod().map(Method::toString).orElse(UUID.randomUUID().toString());
+
+    // Start scoped process instances
     IntStream.range(0, 10)
         .forEach(
             i ->
-                ACTIVE_PROCESS_INSTANCES.add(
-                    startProcessInstance(camundaClient, "service_tasks_v1", "{\"xyz\":\"bar\"}")));
+                activeProcessInstances.add(
+                    startScopedProcessInstance(
+                        camundaClient, "service_tasks_v1", testScopeId, Map.of("xyz", "bar"))));
 
     // given we really have processes on more than one partitions
     final long countPartitionsDeployedTo =
-        ACTIVE_PROCESS_INSTANCES.stream()
+        activeProcessInstances.stream()
             .map(ProcessInstanceEvent::getProcessInstanceKey)
             .map(Protocol::decodePartitionId)
             .distinct()
@@ -86,26 +98,23 @@ public class ClusterMultiplePartitionsBatchOperationIT {
         countPartitionsDeployedTo > 1,
         "Test requires process instances to be deployed to multiple partitions.");
 
-    // Wait for process instances to start
-    waitForProcessInstancesToStart(camundaClient, ACTIVE_PROCESS_INSTANCES.size());
-    waitForElementInstances(camundaClient, ACTIVE_PROCESS_INSTANCES.size() * 2);
+    // Wait for scoped process instances to start
+    waitForScopedProcessInstancesToStart(camundaClient, testScopeId, activeProcessInstances.size());
   }
 
   @AfterEach
-  void afterAll() {
-    deployedProcess = null;
-    ACTIVE_PROCESS_INSTANCES.clear();
+  void afterEach() {
+    activeProcessInstances.clear();
   }
 
   @Test
-  void shouldCancelProcessInstancesOnSeveralPartitionsWithBatch(
-      @Authenticated final CamundaClient camundaClient) {
+  void shouldCancelProcessInstancesOnSeveralPartitionsWithBatch() {
     // when
     final var result =
         camundaClient
             .newCreateBatchOperationCommand()
             .processInstanceCancel()
-            .filter(new ProcessInstanceFilterImpl().processDefinitionId("service_tasks_v1"))
+            .filter(b -> b.variables(getScopedVariables(testScopeId)))
             .send()
             .join();
     final var batchOperationKey = result.getBatchOperationKey();
@@ -115,13 +124,13 @@ public class ClusterMultiplePartitionsBatchOperationIT {
 
     // and
     waitForBatchOperationWithCorrectTotalCount(
-        camundaClient, batchOperationKey, ACTIVE_PROCESS_INSTANCES.size());
+        camundaClient, batchOperationKey, activeProcessInstances.size());
     waitForBatchOperationCompleted(
-        camundaClient, batchOperationKey, ACTIVE_PROCESS_INSTANCES.size(), 0);
+        camundaClient, batchOperationKey, activeProcessInstances.size(), 0);
 
     // Now wait until all process instances are terminated
     final var activeKeys =
-        ACTIVE_PROCESS_INSTANCES.stream().map(ProcessInstanceEvent::getProcessInstanceKey).toList();
+        activeProcessInstances.stream().map(ProcessInstanceEvent::getProcessInstanceKey).toList();
     for (final Long key : activeKeys) {
       waitForProcessInstanceToBeTerminated(camundaClient, key);
     }
@@ -141,7 +150,7 @@ public class ClusterMultiplePartitionsBatchOperationIT {
               final var itemKeys =
                   itemsObj.items().stream().map(BatchOperationItem::getItemKey).toList();
 
-              assertThat(itemsObj.items()).hasSize(ACTIVE_PROCESS_INSTANCES.size());
+              assertThat(itemsObj.items()).hasSize(activeProcessInstances.size());
               assertThat(
                       itemsObj.items().stream()
                           .map(BatchOperationItem::getStatus)
