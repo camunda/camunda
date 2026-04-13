994eafc63758e43d2c59e971431aa7262ed7ca12
John Montgomery <john.montgomery@camunda.com>
2026-04-13 10:50:07 +0100

    test: test archive batches when no search results are found

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
@@ -7,15 +7,25 @@
  */
 package io.camunda.exporter.tasks.archiver;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
-import co.elastic.clients.json.SimpleJsonpMapper;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import co.elastic.clients.elasticsearch.core.SearchResponse;
+import co.elastic.clients.elasticsearch.core.search.HitsMetadata;
+import co.elastic.clients.elasticsearch.indices.ElasticsearchIndicesAsyncClient;
+import co.elastic.clients.elasticsearch.indices.GetIndexResponse;
 import io.camunda.exporter.config.ExporterConfiguration.HistoryConfiguration;
 import io.camunda.exporter.metrics.CamundaExporterMetrics;
 import io.camunda.exporter.tasks.utils.TestExporterResourceProvider;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
+import java.net.ConnectException;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
@@ -25,20 +35,35 @@ final class ElasticsearchArchiverRepositoryTest extends AbstractArchiverReposito
   private static final Logger LOGGER =
       LoggerFactory.getLogger(ElasticsearchArchiverRepositoryTest.class);
 
-  private final RestClientTransport transport = Mockito.spy(createRestClient());
+  private final ElasticsearchAsyncClient client = mock(ElasticsearchAsyncClient.class);
 
-  @Test
-  void shouldCloseTransportOnClose() throws Exception {
-    // when
-    repository.close();
+  @Override
+  @BeforeEach
+  void setup() {
+    super.setup();
+    givenNoArchivingStatus();
+  }
 
-    // then
-    Mockito.verify(transport, Mockito.times(1)).close();
+  @Override
+  void givenSearchRequestsFail() {
+    when(client.search(any(SearchRequest.class), any()))
+        .thenReturn(
+            CompletableFuture.failedFuture(
+                new ConnectException("Simulated ES failure for testing")));
+  }
+
+  @Override
+  void givenNoSearchResultsFound() {
+    final var response = mock(SearchResponse.class);
+    final var hits = mock(HitsMetadata.class);
+    when(response.hits()).thenReturn(hits);
+    when(hits.hits()).thenReturn(List.of());
+    when(client.search(any(SearchRequest.class), any()))
+        .thenReturn(CompletableFuture.completedFuture(response));
   }
 
   @Override
   ElasticsearchArchiverRepository createRepository() {
-    final var client = new ElasticsearchAsyncClient(transport);
     final var metrics = new CamundaExporterMetrics(new SimpleMeterRegistry());
     final var config = new HistoryConfiguration();
     config.setRetention(retention);
@@ -52,8 +77,20 @@ final class ElasticsearchArchiverRepositoryTest extends AbstractArchiverReposito
         LOGGER);
   }
 
-  private RestClientTransport createRestClient() {
-    final var restClient = RestClient.builder(HttpHost.create("http://127.0.0.1:1")).build();
-    return new RestClientTransport(restClient, new SimpleJsonpMapper());
+  @Test
+  void shouldCloseTransportOnClose() throws Exception {
+    // when
+    repository.close();
+
+    // then
+    Mockito.verify(client, Mockito.times(1)).close();
+  }
+
+  private void givenNoArchivingStatus() {
+    final var indicesClient = mock(ElasticsearchIndicesAsyncClient.class);
+    when(client.indices()).thenReturn(indicesClient);
+    final var response = mock(GetIndexResponse.class);
+    when(indicesClient.get(any(Function.class)))
+        .thenReturn(CompletableFuture.completedFuture(response));
   }
 }
