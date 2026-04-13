6354eb56eb403ef9e5871800e90bf4c643a54adf
John Montgomery <john.montgomery@camunda.com>
2026-04-13 16:18:17 +0100

    test: add IT to verify process instance archiving
    currently just a basic smoke test to ensure that the queries used are
    well-formed and work when there is nothing to archive.
    
    (cherry picked from commit ed050c5833a01ea47145dc1116ebe3143b35a4eb)

---
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ProcessInstanceArchiverJobIT.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.tasks.archiver;
+
+import io.camunda.exporter.ExporterResourceProvider;
+import io.camunda.exporter.config.ExporterConfiguration;
+import io.camunda.exporter.metrics.CamundaExporterMetrics;
+import io.camunda.webapps.schema.descriptors.ProcessInstanceDependant;
+import io.camunda.webapps.schema.descriptors.template.ListViewTemplate;
+import java.util.concurrent.Executor;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.slf4j.Logger;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class ProcessInstanceArchiverJobIT extends ArchiverJobIT<ProcessInstanceArchiverJob> {
+  @Override
+  ProcessInstanceArchiverJob createArchiveJob(
+      final ExporterConfiguration config,
+      final ExporterResourceProvider resourceProvider,
+      final ArchiverRepository repository,
+      final CamundaExporterMetrics exporterMetrics,
+      final Logger logger,
+      final Executor executor) {
+
+    final var dependantTemplates =
+        resourceProvider.getIndexTemplateDescriptors().stream()
+            .filter(ProcessInstanceDependant.class::isInstance)
+            .map(ProcessInstanceDependant.class::cast)
+            .toList();
+
+    return new ProcessInstanceArchiverJob(
+        config.getHistory(),
+        repository,
+        resourceProvider.getIndexTemplateDescriptor(ListViewTemplate.class),
+        dependantTemplates,
+        exporterMetrics,
+        logger,
+        executor);
+  }
+}
