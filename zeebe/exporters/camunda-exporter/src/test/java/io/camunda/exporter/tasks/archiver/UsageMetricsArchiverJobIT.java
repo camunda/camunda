a3325fa4aab54a081b30cf2e0e9405656df66aa1
John Montgomery <john.montgomery@camunda.com>
2026-04-13 16:51:21 +0100

    test: ITs for usage  metric archiving
    (cherry picked from commit d9bcd118b993b3612bbfc2eb482b98f700ea8ab8)

---
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/UsageMetricsArchiverJobIT.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.tasks.archiver;
+
+import io.camunda.exporter.ExporterResourceProvider;
+import io.camunda.exporter.config.ExporterConfiguration;
+import io.camunda.webapps.schema.descriptors.template.UsageMetricTUTemplate;
+import io.camunda.webapps.schema.descriptors.template.UsageMetricTemplate;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+
+@TestInstance(Lifecycle.PER_CLASS)
+public class UsageMetricsArchiverJobIT extends ArchiverJobIT<UsageMetricsArchiverJob> {
+  @Override
+  UsageMetricsArchiverJob createArchiveJob(
+      final ExporterConfiguration config,
+      final ExporterResourceProvider resourceProvider,
+      final ArchiverRepository repository) {
+    return new UsageMetricsArchiverJob(
+        repository,
+        resourceProvider.getIndexTemplateDescriptor(UsageMetricTemplate.class),
+        resourceProvider.getIndexTemplateDescriptor(UsageMetricTUTemplate.class),
+        exporterMetrics,
+        LOGGER,
+        executor);
+  }
+}
