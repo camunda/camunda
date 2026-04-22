6fca4e619eba77d4711aca5a781c4c5d62562775
Pranjal Goyal <pranjalgoyal13@gmail.com>
2026-04-22 16:27:36 +0530

    refactor: maven spotless
    (cherry picked from commit 9a56b16032547e57a9539e2adde5d09d943c7560)

--- load-tests/load-tester/src/test/java/io/camunda/zeebe/it/StarterWorkerIT.java
+++ load-tests/load-tester/src/test/java/io/camunda/zeebe/it/StarterWorkerIT.java
@@ -15,8 +15,6 @@ import io.camunda.zeebe.LoadTesterApplication;
 import io.camunda.zeebe.client.ZeebeClient;
 import java.time.Duration;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
