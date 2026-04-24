6e0bc8888ea7e48836c13f65f5c441dbea1d105a
John Montgomery <john.montgomery@camunda.com>
2026-04-24 10:33:01 +0100

    perf: differentiate between scheduled + delay flush reasons

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/CamundaExporter.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/CamundaExporter.java
@@ -360,7 +360,7 @@ public class CamundaExporter implements Exporter {
       final long now = context.clock().millis();
       final long lastFlushTimestamp = updateAndGetLastActiveFlushTimestamp(now);
       if ((now - lastFlushTimestamp) >= flushDelayMs) {
-        metrics.recordFlushReasonScheduled();
+        metrics.recordFlushReasonDelay();
         return true;
       }
     }
