70465faa977fb1d6b33ad2e41da48d0f55060696
tanjinyi <ambrose.tan@camunda.com>
2026-04-27 18:48:35 +0800

    feat: migrate tasklist backup restore to camunda qa

---
+++ qa/acceptance-tests/src/test/java/io/camunda/it/backup/TasklistBackupRestoreIT.java
@@ -0,0 +1,359 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.backup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+import feign.FeignException.NotFound;
+import io.camunda.application.commons.configuration.WorkingDirectoryConfiguration.WorkingDirectory;
+import io.camunda.client.CamundaClient;
+import io.camunda.configuration.Camunda;
+import io.camunda.configuration.PrimaryStorageBackup;
+import io.camunda.configuration.PrimaryStorageBackup.BackupStoreType;
+import io.camunda.it.document.DocumentClient;
+import io.camunda.management.backups.StateCode;
+import io.camunda.management.backups.TakeBackupHistoryResponse;
+import io.camunda.qa.util.cluster.HistoryBackupClient;
+import io.camunda.qa.util.cluster.TestCamundaApplication;
+import io.camunda.qa.util.multidb.MultiDbConfigurator;
+import io.camunda.search.connect.configuration.DatabaseType;
+import io.camunda.security.entity.AuthenticationMethod;
+import io.camunda.webapps.backup.BackupService.SnapshotRequest;
+import io.camunda.webapps.backup.BackupStateDto;
+import io.camunda.webapps.backup.Metadata;
+import io.camunda.webapps.backup.repository.SnapshotNameProvider;
+import io.camunda.webapps.schema.descriptors.backup.SnapshotIndexCollection;
+import io.camunda.zeebe.broker.system.configuration.ConfigurationUtil;
+import io.camunda.zeebe.qa.util.actuator.BackupActuator;
+import io.camunda.zeebe.qa.util.actuator.ExportingActuator;
+import io.camunda.zeebe.qa.util.cluster.TestRestoreApp;
+import io.camunda.zeebe.qa.util.cluster.TestStandaloneApplication;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration;
+import io.camunda.zeebe.qa.util.junit.ZeebeIntegration.TestZeebe;
+import io.camunda.zeebe.test.testcontainers.AzuriteContainer;
+import io.camunda.zeebe.test.util.testcontainers.ContainerLogsDumper;
+import io.camunda.zeebe.test.util.testcontainers.TestSearchContainers;
+import io.camunda.zeebe.util.VersionUtil;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
+import org.agrona.CloseHelper;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AutoClose;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+/**
+ * This test is on purpose no {@link io.camunda.qa.util.multidb.MultiDbTest}, as we have special
+ * requirements on ES and OS, and need direct access.
+ *
+ * <ul>
+ *   <li>We need to configure ES/OS to allow local repositories.
+ *   <li>We need to create snapshot repositories with specific ES/OS clients.
+ *   <li>We need to create snapshots of the Zeebe exporter indices.
+ * </ul>
+ */
+@Testcontainers
+@ZeebeIntegration
+public class TasklistBackupRestoreIT {
+  private static final Logger LOGGER = LoggerFactory.getLogger(TasklistBackupRestoreIT.class);
+  private static final String REPOSITORY_NAME = "tasklist-test-repository";
+  private static final String INDEX_PREFIX = "tasklist-backup-restore";
+  private static final long BACKUP_ID = 123L;
+  private static final Duration CLIENT_TIMEOUT = Duration.ofSeconds(5);
+  private static final SnapshotNameProvider SNAPSHOT_NAME_PROVIDER =
+      new ZeebeSnapshotNameProvider();
+
+  @Container private static final AzuriteContainer AZURITE_CONTAINER = new AzuriteContainer();
+  @AutoClose private static final Executor EXECUTOR = Executors.newSingleThreadExecutor();
+
+  @TestZeebe(autoStart = false)
+  protected TestStandaloneApplication<?> testStandaloneApplication;
+
+  @RegisterExtension
+  @SuppressWarnings("unused")
+  final ContainerLogsDumper logsWatcher =
+      new ContainerLogsDumper(() -> Map.of("azurite", AZURITE_CONTAINER));
+
+  private final String containerName =
+      RandomStringUtils.insecure().nextAlphabetic(10).toLowerCase(Locale.ROOT);
+
+  private CamundaClient camundaClient;
+  private ExportingActuator exportingActuator;
+  private BackupActuator backupActuator;
+  private DocumentClient webappsDbClient;
+  private GenericContainer<?> searchContainer;
+  private HistoryBackupClient historyBackupClient;
+  private MultiDbConfigurator configurator;
+  private WorkingDirectory workingDirectory;
+  private TasklistDataGenerator generator;
+
+  @BeforeAll
+  public static void beforeAll() {
+    VersionUtil.overrideVersionForTesting("8.10.0");
+  }
+
+  @AfterAll
+  public static void afterAll() {
+    VersionUtil.resetVersionForTesting();
+  }
+
+  @AfterEach
+  public void tearDown() {
+    CloseHelper.quietCloseAll(webappsDbClient, camundaClient, generator, searchContainer);
+  }
+
+  public static Stream<DatabaseType> sources() {
+    final String configuredDatabase = System.getProperty("camunda.it.backup.databaseType");
+    if (configuredDatabase != null && !configuredDatabase.isBlank()) {
+      return Stream.of(DatabaseType.valueOf(configuredDatabase.toUpperCase(Locale.ROOT)));
+    }
+    return Stream.of(DatabaseType.ELASTICSEARCH, DatabaseType.OPENSEARCH);
+  }
+
+  @ParameterizedTest
+  @MethodSource("sources")
+  void shouldRestoreTasklistHistoryToTheBackedUpState(final DatabaseType databaseType)
+      throws Exception {
+    // given
+    setup(databaseType);
+
+    testStandaloneApplication.awaitCompleteTopology();
+    generator.createData();
+    generator.assertData();
+
+    exportingActuator.softPause();
+    final List<String> historySnapshots = takeHistoryBackup();
+    takeZeebeBackup();
+    exportingActuator.resume();
+
+    generator.changeData();
+    generator.assertDataAfterChange();
+
+    // when
+    testStandaloneApplication.stop();
+    webappsDbClient.deleteAllIndices(INDEX_PREFIX);
+
+    Awaitility.await("tasklist indices should be deleted")
+        .untilAsserted(() -> assertThat(webappsDbClient.cat(INDEX_PREFIX)).isEmpty());
+
+    webappsDbClient.restore(REPOSITORY_NAME, historySnapshots);
+    restoreZeebe();
+
+    testStandaloneApplication.withBean(
+        "workingDirectory", workingDirectory, WorkingDirectory.class);
+    testStandaloneApplication.start();
+    refreshCamundaClient();
+
+    // then
+    generator.assertData();
+  }
+
+  private void setup(final DatabaseType databaseType) throws Exception {
+    testStandaloneApplication =
+        new TestCamundaApplication()
+            .withAuthenticationMethod(AuthenticationMethod.BASIC)
+            .withUnauthenticatedAccess();
+    configurator = new MultiDbConfigurator(testStandaloneApplication);
+    testStandaloneApplication.withUnifiedConfig(
+        cfg -> configureZeebeBackupStore(cfg, databaseType));
+
+    final String dbUrl;
+    searchContainer =
+        switch (databaseType) {
+          case ELASTICSEARCH -> {
+            final var container =
+                TestSearchContainers.createDefeaultElasticsearchContainer()
+                    .withStartupTimeout(Duration.ofMinutes(5))
+                    .withEnv("path.repo", "~/");
+            container.start();
+            dbUrl = "http://" + container.getHttpHostAddress();
+            configurator.configureElasticsearchSupportIncludingOldExporter(dbUrl, INDEX_PREFIX);
+            yield container;
+          }
+          case OPENSEARCH -> {
+            final var container =
+                TestSearchContainers.createDefaultOpensearchContainer()
+                    .withStartupTimeout(Duration.ofMinutes(5))
+                    .withEnv("path.repo", "~/");
+            container.start();
+            dbUrl = container.getHttpHostAddress();
+            configurator.configureOpenSearchSupportIncludingOldExporter(
+                dbUrl, INDEX_PREFIX, "admin", "admin");
+            yield container;
+          }
+          default ->
+              throw new IllegalArgumentException("Unsupported database type: " + databaseType);
+        };
+
+    testStandaloneApplication.start().awaitCompleteTopology();
+
+    refreshCamundaClient();
+    exportingActuator = ExportingActuator.of(testStandaloneApplication);
+    backupActuator = BackupActuator.of(testStandaloneApplication);
+    historyBackupClient = HistoryBackupClient.of(testStandaloneApplication);
+    webappsDbClient = DocumentClient.create(dbUrl, databaseType, EXECUTOR);
+    webappsDbClient.createRepository(REPOSITORY_NAME);
+    generator = new TasklistDataGenerator(camundaClient);
+    workingDirectory = testStandaloneApplication.bean(WorkingDirectory.class);
+  }
+
+  private void refreshCamundaClient() {
+    CloseHelper.quietClose(camundaClient);
+    camundaClient =
+        testStandaloneApplication.newClientBuilder().defaultRequestTimeout(CLIENT_TIMEOUT).build();
+    if (generator != null) {
+      generator.setCamundaClient(camundaClient);
+    }
+  }
+
+  private void configureZeebeBackupStore(final Camunda cfg, final DatabaseType databaseType) {
+    final var backup = cfg.getData().getPrimaryStorage().getBackup();
+    final var azure = backup.getAzure();
+
+    backup.setStore(BackupStoreType.AZURE);
+    azure.setBasePath(containerName);
+    azure.setConnectionString(AZURITE_CONTAINER.externalConnectionString());
+
+    if (databaseType.isElasticSearch()) {
+      cfg.getData()
+          .getSecondaryStorage()
+          .getElasticsearch()
+          .getBackup()
+          .setRepositoryName(REPOSITORY_NAME);
+    } else {
+      cfg.getData()
+          .getSecondaryStorage()
+          .getOpensearch()
+          .getBackup()
+          .setRepositoryName(REPOSITORY_NAME);
+    }
+  }
+
+  private void restoreZeebe() {
+    final var unifiedRestoreConfig = new Camunda();
+    final var backup = unifiedRestoreConfig.getData().getPrimaryStorage().getBackup();
+
+    backup.setStore(PrimaryStorageBackup.BackupStoreType.AZURE);
+    backup.getAzure().setBasePath(containerName);
+    backup.getAzure().setConnectionString(AZURITE_CONTAINER.externalConnectionString());
+
+    final var brokerCfg = testStandaloneApplication.unifiedConfig();
+    unifiedRestoreConfig.getCluster().setNodeId(brokerCfg.getCluster().getNodeId());
+    unifiedRestoreConfig.getCluster().setPartitionCount(brokerCfg.getCluster().getPartitionCount());
+    unifiedRestoreConfig
+        .getData()
+        .getPrimaryStorage()
+        .setDirectory(
+            ConfigurationUtil.toAbsolutePath(
+                brokerCfg.getData().getPrimaryStorage().getDirectory(),
+                workingDirectory.path().toString()));
+    unifiedRestoreConfig.getCluster().setSize(brokerCfg.getCluster().getSize());
+
+    try (final var restoreApp = new TestRestoreApp(unifiedRestoreConfig).withBackupId(BACKUP_ID)) {
+      assertThatNoException().isThrownBy(restoreApp::start);
+    }
+  }
+
+  private List<String> takeHistoryBackup() {
+    final var takeResponse = historyBackupClient.takeBackup(BACKUP_ID);
+    assertThat(takeResponse)
+        .extracting(TakeBackupHistoryResponse::getScheduledSnapshots)
+        .asInstanceOf(InstanceOfAssertFactories.LIST)
+        .isNotEmpty();
+    final List<String> snapshots = takeResponse.getScheduledSnapshots();
+
+    Awaitility.await("Tasklist backup completed")
+        .atMost(Duration.ofSeconds(60))
+        .untilAsserted(
+            () -> {
+              try {
+                final var backupResponse = historyBackupClient.getBackup(BACKUP_ID);
+                assertThat(backupResponse.getState()).isEqualTo(BackupStateDto.COMPLETED);
+                assertThat(backupResponse.getDetails())
+                    .allMatch(detail -> detail.getState().equals("SUCCESS"));
+              } catch (final NotFound e) {
+                throw new AssertionError("Backup not found", e);
+              }
+            });
+
+    for (final boolean verbose : List.of(true, false)) {
+      assertThat(historyBackupClient.getBackups(verbose, BACKUP_ID + "*"))
+          .allSatisfy(state -> assertThat(state.getState()).isEqualTo(BackupStateDto.COMPLETED));
+    }
+
+    return snapshots;
+  }
+
+  private void takeZeebeBackup() {
+    final var metadata = new Metadata(BACKUP_ID, "current", 1, 1);
+    final List<String> indices;
+    try {
+      indices = webappsDbClient.cat(configurator.zeebeIndexPrefix());
+    } catch (final IOException e) {
+      throw new RuntimeException(e);
+    }
+    assertThat(indices).isNotEmpty();
+
+    final var zeebeIndicesBackupStatus = new AtomicReference<String>();
+    final var backupRepository =
+        webappsDbClient.zeebeBackupRepository(REPOSITORY_NAME, SNAPSHOT_NAME_PROVIDER);
+
+    backupRepository.executeSnapshotting(
+        new SnapshotRequest(
+            REPOSITORY_NAME,
+            SNAPSHOT_NAME_PROVIDER.getSnapshotName(metadata),
+            new SnapshotIndexCollection(indices, List.of()),
+            metadata),
+        () -> {
+          zeebeIndicesBackupStatus.set("COMPLETED");
+          LOGGER.info("Backup of Zeebe secondary-storage records completed");
+        },
+        () -> {
+          zeebeIndicesBackupStatus.set("FAILURE");
+          LOGGER.error("Failed to take backup of Zeebe secondary-storage records");
+        });
+
+    Awaitility.await("Zeebe indices have been backed up")
+        .untilAsserted(() -> assertThat(zeebeIndicesBackupStatus.get()).isNotNull());
+    assertThat(zeebeIndicesBackupStatus.get()).isEqualTo("COMPLETED");
+
+    backupActuator.take(BACKUP_ID);
+    Awaitility.await("Zeebe backup completed")
+        .untilAsserted(
+            () -> {
+              try {
+                final var status = backupActuator.status(BACKUP_ID);
+                assertThat(status.getState()).isEqualTo(StateCode.COMPLETED);
+                assertThat(status.getDetails())
+                    .allSatisfy(
+                        detail -> assertThat(detail.getState()).isEqualTo(StateCode.COMPLETED));
+              } catch (final Exception e) {
+                throw new AssertionError("Backup not found", e);
+              }
+            });
+  }
+}
---
+++ qa/acceptance-tests/src/test/java/io/camunda/it/backup/TasklistDataGenerator.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.it.backup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.camunda.client.CamundaClient;
+import io.camunda.client.api.search.enums.UserTaskState;
+import io.camunda.client.api.search.filter.UserTaskFilter;
+import io.camunda.client.api.search.response.UserTask;
+import io.camunda.zeebe.model.bpmn.Bpmn;
+import io.camunda.zeebe.model.bpmn.BpmnModelInstance;
+import io.camunda.zeebe.model.bpmn.builder.AbstractUserTaskBuilder;
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import org.awaitility.Awaitility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final class TasklistDataGenerator implements AutoCloseable {
+
+  static final String PROCESS_BPMN_PROCESS_ID = "tasklist-backup-process";
+  static final String PROCESS_BPMN_PROCESS_ID_2 = "tasklist-backup-process-2";
+  static final int PROCESS_INSTANCE_COUNT = 49;
+  static final int COMPLETED_TASKS_COUNT = 11;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(TasklistDataGenerator.class);
+  private static final Duration DATA_TIMEOUT = Duration.ofSeconds(90);
+  private static final int SEARCH_LIMIT = 200;
+  private static final String ASSIGNEE = "demo";
+
+  private CamundaClient camundaClient;
+
+  TasklistDataGenerator(final CamundaClient camundaClient) {
+    this.camundaClient = camundaClient;
+  }
+
+  void setCamundaClient(final CamundaClient camundaClient) {
+    this.camundaClient = camundaClient;
+  }
+
+  void createData() {
+    LOGGER.info("Starting Tasklist backup test data generation");
+
+    deployProcess(createModel1(), PROCESS_BPMN_PROCESS_ID);
+    startProcessInstances(PROCESS_BPMN_PROCESS_ID);
+
+    waitForTaskCount();
+    assignAllTasks();
+  }
+
+  void assertData() {
+    Awaitility.await("should expose the backed up Tasklist data")
+        .atMost(DATA_TIMEOUT)
+        .ignoreExceptions()
+        .untilAsserted(this::assertDataOneAttempt);
+  }
+
+  void changeData() {
+    LOGGER.info("Changing Tasklist backup test data");
+
+    completeTasks();
+    deployProcess(createModel2(), PROCESS_BPMN_PROCESS_ID_2);
+    startProcessInstances(PROCESS_BPMN_PROCESS_ID_2);
+  }
+
+  void assertDataAfterChange() {
+    Awaitility.await("should expose the changed Tasklist data")
+        .atMost(DATA_TIMEOUT)
+        .ignoreExceptions()
+        .untilAsserted(this::assertDataAfterChangeOneAttempt);
+  }
+
+  @Override
+  public void close() {
+    camundaClient = null;
+  }
+
+  private void assertDataOneAttempt() {
+    final var originalTasks =
+        searchUserTasks(
+            filter ->
+                filter
+                    .bpmnProcessId(PROCESS_BPMN_PROCESS_ID)
+                    .elementId("task1")
+                    .assignee(ASSIGNEE));
+
+    assertThat(originalTasks).hasSize(PROCESS_INSTANCE_COUNT);
+    assertThat(originalTasks).extracting(UserTask::getElementId).containsOnly("task1");
+    assertThat(originalTasks).extracting(UserTask::getState).containsOnly(UserTaskState.CREATED);
+    assertThat(
+            searchUserTasks(
+                filter -> filter.bpmnProcessId(PROCESS_BPMN_PROCESS_ID_2).elementId("task2")))
+        .isEmpty();
+  }
+
+  private void assertDataAfterChangeOneAttempt() {
+    final var task1Tasks =
+        searchUserTasks(filter -> filter.bpmnProcessId(PROCESS_BPMN_PROCESS_ID).elementId("task1"));
+    assertThat(task1Tasks).hasSize(PROCESS_INSTANCE_COUNT);
+    assertThat(task1Tasks)
+        .filteredOn(task -> UserTaskState.COMPLETED.equals(task.getState()))
+        .hasSize(COMPLETED_TASKS_COUNT);
+    assertThat(task1Tasks)
+        .filteredOn(task -> UserTaskState.CREATED.equals(task.getState()))
+        .hasSize(PROCESS_INSTANCE_COUNT - COMPLETED_TASKS_COUNT);
+
+    final var task2Tasks =
+        searchUserTasks(
+            filter ->
+                filter
+                    .bpmnProcessId(PROCESS_BPMN_PROCESS_ID_2)
+                    .elementId("task2")
+                    .state(UserTaskState.CREATED));
+    assertThat(task2Tasks).hasSize(PROCESS_INSTANCE_COUNT);
+  }
+
+  private void assignAllTasks() {
+    final var tasks =
+        searchUserTasks(filter -> filter.bpmnProcessId(PROCESS_BPMN_PROCESS_ID).elementId("task1"));
+    assertThat(tasks).hasSize(PROCESS_INSTANCE_COUNT);
+
+    tasks.forEach(
+        task ->
+            camundaClient
+                .newAssignUserTaskCommand(task.getUserTaskKey())
+                .assignee(ASSIGNEE)
+                .send()
+                .join());
+  }
+
+  private void completeTasks() {
+    final var tasks =
+        searchUserTasks(
+            filter ->
+                filter
+                    .bpmnProcessId(PROCESS_BPMN_PROCESS_ID)
+                    .elementId("task1")
+                    .state(UserTaskState.CREATED));
+    assertThat(tasks).hasSize(PROCESS_INSTANCE_COUNT);
+
+    tasks.stream()
+        .limit(COMPLETED_TASKS_COUNT)
+        .forEach(
+            task ->
+                camundaClient
+                    .newCompleteUserTaskCommand(task.getUserTaskKey())
+                    .variables(Map.of("varOut", 123))
+                    .send()
+                    .join());
+  }
+
+  private void waitForTaskCount() {
+    Awaitility.await("should import all generated Tasklist user tasks")
+        .atMost(DATA_TIMEOUT)
+        .ignoreExceptions()
+        .untilAsserted(
+            () ->
+                assertThat(
+                        searchUserTasks(
+                            filter ->
+                                filter.bpmnProcessId(PROCESS_BPMN_PROCESS_ID).elementId("task1")))
+                    .hasSize(PROCESS_INSTANCE_COUNT));
+  }
+
+  private List<UserTask> searchUserTasks(final Consumer<UserTaskFilter> filter) {
+    return camundaClient
+        .newUserTaskSearchRequest()
+        .filter(filter)
+        .page(page -> page.limit(SEARCH_LIMIT).from(0))
+        .send()
+        .join()
+        .items();
+  }
+
+  private void startProcessInstances(final String bpmnProcessId) {
+    for (int i = 0; i < PROCESS_INSTANCE_COUNT; i++) {
+      final long processInstanceKey =
+          camundaClient
+              .newCreateInstanceCommand()
+              .bpmnProcessId(bpmnProcessId)
+              .latestVersion()
+              .variables(Map.of("var1", "value1"))
+              .send()
+              .join()
+              .getProcessInstanceKey();
+      LOGGER.debug("Started process instance {} for process {}", processInstanceKey, bpmnProcessId);
+    }
+  }
+
+  private void deployProcess(
+      final BpmnModelInstance bpmnModelInstance, final String bpmnProcessId) {
+    final var deploymentEvent =
+        camundaClient
+            .newDeployResourceCommand()
+            .addProcessModel(bpmnModelInstance, bpmnProcessId + ".bpmn")
+            .send()
+            .join();
+    LOGGER.info("Deployed process {} with key {}", bpmnProcessId, deploymentEvent.getKey());
+  }
+
+  private BpmnModelInstance createModel1() {
+    return Bpmn.createExecutableProcess(PROCESS_BPMN_PROCESS_ID)
+        .startEvent("start")
+        .userTask(
+            "task1",
+            task ->
+                task.zeebeUserTask().zeebeInput("=var1", "varIn").zeebeOutput("=varOut", "var2"))
+        .serviceTask("task5")
+        .zeebeJobType("task5")
+        .endEvent()
+        .done();
+  }
+
+  private BpmnModelInstance createModel2() {
+    return Bpmn.createExecutableProcess(PROCESS_BPMN_PROCESS_ID_2)
+        .startEvent("start")
+        .userTask("task2", AbstractUserTaskBuilder::zeebeUserTask)
+        .endEvent()
+        .done();
+  }
+}
