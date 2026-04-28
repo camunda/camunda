49b1b0b0a43892f798d6b98fca6cc809f9f8d731
Mathias Vandaele <mathias.vandaele@camunda.com>
2026-04-28 21:18:12 +0800

    feat: Modify Service layer for processInstanceKey and elementInstanceKey management

--- service/src/main/java/io/camunda/service/ExpressionServices.java
+++ service/src/main/java/io/camunda/service/ExpressionServices.java
@@ -36,10 +36,16 @@ public final class ExpressionServices extends ApiServices<ExpressionServices> {
         new BrokerExpressionEvaluationRequest()
             .setExpression(request.expression())
             .setVariables(request.variables())
+            .setProcessInstanceKey(request.processInstanceKey())
+            .setElementInstanceKey(request.elementInstanceKey())
             .setTenantId(request.tenantId()),
         authentication);
   }
 
   public record ExpressionEvaluationRequest(
-      String expression, String tenantId, Map<String, Object> variables) {}
+      String expression,
+      String tenantId,
+      Long processInstanceKey,
+      Long elementInstanceKey,
+      Map<String, Object> variables) {}
 }
