7ad830686f4222c235ec193d2c2201f15a99ccec
arienzIT <lucarienti@gmail.com>
2026-04-29 10:21:51 +0200

    fix: build

--- optimize/client/src/modules/components/index.js
+++ optimize/client/src/modules/components/index.js
@@ -53,7 +53,6 @@ export {InstanceViewTable} from './InstanceViewTable';
 export {TenantInfo} from './TenantInfo';
 export {TargetSelection} from './TargetSelection';
 export {Loading} from './Loading';
-export {Breadcrumbs} from './Breadcrumbs';
 export {MenuDropdown} from './MenuDropdown';
 export {MenuButton} from './MenuButton';
 export {MultiValueInput} from './MultiValueInput';
