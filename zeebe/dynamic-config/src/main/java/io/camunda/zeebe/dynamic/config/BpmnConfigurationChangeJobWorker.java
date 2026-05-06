36e7074156159e777f713ab1963ed32abc30fe35
Deepthi Devaki Akkoorath <deepthidevaki@gmail.com>
2026-05-06 11:37:09 +0200

    feat: member join and partition reconfigure operations

--- zeebe/dynamic-config/src/main/java/io/camunda/zeebe/dynamic/config/BpmnConfigurationChangeJobWorker.java
+++ zeebe/dynamic-config/src/main/java/io/camunda/zeebe/dynamic/config/BpmnConfigurationChangeJobWorker.java
@@ -15,8 +15,12 @@ import io.camunda.client.api.worker.JobWorker;
 import io.camunda.zeebe.dynamic.config.changes.ConfigurationChangeAppliers;
 import io.camunda.zeebe.dynamic.config.state.ClusterConfiguration;
 import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation;
+import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation.MemberJoinOperation;
+import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation.MemberLeaveOperation;
+import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation.MemberRemoveOperation;
 import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation.PartitionChangeOperation.PartitionJoinOperation;
 import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation.PartitionChangeOperation.PartitionLeaveOperation;
+import io.camunda.zeebe.dynamic.config.state.ClusterConfigurationChangeOperation.PartitionChangeOperation.PartitionReconfigurePriorityOperation;
 import java.util.Map;
 
 public final class BpmnConfigurationChangeJobWorker implements AutoCloseable {
@@ -87,15 +91,27 @@ public final class BpmnConfigurationChangeJobWorker implements AutoCloseable {
 
   private ClusterConfigurationChangeOperation deserializeOperation(final Map<String, Object> vars) {
     final var type = (String) vars.get("operationType");
-    final int partitionId = ((Number) vars.get("partitionId")).intValue();
     return switch (type) {
       case "partition-join" -> {
+        final int partitionId = ((Number) vars.get("partitionId")).intValue();
         final int priority = ((Number) vars.get("priority")).intValue();
         yield new PartitionJoinOperation(memberId, partitionId, priority);
       }
       case "partition-leave" -> {
+        final int partitionId = ((Number) vars.get("partitionId")).intValue();
         final int minReplicas = ((Number) vars.get("minimumAllowedReplicas")).intValue();
         yield new PartitionLeaveOperation(memberId, partitionId, minReplicas);
+      }
+      case "partition-reconfigure-priority" -> {
+        final int partitionId = ((Number) vars.get("partitionId")).intValue();
+        final int priority = ((Number) vars.get("priority")).intValue();
+        yield new PartitionReconfigurePriorityOperation(memberId, partitionId, priority);
+      }
+      case "member-join" -> new MemberJoinOperation(memberId);
+      case "member-leave" -> new MemberLeaveOperation(memberId);
+      case "member-remove" -> {
+        final var memberToRemove = MemberId.from((String) vars.get("memberToRemove"));
+        yield new MemberRemoveOperation(memberId, memberToRemove);
       }
       default -> throw new IllegalArgumentException("Unknown operationType: " + type);
     };
