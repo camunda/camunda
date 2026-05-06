aaebfd7dcbc2a2f897fde2295e1656cd70da2238
John Montgomery <john.montgomery@camunda.com>
2026-05-06 09:54:55 +0100

    perf: approximate ordinal rollover
    so we can see how things might behave as new ordinals are created

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/store/FakeOrdinalIndexLocatorProvider.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/store/FakeOrdinalIndexLocatorProvider.java
@@ -8,13 +8,24 @@
 package io.camunda.exporter.store;
 
 import io.camunda.webapps.schema.entities.ExporterEntity;
+import io.camunda.zeebe.protocol.Protocol;
 import io.camunda.zeebe.protocol.record.Record;
 import io.camunda.zeebe.protocol.record.value.AuditLogProcessInstanceRelated;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FakeOrdinalIndexLocatorProvider implements IndexLocatorProvider {
+  private static final Logger LOGGER =
+      LoggerFactory.getLogger(FakeOrdinalIndexLocatorProvider.class);
+  private static final long APPROX_IDS_BETWEEN_ROOT_PROCESS_INSTANCES = 20;
+  // aim for roughly 1 hours worth at 300 PI/s (across 3 partitions)
+  private static final long IDS_PER_ORDINAL =
+      100 * 60 * 60 * APPROX_IDS_BETWEEN_ROOT_PROCESS_INSTANCES;
   private final Set<String> ordinalBasedIndexes;
   private final NoopIndexLocator noopIndexLocator = new NoopIndexLocator();
+  private final Set<Integer> ordinals = ConcurrentHashMap.newKeySet();
 
   public FakeOrdinalIndexLocatorProvider(final Set<String> ordinalBasedIndexes) {
     this.ordinalBasedIndexes = ordinalBasedIndexes;
@@ -25,7 +36,12 @@ public class FakeOrdinalIndexLocatorProvider implements IndexLocatorProvider {
     if (record.getValue() instanceof final AuditLogProcessInstanceRelated processInstanceRelated) {
       final long rootProcessInstanceKey = processInstanceRelated.getRootProcessInstanceKey();
       if (rootProcessInstanceKey > 0) {
-        return new FixedOrdinalIndexLocator((int) (rootProcessInstanceKey % 2));
+        final long key = Protocol.decodeKeyInPartition(rootProcessInstanceKey);
+        final int ordinal = (int) (key / IDS_PER_ORDINAL);
+        if (ordinals.add(ordinal)) {
+          LOGGER.info("New ordinal started: {} ({} ordinals total)", ordinal, ordinals.size());
+        }
+        return new FixedOrdinalIndexLocator(ordinal);
       }
     }
     return noopIndexLocator;
