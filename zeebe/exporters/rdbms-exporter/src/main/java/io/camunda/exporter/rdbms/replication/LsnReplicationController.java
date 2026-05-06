55047c135506aeafac14c37f39466cb424735f92
Christian Thiel <christian.thiel@holisticon.de>
2026-05-06 11:20:02 +0200

    feat: add LsnReplicationController implementation

---
+++ zeebe/exporters/rdbms-exporter/src/main/java/io/camunda/exporter/rdbms/replication/LsnReplicationController.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.rdbms.replication;
+
+import io.camunda.db.rdbms.read.replication.ReplicationLogStatus;
+import io.camunda.db.rdbms.read.replication.ReplicationLogStatusProvider;
+import io.camunda.exporter.rdbms.ExporterConfiguration.ReplicationConfiguration;
+import io.camunda.zeebe.exporter.api.context.Controller;
+import io.camunda.zeebe.exporter.api.context.ScheduledTask;
+import io.camunda.zeebe.util.VisibleForTesting;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.InstantSource;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LsnReplicationController implements ReplicationController {
+  public static final int DEFAULT_QUEUE_CAPACITY = 8192;
+
+  private static final Logger LOG = LoggerFactory.getLogger(LsnReplicationController.class);
+
+  private final ReplicationLogStatusProvider lsnProvider;
+  private final Controller controller;
+  private final ReplicationConfiguration replicationConfiguration;
+  private final int partitionId;
+  private final InstantSource clock;
+
+  private final BlockingQueue<LsnPositionEntry> pendingEntries;
+  private final AtomicLong flushedPosition = new AtomicLong(-1);
+  private final AtomicLong replicatedPosition = new AtomicLong(-1);
+  private final AtomicReference<Instant> lastConfirmedReplication;
+  private final AtomicBoolean paused = new AtomicBoolean(false);
+
+  private final ScheduledTask replicationCheckTask;
+  private final int minSyncReplicas;
+  private final Duration maxLag;
+  private final boolean pauseOnMaxLagExceeded;
+
+  public LsnReplicationController(
+      final Controller controller,
+      final ReplicationLogStatusProvider lsnProvider,
+      final ReplicationConfiguration replicationConfiguration,
+      final int partitionId,
+      final InstantSource clock) {
+    this.lsnProvider = lsnProvider;
+    this.controller = controller;
+    this.replicationConfiguration = replicationConfiguration;
+    this.partitionId = partitionId;
+    this.clock = clock;
+
+    pendingEntries = new ArrayBlockingQueue<>(DEFAULT_QUEUE_CAPACITY);
+    replicationCheckTask =
+        controller.scheduleCancellableTask(
+            replicationConfiguration.getPollingInterval(), this::checkReplication);
+    minSyncReplicas = this.replicationConfiguration.getMinSyncReplicas();
+    maxLag = this.replicationConfiguration.getMaxLag();
+    pauseOnMaxLagExceeded = this.replicationConfiguration.isPauseOnMaxLagExceeded();
+
+    lastConfirmedReplication = new AtomicReference<>(this.clock.instant());
+  }
+
+  @Override
+  public void onFlush(final long exporterPosition) {
+    flushedPosition.set(exporterPosition);
+    final long currentLsn = lsnProvider.getCurrent();
+    final long now = clock.millis();
+    if (!pendingEntries.offer(new LsnPositionEntry(exporterPosition, currentLsn, now))) {
+      // it's fine to just drop. When we are able to compact, when we export a new entry it will be
+      // added. If the queue is not full for too much it's not a problem, otherwise we will just hit
+      // the max_lag bound.
+      LOG.warn(
+          "[RDBMS Exporter P{}] Replication queue is full, dropping LSN entry (lsn={}, position={})",
+          partitionId,
+          currentLsn,
+          exporterPosition);
+    }
+  }
+
+  @Override
+  public boolean isReplicationInSync() {
+    return !paused.get();
+  }
+
+  @VisibleForTesting
+  void checkReplication() {
+    try {
+      final var statuses = lsnProvider.getReplicationStatuses();
+      final int connectedReplicas = statuses.size();
+
+      final long confirmedLsn = computeConfirmedLsn(statuses);
+      final var confirmedEntry = removeConfirmedLsnEntries(confirmedLsn);
+      final Duration dbReportedLag = getCurrentDbLag();
+      final boolean dbLagExceeded = isMaxLagExceeded(dbReportedLag);
+
+      updatePausedState(pauseOnMaxLagExceeded && dbLagExceeded, dbReportedLag, connectedReplicas);
+
+      if (confirmedEntry != null) {
+        LOG.info(
+            "[RDBMS Exporter P{}] Updating replicated position to {}",
+            partitionId,
+            replicatedPosition.get());
+        controller.updateLastExportedRecordPosition(replicatedPosition.get());
+      }
+    } catch (final Exception e) {
+      LOG.error(
+          "[RDBMS Exporter P{}] Error while checking replication status, will retry after {}",
+          partitionId,
+          replicationConfiguration.getPollingInterval(),
+          e);
+    } finally {
+      controller.scheduleCancellableTask(
+          replicationConfiguration.getPollingInterval(), this::checkReplication);
+    }
+  }
+
+  @VisibleForTesting
+  Duration getCurrentDbLag() {
+    if (pendingEntries.isEmpty()) {
+      return Duration.ZERO;
+    }
+    return Duration.ofMillis(clock.millis() - pendingEntries.peek().enqueueTimeMs);
+  }
+
+  @VisibleForTesting
+  boolean isMaxLagExceeded(final Duration dbLag) {
+    return dbLag.compareTo(maxLag) > 0;
+  }
+
+  /**
+   * Removes entries from the queue which have been confirmed based on the confirmedLsn.
+   *
+   * @param confirmedLsn the confirmed LSN based on the database
+   * @return the highest confirmed entry
+   */
+  @VisibleForTesting
+  LsnPositionEntry removeConfirmedLsnEntries(final long confirmedLsn) {
+    LsnPositionEntry lastConfirmedEntry = null;
+    LsnPositionEntry entry;
+    long newReplicatedPosition = replicatedPosition.get();
+    while ((entry = pendingEntries.peek()) != null) {
+      if (entry.lsn() > confirmedLsn) {
+        break;
+      }
+
+      lastConfirmedReplication.set(Instant.ofEpochMilli(entry.enqueueTimeMs()));
+      newReplicatedPosition = entry.position();
+      lastConfirmedEntry = pendingEntries.poll();
+    }
+    if (newReplicatedPosition > replicatedPosition.get()) {
+      replicatedPosition.set(newReplicatedPosition);
+      return lastConfirmedEntry;
+    }
+
+    return null;
+  }
+
+  private void updatePausedState(
+      final boolean shouldPause, final Duration dbReportedLag, final int connectedReplicas) {
+    final boolean wasPaused = paused.getAndSet(shouldPause);
+    if (shouldPause && !wasPaused) {
+      LOG.warn(
+          "[RDBMS Exporter P{}] Pausing exporter: DB-reported replication lag ({}) exceeded maxLag ({})",
+          partitionId,
+          dbReportedLag,
+          maxLag);
+    } else if (!shouldPause && wasPaused) {
+      LOG.info(
+          "[RDBMS Exporter P{}] Resuming exporter: replication quorum met ({}/{} replicas) and DB-reported lag ({}) within maxLag ({})",
+          partitionId,
+          connectedReplicas,
+          minSyncReplicas,
+          dbReportedLag,
+          maxLag);
+    }
+  }
+
+  @VisibleForTesting
+  long computeConfirmedLsn(final List<ReplicationLogStatus> statuses) {
+    if (lsnProvider.getCurrent() < 0) {
+      return Long.MIN_VALUE;
+    }
+
+    if (minSyncReplicas == 0) {
+      return Long.MAX_VALUE;
+    }
+
+    if (statuses.size() < minSyncReplicas) {
+      return -1;
+    }
+
+    return statuses.stream()
+        .map(ReplicationLogStatus::logStatus)
+        .sorted(Comparator.<Long>naturalOrder().reversed())
+        .limit(minSyncReplicas)
+        .min(Comparator.naturalOrder())
+        .orElse(-1L);
+  }
+
+  @Override
+  public void close() throws Exception {
+    replicationCheckTask.cancel();
+  }
+
+  record LsnPositionEntry(long position, long lsn, long enqueueTimeMs) {}
+}
