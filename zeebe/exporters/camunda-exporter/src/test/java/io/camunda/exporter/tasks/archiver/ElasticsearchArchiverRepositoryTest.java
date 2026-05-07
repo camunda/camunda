e196f457f54b023b64d60fa36278ce39b59f0719
John Montgomery <john.montgomery@camunda.com>
2026-05-07 15:59:43 +0100

    test: verify we reindex, then delete when archiving documents
    (cherry picked from commit f336b2062fb57508d10c9db7433902193688cac2)

--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/tasks/archiver/ElasticsearchArchiverRepositoryTest.java
@@ -7,11 +7,18 @@
  */
 package io.camunda.exporter.tasks.archiver;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
+import co.elastic.clients.elasticsearch.core.DeleteByQueryRequest;
+import co.elastic.clients.elasticsearch.core.DeleteByQueryResponse;
+import co.elastic.clients.elasticsearch.core.ReindexRequest;
+import co.elastic.clients.elasticsearch.core.ReindexResponse;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
 import co.elastic.clients.elasticsearch.core.search.HitsMetadata;
@@ -22,6 +29,7 @@ import io.camunda.exporter.metrics.CamundaExporterMetrics;
 import io.camunda.exporter.tasks.utils.TestExporterResourceProvider;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import java.net.ConnectException;
+import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
@@ -84,6 +92,49 @@ final class ElasticsearchArchiverRepositoryTest extends AbstractArchiverReposito
 
     // then
     Mockito.verify(client, Mockito.times(1)).close();
+  }
+
+  @Test
+  public void shouldNotDeleteWhenMovingIfReindexingFails() {
+    // given
+    when(client.reindex(any(ReindexRequest.class)))
+        .thenReturn(CompletableFuture.failedFuture(new RuntimeException("Error reindexing")));
+
+    // when
+    final var future =
+        repository.moveDocuments(
+            "from-index", "to-index", "key", List.of("1", "2", "3"), Runnable::run);
+
+    // then
+    assertThat(future)
+        .failsWithin(Duration.ofSeconds(5))
+        .withThrowableThat()
+        .withMessageContaining("Error reindexing");
+
+    verify(client).reindex(any(ReindexRequest.class));
+    verifyNoMoreInteractions(client);
+  }
+
+  @Test
+  public void shouldReindexThenDeleteWhenMovingDocuments() {
+    // given
+    when(client.reindex(any(ReindexRequest.class)))
+        .thenReturn(CompletableFuture.completedFuture(ReindexResponse.of(b -> b.total(10L))));
+    when(client.deleteByQuery(any(DeleteByQueryRequest.class)))
+        .thenReturn(CompletableFuture.completedFuture(DeleteByQueryResponse.of(b -> b.total(10L))));
+
+    // when
+    final var future =
+        repository.moveDocuments(
+            "from-index", "to-index", "key", List.of("1", "2", "3"), Runnable::run);
+
+    // then
+    assertThat(future).succeedsWithin(Duration.ofSeconds(5));
+
+    final var inOrder = Mockito.inOrder(client);
+    inOrder.verify(client).reindex(any(ReindexRequest.class));
+    inOrder.verify(client).deleteByQuery(any(DeleteByQueryRequest.class));
+    inOrder.verifyNoMoreInteractions();
   }
 
   private void givenNoArchivingStatus() {
