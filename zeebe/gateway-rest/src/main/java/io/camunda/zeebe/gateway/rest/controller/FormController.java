0e3b91f6938b2aaf141bf4bc4e0dbf68be7e22c6
remco.westerhoud <remcowesterhoud@msn.com>
2026-05-08 10:12:35 +0200

    fix: don't support v1 for forms controller

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/FormController.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/controller/FormController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @CamundaRestController
-@RequestMapping(path = {"/v1/forms", "/v2/forms"})
+@RequestMapping("/v2/forms")
 public class FormController {
 
   private final FormServices formServices;
