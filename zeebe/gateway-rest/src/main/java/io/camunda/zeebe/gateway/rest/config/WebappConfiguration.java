6a9ab3be7f044debd16cdd75591d8214fddd19ee
Eddie Tsedeke <6949627+tsedekey@users.noreply.github.com>
2026-05-11 20:22:29 +0300

    refactor: replace Environment.getProperty() in SystemController with @ConfigurationProperties

---
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/config/WebappConfiguration.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.zeebe.gateway.rest.config;
+
+public class WebappConfiguration {
+
+  private boolean enterprise = false;
+  private boolean loginDelegated = false;
+  private Cloud cloud = new Cloud();
+
+  public boolean isEnterprise() {
+    return enterprise;
+  }
+
+  public void setEnterprise(final boolean enterprise) {
+    this.enterprise = enterprise;
+  }
+
+  public boolean isLoginDelegated() {
+    return loginDelegated;
+  }
+
+  public void setLoginDelegated(final boolean loginDelegated) {
+    this.loginDelegated = loginDelegated;
+  }
+
+  public Cloud getCloud() {
+    return cloud;
+  }
+
+  public void setCloud(final Cloud cloud) {
+    this.cloud = cloud;
+  }
+
+  public static class Cloud {
+    private String stage;
+    private String mixpanelToken;
+    private String mixpanelApiHost;
+
+    public String getStage() {
+      return stage;
+    }
+
+    public void setStage(final String stage) {
+      this.stage = stage;
+    }
+
+    public String getMixpanelToken() {
+      return mixpanelToken;
+    }
+
+    public void setMixpanelToken(final String mixpanelToken) {
+      this.mixpanelToken = mixpanelToken;
+    }
+
+    public String getMixpanelApiHost() {
+      return mixpanelApiHost;
+    }
+
+    public void setMixpanelApiHost(final String mixpanelApiHost) {
+      this.mixpanelApiHost = mixpanelApiHost;
+    }
+  }
+}
--- zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/system/SystemControllerTestConfiguration.java
+++ zeebe/gateway-rest/src/test/java/io/camunda/zeebe/gateway/rest/controller/system/SystemControllerTestConfiguration.java
@@ -7,32 +7,29 @@
  */
 package io.camunda.zeebe.gateway.rest.controller.system;
 
+import io.camunda.zeebe.gateway.rest.config.WebappConfiguration;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.mock.env.MockEnvironment;
 
-/**
- * Test configuration that provides proper Environment bean for SystemController tests. This is
- * needed because WebMvcTest doesn't load the full application context including application.yml
- * properties.
- */
 @TestConfiguration
 public class SystemControllerTestConfiguration {
+
+  @Bean
+  @Primary
+  public WebappConfiguration testWebappConfiguration() {
+    return new WebappConfiguration();
+  }
 
   @Bean
   @Primary
   public Environment testEnvironment() {
     final MockEnvironment env = new MockEnvironment();
 
-    // Default values for SystemController constructor @Value parameters
-    env.setProperty("camunda.webapp.enterprise", "false");
-    env.setProperty("camunda.webapps.login-delegated", "false");
     env.setProperty("spring.servlet.multipart.max-request-size", "4MB");
-    // Don't set cloud properties - let them default to null
 
-    // Default component configuration
     env.setProperty("camunda.admin.webapp-enabled", "true");
     env.setProperty("camunda.webapps.admin.enabled", "true");
     env.setProperty("camunda.webapps.admin.ui-enabled", "true");
