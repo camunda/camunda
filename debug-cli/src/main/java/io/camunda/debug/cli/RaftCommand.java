2aad08e6354f7bf5361fb6fc2a7aece22638ee1f
Carlo Sana <carlo.sana@camunda.com>
2026-05-11 17:18:58 +0200

    feat: add generic sbe decode command to debugcli
    (cherry picked from commit 0581627248af46fa1803d6e1fb4dc6d9bce6856f)

--- debug-cli/src/main/java/io/camunda/debug/cli/RaftCommand.java
+++ debug-cli/src/main/java/io/camunda/debug/cli/RaftCommand.java
@@ -8,17 +8,13 @@
 package io.camunda.debug.cli;
 
 import io.atomix.raft.storage.system.MetaStore;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import io.camunda.debug.cli.sbe.SbeJsonDecoder;
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
-import org.agrona.concurrent.UnsafeBuffer;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Spec;
-import uk.co.real_logic.sbe.ir.IrDecoder;
-import uk.co.real_logic.sbe.json.JsonPrinter;
 
 @Command(name = "raft", description = "Print raft metadata files")
 public class RaftCommand extends CommonOptions implements Callable<Integer> {
@@ -52,52 +48,14 @@ public class RaftCommand extends CommonOptions implements Callable<Integer> {
       spec.commandLine().getErr().println("Reading file: " + file + "\n");
     }
 
-    final long fileSize;
-    try (final var fc = FileChannel.open(file, java.nio.file.StandardOpenOption.READ)) {
-      fileSize = fc.size();
-      if (fileSize > Integer.MAX_VALUE) {
-        throw new IllegalArgumentException("File is too large to process: " + fileSize + " bytes");
-      }
-      final var metadataContentBuffer =
-          ByteBuffer.allocate((int) fileSize).order(MetaStore.ENDIANESS);
-      final int bytesRead = fc.read(metadataContentBuffer, 0);
-      if (bytesRead != fileSize) {
-        throw new RuntimeException(
-            "Failed to read the entire file. Expected "
-                + fileSize
-                + " bytes, but read "
-                + bytesRead
-                + " bytes.");
-      }
-      metadataContentBuffer.flip();
-
-      final var version = metadataContentBuffer.get();
-      if (version != MetaStore.VERSION) {
-        throw new RuntimeException("Invalid version encoded in the metadata file: " + version);
-      }
-      final UnsafeBuffer metadataWithoutVersion = new UnsafeBuffer();
-      metadataWithoutVersion.wrap(
-          metadataContentBuffer,
-          MetaStore.VERSION_LENGTH,
-          metadataContentBuffer.capacity() - MetaStore.VERSION_LENGTH);
-
-      final byte[] irBytes;
-      try (final var irFileResource =
-          getClass().getClassLoader().getResourceAsStream("sbe/raft-entry-schema.sbeir")) {
-        if (irFileResource == null) {
-          throw new RuntimeException("Failed to get resource raft-entry-schema.sbeir");
-        }
-
-        irBytes = irFileResource.readAllBytes();
-      }
-
-      try (final IrDecoder irDecoder = new IrDecoder(ByteBuffer.wrap(irBytes))) {
-        final var ir = irDecoder.decode();
-        final StringBuilder output = new StringBuilder();
-        new JsonPrinter(ir).print(output, metadataWithoutVersion, 0);
-        return output.toString();
-      }
+    final var buffer = SbeJsonDecoder.readFile(file);
+    final var version = buffer.getByte(0);
+    if (version != MetaStore.VERSION) {
+      throw new RuntimeException("Invalid version encoded in the metadata file: " + version);
     }
+
+    final var ir = SbeJsonDecoder.loadIrFromResource(getClass(), "sbe/raft-entry-schema.sbeir");
+    return SbeJsonDecoder.toJson(buffer, ir, MetaStore.VERSION_LENGTH);
   }
 
   @Override
