7e3f7dd320d23855ab4a3c3f008c5cae1bbe4714
Jonathan Ballet <jonathan.ballet@camunda.com>
2026-05-12 17:44:49 +0200

    feat: internal metrics exporter for load tests
    This exports internal metrics computed from Elasticsearch and not
    exposed (yet) by Operate/Optimize to help investigating how our systems
    are working.

---
+++ load-tests/metrics-exporter/internal/common/metrics.go
@@ -0,0 +1,6 @@
+// Package common holds shared constants used across all collector modules.
+package common
+
+// Namespace is the top-level Prometheus namespace for all metrics exposed by
+// this exporter.
+const Namespace = "camunda_exporter"
---
+++ load-tests/metrics-exporter/internal/operate/operate.go
@@ -0,0 +1,66 @@
+package operate
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
+
+	"github.com/camunda/camunda/load-tests/metrics-exporter/internal/common"
+	"github.com/camunda/camunda/load-tests/metrics-exporter/internal/esutil"
+	"github.com/camunda/camunda/load-tests/metrics-exporter/internal/utils"
+)
+
+const subsystem = "operate"
+
+// Collector gathers metrics for Camunda Operate from Elasticsearch.
+type Collector struct {
+	esClient *esutil.Client
+	logger   *zap.Logger
+
+	rootProcessInstances          prometheus.Gauge
+	rootProcessInstancesCompleted prometheus.Gauge
+}
+
+func New(esClient *esutil.Client, logger *zap.Logger, reg prometheus.Registerer) *Collector {
+	c := &Collector{
+		esClient: esClient,
+		logger:   logger,
+		rootProcessInstances: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: common.Namespace,
+			Subsystem: subsystem,
+			Name:      "root_process_instances",
+			Help:      "Number of Operate root process instance documents.",
+		}),
+		rootProcessInstancesCompleted: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: common.Namespace,
+			Subsystem: subsystem,
+			Name:      "root_process_instances_completed",
+			Help:      "Number of completed Operate root process instance documents.",
+		}),
+	}
+	reg.MustRegister(c.rootProcessInstances, c.rootProcessInstancesCompleted)
+	return c
+}
+
+func (c *Collector) Name() string { return "operate" }
+
+func (c *Collector) Collect(ctx context.Context) error {
+	const (
+		index = "operate-list-view-*"
+		// The query to match all interesting documents.
+		query = `{"term": {"joinRelation": "processInstance"}}`
+	)
+
+	c.logger.Debug("Collecting Operate root process instances")
+	completed, err := utils.CountCompleted(ctx, c.esClient, index, query)
+	if err != nil {
+		return fmt.Errorf("error while collecting Operate root process instances: %w", err)
+	}
+
+	c.rootProcessInstancesCompleted.Set(float64(completed.Completed))
+	c.rootProcessInstances.Set(float64(completed.Total))
+
+	return nil
+}
