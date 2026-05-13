b6477ec905e572aaee805feee670ed833bdcf356
Patrick Dehn <patrick.dehn@camunda.com>
2026-05-13 15:07:10 +0200

    refactor: use Navigate instead of effect

--- webapp/client/apps/orchestration-cluster-webapp/src/modules/auth/components/SessionWatcher.tsx
+++ webapp/client/apps/orchestration-cluster-webapp/src/modules/auth/components/SessionWatcher.tsx
@@ -6,30 +6,22 @@
  * except in compliance with the Camunda License 1.0.
  */
 
-import {useEffect} from 'react';
 import {observer} from 'mobx-react-lite';
-import {useRouter, useLocation} from '@tanstack/react-router';
+import {Navigate, useLocation} from '@tanstack/react-router';
 import {authenticationStore} from '#/modules/auth/authentication.store';
 
 const INITIAL_PATH = '/';
 
 const SessionWatcher: React.FC = observer(() => {
-	const router = useRouter();
 	const location = useLocation();
 	const {status} = authenticationStore;
 
 	const isSessionExpired =
 		status === 'session-expired' || (status === 'session-invalid' && location.pathname !== INITIAL_PATH);
 
-	useEffect(() => {
-		if (isSessionExpired) {
-			void router.navigate({
-				to: '/login',
-				search: location.href === '/' ? {} : {redirect: location.href},
-				replace: true,
-			});
-		}
-	}, [isSessionExpired, location.href, router]);
+	if (isSessionExpired) {
+		return <Navigate to="/login" search={location.href === '/' ? {} : {redirect: location.href}} replace />;
+	}
 
 	// TODO: show a session-expired notification once a notifications module is available
 
