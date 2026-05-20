f68cf3c52538b790a8ca601a067d2ee16c54e4fe
John Montgomery <john.montgomery@camunda.com>
2026-05-20 17:42:08 +0100

    test: bunch more scripts to run sim scenarios

---
+++ max-ordinals-sim-deleter-no-ilm-only.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./ordinal-rollover-sim.py --mode main --duration-min 1 --duration-max 1 --ticks 30000 --rate 100 --csv-row --csv-header
+for max in {2..30}; do
+    ./ordinal-rollover-sim.py --mode main --duration-min 1 --duration-max $max --ticks 30000 --rate 100 --csv-row
+done
+
+for max in {1..30}; do
+    ./ordinal-rollover-sim.py --mode ordinal --duration-min 1 --duration-max $max --ticks 30000 --rate 100 --csv-row --max-ordinals 30 --run-deleter --deleter-only-if-no-ilm
+done
