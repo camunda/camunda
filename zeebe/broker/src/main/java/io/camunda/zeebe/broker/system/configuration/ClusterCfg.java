3ff3455d29bc78a797b12d85967fc9cc493e9243
Carlo Sana <carlo.sana@camunda.com>
2026-05-21 11:00:44 +0200

    feat: require broker to use zone aware if zone is not null

--- zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/ClusterCfg.java
+++ zeebe/broker/src/main/java/io/camunda/zeebe/broker/system/configuration/ClusterCfg.java
@@ -129,6 +129,9 @@ public final class ClusterCfg implements ConfigurationEntry {
   private void validateZoneAwareConfig(final BrokerCfg globalConfig) {
     final var partitioningCfg = globalConfig.getExperimental().getPartitioning();
 
+    if (zone != null && partitioningCfg.getScheme() != Scheme.REGION_AWARE) {
+      throw new IllegalArgumentException(ZONE_SCHEME_ERROR_MSG);
+    }
     if (partitioningCfg.getScheme() != Scheme.REGION_AWARE) {
       return;
     }
