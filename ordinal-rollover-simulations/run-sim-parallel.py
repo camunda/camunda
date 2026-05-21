4398bf544820d2080d46cca74ec63d99abaa7987
John Montgomery <john.montgomery@camunda.com>
2026-05-21 10:42:34 +0100

    test: maintain original order for output

--- ordinal-rollover-simulations/run-sim-parallel.py
+++ ordinal-rollover-simulations/run-sim-parallel.py
@@ -12,6 +12,12 @@ print(header)
 
 commands = subprocess.check_output([commands_generator]).decode("utf-8").strip().splitlines()
 
+commands_output = {}
+
 with ThreadPoolExecutor() as executor:
-  for command in commands:
-    executor.submit(lambda cmd: print(subprocess.check_output(cmd, shell=True).decode("utf-8").strip()), command)
+  for i, command in enumerate(commands):
+    executor.submit(lambda pos, cmd: commands_output.update({pos:subprocess.check_output(cmd, shell=True).decode("utf-8").strip()}), i, command)
+
+# ensure we retain original order for final output
+for pos in sorted(commands_output):
+  print(commands_output[pos])
