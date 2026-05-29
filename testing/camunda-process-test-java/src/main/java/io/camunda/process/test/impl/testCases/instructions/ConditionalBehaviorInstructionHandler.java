03f06d9a8462a184c600dc0a54566edde0438b1e
Dmitri Nikonov <dmitri.nikonov@camunda.com>
2026-05-29 07:57:26 +0200

    refactor: Allow all test case instructions in conditional behavior instruction code

--- testing/camunda-process-test-java/src/main/java/io/camunda/process/test/impl/testCases/instructions/ConditionalBehaviorInstructionHandler.java
+++ testing/camunda-process-test-java/src/main/java/io/camunda/process/test/impl/testCases/instructions/ConditionalBehaviorInstructionHandler.java
@@ -20,16 +20,11 @@ import io.camunda.process.test.api.CamundaProcessTestContext;
 import io.camunda.process.test.api.behavior.BehaviorCondition;
 import io.camunda.process.test.api.behavior.ConditionalBehaviorBuilder;
 import io.camunda.process.test.api.testCases.TestCaseInstruction;
-import io.camunda.process.test.api.testCases.TestCaseInstructionType;
 import io.camunda.process.test.api.testCases.instructions.ConditionalBehaviorInstruction;
 import io.camunda.process.test.impl.testCases.AssertionFacade;
 import io.camunda.process.test.impl.testCases.TestCaseInstructionHandler;
 import io.camunda.process.test.impl.testCases.TestCaseInstructionHandlerRegistry;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * Handler that registers a {@link ConditionalBehaviorInstruction} on the test context. The
@@ -44,29 +39,6 @@ import java.util.TreeSet;
  */
 public class ConditionalBehaviorInstructionHandler
     implements TestCaseInstructionHandler<ConditionalBehaviorInstruction> {
-
-  private static final Set<String> ALLOWED_CONDITION_TYPES =
-      sortedSetOf(
-          TestCaseInstructionType.ASSERT_DECISION,
-          TestCaseInstructionType.ASSERT_ELEMENT_INSTANCE,
-          TestCaseInstructionType.ASSERT_ELEMENT_INSTANCES,
-          TestCaseInstructionType.ASSERT_PROCESS_INSTANCE,
-          TestCaseInstructionType.ASSERT_PROCESS_INSTANCE_MESSAGE_SUBSCRIPTION,
-          TestCaseInstructionType.ASSERT_USER_TASK,
-          TestCaseInstructionType.ASSERT_VARIABLES);
-
-  private static final Set<String> ALLOWED_ACTION_TYPES =
-      sortedSetOf(
-          TestCaseInstructionType.BROADCAST_SIGNAL,
-          TestCaseInstructionType.COMPLETE_JOB,
-          TestCaseInstructionType.COMPLETE_JOB_AD_HOC_SUB_PROCESS,
-          TestCaseInstructionType.COMPLETE_JOB_USER_TASK_LISTENER,
-          TestCaseInstructionType.COMPLETE_USER_TASK,
-          TestCaseInstructionType.CORRELATE_MESSAGE,
-          TestCaseInstructionType.PUBLISH_MESSAGE,
-          TestCaseInstructionType.RESOLVE_INCIDENT,
-          TestCaseInstructionType.THROW_BPMN_ERROR_FROM_JOB,
-          TestCaseInstructionType.UPDATE_VARIABLES);
 
   private final TestCaseInstructionHandlerRegistry registry;
 
@@ -86,13 +58,11 @@ public class ConditionalBehaviorInstructionHandler
           "CONDITIONAL_BEHAVIOR requires at least one condition, but the conditions list is"
               + " empty.");
     }
-    instruction.getConditions().forEach(ConditionalBehaviorInstructionHandler::validateCondition);
 
     if (instruction.getActions().isEmpty()) {
       throw new IllegalArgumentException(
           "CONDITIONAL_BEHAVIOR requires at least one action, but the actions list is empty.");
     }
-    instruction.getActions().forEach(ConditionalBehaviorInstructionHandler::validateAction);
 
     final List<TestCaseInstruction> conditionInstructions = instruction.getConditions();
     final BehaviorCondition condition =
@@ -113,27 +83,5 @@ public class ConditionalBehaviorInstructionHandler
   @Override
   public Class<ConditionalBehaviorInstruction> getInstructionType() {
     return ConditionalBehaviorInstruction.class;
-  }
-
-  private static void validateCondition(final TestCaseInstruction condition) {
-    if (!ALLOWED_CONDITION_TYPES.contains(condition.getType())) {
-      throw new IllegalArgumentException(
-          String.format(
-              "CONDITIONAL_BEHAVIOR condition type must be one of %s, but was: %s",
-              ALLOWED_CONDITION_TYPES, condition.getType()));
-    }
-  }
-
-  private static void validateAction(final TestCaseInstruction action) {
-    if (!ALLOWED_ACTION_TYPES.contains(action.getType())) {
-      throw new IllegalArgumentException(
-          String.format(
-              "CONDITIONAL_BEHAVIOR action type must be one of %s, but was: %s",
-              ALLOWED_ACTION_TYPES, action.getType()));
-    }
-  }
-
-  private static Set<String> sortedSetOf(final String... values) {
-    return Collections.unmodifiableSet(new TreeSet<>(Arrays.asList(values)));
   }
 }
