7f452c6f4d2709a0871165c68e0a7f0bbf23d845
Carlo Sana <carlo.sana@camunda.com>
2026-05-29 10:45:24 +0200

    build: add missing log4j version in client-conventions

--- buildSrc/src/main/kotlin/buildlogic.client-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.client-conventions.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     add("implementation", platform(versionCatalog.findLibrary("io-micrometer-micrometer-bom").get()))
     add("implementation", platform(versionCatalog.findLibrary("io-netty-netty-bom").get()))
     add("implementation", platform(versionCatalog.findLibrary("org-mockito-mockito-bom").get()))
+    add("implementation", platform(versionCatalog.findLibrary("org-apache-logging-log4j-log4j-bom").get()))
     add("implementation", platform(versionCatalog.findLibrary("tools-jackson-jackson-bom").get()))
     add("testRuntimeOnly", versionCatalog.findLibrary("org-apache-logging-log4j-log4j-core").get())
     add("testRuntimeOnly", versionCatalog.findLibrary("org-apache-logging-log4j-log4j-slf4j2-impl").get())
