e13f475c445c826af5aea29bd52ca9fbdd23dbe7
Philipp Ossler <philipp.ossler@gmail.com>
2026-06-04 10:31:11 +0200

    build: Adjust Maven code style

--- testing/camunda-process-test-coverage/src/test/java/io/camunda/process/test/api/coverage/CoverageReportCollectorBuilderTest.java
+++ testing/camunda-process-test-coverage/src/test/java/io/camunda/process/test/api/coverage/CoverageReportCollectorBuilderTest.java
@@ -28,8 +28,11 @@ import io.camunda.process.test.impl.coverage.data.ImmutableCoverageDecisionInsta
 import io.camunda.process.test.impl.coverage.data.ImmutableCoverageProcessInstanceData;
 import io.camunda.process.test.impl.coverage.data.ImmutableCoverageTestData;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CoverageReportCollectorBuilderTest {
 
   @Mock private ProcessInstance processInstance;
