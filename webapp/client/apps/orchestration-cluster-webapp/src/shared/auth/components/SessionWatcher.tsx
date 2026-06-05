7937b614b626bb2198aa5c0e123b81b7a8b9a6ab
Vinicius Goulart <vinicius.goulart@camunda.com>
2026-06-05 19:46:31 +0200

    fix: fix logout redirect
    (cherry picked from commit 967af63521a100d606f909203dbedb0a43fb9f18)

--- webapp/client/apps/orchestration-cluster-webapp/src/shared/auth/components/SessionWatcher.tsx
+++ webapp/client/apps/orchestration-cluster-webapp/src/shared/auth/components/SessionWatcher.tsx
@@ -14,7 +14,10 @@ const SessionWatcher: React.FC = observer(() => {
 	const location = useLocation();
 	const {status} = authenticationStore;
 
-	const isSessionExpired = status === 'session-expired' || (status === 'session-invalid' && location.pathname !== '/');
+	const isSessionExpired =
+		status === 'logged-out' ||
+		status === 'session-expired' ||
+		(status === 'session-invalid' && location.pathname !== '/');
 
 	if (isSessionExpired) {
 		return <Navigate to="/login" search={location.href === '/' ? {} : {redirect: location.href}} replace />;
