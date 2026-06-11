Talank/camunda_camunda

feat: activate jobs by priority via JOB_ACTIVATABLE_BY_PRIORITY CF

zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/ZbColumnFamilies.java
@@ -306,7 +306,9 @@ public enum ZbColumnFamilies implements EnumValue, ScopedColumnFamily {
   // partition to query. An entry is "cross-partition" iff this CF has a value for the lock; the
   // partition P_B = hash(businessId) is derived at query time, not stored. Local-PI lock entries
   // are absent from this CF and continue to be released by today's local completion path.
-  CROSS_PARTITION_MESSAGE_START_LOCK_BUSINESS_ID(147, PARTITION_LOCAL);
+  CROSS_PARTITION_MESSAGE_START_LOCK_BUSINESS_ID(147, PARTITION_LOCAL),
+
+  JOB_ACTIVATABLE_BY_PRIORITY(148, PARTITION_LOCAL);
 
   private final int value;
   private final ColumnFamilyScope columnFamilyScope;

camunda/camunda

zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/ZbColumnFamilies.java
@@ -307,7 +307,9 @@ public enum ZbColumnFamilies implements EnumValue, ScopedColumnFamily {
   // instance key) and pick up the next buffered message for the correlation key once released. An
   // entry is "cross-partition" iff this CF has a value for the lock; local-PI lock entries are
   // absent from this CF and continue to be released by today's local completion path.
-  CROSS_PARTITION_MESSAGE_START_LOCK(147, PARTITION_LOCAL);
+  CROSS_PARTITION_MESSAGE_START_LOCK(147, PARTITION_LOCAL),
+
+  JOB_ACTIVATABLE_BY_PRIORITY(148, PARTITION_LOCAL);
 
   private final int value;
   private final ColumnFamilyScope columnFamilyScope;