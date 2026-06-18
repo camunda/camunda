ae43511a99bc23cbc491c55f928bb0b8786265b7
Francesco Esposito <10479390+phoinixi@users.noreply.github.com>
2026-06-18 12:12:57 +0200

    feat: add bpmn-js viewer component to unified webapp
    Installs bpmn-js 18.16.1 (matching the legacy Operate app) and creates
    a shared BpmnViewer React component in src/operate/shared/BpmnViewer/.
    This infrastructure is required by the Processes list page (DiagramPanel)
    and the ProcessInstance page, both upcoming in the Operate migration.
    
    Closes #55456

---
+++ webapp/client/apps/orchestration-cluster-webapp/src/operate/shared/BpmnViewer/BpmnViewer.test.tsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import {render} from 'vitest-browser-react';
+import {describe, it, expect} from 'vitest';
+import {BpmnViewer} from './index';
+
+const MINIMAL_BPMN = `<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions
+  xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
+  xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
+  xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"
+  id="Definitions_1"
+  targetNamespace="http://bpmn.io/schema/bpmn">
+  <bpmn:process id="Process_1" isExecutable="false">
+    <bpmn:startEvent id="StartEvent_1" />
+  </bpmn:process>
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+      <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">
+        <dc:Bounds x="156" y="81" width="36" height="36" />
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>`;
+
+describe('<BpmnViewer />', () => {
+	it('should render without error given valid BPMN XML', async () => {
+		const screen = await render(<BpmnViewer xml={MINIMAL_BPMN} />);
+
+		await expect.element(screen.getByTestId('bpmn-viewer')).toBeVisible();
+	});
+});
