173985282363ea7c9abad73af7f26826f772d6c4
Konstantinos Mylosis <konstantinos.mylosis@camunda.com>
2026-06-18 09:51:44 +0300

    fix: avoid NPE on null processDefinitionVersion
    handle gracefully and return an empty optional definition
    
    (cherry picked from commit 7a4340cc115381e57d07bc13d795b798648913d8)

--- optimize/backend/src/main/java/io/camunda/optimize/service/DefinitionService.java
+++ optimize/backend/src/main/java/io/camunda/optimize/service/DefinitionService.java
@@ -447,7 +447,7 @@ public class DefinitionService implements ConfigurationReloadable {
                     getDefinition(
                         DefinitionType.PROCESS,
                         processDefinition.getLeft(),
-                        List.of(processDefinition.getRight()),
+                        Stream.ofNullable(processDefinition.getRight()).toList(),
                         Collections.emptyList()))
             .filter(Optional::isPresent)
             .map(Optional::get)
