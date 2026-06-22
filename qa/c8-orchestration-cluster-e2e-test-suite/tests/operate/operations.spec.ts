98b991be9f0aedeae03691bfc027263c54587acd
esraagamal6 <129334285+esraagamal6@users.noreply.github.com>
2026-06-22 14:06:41 +0200

    test: unskip 4 E2E test(s) referencing closed bugs (stable/8.8)

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/operate/operations.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/operate/operations.spec.ts
@@ -97,9 +97,8 @@ test.describe('Operations', () => {
     });
   });
 
-  // Skipped due to bug 42375: https://github.com/camunda/camunda/issues/42375
   // !Note: assert the code after the bug is fixed as it was dicoverd during the test implementation
-  test.skip('Retry and cancel single instance', async ({
+  test('Retry and cancel single instance', async ({
     page,
     operateProcessesPage,
     operateFiltersPanelPage,
