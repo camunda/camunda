60386496780d3b13d9c454e5cddcf3b6d93d895c
megglos <sebastian.bathke@camunda.com>
2026-06-30 12:54:01 +0200

    refactor: move PT auth ITs into auth package
    So they match the identity package filters as defined in the `identity-packages`
    of the pom.xml.

--- qa/acceptance-tests/src/test/java/io/camunda/it/auth/MultiPhysicalTenantAuthorizationIT.java
+++ qa/acceptance-tests/src/test/java/io/camunda/it/auth/MultiPhysicalTenantAuthorizationIT.java
@@ -5,7 +5,7 @@
  * Licensed under the Camunda License 1.0. You may not use this file
  * except in compliance with the Camunda License 1.0.
  */
-package io.camunda.it.physicaltenant;
+package io.camunda.it.auth;
 
 import static io.camunda.client.api.search.enums.OwnerType.GROUP;
 import static io.camunda.client.api.search.enums.OwnerType.MAPPING_RULE;
