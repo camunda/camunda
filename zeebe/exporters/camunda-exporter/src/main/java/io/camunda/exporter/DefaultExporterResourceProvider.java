1fe541eaa1c19d1466568498030099b2a91a3203
Fabio Paini <fabio.paini@camunda.com>
2026-06-30 14:53:33 +0200

    feat: mark process definitions deleted in ES/OS via dedicated export handler
    Add ProcessDeletedHandler to handle ProcessIntent.DELETED records and perform
    a partial document update setting isDeleted=true. Register it alongside the
    existing ProcessHandler in DefaultExporterResourceProvider.
    
    (cherry picked from commit 006356a68b28e3571aee9b01f7287fa00d64bdf1)

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/DefaultExporterResourceProvider.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/DefaultExporterResourceProvider.java
@@ -51,6 +51,7 @@ import io.camunda.exporter.handlers.MappingRuleDeletedHandler;
 import io.camunda.exporter.handlers.MessageSubscriptionFromProcessMessageSubscriptionHandler;
 import io.camunda.exporter.handlers.MigratedVariableHandler;
 import io.camunda.exporter.handlers.PostImporterQueueFromIncidentHandler;
+import io.camunda.exporter.handlers.ProcessDeletedHandler;
 import io.camunda.exporter.handlers.ProcessHandler;
 import io.camunda.exporter.handlers.RoleCreateUpdateHandler;
 import io.camunda.exporter.handlers.RoleDeletedHandler;
@@ -277,6 +278,8 @@ public class DefaultExporterResourceProvider implements ExporterResourceProvider
                 indexDescriptors.get(DecisionInstanceTemplate.class).getFullQualifiedName()),
             new ProcessHandler(
                 indexDescriptors.get(ProcessIndex.class).getFullQualifiedName(), processCache),
+            new ProcessDeletedHandler(
+                indexDescriptors.get(ProcessIndex.class).getFullQualifiedName()),
             new EmbeddedFormHandler(indexDescriptors.get(FormIndex.class).getFullQualifiedName()),
             new FormHandler(
                 indexDescriptors.get(FormIndex.class).getFullQualifiedName(), formCache),
---
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/handlers/ProcessDeletedHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.exporter.handlers;
+
+import io.camunda.exporter.exceptions.PersistenceException;
+import io.camunda.exporter.store.BatchRequest;
+import io.camunda.webapps.schema.descriptors.index.ProcessIndex;
+import io.camunda.webapps.schema.entities.ProcessEntity;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.ValueType;
+import io.camunda.zeebe.protocol.record.intent.ProcessIntent;
+import io.camunda.zeebe.protocol.record.value.deployment.Process;
+import java.util.List;
+import java.util.Map;
+
+public class ProcessDeletedHandler implements ExportHandler<ProcessEntity, Process> {
+
+  private final String indexName;
+
+  public ProcessDeletedHandler(final String indexName) {
+    this.indexName = indexName;
+  }
+
+  @Override
+  public ValueType getHandledValueType() {
+    return ValueType.PROCESS;
+  }
+
+  @Override
+  public Class<ProcessEntity> getEntityType() {
+    return ProcessEntity.class;
+  }
+
+  @Override
+  public boolean handlesRecord(final Record<Process> record) {
+    return record.getIntent() == ProcessIntent.DELETED;
+  }
+
+  @Override
+  public List<String> generateIds(final Record<Process> record) {
+    return List.of(String.valueOf(record.getValue().getProcessDefinitionKey()));
+  }
+
+  @Override
+  public ProcessEntity createNewEntity(final String id) {
+    return new ProcessEntity().setId(id);
+  }
+
+  @Override
+  public void updateEntity(final Record<Process> record, final ProcessEntity entity) {
+    entity.setIsDeleted(true);
+  }
+
+  @Override
+  public void flush(final ProcessEntity entity, final BatchRequest batchRequest)
+      throws PersistenceException {
+    batchRequest.update(indexName, entity.getId(), Map.of(ProcessIndex.IS_DELETED, true));
+  }
+
+  @Override
+  public String getIndexName() {
+    return indexName;
+  }
+}
