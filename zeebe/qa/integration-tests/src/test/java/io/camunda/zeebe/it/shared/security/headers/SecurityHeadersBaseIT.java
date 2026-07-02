Talank/camunda_camunda

fix: abstract test class cannot declare a shared resource as static
SecurityHeadersBaseIT was declaring the HTTP client as static: that was
closed when one of the classes completed the tests. This is probably not
happening in maven as each test runs in a separate JVM

zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/shared/security/headers/SecurityHeadersBaseIT.java
@@ -45,7 +45,7 @@ public abstract class SecurityHeadersBaseIT {
   protected static final ElasticsearchContainer CONTAINER =
       TestSearchContainers.createDefaultElasticsearchContainer();
 
-  @AutoClose protected static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+  @AutoClose protected final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
 
   // Abstract methods to be implemented by subclasses
   protected abstract CamundaClient getCamundaClient();

camunda/camunda

zeebe/qa/integration-tests/src/test/java/io/camunda/zeebe/it/shared/security/headers/SecurityHeadersBaseIT.java
@@ -45,7 +45,7 @@ public abstract class SecurityHeadersBaseIT {
   protected static final ElasticsearchContainer CONTAINER =
       TestSearchContainers.createDefaultElasticsearchContainer();
 
-  @AutoClose protected static final HttpClient HTTP_CLIENT = HttpClient.newHttpClient();
+  @AutoClose protected final HttpClient httpClient = HttpClient.newHttpClient();
 
   // Abstract methods to be implemented by subclasses
   protected abstract CamundaClient getCamundaClient();