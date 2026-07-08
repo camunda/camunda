b07d3d7af8d0cdd1deb6bc147c233e6a67ee6280
Nicola Puppa <puppanicola@gmail.com>
2026-07-08 14:08:50 +0200

    feat: implement appendPhysicalTenantPath in the cloud client builder
    CamundaClientCloudBuilderImpl implements CamundaClientBuilder (via the cloud builder steps), so it must delegate the new appendPhysicalTenantPath option to its inner builder like the other options.
    
    Refs #56722

--- clients/java/src/main/java/io/camunda/client/impl/CamundaClientCloudBuilderImpl.java
+++ clients/java/src/main/java/io/camunda/client/impl/CamundaClientCloudBuilderImpl.java
@@ -167,6 +167,12 @@ public class CamundaClientCloudBuilderImpl
   }
 
   @Override
+  public CamundaClientBuilder appendPhysicalTenantPath(final boolean appendPhysicalTenantPath) {
+    innerBuilder.appendPhysicalTenantPath(appendPhysicalTenantPath);
+    return this;
+  }
+
+  @Override
   @ExperimentalApi("https://github.com/camunda/camunda/issues/14106")
   public CamundaClientBuilder defaultJobWorkerTenantIds(final List<String> tenantIds) {
     Loggers.LOGGER.debug(
