31b6aed3fd226978533daa41113f21b7f794a5e5
Carlo Sana <carlo.sana@camunda.com>
2026-07-15 09:39:06 +0200

    fix: include only exception message in actuator response
    closes #57489
    
    (cherry picked from commit 3357fcb00aad473dee4bf75af460f034dcdb6ecc)

--- dist/src/test/java/io/camunda/zeebe/shared/management/ExportingEndpointTest.java
+++ dist/src/test/java/io/camunda/zeebe/shared/management/ExportingEndpointTest.java
@@ -14,7 +14,11 @@ import static org.mockito.Mockito.when;
 
 import io.camunda.zeebe.gateway.admin.exporting.ExportingControlApi;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
 
@@ -73,5 +77,43 @@ final class ExportingEndpointTest {
     // then
     assertThat(endpoint.post(operation, false))
         .returns(WebEndpointResponse.STATUS_NO_CONTENT, from(WebEndpointResponse::getStatus));
+  }
+
+  @ParameterizedTest
+  @MethodSource("exceptionSource")
+  void shouldReturnResponseCorrectlyWhenExceptionIsThrown(
+      final String operation, final String message) {
+    // given
+    final var service = mock(ExportingControlApi.class);
+    final var endpoint = new ExportingEndpoint(service);
+    final var exception = new RuntimeException(message);
+
+    // when
+    when(service.pauseExporting()).thenReturn(CompletableFuture.failedFuture(exception));
+    when(service.resumeExporting())
+        .thenReturn(CompletableFuture.failedFuture(new CompletionException(exception)));
+
+    // then
+    assertThat(endpoint.post(operation, false))
+        .returns(
+            WebEndpointResponse.STATUS_INTERNAL_SERVER_ERROR, from(WebEndpointResponse::getStatus))
+        .satisfies(
+            resp -> {
+              if (message != null) {
+                assertThat(resp.getBody())
+                    .isInstanceOf(String.class)
+                    .asString()
+                    .contains(exception.getMessage());
+              } else {
+                assertThat(resp.getBody()).isNull();
+              }
+            });
+  }
+
+  private static Stream<Arguments> exceptionSource() {
+    return Stream.of(ExportingEndpoint.PAUSE, ExportingEndpoint.RESUME)
+        .flatMap(
+            operation ->
+                Stream.of("expected error", null).map(str -> Arguments.of(operation, str)));
   }
 }
