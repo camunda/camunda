96789634a6d3acca6e8be5338cd8c1936a0a794c
Nikola Koevski <nikola.koevski@camunda.com>
2026-07-16 16:38:59 +0300

    fix: add CREATE_BATCH_OPERATION_RESUME_PROCESS_INSTANCE to default admin role test
    Full-suite verification surfaced this pre-existing test asserting the exact
    default admin-role BATCH permission set, which didn't yet expect the new
    resume permission constant added earlier in this branch.

--- zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/IdentitySetupInitializeDefaultsTest.java
+++ zeebe/engine/src/test/java/io/camunda/zeebe/engine/processing/authorization/IdentitySetupInitializeDefaultsTest.java
@@ -192,6 +192,7 @@ public class IdentitySetupInitializeDefaultsTest {
                         PermissionType.CREATE_BATCH_OPERATION_DELETE_DECISION_INSTANCE,
                         PermissionType.CREATE_BATCH_OPERATION_DELETE_DECISION_DEFINITION,
                         PermissionType.CREATE_BATCH_OPERATION_DELETE_PROCESS_DEFINITION,
+                        PermissionType.CREATE_BATCH_OPERATION_RESUME_PROCESS_INSTANCE,
                         PermissionType.CREATE_BATCH_OPERATION_SUSPEND_PROCESS_INSTANCE,
                         PermissionType.CREATE_BATCH_OPERATION_UPDATE_JOB,
                         PermissionType.UPDATE,
