35810448ac132a816e9175921702b8515234c0a4
Philipp Ossler <philipp.ossler@gmail.com>
2026-07-23 09:43:48 +0200

    test: Verify decision deletion
    Add a test case to verify that decision definitions and instances are deleted.

--- testing/camunda-process-test-java/src/test/java/io/camunda/process/test/api/ResourceAndHistoryDeletionStrategyIT.java
+++ testing/camunda-process-test-java/src/test/java/io/camunda/process/test/api/ResourceAndHistoryDeletionStrategyIT.java
@@ -18,7 +18,10 @@ package io.camunda.process.test.api;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.camunda.client.CamundaClient;
+import io.camunda.client.api.response.Decision;
+import io.camunda.client.api.response.DecisionRequirements;
 import io.camunda.client.api.response.DeploymentEvent;
+import io.camunda.client.api.search.response.DecisionInstance;
 import io.camunda.client.api.search.response.ProcessDefinition;
 import io.camunda.client.api.search.response.ProcessInstance;
 import io.camunda.process.test.impl.cleanup.ResourceAndHistoryDeletionStrategy;
@@ -39,6 +42,11 @@ public class ResourceAndHistoryDeletionStrategyIT {
   private static final String BEFORE_EACH_PROCESS = "before-each-process";
   private static final String TEST_CASE_PROCESS = "test-case-process";
 
+  private static final String BEFORE_EACH_DECISION = "decision1";
+  private static final String BEFORE_EACH_DECISION_REQUIREMENTS = "drd1";
+  private static final String TEST_CASE_DECISION = "decision2";
+  private static final String TEST_CASE_DECISION_REQUIREMENTS = "drd2";
+
   // to be injected
   private CamundaClient client;
   private CamundaProcessTestContext processTestContext;
@@ -48,13 +56,16 @@ public class ResourceAndHistoryDeletionStrategyIT {
     deployProcess(BEFORE_EACH_PROCESS);
     createProcessInstance(BEFORE_EACH_PROCESS);
 
+    deployDecision(BEFORE_EACH_DECISION);
+    evaluateDecision(BEFORE_EACH_DECISION);
+
     // move time forward to ensure that the test case start time is after the preexisting process
     // instance creation time
     processTestContext.increaseTime(Duration.ofMinutes(1));
   }
 
   @Test
-  void shouldDeleteTestCaseDataWithoutTouchingPreviousDeployments() {
+  void shouldDeleteProcessData() {
     // given
     final Instant testCaseStartTime = processTestContext.getCurrentTime();
 
@@ -111,6 +122,71 @@ public class ResourceAndHistoryDeletionStrategyIT {
         .contains(BEFORE_EACH_PROCESS);
   }
 
+  @Test
+  void shouldDeleteDecisionData() {
+    // given
+    final Instant testCaseStartTime = processTestContext.getCurrentTime();
+
+    final DeploymentEvent testCaseDeployment = deployDecision(TEST_CASE_DECISION);
+    evaluateDecision(TEST_CASE_DECISION);
+
+    final ResourceAndHistoryDeletionStrategy strategy = new ResourceAndHistoryDeletionStrategy();
+
+    // when
+    strategy.cleanup(
+        Mockito.mock(CamundaManagementClient.class),
+        processTestContext::createClient,
+        testCaseStartTime,
+        Collections.singleton(testCaseDeployment));
+
+    // then
+    Awaitility.await("Wait until test-case decision instances are deleted")
+        .atMost(Duration.ofSeconds(30))
+        .untilAsserted(
+            () -> {
+              assertThat(
+                      client
+                          .newDecisionInstanceSearchRequest()
+                          .filter(filter -> filter.decisionDefinitionId(TEST_CASE_DECISION))
+                          .send()
+                          .join()
+                          .items())
+                  .describedAs("Expected test-case decision instances to be deleted")
+                  .isEmpty();
+
+              assertThat(
+                      client
+                          .newDecisionDefinitionSearchRequest()
+                          .filter(filter -> filter.decisionDefinitionId(TEST_CASE_DECISION))
+                          .send()
+                          .join()
+                          .items())
+                  .describedAs("Expected test-case decision definitions to be deleted")
+                  .isEmpty();
+            });
+
+    assertThat(client.newDecisionInstanceSearchRequest().send().join().items())
+        .extracting(DecisionInstance::getDecisionDefinitionId)
+        .describedAs("Expected test-case decision instances to be deleted")
+        .doesNotContain(TEST_CASE_DECISION)
+        .describedAs("Expected preexisting decision instances to remain")
+        .contains(BEFORE_EACH_DECISION);
+
+    assertThat(client.newDecisionDefinitionSearchRequest().send().join().items())
+        .extracting(Decision::getDmnDecisionId)
+        .describedAs("Expected test-case decision definitions to be deleted")
+        .doesNotContain(TEST_CASE_DECISION)
+        .describedAs("Expected preexisting decision definitions to remain")
+        .contains(BEFORE_EACH_DECISION);
+
+    assertThat(client.newDecisionRequirementsSearchRequest().send().join().items())
+        .extracting(DecisionRequirements::getDmnDecisionRequirementsId)
+        .describedAs("Expected test-case decision requirements to be deleted")
+        .doesNotContain(TEST_CASE_DECISION_REQUIREMENTS)
+        .describedAs("Expected preexisting decision requirements to remain")
+        .contains(BEFORE_EACH_DECISION_REQUIREMENTS);
+  }
+
   private DeploymentEvent deployProcess(final String processId) {
     final BpmnModelInstance process =
         Bpmn.createExecutableProcess(processId)
@@ -129,5 +205,17 @@ public class ResourceAndHistoryDeletionStrategyIT {
 
   private void createProcessInstance(final String processId) {
     client.newCreateInstanceCommand().bpmnProcessId(processId).latestVersion().send().join();
+  }
+
+  private DeploymentEvent deployDecision(final String decisionId) {
+    return client
+        .newDeployResourceCommand()
+        .addResourceFromClasspath("resourceAndHistoryDeletionStrategyIT/" + decisionId + ".dmn")
+        .send()
+        .join();
+  }
+
+  private void evaluateDecision(final String decisionId) {
+    client.newEvaluateDecisionCommand().decisionId(decisionId).send().join();
   }
 }
