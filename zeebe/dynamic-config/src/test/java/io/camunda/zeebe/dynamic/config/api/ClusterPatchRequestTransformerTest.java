1b2678a30ff7b492195669986500ef71a44faba1
Kamil Krzywanski <kamilkrzywanski01@gmail.com>
2026-07-23 13:19:06 +0200

    fix: init routing state from topology partition count
    On upgrades from versions without a persisted routing state,
    RoutingStateInitializer used the static partitionCount. If that
    value was changed during the upgrade, HashMod no longer matched
    the partitions that actually exist and message publish failed.
    
    Derive the initial routing state from the cluster configuration
    instead. Static config is not used for this anymore.
    
    (cherry picked from commit bce70fb2efed71db5d00ef9452a15bbc6a9268c8)

--- zeebe/dynamic-config/src/test/java/io/camunda/zeebe/dynamic/config/api/ClusterPatchRequestTransformerTest.java
+++ zeebe/dynamic-config/src/test/java/io/camunda/zeebe/dynamic/config/api/ClusterPatchRequestTransformerTest.java
@@ -189,7 +189,7 @@ final class ClusterPatchRequestTransformerTest {
             .addMember(id1, MemberState.initializeAsActive(Map.of()))
             .updateMember(id0, m -> m.addPartition(1, PartitionState.active(1, partitionConfig)))
             .updateMember(id1, m -> m.addPartition(2, PartitionState.active(1, partitionConfig)));
-    currentTopology = new RoutingStateInitializer(2).modify(currentTopology).join();
+    currentTopology = new RoutingStateInitializer().modify(currentTopology).join();
 
     // when
     final int newPartitionCount = 4;
@@ -220,7 +220,7 @@ final class ClusterPatchRequestTransformerTest {
             .addMember(id1, MemberState.initializeAsActive(Map.of()))
             .updateMember(id0, m -> m.addPartition(1, PartitionState.active(1, partitionConfig)))
             .updateMember(id1, m -> m.addPartition(2, PartitionState.active(1, partitionConfig)));
-    currentTopology = new RoutingStateInitializer(2).modify(currentTopology).join();
+    currentTopology = new RoutingStateInitializer().modify(currentTopology).join();
 
     // when
     final int newPartitionCount = 4;
