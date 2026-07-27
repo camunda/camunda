3a4cb29057868391b36e06c6b94a53c1f754c38f
John Montgomery <john.montgomery@camunda.com>
2026-07-27 15:46:51 +0100

    feat: add target index for ordinal indexes

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/index/TargetIndex.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/index/TargetIndex.java
@@ -7,10 +7,17 @@
  */
 package io.camunda.exporter.index;
 
-public sealed interface TargetIndex permits MainIndex {
+import com.google.common.base.Strings;
+
+public sealed interface TargetIndex permits MainIndex, OrdinalIndex {
   String name();
 
   static TargetIndex mainIndex(final String name) {
     return new MainIndex(name);
+  }
+
+  static TargetIndex ordinalIndex(final String prefix, final int ordinal) {
+    final var suffix = "ord" + Strings.padStart(String.valueOf(ordinal), 5, '0');
+    return new OrdinalIndex(suffix, ordinal, prefix + suffix);
   }
 }
