dd7e28b709e2257f95f6bd588636f07ffc5e766c
Carlo Sana <carlo.sana@camunda.com>
2026-07-30 10:25:06 +0200

    feat: add Hikari metrics scoped per tenant
    After Hikari pool was created manually we lost the metrics exported to
    meter registry.
    Each tenant uses a composite meter registry always exporting the
    physical tenant label so it can be properly filtered in the dashboard.
    MeterRegistry is closed when all datasources are closed.

--- dist/src/test/java/io/camunda/application/commons/rdbms/MyBatisConfigurationPerTenantIT.java
+++ dist/src/test/java/io/camunda/application/commons/rdbms/MyBatisConfigurationPerTenantIT.java
@@ -84,7 +84,9 @@ class MyBatisConfigurationPerTenantIT {
     env.getPropertySources().addFirst(new MapPropertySource("test", props));
     final var resolver = PhysicalTenantResolver.of(env, new Camunda());
     final var dataSources =
-        RdbmsDataSources.of(resolver.mapValues(c -> c.getData().getSecondaryStorage().getRdbms()));
+        RdbmsDataSources.of(
+            resolver.mapValues(c -> c.getData().getSecondaryStorage().getRdbms()),
+            new SimpleMeterRegistry());
     final var factories = MY_BATIS.sqlSessionFactories(dataSources, resolver);
     final var bundles = MY_BATIS.rdbmsMapperBundles(factories, dataSources);
     return new TenantFixture(dataSources, factories, bundles);
