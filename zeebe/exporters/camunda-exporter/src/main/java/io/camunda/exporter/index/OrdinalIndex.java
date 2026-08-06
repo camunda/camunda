e921282d6077d4788e8a3ee35552baa064bb9cc1
John Montgomery <john.montgomery@camunda.com>
2026-08-06 13:35:34 +0100

    feat: extract "index family" from index names
    so we can determine when a particular index name refers to a type of
    index (e.g. list-view etc) when the ordinal part has been removed.

--- zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/index/OrdinalIndex.java
+++ zeebe/exporters/camunda-exporter/src/main/java/io/camunda/exporter/index/OrdinalIndex.java
@@ -7,4 +7,32 @@
  */
 package io.camunda.exporter.index;
 
-public record OrdinalIndex(String prefix, int ordinal, String name) implements TargetIndex {}
+import com.google.common.base.Strings;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+record OrdinalIndex(IndexFamily indexFamily, int ordinal, String name) implements TargetIndex {
+  static final String ORDINAL_SUFFIX = "ord";
+  static final Pattern ORDINAL_INDEX_PATTERN =
+      Pattern.compile("^(.*)" + Pattern.quote(ORDINAL_SUFFIX) + "(\\d{5})$");
+
+  static Optional<TargetIndex> fromName(final String indexName) {
+    final var matcher = ORDINAL_INDEX_PATTERN.matcher(indexName);
+    if (!matcher.matches()) {
+      return Optional.empty();
+    }
+    final var prefix = matcher.group(1);
+    final var ordinal = Integer.parseInt(matcher.group(2));
+    return Optional.of(of(prefix, ordinal));
+  }
+
+  static OrdinalIndex of(final String prefix, final int ordinal) {
+    final var name = prefix + ORDINAL_SUFFIX + Strings.padStart(String.valueOf(ordinal), 5, '0');
+    return new OrdinalIndex(new IndexFamilyImpl(prefix), ordinal, name);
+  }
+
+  @Override
+  public IndexFamily getIndexFamily() {
+    return indexFamily;
+  }
+}
--- zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/index/TargetIndexTest.java
+++ zeebe/exporters/camunda-exporter/src/test/java/io/camunda/exporter/index/TargetIndexTest.java
@@ -17,11 +17,24 @@ class TargetIndexTest {
   void shouldCreateMainIndex() {
     final var index = TargetIndex.mainIndex("index-name");
     assertThat(index.name()).isEqualTo("index-name");
+    assertThat(index.getIndexFamily()).isEqualTo(index);
   }
 
   @Test
   void shouldCreateOrdinalIndex() {
     final var index = TargetIndex.ordinalIndex("index-name", 12);
     assertThat(index.name()).isEqualTo("index-nameord00012");
+    assertThat(index.getIndexFamily()).isEqualTo(new IndexFamilyImpl("index-name"));
+  }
+
+  @Test
+  void shouldReturnIndexFamilyFromNameForMainIndex() {
+    assertThat(TargetIndex.getIndexFamilyFromName("index-name1").name()).isEqualTo("index-name1");
+  }
+
+  @Test
+  void shouldReturnIndexFamilyFromNameForOrdinalIndex() {
+    assertThat(TargetIndex.getIndexFamilyFromName("index-name2ord00012").name())
+        .isEqualTo("index-name2");
   }
 }
