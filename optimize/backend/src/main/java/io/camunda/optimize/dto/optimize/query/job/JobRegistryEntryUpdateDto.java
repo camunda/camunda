b7a95d8c69efa1a2521e5348a6e13f11a258f050
Mehdi Riahi <mehdi.riahi@camunda.com>
2026-08-12 15:31:00 +0200

    feat: add job registry DTOs and enums
    (cherry picked from commit cc7ad085c8e9c58468d0aa7a662f6a36e9781c52)

---
+++ optimize/backend/src/main/java/io/camunda/optimize/dto/optimize/query/job/JobRegistryEntryUpdateDto.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+package io.camunda.optimize.dto.optimize.query.job;
+
+import java.time.OffsetDateTime;
+
+public class JobRegistryEntryUpdateDto {
+
+  private JobStatus status;
+  private String errorMessage;
+  private OffsetDateTime completedAt;
+
+  public JobRegistryEntryUpdateDto(
+      final JobStatus status, final String errorMessage, final OffsetDateTime completedAt) {
+    this.status = status;
+    this.errorMessage = errorMessage;
+    this.completedAt = completedAt;
+  }
+
+  protected JobRegistryEntryUpdateDto() {}
+
+  public JobStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(final JobStatus status) {
+    this.status = status;
+  }
+
+  public String getErrorMessage() {
+    return errorMessage;
+  }
+
+  public void setErrorMessage(final String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+  public OffsetDateTime getCompletedAt() {
+    return completedAt;
+  }
+
+  public void setCompletedAt(final OffsetDateTime completedAt) {
+    this.completedAt = completedAt;
+  }
+}
