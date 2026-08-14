887f4946efae5b1627e6cb3789c0a862860bd3d2
Hilal <hilal.bursali@camunda.com>
2026-08-14 11:16:33 +0200

    test: unskip 2 API test(s) referencing closed bugs (stable/8.8)

--- qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/authorization/search-authorization-api.spec.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/tests/api/v2/authorization/search-authorization-api.spec.ts
@@ -444,8 +444,7 @@ test.describe.parallel('Search Authorization API', () => {
     });
   });
 
-  // Skiped due to bug 39372: https://github.com/camunda/camunda/issues/39372
-  test.skip('Search Authorization - Negative pagination values (known bug) - 200 instead of 400', async ({request}) => {
+  test('Search Authorization - Negative pagination values (known bug) - 200 instead of 400', async ({request}) => {
     await expect(async () => {
       const res = await request.post(buildUrl(AUTHORIZATION_SEARCH_ENDPOINT), {
         headers: jsonHeaders(),
