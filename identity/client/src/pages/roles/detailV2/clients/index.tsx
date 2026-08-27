e245517c45b5b7c184602ed4a5e5b53f2a48a03f
Christoph Fricke <christoph.fricke@holisticon.de>
2026-08-27 17:30:34 +0200

    refactor: migrate role clients details page
    (cherry picked from commit dfd4fc5c14129214d24ed29a38d0ca550a931128)

--- identity/client/src/pages/roles/detailV2/clients/index.tsx
+++ identity/client/src/pages/roles/detailV2/clients/index.tsx
@@ -7,15 +7,14 @@
  */
 
 import { FC } from "react";
-// TODO: Replace with design system empty state
-import { C3EmptyState } from "@camunda/camunda-composite-components";
-import { TrashCan } from "@carbon/react/icons";
+import { Trash2 } from "lucide-react";
 import { useQuery } from "@tanstack/react-query";
 import useTranslate from "src/utility/localization";
 import { usePagination } from "src/utility/api";
 import { roleQueries } from "src/utility/api/roles/queries";
 import EntityList from "src/components/entityListV2";
 import { useEntityModal } from "src/components/modalV2";
+import { ErrorInlineNotification } from "src/components/notificationsV2/InlineNotification";
 import DeleteModal from "src/pages/roles/detailV2/clients/DeleteModal";
 import AssignClientsModal from "src/pages/roles/detailV2/clients/AssignClientsModal";
 import TabEmptyState from "src/components/layoutV2/TabEmptyState";
@@ -59,12 +58,12 @@ const Clients: FC<ClientsProps> = ({ roleId }) => {
 
   if (!loading && !success)
     return (
-      <C3EmptyState
-        heading={t("somethingsWrong")}
-        description={t("unableToLoadResource", {
+      <ErrorInlineNotification
+        title={t("somethingsWrong")}
+        subtitle={t("unableToLoadResource", {
           resourceType: t("client").toLowerCase(),
         })}
-        button={{
+        actionButton={{
           label: t("retry"),
           onClick: () => {
             void reload();
@@ -98,7 +97,7 @@ const Clients: FC<ClientsProps> = ({ roleId }) => {
         menuItems={[
           {
             label: t("remove"),
-            icon: TrashCan,
+            icon: Trash2,
             isDangerous: true,
             onClick: unassignClient,
           },
