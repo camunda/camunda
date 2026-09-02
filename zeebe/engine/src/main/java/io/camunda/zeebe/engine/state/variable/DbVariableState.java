9d2586f504897c09a5777c99bcb7f69ab8b25c69
Carlo Sana <carlo.sana@camunda.com>
2026-09-02 22:50:53 +0200

    perf: return completionCondition in visitVariableLocal
    In DbVariableState visitVariableLocal it never returned the result of
    the completion condition but always false.
    This means that iteration was never short circuited, especially in
    getVariablesAsDocument which is used by jobs, leading to more iterations
    than necessary
    
    (cherry picked from commit 2bd6142e720a4916f4d4b67e9665451df199bbe2)

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/variable/DbVariableState.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/state/variable/DbVariableState.java
@@ -365,6 +365,6 @@ public class DbVariableState implements MutableVariableState {
 
           return !completionCondition.getAsBoolean();
         });
-    return false;
+    return completionCondition.getAsBoolean();
   }
 }
