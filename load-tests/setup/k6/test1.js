22cd1cb0e8754e0a1eaaff6d061c0e3ffaff5825
Christopher Kujawa <ChrisKujawa@users.noreply.github.com>
2026-09-03 14:16:09 +0200

    fix: refresh auth token

--- load-tests/setup/k6/test1.js
+++ load-tests/setup/k6/test1.js
@@ -3,6 +3,44 @@ import { sleep } from 'k6';
 import { Counter } from 'k6/metrics';
 
 const http_response = new Counter('http_response');
+let token = {
+  accessToken: null,
+  expiryDate: 0,
+  fetchAuthToken: function() {
+    const token_url = __ENV.ZEEBE_AUTHORIZATION_SERVER_URL;
+    const client_id = __ENV.ZEEBE_CLIENT_ID;
+    const client_secret = __ENV.ZEEBE_CLIENT_SECRET;
+    const audience = __ENV.ZEEBE_TOKEN_AUDIENCE;
+
+    console.log(`Fetching authentication token from ${token_url}`);
+
+    const payload = {
+      grant_type: 'client_credentials',
+      audience: audience,
+      client_id: client_id,
+      client_secret: client_secret,
+    };
+
+    const response = http.post(token_url, payload);
+    if (response.status != 200) {
+      const result = response.body;
+      throw new Error(`unable to fetch token: ${result}`);
+    }
+
+    this.accessToken = response.json('access_token');
+    this.expiryDate = Date.now() + response.json('expires_in') * 1000;
+
+    console.log(`Authentication token fetched successfully.`);
+    http_response.add(1, { status: response.status, endpoint: 'token-request' });
+  },
+  renew: function () {
+    if (this.expiryDate > Date.now()) {
+      return this;
+    }
+    this.fetchAuthToken();
+    return this;
+  },
+};
 
 export const options = {
   scenarios: {
@@ -16,44 +54,19 @@ export const options = {
 };
 
 export function setup() {
-  const token_url = __ENV.ZEEBE_AUTHORIZATION_SERVER_URL;
-  const client_id = __ENV.ZEEBE_CLIENT_ID;
-  const client_secret = __ENV.ZEEBE_CLIENT_SECRET;
-  const audience = __ENV.ZEEBE_TOKEN_AUDIENCE;
 
-  console.log(`Fetching authentication token from ${token_url}`);
-
-  const payload = {
-    grant_type: 'client_credentials',
-    audience: audience,
-    client_id: client_id,
-    client_secret: client_secret,
-  };
-
-  const response = http.post(token_url, payload);
-  if (response.status != 200) {
-    const result = response.body;
-    throw new Error(`unable to fetch token: ${result}`);
-  }
-
-  const token = response.json('access_token');
-  console.log(`Authentication token fetched successfully.`);
-  http_response.add(1, { status: response.status, endpoint: 'token-request' });
-
-  const context = {
-    token: token,
-    params: {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': "Bearer " + token,
-      }
-    },
-  };
-  return context;
 }
 
 export function checkTopology (context) {
+  const renewedToken = token.renew();
+  const params = {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + renewedToken.accessToken,
+      }
+    };
+
   const url = __ENV.ZEEBE_REST_ADDRESS + '/v1/topology';
-  http.get(url, context.params);
+  http.get(url, params);
   sleep(1);
 };
