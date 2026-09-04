535ba2e818b5c016f248068ffc6499d0732eaafc
Catherina Prokudina <catherina.prokudina@camunda.com>
2026-09-04 17:33:03 +0200

    test: adjsut combobox name to resource type

--- qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityAuthorizationsPage.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityAuthorizationsPage.ts
@@ -30,7 +30,7 @@ export class IdentityAuthorizationsPage {
   readonly authorizationRowByOwnerId: (ownerId: string) => Locator;
   readonly selectResourceTypeTab: (resourceType: string) => Promise<void>;
   readonly resourceTypeComboBox: Locator;
-  readonly authorizationTypeFilterComboBox: Locator;
+  readonly resourceTypeFilterComboBox: Locator;
   readonly getAuthorizationCell: (ownerId: string) => Locator;
   readonly resourceTypeOption: (resourceType: string) => Locator;
 
@@ -90,8 +90,8 @@ export class IdentityAuthorizationsPage {
     this.resourceTypeComboBox = page.getByRole('combobox', {
       name: 'Resource type',
     });
-    this.authorizationTypeFilterComboBox = page.getByRole('combobox', {
-      name: 'Authorization type',
+    this.resourceTypeFilterComboBox = page.getByRole('combobox', {
+      name: 'Resource type',
     });
     this.getAuthorizationCell = (ownerId) =>
       this.authorizationsList.getByRole('cell', {
@@ -102,7 +102,7 @@ export class IdentityAuthorizationsPage {
         name: new RegExp(`^${resourceType}$`, 'i'),
       });
     this.selectResourceTypeTab = async (resourceType) => {
-      await this.authorizationTypeFilterComboBox.click();
+      await this.resourceTypeFilterComboBox.click();
       await this.resourceTypeOption(resourceType).click();
     };
   }
