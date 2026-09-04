ce2c4f32a4fc356c09619d287dff5e58578582c9
Catherina Prokudina <catherina.prokudina@camunda.com>
2026-09-04 18:00:51 +0200

    test: remove a redundant locator
    (cherry picked from commit 5d9f69907c773195454ab4f343eb37383d9390bf)

--- qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityAuthorizationsPage.ts
+++ qa/c8-orchestration-cluster-e2e-test-suite/pages/IdentityAuthorizationsPage.ts
@@ -32,7 +32,6 @@ export class IdentityAuthorizationsPage {
   readonly authorizationRowByOwnerId: (ownerId: string) => Locator;
   readonly selectResourceTypeTab: (resourceType: string) => Promise<void>;
   readonly resourceTypeComboBox: Locator;
-  readonly resourceTypeFilterComboBox: Locator;
   readonly getAuthorizationCell: (ownerId: string) => Locator;
   readonly resourceTypeOption: (resourceType: string) => Locator;
 
@@ -98,9 +97,6 @@ export class IdentityAuthorizationsPage {
     this.resourceTypeComboBox = page.getByRole('combobox', {
       name: 'Resource type',
     });
-    this.resourceTypeFilterComboBox = page.getByRole('combobox', {
-      name: 'Resource type',
-    });
     this.getAuthorizationCell = (ownerId) =>
       this.authorizationsList.getByRole('cell', {
         name: ownerId.toLowerCase().replace(/ /g, ''),
@@ -110,7 +106,7 @@ export class IdentityAuthorizationsPage {
         name: new RegExp(`^${resourceType}$`, 'i'),
       });
     this.selectResourceTypeTab = async (resourceType) => {
-      await this.resourceTypeFilterComboBox.click();
+      await this.resourceTypeComboBox.click();
       await this.resourceTypeOption(resourceType).click();
     };
   }
