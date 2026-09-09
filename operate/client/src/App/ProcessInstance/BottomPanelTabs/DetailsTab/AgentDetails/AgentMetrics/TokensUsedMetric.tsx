1a9801c2b87df2ff15d8d2c4c279c11c743df14e
Christoph Fricke <christoph.fricke@holisticon.de>
2026-09-09 15:31:19 +0200

    fix: hide cache token breakdown column when all entries are hidden

--- operate/client/src/App/ProcessInstance/BottomPanelTabs/DetailsTab/AgentDetails/AgentMetrics/TokensUsedMetric.tsx
+++ operate/client/src/App/ProcessInstance/BottomPanelTabs/DetailsTab/AgentDetails/AgentMetrics/TokensUsedMetric.tsx
@@ -52,20 +52,22 @@ const TokensUsedMetric: React.FC<TokensUsedMetricProps> = ({
             </TokenBreakdown>
           )}
         </TokenBreakdownColumn>
-        <TokenBreakdownColumn>
-          {cacheReadTokens > 0 && (
-            <TokenBreakdown $dotColor="var(--cds-status-gray)">
-              <span>Cache read</span>
-              <span>{cacheReadTokens.toLocaleString()}</span>
-            </TokenBreakdown>
-          )}
-          {cacheCreationTokens > 0 && (
-            <TokenBreakdown $dotColor="var(--cds-status-gray)">
-              <span>Cache write</span>
-              <span>{cacheCreationTokens.toLocaleString()}</span>
-            </TokenBreakdown>
-          )}
-        </TokenBreakdownColumn>
+        {(cacheReadTokens > 0 || cacheCreationTokens > 0) && (
+          <TokenBreakdownColumn>
+            {cacheReadTokens > 0 && (
+              <TokenBreakdown $dotColor="var(--cds-status-gray)">
+                <span>Cache read</span>
+                <span>{cacheReadTokens.toLocaleString()}</span>
+              </TokenBreakdown>
+            )}
+            {cacheCreationTokens > 0 && (
+              <TokenBreakdown $dotColor="var(--cds-status-gray)">
+                <span>Cache write</span>
+                <span>{cacheCreationTokens.toLocaleString()}</span>
+              </TokenBreakdown>
+            )}
+          </TokenBreakdownColumn>
+        )}
       </TokenBreakdownContainer>
     </MetricCard>
   );
