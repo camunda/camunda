b2c27794bd99e0464721f823e739ae91010a77f3
Mehdi Riahi <mehdi.riahi@camunda.com>
2026-09-10 16:34:50 +0200

    feat: reject exporters that share an ILM/ISM lifecycle policy

--- configuration/src/main/java/io/camunda/configuration/beanoverrides/BrokerBasedPropertiesOverride.java
+++ configuration/src/main/java/io/camunda/configuration/beanoverrides/BrokerBasedPropertiesOverride.java
@@ -1306,7 +1306,7 @@ public class BrokerBasedPropertiesOverride {
           override.getExporters().put(name, newCfg);
         });
 
-    ExporterIsolationValidation.validate(override.getExporters());
+    ExporterIsolationValidation.validate(override.getExporters(), camunda);
   }
 
   private static void warnReservedRdbmsExporter(final String genericProperty) {
