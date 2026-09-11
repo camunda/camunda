e4f7fc52f773d6150f8e684c8c88a1bf5edfa53f
Christoph Fricke <christoph.fricke@holisticon.de>
2026-09-11 16:55:21 +0200

    refactor: duplicate global-routes configuration
    (cherry picked from commit 260fd268648ea3a1b786be6ea08d5c5d65d9c469)

---
+++ identity/client/src/components/globalV2/routePaths.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+export const Paths = {
+  setup() {
+    return "/setup";
+  },
+  login() {
+    return "/login";
+  },
+  forbidden() {
+    return "/forbidden";
+  },
+  mappingRules() {
+    return "/mapping-rules";
+  },
+  users() {
+    return "/users";
+  },
+  groups() {
+    return "/groups";
+  },
+  roles() {
+    return "/roles";
+  },
+  tenants() {
+    return "/tenants";
+  },
+  authorizations() {
+    return "/authorizations";
+  },
+  clusterVariables() {
+    return "/cluster-variables";
+  },
+  operationsLog() {
+    return "/operations-log";
+  },
+  mcpProcesses() {
+    return "/mcp-processes";
+  },
+  globalTaskListeners() {
+    return "/global-task-listeners";
+  },
+} as const;
