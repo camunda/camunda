6288076180cca3183f0a4b49eaac70735a3bf74b
Christoph Fricke <christoph.fricke@holisticon.de>
2026-09-11 13:20:39 +0200

    refactor: split carbon from new design-system on app root

---
+++ identity/client/src/components/layoutV2/LateLoading.tsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright Camunda Services GmbH and/or licensed to Camunda Services GmbH under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Licensed under the Camunda License 1.0. You may not use this file
+ * except in compliance with the Camunda License 1.0.
+ */
+
+import { FC, useEffect, useState } from "react";
+import useDebounce from "react-debounced";
+import { Loader2 } from "@camunda/design-system/icons";
+
+type LateLoadingProps = {
+  timeout?: number;
+};
+
+const LateLoading: FC<LateLoadingProps> = ({ timeout = 300 }) => {
+  const debounce = useDebounce(timeout);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    debounce(() => setVisible(true));
+  }, [debounce]);
+
+  if (visible) {
+    return (
+      <Loader2
+        aria-label="Loading"
+        role="status"
+        className="size-11 animate-spin text-primary-action-default"
+      />
+    );
+  }
+
+  return null;
+};
+
+export default LateLoading;
