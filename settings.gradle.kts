6150305a100a838b47f5289ccb62c770e87d806f
Carlo Sana <carlo.sana@camunda.com>
2026-09-15 11:16:46 +0200

    build: add missing versions pinned from maven's pom

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -122,6 +122,7 @@ dependencyResolutionManagement {
       // Versions when not coming from pomVersion have a comment explaining
       // where it's coming from in the maven build.
       version("agrona", pomVersion("version.agrona"))
+      version("animal-sniffer", pomVersion("version.animal-sniffer"))
       version("asm", pomVersion("version.asm"))
       version("archunit", pomVersion("version.archunit"))
       // used in camunda-spring-boot-starter and camunda-spring-boot-3-starter test dependencies
@@ -344,6 +345,50 @@ dependencyResolutionManagement {
       version("wiremock-spring-boot", pomVersion("version.wiremock-spring-boot"))
       version("zeebe-compat", pomVersion("backwards.compat.version"))
       version("zeebe-test-container", pomVersion("version.zeebe-test-container"))
+
+      // Keep all dependency and tool version properties from parent/pom.xml pinned in the Gradle
+      // catalog. Maven-only plugin versions are intentionally excluded.
+      version("parent-auth0-commons", pomVersion("version.auth0.commons"))
+      version("parent-auth0-jwt", pomVersion("version.auth0.jwt"))
+      version("parent-checkstyle", pomVersion("version.checkstyle"))
+      version("parent-commons-beanutils", pomVersion("version.commons-beanutils"))
+      version("parent-conscrypt", pomVersion("version.conscrypt"))
+      version("parent-elasticsearch-container", pomVersion("version.elasticsearch.container"))
+      version(
+        "parent-elasticsearch-test-container",
+        pomVersion("version.elasticsearch-test-container"),
+      )
+      version("parent-findbugs-annotations", pomVersion("version.findbugs-annotations"))
+      version("parent-google-http-client", pomVersion("version.google-http-client"))
+      version("parent-guava-annotations", pomVersion("version.guava.annotations"))
+      version("parent-hdr-histogram", pomVersion("version.hdr-histogram"))
+      version("parent-jakarta-activation", pomVersion("version.jakarta-activation"))
+      version("parent-jakarta-json-api", pomVersion("version.jakarta.json-api"))
+      version("parent-jopt-simple", pomVersion("version.jopt-simple"))
+      version("parent-json-smart", pomVersion("version.json-smart"))
+      version("parent-jsr305", pomVersion("version.jsr305"))
+      version("parent-lz4", pomVersion("version.lz4"))
+      version("parent-model", pomVersion("version.model"))
+      version(
+        "parent-mybatis-spring-boot-starter",
+        pomVersion("version.mybatis-spring-boot-starter"),
+      )
+      version("parent-node", pomVersion("version.node"))
+      version("parent-npm", pomVersion("version.npm"))
+      version("parent-opensearch-container", pomVersion("version.opensearch.container"))
+      version("parent-osgi", pomVersion("version.osgi"))
+      version(
+        "parent-postgres-test-container",
+        pomVersion("version.postgres-test-container"),
+      )
+      version(
+        "parent-postgressql-testcontainer",
+        pomVersion("version.postgressql-testcontainer"),
+      )
+      version("parent-revapi", pomVersion("version.revapi"))
+      version("parent-servlet-api", pomVersion("version.servlet-api"))
+      version("parent-yarn", pomVersion("version.yarn"))
+
       version("optimize-zeebe-docker", optimizePomVersion("zeebe.docker.version"))
       version("optimize-database-type", optimizePomVersion("database.type"))
 
@@ -930,6 +975,12 @@ dependencyResolutionManagement {
         .versionRef("org-camunda-feel-feel-engine")
       library("org-checkerframework-checker-qual", "org.checkerframework", "checker-qual")
         .versionRef("org-checkerframework-checker-qual")
+      library(
+          "org-codehaus-mojo-animal-sniffer-annotations",
+          "org.codehaus.mojo",
+          "animal-sniffer-annotations",
+        )
+        .versionRef("animal-sniffer")
       library("org-codehaus-janino-janino", "org.codehaus.janino", "janino").withoutVersion()
       // version managed by buildlogic.optimize-conventions
       library("org-eclipse-angus-jakarta-mail", "org.eclipse.angus", "jakarta.mail")
