a1323c547bb73c8f48db6589cda2a71af0e4c422
megglos <sebastian.bathke@camunda.com>
2026-09-22 17:09:30 +0200

    fix: default core Java client SaaS REST address to api subdomain
    Align CamundaClientCloudBuilderImpl and the deprecated ZeebeClientCloudBuilderImpl with the Spring starter default: the cloud REST address now targets the 'api' subdomain instead of the legacy 'zeebe' one, which is removed with 8.10. gRPC address and OAuth audience keep the 'zeebe' subdomain.
    
    relates to #63815
    
    (cherry picked from commit ead9491ea5b6a4c6a8a34529df0d1610a58040ec)

--- clients/java/src/test/java/io/camunda/client/CamundaClientTest.java
+++ clients/java/src/test/java/io/camunda/client/CamundaClientTest.java
@@ -431,7 +431,7 @@ public final class CamundaClientTest {
           .hasPort(443)
           .hasScheme("https");
       assertThat(clientConfiguration.getRestAddress())
-          .hasHost(String.format("%s.zeebe.%s", region, domain))
+          .hasHost(String.format("%s.api.%s", region, domain))
           .hasPort(443)
           .hasPath("/" + clusterId)
           .hasScheme("https");
@@ -458,7 +458,7 @@ public final class CamundaClientTest {
           .hasPort(443)
           .hasScheme("https");
       assertThat(clientConfiguration.getRestAddress())
-          .hasHost("bru-2.zeebe.camunda.io")
+          .hasHost("bru-2.api.camunda.io")
           .hasPort(443)
           .hasPath("/" + clusterId)
           .hasScheme("https");
@@ -512,7 +512,7 @@ public final class CamundaClientTest {
           .hasPort(443)
           .hasScheme("https");
       assertThat(clientConfiguration.getRestAddress())
-          .hasHost(String.format("%s.zeebe.camunda.io", region))
+          .hasHost(String.format("%s.api.camunda.io", region))
           .hasPort(443)
           .hasPath("/clusterId")
           .hasScheme("https");
@@ -539,7 +539,7 @@ public final class CamundaClientTest {
           .hasPort(443)
           .hasScheme("https");
       assertThat(clientConfiguration.getRestAddress())
-          .hasHost(String.format("%s.zeebe.camunda.io", defaultRegion))
+          .hasHost(String.format("%s.api.camunda.io", defaultRegion))
           .hasPort(443)
           .hasPath("/clusterId")
           .hasScheme("https");
